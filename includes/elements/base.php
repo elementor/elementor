f6a0a1e78b10824796cda2bab6275d8bff2a6d97
Yakir Sitbon <kingyes1@gmail.com>
2016-05-29 14:45:54 +0300

    Fixes condition with numbers in keys

--- includes/elements/base.php
+++ includes/elements/base.php
@@ -198,7 +198,7 @@ abstract class Element_Base {
 		}
 
 		foreach ( $control['condition'] as $condition_key => $condition_value ) {
-			preg_match( '/([a-z_]+)(?:\[([a-z_]+)])?(!?)$/i', $condition_key, $condition_key_parts );
+			preg_match( '/([a-z_0-9]+)(?:\[([a-z_]+)])?(!?)$/i', $condition_key, $condition_key_parts );
 
 			$pure_condition_key = $condition_key_parts[1];
 			$condition_sub_key = $condition_key_parts[2];
