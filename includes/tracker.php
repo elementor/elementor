368dcc0f8e4ae4bd41c137a0ed279999744daa38
Yakir Sitbon <kingyes1@gmail.com>
2016-06-05 13:42:34 +0300

    Show tracker notice after one hour from installed time

--- includes/tracker.php
+++ includes/tracker.php
@@ -110,8 +110,8 @@ class Tracker {
 	}
 
 	public static function admin_notices() {
-		// Show tracker notice after one day from installed time.
-		if ( self::_get_installed_time() > strtotime( '-1 day' ) )
+		// Show tracker notice after one hour from installed time.
+		if ( self::_get_installed_time() > strtotime( '-1 hour' ) )
 			return;
 
 		if ( '1' === get_option( 'elementor_tracker_notice' ) )
