b5af0ecc012e718ffb5c0920904b8f14f855c65b
Yakir Sitbon <kingyes1@gmail.com>
2016-06-07 16:52:14 +0300

    Fixes text domain

--- includes/controls/structure.php
+++ includes/controls/structure.php
@@ -17,7 +17,7 @@ class Control_Structure extends Control_Base {
 				<div class="elementor-control-structure-preset elementor-control-structure-current-preset">
 					<%= elementor.presetsFactory.getPresetSVG( currentPreset.preset, 240, 80, 5 ).outerHTML %>
 				</div>
-				<div class="elementor-control-structure-reset"><i class="fa fa-repeat"></i> <?php _e( 'Reset Structure' ); ?></div>
+				<div class="elementor-control-structure-reset"><i class="fa fa-repeat"></i> <?php _e( 'Reset Structure', 'elementor' ); ?></div>
 				<%
 				var morePresets = getMorePresets();
 
