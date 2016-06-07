52edd6e79fd7fb6df909655d5ed577ecc8af4587
Yakir Sitbon <kingyes1@gmail.com>
2016-06-07 18:12:43 +0300

    Added comment

--- bin/install-phpcs.sh
+++ bin/install-phpcs.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# Install CodeSniffer for WordPress Coding Standards checks.
+
 pear config-set auto_discover 1
 pear install PHP_CodeSniffer
 git clone git://github.com/WordPress-Coding-Standards/WordPress-Coding-Standards.git $(pear config-get php_dir)/PHP/CodeSniffer/Standards/WordPress
