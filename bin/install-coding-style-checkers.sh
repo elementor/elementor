f23e83211a86d12427142cb98f08eba79edfb86e
Yakir Sitbon <kingyes1@gmail.com>
2016-06-07 18:27:26 +0300

    Added jshint & jscs checkers in travis

--- bin/install-coding-style-checkers.sh
+++ bin/install-coding-style-checkers.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash
 
 # Install CodeSniffer for WordPress Coding Standards checks.
-
 pear config-set auto_discover 1
 pear install PHP_CodeSniffer
 git clone git://github.com/WordPress-Coding-Standards/WordPress-Coding-Standards.git $(pear config-get php_dir)/PHP/CodeSniffer/Standards/WordPress
 phpenv rehash
 phpcs --config-set installed_paths $(pear config-get php_dir)/PHP/CodeSniffer/Standards/WordPress
 phpcs -i
+
+# Install JSCS: JavaScript Code Style checker
+# @link http://jscs.info/
+npm install -g jscs
+
+# Install JSHint, a JavaScript Code Quality Tool
+# @link http://jshint.com/docs/
+npm install -g jshint
