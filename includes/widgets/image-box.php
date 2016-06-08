eb5311679a9ccbdd2d13ff3aa039228a8713a03a
Kobi Zaltzberg <kobi@pojo.me>
2016-06-08 19:03:18 +0300

    Moved control vertical align

--- includes/widgets/image-box.php
+++ includes/widgets/image-box.php
@@ -224,23 +224,6 @@ class Widget_Image_box extends Widget_Base {
 		);
 
 		$this->add_control(
-			'content_vertical_alignment',
-			[
-				'label' => __( 'Content Vertical Alignment', 'elementor' ),
-				'type' => Controls_Manager::SELECT,
-				'options' => [
-					'top' => __( 'Top', 'elementor' ),
-					'middle' => __( 'Middle', 'elementor' ),
-					'bottom' => __( 'Bottom', 'elementor' ),
-				],
-				'default' => 'top',
-				'section' => 'section_style_image',
-				'tab' => self::TAB_STYLE,
-				'prefix_class' => 'elementor-vertical-align-',
-			]
-		);
-
-		$this->add_control(
 			'section_style_content',
 			[
 				'type'  => Controls_Manager::SECTION,
@@ -277,6 +260,23 @@ class Widget_Image_box extends Widget_Base {
 				'selectors' => [
 					'{{WRAPPER}} .elementor-image-box-wrapper .elementor-image-box-content' => 'text-align: {{VALUE}};',
 				],
+			]
+		);
+
+		$this->add_control(
+			'content_vertical_alignment',
+			[
+				'label' => __( 'Vertical Alignment', 'elementor' ),
+				'type' => Controls_Manager::SELECT,
+				'options' => [
+					'top' => __( 'Top', 'elementor' ),
+					'middle' => __( 'Middle', 'elementor' ),
+					'bottom' => __( 'Bottom', 'elementor' ),
+				],
+				'default' => 'top',
+				'section' => 'section_style_content',
+				'tab' => self::TAB_STYLE,
+				'prefix_class' => 'elementor-vertical-align-',
 			]
 		);
 
