2b69d053c64cb9f82d9ff9f13b7364bcd1b29990
yehuda <yehuda@myinbox.in>
2016-06-13 14:46:04 +0300

    fix progressbar

--- assets/js/frontend.js
+++ assets/js/frontend.js
@@ -80,12 +80,13 @@
 			var $progressbar = $( this ),
 				max = parseInt( $progressbar.data( 'max' ), 10 ),
 				$inner = $progressbar.next(),
+				$innerTextWrap = $inner.find( '.elementor-progress-text' ),
 				$percent = $inner.find( '.elementor-progress-percentage' ),
 				innerText = $inner.data( 'inner' ) ? $inner.data( 'inner' ) : '';
 
 			$progressbar.css( 'width', max + '%' );
 			$inner.css( 'width', max + '%' );
-			$inner.text( innerText + '' );
+			$innerTextWrap.html( innerText + '' );
 			$percent.html(  max + '%' );
 
 		}, { offset: '90%' } );
