38385f8769cb2b11a70b2c68c8b1e4265697a991
Kobi Zaltzberg <kobi@pojo.me>
2016-06-15 18:19:19 +0300

    Use built-in functions

--- includes/controls/groups/image-size.php
+++ includes/controls/groups/image-size.php
@@ -21,19 +21,15 @@ class Group_Control_Image_size extends Group_Control_Base {
 
 		$args = $this->get_args();
 
-		if ( ! empty( $args['include'] ) ) {
-			$args['exclude'] = [];
+		if ( $args['include'] ) {
+			$wp_image_sizes = array_intersect_key( $wp_image_sizes, $args['include'] );
+		} elseif ( $args['exclude'] ) {
+			$wp_image_sizes = array_diff_key( $wp_image_sizes, $args['exclude'] );
 		}
 
 		$image_sizes = [];
 
 		foreach ( $wp_image_sizes as $image_size ) {
-			if ( ! empty( $args['include'] ) && ! in_array( $image_size, $args['include'] ) )
-				continue;
-
-			if ( ! empty( $args['exclude'] ) && in_array( $image_size, $args['exclude'] ) )
-				continue;
-
 			$image_sizes[ $image_size ] = ucwords( str_replace( '_', ' ', $image_size ) );
 		}
 
