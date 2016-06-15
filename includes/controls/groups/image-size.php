33ab46f01f289c74117fc8f923b36492c29626e0
Kobi Zaltzberg <kobi@pojo.me>
2016-06-15 17:57:49 +0300

    Spaces

--- includes/controls/groups/image-size.php
+++ includes/controls/groups/image-size.php
@@ -18,12 +18,15 @@ class Group_Control_Image_size extends Group_Control_Base {
 
 	private function _get_image_sizes() {
 		$wp_image_sizes = get_intermediate_image_sizes();
+
 		$args = $this->get_args();
+
 		if ( ! empty( $args['include'] ) ) {
 			$args['exclude'] = [];
 		}
 
 		$image_sizes = [];
+
 		foreach ( $wp_image_sizes as $image_size ) {
 			if ( ! empty( $args['include'] ) && ! in_array( $image_size, $args['include'] ) )
 				continue;
@@ -51,6 +54,7 @@ class Group_Control_Image_size extends Group_Control_Base {
 		// Get the first item for default value
 		$default_value = array_keys( $image_sizes );
 		$default_value = array_shift( $default_value );
+
 		$controls['size'] = [
 			'label' => _x( 'Image Size', 'Image Size Control', 'elementor' ),
 			'type' => Controls_Manager::SELECT,
