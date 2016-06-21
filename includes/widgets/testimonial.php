3db884c145d022c34cd777c01bd0d01d33464479
yehuda <yehuda@myinbox.in>
2016-06-21 16:37:36 +0300

    testimonial widget

--- includes/widgets/testimonial.php
+++ includes/widgets/testimonial.php
@@ -3,26 +3,26 @@ namespace Elementor;
 
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
-class Widget_Testimonials extends Widget_Base {
+class Widget_testimonial extends Widget_Base {
 	private $_carusel_options = [];
 
 	public function get_id() {
-		return 'testimonials';
+		return 'testimonial';
 	}
 
 	public function get_title() {
-		return __( 'Testimonials', 'elementor' );
+		return __( 'testimonial', 'elementor' );
 	}
 
 	public function get_icon() {
-		return 'comment-o';
+		return 'fa-comment-o';
 	}
 
 	protected function _register_controls() {
 		$this->add_control(
-			'section_testimonials',
+			'section_testimonial',
 			[
-				'label' => __( 'Testimonials', 'elementor' ),
+				'label' => __( 'testimonial', 'elementor' ),
 				'type' => Controls_Manager::SECTION,
 			]
 		);
@@ -35,7 +35,7 @@ class Widget_Testimonials extends Widget_Base {
 				'default' => [
 					'url' => Utils::get_placeholder_image_src(),
 				],
-				'section' => 'section_testimonials',
+				'section' => 'section_testimonial',
 			]
 		);
 
@@ -43,37 +43,7 @@ class Widget_Testimonials extends Widget_Base {
 			Group_Control_Image_size::get_type(),
 			[
 				'name' => 'thumbnail',
-				'section' => 'section_testimonials',
-			]
-		);
-
-		$this->add_control(
-			'testimonial_full_name',
-			[
-				'label' => __( 'Full Name', 'elementor' ),
-				'type' => Controls_Manager::TEXT,
-				'default' => 'John Doe',
-				'section' => 'section_testimonials',
-			]
-		);
-
-		$this->add_control(
-			'testimonial_job_title',
-			[
-				'label' => __( 'Job Title', 'elementor' ),
-				'type' => Controls_Manager::TEXT,
-				'default' => 'Elementor Lover',
-				'section' => 'section_testimonials',
-			]
-		);
-
-		$this->add_control(
-			'testimonial_company',
-			[
-				'label' => __( 'Job Company', 'elementor' ),
-				'type' => Controls_Manager::TEXT,
-				'default' => 'Elementor',
-				'section' => 'section_testimonials',
+				'section' => 'section_testimonial',
 			]
 		);
 
@@ -83,7 +53,78 @@ class Widget_Testimonials extends Widget_Base {
 				'label' => __( 'Testimonial Text', 'elementor' ),
 				'type' => Controls_Manager::TEXTAREA,
 				'default' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sed magna in purus luctus ornare sit amet eu risus. Ut tincidunt nec velit at fringilla. Fusce sodales vestibulum leo vel faucibus. Ut convallis vel elit et aliquam. Morbi dolor arcu, egestas quis justo at, pharetra dignissim justo.',
-				'section' => 'section_testimonials',
+				'section' => 'section_testimonial',
+			]
+		);
+
+		$this->add_control(
+			'testimonial_full_name',
+			[
+				'label' => __( 'Full Name', 'elementor' ),
+				'type' => Controls_Manager::TEXT,
+				'default' => 'John Doe',
+				'section' => 'section_testimonial',
+			]
+		);
+
+		$this->add_control(
+			'testimonial_job_title',
+			[
+				'label' => __( 'Job Title', 'elementor' ),
+				'type' => Controls_Manager::TEXT,
+				'default' => 'Elementor Lover',
+				'section' => 'section_testimonial',
+			]
+		);
+
+		$this->add_control(
+			'testimonial_company',
+			[
+				'label' => __( 'Job Company', 'elementor' ),
+				'type' => Controls_Manager::TEXT,
+				'default' => 'Elementor',
+				'section' => 'section_testimonial',
+			]
+		);
+
+		$this->add_control(
+			'testimonial_company_url',
+			[
+				'label' => __( 'Job Company URL', 'elementor' ),
+				'type' => Controls_Manager::URL,
+				'placeholder' => 'http://your-link.com',
+				'default' => [
+					'url' => '#',
+				],
+				'section' => 'section_testimonial',
+			]
+		);
+
+		$this->add_control(
+			'testimonial_detailes_position',
+			[
+				'label' => __( 'Testimonial Detailes Position', 'elementor' ),
+				'type' => Controls_Manager::CHOOSE,
+				'default' => 'Elementor',
+				'section' => 'section_testimonial',
+				'options' => [
+					'left'    => [
+						'title' => __( 'Left', 'elementor' ),
+						'icon' => 'align-left',
+					],
+					'center' => [
+						'title' => __( 'Center', 'elementor' ),
+						'icon' => 'align-center',
+					],
+					'right' => [
+						'title' => __( 'Right', 'elementor' ),
+						'icon' => 'align-right',
+					],
+					'justify' => [
+						'title' => __( 'Justified', 'elementor' ),
+						'icon' => 'align-justify',
+					],
+				],
 			]
 		);
 
@@ -196,7 +237,6 @@ class Widget_Testimonials extends Widget_Base {
 			[
 				'name' => 'job_typography',
 				'label' => __( 'Typography', 'elementor' ),
-				'scheme' => Scheme_Typography::TYPOGRAPHY_4,
 				'tab' => self::TAB_STYLE,
 				'section' => 'section_style_testimonial_job',
 				'selector' => '{{WRAPPER}} .elementor-testimonial-job-title',
@@ -232,7 +272,6 @@ class Widget_Testimonials extends Widget_Base {
 			[
 				'name' => 'company_typography',
 				'label' => __( 'Typography', 'elementor' ),
-				'scheme' => Scheme_Typography::TYPOGRAPHY_4,
 				'tab' => self::TAB_STYLE,
 				'section' => 'section_style_testimonial_company',
 				'selector' => '{{WRAPPER}} .elementor-testimonial-company',
@@ -283,7 +322,6 @@ class Widget_Testimonials extends Widget_Base {
 			[
 				'name' => 'content_typography',
 				'label' => __( 'Typography', 'elementor' ),
-				'scheme' => Scheme_Typography::TYPOGRAPHY_4,
 				'tab' => self::TAB_STYLE,
 				'section' => 'section_style_testimonial_text',
 				'selector' => '{{WRAPPER}} .elementor-testimonial-text',
@@ -296,36 +334,15 @@ class Widget_Testimonials extends Widget_Base {
 		if ( empty( $instance['testimonial_full_name'] ) || empty( $instance['testimonial_text'] ) )
 			return;
 
+		$has_image = false;
 		if ( '' !== $instance['testimonial_image']['id'] ) {
 			$image_url = Group_Control_Image_size::get_attachment_image_src( $instance['testimonial_image']['id'], 'thumbnail', $instance );
-		} else {
-			$image_url = $instance['testimonial_image']['url'];
+			$has_image = ' elementor-has-image';
 		}
+
+		$testimonial_detailes_position = $instance['testimonial_detailes_position'] ? ' elementor-testimonial-detailes-align-' . $instance['testimonial_detailes_position'] : '';
 		?>
 		<div class="elementor-testimonial-wrapper">
-			<div class="elementor-testimonial-image">
-				<figure>
-					<img src="<?php echo esc_attr( $image_url ); ?>" alt="testimonial" />
-				</figure>
-			</div>
-
-			<?php if ( ! empty( $instance['testimonial_full_name'] ) ) : ?>
-				<div class="elementor-testimonial-name">
-					<?php echo $instance['testimonial_full_name']; ?>
-				</div>
-			<?php endif; ?>
-
-			<?php if ( ! empty( $instance['testimonial_job_title'] ) ) : ?>
-				<span class="elementor-testimonial-job-title">
-					<?php echo $instance['testimonial_job_title']; ?>
-				</span>
-			<?php endif; ?>
-
-			<?php if ( ! empty( $instance['testimonial_company'] ) ) : ?>
-				<span class="elementor-testimonial-company">
-					- <?php echo $instance['testimonial_company']; ?>
-				</span>
-			<?php endif; ?>
 
 			<?php if ( ! empty( $instance['testimonial_text'] ) ) : ?>
 				<div class="elementor-testimonial-text">
@@ -340,6 +357,44 @@ class Widget_Testimonials extends Widget_Base {
 				<?php endif; ?>
 				</div>
 			<?php endif; ?>
+
+			<div class="testimonial_description<?php echo $testimonial_detailes_position; ?>">
+
+				<?php if ( isset( $image_url ) ) : ?>
+					<div class="elementor-testimonial-image">
+						<figure>
+							<img src="<?php echo esc_attr( $image_url ); ?>" alt="testimonial" />
+						</figure>
+					</div>
+				<?php endif; ?>
+
+				<div class="testimonial-detailes<?php if ( $has_image ) echo $has_image; ?>">
+					<?php if ( ! empty( $instance['testimonial_full_name'] ) ) : ?>
+						<div class="elementor-testimonial-name">
+							<?php echo $instance['testimonial_full_name']; ?>
+						</div>
+					<?php endif; ?>
+
+					<?php if ( ! empty( $instance['testimonial_job_title'] ) ) : ?>
+						<span class="elementor-testimonial-job-title">
+							<?php echo $instance['testimonial_job_title']; ?>
+						</span>
+					<?php endif; ?>
+
+					<?php if ( ! empty( $instance['testimonial_company'] ) ) : ?>
+						<span class="elementor-testimonial-company">
+							-
+							<?php if ( ! empty( $instance['testimonial_company_url']['url'] ) ) : ?>
+								<a href="<?php echo esc_url( $instance['testimonial_company_url']['url'] ); ?>" class="elementor-testimonial-company-url">
+							<?php endif; ?>
+								<?php echo $instance['testimonial_company']; ?>
+							<?php if ( ! empty( $instance['testimonial_company_url'] ) ) : ?>
+								</a>
+							<?php endif; ?>
+						</span>
+					<?php endif; ?>
+				</div>
+			</div>
 		</div>
 	<?php
 	}
