22cef408cb3f4b94511b891122e09f01aa0fa222
Kobi Zaltzberg <kobi@pojo.me>
2016-06-22 09:12:57 +0300

    Templates main JS file

---
+++ assets/admin/js/dev/components/templates/manager.js
@@ -0,0 +1,61 @@
+var ElementorTemplatesLayoutView = require( 'elementor-templates/views/layout' ),
+	ElementorTemplatesManager;
+
+ElementorTemplatesManager = function() {
+	var self = this,
+		modal,
+		layout;
+
+	this.init = function() {
+	};
+
+	this.getModal = function() {
+		if ( ! modal ) {
+			modal = elementor.modals.createModal( {
+				id: 'elementor-templates-modal',
+				contentWidth: 950,
+				contentHeight: 500,
+				closeButton: false
+			} );
+		}
+
+		return modal;
+	};
+
+	this.getLayout = function() {
+		if ( ! layout ) {
+			layout = new ElementorTemplatesLayoutView();
+		}
+
+		return layout;
+	};
+
+	this.requestRemoteTemplates = function( options ) {
+		var ajaxOptions = {
+			type: 'POST',
+			url: elementor.config.ajaxurl,
+			dataType: 'json',
+			data: {
+				action: 'elementor_template_get'
+			}
+		};
+
+		if ( options ) {
+			Backbone.$.extend( ajaxOptions, options );
+		}
+
+		Backbone.$.ajax( ajaxOptions );
+	};
+
+	this.startModal = function() {
+		self.getModal().show();
+
+		self.requestRemoteTemplates( {
+			success: function( data ) {
+				self.getLayout().showTemplates( data );
+			}
+		} );
+	};
+};
+
+module.exports = new ElementorTemplatesManager();
