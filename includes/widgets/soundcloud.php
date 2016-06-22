f1fbfc473ba8f6ec4f68f028203feaa992ef7d32
yehuda <yehuda@myinbox.in>
2016-06-22 12:02:11 +0300

    more options to soundcloud widget

--- includes/widgets/soundcloud.php
+++ includes/widgets/soundcloud.php
@@ -46,15 +46,6 @@ class Widget_Soundcloud extends Widget_Base {
 		);
 
 		$this->add_control(
-			'color',
-			[
-				'label' => __( 'Color', 'elementor' ),
-				'type' => Controls_Manager::COLOR,
-				'section' => 'section_sc_options',
-			]
-		);
-
-		$this->add_control(
 			'visual',
 			[
 				'label' => __( 'Visual Widget', 'elementor' ),
@@ -83,6 +74,63 @@ class Widget_Soundcloud extends Widget_Base {
 		);
 
 		$this->add_control(
+			'buying',
+			[
+				'label' => __( 'Show/hide buy buttons', 'elementor' ),
+				'type' => Controls_Manager::SELECT,
+				'default' => 'no',
+				'section' => 'section_sc_options',
+				'options' => [
+					'yes' => __( 'Yes', 'elementor' ),
+					'no' => __( 'No', 'elementor' ),
+				]
+			]
+		);
+
+		$this->add_control(
+			'liking',
+			[
+				'label' => __( 'Show/hide like buttons', 'elementor' ),
+				'type' => Controls_Manager::SELECT,
+				'default' => 'no',
+				'section' => 'section_sc_options',
+				'options' => [
+					'yes' => __( 'Yes', 'elementor' ),
+					'no' => __( 'No', 'elementor' ),
+				]
+			]
+		);
+
+		$this->add_control(
+			'download',
+			[
+				'label' => __( 'Show/hide download buttons', 'elementor' ),
+				'type' => Controls_Manager::SELECT,
+				'default' => 'no',
+				'section' => 'section_sc_options',
+				'options' => [
+					'yes' => __( 'Yes', 'elementor' ),
+					'no' => __( 'No', 'elementor' ),
+				]
+			]
+		);
+
+		$this->add_control(
+			'sharing',
+			[
+				'label' => __( 'Show/hide share buttons/dialogues', 'elementor' ),
+				'type' => Controls_Manager::SELECT,
+				'default' => 'no',
+				'section' => 'section_sc_options',
+				'options' => [
+					'yes' => __( 'Yes', 'elementor' ),
+					'no' => __( 'No', 'elementor' ),
+				]
+			]
+		);
+
+
+		$this->add_control(
 			'show_comments',
 			[
 				'label' => __( 'Show Comments', 'elementor' ),
@@ -93,6 +141,44 @@ class Widget_Soundcloud extends Widget_Base {
 					'yes' => __( 'Yes', 'elementor' ),
 					'no' => __( 'No', 'elementor' ),
 				]
+			]
+		);
+
+		$this->add_control(
+			'show_playcount',
+			[
+				'label' => __( 'Show/hide number of sound plays', 'elementor' ),
+				'type' => Controls_Manager::SELECT,
+				'default' => 'no',
+				'section' => 'section_sc_options',
+				'options' => [
+					'yes' => __( 'Yes', 'elementor' ),
+					'no' => __( 'No', 'elementor' ),
+				]
+			]
+		);
+
+		$this->add_control(
+			'show_user',
+			[
+				'label' => __( 'Show/hide the uploader name', 'elementor' ),
+				'type' => Controls_Manager::SELECT,
+				'default' => 'no',
+				'section' => 'section_sc_options',
+				'options' => [
+					'yes' => __( 'Yes', 'elementor' ),
+					'no' => __( 'No', 'elementor' ),
+				]
+			]
+		);
+
+		$this->add_control(
+			'color',
+			[
+				'label' => __( 'Color', 'elementor' ),
+				'type' => Controls_Manager::COLOR,
+				'section' => 'section_sc_options',
+				'tab' => self::TAB_STYLE,
 			]
 		);
 	}
@@ -119,17 +205,27 @@ class Widget_Soundcloud extends Widget_Base {
 
 		$params['url'] = $this->_current_instance['link']['url'];
 		$params['auto_play'] = 'yes' === $this->_current_instance['auto_play'] ? 'true' : 'false';
+		$params['buying'] = 'yes' === $this->_current_instance['buying'] ? 'true' : 'false';
+		$params['liking'] = 'yes' === $this->_current_instance['liking'] ? 'true' : 'false';
+		$params['download'] = 'yes' === $this->_current_instance['download'] ? 'true' : 'false';
+		$params['sharing'] = 'yes' === $this->_current_instance['sharing'] ? 'true' : 'false';
 		$params['show_comments'] = 'yes' === $this->_current_instance['show_comments'] ? 'true' : 'false';
+		$params['show_playcount'] = 'yes' === $this->_current_instance['show_playcount'] ? 'true' : 'false';
+		$params['show_user'] = 'yes' === $this->_current_instance['show_user'] ? 'true' : 'false';
+
 		$params['color'] = str_replace( '#', '', $this->_current_instance['color'] );
+		$visual = 'yes' === $this->_current_instance['visual'] ? 'true' : 'false';
 
 		if ( ! empty( $params ) ) {
 			preg_match( '/<iframe.*src=\"(.*)\".*><\/iframe>/isU', $html, $matches );
 			$url = esc_url( add_query_arg( $params, $matches[1] ) );
 
 			$html = str_replace( $matches[1], $url, $html );
-
-			$visual = 'yes' === $this->_current_instance['visual'] ? 'true' : 'false';
 			$html = str_replace( 'visual=true', 'visual=' . $visual, $html );
+
+			if ( 'false' === $visual ) {
+				$html = str_replace( 'height="400"', 'height="200"', $html );
+			}
 		}
 
 		return $html;
