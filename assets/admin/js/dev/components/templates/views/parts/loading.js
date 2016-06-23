03fa11f0cd1a0284aae99d9a2c416a1415ccc74f
Kobi Zaltzberg <kobi@pojo.me>
2016-06-23 16:19:39 +0300

    Loading view

---
+++ assets/admin/js/dev/components/templates/views/parts/loading.js
@@ -0,0 +1,9 @@
+var TemplatesLoadingView;
+
+TemplatesLoadingView = Marionette.ItemView.extend( {
+	id: 'elementor-templates-loading',
+
+	template: '#tmpl-elementor-templates-loading'
+} );
+
+module.exports = TemplatesLoadingView;
