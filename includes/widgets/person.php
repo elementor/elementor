0381e248529cb1a6184953548e31b21c7f805ff5
yehuda <yehuda@myinbox.in>
2016-06-30 14:13:57 +0300

    person initial stage

---
+++ includes/widgets/person.php
@@ -0,0 +1,230 @@
+<?php
+namespace Elementor;
+
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+class Widget_Image extends Widget_Base {
+
+	public function get_id() {
+		return 'person';
+	}
+
+	public function get_title() {
+		return __( 'Person', 'elementor' );
+	}
+
+	public function get_icon() {
+		return 'insert-image';
+	}
+
+	protected function _register_controls() {
+		$this->add_control(
+			'section_person',
+			[
+				'label' => __( 'Person Image', 'elementor' ),
+				'type' => Controls_Manager::SECTION,
+			]
+		);
+
+		$this->add_control(
+			'image',
+			[
+				'label' => __( 'Choose Image', 'elementor' ),
+				'type' => Controls_Manager::MEDIA,
+				'default' => [
+					'url' => Utils::get_placeholder_image_src(),
+				],
+				'section' => 'section_image',
+			]
+		);
+
+		$this->add_control(
+			'link',
+			[
+				'label' => 'Link to',
+				'type' => Controls_Manager::URL,
+				'placeholder' => __( 'http://your-link.com', 'elementor' ),
+				'section' => 'section_image',
+				'condition' => [
+					'link_to' => 'custom',
+				],
+				'show_label' => false,
+			]
+		);
+
+		$this->add_control(
+			'view',
+			[
+				'label' => __( 'View', 'elementor' ),
+				'type' => Controls_Manager::HIDDEN,
+				'default' => 'traditional',
+				'section' => 'section_image',
+			]
+		);
+
+		$this->add_control(
+			'section_style_image',
+			[
+				'type'  => Controls_Manager::SECTION,
+				'label' => __( 'Image', 'elementor' ),
+				'tab'   => self::TAB_STYLE,
+			]
+		);
+
+		$this->add_control(
+			'size',
+			[
+				'label' => __( 'Size (%)', 'elementor' ),
+				'type' => Controls_Manager::SLIDER,
+				'tab' => self::TAB_STYLE,
+				'section' => 'section_style_image',
+				'default' => [
+					'size' => 100,
+					'unit' => '%',
+				],
+				'size_units' => [ '%' ],
+				'range' => [
+					'%' => [
+						'min' => 1,
+						'max' => 100,
+					],
+				],
+				'selectors' => [
+					'{{WRAPPER}} .elementor-image img' => 'max-width: {{SIZE}}{{UNIT}};',
+				],
+			]
+		);
+
+		$this->add_control(
+			'opacity',
+			[
+				'label' => __( 'Opacity (%)', 'elementor' ),
+				'type' => Controls_Manager::SLIDER,
+				'tab' => self::TAB_STYLE,
+				'section' => 'section_style_image',
+				'default' => [
+					'size' => 1,
+				],
+				'range' => [
+					'px' => [
+						'max' => 1,
+						'min' => 0.10,
+						'step' => 0.01,
+					],
+				],
+				'selectors' => [
+					'{{WRAPPER}} .elementor-image img' => 'opacity: {{SIZE}};',
+				],
+			]
+		);
+
+		$this->add_control(
+			'section_style_caption',
+			[
+				'type'  => Controls_Manager::SECTION,
+				'label' => __( 'Caption', 'elementor' ),
+				'tab'   => self::TAB_STYLE,
+			]
+		);
+
+		$this->add_control(
+			'caption_align',
+			[
+				'label' => __( 'Alignment', 'elementor' ),
+				'type' => Controls_Manager::CHOOSE,
+				'options' => [
+					'left' => [
+						'title' => __( 'Left', 'elementor' ),
+						'icon' => 'align-left',
+					],
+					'center' => [
+						'title' => __( 'Center', 'elementor' ),
+						'icon' => 'align-center',
+					],
+					'right' => [
+						'title' => __( 'Right', 'elementor' ),
+						'icon' => 'align-right',
+					],
+					'justify' => [
+						'title' => __( 'Justified', 'elementor' ),
+						'icon' => 'align-justify',
+					],
+				],
+				'default' => '',
+				'selectors' => [
+					'{{WRAPPER}} .widget-image-caption' => 'text-align: {{VALUE}};',
+				],
+				'tab' => self::TAB_STYLE,
+				'section' => 'section_style_caption',
+			]
+		);
+
+		$this->add_control(
+			'text_color',
+			[
+				'label' => __( 'Text Color', 'elementor' ),
+				'type' => Controls_Manager::COLOR,
+				'tab' => self::TAB_STYLE,
+				'default' => '',
+				'selectors' => [
+					'{{WRAPPER}} .widget-image-caption' => 'color: {{VALUE}};',
+				],
+				'section' => 'section_style_caption',
+				'scheme' => [
+					'type' => Scheme_Color::get_type(),
+					'value' => Scheme_Color::COLOR_3,
+				],
+			]
+		);
+
+		$this->add_group_control(
+			Group_Control_Typography::get_type(),
+			[
+				'name' => 'caption_typography',
+				'selector' => '{{WRAPPER}} .widget-image-caption',
+				'tab' => self::TAB_STYLE,
+				'section' => 'section_style_caption',
+				'scheme' => Scheme_Typography::TYPOGRAPHY_3,
+			]
+		);
+	}
+
+	protected function render( $instance = [] ) {
+		if ( empty( $instance['image']['url'] ) ) {
+			return;
+		}
+		$has_caption = ! empty( $instance['caption'] );
+
+		$image_html = '<div class="elementor-image' . ( ! empty( $instance['shape'] ) ? ' elementor-image-shape-' . $instance['shape'] : '' ) . '">';
+
+		if ( $has_caption ) {
+			$image_html .= '<figure class="wp-caption">';
+		}
+
+		$image_class_html = ! empty( $instance['hover_animation'] ) ? ' class="hover-' . $instance['hover_animation'] . '"' : '';
+
+		$image_html .= sprintf( '<img src="%s" title="%s" alt="%s"%s />', esc_attr( $instance['image']['url'] ), $this->get_image_title( $instance ), $this->get_image_alt( $instance ), $image_class_html );
+
+		$link = $this->get_link_url( $instance );
+		if ( $link ) {
+			$target = '';
+			if ( ! empty( $link['is_external'] ) ) {
+				$target = ' target="_blank"';
+			}
+			$image_html = sprintf( '<a href="%s"%s>%s</a>', $link['url'], $target, $image_html );
+		}
+
+		if ( $has_caption ) {
+			$image_html .= sprintf( '<figcaption class="widget-image-caption wp-caption-text">%s</figcaption>', $instance['caption'] );
+		}
+
+		if ( $has_caption ) {
+			$image_html .= '</figure>';
+		}
+		$image_html .= '</div>';
+
+		echo $image_html;
+	}
+
+	protected function content_template() {}
+}
