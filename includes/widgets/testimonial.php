e43ce02ea693ed58d51f61e2731b6a69a21a5ed8
arielk <relklik@gmail.com>
2016-06-30 15:34:20 +0300

    [testimonial] replace icon

--- includes/widgets/testimonial.php
+++ includes/widgets/testimonial.php
@@ -14,7 +14,7 @@ class Widget_testimonial extends Widget_Base {
 	}
 
 	public function get_icon() {
-		return 'blockquote';
+		return 'testimonial';
 	}
 
 	protected function _register_controls() {
