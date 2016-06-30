3a5d2925251b7273ef48596926cd1d89dc7f26a7
yehuda <yehuda@myinbox.in>
2016-06-30 14:21:00 +0300

    person template

--- includes/widgets/person.php
+++ includes/widgets/person.php
@@ -3,7 +3,8 @@ namespace Elementor;
 
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
-class Widget_Image extends Widget_Base {
+class Widget_person extends Widget_Base {
+	private $_carusel_options = [];
 
 	public function get_id() {
 		return 'person';
@@ -14,126 +15,69 @@ class Widget_Image extends Widget_Base {
 	}
 
 	public function get_icon() {
-		return 'insert-image';
+		return 'blockquote';
 	}
 
 	protected function _register_controls() {
 		$this->add_control(
 			'section_person',
 			[
-				'label' => __( 'Person Image', 'elementor' ),
+				'label' => __( 'Person', 'elementor' ),
 				'type' => Controls_Manager::SECTION,
 			]
 		);
 
 		$this->add_control(
-			'image',
+			'person_image',
 			[
-				'label' => __( 'Choose Image', 'elementor' ),
+				'label' => __( 'Add Image', 'elementor' ),
 				'type' => Controls_Manager::MEDIA,
 				'default' => [
 					'url' => Utils::get_placeholder_image_src(),
 				],
-				'section' => 'section_image',
+				'section' => 'section_person',
 			]
 		);
 
 		$this->add_control(
-			'link',
+			'person_full_name',
 			[
-				'label' => 'Link to',
-				'type' => Controls_Manager::URL,
-				'placeholder' => __( 'http://your-link.com', 'elementor' ),
-				'section' => 'section_image',
-				'condition' => [
-					'link_to' => 'custom',
-				],
-				'show_label' => false,
+				'label' => __( 'Full Name', 'elementor' ),
+				'type' => Controls_Manager::TEXT,
+				'default' => 'John Doe',
+				'section' => 'section_person',
 			]
 		);
 
 		$this->add_control(
-			'view',
+			'person_job_title',
 			[
-				'label' => __( 'View', 'elementor' ),
-				'type' => Controls_Manager::HIDDEN,
-				'default' => 'traditional',
-				'section' => 'section_image',
+				'label' => __( 'Job Title', 'elementor' ),
+				'type' => Controls_Manager::TEXT,
+				'default' => 'Elementor Lover',
+				'section' => 'section_person',
 			]
 		);
 
 		$this->add_control(
-			'section_style_image',
+			'person_text',
 			[
-				'type'  => Controls_Manager::SECTION,
-				'label' => __( 'Image', 'elementor' ),
-				'tab'   => self::TAB_STYLE,
+				'label' => __( 'Person Text', 'elementor' ),
+				'type' => Controls_Manager::TEXTAREA,
+				'default' => 'Click edit button to change this text. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo..',
+				'section' => 'section_person',
 			]
 		);
 
 		$this->add_control(
-			'size',
+			'person_text_position',
 			[
-				'label' => __( 'Size (%)', 'elementor' ),
-				'type' => Controls_Manager::SLIDER,
-				'tab' => self::TAB_STYLE,
-				'section' => 'section_style_image',
-				'default' => [
-					'size' => 100,
-					'unit' => '%',
-				],
-				'size_units' => [ '%' ],
-				'range' => [
-					'%' => [
-						'min' => 1,
-						'max' => 100,
-					],
-				],
-				'selectors' => [
-					'{{WRAPPER}} .elementor-image img' => 'max-width: {{SIZE}}{{UNIT}};',
-				],
-			]
-		);
-
-		$this->add_control(
-			'opacity',
-			[
-				'label' => __( 'Opacity (%)', 'elementor' ),
-				'type' => Controls_Manager::SLIDER,
-				'tab' => self::TAB_STYLE,
-				'section' => 'section_style_image',
-				'default' => [
-					'size' => 1,
-				],
-				'range' => [
-					'px' => [
-						'max' => 1,
-						'min' => 0.10,
-						'step' => 0.01,
-					],
-				],
-				'selectors' => [
-					'{{WRAPPER}} .elementor-image img' => 'opacity: {{SIZE}};',
-				],
-			]
-		);
-
-		$this->add_control(
-			'section_style_caption',
-			[
-				'type'  => Controls_Manager::SECTION,
-				'label' => __( 'Caption', 'elementor' ),
-				'tab'   => self::TAB_STYLE,
-			]
-		);
-
-		$this->add_control(
-			'caption_align',
-			[
-				'label' => __( 'Alignment', 'elementor' ),
+				'label' => __( 'Details Position', 'elementor' ),
 				'type' => Controls_Manager::CHOOSE,
+				'default' => 'center',
+				'section' => 'section_person',
 				'options' => [
-					'left' => [
+					'left'    => [
 						'title' => __( 'Left', 'elementor' ),
 						'icon' => 'align-left',
 					],
@@ -150,29 +94,73 @@ class Widget_Image extends Widget_Base {
 						'icon' => 'align-justify',
 					],
 				],
-				'default' => '',
-				'selectors' => [
-					'{{WRAPPER}} .widget-image-caption' => 'text-align: {{VALUE}};',
-				],
-				'tab' => self::TAB_STYLE,
-				'section' => 'section_style_caption',
 			]
 		);
 
 		$this->add_control(
-			'text_color',
+			'view',
+			[
+				'label' => __( 'View', 'elementor' ),
+				'type' => Controls_Manager::HIDDEN,
+				'default' => 'traditional',
+				'section' => 'section_image_carousel',
+			]
+		);
+
+		// Image
+		$this->add_control(
+			'section_style_person_image',
+			[
+				'label' => __( 'Image', 'elementor' ),
+				'type' => Controls_Manager::SECTION,
+				'tab' => self::TAB_STYLE,
+			]
+		);
+
+		$this->add_group_control(
+			Group_Control_Border::get_type(),
+			[
+				'name' => 'image_border',
+				'tab' => self::TAB_STYLE,
+				'section' => 'section_style_person_image',
+				'selector' => '{{WRAPPER}} .elementor-person-wrapper .elementor-person-image img',
+			]
+		);
+
+		$this->add_control(
+			'image_border_radius',
+			[
+				'label' => __( 'Border Radius', 'elementor' ),
+				'type' => Controls_Manager::DIMENSIONS,
+				'size_units' => [ 'px', '%' ],
+				'tab' => self::TAB_STYLE,
+				'section' => 'section_style_person_image',
+				'selectors' => [
+					'{{WRAPPER}} .elementor-person-wrapper .elementor-person-image img' => 'border-radius: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
+				],
+			]
+		);
+
+		// Name
+		$this->add_control(
+			'section_style_person_name',
+			[
+				'label' => __( 'Name', 'elementor' ),
+				'type' => Controls_Manager::SECTION,
+				'tab' => self::TAB_STYLE,
+			]
+		);
+
+		$this->add_control(
+			'name_text_color',
 			[
 				'label' => __( 'Text Color', 'elementor' ),
 				'type' => Controls_Manager::COLOR,
 				'tab' => self::TAB_STYLE,
+				'section' => 'section_style_person_name',
 				'default' => '',
 				'selectors' => [
-					'{{WRAPPER}} .widget-image-caption' => 'color: {{VALUE}};',
-				],
-				'section' => 'section_style_caption',
-				'scheme' => [
-					'type' => Scheme_Color::get_type(),
-					'value' => Scheme_Color::COLOR_3,
+					'{{WRAPPER}} .elementor-person-name' => 'color: {{VALUE}};',
 				],
 			]
 		);
@@ -180,51 +168,145 @@ class Widget_Image extends Widget_Base {
 		$this->add_group_control(
 			Group_Control_Typography::get_type(),
 			[
-				'name' => 'caption_typography',
-				'selector' => '{{WRAPPER}} .widget-image-caption',
+				'name' => 'name_typography',
+				'label' => __( 'Typography', 'elementor' ),
+				'scheme' => Scheme_Typography::TYPOGRAPHY_4,
 				'tab' => self::TAB_STYLE,
-				'section' => 'section_style_caption',
-				'scheme' => Scheme_Typography::TYPOGRAPHY_3,
+				'section' => 'section_style_person_name',
+				'selector' => '{{WRAPPER}} .elementor-person-name',
+			]
+		);
+
+		$this->add_control(
+			'section_style_person_text',
+			[
+				'label' => __( 'Content', 'elementor' ),
+				'type' => Controls_Manager::SECTION,
+				'tab' => self::TAB_STYLE,
+			]
+		);
+
+		$this->add_control(
+			'content_text_color',
+			[
+				'label' => __( 'Text Color', 'elementor' ),
+				'type' => Controls_Manager::COLOR,
+				'tab' => self::TAB_STYLE,
+				'section' => 'section_style_person_text',
+				'default' => '',
+				'selectors' => [
+					'{{WRAPPER}} .elementor-person-text' => 'color: {{VALUE}};',
+				],
+			]
+		);
+
+		$this->add_group_control(
+			Group_Control_Typography::get_type(),
+			[
+				'name' => 'content_typography',
+				'label' => __( 'Typography', 'elementor' ),
+				'tab' => self::TAB_STYLE,
+				'section' => 'section_style_person_text',
+				'selector' => '{{WRAPPER}} .elementor-person-text',
 			]
 		);
 	}
 
 	protected function render( $instance = [] ) {
-		if ( empty( $instance['image']['url'] ) ) {
+		if ( empty( $instance['person_full_name'] ) || empty( $instance['person_text'] ) )
 			return;
-		}
-		$has_caption = ! empty( $instance['caption'] );
 
-		$image_html = '<div class="elementor-image' . ( ! empty( $instance['shape'] ) ? ' elementor-image-shape-' . $instance['shape'] : '' ) . '">';
-
-		if ( $has_caption ) {
-			$image_html .= '<figure class="wp-caption">';
+		$has_image = false;
+		if ( '' !== $instance['person_image']['url'] ) {
+			$image_url = $instance['person_image']['url'];
+			$has_image = ' elementor-has-image';
 		}
 
-		$image_class_html = ! empty( $instance['hover_animation'] ) ? ' class="hover-' . $instance['hover_animation'] . '"' : '';
+		$person_text_position = $instance['person_text_position'] ? ' elementor-person-text-align-' . $instance['person_text_position'] : '';
+		?>
+		<div class="elementor-person-wrapper<?php echo $person_text_position; ?>">
 
-		$image_html .= sprintf( '<img src="%s" title="%s" alt="%s"%s />', esc_attr( $instance['image']['url'] ), $this->get_image_title( $instance ), $this->get_image_alt( $instance ), $image_class_html );
+			<div class="person_description">
 
-		$link = $this->get_link_url( $instance );
-		if ( $link ) {
-			$target = '';
-			if ( ! empty( $link['is_external'] ) ) {
-				$target = ' target="_blank"';
-			}
-			$image_html = sprintf( '<a href="%s"%s>%s</a>', $link['url'], $target, $image_html );
-		}
+				<?php if ( isset( $image_url ) ) : ?>
+					<div class="elementor-person-image">
+						<figure>
+							<img src="<?php echo esc_attr( $image_url ); ?>" alt="person" />
+						</figure>
+					</div>
+				<?php endif; ?>
 
-		if ( $has_caption ) {
-			$image_html .= sprintf( '<figcaption class="widget-image-caption wp-caption-text">%s</figcaption>', $instance['caption'] );
-		}
+				<div class="person-detailes<?php if ( $has_image ) echo $has_image; ?>">
+					<?php if ( ! empty( $instance['person_full_name'] ) ) : ?>
+						<div class="elementor-person-name">
+							<?php echo $instance['person_full_name']; ?>
+						</div>
+					<?php endif; ?>
 
-		if ( $has_caption ) {
-			$image_html .= '</figure>';
-		}
-		$image_html .= '</div>';
+					<?php if ( ! empty( $instance['person_job_title'] ) ) : ?>
+						<span class="elementor-person-job-title">
+							<?php echo $instance['person_job_title']; ?>
+						</span>
+					<?php endif; ?>
 
-		echo $image_html;
+					<?php if ( ! empty( $instance['person_text'] ) ) : ?>
+						<div class="elementor-person-text">
+							<?php echo $instance['person_text']; ?>
+						</div>
+					<?php endif; ?>
+
+				</div>
+			</div>
+		</div>
+		<?php
 	}
 
-	protected function content_template() {}
+	protected function content_template() {
+		?>
+		<%
+		var imageUrl = false, hasImage = '';
+		if ( '' !== settings.person_image.url ) {
+		imageUrl = settings.person_image.url;
+		hasImage = ' elementor-has-image';
+		}
+
+		var person_text_position = settings.person_text_position ? ' elementor-person-text-align-' + settings.person_text_position : '';
+		%>
+		<div class="elementor-person-wrapper<%- person_text_position %>">
+
+			<div class="person_description">
+
+				<% if ( imageUrl ) { %>
+				<div class="elementor-person-image">
+					<figure>
+						<img src="<%- imageUrl %>" alt="person" />
+					</figure>
+				</div>
+				<% } %>
+
+				<div class="person-detailes<%- hasImage %>">
+
+					<% if ( '' !== settings.person_full_name ) { %>
+					<div class="elementor-person-name">
+						<%= settings.person_full_name %>
+					</div>
+					<% } %>
+
+					<% if ( '' !== settings.person_job_title ) { %>
+						<span class="elementor-person-job-title">
+							<%= settings.person_job_title %>
+						</span>
+					<% } %>
+
+					<% if ( '' !== settings.person_text ) { %>
+					<div class="elementor-person-text">
+						<%= settings.person_text %>
+					</div>
+					<% } %>
+
+				</div>
+			</div>
+		</div>
+		<?php
+	}
 }
