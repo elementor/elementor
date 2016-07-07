969c59c026cdc67963b242b38484520c932aa24d
Kobi Zaltzberg <kobi@pojo.me>
2016-07-07 18:53:09 +0300

    Added animations functionality to column

--- includes/elements/column.php
+++ includes/elements/column.php
@@ -193,6 +193,18 @@ class Element_Column extends Element_Base {
 		);
 
 		$this->add_control(
+			'animation',[
+				'label' => __( 'Animation', 'elementor' ),
+				'type' => Controls_Manager::SELECT,
+				'options' => self::get_animations(),
+				'default' => '',
+				'prefix_class' => 'animated ',
+				'tab' => self::TAB_ADVANCED,
+				'section' => 'section_advanced',
+			]
+		);
+
+		$this->add_control(
 			'css_classes',
 			[
 				'label' => __( 'CSS Classes', 'elementor' ),
@@ -354,16 +366,15 @@ class Element_Column extends Element_Base {
 	}
 
 	public function before_render( $instance, $element_id, $element_data = [] ) {
-		$wrapper_classes = [
+		$column_type = ! empty( $element_data['isInner'] ) ? 'inner' : 'top';
+
+		$this->add_render_attribute( 'wrapper', 'class', [
 			'elementor-column',
 			'elementor-element',
 			'elementor-element-' . $element_id,
 			'elementor-col-' . $instance['_column_size'],
-		];
-
-		$column_type = ! empty( $element_data['isInner'] ) ? 'inner' : 'top';
-
-		$wrapper_classes[] = 'elementor-' . $column_type . '-column';
+			'elementor-' . $column_type . '-column',
+		] );
 
 		foreach ( $this->get_class_controls() as $control ) {
 			if ( empty( $instance[ $control['name'] ] ) )
@@ -372,10 +383,16 @@ class Element_Column extends Element_Base {
 			if ( ! $this->is_control_visible( $instance, $control ) )
 				continue;
 
-			$wrapper_classes[] = $control['prefix_class'] . $instance[ $control['name'] ];
+			$this->add_render_attribute( 'wrapper', 'class', $control['prefix_class'] . $instance[ $control['name'] ] );
 		}
+
+		if ( $instance['animation'] ) {
+			$this->add_render_attribute( 'wrapper', 'data-animation', $instance['animation'] );
+		}
+
+		$this->add_render_attribute( 'wrapper', 'data-element_type', $this->get_id() );
 		?>
-		<div class="<?php echo esc_attr( implode( ' ', $wrapper_classes ) ); ?>" data-element_type="<?php echo $this->get_id(); ?>">
+		<div <?php echo $this->get_render_attribute_string( 'wrapper' ); ?>>
 			<div class="elementor-column-wrap<?php if ( ! empty( $element_data['elements'] ) ) echo ' elementor-element-populated'; ?>">
 				<div class="elementor-widget-wrap">
 		<?php
