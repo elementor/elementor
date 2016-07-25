06db845072d74699358d6ca96be38845cf834aac
Kobi Zaltzberg <kobi@pojo.me>
2016-07-25 18:50:00 +0300

    Compiled files

--- assets/admin/js/app.js
+++ assets/admin/js/app.js
@@ -5569,7 +5569,9 @@ SectionsCollectionView = Marionette.CompositeView.extend( {
 			elTopOffsetRange = sectionHandleHeight - elTopOffset;
 
 		if ( 0 < elTopOffsetRange ) {
-			this.$el.css( 'margin-top', elTopOffsetRange );
+			var $style = Backbone.$( '<style>' ).text( '.elementor-editor-active #elementor-inner{margin-top: ' + elTopOffsetRange + 'px}' );
+
+			elementor.$previewContents.children().children( 'head' ).append( $style );
 		}
 	},
 
