c67a2f6798644102e03538d3f5e14ffb4fccd0bb
Yakir Sitbon <kingyes1@gmail.com>
2016-07-27 08:46:00 +0300

    Added templates to admin menu

--- includes/templates/types/local.php
+++ includes/templates/types/local.php
@@ -4,6 +4,7 @@ namespace Elementor\Templates;
 use Elementor\Controls_Manager;
 use Elementor\DB;
 use Elementor\Plugin;
+use Elementor\Settings;
 use Elementor\Utils;
 
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
@@ -51,6 +52,18 @@ class Type_Local extends Type_Base {
 		register_post_type(
 			self::CPT,
 			apply_filters( 'elementor/templates/types/local/register_post_type_args', $args )
+		);
+
+		add_action( 'admin_menu', [ $this, 'register_admin_menu' ], 100 );
+	}
+
+	public function register_admin_menu() {
+		add_submenu_page(
+			Settings::PAGE_ID,
+			__( 'Templates', 'elementor' ),
+			__( 'Templates', 'elementor' ),
+			'edit_pages',
+			'edit.php?post_type=' . self::CPT
 		);
 	}
 
