40c8bbfa0a59d5966a8dbaa731c33c96434edc83
Kobi Zaltzberg <kobi@pojo.me>
2016-07-31 16:54:05 +0300

    Compiled files

--- assets/admin/js/app.js
+++ assets/admin/js/app.js
@@ -229,6 +229,7 @@ App = Marionette.Application.extend( {
 
 	onPreviewElNotFound: function() {
 		var dialog = this.dialogsManager.createWidget( 'confirm', {
+			id: 'elementor-fatal-error-dialog',
 			headerMessage: elementor.translate( 'preview_el_not_found_header' ),
 			message: elementor.translate( 'preview_el_not_found_message' ),
 			position: {
@@ -236,12 +237,16 @@ App = Marionette.Application.extend( {
 				at: 'center center'
 			},
             strings: {
-			    confirm: elementor.translate( 'close' ),
-                cancel: elementor.translate( 'learn_more' )
+			    confirm: elementor.translate( 'learn_more' ),
+                cancel: elementor.translate( 'go_back' )
             },
 			onConfirm: function() {
+				open( elementor.config.help_the_content_url, '_blank' );
+			},
+			onCancel: function() {
 				parent.history.go( -1 );
-			}
+			},
+			hideOnButtonClick: false
 		} );
 
 		dialog.show();
