b9403c962eb62716fe817b954ac8e58afc44ea89
Kobi Zaltzberg <kobi@pojo.me>
2016-08-07 11:07:00 +0300

    Prevent duplicate rendering on settings changed

--- assets/admin/js/dev/views/base-element.js
+++ assets/admin/js/dev/views/base-element.js
@@ -52,9 +52,7 @@ BaseElementView = Marionette.CompositeView.extend( {
 		this.listenTo( this.model.get( 'settings' ), 'change', this.onSettingsChanged, this );
 		this.listenTo( this.model.get( 'editSettings' ), 'change', this.onSettingsChanged, this );
 
-		this.on( 'render', this.enqueueFonts );
-		this.on( 'render', this.renderStyles );
-		this.on( 'render', this.renderCustomClasses );
+		this.on( 'render', this.renderUI );
 		this.on( 'render', this.runReadyTrigger );
 
 		this.initRemoveDialog();
@@ -182,6 +180,12 @@ BaseElementView = Marionette.CompositeView.extend( {
 		}, this ) );
 	},
 
+	renderUI: function() {
+		this.renderStyles();
+		this.renderCustomClasses();
+		this.enqueueFonts();
+	},
+
 	runReadyTrigger: function() {
 		_.defer( _.bind( function() {
 			elementorBindUI.runReadyTrigger( this.$el );
@@ -202,10 +206,6 @@ BaseElementView = Marionette.CompositeView.extend( {
 			elementor.setFlagEditorChange( true );
 		}
 
-        this.renderStyles();
-		this.renderCustomClasses();
-		this.enqueueFonts();
-
 		// Make sure is correct model
 		if ( settings instanceof BaseSettingsModel ) {
 			var isContentChanged = false;
@@ -217,6 +217,7 @@ BaseElementView = Marionette.CompositeView.extend( {
 			} );
 
 			if ( ! isContentChanged ) {
+				this.renderUI();
 				return;
 			}
 		}
