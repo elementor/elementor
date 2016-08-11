63621af55b972abd7afc431b4ed31d782502a9a7
Yakir Sitbon <kingyes1@gmail.com>
2016-08-11 19:57:19 +0300

    fix

--- elementor/assets/admin/js/lib/waypoints-for-editor.js
+++
@@ -1,7 +0,0 @@
-(function( $ ) {
-	$.fn.waypoint = function(callback) {
-		if ( typeof callback === 'function' ) {
-			callback.call( this );
-		}
-	};
-})( jQuery );
--- elementor/includes/interfaces/group-control.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace Elementor;
-
-if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
-
-interface Group_Control_Interface {
-	public static function get_type();
-}
