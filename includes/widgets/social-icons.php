fcdec0921b3d43a086e492f74bd307334e84c069
Kobi Zaltzberg <kobi@pojo.me>
2016-08-16 15:06:33 +0300

    Always print href

--- includes/widgets/social-icons.php
+++ includes/widgets/social-icons.php
@@ -293,10 +293,10 @@ class Widget_Social_Icons extends Widget_Base {
 		<div class="elementor-social-icons-wrapper">
 			<?php foreach ( $instance['social_icon_list'] as $item ) :
 				$social = str_replace( 'fa fa-', '', $item['social'] );
-				$link = ! empty( $item['link']['url'] ) ? ' href="' . $item['link']['url'] . '""' : '';
+				$link = ! empty( $item['link']['url'] ) ? $item['link']['url'] : '';
 				$target = $item['link']['is_external'] ? ' target="_blank"' : '';
 				?>
-				<a class="elementor-icon elementor-social-icon elementor-social-icon-<?php echo esc_attr( $social ); ?>"<?php echo $link . $target; ?>>
+				<a class="elementor-icon elementor-social-icon elementor-social-icon-<?php echo esc_attr( $social ); ?>" href="<?php echo $link; ?>"<?php echo $target; ?>>
 					<i class="<?php echo $item['social']; ?>"></i>
 				</a>
 			<?php endforeach; ?>
