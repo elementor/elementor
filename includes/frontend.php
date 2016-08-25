67be56f2931858f4ab70786bbcc76b7c63262a95
Frank <frank@iMac.local>
2016-08-25 14:23:51 +0300

    action to register new scripts and css
    This adds the ability to add more css and javascript in the editor
    window since Elementor runs a new instance of  WP_Styles and WP_Scripts

--- includes/frontend.php
+++ includes/frontend.php
@@ -122,6 +122,9 @@ class Frontend {
 			true
 		);
 		wp_enqueue_script( 'elementor-frontend' );
+
+		do_action( 'elementor/frontend/scripts', Plugin::instance() );
+
 	}
 
 	public function enqueue_styles() {
@@ -163,6 +166,9 @@ class Frontend {
 
 		wp_enqueue_style( 'elementor-animations' );
 		wp_enqueue_style( 'elementor-frontend' );
+
+		do_action( 'elementor/frontend/styles', Plugin::instance() );
+
 	}
 
 	public function print_css() {
