3d0c1566c68fc2c6800519509741226516da573b
Yakir Sitbon <kingyes1@gmail.com>
2016-09-11 18:00:29 +0300

    Closed #539 - Remove quick link to Edit with Elementor in non-elementor pages

--- includes/frontend.php
+++ includes/frontend.php
@@ -370,7 +370,9 @@ class Frontend {
 
 	function add_menu_in_admin_bar( \WP_Admin_Bar $wp_admin_bar ) {
 		$post_id = get_the_ID();
-		if ( ! is_singular() || ! User::is_current_user_can_edit( $post_id ) ) {
+		$is_not_builder_mode = ! is_singular() || ! User::is_current_user_can_edit( $post_id ) || 'builder' !== Plugin::instance()->db->get_edit_mode( $post_id );
+
+		if ( $is_not_builder_mode ) {
 			return;
 		}
 
