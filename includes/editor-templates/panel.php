c0ead35bda0e250a3d56f40069e5d3c5308a0972
Kobi Zaltzberg <kobi@pojo.me>
2016-09-28 19:39:39 +0300

    Fixed string parsing

--- includes/editor-templates/panel.php
+++ includes/editor-templates/panel.php
@@ -149,7 +149,7 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 <script type="text/template" id="tmpl-elementor-panel-schemes-disabled">
 	<i class="eicon-nerd"></i>
 	<div id="elementor-panel-schemes-disabled-title">{{{ '<?php echo __( '{0} are disabled', 'elementor' ); ?>'.replace( '{0}', disabledTitle ) }}}</div>
-	<div id="elementor-panel-schemes-disabled-content"><?php echo __( 'You can enable it from the <a href="' . Settings::get_url() . '" target="_blank">Elementor settings page</a>.', 'elementor' ); ?></div>
+	<div id="elementor-panel-schemes-disabled-content"><?php printf( __( 'You can enable it from the <a href="%s" target="_blank">Elementor settings page</a>.', Settings::get_url() ), 'elementor' ); ?></div>
 </script>
 
 <script type="text/template" id="tmpl-elementor-panel-scheme-color-item">
