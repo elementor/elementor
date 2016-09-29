eeb7f992c5d247e8f9b7f4f535e533349e465b76
Kobi Zaltzberg <kobi@pojo.me>
2016-09-29 19:30:26 +0300

    Enqueue typography fonts on preview loaded. Fix #606

--- assets/js/editor.js
+++ assets/js/editor.js
@@ -1427,6 +1427,8 @@ App = Marionette.Application.extend( {
 			elementorFrontend.getScopeWindow().jQuery.holdReady( false );
 		} );
 
+		this.enqueueTypographyFonts();
+
 		//this.introduction.startOnLoadIntroduction(); // TEMP Removed
 
 		this.trigger( 'preview:loaded' );
@@ -1573,6 +1575,15 @@ App = Marionette.Application.extend( {
 			.reply( 'previousMode', oldDeviceMode )
 			.reply( 'currentMode', newDeviceMode )
 			.trigger( 'change' );
+	},
+
+	enqueueTypographyFonts: function() {
+		var self = this,
+			typographyScheme = this.schemes.getScheme( 'typography' );
+
+		_.each( typographyScheme.items, function( item ) {
+			self.helpers.enqueueFont( item.value.font_family );
+		} );
 	},
 
 	translate: function( stringKey, templateArgs ) {
