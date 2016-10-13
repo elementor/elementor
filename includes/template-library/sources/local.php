bb1d967ba995d322e65c1d55f26b1cc1fcb9ae78
Mati Horovitz <mati@pojo.me>
2016-10-13 14:29:42 +0300

    export / import: local & remote fixes

--- includes/template-library/sources/local.php
+++ includes/template-library/sources/local.php
@@ -174,6 +174,8 @@ class Source_Local extends Source_Base {
 			$data = Plugin::instance()->db->get_plain_editor( $item_id );
 		}
 
+		$data = $this->replace_elements_ids( $data );
+
 		return $data;
 	}
 
@@ -183,12 +185,6 @@ class Source_Local extends Source_Base {
 
 	public function export_template( $item_id ) {
 		$template_data = $this->get_content( $item_id, 'raw' );
-
-		$template_data = Plugin::instance()->db->iterate_data( $template_data, function( $element ) {
-			$element['id'] = Utils::generate_random_string();
-
-			return $element;
-		});
 
 		$template_data = $this->process_export_import_data( $template_data, 'on_export' );
 
@@ -322,47 +318,5 @@ class Source_Local extends Source_Base {
 		}
 
 		add_action( 'template_redirect', [ $this, 'block_template_frontend' ] );
-	}
-
-	/**
-	 * @param array $data a set of elements
-	 * @param string $method (on_export|on_import)
-	 *
-	 * @return mixed
-	 */
-	private function process_export_import_data( $data, $method ) {
-
-		return Plugin::instance()->db->iterate_data( $data, function( $element ) use ( $method ) {
-
-			if ( 'widget' === $element['elType'] ) {
-				$element_class = Plugin::instance()->widgets_manager->get_widget_types( $element['widgetType'] );
-			} else {
-				$element_class = Plugin::instance()->elements_manager->get_element_types( $element['elType'] );
-			}
-
-			/* if the widget/element isn't exist, like a plugin that creates a widget but deactivated */
-			if ( ! $element_class ) {
-				return $element;
-			}
-
-			if ( method_exists( $element_class, $method ) ) {
-				$element = $element_class->{$method}( $element );
-			}
-
-			foreach ( $element_class->get_controls() as $control ) {
-				$control_class = Plugin::instance()->controls_manager->get_control( $control['type'] );
-
-				/* if the control isn't exist, like a plugin that creates the control but deactivated */
-				if ( ! $control_class ) {
-					return $element;
-				}
-
-				if ( method_exists( $control_class, $method ) ) {
-					$element['settings'][ $control['name'] ] = $control_class->{$method}( $element['settings'][ $control['name'] ] );
-				}
-			}
-
-			return $element;
-		} );
 	}
 }
