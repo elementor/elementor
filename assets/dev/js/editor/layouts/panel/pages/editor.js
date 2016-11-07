6401814d60fbd12f9b1c08b9de83dc1697b0c4bd
Mati Horovitz <mati@pojo.me>
2016-11-07 17:45:21 +0200

    inner-tabs: move js to a behavior

--- assets/dev/js/editor/layouts/panel/pages/editor.js
+++ assets/dev/js/editor/layouts/panel/pages/editor.js
@@ -11,6 +11,12 @@ EditorCompositeView = Marionette.CompositeView.extend( {
 		};
 	},
 
+	behaviors: {
+		HandleInnerTabs: {
+			behaviorClass: require( 'elementor-behaviors/inner-tabs' )
+		}
+	},
+
 	childViewContainer: '#elementor-controls',
 
 	modelEvents: {
@@ -89,8 +95,6 @@ EditorCompositeView = Marionette.CompositeView.extend( {
 				return this.getAttribute( 'data-tooltip' );
 			}
 		} );
-
-		this.handleInnerTabs( this );
 	},
 
 	onModelDestroy: function() {
@@ -182,68 +186,6 @@ EditorCompositeView = Marionette.CompositeView.extend( {
 			}
 
 			view.$el.addClass( openedClass );
-		} );
-
-		elementor.channels.data.trigger( 'scrollbar:update' );
-	},
-
-	handleInnerTabs: function ( parent ) {
-		var closedClass = 'elementor-tab-close',
-			activeClass = 'elementor-tab-active',
-			tabsWrappers = parent.children.filter( function( view ) {
-				return ( view.model.get( 'is_tabs_wrapper' ) );
-			} );
-
-		_.each( tabsWrappers, function( view ) {
-			view.$el.addClass( 'type-tabs' );
-
-			var tabs_id = view.model.get('name'),
-				tabs = parent.children.filter( function( childView ) {
-					return ( childView.model.get( 'type' ) === 'tab' && childView.model.get( 'tabs_wrapper' ) === tabs_id );
-				} );
-
-			_.each( tabs, function( childView, index ) {
-				view._addChildView( childView );
-
-				var tab_id =  childView.model.get( 'name' ),
-					controlsUnderTab = parent.children.filter( function( view ) {
-						return ( view.model.get( 'inner_tab' ) === tab_id );
-					} );
-
-				if ( 0 === index ) {
-					childView.$el.addClass( activeClass );
-				} else {
-					_.each( controlsUnderTab, function( view ) {
-						view.$el.addClass( closedClass );
-					} );
-				}
-			} );
-		} );
-	},
-
-	onChildviewControlTabClicked: function ( childView ) {
-		var closedClass = 'elementor-tab-close',
-			activeClass = 'elementor-tab-active',
-			tabClicked = childView.model.get( 'name' ),
-			childrenUnderTab = this.children.filter( function( view ) {
-				return ( 'tab' !== view.model.get( 'type' ) && childView.model.get( 'tabs_wrapper' ) === view.model.get( 'tabs_wrapper' ) );
-			} ),
-			siblingTabs = this.children.filter( function( view ) {
-			return ( 'tab' === view.model.get( 'type' ) && childView.model.get( 'tabs_wrapper' ) === view.model.get( 'tabs_wrapper' ) );
-		} );
-
-		childView.$el.addClass( activeClass );
-
-		_.each( siblingTabs, function( view ) {
-			view.$el.removeClass(activeClass);
-		} );
-
-		_.each( childrenUnderTab, function( view ) {
-			if ( view.model.get( 'inner_tab' ) === tabClicked ) {
-				view.$el.removeClass( closedClass );
-			} else {
-				view.$el.addClass( closedClass );
-			}
 		} );
 
 		elementor.channels.data.trigger( 'scrollbar:update' );
