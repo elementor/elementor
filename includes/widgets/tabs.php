ada0521cd904797b1fa5331aaedad0682e39ee5e
Yakir Sitbon <kingyes1@gmail.com>
2016-11-09 19:35:00 +0200

    Remove item id class for now

--- includes/widgets/tabs.php
+++ includes/widgets/tabs.php
@@ -223,7 +223,7 @@ class Widget_Tabs extends Widget_Base {
 			<?php $counter = 1; ?>
 			<div class="elementor-tabs-content-wrapper">
 				<?php foreach ( $tabs as $item ) : ?>
-					<div class="elementor-tab-content elementor-repeater-item-<?php echo $item['_id']; ?>" data-tab="<?php echo $counter; ?>"><?php echo $this->parse_text_editor( $item['tab_content'] ); ?></div>
+					<div class="elementor-tab-content" data-tab="<?php echo $counter; ?>"><?php echo $this->parse_text_editor( $item['tab_content'] ); ?></div>
 				<?php
 					$counter++;
 				endforeach; ?>
