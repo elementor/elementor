ec4ab3d1c371dd4c663b5b1d30234091d5f6027a
Yakir Sitbon <kingyes1@gmail.com>
2016-11-13 19:37:09 +0200

    merged

--- assets/js/editor.js
+++ assets/js/editor.js
@@ -102,7 +102,7 @@ InnerTabsBehavior = Marionette.Behavior.extend( {
 		this.handleInnerTabs( this.view );
 	},
 
-	handleInnerTabs: function ( parent ) {
+	handleInnerTabs: function( parent ) {
 		var closedClass = 'elementor-tab-close',
 			activeClass = 'elementor-tab-active',
 			tabsWrappers = parent.children.filter( function( view ) {
@@ -112,17 +112,17 @@ InnerTabsBehavior = Marionette.Behavior.extend( {
 		_.each( tabsWrappers, function( view ) {
 			view.$el.addClass( 'type-tabs' );
 
-			var tabs_id = view.model.get('name'),
+			var tabsId = view.model.get( 'name' ),
 				tabs = parent.children.filter( function( childView ) {
-					return ( childView.model.get( 'type' ) === 'tab' && childView.model.get( 'tabs_wrapper' ) === tabs_id );
+					return ( 'tab' === childView.model.get( 'type' ) && childView.model.get( 'tabs_wrapper' ) === tabsId );
 				} );
 
 			_.each( tabs, function( childView, index ) {
 				view._addChildView( childView );
 
-				var tab_id =  childView.model.get( 'name' ),
+				var tabId =  childView.model.get( 'name' ),
 					controlsUnderTab = parent.children.filter( function( view ) {
-						return ( view.model.get( 'inner_tab' ) === tab_id );
+						return ( tabId === view.model.get( 'inner_tab' ) );
 					} );
 
 				if ( 0 === index ) {
@@ -136,10 +136,11 @@ InnerTabsBehavior = Marionette.Behavior.extend( {
 		} );
 	},
 
-	onChildviewControlTabClicked: function ( childView ) {
+	onChildviewControlTabClicked: function( childView ) {
 		var closedClass = 'elementor-tab-close',
 			activeClass = 'elementor-tab-active',
 			tabClicked = childView.model.get( 'name' ),
+
 			childrenUnderTab = this.view.children.filter( function( view ) {
 				return ( 'tab' !== view.model.get( 'type' ) && childView.model.get( 'tabs_wrapper' ) === view.model.get( 'tabs_wrapper' ) );
 			} ),
