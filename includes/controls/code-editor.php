21e8c48b9de5643fdcdf1936b7a25388c89156ed
Mati Horovitz <mati@pojo.me>
2016-11-15 18:43:21 +0200

    code-editor: fixes

--- includes/controls/code-editor.php
+++ includes/controls/code-editor.php
@@ -21,7 +21,7 @@ class Control_Code_Editor extends Control_Base {
 		<div class="elementor-control-field">
 			<label class="elementor-control-title">{{{ data.label }}}</label>
 			<div class="elementor-control-input-wrapper">
-				<pre style="height: 160px;" class="elementor-input-style elementor-code-editor" data-setting="{{ data.name }}"></pre>
+				<pre class="elementor-input-style elementor-code-editor" data-setting="{{ data.name }}"></pre>
 			</div>
 		</div>
 		<# if ( data.description ) { #>
