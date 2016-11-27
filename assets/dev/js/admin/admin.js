1431f858652051a25920663648c0fae74565688f
Kobi Zaltzberg <kobi@pojo.me>
2016-11-27 17:02:57 +0200

    Improved caching location

--- assets/dev/js/admin/admin.js
+++ assets/dev/js/admin/admin.js
@@ -11,7 +11,9 @@
 				$switchModeInput: $( '#elementor-switch-mode-input' ),
 				$switchModeButton: $( '#elementor-switch-mode-button' ),
 				$elementorLoader: $( '.elementor-loader' ),
-				$builderEditor: $( '#elementor-editor' )
+				$builderEditor: $( '#elementor-editor' ),
+				$importButton: $( '#elementor-import-template-trigger' ),
+				$importArea: $( '#elementor-import-template-area' )
 			};
 		},
 
@@ -110,14 +112,14 @@
 			}
 
 			var self = this,
-				$importButton = self.cache.$importButton = $( '#elementor-import-template-trigger' ),
-				$importArea = self.cache.$importArea = $( '#elementor-import-template-area' );
+				$importButton = self.cache.$importButton,
+				$importArea = self.cache.$importArea;
 
 			self.cache.$formAnchor = $( 'h1' );
 
 			$( '#wpbody-content' ).find( '.page-title-action' ).after( $importButton );
 
-			self.cache.$formAnchor.after( self.cache.$importArea );
+			self.cache.$formAnchor.after( $importArea );
 
 			$importButton.on( 'click', function() {
 				$( '#elementor-import-template-area' ).toggle();
