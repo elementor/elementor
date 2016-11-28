8275808281c824f958894e44df206ad8e2b68e64
Mati Horovitz <mati@pojo.me>
2016-11-28 15:12:15 +0200

    panel.js: add new action panel/open_editor/' + elementData.elType

--- assets/js/editor.js
+++ assets/js/editor.js
@@ -3177,8 +3177,12 @@ PanelLayoutView = Marionette.LayoutView.extend( {
 			editedElementView: view
 		} );
 
+		// Example: panel/open_editor/widget
+		var action = 'panel/open_editor/' + elementData.elType;
+			elementorFrontend.hooks.doAction( action, this, model, view );
+
 		// Example: panel/open_editor/widget/slides
-		var action = 'panel/open_editor/' + elementData.elType + '/' + elementData.widget_type;
+		action = 'panel/open_editor/' + elementData.elType + '/' + elementData.widget_type;
 		elementorFrontend.hooks.doAction( action, this, model, view );
 	},
 
