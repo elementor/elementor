5f120a45b1ffd3a98e5ea802a8e846651bb7723d
Kobi Zaltzberg <kobi@pojo.me>
2016-12-01 18:24:19 +0200

    Call the parent initialize method*

--- assets/js/editor.js
+++ assets/js/editor.js
@@ -7397,6 +7397,7 @@ ControlRepeaterItemView = ControlBaseItemView.extend( {
 	},
 
 	onRender: function() {
+		ControlBaseItemView.prototype.onRender.apply( this, arguments );
 		this.ui.fieldContainer.sortable( { axis: 'y', handle: '.elementor-repeater-row-tools' } );
 
 		this.toggleMinRowsClass();
