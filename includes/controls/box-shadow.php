47193bd56e164f4abc42b616603af808e7084367
Mati Horovitz <mati@pojo.me>
2016-12-07 14:20:56 +0200

    docs

--- includes/controls/box-shadow.php
+++ includes/controls/box-shadow.php
@@ -17,7 +17,6 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
  *                           }
  *
  * @since                    1.0.0
- *
  */
 class Control_Box_Shadow extends Control_Base_Multiple {
 
