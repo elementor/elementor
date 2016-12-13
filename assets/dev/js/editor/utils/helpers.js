2600ebec856f45168e1f62704c8a2d9cc44cfd3e
Mati Horovitz <mati@pojo.me>
2016-12-13 18:18:22 +0200

    helpers.js: condition: ability to compare with an empty array - PHP

--- assets/dev/js/editor/utils/helpers.js
+++ assets/dev/js/editor/utils/helpers.js
@@ -136,7 +136,7 @@ helpers = {
 			}
 
 			/* If it's a non empty array - check if the conditionValue contains the controlValue,
-			 otherwise check if they are equal and give the ability to check if the value is an empty array */
+			 otherwise check if they are equal. ( and give the ability to check if the value is an empty array ) */
 			var isContains = ( _.isArray( conditionValue ) && ! _.isEmpty( conditionValue ) ) ? _.contains( conditionValue, controlValue ) : _.isEqual( conditionValue, controlValue );
 
 			return isNegativeCondition ? isContains : ! isContains;
