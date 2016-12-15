7c11d3766c26c48c442f2893fb725ed8ebe0cbb0
Mati Horovitz <mati@pojo.me>
2016-12-15 15:03:32 +0200

    revisions: progress

--- assets/dev/js/editor/revisions/revision-view.js
+++ assets/dev/js/editor/revisions/revision-view.js
@@ -6,34 +6,7 @@ module.exports =  Marionette.ItemView.extend( {
 		spinner: '.elementor-state-icon'
 	},
 
-	events: {
-		'click @ui.item': 'onItemClick'
-	},
-
-	onItemClick: function() {
-		var self = this,
-			id = this.model.get( 'id' );
-
-		Backbone.$( '.elementor-revision-current-preview' ).removeClass( 'elementor-revision-current-preview' );
-		self.ui.item.addClass( 'elementor-state-show' );
-
-		elementor.ajax.send( 'get_revision_preview', {
-			data: {
-				id: id
-			},
-			success: function( data ) {
-				var collection = elementor.getRegion( 'sections' ).currentView.collection;
-				collection.reset();
-				collection.set( data );
-
-				self.triggerMethod( 'preview:loaded' );
-
-				self.ui.item.removeClass( 'elementor-state-show' ).addClass( 'elementor-revision-current-preview' );
-			},
-			error: function( data ) {
-				alert( 'An error occurs' );
-				self.ui.previewButton.removeClass( 'elementor-state-show' );
-			}
-		} );
-	},
+	triggers: {
+		'click @ui.item': 'item:click'
+	}
 } );
--- assets/dev/js/editor/revisions/revisions-page.js
+++ assets/dev/js/editor/revisions/revisions-page.js
@@ -21,6 +21,10 @@ module.exports = Marionette.CompositeView.extend( {
 
 	isRevisionApplied: false,
 
+	jqueryXhr: null,
+
+	currentPreviewId: null,
+
 	initialize: function() {
 		this.listenTo( elementor.channels.editor, 'editor:changed', this.setApplyButtonState );
 	},
@@ -33,21 +37,60 @@ module.exports = Marionette.CompositeView.extend( {
 		this.ui.discard.prop( 'disabled', disabled );
 	},
 
+	setEditorData: function( data ) {
+		var collection = elementor.getRegion( 'sections' ).currentView.collection;
+		collection.reset();
+		collection.set( data );
+	},
+
 	onApplyClick: function() {
 		elementor.getPanelView().getChildView( 'footer' )._publishBuilder();
 		this.isRevisionApplied = true;
 		this.setDiscardButtonDisabled( false );
 	},
 
-	onChildviewPreviewLoaded: function() {
-		this.setDiscardButtonDisabled( false );
+	onChildviewItemClick: function( childView ) {
+		var self = this,
+			id = childView.model.get( 'id' );
+
+		if ( id === self.currentPreviewId ) {
+			return;
+		}
+
+		if ( this.jqueryXhr ) {
+			this.jqueryXhr.abort();
+		}
+
+		childView.ui.item.addClass( 'elementor-state-show' );
+
+		this.jqueryXhr = elementor.ajax.send( 'get_revision_preview', {
+			data: {
+				id: id
+			},
+			success: function( data ) {
+				self.setEditorData( data );
+				self.setDiscardButtonDisabled( false );
+
+				self.currentPreviewId = id;
+				self.jqueryXhr = null;
+
+				Backbone.$( '.elementor-revision-current-preview' ).removeClass( 'elementor-revision-current-preview' );
+				childView.ui.item.removeClass( 'elementor-state-show' ).addClass( 'elementor-revision-current-preview' );
+			},
+			error: function( data ) {
+				childView.ui.item.removeClass( 'elementor-state-show elementor-revision-current-preview' );
+				if ( 'abort' === self.jqueryXhr.statusText ) {
+					return;
+				}
+
+				this.currentPreviewId = null;
+				alert( 'An error occurs' );
+			}
+		} );
 	},
 
 	onDiscardClick: function() {
-		var collection = elementor.getRegion( 'sections' ).currentView.collection;
-		collection.reset();
-		collection.set( elementor.config.data );
-
+		this.setEditorData( elementor.config.data );
 		elementor.setFlagEditorChange( this.isRevisionApplied );
 
 		if ( this.isRevisionApplied ) {
@@ -56,6 +99,7 @@ module.exports = Marionette.CompositeView.extend( {
 
 		this.setDiscardButtonDisabled( true );
 
+		this.currentPreviewId = null;
 		Backbone.$( '.elementor-revision-current-preview' ).removeClass( 'elementor-revision-current-preview' );
 	}
 } );
