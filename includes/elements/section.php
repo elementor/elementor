672f9e66efc1a5a59fd129f3a7c960015165b0d9
Josh Marom <josh@pojo.me>
2016-12-18 13:54:14 +0200

    VH in section height - fix

--- includes/elements/section.php
+++ includes/elements/section.php
@@ -217,16 +217,16 @@ class Element_Section extends Element_Base {
 					'size' => 400,
 				],
 				'range' => [
-					'vh' => [
-						'min' => 0,
-						'max' => 100,
-					],
-					'px' => [
+				    'px' => [
 						'min' => 0,
 						'max' => 1440,
 					],
+					'vh' => [
+						'min' => 0,
+						'max' => 100,
+				    ],
 				],
-				'units' => [ 'vh', 'px' ],
+				'size_units' => [ 'px', 'vh' ],
 				'selectors' => [
 					'{{WRAPPER}} > .elementor-container' => 'min-height: {{SIZE}}{{UNIT}};',
 				],
