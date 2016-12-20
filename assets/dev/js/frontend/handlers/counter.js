73a31bf36b27f76ac7da38f6cad628defb7959d1
Kobi Zaltzberg <kobi@pojo.me>
2016-12-20 12:04:57 +0200

    Added support for floating numbers

--- assets/dev/js/frontend/handlers/counter.js
+++ assets/dev/js/frontend/handlers/counter.js
@@ -1,7 +1,14 @@
 module.exports = function( $scope, $ ) {
 	elementorFrontend.utils.waypoint( $scope.find( '.elementor-counter-number' ), function() {
-		var $number = $( this );
+		var $number = $( this ),
+			data = $number.data();
 
-		$number.numerator( $number.data() );
+		var decimalDigits = data.toValue.toString().match( /\.(.*)/ );
+
+		if ( decimalDigits ) {
+			data.rounding = decimalDigits[1].length;
+		}
+
+		$number.numerator( data );
 	}, { offset: '90%' } );
 };
--- assets/js/frontend.js
+++ assets/js/frontend.js
@@ -220,9 +220,16 @@ module.exports = function( $scope, $ ) {
 },{}],5:[function(require,module,exports){
 module.exports = function( $scope, $ ) {
 	elementorFrontend.utils.waypoint( $scope.find( '.elementor-counter-number' ), function() {
-		var $number = $( this );
+		var $number = $( this ),
+			data = $number.data();
 
-		$number.numerator( $number.data() );
+		var decimalDigits = data.toValue.toString().match( /\.(.*)/ );
+
+		if ( decimalDigits ) {
+			data.rounding = decimalDigits[1].length;
+		}
+
+		$number.numerator( data );
 	}, { offset: '90%' } );
 };
 
