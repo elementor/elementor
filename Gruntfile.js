6568baa8fe64f20bffc19a45feca0237cbec1932
Yakir Sitbon <kingyes1@gmail.com>
2016-12-25 16:55:51 +0200

    Exclude docs folder from release

--- Gruntfile.js
+++ Gruntfile.js
@@ -39,6 +39,7 @@ module.exports = function( grunt ) {
 				files: [ {
 					src: [
 						'**/*.php',
+						'!docs/**',
 						'!node_modules/**',
 						'!build/**',
 						'!tests/**',
@@ -332,6 +333,7 @@ module.exports = function( grunt ) {
 				src: [
 					'**',
 					'!node_modules/**',
+					'!docs/**',
 					'!build/**',
 					'!bin/**',
 					'!.git/**',
