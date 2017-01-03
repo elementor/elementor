3e79eab7cf2855f5d8f1171c5b8120a5de2cf086
Mati Horovitz <mati@pojo.me>
2017-01-03 19:53:56 +0200

    upgrades: fixes

--- includes/upgrades.php
+++ includes/upgrades.php
@@ -12,29 +12,39 @@ class Upgrades {
 	public static function init() {
 		$elementor_version = get_option( 'elementor_version' );
 
+		// Normal init
 		if ( ELEMENTOR_VERSION === $elementor_version ) {
 			return;
 		}
 
-		if ( $elementor_version ) {
-			if ( version_compare( $elementor_version, '0.3.2', '<' ) ) {
-				self::_upgrade_v032();
-			}
+		self::check_upgrades( $elementor_version );
 
-			if ( version_compare( $elementor_version, '0.9.2', '<' ) ) {
-				self::_upgrade_v092();
-			}
-
-			if ( version_compare( $elementor_version, '0.11.0', '<' ) ) {
-				self::_upgrade_v0110();
-			}
-
-			if ( version_compare( $elementor_version, ELEMENTOR_VERSION, '<' ) ) {
-				Plugin::instance()->posts_css_manager->clear_cache();
-			}
-		}
+		Plugin::instance()->posts_css_manager->clear_cache();
 
 		update_option( 'elementor_version', ELEMENTOR_VERSION );
+	}
+
+	private static function check_upgrades( $elementor_version ) {
+		// It's a new install
+		if ( ! $elementor_version ) {
+			return;
+		}
+
+		$elementor_upgrades = get_option( 'elementor_upgrades', [] );
+
+		$upgrades = [
+			'0.3.2'  => '_upgrade_v032',
+			'0.9.2'  => '_upgrade_v092',
+			'0.11.0' => '_upgrade_v0110',
+		];
+
+		foreach ( $upgrades as $version => $function ) {
+			if ( version_compare( $elementor_version, $version, '<' ) && ! isset( $elementor_upgrades[ $version ] ) ) {
+				self::$function();
+				$elementor_upgrades[ $version ] = true;
+				update_option( 'elementor_upgrades', $elementor_upgrades );
+			}
+		}
 	}
 
 	private static function _upgrade_v032() {
