f2e9b24135e9bc96a4e493500d118c29d0eb3dfe
Kobi Zaltzberg <kobi@pojo.me>
2017-01-05 18:08:04 +0200

    Compiled files

--- assets/js/editor.js
+++ assets/js/editor.js
@@ -591,8 +591,14 @@ module.exports = Marionette.CompositeView.extend( {
 
 	currentPreviewId: null,
 
+	currentPreviewItem: null,
+
 	initialize: function() {
 		this.listenTo( elementor.channels.editor, 'saved', this.onEditorSaved );
+
+		this.navigate = _.bind( this.navigate, this );
+
+		this.bindHotKeys();
 	},
 
 	setRevisionsButtonsActive: function( active ) {
@@ -650,6 +656,42 @@ module.exports = Marionette.CompositeView.extend( {
 		elementor.changeEditMode( 'edit' );
 	},
 
+	navigate: function( event ) {
+		if ( ! this.currentPreviewId || ! this.currentPreviewItem || this.children.length < 2 ) {
+			return;
+		}
+
+		var UP_ARROW = 38,
+			DOWN_ARROW = 40;
+
+		if ( event.which !== UP_ARROW && event.which !== DOWN_ARROW ) {
+			return;
+		}
+
+		event.preventDefault();
+
+		var currentPreviewItemIndex = this.collection.indexOf( this.currentPreviewItem.model ),
+			requiredIndex = event.which === UP_ARROW ? currentPreviewItemIndex - 1 : currentPreviewItemIndex + 1;
+
+		if ( requiredIndex < 0 ) {
+			requiredIndex = this.collection.length - 1;
+		}
+
+		if ( requiredIndex >= this.collection.length ) {
+			requiredIndex = 0;
+		}
+
+		this.children.findByIndex( requiredIndex ).ui.detailsArea.trigger( 'click' );
+	},
+
+	bindHotKeys: function() {
+		Backbone.$( window ).on( 'keydown', this.navigate );
+	},
+
+	unbindHotKeys: function() {
+		Backbone.$( window ).off( 'keydown', this.navigate );
+	},
+
 	onEditorSaved: function() {
 		this.exitPreviewMode();
 
@@ -677,13 +719,17 @@ module.exports = Marionette.CompositeView.extend( {
 
 		this.exitPreviewMode();
 
-		this.$( '.elementor-revision-current-preview' ).removeClass( 'elementor-revision-current-preview' );
+		if ( this.currentPreviewItem ) {
+			this.currentPreviewItem.$el.removeClass( 'elementor-revision-current-preview' );
+		}
 	},
 
 	onDestroy: function() {
 		if ( this.currentPreviewId ) {
 			this.onDiscardClick();
 		}
+
+		this.unbindHotKeys();
 	},
 
 	onChildviewDetailsAreaClick: function( childView ) {
@@ -702,7 +748,15 @@ module.exports = Marionette.CompositeView.extend( {
 			this.saveAutoDraft();
 		}
 
-		childView.$el.addClass( 'elementor-revision-item-loading' );
+		if ( self.currentPreviewItem ) {
+			self.currentPreviewItem.$el.removeClass( 'elementor-revision-current-preview' );
+		}
+
+		childView.$el.addClass( 'elementor-revision-current-preview elementor-revision-item-loading' );
+
+		self.currentPreviewItem = childView;
+
+		self.currentPreviewId = id;
 
 		this.jqueryXhr = elementor.ajax.send( 'get_revision_preview', {
 			data: {
@@ -713,24 +767,22 @@ module.exports = Marionette.CompositeView.extend( {
 
 				self.setRevisionsButtonsActive( true );
 
-				self.currentPreviewId = id;
-
 				self.jqueryXhr = null;
 
-				self.$( '.elementor-revision-current-preview' ).removeClass( 'elementor-revision-current-preview' );
-
-				childView.$el.removeClass( 'elementor-revision-item-loading' ).addClass( 'elementor-revision-current-preview' );
+				childView.$el.removeClass( 'elementor-revision-item-loading' );
 
 				self.enterPreviewMode();
 			},
 			error: function( data ) {
-				childView.$el.removeClass( 'elementor-revision-item-loading elementor-revision-current-preview' );
+				childView.$el.removeClass( 'elementor-revision-item-loading' );
 
 				if ( 'abort' === self.jqueryXhr.statusText ) {
 					return;
 				}
 
-				this.currentPreviewId = null;
+				self.currentPreviewItem = null;
+
+				self.currentPreviewId = null;
 
 				alert( 'An error occurred' );
 			}
