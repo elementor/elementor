4e98696aa71030fff483f58ba37ab68e6fec2c16
Kobi Zaltzberg <kobi@pojo.me>
2017-01-22 13:27:45 +0200

    Using constant instead of method

--- includes/frontend.php
+++ includes/frontend.php
@@ -116,7 +116,7 @@ class Frontend {
 			'elementor-icons',
 			ELEMENTOR_ASSETS_URL . 'lib/eicons/css/elementor-icons' . $suffix . '.css',
 			[],
-			Plugin::instance()->get_version()
+			ELEMENTOR_VERSION
 		);
 
 		wp_register_style(
@@ -138,7 +138,7 @@ class Frontend {
 			'elementor-frontend',
 			ELEMENTOR_ASSETS_URL . 'css/frontend' . $direction_suffix . $suffix . '.css',
 			[],
-			Plugin::instance()->get_version()
+			ELEMENTOR_VERSION
 		);
 
 		wp_enqueue_style( 'elementor-icons' );
