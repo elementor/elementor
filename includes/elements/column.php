d407eedc6eaefe31c31a155749d8122e754e5f09
Yakir Sitbon <kingyes1@gmail.com>
2017-01-30 19:49:25 +0200

    Fix miss code

--- includes/elements/column.php
+++ includes/elements/column.php
@@ -92,7 +92,7 @@ class Element_Column extends Element_Base {
 				'label' => __( 'Background Overlay', 'elementor' ),
 				'tab' => Controls_Manager::TAB_STYLE,
 				'condition' => [
-					'background_background!' => [ 'classic', 'gradient', 'video' ],
+					'background_background' => [ 'classic', 'gradient', 'video' ],
 				],
 			]
 		);
