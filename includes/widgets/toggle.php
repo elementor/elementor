c1efacdc99bccb388a5ed2054a9d2642b03b2942
Rami Yushuvaev <r_a_m_i@hotmail.com>
2017-02-21 14:27:10 +0200

    Accessibility: add `role` tags to the toggle widget

--- includes/widgets/toggle.php
+++ includes/widgets/toggle.php
@@ -211,17 +211,17 @@ class Widget_Toggle extends Widget_Base {
 	protected function render() {
 		$tabs = $this->get_settings( 'tabs' );
 		?>
-		<div class="elementor-toggle">
+		<div class="elementor-toggle" role="tablist">
 			<?php
 			$counter = 1; ?>
 			<?php foreach ( $tabs as $item ) : ?>
-				<div class="elementor-toggle-title" data-tab="<?php echo $counter; ?>">
+				<div class="elementor-toggle-title" data-tab="<?php echo $counter; ?>" role="tab">
 					<span class="elementor-toggle-icon">
 						<i class="fa"></i>
 					</span>
 					<?php echo $item['tab_title']; ?>
 				</div>
-				<div class="elementor-toggle-content elementor-clearfix" data-tab="<?php echo $counter; ?>"><?php echo $this->parse_text_editor( $item['tab_content'] ); ?></div>
+				<div class="elementor-toggle-content elementor-clearfix" data-tab="<?php echo $counter; ?>" role="tabpanel"><?php echo $this->parse_text_editor( $item['tab_content'] ); ?></div>
 			<?php
 				$counter++;
 			endforeach; ?>
@@ -231,18 +231,18 @@ class Widget_Toggle extends Widget_Base {
 
 	protected function _content_template() {
 		?>
-		<div class="elementor-toggle">
+		<div class="elementor-toggle" role="tablist">
 			<#
 			if ( settings.tabs ) {
 				var counter = 1;
 				_.each(settings.tabs, function( item ) { #>
-					<div class="elementor-toggle-title" data-tab="{{ counter }}">
+					<div class="elementor-toggle-title" data-tab="{{ counter }}" role="tab">
 						<span class="elementor-toggle-icon">
 						<i class="fa"></i>
 					</span>
 						{{{ item.tab_title }}}
 					</div>
-					<div class="elementor-toggle-content elementor-clearfix" data-tab="{{ counter }}">{{{ item.tab_content }}}</div>
+					<div class="elementor-toggle-content elementor-clearfix" data-tab="{{ counter }}" role="tabpanel">{{{ item.tab_content }}}</div>
 				<#
 					counter++;
 				} );
