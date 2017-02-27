3efa2fa8d7ee16a2bcdf87e9c46a9fc91781732a
Mati Horovitz <mati@pojo.me>
2017-02-27 18:30:58 +0200

    under-consturction

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -69,6 +69,8 @@
 				} );
 			} );
 
+			require( 'elementor/includes/under-construction/assets/js/tools.js' )( $ );
+
 			$( '#elementor-clear-cache-button' ).on( 'click', function( event ) {
 				event.preventDefault();
 				var $thisButton = $( this );
@@ -170,6 +172,40 @@
 	} );
 
 }( jQuery, window, document ) );
+
+},{"elementor/includes/under-construction/assets/js/tools.js":2}],2:[function(require,module,exports){
+module.exports = function( $ ) {
+	$( '.elementor_under_construction_mode select' ).change( function( event ) {
+		event.preventDefault();
+		var $this = $( this );
+
+		$this.parents( 'table' ).toggleClass( 'elementor-under-construction-is-enabled', '' !== $this.val() );
+	} ).change();
+
+	$( '.elementor_under_construction_exclude_mode select' ).change( function( event ) {
+		event.preventDefault();
+		var $this = $( this );
+
+		$( '.elementor_under_construction_exclude_roles' ).toggle( 'custom' === $this.val() );
+	} ).change();
+
+	$( '.elementor_under_construction_template_id select' ).change( function() {
+		var $this = $( this ),
+			templateID = $this.val(),
+			$editButton = $this.parents( 'tr' ).find( '.elementor-edit-template' );
+
+		if ( ! templateID ) {
+			$editButton.hide();
+			return;
+		}
+
+		var editUrl = ElementorConfig.home_url + '?p=' + templateID + '&elementor';
+
+		$editButton
+			.prop( 'href', editUrl )
+			.show();
+	} ).change();
+};
 
 },{}]},{},[1])
 //# sourceMappingURL=admin.js.map
