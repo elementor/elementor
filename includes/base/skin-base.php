68d3a760c4b245d423dba983573754428f9b5f3e
Josh Marom <josh@pojo.me>
2017-03-01 11:01:09 +0200

    improve skins add/remove controls methods

--- includes/base/skin-base.php
+++ includes/base/skin-base.php
@@ -41,17 +41,28 @@ abstract class Skin_Base {
 		return $this->parent->get_settings( $control_id );
 	}
 
+	public function start_controls_section( $id, $args ) {
+		$args['condition']['_skin'] = $this->get_id();
+		$this->parent->start_controls_section( $this->get_control_id( $id ), $args );
+	}
+
+	public function end_controls_section() {
+		$this->parent->end_controls_section();
+	}
+
 	public function add_control( $id, $args ) {
+		$args['condition']['_skin'] = $this->get_id();
 		return $this->parent->add_control( $this->get_control_id( $id ), $args );
 	}
 
 	public function add_responsive_control( $id, $args ) {
+		$args['condition']['_skin'] = $this->get_id();
 		$this->parent->add_responsive_control( $this->get_control_id( $id ), $args );
 	}
 
 	public final function add_group_control( $group_name, $args = [] ) {
 		$args['name'] = $this->get_control_id( $args['name'] );
-
+		$args['condition']['_skin'] = $this->get_id();
 		$this->parent->add_group_control( $group_name, $args );
 	}
 
