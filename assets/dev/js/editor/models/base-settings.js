4088609e9b57ce2f365f57df148aa22dbb991d20
Yakir Sitbon <kingyes1@gmail.com>
2017-03-07 15:33:21 +0200

    Fix convert control type

--- assets/dev/js/editor/models/base-settings.js
+++ assets/dev/js/editor/models/base-settings.js
@@ -29,7 +29,7 @@ BaseSettingsModel = Backbone.Model.extend( {
 			}
 
 			if ( undefined !== attrs[ field.name ] ) {
-				if ( isMultipleControl !== _.isObject( attrs[ field.name ] ) ) {
+				if ( isMultipleControl && ! _.isObject( attrs[ field.name ] ) ) {
 					delete attrs[ field.name ];
 				}
 			}
