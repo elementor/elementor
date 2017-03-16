d09989ccc98d38092b837b7a0c2c8fa675a5a29a
Kobi Zaltzberg <kobi@pojo.me>
2017-03-16 09:49:21 +0200

    Removed ‘elementor-page-settings’ expose

--- Gruntfile.js
+++ Gruntfile.js
@@ -113,11 +113,6 @@ module.exports = function( grunt ) {
 							cwd: 'assets/dev/js/editor/components/revisions',
 							src: '**/*.js',
 							expose: 'elementor-revisions'
-						},
-						{
-							cwd: 'assets/dev/js/editor/components/page-settings',
-							src: '**/*.js',
-							expose: 'elementor-page-settings'
 						}
 					] );
 				}
