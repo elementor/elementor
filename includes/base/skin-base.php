553e0667e70e1a81e3197f8b1717ab71de6c1f77
Josh Marom <josh@pojo.me>
2017-03-19 16:01:42 +0200

    Remove and Update Responsive controls

--- includes/base/skin-base.php
+++ includes/base/skin-base.php
@@ -68,6 +68,14 @@ abstract class Skin_Base {
 		$this->parent->add_responsive_control( $this->get_control_id( $id ), $args );
 	}
 
+	public function update_responsive_control( $id, $args ) {
+		$this->parent->update_responsive_control( $this->get_control_id( $id ), $args );
+	}
+
+	public function remove_responsive_control( $id ) {
+		$this->parent->remove_responsive_control( $this->get_control_id( $id ) );
+	}
+
 	public final function add_group_control( $group_name, $args = [] ) {
 		$args['name'] = $this->get_control_id( $args['name'] );
 		$args['condition']['_skin'] = $this->get_id();
