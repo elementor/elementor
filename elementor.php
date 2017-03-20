defa0d20b593a389d0681e1558140acd6949d796
Yakir Sitbon <kingyes1@gmail.com>
2017-03-20 16:25:03 +0200

    Bump to 1.3.5

--- elementor.php
+++ elementor.php
@@ -4,7 +4,7 @@
  * Description: The most advanced frontend drag & drop page builder. Create high-end, pixel perfect websites at record speeds. Any theme, any page, any design.
  * Plugin URI: https://elementor.com/
  * Author: Elementor.com
- * Version: 1.3.4
+ * Version: 1.3.5
  * Author URI: https://elementor.com/
  *
  * Text Domain: elementor
@@ -22,7 +22,7 @@
 
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
-define( 'ELEMENTOR_VERSION', '1.3.4' );
+define( 'ELEMENTOR_VERSION', '1.3.5' );
 
 define( 'ELEMENTOR__FILE__', __FILE__ );
 define( 'ELEMENTOR_PLUGIN_BASE', plugin_basename( ELEMENTOR__FILE__ ) );
