f34f8608a3fc649a2075902655a12af5b9c6e192
Yakir Sitbon <kingyes1@gmail.com>
2017-03-22 17:19:36 +0200

    Fix switcher control with non-string value

--- assets/dev/js/editor/views/controls/switcher.js
+++ assets/dev/js/editor/views/controls/switcher.js
@@ -2,6 +2,10 @@ var ControlBaseItemView = require( 'elementor-views/controls/base' );
 
 module.exports = ControlBaseItemView.extend( {
 	setInputValue: function( input, value ) {
+		// Make sure is string value
+		// TODO: Remove in v1.6
+		value = '' + value;
+
 		this.$( input ).prop( 'checked', this.model.get( 'return_value' ) === value );
 	}
 } );
