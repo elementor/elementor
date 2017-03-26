cb5821219d0a59b0f38f7698115832ca6ecdd691
arielk <relklik@gmail.com>
2017-03-26 13:58:58 +0300

    Send `controlsStack` in repeater also

--- assets/js/editor.js
+++ assets/js/editor.js
@@ -4491,6 +4491,8 @@ ControlsCSSParser = ViewModule.extend( {
 					self.addStyleRules(
 						control.styleFields,
 						itemModel.attributes,
+						controlsStack,
+						
 						placeholders.concat( [ '{{CURRENT_ITEM}}' ] ),
 						replacements.concat( [ '.elementor-repeater-item-' + itemModel.get( '_id' ) ] )
 					);
