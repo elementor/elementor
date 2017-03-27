8591ac0e74f4869dfc8d05573670c186150486ee
Kobi Zaltzberg <kobi@pojo.me>
2017-03-27 12:23:53 +0300

    Compiled files

--- assets/js/editor.js
+++ assets/js/editor.js
@@ -3136,34 +3136,11 @@ module.exports = ControlsStack.extend( {
 	},
 
 	onReloadButtonClick: function() {
-		var self = this,
-			settings = self.model.toJSON();
-
-		settings.id = elementor.config.post_id;
-
-		NProgress.start();
-
-		elementor.ajax.send( 'save_page_settings', {
-			data: settings,
-			success: function() {
-				elementor.pageSettings.setSettings( 'savedSettings', settings );
-
-				elementor.reloadPreview();
-
-				elementor.once( 'preview:loaded', function() {
-					NProgress.done();
-
-					elementor.getPanelView().setPage( 'settingsPage' );
-				} );
-			},
-			error: function() {
-				alert( 'An error occurred' );
-			}
-		} );
+		elementor.pageSettings.save();
 	},
 
 	onDestroy: function() {
-		elementor.pageSettings.resetModel();
+		elementor.pageSettings.save();
 	}
 } );
 
@@ -5817,6 +5794,15 @@ module.exports = ViewModule.extend( {
 			var $title = elementorFrontend.getElements( '$document' ).find( elementor.config.page_title_selector );
 
 			$title.text( newValue );
+		},
+		template: function() {
+			this.save( function() {
+				elementor.reloadPreview();
+
+				elementor.once( 'preview:loaded', function() {
+					elementor.getPanelView().setPage( 'settingsPage' );
+				} );
+			} );
 		}
 	},
 
@@ -5854,8 +5840,36 @@ module.exports = ViewModule.extend( {
 		this.controlsCSS = new ControlsCSSParser();
 	},
 
-	resetModel: function() {
-		this.model.set( this.getSettings( 'savedSettings' ) );
+	save: function( callback ) {
+		var self = this;
+
+		if ( _.isEmpty( self.model.changed ) ) {
+			return;
+		}
+
+		var settings = self.model.toJSON();
+
+		settings.id = elementor.config.post_id;
+
+		NProgress.start();
+
+		elementor.ajax.send( 'save_page_settings', {
+			data: settings,
+			success: function() {
+				NProgress.done();
+
+				self.setSettings( 'savedSettings', settings );
+
+				self.model.changed = {};
+
+				if ( callback ) {
+					callback.apply( self, arguments );
+				}
+			},
+			error: function() {
+				alert( 'An error occurred' );
+			}
+		} );
 	},
 
 	onInit: function() {
@@ -5867,22 +5881,15 @@ module.exports = ViewModule.extend( {
 	},
 
 	onModelChange: function( model ) {
-		var self = this,
-			isStyleNeedUpdate = false;
+		var self = this;
 
 		_.each( model.changed, function( value, key ) {
 			if ( self.changeCallbacks[ key ] ) {
 				self.changeCallbacks[ key ].call( self, value );
 			}
-
-			if ( self.model.controls[ key ].selectors ) {
-				isStyleNeedUpdate = true;
-			}
 		} );
 
-		if ( isStyleNeedUpdate ) {
-			self.updateStylesheet();
-		}
+		self.updateStylesheet();
 	}
 } );
 
