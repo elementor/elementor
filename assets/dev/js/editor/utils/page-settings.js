ecbfe6ea8dd2fc4d8ad8eaf7b1229ae8423c0d3a
Yakir Sitbon <kingyes1@gmail.com>
2017-03-27 19:12:11 +0300

    Change body selector for Page Settings

--- assets/dev/js/editor/utils/page-settings.js
+++ assets/dev/js/editor/utils/page-settings.js
@@ -37,7 +37,7 @@ module.exports = ViewModule.extend( {
 	},
 
 	renderStyles: function() {
-		this.controlsCSS.addStyleRules( this.model.getStyleControls(), this.model.attributes, this.model.controls, [ /\{\{WRAPPER}}/g ], [ '.elementor-page-' + elementor.config.post_id ] );
+		this.controlsCSS.addStyleRules( this.model.getStyleControls(), this.model.attributes, this.model.controls, [ /\{\{WRAPPER}}/g ], [ 'body.elementor-page-' + elementor.config.post_id ] );
 	},
 
 	updateStylesheet: function() {
