80361934d4931d3aab7518d231906a9526e69f80
Yakir Sitbon <kingyes1@gmail.com>
2017-03-28 14:59:08 +0300

    Fix title tag in canvas template for some theme supports

--- includes/page-templates/empty.php
+++ includes/page-templates/empty.php
@@ -7,7 +7,9 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 	<head>
 		<meta charset="<?php bloginfo( 'charset' ); ?>">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<?php if ( ! current_theme_supports( 'title-tag' ) ) : ?>
 		<title><?php wp_title( '|', true, 'right' ); ?></title>
+		<?php endif; ?>
 		<?php wp_head(); ?>
 	</head>
 
