d59bbece6f7d8a92c1f73dab52f8bfabd331380e
Kobi Zaltzberg <kobi@pojo.me>
2017-03-30 15:04:24 +0300

    Include insert template behavior in the classes themselves

--- assets/dev/js/editor/components/template-library/views/parts/header-parts/preview.js
+++ assets/dev/js/editor/components/template-library/views/parts/header-parts/preview.js
@@ -1,15 +1,28 @@
-var TemplateLibraryInsertTemplateBehavior = require( 'elementor-templates/behaviors/insert-template' ),
-	TemplateLibraryHeaderPreviewView;
+var TemplateLibraryHeaderPreviewView;
 
 TemplateLibraryHeaderPreviewView = Marionette.ItemView.extend( {
 	template: '#tmpl-elementor-template-library-header-preview',
 
 	id: 'elementor-template-library-header-preview',
 
-	behaviors: {
-		insertTemplate: {
-			behaviorClass: TemplateLibraryInsertTemplateBehavior
-		}
+	ui: {
+		insertButton: '.elementor-template-library-template-insert'
+	},
+
+	events:  {
+		'click @ui.insertButton': 'onInsertButtonClick'
+	},
+
+	templateView: null,
+
+	initialize: function( options ) {
+		this.templateView = options.templateView;
+
+		this.model = this.templateView.model;
+	},
+
+	onInsertButtonClick: function() {
+		this.templateView.insert( this.ui.insertButton.data( 'action' ) );
 	}
 } );
 
