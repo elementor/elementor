5d06b7a0ccca129d7708348580c817ca51e0f393
Kobi Zaltzberg <kobi@pojo.me>
2017-03-30 15:06:58 +0300

    Allow to get page settings with template content

--- assets/dev/js/editor/components/template-library/views/template/local.js
+++ assets/dev/js/editor/components/template-library/views/template/local.js
@@ -1,6 +1,8 @@
 var TemplateLibraryTemplateView = require( 'elementor-templates/views/template/base' ),
 	TemplateLibraryTemplateLocalView;
 
+var dialog;
+
 TemplateLibraryTemplateLocalView = TemplateLibraryTemplateView.extend( {
 	template: '#tmpl-elementor-template-library-template-local',
 
@@ -14,6 +16,41 @@ TemplateLibraryTemplateLocalView = TemplateLibraryTemplateView.extend( {
 		return _.extend( TemplateLibraryTemplateView.prototype.events.apply( this, arguments ), {
 			'click @ui.deleteButton': 'onDeleteButtonClick'
 		} );
+	},
+
+	initDialog: function() {
+		var self = this;
+
+		dialog = elementor.dialogsManager.createWidget( 'confirm', {
+			headerMessage: elementor.translate( 'import_template_dialog_header' ),
+			message: elementor.translate( 'import_template_dialog_message' ),
+			strings: {
+				confirm: elementor.translate( 'yes' ),
+				cancel: elementor.translate( 'no' )
+			},
+			onConfirm: function() {
+				elementor.templates.importTemplate( self.model, true );
+			},
+			onCancel: function() {
+				TemplateLibraryTemplateView.prototype.insert.apply( self, arguments );
+			}
+		} );
+	},
+
+	getDialog: function() {
+		if ( ! dialog ) {
+			this.initDialog();
+		}
+
+		return dialog;
+	},
+
+	insert: function() {
+		if ( 'page' === this.model.get( 'type' ) ) {
+			this.getDialog().show();
+		} else {
+			TemplateLibraryTemplateView.prototype.insert.apply( this, arguments );
+		}
 	},
 
 	onDeleteButtonClick: function() {
