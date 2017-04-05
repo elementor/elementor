140f0bfbd942c1d9211e628cbe78fedb64def8de
Kobi Zaltzberg <kobi@pojo.me>
2017-04-05 16:45:41 +0300

    Rewrite image carousel according to the new structure

--- assets/dev/js/frontend/handlers/image-carousel.js
+++ assets/dev/js/frontend/handlers/image-carousel.js
@@ -1,31 +1,67 @@
-module.exports = function( $scope, $ ) {
-	var $carousel = $scope.find( '.elementor-image-carousel' );
-	if ( ! $carousel.length ) {
-		return;
+var HandlerModule = require( 'elementor-frontend/handler-module' ),
+	ImageCarouselHandler;
+
+ImageCarouselHandler = HandlerModule.extend( {
+	getDefaultSettings: function() {
+		return {
+			selectors: {
+				carousel: '.elementor-image-carousel'
+			}
+		};
+	},
+
+	getDefaultElements: function() {
+		var selectors = this.getSettings( 'selectors' );
+
+		return {
+			$carousel: this.$element.find( selectors.carousel )
+		};
+	},
+
+	onInit: function() {
+		HandlerModule.prototype.onInit.apply( this, arguments );
+
+		var elementSettings = this.getElementSettings(),
+			isSingleSlide = 1 === +elementSettings.slides_to_show,
+			tabletSlides = isSingleSlide ? 1 : 2,
+			slickOptions = {
+				slidesToShow: +elementSettings.slides_to_show,
+				autoplay: !! elementSettings.autoplay,
+				autoplaySpeed: elementSettings.autoplay_speed,
+				infinite: !! elementSettings.infinite,
+				pauseOnHover: !! elementSettings.pause_on_hover,
+				speed: elementSettings.speed,
+				arrows: 'dots' !== elementSettings.navigation,
+				dots: 'arrows' !== elementSettings.navigation,
+				rtl: 'rtl' === elementSettings.direction,
+				responsive: [
+					{
+						breakpoint: 767,
+						settings: {
+							slidesToShow: tabletSlides,
+							slidesToScroll: tabletSlides
+						}
+					},
+					{
+						breakpoint: 480,
+						settings: {
+							slidesToShow: 1,
+							slidesToScroll: 1
+						}
+					}
+				]
+			};
+
+		if ( isSingleSlide ) {
+			slickOptions.fade = 'fade' === elementSettings.effect;
+		} else {
+			slickOptions.slidesToScroll = +elementSettings.slides_to_scroll;
+		}
+
+		this.elements.$carousel.slick( slickOptions );
 	}
+} );
 
-	var savedOptions = $carousel.data( 'slider_options' ),
-		tabletSlides = 1 === savedOptions.slidesToShow ? 1 : 2,
-		defaultOptions = {
-			responsive: [
-				{
-					breakpoint: 767,
-					settings: {
-						slidesToShow: tabletSlides,
-						slidesToScroll: tabletSlides
-					}
-				},
-				{
-					breakpoint: 480,
-					settings: {
-						slidesToShow: 1,
-						slidesToScroll: 1
-					}
-				}
-			]
-		},
-
-		slickOptions = $.extend( {}, defaultOptions, $carousel.data( 'slider_options' ) );
-
-	$carousel.slick( slickOptions );
+module.exports = function( $scope ) {
+	new ImageCarouselHandler( { $element: $scope } );
 };
