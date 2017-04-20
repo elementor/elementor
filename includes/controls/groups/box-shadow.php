bae6cab259c13aa28482ab342d5597ce0ac62369
Josh Marom <josh@pojo.me>
2017-04-20 17:30:16 +0300

    Add inset option to box shadow control

--- includes/controls/groups/box-shadow.php
+++ includes/controls/groups/box-shadow.php
@@ -21,16 +21,31 @@ class Group_Control_Box_Shadow extends Group_Control_Base {
 			'label_off' => __( 'No', 'elementor' ),
 			'return_value' => 'yes',
 			'separator' => 'before',
+			'render_type' => 'ui',
 		];
 
 		$controls['box_shadow'] = [
 			'label' => _x( 'Box Shadow', 'Box Shadow Control', 'elementor' ),
 			'type' => Controls_Manager::BOX_SHADOW,
-			'selectors' => [
-				'{{SELECTOR}}' => 'box-shadow: {{HORIZONTAL}}px {{VERTICAL}}px {{BLUR}}px {{SPREAD}}px {{COLOR}};',
+			'condition' => [
+				'box_shadow_type!' => '',
+			],
+			'render_type' => 'ui',
+		];
+
+		$controls['box_shadow_position'] = [
+			'label' => _x( 'Position', 'Box Shadow Control', 'elementor' ),
+			'type' => Controls_Manager::SELECT,
+			'options' => [
+				' ' => _x( 'Outline', 'Box Shadow Control', 'elementor' ),
+				'inset' => _x( 'Inset', 'Box Shadow Control', 'elementor' ),
 			],
 			'condition' => [
 				'box_shadow_type!' => '',
+			],
+			'default' => ' ',
+			'selectors' => [
+				'{{SELECTOR}}' => 'box-shadow: {{box_shadow.HORIZONTAL}}px {{box_shadow.VERTICAL}}px {{box_shadow.BLUR}}px {{box_shadow.SPREAD}}px {{box_shadow.COLOR}} {{VALUE}};',
 			],
 		];
 
