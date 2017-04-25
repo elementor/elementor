81376c6511cedfc238f3788708a604e049557a69
Mati Horovitz <mati@pojo.me>
2017-04-25 18:22:58 +0300

    controls-stack: fix section activation with the event trigger

--- assets/js/editor.js
+++ assets/js/editor.js
@@ -7221,7 +7221,7 @@ ControlsStack = Marionette.CompositeView.extend( {
 			} );
 
 		if ( activeSectionView[0] ) {
-			activeSectionView[0].ui.heading.addClass( 'elementor-open' );
+			this.activateSection( activeSection );
 		}
 	},
 
