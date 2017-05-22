8cb2b2a59c2b7b5a12b18f030d5e1fc8d77f8917
Kobi Zaltzberg <kobi@pojo.me>
2017-05-22 16:48:38 +0300

    Fixed method call

--- assets/js/editor.js
+++ assets/js/editor.js
@@ -8542,7 +8542,7 @@ ControlNumberItemView = ControlBaseItemView.extend( {
 			validValue = min;
 		}
 
-		if ( isFinite( max ) && inputValue > max ) {
+		if ( _.isFinite( max ) && inputValue > max ) {
 			validValue = max;
 		}
 
