cce3493a28593b6beb168252af943c79f602791c
Kobi Zaltzberg <kobi@pojo.me>
2017-06-11 17:29:04 +0300

    z-index control

--- includes/elements/column.php
+++ includes/elements/column.php
@@ -584,6 +584,17 @@ class Element_Column extends Element_Base {
 		);
 
 		$this->add_control(
+			'z-index',
+			[
+				'label' => __( 'z-index', 'elementor' ),
+				'type' => Controls_Manager::NUMBER,
+				'selectors' => [
+					'{{WRAPPER}}' => 'z-index: {{VALUE}};',
+				],
+			]
+		);
+
+		$this->add_control(
 			'animation',
 			[
 				'label' => __( 'Entrance Animation', 'elementor' ),
