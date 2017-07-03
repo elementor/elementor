4a6e8453e63d85b53cf77b0b864399043270bfd3
Kobi Zaltzberg <kobi@pojo.me>
2017-07-03 15:36:40 +0300

    Throttling widget render method

--- assets/js/editor.js
+++ assets/js/editor.js
@@ -9920,6 +9920,8 @@ WidgetView = BaseElementView.extend( {
 
 		var onRenderMethod = this.onRender;
 
+		this.render = _.throttle( this.render, 1000 );
+
 		this.onRender = function() {
 			_.defer( _.bind( onRenderMethod, this ) );
 		};
