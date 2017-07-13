0f846f0510041a2d0fdc9dee681a4fa53e18b898
Kobi Zaltzberg <kobi@pojo.me>
2017-07-13 16:33:54 +0300

    Compiled files

--- assets/js/editor.js
+++ assets/js/editor.js
@@ -4529,7 +4529,7 @@ var BaseSettings = require( 'elementor-editor/settings/base/manager' );
 module.exports = BaseSettings.extend( {
 	changeCallbacks: {
 		elementor_page_title_selector: function( newValue ) {
-			var newSelector = '{{WRAPPER}} ' + ( newValue || 'h1.entry-title' ),
+			var newSelector = newValue || 'h1.entry-title',
 				titleSelectors = elementor.settings.page.model.controls.hide_title.selectors = {};
 
 			titleSelectors[ newSelector ] = 'display: none';
