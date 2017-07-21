04efd3c33451a55cf05a0a84da69df34636074df
arielk <relklik@gmail.com>
2017-07-21 18:09:10 +0300

    Replaced icons

--- includes/elements/section.php
+++ includes/elements/section.php
@@ -23,11 +23,11 @@ class Element_Section extends Element_Base {
 			],
 			'save' => [
 				'title' => sprintf( __( 'Save %s', 'elementor' ), $section_label ),
-				'icon' => 'floppy-o',
+				'icon' => 'save'
 			],
 			'remove' => [
 				'title' => sprintf( __( 'Remove %s', 'elementor' ), $section_label ),
-				'icon' => 'times',
+				'icon' => 'close',
 			],
 		];
 	}
@@ -41,7 +41,7 @@ class Element_Section extends Element_Base {
 	}
 
 	public function get_icon() {
-		return 'eicon-columns';
+		return 'eicon-section';
 	}
 
 	public static function get_presets( $columns_count = null, $preset_index = null ) {
@@ -1108,14 +1108,14 @@ class Element_Section extends Element_Base {
 		?>
 		<div class="elementor-element-overlay">
 			<ul class="elementor-editor-element-settings elementor-editor-section-settings">
-				<li class="elementor-editor-element-setting elementor-editor-element-trigger elementor-active" title="<?php printf( __( 'Edit %s', 'elementor' ),  __( 'Section', 'elementor' ) ); ?>"><i class="fa fa-bars"></i></li>
+				<li class="elementor-editor-element-setting elementor-editor-element-trigger elementor-active" title="<?php printf( __( 'Edit %s', 'elementor' ),  __( 'Section', 'elementor' ) ); ?>"><i class="eicon-section"></i></li>
 				<?php foreach ( Element_Section::get_edit_tools() as $edit_tool_name => $edit_tool ) : ?>
 					<?php if ( 'add' === $edit_tool_name ) : ?>
 						<# if ( ! isInner ) { #>
 					<?php endif; ?>
 					<li class="elementor-editor-element-setting elementor-editor-element-<?php echo $edit_tool_name; ?>" title="<?php echo $edit_tool['title']; ?>">
 						<span class="elementor-screen-only"><?php echo $edit_tool['title']; ?></span>
-						<i class="fa fa-<?php echo $edit_tool['icon']; ?>"></i>
+						<i class="eicon-<?php echo $edit_tool['icon']; ?>"></i>
 					</li>
 					<?php if ( 'add' === $edit_tool_name ) : ?>
 						<# } #>
