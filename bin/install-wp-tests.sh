2dd7b0f7509173f89830da5aba865bb344deffd4
Kobi Zaltzberg <kobi@pojo.me>
2017-07-26 12:19:21 +0300

    try

--- bin/install-wp-tests.sh
+++ bin/install-wp-tests.sh
@@ -111,7 +111,7 @@ install_phpcs() {
 	pear install PHP_CodeSniffer-2.9.0
 	git clone git://github.com/WordPress-Coding-Standards/WordPress-Coding-Standards.git $(pear config-get php_dir)/PHP/CodeSniffer/Standards/WordPress
 	phpenv rehash
-	phpcs --config-set installed_paths $(pear config-get php_dir)/PHP/CodeSniffer/Standards/WordPress
+	phpcs --config-set installed_paths $(pear config-get php_dir)/../../../share/pear/PHP/CodeSniffer/Standards/WordPress
 	phpcs -i
 }
 
