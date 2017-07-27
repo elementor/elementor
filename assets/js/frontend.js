a749c75b4ab43fb0d45da0d7343d9d39638f9f99
Kobi Zaltzberg <kobi@pojo.me>
2017-07-27 13:28:27 +0300

    Run stretch section only when it's necessary

--- assets/js/frontend.js
+++ assets/js/frontend.js
@@ -872,7 +872,9 @@ var Shapes = HandlerModule.extend( {
 } );
 
 module.exports = function( $scope ) {
-	new StretchedSection( { $element: $scope } );
+	if ( elementorFrontend.isEditMode() || $scope.hasClass( 'elementor-section-stretched' ) ) {
+		new StretchedSection( { $element: $scope } );
+	}
 
 	if ( elementorFrontend.isEditMode() ) {
 		new Shapes( { $element:  $scope } );
