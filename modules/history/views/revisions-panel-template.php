0b9dbc84bed71d9fa76977873d2c8f009319765d
Mati Horovitz <mati@pojo.me>
2017-08-14 11:20:29 +0300

    history: texts & phpcs

--- modules/history/views/revisions-panel-template.php
+++ modules/history/views/revisions-panel-template.php
@@ -3,12 +3,12 @@
 	<div class="elementor-panel-scheme-buttons">
 			<div class="elementor-panel-scheme-button-wrapper elementor-panel-scheme-discard">
 				<button class="elementor-button" disabled>
-					<i class="fa fa-times"></i><?php _e( 'Discard', 'elementor' ); ?>
+					<i class="fa fa-times"></i><?php esc_html_e( 'Discard', 'elementor' ); ?>
 				</button>
 			</div>
 			<div class="elementor-panel-scheme-button-wrapper elementor-panel-scheme-save">
 				<button class="elementor-button elementor-button-success" disabled>
-					<?php _e( 'Apply', 'elementor' ); ?>
+					<?php esc_html_e( 'Apply', 'elementor' ); ?>
 				</button>
 			</div>
 		</div>
@@ -16,7 +16,7 @@
 
 	<div class="elementor-panel-box">
 		<div class="elementor-panel-heading">
-			<div class="elementor-panel-heading-title"><?php _e( 'Revisions', 'elementor' ); ?></div>
+			<div class="elementor-panel-heading-title"><?php esc_html_e( 'Revisions', 'elementor' ); ?></div>
 		</div>
 		<div id="elementor-revisions-list" class="elementor-panel-box-content"></div>
 	</div>
@@ -24,7 +24,7 @@
 
 <script type="text/template" id="tmpl-elementor-panel-revisions-no-revisions">
 	<i class="elementor-panel-nerd-box-icon eicon-nerd"></i>
-	<div class="elementor-panel-nerd-box-title"><?php _e( 'No Revisions Saved Yet', 'elementor' ); ?></div>
+	<div class="elementor-panel-nerd-box-title"><?php esc_html_e( 'No Revisions Saved Yet', 'elementor' ); ?></div>
 	<div class="elementor-panel-nerd-box-message">{{{ elementor.translate( elementor.config.revisions_enabled ? 'no_revisions_1' : 'revisions_disabled_1' ) }}}</div>
 	<div class="elementor-panel-nerd-box-message">{{{ elementor.translate( elementor.config.revisions_enabled ? 'no_revisions_2' : 'revisions_disabled_2' ) }}}</div>
 </script>
@@ -33,7 +33,7 @@
 	<div class="elementor-revision-item__gravatar">{{{ gravatar }}}</div>
 	<div class="elementor-revision-item__details">
 		<div class="elementor-revision-date">{{{ date }}}</div>
-		<div class="elementor-revision-meta">{{{ elementor.translate( type ) }}} <?php _e( 'By', 'elementor' ); ?> {{{ author }}}</div>
+		<div class="elementor-revision-meta">{{{ elementor.translate( type ) }}} <?php esc_html_e( 'By', 'elementor' ); ?> {{{ author }}}</div>
 	</div>
 	<div class="elementor-revision-item__tools">
 		<i class="elementor-revision-item__tools-delete fa fa-times"></i>
