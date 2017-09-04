c9ccaf3411ff5b492e998c859eb5d87d49297103
Rami Yushuvaev <r_a_m_i@hotmail.com>
2017-09-04 16:58:05 +0300

    Audio Widget phpDocs

--- includes/widgets/audio.php
+++ includes/widgets/audio.php
@@ -5,25 +5,73 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 
+/**
+ * Audio Widget
+ */
 class Widget_Audio extends Widget_Base {
+
+	/**
+	 * Current instance.
+	 *
+	 * @access protected
+	 *
+	 * @var array
+	 */
 	protected $_current_instance = [];
 
+	/**
+	 * Retrieve the widget name.
+	 *
+	 * @access public
+	 *
+	 * @return string Widget name.
+	 */
 	public function get_name() {
 		return 'audio';
 	}
 
+	/**
+	 * Retrieve the widget title.
+	 *
+	 * @access public
+	 *
+	 * @return string Widget title.
+	 */
 	public function get_title() {
 		return __( 'SoundCloud', 'elementor' );
 	}
 
+	/**
+	 * Retrieve the widget icon.
+	 *
+	 * @access public
+	 *
+	 * @return string Widget icon.
+	 */
 	public function get_icon() {
 		return 'eicon-headphones';
 	}
 
+	/**
+	 * Retrieve the list of categories the widget belongs to.
+	 *
+	 * Used to determine where to display the widget in the editor.
+	 *
+	 * @access public
+	 *
+	 * @return array Widget categories.
+	 */
 	public function get_categories() {
 		return [ 'general-elements' ];
 	}
 
+	/**
+	 * Register the widget controls.
+	 *
+	 * Adds different input fields to allow the user to change and customize the widget settings.
+	 *
+	 * @access protected
+	 */
 	protected function _register_controls() {
 		$this->start_controls_section(
 			'section_audio',
@@ -172,6 +220,13 @@ class Widget_Audio extends Widget_Base {
 
 	}
 
+	/**
+	 * Render the widget output on the frontend.
+	 *
+	 * Written in PHP and used to generate the final HTML.
+	 *
+	 * @access protected
+	 */
 	protected function render() {
 		$settings = $this->get_settings();
 
@@ -228,5 +283,12 @@ class Widget_Audio extends Widget_Base {
 		return $html;
 	}
 
+	/**
+	 * Render the widget output in the editor.
+	 *
+	 * Written as a Backbone JavaScript template and used to generate the live preview.
+	 *
+	 * @access protected
+	 */
 	protected function _content_template() {}
 }
