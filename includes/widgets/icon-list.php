19eb030968876b048df7749996c2aee19c956be3
Kobi Zaltzberg <kobi@pojo.me>
2017-10-03 16:17:09 +0300

    fixed align icons for RTL

--- includes/widgets/icon-list.php
+++ includes/widgets/icon-list.php
@@ -161,16 +161,16 @@ class Widget_Icon_List extends Widget_Base {
 				'type' => Controls_Manager::CHOOSE,
 				'options' => [
 					'left' => [
-						'title' => __( 'Left', 'elementor' ),
-						'icon' => 'fa fa-align-left',
+						'title' => __( 'Start', 'elementor' ),
+						'icon' => 'eicon-h-align-left',
 					],
 					'center' => [
 						'title' => __( 'Center', 'elementor' ),
-						'icon' => 'fa fa-align-center',
+						'icon' => 'eicon-h-align-center',
 					],
 					'right' => [
-						'title' => __( 'Right', 'elementor' ),
-						'icon' => 'fa fa-align-right',
+						'title' => __( 'End', 'elementor' ),
+						'icon' => 'eicon-h-align-right',
 					],
 				],
 				'prefix_class' => 'elementor-align-',
@@ -181,6 +181,7 @@ class Widget_Icon_List extends Widget_Base {
 					'left' => 'flex-start',
 					'right' => 'flex-end',
 				],
+				'classes' => 'elementor-control-start-end',
 			]
 		);
 
