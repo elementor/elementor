39e4a8e0bd452916b7236e87cdd4d7f632f6e698
Rami Yushuvaev <r_a_m_i@hotmail.com>
2017-10-16 15:06:44 +0300

    Add phpDocs to Elementor Base Controls

--- includes/controls/base-data.php
+++ includes/controls/base-data.php
@@ -5,12 +5,42 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 
+/**
+ * Base data control.
+ *
+ * A base control for creating data controls.
+ *
+ * @since 1.0.0
+ * @abstract
+ */
 abstract class Base_Data_Control extends Base_Control {
 
+	/**
+	 * Retrieve data control default value.
+	 *
+	 * Get the default value of the data control. Used to return the default
+	 * values while initializing the data control.
+	 *
+	 * @access public
+	 *
+	 * @return string Control default value.
+	 */
 	public function get_default_value() {
 		return '';
 	}
 
+	/**
+	 * Retrieve data control value.
+	 *
+	 * Get the value of the data control from a specific widget.
+	 *
+	 * @access public
+	 *
+	 * @param array $control Control
+	 * @param array $widget  Widget
+	 *
+	 * @return mixed Control values.
+	 */
 	public function get_value( $control, $widget ) {
 		if ( ! isset( $control['default'] ) ) {
 			$control['default'] = $this->get_default_value();
@@ -23,10 +53,34 @@ abstract class Base_Data_Control extends Base_Control {
 		return $widget[ $control['name'] ];
 	}
 
+	/**
+	 * Retrieve data control style value.
+	 *
+	 * Get the style of the control. Used when adding CSS rules to the control.
+	 *
+	 * @access public
+	 *
+	 * @param string $css_property  CSS property.
+	 * @param string $control_value Control value.
+	 *
+	 * @return string Control style value.
+	 */
 	public function get_style_value( $css_property, $control_value ) {
 		return $control_value;
 	}
 
+	/**
+	 * Retrieve data control unique ID.
+	 *
+	 * Get the unique ID of the control. Used to set a uniq CSS ID for the
+	 * element.
+	 *
+	 * @access protected
+	 *
+	 * @param string $input_type Input type. Default is 'default'.
+	 *
+	 * @return string Unique ID.
+	 */
 	protected function get_control_uid( $input_type = 'default' ) {
 		return 'elementor-control-' . $input_type . '-{{{ data._cid }}}';
 	}
--- includes/controls/base-multiple.php
+++ includes/controls/base-multiple.php
@@ -6,17 +6,43 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 /**
- * A base control for controls that return more than a single value. Extends `Base_Data_Control`.
- * Each value of a multiple-value control will be returned as an item in a key => value array
+ * Base multiple control.
+ *
+ * A base control for creating controls that return more than a single value.
+ * Each value of the multi-value control will be returned as an item in a
+ * key=>value array.
  *
  * @since 1.0.0
+ * @abstract
  */
 abstract class Control_Base_Multiple extends Base_Data_Control {
 
+	/**
+	 * Retrieve multiple control default value.
+	 *
+	 * Get the default value of the multiple control. Used to return the default
+	 * values while initializing the multiple control.
+	 *
+	 * @access public
+	 *
+	 * @return array Control default value.
+	 */
 	public function get_default_value() {
 		return [];
 	}
 
+	/**
+	 * Retrieve multiple control value.
+	 *
+	 * Get the value of the multiple control from a specific widget.
+	 *
+	 * @access public
+	 *
+	 * @param array $control Control
+	 * @param array $widget  Widget
+	 *
+	 * @return mixed Control values.
+	 */
 	public function get_value( $control, $widget ) {
 		$value = parent::get_value( $control, $widget );
 
@@ -39,6 +65,18 @@ abstract class Control_Base_Multiple extends Base_Data_Control {
 		);
 	}
 
+	/**
+	 * Retrieve multiple control style value.
+	 *
+	 * Get the style of the control. Used when adding CSS rules to the control.
+	 *
+	 * @access public
+	 *
+	 * @param string $css_property  CSS property.
+	 * @param string $control_value Control value.
+	 *
+	 * @return array Control style value.
+	 */
 	public function get_style_value( $css_property, $control_value ) {
 		return $control_value[ $css_property ];
 	}
