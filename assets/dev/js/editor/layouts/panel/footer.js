800c10c603848c000b31c68f456a49ffdebf4463
Mati Horovitz <mati@pojo.me>
2017-10-30 14:07:57 +0200

    elementor.saver fixes

--- assets/dev/js/editor/layouts/panel/footer.js
+++ assets/dev/js/editor/layouts/panel/footer.js
@@ -10,6 +10,7 @@ module.exports = Marionette.ItemView.extend( {
 	possibleRotateModes: [ 'portrait', 'landscape' ],
 
 	ui: {
+		buttonSave: '#elementor-panel-saver-publish, #elementor-panel-saver-save-draft, #elementor-panel-saver-publish-changes', // Compatibility for Pro <= 1.9.5
 		menuButtons: '.elementor-panel-footer-tool',
 		settings: '#elementor-panel-footer-settings',
 		deviceModeIcon: '#elementor-panel-footer-responsive > i',
