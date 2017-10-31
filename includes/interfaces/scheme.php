7ac1c1f0fcd2126117a30d53a9e5d911008820d4
Rami Yushuvaev <r_a_m_i@hotmail.com>
2017-10-31 19:00:22 +0200

    Add phpDocs to Scheme_Interface

--- includes/interfaces/scheme.php
+++ includes/interfaces/scheme.php
@@ -5,16 +5,51 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 
+/**
+ * Scheme interface.
+ */
 interface Scheme_Interface {
+
+	/**
+	 * Retrieve Scheme type.
+	 *
+	 * @access public
+	 * @static
+	 */
 	public static function get_type();
 
+	/**
+	 * Retrieve scheme title.
+	 *
+	 * @access public
+	 */
 	public function get_title();
 
+	/**
+	 * Retrieve scheme disabled title.
+	 *
+	 * @access public
+	 */
 	public function get_disabled_title();
 
+	/**
+	 * Retrieve scheme titles.
+	 *
+	 * @access public
+	 */
 	public function get_scheme_titles();
 
+	/**
+	 * Retrieve default scheme.
+	 *
+	 * @access public
+	 */
 	public function get_default_scheme();
 
+	/**
+	 * Print template content.
+	 *
+	 * @access public
+	 */
 	public function print_template_content();
 }
