0b69edfd89ac2fe769f759ff29233e520031f21f
Yakir Sitbon <kingyes1@gmail.com>
2017-10-31 16:52:05 +0200

    Remove inline editing from Title field from Tabs/Tuggle/Accordion widgets

--- includes/widgets/tabs.php
+++ includes/widgets/tabs.php
@@ -320,8 +320,6 @@ class Widget_Tabs extends Widget_Base {
 						'data-tab' => $counter,
 						'tabindex' => $id_int . $counter,
 					] );
-
-					$this->add_inline_editing_attributes( $tab_title_setting_key, 'none' );
 					?>
 					<div <?php echo $this->get_render_attribute_string( $tab_title_setting_key ); ?>><?php echo $item['tab_title']; ?></div>
 				<?php
@@ -372,7 +370,7 @@ class Widget_Tabs extends Widget_Base {
 				<div class="elementor-tabs-wrapper" role="tab">
 					<#
 					_.each( settings.tabs, function( item ) { #>
-						<div class="elementor-tab-title elementor-tab-desktop-title elementor-inline-editing" tabindex="{{ tabindex + counter }}" data-tab="{{ counter }}" data-elementor-setting-key="tabs.{{ counter - 1 }}.tab_title" data-elementor-inline-editing-toolbar="none">{{{ item.tab_title }}}</div>
+						<div class="elementor-tab-title elementor-tab-desktop-title" tabindex="{{ tabindex + counter }}" data-tab="{{ counter }}">{{{ item.tab_title }}}</div>
 					<#
 						counter++;
 					} ); #>
