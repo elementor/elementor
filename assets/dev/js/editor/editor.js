c749bcac624b1d682ccb768accc803b45a43dede
Kobi Zaltzberg <kobi@pojo.me>
2017-11-06 15:32:26 +0200

    Removed browser compatibility detection

--- assets/dev/js/editor/editor.js
+++ assets/dev/js/editor/editor.js
@@ -81,10 +81,6 @@ App = Marionette.Application.extend( {
 		this.modules.controls[ controlID[0].toUpperCase() + controlID.slice( 1 ) ] = ControlView;
 	},
 
-	checkEnvCompatibility: function() {
-		return this.envData.gecko || this.envData.webkit;
-	},
-
 	getElementData: function( modelElement ) {
 		var elType = modelElement.get( 'elType' );
 
@@ -134,10 +130,6 @@ App = Marionette.Application.extend( {
 
 	getPanelView: function() {
 		return this.getRegion( 'panel' ).currentView;
-	},
-
-	initEnvData: function() {
-		this.envData = _.pick( tinymce.EditorManager.Env, [ 'desktop', 'webkit', 'gecko', 'ie', 'opera' ] );
 	},
 
 	initComponents: function() {
@@ -426,12 +418,6 @@ App = Marionette.Application.extend( {
 
 		this.initComponents();
 
-		this.initEnvData();
-
-		if ( ! this.checkEnvCompatibility() ) {
-			this.onEnvNotCompatible();
-		}
-
 		this.channels.dataEditMode.reply( 'activeMode', 'edit' );
 
 		this.listenTo( this.channels.dataEditMode, 'switch', this.onEditModeSwitched );
@@ -530,22 +516,6 @@ App = Marionette.Application.extend( {
 		} else {
 			this.enterPreviewMode( 'preview' === activeMode );
 		}
-	},
-
-	onEnvNotCompatible: function() {
-		this.showFatalErrorDialog( {
-			headerMessage: this.translate( 'device_incompatible_header' ),
-			message: this.translate( 'device_incompatible_message' ),
-			strings: {
-				confirm: elementor.translate( 'proceed_anyway' )
-			},
-			hide: {
-				onButtonClick: true
-			},
-			onConfirm: function() {
-				this.hide();
-			}
-		} );
 	},
 
 	onPreviewLoadingError: function() {
@@ -768,7 +738,7 @@ App = Marionette.Application.extend( {
 		var text = '',
 			style = '';
 
-		if ( this.envData.gecko ) {
+		if ( -1 !== navigator.userAgent.search( 'Firefox' ) ) {
 			var asciiText = [
 				' ;;;;;;;;;;;;;;; ',
 				';;;  ;;       ;;;',
