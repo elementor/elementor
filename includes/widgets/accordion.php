b20a648b46ca660c00d3470620184eec44fa45bc
Rami Yushuvaev <r_a_m_i@hotmail.com>
2017-11-29 15:23:54 +0200

    Widgets: Backwards compitability for Toggle & Accordion widgets

--- includes/widgets/accordion.php
+++ includes/widgets/accordion.php
@@ -336,6 +336,14 @@ class Widget_Accordion extends Widget_Base {
 		$settings = $this->get_settings();
 
 		$id_int = substr( $this->get_id_int(), 0, 3 );
+
+		// Backwards Compitability
+		if ( ! isset( $settings['icon'] ) ) {
+			$settings['icon'] = 'fa fa-plus';
+		}
+		if ( ! isset( $settings['icon_active'] ) ) {
+			$settings['icon_active'] = 'fa fa-minus';
+		}
 		?>
 		<div class="elementor-accordion" role="tablist">
 			<?php foreach ( $settings['tabs'] as $index => $item ) :
@@ -377,6 +385,14 @@ class Widget_Accordion extends Widget_Base {
 		?>
 		<div class="elementor-accordion" role="tablist">
 			<#
+			// Backwards Compitability
+			if ( '' === settings.icon ) {
+				settings.icon = 'fa fa-plus';
+			}
+			if ( '' === settings.icon_active ) {
+				settings.icon_active = 'fa fa-minus';
+			}
+
 			if ( settings.tabs ) {
 				var tabindex = view.getIDInt().toString().substr( 0, 3 );
 
