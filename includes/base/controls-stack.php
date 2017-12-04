f06721f4376efd6fa1bca0b49ec42e28cb575bb0
Rami Yushuvaev <r_a_m_i@hotmail.com>
2017-12-04 14:13:22 +0200

    update

--- includes/base/controls-stack.php
+++ includes/base/controls-stack.php
@@ -8,8 +8,8 @@ if ( ! defined( 'ABSPATH' ) ) {
 /**
  * Controls Stack.
  *
- * An abstract class that holds all the basic properties, methods and other
- * actions that communicate with the Elementor editor panel.
+ * A base abstract class that provides the needed properties and methods to
+ * manage and handle controls  in the editor panel to inheriting classes.
  *
  * @since 1.4.0
  * @abstract
