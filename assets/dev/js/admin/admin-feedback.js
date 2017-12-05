e8385b625faad7d2786dd15c8ce708126380969a
Kobi Zaltzberg <kobi@pojo.me>
2017-12-05 11:37:27 +0200

    Adjusting dialog to the new version

--- assets/dev/js/admin/admin-feedback.js
+++ assets/dev/js/admin/admin-feedback.js
@@ -34,7 +34,7 @@
 
 			self.getModal = function() {
 				if ( ! modal ) {
-					modal = self.dialogsManager.createWidget( 'options', {
+					modal = self.dialogsManager.createWidget( 'lightbox', {
 						id: 'elementor-deactivate-feedback-modal',
 						headerMessage: self.cache.$dialogHeader,
 						message: self.cache.$dialogForm,
@@ -46,7 +46,7 @@
 							at: 'center'
 						},
 						onReady: function() {
-							DialogsManager.getWidgetType( 'options' ).prototype.onReady.apply( this, arguments );
+							DialogsManager.getWidgetType( 'lightbox' ).prototype.onReady.apply( this, arguments );
 
 							this.addButton( {
 								name: 'submit',
