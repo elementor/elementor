507eb29d7a88811cd94f622b04d9a8f03fef6c84
Kobi Zaltzberg <kobi@pojo.me>
2017-12-06 14:23:48 +0200

    Re-Render Wysiwyg controls inside repeater on sort changed in order to prevent content losing. Fixes #2897, fixes #2450, fixes #2324

--- assets/dev/js/editor/views/controls/repeater.js
+++ assets/dev/js/editor/views/controls/repeater.js
@@ -146,13 +146,13 @@ ControlRepeaterItemView = ControlBaseItemView.extend( {
 
 	onSortStop: function( event, ui ) {
 		// Reload TinyMCE editors (if exist), it's a bug that TinyMCE content is missing after stop dragging
-		ui.item.find( '.elementor-wp-editor' ).each( function() {
-			var editor = tinymce.get( this.id ),
-				settings = editor.settings;
+		var sortedRowView = this.children.findByIndex( ui.item.index() ),
+			rowControls = sortedRowView.children;
 
-			settings.height = Backbone.$( editor.getContainer() ).height();
-			tinymce.execCommand( 'mceRemoveEditor', true, this.id );
-			tinymce.init( settings );
+		rowControls.each( function( control ) {
+			if ( 'wysiwyg' === control.model.get( 'type' ) ) {
+				control.render();
+			}
 		} );
 	},
 
