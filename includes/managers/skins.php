5a6a9d3e647fdae3c1b5e831384b5cc696421bab
Rami Yushuvaev <r_a_m_i@hotmail.com>
2018-01-04 12:01:43 +0200

    Docs: add phpDocs to Skins_Manager class

--- includes/managers/skins.php
+++ includes/managers/skins.php
@@ -5,14 +5,41 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 
+/**
+ * Elementor skins manager class.
+ *
+ * Elementor skins manager handler class is responsible for registering and
+ * initializing all the supported skins.
+ *
+ * @since 1.0.0
+ */
 class Skins_Manager {
 
+	/**
+	 * Skins.
+	 *
+	 * Holds the registered skins for all the widgets.
+	 *
+	 * @since 1.0.0
+	 * @access private
+	 *
+	 * @var array Registered skins.
+	 */
 	private $_skins = [];
 
 	/**
+	 * Add new skin.
+	 *
+	 * Register a single new skin for a widget.
+	 *
 	 * @since 1.0.0
 	 * @access public
-	*/
+	 *
+	 * @param Widget_Base $widget Elementor widget.
+	 * @param Skin_Base   $skin   Elementor skin.
+	 *
+	 * @return true True if skin added.
+	 */
 	public function add_skin( Widget_Base $widget, Skin_Base $skin ) {
 		$widget_name = $widget->get_name();
 
@@ -26,9 +53,18 @@ class Skins_Manager {
 	}
 
 	/**
+	 * Remove a skin.
+	 *
+	 * Unregister an existing skin from a widget.
+	 *
 	 * @since 1.0.0
 	 * @access public
-	*/
+	 *
+	 * @param Widget_Base $widget  Elementor widget.
+	 * @param string      $skin_id Elementor skin ID.
+	 *
+	 * @return true|\WP_Error True if skin removed, `WP_Error` otherwise.
+	 */
 	public function remove_skin( Widget_Base $widget, $skin_id ) {
 		$widget_name = $widget->get_name();
 
@@ -42,9 +78,17 @@ class Skins_Manager {
 	}
 
 	/**
+	 * Get skins.
+	 *
+	 * Retrieve all the skins assigned for a specific widget.
+	 *
 	 * @since 1.0.0
 	 * @access public
-	*/
+	 *
+	 * @param Widget_Base $widget Elementor widget.
+	 *
+	 * @return false|array Skins, False otherwise.
+	 */
 	public function get_skins( Widget_Base $widget ) {
 		$widget_name = $widget->get_name();
 
@@ -56,9 +100,13 @@ class Skins_Manager {
 	}
 
 	/**
+	 * Skins manager constructor.
+	 *
+	 * Initializing Elementor skins manager by requiring the skin base class.
+	 *
 	 * @since 1.0.0
 	 * @access public
-	*/
+	 */
 	public function __construct() {
 		require ELEMENTOR_PATH . 'includes/base/skin-base.php';
 	}
