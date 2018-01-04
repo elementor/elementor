7235d5a792461ce125c1483dfd57ac17e0cc5651
Mati Horovitz <mati@pojo.me>
2018-01-04 11:12:17 +0200

    jscs

--- assets/js/editor.js
+++ assets/js/editor.js
@@ -12828,7 +12828,7 @@ var RevisionModel = require( './model' );
 
 module.exports = Backbone.Collection.extend( {
 	model: RevisionModel,
-	comparator: function ( model ) {
+	comparator: function( model ) {
 		return -model.get( 'timestamp' );
 	}
 } );
