64be7bd3a7417248a2a51ed677f2fde1164409be
Kobi Zaltzberg <kobi@pojo.me>
2018-01-10 14:35:39 +0200

    Make tags module more generic

--- modules/tags/module.php
+++ modules/tags/module.php
@@ -2,7 +2,7 @@
 namespace Elementor\Modules\Tags;
 
 use Elementor\Core\Base\Module as BaseModule;
-use Elementor\Core\MicroElements\Manager as MicroElementsManager;
+use Elementor\Plugin;
 
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
@@ -11,30 +11,36 @@ class Module extends BaseModule {
 	const DEFAULT_GROUP = 'base';
 
 	public function __construct() {
-		parent::__construct();
+		$this->register_groups();
 
-		$this->register_default_group();
-
-		$this->register_default_tags();
+		$this->register_tags();
 	}
 
 	public function get_name() {
 		return 'micro-elements';
 	}
 
-	private function register_default_group() {
-		MicroElementsManager::register_group( self::DEFAULT_GROUP, [
-			'title' => __( 'Base Tags', 'elementor' ),
-		] );
+	public function get_tag_classes_names() {
+		return [ 'Icon' ];
 	}
 
-	private function register_default_tags() {
-		$tags_classes = [
-			'Icon'
+	public function get_groups() {
+		return [
+			self::DEFAULT_GROUP => [
+				'title' => __( 'Base Tags', 'elementor' ),
+			],
 		];
+	}
 
-		foreach ( $tags_classes as $tag_class ) {
-			MicroElementsManager::register_tag( __NAMESPACE__ . '\Tags\\' .$tag_class );
+	private function register_groups() {
+		foreach ( $this->get_groups() as $group_name => $group_settings ) {
+			Plugin::$instance->micro_elements_manager->register_group( $group_name, $group_settings );
+		}
+	}
+
+	private function register_tags() {
+		foreach ( $this->get_tag_classes_names() as $tag_class ) {
+			Plugin::$instance->micro_elements_manager->register_tag( $this->get_reflection()->getNamespaceName() . '\Tags\\' .$tag_class );
 		}
 	}
 }
