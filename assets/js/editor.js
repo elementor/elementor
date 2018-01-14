edf48b4c544917cf430e881b04b35d16d33e9a34
Yakir Sitbon <kingyes1@gmail.com>
2018-01-14 19:35:47 +0200

    Revert

--- assets/js/editor.js
+++ assets/js/editor.js
@@ -8002,10 +8002,6 @@ PanelElementsCategoryView = Marionette.CompositeView.extend( {
 
 	className: 'elementor-panel-category',
 
-	id: function() {
-		return 'elementor-panel-category-' + this.model.get( 'name' );
-	},
-
 	childView: require( 'elementor-panel/pages/elements/views/element' ),
 
 	childViewContainer: '.panel-elements-category-items',
