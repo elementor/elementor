06ca3a9fd650c8830e3c5e2ff5f05ebeea30ede3
Kobi Zaltzberg <kobi@pojo.me>
2018-01-23 13:04:28 +0200

    Rename classes

--- modules/dynamic-tags/tags/icon.php
+++ modules/dynamic-tags/tags/icon.php
@@ -2,12 +2,12 @@
 namespace Elementor\Modules\DynamicTags\Tags;
 
 use Elementor\Controls_Manager;
-use Elementor\Core\DynamicTags\Markup_Dynamic_Tag;
+use Elementor\Core\DynamicTags\Markup_Tag;
 use Elementor\Modules\DynamicTags\Module;
 
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
-class Icon extends Markup_Dynamic_Tag {
+class Icon extends Markup_Tag {
 
 	public function get_name() {
 		return 'icon';
