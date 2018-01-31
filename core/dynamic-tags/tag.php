e3c5251f44567920402e0e302e933dc488dc62f3
Kobi Zaltzberg <kobi@pojo.me>
2018-01-31 15:11:19 +0200

    Added ability to render tag with/out wrapping

--- core/dynamic-tags/tag.php
+++ core/dynamic-tags/tag.php
@@ -8,11 +8,19 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
 abstract class Tag extends Base_Tag {
 
-	final public function get_content() {
+	final public function get_content( array $options = [] ) {
 		ob_start();
-		?>
-		<span id="elementor-tag-<?php echo $this->get_id(); ?>" class="elementor-tag"><?php $this->render(); ?></span>
-		<?php
+
+		if ( ! empty( $options[ 'wrap' ] ) ) { ?>
+			<span id="elementor-tag-<?php echo $this->get_id(); ?>" class="elementor-tag">
+		<?php }
+
+		$this->render();
+
+		if ( ! empty( $options[ 'wrap' ] ) ) { ?>
+			</span>
+		<?php }
+
 		return ob_get_clean();
 	}
 
