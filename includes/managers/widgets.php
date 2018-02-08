b64df1360dbeb2494dc4c15803ea90c5736bd431
Mati Horovitz <mati@pojo.me>
2018-02-08 17:55:04 +0200

    widgets: remove unnecessary `query_posts`

--- includes/managers/widgets.php
+++ includes/managers/widgets.php
@@ -294,19 +294,11 @@ class Widgets_Manager {
 			throw new \Exception( 'Access denied.' );
 		}
 
-		// Override the global $post for the render.
-		query_posts(
-			[
-				'p' => $request['post_id'],
-				'post_type' => 'any',
-			]
-		);
-
 		$editor = Plugin::$instance->editor;
 		$is_edit_mode = $editor->is_edit_mode();
+		$editor->set_edit_mode( true );
 
 		Plugin::$instance->db->switch_to_post( $request['post_id'] );
-		$editor->set_edit_mode( true );
 
 		$data = $request['data'];
 
