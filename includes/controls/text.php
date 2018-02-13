4735f9e6c8929d67a9ab7b59577e24d149986ce4
Kobi Zaltzberg <kobi@pojo.me>
2018-02-13 17:24:01 +0200

    Handling groups and categories

--- includes/controls/text.php
+++ includes/controls/text.php
@@ -112,7 +112,7 @@ class Control_Text extends Base_Data_Control {
 	protected function get_default_settings() {
 		return [
 			'dynamic' => [
-				'groups' => [ TagsModule::DEFAULT_GROUP ],
+				'categories' => [ TagsModule::TEXT_CATEGORY ],
 			],
 		];
 	}
