21eef8d780ccbe5b3ca2204d3f03623b795fc152
Kobi Zaltzberg <kobi@pojo.me>
2018-02-27 12:42:54 +0200

    Updated Eicons font

--- includes/admin.php
+++ includes/admin.php
@@ -90,7 +90,7 @@ class Admin {
 			'elementor-icons',
 			ELEMENTOR_ASSETS_URL . 'lib/eicons/css/elementor-icons' . $suffix . '.css',
 			[],
-			ELEMENTOR_VERSION
+			'3.1.0'
 		);
 
 		wp_register_style(
