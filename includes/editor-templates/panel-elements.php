2a0b4922eac4eae455c530973713db8b123f88ce
Kobi Zaltzberg <kobi@pojo.me>
2018-03-06 19:41:16 +0200

    Fixed active state class

--- includes/editor-templates/panel-elements.php
+++ includes/editor-templates/panel-elements.php
@@ -7,7 +7,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 ?>
 <script type="text/template" id="tmpl-elementor-panel-elements">
 	<div id="elementor-panel-elements-navigation" class="elementor-panel-navigation">
-		<div id="elementor-panel-elements-navigation-all" class="elementor-panel-navigation-tab active" data-view="categories"><?php echo __( 'Elements', 'elementor' ); ?></div>
+		<div id="elementor-panel-elements-navigation-all" class="elementor-panel-navigation-tab elementor-active" data-view="categories"><?php echo __( 'Elements', 'elementor' ); ?></div>
 		<div id="elementor-panel-elements-navigation-global" class="elementor-panel-navigation-tab" data-view="global"><?php echo __( 'Global', 'elementor' ); ?></div>
 	</div>
 	<div id="elementor-panel-elements-search-area"></div>
