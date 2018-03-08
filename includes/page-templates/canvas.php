062483c75f1e6ee35dac2cecd69cbe7db5769b46
Mati Horovitz <mati@pojo.me>
2018-03-08 11:27:02 +0200

    canvas: fix head

--- includes/page-templates/canvas.php
+++ includes/page-templates/canvas.php
@@ -5,16 +5,16 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 ?>
 <!DOCTYPE html>
-<html <?php language_attributes(); ?> class="no-js">
-	<head>
-		<meta charset="<?php bloginfo( 'charset' ); ?>">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-		<?php if ( ! current_theme_supports( 'title-tag' ) ) : ?>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<?php if ( ! current_theme_supports( 'title-tag' ) ) : ?>
 		<title><?php echo wp_get_document_title(); ?></title>
-		<?php endif; ?>
-		<?php wp_head(); ?>
-	</head>
-	<body <?php body_class(); ?>>
+	<?php endif; ?>
+	<?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
 	<?php
 	/**
 	 * Before canvas page template content.
