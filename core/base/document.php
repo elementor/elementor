38f1248d9868702fd55a835567a568b566c00324
Mati Horovitz <mati@pojo.me>
2018-03-11 18:16:11 +0200

    saver: get 'page is live' message from the document

--- core/base/document.php
+++ core/base/document.php
@@ -187,6 +187,9 @@ abstract class Document extends Controls_Stack {
 			'id' => $this->get_main_id(),
 			'type' => $this->get_name(),
 			'last_edited' => $this->get_last_edited(),
+			'messages' => [
+				'publish_notification' => sprintf( __( 'Hurray! Your %s is live.', 'elementor' ), $this::get_title() ),
+			],
 			'urls' => [
 				'exit_to_dashboard' => $this->get_exit_to_dashboard_url(),
 				'preview' => $this->get_preview_url(),
