c7af443d66bdf5d25105f55a7a42f9b588434a7f
Kobi Zaltzberg <kobi@pojo.me>
2018-03-12 11:14:25 +0200

    `elementor-repeater-fields` => `elementor-repeater-fields-wrapper`

--- includes/controls/repeater.php
+++ includes/controls/repeater.php
@@ -237,7 +237,7 @@ class Control_Repeater extends Base_Data_Control {
 		<label>
 			<span class="elementor-control-title">{{{ data.label }}}</span>
 		</label>
-		<div class="elementor-repeater-fields"></div>
+		<div class="elementor-repeater-fields-wrapper"></div>
 		<div class="elementor-button-wrapper">
 			<button class="elementor-button elementor-button-default elementor-repeater-add" type="button">
 				<i class="eicon-plus" aria-hidden="true"></i><?php _e( 'Add Item', 'elementor' ); ?>
