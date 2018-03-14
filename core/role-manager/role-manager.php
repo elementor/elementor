b7a9d322654ad90920404a83bcc2375d7870f508
Yakir Sitbon <kingyes1@gmail.com>
2018-03-14 19:30:25 +0200

    Fix text domain

--- core/role-manager/role-manager.php
+++ core/role-manager/role-manager.php
@@ -103,7 +103,7 @@ class Role_Manager extends Settings_Page {
 		<div class="elementor-role-row <?php esc_attr( $role_slug ); ?>">
 			<div class="elementor-role-label">
 				<span><?php echo esc_html( $role_data['name'] ); ?></span>
-				<span data-excluded-label="<?php esc_attr_e( 'Role Excluded' ); ?>" class="elementor-role-excluded-indicator"></span>
+				<span data-excluded-label="<?php esc_attr_e( 'Role Excluded', 'elementor' ); ?>" class="elementor-role-excluded-indicator"></span>
 				<span class="elementor-role-toggle"><span class="dashicons dashicons-arrow-down"></span></span>
 			</div>
 			<div class="elementor-role-controls hidden">
