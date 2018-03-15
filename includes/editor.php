5a007f07c69a60cdcfdd431092c59ec80806cf82
Yakir Sitbon <kingyes1@gmail.com>
2018-03-15 17:45:29 +0200

    Remove global settings from non-administrator users

--- includes/editor.php
+++ includes/editor.php
@@ -524,6 +524,7 @@ class Editor {
 			'locked_user' => $locked_user,
 			'user' => [
 				'restrictions' => $plugin->role_manager->get_user_restrictions_array(),
+				'is_administrator' => current_user_can( 'manage_options' ),
 			],
 			'is_rtl' => is_rtl(),
 			'locale' => get_locale(),
