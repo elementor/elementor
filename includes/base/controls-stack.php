88996d698fa2a7c65a01227ed896497be548f26b
Mati Horovitz <mati@pojo.me>
2018-03-18 19:57:16 +0200

    control-stack: sanitize data only after the data is initialized and some `get_controls` that uses the data - can get the data.

--- includes/base/controls-stack.php
+++ includes/base/controls-stack.php
@@ -1831,7 +1831,9 @@ abstract class Controls_Stack {
 	 * @access protected
 	 */
 	protected function _init( $data ) {
-		$this->_data = $this->sanitize_initial_data( array_merge( $this->get_default_data(), $data ) );
+		$this->_data = array_merge( $this->get_default_data(), $data );
+
+		$this->_data = $this->sanitize_initial_data( $this->_data );
 
 		$this->_id = $data['id'];
 
