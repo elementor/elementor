5bf4497aab19b39ead828c38856ed49296410a31
Kobi Zaltzberg <kobi@pojo.me>
2018-03-22 16:51:17 +0200

    Removed old deprecated methods

--- includes/plugin.php
+++ includes/plugin.php
@@ -345,23 +345,6 @@ class Plugin {
 	public $beta_testers;
 
 	/**
-	 * Get version.
-	 *
-	 * Retrieve the current version of Elementor.
-	 *
-	 * @since 1.0.0
-	 * @deprecated 1.2.0 Use `ELEMENTOR_VERSION` const instead.
-	 * @access public
-	 *
-	 * @return string Elementor version.
-	 */
-	public function get_version() {
-		_deprecated_function( sprintf( '%1$s::%2$s', get_called_class(), __FUNCTION__ ), '1.2.0', 'ELEMENTOR_VERSION' );
-
-		return ELEMENTOR_VERSION;
-	}
-
-	/**
 	 * Clone.
 	 *
 	 * Disable class cloning and throw an error on object clone.
