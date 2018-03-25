8b3b25650a6bdf81a3f044aaa9a58f5a0efe912d
Yakir Sitbon <kingyes1@gmail.com>
2018-03-25 14:27:37 +0300

    coding

--- includes/css-file/css-file.php
+++ includes/css-file/css-file.php
@@ -343,7 +343,6 @@ abstract class CSS_File {
 				$output_css_property = preg_replace_callback(
 					'/\{\{(?:([^.}]+)\.)?([^}]*)}}/', function( $matches ) use ( $control, $value_callback, $controls_stack, $value, $css_property ) {
 						$parser_control = $control;
-
 						$value_to_insert = $value;
 
 						if ( ! empty( $matches[1] ) ) {
@@ -352,7 +351,6 @@ abstract class CSS_File {
 							}
 
 							$parser_control = $controls_stack[ $matches[1] ];
-
 							$value_to_insert = call_user_func( $value_callback, $parser_control );
 						}
 
@@ -362,7 +360,6 @@ abstract class CSS_File {
 
 						/** @var Base_Data_Control $control_obj */
 						$control_obj = Plugin::$instance->controls_manager->get_control( $parser_control['type'] );
-
 						$parsed_value = $control_obj->get_style_value( strtolower( $matches[2] ), $value_to_insert );
 
 						if ( '' === $parsed_value ) {
