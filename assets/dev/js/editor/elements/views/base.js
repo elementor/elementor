7be51d9fa57f8b13da2f106179cd9a4dbb3268ad
Kobi Zaltzberg <kobi@pojo.me>
2018-03-28 16:56:27 +0300

    Separating to a different method

--- assets/dev/js/editor/elements/views/base.js
+++ assets/dev/js/editor/elements/views/base.js
@@ -448,6 +448,16 @@ BaseElementView = BaseContainer.extend( {
 		return data;
 	},
 
+	save: function() {
+		var model = this.model;
+
+		elementor.templates.startModal( {
+			onReady: function() {
+				elementor.templates.getLayout().showSaveTemplateView( model );
+			}
+		} );
+	},
+
 	onBeforeRender: function() {
 		this.renderAttributes = {};
 	},
@@ -531,13 +541,7 @@ BaseElementView = BaseContainer.extend( {
 	onClickSave: function( event ) {
 		event.preventDefault();
 
-		var model = this.model;
-
-		elementor.templates.startModal( {
-			onReady: function() {
-				elementor.templates.getLayout().showSaveTemplateView( model );
-			}
-		} );
+		this.save();
 	},
 
 	onDestroy: function() {
