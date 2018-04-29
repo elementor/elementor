4adbc8125586cd82f9ad56423aa7177016ade927
Yakir Sitbon <kingyes1@gmail.com>
2018-04-29 14:40:47 +0300

    No need to store static varibale

--- core/dynamic-tags/manager.php
+++ core/dynamic-tags/manager.php
@@ -227,7 +227,7 @@ class Manager {
 	 * @param string $tag_name
 	 */
 	public function unregister_tag( $tag_name ) {
-		unset ( $this->tags_info[ $tag_name ] );
+		unset( $this->tags_info[ $tag_name ] );
 	}
 
 	/**
