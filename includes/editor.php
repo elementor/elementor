77a4118d6d90fb66966a3d54e861bb1da957b778
Kobi Zaltzberg <kobi@pojo.me>
2018-05-03 09:33:04 +0300

    Updated Eicons

--- includes/editor.php
+++ includes/editor.php
@@ -754,7 +754,7 @@ class Editor {
 			'elementor-icons',
 			ELEMENTOR_ASSETS_URL . 'lib/eicons/css/elementor-icons' . $suffix . '.css',
 			[],
-			'3.3.0'
+			'3.4.0'
 		);
 
 		wp_register_style(
