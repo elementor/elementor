d65a696f16d543ff4e326e5f8ba56b1b78e8ec4f
Yakir Sitbon <kingyes1@gmail.com>
2018-06-07 18:12:25 +0300

    [phpcs]

--- modules/wp-cli/command.php
+++ modules/wp-cli/command.php
@@ -7,7 +7,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly
 }
 
-class Commands extends \WP_CLI_Command {
+class Command extends \WP_CLI_Command {
 
 	/**
 	 * Regenerate the Elementor Page Builder CSS.
@@ -21,7 +21,7 @@ class Commands extends \WP_CLI_Command {
 	 *      - This will regenerate the CSS files for elementor page builder.
 	 *
 	 *  2. wp elementor regenerate-css --network
-	 *  	- This will regenerate the CSS files for elementor page builder on all the sites in network.
+	 *      - This will regenerate the CSS files for elementor page builder on all the sites in network.
 	 *
 	 * @alias regenerate-css
 	 */
