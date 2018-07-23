69af3a1553b3b55940ad467fec1a68a6a0bc9e09
Yakir Sitbon <kingyes1@gmail.com>
2018-07-23 19:14:18 +0300

    Remove comment on role manager action

--- core/role-manager/role-manager.php
+++ core/role-manager/role-manager.php
@@ -240,6 +240,6 @@ class Role_Manager extends Settings_Page {
 		parent::__construct();
 
 		add_action( 'admin_menu', [ $this, 'register_admin_menu' ], 100 );
-		// TODO: Restore it after the release of Pro v2.0 // add_action( 'elementor/role/restrictions/controls', [ $this, 'get_go_pro_link_html' ] );
+		add_action( 'elementor/role/restrictions/controls', [ $this, 'get_go_pro_link_html' ] );
 	}
 }
