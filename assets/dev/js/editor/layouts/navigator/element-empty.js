cb75a0b63536a3bd6cc48fbf57c2dad5a32182bb
Kobi Zaltzberg <kobi@pojo.me>
2018-07-29 12:29:29 +0300

    Navigator: `distance` => `indent`

--- assets/dev/js/editor/layouts/navigator/element-empty.js
+++ assets/dev/js/editor/layouts/navigator/element-empty.js
@@ -4,6 +4,6 @@ module.exports = Marionette.ItemView.extend( {
 	className: 'elementor-empty-view',
 
 	onRender: function() {
-		this.$el.css( 'padding-' + ( elementor.config.is_rtl ? 'right' : 'left' ), this.getOption( 'distance' ) );
+		this.$el.css( 'padding-' + ( elementor.config.is_rtl ? 'right' : 'left' ), this.getOption( 'indent' ) );
 	}
 } );
