5a1f01a42ab5c7f57f36f0d6c481ee9b994cbe17
OmriBar-Zik <omri@bar-zik.com>
2018-08-08 09:14:02 +0300

    Testing: change - deleted skiped tests.

--- tests/phpunit/elementor/includes/test-maintenance-mode.php
+++ tests/phpunit/elementor/includes/test-maintenance-mode.php
@@ -41,33 +41,4 @@ class Elementor_Test_Maintenance_Mode extends Elementor_Test_Base {
 		$this->maintenance_mode->template_redirect();
 		$this->assertNotNull( $GLOBALS['post'] );
 	}
-
-	/**
-	 *
-	 */
-	public function test_maintenance_mode_should_be_active() {
-		$this->markTestSkipped();
-		$template_id = $this->factory()->get_local_template_id();
-		update_option( 'elementor_maintenance_mode_mode', 'coming_soon' );
-		update_option( 'elementor_maintenance_mode_template_id', $template_id );
-
-		query_posts( '' );
-
-		$this->maintenance_mode->template_redirect();
-		// Set current queried object id.
-		global $wp_query, $post;
-		$wp_query->post = $post;
-
-		ob_start();
-
-		require_once get_index_template();
-
-		$content = ob_get_contents();
-
-		var_dump( $content );
-		$this->assertContains( 'elementor-maintenance-mode', $content );
-		$this->assertContains( 'elementor-page-' . $template_id, $content );
-		//Assert `file_contents_get( home_url() )` contains 'elementor-maintenance-mode'  &&  "elementor-page-$post_id"
-
-	}
 }
