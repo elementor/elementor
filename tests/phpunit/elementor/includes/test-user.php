5180441c5d55e795f196c654604fa702c682f252
OmriBar-Zik <omri@bar-zik.com>
2018-08-08 12:34:08 +0300

    Testing\Includes\User: Change - rename methods and optimize.

--- tests/phpunit/elementor/includes/test-user.php
+++ tests/phpunit/elementor/includes/test-user.php
@@ -5,67 +5,68 @@ use \Elementor\User;
 
 class Elementor_Test_User extends \WP_Ajax_UnitTestCase {
 
+	private static $administrator;
+	private static $subscriber;
+	private $fake_notice_id = '123';
 
-	public function test_should_return_false_when_sending_bad_type_to_is_current_user_can_edit() {
+	public static function setUpBeforeClass() {
+		parent::setUpBeforeClass();
+		self::$administrator = self::factory()->user->create( [ 'role' => 'administrator' ] );
+		self::$subscriber = self::factory()->user->create( [ 'role' => 'subscriber' ] );
+	}
 
-		$post_id = $this->factory()->post->create( [ 'post_type' => 'banana' ] );
+	public function test_should_return_false_because_of_invalid_post_type() {
+		$post_id = $this->factory()->post->create( [ 'post_type' => 'invalid post type' ] );
 		$this->assertFalse( User::is_current_user_can_edit( $post_id ) );
 	}
 
-	public function test_should_tell_if_current_user_can_edit_bad_post_status() {
+	public function test_should_return_false_because_of_bad_post_Status() {
 		$post_id = $this->factory()->post->create( [ 'post_status' => 'trash' ] );
 		$this->assertFalse( User::is_current_user_can_edit( $post_id ) );
 	}
 
-	public function test_should_tell_if_current_user_can_edit_user_cant_edit() {
-		$post_id = $this->factory()->post->create( [ 'post_status' => 'post' ] );
+	public function test_if_current_user_can_edit() {
+		$post_id = $this->factory()->post->create();
+		wp_set_current_user( self::$subscriber );
+
 		$this->assertFalse( User::is_current_user_can_edit( $post_id ) );
-	}
 
-	public function test_should_tell_if_current_user_can_edit_passes_all() {
-		wp_set_current_user( $this->factory()->user->create( [ 'role' => 'administrator' ] ) );
-
-		$post_id = $this->factory()->post->create( [ 'post_status' => 'post' ] );
+		wp_set_current_user( self::$administrator );
 
 		$this->assertTrue( User::is_current_user_can_edit( $post_id ) );
 	}
 
-	public function test_is_current_user_can_edit_post_type_should_return_true() {
+	public function test_if_current_user_can_edit_specific_post_type() {
+		wp_set_current_user( self::$administrator );
 		$this->assertTrue( User::is_current_user_can_edit_post_type( 'page' ) );
+		$this->assertTrue( User::is_current_user_can_edit_post_type( 'post' ) );
 	}
 
-	public function test_is_current_user_can_edit_post_type_should_return_false_when_the_post_type_dose_not_exists() {
-		$this->assertFalse( User::is_current_user_can_edit_post_type( 'banana' ) );
-	}
-
-	public function test_is_current_user_can_edit_post_type_should_return_false_when_the_post_type_dose_supported() {
-		wp_set_current_user( $this->factory()->user->create( [ 'role' => 'Subscriber' ] ) );
-
-		$this->assertFalse( User::is_current_user_can_edit_post_type( 'attachment' ) );
-
-		wp_set_current_user( $this->factory()->user->create( [ 'role' => 'administrator' ] ) );
+	public function test_if_current_user_can_edit_post_type_that_is_not_existing() {
+		wp_set_current_user( self::$administrator );
+		$this->assertFalse( User::is_current_user_can_edit_post_type( 'invalid post type' ) );
 	}
 
 	/**
-	 * @expectedException Exception
+	 * @expectedException \Exception
 	 * @group ajax
 	 */
-	public function test_should_throw_error_by_ajax_set_admin_notice_viewed() {
+	public function test_should_throw_error_by_ajax_from_set_admin_notice_viewed() {
 		User::init();
 
 		$this->_handleAjax( 'elementor_set_admin_notice_viewed' );
 	}
 
 	public function test_should_not_notice_viewed() {
-		$this->assertFalse( User::is_user_notice_viewed( 1 ) );
+		$this->assertFalse( User::is_user_notice_viewed( $this->fake_notice_id ) );
 	}
 
 	/**
 	 * @group ajax
 	 */
-	public function test_should_set_admin_notice_viewed_by_ajax() {
+	public function test_should_set_notice_as_viewed_by_admin() {
 		User::init();
-		$_POST['notice_id'] = '123';
+		$_POST['notice_id'] = $this->fake_notice_id;
 		try {
 			$this->_handleAjax( 'elementor_set_admin_notice_viewed' );
 		} catch ( \WPAjaxDieContinueException $e ) {
@@ -76,16 +77,4 @@ class Elementor_Test_User extends \WP_Ajax_UnitTestCase {
 
 		$this->assertTrue( $response['success'] );
 	}
-
-
-	// TODO learn how to use elementor ajax systems.
-	/*public function test_should_set_elementor_admin_notice_viewed() {
-		User::init();
-		\Elementor\Plugin::$instance->ajax->handle_ajax_request();
-
-		$this->assertSame( '', 'asd' );
-	}*/
-	// TODO end.
-
-
 }
