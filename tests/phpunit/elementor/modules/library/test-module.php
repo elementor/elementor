de6d9fb685066886ef24cffc4502c48fb85d41d8
OmriBar-Zik <omri@bar-zik.com>
2018-08-13 13:54:58 +0300

    Tests/Modules/Library: new - tests & asserts.

--- tests/phpunit/elementor/modules/library/test-module.php
+++ tests/phpunit/elementor/modules/library/test-module.php
@@ -1,20 +1,43 @@
 <?php
 namespace Elementor\Testing\Modules\Library;
 
-use Elementor\Plugin;
+use Elementor\Modules\Library\Module;
 use Elementor\Testing\Elementor_Test_Base;
 
 class Elementor_Test_Module extends Elementor_Test_Base {
+	/** @var Module */
+	private static $module;
+
+	public function test_should_confirm_module_activation() {
+		self::$module = new Module();
+		$this->assertDocumentTypeRegistered('page');
+		$this->assertDocumentTypeRegistered('section');
+		$this->assertDocumentGroupRegistered('blocks');
+		$this->assertDocumentGroupRegistered('pages');
+	}
 
 	public function test_should_return_library() {
-		$this->assertEquals( 'library', ( new \Elementor\Modules\Library\Module() )->get_name() );
+		$this->assertEquals( 'library', self::$module->get_name() );
 	}
 
 	public function test_should_localize_settings() {
+		$res = self::$module->localize_settings( [] );
 		$this->assertEquals(
 			[
 				'i18n' => [],
-			], ( new \Elementor\Modules\Library\Module() )->localize_settings( [] )
+			], $res
+		);
+
+		$res = self::$module->localize_settings( [
+			'content one' => [],
+			'content two' => [],
+		] );
+		$this->assertEquals(
+			[
+				'content one' => [],
+				'content two' => [],
+				'i18n' => [],
+			], $res
 		);
 	}
 
