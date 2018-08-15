7786feb02247a091133739cc276ddeae08b161a5
Dakotah <dakotah@dintriglia.com>
2018-08-15 09:29:55 -0600

    Added Fixed Height
    - Added fixed height to Section, so a forced height could be added instead of just having min-height.

--- includes/elements/section.php
+++ includes/elements/section.php
@@ -352,6 +352,7 @@ class Element_Section extends Element_Base {
 					'default' => __( 'Default', 'elementor' ),
 					'full' => __( 'Fit To Screen', 'elementor' ),
 					'min-height' => __( 'Min Height', 'elementor' ),
+					'fixed-height' => __( 'Fixed Height', 'elementor' ),
 				],
 				'prefix_class' => 'elementor-section-height-',
 				'hide_in_inner' => true,
@@ -382,6 +383,30 @@ class Element_Section extends Element_Base {
 				],
 				'condition' => [
 					'height' => [ 'min-height' ],
+				],
+				'hide_in_inner' => true,
+			]
+		);
+		
+		$this->add_responsive_control(
+			'custom_height_fixed',
+			[
+				'label' => __( 'Fixed Height', 'elementor' ),
+				'type' => Controls_Manager::SLIDER,
+				'default' => [
+					'size' => 400,
+				],
+				'range' => [
+					'px' => [
+						'min' => 0,
+						'max' => 1440,
+					],
+				],
+				'selectors' => [
+					'{{WRAPPER}} > .elementor-container' => 'height: {{SIZE}}{{UNIT}};',
+				],
+				'condition' => [
+					'height' => [ 'fixed-height' ],
 				],
 				'hide_in_inner' => true,
 			]
