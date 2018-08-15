2d73d5f97104f351125168b61b268797a068ab03
Yakir Sitbon <kingyes1@gmail.com>
2018-08-15 19:03:11 +0300

    Move feedback methods to own class

--- core/admin/admin.php
+++ core/admin/admin.php
@@ -13,6 +13,11 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 class Admin {
 
+	/**
+	 * @var \Elementor\Core\Admin\Feedback
+	 */
+	private $feedback;
+
 	public function maybe_redirect_to_getting_started() {
 		if ( ! get_transient( 'elementor_activation_redirect' ) ) {
 			return;
@@ -84,12 +89,6 @@ class Admin {
 		);
 
 		wp_enqueue_script( 'elementor-admin-app' );
-
-		if ( in_array( get_current_screen()->id, [ 'plugins', 'plugins-network' ], true ) ) {
-			add_action( 'admin_footer', [ $this, 'print_deactivate_feedback_dialog' ] );
-
-			$this->enqueue_feedback_dialog_scripts();
-		}
 	}
 
 	/**
@@ -449,115 +448,6 @@ class Admin {
 	}
 
 	/**
-	 * Enqueue feedback dialog scripts.
-	 *
-	 * Registers the feedback dialog scripts and enqueues them.
-	 *
-	 * @since 1.0.0
-	 * @access public
-	 */
-	public function enqueue_feedback_dialog_scripts() {
-		$suffix = defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ? '' : '.min';
-
-		wp_register_script(
-			'elementor-admin-feedback',
-			ELEMENTOR_ASSETS_URL . 'js/admin-feedback' . $suffix . '.js',
-			[
-				'jquery',
-				'underscore',
-				'elementor-dialog',
-			],
-			ELEMENTOR_VERSION,
-			true
-		);
-
-		wp_enqueue_script( 'elementor-admin-feedback' );
-
-		wp_localize_script(
-			'elementor-admin-feedback',
-			'ElementorAdminFeedbackArgs',
-			[
-				'is_tracker_opted_in' => Tracker::is_allow_track(),
-				'i18n' => [
-					'submit_n_deactivate' => __( 'Submit & Deactivate', 'elementor' ),
-					'skip_n_deactivate' => __( 'Skip & Deactivate', 'elementor' ),
-				],
-			]
-		);
-	}
-
-	/**
-	 * Print deactivate feedback dialog.
-	 *
-	 * Display a dialog box to ask the user why he deactivated Elementor.
-	 *
-	 * Fired by `admin_footer` filter.
-	 *
-	 * @since 1.0.0
-	 * @access public
-	 */
-	public function print_deactivate_feedback_dialog() {
-		$deactivate_reasons = [
-			'no_longer_needed' => [
-				'title' => __( 'I no longer need the plugin', 'elementor' ),
-				'input_placeholder' => '',
-			],
-			'found_a_better_plugin' => [
-				'title' => __( 'I found a better plugin', 'elementor' ),
-				'input_placeholder' => __( 'Please share which plugin', 'elementor' ),
-			],
-			'couldnt_get_the_plugin_to_work' => [
-				'title' => __( 'I couldn\'t get the plugin to work', 'elementor' ),
-				'input_placeholder' => '',
-			],
-			'temporary_deactivation' => [
-				'title' => __( 'It\'s a temporary deactivation', 'elementor' ),
-				'input_placeholder' => '',
-			],
-			'elementor_pro' => [
-				'title' => __( 'I have Elementor Pro', 'elementor' ),
-				'input_placeholder' => '',
-				'alert' => __( 'Wait! Don\'t deactivate Elementor. You have to activate both Elementor and Elementor Pro in order for the plugin to work.', 'elementor' ),
-			],
-			'other' => [
-				'title' => __( 'Other', 'elementor' ),
-				'input_placeholder' => __( 'Please share the reason', 'elementor' ),
-			],
-		];
-
-		?>
-		<div id="elementor-deactivate-feedback-dialog-wrapper">
-			<div id="elementor-deactivate-feedback-dialog-header">
-				<i class="eicon-elementor-square" aria-hidden="true"></i>
-				<span id="elementor-deactivate-feedback-dialog-header-title"><?php echo __( 'Quick Feedback', 'elementor' ); ?></span>
-			</div>
-			<form id="elementor-deactivate-feedback-dialog-form" method="post">
-				<?php
-				wp_nonce_field( '_elementor_deactivate_feedback_nonce' );
-				?>
-				<input type="hidden" name="action" value="elementor_deactivate_feedback" />
-
-				<div id="elementor-deactivate-feedback-dialog-form-caption"><?php echo __( 'If you have a moment, please share why you are deactivating Elementor:', 'elementor' ); ?></div>
-				<div id="elementor-deactivate-feedback-dialog-form-body">
-					<?php foreach ( $deactivate_reasons as $reason_key => $reason ) : ?>
-						<div class="elementor-deactivate-feedback-dialog-input-wrapper">
-							<input id="elementor-deactivate-feedback-<?php echo esc_attr( $reason_key ); ?>" class="elementor-deactivate-feedback-dialog-input" type="radio" name="reason_key" value="<?php echo esc_attr( $reason_key ); ?>" />
-							<label for="elementor-deactivate-feedback-<?php echo esc_attr( $reason_key ); ?>" class="elementor-deactivate-feedback-dialog-label"><?php echo esc_html( $reason['title'] ); ?></label>
-							<?php if ( ! empty( $reason['input_placeholder'] ) ) : ?>
-								<input class="elementor-feedback-text" type="text" name="reason_<?php echo esc_attr( $reason_key ); ?>" placeholder="<?php echo esc_attr( $reason['input_placeholder'] ); ?>" />
-							<?php endif; ?>
-							<?php if ( ! empty( $reason['alert'] ) ) : ?>
-								<div class="elementor-feedback-text"><?php echo esc_html( $reason['alert'] ); ?></div>
-							<?php endif; ?>
-						</div>
-					<?php endforeach; ?>
-				</div>
-			</form>
-		</div>
-		<?php
-	}
-
-	/**
 	 * Register dashboard widgets.
 	 *
 	 * Adds a new Elementor widgets to WordPress dashboard.
@@ -682,38 +572,6 @@ class Admin {
 	}
 
 	/**
-	 * Ajax elementor deactivate feedback.
-	 *
-	 * Send the user feedback when Elementor is deactivated.
-	 *
-	 * Fired by `wp_ajax_elementor_deactivate_feedback` action.
-	 *
-	 * @since 1.0.0
-	 * @access public
-	 */
-	public function ajax_elementor_deactivate_feedback() {
-		if ( ! isset( $_POST['_wpnonce'] ) || ! wp_verify_nonce( $_POST['_wpnonce'], '_elementor_deactivate_feedback_nonce' ) ) {
-			wp_send_json_error();
-		}
-
-		$reason_text = '';
-
-		$reason_key = '';
-
-		if ( ! empty( $_POST['reason_key'] ) ) {
-			$reason_key = $_POST['reason_key'];
-		}
-
-		if ( ! empty( $_POST[ "reason_{$reason_key}" ] ) ) {
-			$reason_text = $_POST[ "reason_{$reason_key}" ];
-		}
-
-		Api::send_feedback( $reason_key, $reason_text );
-
-		wp_send_json_success();
-	}
-
-	/**
 	 * Get elementor dashboard overview widget footer actions.
 	 *
 	 * Retrieves the footer action links displayed in elementor dashboard widget.
@@ -820,6 +678,8 @@ class Admin {
 	 * @access public
 	 */
 	public function __construct() {
+		$this->feedback = new Feedback();
+
 		add_action( 'admin_init', [ $this, 'maybe_redirect_to_getting_started' ] );
 
 		add_action( 'admin_enqueue_scripts', [ $this, 'enqueue_scripts' ] );
@@ -842,9 +702,6 @@ class Admin {
 
 		// Register Dashboard Widgets.
 		add_action( 'wp_dashboard_setup', [ $this, 'register_dashboard_widgets' ] );
-
-		// Ajax.
-		add_action( 'wp_ajax_elementor_deactivate_feedback', [ $this, 'ajax_elementor_deactivate_feedback' ] );
 
 		// Admin Actions
 		add_action( 'admin_action_elementor_new_post', [ $this, 'admin_action_new_post' ] );
