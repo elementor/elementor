cf9d5bd8e51e6a4fb9f20a197f25d6caeacf5ad0
raz ohad <admin@bainternet.info>
2018-08-21 11:39:01 +0300

    added eslint and removed jshint

--- .grunt/webpack.js
+++ .grunt/webpack.js
@@ -28,6 +28,15 @@ const aliasList = {
 const moduleRules = {
 	rules: [
 		{
+			enforce: "pre",
+			test: /\.js$/,
+			exclude: /node_modules/,
+			loader: "eslint-loader",
+			options: {
+				//failOnError: true,
+			}
+		},
+		{
 			test: /\.js$/,
 			exclude: /node_modules/,
 			use: [
