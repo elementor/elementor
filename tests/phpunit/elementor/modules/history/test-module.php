412b6df49a4a5635dd1cf517aecf81d06214b3d8
OmriBar-Zik <omri@bar-zik.com>
2018-08-23 12:43:31 +0300

    Tests\Modules\history: change - Tests optimization & renames

--- tests/phpunit/elementor/modules/history/test-module.php
+++ tests/phpunit/elementor/modules/history/test-module.php
@@ -2,13 +2,12 @@
 
 namespace Elementor\Testing\Modules\history;
 
-
 use Elementor\Modules\History\Module;
 use Elementor\Testing\Elementor_Test_Base;
 
 class Elementor_Test_Manager extends Elementor_Test_Base {
 
-	public function test_should_test_name() {
+	public function test_should_get_name() {
 		$Module = new Module();
 
 		$this->assertEquals( $Module->get_name(), 'history' );
