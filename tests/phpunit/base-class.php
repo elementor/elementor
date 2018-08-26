98c1eb087c5f39cc87c80ac5fd4a2ab1f3ef6569
OmriBar-Zik <omri@bar-zik.com>
2018-08-26 16:13:38 +0300

    Tests: Change - getSelf from public to protected.

--- tests/phpunit/base-class.php
+++ tests/phpunit/base-class.php
@@ -2,9 +2,10 @@
 namespace Elementor\Testing;
 
 class Elementor_Test_Base extends \WP_UnitTestCase {
+
 	use Elementor_Test;
 
-	public function getSelf() {
+	protected function getSelf() {
 		return $this;
 	}
 }
