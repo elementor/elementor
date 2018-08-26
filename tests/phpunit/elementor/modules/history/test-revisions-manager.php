c745d68a8161b6bca76f29516148f1af9b266aa1
OmriBar-Zik <omri@bar-zik.com>
2018-08-26 13:31:52 +0300

    Tests\Modules\History: New - tests for the method Revisions_Manager::on_delete_revision_request()

--- tests/phpunit/elementor/modules/history/test-revisions-manager.php
+++ tests/phpunit/elementor/modules/history/test-revisions-manager.php
@@ -212,6 +212,64 @@ class Elementor_Test_Revisions_Manager extends Elementor_Test_AJAX {
 		$this->assertArrayHaveKeys( [ 'settings', 'elements' ], $response['data'] );
 	}
 
+	public function test_should_not_delete_revision_on_request_because_of_unset_revision_ID() {
+		$response = $this->setUp_test_for_on_delete_revision_request();
+
+		$this->assertFalse( $response['success'],
+			'the function "on_delete_revision_request" should return "success = false"' );
+		$this->assertEquals( 'You must set the revision ID.', $response['data'],
+			'the function "on_delete_revision_request" should return "data = You must set the revision ID."' );
+	}
+
+	public function test_should_not_delete_revision_on_request_because_of_invalid_revision() {
+		$_POST['id'] = $this->fake_post_id;
+
+		$response = $this->setUp_test_for_on_delete_revision_request();
+
+		$this->assertFalse( $response['success'],
+			'the function "on_delete_revision_request" should return "success = false"' );
+		$this->assertEquals( 'Invalid revision.', $response['data'],
+			'the function "on_delete_revision_request" should return "data = Invalid revision."' );
+	}
+
+
+	public function test_should_not_delete_revision_on_request_because_of_access_denied() {
+		wp_set_current_user( $this->factory()->get_subscriber_user()->ID );
+		$_POST['id'] = $this->factory()->get_default_post();
+
+		$response = $this->setUp_test_for_on_delete_revision_request();
+
+		$this->assertFalse( $response['success'],
+			'the function "on_delete_revision_request" should return "success = false"' );
+		$this->assertEquals( __( 'Access denied.', 'elementor' ), $response['data'],
+			'the function "on_delete_revision_request" should return "data = ' . __( 'Access denied.', 'elementor' ) . '"' );
+	}
+
+	public function test_should_not_delete_revision_on_request_because_the_method_cannot_delete_given_revision() {
+		wp_set_current_user( $this->factory()->create_and_get_administrator_user()->ID );
+		$_POST['id'] = $this->factory()->create_and_get_default_post();
+
+		$response = $this->setUp_test_for_on_delete_revision_request();
+
+		$this->assertFalse( $response['success'],
+			'the function "on_delete_revision_request" should return "success = false"' );
+		$this->assertEquals( __( 'Cannot delete this revision.', 'elementor' ), $response['data'],
+			'the function "on_delete_revision_request" should return "data = ' .
+			__( 'Cannot delete this revision.', 'elementor' ) . '"' );
+	}
+
+	public function test_should_delete_revision_on_request() {
+		wp_set_current_user( $this->factory()->create_and_get_administrator_user()->ID );
+		$_POST['id'] = $this->factory()->create_and_get_parent_and_child_posts()['child_id'];
+
+		$response = $this->setUp_test_for_on_delete_revision_request();
+
+		$this->assertTrue( $response['success'],
+			'the function "on_delete_revision_request" should return "success = true"' );
+		$this->assertArrayNotHasKey( 'data', $response,
+			'the function "on_delete_revision_request" should not return data' );
+	}
+
 	public function test_should_add_revision_support_for_all_post_types() {
 		Revisions_Manager::add_revision_support_for_all_post_types();
 
@@ -280,5 +338,11 @@ class Elementor_Test_Revisions_Manager extends Elementor_Test_AJAX {
 		$_POST['_nonce'] = wp_create_nonce( Editor::EDITING_NONCE_KEY );
 
 		return $this->_handleAjaxAndDecode( 'elementor_get_revision_data' );
+	}
+
+	private function setUp_test_for_on_delete_revision_request() {
+		$_POST['_nonce'] = wp_create_nonce( Editor::EDITING_NONCE_KEY );
+
+		return $this->_handleAjaxAndDecode( 'elementor_delete_revision' );
 	}
 }
