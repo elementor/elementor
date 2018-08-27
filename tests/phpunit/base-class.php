2269c53d2a85bdfa4a7ac7b84a74b2735d63df8f
OmriBar-Zik <omri@bar-zik.com>
2018-08-27 10:48:55 +0300

    Tests: Change - Moved the new asserts to the test trait.

--- tests/phpunit/base-class.php
+++ tests/phpunit/base-class.php
@@ -8,35 +8,4 @@ class Elementor_Test_Base extends \WP_UnitTestCase {
 	protected function getSelf() {
 		return $this;
 	}
-
-	/**
-	 * Asserts that a group is registered in documents
-	 *
-	 * @param string $group_name
-	 */
-	protected function assertDocumentGroupRegistered( $group_name ) {
-		if ( ! is_string( $group_name ) ) {
-			throw \PHPUnit_Util_InvalidArgumentHelper::factory(
-				1,
-				'only string'
-			);
-		}
-
-		self::assertArrayHasKey( $group_name, self::elementor()->documents->get_groups() );
-	}
-
-	/**
-	 * Asserts that a type is registered in documents
-	 *
-	 * @param string $type_name
-	 */
-	protected function assertDocumentTypeRegistered( $type_name ) {
-		if ( ! is_string( $type_name ) ) {
-			throw \PHPUnit_Util_InvalidArgumentHelper::factory(
-				1,
-				'only string'
-			);
-		}
-		self::assertNotNull( self::elementor()->documents->get_document_type( $type_name ) );
-	}
 }
--- tests/phpunit/trait-test-base.php
+++ tests/phpunit/trait-test-base.php
@@ -42,6 +42,38 @@ trait Elementor_Test {
 		return self::$elementor;
 	}
 
+
+	/**
+	 * Asserts that a group is registered in documents
+	 *
+	 * @param string $group_name
+	 */
+	protected function assertDocumentGroupRegistered( $group_name ) {
+		if ( ! is_string( $group_name ) ) {
+			throw \PHPUnit_Util_InvalidArgumentHelper::factory(
+				1,
+				'only string'
+			);
+		}
+
+		self::assertArrayHasKey( $group_name, self::elementor()->documents->get_groups() );
+	}
+
+	/**
+	 * Asserts that a type is registered in documents
+	 *
+	 * @param string $type_name
+	 */
+	protected function assertDocumentTypeRegistered( $type_name ) {
+		if ( ! is_string( $type_name ) ) {
+			throw \PHPUnit_Util_InvalidArgumentHelper::factory(
+				1,
+				'only string'
+			);
+		}
+		self::assertNotNull( self::elementor()->documents->get_document_type( $type_name ) );
+	}
+
 	/**
 	 * Asserts that an array have the specified keys.
 	 *
