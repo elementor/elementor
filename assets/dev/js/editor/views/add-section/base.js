be650e689f7bc9ee0ee25236edc09330417209c2
Yakir Sitbon <kingyes1@gmail.com>
2018-08-28 13:08:55 +0300

    Convert add-section components to ES6 syntax [2]

--- assets/dev/js/editor/views/add-section/base.js
+++ assets/dev/js/editor/views/add-section/base.js
@@ -10,7 +10,7 @@ class AddSectionBase extends Marionette.ItemView {
 		};
 	}
 
-	get ui() {
+	ui() {
 		return {
 			addNewSection: '.elementor-add-new-section',
 			closeButton: '.elementor-add-section-close',
