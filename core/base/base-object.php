0d3b505b82b938f6323a365a11ea1a63e86b71c3
Kobi Zaltzberg <kobi@pojo.me>
2018-09-02 16:02:01 +0300

    Move all settings handling to `Base_Object` class

--- core/base/base-object.php
+++ core/base/base-object.php
@@ -8,38 +8,17 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 class Base_Object {
 
-	protected $settings = [];
+	private $settings = [];
 
-	/**
-	 * Get items.
-	 *
-	 * Utility method that receives an array with a needle and returns all the
-	 * items that match the needle. If needle is not defined the entire haystack
-	 * will be returned.
-	 *
-	 * @access protected
-	 * @static
-	 *
-	 * @param array  $haystack An array of items.
-	 * @param string $needle   Optional. Needle. Default is null.
-	 * @param mixed  $default  Optional. Default value to return when the needle was
-	 *                         not found. Default is null.
-	 *
-	 * @return mixed The whole haystack or the needle from the haystack when requested.
-	 */
-	final protected static function get_items( array $haystack, $needle = null, $default = null ) {
-		if ( $needle ) {
-			return isset( $haystack[ $needle ] ) ? $haystack[ $needle ] : null;
-		}
+	final public function get_settings( $setting = null, $default = null ) {
+		$this->ensure_settings();
 
-		return $haystack;
-	}
-
-	public function get_settings( $setting = null, $default = null ) {
 		return self::get_items( $this->settings, $setting, $default );
 	}
 
-	public function set_settings( $key, $value = null ) {
+	final public function set_settings( $key, $value = null ) {
+		$this->ensure_settings();
+
 		if ( is_array( $key ) ) {
 			$this->settings = $key;
 		} else {
@@ -66,6 +45,41 @@ class Base_Object {
 			unset( $this->settings[ $key ] );
 		} else {
 			$this->settings = [];
+		}
+	}
+
+	/**
+	 * Get items.
+	 *
+	 * Utility method that receives an array with a needle and returns all the
+	 * items that match the needle. If needle is not defined the entire haystack
+	 * will be returned.
+	 *
+	 * @access protected
+	 * @static
+	 *
+	 * @param array  $haystack An array of items.
+	 * @param string $needle   Optional. Needle. Default is null.
+	 * @param mixed  $default  Optional. Default value to return when the needle was
+	 *                         not found. Default is null.
+	 *
+	 * @return mixed The whole haystack or the needle from the haystack when requested.
+	 */
+	final protected static function get_items( array $haystack, $needle = null, $default = null ) {
+		if ( $needle ) {
+			return isset( $haystack[ $needle ] ) ? $haystack[ $needle ] : null;
+		}
+
+		return $haystack;
+	}
+
+	protected function get_init_settings() {
+		return [];
+	}
+
+	private function ensure_settings() {
+		if ( ! $this->settings ) {
+			$this->settings = $this->get_init_settings();
 		}
 	}
 }
