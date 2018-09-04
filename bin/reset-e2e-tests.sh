3ad4f419a8695162c7cd8917143fd6303986c53c
Omri Bar-Zik <omri@bar-zik.com>
2018-09-04 16:56:17 +0300

    Tests: New - Setup for e2e tests

---
+++ bin/reset-e2e-tests.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# credit: "https://github.com/WordPress/gutenberg"
+# under GPL license
+
+# Exit if any command fails.
+set -e
+
+# Include useful functions.
+. "$(dirname "$0")/includes.sh"
+
+# Set up WordPress site used for end-to-end (e2e) tests.
+. "$(dirname "$0")/install-wordpress.sh" --e2e_tests
