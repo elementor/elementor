bb7f2416310c465307024e5ba96c3e6ea8e55eac
Omri Bar-Zik <omri@bar-zik.com>
2018-09-17 18:09:39 +0300

    est/docker: remove - unesesery bash function

--- bin/includes.sh
+++ bin/includes.sh
@@ -4,24 +4,6 @@
 # under GPL license
 
 ##
-# Download from a remote source.
-#
-# Checks for the existence of curl and wget, then downloads the remote file using the first available option.
-#
-# @param {string} remote  The remote file to download.
-# @param {string} [local] Optional. The local filename to use. If it isn't passed, STDOUT is used.
-#
-# @return {bool} Whether the download succeeded or not.
-##
-download() {
-    if command_exists "curl"; then
-        curl -s -o "${2:--}" "$1"
-    elif command_exists "wget"; then
-		wget -nv -O "${2:--}" "$1"
-    fi
-}
-
-##
 # Add error message formatting to a string, and echo it.
 #
 # @param {string} message The string to add formatting to.
