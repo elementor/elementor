8a66cf088ea3faffe21ada997e913533942238ac
Kobi Zaltzberg <kobi@pojo.me>
2018-10-03 16:01:25 +0300

    Added Edge browser to environment object

--- assets/dev/js/utils/environment.js
+++ assets/dev/js/utils/environment.js
@@ -4,5 +4,6 @@ export default {
 	webkit: -1 !== userAgent.indexOf( 'AppleWebKit' ),
 	firefox: -1 !== userAgent.indexOf( 'Firefox' ),
 	ie: /Trident|MSIE/.test( userAgent ),
+	edge: -1 !== userAgent.indexOf( 'Edge' ),
 	mac: -1 !== userAgent.indexOf( 'Macintosh' ),
 };
