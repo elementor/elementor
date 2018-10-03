f7e612f19acaa373f8ae3fa568752d2149d6c95f
Kobi Zaltzberg <kobi@pojo.me>
2018-10-03 14:17:32 +0300

    Hotkeys Screen: Rearrange classes

--- includes/editor-templates/hotkeys.php
+++ includes/editor-templates/hotkeys.php
@@ -7,73 +7,73 @@ if ( ! defined( 'ABSPATH' ) ) {
 ?>
 <script type="text/template" id="tmpl-elementor-hotkeys">
 
-	<div class="hotkeys__content">
-		<div class="hotkeys__col hotkeys__col--50 hotkeys--actions">
+	<div id="elementor-hotkeys__content">
+		<div id="elementor-hotkeys__actions" class="elementor-hotkeys__col">
 
-			<div class="hotkeys__header">
+			<div class="elementor-hotkeys__header">
 				<h3><?php echo __( 'Actions', 'elementor' ); ?></h3>
 			</div>
-			<div class="hotkeys__list">
-				<div class="hotkeys__item">
-					<div class="hotkeys__item--label"><?php echo __( 'Undo', 'elementor' ); ?></div>
-					<div class="hotkeys__item--shortcut">
+			<div class="elementor-hotkeys__list">
+				<div class="elementor-hotkeys__item">
+					<div class="elementor-hotkeys__item--label"><?php echo __( 'Undo', 'elementor' ); ?></div>
+					<div class="elementor-hotkeys__item--shortcut">
 						<span>Ctrl / Cmd</span>
 						<span>Z</span>
 					</div>
 				</div>
 
-				<div class="hotkeys__item">
-					<div class="hotkeys__item--label"><?php echo __( 'Redo', 'elementor' ); ?></div>
-					<div class="hotkeys__item--shortcut">
+				<div class="elementor-hotkeys__item">
+					<div class="elementor-hotkeys__item--label"><?php echo __( 'Redo', 'elementor' ); ?></div>
+					<div class="elementor-hotkeys__item--shortcut">
 						<span>Ctrl / Cmd</span>
 						<span>Shift</span>
 						<span>Z</span>
 					</div>
 				</div>
 
-				<div class="hotkeys__item">
-					<div class="hotkeys__item--label"><?php echo __( 'Copy', 'elementor' ); ?></div>
-					<div class="hotkeys__item--shortcut">
+				<div class="elementor-hotkeys__item">
+					<div class="elementor-hotkeys__item--label"><?php echo __( 'Copy', 'elementor' ); ?></div>
+					<div class="elementor-hotkeys__item--shortcut">
 						<span>Ctrl / Cmd</span>
 						<span>C</span>
 					</div>
 				</div>
 
-				<div class="hotkeys__item">
-					<div class="hotkeys__item--label"><?php echo __( 'Paste', 'elementor' ); ?></div>
-					<div class="hotkeys__item--shortcut">
+				<div class="elementor-hotkeys__item">
+					<div class="elementor-hotkeys__item--label"><?php echo __( 'Paste', 'elementor' ); ?></div>
+					<div class="elementor-hotkeys__item--shortcut">
 						<span>Ctrl / Cmd</span>
 						<span>V</span>
 					</div>
 				</div>
 
-				<div class="hotkeys__item">
-					<div class="hotkeys__item--label"><?php echo __( 'Paste Style', 'elementor' ); ?></div>
-					<div class="hotkeys__item--shortcut">
+				<div class="elementor-hotkeys__item">
+					<div class="elementor-hotkeys__item--label"><?php echo __( 'Paste Style', 'elementor' ); ?></div>
+					<div class="elementor-hotkeys__item--shortcut">
 						<span>Ctrl / Cmd</span>
 						<span>Shift</span>
 						<span>C</span>
 					</div>
 				</div>
 
-				<div class="hotkeys__item">
-					<div class="hotkeys__item--label"><?php echo __( 'Delete', 'elementor' ); ?></div>
-					<div class="hotkeys__item--shortcut">
+				<div class="elementor-hotkeys__item">
+					<div class="elementor-hotkeys__item--label"><?php echo __( 'Delete', 'elementor' ); ?></div>
+					<div class="elementor-hotkeys__item--shortcut">
 						<span>Delete</span>
 					</div>
 				</div>
 
-				<div class="hotkeys__item">
-					<div class="hotkeys__item--label"><?php echo __( 'Duplicate', 'elementor' ); ?></div>
-					<div class="hotkeys__item--shortcut">
+				<div class="elementor-hotkeys__item">
+					<div class="elementor-hotkeys__item--label"><?php echo __( 'Duplicate', 'elementor' ); ?></div>
+					<div class="elementor-hotkeys__item--shortcut">
 						<span>Ctrl / Cmd</span>
 						<span>D</span>
 					</div>
 				</div>
 
-				<div class="hotkeys__item">
-					<div class="hotkeys__item--label"><?php echo __( 'Save', 'elementor' ); ?></div>
-					<div class="hotkeys__item--shortcut">
+				<div class="elementor-hotkeys__item">
+					<div class="elementor-hotkeys__item--label"><?php echo __( 'Save', 'elementor' ); ?></div>
+					<div class="elementor-hotkeys__item--shortcut">
 						<span>Ctrl / Cmd</span>
 						<span>S</span>
 					</div>
@@ -82,59 +82,59 @@ if ( ! defined( 'ABSPATH' ) ) {
 			</div>
 		</div>
 
-		<div class="hotkeys__col hotkeys__col--50 hotkeys--navigation">
+		<div id="elementor-hotkeys__navigation" class="elementor-hotkeys__col">
 
-			<div class="hotkeys__header">
+			<div class="elementor-hotkeys__header">
 				<h3><?php echo __( 'Go To', 'elementor' ); ?></h3>
 			</div>
-			<div class="hotkeys__list">
-				<div class="hotkeys__item">
-					<div class="hotkeys__item--label"><?php echo __( 'Show / Hide Panel', 'elementor' ); ?></div>
-					<div class="hotkeys__item--shortcut">
+			<div class="elementor-hotkeys__list">
+				<div class="elementor-hotkeys__item">
+					<div class="elementor-hotkeys__item--label"><?php echo __( 'Show / Hide Panel', 'elementor' ); ?></div>
+					<div class="elementor-hotkeys__item--shortcut">
 						<span>Ctrl / Cmd</span>
 						<span>P</span>
 					</div>
 				</div>
 
-				<div class="hotkeys__item">
-					<div class="hotkeys__item--label"><?php echo __( 'Responsive Mode', 'elementor' ); ?></div>
-					<div class="hotkeys__item--shortcut">
+				<div class="elementor-hotkeys__item">
+					<div class="elementor-hotkeys__item--label"><?php echo __( 'Responsive Mode', 'elementor' ); ?></div>
+					<div class="elementor-hotkeys__item--shortcut">
 						<span>Ctrl / Cmd</span>
 						<span>Shift</span>
 						<span>M</span>
 					</div>
 				</div>
 
-				<div class="hotkeys__item">
-					<div class="hotkeys__item--label"><?php echo __( 'History', 'elementor' ); ?></div>
-					<div class="hotkeys__item--shortcut">
+				<div class="elementor-hotkeys__item">
+					<div class="elementor-hotkeys__item--label"><?php echo __( 'History', 'elementor' ); ?></div>
+					<div class="elementor-hotkeys__item--shortcut">
 						<span>Ctrl / Cmd</span>
 						<span>Shift</span>
 						<span>H</span>
 					</div>
 				</div>
 
-				<div class="hotkeys__item">
-					<div class="hotkeys__item--label"><?php echo __( 'Navigator', 'elementor' ); ?></div>
-					<div class="hotkeys__item--shortcut">
+				<div class="elementor-hotkeys__item">
+					<div class="elementor-hotkeys__item--label"><?php echo __( 'Navigator', 'elementor' ); ?></div>
+					<div class="elementor-hotkeys__item--shortcut">
 						<span>Ctrl / Cmd</span>
 						<span>Shift</span>
 						<span>I</span>
 					</div>
 				</div>
 
-				<div class="hotkeys__item">
-					<div class="hotkeys__item--label"><?php echo __( 'Template Library', 'elementor' ); ?></div>
-					<div class="hotkeys__item--shortcut">
+				<div class="elementor-hotkeys__item">
+					<div class="elementor-hotkeys__item--label"><?php echo __( 'Template Library', 'elementor' ); ?></div>
+					<div class="elementor-hotkeys__item--shortcut">
 						<span>Ctrl / Cmd</span>
 						<span>Shift</span>
 						<span>L</span>
 					</div>
 				</div>
 
-				<div class="hotkeys__item">
-					<div class="hotkeys__item--label"><?php echo __( 'Keyboard Shortcuts', 'elementor' ); ?></div>
-					<div class="hotkeys__item--shortcut">
+				<div class="elementor-hotkeys__item">
+					<div class="elementor-hotkeys__item--label"><?php echo __( 'Keyboard Shortcuts', 'elementor' ); ?></div>
+					<div class="elementor-hotkeys__item--shortcut">
 						<span>Ctrl / Cmd</span>
 						<span>?</span>
 					</div>
