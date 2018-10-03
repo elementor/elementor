d2b30cef8e3c77293e13572d62b52bb94af6581f
Omri Bar-Zik <omri@bar-zik.com>
2018-10-03 12:35:58 +0300

    Tests\Modules\Library: Fix - PR

--- tests/phpunit/elementor/modules/library/test-module.php
+++ tests/phpunit/elementor/modules/library/test-module.php
@@ -31,4 +31,35 @@ class Elementor_Test_Module extends Elementor_Test_Base {
 		);
 	}
 
+	/**
+	 * Asserts that a group is registered in documents
+	 *
+	 * @param string $group_name
+	 */
+	private function assertDocumentGroupRegistered( $group_name ) {
+		if ( ! is_string( $group_name ) ) {
+			throw \PHPUnit_Util_InvalidArgumentHelper::factory(
+				1,
+				'only string'
+			);
+		}
+
+		self::assertArrayHasKey( $group_name, self::elementor()->documents->get_groups() );
+	}
+
+	/**
+	 * Asserts that a type is registered in documents
+	 *
+	 * @param string $type_name
+	 */
+	private function assertDocumentTypeRegistered( $type_name ) {
+		if ( ! is_string( $type_name ) ) {
+			throw \PHPUnit_Util_InvalidArgumentHelper::factory(
+				1,
+				'only string'
+			);
+		}
+		self::assertNotNull( self::elementor()->documents->get_document_type( $type_name ) );
+	}
+
 }
