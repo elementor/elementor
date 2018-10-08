98ee597f4daa519492779998acac482647ed4ace
Omri Bar-Zik <omri@bar-zik.com>
2018-10-08 12:33:32 +0300

    added uninstall

---
+++ bin/uninstall.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+. "$(dirname "$0")/bootstrap-env.sh"
+
+. "$(dirname "$0")/includes.sh"
+
+cd "$(dirname "$0")/.."
+
+if [ "$EUID" -ne 0 ]
+  then echo "to uninstall plaese run as root"
+  exit
+fi
+
+docker-compose $DOCKER_COMPOSE_FILE_OPTIONS down --volumes --rmi local > /dev/null || true
+
+docker rmi $(cat .docker/local-site/docker-images-id | sed 1d) > /dev/null || true
+
+rm -rf .docker/local-site
