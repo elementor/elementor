ef9ed44aae59cb731428f1542ac1ccd3019f07a6
Omri Bar-Zik <omri@bar-zik.com>
2018-10-10 12:10:57 +0300

    testing cypress

--- assets/dev/js/editor/regions/panel/pages/elements/elements.js
+++ assets/dev/js/editor/regions/panel/pages/elements/elements.js
@@ -1,213 +1,214 @@
 var PanelElementsCategoriesCollection = require( './collections/categories' ),
-	PanelElementsElementsCollection = require( './collections/elements' ),
-	PanelElementsCategoriesView = require( './views/categories' ),
-	PanelElementsElementsView = elementor.modules.layouts.panel.pages.elements.views.Elements,
-	PanelElementsSearchView = require( './views/search' ),
-	PanelElementsGlobalView = require( './views/global' ),
-	PanelElementsLayoutView;
+  PanelElementsElementsCollection = require( './collections/elements' ),
+  PanelElementsCategoriesView = require( './views/categories' ),
+  PanelElementsElementsView = elementor.modules.layouts.panel.pages.elements.views.Elements,
+  PanelElementsSearchView = require( './views/search' ),
+  PanelElementsGlobalView = require( './views/global' ),
+  PanelElementsLayoutView;
 
 PanelElementsLayoutView = Marionette.LayoutView.extend( {
-	template: '#tmpl-elementor-panel-elements',
+  template: '#tmpl-elementor-panel-elements',
 
-	options: {
-		autoFocusSearch: true,
-	},
+  options: {
+    autoFocusSearch: true,
+  },
 
-	regions: {
-		elements: '#elementor-panel-elements-wrapper',
-		search: '#elementor-panel-elements-search-area',
-	},
+  regions: {
+    elements: '#elementor-panel-elements-wrapper',
+    search: '#elementor-panel-elements-search-area',
+  },
 
-	ui: {
-		tabs: '.elementor-panel-navigation-tab',
-	},
+  ui: {
+    tabs: '.elementor-panel-navigation-tab',
+  },
 
-	events: {
-		'click @ui.tabs': 'onTabClick',
-	},
+  events: {
+    'click @ui.tabs': 'onTabClick',
+  },
 
-	regionViews: {},
+  regionViews: {},
 
-	elementsCollection: null,
+  elementsCollection: null,
 
-	categoriesCollection: null,
+  categoriesCollection: null,
 
-	initialize: function() {
-		this.listenTo( elementor.channels.panelElements, 'element:selected', this.destroy );
+  initialize: function() {
+    this.listenTo( elementor.channels.panelElements, 'element:selected', this.destroy );
 
-		this.initElementsCollection();
+    this.initElementsCollection();
 
-		this.initCategoriesCollection();
+    this.initCategoriesCollection();
 
-		this.initRegionViews();
-	},
+    this.initRegionViews();
+  },
 
-	initRegionViews: function() {
-		var regionViews = {
-			elements: {
-				region: this.elements,
-				view: PanelElementsElementsView,
-				options: { collection: this.elementsCollection },
-			},
-			categories: {
-				region: this.elements,
-				view: PanelElementsCategoriesView,
-				options: { collection: this.categoriesCollection },
-			},
-			search: {
-				region: this.search,
-				view: PanelElementsSearchView,
-			},
-			global: {
-				region: this.elements,
-				view: PanelElementsGlobalView,
-			},
-		};
+  initRegionViews: function() {
+    var regionViews = {
+      elements: {
+        region: this.elements,
+        view: PanelElementsElementsView,
+        options: { collection: this.elementsCollection },
+      },
+      categories: {
+        region: this.elements,
+        view: PanelElementsCategoriesView,
+        options: { collection: this.categoriesCollection },
+      },
+      search: {
+        region: this.search,
+        view: PanelElementsSearchView,
+      },
+      global: {
+        region: this.elements,
+        view: PanelElementsGlobalView,
+      },
+    };
 
-		this.regionViews = elementor.hooks.applyFilters( 'panel/elements/regionViews', regionViews );
-	},
+    this.regionViews = elementor.hooks.applyFilters( 'panel/elements/regionViews', regionViews );
+  },
 
-	initElementsCollection: function() {
-		var elementsCollection = new PanelElementsElementsCollection(),
-			sectionConfig = elementor.config.elements.section;
+  initElementsCollection: function() {
+    var elementsCollection = new PanelElementsElementsCollection(),
+      sectionConfig = elementor.config.elements.section;
 
-		elementsCollection.add( {
-			title: elementor.translate( 'inner_section' ),
-			elType: 'section',
-			categories: [ 'basic' ],
-			keywords: [ 'row', 'columns', 'nested' ],
-			icon: sectionConfig.icon,
-		} );
+    elementsCollection.add( {
+      title: elementor.translate( 'inner_section' ),
+      elType: 'section',
+      categories: [ 'basic' ],
+      keywords: [ 'row', 'columns', 'nested' ],
+      icon: sectionConfig.icon,
+    } );
 
-		// TODO: Change the array from server syntax, and no need each loop for initialize
-		_.each( elementor.config.widgets, function( widget ) {
-			if ( elementor.config.document.panel.widgets_settings[ widget.widget_type ] ) {
-				widget = _.extend( widget, elementor.config.document.panel.widgets_settings[ widget.widget_type ] );
-			}
+    // TODO: Change the array from server syntax, and no need each loop for initialize
+    _.each( elementor.config.widgets, function( widget ) {
+      if ( elementor.config.document.panel.widgets_settings[ widget.widget_type ] ) {
+        widget = _.extend( widget, elementor.config.document.panel.widgets_settings[ widget.widget_type ] );
+      }
 
-			if ( ! widget.show_in_panel ) {
-				return;
-			}
+      if ( ! widget.show_in_panel ) {
+        return;
+      }
 
-			elementsCollection.add( {
-				title: widget.title,
-				elType: widget.elType,
-				categories: widget.categories,
-				keywords: widget.keywords,
-				icon: widget.icon,
-				widgetType: widget.widget_type,
-				custom: widget.custom,
-			} );
-		} );
+      elementsCollection.add( {
+        title: widget.title,
+        elType: widget.elType,
+        categories: widget.categories,
+        keywords: widget.keywords,
+        icon: widget.icon,
+        widgetType: widget.widget_type,
+        custom: widget.custom,
+      } );
+    } );
 
-		this.elementsCollection = elementsCollection;
-	},
+    this.elementsCollection = elementsCollection;
+  },
 
-	initCategoriesCollection: function() {
-		var categories = {};
+  initCategoriesCollection: function() {
+    var categories = {};
 
-		this.elementsCollection.each( function( element ) {
-			_.each( element.get( 'categories' ), function( category ) {
-				if ( ! categories[ category ] ) {
-					categories[ category ] = [];
-				}
+    this.elementsCollection.each( function( element ) {
+      _.each( element.get( 'categories' ), function( category ) {
+        if ( ! categories[ category ] ) {
+          categories[ category ] = [];
+        }
 
-				categories[ category ].push( element );
-			} );
-		} );
+        categories[ category ].push( element );
+      } );
+    } );
 
-		var categoriesCollection = new PanelElementsCategoriesCollection();
+    var categoriesCollection = new PanelElementsCategoriesCollection();
 
-		_.each( elementor.config.document.panel.elements_categories, function( categoryConfig, categoryName ) {
-			if ( ! categories[ categoryName ] ) {
-				return;
-			}
+    _.each( elementor.config.document.panel.elements_categories, function( categoryConfig, categoryName ) {
+      if ( ! categories[ categoryName ] ) {
+        return;
+      }
 
-			// Set defaults.
-			if ( 'undefined' === typeof categoryConfig.active ) {
-				categoryConfig.active = true;
-			}
+      // Set defaults.
+      if ( 'undefined' === typeof categoryConfig.active ) {
+        categoryConfig.active = true;
+      }
 
-			if ( 'undefined' === typeof categoryConfig.icon ) {
-				categoryConfig.icon = 'font';
-			}
+      if ( 'undefined' === typeof categoryConfig.icon ) {
+        categoryConfig.icon = 'font';
+      }
 
-			categoriesCollection.add( {
-				name: categoryName,
-				title: categoryConfig.title,
-				icon: categoryConfig.icon,
-				defaultActive: categoryConfig.active,
-				items: categories[ categoryName ],
-			} );
-		} );
+      categoriesCollection.add( {
+        name: categoryName,
+        title: categoryConfig.title,
+        icon: categoryConfig.icon,
+        defaultActive: categoryConfig.active,
+        items: categories[ categoryName ],
+      } );
+    } );
 
-		this.categoriesCollection = categoriesCollection;
-	},
+    this.categoriesCollection = categoriesCollection;
+  },
 
-	activateTab: function( tabName ) {
-		this.ui.tabs
-			.removeClass( 'elementor-active' )
-			.filter( '[data-view="' + tabName + '"]' )
-			.addClass( 'elementor-active' );
+  activateTab: function( tabName ) {
+    this.ui.tabs
+      .removeClass( 'elementor-active' )
+      .filter( '[data-view="' + tabName + '"]' )
+      .addClass( 'elementor-active' );
 
-		this.showView( tabName );
-	},
+    this.showView( tabName );
+  },
 
-	showView: function( viewName ) {
-		var viewDetails = this.regionViews[ viewName ],
-			options = viewDetails.options || {};
+  showView: function( viewName ) {
+    var viewDetails = this.regionViews[ viewName ],
+      options = viewDetails.options || {};
 
-		viewDetails.region.show( new viewDetails.view( options ) );
-	},
+    viewDetails.region.show( new viewDetails.view( options ) );
+  },
 
-	clearSearchInput: function() {
-		this.getChildView( 'search' ).clearInput();
-	},
+  clearSearchInput: function() {
+    this.getChildView( 'search' ).clearInput();
+  },
 
-	changeFilter: function( filterValue ) {
-		elementor.channels.panelElements
-			.reply( 'filter:value', filterValue )
-			.trigger( 'filter:change' );
-	},
+  changeFilter: function( filterValue ) {
+    elementor.channels.panelElements.reply( 'filter:value', filterValue ).trigger( 'filter:change' );
+  },
 
-	clearFilters: function() {
-		this.changeFilter( null );
-		this.clearSearchInput();
-	},
+  clearFilters: function() {
+    this.changeFilter( null );
+    this.clearSearchInput();
+  },
 
-	focusSearch: function() {
-		if ( ! elementor.userCan( 'design' ) ) {
-			return;
-		}
+  focusSearch: function() {
+    if ( ! elementor.userCan( 'design' ) ) {
+      return;
+    }
 
-		this.search.currentView.ui.input.focus();
-	},
+    // eslint-disable-next-line
+    console.log(this);
 
-	onChildviewChildrenRender: function() {
-		elementor.getPanelView().updateScrollbar();
-	},
+    // this.search.currentView.ui.input.focus();
+  },
 
-	onChildviewSearchChangeInput: function( child ) {
-		this.changeFilter( child.ui.input.val(), 'search' );
-	},
+  onChildviewChildrenRender: function() {
+    elementor.getPanelView().updateScrollbar();
+  },
 
-	onDestroy: function() {
-		elementor.channels.panelElements.reply( 'filter:value', null );
-	},
+  onChildviewSearchChangeInput: function( child ) {
+    this.changeFilter( child.ui.input.val(), 'search' );
+  },
 
-	onShow: function() {
-		this.showView( 'categories' );
+  onDestroy: function() {
+    elementor.channels.panelElements.reply( 'filter:value', null );
+  },
 
-		this.showView( 'search' );
+  onShow: function() {
+    this.showView( 'categories' );
 
-		if ( this.options.autoFocusSearch ) {
-			setTimeout( this.focusSearch.bind( this ) );
-		}
-	},
+    this.showView( 'search' );
 
-	onTabClick: function( event ) {
-		this.activateTab( event.currentTarget.dataset.view );
-	},
+    if ( this.options.autoFocusSearch ) {
+      setTimeout( this.focusSearch.bind( this ) );
+    }
+  },
+
+  onTabClick: function( event ) {
+    this.activateTab( event.currentTarget.dataset.view );
+  },
 } );
 
 module.exports = PanelElementsLayoutView;
