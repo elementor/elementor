d92188960016eec32fe89091756840fb9be79e51
Kobi Zaltzberg <kobi@pojo.me>
2018-10-10 13:04:13 +0300

    Repeater: Allow to customize item actions

--- includes/editor-templates/repeater.php
+++ includes/editor-templates/repeater.php
@@ -8,19 +8,25 @@ if ( ! defined( 'ABSPATH' ) ) {
 ?>
 <script type="text/template" id="tmpl-elementor-repeater-row">
 	<div class="elementor-repeater-row-tools">
-		<div class="elementor-repeater-row-handle-sortable">
-			<i class="fa fa-ellipsis-v" aria-hidden="true"></i>
-			<span class="elementor-screen-only"><?php echo __( 'Drag & Drop', 'elementor' ); ?></span>
-		</div>
+		<# if ( itemActions.drag_n_drop ) {  #>
+			<div class="elementor-repeater-row-handle-sortable">
+				<i class="fa fa-ellipsis-v" aria-hidden="true"></i>
+				<span class="elementor-screen-only"><?php echo __( 'Drag & Drop', 'elementor' ); ?></span>
+			</div>
+		<# } #>
 		<div class="elementor-repeater-row-item-title"></div>
-		<div class="elementor-repeater-row-tool elementor-repeater-tool-duplicate">
-			<i class="fa fa-copy" aria-hidden="true"></i>
-			<span class="elementor-screen-only"><?php echo __( 'Duplicate', 'elementor' ); ?></span>
-		</div>
-		<div class="elementor-repeater-row-tool elementor-repeater-tool-remove">
-			<i class="fa fa-remove" aria-hidden="true"></i>
-			<span class="elementor-screen-only"><?php echo __( 'Remove', 'elementor' ); ?></span>
-		</div>
+		<# if ( itemActions.duplicate ) {  #>
+			<div class="elementor-repeater-row-tool elementor-repeater-tool-duplicate">
+				<i class="fa fa-copy" aria-hidden="true"></i>
+				<span class="elementor-screen-only"><?php echo __( 'Duplicate', 'elementor' ); ?></span>
+			</div>
+		<# }
+		if ( itemActions.remove ) {  #>
+			<div class="elementor-repeater-row-tool elementor-repeater-tool-remove">
+				<i class="fa fa-remove" aria-hidden="true"></i>
+				<span class="elementor-screen-only"><?php echo __( 'Remove', 'elementor' ); ?></span>
+			</div>
+		<# } #>
 	</div>
 	<div class="elementor-repeater-row-controls"></div>
 </script>
