5fb0f84c2574b0b06b7581b617484b8898219020
OmriBar-Zik <omri@bar-zik.com>
2018-10-16 00:04:00 +0300

    update circle config

--- bin/bootstrap-env.sh
+++ bin/bootstrap-env.sh
@@ -7,8 +7,3 @@ WP_VERSION=${WP_VERSION-latest}
 DOCKER=${DOCKER-false}
 DOCKER_ENV=${DOCKER_ENV-ci}
 DOCKER_COMPOSE_FILE_OPTIONS="-f docker-compose.yml"
-
-if [ "$DOCKER_ENV" == "ci" ]; then
-	DOCKER_COMPOSE_FILE_OPTIONS="-f .circleci/docker-compose.yml"
-fi
-
