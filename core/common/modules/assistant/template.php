103a73c1266a3ebda15742b9e957a0e71a611838
Kobi Zaltzberg <kobi@pojo.me>
2018-10-29 16:56:03 +0200

    Assistant: Design fixes

--- core/common/modules/assistant/template.php
+++ core/common/modules/assistant/template.php
@@ -10,7 +10,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 <script type="text/template" id="tmpl-elementor-assistant">
 	<div id="elementor-assistant__search">
 		<i class="eicon-search"></i>
-		<input id="elementor-assistant__search__input" placeholder="<?php echo __( 'Search Elementor', 'elementor' ); ?>">
+		<input id="elementor-assistant__search__input" placeholder="<?php echo __( 'Search or create anything in Elementor (pages, posts, integrations, etc.)', 'elementor' ); ?>">
 	</div>
 	<div id="elementor-assistant__content"></div>
 </script>
