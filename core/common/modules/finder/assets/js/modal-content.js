8b168f5da73d0440f5db15428f83da4a4a5b17f2
Kobi Zaltzberg <kobi@pojo.me>
2018-11-06 16:40:54 +0200

    Moved arrows functionality to categories view

--- core/common/modules/finder/assets/js/modal-content.js
+++ core/common/modules/finder/assets/js/modal-content.js
@@ -12,14 +12,12 @@ export default class extends Marionette.LayoutView {
 	ui() {
 		return {
 			searchInput: '#elementor-finder__search__input',
-			categoryItem: '.elementor-finder__results__item',
 		};
 	}
 
 	events() {
 		return {
 			'input @ui.searchInput': 'onSearchInputInput',
-			'mouseenter @ui.categoryItem': 'onCategoryItemMouseEnter',
 		};
 	}
 
@@ -29,73 +27,8 @@ export default class extends Marionette.LayoutView {
 		};
 	}
 
-	initialize() {
-		this.$activeItem = null;
-	}
-
 	showCategoriesView() {
 		this.content.show( new CategoriesView() );
-	}
-
-	activateItem( $item ) {
-		if ( this.$activeItem ) {
-			this.$activeItem.removeClass( 'elementor-active' );
-		}
-
-		$item.addClass( 'elementor-active' );
-
-		this.$activeItem = $item;
-	}
-
-	activateNextItem( reverse ) {
-		const $allItems = jQuery( this.ui.categoryItem.selector );
-
-		let nextItemIndex = 0;
-
-		if ( this.$activeItem ) {
-			nextItemIndex = $allItems.index( this.$activeItem ) + ( reverse ? -1 : 1 );
-
-			if ( nextItemIndex >= $allItems.length ) {
-				nextItemIndex = 0;
-			} else if ( nextItemIndex < 0 ) {
-				nextItemIndex = $allItems.length - 1;
-			}
-		}
-
-		const $nextItem = $allItems.eq( nextItemIndex );
-
-		this.activateItem( $nextItem );
-
-		$nextItem[ 0 ].scrollIntoView( { block: 'nearest' } );
-	}
-
-	goToActiveItem() {
-		this.$activeItem.children( 'a' )[ 0 ].click();
-	}
-
-	addHotKeys() {
-		const DOWN_ARROW = 40,
-			UP_ARROW = 38,
-			ENTER = 13;
-
-		elementorCommon.hotKeys.addHotKeyHandler( DOWN_ARROW, 'finderNextItem', {
-			isWorthHandling: () => elementorCommon.finder.getLayout().getModal().isVisible(),
-			handle: () => this.activateNextItem(),
-		} );
-
-		elementorCommon.hotKeys.addHotKeyHandler( UP_ARROW, 'finderPreviousItem', {
-			isWorthHandling: () => elementorCommon.finder.getLayout().getModal().isVisible(),
-			handle: () => this.activateNextItem( true ),
-		} );
-
-		elementorCommon.hotKeys.addHotKeyHandler( ENTER, 'finderSelectItem', {
-			isWorthHandling: () => elementorCommon.finder.getLayout().getModal().isVisible() && this.$activeItem,
-			handle: () => this.goToActiveItem(),
-		} );
-	}
-
-	onShow() {
-		this.addHotKeys();
 	}
 
 	onSearchInputInput() {
@@ -112,9 +45,5 @@ export default class extends Marionette.LayoutView {
 		}
 
 		this.content.currentView.$el.toggle( ! ! value );
-	}
-
-	onCategoryItemMouseEnter( event ) {
-		this.activateItem( jQuery( event.currentTarget ) );
 	}
 }
