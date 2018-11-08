65d3d39b412e84c4d84814b02ab7a37b70ed58a6
Josh Marom <josh@pojo.me>
2018-11-08 19:14:56 +0200

    Add 0-10 rating scale to star-rating widget

--- includes/widgets/star-rating.php
+++ includes/widgets/star-rating.php
@@ -86,6 +86,19 @@ class Widget_Star_Rating extends Widget_Base {
 		);
 
 		$this->add_control(
+			'rating_scale',
+			[
+				'label' => __( 'Rating Scale', 'elementor' ),
+				'type' => Controls_Manager::SELECT,
+				'options' => [
+					'5' => '0-5',
+					'10' => '0-10',
+				],
+				'default' => '5',
+			]
+		);
+
+		$this->add_control(
 			'rating',
 			[
 				'label' => __( 'Rating', 'elementor' ),
@@ -94,6 +107,24 @@ class Widget_Star_Rating extends Widget_Base {
 				'max' => 5,
 				'step' => 0.1,
 				'default' => 5,
+				'condition' => [
+					'rating_scale' => '5',
+				],
+			]
+		);
+
+		$this->add_control(
+			'rating_10',
+			[
+				'label' => __( 'Rating', 'elementor' ),
+				'type' => Controls_Manager::NUMBER,
+				'min' => 0,
+				'max' => 10,
+				'step' => 0.1,
+				'default' => 10,
+				'condition' => [
+					'rating_scale' => '10',
+				],
 			]
 		);
 
@@ -103,10 +134,10 @@ class Widget_Star_Rating extends Widget_Base {
 				'label' => __( 'Icon', 'elementor' ),
 				'type' => Controls_Manager::SELECT,
 				'options' => [
-					'fontawesome' => 'Font Awesome',
-					'unicode' => 'Unicode',
+					'star_fontawesome' => 'Font Awesome',
+					'star_unicode' => 'Unicode',
 				],
-				'default' => 'fontawesome',
+				'default' => 'star_fontawesome',
 				'render_type' => 'template',
 				'prefix_class' => 'elementor--star-style-',
 				'separator' => 'before',
@@ -293,13 +324,22 @@ class Widget_Star_Rating extends Widget_Base {
 		$this->end_controls_section();
 	}
 
-	protected function render_stars( $icon ) {
+	protected function get_rating() {
 		$settings = $this->get_settings_for_display();
-		$rating = $settings['rating'] > 5 ? 5 : $settings['rating'];
+		$rating_scale = (int) $settings['rating_scale'];
+		$rating_setting = 5 == $settings[ 'rating_scale' ] ? $settings[ 'rating' ] : $settings[ 'rating_10' ];
+		$rating = $rating_setting > $rating_scale ? $rating_scale : $rating_setting;
+
+		return [ $rating, $rating_scale ];
+	}
+
+	protected function render_stars( $icon ) {
+		$rating_data = $this->get_rating();
+		$rating = $rating_data[0];
 		$floored_rating = (int) $rating;
 		$stars_html = '';
 
-		for ( $stars = 1; $stars <= 5; $stars++ ) {
+		for ( $stars = 1; $stars <= $rating_data[1]; $stars++ ) {
 			if ( $stars <= $floored_rating ) {
 				$stars_html .= '<i class="elementor-star-full">' . $icon . '</i>';
 			} elseif ( $floored_rating + 1 === $stars && $rating !== $floored_rating ) {
@@ -314,13 +354,15 @@ class Widget_Star_Rating extends Widget_Base {
 
 	protected function render() {
 		$settings = $this->get_settings_for_display();
+		$rating_data = $this->get_rating();
+		$textual_rating = $rating_data[0] . '/' . $rating_data[1];
 		$icon = '&#61445;';
 
-		if ( 'fontawesome' === $settings['star_style'] ) {
+		if ( 'star_fontawesome' === $settings['star_style'] ) {
 			if ( 'outline' === $settings['unmarked_star_style'] ) {
 				$icon = '&#61446;';
 			}
-		} elseif ( 'unicode' === $settings['star_style'] ) {
+		} elseif ( 'star_unicode' === $settings['star_style'] ) {
 			$icon = '&#9733;';
 
 			if ( 'outline' === $settings['unmarked_star_style'] ) {
@@ -330,13 +372,13 @@ class Widget_Star_Rating extends Widget_Base {
 
 		$this->add_render_attribute( 'icon_wrapper', [
 			'class' => 'elementor-star-rating',
-			'title' => $settings['rating'],
+			'title' => $textual_rating,
 			'itemtype' => 'http://schema.org/Rating',
 			'itemscope' => '',
 			'itemprop' => 'reviewRating',
 		] );
 
-		$schema_rating = '<span itemprop="ratingValue" class="elementor-screen-only">' . $settings['rating'] . '</span>';
+		$schema_rating = '<span itemprop="ratingValue" class="elementor-screen-only">' . $textual_rating . '</span>';
 		$stars_element = '<div ' . $this->get_render_attribute_string( 'icon_wrapper' ) . '>' . $this->render_stars( $icon ) . ' ' . $schema_rating . '</div>';
 		?>
 
@@ -352,25 +394,36 @@ class Widget_Star_Rating extends Widget_Base {
 	protected function _content_template() {
 		?>
 		<#
-			var renderStars = function( icon ) {
-				var rating = settings.rating > 5 ? 5 : settings.rating,
-					starsHtml = '',
-					flooredRating = Math.floor( rating );
+			var getRating = function() {
+				var ratingScale = parseInt( settings.rating_scale, 10 ),
+					ratingSetting = 5 === ratingScale ? settings.rating : settings.rating_10,
+					rating = ratingSetting > ratingScale ? ratingScale : ratingSetting;
 
-				for ( var stars = 1; stars <= 5; stars++ ) {
-					if ( stars <= flooredRating  ) {
-						starsHtml += '<i class="elementor-star-full">' + icon + '</i>';
-					} else if ( flooredRating + 1 === stars && rating !== flooredRating ) {
-						starsHtml += '<i class="elementor-star-' + ( rating - flooredRating ).toFixed( 1 ) * 10 + '">' + icon + '</i>';
-					} else {
-						starsHtml += '<i class="elementor-star-empty">' + icon + '</i>';
+				return [ rating, ratingScale ];
+			},
+
+			    ratingData = getRating(),
+				rating = ratingData[0],
+				textualRating = ratingData[0] + '/' + ratingData[1],
+
+				renderStars = function( icon ) {
+					var starsHtml = '',
+						flooredRating = Math.floor( rating );
+
+					for ( var stars = 1; stars <= ratingData[1]; stars++ ) {
+						if ( stars <= flooredRating  ) {
+							starsHtml += '<i class="elementor-star-full">' + icon + '</i>';
+						} else if ( flooredRating + 1 === stars && rating !== flooredRating ) {
+							starsHtml += '<i class="elementor-star-' + ( rating - flooredRating ).toFixed( 1 ) * 10 + '">' + icon + '</i>';
+						} else {
+							starsHtml += '<i class="elementor-star-empty">' + icon + '</i>';
+						}
 					}
-				}
 
-				return starsHtml;
-			};
+					return starsHtml;
+				},
 
-			var icon = '&#61445;';
+			    icon = '&#61445;';
 
 			if ( 'fontawesome' === settings.star_style ) {
 				if ( 'outline' === settings.unmarked_star_style ) {
@@ -385,8 +438,8 @@ class Widget_Star_Rating extends Widget_Base {
 			}
 
 			view.addRenderAttribute( 'iconWrapper', 'class', 'elementor-star-rating' );
-			view.addRenderAttribute( 'iconWrapper', 'title', settings.rating );
 			view.addRenderAttribute( 'iconWrapper', 'itemtype', 'http://schema.org/Rating' );
+			view.addRenderAttribute( 'iconWrapper', 'title', textualRating );
 			view.addRenderAttribute( 'iconWrapper', 'itemscope', '' );
 			view.addRenderAttribute( 'iconWrapper', 'itemprop', 'reviewRating' );
 
@@ -399,7 +452,7 @@ class Widget_Star_Rating extends Widget_Base {
 			<# } #>
 			<div {{{ view.getRenderAttributeString( 'iconWrapper' ) }}} >
 				{{{ stars }}}
-				<span itemprop="ratingValue" class="elementor-screen-only">{{ settings.rating }}</span>
+				<span itemprop="ratingValue" class="elementor-screen-only">{{ textualRating }}</span>
 			</div>
 		</div>
 
