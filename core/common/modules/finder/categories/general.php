1632a7ec554bb80cba4292f603eff165e5cd6294
Kobi Zaltzberg <kobi@pojo.me>
2018-11-11 12:46:25 +0200

    Finder: 'link' => 'url'

--- core/common/modules/finder/categories/general.php
+++ core/common/modules/finder/categories/general.php
@@ -41,24 +41,24 @@ class General extends Base_Category {
 			'my-templates' => [
 				'title' => _x( 'My Templates', 'Template Library', 'elementor' ),
 				'icon' => 'library-save',
-				'link' => admin_url( 'edit.php?post_type=' . Source_Local::CPT ),
+				'url' => admin_url( 'edit.php?post_type=' . Source_Local::CPT ),
 				'keywords' => [ 'template', 'header', 'footer', 'single', 'archive', 'search', '404 page' ],
 			],
 			'system-info' => [
 				'title' => __( 'System Info', 'elementor' ),
 				'icon' => 'info',
-				'link' => admin_url( 'admin.php?page=elementor-system-info' ),
+				'url' => admin_url( 'admin.php?page=elementor-system-info' ),
 				'keywords' => [ 'system', 'info', 'environment', 'elementor' ],
 			],
 			'role-manager' => [
 				'title' => __( 'Role Manager', 'elementor' ),
 				'icon' => 'person',
-				'link' => Role_Manager::get_url(),
+				'url' => Role_Manager::get_url(),
 				'keywords' => [ 'role', 'manager', 'user', 'elementor' ],
 			],
 			'knowledge-base' => [
 				'title' => __( 'Knowledge Base', 'elementor' ),
-				'link' => admin_url( 'admin.php?page=go_knowledge_base_site' ),
+				'url' => admin_url( 'admin.php?page=go_knowledge_base_site' ),
 				'keywords' => [ 'help', 'knowledge', 'docs', 'elementor' ],
 			],
 		];
