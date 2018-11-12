82eaf3025c18618a4264e7d6589ddd506e6ae18f
Omri Bar-Zik <omri@bar-zik.com>
2018-11-12 12:31:00 +0200

    FIX cypress tests

--- tests/e2e/support/hooks.js
+++ tests/e2e/support/hooks.js
@@ -1,7 +1,13 @@
 /// <reference types="Cypress" />
 beforeEach( function() {
-    cy.login( 'admin' );
+	cy.login( 'admin' );
 } );
 
-after( function() {
+afterEach( function() {
+	cy.window()
+		.then( ( win ) => {
+			if ( win.elementor ) {
+				win.elementor.channels.editor.reply( 'status', false );
+			}
+		} );
 } );
