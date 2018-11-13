1a2d89cacea62ae4871f694ce11df3933782d696
Kobi Zaltzberg <kobi@pojo.me>
2018-11-13 14:52:24 +0200

    Added `EditorModule` to editor's modules list

--- assets/dev/js/modules/editor.js
+++ assets/dev/js/modules/editor.js
@@ -1,3 +1,11 @@
 import elementorModules from './common';
 
+import EditorModule from '../editor/utils/editor-module';
+
+elementorModules.editor = {
+	utils: {
+		Module: EditorModule,
+	},
+};
+
 export default elementorModules;
