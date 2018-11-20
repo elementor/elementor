6fe582e5afb51c42ca9a77e333db59176b3a0d22
Omri Bar-Zik <omri@bar-zik.com>
2018-11-20 15:37:45 +0200

    Tesst/e2e: New - Tests

---
+++ tests/e2e/integration/navigator.spec.js
@@ -0,0 +1,25 @@
+/// <reference types="Cypress" />
+
+describe( '', () => {
+	it( '', () => {
+		cy.addElementorPage();
+		cy.addWidget( 'basic', 'heading' );
+		cy.get( '#elementor-preview-iframe' ).then( ( $iframe ) => {
+			cy.wrap( $iframe.contents().find( '.elementor-widget-heading' ) )
+				.click( { force: true } )
+				.trigger( 'contextmenu' );
+		} );
+		cy.get( '.elementor-context-menu-list__item-navigator' ).click();
+		cy.get( '.elementor-tab-control-advanced' ).click();
+		cy.get( '.elementor-control-_section_responsive' ).scrollIntoView().click();
+		cy.get( '.elementor-control-hide_mobile .elementor-switch-label' ).click();
+
+		cy.get( '.elementor-navigator__item.elementor-editing > .elementor-navigator__element__toggle' )
+			.click( { force: true } );
+
+		cy.get( '#elementor-preview-iframe' ).then( ( $iframe ) => {
+			cy.wrap( $iframe.contents().find( '.elementor-widget-heading' ), { timeout: 5000 } )
+				.should( 'not.be.visible' );
+		} );
+	} );
+} );
--- tests/e2e/support/commands/add-elementor-page.js
+++ tests/e2e/support/commands/add-elementor-page.js
@@ -7,6 +7,9 @@ function addElementorPage() {
 	cy.get( '[id=elementor-switch-mode-button]' ).click();
 	cy.get( '[id=elementor-loading]' ).should( 'be.hidden' );
     Cypress.env( 'history', 1 );
+	cy.get( '#elementor-preview-iframe' ).then( ( $iframe ) => {
+		return $iframe.attr( 'src' ).split( /\?(p.*?)\&/ )[ 1 ].split( /.*=/ )[ 1 ];
+	} );
 }
 
 Cypress.Commands.add( 'addElementorPage', addElementorPage );
