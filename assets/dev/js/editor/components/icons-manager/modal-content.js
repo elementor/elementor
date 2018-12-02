1d5494aace5166794969aa6ed59dfdc3cc585113
raz ohad <admin@bainternet.info>
2018-12-02 17:53:57 +0200

    proggress

--- assets/dev/js/editor/components/icons-manager/modal-content.js
+++ assets/dev/js/editor/components/icons-manager/modal-content.js
@@ -6,4 +6,212 @@ export default class extends Marionette.LayoutView {
 	getTemplate() {
 		return '#tmpl-elementor-icons-manager';
 	}
+
+	getDefaultSettings() {
+		return {
+			name: '',
+			url: '',
+			prefix: '',
+			displayPrefix: '',
+			ver: '',
+			enqueue: [],
+			exclude: [],
+			include: [],
+			icons: [],
+			fetchJson: false,
+		};
+	}
+
+	iconTypes() {
+		return [];
+	}
+
+	ui() {
+		const ui = {};
+		ui.tabLabel = '.icon-type-tab-label';
+		ui.searchInput = '.icons-search';
+		// ui.iconList = '';
+		// ui.iconListItems = '';
+
+		return ui;
+	}
+
+	showTab() {
+		console.log( arguments, this );
+	}
+
+	// onSearch() {
+	// 	const input = this.ui.searchInput,
+	// 		value = input.value;
+	//
+	// 	if ( '' === value ) {
+	// 		return this.showAll();
+	// 	}
+	//
+	// 	return this.filterIcons( value );
+	// }
+	enqueueCSS( url ) {
+		return new Promise( function( resolve ) {
+			if ( ! document.querySelector( 'link[href="' + url + '"]' ) ) {
+				const link = document.createElement( 'link' );
+				link.type = 'text/css';
+				link.rel = 'stylesheet';
+				link.href = url;
+				link.onload = function() {
+					resolve();
+				};
+				const headScript = document.querySelector( 'head' );
+				headScript.append( link );
+			}
+		} );
+	}
+
+	parseCSS( css, prefix = 'fa-', displayPrefix = '', library = false ) {
+		const iconPattern = new RegExp( '\\.' + prefix + '([^\\.!:]*)::?before\\s*{\\s*content:\\s*["|\']\\\\[^\'|"]*["|\'];?\\s*}', 'g' ),
+			index = 1,
+			icons = {};
+		let icon,
+			match = iconPattern.exec( css );
+
+		while ( match ) {
+			icon = {
+				prefix: prefix,
+				selector: prefix + match[ index ].trim( ':' ),
+				name: this.ucwords( match[ index ] ).trim( ':' ).split( '-' ).join( ' ' ),
+				filter: match[ index ].trim( ':' ),
+				displayPrefix: ( displayPrefix || prefix.replace( '-', '' ) ),
+			};
+			icons[ match[ index ] ] = icon;
+			match = iconPattern.exec( css );
+		}
+
+		if ( ! library ) {
+			return icons;
+		}
+
+		library.icons = icons;
+		this.Icons[ library.name ] = icons;
+		this.store.save( library );
+	}
+
+	extractIconsFromCSS( library ) {
+		fetch( library.url, { mode: 'cors' } ).then( ( res ) => {
+			return res.text();
+		} ).then( ( css ) => {
+			this.parseCSS( css, library.prefix, library.displayPrefix || '', library );
+		} );
+	}
+
+	fetchIcons( library ) {
+		jQuery.getJSON( library.fetchJson, ( data ) => {
+			library.icons = data;
+			return this.normalizeIconList( library );
+		} );
+	}
+
+	normalizeIconList( library ) {
+		const icons = {};
+		let icon,
+			name;
+		for ( icon in library.icons ) {
+			name = library.icons[ icon ];
+			icons[ name ] = {
+				prefix: library.prefix,
+				selector: library.prefix + name.trim( ':' ),
+				name: this.ucwords( name ).trim( ':' ).split( '-' ).join( ' ' ),
+				filter: name.trim( ':' ),
+				displayPrefix: ( library.displayPrefix || library.prefix.replace( '-', '' ) ),
+			};
+		}
+		library.icons = icons;
+		this.Icons[ library.name ] = icons;
+		return this.store.save( library );
+	}
+
+	addIconType( config ) {
+		const exists = this.iconTypes.filter( ( conf ) => conf.name === config.name );
+		if ( exists.length ) {
+			return;
+		}
+		this.iconTypes.push( Object.assign( this.getDefaultSettings(), config ) );
+	}
+
+	ucwords( str ) {
+		return ( str + '' ).replace( /^(.)|\s+(.)/g, function( $1 ) {
+			return $1.toUpperCase();
+		} );
+	}
+
+	initIconType( libraryConfig ) {
+		// Enqueue CSS
+		if ( libraryConfig.enqueue ) {
+			libraryConfig.enqueue.forEach( ( assetURL ) => {
+				this.enqueueCSS( assetURL );
+			} );
+		}
+
+		if ( libraryConfig.url ) {
+			this.enqueueCSS( libraryConfig.url );
+		}
+
+		// already saved an stored
+		if ( this.store.isValid( libraryConfig ) ) {
+			return;
+		}
+
+		// comes with icons
+		if ( libraryConfig.icons.length ) {
+			return this.normalizeIconList( libraryConfig );
+		}
+
+		// Get icons from via ajax
+		if ( libraryConfig.fetchJson ) {
+			return this.fetchIcons( libraryConfig );
+		}
+
+		// try pharsing CSS
+		this.extractIconsFromCSS( libraryConfig );
+	}
+
+	events() {
+		return {
+			'click @ui.tabLabel': 'showTab',
+		};
+	}
+
+	store() {
+		return {
+			save( iconLibrary ) {
+				const storageKey = iconLibrary.name + '_icons';
+				this.set( storageKey, iconLibrary );
+			},
+			set( key, value ) {
+				const preparedValue = JSON.stringify( value );
+				window.localStorage.setItem( key, preparedValue );
+			},
+			getIcons( name ) {
+				return this.get( name + '_icons' );
+			},
+			get( key ) {
+				const saved = window.localStorage.getItem( key );
+				if ( ! saved ) {
+					return false;
+				}
+				return JSON.parse( saved );
+			},
+			isValid( iconLibrary ) {
+				const saved = this.get( iconLibrary.name + '_icons' );
+				if ( ! saved ) {
+					return false;
+				}
+				if ( saved.ver !== iconLibrary.ver ) {
+					return false;
+				}
+				return true;
+			},
+			del( key ) {
+				window.localStorage.removeItem( key );
+			},
+		};
+	}
 }
--- includes/editor-templates/icon-manager.php
+++ includes/editor-templates/icon-manager.php
@@ -6,178 +6,28 @@ use Elementor\Control_Icons;
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly
 }
-/*
- * <script>
-	class Tabs {
-        init() {
-            this.cacheElements();
-            this.setupAria();
-            this.appendIndicator();
-            this.bindEvents();
-        },
 
-        cacheElements() {
-            this.$el = $( '.tabs' );
-            this.cache = {
-                $tabList: this.$el.find( 'ul' ),
-                $tab: this.$tabList.find( 'li' ),
-                $tabFirst: this.$tabList.find( 'li:first-child a' ),
-                $tabLink: this.$tab.find( 'a' ),
-                $tabPanel: this.$el.find( 'section' ),
-                $tabPanelFirstContent: this.$el.find( 'section > *:first-child' ),
-                $tabPanelFirst: this.$el.find( 'section:first-child' ),
-                $tabPanelNotFirst: this.$el.find( 'section:not(:first-of-type)' ),
-            }
-        },
-
-			bindEvents() {
-				this.cache.$tabLink.on( 'click', () => {
-					this.changeTab();
-					this.animateIndicator( $( event.currentTarget ) );
-				}.bind( this ) );
-				this.cache.$tabLink.on( 'keydown', () => {
-					this.changeTabKey();
-				}.bind( this ) );
-			},
-
-			changeTab() {
-				const self = $( event.target );
-				event.preventDefault();
-				this.removeTabFocus();
-				this.setSelectedTab( self );
-				this.hideAllTabPanels();
-				this.setSelectedTabPanel( self );
-			},
-
-			animateIndicator: function(elem) {
-				var offset = elem.offset().left;
-				var width = elem.width();
-				var $indicator = this.$tabList.find('.indicator');
-
-				console.log(elem.width());
-
-				$indicator.transition({
-					x: offset,
-					width: elem.width()
-				})
-			},
-
-			appendIndicator: function() {
-				this.$tabList.append('<div class="indicator"></div>');
-			},
-
-			changeTabKey: function() {
-				var self = $(event.target),
-					$target = this.setKeyboardDirection(self, event.keyCode);
-
-				if ($target.length) {
-					this.removeTabFocus(self);
-					this.setSelectedTab($target);
-				}
-				this.hideAllTabPanels();
-				this.setSelectedTabPanel($(document.activeElement));
-				this.animateIndicator($target);
-			},
-
-			hideAllTabPanels: function() {
-				this.$tabPanel.attr('aria-hidden', 'true');
-			},
-
-			removeTabFocus: function(self) {
-				var $this = self || $('[role="tab"]');
-
-				$this.attr({
-					'tabindex': '-1',
-					'aria-selected': null
-				});
-			},
-
-			selectFirstTab: function() {
-				this.$tabFirst.attr({
-					'aria-selected': 'true',
-					'tabindex': '0'
-				});
-			},
-
-			setupAria: function() {
-				this.$tabList.attr('role', 'tablist');
-				this.$tab.attr('role', 'presentation');
-				this.$tabLink.attr({
-					'role': 'tab',
-					'tabindex': '-1'
-				});
-				this.$tabLink.each(function() {
-					var $this = $(this);
-
-					$this.attr('aria-controls', $this.attr('href').substring(1));
-				});
-				this.$tabPanel.attr({
-					'role': 'tabpanel'
-				});
-				this.$tabPanelFirstContent.attr({
-					'tabindex': '0'
-				});
-				this.$tabPanelNotFirst.attr({
-					'aria-hidden': 'true'
-				});
-				this.selectFirstTab();
-			},
-
-			setKeyboardDirection: function(self, keycode) {
-				var $prev = self.parents('li').prev().children('[role="tab"]'),
-					$next = self.parents('li').next().children('[role="tab"]');
-
-				switch (keycode) {
-					case 37:
-						return $prev;
-						break;
-					case 39:
-						return $next;
-						break;
-					default:
-						return false;
-						break;
-				}
-			},
-
-			setSelectedTab: function(self) {
-				self.attr({
-					'aria-selected': true,
-					'tabindex': '0'
-				}).focus();
-			},
-
-			setSelectedTabPanel: function(self) {
-				$('#' + self.attr('href').substring(1)).attr('aria-hidden', null);
-			},
-
-		};
-	}(jQuery);
-
-	Tabs.init();
-</script>
- */
 $tabs_list = [];
-foreach ( Control_Icons::get_icon_manager_tabs() as $tab_key => $tab_label ) {
-	$icon_type_handler = Control_Icons::get_type_handler( $tab_key );
-
-	$tabs_list[ $tab_key ] = [
-		'label' => $tab_label,
+foreach ( Control_Icons::get_icon_manager_tabs() as $tab_key => $icons_tab ) {
+	$tabs_list[] = [
+		'label' => $icons_tab['label'],
 		'key' => $tab_key,
-		'settings' => $icon_type_handler ? $icon_type_handler::get_settings() : [],
+		'settings' => $icons_tab,
 	];
 }
 ?>
 <script type="text/template" id="tmpl-elementor-icons-manager">
 	<div id="elementor-icon-manager__content">
 		<div class="elementor-icon-manager-tabs">
+			<input type="search" class="icons-search" placeholder="<?php esc_attr_e( 'Search...', 'elementor' ); ?>">
 			<ul class="elementor-icon-manager-tabs-list">
-				<?php foreach ( $tabs_list as $tab_key => $tab ) : ?>
-					<li data-tab="<?php echo $tab_key; ?>"><?php echo $tab['label']; ?></li>
+				<?php foreach ( $tabs_list as $tab ) : ?>
+					<li class="icon-type-tab-label" data-tab="<?php echo $tab['key']; ?>" data-settings="<?php echo esc_attr( wp_json_encode( $tab['settings'] ) ); ?>"><?php echo $tab['label']; ?></li>
 				<?php endforeach; ?>
 			</ul>
 			<div class="elementor-icon-manager-tabs-content">
-				<div class="default" data-settings=""></div>
+				<input type="hidden" name="icon_value" id="icon_value" value="">
+				<input type="hidden" name="icon_type" id="icon_type" value="">
 			</div>
 		</div>
 	</div>
