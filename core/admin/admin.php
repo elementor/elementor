146a9a07bee6e6f68b95c8fc020e63e7528d6367
Kobi Zaltzberg <kobi@pojo.me>
2018-12-03 10:39:11 +0200

    Removed admin modules

--- core/admin/admin.php
+++ core/admin/admin.php
@@ -72,18 +72,9 @@ class Admin extends App {
 	 */
 	public function enqueue_scripts() {
 		wp_register_script(
-			'elementor-admin-modules',
-			ELEMENTOR_ASSETS_URL . 'js/admin-modules.js',
-			[],
-			ELEMENTOR_VERSION,
-			false
-		);
-
-		wp_register_script(
 			'elementor-admin',
 			$this->get_js_assets_url( 'admin' ),
 			[
-				'elementor-admin-modules',
 				'elementor-common',
 			],
 			ELEMENTOR_VERSION,
