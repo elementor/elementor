ce0159c1707127ec4b9bd94bc8032ee65ea3125c
raz ohad <admin@bainternet.info>
2018-12-10 10:44:56 +0200

    progress and corrections
    implemented search/include/exclude

--- assets/dev/js/editor/components/icons-manager/icons-manager.js
+++ assets/dev/js/editor/components/icons-manager/icons-manager.js
@@ -10,17 +10,61 @@ export default class extends Module {
 			className: 'elementor-button',
 			text: elementor.translate( 'Insert' ),
 			callback: ( modal ) => {
-				this.updateControlValue( modal.getSettings( 'controlModel' ), this.layout.modalContent.currentView );
+				this.updateControlValue( modal.getSettings( 'controlView' ), this.layout.modalContent.currentView );
 			},
 		} );
+
+		this.layout.getModal().on( 'show', this.onPickerShow );
 	}
 
-	updateControlValue( model, view ) {
-		model.setValue( {
-			value: view.cache.value,
-			library: view.cache.type,
+	onPickerShow() {
+		const controlView = this.getSettings( 'controlView' ),
+			data = controlView.getControlValue(),
+			modalView = elementor.iconManager.layout.modalContent.currentView,
+			include = controlView.model.get( 'include' ),
+			exclude = controlView.model.get( 'exclude' ),
+			searchBar = controlView.model.get( 'search_bar' ),
+			controlIcons = {};
+
+		let tabToShow = Object.keys( ElementorConfig.icons )[ 0 ];
+
+		modalView.reset();
+		if ( include ) {
+			modalView.hideAllTabs();
+			_.each( include, ( icons, tab ) => {
+				modalView.showTabByName( tab );
+			} );
+			controlIcons.include = include;
+			tabToShow = Object.keys( include )[ 0 ];
+		}
+
+		if ( exclude ) {
+			controlIcons.exclude = exclude;
+		}
+
+		modalView.cache.icons = controlIcons;
+		modalView.toggleSearchBarVisibility( searchBar );
+
+		if ( data.library && data.value ) {
+			modalView.showTab( data.library );
+			const $icon = jQuery( '.icon-list-item i[class="' + data.value + '"]' );
+			if ( $icon.length ) {
+				const $iconLi = $icon.parent();
+				modalView.setSelected( $iconLi );
+				setTimeout( () => $iconLi[ 0 ].scrollIntoView(), 10 );
+			}
+			return;
+		}
+
+		modalView.showTab( tabToShow );
+	}
+
+	updateControlValue( view, modal ) {
+		view.setValue( {
+			value: modal.cache.value,
+			library: modal.cache.type,
 		} );
-		model.applySavedValue();
+		view.applySavedValue();
 	}
 
 	show( options ) {
