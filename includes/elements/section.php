9488c03606899611f053797a74388d2df8d4752d
Josh Marom <josh@pojo.me>
2018-12-16 18:09:16 +0200

    Remove double control

--- includes/elements/section.php
+++ includes/elements/section.php
@@ -665,14 +665,6 @@ class Element_Section extends Element_Base {
 			]
 		);
 
-		$this->add_group_control(
-			Group_Control_Css_Filter::get_type(),
-			[
-				'name' => 'css_filters',
-				'selector' => '{{WRAPPER}} .elementor-background-overlay',
-			]
-		);
-
 		$this->add_control(
 			'overlay_blend_mode',
 			[
