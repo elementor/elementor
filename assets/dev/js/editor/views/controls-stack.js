4df68b2467cdf230405e8dc08467bfb256eea145
Kobi Zaltzberg <kobi@pojo.me>
2018-12-18 18:04:36 +0200

    Fixed bug on responsive switchers when returning to desktop mode

--- assets/dev/js/editor/views/controls-stack.js
+++ assets/dev/js/editor/views/controls-stack.js
@@ -213,7 +213,9 @@ ControlsStack = Marionette.CompositeView.extend( {
 	},
 
 	onDeviceModeChange: function() {
-		this.$el.removeClass( 'elementor-responsive-switchers-open' );
+		if ( 'desktop' === elementor.channels.deviceMode.request( 'currentMode' ) ) {
+			this.$el.removeClass( 'elementor-responsive-switchers-open' );
+		}
 	},
 
 	onChildviewControlSectionClicked: function( childView ) {
