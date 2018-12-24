b9d14a9f251fe8549704ad3cebb501144629c09c
Kobi Zaltzberg <kobi@pojo.me>
2018-12-24 16:09:09 +0200

    `elementorFrontend`: Added `wpPreview` mode check and rearranged mode config

--- includes/frontend.php
+++ includes/frontend.php
@@ -1030,7 +1030,10 @@ class Frontend extends App {
 		$is_preview_mode = Plugin::$instance->preview->is_preview_mode( Plugin::$instance->preview->get_post_id() );
 
 		$settings = [
-			'isEditMode' => $is_preview_mode,
+			'environmentMode' => [
+				'edit' => $is_preview_mode,
+				'wpPreview' => is_preview(),
+			],
 			'is_rtl' => is_rtl(),
 			'breakpoints' => Responsive::get_breakpoints(),
 			'version' => ELEMENTOR_VERSION,
