3aac87a25267439c02fc8fe64d84093648791135
Mati Horovitz <mati@pojo.me>
2018-12-24 17:01:54 +0200

    CLI/Logger: Change - Log also to DB.

--- modules/wp-cli/cli-logger.php
+++ modules/wp-cli/cli-logger.php
@@ -1,16 +1,14 @@
 <?php
 namespace Elementor\Modules\WpCli;
 
-use Elementor\Core\Logger\Loggers\Base as Loggers_Base;
-
+use Elementor\Core\Logger\Loggers\Db;
 use Elementor\Core\Logger\Items\Log_Item_Interface as Log_Item_Interface;
 
-class Cli_Logger extends Loggers_Base {
+class Cli_Logger extends Db {
 	public function save_log( Log_Item_Interface $item ) {
 		\WP_CLI::log( $item );
-	}
 
-	protected function get_log() {
-		return [];
+
+		parent::save_log( $item );
 	}
 }
