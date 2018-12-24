449782a391ce9db718de239ff1356e3a29ccde18
Mati Horovitz <mati@pojo.me>
2018-12-24 17:04:43 +0200

    CLI/Logger: Fix - CLI log according to the log type.

--- modules/wp-cli/cli-logger.php
+++ modules/wp-cli/cli-logger.php
@@ -6,8 +6,18 @@ use Elementor\Core\Logger\Items\Log_Item_Interface as Log_Item_Interface;
 
 class Cli_Logger extends Db {
 	public function save_log( Log_Item_Interface $item ) {
-		\WP_CLI::log( $item );
 
+		switch ( $item->type ) {
+			case self::LEVEL_WARNING:
+				\WP_CLI::warning( $item->format() );
+				break;
+			case self::LEVEL_ERROR:
+				\WP_CLI::error( $item->format(), false );
+				break;
+			default:
+				\WP_CLI::log( $item->format() );
+				break;
+		}
 
 		parent::save_log( $item );
 	}
