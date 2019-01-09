564df77f01fb519ec720690b4755bb477b102f07
Kobi Zaltzberg <kobi@pojo.me>
2019-01-09 16:56:27 +0200

    Make the introduction functionality more generic

--- assets/dev/js/editor/elements/views/behaviors/introduction.js
+++ assets/dev/js/editor/elements/views/behaviors/introduction.js
@@ -1,82 +1,64 @@
-module.exports = Marionette.Behavior.extend( {
+export default class extends Marionette.Behavior {
+	ui() {
+		return {
+			editButton: '.elementor-editor-element-edit',
+		};
+	}
 
-	introductionViewed: false,
+	events() {
+		return {
+			'click @ui.editButton': 'show',
+		};
+	}
 
-	ui: {
-		editButton: '.elementor-editor-element-edit',
-	},
+	initialize() {
+		this.initIntroduction();
+	}
 
-	events: {
-		'click @ui.editButton': 'show',
-	},
+	initIntroduction() {
+		let introduction;
 
-	initialize: function() {
-		this.initDialog();
-	},
-
-	initDialog: function() {
-		var dialog;
-
-		this.getDialog = function() {
-			if ( ! dialog ) {
-				dialog = elementorCommon.dialogsManager.createWidget( 'buttons', {
-					className: 'elementor-introduction',
-					headerMessage: elementor.translate( 'meet_right_click_header' ),
-					message: elementor.translate( 'meet_right_click_message' ),
-					iframe: elementor.$preview,
-					position: {
-						my: 'center top+5',
-						at: 'center bottom',
-						collision: 'fit',
+		this.getIntroduction = () => {
+			if ( ! introduction ) {
+				introduction = new elementorModules.editor.utils.Introduction( {
+					introductionKey: 'rightClick',
+					dialogOptions: {
+						className: 'elementor-right-click-introduction',
+						headerMessage: elementor.translate( 'meet_right_click_header' ),
+						message: elementor.translate( 'meet_right_click_message' ),
+						iframe: elementor.$preview,
+						position: {
+							my: 'center top+5',
+							at: 'center bottom',
+							collision: 'fit',
+						},
 					},
-					effects: {
-						hide: 'hide',
-						show: 'show',
-					},
-					hide: {
-						onBackgroundClick: false,
+					onDialogInitCallback: ( dialog ) => {
+						dialog.addButton( {
+							name: 'learn-more',
+							text: elementor.translate( 'learn_more' ),
+							tag: 'div',
+							callback: () => {
+								open( elementor.config.help_right_click_url, '_blank' );
+							},
+						} );
+
+						dialog.addButton( {
+							name: 'ok',
+							text: elementor.translate( 'got_it' ),
+							callback: () => introduction.setViewed(),
+						} );
+
+						dialog.getElements( 'ok' ).addClass( 'elementor-button elementor-button-success' );
 					},
 				} );
-
-				dialog.addButton( {
-					name: 'learn-more',
-					text: elementor.translate( 'learn_more' ),
-					tag: 'div',
-					callback: function() {
-						open( elementor.config.help_right_click_url, '_blank' );
-					},
-				} );
-
-				dialog.addButton( {
-					name: 'ok',
-					text: elementor.translate( 'got_it' ),
-					callback: this.setIntroductionViewed.bind( this ),
-				} );
-
-				dialog.getElements( 'ok' ).addClass( 'elementor-button elementor-button-success' );
 			}
 
-			return dialog;
+			return introduction;
 		};
-	},
+	}
 
-	show: function( event ) {
-		if ( this.introductionViewed ) {
-			return;
-		}
-
-		var dialog = this.getDialog();
-
-		dialog.setSettings( 'position', {
-			of: event.currentTarget,
-		} );
-
-		dialog.show();
-	},
-
-	setIntroductionViewed: function() {
-		this.introductionViewed = true;
-
-		elementorCommon.ajax.addRequest( 'introduction_viewed' );
-	},
-} );
+	show( event ) {
+		this.getIntroduction().show( event.currentTarget );
+	}
+}
