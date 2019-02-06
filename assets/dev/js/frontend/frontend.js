1b38a91f1e7a9b0958bc74dabb4fdfb07755e1b8
Kobi Zaltzberg <kobi@pojo.me>
2019-02-06 12:15:37 +0200

    Added deprecation warning for `elementorFrontend.Module`

--- assets/dev/js/frontend/frontend.js
+++ assets/dev/js/frontend/frontend.js
@@ -15,9 +15,15 @@ class Frontend extends elementorModules.ViewModule {
 		super( ...args );
 
 		this.config = elementorFrontendConfig;
+	}
 
-		// TODO: BC since 2.5.0
-		this.Module = elementorModules.frontend.handlers.Base;
+	// TODO: BC since 2.5.0
+	get Module() {
+		if ( this.isEditMode() ) {
+			parent.elementorCommon.helpers.deprecatedMethod( 'elementorFrontend.Module', '2.5.0', 'elementorModules.frontend.handlers.Base' );
+		}
+
+		return elementorModules.frontend.handlers.Base;
 	}
 
 	getDefaultSettings() {
