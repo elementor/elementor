380adc22e49f6bbba6efdb7db02d3d2a2eba091e
Kobi Zaltzberg <kobi@pojo.me>
2019-02-12 11:12:34 +0200

    History Module: Some arrangements

--- modules/history/assets/js/history/empty.js
+++ modules/history/assets/js/history/empty.js
@@ -1,5 +1,13 @@
-module.exports = Marionette.ItemView.extend( {
-	template: '#tmpl-elementor-panel-history-no-items',
-	id: 'elementor-panel-history-no-items',
-	className: 'elementor-nerd-box',
-} );
+export default class extends Marionette.ItemView {
+	getTemplate() {
+		return '#tmpl-elementor-panel-history-no-items';
+	}
+
+	id() {
+		return 'elementor-panel-history-no-items';
+	}
+
+	onDestroy() {
+		this._parent.$el.removeClass( 'elementor-empty' );
+	}
+}
