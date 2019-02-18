6693890fbd89613c36a2af482112f4370fe5d0c3
nathz21 <47759333+nathz21@users.noreply.github.com>
2019-02-18 22:07:06 +0100

    Ending number of Counter define with shortcode
    This modification gives the possibility to define the ending number of the counter widget via a shortcode.
    Really helpful if you want to display dynamic statistics (number of members, of posts etc.)

--- includes/widgets/counter.php
+++ includes/widgets/counter.php
@@ -113,7 +113,7 @@ class Widget_Counter extends Widget_Base {
 			'ending_number',
 			[
 				'label' => __( 'Ending Number', 'elementor' ),
-				'type' => Controls_Manager::NUMBER,
+				'type' => Controls_Manager::TEXT,
 				'default' => 100,
 			]
 		);
@@ -304,7 +304,7 @@ class Widget_Counter extends Widget_Base {
 		$this->add_render_attribute( 'counter', [
 			'class' => 'elementor-counter-number',
 			'data-duration' => $settings['duration'],
-			'data-to-value' => $settings['ending_number'],
+			'data-to-value' => do_shortcode($settings['ending_number']),
 		] );
 
 		if ( ! empty( $settings['thousand_separator'] ) ) {
