b878478bc229ab16e9229f14aee31947cfb5fe97
Yakir Sitbon <kingyes1@gmail.com>
2019-02-18 19:56:49 +0200

    Remove default value and scheme color

--- includes/widgets/image.php
+++ includes/widgets/image.php
@@ -509,13 +509,8 @@ class Widget_Image extends Widget_Base {
 			[
 				'label' => __( 'Background Color', 'elementor' ),
 				'type' => Controls_Manager::COLOR,
-				'default' => '',
 				'selectors' => [
 					'{{WRAPPER}} .widget-image-caption' => 'background-color: {{VALUE}};',
-				],
-				'scheme' => [
-					'type' => Scheme_Color::get_type(),
-					'value' => Scheme_Color::COLOR_3,
 				],
 			]
 		);
