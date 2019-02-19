653d3c9b54a858eb44ca97daabafd3d37b6f6230
nuritsha <nurit@elementor.com>
2019-02-19 16:08:06 +0200

    documents - fix: replace static get_editor_panel_config() with get_editor_panel_configuration()

--- core/base/document.php
+++ core/base/document.php
@@ -79,8 +79,24 @@ abstract class Document extends Controls_Stack {
 	 * @since 2.1.0
 	 * @access public
 	 * @static
+	 * @deprecated since 2.5.0 use get_editor_panel_configuration()
 	 */
-	public function get_editor_panel_config() {
+	public static function get_editor_panel_config() {
+		return [
+			'widgets_settings' => [],
+			'elements_categories' => static::get_editor_panel_categories(),
+			'messages' => [
+				/* translators: %s: the document title. */
+				'publish_notification' => sprintf( __( 'Hurray! Your %s is live.', 'elementor' ), self::get_title() ),
+			],
+		];
+	}
+
+	/**
+	 * @since 2.5.0
+	 * @access public
+	 */
+	public function get_editor_panel_configuration() {
 		return [
 			'widgets_settings' => [],
 			'elements_categories' => static::get_editor_panel_categories(),
@@ -416,7 +432,7 @@ abstract class Document extends Controls_Stack {
 			'type' => $this->get_name(),
 			'remoteLibrary' => $this->get_remote_library_config(),
 			'last_edited' => $this->get_last_edited(),
-			'panel' => $this->get_editor_panel_config(),
+			'panel' => $this->get_editor_panel_configuration(), //get_editor_panel_config(),
 			'container' => 'body',
 			'urls' => [
 				'exit_to_dashboard' => $this->get_exit_to_dashboard_url(),
