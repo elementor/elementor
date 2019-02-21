979a16a7f4bfe62630dd4cc492a58227054cbe38
Kobi Zaltzberg <kobi@pojo.me>
2019-02-21 11:05:33 +0200

    Fixed motion effects selectors

--- includes/elements/column.php
+++ includes/elements/column.php
@@ -312,7 +312,7 @@ class Element_Column extends Element_Base {
 			Group_Control_Background::get_type(),
 			[
 				'name' => 'background',
-				'selector' => '{{WRAPPER}} > .elementor-element-populated, {{WRAPPER}} > .elementor-column-wrap > .elementor-motion-effects-container > .elementor-motion-effects-layer',
+				'selector' => '{{WRAPPER}}:not(.elementor-motion-effects-element-type-background) > .elementor-element-populated, {{WRAPPER}} > .elementor-column-wrap > .elementor-motion-effects-container > .elementor-motion-effects-layer',
 			]
 		);
 
