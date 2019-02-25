ae83aaa2804ff05dbc1136d039901438032d951d
raz ohad <admin@bainternet.info>
2019-02-25 11:03:47 +0200

    Tweak: Added Dynamic capabilities all Inner Section CSS class and CSS ID

--- includes/elements/column.php
+++ includes/elements/column.php
@@ -789,6 +789,9 @@ class Element_Column extends Element_Base {
 				'label' => __( 'CSS ID', 'elementor' ),
 				'type' => Controls_Manager::TEXT,
 				'default' => '',
+				'dynamic' => [
+					'active' => true,
+				],
 				'title' => __( 'Add your custom id WITHOUT the Pound key. e.g: my-id', 'elementor' ),
 				'label_block' => false,
 				'style_transfer' => false,
@@ -801,6 +804,9 @@ class Element_Column extends Element_Base {
 				'label' => __( 'CSS Classes', 'elementor' ),
 				'type' => Controls_Manager::TEXT,
 				'default' => '',
+				'dynamic' => [
+					'active' => true,
+				],
 				'prefix_class' => '',
 				'title' => __( 'Add your custom class WITHOUT the dot. e.g: my-class', 'elementor' ),
 				'label_block' => false,
