aea4e0cee7720abe8e1795e9dd9591ac561bf2a7
raz ohad <admin@bainternet.info>
2019-02-25 11:04:39 +0200

    Tweak: Added Dynamic capabilities Section CSS class and CSS ID

--- includes/elements/section.php
+++ includes/elements/section.php
@@ -1195,6 +1195,9 @@ class Element_Section extends Element_Base {
 				'label' => __( 'CSS ID', 'elementor' ),
 				'type' => Controls_Manager::TEXT,
 				'default' => '',
+				'dynamic' => [
+					'active' => true,
+				],
 				'title' => __( 'Add your custom id WITHOUT the Pound key. e.g: my-id', 'elementor' ),
 				'label_block' => false,
 				'style_transfer' => false,
@@ -1207,6 +1210,9 @@ class Element_Section extends Element_Base {
 				'label' => __( 'CSS Classes', 'elementor' ),
 				'type' => Controls_Manager::TEXT,
 				'default' => '',
+				'dynamic' => [
+					'active' => true,
+				],
 				'prefix_class' => '',
 				'title' => __( 'Add your custom class WITHOUT the dot. e.g: my-class', 'elementor' ),
 				'label_block' => false,
