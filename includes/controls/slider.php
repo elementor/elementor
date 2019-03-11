9c06021df32c4e6215a112fa83d5c1acc95efc42
Kobi Zaltzberg <kobi@pojo.me>
2019-03-11 15:11:13 +0200

    Control Slider: Added labels and scales

--- includes/controls/slider.php
+++ includes/controls/slider.php
@@ -66,6 +66,8 @@ class Control_Slider extends Control_Base_Units {
 		return array_merge(
 			parent::get_default_settings(), [
 				'label_block' => true,
+				'labels' => [],
+				'scales' => 0,
 			]
 		);
 	}
@@ -87,6 +89,23 @@ class Control_Slider extends Control_Base_Units {
 			<label for="<?php echo $control_uid; ?>" class="elementor-control-title">{{{ data.label }}}</label>
 			<?php $this->print_units_template(); ?>
 			<div class="elementor-control-input-wrapper elementor-clearfix">
+				<# if ( isMultiple && ( data.labels.length || data.scales ) ) { #>
+					<div class="elementor-slider__extra">
+						<# if ( data.labels.length ) { #>
+							<div class="elementor-slider__labels">
+								<# jQuery.each( data.labels, ( index, label ) => { #>
+									<div class="elementor-slider__label">{{{ label }}}</div>
+								<# } ); #>
+							</div>
+						<# } if ( data.scales ) { #>
+							<div class="elementor-slider__scales">
+								<# for ( var i = 0; i < data.scales; i++ ) { #>
+									<div class="elementor-slider__scale"></div>
+								<# } #>
+							</div>
+						<# } #>
+					</div>
+				<# } #>
 				<div class="elementor-slider"></div>
 				<# if ( ! isMultiple ) { #>
 					<div class="elementor-slider-input">
