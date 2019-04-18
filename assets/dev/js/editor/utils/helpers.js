b3cf5a1482831a1fb43e730617b39547f8a0f4ef
Kobi Zaltzberg <kobi@pojo.me>
2019-04-18 16:04:08 +0300

    Removed old deprecated JS properties

--- assets/dev/js/editor/utils/helpers.js
+++ assets/dev/js/editor/utils/helpers.js
@@ -94,17 +94,6 @@ helpers = {
 		return Math.random().toString( 16 ).substr( 2, 7 );
 	},
 
-	/*
-	 * @deprecated 2.0.0
-	 */
-	stringReplaceAll: function( string, replaces ) {
-		var re = new RegExp( Object.keys( replaces ).join( '|' ), 'gi' );
-
-		return string.replace( re, function( matched ) {
-			return replaces[ matched ];
-		} );
-	},
-
 	isActiveControl: function( controlModel, values ) {
 		var condition,
 			conditions;
