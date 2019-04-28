10b09c1c109286c68f4bfaf65eb92bede16dc0d3
Shilo <shilo@elementor.com>
2019-04-28 17:14:03 +0300

    Tweak: removed redundant function

--- assets/dev/js/editor/controls/media.js
+++ assets/dev/js/editor/controls/media.js
@@ -44,6 +44,14 @@ ControlMediaItemView = ControlMultipleBaseItemView.extend( {
 		}
 
 		this.frame.open();
+
+		const selectedId = this.getControlValue( 'id' );
+		if ( ! selectedId ) {
+			return;
+		}
+
+		const selection = this.frame.state().get( 'selection' );
+		selection.add( wp.media.attachment( selectedId ) );
 	},
 
 	deleteImage: function( event ) {
@@ -77,20 +85,8 @@ ControlMediaItemView = ControlMultipleBaseItemView.extend( {
 			],
 		} );
 
-		this.frame.on( 'open', this.onFrameOpen.bind( this ) );
-
 		// When a file is selected, run a callback.
 		this.frame.on( 'insert select', this.select.bind( this ) );
-	},
-
-	onFrameOpen: function() {
-		const selectedId = this.getControlValue( 'id' );
-		if ( ! selectedId ) {
-			return;
-		}
-
-		const selection = this.frame.state().get( 'selection' );
-		selection.add( wp.media.attachment( selectedId ) );
 	},
 
 	/**
