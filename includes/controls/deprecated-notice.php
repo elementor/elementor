b8581323f523d01a12546b39ba4053c375ba91f5
nuritsha <nurit@elementor.com>
2019-05-15 12:25:00 +0300

    New: Deprecated Notice control

---
+++ includes/controls/deprecated-notice.php
@@ -0,0 +1,70 @@
+<?php
+namespace Elementor;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+/**
+ * Elementor Deprecated Notice control.
+ *
+ * A base control specific for creating Deprecation Notices control.
+ * Displays a warning notice in the panel.
+ *
+ * @since 2.6.0
+ */
+class Control_Deprecated_Notice extends Base_UI_Control {
+
+	/**
+	 * Get edprecated-notice control type.
+	 *
+	 * Retrieve the control type, in this case `raw_html`.
+	 *
+	 * @since 2.6.0
+	 * @access public
+	 *
+	 * @return string Control type.
+	 */
+	public function get_type() {
+		return 'deprecated_notice';
+	}
+
+	/**
+	 * Render deprecated notice control output in the editor.
+	 *
+	 * Used to generate the control HTML in the editor using Underscore JS
+	 * template. The variables for the class are available using `data` JS
+	 * object.
+	 *
+	 * @since 2.6.0
+	 * @access public
+	 */
+	public function content_template() {
+		?>
+		<# if ( data.label ) { #>
+		<span class="elementor-control-title">{{{ data.label }}}</span>
+		<# } #>
+		<div class="elementor-control-deprecated-notice elementor-panel-alert elementor-panel-alert-warning">Widget {{{ data.tag }}} is deprecated since: {{{ data.since }}}, use: {{{  data.replacement }}}. Note that {{{ data.tag }}} will be completely removed from Elementor {{{ data.die }}}</div>
+		<?php
+	}
+
+	/**
+	 * Get deprecated-notice control default settings.
+	 *
+	 * Retrieve the default settings of the deprecated notice control. Used to return the
+	 * default settings while initializing the deprecated notice control.
+	 *
+	 * @since 2.6.0
+	 * @access protected
+	 *
+	 * @return array Control default settings.
+	 */
+	protected function get_default_settings() {
+		return [
+			'tag' => '',
+			'since' => '',
+			'die' => '',
+			'replacement' => '',
+		];
+	}
+}
