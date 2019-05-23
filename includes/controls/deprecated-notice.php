2f2b8f9f938f85e0064a6eefbe0385e43658ce3f
nuritsha <nurit@elementor.com>
2019-05-23 10:58:04 +0300

    Tweak: message for widgets only.

--- includes/controls/deprecated-notice.php
+++ includes/controls/deprecated-notice.php
@@ -70,12 +70,11 @@ class Control_Deprecated_Notice extends Base_UI_Control {
 	 */
 	protected function get_default_settings() {
 		return [
-			'element_name' => '', // Element name
-			'element_type' => __( 'Widget', 'elementor' ), // widget | control
+			'widget' => '', // Widgets name
 			'since' => '', // Plugin version widget was deprecated
 			'last' => '', // Plugin version in which the widget will be removed
-			'replacement' => '', // Widget replacement
 			'plugin' => '', // Plugin's title
+			'replacement' => '', // Widget replacement
 		];
 	}
 }
