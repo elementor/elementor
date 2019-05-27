a31234ff657568084590943caf8f21825f0db053
nuritsha <nurit@elementor.com>
2019-05-27 17:08:18 +0300

    Tweak: section video background - support start & end time for self-hosted videos (closes #7941)

--- assets/dev/js/frontend/handlers/section.js
+++ assets/dev/js/frontend/handlers/section.js
@@ -120,7 +120,7 @@ const BackgroundVideo = elementorModules.frontend.handlers.Base.extend( {
 			videoLink = self.getElementSettings( 'background_video_link' ),
 			videoID = elementorFrontend.utils.youtube.getYoutubeIDFromURL( videoLink );
 
-		self.isYTVideo = !! videoID;
+		self.isYTVideo = ! ! videoID;
 
 		if ( videoID ) {
 			elementorFrontend.utils.youtube.onYoutubeApiReady( function( YT ) {
@@ -129,6 +129,11 @@ const BackgroundVideo = elementorModules.frontend.handlers.Base.extend( {
 				}, 1 );
 			} );
 		} else {
+			if ( self.getElementSettings( 'background_video_start' ) || self.getElementSettings( 'background_video_end' ) ) {
+				const startTime = self.getElementSettings( 'background_video_start' ) || 0,
+					endTime = self.getElementSettings( 'background_video_end' );
+				videoLink += '#t=' + startTime + ( endTime ? ',' + endTime : '' );
+			}
 			self.elements.$backgroundVideoHosted.attr( 'src', videoLink ).one( 'canplay', self.changeVideoSize );
 		}
 
