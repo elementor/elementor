1fbb3798abb5a19a64a928c724d00fa03e371484
nuritsha <nurit@elementor.com>
2019-06-12 17:04:21 +0300

    Beta-tester newsletter - add link on tools page.

--- assets/dev/js/admin/beta-tester/beta-tester.js
+++ assets/dev/js/admin/beta-tester/beta-tester.js
@@ -1,17 +1,40 @@
 import BetaTesterLayout from './layout';
 
 class BetaTesterModule extends elementorModules.ViewModule {
-	getDefaultSettings() {}
-
-	getDefaultElements() {}
-
-	bindEvents() {}
-
 	onInit() {
 		elementorModules.ViewModule.prototype.onInit.apply( this, arguments );
+		if ( elementorAdmin.config.user.introduction.beta_tester_newsletter ) {
+			return;
+		}
+		this.showLayout();
+	}
 
+	showLayout() {
 		this.layout = new BetaTesterLayout();
 		this.layout.showModal();
+	}
+
+	getDefaultSettings() {
+		return {
+			selectors: {
+				beta_tester_first_to_know: '#beta-tester-first-to-know',
+			},
+		};
+	}
+
+	getDefaultElements() {
+		let elements = {};
+		const selectors = this.getSettings( 'selectors' );
+
+		elements.$beta_tester_first_to_know = jQuery( selectors.beta_tester_first_to_know );
+
+		return elements;
+	}
+
+	bindEvents() {
+		const elements = this.elements;
+
+		elements.$beta_tester_first_to_know.on( 'click', this.showLayout.bind( this ) );
 	}
 }
 
