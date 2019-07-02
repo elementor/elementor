0e06e0ff273cdecf1b69c04306209e74d313d7bc
Udi Dollberg <udi@elementor.com>
2019-07-02 15:26:43 +0300

    changed alert, counter, progress widgets to ES6 classes syntax

--- assets/dev/js/frontend/handlers/progress.js
+++ assets/dev/js/frontend/handlers/progress.js
@@ -1,7 +1,34 @@
-module.exports = function( $scope, $ ) {
-	elementorFrontend.waypoint( $scope.find( '.elementor-progress-bar' ), function() {
-		var $progressbar = $( this );
+class Progress extends elementorModules.frontend.handlers.Base {
+	getDefaultSettings() {
+		return {
+			selectors: {
+				progressNumber: '.elementor-progress-bar',
+			},
+		};
+	}
 
-		$progressbar.css( 'width', $progressbar.data( 'max' ) + '%' );
+	getDefaultElements() {
+		const selectors = this.getSettings( 'selectors' );
+		return {
+			$progressNumber: this.$element.find( selectors.progressNumber ),
+		};
+	}
+
+	onInit() {
+		super.onInit();
+
+		elementorFrontend.waypoint( this.elements.$progressNumber, function() {
+			var $progressbar = jQuery( this );
+
+			$progressbar.css( 'width', $progressbar.data( 'max' ) + '%' );
+		} );
+	}
+}
+
+module.exports = ( $scope ) => {
+	new Progress( {
+		$element: $scope,
 	} );
 };
+
+// TODO: change the call to this widget in elements-handler.js?
