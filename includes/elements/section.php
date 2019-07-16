09a5cb6fe071f7ca626b5104d41c8f390e9bb72e
Udi Dollberg <udi@elementor.com>
2019-07-16 14:53:46 +0300

    fine tuning for section and column vertical align options, column-section override

--- includes/elements/section.php
+++ includes/elements/section.php
@@ -424,20 +424,20 @@ class Element_Section extends Element_Base {
 				'options' => [
 					'' => __( 'Default', 'elementor' ),
 					'top' => __( 'Top', 'elementor' ),
-					'center' => __( 'Middle', 'elementor' ),
+					'middle' => __( 'Middle', 'elementor' ),
 					'bottom' => __( 'Bottom', 'elementor' ),
-                    'space-between' => __( 'Space Between', 'elementor' ),
-                    'space-around' => __( 'Space Around', 'elementor' ),
-                    'space-evenly' => __( 'Space Evenly', 'elementor' ),
+					'space-between' => __( 'Space Between', 'elementor' ),
+					'space-around' => __( 'Space Around', 'elementor' ),
+					'space-evenly' => __( 'Space Evenly', 'elementor' ),
 				],
-                'selectors_dictionary' => [
-                    'top' => 'flex-start',
-                    'bottom' => 'flex-end',
-                ],
-                'selectors' => [
-                    '{{WRAPPER}} > .elementor-container > .elementor-row > .elementor-column > .elementor-column-wrap' => 'align-items: {{VALUE}}',
-                    '{{WRAPPER}} > .elementor-container > .elementor-row > .elementor-column > .elementor-column-wrap > .elementor-widget-wrap' => 'align-content: {{VALUE}}',
-                ],
+				'selectors_dictionary' => [
+					'top' => 'flex-start',
+					'middle' => 'center',
+					'bottom' => 'flex-end',
+				],
+				'selectors' => [
+					'{{WRAPPER}} > .elementor-container > .elementor-row > .elementor-column > .elementor-column-wrap > .elementor-widget-wrap' => 'align-content: {{VALUE}}',
+				],
 			]
 		);
 
