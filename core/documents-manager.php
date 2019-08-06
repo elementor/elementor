032d4bf2362fde0912423033105f88f4a7af6cc1
Dibyajyoti Panda <dbjpanda@live.com>
2019-08-06 06:02:35 +0530

    Update documents-manager.php

--- core/documents-manager.php
+++ core/documents-manager.php
@@ -408,7 +408,7 @@ class Documents_Manager {
 		}
 
 		// Don't touch not existing or not allowed caps.
-		if ( isset( $caps[0] )){
+		if ( isset( $caps[0] ) ) {
 			if ( empty( $allcaps[ $caps[0] ] ) ) {
 				return $allcaps;
 			}
