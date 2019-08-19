8680caa5ff8eb21c5ad6b6d7519d13478c473bfb
Udi Dollberg <udi@elementor.com>
2019-08-19 12:35:33 +0300

    WIP vimeo background video initial commit - JS Module

---
+++ assets/dev/js/frontend/utils/vimeo.js
@@ -0,0 +1,29 @@
+class VimeoModule extends elementorModules.ViewModule {
+	getDefaultSettings() {
+		return {
+			isInserted: false,
+			APISrc: 'https://player.vimeo.com/api/player.js',
+			selectors: {
+				firstScript: 'script:first',
+			},
+		};
+	}
+
+	getDefaultElements() {
+		return {
+			$firstScript: jQuery( this.getSettings( 'selectors.firstScript' ) ),
+		};
+	}
+
+	insertVimeoAPI() {
+		this.setSettings( 'isInserted', true );
+
+		this.elements.$firstScript.before( jQuery( '<script>', { src: this.getSettings( 'APISrc' ) } ) );
+	}
+
+	getVimeoIDFromURL( url ) {
+		var videoIDParts = url.match( /^(?:https?:\/\/)?(?:www\.)?(?:vimeo\.com\/(?:d+))([^?&"'>]+)/ );
+
+		return videoIDParts && videoIDParts[ 1 ];
+	}
+}
