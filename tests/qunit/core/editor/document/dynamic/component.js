f68183f980bbdc5fc7279fb7bdf0725f2a8be79d
Leonid Vinikov <leonid@elementor.com>
2019-10-17 12:20:41 +0300

    WIP: $e: New - QUnit test dynamic in repeater (sketch).

--- tests/qunit/core/editor/document/dynamic/component.js
+++ tests/qunit/core/editor/document/dynamic/component.js
@@ -3,10 +3,10 @@ import Elements from '../helpers/elements';
 QUnit.module( 'Component: document/dynamic', () => {
 	QUnit.module( 'Single Selection', () => {
 		QUnit.test( 'Settings', ( assert ) => {
-			const eButton = Elements.createMockButtonWidget(),
-				text = '[elementor-tag id="33e3c57" name="post-custom-field" settings="%7B%7D"]',
+			const eButton = Elements.createAutoButton(),
+				dynamicTag = '[elementor-tag id="33e3c57" name="post-custom-field" settings="%7B%7D"]',
 				dynamicValue = '{ dynamic text }',
-				{ id, name, settings } = elementor.dynamicTags.tagTextToTagData( text ),
+				{ id, name, settings } = elementor.dynamicTags.tagTextToTagData( dynamicTag ),
 				tag = elementor.dynamicTags.createTag( id, name, settings ),
 				key = elementor.dynamicTags.createCacheKey( tag );
 
@@ -26,19 +26,51 @@ QUnit.module( 'Component: document/dynamic', () => {
 
 			$e.run( 'document/dynamic/settings', {
 				container: eButton,
-				settings: { text },
+				settings: { text: dynamicTag },
 			} );
 
 			done = assert.async();
+		} );
+
+		QUnit.test( 'In Repeater', ( assert ) => {
+			assert.equal( 1, 2, 'TODO: complete the test' );
+			/*const eForm = Elements.createAutoForm(),
+				eFormItem = eForm.children[ 0 ],
+				dynamicTag = '[elementor-tag id="d96ebd2" name="post-date" settings="%7B%22format%22%3A%22d%2Fm%2FY%22%7D"]', // post-date with non default format.
+				dynamicValue = '{ dynamic text }',
+				{ id, name, settings } = elementor.dynamicTags.tagTextToTagData( dynamicTag ),
+				tag = elementor.dynamicTags.createTag( id, name, settings ),
+				key = elementor.dynamicTags.createCacheKey( tag );
+
+			// Set fake data.
+			elementor.dynamicTags.cache[ key ] = dynamicValue;
+
+			let done;
+
+			eFormItem.view.attachElContent = function( html ) {
+				eFormItem.view.$el.empty().append( html );
+
+				done();
+
+				assert.equal( eForm.view.$el.find( '.button-text' ).html(), dynamicValue,
+					`button text changed to dynamic value: '${ dynamicValue }'` );
+			};
+
+			$e.run( 'document/dynamic/settings', {
+				container: eFormItem,
+				settings: { field_value: dynamicTag },
+			} );
+
+			done = assert.async();*/
 		} );
 	} );
 
 	QUnit.module( 'Multiple Selection', () => {
 		QUnit.test( 'Settings', ( assert ) => {
-			const eButtons = Elements.multiCreateMockButtonWidget(),
-				text = '[elementor-tag id="33e3c57" name="post-custom-field" settings="%7B%7D"]',
+			const eButtons = Elements.multiCreateAutoButton(),
+				dynamicTag = '[elementor-tag id="33e3c57" name="post-custom-field" settings="%7B%7D"]',
 				dynamicValue = '{ dynamic text }',
-				{ id, name, settings } = elementor.dynamicTags.tagTextToTagData( text ),
+				{ id, name, settings } = elementor.dynamicTags.tagTextToTagData( dynamicTag ),
 				tag = elementor.dynamicTags.createTag( id, name, settings ),
 				key = elementor.dynamicTags.createCacheKey( tag );
 
@@ -62,7 +94,7 @@ QUnit.module( 'Component: document/dynamic', () => {
 
 			$e.run( 'document/dynamic/settings', {
 				containers: eButtons,
-				settings: { text },
+				settings: { text: dynamicTag },
 			} );
 
 			done = assert.async();
