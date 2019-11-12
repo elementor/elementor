30a17a02642a9d8b20bd25a41b935648949423fe
Leonid Vinikov <leonid@elementor.com>
2019-11-12 15:16:51 +0200

    WIP: $e: Change - Refactor `Copy & Paste test to: `document/elements/commands/paste`.

--- tests/qunit/core/editor/document/elements/commands/index.js
+++ tests/qunit/core/editor/document/elements/commands/index.js
@@ -1,5 +1,7 @@
 // Alphabetical order.
 
+export { Copy } from './copy';
 export { CopyAll } from './copy-all';
 export { Create } from './create';
 export { Empty } from './empty';
+export { Paste } from './paste';
