ca021e39d9d58175e79a0c51f312f625a5985961
Yakir Sitbon <kingyes1@gmail.com>
2019-11-14 17:40:58 +0200

    Fixes PHP lint

--- includes/editor-templates/templates.php
+++ includes/editor-templates/templates.php
@@ -278,12 +278,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 	$images = [ 'left-1', 'left-2', 'right-1', 'right-2' ];
 
-	foreach( $images as $image ) {
-		?>
-		<img id="elementor-template-library-connect__background-image-<?php echo $image; ?>"
-			 class="elementor-template-library-connect__background-image"
-			 src="<?php echo $base_images_url . $image; ?>.png"/>
-		<?php
-	}
-	?>
+	foreach ( $images as $image ) : ?>
+		<img id="elementor-template-library-connect__background-image-<?php echo $image; ?>" class="elementor-template-library-connect__background-image" src="<?php echo $base_images_url . $image; ?>.png" />
+	<?php endforeach; ?>
 </script>
