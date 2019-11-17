c11f3fb9406943ec86a2b06bb751bbbfda01a2aa
Mati Horovitz <mati@elementor.com>
2019-11-17 17:58:35 +0200

    History/Revisions: Remove - Delete revision is no longer supported. (See: https://core.trac.wordpress.org/ticket/43709)

--- tests/phpunit/elementor/modules/history/test-revisions-manager.php
+++ tests/phpunit/elementor/modules/history/test-revisions-manager.php
@@ -235,46 +235,6 @@ class Elementor_Test_Revisions_Manager extends Elementor_Test_Base {
 		$this->assertArrayHaveKeys( [ 'settings', 'elements' ], $revision_data );
 	}
 
-	/**
-	 * @expectedException \Exception
-	 * @expectedExceptionMessage You must set the revision ID.
-	 */
-	public function test_should_not_delete_revision_on_request_because_of_unset_revision_ID() {
-		Revisions_Manager::ajax_delete_revision( [] );
-	}
-
-	/**
-	 * @expectedException \Exception
-	 * @expectedExceptionMessage Invalid revision.
-	 */
-	public function test_should_not_delete_revision_on_request_because_of_invalid_revision() {
-		$args['id'] = $this->fake_post_id;
-
-		Revisions_Manager::ajax_delete_revision( $args );
-	}
-
-	/**
-	 * @expectedException \Exception
-	 * @expectedExceptionMessage Access denied.
-	 */
-	public function test_should_not_delete_revision_on_request_because_of_access_denied() {
-		wp_set_current_user( $this->factory()->get_subscriber_user()->ID );
-		$args['id'] = $this->factory()->get_default_post();
-
-		Revisions_Manager::ajax_delete_revision( $args );
-	}
-
-	/**
-	 * @expectedException \Exception
-	 * @expectedExceptionMessage Cannot delete this revision.
-	 */
-	public function test_should_not_delete_revision_on_request_because_the_method_cannot_delete_given_revision() {
-		wp_set_current_user( $this->factory()->create_and_get_administrator_user()->ID );
-		$args['id'] = $this->factory()->create_and_get_default_post();
-
-		Revisions_Manager::ajax_delete_revision( $args );
-	}
-
 	private function setup_revision_check() {
 		$parent_and_child_posts = $this->factory()->create_and_get_parent_and_child_posts();
 		$post_id = $parent_and_child_posts['parent_id'];
