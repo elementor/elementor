aa719b25221feff193054e76a624bffbc5fe9950
Leonid Vinikov <leonid@elementor.com>
2019-11-19 10:30:12 +0200

    WIP: Change - Move `requireContainer` from `commands/base/base` to `modules/imports/args-object`.

--- assets/dev/js/editor/document/commands/base/base.js
+++ assets/dev/js/editor/document/commands/base/base.js
@@ -26,31 +26,6 @@ export default class Base extends ArgsObject {
 	}
 
 	/**
-	 * Function requireContainer().
-	 *
-	 * Validate `arg.container` & `arg.containers`.
-	 *
-	 * @param {{}} args
-	 *
-	 * @throws {Error}
-	 */
-	requireContainer( args = this.args ) {
-		if ( ! args.container && ! args.containers ) {
-			throw Error( 'container or containers are required.' );
-		}
-
-		if ( args.container && args.containers ) {
-			throw Error( 'container and containers cannot go together please select one of them.' );
-		}
-
-		const containers = args.containers || [ args.container ];
-
-		containers.forEach( ( container ) => {
-			this.requireArgumentInstance( 'container', elementorModules.editor.Container, { container } );
-		} );
-	}
-
-	/**
 	 * Function initialize().
 	 *
 	 * Initialize command, called after construction.
