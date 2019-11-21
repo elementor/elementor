a56c7251897582ed5a356fdee5e96ec83ad1022b
Leonid Vinikov <leonid@elementor.com>
2019-11-21 15:28:41 +0200

    WIP: Remove - Shortcuts from `document/elements`.

--- assets/dev/js/editor/document/elements/component.js
+++ assets/dev/js/editor/document/elements/component.js
@@ -39,34 +39,4 @@ export default class extends elementorModules.common.Component {
 
 		return temp;
 	}
-
-	defaultShortcuts() {
-		return {
-			copy: {
-				keys: 'ctrl+c',
-				exclude: [ 'input' ],
-			},
-			duplicate: {
-				keys: 'ctrl+d',
-			},
-			delete: {
-				keys: 'del',
-				exclude: [ 'input' ],
-			},
-			paste: {
-				keys: 'ctrl+v',
-				exclude: [ 'input' ],
-				dependency: () => {
-					return elementor.getCurrentElement().isPasteEnabled();
-				},
-			},
-			pasteStyle: {
-				keys: 'ctrl+shift+v',
-				exclude: [ 'input' ],
-				dependency: () => {
-					return elementor.getCurrentElement().pasteStyle && elementorCommon.storage.get( 'clipboard' );
-				},
-			},
-		};
-	}
 }
