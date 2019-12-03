f2cc2363df1318ea8d1b3bd61753669b64989ed0
Leonid Vinikov <leonid@elementor.com>
2019-12-03 13:15:08 +0200

    Tests: Document/Dynamic: New - Multi enable.

--- tests/qunit/core/editor/document/dynamic/commands/enable.spec.js
+++ tests/qunit/core/editor/document/dynamic/commands/enable.spec.js
@@ -34,9 +34,40 @@ export const Enable = () => {
 			} );
 		} );
 
-		// QUnit.module( 'Multiple Selection', () => {
-		//
-		// } );
+		QUnit.module( 'Multiple Selection', () => {
+			QUnit.test( 'Simple', ( assert ) => {
+				const eButtons = ElementsHelper.multiCreateAutoButton(),
+					dynamicTag = '[elementor-tag id="33e3c57" name="post-custom-field" settings="%7B%7D"]',
+					dynamicValue = '{ dynamic text }',
+					{ id, name, settings } = elementor.dynamicTags.tagTextToTagData( dynamicTag ),
+					tag = elementor.dynamicTags.createTag( id, name, settings ),
+					key = elementor.dynamicTags.createCacheKey( tag );
+
+				// Set fake data.
+				elementor.dynamicTags.cache[ key ] = dynamicValue;
+
+				let doneEnable; // eslint-disable-line prefer-const
+
+				eButtons.forEach( ( eButton ) => {
+					eButton.view.attachElContent = function( html ) {
+						eButton.view.$el.empty().append( html );
+
+						assert.equal( eButton.view.$el.find( '.button-text' ).html(), dynamicValue,
+							`button with id: '${ eButton.id }' - button text changed to dynamic value: '${ dynamicValue }'` );
+
+						if ( eButton === eButtons[ eButtons.length - 1 ] ) {
+							doneEnable();
+						}
+					};
+				} );
+
+				DynamicHelper.multiEnable( eButtons, {
+					text: dynamicTag,
+				} );
+
+				doneEnable = assert.async();
+			} );
+		} );
 	} );
 };
 
