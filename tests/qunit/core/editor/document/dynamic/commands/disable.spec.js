c6b62276e2e27081b7042b5098e71733ff2417ab
Leonid Vinikov <leonid@elementor.com>
2019-12-03 13:15:00 +0200

    Tests: Document/Dynamic: New - Multi disable.

--- tests/qunit/core/editor/document/dynamic/commands/disable.spec.js
+++ tests/qunit/core/editor/document/dynamic/commands/disable.spec.js
@@ -41,9 +41,50 @@ export const Disable = () => {
 			} );
 		} );
 
-		// QUnit.module( 'Multiple Selection', () => {
-		//
-		// } );
+		QUnit.module( 'Multiple Selection', () => {
+			QUnit.test( 'Simple', ( assert ) => {
+				const eButtons = ElementsHelper.multiCreateAutoButton(),
+					eButtonText = eButtons[ 0 ].settings.get( 'text' ),
+					dynamicTag = '[elementor-tag id="33e3c57" name="post-custom-field" settings="%7B%7D"]',
+					dynamicValue = '{ dynamic text }',
+					{ id, name, settings } = elementor.dynamicTags.tagTextToTagData( dynamicTag ),
+					tag = elementor.dynamicTags.createTag( id, name, settings ),
+					key = elementor.dynamicTags.createCacheKey( tag );
+
+				// Set fake data.
+				elementor.dynamicTags.cache[ key ] = dynamicValue;
+
+				let doneDisable; // eslint-disable-line prefer-const
+
+
+				eButtons.forEach( ( eButton ) => {
+					eButton.view.attachElContent = function( html ) {
+						eButton.view.$el.empty().append( html );
+
+						if ( eButton === eButtons[ eButtons.length - 1 ] ) {
+							doneDisable();
+						}
+					};
+				} );
+
+				DynamicHelper.multiEnable( eButtons, {
+					text: dynamicTag,
+				} );
+
+				doneDisable = assert.async();
+
+				DynamicHelper.multiDisable( eButtons, {
+					text: dynamicTag,
+				} );
+
+				setTimeout( () => {
+					eButtons.forEach( ( eButton ) => {
+						assert.equal( eButton.view.$el.find( '.button-text' ).html(), eButtonText,
+							`button with id: '${ eButton.id }' - button text changed disabled to non-dynamic value: '${ eButtonText }'` );
+					} );
+				} );
+			} );
+		} );
 	} );
 };
 
