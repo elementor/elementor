9853788df0018efe5b0e030b8d41e02d9f382c55
Leonid Vinikov <leonid@elementor.com>
2019-12-05 13:49:26 +0200

    Tests: Fix - JSCS.

--- tests/qunit/core/editor/document/component.spec.js
+++ tests/qunit/core/editor/document/component.spec.js
@@ -2,16 +2,15 @@ import DocumentHelper from './helper';
 import ElementsHelper from './elements/helper';
 import * as Hooks from './callbacks/hooks/index.spec';
 
-
 QUnit.module( 'Component: document', () => {
 	QUnit.module( `Hooks`, ( hooks ) => {
 		hooks.beforeEach( () => {
 			ElementsHelper.empty();
 		} );
 
-		Object.entries( Hooks ).forEach( ( [ hookNamespace, hooks ] ) => {
+		Object.entries( Hooks ).forEach( ( [ hookNamespace, hook ] ) => {
 			QUnit.module( hookNamespace, () => {
-				DocumentHelper.testCommands( hooks );
+				DocumentHelper.testCommands( hook );
 			} );
 		} );
 	} );
