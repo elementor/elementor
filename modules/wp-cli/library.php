410ae191ea680093fa9dc3b55e9af8d38b716b0c
Mati Horovitz <mati@elementor.com>
2019-12-11 15:55:43 +0200

    Library/CLI: Review - Only `sync` command should support `--network` arg.

--- modules/wp-cli/library.php
+++ modules/wp-cli/library.php
@@ -66,16 +66,10 @@ class Library extends \WP_CLI_Command {
 	/**
 	 * Import template files to the Library.
 	 *
-	 * [--network]
-	 *      Import to all sites in the network.
-	 *
 	 * ## EXAMPLES
 	 *
 	 *  1. wp elementor library import <file-path>
 	 *      - This will import a file or a zip of multiple files to the library.
-	 *
-	 *  2. wp elementor library import --network
-	 *      - This will import the file(s)to each site in the network.
 	 *
 	 * @param $args
 	 * @param $assoc_args
@@ -83,36 +77,23 @@ class Library extends \WP_CLI_Command {
 	 * @since  2.8.0
 	 * @access public
 	 */
-	public function import( $args, $assoc_args ) {
+	public function import( $args ) {
 		if ( empty( $args[0] ) ) {
 			\WP_CLI::error( 'Please set file path.' );
 		}
 
 		$file = $args[0];
 
-		$network = isset( $assoc_args['network'] ) && is_multisite();
+		/** @var Source_Local $source */
+		$source = Plugin::$instance->templates_manager->get_source( 'local' );
 
-		if ( $network ) {
-			/** @var \WP_Site[] $sites */
-			$sites = get_sites();
+		$imported_items = $source->import_template( basename( $file ), $file );
 
-			foreach ( $sites as $keys => $blog ) {
-				// Cast $blog as an array instead of  object
-				$blog_id = $blog->blog_id;
-
-				switch_to_blog( $blog_id );
-
-				\WP_CLI::line( 'Site #' . $blog_id . ' - ' . get_option( 'blogname' ) );
-
-				$this->do_import( $file );
-
-				\WP_CLI::success( 'Done! - ' . get_option( 'home' ) );
-
-				restore_current_blog();
-			}
-		} else {
-			$this->do_import( $file );
+		if ( is_wp_error( $imported_items ) ) {
+			\WP_CLI::error( $imported_items->get_error_message() );
 		}
+
+		\WP_CLI::success( count( $imported_items ) . ' item(s) has been imported.' );
 	}
 
 	/**
@@ -124,7 +105,6 @@ class Library extends \WP_CLI_Command {
 	 *
 	 * --token
 	 *      A connect token from Elementor Account Dashboard.
-	 *
 	 *
 	 * ## EXAMPLES
 	 *
@@ -146,88 +126,8 @@ class Library extends \WP_CLI_Command {
 			\WP_CLI::error( 'Please set connect token.' );
 		}
 
-		$this->do_connect( $assoc_args['token'] );
-	}
-
-	/**
-	 * Disconnect site from Elementor Library.
-	 *
-	 * --user
-	 *      The user to disconnect <id|login|email>
-	 *
-	 * [--network]
-	 *      Disconnect user from all sites in the network.
-	 *
-	 * ## EXAMPLES
-	 *
-	 *  1. wp elementor library disconnect --user=admin
-	 *      - This will disconnect the admin from Elementor library.
-	 *
-	 *  2. wp elementor library disconnect --user=admin --network
-	 *      - This will disconnect the admin from Elementor library in each site in the network.
-	 *
-	 * @param $args
-	 * @param $assoc_args
-	 *
-	 * @since  2.8.0
-	 * @access public
-	 */
-	public function disconnect( $args, $assoc_args ) {
-		if ( ! get_current_user_id() ) {
-			\WP_CLI::error( 'Please set user to connect (--user=<id|login|email>).' );
-		}
-
-		$network = isset( $assoc_args['network'] ) && is_multisite();
-
-		if ( $network ) {
-			/** @var \WP_Site[] $sites */
-			$sites = get_sites();
-
-			foreach ( $sites as $keys => $blog ) {
-				// Cast $blog as an array instead of  object
-				$blog_id = $blog->blog_id;
-
-				switch_to_blog( $blog_id );
-
-				\WP_CLI::line( 'Site #' . $blog_id . ' - ' . get_option( 'blogname' ) );
-
-				$this->do_disconnect();
-
-				\WP_CLI::success( 'Done! - ' . get_option( 'home' ) );
-
-				restore_current_blog();
-			}
-		} else {
-			$this->do_disconnect();
-		}
-	}
-
-	private function do_sync( $force ) {
-		$data = Api::get_library_data( $force );
-
-		if ( empty( $data ) ) {
-			\WP_CLI::error( 'Cannot sync library.' );
-		}
-
-		\WP_CLI::success( 'Library has been synced.' );
-	}
-
-	private function do_import( $file ) {
-		/** @var Source_Local $source */
-		$source = Plugin::$instance->templates_manager->get_source( 'local' );
-
-		$imported_items = $source->import_template( basename( $file ), $file );
-
-		if ( is_wp_error( $imported_items ) ) {
-			\WP_CLI::error( $imported_items->get_error_message() );
-		}
-
-		\WP_CLI::success( count( $imported_items ) . ' item(s) has been imported.' );
-	}
-
-	private function do_connect( $token ) {
 		$_REQUEST['mode'] = 'cli';
-		$_REQUEST['token'] = $token;
+		$_REQUEST['token'] = $assoc_args['token'];
 
 		$app = $this->get_library_app();
 
@@ -236,13 +136,33 @@ class Library extends \WP_CLI_Command {
 		$app->action_get_token();
 	}
 
-	private function do_disconnect() {
+	/**
+	 * Disconnect site from Elementor Library.
+	 *
+	 * --user
+	 *      The user to disconnect <id|login|email>
+	 *
+	 * ## EXAMPLES
+	 *
+	 *  1. wp elementor library disconnect --user=admin
+	 *      - This will disconnect the admin from Elementor library.
+	 *
+	 * @param $args
+	 * @param $assoc_args
+	 *
+	 * @since  2.8.0
+	 * @access public
+	 */
+	public function disconnect() {
+		if ( ! get_current_user_id() ) {
+			\WP_CLI::error( 'Please set user to connect (--user=<id|login|email>).' );
+		}
+
 		$_REQUEST['mode'] = 'cli';
 
-		$app = $this->get_library_app();
-
-		$app->action_disconnect();
+		$this->get_library_app()->action_disconnect();
 	}
+
 	/**
 	 * @return \Elementor\Core\Common\Modules\Connect\Apps\Library
 	 */
