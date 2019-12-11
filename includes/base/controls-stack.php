7bec529305039f455de55c8eccf50104a3562a17
lopper <863201+lopper@users.noreply.github.com>
2019-12-11 10:49:24 -0800

    Fix sanitize_settings to handle settings that are NULL.
    When I edit a page with Elementor in Wordpress 5.3 I get the following error
    
    ```
    
    Fatal error: Uncaught TypeError: Argument 1 passed to Elementor\Controls_Stack::sanitize_settings() must be of the type array, null given, called in /homepages/23/d438281368/htdocs/clickandbuilds/Justponics/wp-content/plugins/elementor/includes/base/controls-stack.php on line 1023 and defined in /homepages/23/d438281368/htdocs/clickandbuilds/Justponics/wp-content/plugins/elementor/includes/base/controls-stack.php:1913 Stack trace: #0 /homepages/23/d438281368/htdocs/clickandbuilds/Justponics/wp-content/plugins/elementor/includes/base/controls-stack.php(1023): Elementor\Controls_Stack->sanitize_settings(NULL) #1 /homepages/23/d438281368/htdocs/clickandbuilds/Justponics/wp-content/plugins/elementor/includes/base/controls-stack.php(1776): Elementor\Controls_Stack->get_data('settings') #2 /homepages/23/d438281368/htdocs/clickandbuilds/Justponics/wp-content/plugins/elementor/core/base/base-object.php(132): Elementor\Controls_Stack->get_init_settings() #3 /homepages/23/d438281368/htdocs/clickandbuilds/Justponics/wp-content/plugi in /homepages/23/d438281368/htdocs/clickandbuilds/Justponics/wp-content/plugins/elementor/includes/base/controls-stack.php on line 1913
    Uncaught TypeError: Argument 1 passed to Elementor\Controls_Stack::sanitize_settings() must be of the type array, null given, called in /homepages/23/d438281368/htdocs/clickandbuilds/Justponics/wp-content/plugins/elementor/includes/base/controls-stack.php on line 1023 and defined in /homepages/23/d438281368/htdocs/clickandbuilds/Justponics/wp-content/plugins/elementor/includes/base/controls-stack.php:1913
    Stack trace:
    #0 /homepages/23/d438281368/htdocs/clickandbuilds/Justponics/wp-content/plugins/elementor/includes/base/controls-stack.php(1023): Elementor\Controls_Stack->sanitize_settings(NULL)
    #1 /homepages/23/d438281368/htdocs/clickandbuilds/Justponics/wp-content/plugins/elementor/includes/base/controls-stack.php(1776): Elementor\Controls_Stack->get_data('settings')
    #2 /homepages/23/d438281368/htdocs/clickandbuilds/Justponics/wp-content/plugins/elementor/core/base/base-object.php(132): Elementor\Controls_Stack->get_init_settings()
    #3 /homepages/23/d438281368/htdocs/clickandbuilds/Justponics/wp-content/plugi
    ```
    The issue is that sanitize_settings expects an array, but a NULL value is passed. I've updated the sanitize_settings function so that it can handle a NULL parameter.

--- includes/base/controls-stack.php
+++ includes/base/controls-stack.php
@@ -1910,7 +1910,11 @@ abstract class Controls_Stack extends Base_Object {
 	 *
 	 * @return array Sanitized settings.
 	 */
-	private function sanitize_settings( array $settings, array $controls = [] ) {
+	private function sanitize_settings( ?array $settings, array $controls = [] ) {
+		if(is_null($settings)){
+			$settings = [];
+		}
+		
 		if ( ! $controls ) {
 			$controls = $this->get_controls();
 		}
