14562532c74fb2f8c6b20ccc318203c1972e87ec
Rami Yushuvaev <r_a_m_i@hotmail.com>
2019-12-15 17:33:31 +0200

    Background video should have a background color
    Sections and columns have have Classic background (color / image), Gradient, Video or Slideshow.
    
    The video background is missing a fallback background color displayed while the video is loaded.
    
    This PR added a background color control to the video bg.

--- includes/controls/groups/background.php
+++ includes/controls/groups/background.php
@@ -136,7 +136,7 @@ class Group_Control_Background extends Group_Control_Base {
 				'{{SELECTOR}}' => 'background-color: {{VALUE}};',
 			],
 			'condition' => [
-				'background' => [ 'classic', 'gradient' ],
+				'background' => [ 'classic', 'gradient', 'video' ],
 			],
 		];
 
