16909e43f5c930fed8591fde40e82001b8809dab
Leonid Vinikov <leonid@elementor.com>
2019-12-15 16:35:26 +0200

    Editor/Modules: Change - Remove `CommandBaseHistory` replace with `CommandBase`.

--- assets/dev/js/editor/modules.js
+++ assets/dev/js/editor/modules.js
@@ -3,7 +3,7 @@ import Introduction from './utils/introduction';
 import ControlsStack from './views/controls-stack';
 import BaseSettings from './elements/models/base-settings';
 import Container from './container/container';
-import CommandsBaseHistory from './document/commands/base/history';
+import CommandsBase from './document/commands/base/base';
 import DocumentUtils from './document/utils/helpers';
 
 elementorModules.editor = {
@@ -21,7 +21,7 @@ elementorModules.editor = {
 	},
 
 	document: {
-		CommandBaseHistory: CommandsBaseHistory,
+		CommandBase: CommandsBase,
 
 		Utils: DocumentUtils,
 	},
