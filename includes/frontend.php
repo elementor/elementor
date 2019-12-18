62978756bd62c99d6e0f90392545e9efffc3e6bf
udidol <36724484+udidol@users.noreply.github.com>
2019-12-18 19:08:50 +0200

    change array key 'featured_image' to 'featuredImage'
    Co-Authored-By: Kobi Zaltzberg <jzaltzberg@gmail.com>

--- includes/frontend.php
+++ includes/frontend.php
@@ -1131,7 +1131,7 @@ class Frontend extends App {
 				'id' => $post->ID,
 				'title' => $post->post_title,
 				'excerpt' => $post->post_excerpt,
-				'featured_image' => get_the_post_thumbnail_url(),
+				'featuredImage' => get_the_post_thumbnail_url(),
 			];
 		} else {
 			$settings['post'] = [
