7d76d1bb5345fe60277114895acbc16a3ee1f0a6
Leonid Vinikov <leonid@elementor.com>
2020-01-02 19:41:44 +0200

    JS API/Document/Save: Fix - Auto command.

--- assets/dev/js/editor/document/save/commands/auto.js
+++ assets/dev/js/editor/document/save/commands/auto.js
@@ -2,16 +2,13 @@ import Base from './base/base';
 
 export class Auto extends Base {
 	apply( args ) {
-		const { force = false, options = {} } = args;
+		const { force = false } = args;
+		let { options } = args;
 
 		if ( ! force && 'edit' !== elementor.channels.dataEditMode.request( 'activeMode' ) ) {
 			return;
 		}
 
-		this.save( options );
-	}
-
-	save( options ) {
 		if ( this.component.isEditorChanged() ) {
 			// TODO: Move to es6.
 			options = _.extend( {
