d7d6471b9926266d53a6c395feb92f3a43eb196d
Udi Dollberg <udi@elementor.com>
2020-01-07 15:52:58 +0200

    Changed empty array values to `false` in URL control `options` parameter

--- includes/widgets/video.php
+++ includes/widgets/video.php
@@ -218,7 +218,7 @@ class Widget_Video extends Widget_Base {
 				'label' => __( 'URL', 'elementor' ),
 				'type' => Controls_Manager::URL,
 				'autocomplete' => false,
-				'options' => [],
+				'options' => false,
 				'label_block' => true,
 				'show_label' => false,
 				'dynamic' => [
