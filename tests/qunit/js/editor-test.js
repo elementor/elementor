6bac323c12bd4a6535e50f5ab91cfea39cc1f57d
Mati Horovitz <mati@elementor.com>
2020-01-08 13:16:46 +0200

    WIP: Editor/Document Loader: Fix tests - Refactor test loading, move all configs to editor-config.js.

--- tests/qunit/js/editor-test.js
+++ tests/qunit/js/editor-test.js
@@ -1,50 +1,31 @@
 import EditorBase from 'elementor-editor/editor-base';
+import config from './editor-config';
+import frontend from './frontend';
 
 export default class EditorTest extends EditorBase {
 	constructor( options ) {
 		super( options );
 
 		QUnit.testStart( ( { module, name } ) => {
-			console.log( 'Testing', module, name );
+			console.log( `Test: ${ module } -> ${ name }` );
 		} );
 
 		QUnit.testDone( ( { module, name } ) => {
-			console.log( 'Done Testing', module, name );
+			console.log( `Done: ${ module } -> ${ name }` );
 
 			this.$previewElementorEl.empty();
 		} );
 	}
 
-	initPreview() {
-		this.$preview = jQuery( '#elementor-preview-iframe' );
-
-		jQuery( document ).ready( () => {
-			this.$preview.trigger( 'load' );
-		} );
-
-		super.initPreview();
-	}
-
-	initFrontend() {
-		elementorFrontend.init = () => console.log( 'initFrontend' );
-
-		elementorFrontend.elements.$body = jQuery( '#elementor-test' );
-
-		super.initFrontend();
+	getConfig() {
+		return config;
 	}
 
 	onPreviewLoaded() {
-		this.$preview = jQuery( '#elementor-preview-iframe' );
-
-		this.$preview[ 0 ].contentWindow.elementorFrontend = {
-			init: () => console.log( 'elementorFrontend::init' ),
-		};
+		this.$preview[ 0 ].contentWindow.elementorFrontend = frontend;
 
 		this.$previewContents = this.$preview.contents();
-		this.$previewContents.find( 'body' ).append( '<div id="elementor"></div>' );
-
-		// Shortcut bind, in other words make shortcuts listen to iframe.
-		elementorFrontend.elements.$window = jQuery( '#elementor-preview-iframe' );
+		this.$previewContents.find( 'body' ).append( '<div class="elementor elementor-1"></div>' );
 
 		super.onPreviewLoaded();
 	}
@@ -59,4 +40,3 @@ export default class EditorTest extends EditorBase {
 		// Do nothing, bypass parent function.
 	}
 }
-
