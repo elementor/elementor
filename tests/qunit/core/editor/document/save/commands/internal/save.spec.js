b5344f167bf11b93b124a35e7908ae0d8511643f
Leonid Vinikov <leonid@elementor.com>
2020-01-16 15:29:07 +0200

    Tests/QUnit: Fix - Complete 'document/save/save' simple test.

--- tests/qunit/core/editor/document/save/commands/internal/save.spec.js
+++ tests/qunit/core/editor/document/save/commands/internal/save.spec.js
@@ -1,6 +1,18 @@
 export const Save = () => {
-	QUnit.test( 'Save', ( assert ) => {
-		assert.equal( 1, 1, 'TODO write the test' );
+	QUnit.module( 'Save', () => {
+		QUnit.test( 'Simple', async ( assert ) => {
+			$e.internal( 'document/save/set-is-modified', { status: true } );
+
+			$e.hooks.deactivate();
+
+			const response = await $e.internal( 'document/save/save' );
+
+			$e.hooks.activate();
+
+			assert.equal( $e.components.get( 'document/save' ).isEditorChanged(), false,
+				'Editor saver flag set to be: "false" after successfully save' );
+			assert.equal( response.data.status, 'draft', 'The response status is: "draft"' );
+		} );
 	} );
 };
 
