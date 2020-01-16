bd2bdef29877edb984917c00909860b31eb8b674
Mati Horovitz <mati@elementor.com>
2020-01-16 18:05:21 +0200

    WIP: Global Manager: Progress - Remove global ElementorConfig from panel header. (for tests)

--- core/kits/assets/js/panel-header-behavior.js
+++ core/kits/assets/js/panel-header-behavior.js
@@ -19,8 +19,8 @@ export default class extends Marionette.Behavior {
 	}
 
 	onBeforeShow() {
-		this.$el.prepend( elementor.compileTemplate( buttonBack, elementor.config.i18n ) );
-		this.$el.append( elementor.compileTemplate( buttonClose, elementor.config.i18n ) );
+		this.$el.prepend( elementor.compileTemplate( buttonBack, { Back: elementor.translate( 'Back' ) } ) );
+		this.$el.append( elementor.compileTemplate( buttonClose, { Close: elementor.translate( 'Close' ) } ) );
 	}
 
 	onClickClose() {
