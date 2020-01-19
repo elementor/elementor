5ad4d83a00bafc6c1be41024391e4faac60ef4af
Leonid Vinikov <leonid@elementor.com>
2020-01-19 18:14:34 +0200

    Tests/Document/Save: New - Test command 'pending'.

--- tests/qunit/core/editor/document/save/commands/index.spec.js
+++ tests/qunit/core/editor/document/save/commands/index.spec.js
@@ -4,3 +4,4 @@ export { Auto } from './auto.spec.js';
 export { Default } from './default.spec.js';
 export { Draft } from './draft.spec.js';
 export { Discard } from './discard.spec.js';
+export { Pending } from './pending.spec.js';
