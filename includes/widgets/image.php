3f42dca944d6d0a26dd9b123b497b8072dca1d66
Josh <Josh@elementor.com>
2020-01-19 18:14:29 +0200

    Fix bug when no image id

--- includes/widgets/image.php
+++ includes/widgets/image.php
@@ -626,31 +626,36 @@ class Widget_Image extends Widget_Base {
 		$link = $this->get_link_url( $settings );
 
 		if ( $link ) {
-			$lightbox_title_src = Manager::get_settings_managers( 'general' )->get_model()->get_settings( 'elementor_lightbox_title_src' );
-			$lightbox_description_src = Manager::get_settings_managers( 'general' )->get_model()->get_settings( 'elementor_lightbox_description_src' );
-			$attachment = get_post( $settings['image']['id'] );
-			$image_data = [
-				'caption' => $attachment->post_excerpt,
-				'description' => $attachment->post_content,
-				'title' => $attachment->post_title,
-			];
-
-			if ( $lightbox_title_src && $image_data[ $lightbox_title_src ] ) {
-				$this->add_render_attribute( 'link', 'data-elementor-lightbox-title', $image_data[ $lightbox_title_src ] );
-			}
-
-			if ( $lightbox_description_src && $image_data[ $lightbox_description_src ] ) {
-				$this->add_render_attribute( 'link', 'data-elementor-lightbox-description', $image_data[ $lightbox_description_src ] );
-			}
-
-			$this->add_render_attribute( 'link', 'data-elementor-open-lightbox', $settings['open_lightbox'] );
-
 			$this->add_link_attributes( 'link', $link );
 
 			if ( Plugin::$instance->editor->is_edit_mode() ) {
 				$this->add_render_attribute( 'link', [
 					'class' => 'elementor-clickable',
 				] );
+			}
+
+			// Add lightbox data attributes
+			if ( 'custom' !== $settings['link_to'] ) {
+				$this->add_render_attribute( 'link', 'data-elementor-open-lightbox', $settings['open_lightbox'] );
+				if ( $settings['image']['id'] ) {
+					$attachment = get_post( $settings['image']['id'] );
+					$lightbox_title_src = Manager::get_settings_managers( 'general' )->get_model()->get_settings( 'elementor_lightbox_title_src' );
+					$lightbox_description_src = Manager::get_settings_managers( 'general' )->get_model()->get_settings( 'elementor_lightbox_description_src' );
+					$image_data = [
+						'alt' => get_post_meta( $attachment->ID, '_wp_attachment_image_alt', true ),
+						'caption' => $attachment->post_excerpt,
+						'description' => $attachment->post_content,
+						'title' => $attachment->post_title,
+					];
+
+					if ( $lightbox_title_src && $image_data[ $lightbox_title_src ] ) {
+						$this->add_render_attribute( 'link', 'data-elementor-lightbox-title', $image_data[ $lightbox_title_src ] );
+					}
+
+					if ( $lightbox_description_src && $image_data[ $lightbox_description_src ] ) {
+						$this->add_render_attribute( 'link', 'data-elementor-lightbox-description', $image_data[ $lightbox_description_src ] );
+					}
+				}
 			}
 		} ?>
 		<div <?php echo $this->get_render_attribute_string( 'wrapper' ); ?>>
