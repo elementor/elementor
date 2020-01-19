da4c80f3e9b03f1a9cd27def0a9e27a9dbf49810
Leonid Vinikov <leonid@elementor.com>
2020-01-19 18:16:47 +0200

    Tests/Document/Save: New - Test command 'publish'.

--- tests/qunit/core/editor/document/save/commands/index.spec.js
+++ tests/qunit/core/editor/document/save/commands/index.spec.js
@@ -5,3 +5,4 @@ export { Default } from './default.spec.js';
 export { Draft } from './draft.spec.js';
 export { Discard } from './discard.spec.js';
 export { Pending } from './pending.spec.js';
+export { Publish } from './publish.spec.js';
