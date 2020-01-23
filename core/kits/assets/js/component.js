f5ed06b73b59547e8aee6cd40c5386f0457cf2b2
Mati Horovitz <mati@elementor.com>
2020-01-23 17:10:17 +0200

    Global Manager: Change - Extract commands to files.

--- core/kits/assets/js/component.js
+++ core/kits/assets/js/component.js
@@ -1,4 +1,5 @@
 import EnqueueFonts from './hooks/ui/enqueue-fonts';
+import * as commands from './commands/';
 
 export default class extends elementorModules.common.Component {
 	pages = {};
@@ -20,65 +21,7 @@ export default class extends elementorModules.common.Component {
 	}
 
 	defaultCommands() {
-		return {
-			open: () => {
-				const kit = elementor.documents.get( elementor.config.kit_id );
-
-				if ( kit && 'open' === kit.editor.status ) {
-					$e.route( 'panel/global/style' );
-					return jQuery.Deferred().resolve();
-				}
-
-				$e.routes.clearHistory( this.getRootContainer() );
-
-				this.toggleHistoryClass();
-
-				$e.internal( 'panel/state-loading' );
-
-				return $e.run( 'editor/documents/switch', {
-					id: elementor.config.kit_id,
-				} ).then( () => {
-					$e.internal( 'panel/state-ready' );
-				} );
-			},
-			close: () => {
-				// The kit is opened directly.
-				if ( elementor.config.initial_document.id === parseInt( elementor.config.kit_id ) ) {
-					return $e.run( 'panel/global/exit' );
-				}
-
-				$e.internal( 'panel/state-loading' );
-
-				return $e.run( 'editor/documents/switch', {
-					id: elementor.config.initial_document.id,
-					onClose: () => {
-						$e.components.get( 'panel/global' ).close();
-						$e.routes.clearHistory( this.getRootContainer() );
-					},
-				} ).then( () => {
-					$e.internal( 'panel/state-ready' );
-				} );
-			},
-			exit: () => {
-				return $e.run( 'editor/documents/close', {
-					id: elementor.config.kit_id,
-					onClose: ( document ) => {
-						location = document.config.urls.exit_to_dashboard;
-					},
-				} );
-			},
-			back: ( event ) => {
-				const panelHistory = $e.routes.getHistory( 'panel' );
-
-				// Don't go back if no where.
-				if ( 1 === panelHistory.length ) {
-					this.getCloseConfirmDialog( event ).show();
-					return;
-				}
-
-				return $e.routes.back( 'panel' );
-			},
-		};
+		return this.importCommands( commands );
 	}
 
 	defaultShortcuts() {
@@ -91,35 +34,6 @@ export default class extends elementorModules.common.Component {
 				},
 			},
 		};
-	}
-
-	getCloseConfirmDialog( event ) {
-		if ( ! this.confirmDialog ) {
-			const modalOptions = {
-				id: 'elementor-kit-warn-on-close',
-				headerMessage: elementor.translate( 'Exit' ),
-				message: elementor.translate( 'Would you like to exit?' ),
-				position: {
-					my: 'center center',
-					at: 'center center',
-				},
-				strings: {
-					confirm: elementor.translate( 'Exit' ),
-					cancel: elementor.translate( 'Cancel' ),
-				},
-				onConfirm: () => {
-					$e.run( 'panel/global/close' );
-				},
-			};
-
-			this.confirmDialog = elementorCommon.dialogsManager.createWidget( 'confirm', modalOptions );
-		}
-
-		this.confirmDialog.setSettings( 'hide', {
-			onEscKeyPress: ! event,
-		} );
-
-		return this.confirmDialog;
 	}
 
 	renderContent( tab ) {
