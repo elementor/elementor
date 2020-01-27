ab81936a37e438dcbcdc06fb88497e9d51945f45
Leonid Vinikov <leonid@elementor.com>
2020-01-27 12:39:35 +0200

    JS API/Document/History: Remove - 'start-transaction'. useless.

--- assets/dev/js/editor/document/history/commands/index.js
+++ assets/dev/js/editor/document/history/commands/index.js
@@ -7,4 +7,3 @@ export { EndLog } from './end-log';
 export { EndTransaction } from './end-transaction';
 export { LogSubItem } from './log-sub-item';
 export { StartLog } from './start-log';
-export { StartTransaction } from './start-transaction';
