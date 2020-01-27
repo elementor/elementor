38a0c881c1972a009cf2c6b49923f6a813227789
Leonid Vinikov <leonid@elementor.com>
2020-01-27 12:41:49 +0200

    JS API/Document/History: New - 'delete-transaction' add warning.

--- assets/dev/js/editor/document/history/commands/delete-transaction.js
+++ assets/dev/js/editor/document/history/commands/delete-transaction.js
@@ -1,6 +1,12 @@
 import Base from './base/base';
 
 export class DeleteTransaction extends Base {
+	initialize() {
+		if ( $e.devTools && ! this.component.isTransactionStarted() ) {
+			$e.devTools.log.warn( 'Transaction is already started' );
+		}
+	}
+
 	apply( args ) {
 		this.component.transactions = [];
 	}
