ee8812ed1283fba6a64d9fd2a3db5b58ddfd71c9
Leonid Vinikov <leonid@elementor.com>
2020-02-03 16:36:44 +0200

    JS API/Document/History: New - Comment for 'add-transaction'.

--- assets/dev/js/editor/document/history/commands/internal/add-transaction.js
+++ assets/dev/js/editor/document/history/commands/internal/add-transaction.js
@@ -4,6 +4,10 @@ export class AddTransaction extends Base {
 	initialize( args ) {
 		super.initialize( args );
 
+		/**
+		 * Debounce always send 'add-transaction' with title & subTitle, when the transaction
+		 * already started, there is no need to save those args they are useless.
+		 */
 		if ( this.component.isTransactionStarted() ) {
 			delete args.title;
 			delete args.subTitle;
