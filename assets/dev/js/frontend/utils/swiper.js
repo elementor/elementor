b8e4e3cac6d673675ac80e41dfc898fd86f5a35d
Udi Dollberg <udi@elementor.com>
2020-02-05 17:45:30 +0200

    small refactors following code review

--- assets/dev/js/frontend/utils/swiper.js
+++ assets/dev/js/frontend/utils/swiper.js
@@ -14,22 +14,27 @@ export default class Swiper {
 	// Backwards compatibility for Elementor Pro <2.9.0 (old Swiper version - <5.0.0)
 	// In Swiper 5.0.0 and up, breakpoints changed from acting as max-width to acting as min-width
 	adjustConfig() {
-		const elementorBPs = elementorFrontend.config.breakpoints,
-			elementorBPValues = Object.values( elementorBPs );
+		const elementorBreakpoints = elementorFrontend.config.breakpoints,
+			elementorBreakpointValues = Object.values( elementorBreakpoints );
 
 		Object.keys( this.config.breakpoints ).forEach( ( configBPKey ) => {
-			if ( parseInt( configBPKey ) === elementorBPs.md ) {
+			const configBPKeyInt = parseInt( configBPKey );
+			let breakpointToUpdate;
+
+			if ( configBPKeyInt === elementorBreakpoints.md ) {
 				// This handles the mobile breakpoint. Elementor's default sm breakpoint is never actually used,
 				// so the mobile breakpoint (md) needs to be handled separately and set to the 0 breakpoint (xs)
-				this.config.breakpoints[ elementorBPs.xs ] = this.config.breakpoints[ configBPKey ];
+				breakpointToUpdate = elementorBreakpoints.xs;
 			} else {
 				// Find the index of the current config breakpoint in the Elementor Breakpoints array
-				const currentBPIndexInElementorBPs = elementorBPValues.findIndex( ( elementorBP ) => parseInt( configBPKey ) === elementorBP );
+				const currentBPIndexInElementorBPs = elementorBreakpointValues.findIndex( ( elementorBP ) => configBPKeyInt === elementorBP );
 
 				// For all other Swiper config breakpoints, move them one breakpoint down on the breakpoint list,
-				// according to the array of Elementor-defined breakpoints
-				this.config.breakpoints[ elementorBPValues[ currentBPIndexInElementorBPs - 1 ] ] = this.config.breakpoints[ configBPKey ];
+				// according to the array of Elementor's global breakpoints
+				breakpointToUpdate = elementorBreakpointValues[ currentBPIndexInElementorBPs - 1 ];
 			}
+
+			this.config.breakpoints[ breakpointToUpdate ] = this.config.breakpoints[ configBPKey ];
 
 			// Then reset the settings in the original breakpoint key to the default values
 			this.config.breakpoints[ configBPKey ] = {
