f869475f550b671195f43f73fbe542e5d8cdef03
Leonid Vinikov <leonid@elementor.com>
2020-02-06 10:58:16 +0200

    Editor: Fix - Set default route, for all elements & add global widget bc.

--- assets/dev/js/editor/regions/panel/pages/editor/component.js
+++ assets/dev/js/editor/regions/panel/pages/editor/component.js
@@ -24,11 +24,14 @@ export default class Component extends BaseComponent {
 	defaultCommands() {
 		return {
 			open: ( args ) => {
-				this.openEditor( args.model, args.view );
+				if ( ! this.setDefaultTab( args ) ) {
+					// TODO: Remove - Backwards compatibility.
+					args.model.trigger( 'request:edit' );
+				} else {
+					this.openEditor( args.model, args.view );
 
-				this.setDefaultTab( args );
-
-				$e.route( this.getDefaultRoute(), args );
+					$e.route( this.getDefaultRoute(), args );
+				}
 
 				// BC: Run hooks after the route render's the view.
 				const action = 'panel/open_editor/' + args.model.get( 'elType' );
@@ -58,18 +61,22 @@ export default class Component extends BaseComponent {
 
 	setDefaultTab( args ) {
 		let defaultTab;
+
+		const editSettings = args.model.get( 'editSettings' );
+
 		if ( this.activeTabs[ args.model.id ] ) {
 			defaultTab = this.activeTabs[ args.model.id ];
-		} else {
-			defaultTab = jQuery( this.getTabsWrapperSelector() ).find( '.elementor-component-tab' ).eq( 0 ).data( 'tab' );
+		} else if ( editSettings && editSettings.get( 'defaultEditRoute' ) ) {
+			defaultTab = editSettings.get( 'defaultEditRoute' );
 		}
 
-		// For unit test.
-		if ( ! defaultTab ) {
-			defaultTab = 'content';
+		if ( defaultTab ) {
+			this.setDefaultRoute( defaultTab );
+
+			return true;
 		}
 
-		this.setDefaultRoute( defaultTab );
+		return false;
 	}
 
 	openEditor( model, view ) {
