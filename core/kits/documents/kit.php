323af49f8635af01584b5744c8bf3660d2df6c59
Yakir Sitbon <kingyes1@gmail.com>
2020-02-09 19:43:35 +0200

    Fixes translate string

--- core/kits/documents/kit.php
+++ core/kits/documents/kit.php
@@ -138,7 +138,7 @@ class Kit extends PageBase {
 				[
 					'name' => $current_section['section'] . '_schemes_notice',
 					'type' => Controls_Manager::RAW_HTML,
-					'raw' => __( 'In order for Theme Style to affect Elementor elements, please disable Elementor\'s Default Colors and Fonts from <a href="' . Settings::get_url() . '" target="_blank">Elementor Settings.</a>', 'elementor' ),
+					'raw' => sprintf( __( 'In order for Theme Style to affect Elementor elements, please disable Elementor\'s Default Colors and Fonts from <a href="%s" target="_blank">Elementor Settings.</a>', 'elementor' ), Settings::get_url() ),
 					'content_classes' => 'elementor-panel-alert elementor-panel-alert-warning',
 					'render_type' => 'ui',
 				]
