3a474d4fb871730cefa4bd1f686dffc2cd92df8b
Udi Dollberg <udi@elementor.com>
2020-02-09 13:03:14 +0200

    Fixed check for pause on hover and paused on interaction. When the toggles are saved as off, it saves a "no" value in the DB

--- assets/dev/js/frontend/handlers/image-carousel.js
+++ assets/dev/js/frontend/handlers/image-carousel.js
@@ -52,7 +52,7 @@ class ImageCarouselHandler extends elementorModules.frontend.handlers.Base {
 		if ( ! this.isEdit && 'yes' === elementSettings.autoplay ) {
 			swiperOptions.autoplay = {
 				delay: elementSettings.autoplay_speed,
-				disableOnInteraction: !! elementSettings.pause_on_interaction,
+				disableOnInteraction: 'yes' === elementSettings.pause_on_interaction,
 			};
 		}
 
@@ -115,7 +115,7 @@ class ImageCarouselHandler extends elementorModules.frontend.handlers.Base {
 		// Expose the swiper instance in the frontend
 		this.elements.$carousel.data( 'swiper', this.swiper );
 
-		if ( elementSettings.pause_on_hover ) {
+		if ( 'yes' === elementSettings.pause_on_hover ) {
 			this.elements.$carousel.on( {
 				mouseenter: () => {
 					this.swiper.autoplay.stop();
