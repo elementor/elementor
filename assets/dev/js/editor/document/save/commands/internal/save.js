67df6c21aedf65fc0fb481e5ebf9e9b345355649
Leonid Vinikov <leonid@elementor.com>
2020-02-10 11:02:44 +0200

    JS API/Document/Save: Fix - Internal save command.

--- assets/dev/js/editor/document/save/commands/internal/save.js
+++ assets/dev/js/editor/document/save/commands/internal/save.js
@@ -46,15 +46,25 @@ export class Save extends CommandInternalBase {
 	onSaveSuccess( data, status, oldStatus, elements, document, callback = null ) {
 		this.onAfterAjax( document );
 
+		// Remove document cache.
 		elementor.documents.invalidateCache( document.id );
+
+		const statusChanged = status !== oldStatus,
+			result = {
+				data,
+				statusChanged,
+			};
 
 		// Document is switched during the save, do nothing.
 		if ( document !== elementor.documents.getCurrent() ) {
-			return;
+			return result;
 		}
 
-		const statusChanged = status !== oldStatus;
+		if ( ! document.editor.isChangedDuringSave ) {
+			document.editor.isSaved = true;
+		}
 
+		// TODO: Move to hook.
 		if ( 'autosave' !== status ) {
 			if ( statusChanged ) {
 				$e.run( 'document/elements/settings', {
@@ -72,10 +82,6 @@ export class Save extends CommandInternalBase {
 			if ( ! document.editor.isChangedDuringSave ) {
 				$e.internal( 'document/save/set-is-modified', { status: false } );
 			}
-		}
-
-		if ( ! document.editor.isChangedDuringSave ) {
-			document.editor.isSaved = true;
 		}
 
 		if ( data.config ) {
@@ -98,11 +104,6 @@ export class Save extends CommandInternalBase {
 		if ( statusChanged ) {
 			elementor.saver.trigger( 'page:status:change', status, oldStatus );
 		}
-
-		const result = {
-			data,
-			statusChanged,
-		};
 
 		if ( _.isFunction( callback ) ) {
 			callback.call( this, result );
