f2bb5d4f9454aff3333a6f590164a7b538ced6b5
Udi Dollberg <udi@elementor.com>
2020-02-12 10:06:07 +0200

    Offset the tooltip on the panel footer "Save Options" button to make it the same height as the rest of the footer buttons

--- assets/dev/js/editor/document/save/behaviors/footer-saver.js
+++ assets/dev/js/editor/document/save/behaviors/footer-saver.js
@@ -111,12 +111,23 @@ module.exports = class FooterSaver extends Marionette.Behavior {
 
 	addTooltip() {
 		// Create tooltip on controls
-		this.$el.find( '.tooltip-target' ).tipsy( {
-			// `n` for down, `s` for up
-			gravity: 's',
-			title() {
-				return this.getAttribute( 'data-tooltip' );
-			},
+		this.$el.find( '.tooltip-target' ).each( ( index, button ) => {
+			const $button = jQuery( button );
+			let offset = 0;
+
+			if ( $button.attr( 'id' ) === this.ui.buttonSaveOptions.attr( 'id' ) ) {
+				// The "Save Options" button's tooltip needs to be raised to the same height as other buttons
+				offset = 7;
+			}
+
+			$button.tipsy( {
+				// `n` for down, `s` for up
+				gravity: 's',
+				offset: offset,
+				title() {
+					return this.getAttribute( 'data-tooltip' );
+				},
+			} );
 		} );
 	}
 
