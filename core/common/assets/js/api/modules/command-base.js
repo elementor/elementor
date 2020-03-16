bf3adb5e6a7a9e434cae176c2aac2e8b44e102cf
Leonid Vinikov <leonid@elementor.com>
2020-03-16 15:43:51 +0200

    JS API/Core/Modules: New - CommandBase.

---
+++ core/common/assets/js/api/modules/command-base.js
@@ -0,0 +1,161 @@
+import ArgsObject from 'elementor-assets-js/modules/imports/args-object';
+
+export default class CommandBase extends ArgsObject {
+	/**
+	 * Current component.
+	 *
+	 * @type {Component}
+	 */
+	component;
+
+	/**
+	 * Function constructor().
+	 *
+	 * Create Commands Base.
+	 *
+	 * @param [args={}]
+	 * @param [commandsAPI={}]
+	 */
+	constructor( args, commandsAPI = $e.commands ) {
+		super( args );
+
+		// Acknowledge self about which command it run.
+		this.currentCommand = commandsAPI.getCurrentLast();
+
+		// Assign instance of current component.
+		this.component = commandsAPI.getComponent( this.currentCommand );
+
+		// Who ever need do something before without `super` the constructor can use `initialize` method.
+		this.initialize( args );
+
+		// Refresh args, maybe the changed via `initialize`.
+		args = this.args;
+
+		// Validate args before run.
+		this.validateArgs( args );
+	}
+
+	/**
+	 * Function requireContainer().
+	 *
+	 * Validate `arg.container` & `arg.containers`.
+	 *
+	 * @param {{}} args
+	 *
+	 * @throws {Error}
+	 */
+	requireContainer( args = this.args ) {
+		if ( ! args.container && ! args.containers ) {
+			throw Error( 'container or containers are required.' );
+		}
+
+		if ( args.container && args.containers ) {
+			throw Error( 'container and containers cannot go together please select one of them.' );
+		}
+
+		const containers = args.containers || [ args.container ];
+
+		containers.forEach( ( container ) => {
+			this.requireArgumentInstance( 'container', elementorModules.editor.Container, { container } );
+		} );
+	}
+
+	/**
+	 * Function initialize().
+	 *
+	 * Initialize command, called after construction.
+	 *
+	 * @param [args={}]
+	 */
+	initialize( args = {} ) {} // eslint-disable-line no-unused-vars
+
+	/**
+	 * Function validateArgs().
+	 *
+	 * Validate command arguments.
+	 *
+	 * @param [args={}]
+	 */
+	validateArgs( args = {} ) {} // eslint-disable-line no-unused-vars
+
+	/**
+	 * Function apply().
+	 *
+	 * Do the actual command.
+	 *
+	 * @param [args={}]
+	 *
+	 * @returns {*}
+	 */
+	apply( args = {} ) { // eslint-disable-line no-unused-vars
+		elementorModules.ForceMethodImplementation();
+	}
+
+	/**
+	 * Function run().
+	 *
+	 * Run command.
+	 *
+	 * @returns {*}
+	 */
+	run() {
+		elementorModules.ForceMethodImplementation();
+	}
+
+	/**
+	 * Function onBeforeRun.
+	 *
+	 * Called before run().
+	 *
+	 * @param [args={}]
+	 */
+	onBeforeRun( args = {} ) { // eslint-disable-line no-unused-vars
+		elementorModules.ForceMethodImplementation();
+	}
+
+	/**
+	 * Function onAfterRun.
+	 *
+	 * Called after run().
+	 *
+	 * @param [args={}]
+	 * @param [result={*}]
+	 */
+	onAfterRun( args = {}, result ) { // eslint-disable-line no-unused-vars
+		elementorModules.ForceMethodImplementation();
+	}
+
+	/**
+	 * Function onBeforeApply.
+	 *
+	 * Called before apply().
+	 *
+	 * @param [args={}]
+	 */
+	onBeforeApply( args = {} ) { // eslint-disable-line no-unused-vars
+		elementorModules.ForceMethodImplementation();
+	}
+
+	/**
+	 * Function onAfterApply.
+	 *
+	 * Called after apply().
+	 *
+	 * @param [args={}]
+	 * @param [result={*}]
+	 */
+	onAfterApply( args = {}, result ) { // eslint-disable-line no-unused-vars
+		elementorModules.ForceMethodImplementation();
+	}
+
+	/**
+	 * Function onCatchApply.
+	 *
+	 * Called after apply() failed.
+	 *
+	 * @param {Error} e
+	 */
+	onCatchApply( e ) { // eslint-disable-line no-unused-vars
+		elementorModules.ForceMethodImplementation();
+	}
+}
