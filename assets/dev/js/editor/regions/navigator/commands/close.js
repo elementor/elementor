c11062bfab480b6e3478d6f6427e7d120fbe9afa
Leonid Vinikov <leonid@elementor.com>
2020-03-17 14:46:48 +0200

    Navigator/Commands/Close: Fix - Command return.

--- assets/dev/js/editor/regions/navigator/commands/close.js
+++ assets/dev/js/editor/regions/navigator/commands/close.js
@@ -2,7 +2,7 @@ import CommandBase from 'elementor-api/modules/command-base';
 
 export class Close extends CommandBase {
 	apply() {
-		this.component.close();
+		return this.component.close();
 	}
 }
 
