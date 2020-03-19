03d863c784abe8790eafebaace03f7e69a9fce96
Leonid Vinikov <leonid@elementor.com>
2020-03-19 14:55:02 +0200

    JS API/Core/Routes: Fix - Remove unsed validateInstance.

--- core/common/assets/js/api/core/routes.js
+++ core/common/assets/js/api/core/routes.js
@@ -175,8 +175,6 @@ export default class Routes extends Commands {
 		return match;
 	}
 
-	validateInstance( instance, component, command ) {} // eslint-disable-line no-unused-vars
-
 	error( message ) {
 		throw Error( 'Routes: ' + message );
 	}
