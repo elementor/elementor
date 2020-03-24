e5bdb1582b67f879e57db8468a47111e53b05c1f
Leonid Vinikov <leonid@elementor.com>
2020-03-24 11:27:38 +0200

    Data/Base: Fix - Endpoint add get_item.

--- data/base/endpoint.php
+++ data/base/endpoint.php
@@ -5,6 +5,10 @@ namespace Elementor\Data\Base;
 use WP_REST_Server;
 
 abstract class Endpoint {
+
+	/**
+	 * @var \Elementor\Data\Base\Controller
+	 */
 	private $controller;
 
 	/**
@@ -21,13 +25,36 @@ abstract class Endpoint {
 	abstract protected function get_name();
 
 	protected function register() {
-		register_rest_route( $this->controller->get_namespace(), '/' . $this->controller->get_reset_base() . '/' . $this->get_name(), [
+		$this->register_get_items_route();
+	}
+
+	public function register_get_item_route() {
+		$args = [
+			'id' => [
+				'description' => __( 'Unique identifier for the object.' ),
+				'type' => 'integer',
+			],
+		];
+
+		$this->register_route( '/(?P<id>[\d]+)', WP_REST_Server::READABLE, function ( $request ) {
+			return rest_ensure_response( $this->get_item( $request->get_param( 'id' ), $request ) );
+		}, $args );
+	}
+
+	public function register_get_items_route() {
+		$this->register_route( '', WP_REST_Server::READABLE, function ( $request ) {
+			return rest_ensure_response( $this->get_items( $request ) );
+		} );
+	}
+
+	public function register_route( $route = '', $methods = WP_REST_Server::READABLE, $callback = null, $args = [] ) {
+		$route = '/' . $this->controller->get_reset_base() . '/' . $this->get_name() . $route;
+
+		register_rest_route( $this->controller->get_namespace(), $route, [
 			[
-				'methods' => WP_REST_Server::READABLE,
-				'callback' => function ( $request ) {
-					return rest_ensure_response( $this->get_items( $request ) );
-				},
-				'args' => [],
+				'args' => $args,
+				'methods' => $methods,
+				'callback' => $callback,
 			],
 		] );
 	}
@@ -35,8 +62,23 @@ abstract class Endpoint {
 	/**
 	 * Retrieves a collection of items.
 	 *
-	 * @param WP_REST_Request $request Full data about the request.
-	 * @return WP_Error|WP_REST_Response Response object on success, or WP_Error object on failure.
+	 * @param \WP_REST_Request $request Full data about the request.
+	 *
+	 * @return \WP_Error|\WP_REST_Response Response object on success, or WP_Error object on failure.
 	 */
-	abstract protected function get_items( $request );
+	protected function get_items( $request ) {
+		return $this->controller->get_items( $request );
+	}
+
+	/**
+	 * Retrieves one item from the collection.
+	 *
+	 * @param string           $id
+	 * @param \WP_REST_Request $request Full data about the request.
+	 *
+	 * @return \WP_Error|\WP_REST_Response Response object on success, or WP_Error object on failure.
+	 */
+	protected function get_item( $id, $request ) {
+		return $this->controller->get_item( $request );
+	}
 }
