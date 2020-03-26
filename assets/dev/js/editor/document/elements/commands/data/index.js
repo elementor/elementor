58394401b56a5a3848af469a1359a89d670d0e2d
Leonid Vinikov <leonid@elementor.com>
2020-03-26 17:44:32 +0200

    Data/Document/Elements: Fix - Validate args: document_id arg => string.

--- assets/dev/js/editor/document/elements/commands/data/index.js
+++ assets/dev/js/editor/document/elements/commands/data/index.js
@@ -2,7 +2,7 @@ import CommandData from 'elementor-api/modules/command-data';
 
 class Index extends CommandData {
 	validateArgs( args ) {
-		this.requireArgumentType( 'document_id', 'string', args );
+		this.requireArgumentType( 'document_id', 'number', args );
 	}
 }
 
