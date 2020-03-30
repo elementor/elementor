d75b72e08d5bdc472092429583f7ec32544ce6a2
Leonid Vinikov <leonid@elementor.com>
2020-03-30 21:41:05 +0300

    JS API/Data/Globals: Fix - Command colors.

--- assets/dev/js/editor/data/globals/commands/colors.js
+++ assets/dev/js/editor/data/globals/commands/colors.js
@@ -1,13 +1,7 @@
 import CommandData from 'elementor-api/modules/command-data';
 
 export class Colors extends CommandData {
-	applyAfterGet( data, args = {} ) {
-		if ( args.test ) {
-			return { myFakeDataKey: 'myFakeDataValue' };
-		}
 
-		return data;
-	}
 }
 
 export default Colors;
