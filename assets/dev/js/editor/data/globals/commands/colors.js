8c8250dad12ed648986a608c8b6e8a154d87be6d
Leonid Vinikov <leonid@elementor.com>
2020-03-31 13:17:53 +0300

    JS API/Data/Globals: Fix - Commands, add comments.

--- assets/dev/js/editor/data/globals/commands/colors.js
+++ assets/dev/js/editor/data/globals/commands/colors.js
@@ -1,7 +1,7 @@
 import CommandData from 'elementor-api/modules/command-data';
 
 export class Colors extends CommandData {
-
+	// Nothing to do.
 }
 
 export default Colors;
--- assets/dev/js/editor/data/globals/commands/typography.js
+++ assets/dev/js/editor/data/globals/commands/typography.js
@@ -1,6 +1,7 @@
 import CommandData from 'elementor-api/modules/command-data';
 
 export class Typography extends CommandData {
+	// Nothing to do.
 }
 
 export default Typography;
