f458ec6aab6c46a9cdd1c034bcb50a55abeefc7d
Leonid Vinikov <leonid@elementor.com>
2020-04-23 14:41:23 +0300

    Document/Hooks: New - UI Hook on 'document/globals/enable' - enqueue-font.

---
+++ assets/dev/js/editor/document/hooks/ui/globals/enable/enqueue-fonts.js
@@ -0,0 +1,33 @@
+import After from 'elementor-api/modules/hooks/ui/after';
+
+export class EnqueueFonts extends After {
+	getCommand() {
+		return 'document/globals/enable';
+	}
+
+	getId() {
+		return 'enqueue-fonts';
+	}
+
+	getConditions( args ) {
+		return Object.values( args.settings ).some( ( setting ) => setting.includes( 'typography' ) );
+	}
+
+	apply( args ) {
+		const { settings } = args;
+
+		Object.values( settings ).forEach( async ( setting ) => {
+			if ( setting.includes( 'typography' ) ) {
+				const result = await $e.data.get( setting );
+
+				if ( result.data?.font_family ) {
+					elementor.helpers.enqueueFont( result.data.font_family );
+				}
+			}
+		} );
+
+		return true;
+	}
+}
+
+export default EnqueueFonts;
