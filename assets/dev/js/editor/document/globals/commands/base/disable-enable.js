5c6d8592509782de38bece47bf8fc55bb9c2c0db
Mati Horovitz <mati@elementor.com>
2020-05-04 12:28:33 +0300

    JS API/Globals: Fix - File import names.

--- assets/dev/js/editor/document/globals/commands/base/disable-enable.js
+++ assets/dev/js/editor/document/globals/commands/base/disable-enable.js
@@ -1,4 +1,4 @@
-import CommandHistory from 'elementor-document/commands/base/history';
+import CommandHistory from 'elementor-document/commands/base/command-history';
 import ElementsSettings from 'elementor-document/elements/commands/settings';
 
 export default class DisableEnable extends CommandHistory {
