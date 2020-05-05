b5233d6df9f2f2c6eab6e2eab5ce594b525658a3
udidol <udi.dollberg@gmail.com>
2020-05-05 15:27:58 +0300

    Refactored the global colors preview list to be object-based instead of array-based

--- assets/dev/js/editor/controls/color.js
+++ assets/dev/js/editor/controls/color.js
@@ -51,28 +51,59 @@ export default class extends ControlBaseDataView {
 		jQuery( this.colorPicker.picker.getRoot().root ).addClass( 'elementor-control-unit-1 elementor-control-tag-area' );
 	}
 
+	createGlobalColorPreviewMarkup( color ) {
+		const $color = jQuery( '<div>', { class: 'elementor-global-preview elementor-global-color', 'data-elementor-global-name': color.name } ),
+			$colorPreview = jQuery( '<div>', { class: 'elementor-global-color__preview', style: 'background-color:' + color.code } ),
+			$colorTitle = jQuery( '<span>', { class: 'elementor-global-color__title' } )
+				.html( color.name ),
+			$colorHex = jQuery( '<span>', { class: 'elementor-global-color__hex' } )
+				.html( color.code );
+
+		$color.append( $colorPreview, $colorTitle, $colorHex );
+
+		return $color;
+	}
+
 	// TODO: Replace placeholders with real global colors
+	getPlaceholderColorsList() {
+		return {
+			Primary: {
+				name: 'Primary',
+				code: '#4631DA',
+			},
+			Secondary: {
+				name: 'Secondary',
+				code: '#71D7F7',
+			},
+			Text: {
+				name: 'Text',
+				code: '#495157',
+			},
+			Accent: {
+				name: 'Accent',
+				code: '#A4AFB7',
+			},
+			OrangeRed: {
+				name: 'Orange Red',
+				code: '#FF650E',
+			},
+			Crimson: {
+				name: 'Crimson',
+				code: '#F3113A',
+			},
+			GrassGreen: {
+				name: 'Grass Green',
+				code: '#048647',
+			},
+		};
+	}
+
 	getGlobalColors() {
 		const $globalColorsRepeaterContainer = jQuery( '<div>', { class: 'elementor-global-colors-repeater-container' } ),
-			globalColors = [
-			[ 'Primary', '#4631DA' ],
-			[ 'Secondary', '#71D7F7' ],
-			[ 'Text', '#495157' ],
-			[ 'Accent', '#A4AFB7' ],
-			[ 'Orange Red', '#FF650E' ],
-			[ 'Crimson', '#F3113A' ],
-			[ 'Grass Green', '#048647' ],
-		];
+			globalColors = this.getPlaceholderColorsList();
 
-		globalColors.forEach( ( color ) => {
-			const $color = jQuery( '<div>', { class: 'elementor-global-preview elementor-global-color', 'data-elementor-global-name': color[ 0 ] } ),
-				$colorPreview = jQuery( '<div>', { class: 'elementor-global-color__preview', style: 'background-color:' + color[ 1 ] } ),
-				$colorTitle = jQuery( '<span>', { class: 'elementor-global-color__title' } )
-					.html( color[ 0 ] ),
-				$colorHex = jQuery( '<span>', { class: 'elementor-global-color__hex' } )
-					.html( color[ 1 ] );
-
-			$color.append( $colorPreview, $colorTitle, $colorHex );
+		globalColors.values( globalColors ).forEach( ( color ) => {
+			const $color = this.createGlobalColorPreviewMarkup( color );
 
 			$globalColorsRepeaterContainer.append( $color );
 		} );
