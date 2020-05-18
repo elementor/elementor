d8273f733909a16d99d609c87f91b402a30b4a7f
Mati Horovitz <mati@elementor.com>
2020-05-18 12:04:05 +0300

    Elementor App: Progress - Menu - Fix "All Parts" URL.

--- core/app/assets/js/components/site-editor/organisms/menu.js
+++ core/app/assets/js/components/site-editor/organisms/menu.js
@@ -7,7 +7,7 @@ export default class Menu extends React.Component {
 	render() {
 		return (
 			<nav className="elementor-app__site-editor__menu">
-				<MenuItem id="all-parts" text={ __( 'All Parts', 'elementor' ) } 	icon="eicon-filter" url="site-editor/templates" />
+				<MenuItem id="all-parts" text={ __( 'All Parts', 'elementor' ) } 	icon="eicon-filter" url="/site-editor/templates" />
 				<div className="elementor-app__site-editor__menu__items-title">
 					{ __( 'Site Parts', 'elementor' ) }
 				</div>
