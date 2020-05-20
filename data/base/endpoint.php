d6d966edcb4de5add2986ccb459931314ecb9233
Leonid Vinikov <czf.leo123@gmail.com>
2020-05-20 10:22:37 +0300

    Data/Base: Fix - JSCS.
    Co-authored-by: Mati Horovitz <21468434+matipojo@users.noreply.github.com>

--- data/base/endpoint.php
+++ data/base/endpoint.php
@@ -220,7 +220,7 @@ abstract class Endpoint {
 	/**
 	 * Retrieves one item from the collection.
 	 *
-	 * @param string           $id
+	 * @param string $id
 	 * @param \WP_REST_Request $request Full data about the request.
 	 *
 	 * @return \WP_Error|\WP_REST_Response Response object on success, or WP_Error object on failure.
