6434da6c9b6bf09e1f29de39a590d9b35b8d9a12
Leonid Vinikov <leonid@elementor.com>
2020-05-25 12:58:19 +0300

    Data: Change - Manager, register_controller 'private' => 'public'.

--- data/manager.php
+++ data/manager.php
@@ -40,7 +40,7 @@ class Manager extends BaseModule {
 	 *
 	 * @param string $controller_class_name
 	 */
-	private function register_controller( $controller_class_name ) {
+	public function register_controller( $controller_class_name ) {
 		$controller_instance = new $controller_class_name();
 
 		$this->register_controller_instance( $controller_instance );
