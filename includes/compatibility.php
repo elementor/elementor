6e0558d6d53e24313eb40073acac6901db262617
udidol <36724484+udidol@users.noreply.github.com>
2020-05-31 13:01:43 +0300

    Update includes/compatibility.php
    Co-authored-by: Mati Horovitz <21468434+matipojo@users.noreply.github.com>

--- includes/compatibility.php
+++ includes/compatibility.php
@@ -338,7 +338,7 @@ class Compatibility {
 		if ( ! empty( $wp_importer ) ) {
 			$wp_importer_version = $wp_importer['wordpress-importer.php']['Version'];
 
-			if ( isset( $wp_importer_version ) && ! empty( $wp_importer_version ) ) {
+			if ( ! empty( $wp_importer_version ) ) {
 				if ( version_compare( $wp_importer_version, '0.7', '>=' ) ) {
 					return $post_meta;
 				}
