d92a69a7c9c0c901c8d954e80e1df54c1ffcb74d
Mati Horovitz <mati@elementor.com>
2020-06-03 00:59:24 +0300

    Elementor App: Progress - Load style via scss import, support RTL & global variables, mixins, etc.

---
+++ .grunt-config/loaders/app-imports.js
@@ -0,0 +1,21 @@
+const path = require( 'path' );
+const dir = path.resolve( __dirname, '../../' );
+const fs = require('fs');
+
+module.exports = function() {
+	const importsFile = dir + '/core/app/assets/scss/app-imports.scss',
+		relativePath = path.relative( path.dirname(importsFile), this.resourcePath ).replace( /\\/g, '/' ),
+		importStatement = `@import "${ relativePath }";`,
+		importContent = fs.readFileSync(importsFile).toString().split("\n"),
+		importExists = -1 !== importContent.indexOf( importStatement );
+
+	if ( ! importExists ) {
+		fs.open(importsFile, 'a', 666, function( e, id ) {
+			fs.write( id, importStatement + "\n", null, 'utf8', function(){
+				fs.close(id);
+			});
+		});
+	}
+	return ``;
+}
+
