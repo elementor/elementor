bed32577a50f241a55639867e3eb60b9c08d2c2d
Rotem Ben-Itzhak <rotem@elementor.com>
2020-06-03 11:30:48 +0300

    Changing app-assets-js-layout page.js path from the package files.

--- core/app/assets/js/package.js
+++ core/app/assets/js/package.js
@@ -1,7 +1,7 @@
 // Alphabetical order.
 import Button from './molecules/button';
 import NotFound from './pages/not-found';
-import Page from './layout/page';
+import Page from './layout/page/page';
 
 export default {
 		Button,
