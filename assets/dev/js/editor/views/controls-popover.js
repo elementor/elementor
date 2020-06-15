24ebbfa53fb657107c214298079b66c0ecaf6d7b
udidol <udi.dollberg@gmail.com>
2020-06-15 19:40:49 +0300

    Globals: Popover Toggle - Removed redundant comments, fixed wrong translation string

--- assets/dev/js/editor/views/controls-popover.js
+++ assets/dev/js/editor/views/controls-popover.js
@@ -25,16 +25,12 @@ export default class ControlsPopover {
 	createPopoverHeader() {
 		const $popoverToggleControl = this.$popover.prev(),
 			// Get the existing reset button
-			//$resetInput = $popoverToggleControl.find( '.elementor-control-popover-toggle-reset' ),
 			$resetLabel = $popoverToggleControl.find( '.elementor-control-popover-toggle-reset-label' );
 
 		this.$popoverHeader = jQuery( '<div>', { class: 'e-group-control-header' } )
 			.html( '<span>' + elementor.translate( 'typography' ) + '</span>' );
 
 		this.$headerControlsWrapper = jQuery( '<div>', { class: 'e-control-tools' } );
-
-		// Hide the reset "radio button"
-		//$resetInput.hide();
 
 		// Give the reset button the control tool styling,
 		// and add a click event so clicking on it closes the popover
@@ -43,7 +39,6 @@ export default class ControlsPopover {
 			.on( 'click', () => this.onResetButtonClick() );
 
 		// Move the popover toggle reset button into the popover header
-		//this.$headerControlsWrapper.append( $resetInput, $resetLabel );
 		this.$headerControlsWrapper.append( $resetLabel );
 		this.$popoverHeader.append( this.$headerControlsWrapper );
 
@@ -85,10 +80,8 @@ export default class ControlsPopover {
 
 		this.$addButton.on( 'click', () => this.onAddButtonClick() );
 
-		//this.toggleButtonListener( '$addButton', true );
-
 		this.$addButton.tipsy( {
-			title: () => elementor.translate( 'create_global_color' ),
+			title: () => elementor.translate( 'create_global_style' ),
 			gravity: () => 's',
 		} );
 	}
