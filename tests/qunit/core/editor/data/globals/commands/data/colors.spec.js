2076d19e5784eb8bda5bd56faae962c5b60bcfe9
Leonid Vinikov <leonid@elementor.com>
2020-06-15 19:12:58 +0300

    Tests/Data/Globals: New - Test 'global/colors'.

---
+++ tests/qunit/core/editor/data/globals/commands/data/colors.spec.js
@@ -0,0 +1,44 @@
+import * as eData from 'elementor-tests-qunit/mock/e-data';
+
+export const Colors = () => {
+	QUnit.module( 'Colors', ( hooks ) => {
+		hooks.before( () => {
+			eData.mockAdd( 'create', 'globals/colors', ( result, requestData ) => {
+				const { query } = requestData.args,
+					{ data } = requestData.args;
+
+				return { ... query, ... data };
+			} );
+			eData.mock();
+		} );
+
+		hooks.after( () => {
+			eData.mockClear();
+			eData.free();
+		} );
+
+		QUnit.test( 'get', async ( assert ) => {
+			const data = { test: true };
+
+			$e.data.setCache( $e.components.get( 'globals' ), 'globals/colors', {}, data );
+
+			eData.cache();
+
+			const resultId = await $e.data.get( 'globals/colors?id=test' ),
+				resultAll = await $e.data.get( 'globals/colors' );
+
+			eData.mock(); // Back to default.
+
+			assert.equal( resultId.data, true );
+			assert.deepEqual( resultAll.data, data );
+		} );
+
+		QUnit.test( 'create', async ( assert ) => {
+			const result = await $e.data.create( 'globals/colors', { test: true } );
+
+			assert.equal( result.data.test, true );
+		} );
+	} );
+};
+
+export default Colors;
--- tests/qunit/core/editor/data/globals/component.spec.js
+++ tests/qunit/core/editor/data/globals/component.spec.js
@@ -1,5 +1,10 @@
+import DocumentHelper from 'elementor-tests-qunit/core/editor/document/helper';
+import * as dataCommands from './commands/data/index.spec';
+
 jQuery( () => {
 	QUnit.module( 'Component: globals', ( hooks ) => {
+		DocumentHelper.testCommands( dataCommands );
+
 		require( './colors/component.spec' );
 		require( './typography/component.spec' );
 	} );
