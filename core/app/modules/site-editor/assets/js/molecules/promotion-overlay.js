8417f3a2d5b26b9b761ef5d616066c3b80eb7243
Josh <Josh@elementor.com>
2020-06-15 14:57:47 +0300

    Card/SitePart progress

--- core/app/modules/site-editor/assets/js/molecules/promotion-overlay.js
+++ core/app/modules/site-editor/assets/js/molecules/promotion-overlay.js
@@ -1,15 +1,15 @@
 import CardOverlay from 'elementor-app/ui/card/card-overlay';
 
+import './promotion-overlay.scss';
+
 export default function PromotionOverlay( props ) {
 	const promotionUrlWithType = `${ props.promotionUrl }?type=${ props.type }`;
 
 	return (
 		<CardOverlay className="promotion-overlay">
-			<a target="_blank" rel="noopener noreferrer" href={promotionUrlWithType}>
-				<i className="eicon-lock" />
-				<span>
-				{__( 'Get Pro', 'elementor' )}
-			</span>
+			<a className="promotion-overlay__link" target="_blank" rel="noopener noreferrer" href={promotionUrlWithType}>
+				<i className="promotion-overlay__icon eicon-lock" />
+				<span className="promotion-overlay__button eps-button">{__( 'Get Pro', 'elementor' )}</span>
 			</a>
 		</CardOverlay>
 	);
