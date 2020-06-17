02b8edbfc006b4f4c7149b3b9fa84372913ab391
Josh <Josh@elementor.com>
2020-06-18 02:16:25 +0300

    Styling the menu

--- core/app/assets/js/ui/menu/menu.js
+++ core/app/assets/js/ui/menu/menu.js
@@ -4,8 +4,8 @@ import MenuItem from './menu-item';
 export default function Menu( props ) {
 	return (
 		<nav className="e-app-menu">
-			<MenuItem id="all-parts" text={ __( 'All Parts', 'elementor' ) } icon="eicon-filter" url="/site-editor/templates" />
-			<div className="e-app-menu__title">
+			<MenuItem id="all-parts" text={ __( 'All Parts', 'elementor' ) } className="e-app-menu-item--active" icon="eicon-filter" url="/site-editor/templates" />
+			<div className="e-app-menu__title u-mt-44 u-mb-16">
 				{ __( 'Site Parts', 'elementor' ) }
 			</div>
 			{ (
