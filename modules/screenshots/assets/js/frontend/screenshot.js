e0fb79add69902aba4369025846e9f06fc041a16
Nevo Golan <nevog@elementor.com>
2020-06-25 14:36:08 +0300

    Editor/Screenshot: Change screenshot main js file to modern syntax.

--- modules/screenshots/assets/js/frontend/screenshot.js
+++ modules/screenshots/assets/js/frontend/screenshot.js
@@ -1,104 +1,146 @@
-/* global ElementorScreenshotConfig, elementorCommon */
-var ScreenshotModule = function() {
-	var createScreenshot = function() {
-		var $elementor = jQuery( ElementorScreenshotConfig.selector );
+/* global ElementorScreenshotConfig, elementorFrontendConfig, jQuery */
+class Screenshot {
 
-		if ( ! $elementor.length ) {
-			console.log( 'Screenshots: Elementor content was not found.' );
+	/**
+	 * Holds the screen shot Iframe
+	 */
+	$elementor;
+
+	/**
+	 * The current post id
+	 */
+	postId;
+
+	constructor() {
+		jQuery( () => this.init() )
+	}
+
+	init() {
+		this.$elementor = jQuery( ElementorScreenshotConfig.selector );
+		this.postId = elementorFrontendConfig.post.id;
+
+		if ( ! this.$elementor.length ) {
+			console.warn( 'Screenshots: Elementor content was not found.' );
 			return;
 		}
 
-		html2canvas( document.querySelector( ElementorScreenshotConfig.selector ), {
-			useCORS: true,
-			foreignObjectRendering: true,
-		} ).then( function( canvas ) {
-			var cropCanvas = document.createElement( 'canvas' ),
-				cropContext = cropCanvas.getContext( '2d' ),
-				ratio = 500 / canvas.width;
+		return Promise.resolve()
+			.then( this.handleIFrames.bind( this ) )
+			.then( this.handleSlides.bind( this ) )
+			.then( this.createCanvas.bind( this ) )
+			.then( this.cropCanvas.bind( this ) )
+			.then( this.save.bind( this ) )
+	}
 
-			cropCanvas.width = 500;
-			cropCanvas.height = 700;
-
-			// jQuery( 'body' ) .append( renderResult.image );
-
-			cropContext.drawImage( canvas, 0, 0, canvas.width, canvas.height, 0, 0, canvas.width * ratio, canvas.height * ratio );
-
-			elementorCommon.ajax.addRequest( 'screenshot_save', {
-				data: {
-					post_id: elementorFrontendConfig.post.id,
-					screenshot: cropCanvas.toDataURL( 'image/png' ),
+	handleIFrames() {
+		this.$elementor.find( 'iframe' ).each( () => {
+			const $iframe = jQuery( this )
+			const $iframeMask = jQuery( '<div />', {
+				css: {
+					background: 'gray',
+					width: $iframe.width(),
+					height: $iframe.height(),
 				},
 			} );
+
+			if ( $iframe.next().is( '.elementor-custom-embed-image-overlay' ) ) {
+				const regex = /url\(\"(.*)\"/gm;
+				const url = $iframe.next().css( 'backgroundImage' );
+				const matches = regex.exec( url );
+
+				$iframeMask.css( { background: $iframe.next().css( 'background' ) } );
+
+				$iframeMask.append( jQuery( '<img />', {
+					src: matches[ 1 ],
+					css: {
+						width: $iframe.width(),
+						height: $iframe.height(),
+					},
+				} ) );
+
+				$iframe.next().remove();
+
+			} else if ( -1 !== $iframe.attr( 'src' ).search( 'youtu' ) ) {
+				const regex = /^.*(?:youtu.be\/|youtube(?:-nocookie)?.com\/(?:(?:watch)??(?:.*&)?vi?=|(?:embed|v|vi|user)\/))([^?&"'>]+)/;
+				const matches = regex.exec( $iframe.attr( 'src' ) );
+
+				$iframeMask.append( jQuery( '<img />', {
+					src: screenshotProxy( 'https://img.youtube.com/vi/' + matches[ 1 ] + '/0.jpg' ),
+					crossOrigin: 'Anonymous',
+					css: {
+						width: $iframe.width(),
+						height: $iframe.height(),
+					},
+				} ) );
+			}
+
+			$iframe.before( $iframeMask );
+			$iframe.remove();
 		} );
-	};
 
-	var screenshotProxy = function( url ) {
-		url = ElementorScreenshotConfig.home_url + '?screenshot_proxy&nonce=' + ElementorScreenshotConfig.nonce + '&href=' + url;
+		return Promise.resolve();
+	}
 
-		return url;
-	};
+	handleSlides() {
+		this.$elementor.find( '.elementor-slides' ).each( () => {
+			const $this = jQuery( this );
 
-	var init = function() {
-		jQuery( function() {
-			var $elementor = jQuery( ElementorScreenshotConfig.selector );
-
-			// Iframes cannot be captured.
-			$elementor.find( 'iframe' ).each( function() {
-				var $iframe = jQuery( this ),
-					$iframeMask = jQuery( '<div />', {
-						css: {
-							background: 'gray',
-							width: $iframe.width(),
-							height: $iframe.height(),
-						},
-					} );
-
-				if ( $iframe.next().is( '.elementor-custom-embed-image-overlay' ) ) {
-					var regex = /url\(\"(.*)\"/gm;
-					var url = $iframe.next().css( 'backgroundImage' );
-					var matches = regex.exec( url );
-
-					$iframeMask.css( {
-						background: $iframe.next().css( 'background' )
-					} );
-
-					$iframeMask.append( jQuery( '<img />', {
-						src: matches[ 1 ],
-						css: {
-							width: $iframe.width(),
-							height: $iframe.height(),
-						},
-					} ) );
-
-					$iframe.next().remove();
-				} else if ( -1 !== $iframe.attr( 'src' ).search( 'youtu' ) ) {
-					var regex = /^.*(?:youtu.be\/|youtube(?:-nocookie)?.com\/(?:(?:watch)??(?:.*&)?vi?=|(?:embed|v|vi|user)\/))([^?&"'>]+)/;
-					var matches = regex.exec( $iframe.attr( 'src' ) );
-
-					$iframeMask.append( jQuery( '<img />', {
-						src: screenshotProxy( 'https://img.youtube.com/vi/' + matches[1] + '/0.jpg' ),
-						crossOrigin: 'Anonymous',
-						css: {
-							width: $iframe.width(),
-							height: $iframe.height(),
-						},
-					} ) );
-				}
-
-				$iframe.before( $iframeMask );
-				$iframe.remove();
-			} );
-
-			$elementor.find( '.elementor-slides' ).each( function() {
-				var $this = jQuery( this );
-				$this.attr( 'data-slider_options', $this.attr( 'data-slider_options' ).replace( '"autoplay":true', '"autoplay":false' ) );
-			} );
-
-			setTimeout( createScreenshot, 5000 );
+			$this
+				.attr(
+					'data-slider_options',
+					$this.attr( 'data-slider_options' )
+						.replace( '"autoplay":true', '"autoplay":false' )
+				);
 		} );
-	};
 
-	init();
-};
+		return Promise.resolve();
+	}
 
-new ScreenshotModule();
+	/**
+	 * Creates the canvas capture the screenshot.
+	 *
+	 * @returns {Promise<unknown>}
+	 */
+	createCanvas() {
+		return html2canvas( this.$elementor.get( 0 ), {
+			useCORS: true,
+			foreignObjectRendering: true,
+		} );
+	}
+
+	/**
+	 * Crop canvas.
+	 *
+	 * @param canvas
+	 * @returns {Promise<unknown>}
+	 */
+	cropCanvas( canvas ) {
+		const cropCanvas = document.createElement( 'canvas' );
+		const cropContext = cropCanvas.getContext( '2d' );
+		const ratio = 500 / canvas.width;
+
+		cropCanvas.width = 500;
+		cropCanvas.height = 700;
+
+		cropContext.drawImage( canvas, 0, 0, canvas.width, canvas.height, 0, 0, canvas.width * ratio, canvas.height * ratio );
+
+		return Promise.resolve( cropCanvas )
+	}
+
+	/**
+	 * Send the image to the server.
+	 *
+	 * @param canvas
+	 * @returns {*}
+	 */
+	save( canvas ) {
+		return elementorCommon.ajax.addRequest( 'screenshot_save', {
+			data: {
+				post_id: this.postId,
+				screenshot: canvas.toDataURL( 'image/png' ),
+			},
+		} );
+	}
+}
+
+new Screenshot()
