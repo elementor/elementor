8636711cdd7e0c616d7278e86a4a30869cb3d787
leonid <leonid@elementor.com>
2020-06-28 15:44:28 +0300

    Data/Globals: Fix - Require command-base compatibility.

--- assets/dev/js/editor/document/commands/base/command-disable-enable.js
+++ assets/dev/js/editor/document/commands/base/command-disable-enable.js
@@ -21,16 +21,17 @@ export default class CommandDisableEnable extends CommandHistory {
 	}
 
 	static restore( historyItem, isRedo ) {
-		const data = historyItem.get( 'data' );
+		const data = historyItem.get( 'data' ),
+			commandInstance = $e.commands.get( data.command ).class;
 
 		// Upon `disable` command toggle `isRedo`.
-		if ( this.getDisableCommand() === data.command ) {
+		if ( commandInstance.getDisableCommand() === data.command ) {
 			isRedo = ! isRedo;
 		}
 
 		historyItem.get( 'containers' ).forEach( ( container ) => {
 			const settings = data.changes[ container.id ],
-				toggle = isRedo ? this.getEnableCommand() : this.getDisableCommand();
+				toggle = isRedo ? commandInstance.getEnableCommand() : commandInstance.getDisableCommand();
 
 			$e.run( toggle, {
 				container,
@@ -43,7 +44,6 @@ export default class CommandDisableEnable extends CommandHistory {
 
 	initialize( args ) {
 		super.initialize( args );
-
 		/**
 		 * Which command is running.
 		 *
