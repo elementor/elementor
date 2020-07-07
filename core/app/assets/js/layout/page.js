ef5f14ad830945fb2f12eff20abff7166c6cf4c7
Josh <Josh@elementor.com>
2020-07-07 20:29:19 +0300

    Toggle dark mode based on user preferences

--- core/app/assets/js/layout/page.js
+++ core/app/assets/js/layout/page.js
@@ -3,8 +3,19 @@ import Sidebar from './sidebar';
 import Content from './content';
 
 export default function Page( props ) {
+	const uiTheme = elementorAppConfig.ui_theme;
+	let userPrefersTheme = '';
+
+	if ( 'auto' === uiTheme ) {
+		if ( window.matchMedia && window.matchMedia( '(prefers-color-scheme: dark)' ).matches ) {
+			userPrefersTheme = ' dark';
+		}
+	} else if ( 'dark' === uiTheme ) {
+		userPrefersTheme = ' dark';
+	}
+
 	return (
-		<div className={ props.className }>
+		<div className={ props.className + userPrefersTheme }>
 			<div className="dialog-widget dialog-lightbox-widget dialog-type-buttons dialog-type-lightbox elementor-templates-modal">
 				<div className="dialog-widget-content dialog-lightbox-widget-content elementor-app">
 					<Header title={ props.title } buttons={ props.headerButtons } />
