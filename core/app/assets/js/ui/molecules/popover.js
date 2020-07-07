308d1eb314e6ff75221a52be6c464ea9fa1f961e
Josh <Josh@elementor.com>
2020-07-07 21:19:20 +0300

    Site editor popover style fine-tuning

--- core/app/assets/js/ui/molecules/popover.js
+++ core/app/assets/js/ui/molecules/popover.js
@@ -1,11 +1,7 @@
 export default function Popover( props ) {
 	return (
 		<ul className={`eps-popover ${ props.className }`}>
-			{ (
-				props.children.map( ( item ) => {
-					return <li className="eps-popover__item" key={ item.text }>{ item }</li>;
-				} )
-			) }
+			{ props.children }
 		</ul>
 	);
 }
