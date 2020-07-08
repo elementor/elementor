3c96313dcdc657cbdc04b2df2bb8ff1b23f288db
Rotem Ben-Itzhak <rotem@elementor.com>
2020-07-08 13:41:36 +0300

    Changing data structure.

--- core/app/modules/import-export/assts/js/shared/kit-content-list/kit-content-list.js
+++ core/app/modules/import-export/assts/js/shared/kit-content-list/kit-content-list.js
@@ -1,3 +1,5 @@
+import { useState, useEffect } from 'react';
+
 import List from '../../ui/list/list';
 import Text from '../../ui/text/text';
 import Box from '../../ui/box/box';
@@ -9,87 +11,111 @@ import Button from 'elementor-app/ui/molecules/button';
 import './kit-content-list.scss';
 
 export default function KitContentList( props ) {
-	const getButton = () => (
-			<Grid item>
-				<Button variant="cta" text={ __( 'Lear More', 'elementor' ) } url="/#" />
-			</Grid>
-		),
-		getNotice = ( notice ) => (
-			<Box type="notice">
-				<Text size="sm">
-					{ notice }
-				</Text>
-			</Box>
-		),
-		getProFeaturesIndication = () => (
-			<Text size="md" tag="span" color="cta" className="kit-content-list__pro-indication">
-				<strong>{ __( 'Pro Features', 'elementor' ) }</strong>
-			</Text>
-		),
-		getFeatures = ( features ) => {
-			const lockedFeatures = features.locked?.length ? <span className="kit-content-list__locked-features">{ features.locked.join( ', ' ) }</span> : null;
-			let openFeatures = features.open?.join( ', ' );
-
-			if ( openFeatures && lockedFeatures ) {
-				openFeatures += ', ';
-			}
-
-			return (
-				<>
-					{ openFeatures }
-					{ lockedFeatures }
-				</>
-			);
-		},
-		getContentSelection = ( item ) => {
-			if ( 'content' !== item.type || 'export' !== props.type ) {
-				return;
-			}
-
-			return (
-				<Grid container justify="center" className="kit-content-selection-container">
-					<KitContentSelect options={ item.data.contentSelection } />
-				</Grid>
-			);
-		},
-		exportContent = [
-			{
-				type: 'templates',
-				data: {
-					title: __( 'Global Templates', 'elementor' ),
-					features: {
-						open: [
-							__( 'Saved Templates', 'elementor' ),
-						],
-						locked: [
-							__( 'Site Parts', 'elementor' ),
-							__( 'Popups', 'elementor' ),
-							__( 'Global Widgets', 'elementor' ),
-						],
-					},
-					notice: 'Site Parts, Global widgets and Popups will are available only when Elementor Pro license is Connected',
-				},
-			},
-			{
-				type: 'styles',
-				data: {
-					title: __( 'Global Styles And Settings', 'elementor' ),
-					description: __( 'Theme Style, Global Colors and Typography, Layout, Lightbox and Site Identity settings', 'elementor' ),
-				},
-			},
-			{
-				type: 'content',
-				data: {
-					title: __( 'Content', 'elementor' ),
-					description: __( 'Published pages, posts, related taxonomies, menu and custom post types.', 'elementor' ),
-					contentSelection: [
-						__( 'Select custom post types (maximum of 20 posts will be included)', 'elementor' ),
-						'Option 2',
-						'Option 3',
+	const [ postsList, setPostsList ] = useState( [ __( 'Select custom post types (maximum of 20 posts will be included)', 'elementor' ) ] );
+	const exportContent = [
+		{
+			type: 'templates',
+			data: {
+				title: __( 'Global Templates', 'elementor' ),
+				features: {
+					open: [
+						__( 'Saved Templates', 'elementor' ),
+					],
+					locked: [
+						__( 'Site Parts', 'elementor' ),
+						__( 'Popups', 'elementor' ),
+						__( 'Global Widgets', 'elementor' ),
 					],
 				},
+				notice: 'Site Parts, Global widgets and Popups will are available only when Elementor Pro license is Connected',
 			},
-		];
+		},
+		{
+			type: 'styles',
+			data: {
+				title: __( 'Global Styles And Settings', 'elementor' ),
+				description: __( 'Theme Style, Global Colors and Typography, Layout, Lightbox and Site Identity settings', 'elementor' ),
+			},
+		},
+		{
+			type: 'content',
+			data: {
+				title: __( 'Content', 'elementor' ),
+				description: __( 'Published pages, posts, related taxonomies, menu and custom post types.', 'elementor' ),
+				contentSelection: postsList,
+			},
+		},
+	],
+	getButton = () => (
+		<Grid item>
+			<Button variant="cta" text={ __( 'Lear More', 'elementor' ) } url="/#" />
+		</Grid>
+	),
+	getNotice = ( notice ) => (
+		<Box type="notice">
+			<Text size="sm">
+				{ notice }
+			</Text>
+		</Box>
+	),
+	getProFeaturesIndication = () => (
+		<Text size="md" tag="span" color="cta" className="kit-content-list__pro-indication">
+			<strong>{ __( 'Pro Features', 'elementor' ) }</strong>
+		</Text>
+	),
+	getFeatures = ( features ) => {
+		const lockedFeatures = features.locked?.length ? <span className="kit-content-list__locked-features">{ features.locked.join( ', ' ) }</span> : null;
+		let openFeatures = features.open?.join( ', ' );
+
+		if ( openFeatures && lockedFeatures ) {
+			openFeatures += ', ';
+		}
+
+		return (
+			<>
+				{ openFeatures }
+				{ lockedFeatures }
+			</>
+		);
+	},
+	getContentSelection = ( item ) => {
+		if ( 'content' !== item.type || 'export' !== props.type ) {
+			return;
+		}
+
+		return (
+			<Grid container justify="center" className="kit-content-selection-container">
+				<KitContentSelect options={ item.data.contentSelection } />
+			</Grid>
+		);
+	};
+
+	const getApiData = () => {
+		return new Promise( ( res ) => {
+			setTimeout( () => {
+				const posts = [
+					'post 1',
+					'post 2',
+					'post 3',
+				];
+
+				res( posts );
+			}, 2000 );
+		} );
+	};
+
+	useEffect( () => {
+		//console.log( 'running useEffect' );
+		const getData = async () => {
+			const data = await getApiData();
+
+			setPostsList( ( prevState ) => {
+				return prevState.concat( data );
+			} );
+		};
+
+		getData();
+	}, [] );
 
 	return (
 		<List separated className="kit-content-list">
