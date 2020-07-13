64ee8d1d9e0f21df3f7139a7c45a62ee45bd45e1
Nevo Golan <nevog@elementor.com>
2020-07-13 16:27:57 +0300

    Frontend:RenderMode - Fix - 'get_oembed_data' already called inside 'get_embed_thumbnail_html'.

--- includes/widgets/video.php
+++ includes/widgets/video.php
@@ -806,9 +806,6 @@ class Widget_Video extends Widget_Base {
 			$post_id = get_queried_object_id();
 
 			if ( $is_static_render_mode ) {
-				// Make sure that the oembed data saved in cache.
-				Embed::get_oembed_data( $video_url, $post_id );
-
 				$video_html = Embed::get_embed_thumbnail_html( $video_url, $post_id );
 			} else {
 				$video_html = Embed::get_embed_html( $video_url, $embed_params, $embed_options );
