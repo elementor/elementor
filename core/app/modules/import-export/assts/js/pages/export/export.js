07440990455c8c0d01f891ac045c36f3e0a1f834
Rotem Ben-Itzhak <rotem@elementor.com>
2020-07-13 12:06:11 +0300

    Moving loading handling to the main exportc component.

--- core/app/modules/import-export/assts/js/pages/export/export.js
+++ core/app/modules/import-export/assts/js/pages/export/export.js
@@ -1,13 +1,18 @@
+import { useState } from 'react';
+
 import Layout from '../../templates/layout';
 import ExportContentList from './export-content-list/export-content-list';
+import Loading from '../../shared/loading/loading';
 
 import '../import-export.scss';
 import './export.scss';
 
 export default function Export() {
+	const [ isLoading, setIsLoading ] = useState( false );
+
 	return (
 		<Layout type="export">
-			<ExportContentList />
+			{ isLoading ? <Loading /> : <ExportContentList setIsLoading={ setIsLoading } /> }
 		</Layout>
 	);
 }
