d37f40069b0fb3a4bcf195101cca4adcb09d5867
Kobi Zaltzberg <kobi@elementor.com>
2020-07-15 09:10:32 +0300

    Typo

--- tests/phpunit/elementor/core/test-documents-manager.php
+++ tests/phpunit/elementor/core/test-documents-manager.php
@@ -15,7 +15,7 @@ class Test_Documents_Manager extends Elementor_Test_Base {
 		$titles = [
 			'An - happy - title :)' => 'An - happy - title :)',
 			"One more title <script>alert('Hello buddy')</script>" => 'One more title &lt;script&gt;alert(&#039;Hello buddy&#039;)&lt;/script&gt;',
-			'Title with image? Why not? <img src="x" onerror="alert(\'XSS\')">. It sounds grate for me!!' => 'Title with image? Why not? &lt;img src=&quot;x&quot; onerror=&quot;alert(&#039;XSS&#039;)&quot;&gt;. It sounds grate for me!!',
+			'Title with image? Why not? <img src="x" onerror="alert(\'XSS\')">. It sounds great for me!!' => 'Title with image? Why not? &lt;img src=&quot;x&quot; onerror=&quot;alert(&#039;XSS&#039;)&quot;&gt;. It sounds great for me!!',
 		];
 
 		foreach( $titles as $title => $desired_title ) {
