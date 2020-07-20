5d36e6c03ec6a65c7fcc1fb592eddca1f9b0c741
Rotem Ben-Itzhak <rotem@elementor.com>
2020-07-20 16:17:52 +0300

    Allowing kit download only when kit content is selected.

--- core/app/modules/import-export/assts/js/pages/export/download-button/download-button.js
+++ core/app/modules/import-export/assts/js/pages/export/download-button/download-button.js
@@ -3,14 +3,18 @@ import { ExportConsumer } from '../../../context/export';
 import Button from 'elementor-app/ui/molecules/button';
 
 export default function DownloadButton( props ) {
-	const getDownloadUrl = ( data ) => {
+	const getDownloadUrl = ( context, isDownloadAllowed ) => {
+		if ( ! isDownloadAllowed ) {
+			return '';
+		}
+
 		const currentBaseUrl = window.location.origin + window.location.pathname + window.location.search,
 			queryConnection = currentBaseUrl.indexOf( '?' ) > -1 ? '&' : '?',
 			exportData = {
 				elementor_export_kit: {
-					title: data.title,
-					include: data.includes,
-					custom_post_types: data.postTypes,
+					title: context.title,
+					include: context.includes,
+					custom_post_types: context.postTypes,
 				},
 			};
 
@@ -20,17 +24,24 @@ export default function DownloadButton( props ) {
 	return (
 		<ExportConsumer>
 			{
-				( context ) => (
-					<Button
-						url={ getDownloadUrl( context ) }
-						onClick={ () => props.setIsDownloading(true) }
-						target="_blank"
-						variant="contained"
-						size="lg"
-						color="primary"
-						text={ __( 'Next', 'elementor' ) }
-					/>
-				)
+				( context ) => {
+					const isDownloadAllowed = context.includes.length;
+
+					return (
+						<Button
+							variant="contained"
+							size="lg"
+							text={ __( 'Next', 'elementor' ) }
+							color={ isDownloadAllowed ? 'primary' : 'disabled' }
+							url={ getDownloadUrl( context, isDownloadAllowed ) }
+							onClick={ () => {
+								if ( isDownloadAllowed ) {
+									props.setIsDownloading( true );
+								}
+							} }
+						/>
+					);
+				}
 			}
 		</ExportConsumer>
 	);
