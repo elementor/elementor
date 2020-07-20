02bbb2cfef42c77901b842f0e746ce275a3efe2a
Rotem Ben-Itzhak <rotem@elementor.com>
2020-07-20 16:18:25 +0300

    Adding loader icon.

--- core/app/modules/import-export/assts/js/pages/import-process/import-process.js
+++ core/app/modules/import-export/assts/js/pages/import-process/import-process.js
@@ -2,6 +2,7 @@ import Layout from '../../templates/layout';
 import Message from '../../ui/message/message';
 import Box from '../../ui/box/box';
 import Footer from '../../shared/footer/footer';
+import Icon from 'elementor-app/ui/atoms/icon';
 import Heading from 'elementor-app/ui/atoms/heading';
 import Text from 'elementor-app/ui/atoms/text';
 import Button from 'elementor-app/ui/molecules/button';
@@ -9,10 +10,18 @@ import Button from 'elementor-app/ui/molecules/button';
 import './import-process.scss';
 
 export default function ImportProcess() {
+	const getFooter = () => (
+		<Footer separator justify="end">
+			<Button variant="contained" color="disabled" text={ __( 'Cancel', 'elementor' ) } url="/import/process" />
+		</Footer>
+	);
+
 	return (
-		<Layout type="import">
+		<Layout type="import" footer={ getFooter() }>
 			<Message className="e-app-import-process">
-				<Heading variant="lg">
+				<Icon className="e-app-import-process__icon eicon-loading" />
+
+				<Heading variant="lg" className="e-app-import-process__main-title">
 					{ __( 'Your Kit Is Being Imported', 'elementor' ) }
 				</Heading>
 
@@ -23,10 +32,6 @@ export default function ImportProcess() {
 				<Box className="e-app-import-process__box">
 					{ __( 'Importing global templates', 'elementor' ) }
 				</Box>
-
-				<Footer separator justify="end">
-					<Button variant="contained" color="disabled" text={ __( 'Cancel', 'elementor' ) } url="/import/process" />
-				</Footer>
 			</Message>
 		</Layout>
 	);
