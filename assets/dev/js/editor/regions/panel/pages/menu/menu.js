55db78739f05373aadf9d2e457a03eb80388bea8
Kobi Zaltzberg <kobi@elementor.com>
2020-07-21 16:22:45 +0300

    Allow to extend panel menu

--- assets/dev/js/editor/regions/panel/pages/menu/menu.js
+++ assets/dev/js/editor/regions/panel/pages/menu/menu.js
@@ -1,148 +1,101 @@
-var PanelMenuGroupView = require( 'elementor-panel/pages/menu/views/group' ),
-	PanelMenuPageView;
+import MenuPageView from 'elementor-panel/pages/menu/base';
 
-PanelMenuPageView = Marionette.CompositeView.extend( {
-	id: 'elementor-panel-page-menu',
+export default class PanelMenu extends MenuPageView {
+	initialize() {
+		this.collection = PanelMenu.getGroups();
+	}
 
-	template: '#tmpl-elementor-panel-menu',
-
-	childView: PanelMenuGroupView,
-
-	childViewContainer: '#elementor-panel-page-menu-content',
-
-	// Remove empty groups that exist for BC.
-	filter: ( child ) => {
-		return child.get( 'items' ).length;
-	},
-
-	initialize: function() {
-		this.collection = PanelMenuPageView.getGroups();
-
-		this.registerDocumentItems();
-
-		// On switch a document, re create document items.
-		elementor.once( 'document:loaded', this.registerDocumentItems );
-	},
-
-	getArrowClass: function() {
+	getArrowClass() {
 		return 'eicon-chevron-' + ( elementorCommon.config.isRTL ? 'right' : 'left' );
-	},
+	}
 
-	onRender: function() {
+	onRender() {
 		elementor.getPanelView().getHeaderView().ui.menuIcon.removeClass( 'eicon-menu-bar' ).addClass( this.getArrowClass() );
-	},
+	}
 
-	onDestroy: function() {
+	onDestroy() {
 		elementor.getPanelView().getHeaderView().ui.menuIcon.removeClass( this.getArrowClass() ).addClass( 'eicon-menu-bar' );
-	},
+	}
+}
 
-	registerDocumentItems() {
-		// Todo: internal command.
-		elementor.modules.layouts.panel.pages.menu.Menu.addItem( {
-			name: 'view-page',
-			icon: 'eicon-preview-thin',
-			title: elementor.translate( 'view_page' ),
-			type: 'link',
-			link: elementor.config.document.urls.permalink,
-		}, 'navigate_from_page' );
+PanelMenu.groups = null;
 
-		// Todo: internal command.
-		elementor.modules.layouts.panel.pages.menu.Menu.addItem( {
-			name: 'exit-to-dashboard',
-			icon: 'eicon-wordpress',
-			title: elementor.translate( 'exit_to_dashboard' ),
-			type: 'link',
-			link: elementor.config.document.urls.exit_to_dashboard,
-		}, 'navigate_from_page' );
-	},
-}, {
-	groups: null,
+PanelMenu.initGroups = () => {
+	PanelMenu.groups = new Backbone.Collection( [] );
 
-	initGroups: function() {
-		this.groups = new Backbone.Collection( [] );
+	// Keep the old `more` for BC, since 3.0.0.
+	PanelMenu.groups.add( {
+		name: 'more',
+		title: elementor.translate( 'more' ),
+		items: [],
+	} );
 
-		// Keep the old `more` for BC, since 3.0.0.
-		this.groups.add( {
-			name: 'more',
-			title: elementor.translate( 'more' ),
-			items: [],
-		} );
+	PanelMenu.groups.add( {
+		name: 'navigate_from_page',
+		title: elementor.translate( 'navigate_from_page' ),
+		items: [],
+	} );
 
-		this.groups.add( {
-			name: 'navigate_from_page',
-			title: elementor.translate( 'navigate_from_page' ),
-			items: [],
-		} );
+	// Todo: internal command.
+	PanelMenu.addItem( {
+		name: 'view-page',
+		icon: 'eicon-preview-thin',
+		title: elementor.translate( 'view_page' ),
+		type: 'link',
+		link: elementor.config.document.urls.permalink,
+	}, 'navigate_from_page' );
 
-		if ( elementor.config.user.is_administrator ) {
-			this.addAdminMenu();
-		}
-	},
+	// Todo: internal command.
+	PanelMenu.addItem( {
+		name: 'exit-to-dashboard',
+		icon: 'eicon-wordpress',
+		title: elementor.translate( 'exit_to_dashboard' ),
+		type: 'link',
+		link: elementor.config.document.urls.exit_to_dashboard,
+	}, 'navigate_from_page' );
 
-	addAdminMenu: function() {
-		this.groups.add( {
-			name: 'style',
-			title: elementor.translate( 'global_style' ),
-			items: [],
-		}, { at: 0 } );
+	if ( elementor.config.user.is_administrator ) {
+		PanelMenu.addAdminMenu();
+	}
+};
 
-		this.addItem( {
-			name: 'editor-preferences',
-			icon: 'eicon-user-preferences',
-			title: elementor.translate( 'user_preferences' ),
-			type: 'page',
-			callback: () => $e.route( 'panel/editor-preferences' ),
-		}, 'style' );
+PanelMenu.addAdminMenu = () => {
+	PanelMenu.groups.add( {
+		name: 'style',
+		title: elementor.translate( 'global_style' ),
+		items: [],
+	}, { at: 0 } );
 
-		this.groups.add( {
-			name: 'settings',
-			title: elementor.translate( 'settings' ),
-			items: [],
-		}, { at: 1 } );
+	PanelMenu.addItem( {
+		name: 'editor-preferences',
+		icon: 'eicon-user-preferences',
+		title: elementor.translate( 'user_preferences' ),
+		type: 'page',
+		callback: () => $e.route( 'panel/editor-preferences' ),
+	}, 'style' );
 
-		this.addItem( {
-			name: 'finder',
-			icon: 'eicon-search',
-			title: elementorCommon.translate( 'find_anything', 'finder' ),
-			callback: () => $e.route( 'finder' ),
-		}, 'navigate_from_page', 'view-page' );
-	},
+	PanelMenu.groups.add( {
+		name: 'settings',
+		title: elementor.translate( 'settings' ),
+		items: [],
+	}, { at: 1 } );
 
-	getGroups: function() {
-		if ( ! this.groups ) {
-			this.initGroups();
-		}
+	PanelMenu.addItem( {
+		name: 'finder',
+		icon: 'eicon-search',
+		title: elementorCommon.translate( 'find_anything', 'finder' ),
+		callback: () => $e.route( 'finder' ),
+	}, 'navigate_from_page', 'view-page' );
+};
 
-		return this.groups;
-	},
+PanelMenu.getGroups = () => {
+	if ( ! PanelMenu.groups ) {
+		PanelMenu.initGroups();
+	}
 
-	addItem: function( itemData, groupName, before ) {
-		var group = this.getGroups().findWhere( { name: groupName } );
+	return PanelMenu.groups;
+};
 
-		if ( ! group ) {
-			return;
-		}
-
-		var items = group.get( 'items' ),
-			beforeItem;
-
-		// Remove if exist.
-		const exists = _.findWhere( items, { name: itemData.name } );
-
-		if ( exists ) {
-			items.splice( items.indexOf( exists ), 1 );
-		}
-
-		if ( before ) {
-			beforeItem = _.findWhere( items, { name: before } );
-		}
-
-		if ( beforeItem ) {
-			items.splice( items.indexOf( beforeItem ), 0, itemData );
-		} else {
-			items.push( itemData );
-		}
-	},
-} );
-
-module.exports = PanelMenuPageView;
+PanelMenu.addItem = ( itemData, groupName, before ) => {
+	MenuPageView.addItem( PanelMenu.getGroups(), itemData, groupName, before );
+};
