d7be9b7536858d7be17dad645e01d01ee37802bd
Mati Horovitz <mati@elementor.com>
2020-07-26 18:47:27 +0300

    Kit CSS Variables: Fix - Move code to kitsManager + clean up.

--- assets/dev/js/editor/data/globals/component.js
+++ assets/dev/js/editor/data/globals/component.js
@@ -4,16 +4,11 @@ import ColorsComponent from './colors/component';
 
 import * as commandsData from './commands/data/';
 
-const ControlsCSSParser = require( 'elementor-editor-utils/controls-css-parser' );
-
 export default class Component extends ComponentBase {
 	__construct( args = {} ) {
 		super.__construct( args );
 
 		elementorCommon.elements.$window.on( 'elementor:loaded', this.onElementorLoaded.bind( this ) );
-
-		elementor.on( 'document:loaded', () => this.updateTempStylesheet() );
-		elementor.on( 'preview:loaded', () => this.updateTempStylesheet() );
 	}
 
 	getNamespace() {
@@ -34,56 +29,5 @@ export default class Component extends ComponentBase {
 	onElementorLoaded() {
 		// Add globals to cache before render.
 		$e.data.get( 'globals/index' ).then( () => elementor.trigger( 'globals:loaded' ) );
-	}
-
-	/**
-	 * In case there is a new global color/typography convert current globals to CSS variables.
-	 */
-	updateTempStylesheet() {
-		if ( ! this.controlsCSS ) {
-			this.controlsCSS = new ControlsCSSParser( {
-				id: 'e-kit-temp-variables',
-				settingsModel: new elementorModules.editor.elements.models.BaseSettings( {}, {} ),
-			} );
-		}
-
-		// The kit document has it's own CSS.
-		if ( 'kit' === elementor.documents.getCurrent().config.type ) {
-			this.controlsCSS.stylesheet.empty();
-			return;
-		}
-
-		$e.data.get( 'globals/index' ).then( ( { data } ) => {
-			if ( data.colors ) {
-				Object.values( data.colors ).forEach( ( item ) => {
-					const controls = elementor.config.kit_config.design_system_controls.colors,
-						values = {
-							_id: item.id,
-							color: item.value,
-						};
-
-					this.controlsCSS.addStyleRules( controls, values, controls, [ '{{WRAPPER}}' ], [ 'body' ] );
-				} );
-			}
-
-			if ( data.typography ) {
-				Object.values( data.typography ).forEach( ( item ) => {
-					const controls = elementor.config.kit_config.design_system_controls.typography,
-						values = {
-							_id: item.id,
-							...item.value,
-						};
-
-					// Enqueue fonts.
-					if ( item.value.typography_font_family ) {
-						elementor.helpers.enqueueFont( item.value.typography_font_family );
-					}
-
-					this.controlsCSS.addStyleRules( controls, values, controls, [ '{{WRAPPER}}' ], [ 'body' ] );
-				} );
-			}
-
-			this.controlsCSS.addStyleToDocument();
-		} );
 	}
 }
