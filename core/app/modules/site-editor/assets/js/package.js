5b845e4115a4b220ef269d02c3f1a0eb067cf4e4
Nevo Golan <nevog@elementor.com>
2020-08-05 18:12:37 +0300

    WIP : Site Editor - New - dialog in package for pro.

--- core/app/modules/site-editor/assets/js/package.js
+++ core/app/modules/site-editor/assets/js/package.js
@@ -1,15 +1,31 @@
 // Alphabetical order.
 import AllPartsButton from './organisms/all-parts-button';
+import Dialog from './ui/dialog/dialog';
+import DialogActions from './ui/dialog/dialog-actions';
+import DialogButton from './ui/dialog/dialog-button';
+import DialogContent from './ui/dialog/dialog-content';
+import DialogText from './ui/dialog/dialog-text';
+import DialogTitle from './ui/dialog/dialog-title';
+import ErrorDialog from './organisms/error-dialog';
 import Layout from './templates/layout';
+import Module from './module';
+import NotFound from './pages/not-found';
 import SiteParts from './organisms/site-parts';
 import SitePart from './molecules/site-part';
 import { Context as TemplateTypesContext } from './context/template-types';
-import Module from './module';
 
 export default {
 	AllPartsButton,
+	Dialog,
+	DialogActions,
+	DialogButton,
+	DialogContent,
+	DialogText,
+	DialogTitle,
+	ErrorDialog,
 	Layout,
 	Module,
+	NotFound,
 	SitePart,
 	SiteParts,
 	TemplateTypesContext,
