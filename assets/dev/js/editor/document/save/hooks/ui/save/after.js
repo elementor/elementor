b0632ca0bf48452a61eee6594e2d075ab879fdde
udidol <udi.dollberg@gmail.com>
2020-08-10 15:39:25 +0300

    Theme Builder: Save Toaster - Move the Toaster functionlity to its own hook in Elementor Pro.

--- assets/dev/js/editor/document/save/hooks/ui/save/after.js
+++ assets/dev/js/editor/document/save/hooks/ui/save/after.js
@@ -28,32 +28,17 @@ export class FooterSaverAfterSave extends HookUIAfter {
 	}
 
 	onPageStatusChange( newStatus ) {
+		if ( elementor.config.document.support_site_editor ) {
+			return;
+		}
+
 		if ( 'publish' === newStatus ) {
 			const options = {
 				message: elementor.config.document.panel.messages.publish_notification,
 			};
 
 			// Don't add the "Have a look" link in the theme builder.
-			if ( elementor.config.document.support_site_editor ) {
-				options.buttons = [
-					{
-						name: 'open_site_editor',
-						text: '<i class="eicon-external-link-square"></i><span class="e-theme-builder-toaster-button-text">' + elementorPro.translate( 'open_site_editor' ) + '</span>',
-						callback() {
-							$e.run( 'app/open' );
-						},
-					},
-					{
-						name: 'view_live_site',
-						text: '<i class="eicon-preview-medium"></i><span class="e-theme-builder-toaster-button-text">' + elementorPro.translate( 'view_live_site' ) + '</span>',
-						callback() {
-							open( elementor.config.document.urls.permalink );
-						},
-					},
-				];
-
-				options.classes = 'e-theme-builder-save-toaster';
-			} else if ( elementor.config.document.urls.have_a_look ) {
+			if ( elementor.config.document.urls.have_a_look ) {
 				options.buttons = [
 					{
 						name: 'view_page',
