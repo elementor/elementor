0c818ebf16c72089653f41ac05afb743ce480007
Josh <Josh@elementor.com>
2020-08-17 17:21:59 +0300

    App: Utils for components

--- core/app/assets/js/utils/utils.js
+++ core/app/assets/js/utils/utils.js
@@ -6,4 +6,15 @@ export default class Utils {
 
 		return `${ pixels * 0.0625 }rem`;
 	}
+
+	static arrayToClassName = ( array ) => {
+		return array.filter( ( classItem ) => '' !== classItem ).join( ' ' );
+	}
+
+	static stringToRemValues = ( string ) => {
+		return string
+			.split( ' ' )
+			.map( ( value ) => Utils.pxToRem( value ) )
+			.join( ' ' );
+	}
 }
