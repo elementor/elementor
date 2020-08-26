b7182c2c454e180347bae2c7d105f8ee78e80485
andrea-buzz <54395135+andrea-buzz@users.noreply.github.com>
2020-08-26 13:02:00 +0200

    Update api.php
    FIX Illegal string offset 'pro_widgets'

--- includes/api.php
+++ includes/api.php
@@ -157,7 +157,7 @@ class Api {
 	public static function get_promotion_widgets() {
 		$data = self::get_info_data();
 
-		return $data['pro_widgets'];
+		return empty( $data['pro_widgets']) ? false : $data['pro_widgets'];
 	}
 
 	/**
