921c5a729909b66e95bfbf7c54191fd6cccb23a9
udidol <udi.dollberg@gmail.com>
2020-09-14 18:51:49 +0300

    Add the WP translation functions to webpack instead of the window scope.

--- .grunt-config/webpack.js
+++ .grunt-config/webpack.js
@@ -92,6 +92,8 @@ const plugins = [
 		React: 'react',
 		ReactDOM: 'react-dom',
 		PropTypes: 'prop-types',
+		__: ['@wordpress/i18n', '__'],
+		sprintf: ['@wordpress/i18n', 'sprintf'],
 	} )
 ];
 
--- assets/dev/js/editor/editor-base.js
+++ assets/dev/js/editor/editor-base.js
@@ -1,6 +1,5 @@
 /* global ElementorConfig */
 
-import { __, sprintf } from '@wordpress/i18n';
 import ColorControl from './controls/color';
 import DateTimeControl from 'elementor-controls/date-time';
 import EditorDocuments from 'elementor-editor/component';
@@ -418,9 +417,6 @@ export default class EditorBase extends Marionette.Application {
 		window.elementorFrontend = frontendWindow.elementorFrontend;
 
 		frontendWindow.elementor = this;
-
-		window.__ = __;
-		window.sprintf = sprintf;
 
 		frontendWindow.elementorCommon = elementorCommon;
 
