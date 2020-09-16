2ca3715b174afe7ffc19dd20027037b1094874c8
Nevo Golan <nevog@elementor.com>
2020-09-16 11:26:16 +0300

    Kits: Fix - Global that was deleted ruin the element style that it was attached to.
    For now it clears the cache for every save of the kit settings

--- tests/phpunit/elementor/core/kits/test-manager.php
+++ tests/phpunit/elementor/core/kits/test-manager.php
@@ -4,6 +4,7 @@ namespace Elementor\Tests\Phpunit\Elementor\Core\Kits;
 use Elementor\Core\Kits\Documents\Kit;
 use Elementor\Plugin;
 use Elementor\Testing\Elementor_Test_Base;
+use Elementor\Core\Files\Manager as Files_Manager;
 
 class Test_Manager extends Elementor_Test_Base {
 
@@ -39,7 +40,7 @@ class Test_Manager extends Elementor_Test_Base {
 		$this->assertNotEquals( $active_id, $active_id_after_invalidate, $test_description );
 	}
 
-	public function test_when_updating_blogname_or_blogdescription_option_it_should_update_the_kit(  ) {
+	public function test_when_updating_blogname_or_blogdescription_option_it_should_update_the_kit() {
 		$name = get_option( 'blogname' );
 		$description = get_option( 'blogdescription' );
 
@@ -59,5 +60,26 @@ class Test_Manager extends Elementor_Test_Base {
 
 		update_option( 'blogname', $name );
 		update_option( 'blogdescription', $description );
+	}
+
+	public function test_it_should_clear_files_cache_when_saving_kit() {
+		wp_set_current_user( $this->factory()->get_administrator_user()->ID );
+
+		$kit = Plugin::$instance->kits_manager->get_active_kit();
+
+		$file_manager = Plugin::instance()->files_manager;
+
+		$mock = $this->getMockBuilder( Files_Manager::class )
+		             ->setMethods( [ 'clear_cache' ] )
+		             ->getMock();
+
+		$mock->expects( $this->once() )
+		     ->method( 'clear_cache' );
+
+		Plugin::instance()->files_manager = $mock;
+
+		$kit->save( [] );
+
+		Plugin::instance()->files_manager = $file_manager;
 	}
 }
