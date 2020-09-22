9f32392017ff87c936ed0fec6a5cb1f763765946
Nevo Golan <nevog@elementor.com>
2020-09-22 18:40:27 +0300

    build: introduce a commit linter

---
+++ bin/pre-commit.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+## Linting js files
+for file in $(git diff --cached --name-only | grep -E '\.(js|jsx)$')
+do
+  git show ":$file" | node_modules/.bin/eslint --stdin --stdin-filename "$file" # we only want to lint the staged changes, not any un-staged changes
+  if [ $? -ne 0 ]; then
+    echo "ESLint failed on staged file '$file'. Please check your code and try again. You can run ESLint manually via npm run lint:js."
+    exit 1 # exit with failure status
+  fi
+done
+
