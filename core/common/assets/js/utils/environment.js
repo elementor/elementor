3187586f0799fb9d890e28a6eebfba86a78d709a
Rotem Ben-Itzhak <rotem@elementor.com>
2020-09-28 19:30:44 +0300

    Edge based on Chromium was not detected + no detection specifically for Chrome.

--- core/common/assets/js/utils/environment.js
+++ core/common/assets/js/utils/environment.js
@@ -1,10 +1,13 @@
-const userAgent = navigator.userAgent;
+const userAgent = navigator.userAgent,
+	environment = {
+		webkit: -1 !== userAgent.indexOf( 'AppleWebKit' ),
+		firefox: -1 !== userAgent.indexOf( 'Firefox' ),
+		ie: /Trident|MSIE/.test( userAgent ),
+		edge: -1 !== userAgent.indexOf( 'Edg' ),
+		mac: -1 !== userAgent.indexOf( 'Macintosh' ),
+		safari: /^((?!chrome|android).)*safari/i.test( userAgent ),
+	};
 
-export default {
-	webkit: -1 !== userAgent.indexOf( 'AppleWebKit' ),
-	firefox: -1 !== userAgent.indexOf( 'Firefox' ),
-	ie: /Trident|MSIE/.test( userAgent ),
-	edge: -1 !== userAgent.indexOf( 'Edge' ),
-	mac: -1 !== userAgent.indexOf( 'Macintosh' ),
-	safari: /^((?!chrome|android).)*safari/i.test( userAgent ),
-};
+environment.chrome = -1 !== userAgent.indexOf( 'Chrome' ) && -1 === userAgent.indexOf( 'OPR' ) && ! environment.edge;
+
+export default environment;
