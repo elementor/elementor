85d57e77ad1d23c020dadf28643c47e7aea1ac98
udidol <udi.dollberg@gmail.com>
2020-09-29 15:12:46 +0300

    Remove redundant comment

--- assets/dev/js/editor/elements/views/widget.js
+++ assets/dev/js/editor/elements/views/widget.js
@@ -135,7 +135,6 @@ WidgetView = BaseElementView.extend( {
 	},
 
 	handleEmptyWidget: function() {
-		// TODO: REMOVE THIS !!
 		this.$el
 			.addClass( 'elementor-widget-empty' )
 			.append( '<i class="elementor-widget-empty-icon ' + this.getEditModel().getIcon() + '"></i>' );
