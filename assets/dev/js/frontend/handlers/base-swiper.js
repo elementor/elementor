625e47f0f4bd76bfb9d2984d173ed6adb585d5e6
udidol <udi.dollberg@gmail.com>
2020-10-25 16:45:39 +0200

    Testimonial Carousel: Fix - When dragging a new carousel there is a console error.

--- assets/dev/js/frontend/handlers/base-swiper.js
+++ assets/dev/js/frontend/handlers/base-swiper.js
@@ -11,21 +11,12 @@ export default class SwiperHandlerBase extends BaseHandler {
 		return this.elements.$slides.length;
 	}
 
-	hasThumbs() {
-		return false;
-	}
-
 	// This method live-handles the 'Pause On Hover' control's value being changed in the Editor Panel
 	togglePauseOnHover( toggleOn ) {
 		// These variables exist because different Swiper-based widgets have different container variable names,
 		// E.g. The Media Carousel widget has two Swiper instances in Slideshow mode
-		let swiperInstance = this.swiper,
-			swiperContainer = this.elements.$swiperContainer;
-
-		if ( this.hasThumbs() ) {
-			swiperInstance = this.swipers.main;
-			swiperContainer = this.elements.$mainSwiper;
-		}
+		const swiperInstance = this.swiper || this.swipers.main,
+			swiperContainer = this.elements.$swiperContainer || this.elements.$mainSwiper;
 
 		if ( toggleOn ) {
 			swiperContainer.on( {
