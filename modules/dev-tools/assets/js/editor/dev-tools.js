4cdd33005fc5ef1f2654d5001d67239a3218ca02
udidol <36724484+udidol@users.noreply.github.com>
2020-11-18 18:18:54 +0200

    DevTools Deprecation Module Improvements (ED-620) (#12923)
    * DevTools Deprecation Module: Added a 'do_deprecated_action()' method.
    
    * DevTools Deprecation Module: Added unit tests for the 'do_deprecated_action()' method + Fixed lint error and PHPDocs in the deprecations class file.
    
    * Deprecation Module: Tweak - Changed the soft deprecation mechanism to use unique keys for deprecated items, so the deprecation notice doesn't repeat itself.
    
    * DevTools Deprecation Module: Tweak - Adjusted Unit Tests to match the new structure of the soft notices config object (it was previously a nested array).

--- modules/dev-tools/assets/js/editor/dev-tools.js
+++ modules/dev-tools/assets/js/editor/dev-tools.js
@@ -5,12 +5,10 @@ export default class extends elementorModules.editor.utils.Module {
 
 	notifyDeprecated() {
 		// eslint-disable-next-line camelcase
-		const notices = elementor.config.dev_tools?.deprecation.soft_notices;
+		const notices = elementor.config.dev_tools.deprecation.soft_notices;
 
-		if ( notices?.length ) {
-			notices.forEach( ( notice ) => {
-				elementorCommon.helpers.softDeprecated( ... notice );
-			} );
-		}
+		Object.entries( notices ).forEach( ( [ key, notice ] ) => {
+			elementorCommon.helpers.softDeprecated( key, ...notice );
+		} );
 	}
 }
