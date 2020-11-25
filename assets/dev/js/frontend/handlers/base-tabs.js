6b4a5f4ba624e29fd6ecca80f0b435d8471654ce
Josh <Josh@elementor.com>
2020-11-25 19:47:47 +0200

    Make tabs widget accessible

--- assets/dev/js/frontend/handlers/base-tabs.js
+++ assets/dev/js/frontend/handlers/base-tabs.js
@@ -2,6 +2,7 @@ export default class baseTabs extends elementorModules.frontend.handlers.Base {
 	getDefaultSettings() {
 		return {
 			selectors: {
+				tablist: '[role="tablist"]',
 				tabTitle: '.elementor-tab-title',
 				tabContent: '.elementor-tab-content',
 			},
@@ -13,6 +14,22 @@ export default class baseTabs extends elementorModules.frontend.handlers.Base {
 			toggleSelf: true,
 			hidePrevious: true,
 			autoExpand: true,
+			keys: {
+				end: 35,
+				home: 36,
+				left: 37,
+				up: 38,
+				right: 39,
+				down: 40,
+				enter: 13,
+				space: 32,
+			},
+			keyDirection: {
+				37: elementorFrontendConfig.is_rtl ? 1 : -1,
+				38: -1,
+				39: elementorFrontendConfig.is_rtl ? -1 : 1,
+				40: 1,
+			},
 		};
 	}
 
@@ -50,6 +67,47 @@ export default class baseTabs extends elementorModules.frontend.handlers.Base {
 		this.setSettings( originalToggleMethods );
 	}
 
+	handleKeyboardArrows( event ) {
+		const key = event.keyCode,
+			tab = event.currentTarget,
+			keys = this.getSettings( 'keys' ),
+			isHorizontalKey = key === keys.left || key === keys.right,
+			isVerticalKey = key === keys.up || key === keys.down,
+			isHomeEndKey = key === keys.home || key === keys.end,
+			tablist = tab.closest( this.getSettings( 'selectors' ).tablist ),
+			isVertical = 'vertical' === tablist.getAttribute( 'aria-orientation' );
+
+		if ( ( isVerticalKey && isVertical ) || ( isHorizontalKey && ! isVertical ) || isHomeEndKey ) {
+			if ( isVerticalKey || isHomeEndKey ) {
+				event.preventDefault();
+			}
+
+			this.switchTabOnKeyPress( key, tab, tablist );
+		}
+	}
+
+	switchTabOnKeyPress( keyPressed, currentTab, tablist ) {
+		const direction = this.getSettings( 'keyDirection' )[ keyPressed ],
+			tabIndex = currentTab.getAttribute( 'data-tab' ) - 1,
+			isHomeKey = this.getSettings( 'keys' ).home === keyPressed,
+			isEndKey = this.getSettings( 'keys' ).end === keyPressed;
+
+		if ( ( ! direction || 'number' !== typeof tabIndex ) && ! isHomeKey && ! isEndKey ) {
+			return;
+		}
+
+		const $tabs = jQuery( tablist ).find( this.getSettings( 'selectors' ).tabTitle ),
+			nextTab = $tabs[ tabIndex + direction ];
+
+		if ( nextTab ) {
+			nextTab.focus();
+		} else if ( isEndKey || -1 === tabIndex + direction ) {
+			$tabs.last().focus();
+		} else {
+			$tabs.first().focus();
+		}
+	}
+
 	deactivateActiveTab( tabIndex ) {
 		const settings = this.getSettings(),
 			activeClass = settings.classes.active,
@@ -58,8 +116,13 @@ export default class baseTabs extends elementorModules.frontend.handlers.Base {
 			$activeContent = this.elements.$tabContents.filter( activeFilter );
 
 		$activeTitle.add( $activeContent ).removeClass( activeClass );
+		$activeTitle.attr( {
+			tabindex: '-1',
+			'aria-selected': 'false',
+		} );
 
 		$activeContent[ settings.hideTabFn ]();
+		$activeContent.attr( 'hidden', 'hidden' );
 	}
 
 	activateTab( tabIndex ) {
@@ -70,8 +133,13 @@ export default class baseTabs extends elementorModules.frontend.handlers.Base {
 			animationDuration = 'show' === settings.showTabFn ? 0 : 400;
 
 		$requestedTitle.add( $requestedContent ).addClass( activeClass );
+		$requestedTitle.attr( {
+			tabindex: '0',
+			'aria-selected': 'true',
+		} );
 
 		$requestedContent[ settings.showTabFn ]( animationDuration, () => elementorFrontend.elements.$window.trigger( 'resize' ) );
+		$requestedContent.removeAttr( 'hidden' );
 	}
 
 	isActiveTab( tabIndex ) {
@@ -81,15 +149,38 @@ export default class baseTabs extends elementorModules.frontend.handlers.Base {
 	bindEvents() {
 		this.elements.$tabTitles.on( {
 			keydown: ( event ) => {
-				if ( 'Enter' === event.key ) {
-					event.preventDefault();
+				const key = event.keyCode,
+					keys = this.getSettings( 'keys' );
 
-					this.changeActiveTab( event.currentTarget.getAttribute( 'data-tab' ) );
+				switch ( key ) {
+					case keys.end:
+					case keys.home:
+					// Up and down are in keydown
+					// because we need to prevent page scroll >:)
+					case keys.up:
+					case keys.down:
+						this.handleKeyboardArrows( event );
+						break;
+				}
+			},
+			keyup: ( event ) => {
+				const key = event.keyCode,
+					keys = this.getSettings( 'keys' );
+
+				switch ( key ) {
+					case keys.left:
+					case keys.right:
+						this.handleKeyboardArrows( event );
+						break;
+					case keys.enter:
+					case keys.space:
+						event.preventDefault();
+						this.changeActiveTab( event.currentTarget.getAttribute( 'data-tab' ) );
+						break;
 				}
 			},
 			click: ( event ) => {
 				event.preventDefault();
-
 				this.changeActiveTab( event.currentTarget.getAttribute( 'data-tab' ) );
 			},
 		} );
