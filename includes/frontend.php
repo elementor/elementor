6d106ede6b05f25bf8e3a7c74d9ca8b3dec12b8b
rotemee <61740845+rotemee@users.noreply.github.com>
2020-12-06 16:46:26 +0200

    Tweak/dynamic import swiper - ED-447 (#13006)
    * Webpack: Tweak - Updating webpack to version 5 and grunt-webpack to version 4.
    
    * Dynamic Imports Infrastructure: Adding dynamic imports config to webpack, creating new defaultConfig that runs on the default process of webpack to generate the also the dev assets and not just the .min files.
    
    * Frontend JS File: Adding dynamic imports support to the main entry point of the frontend, allowing to define the assets loading path on the fly.
    
    * Widgets Handlers: Separating the static widgets handlers to a separate file that can be loaded on demand and overwrite the dynamic handlers that exist in the code by default.
    
    * Frontend PHP: Calling the runtime.js file when the frontend assets are loaded.
    
    * Settings: Adding the option to enable/disable the optimized JS mode from the admin.
    
    * Fixing Lint errors.
    
    * Webpack: Fix - Minified files are not created for te frontend entries.
    
    * Reorganizing code.
    
    * Elements And Widgets Handlers: Tweak - The current getHandlers method is not semantically current, and the option to get all handlers at once does not make sense.
    
    * Swiper Utility: Tweak - Swiper library was loaded by default event when not needed.
    
    * Image Carousel and Background Slideshow Functionality: Tweak - Were using the Swiper library when it was loaded by default.
    
    * Frontend Assets Loading: Tweak - Swiper library should be loaded by default when the optimized JS mode is not enabled.
    
    * Elements And Widgets Handlers: Tweak - The current getHandlers method is not semantically current, and the option to get all handlers at once does not make sense.
    
    * Elements And Widgets Handlers: Tweak - The detection if the handler is class or not, was not working properly.
    
    * Elements And Widgets Handlers: Tweak - The detection if the handler is class or not, was not working properly.
    
    * Elementor Frontend Config: Tweak - Adding indication for optimized js mode state.
    
    * Swiper Utility: Tweak - Swiper library was loaded even when optimized js mode is disabled.
    
    * Webpack: Tweak - Production files didn't work properly.
    
    * Webpack: Tweak - Production files didn't work properly.
    
    * Webpack: Tweak - Making the frontend.js file to be dependent on the frontend-modules.js to reduce the size of the frontend.js file, and changing webpack runtime code file to webpack.runtime.
    
    * Webpack: Tweak - Updating the "terser-webpack-plugin" that was causing deprecations warnings in the build process.
    
    * Webpack: Tweak - Adding a custom plugin for removing chunks files before build process begins.
    
    * Webpack: Tweak - Adding a custom plugin for removing chunks files before build process begins.
    
    * Elements Handlers: Tweak - Updating files names.
    
    * Webpack Remove Chunks Plugin: Fix - Lint issues.
    
    * Admin Settings: Tweak - Updating the optimized JS mode description.
    
    * Tweak - Code syntax adjustments.
    
    * Lightbox Utility: Fix - Swiper is not working when not using the global swiper instance that is part of the elementorFrontend object.

--- includes/frontend.php
+++ includes/frontend.php
@@ -391,14 +391,6 @@ class Frontend extends App {
 			true
 		);
 
-		wp_register_script(
-			'swiper',
-			$this->get_js_assets_url( 'swiper', 'assets/lib/swiper/' ),
-			[],
-			'5.3.6',
-			true
-		);
-
 		/**
 		 * @deprecated since 2.7.0 Use Swiper instead
 		 */
@@ -445,13 +437,7 @@ class Frontend extends App {
 		wp_register_script(
 			'elementor-frontend',
 			$this->get_js_assets_url( 'frontend' ),
-			[
-				'elementor-frontend-modules',
-				'elementor-dialog',
-				'elementor-waypoints',
-				'swiper',
-				'share-link',
-			],
+			$this->get_elementor_frontend_dependencies(),
 			ELEMENTOR_VERSION,
 			true
 		);
@@ -594,7 +580,7 @@ class Frontend extends App {
 
 		wp_enqueue_script( 'elementor-frontend' );
 
-		if ( 'enabled' !== get_option( 'elementor_optimized_js_loading' ) ) {
+		if ( ! $this->is_optimized_js_mode() ) {
 			wp_enqueue_script(
 				'preloaded-elements-handlers',
 				$this->get_js_assets_url( 'preloaded-elements-handlers', 'assets/js/' ),
@@ -1178,6 +1164,8 @@ class Frontend extends App {
 			'environmentMode' => [
 				'edit' => $is_preview_mode,
 				'wpPreview' => is_preview(),
+				'isScriptDebug' => Utils::is_script_debug(),
+				'isOptimizedJS' => $this->is_optimized_js_mode(),
 			],
 			'i18n' => [
 				'shareOnFacebook' => __( 'Share on Facebook', 'elementor' ),
@@ -1308,5 +1296,32 @@ class Frontend extends App {
 		$more_link = apply_filters( 'the_content_more_link', sprintf( ' <a href="%s#more-%s" class="more-link elementor-more-link">%s</a>', get_permalink(), $post->ID, $more_link_text ), $more_link_text );
 
 		return force_balance_tags( $parts['main'] ) . $more_link;
+	}
+
+	private function is_optimized_js_mode() {
+		return 'enabled' === get_option( 'elementor_optimized_js_loading' );
+	}
+
+	private function get_elementor_frontend_dependencies() {
+		$dependencies = [
+			'elementor-frontend-modules',
+			'elementor-dialog',
+			'elementor-waypoints',
+			'share-link',
+		];
+
+		if ( ! $this->is_optimized_js_mode() ) {
+			wp_register_script(
+				'swiper',
+				$this->get_js_assets_url( 'swiper', 'assets/lib/swiper/' ),
+				[],
+				'5.3.6',
+				true
+			);
+
+			$dependencies[] = 'swiper';
+		}
+
+		return $dependencies;
 	}
 }
