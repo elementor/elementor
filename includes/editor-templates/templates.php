72bb8fcd3f01ca39fadf3f7eefb293b75877b88a
udidol <udi.dollberg@gmail.com>
2020-12-28 10:48:02 +0200

    Landing Pages: Fixes - Replaced the '_elementor_template_type' string with existing constant. | CSS and templating fixes to make the Landing Pages tab identical to the Pages tab. | Fixed folder structure for landing pages library hook to match the command name.

--- includes/editor-templates/templates.php
+++ includes/editor-templates/templates.php
@@ -136,7 +136,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 <script type="text/template" id="tmpl-elementor-template-library-template-remote">
 	<div class="elementor-template-library-template-body">
-		<# if ( 'page' === type || 'landing-page' === type ) { #>
+		<# if ( 'page' === type || 'lp' === type ) { #>
 			<div class="elementor-template-library-template-screenshot" style="background-image: url({{ thumbnail }});"></div>
 		<# } else { #>
 			<img src="{{ thumbnail }}">
