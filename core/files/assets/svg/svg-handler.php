cbe1a35666d48e0e89b2be3f17cdfa56f255da7e
Yakir Sitbon <kingyes1@gmail.com>
2020-12-28 22:49:46 +0200

    Apply suggestions from code review

--- core/files/assets/svg/svg-handler.php
+++ core/files/assets/svg/svg-handler.php
@@ -510,7 +510,7 @@ class Svg_Handler extends Files_Upload_Handler {
 		// If the server's PHP version is 8 or up, make sure to Disable the ability to load external entities
 		$php_version_under_eight = version_compare( PHP_VERSION, '8.0.0', '<' );
 		if ( $php_version_under_eight ) {
-			$libxml_disable_entity_loader = libxml_disable_entity_loader( true ); // phpcs:ignore PHPCompatibility.FunctionUse.RemovedFunctions.libxml_disable_entity_loaderDeprecated
+			$libxml_disable_entity_loader = libxml_disable_entity_loader( true ); // phpcs:ignore Generic.PHP.DeprecatedFunctions.Deprecated
 		}
 		// Suppress the errors
 		$libxml_use_internal_errors = libxml_use_internal_errors( true );
@@ -535,7 +535,7 @@ class Svg_Handler extends Files_Upload_Handler {
 
 		// Restore defaults
 		if ( $php_version_under_eight ) {
-			libxml_disable_entity_loader( $libxml_disable_entity_loader ); // phpcs:ignore PHPCompatibility.FunctionUse.RemovedFunctions.libxml_disable_entity_loaderDeprecated
+			libxml_disable_entity_loader( $libxml_disable_entity_loader ); // phpcs:ignore Generic.PHP.DeprecatedFunctions.Deprecated
 		}
 		libxml_use_internal_errors( $libxml_use_internal_errors );
 
