da87298fc9bdff2c31b583a7b0693109017edbe2
Josh Marom <josh@elementor.com>
2021-01-13 10:43:41 +0200

    Progress with top bar UI

--- assets/dev/js/editor/top-bar.js
+++ assets/dev/js/editor/top-bar.js
@@ -22,6 +22,35 @@ export default class TopBar extends Marionette.ItemView {
 		};
 	}
 
+	ui() {
+		return {
+			addNewSection: '.elementor-add-new-section',
+			closeButton: '.elementor-add-section-close',
+			addSectionButton: '.elementor-add-section-button',
+			addTemplateButton: '.elementor-add-template-button',
+			selectPreset: '.elementor-select-preset',
+			presets: '.elementor-preset',
+		};
+	}
+
+	events() {
+		return {
+			'click @ui.addSectionButton': 'onAddSectionButtonClick',
+			'click @ui.addTemplateButton': 'onAddTemplateButtonClick',
+			'click @ui.closeButton': 'onCloseButtonClick',
+			'click @ui.presets': 'onPresetSelected',
+		};
+	}
+
+	behaviors() {
+		return {
+			contextMenu: {
+				behaviorClass: require( 'elementor-behaviors/context-menu' ),
+				groups: this.getContextMenuGroups(),
+			},
+		};
+	}
+
 	onClick() {
 		const clickCallback = this.getOption( 'click' );
 
--- includes/editor-templates/responsive-top-bar.php
+++ includes/editor-templates/responsive-top-bar.php
@@ -7,5 +7,28 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 ?>
 <script type="text/template" id="tmpl-elementor-templates-responsive-top-bar">
-	<div><h1>Hello World</h1></div>
+	<aside class="e-mq-bar">
+		<div class="e-mq-bar__col">
+
+		</div>
+		<div class="e-mq-bar__col">
+			<div class="e-mq-switcher">
+				<input class="e-mq-switcher__option" type="radio" value="mobile" name="breakpoint" id="e-mq-switch-mobile">
+				<label for="e-mq-switch-mobile"><?php echo __('Mobile', 'elementor') ?></label>
+
+				<input class="e-mq-switcher__option" type="radio" value="tablet" name="breakpoint" id="e-mq-switch-tablet">
+				<label for="e-mq-switch-tablet"><?php echo __('Tablet', 'elementor') ?></label>
+
+				<input class="e-mq-switcher__option" type="radio" value="desktop" name="breakpoint" id="e-mq-switch-desktop">
+				<label for="e-mq-switch-desktop"><?php echo __('Desktop', 'elementor') ?></label>
+			</div>
+		</div>
+		<div class="e-mq-bar__col">
+
+			<button class="e-mq-bar__close">
+				<span class="elementor-screen-only">Close</span>
+				<i class="eicon-close" aria-hidden="true"></i>
+			</button>
+		</div>
+	</aside>
 </script>
