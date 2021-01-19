5587784a8ba9cf8756f39f829c5181f69f5d15e3
Josh Marom <josh@elementor.com>
2021-01-19 19:00:02 +0200

    Handle top-bar events and interactions

--- assets/dev/js/editor/top-bar.js
+++ assets/dev/js/editor/top-bar.js
@@ -29,20 +29,53 @@ export default class TopBar extends Marionette.ItemView {
 			switcherOption: prefix + 'switcher__option',
 			switcher: prefix + 'switcher',
 			closeButton: prefix + 'bar__close-button',
-			optionsMenuToggle: prefix + 'bar__options-button',
+			breakpointSettingsButton: prefix + 'bar__settings-button',
 		};
 	}
 
 	events() {
 		return {
+			click: 'onClick',
 			'change @ui.switcherOption': 'onBreakpointSelected',
 			'click @ui.closeButton': 'onCloseButtonClick',
+			'click @ui.breakpointSettingsButton': 'onBreakpointSettingsOpen',
 		};
 	}
 
+	initialize() {
+		this.listenTo( elementor.channels.deviceMode, 'change', this.onDeviceModeChange );
+	}
+
+	onDeviceModeChange() {
+		const currentDeviceMode = elementor.channels.deviceMode.request( 'currentMode' ),
+			$currentDeviceSwitcherOption = this.ui.switcherOption.filter( '[value=' + currentDeviceMode + ']' );
+
+		if ( ! $currentDeviceSwitcherOption.prop( 'checked' ) ) {
+			$currentDeviceSwitcherOption.prop( 'checked', true );
+		}
+	}
+
 	onBreakpointSelected( e ) {
-		const selectedBreakpoint = jQuery( e.target ).attr( 'value' );
-		elementor.changeDeviceMode( selectedBreakpoint );
+		const currentDeviceMode = elementor.channels.deviceMode.request( 'currentMode' ),
+			selectedDeviceMode = e.target.value;
+
+		if ( currentDeviceMode !== selectedDeviceMode ) {
+			elementor.changeDeviceMode( selectedDeviceMode );
+		}
+	}
+
+	onBreakpointSettingsOpen() {
+		$e.run( 'editor/documents/switch', {
+			id: elementor.config.kit_id,
+			mode: 'autosave',
+		} )
+			.then( () => $e.route( 'panel/global/settings-layout' ) )
+			// TODO: Replace with a standard routing solution once one is available
+			.then( () => jQuery( '.elementor-control-section_breakpoints' ).trigger( 'click' ) );
+	}
+
+	onCloseButtonClick() {
+		elementor.exitDeviceMode();
 	}
 
 	onChange() {
