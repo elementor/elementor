d8a9465817bb47d9f57d4db796d133036574dddf
Rotem Ben-Itzhak <rotem@elementor.com>
2021-01-19 11:40:05 +0200

    Import: Tweak - Implementig Dialog component.

--- core/app/modules/import-export/assts/js/pages/import/import.js
+++ core/app/modules/import-export/assts/js/pages/import/import.js
@@ -4,6 +4,7 @@ import Layout from '../../templates/layout';
 import Notice from '../../ui/notice/notice';
 import Title from '../../ui/title/title';
 import DropZone from 'elementor-app/organisms/drop-zone';
+import Dialog from 'elementor-app/ui/dialog/dialog';
 
 import useFile from '../../hooks/use-file/use-file';
 
@@ -11,16 +12,31 @@ import './import.scss';
 
 export default function Import() {
 	const [ isLoading, setLoading ] = useState( false ),
+		[ isImportFailed, setIsImportFailed ] = useState( false ),
 		{ setFile } = useFile(),
 		onFileSelect = ( files ) => {
 			setFile( files[ 0 ] );
+			setIsImportFailed( false );
 		};
+
+	const getDialog = () => (
+		<Dialog
+			title={ __( 'Import Failed', 'elementor' ) }
+			text={ __( 'We are sorry, but some problem accrued. Please try again. If the problem continues, contact our support.', 'elementor' ) }
+			approveButtonUrl="https://go.elementor.com/app-theme-builder-load-issue"
+			approveButtonColor="danger"
+			approveButtonTarget="_blank"
+			approveButtonText={ __( 'Exit', 'elementor' ) }
+			dismissButtonText={ __( 'Cancel', 'elementor' ) }
+		/>
+	);
 
 	useEffect( () => {
 		if ( isLoading ) {
 			setTimeout( () => {
 				setLoading( false );
-			}, 5000 );
+				setIsImportFailed( true );
+			}, 1000 );
 		}
 	}, [ isLoading ] );
 
@@ -32,8 +48,8 @@ export default function Import() {
 					secondary={ __( 'Upload the kit file.You will be able to see the Kit’s content at the end of the process.', 'elementor' ) }
 				/>
 
-				<Notice color="warning" className="e-app-import__notice">
-					{ __( 'Important: It is strongly recommended that you backup your database before Importing a Kit.', 'elementor' ) }
+				<Notice label={ __( 'Important', 'elementor' ) } color="warning" className="e-app-import__notice">
+					{ __( 'It is strongly recommended that you backup your database before Importing a Kit.', 'elementor' ) }
 				</Notice>
 
 				<DropZone
@@ -48,6 +64,7 @@ export default function Import() {
 					} }
 				/>
 			</section>
+			{ isImportFailed && getDialog() }
 		</Layout>
 	);
 }
