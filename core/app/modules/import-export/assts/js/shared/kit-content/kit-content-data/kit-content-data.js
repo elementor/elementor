58fe62a1b16fe60e88d6bc1c89652a867f5b7f40
Rotem Ben-Itzhak <rotem@elementor.com>
2021-01-19 11:40:22 +0200

    Import Export: Tweak - Updating Kit Content Data.

--- core/app/modules/import-export/assts/js/shared/kit-content/kit-content-data/kit-content-data.js
+++ core/app/modules/import-export/assts/js/shared/kit-content/kit-content-data/kit-content-data.js
@@ -2,34 +2,52 @@ const kitContentData = [
 	{
 		type: 'templates',
 		data: {
-			title: __( 'Global Templates', 'elementor' ),
+			title: __( 'Templates', 'elementor' ),
 			features: {
 				open: [
-					__( 'Saved Templates', 'elementor' ),
+					__( 'Landing Pages', 'elementor' ),
+					__( 'Saved Templates (Sections, Page)', 'elementor' ),
 				],
 				locked: [
-					__( 'Site Parts', 'elementor' ),
 					__( 'Popups', 'elementor' ),
-					__( 'Global Widgets', 'elementor' ),
 				],
 			},
 			notice: __( 'Site Parts, Global widgets and Popups will are available only when Elementor Pro license is Connected', 'elementor' ),
 		},
 	},
 	{
-		type: 'settings',
+		type: 'theme-builder',
 		data: {
-			title: __( 'Global Styles And Settings', 'elementor' ),
-			description: __( 'Theme Style, Global Colors and Typography, Layout, Lightbox and Site Identity settings', 'elementor' ),
+			title: __( 'Theme builder', 'elementor' ),
+			features: {
+				open: [],
+				locked: [
+					__( 'Headers', 'elementor' ),
+					__( 'Footers', 'elementor' ),
+					__( 'Archives', 'elementor' ),
+					__( 'Single Posts', 'elementor' ),
+					__( 'Single Pages', 'elementor' ),
+					__( 'Search Results', 'elementor' ),
+					__( '404 Error Page', 'elementor' ),
+				],
+			},
+			notice: __( 'Site Parts, Global widgets and Popups will are available only when Elementor Pro license is Connected', 'elementor' ),
 		},
 	},
 	{
 		type: 'content',
 		data: {
 			title: __( 'Content', 'elementor' ),
-			description: __( 'Published pages, posts, related taxonomies, menu and custom post types.', 'elementor' ),
+			description: __( 'Elementor Pages, Custom Post Type', 'elementor' ),
 		},
 		hasSelect: true,
+	},
+	{
+		type: 'settings',
+		data: {
+			title: __( 'Site Settings', 'elementor' ),
+			description: __( 'Global Colors, Global  Typography, Theme Style settings, Layout Settings, Lightbox Settings, Background Settings', 'elementor' ),
+		},
 	},
 ];
 
