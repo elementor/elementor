ef3d6995144aa45fe6086ca8b8377bafa5207a4a
Etay Frank <etayf@elementor.com>
2021-01-25 18:49:33 +0200

    Favorite Widgets Section: Changed - add condition to add.js route (ED1501)

--- modules/widgets-panel/assets/js/editor/commands/add.js
+++ modules/widgets-panel/assets/js/editor/commands/add.js
@@ -11,7 +11,10 @@ export class Add extends CommandBase {
 		const result = await $e.data.create( `panel-favorites/favorites?id=${ widget }`, {} );
 		if ( result.data ) {
 			// Get the categories of widget from front-end
-			const widgetsArr = elementor.widgetsCache[ widget ].categories;
+			const widgetsArr = elementor.widgetsCache[ `${ widget }` ].categories;
+			if ( 'undefined' === typeof widgetsArr ) {
+				widgetsArr.categories = [];
+			}
 			// Get element position in array
 			const elmPos = widgetsArr.indexOf( 'favorites' );
 			// Check - if element in array do nothing, else add it to array
