55b4112707d541480aa01667dca39dc631aa6968
Rotem Ben-Itzhak <rotem@elementor.com>
2021-02-04 10:23:28 +0200

    Export: Tweak - Adding LearnMore link to page header.

--- core/app/modules/import-export/assets/js/pages/export/export-kit/export-kit.js
+++ core/app/modules/import-export/assets/js/pages/export/export-kit/export-kit.js
@@ -1,5 +1,6 @@
 import Layout from '../../../templates/layout';
 import PageHeader from '../../../ui/page-header/page-header';
+import InfoLink from '../../../ui/info-link/info-link';
 import ExportButton from './components/export-button/export-button';
 import KitContent from './components/kit-content/kit-content';
 import WizardFooter from 'elementor-app/organisms/wizard-footer';
@@ -9,6 +10,12 @@ export default function ExportKit() {
 			<WizardFooter separator justify="end">
 				<ExportButton />
 			</WizardFooter>
+		),
+		getLearnMoreLink = () => (
+			<InfoLink
+				text={ __( 'Learn More', 'elementor' ) }
+				url="https://elementor.com/help/what-are-kits?utm_source=editor-app&utm_medium=wp-dash&utm_campaign=learn"
+			/>
 		);
 
 	return (
@@ -18,7 +25,9 @@ export default function ExportKit() {
 					heading={ __( 'Choose What To Include In The Kit', 'elementor' ) }
 					description={ [
 						__( 'Choose the kit components to export, such as pages, site setting, headers and more.', 'elementor' ),
-						__( 'By default, we will export all the components.', 'elementor' ),
+						<>
+							{ __( 'By default, we will export all the components.', 'elementor' ) } { getLearnMoreLink() }
+						</>,
 					] }
 				/>
 
