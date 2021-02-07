7dc1978c3444c061570bb2038ad1bafd38cfee92
Leonid <leonid@elementor.com>
2021-02-07 19:24:05 +0200

    Document/Elements/Create: Fix - Invalid structure for section with index '0'. (R1)
    Test.

--- tests/qunit/tests/assets/dev/js/editor/document/hooks/data/document/elements/create/section-columns.spec.js
+++ tests/qunit/tests/assets/dev/js/editor/document/hooks/data/document/elements/create/section-columns.spec.js
@@ -9,6 +9,19 @@ export const SectionColumns = () => {
 			assert.equal( eSection.settings.get( 'structure' ), structure );
 		} );
 
+		QUnit.test( 'create section at index 0 - ensure options does not forward to columns', ( assert ) => {
+			// Resolve issue: ED-1903.
+			// Arrange.
+			const eSectionInitial = ElementsHelper.createSectionStructure( 3, '32' ),
+				eSectionAt0 = ElementsHelper.createSectionStructure( 3, '31', false, {
+					at: 0,
+				} );
+
+			// Assert.
+			assert.equal( eSectionInitial.children[ 0 ].settings.get( '_column_size' ), 50 );
+			assert.equal( eSectionAt0.children[ 0 ].settings.get( '_column_size' ), 25 );
+		} );
+
 		// TODO: Create section with two columns validate both _column_size is 50,
 		// Redo section, validate again if both columns _column_size is 50.
 
