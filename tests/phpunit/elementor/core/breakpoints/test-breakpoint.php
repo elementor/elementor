6e204f99512943f6e8399598de4307a071b8c3f5
udidol <udi.dollberg@gmail.com>
2021-02-09 13:45:55 +0200

    Breakpoints: Added Unit Tests for the Breakpoints Manager and Breakpoint classes.

---
+++ tests/phpunit/elementor/core/breakpoints/test-breakpoint.php
@@ -0,0 +1,151 @@
+<?php
+namespace Elementor\Tests\Phpunit\Elementor\Core\Breakpoints;
+
+use Elementor\Core\Breakpoints\Breakpoint;
+use Elementor\Core\Breakpoints\Manager as Breakpoints_Manager;
+use Elementor\Plugin;
+use Elementor\Testing\Elementor_Test_Base;
+use Elementor\Testing\Traits\Extra_Assertions;
+
+class Test_Breakpoint extends Elementor_Test_Base {
+
+	use Extra_Assertions;
+
+	/**
+	 * Test Get Name
+	 *
+	 * Tests Breakpoint::get_name() for all breakpoints.
+	 *
+	 * @since 3.2.0
+	 */
+	public function test_get_name() {
+		$breakpoints = Plugin::$instance->breakpoints->get_breakpoints();
+
+		foreach ( $breakpoints as $breakpoint_name => $breakpoint ) {
+			/** @var Breakpoint $breakpoint */
+			$this->assertEquals( $breakpoint_name, $breakpoint->get_name() );
+		}
+	}
+
+	/**
+	 * Test Get Config
+	 *
+	 * Tests Breakpoint::get_config() for all breakpoints.
+	 *
+	 * @since 3.2.0
+	 */
+	public function test_get_config() {
+		$breakpoints = Plugin::$instance->breakpoints->get_breakpoints();
+
+		foreach ( $breakpoints as $breakpoint ) {
+			/** @var Breakpoint $breakpoint */
+			$breakpoint_config = $breakpoint->get_config();
+
+			$this->assertArrayHaveKeys( [ 'value', 'direction', 'is_enabled', 'selector' ], $breakpoint_config );
+			$this->assertIsNumeric( $breakpoint_config['value'] );
+			$this->assertIsString( $breakpoint_config['direction'] );
+			$this->assertIsString( $breakpoint_config['selector'] );
+			$this->assertIsBool( $breakpoint_config['is_enabled'] );
+		}
+	}
+
+	/**
+	 * Test Is Enabled
+	 *
+	 * Tests Breakpoint::get_config() for all breakpoints.
+	 *
+	 * @since 3.2.0
+	 */
+	public function test_is_enabled() {
+		$breakpoints = Plugin::$instance->breakpoints->get_breakpoints();
+		$active_breakpoint_names = array_keys( Plugin::$instance->breakpoints->get_config() );
+
+		foreach ( $breakpoints as $breakpoint ) {
+			/** @var Breakpoint $breakpoint */
+			if ( $breakpoint->is_enabled() ) {
+				// Check if the enabled breakpoint is in the array of active breakpoint names, as returned from the
+				// Breakpoints Manager's `get_config()` method.
+				$this->assertContains( $breakpoint->get_name(), $active_breakpoint_names );
+			} else {
+				$this->assertNotContains( $breakpoint->get_name(), $active_breakpoint_names );
+			}
+		}
+	}
+
+	/**
+	 * Test Get Value
+	 *
+	 * Tests Breakpoint::get_value() for all breakpoints.
+	 *
+	 * @since 3.2.0
+	 */
+	public function test_get_value() {
+		$breakpoints = Plugin::$instance->breakpoints->get_breakpoints();
+		$breakpoints_default_config = Breakpoints_Manager::get_default_config();
+
+		foreach ( $breakpoints as $breakpoint_name => $breakpoint ) {
+			/** @var Breakpoint $breakpoint */
+			$value_from_db = Plugin::$instance->kits_manager->get_current_settings( Breakpoints_Manager::BREAKPOINT_OPTION_PREFIX . $breakpoint_name );
+
+			if ( $value_from_db ) {
+				$this->assertEquals( $value_from_db, $breakpoint->get_value() );
+			} else {
+				$this->assertEquals( $breakpoints_default_config[ $breakpoint_name ]['default_value'], $breakpoint->get_value() );
+			}
+		}
+	}
+
+	/**
+	 * Test Is Custom
+	 *
+	 * Tests Breakpoint::is_custom() for all breakpoints.
+	 *
+	 * @since 3.2.0
+	 */
+	public function test_is_custom() {
+		$breakpoints = Plugin::$instance->breakpoints->get_breakpoints();
+		$breakpoints_default_config = Breakpoints_Manager::get_default_config();
+
+		foreach ( $breakpoints as $breakpoint_name => $breakpoint ) {
+			/** @var Breakpoint $breakpoint */
+			$value = $breakpoint->get_value();
+			$default_value = $breakpoints_default_config[ $breakpoint_name ]['default_value'];
+
+			$this->assertEquals( $value !== $default_value, $breakpoint->is_custom(), $breakpoint_name );
+		}
+	}
+
+	/**
+	 * Test Get Default Value
+	 *
+	 * Tests Breakpoint::get_default_value() for all breakpoints.
+	 *
+	 * @since 3.2.0
+	 */
+	public function test_get_default_value() {
+		$breakpoints = Plugin::$instance->breakpoints->get_breakpoints();
+		$breakpoints_default_config = Breakpoints_Manager::get_default_config();
+
+		foreach ( $breakpoints as $breakpoint_name => $breakpoint ) {
+			/** @var Breakpoint $breakpoint */
+			$this->assertEquals( $breakpoints_default_config[ $breakpoint_name ]['default_value'], $breakpoint->get_default_value() );
+		}
+	}
+
+	/**
+	 * Test Get Direction
+	 *
+	 * Tests Breakpoint::get_direction() for all breakpoints.
+	 *
+	 * @since 3.2.0
+	 */
+	public function test_get_direction() {
+		$breakpoints = Plugin::$instance->breakpoints->get_breakpoints();
+		$breakpoints_default_config = Breakpoints_Manager::get_default_config();
+
+		foreach ( $breakpoints as $breakpoint_name => $breakpoint ) {
+			/** @var Breakpoint $breakpoint */
+			$this->assertEquals( $breakpoints_default_config[ $breakpoint_name ]['direction'], $breakpoint->get_direction() );
+		}
+	}
+}
---
+++ tests/phpunit/elementor/core/breakpoints/test-manager.php
@@ -0,0 +1,111 @@
+<?php
+namespace Elementor\Tests\Phpunit\Elementor\Core\Breakpoints;
+
+use Elementor\Core\Breakpoints\Breakpoint;
+use Elementor\Core\Breakpoints\Manager as Breakpoints_Manager;
+use Elementor\Plugin;
+use Elementor\Testing\Elementor_Test_Base;
+
+class Test_Manager extends Elementor_Test_Base {
+
+	/**
+	 * Test Get Name
+	 *
+	 * Test the Breakpoints_Manager::get_name() method.
+	 *
+	 * @since 3.2.0
+	 */
+	public function test_get_name() {
+		$this->assertEquals( 'breakpoints', Plugin::$instance->breakpoints->get_name() );
+	}
+
+	/**
+	 * Test Get Breakpoints
+	 *
+	 * Test the Breakpoints_Manager::get_breakpoints() method.
+	 *
+	 * @since 3.2.0
+	 */
+	public function test_get_breakpoints() {
+		$breakpoints = Plugin::$instance->breakpoints->get_breakpoints();
+		$all_breakpoint_names = array_keys( Breakpoints_Manager::get_default_config() );
+
+		$this->assertIsArray( $breakpoints );
+
+		foreach ( $breakpoints as $breakpoint_name => $breakpoint ) {
+			/** @var Breakpoint $breakpoint */
+			// Check that the items in the breakpoints array are actual breakpoints.
+			$this->assertTrue( $breakpoint instanceof Breakpoint );
+
+			// Check that the generated breakpoint is one of the breakpoints in the default config array.
+			$this->assertContains( $breakpoint_name, $all_breakpoint_names );
+		}
+	}
+
+	/**
+	 * Test Get Config
+	 *
+	 * Test the Breakpoints_Manager::get_config() method.
+	 *
+	 * @since 3.2.0
+	 */
+	public function test_get_config() {
+		$breakpoints = Plugin::$instance->breakpoints->get_breakpoints();
+		$config = Plugin::$instance->breakpoints->get_config();
+
+		// Create an array and populate it with the keys of only enabled breakpoints.
+		$enabled_breakpoint_keys = [];
+
+		foreach ( $breakpoints as $breakpoint_name => $breakpoint ) {
+			/** @var Breakpoint $breakpoint */
+			if ( $breakpoint->is_enabled() ) {
+				$enabled_breakpoint_keys[] = $breakpoint->get_name();
+
+				// This checks both that the config item is an array and that it contains the correct keys.
+				$this->assertArrayHaveKeys( [ 'value', 'direction', 'is_enabled', 'selector' ], $config[ $breakpoint_name ] );
+			}
+		}
+
+		$this->assertEquals( $enabled_breakpoint_keys, array_keys( $config ) );
+	}
+
+	/**
+	 * Test Has Custom Breakpoints
+	 *
+	 * Test the Breakpoints_Manager::has_custom_breakpoints() method.
+	 *
+	 * @since 3.2.0
+	 */
+	public function test_has_custom_breakpoints() {
+		$config = Plugin::$instance->breakpoints->get_config();
+		$breakpoints_default_config = Breakpoints_Manager::get_default_config();
+
+		$has_custom_breakpoints = false;
+
+		foreach ( $config as $breakpoint_name => $breakpoint ) {
+			if ( $breakpoints_default_config[ $breakpoint_name ]['default_value'] !== $breakpoint['value'] ) {
+				$has_custom_breakpoints = true;
+				break;
+			}
+		}
+
+		$this->assertEquals( $has_custom_breakpoints, Plugin::$instance->breakpoints->has_custom_breakpoints() );
+	}
+
+	/**
+	 * Test Get Device Min Breakpoint
+	 *
+	 * Test the Breakpoints_Manager::get_device_min_breakpoint() method.
+	 * This test only tests the default breakpoints (mobile, tablet).
+	 *
+	 * @since 3.2.0
+	 */
+	public function test_get_device_min_breakpoint() {
+		$config = Plugin::$instance->breakpoints->get_config();
+
+		// Test for mobile specifically, which always has a min point of 0.
+		$this->assertEquals( 0, Plugin::$instance->breakpoints->get_device_min_breakpoint( Breakpoints_Manager::BREAKPOINT_KEY_MOBILE ) );
+
+		$this->assertEquals( $config[ Breakpoints_Manager::BREAKPOINT_KEY_MOBILE ]['value'] + 1, Plugin::$instance->breakpoints->get_device_min_breakpoint( Breakpoints_Manager::BREAKPOINT_KEY_TABLET ) );
+	}
+}
