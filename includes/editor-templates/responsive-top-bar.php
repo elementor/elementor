ec221c4349553a97c7b90bddd9865c1697a5ef42
Josh Marom <josh@elementor.com>
2021-02-11 18:43:02 +0200

    Fix Tipsy

--- includes/editor-templates/responsive-top-bar.php
+++ includes/editor-templates/responsive-top-bar.php
@@ -27,7 +27,7 @@ $breakpoints = [
 							class="e-mq-switcher__option e-mq-switcher__option-%1$s"
 							id="e-mq-switch-%1$s"
 							value="%1$s">
-					<label class="e-mq-switcher__label" for="e-mq-switch-%1$s">
+					<label class="e-mq-switcher__label" for="e-mq-switch-%1$s" data-tooltip="%2$s">
 						<i class="eicon-device-%1$s" aria-hidden="true"></i>
 						<span class="screen-reader-text">%2$s</span>
 					</label>', $name, $label ); } ?>
