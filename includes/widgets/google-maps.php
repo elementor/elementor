f55f40f20e3454c03fac52ba17380033c5d7a74c
Etay Frank <etayf@elementor.com>
2021-02-17 12:26:57 +0200

    Google Maps widget: Change - Add more height units options (ED-2112)

--- includes/widgets/google-maps.php
+++ includes/widgets/google-maps.php
@@ -150,7 +150,16 @@ class Widget_Google_Maps extends Widget_Base {
 						'min' => 40,
 						'max' => 1440,
 					],
+					'vh' => [
+						'min' => 0,
+						'max' => 100,
+					],
+					'vw' => [
+						'min' => 0,
+						'max' => 100,
+					],
 				],
+				'size_units' => [ 'px', 'vh', 'vw' ],
 				'selectors' => [
 					'{{WRAPPER}} iframe' => 'height: {{SIZE}}{{UNIT}};',
 				],
