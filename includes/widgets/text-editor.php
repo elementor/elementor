d6a6c304c5ec3f8796eac495d7315c9237a58be3
Rodolphe Bertozzo <56270626+rodolphebertozzo@users.noreply.github.com>
2021-02-20 22:42:58 +0100

    dom optimization

--- includes/widgets/text-editor.php
+++ includes/widgets/text-editor.php
@@ -241,7 +241,7 @@ class Widget_Text_Editor extends Widget_Base {
 					],
 				],
 				'selectors' => [
-					'{{WRAPPER}} .elementor-text-editor' => 'max-width: {{SIZE}}{{UNIT}};',
+					'{{WRAPPER}}' => 'max-width: {{SIZE}}{{UNIT}};',
 				],
 			]
 		);
