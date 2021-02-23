d7d61b1187268287c2154199d394ba98332a9466
udidol <udi.dollberg@gmail.com>
2021-02-23 11:46:02 +0200

    Site Settings: Breakpoints - if the `active_breakpoints` control is updated, make sure to empty the setting before re-populating it. | Added comments and small tweaks, like replacing `indexOf` with `startsWith`,

--- core/kits/assets/js/hooks/ui/document/elements/settings/update-breakpoints-preview.js
+++ core/kits/assets/js/hooks/ui/document/elements/settings/update-breakpoints-preview.js
@@ -21,20 +21,24 @@ export class KitUpdateBreakpointsPreview extends $e.modules.hookUI.After {
 		// If the 'active_breakpoints' control was changed, we need to make sure that all of the breakpoints in the new
 		// setting are now set to active.
 		if ( settings.active_breakpoints ) {
+			// Clear the active breakpoints object before repopulating it, to make sure unselected breakpoints are removed.
+			elementorFrontend.config.responsive.activeBreakpoints = {};
+
 			settings.active_breakpoints.forEach( ( breakpointName ) => {
 				breakpointName = breakpointName.replace( 'viewport_', '' );
 				// Set its state to enabled.
 				elementorFrontend.config.responsive.breakpoints[ breakpointName ].is_enabled = true;
-				// If the breakpoint wasn't active before, add the breakpoint to the active breakpoints config object.
-				if ( ! elementorFrontend.config.responsive.activeBreakpoints[ breakpointName ] ) {
-					elementorFrontend.config.responsive.activeBreakpoints[ breakpointName ] = elementorFrontend.config.responsive.breakpoints[ breakpointName ];
-				}
+				// Add/re-add the breakpoint to the emptied activeBreakpoints object.
+				elementorFrontend.config.responsive.activeBreakpoints[ breakpointName ] = elementorFrontend.config.responsive.breakpoints[ breakpointName ];
 			} );
+
+			// If this is the modified setting, no need to do further checks.
+			return;
 		}
 
 		// If a breakpoint value was updated, update the value in the config.
 		Object.entries( settings ).forEach( ( [ key, value ] ) => {
-			if ( -1 !== key.indexOf( 'viewport_' ) ) {
+			if ( key.startsWith( 'viewport_' ) ) {
 				const keyWithoutPrefix = key.replace( 'viewport_', '' );
 				// Update both the config for all breakpoints and the one for active breakpoints.
 				elementorFrontend.config.responsive.breakpoints[ keyWithoutPrefix ].value = value;
