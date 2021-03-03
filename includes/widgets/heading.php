5b414fdb8f8c1d344c1f45b2afefe446a6be4d13
Mati Horovitz <21468434+matipojo@users.noreply.github.com>
2021-03-03 17:27:39 +0200

    Frontend: Fix - Sanitize HTML wrappers tags before render. (#14110)
    Replace the validation before saving with validation before rendering in order to cover existing sites that may have unsafe HTML.

--- includes/widgets/heading.php
+++ includes/widgets/heading.php
@@ -313,7 +313,7 @@ class Widget_Heading extends Widget_Base {
 			$title = sprintf( '<a %1$s>%2$s</a>', $this->get_render_attribute_string( 'url' ), $title );
 		}
 
-		$title_html = sprintf( '<%1$s %2$s>%3$s</%1$s>', $settings['header_size'], $this->get_render_attribute_string( 'title' ), $title );
+		$title_html = sprintf( '<%1$s %2$s>%3$s</%1$s>', Utils::validate_html_tag( $settings['header_size'] ), $this->get_render_attribute_string( 'title' ), $title );
 
 		echo $title_html;
 	}
--- includes/widgets/toggle.php
+++ includes/widgets/toggle.php
@@ -554,7 +554,7 @@ class Widget_Toggle extends Widget_Base {
 				$this->add_inline_editing_attributes( $tab_content_setting_key, 'advanced' );
 				?>
 				<div class="elementor-toggle-item">
-					<<?php echo esc_html( $settings['title_html_tag'] ); ?> <?php echo $this->get_render_attribute_string( $tab_title_setting_key ); ?>>
+					<<?php echo Utils::validate_html_tag( $settings['title_html_tag'] ); ?> <?php echo $this->get_render_attribute_string( $tab_title_setting_key ); ?>>
 						<?php if ( $has_icon ) : ?>
 						<span class="elementor-toggle-icon elementor-toggle-icon-<?php echo esc_attr( $settings['icon_align'] ); ?>" aria-hidden="true">
 							<?php
@@ -568,7 +568,7 @@ class Widget_Toggle extends Widget_Base {
 						</span>
 						<?php endif; ?>
 						<a href="" class="elementor-toggle-title"><?php echo $item['tab_title']; ?></a>
-					</<?php echo esc_html( $settings['title_html_tag'] ); ?>>
+					</<?php echo Utils::validate_html_tag( $settings['title_html_tag'] ); ?>>
 					<div <?php echo $this->get_render_attribute_string( $tab_content_setting_key ); ?>><?php echo $this->parse_text_editor( $item['tab_content'] ); ?></div>
 				</div>
 			<?php endforeach; ?>
