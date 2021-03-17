9875f715bf73fa882c9cb43841243d8517b6ab78
Evyatar Daud <evyatard@elementor.com>
2021-03-17 08:11:05 +0200

    Add comment.

--- modules/shapes/assets/js/frontend/handlers/text-path.js
+++ modules/shapes/assets/js/frontend/handlers/text-path.js
@@ -204,6 +204,7 @@ export default class TextPathHandler extends elementorModules.frontend.handlers.
 		let parentElement = this.elements.textPath;
 		parentElement = parentElement.querySelector( 'a' ) || parentElement;
 
+		// Catch all RTL chars and reverse their order.
 		const pattern = /([\u0591-\u07FF\u200F\u202B\u202E\uFB1D-\uFDFD\uFE70-\uFEFC\s$&+,:;=?@#|'<>.^*()%!-]+)/ig;
 
 		// Reverse the text.
