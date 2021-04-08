30a3eb82ec2f8ba74622a946a605923a66e32d44
Kobi Zaltzberg <kobi@elementor.com>
2021-04-08 15:46:07 +0300

    Uploads Manager: Change - Added some fixes and improvements

--- core/files/file-types/zip.php
+++ core/files/file-types/zip.php
@@ -65,7 +65,7 @@ class Zip extends Base {
 
 		$zip = new \ZipArchive();
 
-		$temp_extraction_directory = Plugin::$instance->uploads_manager->get_temp_dir() . uniqid();
+		$temp_extraction_directory = Plugin::$instance->uploads_manager->create_unique_temp_dir();
 
 		$zip->open( $file_path );
 
