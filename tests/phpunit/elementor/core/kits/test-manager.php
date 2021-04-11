f8df1bcff99705de466a75d48fb25738ee9e396e
Leonid <leonid@elementor.com>
2021-04-11 14:27:11 +0300

    Admin-Bar: Fix - Site Settings is positioned above of the separating border. (R1N1)
    ref: ED-2527

--- tests/phpunit/elementor/core/kits/test-manager.php
+++ tests/phpunit/elementor/core/kits/test-manager.php
@@ -180,7 +180,8 @@ class Test_Manager extends Elementor_Test_Base {
 			'title' => 'Site Settings',
 			'sub_title' => 'Site',
 			'href' => $document->get_edit_url() . '#' . Manager::E_HASH_COMMAND_OPEN_SITE_SETTINGS,
-			'class' => 'elementor-general-section first elementor-site-settings',
+			'class' => 'elementor-site-settings',
+			'parent_class' => 'elementor-second-section',
 		];
 
 		$adminbar_settings = ( new Adminbar_Module() )->get_settings();
