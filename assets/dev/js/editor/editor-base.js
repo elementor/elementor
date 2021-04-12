7e414987e3292b15cc3e0f8a62d77532460aa7ac
Kobi Zaltzberg <kobi@elementor.com>
2021-04-12 10:33:17 +0300

    Responsive Bar: Added entries validation functionality

--- assets/dev/js/editor/editor-base.js
+++ assets/dev/js/editor/editor-base.js
@@ -578,11 +578,19 @@ export default class EditorBase extends Marionette.Application {
 			.trigger( 'resize' );
 	}
 
-	getBreakpointResizeOptions( currentBreakpoint ) {
-		const { activeBreakpoints } = elementorFrontend.config.responsive,
+	getCurrentDeviceConstrains() {
+		const currentBreakpoint = elementor.channels.deviceMode.request( 'currentMode' ),
+			{ activeBreakpoints } = elementorFrontend.config.responsive,
 			currentBreakpointData = activeBreakpoints[ currentBreakpoint ],
 			currentBreakpointMinPoint = Stylesheet.getDeviceMinBreakpoint( currentBreakpoint );
 
+		return {
+			maxWidth: currentBreakpointData.value,
+			minWidth: currentBreakpointMinPoint || 375,
+		};
+	}
+
+	getBreakpointResizeOptions( currentBreakpoint ) {
 		const specialBreakpointsHeights = {
 			mobile: {
 				minHeight: 480,
@@ -596,16 +604,13 @@ export default class EditorBase extends Marionette.Application {
 			},
 		};
 
-		let breakpointConstrains = {
-			maxWidth: currentBreakpointData.value,
-			minWidth: currentBreakpointMinPoint || 375,
-		};
+		let deviceConstrains = this.getCurrentDeviceConstrains();
 
 		if ( specialBreakpointsHeights[ currentBreakpoint ] ) {
-			breakpointConstrains = { ...breakpointConstrains, ...specialBreakpointsHeights[ currentBreakpoint ] };
+			deviceConstrains = { ...deviceConstrains, ...specialBreakpointsHeights[ currentBreakpoint ] };
 		}
 
-		return breakpointConstrains;
+		return deviceConstrains;
 	}
 
 	updatePreviewResizeOptions( preserveCurrentSize = false ) {
--- assets/dev/js/editor/regions/responsive-bar/view.js
+++ assets/dev/js/editor/regions/responsive-bar/view.js
@@ -24,8 +24,8 @@ export default class View extends Marionette.ItemView {
 	events() {
 		return {
 			'change @ui.switcherInput': 'onBreakpointSelected',
-			'change @ui.sizeInputWidth': 'onSizeInputChange',
-			'change @ui.sizeInputHeight': 'onSizeInputChange',
+			'input @ui.sizeInputWidth': 'onSizeInputChange',
+			'input @ui.sizeInputHeight': 'onSizeInputChange',
 			'click @ui.closeButton': 'onCloseButtonClick',
 			'click @ui.breakpointSettingsButton': 'onBreakpointSettingsOpen',
 		};
@@ -45,6 +45,24 @@ export default class View extends Marionette.ItemView {
 				},
 			}
 		);
+	}
+
+	restoreLastValidPreviewSize() {
+		const lastSize = elementor.channels.responsivePreview.request( 'size' );
+
+		this.ui.sizeInputWidth
+			.val( lastSize.width )
+			.tipsy( {
+				trigger: 'manual',
+				gravity: 'n',
+				title: () => __( 'The value inserted isn\'t in the breakpoint boundaries', 'elementor' ),
+			} );
+
+		const tipsy = this.ui.sizeInputWidth.data( 'tipsy' );
+
+		tipsy.show();
+
+		setTimeout( () => tipsy.hide(), 3000 );
 	}
 
 	onDeviceModeChange() {
@@ -94,6 +112,10 @@ export default class View extends Marionette.ItemView {
 	}
 
 	onPreviewResize() {
+		if ( this.updatingPreviewSize ) {
+			return;
+		}
+
 		const size = elementor.channels.responsivePreview.request( 'size' );
 
 		this.ui.sizeInputWidth.val( size.width );
@@ -109,10 +131,24 @@ export default class View extends Marionette.ItemView {
 	}
 
 	onSizeInputChange() {
+		clearTimeout( this.restorePreviewSizeTimeout );
+
 		const size = {
 			width: this.ui.sizeInputWidth.val(),
 			height: this.ui.sizeInputHeight.val(),
 		};
+
+		const currentDeviceConstrains = elementor.getCurrentDeviceConstrains();
+
+		if ( size.width < currentDeviceConstrains.minWidth || size.width > currentDeviceConstrains.maxWidth ) {
+			this.restorePreviewSizeTimeout = setTimeout( () => this.restoreLastValidPreviewSize(), 1500 );
+
+			return;
+		}
+
+		this.updatingPreviewSize = true;
+
+		setTimeout( () => this.updatingPreviewSize = false, 300 );
 
 		elementor.updatePreviewSize( size );
 	}
