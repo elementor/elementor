a4442b3607d93f7c225163e37c45666f61f15070
udidol <udi.dollberg@gmail.com>
2021-04-12 14:55:49 +0300

    Uploads Manager: Zip File Type - fix extraction recursion for cases of multiple levels of nested folders.

--- core/files/file-types/zip.php
+++ core/files/file-types/zip.php
@@ -138,7 +138,7 @@ class Zip extends Base {
 		foreach ( $possible_file_names as $possible_file_name ) {
 			$full_possible_file_name = $temp_path . '/' . $possible_file_name;
 			if ( is_dir( $full_possible_file_name ) ) {
-				$file_names = $file_names + $this->find_temp_files( $full_possible_file_name );
+				$file_names = array_merge( $file_names, $this->find_temp_files( $full_possible_file_name ) );
 			} else {
 				$file_names[] = $full_possible_file_name;
 			}
