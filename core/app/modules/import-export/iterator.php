fa1a7f32482797f092c759f7b8e22473c919bc3e
Kobi Zaltzberg <kobi@elementor.com>
2021-04-13 10:13:56 +0300

    I/E: Change - Using Uploads Manager

--- core/app/modules/import-export/iterator.php
+++ core/app/modules/import-export/iterator.php
@@ -11,26 +11,10 @@ abstract class Iterator extends Base_Object {
 
 	private $current_archive_path = '';
 
-	private $temp_dir;
-
 	abstract public function run();
 
 	protected function get_archive_file_path( $file_name ) {
 		return $this->get_current_archive_path() . $file_name;
-	}
-
-	protected function get_temp_dir() {
-		if ( ! $this->temp_dir ) {
-			$wp_upload_dir = wp_upload_dir();
-
-			$this->temp_dir = implode( DIRECTORY_SEPARATOR, [ $wp_upload_dir['basedir'], 'elementor', 'tmp', 'kit' ] ) . DIRECTORY_SEPARATOR;
-
-			if ( ! is_dir( $this->temp_dir ) ) {
-				wp_mkdir_p( $this->temp_dir );
-			}
-		}
-
-		return $this->temp_dir;
 	}
 
 	public function get_current_archive_path() {
