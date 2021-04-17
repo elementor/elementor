93b8438aff7c32bb59b2069ec9b10b3d57ca1119
Josh Marom <josh@josh.co.il>
2021-04-18 00:29:44 +0300

    Remove view file

--- assets/dev/js/editor/regions/optimizer/optimizer.js
+++ assets/dev/js/editor/regions/optimizer/optimizer.js
@@ -1,5 +1,3 @@
-import View from './view';
-
 export default class extends Marionette.Region {
 	initialize() {
 		this.show( new View() );
@@ -12,6 +10,51 @@ export default class extends Marionette.Region {
 	onPanelResizeStop() {
 		this.$el.removeClass( 'ui-resizable-resizing' );
 	}*/
+}
+
+class View extends Marionette.ItemView {
+	getTemplate() {
+		return '#tmpl-elementor-templates-optimizer';
+	}
+
+	className() {
+		return 'elementor-optimizer';
+	}
+
+	ui() {
+		return {
+			iframe: '#optimizer-iframe',
+		};
+	}
+
+	events() {
+		return {
+			'load @ui.iframe': 'onIframeLoad',
+		};
+	}
+
+	initialize() {
+		const previewUrl = elementor.config.initial_document.urls.wp_preview;
+
+		setTimeout( () => {
+			this.ui.iframe.attr( 'src', previewUrl )
+				.css( {
+					position: 'absolute',
+					top: '50px',
+					right: '50px',
+					width: '1200px',
+					height: '1000px',
+					transform: 'scale(0.25)',
+				} );
+			this.ui.iframe.trigger( 'load' );
+
+			console.error( this.ui.iframe );
+		}, 500 );
+	}
+
+	onIframeLoad() {
+		console.error( 'iframeloaded', this.ui.iframe );
+	}
 }
 
 /*
