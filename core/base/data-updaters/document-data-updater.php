c5a71a34bacf8cede34f6c31c4314bd1e4c00002
Rotem Ben-Itzhak <rotem@elementor.com>
2021-04-19 15:54:35 +0300

    Document and Data updaters: Tweak - The page assets handling was moved to the assets-data-updater.

--- core/base/data-updaters/document-data-updater.php
+++ core/base/data-updaters/document-data-updater.php
@@ -1,6 +1,8 @@
 <?php
 namespace Elementor\Core\Base\Data_Updaters;
 
+use Elementor\Element_Base;
+
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly
 }
@@ -10,9 +12,11 @@ abstract class Document_Data_Updater {
 
 	abstract public function is_update_needed();
 
-	public function update_unique_widget( object $element_data ) {}
+	public function update_unique_widget( Element_Base $element_data ) {}
 
-	public function update_element( object $element_data ) {}
+	public function update_element( Element_Base $element_data ) {}
+
+	public function after_elements_iteration( $event ) {}
 
 	public function __construct( $document ) {
 		$this->document = $document;
