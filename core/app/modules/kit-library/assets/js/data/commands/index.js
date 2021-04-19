44e5d2569a2d94a3fa4dba7d1528a97169382069
Nevo Golan <nevog@elementor.com>
2021-04-19 17:19:39 +0300

    New: Kit-Library - Connect to download link.
    ref: ED-2712

--- core/app/modules/kit-library/assets/js/data/commands/index.js
+++ core/app/modules/kit-library/assets/js/data/commands/index.js
@@ -1,3 +1,5 @@
+export { KitsDownloadLink } from './kits-download-link';
+
 export class Index extends $e.modules.CommandData {
 	static getEndpointFormat() {
 		return 'kits/{id}';
--- core/app/modules/kit-library/data/api-client.php
+++ core/app/modules/kit-library/data/api-client.php
@@ -1,6 +1,8 @@
 <?php
 namespace Elementor\Core\App\Modules\KitLibrary\Data;
 
+use Elementor\Plugin;
+use Elementor\Core\Common\Modules\Connect\Apps\Connect;
 use Elementor\Core\App\Modules\KitLibrary\Data\Exceptions\Api_Wp_Error_Exception;
 use Elementor\Core\App\Modules\KitLibrary\Data\Exceptions\Api_Response_Exception;
 
@@ -12,9 +14,19 @@ class Api_Client {
 	const DEFAULT_BASE_ENDPOINT = 'https://kits.dev.elementor.red';
 
 	/**
+	 * @var \WP_Http
+	 */
+	private $http;
+
+	/**
 	 * @var string
 	 */
 	private $base_endpoint;
+
+	/**
+	 * @var Connect
+	 */
+	private $connect;
 
 	/**
 	 * Get all the kits
@@ -24,7 +36,7 @@ class Api_Client {
 	 * @throws Api_Wp_Error_Exception
 	 */
 	public function get_all() {
-		return $this->send_request( 'kits' );
+		return $this->send_request( 'GET', 'kits' );
 	}
 
 	/**
@@ -35,7 +47,7 @@ class Api_Client {
 	 * @throws Api_Wp_Error_Exception
 	 */
 	public function get_taxonomies() {
-		return $this->send_request( 'taxonomies' );
+		return $this->send_request( 'GET', 'taxonomies' );
 	}
 
 	/**
@@ -48,30 +60,43 @@ class Api_Client {
 	 * @throws Api_Wp_Error_Exception
 	 */
 	public function get_manifest( $id ) {
-		return $this->send_request( "manifests/{$id}" );
+		return $this->send_request( 'GET', "manifests/{$id}" );
 	}
 
 	/**
 	 * @param $id
+	 *
+	 * @return mixed
+	 * @throws Api_Response_Exception
+	 * @throws Api_Wp_Error_Exception
 	 */
-	public function download( $id ) {
-		// TODO
+	public function download_link( $id ) {
+		return $this->send_request( 'GET', "manifests/{$id}", [
+			'headers' => $this->get_headers_for_connect(),
+		] );
 	}
 
 	/**
 	 * Send a request.
 	 *
+	 * @param       $method
 	 * @param       $path
-	 * @param array $query_args
+	 * @param array $args
 	 *
-	 * @return array
+	 * @return mixed
 	 * @throws Api_Response_Exception
 	 * @throws Api_Wp_Error_Exception
 	 */
-	public function send_request( $path, $query_args = [] ) {
-		$response = wp_remote_get(
-			add_query_arg( $query_args, "{$this->base_endpoint}/{$path}" )
-		);
+	public function send_request( $method, $path, $args = [] ) {
+		$args = wp_parse_args( [
+			'method' => $method,
+			'headers' => [
+				'X-Elementor-Local-Id' => get_current_user_id(),
+				'X-Elementor-Home-Url' => trailingslashit( home_url() ),
+			],
+		], $args );
+
+		$response = $this->http->request( "{$this->base_endpoint}/{$path}", $args );
 
 		// Throw exception when something went wrong.
 		if ( is_wp_error( $response ) ) {
@@ -89,11 +114,34 @@ class Api_Client {
 	}
 
 	/**
-	 * API constructor.
+	 * @param array $request_body
 	 *
-	 * @param string $base_endpoint
+	 * @return array
+	 */
+	private function get_headers_for_connect( $request_body = [] ) {
+		return [
+			'X-Elementor-Access-Token' => $this->connect->get( 'access_token' ),
+			'X-Elementor-Client-Id' => $this->connect->get( 'client_id' ),
+			'X-Elementor-Site-Key' => $this->connect->get_site_key(),
+			'X-Elementor-Signature' => hash_hmac(
+				'sha256',
+				wp_json_encode( $request_body, JSON_NUMERIC_CHECK ),
+				$this->connect->get( 'access_token_secret' )
+			),
+		];
+	}
+
+	/**
+	 * Api_Client constructor.
+	 *
+	 * @param $base_endpoint
 	 */
 	public function __construct( $base_endpoint ) {
+		$this->http = new \WP_Http();
 		$this->base_endpoint = $base_endpoint;
+
+		add_action( 'rest_api_init', function () {
+			$this->connect = Plugin::$instance->common->get_component( 'connect' );
+		}, 11 );
 	}
 }
