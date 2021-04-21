62dc211e1f10a34f133778049b018bfca759b96e
Leonid <leonid@elementor.com>
2021-04-21 20:36:36 +0300

    PHPUnit/Modules/Usage: New - Complete schema usage tests.(R1-Part3)(N3)

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,6 +1,6 @@
 <?php
 
-require __DIR__ . '../vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
 
 use Elementor\Autoloader;
 use Elementor\Core\Experiments\Manager as Experiments_Manager;
