c1c39d534a46066384d87d1c85dc43d136a8606d
Rotem Ben-Itzhak <rotem@elementor.com>
2021-04-23 10:18:11 +0300

    Assets Loader: Tweak - Adding comments and re-organizing the code.

--- core/utils/assets-loader.php
+++ core/utils/assets-loader.php
@@ -45,6 +45,12 @@ class Assets_Loader extends Module {
 		return $this->assets;
 	}
 
+	/**
+	 * @param array $assets {
+	 *     @type array 'styles'
+	 *     @type array 'scripts'
+	 * }
+	 */
 	public function enable_assets( array $assets_data ) {
 		if ( ! $this->assets ) {
 			$this->init_assets();
@@ -57,6 +63,12 @@ class Assets_Loader extends Module {
 		}
 	}
 
+	/**
+	 * @param array $assets {
+	 *     @type array 'styles'
+	 *     @type array 'scripts'
+	 * }
+	 */
 	public function add_assets( array $assets ) {
 		if ( ! $this->assets ) {
 			$this->init_assets();
@@ -65,8 +77,33 @@ class Assets_Loader extends Module {
 		$this->assets = array_replace_recursive( $this->assets, $assets );
 	}
 
+	public function enqueue_assets() {
+		$assets = $this->get_assets();
+		$is_preview_mode = Plugin::$instance->preview->is_preview_mode();
+		$is_optimized_assets_loading = Plugin::$instance->experiments->is_feature_active( 'e_optimized_assets_loading' );
+
+		foreach ( $assets as $assets_type => $assets_type_data ) {
+			foreach ( $assets_type_data as $asset_name => $asset_data ) {
+				if ( ! empty( $asset_data['enabled'] ) || $is_preview_mode || ! $is_optimized_assets_loading ) {
+					if ( 'scripts' === $assets_type ) {
+						wp_enqueue_script( $asset_name, $asset_data['src'], $asset_data['dependencies'], $asset_data['version'], true );
+					} else {
+						wp_enqueue_style( $asset_name, $asset_data['src'], $asset_data['dependencies'], $asset_data['version'] );
+					}
+				}
+			}
+		}
+	}
+
 	/**
-	 * @param array ( 'content_type' => string, 'assets_category' => string, 'asset_key' => string, 'current_version' => string, 'asset_path' => string, 'data' => array )
+	 * @param array $config {
+	 *     @type string 'content_type'
+	 *     @type string 'assets_category'
+	 *     @type string 'asset_key'
+	 *     @type number 'current_version'
+	 *     @type string 'asset_path'
+	 *     @type array 'data'
+	 * }
 	 */
 	public function init_asset_inline_content( $config ) {
 		$content_type = $config['content_type'];
@@ -75,7 +112,7 @@ class Assets_Loader extends Module {
 		$current_version = $config['current_version'];
 
 		if ( ! $this->assets_inline_content ) {
-			$this->init_assets_inline_content( $content_type, $assets_category );
+			$this->init_assets_inline_content_data( $content_type, $assets_category );
 		}
 
 		$assets_data = $this->assets_inline_content[ $content_type ][ $assets_category ];
@@ -91,7 +128,17 @@ class Assets_Loader extends Module {
 		}
 	}
 
-	public function get_asset_inline_content( $config ) {
+	/**
+	 * @param array $config {
+	 *     @type string 'content_type'
+	 *     @type string 'assets_category'
+	 *     @type string 'asset_key'
+	 *     @type number 'current_version'
+	 *     @type string 'asset_path'
+	 *     @type array 'data'
+	 * }
+	 */
+	public function get_asset_inline_content( array $config ) {
 		$this->init_asset_inline_content( $config );
 
 		$content_type = $config['content_type'];
@@ -101,28 +148,37 @@ class Assets_Loader extends Module {
 		return $this->assets_inline_content[ $content_type ][ $assets_category ][ $asset_key ]['content'];
 	}
 
-	public function save_asset_inline_content( $content_type, $assets_category, $asset_key, $content, $version ) {
-		if ( ! $this->assets_inline_content ) {
-			$this->init_assets_inline_content( $content_type, $assets_category );
-		}
-
-		if ( ! isset( $this->assets_inline_content[ $content_type ][ $asset_key ] ) ) {
-			$this->assets_inline_content[ $content_type ][ $assets_category ][ $asset_key ] = [];
-		}
-
-		$this->assets_inline_content[ $content_type ][ $assets_category ][ $asset_key ]['content'] = $content;
-
-		$this->assets_inline_content[ $content_type ][ $assets_category ][ $asset_key ]['version'] = $version;
-
-		update_option( self::INLINE_CONTENT_KEY, $this->assets_inline_content );
-	}
-
 	private function is_asset_version_changed( $asset_data, $current_version ) {
 		if ( ! isset( $asset_data['version'] ) ) {
 			return false;
 		}
 
 		return $current_version !== $asset_data['version'];
+	}
+
+	private function get_asset_css( $config ) {
+		$asset_path = $config['asset_path'];
+
+		if ( ! file_exists( $asset_path ) ) {
+			return '';
+		}
+
+		$content_type = $config['content_type'];
+		$assets_category = $config['assets_category'];
+		$asset_key = $config['asset_key'];
+		$asset_url = $config['data']['asset_url'];
+
+		$asset_css_file_size = $this->get_file_data( $content_type, $assets_category, $asset_key, $asset_path, 'size' );
+
+		// If the file size is more than 8KB then calling the external CSS file, otherwise, printing inline CSS.
+		if ( $asset_css_file_size > 8000 ) {
+			$asset_css = sprintf( '<link rel="stylesheet" href="%s">', $asset_url );
+		} else {
+			$asset_css = $this->get_file_data( $content_type, $assets_category, $asset_key, $asset_path, 'content' );
+			$asset_css = sprintf( '<style>%s</style>', $asset_css );
+		}
+
+		return $asset_css;
 	}
 
 	private function get_file_data( $content_type, $assets_category, $asset_key, $asset_path, $data_type = '' ) {
@@ -157,6 +213,55 @@ class Assets_Loader extends Module {
 		return $data;
 	}
 
+	/**
+	 * @param string $content_type
+	 * @param string $assets_category
+	 * @param string $asset_key
+	 * @param string $content
+	 * @param number $version
+	 */
+	private function save_asset_inline_content( $content_type, $assets_category, $asset_key, $content, $version ) {
+		if ( ! $this->assets_inline_content ) {
+			$this->init_assets_inline_content_data( $content_type, $assets_category );
+		}
+
+		if ( ! isset( $this->assets_inline_content[ $content_type ][ $assets_category ][ $asset_key ] ) ) {
+			$this->assets_inline_content[ $content_type ][ $assets_category ][ $asset_key ] = [];
+		}
+
+		$this->assets_inline_content[ $content_type ][ $assets_category ][ $asset_key ]['content'] = $content;
+
+		$this->assets_inline_content[ $content_type ][ $assets_category ][ $asset_key ]['version'] = $version;
+
+		update_option( self::INLINE_CONTENT_KEY, $this->assets_inline_content );
+	}
+
+	/**
+	 * @param string $content_type
+	 * @param string $assets_category
+	 */
+	private function init_assets_inline_content_data( $content_type, $assets_category ) {
+		$this->assets_inline_content = get_option( self::INLINE_CONTENT_KEY, [] );
+
+		if ( ! isset( $this->assets_inline_content[ $content_type ] ) ) {
+			$this->assets_inline_content[ $content_type ] = [];
+		}
+
+		if ( ! isset( $this->assets_inline_content[ $content_type ][ $assets_category ] ) ) {
+			$this->assets_inline_content[ $content_type ][ $assets_category ] = [];
+		}
+	}
+
+	private function reset_inline_content_css() {
+		$assets_inline_content = get_option( self::INLINE_CONTENT_KEY, [] );
+
+		if ( array_key_exists( 'css', $assets_inline_content ) ) {
+			unset( $assets_inline_content['css'] );
+
+			update_option( self::INLINE_CONTENT_KEY, $assets_inline_content );
+		}
+	}
+
 	private function register_assets() {
 		$assets = $this->get_assets();
 
@@ -168,71 +273,6 @@ class Assets_Loader extends Module {
 					wp_register_style( $asset_name, $asset_data['src'], $asset_data['dependencies'], $asset_data['version'] );
 				}
 			}
-		}
-	}
-
-	public function enqueue_assets() {
-		$assets = $this->get_assets();
-		$is_preview_mode = Plugin::$instance->preview->is_preview_mode();
-		$is_optimized_assets_loading = Plugin::$instance->experiments->is_feature_active( 'e_optimized_assets_loading' );
-
-		foreach ( $assets as $assets_type => $assets_type_data ) {
-			foreach ( $assets_type_data as $asset_name => $asset_data ) {
-				if ( ! empty( $asset_data['enabled'] ) || $is_preview_mode || ! $is_optimized_assets_loading ) {
-					if ( 'scripts' === $assets_type ) {
-						wp_enqueue_script( $asset_name, $asset_data['src'], $asset_data['dependencies'], $asset_data['version'], true );
-					} else {
-						wp_enqueue_style( $asset_name, $asset_data['src'], $asset_data['dependencies'], $asset_data['version'] );
-					}
-				}
-			}
-		}
-	}
-
-	private function init_assets_inline_content( $content_type = '', $assets_category = '' ) {
-		$this->assets_inline_content = get_option( self::INLINE_CONTENT_KEY, [] );
-
-		if ( $content_type && ! isset( $this->assets_inline_content[ $content_type ] ) ) {
-			$this->assets_inline_content[ $content_type ] = [];
-		}
-
-		if ( $assets_category && ! isset( $this->assets_inline_content[ $content_type ][ $assets_category ] ) ) {
-			$this->assets_inline_content[ $content_type ][ $assets_category ] = [];
-		}
-	}
-
-	private function get_asset_css( $config ) {
-		$asset_path = $config['asset_path'];
-
-		if ( ! file_exists( $asset_path ) ) {
-			return '';
-		}
-
-		$content_type = $config['content_type'];
-		$assets_category = $config['assets_category'];
-		$asset_key = $config['asset_key'];
-		$asset_url = $config['data']['asset_url'];
-
-		$asset_css_file_size = $this->get_file_data( $content_type, $assets_category, $asset_key, $asset_path, 'size' );
-
-		// If the file size is more than 8KB then calling the external CSS file, otherwise, printing inline CSS.
-		if ( $asset_css_file_size > 8000 ) {
-			$asset_css = sprintf( '<link rel="stylesheet" href="%s">', $asset_url );
-		} else {
-			$asset_css = $this->get_file_data( $content_type, $assets_category, $asset_key, $asset_path, 'content' );
-			$asset_css = sprintf( '<style>%s</style>', $asset_css );
-		}
-
-		return $asset_css;
-	}
-
-	private function reset_inline_content_css() {
-		$assets_inline_content = get_option( self::INLINE_CONTENT_KEY, [] );
-
-		if ( array_key_exists( 'css', $assets_inline_content ) ) {
-			unset( $assets_inline_content['css'] );
-
-			update_option( self::INLINE_CONTENT_KEY, $assets_inline_content );
 		}
 	}
 
