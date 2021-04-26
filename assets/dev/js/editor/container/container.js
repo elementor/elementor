3618983a1d44562f59f4b970346366ee23960597
Leonid <czf.leo123@gmail.com>
2021-04-26 09:50:50 +0300

    JS API/Document/Repeater: Fix - Repeater items do not render after widget settings changed. (R1)
    ref: ED-1651

--- assets/dev/js/editor/container/container.js
+++ assets/dev/js/editor/container/container.js
@@ -241,32 +241,34 @@ export default class Container extends ArgsObject {
 	/**
 	 * Function addRepeaterItem().
 	 *
+	 * The method add repeater item, find the repeater control by it name, and create new container for the item.
+	 *
 	 * @param {string} repeaterName
-	 * @param {Backbone.Model} rowSettingsModel
+	 * @param {Backbone.Model} itemSettingsModel
 	 * @param {number} index
 	 *
 	 * @returns {Container}
 	 */
-	addRepeaterItem( repeaterName, rowSettingsModel, index ) {
-		let rowId = rowSettingsModel.get( '_id' );
+	addRepeaterItem( repeaterName, itemSettingsModel, index ) {
+		let rowId = itemSettingsModel.get( '_id' );
 
 		// TODO: Temp backwards compatibility. since 2.8.0.
 		if ( ! rowId ) {
 			rowId = 'bc-' + elementorCommon.helpers.getUniqueId();
-			rowSettingsModel.set( '_id', rowId );
+			itemSettingsModel.set( '_id', rowId );
 		}
 
 		const container = new Container( {
 			type: 'repeater',
-			id: rowSettingsModel.get( '_id' ),
+			id: itemSettingsModel.get( '_id' ),
 			model: new Backbone.Model( {
 				name: repeaterName,
 			} ),
-			settings: rowSettingsModel,
+			settings: itemSettingsModel,
 			view: this.view,
 			parent: this.repeaters[ repeaterName ],
 			label: this.label + ' ' + __( 'Item', 'elementor' ),
-			controls: rowSettingsModel.options.controls,
+			controls: itemSettingsModel.options.controls,
 			renderer: this.renderer,
 		} );
 
