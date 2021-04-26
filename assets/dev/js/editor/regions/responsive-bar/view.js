ae4c9d46b82d7c3bc55747076f99e68eec12fc20
Guy Cohen <guyc@elementor.com>
2021-04-26 14:57:53 +0300

    Responsive Bar: Tweak - [ED-1209] Scale added to responsive preview

--- assets/dev/js/editor/regions/responsive-bar/view.js
+++ assets/dev/js/editor/regions/responsive-bar/view.js
@@ -16,6 +16,10 @@ export default class View extends Marionette.ItemView {
 			switcher: prefix + '-switcher',
 			sizeInputWidth: prefix + '__input-width',
 			sizeInputHeight: prefix + '__input-height',
+			scaleValue: prefix + '-scale__value span',
+			scalePlusButton: prefix + '-scale__plus',
+			scaleMinusButton: prefix + '-scale__minus',
+			scaleResetButton: prefix + '-scale__reset',
 			closeButton: prefix + '__close-button',
 			breakpointSettingsButton: prefix + '__settings-button',
 		};
@@ -26,14 +30,25 @@ export default class View extends Marionette.ItemView {
 			'change @ui.switcherInput': 'onBreakpointSelected',
 			'input @ui.sizeInputWidth': 'onSizeInputChange',
 			'input @ui.sizeInputHeight': 'onSizeInputChange',
+			'click @ui.scalePlusButton': 'onPlusClick',
+			'click @ui.scaleMinusButton': 'onMinusClick',
+			'click @ui.scaleResetButton': 'onResetClick',
 			'click @ui.closeButton': 'onCloseButtonClick',
 			'click @ui.breakpointSettingsButton': 'onBreakpointSettingsOpen',
 		};
 	}
 
 	initialize() {
+		// I can't select the elements below from the ui() method
+		this.elementorPreview = jQuery( '#elementor-preview' );
+		this.elementorPreviewWrapper = jQuery( '#elementor-preview-responsive-wrapper' );
+		this.previewIframe = jQuery( '#elementor-preview-iframe' );
+
+		this.setScalePresentage();
+
 		this.listenTo( elementor.channels.deviceMode, 'change', this.onDeviceModeChange );
 		this.listenTo( elementor.channels.responsivePreview, 'resize', this.onPreviewResize );
+		this.listenTo( elementor.channels.deviceMode, 'close', this.resetScale );
 	}
 
 	addTipsyToIconButtons() {
@@ -66,6 +81,11 @@ export default class View extends Marionette.ItemView {
 	}
 
 	onDeviceModeChange() {
+		// I can't select jQuery( .ui-resizable- ) when preview is loaded, because it's generated dynamically from jQueryUi-resaizable()
+		this.previewRightHandle = jQuery( '#elementor-preview-responsive-wrapper .ui-resizable-e' );
+		this.previewLeftHandle = jQuery( '#elementor-preview-responsive-wrapper .ui-resizable-w' );
+		this.previewBottomHandle = jQuery( '#elementor-preview-responsive-wrapper .ui-resizable-s' );
+
 		const currentDeviceMode = elementor.channels.deviceMode.request( 'currentMode' ),
 			$currentDeviceSwitcherInput = this.ui.switcherInput.filter( '[value=' + currentDeviceMode + ']' );
 
@@ -84,6 +104,11 @@ export default class View extends Marionette.ItemView {
 		if ( currentDeviceMode !== selectedDeviceMode ) {
 			elementor.changeDeviceMode( selectedDeviceMode, false );
 		}
+	}
+
+	isDesktopDevice() {
+		const currentDeviceMode = elementor.channels.deviceMode.request( 'currentMode' );
+		return 'desktop' === currentDeviceMode;
 	}
 
 	onBreakpointSettingsOpen() {
@@ -120,6 +145,8 @@ export default class View extends Marionette.ItemView {
 
 		this.ui.sizeInputWidth.val( size.width );
 		this.ui.sizeInputHeight.val( size.height );
+
+		this.scalePreview();
 	}
 
 	onRender() {
@@ -128,6 +155,7 @@ export default class View extends Marionette.ItemView {
 
 	onCloseButtonClick() {
 		elementor.changeDeviceMode( 'desktop' );
+		elementor.exitDeviceMode();
 	}
 
 	onSizeInputChange() {
@@ -151,5 +179,114 @@ export default class View extends Marionette.ItemView {
 		setTimeout( () => this.updatingPreviewSize = false, 300 );
 
 		elementor.updatePreviewSize( size );
+		this.autoScale();
+	}
+
+	// isInDeviceConstraints( width ) {
+	// 	const currentDeviceConstrains = elementor.getCurrentDeviceConstrains();
+	// 	return width <= currentDeviceConstrains.maxWidth && width >= currentDeviceConstrains.minWidth;
+	// }
+
+	autoScale() {
+		const handlesWidth = 40,
+			previewWidth = this.elementorPreview.width() - handlesWidth,
+			iframeScaleWidth = this.ui.sizeInputWidth.val() * this.scalePresentage / 100;
+
+		if ( iframeScaleWidth > previewWidth ) {
+			const scalePresentage = previewWidth / this.ui.sizeInputWidth.val() * 100;
+
+			this.setScalePresentage( scalePresentage );
+		}
+
+		this.scalePreview();
+	}
+
+	// autoResize() {
+	// 	const handlesWidth = 40,
+	// 		previewWidth = this.elementorPreview.width() - handlesWidth,
+	// 		iframeScaleWidth = this.ui.sizeInputWidth.val() * this.scalePresentage / 100;
+	// 		//resizeIframeTo = Math.floor( previewWidth - ( ( previewWidth / 100 * this.scalePresentage ) - previewWidth ) ); // TODO: Works but still need ajust
+	// 		//isInDeviceConstraints = this.isInDeviceConstraints( resizeIframeTo ); // TODO: should I resize Iframe only if in device constraints
+
+	// 	if ( iframeScaleWidth > previewWidth ) {
+	// 		const resizeIframeTo = Math.floor( previewWidth - ( ( previewWidth / 100 * this.scalePresentage ) - previewWidth ) ); // TODO: Works but still need ajust
+
+	// 		this.ui.sizeInputWidth.val( resizeIframeTo );
+
+	// 		const size = {
+	// 			width: resizeIframeTo,
+	// 			height: this.ui.sizeInputHeight.val(),
+	// 		};
+
+	// 		elementor.updatePreviewSize( size );
+	// 	}
+	// 	//this.scalePreview();
+	// }
+
+	scalePreview() {
+		const scale = this.scalePresentage / 100;
+		this.previewIframe.css( 'transform', 'scale(' + ( scale ) + ')' );
+		this.elementorPreviewWrapper.removeClass( 'elementor-preview-responsive-wrapper--scrollable' );
+
+		if ( this.isDesktopDevice() ) {
+			// Make elementorPreviewWrapper scrollable
+			if ( this.scalePresentage > 100 ) {
+				this.elementorPreviewWrapper.addClass( 'elementor-preview-responsive-wrapper--scrollable' );
+			}
+			return;
+		}
+
+		this.handleUiResizableHandles();
+	}
+
+	handleUiResizableHandles() {
+		const previewSize = elementor.channels.responsivePreview.request( 'size' ),
+			translateLeftRightHandle = ( ( previewSize.width / 2 ) * ( ( this.scalePresentage / 100 ) - 1 ) ),
+			translateTopHandle = ( ( previewSize.height / 2 ) * ( ( this.scalePresentage / 100 ) - 1 ) ),
+			translateBottomHandle = ( ( previewSize.height ) * ( ( this.scalePresentage / 100 ) - 1 ) );
+
+		this.previewLeftHandle.css( 'transform', 'translate( ' + -translateLeftRightHandle + 'px, ' + translateTopHandle + 'px )' );
+		this.previewRightHandle.css( 'transform', 'translate( ' + translateLeftRightHandle + 'px, ' + translateTopHandle + 'px )' );
+		this.previewBottomHandle.css( 'transform', 'translate( 0,' + translateBottomHandle + 'px )' );
+	}
+
+	scalePreviewDesktop() {
+		const scale = this.scalePresentage / 100;
+		this.previewIframe.css( 'transform', 'scale(' + ( scale ) + ')' );
+	}
+
+	onPlusClick() {
+		const scaleUp = 0 === this.scalePresentage % 10 ? this.scalePresentage + 10 : Math.ceil( this.scalePresentage / 10 ) * 10,
+			scalePresentage = scaleUp > 200 ? 200 : scaleUp;
+
+		this.setScalePresentage( scalePresentage );
+		// this.autoResize();
+		this.scalePreview();
+	}
+
+	onMinusClick() {
+		const scaleDown = 0 === this.scalePresentage % 10 ? this.scalePresentage - 10 : Math.floor( this.scalePresentage / 10 ) * 10,
+			scalePresentage = scaleDown < 50 ? 50 : scaleDown;
+
+		this.setScalePresentage( scalePresentage );
+		//this.autoResize();
+		this.scalePreview();
+	}
+
+	onResetClick() {
+		this.resetScale();
+	}
+
+	resetScale() {
+		this.setScalePresentage();
+		// this.autoResize();
+		this.scalePreview();
+	}
+
+	setScalePresentage( scalePresentage = 100 ) {
+		this.scalePresentage = scalePresentage;
+		if ( this.ui.scaleValue ) {
+			this.ui.scaleValue.text( parseInt( this.scalePresentage ) );
+		}
 	}
 }
