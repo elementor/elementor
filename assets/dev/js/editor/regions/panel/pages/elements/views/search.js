aafed4c021cc6acc82c61ba3dd8ec6bad4b1a1c5
StyleShit <32631382+StyleShit@users.noreply.github.com>
2021-04-27 16:44:22 +0300

    POC: Language transform in elements panel search. (#14730)
    #ED-2876

--- assets/dev/js/editor/regions/panel/pages/elements/views/search.js
+++ assets/dev/js/editor/regions/panel/pages/elements/views/search.js
@@ -3,6 +3,8 @@ var PanelElementsSearchView;
 PanelElementsSearchView = Marionette.ItemView.extend( {
 	template: '#tmpl-elementor-panel-element-search',
 
+	localizedValue: '',
+
 	id: 'elementor-panel-elements-search-wrapper',
 
 	ui: {
@@ -10,7 +12,7 @@ PanelElementsSearchView = Marionette.ItemView.extend( {
 	},
 
 	events: {
-		'input @ui.input': 'onInputChanged',
+		'keyup @ui.input': 'onInputChanged',
 	},
 
 	clearInput: function() {
@@ -24,6 +26,25 @@ PanelElementsSearchView = Marionette.ItemView.extend( {
 			this.clearInput();
 		}
 
+		// Don't catch keyboard shortcut.
+		if ( event.shiftKey || event.ctrlKey || event.altKey ) {
+			return;
+		}
+
+		// Reset localized value if the input is empty or some chars were deleted.
+		if ( ! event.target.value || event.target.value.length < this.localizedValue.length ) {
+			this.localizedValue = '';
+		}
+
+		const isLetter = ( event.keyCode >= 65 && event.keyCode <= 90 ),
+			isSpace = ( 32 === event.keyCode );
+
+		if ( isLetter || isSpace ) {
+			this.localizedValue += String.fromCharCode( event.keyCode );
+		}
+
+		// Broadcast the localized value.
+		elementor.channels.panelElements.reply( 'filter:localized', this.localizedValue );
 		this.triggerMethod( 'search:change:input' );
 	},
 } );
