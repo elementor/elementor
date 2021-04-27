a6c45ee57947e1468299dbcd5a3511491efc065e
StyleShit <32631382+StyleShit@users.noreply.github.com>
2021-04-27 12:07:15 +0300

    Fix: Slides widget not working. (#14617)
    #ED-2715

--- assets/dev/js/editor/container/container.js
+++ assets/dev/js/editor/container/container.js
@@ -468,7 +468,7 @@ export default class Container extends ArgsObject {
 	}
 
 	getGlobalDefault( controlName ) {
-		const controlGlobalArgs = this.controls[ controlName ].global;
+		const controlGlobalArgs = this.controls[ controlName ]?.global;
 
 		if ( controlGlobalArgs?.default ) {
 			// Temp fix.
