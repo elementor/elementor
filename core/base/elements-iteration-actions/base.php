4cdcf50af54d1ff083189919629cae210ffc7d15
rotemee <61740845+rotemee@users.noreply.github.com>
2021-04-28 10:10:41 +0300

    Document: Tweak - The elements iteration might also be triggered by other processes regardless of any specific experiment - ED-2895 (#14716)
    * Document: Tweak - The elements iteration might also be triggered by other processes regardless of any specific experiment.

--- core/base/elements-iteration-actions/base.php
+++ core/base/elements-iteration-actions/base.php
@@ -8,16 +8,66 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 abstract class Base {
+	/**
+	 * The current document that the Base class instance was created from.
+	 */
 	protected $document;
 
-	protected $mode;
+	/**
+	 * Indicates if the methods are being triggered on page save or at render time (value will be either 'save' or 'render').
+	 *
+	 * @var string
+	 */
+	protected $mode = '';
 
+	/**
+	 * Is Action Needed.
+	 *
+	 * Runs only at runtime and used as a flag to determine if all methods should run on page render.
+	 * If returns false, all methods will run only on page save.
+	 * If returns true, all methods will run on both page render and on save.
+	 *
+	 * @since 3.3.0
+	 * @access public
+	 *
+	 * @return bool
+	 */
 	abstract public function is_action_needed();
 
+	/**
+	 * Unique Element Action.
+	 *
+	 * Will be triggered for each unique page element - section / column / widget unique type (heading, icon etc.).
+	 *
+	 * @since 3.3.0
+	 * @access public
+	 *
+	 * @return void
+	 */
 	public function unique_element_action( Element_Base $element_data ) {}
 
+	/**
+	 * Element Action.
+	 *
+	 * Will be triggered for each page element - section / column / widget.
+	 *
+	 * @since 3.3.0
+	 * @access public
+	 *
+	 * @return void
+	 */
 	public function element_action( Element_Base $element_data ) {}
 
+	/**
+	 * After Elements Iteration.
+	 *
+	 * Will be triggered after all page elements iteration has ended.
+	 *
+	 * @since 3.3.0
+	 * @access public
+	 *
+	 * @return void
+	 */
 	public function after_elements_iteration() {}
 
 	public function set_mode( $mode ) {
