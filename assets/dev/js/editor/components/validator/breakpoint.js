b3c452df138c15a498d60cdbad66bd08c6f776c7
udidol <udi.dollberg@gmail.com>
2021-05-03 15:45:54 +0300

    Site Settings: Breakpoints - Handle validation for default breakpoint values, for cases when an existing custom breakpoint value is deleted (replaced by an empty value).

--- assets/dev/js/editor/components/validator/breakpoint.js
+++ assets/dev/js/editor/components/validator/breakpoint.js
@@ -12,11 +12,21 @@ export default class BreakpointValidator extends NumberValidator {
 		};
 	}
 
+	initBreakpointProperties() {
+		const validationTerms = this.getSettings( 'validationTerms' );
+
+		this.breakpointKeysArray = Object.keys( elementorFrontend.config.responsive.activeBreakpoints );
+		this.breakpointIndex = this.breakpointKeysArray.indexOf( validationTerms.breakpointName );
+		this.topBreakpoint = elementorFrontend.config.responsive.activeBreakpoints[ this.breakpointKeysArray[ this.breakpointIndex + 1 ] ]?.value;
+		this.bottomBreakpoint = Stylesheet.getDeviceMinBreakpoint( validationTerms.breakpointName );
+	}
+
 	validationMethod( newValue ) {
 		const validationTerms = this.getSettings( 'validationTerms' ),
 			errors = NumberValidator.prototype.validationMethod.call( this, newValue );
 
-		if ( _.isFinite( newValue ) ) {
+		// Validate both numeric and empty values, since breakpoints utilize default values when empty.
+		if ( _.isFinite( newValue ) || '' === newValue ) {
 			if ( ! this.validateMinMaxForBreakpoint( newValue, validationTerms ) ) {
 				errors.push( 'Value is not between the breakpoints above or under the edited breakpoint' );
 			}
@@ -26,32 +36,41 @@ export default class BreakpointValidator extends NumberValidator {
 	}
 
 	validateMinMaxForBreakpoint( newValue, validationTerms ) {
+		const breakpointDefaultValue = elementorFrontend.config.responsive.breakpoints[ validationTerms.breakpointName ].default_value;
+
 		let isValid = true;
 
-		if ( ! this.breakpointKeysArray ) {
-			this.breakpointKeysArray = Object.keys( elementorFrontend.config.responsive.activeBreakpoints );
-		}
-
-		const breakpointIndex = this.breakpointKeysArray.indexOf( validationTerms.breakpointName ),
-			topBreakpointKey = this.breakpointKeysArray[ breakpointIndex + 1 ];
-
-		let bottomBreakpoint = Stylesheet.getDeviceMinBreakpoint( validationTerms.breakpointName );
+		this.initBreakpointProperties();
 
 		// Since the following comparison is <=, allow usage of the 320px value for the mobile breakpoint.
-		if ( 'mobile' === validationTerms.breakpointName ) {
-			bottomBreakpoint -= 1;
+		if ( 'mobile' === validationTerms.breakpointName && 320 === this.bottomBreakpoint ) {
+			this.bottomBreakpoint -= 1;
 		}
 
-		// If there is a breakpoint below the currently edited breakpoint, check that the value is not under the bottom
-		// breakpoint's value.
-		if ( bottomBreakpoint && ( newValue <= bottomBreakpoint ) ) {
-			isValid = false;
+		// If there is a breakpoint below the currently edited breakpoint
+		if ( this.bottomBreakpoint ) {
+			// Check that the new value is not under the bottom breakpoint's value.
+			if ( newValue && newValue <= this.bottomBreakpoint ) {
+				isValid = false;
+			}
+
+			// If the new value is empty, check that the default breakpoint value is not below the bottom breakpoint.
+			if ( ! newValue && breakpointDefaultValue <= this.bottomBreakpoint ) {
+				isValid = false;
+			}
 		}
 
-		// If there is a breakpoint above the currently edited breakpoint, check that the value is not above the top
-		// breakpoint's value.
-		if ( topBreakpointKey && newValue > elementorFrontend.config.responsive.activeBreakpoints[ topBreakpointKey ].value ) {
-			isValid = false;
+		// If there is a breakpoint above the currently edited breakpoint.
+		if ( this.topBreakpoint ) {
+			// Check that the value is not above the top breakpoint's value.
+			if ( newValue && newValue >= this.topBreakpoint ) {
+				isValid = false;
+			}
+
+			// If the new value is empty, check that the default breakpoint value is not above the top breakpoint.
+			if ( ! newValue && breakpointDefaultValue >= this.topBreakpoint ) {
+				isValid = false;
+			}
 		}
 
 		return isValid;
