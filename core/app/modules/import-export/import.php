5763f35ec914fda79ff52c34d72335bd47b41482
dtbaker <dtbaker@gmail.com>
2021-05-10 15:41:52 +1000

    Add support for Envato Template Kit import
    The thousands of template kits available at https://themeforest.net/category/template-kits have a slightly different JSON and file format to what is currently expected here.
    We can modify some keys in the JSON structure to support the Envato Template Kit import process.
    
    This isn't perfect but works most of the time.
    Also a lot of the other "Template Kit Import plugin" features (such as Required 3rd Party Plugins) are not supported in this just yet. Hopefully one day!

--- core/app/modules/import-export/import.php
+++ core/app/modules/import-export/import.php
@@ -20,6 +20,29 @@ class Import extends Iterator {
 
 		$settings = $this->read_json_file( 'manifest' );
 
+		if ( ! empty( $settings['manifest_version'] ) ) {
+			// The user is attempting to import one of the thousands of Template Kits available from https://themeforest.net/category/template-kits
+			// These template kits have a slightly different JSON and file structure to the ones exported by the new beta Elementor Export feature.
+			// To make this compatible we slightly adjust the format of the Envato manifest file.
+			// This works for majority of cases, but there are still a few that fail such as:
+			// - When required plugins are missing
+			// - When there is a global Kit style
+			$original_templates    = $settings['templates'];
+			$settings['templates'] = [];
+			foreach ( $original_templates as $original_template ) {
+				$new_template = $original_template;
+				// Elementor looks for "doc_type" instead of "type"
+				$new_template['doc_type'] = $original_template['type'];
+				// Elementor looks for "title" instead of "name"
+				$new_template['title'] = $original_template['name'];
+				// Elementor uses the template file name as the "ID" of its indexing, rather than specifying an exact path to the template.
+				// This extracts the "file name" part out of our exact source list and we treat that as an ID.
+				$file_name_without_extension = str_replace( '.json', '', basename( $original_template['source'] ) );
+				// Append our template to the global list:
+				$settings['templates'][ $file_name_without_extension ] = $new_template;
+			}
+		}
+
 		$root_directory = new Root( $this );
 
 		$import_result = $root_directory->run_import( $settings );
