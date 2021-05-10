173acfe0d983b1e757a21211b7d0e525a6f74d7a
Mati Horovitz <mati@elementor.com>
2021-05-10 19:05:16 +0300

    Fix: PHP Lint errors [ED-3164, ED-2901]

--- includes/editor-templates/editor-wrapper.php
+++ includes/editor-templates/editor-wrapper.php
@@ -27,10 +27,10 @@ $notice = Plugin::$instance->editor->notice_bar->get_notice();
 <head>
 	<meta charset="utf-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-	<title><?php echo esc_html__( 'Elementor', 'elementor' ) . ' | ' . get_the_title(); ?></title>
+	<title><?php echo esc_html__( 'Elementor', 'elementor' ) . ' | ' . esc_html( get_the_title() ); ?></title>
 	<?php wp_head(); ?>
 	<script>
-		var ajaxurl = '<?php echo admin_url( 'admin-ajax.php', 'relative' ); ?>';
+		var ajaxurl = '<?php Utils::print_unescaped_internal_string( admin_url( 'admin-ajax.php', 'relative' ) ); ?>';
 	</script>
 </head>
 <body class="<?php echo esc_attr( implode( ' ', $body_classes ) ); ?>">
