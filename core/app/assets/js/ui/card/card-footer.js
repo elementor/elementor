918fb8a5fa5195d0323db755e75155333be50389
Rotem Ben-Itzhak <rotem@elementor.com>
2021-05-21 01:00:47 +0300

    Tweak - Expanding the card components capabilities.

--- core/app/assets/js/ui/card/card-footer.js
+++ core/app/assets/js/ui/card/card-footer.js
@@ -1,8 +1,28 @@
+import { arrayToClassName, pxToRem } from 'elementor-app/utils/utils.js';
+
 import './card.scss';
 
 export default function CardFooter( props ) {
+	const classNameBase = 'eps-card__footer',
+		classes = [ classNameBase, props.className ],
+		style = {};
+
+	if ( props.hasOwnProperty( 'padding' ) ) {
+		style[ '--eps-card-footer-padding' ] = pxToRem( props.padding );
+
+		classes.push( classNameBase + '--padding' );
+	}
+
+	if ( props.passive ) {
+		classes.push( classNameBase + '--passive' );
+	}
+
+	if ( props.active ) {
+		classes.push( classNameBase + '--active' );
+	}
+
 	return (
-		<footer className={ `eps-card__footer ${ props.className }` }>
+		<footer className={ arrayToClassName( classes ) } style={ style }>
 			{ props.children }
 		</footer>
 	);
@@ -10,6 +30,9 @@ export default function CardFooter( props ) {
 
 CardFooter.propTypes = {
 	className: PropTypes.string,
+	padding: PropTypes.string,
+	passive: PropTypes.bool,
+	active: PropTypes.bool,
 	children: PropTypes.object.isRequired,
 };
 
--- core/app/assets/js/ui/card/card-header.js
+++ core/app/assets/js/ui/card/card-header.js
@@ -1,8 +1,28 @@
+import { arrayToClassName, pxToRem } from 'elementor-app/utils/utils.js';
+
 import './card.scss';
 
 export default function CardHeader( props ) {
+	const classNameBase = 'eps-card__header',
+		classes = [ classNameBase, props.className ],
+		style = {};
+
+	if ( props.hasOwnProperty( 'padding' ) ) {
+		style[ '--eps-card-header-padding' ] = pxToRem( props.padding );
+
+		classes.push( classNameBase + '--padding' );
+	}
+
+	if ( props.passive ) {
+		classes.push( classNameBase + '--passive' );
+	}
+
+	if ( props.active ) {
+		classes.push( classNameBase + '--active' );
+	}
+
 	return (
-		<header className={ `eps-card__header ${ props.className }` }>
+		<header className={ arrayToClassName( classes ) } style={ style }>
 			{ props.children }
 		</header>
 	);
@@ -10,6 +30,9 @@ export default function CardHeader( props ) {
 
 CardHeader.propTypes = {
 	className: PropTypes.string,
+	padding: PropTypes.string,
+	passive: PropTypes.bool,
+	active: PropTypes.bool,
 	children: PropTypes.any.isRequired,
 };
 
