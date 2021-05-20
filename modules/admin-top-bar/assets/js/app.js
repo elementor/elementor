505afd50cc51a3c12bce8522f85b9ff7c7c7abe6
Nisan sabag <nisans@elementor.com>
2021-05-20 17:55:36 +0300

    Adding mobile mode according to the guidelines

--- modules/admin-top-bar/assets/js/app.js
+++ modules/admin-top-bar/assets/js/app.js
@@ -2,9 +2,25 @@ import BarHeading from './components/bar-heading/bar-heading';
 import BarButton from './components/bar-button/bar-button';
 import { useEffect, useState, useRef } from 'react';
 
+function getIsMobile() {
+	const isMobile = window.innerWidth < 768;
+
+	return isMobile;
+}
+
 export default function AdminTopBar() {
 	const actionButtonsRef = useRef();
 	const [ pageTitle, setPageTitle ] = useState( null );
+	const [ isMobile, setIsMobile ] = useState( getIsMobile() );
+
+	useEffect( () => {
+		function handleResize() {
+			setIsMobile( getIsMobile() );
+		}
+
+		window.addEventListener( 'resize', handleResize );
+		return () => window.removeEventListener( 'resize', handleResize );
+	}, [] );
 
 	useEffect( () => {
 		const pageTitleElement = document.querySelector( '.wp-heading-inline' );
@@ -35,24 +51,22 @@ export default function AdminTopBar() {
 			window.open( connectUrl );
 		};
 
-		if ( isUserConnected ) {
-			connectUrl = 'https://go.elementor.com/wp-dash-admin-bar-account/';
-			return <BarButton icon="eicon-user-circle-o" onClick={connectAction}>{ __( 'My Elementor', 'elementor' ) }</BarButton>;
-		}
-		connectUrl = elementorCommonConfig.connect.connect_url;
-		return <BarButton icon="eicon-user-circle-o" onClick={connectAction}>{ __( 'Connect Account', 'elementor' ) }</BarButton>;
+		connectUrl = isUserConnected ? 'https://go.elementor.com/wp-dash-admin-bar-account/' : elementorCommonConfig.connect.connect_url;
+		const buttonText = isUserConnected ? __( 'My Elementor', 'elementor' ) : __( 'Connect Account', 'elementor' );
+		const connectButtonText = ! isMobile ? buttonText : '';
+		return <BarButton icon="eicon-user-circle-o" onClick={connectAction}>{ connectButtonText }</BarButton>;
 	};
 
 	return (
 		<div id="elementor-admin-top-bar">
 			<div className="bar-main-area bar-flex">
 				<BarHeading>{ pageTitle }</BarHeading>
-				<div className="top-bar-action-buttons-wrapper" ref={ actionButtonsRef } ></div>
+				{ ! isMobile ? <div className="top-bar-action-buttons-wrapper" ref={ actionButtonsRef } ></div> : '' }
 			</div>
 
 			<div className="bar-action-area bar-flex">
-				<BarButton onClick={finderAction} icon="eicon-search-bold">{ __( 'Finder', 'elementor' ) }</BarButton>
-				{connectButton()}
+				{ ! isMobile ? ( <BarButton onClick={finderAction} icon="eicon-search-bold">{ __( 'Finder', 'elementor' ) }</BarButton> ) : '' }
+				{ connectButton() }
 			</div>
 		</div>
 	);
