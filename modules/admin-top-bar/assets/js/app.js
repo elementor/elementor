4eaa416d2d174432ddc66e24ecd74a3749b64744
Nisan sabag <nisans@elementor.com>
2021-05-23 15:02:55 +0300

    Refactor mobile support
    In mobile mode the action buttons will be shown i admin top bar, in desktop mode the action buttons will be shown i admin page

--- modules/admin-top-bar/assets/js/app.js
+++ modules/admin-top-bar/assets/js/app.js
@@ -2,34 +2,19 @@ import BarHeading from './components/bar-heading/bar-heading';
 import BarButton from './components/bar-button/bar-button';
 import { useEffect, useState, useRef } from 'react';
 
-function getIsMobile() {
-	const isMobile = window.innerWidth < 768;
-
-	return isMobile;
-}
-
 export default function AdminTopBar() {
+	let actionButtonElements;
+	let clonedActionButtonElements;
 	const actionButtonsRef = useRef();
 	const [ pageTitle, setPageTitle ] = useState( null );
-	const [ isMobile, setIsMobile ] = useState( getIsMobile() );
 
-	// Indicate that the admin top bar is visible and the page content needs to push down below the admin top bar for visibility
+	// Indicate that the admin top bar is visible and the page content needs to push down below the admin top bar for visibility.
 	useEffect( () => {
 		const adminTopBarElement = document.querySelector( '#e-admin-top-bar' );
 		adminTopBarElement.classList.add( 'top-bar-active' );
 	}, [] );
 
-	// Handle the mobile mode
-	useEffect( () => {
-		function handleResize() {
-			setIsMobile( getIsMobile() );
-		}
-
-		window.addEventListener( 'resize', handleResize );
-		return () => window.removeEventListener( 'resize', handleResize );
-	}, [] );
-
-	// Handle the page title visibility in admin top bar
+	// Handle the page title visibility in admin top bar.
 	useEffect( () => {
 		const pageTitleElement = document.querySelector( '.wp-heading-inline' );
 		if ( pageTitleElement ) {
@@ -39,13 +24,15 @@ export default function AdminTopBar() {
 		}
 	}, [] );
 
-	// Handle the action buttons visibility in admin top bar
+	// Handle the action buttons visibility in admin top bar on initiation.
 	useEffect( () => {
-		const actionButtonElements = document.querySelectorAll( '.page-title-action' );
+		actionButtonElements = document.querySelectorAll( '.page-title-action' );
 		if ( actionButtonElements ) {
 			actionButtonElements.forEach( ( actionButtonElement ) => {
-				actionButtonsRef.current.appendChild( actionButtonElement );
+				actionButtonsRef.current.appendChild( actionButtonElement.cloneNode( true ) );
 			} );
+
+			clonedActionButtonElements = document.querySelectorAll( '.top-bar-action-buttons-wrapper' );
 		}
 	}, [] );
 
@@ -62,19 +49,21 @@ export default function AdminTopBar() {
 
 		connectUrl = isUserConnected ? 'https://go.elementor.com/wp-dash-admin-bar-account/' : elementorCommonConfig.connect.connect_url;
 		const buttonText = isUserConnected ? __( 'My Elementor', 'elementor' ) : __( 'Connect Account', 'elementor' );
-		const connectButtonText = ! isMobile ? buttonText : '';
-		return <BarButton icon="eicon-user-circle-o" onClick={connectAction}>{ connectButtonText }</BarButton>;
+		// const connectButtonText = ! isMobile ? buttonText : '';
+		return <BarButton icon="eicon-user-circle-o" onClick={connectAction}>{ buttonText }</BarButton>;
 	};
 
 	return (
 		<div id="elementor-admin-top-bar">
 			<div className="bar-main-area bar-flex">
 				<BarHeading>{ pageTitle }</BarHeading>
-				{ ! isMobile ? <div className="top-bar-action-buttons-wrapper" ref={ actionButtonsRef } ></div> : '' }
+				<div className="top-bar-action-buttons-wrapper" ref={ actionButtonsRef } ></div>
 			</div>
 
-			<div className="bar-action-area bar-flex">
-				{ ! isMobile ? ( <BarButton onClick={finderAction} icon="eicon-search-bold">{ __( 'Finder', 'elementor' ) }</BarButton> ) : '' }
+			<div className="secondary-main-area bar-flex">
+				<div className="bar-buttons-wrapper">
+					<BarButton onClick={finderAction} icon="eicon-search-bold">{ __( 'Finder', 'elementor' ) }</BarButton>
+				</div>
 				{ connectButton() }
 			</div>
 		</div>
