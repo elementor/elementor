dd6f991e5646bf65c7175b97197fba3db242af8f
Nisan sabag <nisans@elementor.com>
2021-05-25 15:51:29 +0300

    Review fixes

--- modules/admin-top-bar/module.php
+++ modules/admin-top-bar/module.php
@@ -55,15 +55,15 @@ class Module extends BaseApp {
 		wp_enqueue_style( 'elementor-admin-top-bar' );
 
 		wp_enqueue_script( 'elementor-admin-top-bar', $this->get_js_assets_url( 'admin-top-bar' ), [
-				'react',
-				'react-dom',
-			], ELEMENTOR_VERSION, true );
+			'react',
+			'react-dom',
+		], ELEMENTOR_VERSION, true );
 
 		$min_suffix = Utils::is_script_debug() ? '' : '.min';
 
 		wp_enqueue_script( 'tipsy', ELEMENTOR_ASSETS_URL . 'lib/tipsy/tipsy' . $min_suffix . '.js', [
-				'jquery',
-			], '1.0.0', true );
+			'jquery',
+		], '1.0.0', true );
 
 		$this->print_config();
 	}
