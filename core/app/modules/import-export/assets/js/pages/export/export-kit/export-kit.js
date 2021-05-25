dad446197099c711792f4595249b424854ca88e3
Rotem Ben-Itzhak <rotem@elementor.com>
2021-05-25 14:14:46 +0300

    Tweak - Adding the kit information section.

--- core/app/modules/import-export/assets/js/pages/export/export-kit/export-kit.js
+++ core/app/modules/import-export/assets/js/pages/export/export-kit/export-kit.js
@@ -4,6 +4,8 @@ import Layout from '../../../templates/layout';
 import PageHeader from '../../../ui/page-header/page-header';
 import ExportButton from './components/export-button/export-button';
 import KitContent from '../../../shared/kit-content/kit-content';
+import Panel from '../../../ui/panel/panel';
+import Grid from 'elementor-app/ui/grid/grid';
 import InlineLink from 'elementor-app/ui/molecules/inline-link';
 import WizardFooter from 'elementor-app/organisms/wizard-footer';
 
@@ -33,6 +35,28 @@ export default function ExportKit() {
 				/>
 
 				<KitContent />
+
+				<Panel>
+					<Panel.Header>
+						<Panel.Headline>{ __( 'Kit Information', 'elementor' ) }</Panel.Headline>
+					</Panel.Header>
+
+					<Panel.Body>
+						<Grid container spacing={20}>
+							<Grid item md={4}>
+								Column1
+							</Grid>
+
+							<Grid item md={4}>
+								Column2
+							</Grid>
+
+							<Grid item md={4}>
+								Column3
+							</Grid>
+						</Grid>
+					</Panel.Body>
+				</Panel>
 			</section>
 		</Layout>
 	);
