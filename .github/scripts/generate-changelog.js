10be177ae8858152cd2e175a7ce5f39cd4fa2240
Ron Korland <ronk@elementor.com>
2021-05-31 09:20:03 +0300

    Tweak: Add Internal and ignore Internal PR from change-log

--- .github/scripts/generate-changelog.js
+++ .github/scripts/generate-changelog.js
@@ -44,7 +44,8 @@ const { owner, repo } = repoToOwnerAndOwner(REPOSITORY);
 			.filter(({ commit }) => commit.verification.verified)
 			.map(({ commit }) => commit.message)
 			.map(message => message.split('\n')[0])
-			.filter((message) => /\(#\d{1,72}\)/.test(message));
+			.filter((message) => /\(#\d{1,72}\)/.test(message))
+			.filter((message) => !message.startsWith('Internal:'))
 		const markdown = prAndVerifiedCommits.map((message) => `* ${message}`).join('\n');
 		fs.writeFileSync('temp-changelog.txt', markdown);
 	} catch (err) {
