2c77dcd07c6c431b164316f7764b206e69e45d60
Nevo Golan <nevog@elementor.com>
2021-06-03 19:40:53 +0300

    Internal: Kit-Library - Show react query dev tools only in elementor debug mode [ED-3505] (#15182)

--- core/app/modules/kit-library/assets/js/app.js
+++ core/app/modules/kit-library/assets/js/app.js
@@ -17,8 +17,6 @@ const queryClient = new QueryClient( {
 	},
 } );
 
-const isDebug = elementorCommon.config.isDebug || false;
-
 export default function App() {
 	return (
 		<div className="e-kit-library">
@@ -31,7 +29,7 @@ export default function App() {
 						<Overview path="/overview/:id"/>
 					</Router>
 				</SettingsProvider>
-				{ isDebug && <ReactQueryDevtools initialIsOpen={ false }/> }
+				{ elementorCommon.config.isElementorDebug && <ReactQueryDevtools initialIsOpen={ false }/> }
 			</QueryClientProvider>
 		</div>
 	);
