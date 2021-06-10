01c8045d058f7d27d07cd77db539994d61c5f566
Nisan sabag <nisans@elementor.com>
2021-06-10 19:18:31 +0300

    Review fixes

--- assets/dev/js/admin/admin.js
+++ assets/dev/js/admin/admin.js
@@ -143,7 +143,8 @@ import environment from '../../../../core/common/assets/js/utils/environment';
 				event.preventDefault();
 				var $thisButton = $( this );
 
-				$thisButton.removeClass( 'success' ).addClass( 'loading' );
+				$thisButton.removeClass( 'success error' ).addClass( 'loading' )
+					.next( ".e-recreate-kit-error-message" ).remove();
 
 				$.post( ajaxurl, {
 					action: 'elementor_recreate_kit',
@@ -152,8 +153,9 @@ import environment from '../../../../core/common/assets/js/utils/environment';
 					.done( function() {
 						$thisButton.removeClass( 'loading' ).addClass( 'success' );
 					} )
-					.fail( function() {
+					.fail( function( reason ) {
 						$thisButton.removeClass( 'loading' ).addClass( 'error' );
+						$thisButton.after(`<div class="e-recreate-kit-error-message">${ reason }</div>`);
 					} );
 			} );
 
