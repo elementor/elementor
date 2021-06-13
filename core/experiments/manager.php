86da54e4277969235522aa2a37526398e073fb6a
udidol <udi.dollberg@gmail.com>
2021-06-13 17:27:31 +0300

    Fix: Additional Custom Breakpoints experiment description.

--- core/experiments/manager.php
+++ core/experiments/manager.php
@@ -272,7 +272,8 @@ class Manager extends Base_Object {
 		$this->add_feature( [
 			'name' => 'additional_custom_breakpoints',
 			'title' => __( 'Additional Custom Breakpoints', 'elementor' ),
-			'description' => __( 'Optimize your site\'s performance when using responsive controls.', 'elementor' ),
+			'description' => __( 'Add Additional Custom Breakpoints (beyond just \'mobile\' and \'tablet\') Optimize your site\'s performance when using responsive controls.', 'elementor' )
+							. '<br /><strong>' . __( 'Please note! Conditioning controls on values of responsive controls is not supported when this mode is active.', 'elementor' ) . '</strong>',
 			'release_status' => self::RELEASE_STATUS_DEV,
 			'new_site' => [
 				'default_active' => true,
