7a3fa386293576202d1fef4ee017d3d5f3431e80
StyleShit <32631382+StyleShit@users.noreply.github.com>
2021-06-28 13:14:50 +0300

    Tweak: PHP Lint [ED-3960] (#15506)

--- includes/settings/tools.php
+++ includes/settings/tools.php
@@ -254,11 +254,11 @@ class Tools extends Settings_Page {
 					'tools' => [
 						'fields' => [
 							'clear_cache' => [
-								'label' => __( 'Regenerate CSS & Data', 'elementor' ),
+								'label' => esc_html__( 'Regenerate CSS & Data', 'elementor' ),
 								'field_args' => [
 									'type' => 'raw_html',
-									'html' => sprintf( '<button data-nonce="%s" class="button elementor-button-spinner" id="elementor-clear-cache-button">%s</button>', wp_create_nonce( 'elementor_clear_cache' ), __( 'Regenerate Files & Data', 'elementor' ) ),
-									'desc' => __( 'Styles set in Elementor are saved in CSS files in the uploads folder and in the site’s database. Recreate those files and settings, according to the most recent settings.', 'elementor' ),
+									'html' => sprintf( '<button data-nonce="%s" class="button elementor-button-spinner" id="elementor-clear-cache-button">%s</button>', wp_create_nonce( 'elementor_clear_cache' ), esc_html__( 'Regenerate Files & Data', 'elementor' ) ),
+									'desc' => esc_html__( 'Styles set in Elementor are saved in CSS files in the uploads folder and in the site’s database. Recreate those files and settings, according to the most recent settings.', 'elementor' ),
 								],
 							],
 							'reset_api_data' => [
