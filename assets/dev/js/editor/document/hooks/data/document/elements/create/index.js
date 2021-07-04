e35d7b75fe1112d24515fb0c16e4bc289b732d4b
Evyatar Daud <evyatard@elementor.com>
2021-07-04 09:29:59 +0300

    Set `flex-direction: column` to nested Containers.

--- assets/dev/js/editor/document/hooks/data/document/elements/create/index.js
+++ assets/dev/js/editor/document/hooks/data/document/elements/create/index.js
@@ -3,5 +3,6 @@
 export { CreateSectionColumnsResetLayout } from './create-section-columns-reset-layout';
 export { InnerSectionColumns } from './inner-section-columns';
 export { IsValidChild } from './is-valid-child';
+export { NestedContainerDirection } from './nested-container-direction';
 export { SectionColumns } from './section-columns';
 export { SectionColumnsLimit } from './section-columns-limit';
