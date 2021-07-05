1ce89585d2f62c61b18e29ca4e3b83002fbf0de3
rotemee <61740845+rotemee@users.noreply.github.com>
2021-07-05 15:56:51 +0300

    Fix: Closing the import-export wizard was not leading back to the dedicated admin tab [ED-4015] (#15549)

--- core/app/modules/import-export/assets/js/templates/layout.js
+++ core/app/modules/import-export/assets/js/templates/layout.js
@@ -35,7 +35,13 @@ export default function Layout( props ) {
 			headerButtons: [ getInfoButtonProps(), ...props.headerButtons ],
 			content: getContent(),
 			footer: props.footer,
-		};
+		},
+		moduleAdminTab = '#tab-import-export-kit';
+
+	// Targeting the return_url value to the import-export dedicated admin tab.
+	if ( -1 === elementorAppConfig.return_url.indexOf( moduleAdminTab ) ) {
+		elementorAppConfig.return_url += moduleAdminTab;
+	}
 
 	return <Page { ...config } />;
 }
