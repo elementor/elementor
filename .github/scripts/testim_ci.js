4b5b863ecb91db304dda6b590719c3ab09187dff
Maor <zelinger.maor1@gmail.com>
2021-07-07 12:44:42 +0300

    Add error handaling

--- .github/scripts/testim_ci.js
+++ .github/scripts/testim_ci.js
@@ -6,17 +6,17 @@ const [project, token, ngrokToken] = myArgs
 
 const runProgram = (program, options) => {
   return new Promise((resolve, reject) => {
-    const wpCli = spawn(program, options);
+    const subprocess = spawn(program, options);
 
-    wpCli.stdout.on("data", (data) => {
+    subprocess.stdout.on("data", (data) => {
       console.log(data);
     });
 
-    wpCli.stderr.on("data", (data) => {
+    subprocess.stderr.on("data", (data) => {
       console.error(`Error: ${data}`);
     });
 
-    wpCli.on("close", (code) => {
+    subprocess.on("close", (code) => {
       console.log(`${options[0]} exited with code ${code}`);
       if (code !== 0) reject(code);
       resolve(code);
@@ -27,34 +27,42 @@ const runProgram = (program, options) => {
 const runTestim = async () => {
   // Start tunnel
   const tunnelUrl = await ngrok.connect({authtoken: ngrokToken, addr:8889});
-  // Use CLI to set local WP tunnel url
-  await Promise.all([
-    runProgram('npx', [
-      "wp-env",
-      "run",
-      "tests-cli",
-      `"wp config set WP_HOME ${tunnelUrl}"`,
-    ]),
-    runProgram('npx', [
-      "wp-env",
-      "run",
-      "tests-cli",
-      `"wp config set WP_SITEURL ${tunnelUrl}"`,
-    ]),
-  ]).catch(e => console.error(e));
-  // Run Testim
-  const exitCode = await runProgram('npx',[
-    'testim',
-    '--project',project,
-    '--token',token,
-    '--label','CI',
-    '--grid','Testim-Grid',
-    '--base-url',tunnelUrl,
-    '--params', '{"username":"admin","password":"password"}'
-  ]).catch(e => console.error(e));
-  // Close tunnel
-  await ngrok.kill();
-  return exitCode
+  try {
+    // Use CLI to set local WP tunnel url
+    await Promise.all([
+      runProgram('npx', [
+        "wp-env",
+        "run",
+        "tests-cli",
+        `"wp config set WP_HOME ${tunnelUrl}"`,
+      ]),
+      runProgram('npx', [
+        "wp-env",
+        "run",
+        "tests-cli",
+        `"wp config set WP_SITEURL ${tunnelUrl}"`,
+      ]),
+    ])
+    // Run Testim
+    const exitCode = await runProgram('npx',[
+      'testim',
+      '--project',project,
+      '--token',token,
+      '--label','CI',
+      '--grid','Testim-Grid',
+      '--base-url',tunnelUrl,
+      '--params', '{"username":"admin","password":"password"}'
+    ])
+    // Close tunnel
+    await ngrok.kill();
+    return exitCode
+  } catch (e) {
+    console.error(e)
+    await ngrok.kill();
+    return 1
+  }
 }
 
-runTestim().then(code => process.exit(code))
+runTestim()
+  .then(code => process.exit(code))
+  .catch(e => process.exit(1))
