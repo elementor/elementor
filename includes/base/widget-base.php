1221d1fbd1fc7944b3fecea5ca16b296f8b36c72
Rotem Ben-Itzhak <rotem@elementor.com>
2021-07-14 16:40:04 +0300

    Tweak - Adding support in a link tag.

--- includes/base/widget-base.php
+++ includes/base/widget-base.php
@@ -1007,7 +1007,16 @@ abstract class Widget_Base extends Element_Base {
 			return;
 		}
 
-		echo wp_kses( $this->get_widget_css(), [ 'style' => [] ] );
+		$widget_css = $this->get_widget_css();
+
+		echo wp_kses( $widget_css, [
+				'style' => [],
+				'link' => [
+					'rel' => true,
+					'href' => true,
+				],
+			]
+		);
 	}
 
 	private function get_widgets_css_data_manager() {
