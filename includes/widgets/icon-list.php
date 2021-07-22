b04f7d693a8612bd8e4432a311e7d0a1cc839ff5
Guy Cohen <guyc@elementor.com>
2021-07-22 18:08:52 +0300

    Revert "Revert "Fix: Icon List - Text size increased on Icon list widget with size set to `em` and a link [ED-3177] (#15544)" (#15701)" (#15711)

--- includes/widgets/icon-list.php
+++ includes/widgets/icon-list.php
@@ -521,7 +521,7 @@ class Widget_Icon_List extends Widget_Base {
 			Group_Control_Typography::get_type(),
 			[
 				'name' => 'icon_typography',
-				'selector' => '{{WRAPPER}} .elementor-icon-list-item, {{WRAPPER}} .elementor-icon-list-item a',
+				'selector' => '{{WRAPPER}} .elementor-icon-list-item > .elementor-icon-list-text, {{WRAPPER}} .elementor-icon-list-item > a',
 				'global' => [
 					'default' => Global_Typography::TYPOGRAPHY_TEXT,
 				],
