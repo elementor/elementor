e2e06077557fec125c8c5b989e9ce6b299e98e2d
StyleShit <evyatard@elementor.com>
2021-07-28 08:25:03 +0300

    Comment.

--- assets/dev/js/editor/document/hooks/ui/settings/set-direction-mode.js
+++ assets/dev/js/editor/document/hooks/ui/settings/set-direction-mode.js
@@ -3,6 +3,9 @@ import After from 'elementor-api/modules/hooks/ui/after';
 /**
  * UI hook to set direction mode when changing element's settings.
  * Currently used to determine the direction of the flex icons in the Container element.
+ * It should be generic and work with any element, and since each element might use a different
+ * setting to determine its direction mode, the hook should listen to any setting change.
+ * Therefore, there isn't a `getConditions()` method.
  */
 export class SetDirectionMode extends After {
 	getCommand() {
