5ea3279a7aaeee63b0f0decfee3d0dd65703e3bf
Udi <udi@elementor.com>
2021-07-30 09:54:29 +0300

    Removed redundant updating of the frontend responsive config

--- core/kits/assets/js/hooks/ui/document/elements/settings/update-breakpoints-preview.js
+++ core/kits/assets/js/hooks/ui/document/elements/settings/update-breakpoints-preview.js
@@ -19,32 +19,15 @@ export class KitUpdateBreakpointsPreview extends $e.modules.hookUI.After {
 		const { settings } = args;
 
 		if ( settings.active_breakpoints ) {
-			// Updating the config is necessary, even if the page has to be reloaded for these settings to take place,
-			// because users can add breakpoints and then immediately give them a value, before saving the site
-			// settings. The breakpoint control's validator needs to have the updated active breakpoints config in
-			// order to validate the user input properly.
+			// Updating the current document config necessary, even if the page has to be reloaded for these settings
+			// to take place, because users can add breakpoints and then immediately choose a value for them, before
+			// saving the site settings. The breakpoint control's validator needs to have the actual active breakpoints
+			// in the panel at that moment breakpoints config in order to validate the user input properly.
 			elementor.documents.currentDocument.config.settings.settings.active_breakpoints = settings.active_breakpoints;
 
 			// This flag is used to notify users that if they make a change to the active breakpoints list, they need
 			// to reload the editor for the changes to take effect.
 			elementor.activeBreakpointsUpdated = true;
-
-			// Reset all breakpoints in the config to not-enabled, to make sure the config is updated with only the
-			// enabled breakpoints following the input change.
-			Object.keys( elementorFrontend.config.responsive.breakpoints ).forEach( ( breakpointName ) => {
-				elementorFrontend.config.responsive.breakpoints[ breakpointName ].is_enabled = false;
-			} );
-
-			// Clear the active breakpoints object before repopulating it, to make sure unselected breakpoints are removed.
-			elementorFrontend.config.responsive.activeBreakpoints = {};
-
-			settings.active_breakpoints.forEach( ( breakpointName ) => {
-				breakpointName = breakpointName.replace( 'viewport_', '' );
-				// Set its state to enabled.
-				elementorFrontend.config.responsive.breakpoints[ breakpointName ].is_enabled = true;
-				// Add/re-add the breakpoint to the emptied activeBreakpoints object.
-				elementorFrontend.config.responsive.activeBreakpoints[ breakpointName ] = elementorFrontend.config.responsive.breakpoints[ breakpointName ];
-			} );
 
 			// If this is the modified setting, no need to do further checks.
 			return;
