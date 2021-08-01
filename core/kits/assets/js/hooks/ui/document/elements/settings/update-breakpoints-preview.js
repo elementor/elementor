130d5014958ab4d2b3bc2316e3055bb930d6479a
udidol <36724484+udidol@users.noreply.github.com>
2021-08-01 14:53:06 +0300

    Fix: Site Settings - Widescreen input not rejecting illegal values (ED-4349) (#15783)

--- core/kits/assets/js/hooks/ui/document/elements/settings/update-breakpoints-preview.js
+++ core/kits/assets/js/hooks/ui/document/elements/settings/update-breakpoints-preview.js
@@ -19,7 +19,18 @@ export class KitUpdateBreakpointsPreview extends $e.modules.hookUI.After {
 		const { settings } = args;
 
 		if ( settings.active_breakpoints ) {
+			// Updating the current document config necessary, even if the page has to be reloaded for these settings
+			// to take place, because users can add breakpoints and then immediately choose a value for them, before
+			// saving the site settings. The breakpoint control's validator needs to have the actual active breakpoints
+			// in the panel at that moment breakpoints config in order to validate the user input properly.
+			elementor.documents.currentDocument.config.settings.settings.active_breakpoints = settings.active_breakpoints;
+
+			// This flag is used to notify users that if they make a change to the active breakpoints list, they need
+			// to reload the editor for the changes to take effect.
 			elementor.activeBreakpointsUpdated = true;
+
+			// If this is the modified setting, no need to do further checks.
+			return;
 		}
 
 		// If a breakpoint value was updated, update the value in the config.
