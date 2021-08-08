40a88d4ff39fe1b79741eaa67c3ae30e41cc1eb3
Leonid <czf.leo123@gmail.com>
2021-08-08 18:31:53 +0300

    Finder: Fix - BC.

--- modules/library/documents/library-document.php
+++ modules/library/documents/library-document.php
@@ -46,7 +46,7 @@ abstract class Library_Document extends Document {
 		$properties['register_type'] = true;
 		$properties['cpt'] = [ Source_Local::CPT ];
 
-		// TODO - Remove - Backwards compatibility, since 'Theme_Document' does not support 'static::get_type'.
+		// TODO: Remove - Backwards compatibility, since 'Theme_Document' does not support 'static::get_type'.
 		$properties['show_in_finder'] = false;
 
 		return $properties;
