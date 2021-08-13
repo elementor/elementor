d76f477eb6d8e06f3f726720bc53f5a0c109ffab
Leonid <czf.leo123@gmail.com>
2021-08-13 15:32:30 +0300

    JSCS.

--- assets/dev/js/editor/document/command-bases/command-disable-enable.js
+++ assets/dev/js/editor/document/command-bases/command-disable-enable.js
@@ -2,7 +2,7 @@ import CommandHistoryBase from './command-history-base';
 import ElementsSettings from 'elementor-document/elements/commands/settings';
 
 /**
- * The class serve for base for commands who needs to 'enable/disable' behavior.
+ * The class serve as base for commands who needs to 'enable/disable' behavior.
  */
 export default class CommandDisableEnable extends CommandHistoryBase {
 	static getName() {
