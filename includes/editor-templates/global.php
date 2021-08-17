e6247c41ea257567100523ce9a88b3cd66d2ea71
StyleShit <evyatard@elementor.com>
2021-08-17 16:39:18 +0300

    Fix

--- includes/editor-templates/global.php
+++ includes/editor-templates/global.php
@@ -25,10 +25,10 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 				$button_title = ( $experiments_manager->is_feature_active( 'container' ) ) ? $add_container_title : $add_section_title;
 			?>
-			<div class="elementor-add-section-area-button elementor-add-section-button" title="<?php echo esc_html( $button_title ); ?>">
+			<div class="elementor-add-section-area-button elementor-add-section-button" title="<?php echo esc_attr( $button_title ); ?>">
 				<i class="eicon-plus"></i>
 			</div>
-			<div class="elementor-add-section-area-button elementor-add-template-button" title="<?php echo esc_html__( 'Add Template', 'elementor' ); ?>">
+			<div class="elementor-add-section-area-button elementor-add-template-button" title="<?php echo esc_attr__( 'Add Template', 'elementor' ); ?>">
 				<i class="eicon-folder"></i>
 			</div>
 			<div class="elementor-add-section-drag-title"><?php echo esc_html__( 'Drag widget here', 'elementor' ); ?></div>
