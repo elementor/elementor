17090f8ec9903094126092fd5feb89e36708fcf3
Udi <udi@elementor.com>
2021-08-17 10:38:11 +0300

    Fix: Gradient control doesn't work on frontend when using Global Colors (ED-3517)

--- core/files/css/base.php
+++ core/files/css/base.php
@@ -700,6 +700,10 @@ abstract class Base extends Base_File {
 			$value = $control['selectors_dictionary'][ $value ];
 		}
 
+		if ( ! $value && ! empty( $control['global']['default'] ) ) {
+			return $this->get_selector_global_value( $control, $control['global']['default'] );
+		}
+
 		if ( ! is_numeric( $value ) && ! is_float( $value ) && empty( $value ) ) {
 			return null;
 		}
