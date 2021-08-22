c5a5615ba407403eaf38c8a5ab4b711e7c0c4c71
StyleShit <evyatard@elementor.com>
2021-08-22 14:42:42 +0300

    Review fixes.

--- assets/dev/js/frontend/handlers/container/container.js
+++ assets/dev/js/frontend/handlers/container/container.js
@@ -1,11 +1,5 @@
 export default [
-	() => import( /* webpackChunkName: 'container' */ '../background-slideshow' ),
-	() => import( /* webpackChunkName: 'container' */ '../background-video' ),
 	() => import( /* webpackChunkName: 'container' */ './handles-position' ),
 	// TODO: Move to a global handler for all widgets.
-	() => {
-		if ( elementorFrontend.isEditMode() ) {
-			return import( /* webpackChunkName: 'container' */ '../floating-bar' );
-		}
-	},
+	() => import( /* webpackChunkName: 'container' */ '../floating-bar' ),
 ];
