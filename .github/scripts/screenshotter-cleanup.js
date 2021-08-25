03c03458ef8d8f83544c1fd117f9a02cef7f364b
Mati Horovitz <mati@elementor.com>
2021-08-25 22:49:42 +0300

    Fix cleanup script

--- .github/scripts/screenshotter-cleanup.js
+++ .github/scripts/screenshotter-cleanup.js
@@ -3,7 +3,7 @@
 // The passed screenshots are redundant, delete them in order to reduce the artifact size.
 
 const fs = require('fs'),
-	basePath = '/tmp/wordpress/backstop_data/html_report',
+	basePath = '/tmp/wordpress/backstop_data',
 	configContent = fs.readFileSync( `${ basePath }/html_report/config.js` ) + '' /* hack to get it as a string */,
 	// `report` is a built in function that the backstop config.js run's, use it to get the config object.
 	report = config => config,
@@ -14,8 +14,8 @@ let deletedCount = 0;
 config.tests
 	.filter( test => 'pass' === test.status )
 	.forEach( test => {
-		fs.rmSync( `${ basePath }/${ test.pair.reference}` );
-		fs.rmSync( `${ basePath }/${ test.pair.test}` );
+		fs.rmSync( `${ basePath }/html_report/${ test.pair.reference}` );
+		fs.rmSync( `${ basePath }/html_report/${ test.pair.test}` );
 		deletedCount++;
 	} );
 
