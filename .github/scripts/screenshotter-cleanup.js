015403bd51e6f9df85c43186a0d7de302e2d869e
Mati Horovitz <mati@elementor.com>
2021-08-25 22:27:57 +0300

    Fix cleanup script

--- .github/scripts/screenshotter-cleanup.js
+++ .github/scripts/screenshotter-cleanup.js
@@ -1,21 +1,22 @@
 'use strict';
 
-const fs = require('fs');
+// The passed screenshots are redundant, delete them in order to reduce the artifact size.
 
-const basePath = '/tmp/wordpress/backstop_data';
+const fs = require('fs'),
+	basePath = '/tmp/wordpress/backstop_data',
+	configContent = fs.readFileSync( `${ basePath }/html_report/config.js` ) + '' /* hack to get it as a string */,
+	// `report` is a built in function that the backstop config.js run's, use it to get the config object.
+	report = config => config,
+	config = eval( configContent );
 
-// `report` is a built in function that the backstop config.js run's.
-const report = ( data ) => {
-	console.log( data );
+let deletedCount = 0;
 
-	data.tests
-		.filter( test => 'pass' === test.status )
-		.forEach( test => {
-			fs.rmSync( `${ basePath }/${ test.pair.reference}` );
-			fs.rmSync( `${ basePath }/${ test.pair.test}` );
-		} )
-}
+config.tests
+	.filter( test => 'pass' === test.status )
+	.forEach( test => {
+		fs.rmSync( `${ basePath }/${ test.pair.reference}` );
+		fs.rmSync( `${ basePath }/${ test.pair.test}` );
+		deletedCount++;
+	} );
 
-const config = require( `${ basePath }/html_report/config.js` );
-
-console.log(config);
+console.log( `${ deletedCount } pairs were deleted` );
