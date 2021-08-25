1fc9c234fec02fcdebbe76fad78d86edb9f136d1
Leonid <czf.leo123@gmail.com>
2021-08-25 07:43:06 +0300

    Data/V1: Remove - Tests.

--- tests/phpunit/elementor/data/base/mock/template/endpoint/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace Elementor\Tests\Phpunit\Elementor\Data\Base\Mock\Template\Endpoint;
-
-class Index extends \Elementor\Tests\Phpunit\Elementor\Data\Base\Mock\Template\Endpoint {
-	public function get_name() {
-		return 'index';
-	}
-}
