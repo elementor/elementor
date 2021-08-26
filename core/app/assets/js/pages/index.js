d7747a0fd9890463602f71bef7edc163eb39c58e
Rotem Ben-Itzhak <rotem@elementor.com>
2021-08-26 14:44:09 +0300

    Fix: Unable to import a kit via URL when not logged-in to WP.

--- core/app/assets/js/pages/index.js
+++ core/app/assets/js/pages/index.js
@@ -1,9 +1,18 @@
 import { Redirect } from '@reach/router';
+import actionsMap from 'elementor-app/url-actions/actions-map';
 
 export default function Index() {
-	const url = elementorAppConfig.menu_url.split( '#' )?.[1] || '/not-found';
+	const urlSearchParams = new URLSearchParams( window.location.search ),
+		queryParams = Object.fromEntries( urlSearchParams.entries() );
+
+	// The 'action' query param is translated into a route URL.
+	let	url = queryParams?.action && actionsMap[ queryParams.action ];
+
+	if ( ! url ) {
+		url = elementorAppConfig.menu_url.split( '#' )?.[1] || '/not-found';
+	}
 
 	return (
-		<Redirect to={url} noThrow={ true } />
+		<Redirect to={ url } noThrow={ true } />
 	);
 }
