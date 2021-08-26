5c95caba9b960967696b4afe18e9f4a1c7f82f30
Rotem Ben-Itzhak <rotem@elementor.com>
2021-08-26 15:01:18 +0300

    Tweak - Refactoring code.

--- core/app/assets/js/pages/index.js
+++ core/app/assets/js/pages/index.js
@@ -3,16 +3,11 @@ import actionsMap from 'elementor-app/url-actions/actions-map';
 
 export default function Index() {
 	const urlSearchParams = new URLSearchParams( window.location.search ),
-		queryParams = Object.fromEntries( urlSearchParams.entries() );
-
-	// The 'action' query param is translated into a route URL.
-	let url = queryParams?.action && actionsMap[ queryParams.action ];
-
-	if ( ! url ) {
-		url = elementorAppConfig.menu_url.split( '#' )?.[1] || '/not-found';
-	}
+		queryParams = Object.fromEntries( urlSearchParams.entries() ),
+		// The 'action' query param is translated into a route URL.
+		url = actionsMap[ queryParams.action ] || elementorAppConfig.menu_url.split( '#' )?.[1];
 
 	return (
-		<Redirect to={ url } noThrow={ true } />
+		<Redirect to={ url || '/not-found' } noThrow={ true } />
 	);
 }
