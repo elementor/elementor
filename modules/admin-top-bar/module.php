6f1092d35eeee8041a44f21a69aa0775fc163832
Leonid <czf.leo123@gmail.com>
2021-08-29 13:58:17 +0300

    Tweak: Removed admin top bar from main WP dashboard screen.

--- modules/admin-top-bar/module.php
+++ modules/admin-top-bar/module.php
@@ -93,16 +93,24 @@ class Module extends BaseApp {
 	public function __construct() {
 		parent::__construct();
 
-		add_action( 'in_admin_header', function () {
-			$this->render_admin_top_bar();
-		} );
+		add_action( 'current_screen', function () {
+			$current_screen = get_current_screen();
 
-		add_action( 'admin_enqueue_scripts', function () {
-			$this->enqueue_scripts();
-		} );
+			if ( ! $current_screen || 'dashboard' === $current_screen->id ) {
+				return;
+			}
 
-		add_action( 'wp_dashboard_setup', function () {
-			$this->register_dashboard_widgets();
+			add_action( 'in_admin_header', function () {
+				$this->render_admin_top_bar();
+			} );
+
+			add_action( 'admin_enqueue_scripts', function () {
+				$this->enqueue_scripts();
+			} );
+
+			add_action( 'wp_dashboard_setup', function () {
+				$this->register_dashboard_widgets();
+			} );
 		} );
 	}
 }
