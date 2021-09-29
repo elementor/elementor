be514ba6d306038f8889f9d9a1d0a8396bbf70b4
Itzhak <ItzhakA@elementor.com>
2021-09-29 18:23:45 +0300

    Fix: Review Fixes

--- includes/widgets/google-maps.php
+++ includes/widgets/google-maps.php
@@ -112,11 +112,10 @@ class Widget_Google_Maps extends Widget_Base {
 					'api_key_notification',
 					[
 						'type' => Controls_Manager::RAW_HTML,
-						/* translators: %1$s Integration settings link open tag, %2$s: Link close tag, %3$s Create API key link open tag, %4$s: Link close tag. */
+						/* translators: %1$s: Integration settings link open tag, %2$s: Create API key link open tag, %3$s: Link close tag. */
 						'raw' => sprintf(
-							esc_html__( 'Set your Google Maps API Key in Elementor\'s %1$sIntegrations Settings%3$s page. Create your key %3$shere.%3$s', 'elementor' ),
+							esc_html__( 'Set your Google Maps API Key in Elementor\'s %1$sIntegrations Settings%3$s page. Create your key %2$shere.%3$s', 'elementor' ),
 							'<a href="' . Settings::get_url() . '#tab-integrations' . '" target="_blank">',
-							'</a>',
 							'<a href="https://developers.google.com/maps/documentation/embed/get-api-key" target="_blank">',
 							'</a>'
 						),
