5d7636126f39217bc2ee6688d1f323821eddee24
StyleShit <evyatard@elementor.com>
2021-09-29 14:19:55 +0300

    Fix: Dragging UI freezes [ED-4991]

--- assets/dev/js/editor/utils/jquery-html5-dnd.js
+++ assets/dev/js/editor/utils/jquery-html5-dnd.js
@@ -256,7 +256,12 @@
 
 			currentElement = this;
 
-			elementsCache.$element.parents().each( function() {
+			// Get both parents and children and do a drag-leave on them in order to prevent a UI glitches
+			// that happens when the user drags from parent to child and vice versa.
+			const parents = elementsCache.$element.parents(),
+				children = elementsCache.$element.children();
+
+			parents.add( children ).each( function() {
 				var droppableInstance = $( this ).data( 'html5Droppable' );
 
 				if ( ! droppableInstance ) {
