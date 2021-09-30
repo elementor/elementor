cee05a1440137a1ecada52994986b33290d26a5d
Itzhak <ItzhakA@elementor.com>
2021-09-30 14:43:36 +0300

    Fix: PHPLint

--- includes/widgets/google-maps.php
+++ includes/widgets/google-maps.php
@@ -115,7 +115,7 @@ class Widget_Google_Maps extends Widget_Base {
 						/* translators: %1$s: Integration settings link open tag, %2$s: Create API key link open tag, %3$s: Link close tag. */
 						'raw' => sprintf(
 							esc_html__( 'Set your Google Maps API Key in Elementor\'s %1$sIntegrations Settings%3$s page. Create your key %2$shere.%3$s', 'elementor' ),
-							'<a href="' . Settings::get_url() . '#tab-integrations' . '" target="_blank">',
+							'<a href="' . Settings::get_url() . '#tab-integrations" target="_blank">',
 							'<a href="https://developers.google.com/maps/documentation/embed/get-api-key" target="_blank">',
 							'</a>'
 						),
