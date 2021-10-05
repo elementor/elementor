6ca864ca37cd70388a7ecab7687dd4d0a7aacdf6
Leonid <czf.leo123@gmail.com>
2021-10-05 19:04:18 +0300

    Revert "New: Temporal solution for rendering tab titles."
    This reverts commit 39f0c0e835351620f3b741819cf3a15aa2a7207d.

--- modules/nested-elements/assets/js/editor/hooks/index.js
+++ modules/nested-elements/assets/js/editor/hooks/index.js
@@ -1,3 +1,2 @@
-export { NestedElementsRenderTabTitle } from './data/document/elements/settings/nested-elements-render-tab-title';
 export { NestedElementsCreate } from './data/document/repeater/insert/nested-elements-create';
 export { NestedElementsRemove } from './data/document/repeater/remove/nested-elements-remove';
