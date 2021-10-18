beb41bfdf78e3ade186b5d026a3abd209066b5f5
Itzhak Avraham <ItzhakA@elementor.com>
2021-10-18 17:57:54 +0300

    New: Added favorite widgets section to the editor panel [ED-1501] (#15068)

--- assets/dev/js/editor/editor-base.js
+++ assets/dev/js/editor/editor-base.js
@@ -4,6 +4,7 @@ import ColorControl from './controls/color';
 import DateTimeControl from 'elementor-controls/date-time';
 import EditorDocuments from './components/documents/component';
 import environment from 'elementor-common/utils/environment';
+import Favorites from 'elementor/modules/favorites/assets/js/editor/module';
 import HistoryManager from 'elementor/modules/history/assets/js/module';
 import HotkeysScreen from './components/hotkeys/hotkeys';
 import IconsManager from './components/icons-manager/icons-manager';
@@ -351,6 +352,10 @@ export default class EditorBase extends Marionette.Application {
 		this.iconManager = new IconsManager();
 
 		this.noticeBar = new NoticeBar();
+
+		if ( elementorCommon.config.experimentalFeatures[ 'favorite-widgets' ] ) {
+			this.favorites = new Favorites();
+		}
 
 		this.history = new HistoryManager();
 
