44b52baffe418a84ae42549e783aa8808532d800
Mati Horovitz <mati@elementor.com>
2021-10-21 20:14:07 +0300

    Debug

--- tests/wp-env/config/import-temapltes.sh
+++ tests/wp-env/config/import-temapltes.sh
@@ -3,9 +3,7 @@ set -eox pipefail
 
 wp theme activate hello-elementor
 
-ls /var/www/html/elementor-templates
-ls /var/www/html/wp-content/plugins/
-ls /var/www/html/wp-content/plugins/build/tests/lighthouse/
+ls /var/www/html/wp-content/plugins/build
 
 WP_CLI_CONFIG_PATH=elementor-config/wp-cli.yml wp rewrite structure \"/%postname%/\" --hard
 wp elementor flush-css
