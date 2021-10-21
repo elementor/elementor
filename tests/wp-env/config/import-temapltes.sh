74ceff6ac2691b5aa76c1d7b6ff75fb2ebe4be75
Mati Horovitz <mati@elementor.com>
2021-10-21 19:12:34 +0300

    Tweak: Lighthouse - Use the new `import-dir` command

--- tests/wp-env/config/import-temapltes.sh
+++ tests/wp-env/config/import-temapltes.sh
@@ -2,15 +2,7 @@
 set -eox pipefail
 
 wp theme activate hello-elementor
-
-TEMPLATES_FILE_NAMES=`ls /var/www/html/elementor-templates/*.json`
-for TEMPLATE_FILE_NAME in $TEMPLATES_FILE_NAMES
-do
-   TEMPLATE_NAME=$(basename "$TEMPLATE_FILE_NAME" .json)
-   wp elementor library import "/var/www/html/elementor-templates/${TEMPLATE_NAME}.json" --user="admin"
-   TEMPLATE_ID=$(wp db query "SELECT id FROM wp_posts WHERE post_name='${TEMPLATE_NAME}' ORDER BY 'id' ASC LIMIT 1;" --skip-column-names --silent)
-   wp db query "UPDATE wp_posts SET post_type='page' WHERE id=${TEMPLATE_ID};"
-done
+wp elementor library import_dir /var/www/html/wp-content/elementor-templates --user=admin
 
 WP_CLI_CONFIG_PATH=elementor-config/wp-cli.yml wp rewrite structure \"/%postname%/\" --hard
 wp elementor flush-css
