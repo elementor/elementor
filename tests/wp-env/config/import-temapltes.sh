aa8b649645b6b58bf9dd549ffb2f1f075f0b943d
Mati Horovitz <mati@elementor.com>
2021-10-21 19:39:30 +0300

    Fix Lighthouse templates path

--- tests/wp-env/config/import-temapltes.sh
+++ tests/wp-env/config/import-temapltes.sh
@@ -2,7 +2,7 @@
 set -eox pipefail
 
 wp theme activate hello-elementor
-wp elementor library import_dir /var/www/html/wp-content/elementor-templates --user=admin
+wp elementor library import_dir /var/www/html/wp-content/plugins/elementor/tests/lighthouse/templates --user=admin
 
 WP_CLI_CONFIG_PATH=elementor-config/wp-cli.yml wp rewrite structure \"/%postname%/\" --hard
 wp elementor flush-css
