982824e8f4508fb789ddba9c62ebc1a4cc50577f
Leonid <czf.leo123@gmail.com>
2021-10-24 17:30:21 +0300

    Fix: Toggle folding all, make it more readable.

--- assets/dev/js/editor/regions/navigator/elements/commands/toggle-folding-all.js
+++ assets/dev/js/editor/regions/navigator/elements/commands/toggle-folding-all.js
@@ -3,8 +3,9 @@ import CommandBase from 'elementor-api/modules/command-base';
 export class ToggleFoldingAll extends CommandBase {
 	apply( args ) {
 		const layout = this.component.manager.region.getLayout(),
-			state = args.hasOwnProperty( 'state' ) ? args.state : 'expand' === layout.ui.toggleAll.data( 'elementor-action' ),
-			classes = [ 'eicon-collapse', 'eicon-expand' ];
+			state = args.hasOwnProperty( 'state' ) ? args.state : 'expand' === layout.ui.toggleAll.data( 'elementor-action' );
+
+		let	classes = [ 'eicon-collapse', 'eicon-expand' ];
 
 		elementor.getPreviewContainer().children.forEachRecursive( ( container ) => {
 			$e.run( 'navigator/elements/toggle-folding', {
@@ -13,10 +14,14 @@ export class ToggleFoldingAll extends CommandBase {
 			} );
 		} );
 
+		if ( ! state ) {
+			classes = classes.reverse();
+		}
+
 		layout.ui.toggleAll
 			.data( 'elementor-action', state ? 'collapse' : 'expand' )
-			.removeClass( classes[ +state ] )
-			.addClass( classes[ +! state ] );
+			.removeClass( classes[ 0 ] )
+			.addClass( classes[ 1 ] );
 	}
 }
 
