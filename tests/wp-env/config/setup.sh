3f852dc7956cd41a89859a2aafbf0cd44bb2bfc5
Mati Horovitz <mati@elementor.com>
2021-10-25 17:01:37 +0300

    Fix theme & cleanup

--- tests/wp-env/config/setup.sh
+++ tests/wp-env/config/setup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -eox pipefail
 
+wp theme activate hello-elementor
+
 wp --user=admin elementor library import-dir /var/www/html/elementor-templates
 
 WP_CLI_CONFIG_PATH=elementor-config/wp-cli.yml wp rewrite structure \"/%postname%/\" --hard
