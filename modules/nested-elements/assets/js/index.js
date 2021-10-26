24fc91674c29c2a0f4692eafe37b166b2eb85b64
Leonid Vinikov <czf.leo123@gmail.com>
2021-10-26 16:46:55 +0300

    Update modules/nested-elements/assets/js/index.js

--- modules/nested-elements/assets/js/index.js
+++ modules/nested-elements/assets/js/index.js
@@ -1,5 +1,5 @@
 elementorCommon.elements.$window.on( 'elementor:pre-start', () => {
 	// Inject promise of module loading.
-	// Give the opportunity tell `elementor` to wait for the module(s) to be loaded, and then continue to original load.
+	// Give the opportunity tell the editor: wait for the module(s) to be loaded, and then continue to original load.
 	elementor.modules.nestedElements = import( 'elementor/modules/nested-elements/assets/js/editor/module' );
 } );
