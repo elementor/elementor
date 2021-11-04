6835bc9e99d75e055113549abdaa583710d66f2f
Rotem Ben-Itzhak <rotem@elementor.com>
2021-11-04 19:03:07 +0200

    Tweak: Adding plugins selection basic functionality.

--- core/app/modules/import-export/assets/js/shared/plugins-selection/plugins-selection.js
+++ core/app/modules/import-export/assets/js/shared/plugins-selection/plugins-selection.js
@@ -1,27 +1,35 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import Table from '../../ui/table/table';
+import Heading from 'elementor-app/ui/atoms/heading';
+import Text from 'elementor-app/ui/atoms/text';
+
+import './plugins-selection.scss';
+
+const ELEMENTOR_PLUGIN_NAME = 'Elementor',
+	ELEMENTOR_PRO_PLUGIN_NAME = 'Elementor Pro';
 
 export default function PluginsSelection( props ) {
+	const hasPro = false;
+
 	const [ selectedData, setSelectedData ] = useState( null ),
+		elementorPluginsNames = [ ELEMENTOR_PLUGIN_NAME, ELEMENTOR_PRO_PLUGIN_NAME ],
+		elementorPluginsData = {},
+		plugins = [ ...props.plugins ].filter( ( data ) => {
+			const isElementorPlugin = elementorPluginsNames.includes( data.name );
+
+			if ( isElementorPlugin ) {
+				elementorPluginsData[ data.name ] = data;
+			}
+
+			return ! isElementorPlugin;
+		} ),
 		data = {
-		onChange: setSelectedData,
-		selection: true,
-		headers: [ 'Plugin Name', 'Version' ],
-		rows: [
-			{
-				id: 'settings',
-				columns: [ 'Setting2', 'Setting2' ],
-			},
-			{
-				id: 'templates',
-				columns: [ 'fgefdgdgf', 'Template2' ],
-			},
-			{
-				id: 'content',
-				columns: [ 'fgdfg', 'Content2' ],
-			},
-		],
-	};
+			headers: [ 'Plugin Name', 'Version' ],
+		};
+
+	if ( elementorPluginsData[ ELEMENTOR_PRO_PLUGIN_NAME ] ) {
+		plugins.unshift( elementorPluginsData[ ELEMENTOR_PRO_PLUGIN_NAME ] );
+	}
 
 	useEffect( () => {
 		console.log( 'props.plugins', props.plugins );
@@ -29,16 +37,25 @@ export default function PluginsSelection( props ) {
 
 	useEffect( () => {
 		if ( selectedData ) {
-			console.log( selectedData );
+			const corePluginData = elementorPluginsData[ ELEMENTOR_PLUGIN_NAME ],
+				selectedPluginsData = [ corePluginData ];
+
+			selectedData.map( ( pluginIndex ) => selectedPluginsData.push( plugins[ pluginIndex ] ) );
+
+			console.log( selectedPluginsData );
 		}
 	}, [ selectedData ] );
+
+	if ( ! props.plugins.length ) {
+		return null;
+	}
 
 	return (
 		<Table selection onSelect={ setSelectedData }>
 			<Table.Head>
 				<Table.Row>
 					<Table.Cell tag="th">
-						<Table.Checkbox allSelectedCount={ data.rows.length } />
+						<Table.Checkbox allSelectedCount={ plugins.length } />
 					</Table.Cell>
 
 					{
@@ -51,13 +68,33 @@ export default function PluginsSelection( props ) {
 
 			<Table.Body>
 				{
-					props.plugins.map( ( plugin, index ) => (
+					hasPro &&
+					<Table.Row key={ index }>
+						<Table.Cell tag="td">
+							<Table.Checkbox index={ index } />
+						</Table.Cell>
+
+						<Table.Cell tag="td">
+							<Text className="e-app-import-export-plugins-selection__cell-content">
+								{ ELEMENTOR_PRO_PLUGIN_NAME }
+							</Text>
+						</Table.Cell>
+
+						<Table.Cell tag="td">Version { plugin.version }</Table.Cell>
+					</Table.Row>
+				}
+				{
+					plugins.map( ( plugin, index ) => (
 						<Table.Row key={ index }>
 							<Table.Cell tag="td">
 								<Table.Checkbox index={ index } />
 							</Table.Cell>
 
-							<Table.Cell tag="td">{ plugin.name }</Table.Cell>
+							<Table.Cell tag="td">
+								<Text className="e-app-import-export-plugins-selection__cell-content">
+									{ plugin.name }
+								</Text>
+							</Table.Cell>
 
 							<Table.Cell tag="td">Version { plugin.version }</Table.Cell>
 						</Table.Row>
