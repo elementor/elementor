50a2fb4ca2847a605987dacde6bc0854cd94c9f1
Leonid <czf.leo123@gmail.com>
2021-11-08 17:25:33 +0200

    Move empty to function.

--- modules/nested-elements/assets/js/editor/views/widget-container/empty.js
+++ modules/nested-elements/assets/js/editor/views/widget-container/empty.js
@@ -1,72 +1,57 @@
-export default class Empty extends React.Component {
-	constructor( props ) {
-		super( props );
+import { useState } from 'react';
 
-		this.state = {
-			isRenderPresets: false,
-		};
-	}
+export default function Empty() {
+	const [ isRenderPresets, setIsRenderPresets ] = useState( false );
 
-	onClickItemPlus() {
-		this.setState( { isRenderPresets: true } );
-	}
+	const onPresetSelected = ( preset, container ) => {
+			const options = {
+				createForTarget: true,
+			};
 
-	onClickCloseStructure() {
-		this.setState( { isRenderPresets: false } );
-	}
-
-	onPresetSelected( preset, container ) {
-		const options = {
-			createForTarget: true,
-		};
-
-		// Create new one by selected preset.
-		$e.components.get( 'editor' ).defaultUtils().container.createContainerFromPresetEx(
-			preset,
-			container,
-			options,
-		);
-	}
-
-	renderEmpty() {
-		return (
-			<div className="elementor-first-add">
-				<div className="elementor-icon eicon-plus" onClick={() => this.onClickItemPlus()}/>
-				<div className="elementor-icon eicon-folder"/>
-			</div>
-		);
-	}
-
-	renderSelectPreset() {
-		return (
-			<>
-				<div className="elementor-add-section-close">
-					<i onClick={() => this.onClickCloseStructure()} className="eicon-close" aria-hidden="true"/>
-					<span className="elementor-screen-only">{__( 'Close', 'elementor' )}</span>
+			// Create new one by selected preset.
+			$e.components.get( 'editor' ).defaultUtils().container.createContainerFromPresetEx(
+				preset,
+				container,
+				options,
+			);
+		},
+		renderEmpty = () => {
+			return (
+				<div className="elementor-first-add">
+					<div className="elementor-icon eicon-plus" onClick={() => setIsRenderPresets( true )}/>
+					<div className="elementor-icon eicon-folder"/>
 				</div>
-				<div className="e-view e-container-select-preset">
-					<div className="e-container-select-preset__title">{__( 'Select your Structure', 'elementor' )}</div>
-					<div className="e-container-select-preset__list">
-						{
-							elementor.presetsFactory.getContainerPresets().map( ( preset ) => (
-								<div onClick={() => this.onPresetSelected( preset, this.props.container )}
-									key={preset} className="e-container-preset" data-preset={preset}
-									dangerouslySetInnerHTML={{ __html: elementor.presetsFactory.getContainerPreset( preset ) }}/>
-							) )
-						}
+			);
+		},
+		renderSelectPreset = () => {
+			return (
+				<>
+					<div className="elementor-add-section-close">
+						<i onClick={() => setIsRenderPresets( false )} className="eicon-close" aria-hidden="true"/>
+						<span className="elementor-screen-only">{__( 'Close', 'elementor' )}</span>
 					</div>
-				</div>
-			</>
-		);
-	}
+					<div className="e-view e-container-select-preset">
+						<div
+							className="e-container-select-preset__title">{__( 'Select your Structure', 'elementor' )}</div>
+						<div className="e-container-select-preset__list">
+							{
+								elementor.presetsFactory.getContainerPresets().map( ( preset ) => (
+									<div onClick={() => onPresetSelected( preset, this.props.container )}
+										key={preset} className="e-container-preset" data-preset={preset}
+										dangerouslySetInnerHTML={{ __html: elementor.presetsFactory.getContainerPreset( preset ) }}/>
+								) )
+							}
+						</div>
+					</div>
+				</>
+			);
+		};
 
-	render() {
-		return this.state.isRenderPresets ?
-			this.renderSelectPreset() : this.renderEmpty();
-	}
+	return isRenderPresets ?
+		renderSelectPreset() : renderEmpty();
 }
 
 Empty.propTypes = {
-    container: PropTypes.object.isRequired,
+	container: PropTypes.object.isRequired,
 };
 
