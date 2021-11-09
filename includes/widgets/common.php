elementor/elementor

commit 075519ad80eab6996df6e1ffc134cf3928329170

Fix: data attributes are being printed to DOM when not needed in CSS Transform [ED-5419] (#16850)

includes/widgets/common.php
@@ -744,7 +744,6 @@ class Widget_Common extends Widget_Base {
 						'selectors' => [
 							'{{WRAPPER}} > .elementor-widget-container' => '--e-transform-transition-duration: {{SIZE}}ms',
 						],
-						'frontend_available' => true,
 					]
 				);
 			}

commit a8cf6124988a9b627f652fea8d6a0981db6470e8

Internal: Try to fix lighthouse that fails since #16850

includes/widgets/common.php
@@ -1083,6 +1083,7 @@ class Widget_Common extends Widget_Base {
 						'selectors' => [
 							'{{WRAPPER}} > .elementor-widget-container' => '--e-transform-transition-duration: {{SIZE}}ms',
 						],
+						'frontend_available' => true,
 					]
 				);
 			}