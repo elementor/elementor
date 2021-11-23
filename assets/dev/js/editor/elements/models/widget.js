9a2ab18f2c728c88546512f682302392e6ae20f7
Leonid Vinikov <czf.leo123@gmail.com>
2021-11-23 19:19:28 +0200

    Update assets/dev/js/editor/elements/models/widget.js

--- assets/dev/js/editor/elements/models/widget.js
+++ assets/dev/js/editor/elements/models/widget.js
@@ -3,6 +3,6 @@ import ElementModel from './element';
 export default class Container extends ElementModel {
 	isValidChild() {
 		// By default widget cannot contain any element.
-        return false;
+		return false;
 	}
 }
