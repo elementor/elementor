5cdee401c070086480b25863a33e5c9d9b5bd8dc
StyleShit <lurkers2030@gmail.com>
2021-11-29 08:12:54 +0200

    QUnit.

--- assets/dev/js/editor/regions/panel/pages/editor/component.js
+++ assets/dev/js/editor/regions/panel/pages/editor/component.js
@@ -88,7 +88,7 @@ export default class Component extends ComponentBase {
 	onRoute( route ) {
 		super.onRoute( route );
 
-		SetDirectionMode.set( elementor.getCurrentElement().container );
+		SetDirectionMode.set( elementor.getCurrentElement().getContainer() );
 	}
 
 	/**
