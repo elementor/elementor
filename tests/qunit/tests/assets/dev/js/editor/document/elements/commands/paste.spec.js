9edca6c4d04cc509dbe27191024fd120afcfffdf
Mati Horovitz <21468434+matipojo@users.noreply.github.com>
2021-12-13 15:34:23 +0200

    Revert "Fix: Elements are pasted in reverse order when copying and pasting multi-selected elements [ED-5723] (#17148)"
    This reverts commit 3f3a78e37abeba8540ddb9d144593683c566d1b2.

--- tests/qunit/tests/assets/dev/js/editor/document/elements/commands/paste.spec.js
+++ tests/qunit/tests/assets/dev/js/editor/document/elements/commands/paste.spec.js
@@ -54,48 +54,19 @@ export const Paste = () => {
 				const eSection1 = ElementsHelper.createSection(),
 					eSection2 = ElementsHelper.createSection(),
 					eColumns = ElementsHelper.multiCreateColumn( [ eSection1, eSection2 ] ),
-					eButton = ElementsHelper.createButton( eColumns[ 0 ] ),
-					eHeading = ElementsHelper.createHeading( eColumns[ 0 ] ),
-					toCopy = [ eButton, eHeading ];
+					eButtons = ElementsHelper.multiCreateButton( eColumns );
 
-				ElementsHelper.multiCopy( toCopy );
+				ElementsHelper.copy( eButtons[ 0 ] );
 
-				ElementsHelper.paste( eColumns[ 1 ] );
+				ElementsHelper.multiPaste( eColumns );
 
-				// Check pasted elements existence.
-				assert.equal( eColumns[ 1 ].children.length, 2, `Both elements copied.` );
-
-				// Check whether they preserved their order.
-				for ( let i = 0; i < toCopy.length; i++ ) {
-					assert.equal(
-						eColumns[ 1 ].model.get( 'elements' ).models[ i ].get( 'widgetType' ),
-						toCopy[ i ].model.get( 'widgetType' ),
-						`Element ${ i + 1 } preserved its order.`
-					);
-				}
-			} );
-
-			QUnit.only( 'On preview container', ( assert ) => {
-				const eColumn = ElementsHelper.createSection( 1, true ),
-					eButton = ElementsHelper.createButton( eColumn ),
-					eHeading = ElementsHelper.createHeading( eColumn ),
-					toCopy = [ eButton, eHeading ];
-
-				ElementsHelper.multiCopy( toCopy );
-
-				const pasted = ElementsHelper.paste( elementor.getPreviewContainer(), true ),
-					parents = pasted.map( ( container ) => container.parent.parent );
-
-				// Check pasted elements existence.
-				assert.ok( parents.every( ( parent ) => parent ), `Both elements copied.` );
-
-				// Check whether they preserved their order.
-				assert.equal(
-					elementor.getContainer( elementor.elements.models[ elementor.elements.length - 1 ].get( 'id' ) )
-						.children[ 0 ].children[ 0 ].model.get( 'widgetType' ),
-					toCopy[ toCopy.length - 1 ].model.get( 'widgetType' ),
-					'Elements preserved their position.'
-				);
+				// Check pasted button exist.
+				let count = 1;
+				eColumns.forEach( ( eColumn ) => {
+					assert.equal( eColumn.children.length, 2,
+						`Button #${ count } were pasted.` );
+					++count;
+				} );
 			} );
 
 			QUnit.test( 'History', ( assert ) => {
