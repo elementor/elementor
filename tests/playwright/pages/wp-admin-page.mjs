ee8381fd498a30bb1f8a50c4b7f374b15cace7b4
Leonid <czf.leo123@gmail.com>
2021-12-21 12:27:15 +0200

    Fix: Playwright tests.

--- tests/playwright/pages/wp-admin-page.mjs
+++ tests/playwright/pages/wp-admin-page.mjs
@@ -5,13 +5,13 @@ export const CLEAN_POST_ID = 1;
 
 export default class wpAdminPage extends BasePage {
 	async login() {
-		const loggedIn = await this.page.$( 'text=Dashboard' );
+		await this.page.goto( '/wp-admin' );
+
+		const loggedIn = await this.page.locator( 'text=Dashboard', { timeout: 2000 } );
 
 		if ( loggedIn ) {
 			return;
 		}
-
-		await this.page.goto( '/wp-admin' );
 
 		await this.page.waitForSelector( 'text=Log In' );
 		await this.page.fill( 'input[name="log"]', this.config.user.username );
