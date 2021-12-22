a34bc3e5f468345b9fe50e343eaf9ece02661054
Nisan sabag <nisans@elementor.com>
2021-12-22 10:18:27 +0200

    Tweak: Added a deprecation notice for PHP 5.6 in WP dashboard [ED-5770]

--- core/admin/admin.php
+++ core/admin/admin.php
@@ -3,6 +3,7 @@ namespace Elementor\Core\Admin;
 
 use Elementor\Api;
 use Elementor\Beta_Testers;
+use Elementor\Core\Admin\Notices\Update_Php_Notice;
 use Elementor\Core\Base\App;
 use Elementor\Plugin;
 use Elementor\Settings;
@@ -708,6 +709,12 @@ class Admin extends App {
 
 		add_action( 'in_plugin_update_message-' . ELEMENTOR_PLUGIN_BASE, function( $plugin_data ) {
 			$this->version_update_warning( ELEMENTOR_VERSION, $plugin_data['new_version'] );
+		} );
+
+		add_filter( 'elementor/core/admin/notices', function( $notices ) {
+			$notices[] = new Update_Php_Notice();
+
+			return $notices;
 		} );
 	}
 
