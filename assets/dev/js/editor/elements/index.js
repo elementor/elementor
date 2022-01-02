630cb0e2b87befc486f1318cce6711647aafbe71
Leonid <czf.leo123@gmail.com>
2022-01-02 14:31:14 +0200

    Enable container in QUnit tests.

--- assets/dev/js/editor/elements/index.js
+++ assets/dev/js/editor/elements/index.js
@@ -2,4 +2,3 @@ export { default as Column } from './elements/column';
 export { default as Document } from './elements/document';
 export { default as Section } from './elements/section';
 export { default as Widget } from './elements/widget';
-
