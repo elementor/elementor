175608d8ae6ea0ecfd34dc0503e793f9be88629e
Manor Hazaz <manorh@elementor.com>
2022-01-06 12:33:01 +0200

    Changes

--- includes/controls/groups/text-stroke.php
+++ includes/controls/groups/text-stroke.php
@@ -79,7 +79,7 @@ class Group_Control_Text_Stroke extends Group_Control_Base {
 			'responsive' => true,
 			'selector' => '{{WRAPPER}}',
 			'selectors' => [
-				'{{SELECTOR}}' => '-webkit-text-stroke-width: {{SIZE}}{{UNIT}};',
+				'{{SELECTOR}}' => '-webkit-text-stroke-width: {{SIZE}}{{UNIT}}; stroke-width: {{SIZE}}{{UNIT}}',
 			],
 		];
 
@@ -89,7 +89,7 @@ class Group_Control_Text_Stroke extends Group_Control_Base {
 			'default' => '#000',
 			'selector' => '{{WRAPPER}}',
 			'selectors' => [
-				'{{SELECTOR}}' => '-webkit-text-stroke-color: {{VALUE}};',
+				'{{SELECTOR}}' => '-webkit-text-stroke-color: {{VALUE}}; stroke: {{VALUE}}',
 			],
 		];
 
