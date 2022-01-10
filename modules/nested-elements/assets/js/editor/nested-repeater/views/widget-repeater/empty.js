bb4cfbced2c70ad234b130b12c7190d4fa7b6897
Leonid <czf.leo123@gmail.com>
2022-01-10 16:27:25 +0200

    Review request: Part `Empty`, to `AddSectionArea` & `SelectPreset`.

--- modules/nested-elements/assets/js/editor/nested-repeater/views/widget-repeater/empty.js
+++ modules/nested-elements/assets/js/editor/nested-repeater/views/widget-repeater/empty.js
@@ -1,88 +1,17 @@
-import { useEffect, useRef, useState } from 'react';
+import { useState } from 'react';
+
+import AddSectionArea from './add-section-area';
+import SelectPreset from './select-preset';
 
 export default function Empty( props ) {
-	const containerHelper = $e.components.get( 'editor' ).defaultUtils().container,
-		addAreaElementRef = useRef(),
-		[ isRenderPresets, setIsRenderPresets ] = useState( false );
+	const [ isRenderPresets, setIsRenderPresets ] = useState( false );
 
-	// Make droppable area.
-	useEffect( () => {
-		const $addAreaElementRef = jQuery( addAreaElementRef.current ),
-			defaultDroppableOptions = props.container.view.getDroppableOptions();
+	props = {
+		...props,
+		setIsRenderPresets,
+	};
 
-		// Make some adjustments to behave like 'AddSectionArea', use default droppable options from container element.
-		defaultDroppableOptions.placeholder = false;
-		defaultDroppableOptions.items = '> .elementor-add-section-inner';
-		defaultDroppableOptions.hasDraggingOnChildClass = 'elementor-dragging-on-child';
-
-		// Make element drop-able.
-		$addAreaElementRef.html5Droppable( defaultDroppableOptions );
-	}, [ ! isRenderPresets, ! props.container.view.isDisconnected() ] );
-
-	const onPresetSelected = ( preset, container ) => {
-			const options = {
-				createWrapper: false,
-			};
-
-			// Create new one by selected preset.
-			containerHelper.createContainerFromPreset( preset, container, options );
-		},
-		renderEmpty = () => {
-			const args = {
-				importOptions: {
-					target: props.container,
-				},
-			};
-
-			return (
-				<div className="elementor-add-section" onClick={() => containerHelper.openEditMode( props.container )}
-						ref={addAreaElementRef}>
-					<div className="elementor-add-section-inner">
-						<div className="e-view elementor-add-new-section">
-							<div className="elementor-add-section-area-button elementor-add-section-button"
-									onClick={() => setIsRenderPresets( true )}
-									title={__( 'Add new container', 'elementor' )}>
-								<i className="eicon-plus"/>
-							</div>
-							<div className="elementor-add-section-area-button elementor-add-template-button"
-									onClick={() => $e.run( 'library/open', args )}
-									title={__( 'Add Template', 'elementor' )}>
-								<i className="eicon-folder"/>
-							</div>
-							<div className="elementor-add-section-drag-title">
-								{__( 'Drag widgets here to create nested widget.', 'elementor' )}
-							</div>
-						</div>
-					</div>
-				</div>
-			);
-		},
-		renderSelectPreset = () => {
-			return (
-				<>
-					<div className="elementor-add-section-close">
-						<i onClick={() => setIsRenderPresets( false )} className="eicon-close" aria-hidden="true"/>
-						<span className="elementor-screen-only">{__( 'Close', 'elementor' )}</span>
-					</div>
-					<div className="e-view e-container-select-preset">
-						<div
-							className="e-container-select-preset__title">{__( 'Select your Structure', 'elementor' )}</div>
-						<div className="e-container-select-preset__list">
-							{
-								elementor.presetsFactory.getContainerPresets().map( ( preset ) => (
-									<div onClick={() => onPresetSelected( preset, props.container )}
-										key={preset} className="e-container-preset" data-preset={preset}
-										dangerouslySetInnerHTML={{ __html: elementor.presetsFactory.getContainerPreset( preset ) }}/>
-								) )
-							}
-						</div>
-					</div>
-				</>
-			);
-		};
-
-	return isRenderPresets ?
-		renderSelectPreset() : renderEmpty();
+	return isRenderPresets ? <SelectPreset {...props} /> : <AddSectionArea {...props} />;
 }
 
 Empty.propTypes = {
