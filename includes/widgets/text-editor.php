2fc76347fe5754ed6e42b3cac123b3c9bbf78183
Rodolphe Bertozzo <56270626+rodolphebertozzo@users.noreply.github.com>
2022-01-17 23:07:58 +0100

    Update text-editor.php

--- includes/widgets/text-editor.php
+++ includes/widgets/text-editor.php
@@ -212,7 +212,7 @@ class Widget_Text_Editor extends Widget_Base {
 		);
 
 		$this->add_responsive_control(
-			'space',
+			'max_width',
 			[
 				'label' => __( 'Max Width', 'elementor' ),
 				'type' => Controls_Manager::SLIDER,
