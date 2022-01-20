60e492f122bf7ce179aa22ef00038dfed283d22a
StyleShit <lurkers2030@gmail.com>
2022-01-20 14:49:17 +0200

    Change default

--- includes/elements/container.php
+++ includes/elements/container.php
@@ -380,6 +380,9 @@ class Container extends Element_Base {
 				'condition' => [
 					'content_width' => 'boxed',
 				],
+				'default' => [
+					'unit' => 'px',
+				],
 				// Use the default width from the kit as a placeholder.
 				'placeholder' => $this->active_kit->get_settings_for_display( 'container_width' ),
 			] )
