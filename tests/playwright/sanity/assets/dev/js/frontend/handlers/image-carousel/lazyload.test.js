a958dd2b14c2c4bb84bed011ea540010c30bdbfc
Manor Hazaz <manorh@elementor.com>
2022-02-07 16:03:25 +0200

    Changes

--- tests/playwright/sanity/assets/dev/js/frontend/handlers/image-carousel/lazyload.test.js
+++ tests/playwright/sanity/assets/dev/js/frontend/handlers/image-carousel/lazyload.test.js
@@ -12,18 +12,18 @@ test( 'Image Carousel widget sanity test lazyload', async ( { page } ) => {
     await editor.addWidget( 'image-carousel' );
 
     // Set Image carousel settings
-    await page.selectOption('.elementor-control-slides_to_show >> select', '1');
+    await page.selectOption( '.elementor-control-slides_to_show >> select', '1' );
 
-    await page.click('.elementor-control-section_additional_options');
-    await page.click('.elementor-control-lazyload >> span');
-    await page.selectOption('.elementor-control-autoplay >> select', 'no');
+    await page.click( '.elementor-control-section_additional_options' );
+    await page.click( '.elementor-control-lazyload >> span' );
+    await page.selectOption( '.elementor-control-autoplay >> select', 'no' );
 
     // Add images
     const images = [ 'elementor1', 'elementor2', 'elementor3', 'elementor4' ];
     const imageFileType = '.png';
 
-    await page.click( '#elementor-controls >> text=Image Carousel');
-    await page.click( '[aria-label="Add\\ Images"]');
+    await page.click( '#elementor-controls >> text=Image Carousel' );
+    await page.click( '[aria-label="Add\\ Images"]' );
     await page.waitForTimeout( 3000 ); //waitForNetwork
 
     for ( const image of images ) {
@@ -32,14 +32,14 @@ test( 'Image Carousel widget sanity test lazyload', async ( { page } ) => {
         if( alreadyLoaded ) {
             await page.click( '[aria-label="' + image + '"]' );
         } else {
-            await page.click('text=Upload files');
+            await page.click( 'text=Upload files' );
 
             await page.setInputFiles( 'input[type="file"]', resourcesBaseDir + image + imageFileType );
         }
     }
 
-    await page.click('text=Create a new gallery');
-    await page.click('text=Insert gallery');
+    await page.click( 'text=Create a new gallery' );
+    await page.click( 'text=Insert gallery' );
 
     const widget = await editor.getFrame().waitForSelector( '.elementor-image-carousel' );
     const widgetImages = await widget.$$( '.swiper-slide >> img' );
@@ -47,12 +47,12 @@ test( 'Image Carousel widget sanity test lazyload', async ( { page } ) => {
     // The lazyload loads images from data-src into src.
     // If the src does not exist, the data-src should exist with the image src.
     for ( const image of widgetImages ) {
-        const src = await image.getAttribute('src');
+        const src = await image.getAttribute( 'src' );
 
         if( src ) {
             expect( src ).toContain( '.png' );
         } else {
-            const dataSrc = await image.getAttribute('data-src');
+            const dataSrc = await image.getAttribute( 'data-src' );
             expect( dataSrc ).toContain( '.png' );
         }
     }
