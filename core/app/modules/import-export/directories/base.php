8521413f7f8aa659a75746387d7ecdb4cfd500d4
Guy Cohen <guyc@elementor.com>
2022-02-13 16:19:00 +0200

    Tweak: One click site - Import base [ED-5813]

--- core/app/modules/import-export/directories/base.php
+++ core/app/modules/import-export/directories/base.php
@@ -4,6 +4,7 @@ namespace Elementor\Core\App\Modules\ImportExport\Directories;
 use Elementor\Core\App\Modules\ImportExport\Export;
 use Elementor\Core\App\Modules\ImportExport\Import;
 use Elementor\Core\App\Modules\ImportExport\Iterator;
+use Elementor\Plugin;
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly
@@ -36,6 +37,10 @@ abstract class Base {
 	 */
 	protected $importer;
 
+	private static $map_old_new_post_ids = [];
+
+	private static $documents_elements;
+
 	abstract protected function get_name();
 
 	public function __construct( Iterator $iterator, Base $parent = null ) {
@@ -50,6 +55,22 @@ abstract class Base {
 		$this->parent = $parent;
 
 		$this->register_directories();
+
+		add_filter( 'elementor/document/save/data', [ $this, 'prevent_saving_elements_on_post_creation' ], 10, 2 );
+	}
+
+	public function prevent_saving_elements_on_post_creation( $data, $document ) {
+		if ( $data['elements'] ) {
+			$this->set_documents_elements_by_id( $document->get_main_id(), $data['elements'] );
+
+			$data['elements'] = [];
+		}
+
+		return $data;
+	}
+
+	private function set_documents_elements_by_id( $id, $elements ) {
+		self::$documents_elements[ $id ] = $elements;
 	}
 
 	final public function get_path() {
@@ -85,6 +106,10 @@ abstract class Base {
 
 		$meta_data = $this->import( $settings );
 
+		if ( isset( $meta_data['succeed'] ) ) {
+			self::$map_old_new_post_ids = $meta_data['succeed'] + self::$map_old_new_post_ids;
+		}
+
 		foreach ( $this->sub_directories as $sub_directory ) {
 			$sub_directory_name = $sub_directory->get_name();
 
@@ -95,7 +120,20 @@ abstract class Base {
 			$meta_data[ $sub_directory_name ] = $sub_directory->run_import( $settings[ $sub_directory_name ] );
 		}
 
+		$is_root = array_key_exists( 'site-settings', $settings );
+
+		if ( $is_root ) {
+			$this->save_elements_of_imported_posts();
+		}
+
 		return $meta_data;
+	}
+
+	private function save_elements_of_imported_posts() {
+		foreach ( self::$documents_elements as $new_id => $document_elements ) {
+			$document = Plugin::$instance->documents->get( $new_id );
+			$document->on_import_replace_dynamics_elements_id( $document_elements, self::$map_old_new_post_ids );
+		}
 	}
 
 	/**
