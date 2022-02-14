73b221c701f9d55095d0ebf31648299e0cc5d0b9
Nisan sabag <nisans@elementor.com>
2022-02-14 14:27:28 +0200

    Fix: Playwright for global widget search fails [ED-6523]

--- tests/playwright/sanity/hidden-widgets-on-search.test.js
+++ tests/playwright/sanity/hidden-widgets-on-search.test.js
@@ -1,10 +1,10 @@
 const { test, expect } = require( '@playwright/test' );
-const { WpAdminPage } = require( '../pages/wp-admin-page' );
+const WpAdminPage = require( '../pages/wp-admin-page' );
 
 // When searching for a widget that is not hidden on search, the widget should be shown in search result.
-test( 'Spacer widget should be shown in search result', async ( { page } ) => {
-	const wpAdmin = new WpAdminPage( page );
-	await wpAdmin.createNewPage();
+test( 'Spacer widget should be shown in search result', async ( { page }, testInfo ) => {
+	const wpAdmin = new WpAdminPage( page, testInfo );
+	await wpAdmin.useElementorCleanPost();
 
 	await page.waitForSelector( '.elementor-panel-category-items .eicon-t-letter' );
 
@@ -17,9 +17,9 @@ test( 'Spacer widget should be shown in search result', async ( { page } ) => {
 } );
 
 // When searching for a widget that is hidden on search, the widget should not be shown in search result.
-test( 'Wordpress widget should not be shown in search result', async ( { page } ) => {
-	const wpAdmin = new WpAdminPage( page );
-	await wpAdmin.createNewPage();
+test( 'Wordpress widget should not be shown in search result', async ( { page }, testInfo ) => {
+	const wpAdmin = new WpAdminPage( page, testInfo );
+	await wpAdmin.useElementorCleanPost();
 
 	// Click text=WordPress
 	await page.click( 'text=WordPress' );
