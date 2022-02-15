d39e2a8656481a67fa902576fcefbb8753ecf69e
Manor Hazaz <manorh@elementor.com>
2022-02-15 14:10:06 +0200

    Add testing

--- tests/phpunit/elementor/core/utils/test-wp-import.php
+++ tests/phpunit/elementor/core/utils/test-wp-import.php
@@ -13,16 +13,16 @@ class Test_WP_Import extends Elementor_Test_Base {
 		$this->remove_default_kit();
 	}
 
-	public function test_run() {
+	public function test_run__import_two_pages() {
 		// Arrange.
-		$importer = new WP_Import( __DIR__ . '/mock/fresh-wordpress-database.xml' );
+		$importer = new WP_Import( __DIR__ . '/mock/page.xml' );
 
 		// Act.
 		$result = $importer->run();
-		$posts = get_posts( [
+		$pages = get_posts( [
 			'numberposts' => -1,
 			'post_status' => 'any',
-			'post_type' => get_post_types('', 'names'),
+			'post_type' => 'page',
 		] );
 
 		// Assert.
@@ -36,13 +36,77 @@ class Test_WP_Import extends Elementor_Test_Base {
 				'posts' => [
 					'failed' => [],
 					'succeed' => [
-						1 => 1,
-						2 => 2,
-						3 => 3,
+						189 => 6,
+						190 => 7,
+						195 => 8,
+						196 => 9,
 					],
 				],
 			],
 		], $result );
-		$this->assertCount( 3, $posts );
+		$this->assertCount( 4, $pages );
+	}
+
+
+	public function test_run__import_post() {
+		// Arrange.
+		$importer = new WP_Import( __DIR__ . '/mock/post.xml' );
+		$post_old_id = 200;
+		$post_new_id = 11;
+
+		// Act.
+		$result = $importer->run();
+		$post = get_post( $post_new_id );
+
+		// Assert.
+		$this->assertEquals( [
+			'status' => 'success',
+			'errors' => [],
+			'summary' => [
+				'categories' => 0,
+				'tags' => 0,
+				'terms' => 0,
+				'posts' => [
+					'failed' => [],
+					'succeed' => [
+						$post_old_id => $post_new_id,
+					],
+				],
+			],
+		], $result );
+
+		$this->assertEquals( 'publish', $post->post_status );
+	}
+
+	public function test_run__import_menu() {
+		// Arrange.
+		$importer = new WP_Import( __DIR__ . '/mock/nav_menu_item.xml' );
+
+		// Act.
+		$result = $importer->run();
+		die;
+		$posts = get_posts( [
+			'numberposts' => -1,
+			'post_status' => 'any',
+			'post_type' => 'nav_menu_item',
+		] );
+
+		// Assert.
+		$this->assertEquals( [
+			'status' => 'success',
+			'errors' => [],
+			'summary' => [
+				'categories' => 0,
+				'tags' => 0,
+				'terms' => 0,
+				'posts' => [
+					'failed' => [],
+					'succeed' => [
+						200 => 9,
+					],
+				],
+			],
+		], $result );
+		$this->assertCount( 1, $posts );
 	}
 }
