b031fce83ecdb010846a0b2b58eec5ea71f9d0c7
Rodolphe Bertozzo <56270626+rodolphebertozzo@users.noreply.github.com>
2022-02-20 20:31:33 +0100

    Update icon.php

--- includes/widgets/icon.php
+++ includes/widgets/icon.php
@@ -371,7 +371,7 @@ class Widget_Icon extends Widget_Base {
 			[
 				'label' => esc_html__( 'Border Radius', 'elementor' ),
 				'type' => Controls_Manager::DIMENSIONS,
-				'size_units' => [ 'px', '%' ],
+				'size_units' => [ 'px', '%', 'em' ],
 				'selectors' => [
 					'{{WRAPPER}} .elementor-icon' => 'border-radius: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
 				],
