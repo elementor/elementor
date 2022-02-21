6d752c6da75ae46239b8b1ccc49d8ea99d68c97d
StyleShit <lurkers2030@gmail.com>
2022-02-21 14:17:32 +0200

    WIP

--- modules/web-cli/module.php
+++ modules/web-cli/module.php
@@ -14,11 +14,18 @@ class Module extends BaseModule {
 	}
 
 	public function __construct() {
-		// TODO: move to hook
+		add_action( 'elementor/editor/before_enqueue_scripts', [ $this, 'register_scripts' ] );
+		add_action( 'admin_enqueue_scripts', [ $this, 'register_scripts' ] );
+		add_action( 'wp_enqueue_scripts', [ $this, 'register_scripts' ] );
+	}
+
+	public function register_scripts() {
 		wp_register_script(
 			'web-cli',
 			$this->get_js_assets_url( 'web-cli' ),
-			[],
+			[
+				'elementor-common-modules',
+			],
 			ELEMENTOR_VERSION,
 			true
 		);
