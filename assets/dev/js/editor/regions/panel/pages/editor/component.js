5e075b3b3535b79ecfcebbf6084bebf2dab4512f
StyleShit <lurkers2030@gmail.com>
2022-03-06 12:10:49 +0200

    Fix: Container is not functioning as expected [ED-6592]

--- assets/dev/js/editor/regions/panel/pages/editor/component.js
+++ assets/dev/js/editor/regions/panel/pages/editor/component.js
@@ -90,7 +90,7 @@ export default class Component extends ComponentBase {
 
 		const currentElement = elementor.getCurrentElement();
 
-		if ( ! currentElement ) {
+		if ( ! currentElement?.getCotnainer?.() ) {
 			return;
 		}
 
