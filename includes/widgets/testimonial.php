87ab7058d7bd21207e2ba5155bd9003271e7a573
Rodolphe Bertozzo <56270626+rodolphebertozzo@users.noreply.github.com>
2022-03-08 14:00:08 +0100

    Update testimonial.php

--- includes/widgets/testimonial.php
+++ includes/widgets/testimonial.php
@@ -414,7 +414,7 @@ class Widget_Testimonial extends Widget_Base {
 		$this->add_group_control(
 			Group_Control_Text_Stroke::get_type(),
 			[
-				'name' => 'text_stroke',
+				'name' => 'job_stroke',
 				'selector' => '{{WRAPPER}} .elementor-testimonial-job',
 			]
 		);
