1d87158ea027959a194ce056e3894b1fa6640dab
Guy Cohen <guyc@elementor.com>
2022-03-10 18:09:32 +0200

    Tweak: Add the ability to identify a kit [ED-6511] (#18024)

--- tests/phpunit/elementor/core/kits/test-manager.php
+++ tests/phpunit/elementor/core/kits/test-manager.php
@@ -21,6 +21,28 @@ class Test_Manager extends Elementor_Test_Base {
 		$this->assertEquals( $manager->get_active_id(), $id );
 	}
 
+	public function test_create_new_kit() {
+		// Arrange
+		$manager = Plugin::$instance->kits_manager;
+
+		// Act
+		$id = $manager->create_new_kit();
+
+		// Assert
+		$this->assertEquals( $manager->get_active_id(), $id );
+	}
+
+	public function test_create_new_kit_with_title_and_settings() {
+		// Arrange
+		$manager = Plugin::$instance->kits_manager;
+
+		// Act
+		$id = $manager->create_new_kit( 'test_title', ['test_setting' => 'test_setting'] );
+
+		// Assert
+		$this->assertEquals( $manager->get_active_id(), $id );
+	}
+
 	public function test_get_active_kit() {
 		// Arrange
 		$manager = Plugin::$instance->kits_manager;
