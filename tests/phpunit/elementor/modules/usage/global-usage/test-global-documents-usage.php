b9fc092eac6774746dc397f7831b3a4c9d0bf5e6
Leonid <czf.leo123@gmail.com>
2022-03-13 11:30:34 +0200

    Add test_remove().

--- tests/phpunit/elementor/modules/usage/global-usage/test-global-documents-usage.php
+++ tests/phpunit/elementor/modules/usage/global-usage/test-global-documents-usage.php
@@ -37,4 +37,31 @@ class Test_Global_Documents_Usage extends Elementor_Test_Base {
 			],
 		], $global_settings_page_usage->create_from_global()->get_collection()->all() );
 	}
+
+	public function test_remove() {
+		// Arrange.
+		$document = $this->factory()->create_post();
+		$document->save( [
+			'settings' => [
+				'background_background' => 'red',
+			],
+		] );
+
+		$global_settings_page_usage = new Global_Documents_Usage();
+
+		// Assert current.
+		$this->assertEqualSets( [
+			'wp-post' => [
+				'background_background' => 1,
+			],
+		], $global_settings_page_usage->create_from_global()->get_collection()->all() );
+
+		// Act - remove & save, manually add the same document to global usage.
+		$global_settings_page_usage->remove( $document );
+		$global_settings_page_usage->save_global();
+
+		// That's why it will be 2. since we are testing functionality of `save_global`.
+		// Assert.
+		$this->assertEmpty( $global_settings_page_usage->create_from_global()->get_collection()->all() );
+	}
 }
