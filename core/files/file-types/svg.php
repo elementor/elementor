e0d5661d268df9bf3c1117f1ff8eba4ad947727d
Filipe Seabra <filipe.seabra@pipefy.com>
2022-03-14 17:08:16 -0300

    #18155 - verify if svg file exists before updating _elementor_inline_svg

--- core/files/file-types/svg.php
+++ core/files/file-types/svg.php
@@ -290,7 +290,7 @@ class Svg extends Base {
 
 		$attachment_file = get_attached_file( $attachment_id );
 
-		if ( ! $attachment_file ) {
+		if ( ! $attachment_file || ! file_exists( $attachment_file ) ) {
 			return '';
 		}
 
