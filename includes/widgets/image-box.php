67976c0f2c9404023356c723425e18d0e9fb60d0
Rodolphe Bertozzo <56270626+rodolphebertozzo@users.noreply.github.com>
2022-03-15 00:35:32 +0100

    Update image-box.php

--- includes/widgets/image-box.php
+++ includes/widgets/image-box.php
@@ -550,14 +550,6 @@ class Widget_Image_Box extends Widget_Base {
 		);
 
 		$this->add_group_control(
-			Group_Control_Text_Stroke::get_type(),
-			[
-				'name' => 'description_stroke',
-				'selector' => '{{WRAPPER}} .elementor-image-box-title',
-			]
-		);
-
-		$this->add_group_control(
 			Group_Control_Text_Shadow::get_type(),
 			[
 				'name' => 'description_shadow',
