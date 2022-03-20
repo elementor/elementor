cdda8bef8c7c5c9f674c2e35279e52a90039ad4a
Leonid <czf.leo123@gmail.com>
2022-03-20 13:12:58 +0200

    Fix: Review request.

--- modules/nested-tabs/assets/js/index.js
+++ modules/nested-tabs/assets/js/index.js
@@ -1,4 +1,6 @@
 elementorCommon.elements.$window.on( 'elementor:init-components', async () => {
+	// The module should be loaded only when `nestedElements` is available.
 	await elementor.modules.nestedElements;
+
 	elementor.modules.nestedTabs = new ( await import( './editor/module' ) ).default();
 } );
