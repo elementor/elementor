db41f5fc86542edb51a0bf1a3ded2ea732d967ca
Leonid <czf.leo123@gmail.com>
2022-03-20 19:27:06 +0200

    WIP - Change technical name of `nested-tabs` to `tabs-v2`.

--- modules/tabs-v2/assets/js/index.js
+++ modules/tabs-v2/assets/js/index.js
@@ -2,5 +2,5 @@ elementorCommon.elements.$window.on( 'elementor:init-components', async () => {
 	// The module should be loaded only when `nestedElements` is available.
 	await elementor.modules.nestedElements;
 
-	elementor.modules.nestedTabs = new ( await import( './editor/module' ) ).default();
+	new ( await import( './editor/module' ) ).default();
 } );
