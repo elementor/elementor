0875905b65c0c2d258da7da48be2236ac341e6dc
Manor Hazaz <Manorh@elementor.com>
2022-03-23 09:05:56 +0200

    Linter changes

--- assets/lib/share-link/share-link.js
+++ assets/lib/share-link/share-link.js
@@ -18,14 +18,14 @@
 			} );
 
 			// Add "Enter" and "Space" event only if the element has role=button attribute.
-			if( 'button' === $element.attr( 'role' ) ) {
+			if ( 'button' === $element.attr( 'role' ) ) {
 				$element.on( 'keyup', ( event ) => {
 					if ( 13 === event.keyCode || 32 === event.keyCode ) {
 						event.preventDefault();
 
 						openShareLink( networkName );
 					}
-				});
+				} );
 			}
 		};
 
