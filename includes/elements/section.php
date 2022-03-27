b316d582eac77021e993e3081af6d459d68586e1
Tzvi Rabinovitch <tzvir@elementor.com>
2022-03-27 18:00:23 +0300

    Fix: Applying motion effects on a section disabled corner radius [ED-6877]

--- includes/elements/section.php
+++ includes/elements/section.php
@@ -837,7 +837,7 @@ class Element_Section extends Element_Base {
 				'type' => Controls_Manager::DIMENSIONS,
 				'size_units' => [ 'px', '%' ],
 				'selectors' => [
-					'{{WRAPPER}}, {{WRAPPER}} > .elementor-background-overlay' => 'border-radius: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
+					'{{WRAPPER}}, {{WRAPPER}} > .elementor-background-overlay, {{WRAPPER}} > .elementor-motion-effects-container' => 'border-radius: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
 				],
 			]
 		);
