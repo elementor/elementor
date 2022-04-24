beaef325cb211d1be12233b4f1c23397c0d10624
Leonid <czf.leo123@gmail.com>
2022-04-24 14:01:38 +0300

    Fix: Self review request.

--- modules/nested-elements/assets/js/index.js
+++ modules/nested-elements/assets/js/index.js
@@ -1,5 +1,8 @@
 elementorCommon.elements.$window.on( 'elementor:init-components', () => {
-	elementor.modules.nestedElements = import( './editor/module' ).then( ( module ) => {
+	// Put promise of loading so other modules can use/await it.
+	elementor.modules.nestedElements = import( './editor/module' );
+
+	elementor.modules.nestedElements.then( ( module ) => {
 		elementor.modules.nestedElements = new module.default;
 	} );
 } );
