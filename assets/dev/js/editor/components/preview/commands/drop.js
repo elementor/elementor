ba7565e87077ae66568bcec1a0e23591fc21b1e2
Leonid <czf.leo123@gmail.com>
2022-04-26 12:20:30 +0300

    Fix: Remove useless code.

--- assets/dev/js/editor/components/preview/commands/drop.js
+++ assets/dev/js/editor/components/preview/commands/drop.js
@@ -8,11 +8,6 @@ export class Drop extends $e.modules.CommandBase {
 	apply( args = {} ) {
 		const { containers = [ args.container ], options = {} } = args;
 
-		// Do not pass `args.options` to `createElementFromModel`.
-		if ( args.options ) {
-			delete args.options;
-		}
-
 		containers.forEach( ( container ) => {
 			container.view.createElementFromModel( args.model, options );
 		} );
