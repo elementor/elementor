b100290c6708c373d3c54701e09d69440e2f98fe
Leonid <czf.leo123@gmail.com>
2022-04-26 10:23:08 +0300

    Internal: Nested Infra - Fix nested element drop.

--- assets/dev/js/editor/components/preview/commands/drop.js
+++ assets/dev/js/editor/components/preview/commands/drop.js
@@ -1,5 +1,20 @@
 export class Drop extends $e.modules.CommandBase {
+	validateArgs( args = {} ) {
+		this.requireContainer( args );
+
+		this.requireArgumentType( 'model', 'object', args );
+	}
+
 	apply( args = {} ) {
-		return elementor.getPreviewContainer().view.createElementFromModel( args, args.options || {} );
+		const { containers = [ args.container ], options = {} } = args;
+
+		// Do not pass `args.options` to `createElementFromModel`.
+		if ( args.options ) {
+			delete args.options;
+		}
+
+		containers.forEach( ( container ) => {
+			container.view.createElementFromModel( args.model, options );
+		} );
 	}
 }
