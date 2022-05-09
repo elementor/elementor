7b045e0982955c2ebe4d83e307b83564a8366dc4
Leonid <czf.leo123@gmail.com>
2022-05-09 14:19:37 +0300

    Fix

--- assets/dev/js/editor/elements/views/base-widget.js
+++ assets/dev/js/editor/elements/views/base-widget.js
@@ -35,6 +35,17 @@ class BaseWidgetView extends BaseElementView {
 		return baseClasses + ' elementor-widget ' + elementor.getElementData( this.getEditModel() ).html_wrapper_class;
 	}
 
+	normalizeWidgetAttributes() {
+		const editModel = this.getEditModel(),
+			skinType = editModel.getSetting( '_skin' ) || 'default';
+
+		this.$el
+			.attr( 'data-widget_type', editModel.get( 'widgetType' ) + '.' + skinType )
+			.removeClass( 'elementor-widget-empty' )
+			.children( '.elementor-widget-empty-icon' )
+			.remove();
+	}
+
 	getTemplate() {
 		const editModel = this.getEditModel();
 
