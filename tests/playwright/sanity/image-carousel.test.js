9e33e6016cb9e4067d318f254eb28dc3b87e021f
Shay <shay@elementor.com>
2022-05-12 14:56:12 +0300

    reduce the screenshots sizes

--- tests/playwright/sanity/image-carousel.test.js
+++ tests/playwright/sanity/image-carousel.test.js
@@ -1,12 +1,14 @@
 const { test, expect } = require( '@playwright/test' );
 const WpAdminPage = require( '../pages/wp-admin-page.js' );
 
-test.only( 'Image Carousel', async ( { page }, testInfo ) => {
+test( 'Image Carousel', async ( { page }, testInfo ) => {
 	// Arrange.
 	const wpAdmin = new WpAdminPage( page, testInfo ),
 		editor = await wpAdmin.useElementorCleanPost();
+
   //close Navigator
   await page.click('#elementor-navigator__close');
+
 	// Act.
 	await editor.addWidget( 'image-carousel' );
 	
@@ -15,19 +17,24 @@ test.only( 'Image Carousel', async ( { page }, testInfo ) => {
 
   // Click text=Media Library
   await page.click('text=Media Library');
-  // Upload A.png, B.png, C.png, D.png, E.png
+
+  // Upload the images to WP media library 
   await page.setInputFiles( 'input[type="file"]', './tests/playwright/resources/A.jpg' );
   await page.setInputFiles( 'input[type="file"]', './tests/playwright/resources/B.jpg' );
   await page.setInputFiles( 'input[type="file"]', './tests/playwright/resources/C.jpg' );
   await page.setInputFiles( 'input[type="file"]', './tests/playwright/resources/D.jpg' );
   await page.setInputFiles( 'input[type="file"]', './tests/playwright/resources/E.jpg' );
+
  // Click text=Create a new gallery
   await page.locator('text=Create a new gallery').click();
+
   // Click text=Insert gallery
   await page.locator('text=Insert gallery').click();
-    // Click #elementor-controls >> :nth-match(div:has-text("Additional Options"), 3)
+
+    // Open Additional options
     await page.click('#elementor-controls >> :nth-match(div:has-text("Additional Options"), 3)');
+
     // Disable AutoPlay
     await page.selectOption('select', 'no');
-  expect(await editor.getPreviewFrame().locator('div.elementor-image-carousel-wrapper.swiper-container.swiper-container-initialized').screenshot()).toMatchSnapshot('carousel.png');
+  expect(await editor.getPreviewFrame().locator('div.elementor-image-carousel-wrapper.swiper-container.swiper-container-initialized').screenshot({type: 'jpeg', quality: 70})).toMatchSnapshot('carousel.jpeg');
 } );
