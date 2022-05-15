d7e9850ae38565911705b16488d8a3077e412f43
Guy Cohen <guyc@elementor.com>
2022-05-15 18:18:02 +0300

    Fix update of svg icons

--- assets/lib/font-awesome/update-elementor-files.php
+++ assets/lib/font-awesome/update-elementor-files.php
@@ -1,9 +1,12 @@
 <?php
 // How to update a version for Font Awesome:
-// 1. Download the new version of Font Awesome
-// 2. Copy /js and /json folders(created by elementor) from old font-awesome to new font-awesome.
-// 3. run this script.
+// 1. Download the new version of Font Awesome.
+// 2. Change $old_version to the version we are updating from.
+// 3. Copy /js and /json folders(created by elementor) from old font-awesome to new font-awesome.
+// 4. run this script.
 // Files in /js and /json folders should be updated with the new icons.
+
+$old_version = '5.15.3';
 
 // Metadata - All icons data
 $metadata = file_get_contents( 'metadata/icons.json' );
@@ -21,16 +24,25 @@ foreach ( $icons_groups as $icon_group ) {
 	$group_svg_array = json_decode( $group_svg, true );
 
 	// Get new group icons from latest version
-	$new_group = array_filter( $metadata_array, function( $icon, $icon_name ) use ( $group_array, $icon_group ) {
+	$updated_group = array_filter( $metadata_array, function( $icon, $icon_name ) use ( $old_version, $icon_group ) {
 		if ( in_array( $icon_group, $icon['styles'], true ) ) {
-			return ! in_array( (string) $icon_name, $group_array['icons'], true );
+			$icon_has_changed = false;
+
+			foreach ( $icon['changes'] as $version ) {
+				if( version_compare( $old_version , $version, '<' ) ) {
+					$icon_has_changed = true;
+					break;
+				}
+			}
+
+			return $icon_has_changed;
 		}
 	}, ARRAY_FILTER_USE_BOTH );
 
 	$pattern = '/(?<=\<path d\=\").*(?=\"\/><\/svg>)/';
 	$match_svg_path = [];
-	foreach ( $new_group as $icon_name => $icon ) {
-		// Add $new_group svg to $group_svg_array icons
+	foreach ( $updated_group as $icon_name => $icon ) {
+		// Add or update $updated_group svg to $group_svg_array icons
 		preg_match( $pattern, $icon['svg'][ $icon_group ]['raw'], $match_svg_path );
 
 		$group_svg_array['icons'][ $icon_name ] = [
@@ -40,8 +52,11 @@ foreach ( $icons_groups as $icon_group ) {
 			$icon['unicode'],
 			$match_svg_path[0],
 		];
-		// Add $new_group to $group_array
-		$group_array['icons'][] = (string) $icon_name;
+		
+		// Add $updated_group to $group_array
+		if ( ! in_array( (string) $icon_name, $group_array['icons'], true ) ) {
+			$group_array['icons'][] = (string) $icon_name;
+		}
 	}
 
 	// Arrange $group_array and $group_svg_array by alphabetical order
