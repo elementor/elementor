a7521ca3a8f7d35b526eaff4d68233bd2a948456
Koen Van den Wijngaert <koen@neok.be>
2022-05-16 09:04:22 +0200

    fix(command): get all sites instead of first 100
    This fix addresses issue #18639 where all multisite-related commands
    only loop over the first 100 sites, due to the default value of the
    `number` argument for the `WP_Site_Query` used by the `get_sites`
    function.
    
    Since we only ever use the `blog_id`, I've limited the results to being
    an array of `blog_id`s as well, so we don't clog up the memory with
    `WP_Site` objects that remain unused.

--- includes/maintenance.php
+++ includes/maintenance.php
@@ -36,7 +36,7 @@ class Maintenance {
 
 		if ( is_multisite() && $network_wide ) {
 			static::create_default_kit(
-				get_sites( [ 'fields' => 'ids' ] )
+				get_sites( [ 'fields' => 'ids', 'number' => 0 ] )
 			);
 
 			return;
