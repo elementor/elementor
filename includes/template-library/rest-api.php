f31c5dd432aa7a515219a92176ec77b86738f4e4
Nevo Golan <nevog@elementor.com>
2022-05-31 20:59:58 +0300

    wip

--- includes/template-library/rest-api.php
+++ includes/template-library/rest-api.php
@@ -27,35 +27,52 @@ class REST_API {
 	 * @return void
 	 */
 	private function add_document_type_rest_field() {
-		$document_types = Plugin::$instance->documents->get_document_types( [
-			'show_in_library' => true,
-		] );
-
 		register_rest_field(
 			Source_Local::CPT,
 			'document_type',
 			[
-				'get_callback' => function ( array $object ) {
-					return get_post_meta( $object['id'], Document::TYPE_META_KEY, true );
-				},
-				'update_callback' => function ( $value, \WP_Post $object ) {
-					$document = Plugin::$instance->documents->get( $object->ID );
-
-					if ( ! $document ) {
-						return;
-					}
-
-					$document
-						->set_is_built_with_elementor( true )
-						->update_meta( Document::TYPE_META_KEY, $value );
-				},
-				'schema' => [
-					'description' => __( 'Elementor document type.', 'elementor' ),
-					'type' => 'string',
-					'required' => true,
-					'enum' => array_keys( $document_types ),
-				],
+				'get_callback' => $this->get_document_type_get_callback(),
+				'update_callback' => $this->get_document_type_update_callback(),
+				'schema' => $this->get_document_type_schema(),
 			]
 		);
+	}
+
+	/**
+	 * @return \Closure
+	 */
+	private function get_document_type_get_callback() {
+		return function ( array $object ) {
+			return get_post_meta( $object['id'], Document::TYPE_META_KEY, true );
+		};
+	}
+
+	/**
+	 * @return \Closure
+	 */
+	private function get_document_type_update_callback() {
+		return function ( $value, \WP_Post $object ) {
+			Plugin::$instance
+				->documents
+				->get( $object->ID, false )
+				->set_is_built_with_elementor( true )
+				->update_meta( Document::TYPE_META_KEY, $value );
+		};
+	}
+
+	/**
+	 * @return array
+	 */
+	private function get_document_type_schema() {
+		$document_types = Plugin::$instance->documents->get_document_types( [
+			'show_in_library' => true,
+		] );
+
+		return [
+			'description' => __( 'Elementor document type.', 'elementor' ),
+			'type' => 'string',
+			'required' => true,
+			'enum' => array_keys( $document_types ),
+		];
 	}
 }
