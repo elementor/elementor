dfab5ad16a655fa8330a71e7eb77c7ddd85f280e
Leonid <czf.leo123@gmail.com>
2022-06-01 15:49:41 +0300

    Fix: Remove - Delete 'delete' from Building blocks context menu

--- modules/nested-elements/assets/js/editor/nested-repeater/component.js
+++ modules/nested-elements/assets/js/editor/nested-repeater/component.js
@@ -15,6 +15,25 @@ export default class Component extends $e.modules.ComponentBase {
 		super.registerAPI();
 
 		elementor.addControlView( 'nested-elements-repeater', RepeaterControl );
+
+		// Disabling 'delete' from context menu of building blocks containers.
+		const callback = ( groups, view ) => {
+			const model = view.options.model;
+
+			// Remove the 'delete' item from the container context menu.
+			if ( 'container' === model.get( 'elType' ) ) {
+				const newValues = [ ... groups ],
+					deleteItemIndex = newValues.findIndex( ( item ) => 'delete' === item?.name );
+
+				delete newValues[ deleteItemIndex ];
+
+				groups = newValues;
+			}
+
+			return groups;
+		};
+
+		elementor.hooks.addFilter( 'elements/container/contextMenuGroups', callback );
 	}
 
 	getNamespace() {
