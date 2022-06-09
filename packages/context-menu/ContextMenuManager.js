115a676d7d854b24e224bcaca5c0b2d79dc20b6e
Nahman ALLOUCHE <nahmana@elementor.com>
2022-06-09 21:54:29 +0300

    some fixes

---
+++ packages/context-menu/ContextMenuManager.js
@@ -0,0 +1,141 @@
+export class ContextMenuManager {
+	groups = [];
+
+	allowedProperties = {
+		group: [ 'id', 'title', 'disabled', 'parentItemId' ],
+		item: [ 'id', 'label', 'icon', 'shortcut', 'badge', 'action', 'onMouseEnter', 'onMouseLeave' ],
+	};
+
+	groupDefault = {
+		id: '',
+		title: '',
+		disabled: false,
+		parentItemId: '',
+		items: [],
+	};
+
+	itemDefault = {
+		id: '',
+		label: '',
+		icon: false,
+		shortcut: '',
+		badge: null,
+		action: null,
+		onMouseEnter: null,
+		onMouseLeave: null,
+		groups: [],
+	};
+
+	getGroup( groupId ) {
+		return this.groups.find( ( group ) => group.id === groupId );
+	}
+
+	groupExists( groupId ) {
+		return !! this.getGroup( groupId );
+	}
+
+	getItem( groupId, itemId ) {
+		const currentGroup = this.groups.find( ( group ) => group.id === groupId );
+		return currentGroup.items.find( ( item ) => item.id === itemId );
+	}
+
+	itemExists( groupId, itemId ) {
+		return !! this.getItem( groupId, itemId );
+	}
+
+	updateGroupData( groupId, groupData ) {
+		const groupIndex = this.groups.findIndex( ( group ) => group.id === groupId );
+
+		if ( ! this.groups[ groupIndex ] ) {
+			return;
+		}
+
+		this.groups[ groupIndex ] = { ...this.groups[ groupIndex ], ...groupData };
+	}
+
+	createGroupsTree( parentItemId = '' ) {
+		return this.groups
+			.filter( ( group ) => group.parentItemId === parentItemId )
+			.map( ( rootGroup ) => {
+				rootGroup.items = rootGroup.items.map( ( item ) => {
+					item.groups = this.createGroupsTree( item.id );
+
+					return item;
+				} );
+
+				return rootGroup;
+		} );
+	}
+
+	sanitizeProperties( allowed, newObject ) {
+		return Object.fromEntries(
+			Object.entries( newObject )
+				.filter( ( [ key ] ) => allowed.includes( key ) )
+		);
+	}
+
+	getConfig() {
+		return this.createGroupsTree();
+	}
+
+	createGroup( newGroup ) {
+		if ( ! newGroup.id || this.groupExists( newGroup.id ) ) {
+			return;
+		}
+
+		this.groups.push( {
+			...structuredClone( this.groupDefault ),
+			...this.sanitizeProperties( this.allowedProperties.group, newGroup ),
+		} );
+	}
+
+	updateGroup( groupId, groupData ) {
+		this.updateGroupData( groupId, this.sanitizeProperties( this.allowedProperties.group, groupData ) );
+	}
+
+	deleteGroup( groupId ) {
+		this.groups = this.groups.filter( ( group ) => group.id !== groupId );
+	}
+
+	addItem( groupId, newItem ) {
+		const groupIndex = this.groups.findIndex( ( group ) => group.id === groupId );
+		if ( ! newItem.id || ! this.groups[ groupIndex ] || this.itemExists( groupId, newItem.id ) ) {
+			return;
+		}
+
+		this.groups[ groupIndex ].items.push( {
+			...structuredClone( this.itemDefault ),
+			...this.sanitizeProperties( this.allowedProperties.item, newItem ),
+		} );
+	}
+
+	updateItem( groupId, itemId, itemData ) {
+		const currentGroup = this.getGroup( groupId );
+
+		if ( ! currentGroup ) {
+			return;
+		}
+
+		const itemIndex = currentGroup.items.findIndex( ( item ) => item.id === itemId );
+
+		if ( ! currentGroup.items[ itemIndex ] ) {
+			return;
+		}
+
+		itemData = this.sanitizeProperties( this.allowedProperties.item, itemData );
+		currentGroup.items[ itemIndex ] = { ...currentGroup.items[ itemIndex ], ...itemData };
+		this.updateGroupData( groupId, currentGroup );
+	}
+
+	deleteItem( groupId, itemId ) {
+		const currentGroup = this.getGroup( groupId );
+
+		if ( ! currentGroup ) {
+			return;
+		}
+
+		currentGroup.items = currentGroup.items.filter( ( item ) => item.id !== itemId );
+		this.updateGroupData( groupId, currentGroup );
+	}
+}
+
--- tests/jest/unit/packages/context-menu/ContextMenuManager.test.js
+++ tests/jest/unit/packages/context-menu/ContextMenuManager.test.js
@@ -1,13 +1,16 @@
-import { ContextMenuManager } from 'elementor/packages/context-menu/manager';
+import { ContextMenuManager } from 'elementor/packages/context-menu/ContextMenuManager';
 
-describe( 'Context menu manager', () => {
+describe( 'ContextMenuManager', () => {
 	beforeAll( () => {
+		// The global 'structuredClone' is not supported yet at jest.
 		global.structuredClone = ( object ) => JSON.parse( JSON.stringify( object ) );
 	} );
 
 	test( 'Check main flow', () => {
+		// Arrange
 		const manager = new ContextMenuManager();
 
+		// Act
 		manager.createGroup(
 			{ id: 'groupId-1' }
 		);
@@ -26,7 +29,8 @@ describe( 'Context menu manager', () => {
 			{ id: 'item-2' }
 		);
 
-		expect( manager.getConfig( {} ) ).toEqual( [
+		// Assert
+		expect( manager.getConfig() ).toEqual( [
 			{
 				title: '',
 				disabled: false,
@@ -39,8 +43,8 @@ describe( 'Context menu manager', () => {
 						shortcut: '',
 						badge: null,
 						action: null,
-						mouseEnter: null,
-						mouseLeave: null,
+						onMouseEnter: null,
+						onMouseLeave: null,
 						id: 'item-1',
 						groups: [
 							{
@@ -60,8 +64,8 @@ describe( 'Context menu manager', () => {
 						shortcut: '',
 						badge: null,
 						action: null,
-						mouseEnter: null,
-						mouseLeave: null,
+						onMouseEnter: null,
+						onMouseLeave: null,
 					},
 				],
 			},
@@ -69,15 +73,18 @@ describe( 'Context menu manager', () => {
 	} );
 
 	test( 'updateGroup', () => {
+		// Arrange
 		const manager = new ContextMenuManager();
 
 		manager.createGroup(
 			{ id: 'groupId-1' }
 		);
 
+		// Act
 		manager.updateGroup( 'groupId-1', { title: 'I am a title' } );
 
-		expect( manager.getConfig( {} ) ).toEqual( [
+		// Assert
+		expect( manager.getConfig() ).toEqual( [
 			{
 				title: 'I am a title',
 				disabled: false,
@@ -89,6 +96,7 @@ describe( 'Context menu manager', () => {
 	} );
 
 	test( 'updateItem', () => {
+		// Arrange
 		const manager = new ContextMenuManager();
 
 		manager.createGroup(
@@ -100,9 +108,11 @@ describe( 'Context menu manager', () => {
 			{ id: 'item-1' }
 		);
 
+		// Act
 		manager.updateItem( 'groupId-1', 'item-1', { shortcut: '^ ctrl' } );
 
-		expect( manager.getConfig( {} ) ).toEqual( [
+		// Assert
+		expect( manager.getConfig() ).toEqual( [
 			{
 				title: '',
 				disabled: false,
@@ -115,8 +125,8 @@ describe( 'Context menu manager', () => {
 						shortcut: '^ ctrl',
 						badge: null,
 						action: null,
-						mouseEnter: null,
-						mouseLeave: null,
+						onMouseEnter: null,
+						onMouseLeave: null,
 						id: 'item-1',
 						groups: [],
 					},
@@ -126,6 +136,7 @@ describe( 'Context menu manager', () => {
 	} );
 
 	test( 'deleteGroup', () => {
+		// Arrange
 		const manager = new ContextMenuManager();
 
 		manager.createGroup(
@@ -136,9 +147,11 @@ describe( 'Context menu manager', () => {
 			{ id: 'groupId-2' }
 		);
 
+		// Act
 		manager.deleteGroup( 'groupId-1' );
 
-		expect( manager.getConfig( {} ) ).toEqual( [
+		// Assert
+		expect( manager.getConfig() ).toEqual( [
 			{
 				title: '',
 				disabled: false,
@@ -150,6 +163,7 @@ describe( 'Context menu manager', () => {
 	} );
 
 	test( 'deleteItem', () => {
+		// Arrange
 		const manager = new ContextMenuManager();
 
 		manager.createGroup(
@@ -166,9 +180,11 @@ describe( 'Context menu manager', () => {
 			{ id: 'item-2' }
 		);
 
+		// Act
 		manager.deleteItem( 'groupId-1', 'item-1' );
 
-		expect( manager.getConfig( {} ) ).toEqual( [
+		// Assert
+		expect( manager.getConfig() ).toEqual( [
 			{
 				title: '',
 				disabled: false,
@@ -181,8 +197,8 @@ describe( 'Context menu manager', () => {
 						shortcut: '',
 						badge: null,
 						action: null,
-						mouseEnter: null,
-						mouseLeave: null,
+						onMouseEnter: null,
+						onMouseLeave: null,
 						id: 'item-2',
 						groups: [],
 					},
@@ -192,8 +208,10 @@ describe( 'Context menu manager', () => {
 	} );
 
 	test( 'Validation of not authorized properties', () => {
+		// Arrange
 		const manager = new ContextMenuManager();
 
+		// Act
 		manager.createGroup(
 			{ id: 'groupId-1', randomProperty: 7 }
 		);
@@ -203,7 +221,8 @@ describe( 'Context menu manager', () => {
 			{ id: 'item-1', groups: [ { id: 'some-id' } ] } // Cannot explicitly add groups inside items.
 		);
 
-		expect( manager.getConfig( {} ) ).toEqual( [
+		// Assert
+		expect( manager.getConfig() ).toEqual( [
 			{
 				title: '',
 				disabled: false,
@@ -216,13 +235,133 @@ describe( 'Context menu manager', () => {
 						shortcut: '',
 						badge: null,
 						action: null,
-						mouseEnter: null,
-						mouseLeave: null,
+						onMouseEnter: null,
+						onMouseLeave: null,
 						id: 'item-1',
 						groups: [],
 					},
 				],
 			},
 		] );
+	} );
+
+	test( 'Validation of not existing', () => {
+		// Arrange
+		const manager = new ContextMenuManager();
+
+		manager.createGroup(
+			{ id: 'groupId-1' }
+		);
+
+		// Act
+		manager.addItem(
+			'groupId-18',
+			{ id: 'item-1' }
+		);
+
+		manager.updateGroup( 'groupId-7', { title: 'I am a title' } );
+
+		// Assert
+		expect( manager.getConfig() ).toEqual( [
+			{
+				title: '',
+				disabled: false,
+				id: 'groupId-1',
+				parentItemId: '',
+				items: [],
+			},
+		] );
+	} );
+
+	test( 'Preventing duplicating groups and items', () => {
+		// Arrange
+		const manager = new ContextMenuManager();
+
+		manager.createGroup(
+			{ id: 'groupId-1' }
+		);
+
+		manager.addItem(
+			'groupId-1',
+			{ id: 'item-1' }
+		);
+
+		// Act
+		manager.createGroup(
+			{ id: 'groupId-1' }
+		);
+
+		manager.addItem(
+			'groupId-1',
+			{ id: 'item-1' }
+		);
+
+		// Assert
+		expect( manager.getConfig() ).toEqual( [
+			{
+				title: '',
+				disabled: false,
+				id: 'groupId-1',
+				parentItemId: '',
+				items: [
+					{
+						label: '',
+						icon: false,
+						shortcut: '',
+						badge: null,
+						action: null,
+						onMouseEnter: null,
+						onMouseLeave: null,
+						id: 'item-1',
+						groups: [],
+					},
+				],
+			},
+		] );
+	} );
+
+	test( 'Not displaying groups that their parentItemId points to non-existing items', () => {
+		// Arrange
+		const manager = new ContextMenuManager();
+
+		manager.createGroup(
+			{ id: 'groupId-1' }
+		);
+
+		// Act
+		manager.createGroup(
+			{ id: 'groupId-2', parentItemId: 'groupId-7' }
+		);
+
+		// Assert
+		expect( manager.getConfig() ).toEqual( [
+			{
+				title: '',
+				disabled: false,
+				id: 'groupId-1',
+				parentItemId: '',
+				items: [],
+			},
+		] );
+	} );
+
+	test( 'Not displaying groups that their parentItemId points to self descendant items', () => {
+		// Arrange
+		const manager = new ContextMenuManager();
+
+		manager.createGroup(
+			{ id: 'groupId-1' }
+		);
+
+		manager.addItem(
+			'groupId-1',
+			{ id: 'item-1' }
+		);
+
+		// Act
+		manager.updateGroup( 'groupId-1', { parentItemId: 'item-1' } );
+
+		// Assert
+		expect( manager.getConfig() ).toEqual( [] );
 	} );
 } );
