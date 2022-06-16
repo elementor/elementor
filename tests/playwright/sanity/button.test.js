8213e70e3145d4d3e89f08efc69e9d9ee86e2650
Nevo Golan <nevos12@gmail.com>
2022-06-16 17:23:52 +0300

    Fix: Choose controls are not working as expected [ED-7373] (#18916)

--- tests/playwright/sanity/button.test.js
+++ tests/playwright/sanity/button.test.js
@@ -14,3 +14,29 @@ test( 'Button widget sanity test', async ( { page }, testInfo ) => {
 	// Assert.
 	expect( await button.innerText() ).toBe( 'Click here' );
 } );
+
+test( 'Button controls should return to default', async ( { page }, testInfo ) => {
+	// Arrange.
+	const wpAdmin = new WpAdminPage( page, testInfo ),
+		editor = await wpAdmin.useElementorCleanPost();
+
+	editor.addWidget( 'button' );
+
+	await editor.getPreviewFrame().waitForSelector( 'a[role="button"]:has-text("Click here")' );
+
+	const widget = await editor.getPreviewFrame().locator( 'div[data-element_type="widget"]' ),
+		controlSeletor = 'div.elementor-control-responsive-desktop:has-text("Alignment") label[data-tooltip="Center"]',
+		alignCenterClassRegex = /elementor-align-center/;
+
+	// Act
+	await editor.page.click( controlSeletor );
+
+	// Assert
+	await expect( widget ).toHaveClass( alignCenterClassRegex );
+
+	// Act
+	await editor.page.click( controlSeletor );
+
+	// Assert
+	await expect( widget ).not.toHaveClass( alignCenterClassRegex );
+} );
