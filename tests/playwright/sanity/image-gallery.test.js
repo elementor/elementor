bde7fe42247d874f8c85e2dd155e1f7a84a5967f
Shay <42204748+ShayHel@users.noreply.github.com>
2022-06-16 19:20:12 +0300

    Update image-gallery.test.js

--- tests/playwright/sanity/image-gallery.test.js
+++ tests/playwright/sanity/image-gallery.test.js
@@ -3,32 +3,32 @@ const WpAdminPage = require( '../pages/wp-admin-page.js' );
 
 test.only( 'Image Carousel', async ( { page }, testInfo ) => {
 	// Arrange.
-const wpAdmin = new WpAdminPage( page, testInfo ),
-      editor = await wpAdmin.useElementorCleanPost();
+	const wpAdmin = new WpAdminPage( page, testInfo ),
+	editor = await wpAdmin.useElementorCleanPost();
+	
+	// Close Navigator
+	await page.click( '#elementor-navigator__close' );
+	
+	// Act.
+	await editor.addWidget( 'image-gallery' );
 
-  // Close Navigator
-  await page.click( '#elementor-navigator__close' );
+	await page.locator( '[aria-label="Add Images"]' ).click();
 
-  // Act.
-  await editor.addWidget( 'image-gallery' );
+	// Open Media Library
+	await page.click( 'text=Media Library' );
+	
+	// Upload the images to WP media library
+	await page.setInputFiles( 'input[type="file"]', './tests/playwright/resources/A.jpg' );
+	await page.setInputFiles( 'input[type="file"]', './tests/playwright/resources/B.jpg' );
+	await page.setInputFiles( 'input[type="file"]', './tests/playwright/resources/C.jpg' );
+	await page.setInputFiles( 'input[type="file"]', './tests/playwright/resources/D.jpg' );
+	await page.setInputFiles( 'input[type="file"]', './tests/playwright/resources/E.jpg' );
 
-  await page.locator( '[aria-label="Add Images"]' ).click();
+	// Create a new gallery
+	await page.locator( 'text=Create a new gallery' ).click();
 
-  // Open Media Library
-  await page.click( 'text=Media Library' );
+	// Insert gallery
+	await page.locator( 'text=Insert gallery' ).click();
 
-  // Upload the images to WP media library
-  await page.setInputFiles( 'input[type="file"]', './tests/playwright/resources/A.jpg' );
-  await page.setInputFiles( 'input[type="file"]', './tests/playwright/resources/B.jpg' );
-  await page.setInputFiles( 'input[type="file"]', './tests/playwright/resources/C.jpg' );
-  await page.setInputFiles( 'input[type="file"]', './tests/playwright/resources/D.jpg' );
-  await page.setInputFiles( 'input[type="file"]', './tests/playwright/resources/E.jpg' );
-
-  // Create a new gallery
-  await page.locator( 'text=Create a new gallery' ).click();
-
-  // Insert gallery
-  await page.locator( 'text=Insert gallery' ).click();
-
-  expect( await editor.getPreviewFrame().locator( 'div#gallery-1' ).screenshot( { type: 'jpeg', quality: 70 } ) ).toMatchSnapshot( 'gallery.jpeg' );
+	expect( await editor.getPreviewFrame().locator( 'div#gallery-1' ).screenshot( { type: 'jpeg', quality: 70 } ) ).toMatchSnapshot( 'gallery.jpeg' );
 } );
