8c203a09e159cee55c6d6cd1d47d0c21ff33a42c
Shay <shay@elementor.com>
2022-06-19 14:04:48 +0300

    Accordion Playwright sanity test

--- tests/playwright/sanity/accordion.test.js
+++ tests/playwright/sanity/accordion.test.js
@@ -12,28 +12,5 @@ test( 'Accordion', async ( { page }, testInfo ) => {
 	// Act.
 	await editor.addWidget( 'accordion' );
 
-  await page.locator( '[aria-label="Add Images"]' ).click();
-
-  // Open Media Library
-  await page.click( 'text=Media Library' );
-
-  // Upload the images to WP media library
-  await page.setInputFiles( 'input[type="file"]', './tests/playwright/resources/A.jpg' );
-  await page.setInputFiles( 'input[type="file"]', './tests/playwright/resources/B.jpg' );
-  await page.setInputFiles( 'input[type="file"]', './tests/playwright/resources/C.jpg' );
-  await page.setInputFiles( 'input[type="file"]', './tests/playwright/resources/D.jpg' );
-  await page.setInputFiles( 'input[type="file"]', './tests/playwright/resources/E.jpg' );
-
-  // Create a new gallery
-  await page.locator( 'text=Create a new gallery' ).click();
-
-  // Insert gallery
-  await page.locator( 'text=Insert gallery' ).click();
-
-  // Open The Additional options Section
-  await page.click( '#elementor-controls >> :nth-match(div:has-text("Additional Options"), 3)' );
-
-  // Disable AutoPlay
-  await page.selectOption( 'select', 'no' );
-  expect( await editor.getPreviewFrame().locator( 'div.elementor-image-carousel-wrapper.swiper-container.swiper-container-initialized' ).screenshot( { type: 'jpeg', quality: 70 } ) ).toMatchSnapshot( 'accordion.jpeg' );
+  expect( await editor.getPreviewFrame().locator( '.elementor-widget-wrap > .elementor-background-overlay' ).screenshot( { type: 'jpeg', quality: 70 } ) ).toMatchSnapshot( 'accordion.jpeg' );
 } );
