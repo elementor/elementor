67c0573ecb7fe783c0bd75136826839fef6607b8
Hein van Vlastuin <vv@abanganimedia.co.za>
2022-06-20 12:26:59 +0200

    pr tweaks

--- assets/dev/js/editor/elements/views/container.js
+++ assets/dev/js/editor/elements/views/container.js
@@ -188,13 +188,13 @@ const ContainerView = BaseElementView.extend( {
 	addNewContainer() {
 		/* Check if the current container has a parent container */
 		const containerAncestry = this.getContainer().getParentAncestry(),
-			containerParentSelector = ( 'container' !== containerAncestry[ 1 ].type ) ? this.getContainer() : this.getContainer().parent;
+			targetContainer = ( 'container' !== containerAncestry[ 1 ].type ) ? this.getContainer() : this.getContainer().parent;
 
 		$e.run( 'document/elements/create', {
 			model: {
 				elType: 'container',
 			},
-			container: containerParentSelector,
+			container: targetContainer,
 		} );
 	},
 
