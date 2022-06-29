4366c167fde1bfde945502fd36b7c681999dc8af
Maor-Kavod <maork@elementor.com>
2022-06-29 11:50:12 +0300

    lint

--- includes/managers/image.php
+++ includes/managers/image.php
@@ -150,7 +150,7 @@ class Images_Manager {
 
 	private function delete_custom_images( $post_id ) {
 		$image_meta = wp_get_attachment_metadata( $post_id );
-		if(empty($image_meta)){
+		if( empty( $image_meta ) ){
 			return;
 		}
 		( new Collection( $image_meta['sizes'] ) )
