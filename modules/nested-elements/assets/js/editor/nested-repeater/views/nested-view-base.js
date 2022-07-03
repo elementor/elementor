84223289b7380a587d10bb0602a128e28a141882
Nevo Golan <nevog@elementor.com>
2022-07-03 11:22:24 +0300

    wip

--- modules/nested-elements/assets/js/editor/nested-repeater/views/nested-view-base.js
+++ modules/nested-elements/assets/js/editor/nested-repeater/views/nested-view-base.js
@@ -1,11 +1,6 @@
 export default class NestedViewBase extends elementor.modules.elements.views.BaseWidget {
-	/**
-	 * Sometimes the children placement is not in the end of the element, but somewhere else, eg: deep inside the element template.
-	 * If `defaults_placeholder_selector` is set, it will be used to find the correct place to insert the children.
-	 *
-	 * @param  containerView
-	 * @param  childView
-	 */
+	// Sometimes the children placement is not in the end of the element, but somewhere else, eg: deep inside the element template.
+	// If `defaults_placeholder_selector` is set, it will be used to find the correct place to insert the children.
 	getChildViewContainer( containerView, childView ) {
 		const customSelector = this.model.config.defaults.elements_placeholder_selector;
 
