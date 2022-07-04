ee27beae295c158764c8f1522401d6d6f1a34ace
Hazaz <manorh@elementor.com>
2022-07-04 10:48:49 +0300

    WIP

--- assets/dev/js/editor/regions/panel/pages/menu/menu.js
+++ assets/dev/js/editor/regions/panel/pages/menu/menu.js
@@ -155,7 +155,7 @@ PanelMenu.createExitIntroductionDialog = () => {
 				elementor.config.user.introduction.exit_to = true;
 				PanelMenu.exitShouldRedirect = true;
 			},
-			onConfirm: () => {
+			onConfirm: async () => {
 				$e.run( 'document/elements/settings', {
 					container: elementor.settings.editorPreferences.getEditedView().getContainer(),
 					settings: {
@@ -166,7 +166,7 @@ PanelMenu.createExitIntroductionDialog = () => {
 					},
 				} );
 
-				elementor.settings.editorPreferences.save();
+				await elementor.settings.editorPreferences.save();
 				window.location.href = PanelMenu.getExitUrl();
 			},
 			onCancel: () => {
