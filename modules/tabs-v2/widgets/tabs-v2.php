0bdd4cbe5f3356afd126e75a154fc0013201eed0
Hein van Vlastuin <vv@abanganimedia.co.za>
2022-07-05 15:24:22 +0200

    border control include

--- modules/tabs-v2/widgets/tabs-v2.php
+++ modules/tabs-v2/widgets/tabs-v2.php
@@ -5,6 +5,7 @@ use Elementor\Controls_Manager;
 use Elementor\Core\Kits\Documents\Tabs\Global_Colors;
 use Elementor\Core\Kits\Documents\Tabs\Global_Typography;
 use Elementor\Group_Control_Background;
+use Elementor\Group_Control_Border;
 use Elementor\Group_Control_Box_Shadow;
 use Elementor\Group_Control_Text_Shadow;
 use Elementor\Group_Control_Text_Stroke;
