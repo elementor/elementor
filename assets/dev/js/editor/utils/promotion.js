c87b802464bb3250ed74b51de0247028b40e6592
Nevo Golan <nevog@elementor.com>
2022-07-11 13:35:15 +0300

    Tweak: Editor - Change promotion message [ED-7669]

--- assets/dev/js/editor/utils/promotion.js
+++ assets/dev/js/editor/utils/promotion.js
@@ -1,8 +1,26 @@
 export default class extends elementorModules.Module {
+	actionButtonConfig;
+
 	constructor() {
 		super();
 
+		this.initActionButtonConfig();
 		this.initDialog();
+	}
+
+	initActionButtonConfig() {
+		// We also need to check if the 'connect' URL exist for not breaking older pro versions that does not contain this link.
+		const isProExistNotConnected = ! ! ( elementor.helpers.hasPro() && elementorProEditorConfig.urls.connect );
+
+		// `isProExistNotConnected` is for BC.
+		 this.setActionButtonConfig( {
+			text: isProExistNotConnected ? __( 'Connect & Activate', 'elementor' ) : __( 'See it in Action', 'elementor' ),
+			url: isProExistNotConnected ? elementorProEditorConfig.urls.connect : null,
+		} );
+	}
+
+	setActionButtonConfig( { text, url } ) {
+		this.actionButtonConfig = { text, url };
 	}
 
 	initDialog() {
@@ -20,21 +38,6 @@ export default class extends elementorModules.Module {
 			},
 		} );
 
-		// We also need to check if the 'connect' URL exist for not breaking older pro versions that does not contain this link.
-		const isProExistNotConnected = ! ! ( elementor.helpers.hasPro() && elementorProEditorConfig.urls.connect );
-
-		this.dialog.addButton( {
-			name: 'action',
-			text: isProExistNotConnected ? __( 'Connect & Activate', 'elementor' ) : __( 'See it in Action', 'elementor' ),
-			callback: () => {
-				const url = isProExistNotConnected ? elementorProEditorConfig.urls.connect : this.actionURL;
-
-				open( url, '_blank' );
-			},
-		} );
-
-		this.dialog.getElements( 'action' ).addClass( 'elementor-button elementor-button-success' );
-
 		const $promotionTitle = jQuery( '<div>', { id: 'elementor-element--promotion__dialog__title' } ),
 			$proIcon = jQuery( '<i>', { class: 'eicon-pro-icon' } ),
 			$closeButton = jQuery( '<i>', { class: 'eicon-close' } );
@@ -46,14 +49,31 @@ export default class extends elementorModules.Module {
 		this.$promotionTitle = $promotionTitle;
 	}
 
+	createActionButton( actionURL ) {
+		const $actionButton = this.dialog.getElements( 'action' );
+
+		if ( $actionButton ) {
+			$actionButton.remove();
+		}
+
+		this.dialog.addButton( {
+			name: 'action',
+			text: this.actionButtonConfig.text,
+			classes: 'elementor-button elementor-button-success',
+			callback: () => {
+				open( this.actionButtonConfig.url || actionURL, '_blank' );
+			},
+		} );
+	}
+
 	showDialog( options ) {
 		if ( ! this.dialog ) {
 			this.initDialog();
 		}
 
-		this.actionURL = options.actionURL;
-
 		this.$promotionTitle.text( options.headerMessage );
+
+		this.createActionButton( options.actionURL );
 
 		this.dialog
 			.setMessage( options.message )
