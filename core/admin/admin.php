75571cf0b99e2cf5572852a058c2e03c7d91f8d8
Tzvi Rabinovitch <tzvir@elementor.com>
2022-07-11 21:06:58 +0300

    fixe source not being set

--- core/admin/admin.php
+++ core/admin/admin.php
@@ -574,9 +574,8 @@ class Admin extends App {
 		 * @param array $meta Post meta data.
 		 */
 		$meta = apply_filters( 'elementor/admin/create_new_post/meta', $meta );
-
 		if ( isset( $_GET['meta'] ) ) {
-			$meta = array_merge( sanitize_text_field( $_GET['meta'] ), $meta );
+			$meta = array_merge( array_map( sanitize_text_field, $_GET['meta'] ), $meta );
 		}
 		$post_data['post_type'] = $post_type;
 
