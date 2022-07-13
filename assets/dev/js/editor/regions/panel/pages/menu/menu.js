8ea87063cc3fe889c7773c3de9fd746c67f413ba
Nevo Golan <nevog@elementor.com>
2022-07-13 18:43:57 +0300

    Internal: Editor - Rearrange promotion dialog [ED-7697]

--- assets/dev/js/editor/regions/panel/pages/menu/menu.js
+++ assets/dev/js/editor/regions/panel/pages/menu/menu.js
@@ -78,14 +78,29 @@ PanelMenu.addAdminMenu = () => {
 		icon: 'eicon-commenting-o',
 		title: __( 'Notes', 'elementor' ),
 		callback() {
-			elementor.promotion.showDialog( {
-				headerMessage: __( 'Notes', 'elementor' ),
-				message: __( 'With Notes, teamwork gets even better. Stay in sync with comments, feedback & more on your website.', 'elementor' ),
-				top: '-3',
-				inlineStart: '+10',
-				element: this.$el,
-				actionURL: 'https://go.elementor.com/go-pro-notes/',
-			} );
+			const isProExistNotConnected = ! ! ( elementor.helpers.hasPro() && elementorProEditorConfig.urls.connect ),
+				dialogOptions = {
+					title: __( 'Notes', 'elementor' ),
+					content: __(
+						'With Notes, teamwork gets even better. Stay in sync with comments, feedback & more on your website.',
+						'elementor',
+					),
+					targetElement: this.$el,
+					position: {
+						blockStart: '-3',
+						inlineStart: '+10',
+					},
+					actionButton: {
+						url: isProExistNotConnected
+							? elementorProEditorConfig.urls.connect
+							: 'https://go.elementor.com/go-pro-notes/',
+						text: isProExistNotConnected
+							? __( 'Connect & Activate', 'elementor' )
+							: __( 'See it in Action', 'elementor' ),
+					},
+				};
+
+			elementor.promotion.showDialog( dialogOptions );
 		},
 	}, 'navigate_from_page', 'view-page' );
 
