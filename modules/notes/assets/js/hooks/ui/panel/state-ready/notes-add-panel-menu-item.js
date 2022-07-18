d98c95c88ace702dbe706dd0de69e6b1297c9117
Mark <markt@elementor.com>
2022-07-18 14:57:19 +0300

    FIx: Notes - Promotion dialog not opening in Panel menu [ED-7744]

--- modules/notes/assets/js/hooks/ui/panel/state-ready/notes-add-panel-menu-item.js
+++ modules/notes/assets/js/hooks/ui/panel/state-ready/notes-add-panel-menu-item.js
@@ -13,13 +13,24 @@ export class NotesAddPanelMenuItem extends $e.modules.hookUI.After {
 			icon: 'eicon-commenting-o',
 			title: __( 'Notes', 'elementor' ),
 			callback() {
+				const hasProAndNotConnected = elementor.helpers.hasProAndNotConnected();
+
 				elementor.promotion.showDialog( {
-					headerMessage: __( 'Notes', 'elementor' ),
-					message: __( 'With Notes, teamwork gets even better. Stay in sync with comments, feedback & more on your website.', 'elementor' ),
-					top: '-3',
-					inlineStart: '+10',
-					element: this.$el,
-					actionURL: 'https://go.elementor.com/go-pro-notes/',
+					title: __( 'Notes', 'elementor' ),
+					content: __( 'With Notes, teamwork gets even better. Stay in sync with comments, feedback & more on your website.', 'elementor' ),
+					position: {
+						top: '-3',
+						inlineStart: '+10',
+					},
+					targetElement: this.$el,
+					actionButton: {
+						url: hasProAndNotConnected
+							? elementorProEditorConfig.urls.connect
+							: 'https://go.elementor.com/go-pro-notes/',
+						text: hasProAndNotConnected
+							? __( 'Connect & Activate', 'elementor' )
+							: __( 'See it in Action', 'elementor' ),
+					},
 				} );
 			},
 		}, 'navigate_from_page', 'finder' );
