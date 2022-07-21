3e6997dac98e6a880078f7dd38b6022ab8f80c04
Ariel Klikstein <ariel@elementor.com>
2022-07-21 19:20:35 +0300

    Tweak: Add promotion for experiments

--- core/admin/admin-notices.php
+++ core/admin/admin-notices.php
@@ -29,6 +29,7 @@ class Admin_Notices extends Module {
 		'mc4wp_promote',
 		'popup_maker_promote',
 		'role_manager_promote',
+		'experiment_promotion'
 	];
 
 	private $elementor_pages_count = null;
@@ -135,7 +136,7 @@ class Admin_Notices extends Module {
 			__( 'There is a new version of Elementor Page Builder available. <a href="%1$s" class="thickbox open-plugin-details-modal" aria-label="%2$s">View version %3$s details</a> or <a href="%4$s" class="update-link" aria-label="%5$s">update now</a>.', 'elementor' ),
 			esc_url( $details_url ),
 			esc_attr( sprintf(
-				/* translators: %s: Elementor version. */
+			/* translators: %s: Elementor version. */
 				__( 'View Elementor version %s details', 'elementor' ),
 				$new_version
 			) ),
@@ -287,6 +288,40 @@ class Admin_Notices extends Module {
 				'text' => esc_html__( 'Hide Notification', 'elementor' ),
 				'classes' => [ 'e-notice-dismiss' ],
 				'url' => esc_url_raw( $dismiss_url ),
+				'new_tab' => true,
+				'type' => 'cta',
+			],
+		];
+
+		$this->print_admin_notice( $options );
+
+		return true;
+	}
+
+	private function notice_experiment_promotion() {
+		$notice_id = 'experiment_promotion';
+
+		if ( ! current_user_can( 'manage_options' ) || User::is_user_notice_viewed( $notice_id ) ) {
+			return false;
+		}
+
+		if ( Plugin::$instance->experiments->is_feature_active( 'e_dom_optimization') && Plugin::$instance->experiments->is_feature_active( 'additional_custom_breakpoints') && Plugin::$instance->experiments->is_feature_active( 'e_optimized_css_loading') && Plugin::$instance->experiments->is_feature_active( 'e_optimized_assets_loading') ) {
+			return false;
+		}
+
+		$options = [
+			'title' => esc_html__( 'Improve your site’s performance score.', 'elementor' ),
+			'description' => esc_html__( 'With our experimental speed boosting features you can go faster than ever before:', 'elementor' ),
+			'id' => $notice_id,
+			'button' => [
+				'text' => esc_html__( 'Try it out', 'elementor' ),
+				'url' => '/wp-admin/admin.php?page=elementor#tab-experiments',
+				'type' => 'cta',
+			],
+			'button_secondary' => [
+				'text' => esc_html__( 'Learn more', 'elementor' ),
+				'classes' => [ 'e-notice-dismiss' ],
+				'url' => 'https://go.elementor.com/wp-dash-experiment-promotion/',
 				'new_tab' => true,
 				'type' => 'cta',
 			],
@@ -515,6 +550,7 @@ class Admin_Notices extends Module {
 		$default_options = [
 			'id' => null,
 			'title' => '',
+			'label' => '',
 			'description' => '',
 			'classes' => [ 'notice', 'e-notice' ], // We include WP's default notice class so it will be properly handled by WP's js handler
 			'type' => '',
@@ -559,34 +595,34 @@ class Admin_Notices extends Module {
 				<?php echo $icon; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
 			</div>
 			<div class="e-notice__content">
-			<?php if ( $options['title'] ) { ?>
-				<h3><?php echo wp_kses_post( $options['title'] ); ?></h3>
-			<?php } ?>
+				<?php if ( $options['title'] ) { ?>
+					<h3><?php echo wp_kses_post( $options['title'] ); ?></h3>
+				<?php } ?>
 
-			<?php if ( $options['description'] ) { ?>
-				<p><?php echo wp_kses_post( $options['description'] ); ?></p>
-			<?php } ?>
+				<?php if ( $options['description'] ) { ?>
+					<p><?php echo wp_kses_post( $options['label'] ); ?><?php echo wp_kses_post( $options['description'] ); ?></p>
+				<?php } ?>
 
-			<?php if ( ! empty( $options['button']['text'] ) || ! empty( $options['button_secondary']['text'] ) ) { ?>
-				<div class="e-notice__actions">
-					<?php
-					foreach ( [ $options['button'], $options['button_secondary'] ] as $index => $button_settings ) {
-						if ( empty( $button_settings['variant'] ) && $index ) {
-							$button_settings['variant'] = 'outline';
-						}
+				<?php if ( ! empty( $options['button']['text'] ) || ! empty( $options['button_secondary']['text'] ) ) { ?>
+					<div class="e-notice__actions">
+						<?php
+						foreach ( [ $options['button'], $options['button_secondary'] ] as $index => $button_settings ) {
+							if ( empty( $button_settings['variant'] ) && $index ) {
+								$button_settings['variant'] = 'outline';
+							}
 
-						if ( empty( $button_settings['text'] ) ) {
-							continue;
-						}
+							if ( empty( $button_settings['text'] ) ) {
+								continue;
+							}
 
-						$button = new Button( $button_settings );
-						$button->print_button();
-					} ?>
-				</div>
-			<?php } ?>
+							$button = new Button( $button_settings );
+							$button->print_button();
+						} ?>
+					</div>
+				<?php } ?>
 			</div>
 		</div>
-		<?php }
+	<?php }
 
 	public function admin_notices() {
 		$this->install_time = Plugin::$instance->get_install_time();
