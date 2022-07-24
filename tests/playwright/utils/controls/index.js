c25571c07c64e2a8afef7382bc5030f72c1d12cb
Shay <42204748+ShayHel@users.noreply.github.com>
2022-07-24 14:04:31 +0300

    Internal: Tests - Playwright Widgets Regression Tests POC [ED-7498] (#19086)

---
+++ tests/playwright/utils/controls/index.js
@@ -0,0 +1,11 @@
+const { Choose } = require( './choose' );
+const { ControlBase } = require( './control-base' );
+const { Select } = require( './select' );
+const { Textarea } = require( './textarea' );
+
+module.exports = {
+	Choose,
+	ControlBase,
+	Select,
+	Textarea,
+};
