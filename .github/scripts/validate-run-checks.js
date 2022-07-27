ae3cd0ec27bf64954ef659f89b749c4bea205ee7
Maor-Kavod <maork@elementor.com>
2022-07-27 14:07:09 +0300

    test

---
+++ .github/scripts/validate-run-checks.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const { repoToOwnerAndOwner, getPrCommits } = require('./repo-utils');
+const { Octokit } = require("@octokit/core");
+const { REPOSITORY, CURRENT_SHA , TOKEN } = process.env;
+
+
+const octokit = new Octokit({ auth: TOKEN });
+
+(async () => {
+	try {
+		const { owner, repo } = repoToOwnerAndOwner(REPOSITORY);
+		const result = await octo.request(
+			'GET /repos/{owner}/{repo}/commits/{ref}/check-runs?per_page=100',
+			{
+			  owner,
+			  repo,
+			  ref: CURRENT_SHA
+			}
+		  )
+		  const checkRuns = result.data.check_runs;
+		  checkRuns.forEach(checkRun => {
+			console.log(checkRun);
+			if (checkRun.status === 'queued' || checkRun.status === 'in_progress') {
+				throw new Error(`Check run ${checkRun.name} is ${checkRun.status}, aborting deploy process, please wait for all checks to complete`);
+			}
+			if (checkRun.conclusion !== 'success') {
+				throw new Error(`Check run ${checkRun.name} failed with conclusion ${checkRun.conclusion}, message: ${checkRun.output.summary.text}`);
+			}
+		  }
+		  );
+	} catch (err) {
+		console.error(`Failed to get check runs: ${err}`);
+		process.exit(1);
+	}
+})();
