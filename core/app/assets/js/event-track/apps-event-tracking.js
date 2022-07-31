7c193de1f77701b99a9da2276ffe867cb78b0c06
David Seveloff <davids@elementor.com>
2022-07-31 14:50:57 +0300

    Before Amit's payload mode's

--- core/app/assets/js/event-track/apps-event-tracking.js
+++ core/app/assets/js/event-track/apps-event-tracking.js
@@ -1,20 +1,10 @@
 export const appsEventTrackingDispatch = ( command, eventParams ) => {
-	const detailsKeys = [ 'action', 'category', 'event', 'event_type', 'site_part', 'source', 'step' ];
-	const dataKeys = [ 'document_type', 'document_name', 'element', 'error_type', 'grid_location', 'item', 'kit_name', 'layout', 'search_term', 'section', 'site_area', 'site_part', 'sort_direction', 'sort_type', 'tag', 'view_type_clicked' ];
-	const data = {};
-	const details = {};
+	// Name modifications in eventParams object.
+	const keyRename = ( obj, oldKey, newKey ) => {
+		delete Object.assign( obj, { [ newKey ]: obj[ oldKey ] } )[ oldKey ];
+	}
 
-	const init = () => {
-		objectCreator( detailsKeys, data );
-		objectCreator( dataKeys, details );
-		data.placement = 'kit-library';
-
-		if ( ! Object.keys( length ).length ) {
-			data.details = details;
-		}
-	};
-
-	// Add existing event params to the data object.
+	// Add existing eventParams key value pair to the data object.
 	const objectCreator = ( array, obj ) => {
 		for ( const key of array ) {
 			if ( eventParams.hasOwnProperty( key ) && eventParams[ key ] !== null ) {
@@ -24,8 +14,31 @@ export const appsEventTrackingDispatch = ( command, eventParams ) => {
 		return obj;
 	};
 
+	keyRename( eventParams, 'source', 'source_page' );
+
+	const dataKeys = [ 'action', 'category', 'event', 'event_type', 'site_part', 'source_page', 'step' ];
+	const detailsKeys = [ 'document_type', 'document_name', 'element', 'error_type', 'grid_location', 'item', 'kit_name', 'layout', 'search_term', 'section', 'site_area', 'site_part', 'sort_direction', 'sort_type', 'tag', 'view_type_clicked' ];
+	const data = {};
+	const details = {};
+
+	const init = () => {
+		objectCreator( detailsKeys, details );
+		objectCreator( dataKeys, data );
+
+		const commandSplit = command.split( '/' )
+		data.placement = commandSplit[ 0 ];
+		data.event = commandSplit[ 1 ];
+
+		// If 'details' is not empty, add the details object to the data object.
+		if ( Object.keys( details ).length ) {
+			data.details = details;
+		}
+	};
+
 	init();
 
 	$e.run( command, data );
+
 	console.log( 'appsEventTrackingDispatch', command, JSON.stringify( data ) );
+	console.log( 'data', data );
 };
