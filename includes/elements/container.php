0063c360b4475d0cc1216546260de07b43fafa26
Hein van Vlastuin <vv@abanganimedia.co.za>
2022-08-02 17:12:13 +0200

    pr test

--- includes/elements/container.php
+++ includes/elements/container.php
@@ -300,7 +300,7 @@ class Container extends Element_Base {
 			]
 		);
 
-		$min_affected_device = Breakpoints_Manager::BREAKPOINT_KEY_MOBILE_EXTRA;
+		$min_affected_device = Breakpoints_Manager::BREAKPOINT_KEY_TABLET;
 
 		$this->add_control(
 			'content_width',
@@ -349,7 +349,6 @@ class Container extends Element_Base {
 				Breakpoints_Manager::BREAKPOINT_KEY_LAPTOP => $min_affected_device,
 				Breakpoints_Manager::BREAKPOINT_KEY_TABLET_EXTRA => $min_affected_device,
 				Breakpoints_Manager::BREAKPOINT_KEY_TABLET => $min_affected_device,
-				Breakpoints_Manager::BREAKPOINT_KEY_MOBILE_EXTRA => $min_affected_device,
 			],
 			'separator' => 'none',
 		];
@@ -387,7 +386,7 @@ class Container extends Element_Base {
 					'unit' => 'px',
 				],
 				'device_args' => [
-					'desktop' => [
+					Breakpoints_Manager::BREAKPOINT_KEY_DESKTOP => [
 						// Use the default width from the kit as a placeholder.
 						'placeholder' => $this->active_kit->get_settings_for_display( 'container_width' ),
 					],
@@ -431,7 +430,7 @@ class Container extends Element_Base {
 						'label' => esc_html_x( 'Gap between elements', 'Flex Container Control', 'elementor' ),
 						// Use the default "Gap between elements" from the kit as a placeholder value.
 						'device_args' => [
-							'desktop' => [
+							Breakpoints_Manager::BREAKPOINT_KEY_DESKTOP => [
 								// Use the default width from the kit as a placeholder.
 								'placeholder' => $this->active_kit->get_settings_for_display( 'space_between_widgets' ),
 							],
