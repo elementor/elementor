12f37b8ee095fd03bbbb43f27b3585e8830d7715
Hein van Vlastuin <vv@abanganimedia.co.za>
2022-08-04 09:59:58 +0200

    reverse

--- includes/elements/container.php
+++ includes/elements/container.php
@@ -289,7 +289,7 @@ class Container extends Element_Base {
 			]
 		);
 
-		$min_affected_device = Breakpoints_Manager::BREAKPOINT_KEY_MOBILE_EXTRA;
+		$min_affected_device = Breakpoints_Manager::BREAKPOINT_KEY_TABLET;
 
 		$this->add_control(
 			'content_width',
@@ -338,7 +338,6 @@ class Container extends Element_Base {
 				Breakpoints_Manager::BREAKPOINT_KEY_LAPTOP => $min_affected_device,
 				Breakpoints_Manager::BREAKPOINT_KEY_TABLET_EXTRA => $min_affected_device,
 				Breakpoints_Manager::BREAKPOINT_KEY_TABLET => $min_affected_device,
-				Breakpoints_Manager::BREAKPOINT_KEY_MOBILE_EXTRA => $min_affected_device,
 			],
 			'separator' => 'none',
 		];
@@ -352,19 +351,9 @@ class Container extends Element_Base {
 				'condition' => [
 					'content_width' => 'full',
 				],
-				'device_args' => [
-					Breakpoints_Manager::BREAKPOINT_KEY_DESKTOP => [
-						'placeholder' => [
-							'size' => '100',
-							'unit' => '%',
-						],
-					],
-					Breakpoints_Manager::BREAKPOINT_KEY_MOBILE => [
-						'placeholder' => [
-							'size' => '100',
-							'unit' => '%',
-						],
-					],
+				'placeholder' => [
+					'size' => '100',
+					'unit' => '%',
 				],
 			] )
 		);
@@ -385,12 +374,6 @@ class Container extends Element_Base {
 					Breakpoints_Manager::BREAKPOINT_KEY_DESKTOP => [
 						// Use the default width from the kit as a placeholder.
 						'placeholder' => $this->active_kit->get_settings_for_display( 'container_width' ),
-					],
-					Breakpoints_Manager::BREAKPOINT_KEY_MOBILE => [
-						'placeholder' => [
-							'size' => '100',
-							'unit' => '%',
-						],
 					],
 				],
 			] )
@@ -430,12 +413,8 @@ class Container extends Element_Base {
 				'fields_options' => [
 					'gap' => [
 						'label' => esc_html_x( 'Gap between elements', 'Flex Container Control', 'elementor' ),
-						'device_args' => [
-							Breakpoints_Manager::BREAKPOINT_KEY_DESKTOP => [
-								// Use the default gap from the kit as a placeholder.
-								'placeholder' => $this->active_kit->get_settings_for_display( 'space_between_widgets' ),
-							],
-						],
+						// Use the default "elements gap" from the kit as a placeholder.
+						'placeholder' => $this->active_kit->get_settings_for_display( 'space_between_widgets' ),
 					],
 				],
 			]
