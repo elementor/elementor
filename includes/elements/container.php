c84eb1f0c3d64061ea0b3990e621ce1ae98c67a1
Hein van Vlastuin <vv@abanganimedia.co.za>
2022-08-04 16:57:15 +0200

    boxed width placeholder try 2

--- includes/elements/container.php
+++ includes/elements/container.php
@@ -372,13 +372,13 @@ class Container extends Element_Base {
 					'unit' => 'px',
 				],
 				'device_args' => [
-					Breakpoints_Manager::BREAKPOINT_KEY_DESKTOP => [
+					'desktop' => [
 						// Use the default width from the kit as a placeholder.
 						'placeholder' => $this->active_kit->get_settings_for_display( 'container_width' ),
 					],
 					'mobile' => [
 						'placeholder' => [
-							'size' => '100',
+							'size' => 100,
 							'unit' => '%',
 						],
 					],
