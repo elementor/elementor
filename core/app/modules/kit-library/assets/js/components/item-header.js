73bdaa45d804a3cd64a1025906763939d95ef0b1
Davids Seveloff <94905340+davseve@users.noreply.github.com>
2022-08-08 14:44:14 +0300

    Tweak: Add to the GoPro link URL from the Kit Library more UTM parameters [ED-7745] (#19336)

--- core/app/modules/kit-library/assets/js/components/item-header.js
+++ core/app/modules/kit-library/assets/js/components/item-header.js
@@ -39,6 +39,22 @@ function useKitCallToActionButton( model, { apply, isApplyLoading, onConnect } )
 		}
 
 		if ( type === TYPE_PROMOTION && subscriptionPlan ) {
+			const getButtonURL = () => {
+				let url = subscriptionPlan.promotion_url;
+
+				if ( model.title ) {
+					// Remove special characters, replace spaces with '-' and convert url kit name to lowercase.
+					const cleanTitle = model.title.replace( /\s/g, '-' ).replace( /[^\w-]/g, '' ).toLowerCase();
+					url += `&utm_term=${ cleanTitle }`;
+				}
+
+				if ( model.id ) {
+					url += `&utm_content=${ model.id }`;
+				}
+
+				return url;
+			};
+
 			return {
 				id: 'promotion',
 				// Translators: %s is the subscription plan name.
@@ -47,7 +63,7 @@ function useKitCallToActionButton( model, { apply, isApplyLoading, onConnect } )
 				variant: 'contained',
 				color: 'cta',
 				size: 'sm',
-				url: subscriptionPlan.promotion_url,
+				url: getButtonURL(),
 				target: '_blank',
 				includeHeaderBtnClass: false,
 			};
