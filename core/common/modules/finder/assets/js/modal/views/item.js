345c40358b70554b05505ccec4bcd4fe13774f6a
StyleShit <lurkers2030@gmail.com>
2022-08-10 16:31:10 +0300

    Internal: Restrict users from being able to create new Pro Assets via Finder when license is inactive [ED-7729]

--- core/common/modules/finder/assets/js/modal/views/item.js
+++ core/common/modules/finder/assets/js/modal/views/item.js
@@ -6,4 +6,47 @@ export default class extends Marionette.ItemView {
 	getTemplate() {
 		return '#tmpl-elementor-finder__results__item';
 	}
+
+	events() {
+		this.$el[ 0 ].addEventListener( 'click', this.onClick.bind( this ), true );
+	}
+
+	onClick( e ) {
+		const lockOptions = this.model.get( 'lock' );
+
+		if ( ! lockOptions?.is_locked ) {
+			return;
+		}
+
+		e.preventDefault();
+		e.stopImmediatePropagation();
+
+		elementorCommon.dialogsManager.createWidget( 'confirm', {
+			id: 'elementor-finder__lock-dialog',
+			headerMessage: lockOptions.content.heading,
+			message: lockOptions.content.description,
+			position: {
+				my: 'center center',
+				at: 'center center',
+			},
+			strings: {
+				confirm: lockOptions.button.text,
+				cancel: __( 'Cancel', 'elementor' ),
+			},
+			onConfirm: () => {
+				const link = this.replaceLockLinkPlaceholders( lockOptions.button.url );
+
+				window.open( link, '_blank' );
+			},
+		} ).show();
+	}
+
+	replaceLockLinkPlaceholders( link ) {
+		// %1$s => utm_source
+		// %2$s => utm_medium
+
+		return link
+			.replace( /%1\$s/g, 'finder-create-new' ) // TODO: Should be same as renew?
+			.replace( /%2\$s/g, 'wp-dash' ); // TODO: Shouldn't it be based on the context?
+	}
 }
