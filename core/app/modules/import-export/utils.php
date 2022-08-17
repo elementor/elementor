03856d34d73cacfb1659ced59dfcfa13f81cf245
Hazaz <manorh@elementor.com>
2022-08-17 15:56:47 +0300

    Fix: Importing fails when post type file is missing [ED-8002]

--- core/app/modules/import-export/utils.php
+++ core/app/modules/import-export/utils.php
@@ -25,12 +25,20 @@ class Utils {
 
 		if ( isset( $imported_data['content'] ) ) {
 			foreach ( $imported_data['content'] as $post_type ) {
+				if ( empty( $post_type['succeed'] ) ) {
+					continue;
+				}
+
 				$result += $post_type['succeed'];
 			}
 		}
 
 		if ( isset( $imported_data['wp-content'] ) ) {
 			foreach ( $imported_data['wp-content'] as $post_type ) {
+				if ( empty( $post_type['succeed'] ) ) {
+					continue;
+				}
+
 				$result += $post_type['succeed'];
 			}
 		}
