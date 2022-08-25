2d34b3acb8f1df940dc8f6f6eefeca7dd493becf
Maor-Kavod <maork@elementor.com>
2022-08-25 12:22:39 +0300

    fix

--- .github/scripts/update-plugin-versions.sh
+++ .github/scripts/update-plugin-versions.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -eo pipefail
 
-sed -i -E "s/Stable tag: .*/Stable tag: ${{ inputs.PACKAGE_VERSION }}/g" ./readme.txt
-sed -i -E "s/Version: .*/Version: ${{ inputs.PACKAGE_VERSION }}/g" elementor.php
-sed -i -E "s/ELEMENTOR_VERSION', '.*'/ELEMENTOR_VERSION', '${{ inputs.PACKAGE_VERSION }}'/g" elementor.php
+sed -i -E "s/Stable tag: .*/Stable tag: ${PACKAGE_VERSION }/g" ./readme.txt
+sed -i -E "s/Version: .*/Version: ${ PACKAGE_VERSION }/g" elementor.php
+sed -i -E "s/ELEMENTOR_VERSION', '.*'/ELEMENTOR_VERSION', '${ PACKAGE_VERSION }'/g" elementor.php
