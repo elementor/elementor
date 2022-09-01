e1933e8f6c6d6f13e0f9baa208c7c74163aa87a5
StyleShit <32631382+StyleShit@users.noreply.github.com>
2022-09-01 17:08:21 +0300

    Tweak: Improved performance of Inline Fonts Icons experiment [ED-8118] (#19672)

--- includes/managers/icons.php
+++ includes/managers/icons.php
@@ -280,19 +280,14 @@ class Icons_Manager {
 
 		$attributes['class'][] = self::FONT_ICON_SVG_CLASS_NAME;
 		$attributes['class'][] = 'e-' . $icon_data['key'];
+		$attributes['viewBox'] = '0 0 ' . $icon_data['width'] . ' ' . $icon_data['height'];
+		$attributes['xmlns'] = 'http://www.w3.org/2000/svg';
 
-		/**
-		 * If in edit mode inline the full svg, otherwise use the symbol.
-		 * Will be displayed only after page update or widget "blur".
-		 */
-		if ( Plugin::$instance->editor->is_edit_mode() ) {
-			return '<svg xmlns="http://www.w3.org/2000/svg" ' . Utils::render_html_attributes( $attributes ) . '
-				viewBox="0 0 ' . esc_attr( $icon_data['width'] ) . ' ' . esc_attr( $icon_data['height'] ) . '">
-				<path d="' . esc_attr( $icon_data['path'] ) . '"></path>
-			</svg>';
-		}
-
-		return '<svg ' . Utils::render_html_attributes( $attributes ) . '><use xlink:href="#' . esc_attr( $icon_data['key'] ) . '" /></svg>';
+		return (
+			'<svg ' . Utils::render_html_attributes( $attributes ) . '">' .
+				'<path d="' . esc_attr( $icon_data['path'] ) . '"></path>' .
+			'</svg>'
+		);
 	}
 
 	public static function render_uploaded_svg_icon( $value ) {
