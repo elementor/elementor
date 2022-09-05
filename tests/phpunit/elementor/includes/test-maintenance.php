d5eec7c019b1560f94c55b92a3bf60e50caca949
Maor-Kavod <maork@elementor.com>
2022-09-05 14:32:50 +0300

    lint

--- tests/phpunit/elementor/includes/test-maintenance.php
+++ tests/phpunit/elementor/includes/test-maintenance.php
@@ -11,24 +11,24 @@ class Test_Maintenance extends Elementor_Test_Base {
 	public function test_new_install_options() {
 		// Arrange.
 		$user = $this->act_as_admin();
-        delete_option( 'elementor_install_history' );
+		delete_option( 'elementor_install_history' );
 
 		// Act.
-        \Elementor\Maintenance::insert_defaults_options();
-        
+		\Elementor\Maintenance::insert_defaults_options();
+
 		// Assert.
 		$this->assertEquals( 'swap', get_option( static::OPTION_KEY ) );
 	}
 
-    public function test_not_new_install_options() {
-        // Arrange.
-        $user = $this->act_as_admin();
-        update_option( 'elementor_install_history', [ '1.0.0' ] );
+	public function test_not_new_install_options() {
+		// Arrange.
+		$user = $this->act_as_admin();
+		update_option( 'elementor_install_history', [ '1.0.0' ] );
 
-        // Act.
-        \Elementor\Maintenance::insert_defaults_options();
+		// Act.
+		\Elementor\Maintenance::insert_defaults_options();
 
-        // Assert.
-        $this->assertNotEquals( 'swap', get_option( static::OPTION_KEY ) );
-    }
+		// Assert.
+		$this->assertNotEquals( 'swap', get_option( static::OPTION_KEY ) );
+	}
 }
