a588024e5b36980a677e5672bf21fa667443ed20
Maor-Kavod <maork@elementor.com>
2022-09-05 16:19:39 +0300

    revert auto

--- includes/settings/settings.php
+++ includes/settings/settings.php
@@ -338,7 +338,7 @@ class Settings extends Settings_Page {
 								'label' => esc_html__( 'Google Fonts Load', 'elementor' ),
 								'field_args' => [
 									'type' => 'select',
-									'std' => 'swap',
+									'std' => 'auto',
 									'options' => [
 										'auto' => esc_html__( 'Default', 'elementor' ),
 										'block' => esc_html__( 'Blocking', 'elementor' ),
