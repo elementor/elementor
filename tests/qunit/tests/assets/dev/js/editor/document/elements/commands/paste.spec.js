3ac759033d1a00836e61b76eddd10ae4e17833df
Hein van Vlastuin <vv@abanganimedia.co.za>
2022-09-07 08:57:42 +0200

    qunit test

--- tests/qunit/tests/assets/dev/js/editor/document/elements/commands/paste.spec.js
+++ tests/qunit/tests/assets/dev/js/editor/document/elements/commands/paste.spec.js
@@ -147,22 +147,15 @@ export const Paste = () => {
 				// Check pasted elements existence.
 				assert.ok( parents.every( ( parent ) => parent ), `Both elements pasted.` );
 
-				if ( elementorCommon.config.experimentalFeatures.container ) {
-					// Check whether they preserved their order.
-					assert.equal(
-						1,
-						1,
-						'Elements preserved their position.',
-					);
-				} else {
-					// Check whether they preserved their order.
-					assert.equal(
-						elementor.getContainer( elementor.elements.models[ elementor.elements.length - 1 ].get( 'id' ) )
-							.children[ 0 ].children[ 0 ].model.get( 'widgetType' ),
-						toCopy[ toCopy.length - 1 ].model.get( 'widgetType' ),
-						'Elements preserved their position.',
-					);
-				}
+
+				// Check whether they preserved their order.
+				assert.equal(
+					elementor.getContainer( elementor.elements.models[ elementor.elements.length - 1 ].get( 'id' ) )
+						.children[ 0 ].children[ 0 ].model.get( 'widgetType' ),
+					toCopy[ toCopy.length - 1 ].model.get( 'widgetType' ),
+					'Elements preserved their position.',
+				);
+
 			} );
 
 			QUnit.test( 'History', ( assert ) => {
