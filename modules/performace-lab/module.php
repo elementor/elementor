b6989249854b6ea53295a7607fa4f9090a8adaa8
Maor-Kavod <maork@elementor.com>
2022-09-10 11:53:11 +0300

    Changes requested

--- modules/performace-lab/module.php
+++ modules/performace-lab/module.php
@@ -1,5 +1,5 @@
 <?php
-namespace Elementor\Modules\ThirdPartyCustomization;
+namespace Elementor\Modules\PerformaceLab;
 
 use Elementor\Core\Base\Module as BaseModule;
 use Elementor\Plugin;
@@ -12,20 +12,27 @@ class Module extends BaseModule {
 
 
 	public function get_name() {
-		return 'third-party-customization';
+		return 'performace-lab';
 	}
 
-	public static function performace_lab_get_webp_src( $attachment_id, $size = 'full', $url ) {
+	private function does_performace_lab_is_active() {
 		if ( function_exists( 'webp_uploads_img_tag_update_mime_type' ) ) {
 			$webp_option_name = 'site-health/webp-support';
 			$perflab_modules_settings = get_option( 'perflab_modules_settings' );
 			if ( isset( $perflab_modules_settings ) && isset( $perflab_modules_settings[ $webp_option_name ] ) &&
 							'1' === $perflab_modules_settings[ $webp_option_name ]['enabled'] ) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	private function performace_lab_get_webp_src( $attachment_id, $size = 'full', $url ) {
+		if ( $this->does_performace_lab_is_active() ) {
 				$image_object = wp_get_attachment_image_src( $attachment_id, $size );
 				$image_src = webp_uploads_img_tag_update_mime_type( $image_object[0], 'webp', $attachment_id );
-				if ( ! empty( $image_src ) ) {
-					return $image_src;
-				}
+			if ( ! empty( $image_src ) ) {
+				return $image_src;
 			}
 		}
 		return $url;
@@ -33,13 +40,17 @@ class Module extends BaseModule {
 
 
 	public function __construct() {
+
 		parent::__construct();
-		add_filter('elementor/css-file/css_property', function( $value, $css_property, $matches, $control ) {
-			if ( 0 === strpos( $css_property, 'background-image' ) && '{{URL}}' === $matches[0] ) {
-				$value['url'] = $this->performace_lab_get_webp_src( $value['id'], 'full', $value['url'] );
-			}
-			return $value;
-		}, 10, 4 );
+
+		if ( $this->does_performace_lab_is_active() ) {
+			add_filter('elementor/css-file/css_property', function( $value, $css_property, $matches, $control ) {
+				if ( 0 === strpos( $css_property, 'background-image' ) && '{{URL}}' === $matches[0] ) {
+					$value['url'] = $this->performace_lab_get_webp_src( $value['id'], 'full', $value['url'] );
+				}
+				return $value;
+			}, 10, 4 );
+		}
 
 		add_action( 'activated_plugin', function( $plugin ) {
 			if ( 'performance-lab/load.php' === $plugin ) {
--- tests/phpunit/elementor/modules/third-party-customization/test-module.php
+++ tests/phpunit/elementor/modules/third-party-customization/test-module.php
@@ -2,7 +2,8 @@
 namespace Elementor\Testing\Modules\ThirdPartyCustomization;
 
 use ElementorEditorTesting\Elementor_Test_Base;
-use Elementor\Modules\ThirdPartyCustomization as ThirdPartyCustomization;
+
+use Elementor\Modules\PerformaceLab\Module as PerformaceLab;
 
 class Elementor_Test_Third_Party_Customization_Module extends Elementor_Test_Base {
 
@@ -10,7 +11,11 @@ class Elementor_Test_Third_Party_Customization_Module extends Elementor_Test_Bas
 
 		$image_id = $this->factory()->attachment->create_upload_object( __DIR__ . '/assets/pixel.jpg' );
 		$image_url = wp_get_attachment_url( $image_id );
-		$webp_src = ThirdPartyCustomization\Module::performace_lab_get_webp_src( $image_id, 'full', $image_url );
+		$reflection = new \ReflectionClass( PerformaceLab::class );
+		$method = $reflection->getMethod( 'performace_lab_get_webp_src' );
+		$method->setAccessible( true );
+		$performace_lab = new PerformaceLab();
+		$webp_src = $method->invokeArgs( $performace_lab, [ $image_id, 'full', $image_url ] );
 		$this->assertEquals( $image_url, $webp_src );
 
 	}
