0305b3b0d1d9317877d12377237f56c67e4accda
Hein van Vlastuin <vv@abanganimedia.co.za>
2022-09-12 21:06:14 +0200

    copyPaste

--- tests/qunit/tests/assets/dev/js/editor/document/ui/commands/paste.spec.js
+++ tests/qunit/tests/assets/dev/js/editor/document/ui/commands/paste.spec.js
@@ -109,7 +109,7 @@ const validateRule = ( assert, target, targetElType, source, sourceElType, isAll
 	} else {
 		targetIsInner = target.model.get( 'isInner' );
 		sourceIsInner = source.model.get( 'isInner' );
-		// copiedContainer = UIHelper.copyPaste( source, target );
+		copiedContainer = UIHelper.copyPaste( source, target );
 
 		message = `Copy: "${ sourceIsInner ? 'InnerSection::' : '' }${ sourceElType }"
 		 And Paste to: "${ targetIsInner ? 'InnerSection::' : '' }${ targetElType }" "${ isAllowed ? 'ALLOW' : 'BLOCK' }"`;
