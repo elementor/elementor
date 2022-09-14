3b4c97f2c2783de97396af80ed35e08de3f1f36d
Hein van Vlastuin <vv@abanganimedia.co.za>
2022-09-14 20:31:30 +0200

    screenshots update

--- tests/playwright/sanity/container.test.js
+++ tests/playwright/sanity/container.test.js
@@ -56,10 +56,10 @@ test.describe( 'Container tests', () => {
 		const editor = await wpAdmin.useElementorCleanPost(),
 			containerId = await editor.addElement( { elType: 'container' }, 'document' );
 
-		// Act.
-		// Set row direction.
-		await page.click( '.elementor-control-flex_direction i.eicon-arrow-right' );
+		// Close Navigator
+		await page.click( '#elementor-navigator__close' );
 
+		// Act.
 		// Add widgets.
 		await editor.addWidget( 'accordion', containerId );
 		await editor.addWidget( 'divider', containerId );
@@ -67,45 +67,62 @@ test.describe( 'Container tests', () => {
 		await editor.addWidget( 'toggle', containerId );
 		await editor.addWidget( 'video', containerId );
 
+		// Select spacer element.
+		const spacerElement = await editor.getPreviewFrame().waitForSelector( '.elementor-widget-spacer' );
+		await spacerElement.hover();
+		const spacerEditButton = await editor.getPreviewFrame().waitForSelector( '.elementor-widget-spacer .elementor-editor-element-edit' );
+		await spacerEditButton.click();
+		// Set background colour.
+		await page.locator( '.elementor-tab-control-advanced' ).click();
+		await page.locator( '.elementor-control-_section_background .elementor-panel-heading-title' ).click();
+		await page.locator( '.elementor-control-_background_background .eicon-paint-brush' ).click();
+		await page.locator( '.elementor-control-_background_color .pcr-button' ).click();
+		await page.locator( '.pcr-app.visible .pcr-interaction input.pcr-result' ).fill( '#A81830' );
+		// Select container.
+		const containerElement = await editor.getPreviewFrame().waitForSelector( '.elementor-element-' + containerId );
+		await containerElement.hover();
+		const containerEditButton = await editor.getPreviewFrame().waitForSelector( '.elementor-element-' + containerId + ' .elementor-editor-element-edit' );
+		await containerEditButton.click();
+		// Set row direction.
+		await page.click( '.elementor-control-flex_direction i.eicon-arrow-right' );
+
 		const container = editor.getPreviewFrame().locator( '.elementor-element-' + containerId );
 
 		// Assert
-		await expect( container ).toHaveCount( 1 );
-
 		expect( await container.screenshot( {
 			type: 'jpeg',
-			quality: 70
+			quality: 70,
 		} ) ).toMatchSnapshot( 'container-row.jpeg' );
 
 		// Act
 		// Set full content width
-		await editor.selectOption( '.elementor-control-content_width >> select', 'full' );
+		await page.selectOption( '.elementor-control-content_width >> select', 'full' );
 
 		expect( await container.screenshot( {
 			type: 'jpeg',
-			quality: 70
+			quality: 70,
 		} ) ).toMatchSnapshot( 'container-row-full.jpeg' );
 
 		// Act
 		// Flex-direction: column
 		await page.click( '.elementor-control-flex_direction i.eicon-arrow-down' );
 		// Align items: flex-start
-		await page.click( '.elementor-control-align_items i.eicon-align-start-v' );
+		await page.click( '.elementor-control-flex_align_items i.eicon-align-start-v' );
 
 		// Assert
 		expect( await container.screenshot( {
 			type: 'jpeg',
-			quality: 70
+			quality: 70,
 		} ) ).toMatchSnapshot( 'container-column-full-start.jpeg' );
 
 		// Act
 		// Content Width: boxed
-		await editor.selectOption( '.elementor-control-content_width >> select', 'boxed' );
+		await page.selectOption( '.elementor-control-content_width >> select', 'boxed' );
 
 		// Assert
 		expect( await container.screenshot( {
 			type: 'jpeg',
-			quality: 70
+			quality: 70,
 		} ) ).toMatchSnapshot( 'container-column-boxed-start.jpeg' );
 
 		await wpAdmin.setExperiments( {
@@ -119,56 +136,67 @@ test.describe( 'Container tests', () => {
 		await wpAdmin.setExperiments( {
 			container: true,
 		} );
-	
+
 		const editor = await wpAdmin.useElementorCleanPost(),
 			container = await editor.addElement( { elType: 'container' }, 'document' );
-	
+
+		// Close Navigator
+		await page.click( '#elementor-navigator__close' );
+
 		// Act.
 		// Add widget.
 		await editor.addWidget( 'heading', container );
 		const heading = editor.getPreviewFrame().locator( '.elementor-widget-heading' );
 		const pageView = editor.getPreviewFrame().locator( 'body' );
+		// Select container.
+		const containerElement = await editor.getPreviewFrame().waitForSelector( '.elementor-element-' + container );
+		await containerElement.hover();
+		const containerEditButton = await editor.getPreviewFrame().waitForSelector( '.elementor-editor-element-edit' );
+		await containerEditButton.click();
 		// Set position absolute.
 		await page.locator( '.elementor-tab-control-advanced' ).click();
-		await page.selectOption( '.elementor-control-_position >> select', 'absolute' );
+		await page.selectOption( '.elementor-control-position >> select', 'absolute' );
 		await page.locator( '.elementor-control-_offset_x .elementor-control-input-wrapper input' ).fill( '50' );
 		await page.locator( '.elementor-control-_offset_y .elementor-control-input-wrapper input' ).fill( '50' );
-	
+
 		// Assert
+		// Take screenshot.
 		expect( await pageView.screenshot( {
 			type: 'jpeg',
-			quality: 70
+			quality: 70,
 		} ) ).toMatchSnapshot( 'heading-boxed-absolute.jpeg' );
-	
+
 		// Act
 		// Set full content width
-		await editor.selectOption( '.elementor-control-content_width >> select', 'full' );
-	
+		await page.locator( '.elementor-tab-control-layout' ).click();
+		await page.selectOption( '.elementor-control-content_width >> select', 'full' );
+
 		// Assert
 		expect( await pageView.screenshot( {
 			type: 'jpeg',
-			quality: 70
+			quality: 70,
 		} ) ).toMatchSnapshot( 'heading-full-absolute.jpeg' );
 
 		// Act
 		// Set position fixed.
 		await page.locator( '.elementor-tab-control-advanced' ).click();
-		await page.selectOption( '.elementor-control-_position >> select', 'fixed' );
-	
+		await page.selectOption( '.elementor-control-position >> select', 'fixed' );
+
 		// Assert
 		expect( await pageView.screenshot( {
 			type: 'jpeg',
-			quality: 70
+			quality: 70,
 		} ) ).toMatchSnapshot( 'heading-full-fixed.jpeg' );
 
 		// Act
 		// Set boxed content width
-		await editor.selectOption( '.elementor-control-content_width >> select', 'boxed' );
-	
+		await page.locator( '.elementor-tab-control-layout' ).click();
+		await page.selectOption( '.elementor-control-content_width >> select', 'boxed' );
+
 		// Assert
 		expect( await pageView.screenshot( {
 			type: 'jpeg',
-			quality: 70
+			quality: 70,
 		} ) ).toMatchSnapshot( 'heading-boxed-fixed.jpeg' );
 
 		await wpAdmin.setExperiments( {
