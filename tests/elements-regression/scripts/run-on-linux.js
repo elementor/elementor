3c974793c2d1af7dfbc7559f21681a13439ef363
Nevo Golan <nevog@elementor.com>
2022-09-20 14:17:54 +0300

    add run on linux script

---
+++ tests/elements-regression/scripts/run-on-linux.js
@@ -0,0 +1,45 @@
+const { spawn, exec } = require( 'child_process' );
+const packageJson = require( '../../../package.json' );
+
+function isDockerExist() {
+	return new Promise( ( resolve, reject ) => {
+		exec( 'docker -v', ( error ) => {
+			resolve( ! error );
+		} );
+  } );
+}
+
+async function run( args ) {
+	const playwrightVersion = packageJson.devDependencies[ '@playwright/test' ];
+	const workingDir = process.cwd();
+
+	const command = 'docker run';
+	const options = [
+		'--rm',
+		'--network host',
+		`--volume ${ workingDir }:/work`,
+		'--workdir /work/',
+		'--interactive',
+		'--tty',
+	];
+	const image = `mcr.microsoft.com/playwright:v${ playwrightVersion }-focal`;
+	const commandToRun = `/bin/bash -c "npm i && npm run test:elements-regression -- ${ args.join( ' ' ) }"`;
+
+	const dockerCommand = spawn( `${ command } ${ options.join( ' ' ) } ${ image } ${ commandToRun }`, {
+		stdio: 'inherit',
+		stderr: 'inherit',
+		shell: true,
+	} );
+}
+
+async function main() {
+  if ( ! await isDockerExist() ) {
+	console.error( 'Docker is not installed, please install it first.' );
+
+	process.exit( 1 );
+  }
+
+  await run( process.argv.slice( 2 ) );
+}
+
+main();
