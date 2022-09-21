18c9e04b9fbf70a0c1577e6a6938ac5a227851ed
StyleShit <lurkers2030@gmail.com>
2022-09-21 08:00:09 +0300

    eslint

--- packages/editor/.eslintrc.js
+++ packages/editor/.eslintrc.js
@@ -1,10 +1,12 @@
 module.exports = {
 	root: true,
 	extends: [
-		'plugin:@typescript-eslint/recommended',
 		'plugin:react/recommended',
 		'plugin:@wordpress/eslint-plugin/recommended-with-formatting',
+
+		// Typescript rules should be last in order to override default eslint rules the conflicts with typescript.
 		'plugin:import/typescript',
+		'plugin:@typescript-eslint/recommended',
 	],
 	plugins: [
 		'react',
@@ -14,15 +16,20 @@ module.exports = {
 	rules: {
 		'computed-property-spacing': [ 'error', 'always' ],
 		'comma-dangle': [ 'error', 'always-multiline' ],
-		'no-undef': 'off',
 		'dot-notation': 'error',
 		'no-shadow': 'error',
 		'no-lonely-if': 'error',
 		'no-mixed-operators': 'error',
 		'no-nested-ternary': 'error',
 		'no-cond-assign': 'error',
-		'no-unused-vars': 'off', // Using the @typescript-eslint/no-unused-vars rule instead.
-		indent: [ 'off', 'tab', { SwitchCase: 1 } ],
+		indent: [ 'error', 'tab', {
+			SwitchCase: 1,
+			// Prevent conflicts with `react/jsx-indent-props`.
+			ignoredNodes: [
+				'JSXAttribute',
+				'JSXSpreadAttribute',
+			],
+		} ],
 		'padded-blocks': [ 'error', 'never' ],
 		'one-var-declaration-per-line': 'error',
 		'array-bracket-spacing': [ 'error', 'always' ],
