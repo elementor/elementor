8530577a2679f82340d5c0b05648d9eecd835f46
Hein van Vlastuin <vv@abanganimedia.co.za>
2022-09-28 15:22:55 +0200

    pr review

--- tests/playwright/pages/wp-admin-page.js
+++ tests/playwright/pages/wp-admin-page.js
@@ -123,35 +123,4 @@ module.exports = class WpAdminPage extends BasePage {
 	async setWidgetToFlexGrow() {
 		await this.page.locator( '.elementor-control-_flex_size .elementor-control-input-wrapper .eicon-grow' ).click();
 	}
-
-	/**
-	 * Autopopulate the Image Carousel.
-	 *
-	 * @return {Promise<void>}
-	 */
-	async populateImageCarousel() {
-		await this.page.locator( '[aria-label="Add Images"]' ).click();
-
-		// Open Media Library
-		await this.page.click( 'text=Media Library' );
-
-		// Upload the images to WP media library
-		await this.page.setInputFiles( 'input[type="file"]', './tests/playwright/resources/A.jpg' );
-		await this.page.setInputFiles( 'input[type="file"]', './tests/playwright/resources/B.jpg' );
-		await this.page.setInputFiles( 'input[type="file"]', './tests/playwright/resources/C.jpg' );
-		await this.page.setInputFiles( 'input[type="file"]', './tests/playwright/resources/D.jpg' );
-		await this.page.setInputFiles( 'input[type="file"]', './tests/playwright/resources/E.jpg' );
-
-		// Create a new gallery
-		await this.page.locator( 'text=Create a new gallery' ).click();
-
-		// Insert gallery
-		await this.page.locator( 'text=Insert gallery' ).click();
-
-		// Open The Additional options Section
-		await this.page.click( '#elementor-controls >> :nth-match(div:has-text("Additional Options"), 3)' );
-
-		// Disable AutoPlay
-		await this.page.selectOption( 'select', 'no' );
-	}
 };
