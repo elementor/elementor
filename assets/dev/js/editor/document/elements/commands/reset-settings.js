063a4d8936afea0c6dea2e551c6c39e3b49cb821
Nevo Golan <nevog@elementor.com>
2022-10-02 15:18:01 +0300

    Fix: Editor - Reset control not working for global values [ED-8381]

--- assets/dev/js/editor/document/elements/commands/reset-settings.js
+++ assets/dev/js/editor/document/elements/commands/reset-settings.js
@@ -13,31 +13,65 @@ export class ResetSettings extends $e.modules.editor.document.CommandHistoryBase
 	}
 
 	apply( args ) {
-		const { containers = [ args.container ], options = {}, settings = [] } = args;
+		const {
+			containers = [ args.container ],
+			options = {},
+			settings = [], // In case of `settings` is empty, reset all settings.
+		} = args;
 
 		containers.forEach( ( container ) => {
-			const controls = Object.entries( container.settings.controls ),
-				defaultValues = {};
+			const defaultValues = this.#getDefaultValues(
+				container.settings.controls,
+				settings || [],
+			);
 
-			controls.forEach( ( [ controlName, control ] ) => {
-				// If settings were specific, restore only them.
-				if ( settings && settings.length ) {
-					if ( ! settings.find( ( key ) => key === controlName ) ) {
-						return;
-					}
-				}
-
-				defaultValues[ controlName ] = control.default;
-			} );
-
-			$e.run( 'document/elements/settings', {
-				container,
-				options,
-				settings: defaultValues,
-			} );
+			this.#updateLocalSettings( { container, options, settings: defaultValues } );
+			this.#updateGlobalSettings( { container, options, settings: defaultValues } );
 
 			container.render();
+			container.panel.refresh();
 		} );
+	}
+
+	#updateLocalSettings( { container, options, settings } ) {
+		const localSettings = Object.entries( settings )
+			.filter( ( [ controlName ] ) => '__globals__' !== controlName );
+
+		$e.run( 'document/elements/settings', {
+			container,
+			options,
+			settings: Object.fromEntries( localSettings ),
+		} );
+	}
+
+	#updateGlobalSettings( { container, options, settings } ) {
+		Object.entries( settings.__globals__ || {} ).forEach( ( [ controlName, globalValue ] ) => {
+			const isGlobalActive = container.settings.get( '__globals__' )?.[ controlName ];
+
+			$e.run( isGlobalActive ? 'document/globals/settings' : 'document/globals/enable', {
+				container,
+				settings: { [ controlName ]: globalValue },
+			} );
+		} );
+	}
+
+	#getDefaultValues( controls, only = [] ) {
+		return Object.entries( controls )
+			.filter( ( [ controlName, control ] ) =>
+				0 === only.length || only.includes( controlName ),
+			)
+			.reduce( ( carry, [ controlName, control ] ) => {
+				const values = { [ controlName ]: control.default };
+
+				if ( control?.global?.default ) {
+					values.__globals__ = {
+						...( carry.__globals__ || {} ),
+						[ controlName ]: control.global.default,
+					};
+				}
+
+				return { ...carry, ...values };
+			}, {} );
 	}
 }
 
