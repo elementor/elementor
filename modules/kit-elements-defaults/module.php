f1f7bfe1a9950b2722fb3c39537094c829272f1d
StyleShit <lurkers2030@gmail.com>
2022-10-20 07:44:58 +0300

    Fix meta

--- modules/kit-elements-defaults/module.php
+++ modules/kit-elements-defaults/module.php
@@ -12,7 +12,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 class Module extends BaseModule {
 
-	const META_KEY = '_elementor_elements_defaults_values';
+	const META_KEY = '_elementor_element_defaults_values';
 
 	public function get_name() {
 		return 'kit-elements-defaults';
