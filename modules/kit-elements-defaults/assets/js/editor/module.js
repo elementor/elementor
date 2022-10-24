90b0d81494fefb87ac7cc943e68c0d217d10014f
Nevo Golan <nevog@elementor.com>
2022-10-24 20:27:32 +0300

    wip

--- modules/kit-elements-defaults/assets/js/editor/module.js
+++ modules/kit-elements-defaults/assets/js/editor/module.js
@@ -16,24 +16,26 @@ export default class Module extends elementorModules.editor.utils.Module {
 			return;
 		}
 
-		elementor.hooks.addFilter( 'elements/widget/contextMenuGroups', ( groups, view ) => {
-			return groups.map( ( group ) => {
-				if ( group.name !== 'save' ) {
-					return group;
-				}
+		[ 'widget', 'container' ].forEach( ( elType ) => {
+			elementor.hooks.addFilter( `elements/${ elType }/contextMenuGroups`, ( groups, view ) => {
+				return groups.map( ( group ) => {
+					if ( group.name !== 'save' ) {
+						return group;
+					}
 
-				group.actions = [
-					...group.actions,
-					{
-						name: 'save-as-default',
-						title: __( 'Save as Default', 'elementor' ),
-						callback: () => {
-							$e.run( 'kit-elements-defaults/confirm-creation', { container: view.getContainer() } );
+					group.actions = [
+						...group.actions,
+						{
+							name: 'save-as-default',
+							title: __( 'Save as Default', 'elementor' ),
+							callback: () => {
+								$e.run( 'kit-elements-defaults/confirm-creation', { container: view.getContainer() } );
+							},
 						},
-					},
-				];
+					];
 
-				return group;
+					return group;
+				} );
 			} );
 		} );
 	}
