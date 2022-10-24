8470a6fb6d52e4fb39f14c10c96888331bcbd7b0
Tzvi Rabinovitch <tzvir@elementor.com>
2022-10-24 16:37:56 +0300

    Playwright WIP

--- tests/playwright/sanity/container.test.js
+++ tests/playwright/sanity/container.test.js
@@ -409,6 +409,7 @@ async function creatCanvasPage( wpAdmin ) {
 	await editor.page.waitForLoadState( 'networkidle' );
 	await editor.changeEditorLayout( 'elementor_canvas' );
 	await editor.page.waitForLoadState( 'networkidle' );
+	await this.page.waitForTimeout( 2000 ); // I could not find another way
 	return editor;
 }
 
