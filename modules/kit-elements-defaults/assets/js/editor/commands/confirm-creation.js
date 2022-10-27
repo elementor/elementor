842a59172221a894ec7ef111520880347361e1f4
Nevo Golan <nevog@elementor.com>
2022-10-27 12:42:01 +0300

    wip

--- modules/kit-elements-defaults/assets/js/editor/commands/confirm-creation.js
+++ modules/kit-elements-defaults/assets/js/editor/commands/confirm-creation.js
@@ -1,90 +1,21 @@
-export default class ConfirmCreation extends $e.modules.editor.CommandContainerBase {
-	static introduction;
-	static #introductionKey = 'kit_elements_defaults_create_dialog';
+import { getConfirmCreationDialog } from '../confirm-creation-dialog';
 
+export default class ConfirmCreation extends $e.modules.editor.CommandContainerBase {
 	validateArgs() {
 		this.requireContainer();
 	}
 
 	async apply( { container } ) {
-		if ( ! this.constructor.introduction ) {
-			this.constructor.introduction = this.#createIntroduction();
-			this.constructor.introduction.introductionViewed = !! elementor.config.user.introduction?.[ this.constructor.#introductionKey ];
-		}
+		const confirmCreationDialog = getConfirmCreationDialog( {
+			onConfirm: () => $e.run( 'kit-elements-defaults/create', { container } ),
+		} );
 
-		const introduction = this.constructor.introduction;
-
-		if ( introduction.introductionViewed ) {
+		if ( confirmCreationDialog.doNotShowAgain ) {
 			$e.run( 'kit-elements-defaults/create', { container } );
 
 			return;
 		}
 
-		const dialog = introduction.getDialog();
-
-		// Set the confirm callback from outside because the introduction modal is a singleton,
-		// and each run it should reference to diffrent container.
-		dialog.onConfirm = () => {
-			// `getElements` return JQuery object.
-			if ( dialog.getElements( 'checkbox-dont-show-again' ).prop( 'checked' ) ) {
-				introduction.setViewed();
-			}
-
-			$e.run( 'kit-elements-defaults/create', { container } );
-		};
-
-		introduction.show();
-	}
-
-	#createIntroduction() {
-		const dialogId = 'e-kit-elements-defaults-create-dialog';
-
-		const introduction = new elementorModules.editor.utils.Introduction( {
-			introductionKey: this.constructor.#introductionKey,
-			dialogType: 'confirm',
-			dialogOptions: {
-				id: dialogId,
-				headerMessage: __( 'Sure you want to change default settings?', 'elementor' ),
-				message: __( 'Your changes will automatically apply to all future use of this widget.', 'elementor' ) +
-					'<br/><br/>' +
-					__( 'Note: Your new default settings can include sensitive information such as API key, CSS ID, and more.', 'elementor' ),
-				effects: {
-					show: 'fadeIn',
-					hide: 'fadeOut',
-				},
-				hide: {
-					onBackgroundClick: true,
-				},
-				strings: {
-					confirm: __( 'Save', 'elementor' ),
-					cancel: __( 'Cancel', 'elementor' ),
-				},
-			},
-		} );
-
-		const { checkbox, label } = this.#createCheckboxAndLabel( dialogId );
-		introduction.getDialog().addElement( 'checkbox-dont-show-again', checkbox );
-		introduction.getDialog().getElements( 'message' )?.append?.( label ); // `getElements` return JQuery object.
-
-		return introduction;
-	}
-
-	#createCheckboxAndLabel( dialogId ) {
-		const checkboxId = `${ dialogId }-dont-show-again`;
-
-		const checkbox = document.createElement( 'input' );
-
-		checkbox.type = 'checkbox';
-		checkbox.name = checkboxId;
-		checkbox.id = checkboxId;
-		checkbox.checked = true;
-
-		const label = document.createElement( 'label' );
-
-		label.htmlFor = checkboxId;
-		label.textContent = __( 'Do not show this message again', 'elementor' );
-		label.prepend( checkbox );
-
-		return { checkbox, label };
+		confirmCreationDialog.show();
 	}
 }
--- tests/jest/unit/modules/kit-elements-defaults/assets/js/editor/commands/confirm-creation.test.js
+++ tests/jest/unit/modules/kit-elements-defaults/assets/js/editor/commands/confirm-creation.test.js
@@ -1,4 +1,5 @@
 import createContainer from 'elementor/tests/jest/utils/create-container';
+import { introductionManager, removeConfirmCreationDialog } from 'elementor/modules/kit-elements-defaults/assets/js/editor/confirm-creation-dialog';
 
 describe( `$e.run( 'kit-elements-defaults/confirm-creation' )`, () => {
 	let ConfirmCreationCommand;
@@ -48,7 +49,7 @@ describe( `$e.run( 'kit-elements-defaults/confirm-creation' )`, () => {
 		delete global.elementorModules;
 		delete global.elementor;
 
-		ConfirmCreationCommand.introduction = null;
+		removeConfirmCreationDialog();
 
 		jest.resetAllMocks();
 	} );
@@ -65,7 +66,7 @@ describe( `$e.run( 'kit-elements-defaults/confirm-creation' )`, () => {
 
 		// Assert
 		expect( global.$e.run ).toHaveBeenCalledWith( 'kit-elements-defaults/create', { container } );
-		expect( ConfirmCreationCommand.introduction.show ).not.toHaveBeenCalled();
+		expect( introductionManager.show ).not.toHaveBeenCalled();
 	} );
 
 	it( 'should show dialog before `create` and when confirm it calls `create`', () => {
@@ -75,30 +76,30 @@ describe( `$e.run( 'kit-elements-defaults/confirm-creation' )`, () => {
 
 		// Act
 		command.apply( { container } );
-		ConfirmCreationCommand.introduction.dialog.onConfirm();
+		introductionManager.dialog.onConfirm();
 
 		// Assert
-		expect( ConfirmCreationCommand.introduction.show ).toHaveBeenCalledTimes( 1 );
+		expect( introductionManager.show ).toHaveBeenCalledTimes( 1 );
 		expect( global.$e.run ).toHaveBeenCalledTimes( 1 );
 		expect( global.$e.run ).toHaveBeenCalledWith( 'kit-elements-defaults/create', { container } );
-		expect( ConfirmCreationCommand.introduction.setViewed ).not.toHaveBeenCalled();
+		expect( introductionManager.setViewed ).not.toHaveBeenCalled();
 	} );
 
-	it( 'should show the dialog on the fisrt time and after checking the checkbox it should not be shown', () => {
+	it( 'should show the dialog on the first time and after checking the checkbox it should not be shown', () => {
 		// Arrange
 		const container = createContainer();
 		const command = new ConfirmCreationCommand();
 
 		// Act
 		command.apply( { container } );
-		ConfirmCreationCommand.introduction.dialog.getElements().prop.mockReturnValueOnce( true );
-		ConfirmCreationCommand.introduction.dialog.onConfirm();
+		introductionManager.dialog.getElements().prop.mockReturnValueOnce( true );
+		introductionManager.dialog.onConfirm();
 
 		// Assert
-		expect( ConfirmCreationCommand.introduction.show ).toHaveBeenCalledTimes( 1 );
+		expect( introductionManager.show ).toHaveBeenCalledTimes( 1 );
 		expect( global.$e.run ).toHaveBeenCalledTimes( 1 );
 		expect( global.$e.run ).toHaveBeenCalledWith( 'kit-elements-defaults/create', { container } );
-		expect( ConfirmCreationCommand.introduction.setViewed ).toHaveBeenCalled();
+		expect( introductionManager.setViewed ).toHaveBeenCalled();
 	} );
 
 	it( 'should use a fresh container for each `onConfirm` call', () => {
@@ -110,13 +111,13 @@ describe( `$e.run( 'kit-elements-defaults/confirm-creation' )`, () => {
 
 		// Act
 		command.apply( { container: container1 } );
-		ConfirmCreationCommand.introduction.dialog.onConfirm();
+		introductionManager.dialog.onConfirm();
 
 		command.apply( { container: container2 } );
-		ConfirmCreationCommand.introduction.dialog.onConfirm();
+		introductionManager.dialog.onConfirm();
 
 		// Assert
-		expect( ConfirmCreationCommand.introduction.show ).toHaveBeenCalledTimes( 2 );
+		expect( introductionManager.show ).toHaveBeenCalledTimes( 2 );
 		expect( global.$e.run ).toHaveBeenNthCalledWith( 1, 'kit-elements-defaults/create', { container: container1 } );
 		expect( global.$e.run ).toHaveBeenNthCalledWith( 2, 'kit-elements-defaults/create', { container: container2 } );
 	} );
