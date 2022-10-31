36c4d130dc6e9329ef9fca0d1012cc5be60820c2
Tzvi Rabinovitch <tzvir@elementor.com>
2022-10-31 13:52:37 +0200

    Added playwright test

---
+++ tests/playwright/sanity/dark-mode.test.js
@@ -0,0 +1,20 @@
+const { test, expect } = require( '@playwright/test' );
+const WpAdminPage = require( '../pages/wp-admin-page.js' );
+
+test.only( 'navigator empty placeholder should be in dark mode', async ( { page }, testInfo ) => {
+	// Arrange.
+	const wpAdmin = new WpAdminPage( page, testInfo ),
+		editor = await wpAdmin.openNewPage();
+
+	// Act.
+	await editor.addElement( { elType: 'container' }, 'document' );
+	await editor.changeUiTheme( 'dark' );
+	const navigator = await editor.page.locator( '#elementor-navigator' );
+	await navigator.locator( '.elementor-navigator__element__list-toggle' ).click();
+
+	// Assert
+	expect( await navigator.screenshot( {
+		type: 'jpeg',
+		quality: 70,
+	} ) ).toMatchSnapshot( 'navigator-empty-dark-mode.png' );
+} );
