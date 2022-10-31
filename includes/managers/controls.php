8717de77c5dd234693801696d88f0b1018efbe60
Rami Yushuvaev <ramiy@elementor.com>
2022-10-31 15:44:08 +0200

    Tweak: Replace remaining Elementor Pro labels

--- includes/managers/controls.php
+++ includes/managers/controls.php
@@ -1066,7 +1066,7 @@ class Controls_Manager {
 				<div class="elementor-nerd-box-message"><?php Utils::print_unescaped_internal_string( $message ); ?></div>
 			<?php }
 
-			// Show a `Go Pro` button only if the user doesn't have Pro.
+			// Show the upgrade button only if the user doesn't have Pro.
 			if ( $texts['link'] && ! Utils::has_pro() ) { ?>
 				<a class="elementor-nerd-box-link elementor-button elementor-button-default elementor-button-go-pro" href="<?php echo esc_url( ( $texts['link'] ) ); ?>" target="_blank">
 					<?php echo esc_html__( 'Upgrade Now', 'elementor' ); ?>
