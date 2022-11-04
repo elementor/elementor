3c2f27a6fd39d5ca4ce5a90c4063c8194d026cac
David Seveloff <davids@elementor.com>
2022-11-04 16:20:02 +0200

    Test improved

--- tests/jest/unit/app/modules/kit-library/assets/js/hooks/use-kit-call-to-action-updated-url.test.js
+++ tests/jest/unit/app/modules/kit-library/assets/js/hooks/use-kit-call-to-action-updated-url.test.js
@@ -1,58 +1,60 @@
 import useKitCallToActionUpdatedUrl from 'elementor/app/modules/kit-library/assets/js/hooks/use-add-kit-promotion-utm.js';
 
-test( 'useKitCallToActionUpdatedUrl should return the url with utm_term and utm_content', () => {
-	// Arrange
-	const url = 'https://elementor.com/pro/?utm_source=kit-library&utm_medium=wp-dash&utm_campaign=gopro';
-	const kitTitle = 'Digital Marketing Studio';
-	const kitId = '6242d5f21475e1001dc794ae';
-	const expectedUrl = 'https://elementor.com/pro/?utm_source=kit-library&utm_medium=wp-dash&utm_campaign=gopro&utm_term=digital-marketing-studio&utm_content=6242d5f21475e1001dc794ae';
+describe( 'useKitCallToActionUpdatedUrl', () => {
+	test( 'should return the url with utm_term and utm_content', () => {
+		// Arrange
+		const url = 'https://elementor.com/pro/?utm_source=kit-library&utm_medium=wp-dash&utm_campaign=gopro';
+		const kitTitle = 'Digital Marketing Studio';
+		const kitId = '6242d5f21475e1001dc794ae';
+		const expectedUrl = 'https://elementor.com/pro/?utm_source=kit-library&utm_medium=wp-dash&utm_campaign=gopro&utm_term=digital-marketing-studio&utm_content=6242d5f21475e1001dc794ae';
 
-	// Act
-	const updatedUrl = useKitCallToActionUpdatedUrl( url, kitId, kitTitle );
+		// Act
+		const updatedUrl = useKitCallToActionUpdatedUrl( url, kitId, kitTitle );
 
-	// Assert
-	expect( updatedUrl ).toBe( expectedUrl );
-} );
+		// Assert
+		expect( updatedUrl ).toBe( expectedUrl );
+	} );
 
-test( 'query params with  multiple spaces, numbers and invalid characters', () => {
-	// Arrange
-	const url = 'https://elementor.com/pro/?utm_source=kit-library&utm_medium=wp-dash&utm_campaign=gopro';
-	const kitTitle = 'Digital *  Marketing Studio 2';
-	const kitId = '6242d5f21475e1001dc794ae';
-	const expectedUrl = 'https://elementor.com/pro/?utm_source=kit-library&utm_medium=wp-dash&utm_campaign=gopro&utm_term=digital---marketing-studio-2&utm_content=6242d5f21475e1001dc794ae';
+	test( 'query params with  multiple spaces, numbers and invalid characters', () => {
+		// Arrange
+		const url = 'https://elementor.com/pro/?utm_source=kit-library&utm_medium=wp-dash&utm_campaign=gopro';
+		const kitTitle = 'Digital *  Marketing Studio 2';
+		const kitId = '6242d5f21475e1001dc794ae';
+		const expectedUrl = 'https://elementor.com/pro/?utm_source=kit-library&utm_medium=wp-dash&utm_campaign=gopro&utm_term=digital---marketing-studio-2&utm_content=6242d5f21475e1001dc794ae';
 
-	// Act
-	const updatedUrl = useKitCallToActionUpdatedUrl( url, kitId, kitTitle );
+		// Act
+		const updatedUrl = useKitCallToActionUpdatedUrl( url, kitId, kitTitle );
 
-	// Assert
-	expect( updatedUrl ).toBe( expectedUrl );
-} );
+		// Assert
+		expect( updatedUrl ).toBe( expectedUrl );
+	} );
 
-test( 'no query params', () => {
-	// Arrange
-	const url = 'https://elementor.com/pro/';
-	const kitTitle = 'Digital Marketing Studio';
-	const kitId = '6242d5f21475e1001dc794ae';
-	const expectedUrl = 'https://elementor.com/pro/?utm_term=digital-marketing-studio&utm_content=6242d5f21475e1001dc794ae';
+	test( 'no query params', () => {
+		// Arrange
+		const url = 'https://elementor.com/pro/';
+		const kitTitle = 'Digital Marketing Studio';
+		const kitId = '6242d5f21475e1001dc794ae';
+		const expectedUrl = 'https://elementor.com/pro/?utm_term=digital-marketing-studio&utm_content=6242d5f21475e1001dc794ae';
 
-	// Act
-	const updatedUrl = useKitCallToActionUpdatedUrl( url, kitId, kitTitle );
+		// Act
+		const updatedUrl = useKitCallToActionUpdatedUrl( url, kitId, kitTitle );
 
-	// Assert
-	expect( updatedUrl ).toBe( expectedUrl );
-} );
+		// Assert
+		expect( updatedUrl ).toBe( expectedUrl );
+	} );
 
-test( 'no query params', () => {
-	// Arrange
-	const url = 'https://elementor.com/pro/?utm_source=kit-library&utm_medium=wp-dash&utm_campaign=gopro&utm_term=already_exists';
-	const kitTitle = 'Digital Marketing Studio';
-	const kitId = '6242d5f21475e1001dc794ae';
-	const expectedUrl = 'https://elementor.com/pro/?utm_source=kit-library&utm_medium=wp-dash&utm_campaign=gopro&utm_term=digital-marketing-studio&utm_content=6242d5f21475e1001dc794ae';
+	test( 'no query params', () => {
+		// Arrange
+		const url = 'https://elementor.com/pro/?utm_source=kit-library&utm_medium=wp-dash&utm_campaign=gopro&utm_term=already_exists';
+		const kitTitle = 'Digital Marketing Studio';
+		const kitId = '6242d5f21475e1001dc794ae';
+		const expectedUrl = 'https://elementor.com/pro/?utm_source=kit-library&utm_medium=wp-dash&utm_campaign=gopro&utm_term=digital-marketing-studio&utm_content=6242d5f21475e1001dc794ae';
 
-	// Act
-	const updatedUrl = useKitCallToActionUpdatedUrl( url, kitId, kitTitle );
+		// Act
+		const updatedUrl = useKitCallToActionUpdatedUrl( url, kitId, kitTitle );
 
-	// Assert
-	expect( updatedUrl ).toBe( expectedUrl );
+		// Assert
+		expect( updatedUrl ).toBe( expectedUrl );
+	} );
 } );
 
