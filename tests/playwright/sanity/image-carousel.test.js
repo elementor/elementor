e06a1a9f437dd9eb6e56711837be4e4d235b2f83
Hein van Vlastuin <vv@abanganimedia.co.za>
2022-11-07 19:46:35 +0200

    update image carousel

--- tests/playwright/sanity/image-carousel.test.js
+++ tests/playwright/sanity/image-carousel.test.js
@@ -18,26 +18,8 @@ test( 'Image Carousel', async ( { page }, testInfo ) => {
   // Hide slider navigation.
   await page.selectOption( '.elementor-control-navigation >> select', 'none' );
 
-  // Upload the images to WP media library
-  await page.locator( '[aria-label="Add Images"]' ).click();
-  await page.click( 'text=Media Library' );
-  await page.setInputFiles( 'input[type="file"]', './tests/playwright/resources/A.jpg' );
-  await page.setInputFiles( 'input[type="file"]', './tests/playwright/resources/B.jpg' );
-  await page.setInputFiles( 'input[type="file"]', './tests/playwright/resources/C.jpg' );
-  await page.setInputFiles( 'input[type="file"]', './tests/playwright/resources/D.jpg' );
-  await page.setInputFiles( 'input[type="file"]', './tests/playwright/resources/E.jpg' );
-
-  // Create a new gallery
-  await page.locator( 'text=Create a new gallery' ).click();
-
-  // Insert gallery
-  await page.locator( 'text=Insert gallery' ).click();
-
-  // Open The Additional options Section
-  await page.click( '#elementor-controls >> :nth-match(div:has-text("Additional Options"), 3)' );
-
-  // Disable AutoPlay
-  await page.selectOption( 'select', 'no' );
+  // Populate the widget with images.
+  await editor.populateImageCarousel();
 
   expect( await editor.getPreviewFrame().locator( 'div.elementor-image-carousel-wrapper.swiper-container.swiper-container-initialized' ).screenshot( { type: 'jpeg', quality: 70 } ) ).toMatchSnapshot( 'carousel.jpeg' );
 
