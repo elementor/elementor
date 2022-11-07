c956a27aa4a5278af3598ca93d95b9a4a48762cd
Nevo Golan <20494189+Nevoss@users.noreply.github.com>
2022-11-07 18:41:45 +0200

    New: Kit defaults - Import/Export runner [ED-8179] (#20099)

--- modules/kit-elements-defaults/module.php
+++ modules/kit-elements-defaults/module.php
@@ -5,6 +5,7 @@ use Elementor\Core\Experiments\Manager as Experiments_Manager;
 use Elementor\Core\Base\Module as BaseModule;
 use Elementor\Modules\KitElementsDefaults\Data\Controller;
 use Elementor\Plugin;
+use Elementor\Modules\KitElementsDefaults\ImportExport\Import_Export;
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly
@@ -54,5 +55,9 @@ class Module extends BaseModule {
 		Plugin::$instance->data_manager_v2->register_controller( new Controller() );
 
 		( new Usage() )->register();
+
+		if ( is_admin() ) {
+			( new Import_Export() )->register();
+		}
 	}
 }
