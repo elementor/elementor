9f0e735f5ddf931832083dbfd10fc1526f0b0cfc
Nevo Golan <20494189+Nevoss@users.noreply.github.com>
2022-11-08 19:25:26 +0200

    wip

--- s.bash
+++
@@ -1,2 +0,0 @@
-for file in tests/elements-regression/tests/**/*.jpeg; do ext="${file##*.}"; filename="${file%%-linux.*}"; mv "$file" "${filename}(editor)-linux.${ext}"; done
-for file in tests/elements-regression/tests/**/*\(editor\).jpeg; do rm $file; done
