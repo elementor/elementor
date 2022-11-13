61208f67cdfed0716a57ac0d67d5029dc9f7d708
Mark <markt@elementor.com>
2022-11-13 11:47:21 +0200

    Internal: Restrict users from being able to access Submissions feature when license is inactive [HTS-24]

--- includes/settings/settings.php
+++ includes/settings/settings.php
@@ -384,7 +384,6 @@ class Settings extends Settings_Page {
 			'elementor_custom_fonts',
 			'elementor_custom_icons',
 			'elementor-license',
-			'e-form-submissions',
 			'elementor_custom_custom_code',
 			'popup_templates',
 		];
