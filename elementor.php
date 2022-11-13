c0b25709e326256e3a7555621e91e92bfb1a4ee3
Ariel Klikstein <ariel@elementor.com>
2022-11-13 11:30:09 +0200

    internal: stable version

--- elementor.php
+++ elementor.php
@@ -28,7 +28,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 define( 'ELEMENTOR_VERSION', '3.8.0' );
-define( 'ELEMENTOR_PREVIOUS_STABLE_VERSION', '3.8.0' );
+define( 'ELEMENTOR_PREVIOUS_STABLE_VERSION', '3.7.8' );
 
 define( 'ELEMENTOR__FILE__', __FILE__ );
 define( 'ELEMENTOR_PLUGIN_BASE', plugin_basename( ELEMENTOR__FILE__ ) );
