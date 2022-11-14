693760ec431e2771ef78f17cb351e416c5d7072a
Mati Horovitz <mati@elementor.com>
2022-11-14 17:38:08 +0200

    Internal: Elements Regression - Summary for tested controls [ED-8292]

--- tests/elements-regression/src/utils.js
+++ tests/elements-regression/src/utils.js
@@ -1,3 +1,33 @@
 module.exports = {
 	isObject: ( value ) => value && 'object' === typeof value && value.constructor === Object,
+
+	/**
+	 * @param {Object} tested   e.g. { heading: { title: 'Hello World' } }
+	 * @param {Object} expected e.g. { heading: { controls: { title: 'Hello World' } } }
+	 * @return {{
+	 * 	testedWidgetsCount: number,
+	 * 	expectedWidgetsCount: number,
+	 * 	testedControlsCount: number,
+	 * 	expectedControlsCount: number,
+	 * }}
+	 */
+	summary: ( tested, expected ) => {
+		const testedWidgetsCount = Object.keys( tested ).length,
+			expectedWidgetsCount = Object.keys( expected ).length;
+
+		const testedControlsCount = Object.values( tested ).reduce( ( acc, controls ) => {
+			return acc + Object.keys( controls ).length;
+		}, 0 );
+
+		const expectedControlsCount = Object.values( expected ).reduce( ( acc, element ) => {
+			return acc + Object.keys( element.controls ).length;
+		}, 0 );
+
+		return {
+			testedWidgetsCount,
+			expectedWidgetsCount,
+			testedControlsCount,
+			expectedControlsCount,
+		};
+	},
 };
