ce3fb65c6fa566e224001c82a4bb89ff4f66e421
Nevo Golan <20494189+Nevoss@users.noreply.github.com>
2022-11-15 17:09:12 +0200

    wip

--- includes/widgets/common.php
+++ includes/widgets/common.php
@@ -722,7 +722,7 @@ class Widget_Common extends Widget_Base {
 		$this->add_control(
 			'_background_hover_transition',
 			[
-				'label' => esc_html__( 'Transition Duration', 'elementor' ),
+				'label' => esc_html__( 'Transition Duration', 'elementor' ) . ' (ms)',
 				'type' => Controls_Manager::SLIDER,
 				'range' => [
 					'px' => [
