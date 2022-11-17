93750234ab95de6296b628e1f1c459862bf7c063
David Seveloff <davids@elementor.com>
2022-11-17 11:22:15 +0200

    WIP - Test working before peer review

--- tests/playwright/sanity/modules/tabs-v2/tabs.test.js
+++ tests/playwright/sanity/modules/tabs-v2/tabs.test.js
@@ -1,9 +1,10 @@
 const { test, expect } = require( '@playwright/test' );
 const WpAdminPage = require( '../../../pages/wp-admin-page' );
 
-test.beforeAll( async ( { page } ) => {
-	const wpAdmin = new WpAdminPage( page );
+test.describe( 'tabs v2 icon vertical alignment', async ( { page }, testInfo ) => {
 
+	// Arrange.
+	const wpAdmin = new WpAdminPage( page, testInfo );
 	await wpAdmin.setExperiments( {
 		container: true,
 		'nested-elements': true,
@@ -19,84 +20,22 @@ test.beforeAll( async ( { page } ) => {
 	// Set icons to tabs according 'TabsIcons' array.
 	await setIconsToTabs( page, TabsIcons );
 
-} );
-
-test.describe( 'tabs v2 icon vertical alignment', () => {
-	test( 'Tabs icons', async ( { page } ) => {
-
-	}
-
-} );
-
-
-test( 'tabs v2 icon vertical alignment', async ( { page }, testInfo ) => {
-	// Arrange.
-	const wpAdmin = new WpAdminPage( page, testInfo );
-	// await wpAdmin.setExperiments( {
-	// 	container: true,
-	// 	'nested-elements': true,
-	// } );
-
-	// const editor = await wpAdmin.useElementorCleanPost(),
-	// 	container = await editor.addElement( { elType: 'container' }, 'document' );
-	//
-	// // Add widgets.
-	// await editor.addWidget( 'tabs-v2', container );
-	// await editor.getPreviewFrame().waitForSelector( '.elementor-tabs-content-wrapper .e-con.elementor-active' );
-	//
-	// // Set icons to tabs according 'TabsIcons' array.
-	// await setIconsToTabs( page, TabsIcons );
-
 	const icon = await editor.getPreviewFrame().locator( '.elementor-widget-tabs-v2 .elementor-tab-title .elementor-tab-icon' ).first();
 	const activeTabIcon = await editor.getPreviewFrame().locator( '.elementor-widget-tabs-v2 .elementor-tab-title .elementor-tab-icon-active' ).first();
 
-	// Initial state
-	await page.locator( '.elementor-control.elementor-control-tabs_location_horizontal .elementor-control-content .elementor-control-field .elementor-control-input-wrapper .elementor-choices label' ).first().click();
-
-	// Assert
-	await expect( icon ).toHaveCSS( 'align-items', 'center' );
-	await clickTab( editor, 'right' );
-	await expect( activeTabIcon ).toHaveCSS( 'align-items', 'center' );
-
-	// End
-	await page.locator( '.eicon-align-end-h' ).first().click();
-
-	// Assert
-	await expect( icon ).toHaveCSS( 'align-items', 'center' );
-	await clickTab( editor, 'middle' );
-	await expect( activeTabIcon ).toHaveCSS( 'align-items', 'center' );
-
-	// Center
-	await page.locator( '.eicon-h-align-center' ).first().click();
-
-	// Assert
-	await expect( icon ).toHaveCSS( 'align-items', 'center' );
-	await clickTab( editor, 'left' );
-	await expect( activeTabIcon ).toHaveCSS( 'align-items', 'center' );
-
-	// Justified
-	await page.locator( '.eicon-h-align-stretch' ).first().click();
-
-	// Assert
-	await expect( icon ).toHaveCSS( 'align-items', 'center' );
-	await expect( activeTabIcon ).toHaveCSS( 'align-items', 'center' );
+	let locationOption;
+	// Check first tab icon vertical alignment.
+	for ( locationOption of locationOptions ) {
+		test( `Should be vertically centered when location is on ${ locationOption }`, async ( { page } ) = {
+			await page.locator( `.elementor-control-tabs_location_horizontal [original-title='${ locationOption }']` ).first().click();
+			await expect( icon ).toHaveCSS( 'align-items', 'center' );
+			await clickTab( editor, '1' );
+			await expect( activeTabIcon ).toHaveCSS( 'align-items', 'center' );
+			await clickTab( editor, '0' );
+		} );
+	}
 } );
 
-const clickTab = async ( editor, tabPosition ) => {
-	const tabs = await editor.getPreviewFrame().locator( '.elementor-widget-tabs-v2 .elementor-tabs-wrapper' ).first();
-	tabs.click( { button: tabPosition, clickCount: 1 } );
-	// await editor.getPreviewFrame().locator( `.elementor-widget-tabs-v2 .elementor-tab-title:nth-child(${ tabNumber })` ).first().click();
-};
-
-const addIcon = async ( page, icon ) => {
-	await page.locator( `#elementor-icons-manager__tab__content .${ icon }` ).first().click();
-	await page.locator( '.dialog-lightbox-insert_icon' ).click();
-};
-
-const locationOptions = {
-	horizontal: 'elementor-control-tabs_location_horizontal',
-	vertical: 'elementor-control-tabs_location_vertical',
-};
 const TabsIcons = [
 	{
 		icon: 'fa-arrow-alt-circle-right',
@@ -112,6 +51,13 @@ const TabsIcons = [
 	},
 ];
 
+// Set icons to tabs, used in setIconsToTabs function.
+const addIcon = async ( page, icon ) => {
+	await page.locator( `#elementor-icons-manager__tab__content .${ icon }` ).first().click();
+	await page.locator( '.dialog-lightbox-insert_icon' ).click();
+};
+
+// Iterate tabs and add an icon and an active Icon to each one.
 const setIconsToTabs = async ( page, TabIcons ) => {
 	for ( const tab of TabIcons ) {
 		const index = TabsIcons.indexOf( tab ) + 1;
@@ -122,3 +68,10 @@ const setIconsToTabs = async ( page, TabIcons ) => {
 		await addIcon( page, tab.activeIcon );
 	}
 };
+
+// Click on tab by position.
+const clickTab = async ( editor, tabPosition ) => {
+	await editor.getPreviewFrame().locator( `.elementor-widget-tabs-v2 .elementor-tabs-wrapper .elementor-tab-title >> nth=${ tabPosition } ` ).first().click();
+};
+
+const locationOptions = [ 'Start', 'Center', 'End', 'Justified' ];
