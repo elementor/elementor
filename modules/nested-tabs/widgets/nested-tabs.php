6984a4af2c24e761e0409fe31c1cdb1870ed53a3
David Seveloff <davids@elementor.com>
2022-11-22 14:54:17 +0200

    Gap and space works - Before tests were written.

--- modules/nested-tabs/widgets/nested-tabs.php
+++ modules/nested-tabs/widgets/nested-tabs.php
@@ -369,21 +369,6 @@ class NestedTabs extends Widget_Nested_Base {
 			'tab' => Controls_Manager::TAB_STYLE,
 		] );
 
-		$this->add_responsive_control( 'tabs_title_spacing', [
-			'label' => esc_html__( 'Distance from content', 'elementor' ),
-			'type' => Controls_Manager::SLIDER,
-			'range' => [
-				'px' => [
-					'min' => 0,
-					'max' => 400,
-				],
-			],
-			'size_units' => [ 'px' ],
-			'selectors' => [
-				'{{WRAPPER}}' => '--n-tabs-gap: {{SIZE}}{{UNIT}}',
-			],
-		] );
-
 		$this->add_responsive_control( 'tabs_title_space_between', [
 			'label' => esc_html__( 'Gap between tabs', 'elementor' ),
 			'type' => Controls_Manager::SLIDER,
@@ -396,6 +381,21 @@ class NestedTabs extends Widget_Nested_Base {
 			'size_units' => [ 'px' ],
 			'selectors' => [
 				'{{WRAPPER}}' => '--n-tabs-title-gap: {{SIZE}}{{UNIT}}',
+			],
+		] );
+
+		$this->add_responsive_control( 'tabs_title_spacing', [
+			'label' => esc_html__( 'Distance from content', 'elementor' ),
+			'type' => Controls_Manager::SLIDER,
+			'range' => [
+				'px' => [
+					'min' => 0,
+					'max' => 400,
+				],
+			],
+			'size_units' => [ 'px' ],
+			'selectors' => [
+				'{{WRAPPER}}' => '--n-tabs-gap: {{SIZE}}{{UNIT}}',
 			],
 		] );
 
