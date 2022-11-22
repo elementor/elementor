7b602a2fff0f5ac8eb44cc13f9e10f69d2be3e20
Mati Horovitz <mati@elementor.com>
2022-11-22 18:33:26 +0200

    Progress: Extract panel components

---
+++ packages/editor/src/components/panel-wrapper/Loading.jsx
@@ -0,0 +1,7 @@
+export const Loading = () => {
+	return (
+		<div id="elementor-panel-state-loading">
+			<i className="eicon-loading eicon-animation-spin"></i>
+		</div>
+	);
+}
---
+++ packages/editor/src/components/panel-wrapper/SwitchMode.jsx
@@ -0,0 +1,14 @@
+export const SwitchMode = () => {
+	return (
+		<div id="elementor-mode-switcher">
+			<div id="elementor-mode-switcher-inner">
+				<input id="elementor-mode-switcher-preview-input" type="checkbox"/>
+				<label htmlFor="elementor-mode-switcher-preview-input" id="elementor-mode-switcher-preview"
+					   title="Preview">
+					<i className="eicon" aria-hidden="true" title="Hide Panel"></i>
+					<span className="elementor-screen-only">Preview</span>
+				</label>
+			</div>
+		</div>
+	);
+}
