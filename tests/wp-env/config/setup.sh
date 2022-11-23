0d4796707efca844dbcf8c13c3af7276d5647309
Yotam Shapira <yotams@elementor.com>
2022-11-23 08:37:57 +0200

    cleanup

--- tests/wp-env/config/setup.sh
+++ tests/wp-env/config/setup.sh
@@ -12,4 +12,4 @@ wp elementor flush-css
 wp post list --post_type=page
 
 # Remove the Guttenberg welcome guide popup
-wp user meta add admin wp_persisted_preferences 'a:2:{s:14:\"core/edit-post\";a:2:{s:26:\"isComplementaryAreaVisible\";b:1;s:12:\"welcomeGuide\";b:0;}s:9:\"_modified\";s:24:\"2022-11-21T10:38:46.200Z\";}'
+wp user meta add admin wp_persisted_preferences 'a:2:{s:14:\"core/edit-post\";a:2:{b:1;s:12:\"welcomeGuide\";b:0;}}'
