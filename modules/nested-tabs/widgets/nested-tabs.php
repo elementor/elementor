3d8b6f575e57c061d4565702ce675db79f1f5209
Hein van Vlastuin <vv@abanganimedia.co.za>
2022-11-27 11:51:44 +0200

    php lint fix

--- modules/nested-tabs/widgets/nested-tabs.php
+++ modules/nested-tabs/widgets/nested-tabs.php
@@ -690,7 +690,7 @@ class NestedTabs extends Widget_Nested_Base {
 				'label' => esc_html__( 'Color', 'elementor' ),
 				'type' => Controls_Manager::COLOR,
 				'selectors' => [
-					"{{WRAPPER}} .e-n-tab-title:hover" => '--n-tabs-title-color-hover: {{VALUE}}',
+					'{{WRAPPER}} .e-n-tab-title:hover' => '--n-tabs-title-color-hover: {{VALUE}}',
 				],
 			]
 		);
