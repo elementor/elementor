831f67979b5a41f8029bfdf64f26a5344fc09b17
Yakir Sitbon <kingyes1@gmail.com>
2022-11-28 18:36:08 +0200

    Tweak: Add `preload` attribute to self hosted videos in Video widget [ED-8872] (#20554)
    Fixes #17308

--- includes/widgets/video.php
+++ includes/widgets/video.php
@@ -514,6 +514,28 @@ class Widget_Video extends Widget_Base {
 		);
 
 		$this->add_control(
+			'preload',
+			[
+				'label' => esc_html__( 'Preload', 'elementor' ),
+				'type' => Controls_Manager::SELECT,
+				'options' => [
+					'metadata' => esc_html__( 'Metadata', 'elementor' ),
+					'auto' => esc_html__( 'Auto', 'elementor' ),
+					'none' => esc_html__( 'None', 'elementor' ),
+				],
+				'description' => sprintf(
+					esc_html__( 'Preload attribute lets you specify how the video should be loaded when the page loads. %1$sLearn More%2$s', 'elementor' ),
+					'<a target="_blank" href="https://go.elementor.com/preload-video/">',
+					'</a>'
+				),
+				'default' => 'metadata',
+				'condition' => [
+					'video_type' => 'hosted',
+				],
+			]
+		);
+
+		$this->add_control(
 			'poster',
 			[
 				'label' => esc_html__( 'Poster', 'elementor' ),
@@ -1161,7 +1183,9 @@ class Widget_Video extends Widget_Base {
 	private function get_hosted_params() {
 		$settings = $this->get_settings_for_display();
 
-		$video_params = [];
+		$video_params = [
+			'preload' => $settings['preload'],
+		];
 
 		foreach ( [ 'autoplay', 'loop', 'controls' ] as $option_name ) {
 			if ( $settings[ $option_name ] ) {
