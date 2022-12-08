22b5e4ddb72d09e0c8842bed344d7f2b9d688216
Yakir Sitbon <kingyes1@gmail.com>
2022-12-08 14:27:32 +0200

    Tweak: Added more sections on System info [ED-8741]

--- modules/usage/module.php
+++ modules/usage/module.php
@@ -582,6 +582,35 @@ class Module extends BaseModule {
 		}
 	}
 
+	public static function get_settings_usage() {
+		$usage = [];
+
+		$settings_tab = Plugin::$instance->settings->get_tabs();
+		$settings = array_merge( $settings_tab[ \Elementor\Settings::TAB_GENERAL ]['sections'], $settings_tab[ \Elementor\Settings::TAB_ADVANCED ]['sections'] );
+
+		foreach ( $settings as $setting_data ) {
+			foreach ( $setting_data['fields'] as $field_name => $field_data ) {
+				if ( empty( $field_data['field_args']['type'] ) || 'hidden' === $field_data['field_args']['type'] ) {
+					continue;
+				}
+
+				$setting_value = get_option( 'elementor_' . $field_name );
+
+				if ( empty( $setting_value ) ) {
+					continue;
+				}
+
+				if ( ! empty( $field_data['field_args']['std'] ) && $setting_value === $field_data['field_args']['std'] ) {
+					continue;
+				}
+
+				$usage[ $field_name ] = $setting_value;
+			}
+		}
+
+		return $usage;
+	}
+
 	/**
 	 * Add system info report.
 	 */
@@ -589,6 +618,11 @@ class Module extends BaseModule {
 		System_Info::add_report( 'usage', [
 			'file_name' => __DIR__ . '/usage-reporter.php',
 			'class_name' => __NAMESPACE__ . '\Usage_Reporter',
+		] );
+
+		System_Info::add_report( 'settings', [
+			'file_name' => __DIR__ . '/settings-reporter.php',
+			'class_name' => __NAMESPACE__ . '\Settings_Reporter',
 		] );
 	}
 
