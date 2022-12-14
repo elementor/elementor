05f5426708dc04446a6e173c2343dd9baf689a4d
Nevo Golan <20494189+Nevoss@users.noreply.github.com>
2022-12-14 12:26:30 +0200

    review fixes

--- modules/kit-elements-defaults/utils/settings-sanitizer.php
+++ modules/kit-elements-defaults/utils/settings-sanitizer.php
@@ -5,7 +5,6 @@ use Elementor\Core\Breakpoints\Manager as Breakpoints_Manager;
 use Elementor\Element_Base;
 use Elementor\Elements_Manager;
 use Elementor\Core\Base\Document;
-use Elementor\Core\Utils\Collection;
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
@@ -102,25 +101,19 @@ class Settings_Sanitizer {
 
 		$available_settings = $this->get_avaliable_settings( $controls );
 
-		$this->pending_settings = ( new Collection( $this->pending_settings ) )
-			// Remove all the settings that are not available in the current element + save the special settings.
-			->only( array_merge(
-				$available_settings,
-				static::SPECIAL_SETTINGS
-			) )
-			->map( function ( $value, $key ) use ( $available_settings ) {
-				$is_special_setting = in_array( $key, static::SPECIAL_SETTINGS, true );
-
-				// Remove all the special settings that are not available in the current element.
-				if ( $is_special_setting ) {
-					return ( new Collection( $value ) )
-						->only( $available_settings )
-						->all();
+		foreach ( $this->pending_settings as $key => $value ) {
+			if ( in_array( $key, static::SPECIAL_SETTINGS, true ) ) {
+				// When the key is special settings like __dynamic__ or __globals__ it should run over the values.
+				foreach ( $this->pending_settings[ $key ] as $special_setting_key => $special_setting_value ) {
+					if ( ! in_array( $special_setting_key, $available_settings, true ) ) {
+						unset( $this->pending_settings[ $key ][ $special_setting_key ] );
+					}
 				}
-
-				return $value;
-			} )
-			->all();
+			} elseif ( ! in_array( $key, $available_settings, true ) ) {
+				// For regular settings, it should remove if not in the available settings.
+				unset( $this->pending_settings[ $key ] );
+			}
+		}
 
 		return $this;
 	}
@@ -234,29 +227,28 @@ class Settings_Sanitizer {
 	 * @return array
 	 */
 	private function get_avaliable_settings( array $controls ) {
-		$control_keys = new Collection(
-			array_keys( $controls )
-		);
+		$control_keys = array_keys( $controls );
 
-		$optional_responsive_keys = new Collection( [
+		$optional_responsive_keys = [
 			Breakpoints_Manager::BREAKPOINT_KEY_MOBILE,
 			Breakpoints_Manager::BREAKPOINT_KEY_MOBILE_EXTRA,
 			Breakpoints_Manager::BREAKPOINT_KEY_TABLET,
 			Breakpoints_Manager::BREAKPOINT_KEY_TABLET_EXTRA,
 			Breakpoints_Manager::BREAKPOINT_KEY_LAPTOP,
 			Breakpoints_Manager::BREAKPOINT_KEY_WIDESCREEN,
-		] );
+		];
 
-		return $control_keys
-			->map( function ( $control_key ) use ( $optional_responsive_keys ) {
-				// Add the responsive settings.
-				return $optional_responsive_keys->map( function ( $responsive_key ) use ( $control_key ) {
-					return "{$control_key}_{$responsive_key}";
-				} )
-				->push( $control_key ) // Add the setting itself (not responsive).
-				->all();
-			} )
-			->flatten()
-			->values();
+		$settings = [];
+
+		foreach ( $control_keys as $control_key ) {
+			// Add the responsive settings.
+			foreach ( $optional_responsive_keys as $responsive_key ) {
+				$settings[] = "{$control_key}_{$responsive_key}";
+			}
+			// Add the setting itself (not responsive).
+			$settings[] = $control_key;
+		}
+
+		return $settings;
 	}
 }
