6fb6b88ab4db7b0909475b3a46f2a33a95f8fbf2
Mati Horovitz <mati@elementor.com>
2022-12-18 13:31:07 +0200

    PHP Lint

--- core/utils/str.php
+++ core/utils/str.php
@@ -18,7 +18,7 @@ class Str {
 	public static function encode_idn_url( $url ) {
 		return preg_replace_callback( '/(https?:\/\/)(.+)/', function ( $matches ) {
 			// WP >= 6.2-alpha
-			if ( class_exists('\WpOrg\Requests\IdnaEncoder' ) ) {
+			if ( class_exists( '\WpOrg\Requests\IdnaEncoder' ) ) {
 				$class = \WpOrg\Requests\IdnaEncoder::class;
 			} else {
 				$class = \Requests_IDNAEncoder::class;
