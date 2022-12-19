a7a18f777e95f5284ae84f592cd4933cc27583c4
Nevo Golan <20494189+Nevoss@users.noreply.github.com>
2022-12-19 17:39:28 +0200

    New: Editor V2 - Backend experiment + loading the scripts (#20562)

--- core/editor/editor.php
+++ core/editor/editor.php
@@ -7,6 +7,8 @@ use Elementor\Core\Breakpoints\Manager as Breakpoints_Manager;
 use Elementor\Core\Common\Modules\Ajax\Module;
 use Elementor\Core\Common\Modules\Ajax\Module as Ajax;
 use Elementor\Core\Debug\Loading_Inspection_Manager;
+use Elementor\Core\Editor\Config_Providers\Config_Provider_Factory;
+use Elementor\Core\Experiments\Manager as Experiments_Manager;
 use Elementor\Core\Files\Uploads_Manager;
 use Elementor\Core\Schemes\Manager as Schemes_Manager;
 use Elementor\Core\Settings\Manager as SettingsManager;
@@ -38,6 +40,8 @@ class Editor {
 	 * User capability required to access Elementor editor.
 	 */
 	const EDITING_CAPABILITY = 'edit_posts';
+
+	const EDITOR_V2_EXPERIMENT_NAME = 'editor_v2';
 
 	/**
 	 * Post ID.
@@ -74,6 +78,11 @@ class Editor {
 	public $promotion;
 
 	/**
+	 * @var Editor_Loader
+	 */
+	private $loader;
+
+	/**
 	 * Init.
 	 *
 	 * Initialize Elementor editor. Registers all needed actions to run Elementor,
@@ -90,6 +99,8 @@ class Editor {
 		if ( empty( $_REQUEST['post'] ) ) {
 			return;
 		}
+
+		$this->loader = new Editor_Loader( Config_Provider_Factory::create() );
 
 		$this->set_post_id( absint( $_REQUEST['post'] ) );
 
@@ -160,7 +171,7 @@ class Editor {
 
 		do_action( 'elementor/editor/init' );
 
-		$this->print_editor_template();
+		$this->loader->print_root_template();
 
 		// From the action it's an empty string, from tests its `false`
 		if ( false !== $die ) {
@@ -311,6 +322,8 @@ class Editor {
 	}
 
 	/**
+	 * NOTICE: This method not in use, it's here for backward compatibility.
+	 *
 	 * Print Editor Template.
 	 *
 	 * Include the wrapper template of the editor.
@@ -341,164 +354,9 @@ class Editor {
 		$wp_styles = new \WP_Styles(); // phpcs:ignore WordPress.WP.GlobalVariablesOverride.Prohibited
 		$wp_scripts = new \WP_Scripts(); // phpcs:ignore WordPress.WP.GlobalVariablesOverride.Prohibited
 
-		$suffix = ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG || defined( 'ELEMENTOR_TESTS' ) && ELEMENTOR_TESTS ) ? '' : '.min';
+		$suffix = ( Utils::is_script_debug() || Utils::is_elementor_tests() ) ? '' : '.min';
 
-		wp_register_script(
-			'elementor-editor-modules',
-			ELEMENTOR_ASSETS_URL . 'js/editor-modules' . $suffix . '.js',
-			[
-				'elementor-common-modules',
-			],
-			ELEMENTOR_VERSION,
-			true
-		);
-
-		wp_register_script(
-			'elementor-editor-document',
-			ELEMENTOR_ASSETS_URL . 'js/editor-document' . $suffix . '.js',
-			[
-				'elementor-common-modules',
-			],
-			ELEMENTOR_VERSION,
-			true
-		);
-		// Hack for waypoint with editor mode.
-		wp_register_script(
-			'elementor-waypoints',
-			ELEMENTOR_ASSETS_URL . 'lib/waypoints/waypoints-for-editor.js',
-			[
-				'jquery',
-			],
-			'4.0.2',
-			true
-		);
-
-		wp_register_script(
-			'perfect-scrollbar',
-			ELEMENTOR_ASSETS_URL . 'lib/perfect-scrollbar/js/perfect-scrollbar' . $suffix . '.js',
-			[],
-			'1.4.0',
-			true
-		);
-
-		wp_register_script(
-			'jquery-easing',
-			ELEMENTOR_ASSETS_URL . 'lib/jquery-easing/jquery-easing' . $suffix . '.js',
-			[
-				'jquery',
-			],
-			'1.3.2',
-			true
-		);
-
-		wp_register_script(
-			'nprogress',
-			ELEMENTOR_ASSETS_URL . 'lib/nprogress/nprogress' . $suffix . '.js',
-			[],
-			'0.2.0',
-			true
-		);
-
-		wp_register_script(
-			'tipsy',
-			ELEMENTOR_ASSETS_URL . 'lib/tipsy/tipsy' . $suffix . '.js',
-			[
-				'jquery',
-			],
-			'1.0.0',
-			true
-		);
-
-		wp_register_script(
-			'jquery-elementor-select2',
-			ELEMENTOR_ASSETS_URL . 'lib/e-select2/js/e-select2.full' . $suffix . '.js',
-			[
-				'jquery',
-			],
-			'4.0.6-rc.1',
-			true
-		);
-
-		wp_register_script(
-			'flatpickr',
-			ELEMENTOR_ASSETS_URL . 'lib/flatpickr/flatpickr' . $suffix . '.js',
-			[
-				'jquery',
-			],
-			'1.12.0',
-			true
-		);
-
-		wp_register_script(
-			'ace',
-			'https://cdnjs.cloudflare.com/ajax/libs/ace/1.2.5/ace.js',
-			[],
-			'1.2.5',
-			true
-		);
-
-		wp_register_script(
-			'ace-language-tools',
-			'https://cdnjs.cloudflare.com/ajax/libs/ace/1.2.5/ext-language_tools.js',
-			[
-				'ace',
-			],
-			'1.2.5',
-			true
-		);
-
-		wp_register_script(
-			'jquery-hover-intent',
-			ELEMENTOR_ASSETS_URL . 'lib/jquery-hover-intent/jquery-hover-intent' . $suffix . '.js',
-			[],
-			'1.0.0',
-			true
-		);
-
-		wp_register_script(
-			'nouislider',
-			ELEMENTOR_ASSETS_URL . 'lib/nouislider/nouislider' . $suffix . '.js',
-			[],
-			'13.0.0',
-			true
-		);
-
-		wp_register_script(
-			'pickr',
-			ELEMENTOR_ASSETS_URL . 'lib/pickr/pickr.min.js',
-			[],
-			'1.5.0',
-			true
-		);
-
-		wp_register_script(
-			'elementor-editor',
-			ELEMENTOR_ASSETS_URL . 'js/editor' . $suffix . '.js',
-			[
-				'elementor-common',
-				'elementor-editor-modules',
-				'elementor-editor-document',
-				'wp-auth-check',
-				'jquery-ui-sortable',
-				'jquery-ui-resizable',
-				'perfect-scrollbar',
-				'nprogress',
-				'tipsy',
-				'imagesloaded',
-				'heartbeat',
-				'jquery-elementor-select2',
-				'flatpickr',
-				'ace',
-				'ace-language-tools',
-				'jquery-hover-intent',
-				'nouislider',
-				'pickr',
-				'react',
-				'react-dom',
-			],
-			ELEMENTOR_VERSION,
-			true
-		);
+		$this->loader->register_scripts();
 
 		/**
 		 * Before editor enqueue scripts.
@@ -616,7 +474,7 @@ class Editor {
 
 		Utils::print_js_config( 'elementor-editor', 'ElementorConfig', $config );
 
-		wp_enqueue_script( 'elementor-editor' );
+		$this->loader->enqueue_scripts();
 
 		wp_set_script_translations( 'elementor-editor', 'elementor' );
 
@@ -881,6 +739,8 @@ class Editor {
 		add_action( 'admin_action_elementor', [ $this, 'init' ] );
 		add_action( 'template_redirect', [ $this, 'redirect_to_new_url' ] );
 
+		$this->register_editor_v2_experiment();
+
 		// Handle autocomplete feature for URL control.
 		add_filter( 'wp_link_query_args', [ $this, 'filter_wp_link_query_args' ] );
 		add_filter( 'wp_link_query', [ $this, 'filter_wp_link_query' ] );
@@ -971,5 +831,22 @@ class Editor {
 
 	public function set_post_id( $post_id ) {
 		$this->post_id = $post_id;
+	}
+
+	/**
+	 * Adding Editor V2 experiment.
+	 *
+	 * @return void
+	 * @throws \Exception
+	 */
+	private function register_editor_v2_experiment() {
+		Plugin::$instance->experiments->add_feature( [
+			'name' => static::EDITOR_V2_EXPERIMENT_NAME,
+			'title' => esc_html__( 'Editor V2', 'elementor' ),
+			'description' => esc_html__( 'Enable the new editor.', 'elementor' ),
+			'hidden' => true,
+			'default' => Experiments_Manager::STATE_INACTIVE,
+			'status' => Experiments_Manager::RELEASE_STATUS_ALPHA,
+		] );
 	}
 }
