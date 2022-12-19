a1f70832bb46c46fb5184dd865eb701ff160db34
Ariel Klikstein <ariel@elementor.com>
2022-12-19 16:51:23 +0200

    Internal: change select class

--- assets/dev/js/editor/controls/media.js
+++ assets/dev/js/editor/controls/media.js
@@ -14,7 +14,7 @@ ControlMediaItemView = ControlMultipleBaseItemView.extend( {
 		ui.removeButton = '.elementor-control-media__remove';
 		ui.fileName = '.elementor-control-media__file__content__info__name';
 
-		ui.mediaInputImageSize = '.elementor-control-media__size';
+		ui.mediaInputImageSize = '.e-thumb-size-select';
 
 		return ui;
 	},
--- includes/controls/media.php
+++ includes/controls/media.php
@@ -276,7 +276,7 @@ class Control_Media extends Control_Base_Multiple {
 				<div class="elementor-control-field">
 					<label class="elementor-control-title" for="<?php $this->print_control_uid( 'size' ); ?>"><?php echo esc_html__( 'Thumb Size', 'elementor' ); ?></label>
 					<div class="elementor-control-input-wrapper elementor-control-unit-5">
-						<select id="<?php $this->print_control_uid( 'size' ); ?>" data-setting="size">
+						<select class="e-thumb-size-select" id="<?php $this->print_control_uid( 'size' ); ?>" data-setting="size">
 							<?php foreach ( $this->get_image_sizes() as $size_key => $size_title ) : ?>
 								<option value="<?php echo esc_attr( $size_key ); ?>"><?php echo esc_html( $size_title ); ?></option>
 							<?php endforeach; ?>
