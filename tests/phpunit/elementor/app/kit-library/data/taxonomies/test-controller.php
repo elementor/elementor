546474966e6fee7cd5aac2d41d85f5d04ec2a985
Ran <rans@elementor.com>
2022-12-21 14:24:28 +0200

    🛠️ Fixed tests

--- tests/phpunit/elementor/app/kit-library/data/taxonomies/test-controller.php
+++ tests/phpunit/elementor/app/kit-library/data/taxonomies/test-controller.php
@@ -85,17 +85,6 @@ class Test_Controller extends Elementor_Test_Base {
 				'text' => 'Creative',
 				'type' => 'tags',
 			],
-
-			// Subscription plans added as taxonomies locally and not from server
-			[
-				'text' => Repository::SUBSCRIPTION_PLAN_FREE_TAG,
-				'type' => 'subscription_plans'
-			],
-			[
-				'text' => $subscription_plans[Module::ACCESS_LEVEL_PRO]['label'],
-				'type' => 'subscription_plans'
-			],
-
 		], $result['data'] );
 	}
 }
