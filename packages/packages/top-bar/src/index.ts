1a1da7c2a59a2e20d39a5bbbfa8e7f8db309670a
Nevo Golan <20494189+Nevoss@users.noreply.github.com>
2022-12-21 21:42:23 +0200

    fix ts

--- packages/packages/top-bar/src/index.ts
+++ packages/packages/top-bar/src/index.ts
@@ -1,12 +1,8 @@
 import { lazy } from 'react';
 import elementorLocations from '@elementor/locations'; // TODO: Fix ts-error.
 
-console.log( 'loaded: editor-top-bar' );
-
 // TODO: Remove the lazy, or maybe wrap the whole app with <Suspense /> and check if it helps with dynamically
 //  imported components that should appear on load.
 elementorLocations.register( 'editor/top', lazy( () => {
-	console.log( 'lazy: editor-top-bar' );
-
 	return import( './components/top-bar' );
 } ) );
