245c26b52a1e7b2c477f87a5ec28202d5e847647
Rodolphe Bertozzo <56270626+rodolphebertozzo@users.noreply.github.com>
2022-12-21 22:50:17 +0100

    Add for image carousel widget https://github.com/elementor/elementor/issues/12533

--- includes/widgets/image-carousel.php
+++ includes/widgets/image-carousel.php
@@ -818,6 +818,31 @@ class Widget_Image_Carousel extends Widget_Base {
 				'selector' => '{{WRAPPER}} .elementor-image-carousel-caption',
 			]
 		);
+		
+		$this->add_responsive_control(
+			'caption_space',
+			[
+				'label' => esc_html__( 'Spacing', 'elementor' ),
+				'type' => Controls_Manager::SLIDER,
+				'size_units' => [ 'px', '%', 'em', 'rem' ],
+				'default' => [
+					'unit' => 'px',
+				],
+				'range' => [
+					'px' => [
+						'min' => 0,
+						'max' => 100,
+					],
+					'%' => [
+						'min' => 0,
+						'max' => 100,
+					],
+				],
+				'selectors' => [
+					'{{WRAPPER}} .elementor-image-carousel-caption' => 'margin-top: {{SIZE}}{{UNIT}};',
+				],
+			]
+		);
 
 		$this->end_controls_section();
 
