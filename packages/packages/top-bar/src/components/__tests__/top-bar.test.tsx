0ba9b10c9ff4c4207e45329f01c11cc2e721ce73
Rotem Ben-Itzhak <rotem@elementor.com>
2022-12-27 17:56:20 +0200

    tweak: removing redundant

--- packages/packages/top-bar/src/components/__tests__/top-bar.test.tsx
+++ packages/packages/top-bar/src/components/__tests__/top-bar.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import TopBar from '../top-bar';
 
 describe( '@elementor/top-bar TopBar component', () => {
