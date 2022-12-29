1482549a7a78f073b78cb27a2f52a694df0221fe
Nevo Golan <20494189+Nevoss@users.noreply.github.com>
2022-12-29 09:58:40 +0200

    wip

--- core/editor/config-providers/editor-v1-config-provider.php
+++ core/editor/config-providers/editor-v1-config-provider.php
@@ -13,7 +13,7 @@ class Editor_V1_Config_Provider implements Config_Provider_Interface {
 				// Loader script
 				[
 					'handle' => 'elementor-editor-loader-v1',
-					'src' => ELEMENTOR_URL . 'core/editor/assets/js/editor-loader-v1{{MIN_SUFFIX}}.js',
+					'src' => '{{ASSETS_URL}}/js/editor-loader-v1{{MIN_SUFFIX}}.js',
 					'deps' => [ 'elementor-editor' ],
 				],
 			]
