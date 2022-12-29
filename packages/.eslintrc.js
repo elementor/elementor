1bf586c6d16a6e786176dbe632fb52c94cb39470
Nevo Golan <20494189+Nevoss@users.noreply.github.com>
2022-12-29 09:35:58 +0200

    Internal: Editor V2 - Adjust eslint [ED-9246]

--- packages/.eslintrc.js
+++ packages/.eslintrc.js
@@ -16,14 +16,25 @@ module.exports = {
 		},
 	},
 	rules: {
+		// ignore when trying to use '@elementor/*' packages
 		'import/no-unresolved': [ 'error', { ignore: [ '@elementor/.+' ] } ],
+
+		// Don't allow relative import from package to package
+		'import/no-relative-packages': [ 'error' ],
+		'no-restricted-syntax': [
+			'error',
+			{
+				selector: 'ImportDeclaration[source.value=/^@elementor\\u002F.+\\u002F/]',
+				message: 'Path access on Elementor dependencies is not allowed, please access using only root level, e.g: "@elementor/locations" and not "@elementor/locations/src/index.ts".',
+			},
+		],
+
+		// Disable import/named rule, TS will handle it.
+		'import/named': [ 'off' ],
 
 		// Disable the js no-unused-vars rule, and enable the ts version.
 		'no-unused-vars': 'off',
 		'@typescript-eslint/no-unused-vars': [ 'error' ],
-
-		// Disable import/named rule, TS will handle it.
-		'import/named': [ 'off' ],
 	},
 	overrides: [
 		{
