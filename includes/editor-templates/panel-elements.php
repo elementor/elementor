7842d80fb736d240cc9f123f28d7a313463d116c
Ariel Klikstein <ariel@elementor.com>
2022-12-30 16:14:35 +0200

    Internal: Panel style

--- includes/editor-templates/panel-elements.php
+++ includes/editor-templates/panel-elements.php
@@ -25,7 +25,12 @@ if ( ! defined( 'ABSPATH' ) ) {
 </script>
 
 <script type="text/template" id="tmpl-elementor-panel-elements-category">
-	<div class="elementor-panel-category-title">{{{ title }}}</div>
+	<div class="elementor-panel-heading elementor-panel-category-title">
+		<div class="elementor-panel-heading-toggle">
+			<i class="eicon" aria-hidden="true"></i>
+		</div>
+		<div class="elementor-panel-heading-title">{{{ title }}}</div>
+	</div>
 	<div class="elementor-panel-category-items elementor-responsive-panel"></div>
 </script>
 
