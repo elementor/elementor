349397243378ddc529d8d81806a2d3727136584d
Nevo Golan <20494189+Nevoss@users.noreply.github.com>
2022-12-31 22:08:52 +0200

    Internal: Editor V2 - Adapt styles [ED-9206] (#20851)

--- core/editor/config-providers/editor-v1-config-provider.php
+++ core/editor/config-providers/editor-v1-config-provider.php
@@ -6,18 +6,30 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 class Editor_V1_Config_Provider implements Config_Provider_Interface {
-	public function get_scripts() {
-		return Editor_Common_Assets::get_scripts();
+	public function get_script_configs() {
+		return array_merge(
+			Editor_Common_Assets::get_script_configs(),
+			[
+				// Loader script
+				[
+					'handle' => 'elementor-editor-loader-v1',
+					'src' => '{{ASSETS_URL}}/js/editor-loader-v1{{MIN_SUFFIX}}.js',
+					'deps' => [ 'elementor-editor' ],
+				],
+			]
+		);
 	}
 
-	public function get_loader_scripts() {
-		return [
-			[
-				'handle' => 'elementor-editor-loader-v1',
-				'src' => ELEMENTOR_URL . 'core/editor/assets/js/editor-loader-v1.js',
-				'deps' => [ 'elementor-editor' ],
-			],
-		];
+	public function get_script_handles_to_enqueue() {
+		return [ 'elementor-editor-loader-v1' ];
+	}
+
+	public function get_style_configs() {
+		return Editor_Common_Assets::get_style_configs();
+	}
+
+	public function get_style_handles_to_enqueue() {
+		return [ 'elementor-editor' ];
 	}
 
 	public function get_template_body_file_path() {
