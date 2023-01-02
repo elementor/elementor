dc1000c09f66960ff9dfcbeabf5ad5f1e5e8abf5
Maor-Kavod <maork@elementor.com>
2023-01-02 15:55:11 +0200

    Tweak: Added correlation between starting and ending date in Date and Time rule in Popup (Core) [ED-9266]

--- assets/dev/js/editor/controls/date-time.js
+++ assets/dev/js/editor/controls/date-time.js
@@ -10,6 +10,28 @@ export default class extends ControlBaseDataView {
 		this.ui.input.flatpickr( options );
 	}
 
+	events() {
+		return {
+			'change @ui.input': 'onBaseInputChange',
+		};
+	}
+
+	onBaseInputChange() {
+		if ( this.model.attributes.verify_min_date_field?.active ) {
+				const startDate = document.querySelector( `[data-setting=${ this.model.attributes.verify_min_date_field.compare_field_name }]` )?.value;
+				if ( ! startDate ) {
+					return;
+				}
+				const endDate = this.ui.input[ 0 ].value;
+				const startDateTimestamp = new Date( startDate ).getTime();
+				const endDateTimestamp = new Date( endDate ).getTime();
+				if ( startDateTimestamp > endDateTimestamp ) {
+					this.ui.input[ 0 ].value = '';
+					// TODO: Add a message to the user
+				}
+		}
+	}
+
 	onBeforeDestroy() {
 		this.ui.input.flatpickr().destroy();
 	}
