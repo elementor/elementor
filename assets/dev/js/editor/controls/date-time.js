b73ecd3b5dffa917398b0391980aae58b76111fa
Maor-Kavod <maork@elementor.com>
2023-01-04 10:25:32 +0200

    Add support for operator option

--- assets/dev/js/editor/controls/date-time.js
+++ assets/dev/js/editor/controls/date-time.js
@@ -18,17 +18,47 @@ export default class extends ControlBaseDataView {
 
 	onBaseInputChange() {
 		if ( this.model.attributes.verify_min_date_field?.active ) {
-				const startDate = document.querySelector( `[data-setting=${ this.model.attributes.verify_min_date_field.compare_field_name }]` )?.value;
-				if ( ! startDate ) {
-					return;
-				}
-				const endDate = this.ui.input[ 0 ].value;
-				const startDateTimestamp = new Date( startDate ).getTime();
-				const endDateTimestamp = new Date( endDate ).getTime();
-				if ( startDateTimestamp > endDateTimestamp ) {
-					this.ui.input[ 0 ].value = '';
-					// TODO: Add a message to the user
-				}
+			const startDate = document.querySelector( `[data-setting=${ this.model.attributes.verify_min_date_field.compare_field_name }]` )?.value;
+			if ( ! startDate ) {
+				return;
+			}
+
+			const endDate = this.ui.input[ 0 ].value;
+			const startDateTimestamp = new Date( startDate ).getTime();
+			const endDateTimestamp = new Date( endDate ).getTime();
+
+			switch ( this.model.attributes.verify_min_date_field.compare_field_operator ) {
+				case '>=':
+					if ( startDateTimestamp >= endDateTimestamp ) {
+						this.ui.input[ 0 ].value = '';
+					}
+					break;
+				case '<=':
+					if ( startDateTimestamp <= endDateTimestamp ) {
+						this.ui.input[ 0 ].value = '';
+					}
+					break;
+				case '>':
+					if ( startDateTimestamp > endDateTimestamp ) {
+						this.ui.input[ 0 ].value = '';
+					}
+					break;
+				case '<':
+					if ( startDateTimestamp < endDateTimestamp ) {
+						this.ui.input[ 0 ].value = '';
+					}
+					break;
+				case '==':
+					if ( startDateTimestamp === endDateTimestamp ) {
+						this.ui.input[ 0 ].value = '';
+					}
+					break;
+				case '!=':
+					if ( startDateTimestamp !== endDateTimestamp ) {
+						this.ui.input[ 0 ].value = '';
+					}
+					break;
+			}
 		}
 	}
 
