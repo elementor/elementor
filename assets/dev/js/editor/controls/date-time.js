b48fb31f989d770bd7b2f920568a320445d03ff5
Maor-Kavod <maork@elementor.com>
2023-01-05 12:58:30 +0200

    Requested changes

--- assets/dev/js/editor/controls/date-time.js
+++ assets/dev/js/editor/controls/date-time.js
@@ -19,8 +19,8 @@ export default class extends ControlBaseDataView {
 
 	onBaseInputChange() {
 		super.onBaseInputChange( ...arguments );
-		if ( this.model.attributes.datetime_verify ) {
-			const startDate = this.options.container.settings.get( this.model.attributes.datetime_verify.control_name );
+		if ( this.model.attributes?.validation?.minDate ) {
+			const startDate = this.options.container.settings.get( this.model.attributes.validation.minDate.control_name );
 			if ( ! startDate ) {
 				return;
 			}
@@ -29,7 +29,7 @@ export default class extends ControlBaseDataView {
 			const startDateTimestamp = new Date( startDate ).getTime();
 			const endDateTimestamp = new Date( endDate ).getTime();
 
-			switch ( this.model.attributes.datetime_verify.operator ) {
+			switch ( this.model.attributes.validation.minDate.operator ) {
 				case '>=':
 					if ( startDateTimestamp >= endDateTimestamp ) {
 						this.ui.input[ 0 ].value = '';
