3cab5ea6e689df44ca17c58d88e503a15ee5dee1
Rotem Ben-Itzhak <rotem@elementor.com>
2023-01-09 14:25:34 +0200

    tweak: adding a comment for the createStore

--- packages/packages/editor/src/init.tsx
+++ packages/packages/editor/src/init.tsx
@@ -3,6 +3,7 @@ import Shell from './components/shell';
 import { createStore, getStore, StoreProvider, Store } from '@elementor/store';
 
 export default function init( domElement: HTMLElement ): void {
+	// Create the store singleton, that can not be re-created until running resetStore().
 	createStore();
 
 	// The store can't be null due to createStore() above.
