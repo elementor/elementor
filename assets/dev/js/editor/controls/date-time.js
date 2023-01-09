baf801b170b069d58ac4d6c34be14258ccf3f93c
Maor-Kavod <maork@elementor.com>
2023-01-09 11:32:52 +0200

    Lint

--- assets/dev/js/editor/controls/date-time.js
+++ assets/dev/js/editor/controls/date-time.js
@@ -22,14 +22,14 @@ export default class extends ControlBaseDataView {
 		const { control_name: controlName, operator } = this.model.get( 'validation' ).date_time;
 		const startDate = this.options.container.settings.get( controlName );
 		const endDate = this.ui.input.val();
-		
+
 		if ( ! startDate || ! endDate ) {
 			return;
 		}
-		
+
 		const startDateTimestamp = new Date( startDate ).getTime();
 		const endDateTimestamp = new Date( endDate ).getTime();
-		
+
 		if ( elementor.conditions.compare( startDateTimestamp, endDateTimestamp, operator ) ) {
 			this.ui.input.val( '' );
 		}
