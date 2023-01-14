4f8b6834641e771c760c35c81724877b2bd69b2f
Mark <markt@elementor.com>
2023-01-14 10:14:49 +0200

    BC

---
+++ assets/dev/js/editor/utils/notifications.js
@@ -0,0 +1,6 @@
+const Notifications = require( 'elementor-utils/notifications' );
+
+// For backward compatibility.
+// TODO: Remove in the future versions.
+
+module.exports = Notifications.extend( {} );
