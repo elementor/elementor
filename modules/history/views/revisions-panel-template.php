03cb4f81839b5a2921daae7ef1b421ee5bd37b33
Ariel Klikstein <ariel@elementor.com>
2023-01-15 21:19:11 +0200

    Internal: Finder, Theme Builder, History

--- modules/history/views/revisions-panel-template.php
+++ modules/history/views/revisions-panel-template.php
@@ -8,13 +8,13 @@ if ( ! defined( 'ABSPATH' ) ) {
 	<div class="elementor-panel-box">
 	<div class="elementor-panel-scheme-buttons">
 			<div class="elementor-panel-scheme-button-wrapper elementor-panel-scheme-discard">
-				<button class="elementor-button" disabled>
+				<button class="elementor-button e-btn-txt" disabled>
 					<i class="eicon-close" aria-hidden="true"></i>
 					<?php echo esc_html__( 'Discard', 'elementor' ); ?>
 				</button>
 			</div>
 			<div class="elementor-panel-scheme-button-wrapper elementor-panel-scheme-save">
-				<button class="elementor-button elementor-button-success" disabled>
+				<button class="elementor-button" disabled>
 					<?php echo esc_html__( 'Apply', 'elementor' ); ?>
 				</button>
 			</div>
@@ -22,9 +22,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 	</div>
 
 	<div class="elementor-panel-box">
-		<div class="elementor-panel-heading">
-			<div class="elementor-panel-heading-title"><?php echo esc_html__( 'Revisions', 'elementor' ); ?></div>
-		</div>
 		<div id="elementor-revisions-list" class="elementor-panel-box-content"></div>
 	</div>
 </script>
