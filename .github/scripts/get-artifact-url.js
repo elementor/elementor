57127f930f292bddfca411812297130952c09c1f
Maor-Kavod <maork@elementor.com>
2023-01-15 20:18:04 +0200

    test

---
+++ .github/scripts/get-artifact-url.js
@@ -0,0 +1,29 @@
+'use strict';
+
+
+const { Octokit } = require("@octokit/core");
+
+const { TOKEN } = process.env;
+const octokit = new Octokit({ auth: TOKEN });
+
+(async () => {
+	try {
+		const releases = await octokit.paginate(
+			octokit.repos.listReleases.endpoint.merge({
+				owner: process.env.REPO_OWNER,
+				repo: process.env.REPO_NAME,
+			})
+		);
+		for (const release of releases) {
+			const assets = release.assets;
+			for (const asset of assets) {
+				if (asset.name === process.env.FILE_NAME) {
+					return asset.browser_download_url;
+				}
+			}
+		}
+	} catch (err) {
+		console.error(`Failed to get artifact url: ${err}`);
+		return '';
+	}
+})();
