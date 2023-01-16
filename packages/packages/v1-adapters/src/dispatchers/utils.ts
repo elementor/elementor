bc24fa2d14ed3571468d5482124f34e589186d98
StyleShit <evyatar.daud@gmail.com>
2023-01-16 08:35:04 +0200

    shorten comment

--- packages/packages/v1-adapters/src/dispatchers/utils.ts
+++ packages/packages/v1-adapters/src/dispatchers/utils.ts
@@ -2,7 +2,7 @@ import { jQueryDeferred } from './types/util-types';
 
 export function isJQueryDeferred<T>( value: any ): value is jQueryDeferred<T> {
 	// TODO: Copied from:
-	//  https://github.com/elementor/elementor/blob/6a74fc94df735dc82a7c1f790311a7548228febe/modules/web-cli/assets/js/core/commands.js#L410
+	//  https://github.com/elementor/elementor/blob/6a74fc9/modules/web-cli/assets/js/core/commands.js#L410
 
 	return value && 'object' === typeof value && value.promise && value.then && value.fail;
 }
