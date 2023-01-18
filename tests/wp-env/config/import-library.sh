6c0e5e7e942e71c04588ff54263b28271612d916
Yotam Shapira <yotams@elementor.com>
2023-01-18 18:45:39 +0200

    Use REST API to import templates

--- tests/wp-env/config/import-library.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -eox pipefail
-
-wp --user=admin elementor experiments activate container
-wp --user=admin elementor experiments activate nested-elements
-
-wp --user=admin elementor library import-dir elementor-playwright-templates
