218a78197b033f419c3cbfba16e83bbe175b9517
StyleShit <32631382+StyleShit@users.noreply.github.com>
2023-01-18 11:01:43 +0200

    Internal: Editor V2 - Package for communication with V1 [ED-9290] (#20951)

--- core/editor/assets/js/editor-loader-v2.js
+++ core/editor/assets/js/editor-loader-v2.js
@@ -1,3 +1,9 @@
+window.__elementorEditorV1LoadingPromise = new Promise( ( resolve ) => {
+	window.addEventListener( 'elementor/init', () => {
+		resolve();
+	}, { once: true } );
+} );
+
 window.elementor.start();
 
 const { init } = window.__UNSTABLE__elementorPackages.editor;
