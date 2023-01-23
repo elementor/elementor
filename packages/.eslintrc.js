15e66b62ca807305d2db34ed56052ad6b0902d26
Nevo Golan <20494189+Nevoss@users.noreply.github.com>
2023-01-23 14:47:41 +0200

    wip

--- packages/.eslintrc.js
+++ packages/.eslintrc.js
@@ -6,7 +6,7 @@ module.exports = {
 	],
 	extends: [
 		'plugin:@wordpress/eslint-plugin/recommended-with-formatting',
-		'plugin:@typescript-eslint/recommended',
+		'plugin:@typescript-eslint/eslint-recommended',
 		'plugin:import/typescript',
 	],
 	settings: {
