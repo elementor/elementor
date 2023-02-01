e66b16f7815997135dc1c2d0414516d7df9da126
Ariel Klikstein <ariel@elementor.com>
2023-02-01 16:01:03 +0200

    Internal: Promoted Accessibility Improvements experiment to be active by default for all [ED-9604]

--- core/experiments/manager.php
+++ core/experiments/manager.php
@@ -306,10 +306,7 @@ class Manager extends Base_Object {
 				. ' <a href="https://go.elementor.com/wp-dash-a11y-improvements/" target="_blank">'
 				. esc_html__( 'Learn More', 'elementor' ) . '</a>',
 			'release_status' => self::RELEASE_STATUS_STABLE,
-			'new_site' => [
-				'default_active' => true,
-				'minimum_installation_version' => '3.1.0-beta',
-			],
+			'default' => self::STATE_ACTIVE,
 			'generator_tag' => true,
 		] );
 
