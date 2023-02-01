e00ea92398855a0fe72d5a1085ad357a922e04b9
LenaLunev <lenal@elementor.com>
2023-02-01 16:11:16 +0200

    Revert "Merge"
    This reverts commit f64c73866e32960daa4a33bb8eae567208ee7ba0.

--- elementor/core/experiments/exceptions/dependency-exception.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Elementor\Core\Experiments\Exceptions;
-
-if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly
-}
-
-class Dependency_Exception extends \Exception {
-}
