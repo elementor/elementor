f0f3b91369f065aa2f521092ac610734f62d89f2
StyleShit <evyatar.daud@gmail.com>
2023-02-02 14:01:12 +0200

    WIP

--- packages/packages/v1-adapters/src/hooks/use-is-preview-mode.ts
+++ packages/packages/v1-adapters/src/hooks/use-is-preview-mode.ts
@@ -2,10 +2,10 @@ import { useEffect, useState } from 'react';
 import { editModeChangeEvent, getCurrentEditMode, listenTo } from '../';
 
 export default function useIsPreviewMode() {
-	const [ isActive, setIsActive ] = useState( () => isPreviewMode() );
+	const [ isPreview, setIsPreview ] = useState( () => isPreviewMode() );
 
 	useEffect( () => {
-		const updateState = () => setIsActive( isPreviewMode() );
+		const updateState = () => setIsPreview( isPreviewMode() );
 
 		const cleanup = listenTo(
 			editModeChangeEvent(),
@@ -15,7 +15,7 @@ export default function useIsPreviewMode() {
 		return cleanup;
 	}, [] );
 
-	return isActive;
+	return isPreview;
 }
 
 function isPreviewMode() {
