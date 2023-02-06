15672dd92ba33913e5a579efbab59ae0bb8af0e8
StyleShit <evyatar.daud@gmail.com>
2023-02-06 12:02:02 +0200

    WIP

--- packages/packages/elements-panel/src/hooks/__tests__/use-props.test.tsx
+++ packages/packages/elements-panel/src/hooks/__tests__/use-props.test.tsx
@@ -1,12 +1,6 @@
-import init from '../init';
-import { registerToggleAction } from '@elementor/top-bar';
+import useProps from '../use-props';
 import { renderHook } from '@testing-library/react-hooks';
 import { openRoute, useIsPreviewMode, useIsRouteActive } from '@elementor/v1-adapters';
-import { render } from '@testing-library/react';
-
-jest.mock( '@elementor/top-bar', () => ( {
-	registerToggleAction: jest.fn(),
-} ) );
 
 jest.mock( '@elementor/v1-adapters', () => ( {
 	openRoute: jest.fn(),
@@ -14,46 +8,24 @@ jest.mock( '@elementor/v1-adapters', () => ( {
 	useIsRouteActive: jest.fn(),
 } ) );
 
-const mockedRegisterToggleAction = jest.mocked( registerToggleAction );
 const mockedIsPreviewMode = jest.mocked( useIsPreviewMode );
 const mockedIsRouteActive = jest.mocked( useIsRouteActive );
 
-describe( '@elementor/elements-panel - Init', () => {
+describe( '@elementor/elements-panel - useProps', () => {
 	afterEach( () => {
 		jest.clearAllMocks();
 	} );
 
-	it( 'should register a toggle action', () => {
+	it( 'should open the elements panel on click', () => {
 		// Arrange.
-		init();
-
-		// Extract the arguments of the first call of `registerToggleAction()` from the top-bar.
-		const args = mockedRegisterToggleAction.mock.calls[ 0 ];
-		const menuName = args[ 0 ];
-		const { name, useProps } = args[ 1 ];
-
-		// Check if `useProps` exists.
-		if ( ! useProps ) {
-			fail( 'useProps is not defined' );
-		}
-
-		// Trigger the `useProps` hook & assert the result.
 		const { result } = renderHook( () => useProps() );
 
 		// Act.
 		result.current.onClick?.();
 
-		const Icon = result.current.icon;
-
-		const { container } = render( <Icon /> );
-
 		// Assert.
-		expect( menuName ).toBe( 'tools' );
-		expect( name ).toBe( 'open-elements-panel' );
 		expect( openRoute ).toHaveBeenCalledTimes( 1 );
 		expect( openRoute ).toHaveBeenCalledWith( 'panel/elements/categories' );
-		expect( result.current.title ).toBe( 'Add element' );
-		expect( container.querySelector( 'svg' ) ).not.toBeNull();
 	} );
 
 	it.each( [
@@ -95,14 +67,6 @@ describe( '@elementor/elements-panel - Init', () => {
 		},
 	] )( '$name', ( { isPreview, currentRoute, expected } ) => {
 		// Arrange.
-		init();
-
-		const { useProps } = mockedRegisterToggleAction.mock.calls[ 0 ][ 1 ];
-
-		if ( ! useProps ) {
-			fail( 'useProps is not defined' );
-		}
-
 		mockedIsPreviewMode.mockReturnValue( isPreview );
 		mockedIsRouteActive.mockImplementation( ( route ) => route === currentRoute );
 
