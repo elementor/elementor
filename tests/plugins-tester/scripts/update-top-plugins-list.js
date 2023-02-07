4c0ba0a1a594cd662fe29468f5042ea975151e3c
Mati Horovitz <mati@elementor.com>
2023-02-07 15:03:03 +0200

    JS Lint

--- tests/plugins-tester/scripts/update-top-plugins-list.js
+++ tests/plugins-tester/scripts/update-top-plugins-list.js
@@ -2,6 +2,7 @@ import fetch from 'node-fetch';
 import fs from 'fs';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
+
 const __filename = fileURLToPath( import.meta.url );
 const __dirname = dirname( __filename );
 
@@ -9,33 +10,33 @@ const URL = 'https://api.wordpress.org/plugins/info/1.2/?action=query_plugins&re
 const FILE_NAME = __dirname + '/../config/top-plugins.json';
 
 const fetchPluginsList = async ( url, filename ) => {
-    try {
-        const response = await fetch( url );
-        const data = await response.json();
+	try {
+		const response = await fetch( url );
+		const data = await response.json();
 
-        const pluginsList = [];
-        data.plugins.forEach( ( plugin ) => {
-            if ( 'elementor' === plugin.slug ) {
-                return;
-            }
+		const pluginsList = [];
+		data.plugins.forEach( ( plugin ) => {
+			if ( 'elementor' === plugin.slug ) {
+				return;
+			}
 
-            pluginsList.push( plugin.slug );
-        } );
+			pluginsList.push( plugin.slug );
+		} );
 
 		// Sort the list
 		pluginsList.sort();
 
-        // eslint-disable-next-line no-console
-        console.log( pluginsList );
+		// eslint-disable-next-line no-console
+		console.log( pluginsList );
 
-        fs.writeFileSync( filename, JSON.stringify( pluginsList, null, 2 ) );
+		fs.writeFileSync( filename, JSON.stringify( pluginsList, null, 2 ) );
 
 		// eslint-disable-next-line no-console
 		console.log( `${ FILE_NAME } has been updated successfully` );
-    } catch ( error ) {
-	// eslint-disable-next-line no-console
-        console.log( error );
-    }
+	} catch ( error ) {
+		// eslint-disable-next-line no-console
+		console.log( error );
+	}
 };
 
 fetchPluginsList( URL, FILE_NAME );
