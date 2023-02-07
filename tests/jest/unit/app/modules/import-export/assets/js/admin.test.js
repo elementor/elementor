c66b9abab1799a4e42c7f81f0295144abc134e64
Zeyad Mahagna <zeyadm@elementor.com>
2023-02-07 12:48:13 +0200

    test file eslinted

--- tests/jest/unit/app/modules/import-export/assets/js/admin.test.js
+++ tests/jest/unit/app/modules/import-export/assets/js/admin.test.js
@@ -1,5 +1,5 @@
 describe( 'Import Export Admin Test', () => {
-	it( 'should check if revert button handler is added', function () {
+	it( 'should check if revert button handler is added', function() {
 		window.document.body.innerHTML = `
 			<button id="elementor-import-export__revert_kit">
 			</button>
@@ -8,19 +8,21 @@ describe( 'Import Export Admin Test', () => {
 			'import-export': {
 				lastImportedSession: {
 					kit_title: 'Kit Title',
-				}
-			}
+				},
+			},
 		};
 
 		window.elementorCommon = {
 			dialogsManager: {
-				createWidget: jest.fn( ( name, options ) => { return {
-					show: jest.fn( () => {} )
-				} } )
-			}
-		}
+				createWidget: jest.fn( () => {
+					return {
+						show: jest.fn( () => {} ),
+					};
+				} ),
+			},
+		};
 
-		const Admin = require( 'elementor/app/modules/import-export/assets/js/admin.js' );
+		require( 'elementor/app/modules/import-export/assets/js/admin.js' );
 		window.dispatchEvent( new Event( 'load' ) );
 
 		document.getElementById( 'elementor-import-export__revert_kit' ).click();
