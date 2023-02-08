bafb883afb73e9257507e760a7b3e3d3ad476cd1
Niv <nivn@gmail.com>
2023-02-08 19:15:42 +0200

    .12.0: Auto stash before revert of "Tweak: Refactor - separate overview widget to sections [ED-9571] (#21196)"

--- core/admin/admin.php
+++ core/admin/admin.php
@@ -392,9 +392,7 @@ class Admin extends App {
 	 * Elementor dashboard widget.
 	 *
 	 * Displays the Elementor dashboard widget.
-	 *
 	 * Fired by `wp_add_dashboard_widget` function.
-	 *
 	 * @since 1.9.0
 	 * @access public
 	 */
@@ -481,13 +479,10 @@ class Admin extends App {
 
 	/**
 	 * Get elementor dashboard overview widget footer actions.
-	 *
 	 * Retrieves the footer action links displayed in elementor dashboard widget.
-	 *
-	 * @since 1.9.0
-	 * @access private
+	 * @since 3.12.0
 	 */
-	private function get_dashboard_overview_widget_footer_actions() {
+	public static function static_get_dashboard_overview_widget_footer_actions() {
 		$base_actions = [
 			'blog' => [
 				'title' => esc_html__( 'Blog', 'elementor' ),
@@ -532,12 +527,19 @@ class Admin extends App {
 	}
 
 	/**
+	 * Get elementor dashboard overview widget footer actions.
+	 * Retrieves the footer action links displayed in elementor dashboard widget.
+	 * @since 1.9.0
+	 * @access private
+	 */
+	private function get_dashboard_overview_widget_footer_actions() {
+		return self::static_get_dashboard_overview_widget_footer_actions();
+	}
+
+	/**
 	 * Admin action new post.
-	 *
 	 * When a new post action is fired the title is set to 'Elementor' and the post ID.
-	 *
 	 * Fired by `admin_action_elementor_new_post` action.
-	 *
 	 * @since 1.9.0
 	 * @access public
 	 */
