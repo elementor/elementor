9b4dfd93fde518e99c136bb8626e6b288d8250bc
Rami Yushuvaev <92088692+rami-elementor@users.noreply.github.com>
2023-02-09 12:47:51 +0200

    Remove translation strings

--- includes/plugin.php
+++ includes/plugin.php
@@ -583,11 +583,7 @@ class Plugin {
 	public function __clone() {
 		_doing_it_wrong(
 			__FUNCTION__,
-			sprintf(
-				/* translators: %s: Class name. */
-				esc_html__( 'Cloning instances of the singleton "%s" class is forbidden.', 'elementor' ),
-				get_class( $this ) // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
-			),
+			sprintf( 'Cloning instances of the singleton "%s" class is forbidden.', get_class( $this ) ), // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 			'1.0.0'
 		);
 	}
@@ -603,11 +599,7 @@ class Plugin {
 	public function __wakeup() {
 		_doing_it_wrong(
 			__FUNCTION__,
-			sprintf(
-				/* translators: %s: Class name. */
-				esc_html__( 'Unserializing instances of the singleton "%s" class is forbidden.', 'elementor' ),
-				get_class( $this ) // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
-			),
+			sprintf( 'Unserializing instances of the singleton "%s" class is forbidden.', get_class( $this ) ), // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 			'1.0.0'
 		);
 	}
