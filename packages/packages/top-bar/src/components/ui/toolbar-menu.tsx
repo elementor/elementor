4b18cf187f46917c18b94cf1994580600509ece8
rotemee <61740845+rotemee@users.noreply.github.com>
2023-02-14 14:56:49 +0200

    Internal: Editor V2 Top-Bar UI Adjustments [ED-9654] (#21263)

--- packages/packages/top-bar/src/components/ui/toolbar-menu.tsx
+++ packages/packages/top-bar/src/components/ui/toolbar-menu.tsx
@@ -5,7 +5,7 @@ import { MenuContextProvider } from '../../contexts/menu-context';
 export default function ToolbarMenu( { children }: { children?: ReactNode } ) {
 	return (
 		<MenuContextProvider type={ 'toolbar' }>
-			<Box sx={ { display: 'flex', alignItems: 'center', gap: '6px' } }>
+			<Box sx={ { display: 'flex', alignItems: 'center', gap: 4 } }>
 				{ children }
 			</Box>
 		</MenuContextProvider>
