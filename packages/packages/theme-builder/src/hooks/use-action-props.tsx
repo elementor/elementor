5dbe4c7bbafd8155bfe72dcba47d979ffa03a5e1
Rotem Ben-Itzhak <rotem@elementor.com>
2023-02-16 11:08:57 +0200

    tweak: renaming hook

--- packages/packages/theme-builder/src/hooks/use-action-props.tsx
+++ packages/packages/theme-builder/src/hooks/use-action-props.tsx
@@ -2,7 +2,7 @@ import { __ } from '@wordpress/i18n';
 import ThemeBuilderIcon from '../icons/theme-builder-icon';
 import { runCommand } from '@elementor/v1-adapters';
 
-export default function useDocumentPreviewProps() {
+export default function useActionProps() {
 	return {
 		icon: ThemeBuilderIcon,
 		title: __( 'Theme Builder', 'elementor' ),
