6d2e6064f19df50b9f03cfdbc4ca62e32fa63aa8
Hein van Vlastuin <vv@abanganimedia.co.za>
2023-02-16 17:13:29 +0200

    pr fix

--- modules/nested-tabs/assets/js/frontend/handlers/nested-tabs.js
+++ modules/nested-tabs/assets/js/frontend/handlers/nested-tabs.js
@@ -385,13 +385,13 @@ export default class NestedTabs extends Base {
 		if ( pressShiftTabOnFirstFocusableItem || isEscapePressed ) {
 			event.preventDefault();
 
-			$activeTabTitleVisible?.trigger( 'focus' );
+			$activeTabTitleVisible?.focus();
 		} else if ( pressTabOnLastFocusableItem ) {
 			event.preventDefault();
 
 			this.setTabindexOfActiveContainerItems( '-1' );
 
-			$nextTabTitleVisible?.trigger( 'focus' );
+			$nextTabTitleVisible?.focus();
 		}
 	}
 
@@ -404,7 +404,7 @@ export default class NestedTabs extends Base {
 
 		if ( isOnlyTabPressed && this.tabTitleHasActiveContentContainer( currentTabTitleIndex ) && !! $firstFocusableItem ) {
 			event.preventDefault();
-			$firstFocusableItem.trigger( 'focus' );
+			$firstFocusableItem.focus();
 		}
 	}
 
