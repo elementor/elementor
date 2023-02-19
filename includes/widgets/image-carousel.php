84191c0e235f857e0bfebd6357542f3964c440f5
Rodolphe Bertozzo <56270626+rodolphebertozzo@users.noreply.github.com>
2023-02-19 12:18:51 +0100

    Adapt

--- includes/widgets/image-carousel.php
+++ includes/widgets/image-carousel.php
@@ -823,7 +823,7 @@ class Widget_Image_Carousel extends Widget_Base {
 			[
 				'label' => esc_html__( 'Spacing', 'elementor' ),
 				'type' => Controls_Manager::SLIDER,
-				'size_units' => [ 'px', '%', 'em', 'rem' ],
+				'size_units' => [ 'px', '%', 'em', 'rem', 'custom' ],
 				'default' => [
 					'unit' => 'px',
 				],
