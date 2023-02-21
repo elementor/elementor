565bb39af983caa408d6c31aa4cd930cab7cab0d
Louis <louiswol94@gmail.com>
2023-02-21 14:02:46 +0200

    Push latest test code

--- modules/nested-elements/assets/js/editor/nested-repeater/hooks/data/document/repeater/move/nested-repeater-move-container.js
+++ modules/nested-elements/assets/js/editor/nested-repeater/hooks/data/document/repeater/move/nested-repeater-move-container.js
@@ -11,14 +11,38 @@ export class NestedRepeaterMoveContainer extends Base {
 	}
 
 	apply( { container, sourceIndex, targetIndex, options } ) {
+		const containerModelCid = this.getRepeaterItemContainerModelCidIfExists(
+			container,
+			sourceIndex,
+			'',
+			''
+		);
+
 		$e.run( 'document/elements/move', {
-			container: findChildContainerOrFail( container, sourceIndex, options.containerModelCid ),
+			//container: findChildContainerOrFail( container, sourceIndex, options.containerModelCid ),
+			container: findChildContainerOrFail( container, sourceIndex, containerModelCid ),
 			target: container,
 			options: {
 				at: targetIndex,
 				edit: false, // Not losing focus.
 			},
 		} );
+	}
+
+	// Have to re-search the model cid.
+	getRepeaterItemContainerModelCidIfExists( container, index, attribute_path, control_key ) {
+		//if ( this.itemDoesNotNeedChildContainer( attribute_path, control_key ) ) {
+			//return false;
+		//}
+
+		const contentIndex = index;
+		const childContainer = container.children.find( ( child ) => +child.view.el.dataset.content === contentIndex );
+
+		if ( ! childContainer ) {
+			return false;
+		}
+
+		return childContainer.model.cid;
 	}
 }
 
