d246616cbc872be7749ce87ec9238e8320e35e20
Maor Kavod <maork@elementor.com>
2023-02-22 10:26:37 +0200

    update

--- test.sh
+++
@@ -1,4 +0,0 @@
-CURRENT_CHANNEL_VERSION=$(git ls-remote --tags | grep -v "\-rc" | grep "cloud" | grep "3.11.1" | tail -n1 | awk -F'cloud' '{print $2}')
-
-echo $CURRENT_CHANNEL_VERSION
-echo "asdfasd"
