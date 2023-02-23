9ca0f8c00465c095f9460c4e4b764c7ff693287c
LenaLunev <lenal@elementor.com>
2023-02-23 11:35:25 +0200

    Phpunit tests

---
+++ tests/phpunit/elementor/modules/announcements/test-module.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Elementor\Testing\Modules\Announcements;
+
+use Elementor\Modules\Announcements\Module;
+use ElementorEditorTesting\Elementor_Test_Base;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+class Elementor_Test_Module extends Elementor_Test_Base {
+	/**
+	 * @var Module
+	 */
+	protected $module;
+
+	/**
+	 * @var string module name
+	 */
+	public $name = 'announcements';
+
+	public function setUp() {
+		parent::setUp();
+
+		wp_set_current_user( self::factory()->get_administrator_user()->ID );
+		//To be in admin page
+		set_current_screen( 'edit-post' );
+
+		$this->module = new Module();
+	}
+
+	public function test_get_name(): void {
+		$this->assertTrue(
+			$this->name === $this->module->get_name(),
+			'Test module name is correct'
+		);
+	}
+
+	public function test_is_active(): void {
+		$this->assertTrue(
+			$this->module->is_active(),
+			'Test module name is correct'
+		);
+	}
+
+	public function test_construct_assets() {
+		// Act
+		$this->module->__construct();
+		do_action( 'elementor/editor/after_enqueue_scripts' );
+
+		// Assert
+		$announcement_script = array_search( 'announcements-app', wp_scripts()->queue );
+		$this->assertTrue( $announcement_script >= 0 );
+
+		$announcement_style = array_search( 'announcements-app', wp_styles()->queue );
+		$this->assertTrue( $announcement_style >= 0 );
+	}
+
+	public function test_construct_render() {
+		// Act
+		$this->module->__construct();
+
+		ob_start();
+		do_action( 'elementor/editor/footer');
+		$result = ob_get_clean();
+
+		// Assert
+		$this->assertStringContainsString( '<div id="e-announcements-root"></div>', $result );
+	}
+}
