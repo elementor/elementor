1efdac0a5580f199debb9ecd9d69be53a3d5706b
StyleShit <evyatar.daud@gmail.com>
2023-02-26 15:42:18 +0200

    WIP

--- packages/packages/user-preferences/src/hooks/use-action-props.ts
+++ packages/packages/user-preferences/src/hooks/use-action-props.ts
@@ -1,9 +1,9 @@
 import { __ } from '@wordpress/i18n';
+import { ToggleActionProps } from '@elementor/top-bar';
 import ToggleRightIcon from '../icons/toggle-right-icon';
 import { openRoute, useRouteStatus } from '@elementor/v1-adapters';
 
-// TODO: Use return type from the TopBar package once merged.
-export default function useActionProps() {
+export default function useActionProps(): ToggleActionProps {
 	const { isActive, isBlocked } = useRouteStatus( 'panel/editor-preferences' );
 
 	return {
