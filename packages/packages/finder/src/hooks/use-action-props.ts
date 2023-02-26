2989845a3c160b303157dec8f0434fe3f4f83a25
StyleShit <evyatar.daud@gmail.com>
2023-02-26 14:02:06 +0200

    Internal: Editor V2 - Fix Finder disabled state [ED-9886]

--- packages/packages/finder/src/hooks/use-action-props.ts
+++ packages/packages/finder/src/hooks/use-action-props.ts
@@ -3,13 +3,15 @@ import { SearchIcon } from '@elementor/icons';
 import { runCommand, useRouteStatus } from '@elementor/v1-adapters';
 
 export default function useActionProps() {
-	const { isActive, isBlocked } = useRouteStatus( 'finder' );
+	const { isActive } = useRouteStatus( 'finder', {
+		blockOnKitRoutes: false,
+		blockOnPreviewMode: false,
+	} );
 
 	return {
 		title: __( 'Finder', 'elementor' ),
 		icon: SearchIcon,
 		onClick: () => runCommand( 'finder/toggle' ),
 		selected: isActive,
-		disabled: isBlocked,
 	};
 }
