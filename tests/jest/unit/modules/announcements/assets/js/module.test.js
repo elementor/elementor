0637d6abbc6326655cb142573b61343d8458eb95
LenaLunev <lenal@elementor.com>
2023-02-26 11:44:30 +0200

    Jest test

--- tests/jest/unit/modules/announcements/assets/js/module.test.js
+++ tests/jest/unit/modules/announcements/assets/js/module.test.js
@@ -1,24 +1,22 @@
 describe( 'modules/announcements/assets/js/index.js', () => {
-	let announcement;
-
 	beforeAll( () => {
 		document.body.innerHTML = '<div id="e-announcements-root"></div>';
 
 		global.elementorAnnouncementsConfig = {
-			announcements: {
+			announcements: [ {
 				title: 'Title of the announcement',
 				description: 'Description of the announcement',
 				media: {
 					type: 'image',
 					src: '',
 				},
-				cta: {
+				cta: [ {
 					label: 'Main CTA',
 					variant: 'primary',
 					target: '_blank',
 					url: 'https://test-url.com',
-				},
-			},
+				} ],
+			} ],
 		};
 	} );
 
@@ -28,19 +26,13 @@ describe( 'modules/announcements/assets/js/index.js', () => {
 
 	test( 'Should render react app', async () => {
 		// Act.
-		announcement = await import( 'elementor/modules/announcements/assets/js/index' );
+		await import( 'elementor/modules/announcements/assets/js/index' );
 
 		// Assert.
-		const announcementTitle = document.querySelectorAll( 'div.announcement-body-title' );
-		const announcementDescription = document.querySelectorAll( 'div.announcement-body-description' );
-		const announcementFooter = document.querySelectorAll( 'div.announcement-footer-container' );
-
 		expect( document.querySelectorAll( 'div.announcement-body-image' ).length ).toEqual( 1 );
-		expect( announcementTitle.length ).toEqual( 1 );
-		expect( announcementTitle.text().contains( 'Title of the announcement' ) ).toBe( true );
-		expect( announcementDescription.length ).toEqual( 1 );
-		expect( announcementDescription.text().contains( 'Description of the announcement' ) ).toBe( true );
-		expect( announcementFooter.length ).toEqual( 1 );
-		expect( announcementFooter.querySelectorAll( '.button-item' ).length ).toEqual( 1 );
+		expect( document.querySelectorAll( 'div.announcement-body-title' ).length ).toEqual( 1 );
+		expect( document.querySelectorAll( 'div.announcement-body-description' ).length ).toEqual( 1 );
+		expect( document.querySelectorAll( 'div.announcement-footer-container' ).length ).toEqual( 1 );
+		expect( document.querySelectorAll( 'div.announcement-footer-container a.button-item' ).length ).toEqual( 1 );
 	} );
 } );
