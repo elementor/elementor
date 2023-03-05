758222e876a2db6da0163956799964f4a90028b4
udidollberg <udi@elementor.com>
2023-03-05 12:20:03 +0200

    Avoid code duplication in test

--- tests/playwright/sanity/basic-gallery.test.js
+++ tests/playwright/sanity/basic-gallery.test.js
@@ -10,25 +10,7 @@ test( 'Basic Gallery', async ( { page }, testInfo ) => {
 	await editor.closeNavigatorIfOpen();
 
 	// Act.
-	await editor.addWidget( 'image-gallery' );
-
-	await page.locator( '[aria-label="Add Images"]' ).click();
-
-	// Open Media Library
-	await page.click( 'text=Media Library' );
-
-	// Upload the images to WP media library
-	await page.setInputFiles( 'input[type="file"]', './tests/playwright/resources/A.jpg' );
-	await page.setInputFiles( 'input[type="file"]', './tests/playwright/resources/B.jpg' );
-	await page.setInputFiles( 'input[type="file"]', './tests/playwright/resources/C.jpg' );
-	await page.setInputFiles( 'input[type="file"]', './tests/playwright/resources/D.jpg' );
-	await page.setInputFiles( 'input[type="file"]', './tests/playwright/resources/E.jpg' );
-
-	// Create a new gallery
-	await page.locator( 'text=Create a new gallery' ).click();
-
-	// Insert gallery
-	await page.locator( 'text=Insert gallery' ).click();
+	await insertImageGalleryWidget( editor, page );
 
 	await editor.togglePreviewMode();
 	expect( await editor.getPreviewFrame().locator( 'div#gallery-1' ).screenshot( { type: 'jpeg', quality: 90 } ) ).toMatchSnapshot( 'gallery.jpeg' );
@@ -48,32 +30,7 @@ test( 'Basic Gallery Lightbox test with latest Swiper', async ( { page }, testIn
 	await editor.closeNavigatorIfOpen();
 
 	// Act.
-	await editor.addWidget( 'image-gallery' );
-
-	await page.locator( '[aria-label="Add Images"]' ).click();
-
-	// Open Media Library
-	await page.click( 'text=Media Library' );
-
-	// Upload the images to WP media library
-	await page.setInputFiles( 'input[type="file"]', './tests/playwright/resources/A.jpg' );
-	await page.setInputFiles( 'input[type="file"]', './tests/playwright/resources/B.jpg' );
-	await page.setInputFiles( 'input[type="file"]', './tests/playwright/resources/C.jpg' );
-	await page.setInputFiles( 'input[type="file"]', './tests/playwright/resources/D.jpg' );
-	await page.setInputFiles( 'input[type="file"]', './tests/playwright/resources/E.jpg' );
-
-	// Create a new gallery
-	await page.locator( 'text=Create a new gallery' ).click();
-
-	// Insert gallery
-	await page.locator( 'text=Insert gallery' ).click();
-
-	await editor.togglePreviewMode();
-	await editor.getPreviewFrame().locator( 'div#gallery-1 img' ).first().click();
-	await editor.getPreviewFrame().locator( '.elementor-swiper-button-next' ).first().click();
-	await page.waitForTimeout( 500 );
-
-	expect( await editor.getPreviewFrame().locator( '.elementor-lightbox' ).screenshot( { type: 'jpeg', quality: 100 } ) ).toMatchSnapshot( 'gallery-lightbox-swiper.jpeg' );
+	await testBasicSwiperGallery( editor, page );
 
 	await wpAdmin.setExperiments( {
 		e_swiper_latest: false,
@@ -94,6 +51,10 @@ test( 'Basic Gallery Lightbox test with older Swiper', async ( { page }, testInf
 	await editor.closeNavigatorIfOpen();
 
 	// Act.
+	await testBasicSwiperGallery( editor, page );
+} );
+
+async function insertImageGalleryWidget( editor, page ) {
 	await editor.addWidget( 'image-gallery' );
 
 	await page.locator( '[aria-label="Add Images"]' ).click();
@@ -113,6 +74,11 @@ test( 'Basic Gallery Lightbox test with older Swiper', async ( { page }, testInf
 
 	// Insert gallery
 	await page.locator( 'text=Insert gallery' ).click();
+}
+
+async function testBasicSwiperGallery( editor, page ) {
+	// Act.
+	await insertImageGalleryWidget( editor, page );
 
 	await editor.togglePreviewMode();
 	await editor.getPreviewFrame().locator( 'div#gallery-1 img' ).first().click();
@@ -120,4 +86,4 @@ test( 'Basic Gallery Lightbox test with older Swiper', async ( { page }, testInf
 	await page.waitForTimeout( 500 );
 
 	expect( await editor.getPreviewFrame().locator( '.elementor-lightbox' ).screenshot( { type: 'jpeg', quality: 100 } ) ).toMatchSnapshot( 'gallery-lightbox-swiper.jpeg' );
-} );
+}
