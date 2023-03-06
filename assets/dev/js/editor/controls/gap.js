8853159f4d3ba70264cd3ec3b0100af0867077ed
David Seveloff <davids@elementor.com>
2023-03-06 17:56:21 +0200

    JSLint fix

--- assets/dev/js/editor/controls/gap.js
+++ assets/dev/js/editor/controls/gap.js
@@ -1,10 +1,10 @@
-var ControlDimensionsView = require( 'elementor-controls/dimensions' ),
+let ControlDimensionsView = require( 'elementor-controls/dimensions' ),
 	ControlGapItemView;
 
 ControlGapItemView = ControlDimensionsView.extend( {
 
 	ui() {
-		var ui = ControlDimensionsView.prototype.ui.apply(this, arguments);
+		let ui = ControlDimensionsView.prototype.ui.apply( this, arguments );
 
 		ui.controls = '.elementor-control-gap > input:enabled';
 		ui.link = 'button.elementor-link-gaps';
