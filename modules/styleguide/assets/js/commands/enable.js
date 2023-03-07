cad5d60f0c56b270877128baf80f7a28733529d3
Mark <markt@elementor.com>
2023-03-07 12:42:33 +0200

    Tests

--- modules/styleguide/assets/js/commands/enable.js
+++ modules/styleguide/assets/js/commands/enable.js
@@ -1,6 +1,6 @@
 export class Enable extends $e.modules.CommandBase {
-	apply() {
-		$e.components.get( 'preview/styleguide' ).enableStyleguidePreview();
+	apply( args ) {
+		$e.components.get( 'preview/styleguide' ).enableStyleguidePreview( args );
 	}
 }
 
