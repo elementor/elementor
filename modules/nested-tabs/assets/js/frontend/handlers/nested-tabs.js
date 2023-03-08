23c4330913eafa839de332a066c73c0bf5e1d08e
Hein van Vlastuin <heinv@elementor.red>
2023-03-08 19:51:46 +0200

    timeout

--- modules/nested-tabs/assets/js/frontend/handlers/nested-tabs.js
+++ modules/nested-tabs/assets/js/frontend/handlers/nested-tabs.js
@@ -139,10 +139,7 @@ export default class NestedTabs extends Base {
 			$activeTitle = this.elements.$tabTitles.filter( activeTitleFilter ),
 			$activeContent = this.elements.$tabContents.filter( activeContentFilter );
 
-		// $activeTitle.add( $activeContent ).removeClass( activeClass );
-		$activeContent.removeClass( activeClass );
-		setTimeout( $activeTitle.removeClass( activeClass ), 50 );
-
+		$activeTitle.add( $activeContent ).removeClass( activeClass );
 		$activeTitle.attr( {
 			tabindex: '-1',
 			'aria-selected': 'false',
@@ -172,10 +169,9 @@ export default class NestedTabs extends Base {
 			$requestedContent = this.elements.$tabContents.filter( this.getTabContentFilterSelector( previousTabIndex ) );
 		}
 
-		// $requestedTitle.add( $requestedContent ).addClass( activeClass );
-
-		$requestedContent.addClass( activeClass );
-		setTimeout( $requestedTitle.addClass( activeClass ), 50 );
+		setTimeout( () => {
+			$requestedTitle.add( $requestedContent ).addClass( activeClass );
+		}, 10 );
 
 		$requestedTitle.attr( {
 			tabindex: '0',
