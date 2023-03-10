366daae106beb249479b03f449ee888d3864979c
Hein van Vlastuin <heinv@elementor.red>
2023-03-10 16:14:15 +0200

    wip

--- modules/nested-tabs/assets/js/frontend/handlers/nested-tabs.js
+++ modules/nested-tabs/assets/js/frontend/handlers/nested-tabs.js
@@ -139,40 +139,17 @@ export default class NestedTabs extends Base {
 			$activeTitle = this.elements.$tabTitles.filter( activeTitleFilter ),
 			$activeContent = this.elements.$tabContents.filter( activeContentFilter );
 
+		$activeTitle[0]?.classList.remove( activeClass );
+		$activeContent[0]?.classList.remove( activeClass );
 
-			$activeTitle.add( $activeContent ).removeClass( activeClass ),
+		$activeTitle.attr( {
+			tabindex: '-1',
+			'aria-selected': 'false',
+			'aria-expanded': 'false',
+		} );
 
-			$activeTitle.attr( {
-				tabindex: '-1',
-				'aria-selected': 'false',
-				'aria-expanded': 'false',
-			} ),
-
-			$activeContent[ settings.hideTabFn ]( 0, () => this.onHideTabContent( $activeContent ) ),
-			$activeContent.attr( 'hidden', 'hidden' )
-
-
-		// 	<h1 id="demo"></h1>
-		// <h2 id="test"></h2>
-		//
-		// <script>
-		// 	async function myDisplay() {
-		// 	let myPromise = new Promise(function(resolve, reject) {
-		// 	setTimeout( () => {
-		// 	resolve("I love You !!");
-		// }, 2000 );
-		// });
-		// 	document.getElementById("demo").innerHTML = await myPromise;
-		// 	test();
-		// }
-		//
-		// 	myDisplay();
-		//
-		// 	function test() {
-		// 	document.getElementById("test").innerHTML = 'tweede';
-		// }
-		// </script>
-
+		$activeContent[ settings.hideTabFn ]( 0, () => this.onHideTabContent( $activeContent ) );
+		$activeContent.attr( 'hidden', 'hidden' );
 	}
 
 	onHideTabContent( $activeContent ) {}
@@ -194,9 +171,8 @@ export default class NestedTabs extends Base {
 			$requestedContent = this.elements.$tabContents.filter( this.getTabContentFilterSelector( previousTabIndex ) );
 		}
 
-		// setTimeout( () => {
-			$requestedTitle.add( $requestedContent ).addClass( activeClass );
-		// } );
+		$requestedTitle[0]?.classList.add( activeClass );
+		$requestedContent[0]?.classList.add( activeClass );
 
 		$requestedTitle.attr( {
 			tabindex: '0',
