f3df33853304828221d580895b887d2b38a1e63e
StyleShit <evyatar.daud@gmail.com>
2023-03-12 14:56:12 +0200

    Add exports

--- packages/packages/top-bar/src/index.ts
+++ packages/packages/top-bar/src/index.ts
@@ -15,6 +15,8 @@ export { default as ToolbarMenu } from './components/ui/toolbar-menu';
 export { default as ToolbarMenuItem } from './components/ui/toolbar-menu-item';
 export { default as ToolbarMenuMore } from './components/ui/toolbar-menu-more';
 export { default as ToolbarMenuToggleItem } from './components/ui/toolbar-menu-toggle-item';
+export { Divider } from '@elementor/ui';
+export type { MenuProps, MenuItemProps } from '@elementor/ui';
 
 import init from './init';
 
