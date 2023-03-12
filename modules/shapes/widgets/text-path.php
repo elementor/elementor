0d48597e1d3d7ad75b695e5678ebc2e2b3403783
StyleShit <evyatar.daud@gmail.com>
2023-03-12 15:22:23 +0200

    Tweak: Improved code security enforcement in Text Path widget [ED-10009]

--- modules/shapes/widgets/text-path.php
+++ modules/shapes/widgets/text-path.php
@@ -669,6 +669,7 @@ class TextPath extends Widget_Base {
 			'class' => 'e-text-path',
 			'data-text' => esc_attr( $settings['text'] ),
 			'data-url' => esc_url( $path_url ),
+			'data-link-url' => esc_url( $settings['link']['url'] ?? '' ),
 		] );
 
 		// Add hover animation.
