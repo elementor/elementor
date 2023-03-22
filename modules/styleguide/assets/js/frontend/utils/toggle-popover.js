dcb01da0bfa874b51cb364915ab17b498a6abd73
ManorHazazE <manorh@elementor.com>
2023-03-22 11:28:32 +0200

    Minor style tweaks

--- modules/styleguide/assets/js/frontend/utils/toggle-popover.js
+++
@@ -1,5 +0,0 @@
-export const togglePopover = ( type, name, id, controlName ) => {
-	window.top.$e.run( 'controls/toggle-control', {
-		controlPath: `${ type }_${ name }/${ id }/${ controlName }`,
-	} );
-};
