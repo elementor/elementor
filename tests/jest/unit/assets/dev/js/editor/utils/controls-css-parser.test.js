cff5dd30cf557b184abb0ab1d4ef82bfc42ae451
Louis <louiswol94@gmail.com>
2023-03-23 14:24:11 +0200

    JS Lint

--- tests/jest/unit/assets/dev/js/editor/utils/controls-css-parser.test.js
+++ tests/jest/unit/assets/dev/js/editor/utils/controls-css-parser.test.js
@@ -7,19 +7,19 @@ describe( 'ControlsCSSParser', () => {
 
 		// Act.
 		let values = {
-			"unit": "fr",
-			"size": 2,
-			"sizes": []
+			unit: 'fr',
+			size: 2,
+			sizes: [],
 		};
-		  
+
 		let string = '--e-con-grid-template-columns: {{SIZE}}';
 		string = cssParser.parseSizeUnitsSelectorsDictionary( string, values );
-		  
+
 		let stringTwo = '--e-con-grid-template-columns: {{SIZE}}{{UNIT}}';
 		stringTwo = cssParser.parseSizeUnitsSelectorsDictionary( stringTwo, values );
 
-		const firstComparisonResult = string === '--e-con-grid-template-columns: 2';
-		const secondComparisonResult = stringTwo === '--e-con-grid-template-columns: 2fr';
+		const firstComparisonResult = '--e-con-grid-template-columns: 2' === string;
+		const secondComparisonResult = '--e-con-grid-template-columns: 2fr' === stringTwo;
 
 		// Assert.
 		expect( firstComparisonResult ).toBe( true );
