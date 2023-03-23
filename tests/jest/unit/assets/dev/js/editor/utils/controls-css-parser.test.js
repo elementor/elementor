aa2ab4929fc2ecc0d293252ca9294fc0a3f02bc9
Louis <louiswol94@gmail.com>
2023-03-23 13:56:33 +0200

    Add Test

---
+++ tests/jest/unit/assets/dev/js/editor/utils/controls-css-parser.test.js
@@ -0,0 +1,28 @@
+import ControlsCSSParser from 'elementor-editor-utils/controls-css-parser';
+
+describe( 'ControlsCSSParser', () => {
+	test( 'parseSizeUnitsSelectorsDictionary', () => {
+		// Arrange.
+		const cssParser = new ControlsCSSParser();
+
+		// Act.
+		let values = {
+			"unit": "fr",
+			"size": 2,
+			"sizes": []
+		};
+		  
+		let string = '--e-con-grid-template-columns: {{SIZE}}';
+		string = cssParser.parseSizeUnitsSelectorsDictionary( string, values );
+		  
+		let stringTwo = '--e-con-grid-template-columns: {{SIZE}}{{UNIT}}';
+		stringTwo = cssParser.parseSizeUnitsSelectorsDictionary( stringTwo, values );
+
+		const firstComparisonResult = string === '--e-con-grid-template-columns: 2';
+		const secondComparisonResult = stringTwo === '--e-con-grid-template-columns: 2fr';
+
+		// Assert.
+		expect( firstComparisonResult ).toBe( true );
+		expect( secondComparisonResult ).toBe( true );
+	} );
+} );
