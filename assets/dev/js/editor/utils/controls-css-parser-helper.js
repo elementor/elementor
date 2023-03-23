0fb6283cd97ab214add4afa673e528accdb21f52
Louis <louiswol94@gmail.com>
2023-03-23 21:42:30 +0200

    JSLint

--- assets/dev/js/editor/utils/controls-css-parser-helper.js
+++ assets/dev/js/editor/utils/controls-css-parser-helper.js
@@ -1,5 +1,4 @@
 export default class ControlsCSSParserHelper {
-
 	/**
 	 * Parse size units selectors dictionary
 	 *
@@ -16,15 +15,15 @@ export default class ControlsCSSParserHelper {
 		return constrolSelector.replace( /{{(.*?)}}/g, function( match, placeholder ) {
 			const keys = placeholder.toLowerCase().split( '.' );
 			let value = controlValues;
-	
+
 			for ( let i = 0; i < keys.length; i++ ) {
 				value = value[ keys[ i ] ];
-	
+
 				if ( value === undefined ) {
 					return match;
 				}
 			}
-	
+
 			return value;
 		} );
 	}
