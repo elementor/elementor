da83212808ba2b3290f3e64ad8519ee9bb609c0b
Hein van Vlastuin <heinv@elementor.red>
2023-03-23 17:56:20 +0200

    import fix

--- tests/playwright/sanity/modules/nested-tabs/helper.js
+++ tests/playwright/sanity/modules/nested-tabs/helper.js
@@ -1,4 +1,4 @@
-export async function editTab( editor, tabIndex ) {
+async function editTab( editor, tabIndex ) {
 	const tabTitleSelector = '.e-n-tabs-heading .e-n-tab-title';
 	await editor.getPreviewFrame().waitForSelector( `${ tabTitleSelector }.e-active` );
 	const tabTitle = await editor.getPreviewFrame().locator( `${ tabTitleSelector }>>nth=${ tabIndex }` );
@@ -8,16 +8,16 @@ export async function editTab( editor, tabIndex ) {
 }
 
 // Click on tab by position.
-export async function clickTab( context, tabPosition ) {
+async function clickTab( context, tabPosition ) {
 	await context.locator( `.elementor-widget-n-tabs .e-n-tab-title >> nth=${ tabPosition } ` ).first().click();
 }
 
 // Click on tab by position.
-export async function clickMobileTab( context, tabPosition ) {
+async function clickMobileTab( context, tabPosition ) {
 	await context.locator( `.elementor-widget-n-tabs .e-collapse >> nth=${ tabPosition } ` ).first().click();
 }
 
-export async function setup( wpAdmin, customExperiment = '' ) {
+async function setup( wpAdmin, customExperiment = '' ) {
 	let experiments = {
 		container: 'active',
 		'nested-elements': 'active',
@@ -27,7 +27,7 @@ export async function setup( wpAdmin, customExperiment = '' ) {
 	await wpAdmin.setExperiments( experiments );
 }
 
-export async function cleanup( wpAdmin, customExperiment = '' ) {
+async function cleanup( wpAdmin, customExperiment = '' ) {
 	let experiments = {
 		'nested-elements': 'inactive',
 		container: 'inactive',
@@ -37,7 +37,7 @@ export async function cleanup( wpAdmin, customExperiment = '' ) {
 	await wpAdmin.setExperiments( experiments );
 }
 
-export async function setTabItemColor( page, editor, panelClass, tabState, colorPickerClass, color ) {
+async function setTabItemColor( page, editor, panelClass, tabState, colorPickerClass, color ) {
 	await editor.activatePanelTab( 'style' );
 	if ( 'tabs' !== panelClass ) {
 		await page.locator( `.elementor-control-${ panelClass }` ).click();
@@ -47,7 +47,7 @@ export async function setTabItemColor( page, editor, panelClass, tabState, color
 	await page.fill( '.pcr-app.visible .pcr-interaction input.pcr-result', color );
 }
 
-export async function setTabBorderColor( page, editor, state, stateExtended, color, borderWidth, borderStyle = 'solid' ) {
+async function setTabBorderColor( page, editor, state, stateExtended, color, borderWidth, borderStyle = 'solid' ) {
 	await editor.activatePanelTab( 'style' );
 	await page.locator( `.elementor-control-section_tabs_style` ).click();
 	await page.locator( `.elementor-control-tabs_title_${ state }` ).click();
@@ -57,14 +57,7 @@ export async function setTabBorderColor( page, editor, state, stateExtended, col
 	await page.fill( '.pcr-app.visible .pcr-interaction input.pcr-result', color );
 }
 
-export async function getDropdownContainerId( editor, widgetId, itemNumber = 1 ) {
-	const widgetSelector = `.elementor-widget.elementor-element-${ widgetId }`;
-	await editor.getPreviewFrame().waitForSelector( `${ widgetSelector }.elementor-element-editable` );
-	await editor.getPreviewFrame().locator( `${ widgetSelector } .e-n-tab-title.e-normal:nth-child(${ itemNumber })` ).click();
-	return await editor.getPreviewFrame().locator( `${ widgetSelector } .e-con.e-active` ).getAttribute( 'data-id' );
-}
-
-export async function selectDropdownContainer( editor, widgetId, itemNumber = 0 ) {
+async function selectDropdownContainer( editor, widgetId, itemNumber = 0 ) {
 	const isActiveTab = await editor.getPreviewFrame().locator( `.e-normal >> nth=${ itemNumber }` ).evaluate( ( element ) => element.classList.contains( 'e-active ' ) );
 
 	if ( ! isActiveTab ) {
@@ -76,3 +69,14 @@ export async function selectDropdownContainer( editor, widgetId, itemNumber = 0 
 	await elementEditButton.click();
 	await editor.getPreviewFrame().waitForSelector( '.e-con.e-active.elementor-element-editable' );
 }
+
+module.exports = {
+	editTab,
+	clickTab,
+	clickMobileTab,
+	cleanup,
+	setup,
+	setTabBorderColor,
+	setTabItemColor,
+	selectDropdownContainer,
+};
