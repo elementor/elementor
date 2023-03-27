9db6680ce5ec5429fd8e03038e6d11148508b50a
Hein van Vlastuin <heinv@elementor.red>
2023-03-27 09:10:41 +0200

    pr tweak

--- includes/controls/groups/grid-container.php
+++ includes/controls/groups/grid-container.php
@@ -149,6 +149,7 @@ class Group_Control_Grid_Container extends Group_Control_Base {
 			'label' => esc_html_x( 'Justify Content', 'Grid Container Control', 'elementor' ),
 			'type' => Controls_Manager::CHOOSE,
 			'label_block' => true,
+			'default' => '',
 			'options' => [
 				'start' => [
 					'title' => esc_html_x( 'Start', 'Grid Container Control', 'elementor' ),
@@ -173,6 +174,7 @@ class Group_Control_Grid_Container extends Group_Control_Base {
 			'label' => esc_html_x( 'Align Content', 'Grid Container Control', 'elementor' ),
 			'type' => Controls_Manager::CHOOSE,
 			'label_block' => true,
+			'default' => '',
 			'options' => [
 				'start' => [
 					'title' => esc_html_x( 'Start', 'Grid Container Control', 'elementor' ),
