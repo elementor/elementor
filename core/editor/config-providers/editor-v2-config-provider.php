3c9b30f86bcb9154632b5f4056f934d390d1fb58
StyleShit <evyatar.daud@gmail.com>
2023-03-28 09:52:20 +0300

    WIP

--- core/editor/config-providers/editor-v2-config-provider.php
+++ core/editor/config-providers/editor-v2-config-provider.php
@@ -13,6 +13,7 @@ class Editor_V2_Config_Provider implements Config_Provider_Interface {
 	const EXTENSION_PACKAGES = [
 		'documents',
 		'documents-ui',
+		'pro-documents',
 		'elements-panel',
 		'finder',
 		'help',
