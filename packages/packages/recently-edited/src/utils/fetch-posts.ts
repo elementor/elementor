4ab068695495a446747962ea18f16fc103cd04e2
LenaLunev <lenal@elementor.com>
2023-03-29 16:23:34 +0300

    Add nonce to fetch

--- packages/packages/recently-edited/src/utils/fetch-posts.ts
+++ packages/packages/recently-edited/src/utils/fetch-posts.ts
@@ -12,20 +12,35 @@ export interface Post {
 
 type ExtendedWindow = Window & {
 	elementorWebCliConfig: {
+		nonce: string,
 		urls: {
 			rest: string,
+		}
+	},
+	elementor: {
+		config: {
+			initial_document: {
+				id: number
+			}
 		}
 	}
 }
 
 export default async function getRecentlyEditedPosts() {
 	let url: URL;
+	const currentDocId = ( window as unknown as ExtendedWindow ).elementor?.config?.initial_document?.id;
+	const nonce = ( window as unknown as ExtendedWindow ).elementorWebCliConfig?.nonce;
+
+	if ( ! currentDocId ) {
+		return [];
+	}
 	const fetchArgs = {
 		posts_per_page: 5,
+		post__not_in: currentDocId,
 	};
 
 	try {
-		const baseUrl = ( window as unknown as ExtendedWindow ).elementorWebCliConfig?.urls.rest;
+		const baseUrl = ( window as unknown as ExtendedWindow ).elementorWebCliConfig?.urls?.rest;
 		url = new URL( baseUrl + 'elementor/v1/site-navigation/recent-posts' );
 	} catch ( e ) {
 		return [];
@@ -34,7 +49,12 @@ export default async function getRecentlyEditedPosts() {
 	Object.entries( fetchArgs ).map( ( [ key, value ] ) => url.searchParams.set( key, String( value ) ) );
 
 	try {
-		const posts = await fetch( url.toString() )
+		const posts = await fetch( url.toString(), {
+			credentials: 'same-origin',
+			headers: {
+				'X-WP-Nonce': nonce,
+			},
+		} )
 			.then( ( response ) => response.json() )
 			.then( ( response ) => response as Post[] );
 		return posts;
