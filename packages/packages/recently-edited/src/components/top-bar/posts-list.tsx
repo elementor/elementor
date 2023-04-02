7917dd3b7fd06b09ac8855e4f7a87da4c0c8b012
LenaLunev <lenal@elementor.com>
2023-04-02 22:29:14 +0300

    Add empty state

--- packages/packages/recently-edited/src/components/top-bar/posts-list.tsx
+++ packages/packages/recently-edited/src/components/top-bar/posts-list.tsx
@@ -1,9 +1,10 @@
 import {
 	MenuItem,
 	ListSubheader,
+	Typography,
 } from '@elementor/ui';
 
-import { __ } from '@wordpress/i18n';
+import { __, isRTL } from '@wordpress/i18n';
 import DocTypeChip, { Props } from '../top-bar/chip-doc-type';
 import { Post } from '../../hooks/use-recent-posts';
 
@@ -14,20 +15,26 @@ export type RecentPostsProps = {
 export default function PostsList( { recentPosts }: RecentPostsProps ) {
 	return (
 		<>
-			<ListSubheader sx={ { fontSize: 12, fontStyle: 'italic', pl: 4 } } component="div" id="nested-list-subheader">
+			<ListSubheader sx={ { fontSize: 12, fontStyle: 'italic', fontWeight: 300, pl: 4 } } component="div" id="nested-list-subheader">
 				{ __( 'Recent', 'elementor' ) }
 			</ListSubheader>
 
-			{ recentPosts.map( ( { title, edit_url: editUrl, type, id } ) => (
-				<MenuItem
-					key={ id }
-					component="a"
-					href={ editUrl }
-				>
-					{ title }
-					<DocTypeChip postType={ type.post_type } docType={ type.doc_type as Props['docType'] } label={ type.label } />
-				</MenuItem>
-			) ) }
+			{ recentPosts.length
+				? ( recentPosts.map( ( { title, edit_url: editUrl, type, id } ) => (
+					<MenuItem
+						key={ id }
+						component="a"
+						href={ editUrl }
+					>
+						{ title }
+						<DocTypeChip postType={ type.post_type } docType={ type.doc_type as Props['docType'] } label={ type.label } />
+					</MenuItem>
+				) ) ) : (
+					<Typography variant="caption" sx={ { color: 'grey.500', fontStyle: 'italic', fontWeight: 300, p: 4, ...( isRTL() ? { pl: '25px' } : { pr: '25px' } ) } } component="div" aria-label={ undefined }>
+						{ __( 'There are no other pages or templates on this site yet.', 'elementor' ) }
+					</Typography>
+				)
+			}
 		</>
 	);
 }
