9abb3bdfb2864220d91909d1a98c30d822d7af5b
Davids Seveloff <94905340+davseve@users.noreply.github.com>
2023-04-04 11:47:40 +0300

    Internal: [Grid] Add Outline control and canvas grid indications [ED-10018] (#21901)

--- tests/playwright/pages/editor-page.js
+++ tests/playwright/pages/editor-page.js
@@ -603,4 +603,22 @@ module.exports = class EditorPage extends BasePage {
 	async setSelectControlValue( controlId, value ) {
 		await this.page.selectOption( '.elementor-control-' + controlId + ' select', value );
 	}
+
+	/**
+	 * Change switcher control value.
+	 *
+	 * @param {string}  controlId
+	 * @param {boolean} setState  [true|false]
+	 *
+	 * @return {Promise<void>}
+	 */
+	async setSwitcherControlValue( controlId, setState = true ) {
+		const controlSelector = '.elementor-control-' + controlId,
+			controlLabel = await this.page.locator( controlSelector + ' label.elementor-switch' ),
+			currentState = await this.page.locator( controlSelector + ' input[type="checkbox"]' ).isChecked();
+
+		if ( currentState !== Boolean( setState ) ) {
+			await controlLabel.click();
+		}
+	}
 };
