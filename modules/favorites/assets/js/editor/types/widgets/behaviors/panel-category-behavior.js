d3fe486a946e30ef3bbb819a7014e73edf9790d2
maork-elementor <maork@elementor.com>
2023-04-13 09:26:08 +0300

    fix: Improve Favorites Panel UX
    - Hide panel if favorites collection is empty
    - Fix bug causing panel to show when favorites category is empty
    - Add bottom margin to element with id `elementor-favorites-empty-notice-wrapper`
    - Adjust grid layout of element with class `elementor-responsive-panel`

--- modules/favorites/assets/js/editor/types/widgets/behaviors/panel-category-behavior.js
+++ modules/favorites/assets/js/editor/types/widgets/behaviors/panel-category-behavior.js
@@ -2,9 +2,14 @@ import CategoryEmptyView from '../views/category-empty-view';
 
 export default class PanelCategoryBehavior extends Marionette.Behavior {
 	initialize() {
+
 	}
 
 	onRender() {
+		const hasFavorites = this.view.collection.length;
+		if ( ! hasFavorites ) {
+			this.$el.hide();
+		}
 		if ( this.isFavoritesCategory() ) {
 			this.view.toggle( ! this.view.isEmpty(), false );
 		}
