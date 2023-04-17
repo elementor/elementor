5fefb92ebc6ce6080dad3229c1f38ad4bc782d9a
Hein van Vlastuin <heinv@elementor.red>
2023-04-17 14:28:43 +0200

    test.only

--- tests/elements-regression/playwright.config.js
+++ tests/elements-regression/playwright.config.js
@@ -15,7 +15,7 @@ export default {
 		timeout: 5000,
 	},
 
-	forbidOnly: !! process.env.CI,
+	// forbidOnly: !! process.env.CI,
 	retries: process.env.CI ? 1 : 0,
 	workers: process.env.CI ? 3 : 1,
 	fullyParallel: true,
