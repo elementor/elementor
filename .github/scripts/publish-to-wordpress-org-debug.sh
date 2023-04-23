903a4430749b1aabeabf5c9f0dae3c51a6e46fbd
Maor-Kavod <maork@elementor.com>
2023-04-23 15:18:30 +0300

    Internal: Debug wordpress svn trunk

---
+++ .github/scripts/publish-to-wordpress-org-debug.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+set -eo pipefail
+
+if [[ -z "$SVN_USERNAME" ]]; then
+	echo "Set the SVN_USERNAME secret"
+	exit 1
+fi
+
+if [[ -z "$SVN_PASSWORD" ]]; then
+	echo "Set the SVN_PASSWORD secret"
+	exit 1
+fi
+
+if [[ -z "$PLUGIN_VERSION" ]]; then
+	echo "Set the PLUGIN_VERSION env var"
+	exit 1
+fi
+
+echo "Publish version: ${PLUGIN_VERSION}"
+
+ELEMENTOR_PATH="$GITHUB_WORKSPACE/elementor"
+SVN_PATH="$GITHUB_WORKSPACE/svn"
+
+cd $ELEMENTOR_PATH
+mkdir -p $SVN_PATH
+cd $SVN_PATH
+
+echo "Checkout from SVN"
+svn co https://plugins.svn.wordpress.org/elementor/trunk
+
+echo "Clean trunk folder"
+cd $SVN_PATH/trunk
+find . -maxdepth 1 -not -name ".svn" -not -name "." -not -name ".." -exec rm -rf {} +
+
+echo "Copy files"
+rsync -ah --progress $ELEMENTOR_PATH/* $SVN_PATH/trunk
+
+echo "Preparing files"
+cd $SVN_PATH/trunk
+
+echo "svn delete"
+svn status | grep -v '^.[ \t]*\\..*' | { grep '^!' || true; } | awk '{print $2}' | xargs -r svn delete;
+
+echo "svn add"
+svn status | grep -v '^.[ \t]*\\..*' | { grep '^?' || true; } | awk '{print $2}' | xargs -r svn add;
+
+svn status
