06fe7c0ad53091e76660561cba56aec6bcea9c6d
Hein van Vlastuin <heinv@elementor.red>
2023-04-26 12:16:08 +0200

    pr feedback

--- modules/nested-tabs/assets/js/frontend/utils.js
+++ modules/nested-tabs/assets/js/frontend/utils.js
@@ -1,4 +1,8 @@
 export function isMenuInDropdownMode( elementSettings ) {
+	if ( !! elementSettings.item_layout && 'dropdown' === elementSettings.item_layout ) {
+		return true;
+	}
+
 	const activeBreakpointsList = elementorFrontend.breakpoints.getActiveBreakpointsList( { withDesktop: true } ),
 		breakpointIndex = activeBreakpointsList.indexOf( elementSettings.breakpoint_selector ),
 		currentDeviceModeIndex = activeBreakpointsList.indexOf( elementorFrontend.getCurrentDeviceMode() );
