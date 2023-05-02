51a2a4bfd36910ab4ffc75ac62c92ec066ae03ef
Zeyad Mahagna <zeyadm@elementor.com>
2023-05-02 08:59:13 +0300

    Fix for out-of-focus editor click nulling active model id

--- assets/dev/js/editor/regions/panel/pages/editor/component.js
+++ assets/dev/js/editor/regions/panel/pages/editor/component.js
@@ -108,6 +108,7 @@ export default class Component extends ComponentBase {
 	 * @return {void}
 	 */
 	onCloseRoute( route ) {
+		this.activeModelId = null;
 		super.onCloseRoute( route );
 
 		$e.uiStates.remove( 'document/direction-mode' );
