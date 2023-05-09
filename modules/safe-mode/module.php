efd6430def4c0a80b4d4dcb5ec35b360a4d5b36c
Mati Horovitz <mati@elementor.com>
2023-05-09 13:21:48 +0300

    Fix text

--- modules/safe-mode/module.php
+++ modules/safe-mode/module.php
@@ -69,7 +69,7 @@ class Module extends \Elementor\Core\Base\Module {
 	 */
 	public function ajax_enable_safe_mode($data ) {
 		if ( ! current_user_can( 'install_plugins' ) ) {
-			throw new \Exception( 'Access denied' );
+			throw new \Exception( 'Access denied.' );
 		}
 
 		// It will run `$this->>update_safe_mode`.
