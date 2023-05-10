35732bac4c29959e510c4767626f23473a568995
David Seveloff <davids@elementor.com>
2023-05-10 15:05:03 +0300

    Description comment added

--- modules/nested-elements/assets/js/editor/views/view.js
+++ modules/nested-elements/assets/js/editor/views/view.js
@@ -38,6 +38,12 @@ export class View extends $e.components.get( 'nested-elements/nested-repeater' )
 		return events;
 	}
 
+	/**
+	 * Function renderHTML().
+	 *
+	 * The `renderHTML()` method is overridden as it causes redundant renders when removing focus from any nested element.
+	 * This is because the original `renderHTML()` method sets `editModel.renderOnLeave = true;`.
+	 */
 	renderHTML() {
 		const templateType = this.getTemplateType(),
 			editModel = this.getEditModel();
