356b35bd4a12d59dd10d9d9bad334e2dbaa78bb2
Michael Meirovich <michaelme@elementor.com>
2023-06-07 16:35:40 +0300

    Further support for PHP 7.0 in deprecation messages

--- modules/dev-tools/deprecation.php
+++ modules/dev-tools/deprecation.php
@@ -214,7 +214,9 @@ class Deprecation {
 			$caller = $this->get_plugin_from_calling_source( $calling_source );
 			$plugin_name = $caller['name'];
 			$source_type = $caller['type'];
-			$source_message = sprintf( '%s on file %s:%d.', $calling_source['function'] ?? '', $calling_source['file'] ?? '', $calling_source['line'] ?? '' );
+			if ( array_key_exists( 'file', $calling_source ) ) {
+				$source_message = sprintf( '%s on file %s:%d.', $calling_source['function'], $calling_source['file'], $calling_source['line'] );
+			}
 		}
 
 		$print_deprecated = [
