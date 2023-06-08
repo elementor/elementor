80988a1a10d420c68c28bae4f73bb09d82908202
DAnn2012 <dannunziata@hotmail.it>
2023-06-08 11:35:24 +0200

    Update icon-box.php

--- includes/widgets/icon-box.php
+++ includes/widgets/icon-box.php
@@ -174,7 +174,7 @@ class Widget_Icon_Box extends Widget_Base {
 				'dynamic' => [
 					'active' => true,
 				],
-				'placeholder' => esc_html__( 'https://your-link.com', 'elementor' ),
+				'placeholder' => esc_html__( 'https://your-link.example.com', 'elementor' ),
 				'separator' => 'before',
 			]
 		);
