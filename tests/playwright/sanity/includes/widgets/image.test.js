fdf2b6d82ff1e02dcbc4ca2d27e991b8166dd1f2
Dmytro Maziar <dmytrom@elementor.com>
2023-06-09 13:34:41 +0200

    Internal: added tests for image-box widget

--- tests/playwright/sanity/includes/widgets/image.test.js
+++ tests/playwright/sanity/includes/widgets/image.test.js
@@ -4,44 +4,83 @@ const EditorPage = require( '../../../pages/editor-page.js' );
 import EditorSelectors from '../../../selectors/editor-selectors.js';
 import Content from '../../../pages/elementor-panel-tabs/content.js';
 
-test( 'Image size test', async ( { page }, testInfo ) => {
-	const wpAdmin = new WpAdminPage( page, testInfo );
-	const editor = new EditorPage( page, testInfo );
-	const contentTab = new Content( page, testInfo );
-	const imageTitle = 'About-Pic-3-1';
+const data = [
+	{
+		widgetTitle: 'image',
+		image: EditorSelectors.image.image,
+		widget: EditorSelectors.image.widget,
+		select: EditorSelectors.image.imageSizeSelect,
+	},
+	{
+		widgetTitle: 'image-box',
+		image: EditorSelectors.imageBox.image,
+		widget: EditorSelectors.imageBox.widget,
+		select: EditorSelectors.imageBox.imageSizeSelect,
+	},
+];
 
-	await wpAdmin.openNewPage();
-	await editor.addWidget( 'image' );
-	await contentTab.chooseImage( `${ imageTitle }.png` );
+for ( const i in data ) {
+	test( `${ data[ i ].widgetTitle }: Image size test`, async ( { page }, testInfo ) => {
+		const wpAdmin = new WpAdminPage( page, testInfo );
+		const editor = new EditorPage( page, testInfo );
+		const contentTab = new Content( page, testInfo );
+		const imageTitle = 'About-Pic-3-1';
 
-	const imageSize = [ 'thumbnail', 'large', 'full' ];
-	for ( const id in imageSize ) {
-		await wpAdmin.waitForPanel();
-		await contentTab.selectImageSize( EditorSelectors.image.widget, imageSize[ id ] );
-		await contentTab.verifyImageSrc( { selector: EditorSelectors.image.image, isPublished: false } );
-		await editor.verifyClassInElement( { selector: EditorSelectors.image.image, className: `size-${ imageSize[ id ] }`, isPublished: false } );
+		await wpAdmin.openNewPage();
+		await editor.addWidget( data[ i ].widgetTitle );
+		await contentTab.chooseImage( `${ imageTitle }.png` );
+
+		const imageSize = [ 'thumbnail', 'large', 'full' ];
+		for ( const id in imageSize ) {
+			await wpAdmin.waitForPanel();
+			await contentTab.selectImageSize(
+				{
+					widget: data[ i ].widget,
+					select: data[ i ].select,
+					imageSize: imageSize[ id ],
+				} );
+			await contentTab.verifyImageSrc( { selector: data[ i ].image, isPublished: false } );
+			await editor.verifyClassInElement(
+				{
+					selector: data[ i ].image,
+					className: `size-${ imageSize[ id ] }`,
+					isPublished: false,
+				} );
+			await editor.publishAndViewPage();
+			await wpAdmin.waitForPanel();
+			await contentTab.verifyImageSrc( { selector: data[ i ].image, isPublished: true } );
+			await editor.verifyClassInElement(
+				{
+					selector: data[ i ].image,
+					className: `size-${ imageSize[ id ] }`,
+					isPublished: true,
+				} );
+			await wpAdmin.editWithElementor();
+		}
+	} );
+}
+
+for ( const i in data ) {
+	test( `${ data[ i ].widgetTitle }: Custom image size test`, async ( { page }, testInfo ) => {
+		const wpAdmin = new WpAdminPage( page, testInfo );
+		const editor = new EditorPage( page, testInfo );
+		const contentTab = new Content( page, testInfo );
+		const imageTitle = 'About-Pic-3-1';
+
+		await wpAdmin.openNewPage();
+		await editor.addWidget( data[ i ].widgetTitle );
+		await contentTab.chooseImage( `${ imageTitle }.png` );
+		await contentTab.setCustomImageSize(
+			{
+				selector: data[ i ].image,
+				select: data[ i ].select,
+				imageTitle, width: '300', height: '300',
+			} );
+		await editor.verifyImageSize( { selector: data[ i ].image, width: 300, height: 300, isPublished: false } );
 		await editor.publishAndViewPage();
-		await wpAdmin.waitForPanel();
-		await contentTab.verifyImageSrc( { selector: EditorSelectors.image.image, isPublished: true } );
-		await editor.verifyClassInElement( { selector: EditorSelectors.image.image, className: `size-${ imageSize[ id ] }`, isPublished: true } );
-		await wpAdmin.editWithElementor();
-	}
-} );
-
-test( 'Custom image size test', async ( { page }, testInfo ) => {
-	const wpAdmin = new WpAdminPage( page, testInfo );
-	const editor = new EditorPage( page, testInfo );
-	const contentTab = new Content( page, testInfo );
-	const imageTitle = 'About-Pic-3-1';
-
-	await wpAdmin.openNewPage();
-	await editor.addWidget( 'image' );
-	await contentTab.chooseImage( `${ imageTitle }.png` );
-	await contentTab.setCustomImageSize( { selector: EditorSelectors.image.image, imageTitle, width: '400', height: '400' } );
-	await editor.verifyImageSize( { selector: EditorSelectors.image.image, width: 400, height: 400, isPublished: false } );
-	await editor.publishAndViewPage();
-	await editor.verifyImageSize( { selector: EditorSelectors.image.image, width: 400, height: 400, isPublished: true } );
-} );
+		await editor.verifyImageSize( { selector: data[ i ].image, width: 300, height: 300, isPublished: true } );
+	} );
+}
 
 test( 'Lightbox image captions aligned center', async ( { page }, testInfo ) => {
 	const wpAdmin = new WpAdminPage( page, testInfo );
