191d6acbaee28d73db2e23a2613142e90dbbc81e
rotemee <61740845+rotemee@users.noreply.github.com>
2023-06-12 14:44:13 +0300

    New: Image AI Tools [ED-11009] (#22716)
    Co-authored-by: Ohad <ohad@elementor.com>
    Co-authored-by: Yakir Sitbon <kingyes1@gmail.com>

--- modules/ai/assets/js/editor/pages/form-media/panel-content/image-variations-form.js
+++ modules/ai/assets/js/editor/pages/form-media/panel-content/image-variations-form.js
@@ -3,18 +3,21 @@ import GenerateButton from '../../../components/generate-button';
 import PromptForm from './form-controls/prompt-form';
 import PromptErrorMessage from '../../../components/prompt-error-message';
 import RefreshIcon from '../../../icons/refresh-icon';
+import ChevronLeftIcon from '../../../icons/chevron-left-icon';
 
 const ImageVariationsForm = ( {
 	panelActive,
 	editImage,
 	prompt = '',
 	setPrompt,
-	generateNewPrompt,
 	promptSettings,
 	updatePromptSettings,
 	submitPrompt,
 	error,
 	images,
+	disableAspectRatio = false,
+	backToTools,
+	viewData,
 } ) => {
 	const handleSubmit = ( event ) => {
 		event.preventDefault();
@@ -23,11 +26,25 @@ const ImageVariationsForm = ( {
 
 	return (
 		<Box component="form" onSubmit={ handleSubmit }>
+			<Box sx={ { mb: 3 } }>
+				<Button
+					size="small"
+					variant="text"
+					color="secondary"
+					startIcon={ <ChevronLeftIcon /> }
+					onClick={ ( e ) => {
+						e.preventDefault();
+						backToTools();
+					} }
+				>
+					{ __( 'Back', 'elementor' ) }
+				</Button>
+			</Box>
 			<Typography variant="h3" sx={ { mb: 3 } }>
-				{ __( 'Imagine anything create everything', 'elementor' ) }
+				{ __( 'Variations', 'elementor' ) }
 			</Typography>
-			<Typography variant="body1" sx={ { mb: 8 } }>
-				{ __( 'Generate images by selecting the desired type and style, and entering a prompt.', 'elementor' ) }
+			<Typography variant="body1" color="secondary" sx={ { mb: 8 } }>
+				{ __( 'Create new versions of the original image.', 'elementor' ) }
 			</Typography>
 
 			{ error && <PromptErrorMessage error={ error } sx={ { mb: 6 } } actionPosition="bottom" onRetry={ handleSubmit } /> }
@@ -42,8 +59,8 @@ const ImageVariationsForm = ( {
 				<img src={ editImage?.image_url || editImage.url } alt={ prompt } style={ {
 					width: 'auto',
 					height: 'auto',
-					maxWidth: 160,
-					maxHeight: 160,
+					maxWidth: '100%',
+					maxHeight: 166,
 					objectFit: 'contained',
 				} } />
 			</Box>
@@ -55,6 +72,8 @@ const ImageVariationsForm = ( {
 				promptSettings,
 				updatePromptSettings,
 				hasImage: true,
+				disableAspectRatio,
+				viewData,
 			} } />
 
 			<Stack gap={ 5 } sx={ { my: 6 } }>
@@ -68,6 +87,10 @@ const ImageVariationsForm = ( {
 								color="secondary"
 								startIcon={ <RefreshIcon /> }
 								disabled={ ( ! panelActive || '' === prompt ) }
+								sx={ {
+									// TODO: Remove on @elementor/ui 1.4.51.
+									color: 'background.paper',
+								} }
 							>
 								{ __( 'Generate again', 'elementor' ) }
 							</Button>
@@ -78,10 +101,6 @@ const ImageVariationsForm = ( {
 							</GenerateButton>
 						)
 				}
-
-				<Button variant="text" onClick={ generateNewPrompt } disabled={ ! panelActive } color="secondary">
-					{ __( 'New prompt', 'elementor' ) }
-				</Button>
 			</Stack>
 		</Box>
 	);
@@ -91,13 +110,15 @@ ImageVariationsForm.propTypes = {
 	panelActive: PropTypes.bool,
 	prompt: PropTypes.string,
 	setPrompt: PropTypes.func,
-	generateNewPrompt: PropTypes.func,
 	editImage: PropTypes.object,
 	promptSettings: PropTypes.object,
 	updatePromptSettings: PropTypes.func,
 	submitPrompt: PropTypes.func,
 	error: PropTypes.string,
 	images: PropTypes.array,
+	disableAspectRatio: PropTypes.bool,
+	backToTools: PropTypes.func,
+	viewData: PropTypes.object,
 };
 
 export default ImageVariationsForm;
