f2d46bf4be40d025f3ed6be3927c10cf87d80e1f
Dmytro Maziar <dmytrom@elementor.com>
2023-06-15 12:34:25 +0200

    Internal: added more assertions

--- tests/playwright/sanity/includes/widgets/tabs.test.js
+++ tests/playwright/sanity/includes/widgets/tabs.test.js
@@ -30,14 +30,21 @@ test( 'Tabs widget sanity test', async ( { page }, testInfo ) => {
 	const editor = new EditorPage( page, testInfo );
 	const contentTab = new Content( page, testInfo );
 	const tabText = 'Super tab content test';
-	const tabTitle = 'Super test tab';
+	const newTabTitle = 'Super test tab';
+	const defaultText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.';
 
 	await wpAdmin.openNewPage();
 	await editor.closeNavigatorIfOpen();
 	await editor.addWidget( 'tabs' );
-	await contentTab.addNewTab( tabTitle, tabText );
+	await contentTab.addNewTab( newTabTitle, tabText );
 	await editor.getPreviewFrame().getByRole( 'tab', { name: 'Tab #1' } ).click();
 	await editor.getPreviewFrame().getByRole( 'tab', { name: 'Tab #1' } ).click();
-	await editor.getPreviewFrame().getByRole( 'tab', { name: tabTitle } ).click();
+	await expect( editor.getPreviewFrame().getByText( defaultText ).first() ).toBeVisible();
+	await editor.getPreviewFrame().getByRole( 'tab', { name: newTabTitle } ).click();
 	await expect( editor.getPreviewFrame().getByText( 'Super tab content test' ) ).toBeVisible();
+	await editor.publishAndViewPage();
+	await page.getByRole( 'tab', { name: 'Tab #1' } ).click();
+	await expect( page.getByText( defaultText ).first() ).toBeVisible();
+	await page.getByRole( 'tab', { name: newTabTitle } ).click();
+	await expect( page.getByText( 'Super tab content test' ) ).toBeVisible();
 } );
