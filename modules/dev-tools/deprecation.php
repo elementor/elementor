19ce7dfd3fe623531b5866c3994e28c46faf72d7
Michael <127856653+confucius-elementor@users.noreply.github.com>
2023-06-20 09:28:14 +0300

    Update modules/dev-tools/deprecation.php
    Co-authored-by: Yakir Sitbon <kingyes1@gmail.com>

--- modules/dev-tools/deprecation.php
+++ modules/dev-tools/deprecation.php
@@ -306,7 +306,7 @@ class Deprecation {
 	 */
 	private function should_print_deprecated( $version, $base_version ) {
 		$is_elementor_debug = defined( 'ELEMENTOR_DEBUG' ) && ELEMENTOR_DEBUG;
-		$is_wp_debug = ( defined( 'WP_DEBUG' ) && WP_DEBUG );
+		$is_wp_debug = defined( 'WP_DEBUG' ) && WP_DEBUG;
 		$user_is_admin = current_user_can( 'manage_options' );
 		$user_is_logged_in = is_user_logged_in();
 
