8c843cdc42715ad164b0e60162d436985079798c
Barco van Rhijn <barcov@elementor.red>
2023-07-03 12:43:57 +0200

    Sanitize terminal commands

--- tests/playwright/utilities/terminal.js
+++ tests/playwright/utilities/terminal.js
@@ -6,26 +6,38 @@ export default class Terminal {
 	* Activate or Deactivate experiments
 	* @param action {string} - activate | deactivate
 	* @param experiments {string} - comma separated list of experiments. e.g. container,nested-elements,'grid container'
+	*
+	* @example experiments( 'activate', 'container,nested-elements,grid container' )
+	* @example experiments( 'deactivate', 'container,nested-elements,grid container' )
 	*/
 	async experiments( action, experiments ) {
-		const wpPath = await this.getWordpressInstallPath();
+		if ( action !== 'activate' && action !== 'deactivate' ) {
+			throw new Error( 'Error Invalid action specified while activating experiments, expecting "activate" / "deactivate" received ' + action );
+		}
 
-		const wpEnv = `npx wp-env run cli "bash -c 'wp elementor experiments ${ action } ${ experiments }'"`;
-		const local = `bash -c "wp elementor experiments ${ action } ${ experiments } --path=${ wpPath }"`;
-
-		const result = await this.executeTerminal( process.env.CI ? wpEnv : local );
+		const cmd = `elementor experiments ${ action } ${ experiments }'"`;
+		const result = await this.executeTerminal( cmd );
 		await this.logErrors( result );
 	}
 
 	/*
-	 * Executes a command in the terminal
-	 * @param {string} cmd the command to execute
+	 * Executes a `wp` command in the terminal
+	 * @param {string} the command to execute
+	 *
+	 * @example executeTerminal( 'elementor experiments activate container' )
+	 * @example executeTerminal( 'elementor experiments default' )
+	 * @example executeTerminal( 'plugin activate woocommerce' )
 	 *
 	 * @return {string} the output of the command
 	 */
 	async executeTerminal( cmd ) {
+		const wpPath = await this.getWordpressInstallPath();
+
+		const wpEnv = `npx wp-env run cli "bash -c 'wp ${ cmd }'"`;
+		const local = `bash -c "wp ${ cmd } --path=${ wpPath }"`;
+
 		try {
-			const stdout = execSync( cmd );
+			const stdout = execSync( process.env.CI ? wpEnv : local );
 			return stdout.toString();
 		} catch ( err ) {
 			return 'Error' + err.toString();
