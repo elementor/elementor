6e6c5638d0df17261e1660e05b838c89c5c1f6dd
Rotem Ben-Itzhak <rotem@elementor.com>
2023-07-03 10:37:48 +0300

    Internal: Images AI Refactor

---
+++ modules/ai/assets/js/editor/pages/form-media/components/image-style-select.js
@@ -0,0 +1,22 @@
+import PromptActionSelection from '../../../components/prompt-action-selection';
+import { IMAGE_PROMPT_CATEGORIES } from '../constants';
+
+const ImageStyleSelect = ( { type = '', ...props } ) => {
+	// The image styles are determined by the selected type.
+	const imageStyles = Object.entries( IMAGE_PROMPT_CATEGORIES[ type ]?.subCategories || {} ).map( ( [ value, label ] ) => ( { label, value } ) );
+
+	return (
+		<PromptActionSelection
+			options={ imageStyles }
+			wrapperStyle={ { width: '100%' } }
+			label={ __( 'Style', 'elementor' ) }
+			{ ...props }
+		/>
+	);
+};
+
+ImageStyleSelect.propTypes = {
+	type: PropTypes.string.isRequired,
+};
+
+export default ImageStyleSelect;
