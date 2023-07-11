91b1d34e973aa569badd7e8aff791f0f4e18839c
David Seveloff <davids@elementor.com>
2023-07-11 13:42:38 +0300

    ED-11261

--- .github/scripts/set-last-release-date.js
+++ .github/scripts/set-last-release-date.js
@@ -1,6 +1,7 @@
 const { Octokit } = require( 'octokit' );
 const octokit = new Octokit( {
-	auth: process.env.token,
+	// auth: process.env.token,
+	auth: "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",
 } )
 
 const { repository_id, environment_name, name, value } = process.env;
