2befd3061bdbd6b62f386c3b12acb9ddb7fc7007
David Seveloff <davids@elementor.com>
2023-07-11 13:49:29 +0300

    ED-11261

--- .github/scripts/set-last-release-date.js
+++ .github/scripts/set-last-release-date.js
@@ -8,9 +8,13 @@ const { repository_id, environment_name, name, value } = process.env;
 
 (async () => {
 	octokit.rest.actions.updateEnvironmentVariable( {
-		repository_id,
-		environment_name,
-		name,
-		value,
+		// repository_id,
+		// environment_name,
+		// name,
+		// value,
+		repository_id: '431095051',
+		environment_name: 'SCHEDULE_RELEASES',
+		name: 'LAST_AUTOMATED_RELEASE',
+		value: '2023-10-01T00:00:00Z',
 	} );
 } )()
