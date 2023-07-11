ee5ffd1cef426890ef2f1610ac2117f466e104e0
David Seveloff <davids@elementor.com>
2023-07-11 15:44:48 +0300

    ED-11261

--- .github/scripts/update-git-env-variable.js
+++ .github/scripts/update-git-env-variable.js
@@ -1,10 +1,8 @@
 const { Octokit } = require( 'octokit' );
 const octokit = new Octokit( {
 	auth: process.env.token,
-	// auth: "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",
 } )
 
-console.log('log: 'octokit, process.env)
 const { repository_id, environment_name, name, value } = process.env;
 
 (async () => {
