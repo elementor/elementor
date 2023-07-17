bd7e74def81e2949df97b12a0e7674b230e9ca2e
David Seveloff <davids@elementor.com>
2023-07-17 14:18:20 +0300

    WIP

--- tests/playwright/sanity/modules/nested-accordion/nested-accordion.test.js
+++ tests/playwright/sanity/modules/nested-accordion/nested-accordion.test.js
@@ -694,7 +694,6 @@ test.describe( 'Nested Accordion @nested-accordion', () => {
 					await nestedAccordionItem.nth( 0 ).hover();
 
 					// Assert
-					await editor.waitForSelector( '.elementor-widget-n-accordion' );
 					await expect.soft( await frame.locator( '.elementor-widget-n-accordion' ).screenshot( { type: 'png' } ) ).toMatchSnapshot( 'nested-accordion-stroke-and-text-shadow.png' );
 				} );
 			} );
@@ -706,7 +705,6 @@ test.describe( 'Nested Accordion @nested-accordion', () => {
 					await page.hover( page.locator( '.e-n-accordion-item' ).nth( 1 ) );
 
 					// Assert
-					await page.waitForSelector( '.elementor-widget-n-accordion' );
 					await expect.soft( await page.locator( '.elementor-widget-n-accordion' ).screenshot( { type: 'png' } ) ).toMatchSnapshot( 'nested-accordion-stroke-and-text-shadow-front.png' );
 				} );
 			} );
