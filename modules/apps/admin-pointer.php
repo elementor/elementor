710a69e8f6de6ba2e6f749547e2742c91eb24f5d
ariel Klikstein <ariel@elementor.com>
2023-07-19 14:43:41 +0300

    Internal: Finish

--- modules/apps/admin-pointer.php
+++ modules/apps/admin-pointer.php
@@ -30,7 +30,7 @@ class Admin_Pointer {
 		$pointer_content .= '<p>' . esc_html__( 'Discover our collection of plugins and add-ons carefully selected to to enhance your Elementor website and unleash your creativity.', 'elementor' ) . '</p>';
 
 		$pointer_content .= sprintf(
-			'<p><a href="%s">%s</a></p>',
+			'<p><a class="button button-primary" href="%s">%s</a></p>',
 			admin_url( 'admin.php?page=' . Module::PAGE_ID ),
 			esc_html__( 'Explore Apps', 'elementor' )
 		)
