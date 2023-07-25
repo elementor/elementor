cf415211e62065adcda47e745439966484bbe0bb
Hein van Vlastuin <heinv@elementor.red>
2023-07-25 20:03:39 +0200

    fix escaping issues

--- modules/nested-tabs-html/widgets/nested-tabs-html.php
+++ modules/nested-tabs-html/widgets/nested-tabs-html.php
@@ -1095,13 +1095,13 @@ class NestedTabsHtml extends Widget_Nested_Base {
 
 		ob_start();
 		?>
-			<button <?php echo $render_attributes; ?>>
-				<span <?php echo $icon_class; ?>>
-					<?php echo $icon_html; ?>
-					<?php echo $icon_active_html; ?>
+			<button <?php echo esc_attr( $render_attributes ); ?>>
+				<span <?php echo esc_attr( $icon_class ); ?>>
+					<?php echo esc_attr( $icon_html ); ?>
+					<?php echo esc_attr( $icon_active_html ); ?>
 				</span>
-				<span <?php echo $text_class; ?>>
-					<?php echo $title; ?>
+				<span <?php echo esc_attr( $text_class ); ?>>
+					<?php echo esc_html( $title ); ?>
 				</span>
 			</button>
 		<?php
