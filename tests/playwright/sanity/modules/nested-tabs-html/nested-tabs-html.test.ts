b9b0142d5e9d571a0a692a41415f76143208b7d8
Hein van Vlastuin <heinv@elementor.red>
2023-07-25 12:22:29 +0200

    add playwright tag

--- tests/playwright/sanity/modules/nested-tabs-html/nested-tabs-html.test.ts
+++ tests/playwright/sanity/modules/nested-tabs-html/nested-tabs-html.test.ts
@@ -9,7 +9,7 @@ import { testCarouselIsVisibleWhenUsingDirectionRightOrLeft } from './tests/caro
 import { editTab, clickTab, setup, cleanup, setTabItemColor, setTabBorderColor, setBackgroundVideoUrl, isTabTitleVisible, selectDropdownContainer } from './helper';
 import ImageCarousel from '../../../pages/widgets/image-carousel';
 
-test.describe( 'Nested Tabs tests @nested-tabs', () => {
+test.describe( 'Nested Tabs tests @nested-tabs-html', () => {
 	let pageId: string;
 	const templatePath = `../templates/nested-tabs-with-icons.json`;
 
