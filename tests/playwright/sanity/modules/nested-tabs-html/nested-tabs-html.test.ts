c0d0fff02c3d69b60b304c6b3c2389a4e0976270
Hein van Vlastuin <heinv@elementor.red>
2023-08-07 16:05:10 +0200

    test update

--- tests/playwright/sanity/modules/nested-tabs-html/nested-tabs-html.test.ts
+++ tests/playwright/sanity/modules/nested-tabs-html/nested-tabs-html.test.ts
@@ -99,6 +99,7 @@ test.describe( 'Nested Tabs tests @nested-tabs-html', () => {
 
 		await test.step( 'Keyboard handling on the Front End', async () => {
 			await editor.saveAndReloadPage();
+			await page.waitForSelector( '.elementor-widget-n-tabs-html' );
 			await editor.publishAndViewPage();
 			await page.waitForSelector( '.elementor-widget-n-tabs-html' );
 
