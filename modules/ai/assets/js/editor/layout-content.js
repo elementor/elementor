dd87c41931760f7411dffbfe6b1571970af8e701
Rotem Ben-Itzhak <rotem@elementor.com>
2023-08-08 09:32:50 +0300

    internal: ai-builder unsaved changes alert

--- modules/ai/assets/js/editor/layout-content.js
+++ modules/ai/assets/js/editor/layout-content.js
@@ -2,84 +2,30 @@ import Connect from './pages/connect';
 import FormLayout from './pages/form-layout';
 import GetStarted from './pages/get-started';
 import Loader from './components/loader';
-import PromptDialog from './components/prompt-dialog';
-import StyledChip from './components/ui/styled-chip';
 import UpgradeChip from './components/upgrade-chip';
 import useUserInfo from './hooks/use-user-info';
 import WizardDialog from './components/wizard-dialog';
-import { DialogTitle, Typography, Stack, IconButton } from '@elementor/ui';
-import { AIIcon, XIcon } from '@elementor/icons';
-
-const promptDialogStyleProps = {
-	sx: {
-		pointerEvents: 'none',
-		'&.MuiDialog-root': {
-			top: 'initial',
-		},
-		'& .MuiDialog-container': {
-			alignItems: 'flex-end',
-		},
-	},
-	PaperProps: {
-		sx: {
-			m: 0,
-			top: 'initial',
-			bottom: ( { spacing } ) => spacing( 9 ),
-			maxHeight: '42vh',
-			pointerEvents: 'auto',
-		},
-	},
-};
-
-const DialogHeader = ( { onClose, children } ) => (
-	<DialogTitle sx={ { fontWeight: 'normal' } }>
-		<AIIcon fontSize="large" sx={ { mr: 3 } } />
-
-		<Typography component="span" variant="subtitle1" sx={ { fontWeight: 'bold', textTransform: 'uppercase' } }>
-			{ __( 'AI Builder', 'elementor' ) }
-		</Typography>
-
-		<StyledChip label={ __( 'Beta', 'elementor' ) } color="default" sx={ { ml: 3 } } />
-
-		<Stack direction="row" spacing={ 3 } alignItems="center" sx={ { ml: 'auto' } }>
-			{ children }
-
-			<IconButton
-				size="small"
-				aria-label="close"
-				onClick={ onClose }
-				sx={ { '&.MuiButtonBase-root': { mr: -4 } } }
-			>
-				<XIcon />
-			</IconButton>
-		</Stack>
-	</DialogTitle>
-);
-
-DialogHeader.propTypes = {
-	children: PropTypes.node,
-	onClose: PropTypes.func.isRequired,
-};
+import LayoutDialog from './pages/form-layout/components/layout-dialog';
 
 const LayoutContent = ( { onClose, onConnect, onGenerationStart, onGenerationEnd, onInsert, onSelect } ) => {
 	const { isLoading, isConnected, isGetStarted, connectUrl, fetchData, hasSubscription, credits, usagePercentage } = useUserInfo();
 
 	if ( isLoading ) {
 		return (
-			<PromptDialog onClose={ onClose } { ...promptDialogStyleProps } maxWidth="md">
-				<DialogHeader onClose={ onClose } />
+			<LayoutDialog onClose={ onClose }>
+				<LayoutDialog.Header onClose={ onClose } />
 
-				<PromptDialog.Content dividers>
+				<LayoutDialog.Content dividers>
 					<Loader />
-				</PromptDialog.Content>
-			</PromptDialog>
+				</LayoutDialog.Content>
+			</LayoutDialog>
 		);
 	}
 
 	if ( ! isConnected ) {
 		return (
 			<WizardDialog onClose={ onClose }>
-				<DialogHeader onClose={ onClose } />
+				<LayoutDialog onClose={ onClose } />
 
 				<WizardDialog.Content dividers>
 					<Connect
@@ -97,7 +43,7 @@ const LayoutContent = ( { onClose, onConnect, onGenerationStart, onGenerationEnd
 	if ( ! isGetStarted ) {
 		return (
 			<WizardDialog onClose={ onClose }>
-				<DialogHeader onClose={ onClose } />
+				<LayoutDialog onClose={ onClose } />
 
 				<WizardDialog.Content dividers>
 					<GetStarted onSuccess={ fetchData } />
@@ -109,26 +55,17 @@ const LayoutContent = ( { onClose, onConnect, onGenerationStart, onGenerationEnd
 	const showUpgradeChip = ! hasSubscription || 80 <= usagePercentage;
 
 	return (
-		<PromptDialog
-			maxWidth="md"
+		<FormLayout
+			credits={ credits }
 			onClose={ onClose }
-			{ ...promptDialogStyleProps }
-		>
-			<DialogHeader onClose={ onClose }>
-				{ showUpgradeChip && <UpgradeChip hasSubscription={ hasSubscription } usagePercentage={ usagePercentage } /> }
-			</DialogHeader>
-
-			<PromptDialog.Content dividers sx={ { '&.MuiDialogContent-root': { p: 0 } } }>
-				<FormLayout
-					credits={ credits }
-					onClose={ onClose }
-					onInsert={ onInsert }
-					onGenerationStart={ onGenerationStart }
-					onGenerationEnd={ onGenerationEnd }
-					onSelect={ onSelect }
-				/>
-			</PromptDialog.Content>
-		</PromptDialog>
+			onInsert={ onInsert }
+			onGenerationStart={ onGenerationStart }
+			onGenerationEnd={ onGenerationEnd }
+			onSelect={ onSelect }
+			DialogHeaderProps={ {
+				children: showUpgradeChip && <UpgradeChip hasSubscription={ hasSubscription } usagePercentage={ usagePercentage } />,
+			} }
+		/>
 	);
 };
 
