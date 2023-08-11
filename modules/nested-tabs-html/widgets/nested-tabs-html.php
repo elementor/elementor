c961b0a9efc4ff3afea6f4a69c48bb49ddbb5173
Barco van Rhijn <barcov@elementor.red>
2023-08-11 10:22:17 +0200

    Remove core padding fallbacks

--- modules/nested-tabs-html/widgets/nested-tabs-html.php
+++ modules/nested-tabs-html/widgets/nested-tabs-html.php
@@ -1056,9 +1056,6 @@ class NestedTabsHtml extends Widget_Nested_Base {
 			]
 		);
 
-		// To do: remove after we update the container styling from physical to logical properties.
-		$padding_physical_properties = '--padding-top: {{TOP}}{{UNIT}}; --padding-right: {{RIGHT}}{{UNIT}}; --padding-bottom: {{BOTTOM}}{{UNIT}}; --padding-left: {{LEFT}}{{UNIT}};';
-
 		$this->add_responsive_control(
 			'box_padding',
 			[
@@ -1066,7 +1063,7 @@ class NestedTabsHtml extends Widget_Nested_Base {
 				'type' => Controls_Manager::DIMENSIONS,
 				'size_units' => [ 'px', '%', 'em', 'rem', 'vw', 'custom' ],
 				'selectors' => [
-					$content_selector => "$padding_physical_properties --padding-block-start: {{TOP}}{{UNIT}}; --padding-inline-end: {{RIGHT}}{{UNIT}}; --padding-block-end: {{BOTTOM}}{{UNIT}}; --padding-inline-start: {{LEFT}}{{UNIT}};",
+					$content_selector => '--padding-block-start: {{TOP}}{{UNIT}}; --padding-inline-end: {{RIGHT}}{{UNIT}}; --padding-block-end: {{BOTTOM}}{{UNIT}}; --padding-inline-start: {{LEFT}}{{UNIT}};',
 				],
 			]
 		);
--- modules/nested-tabs/widgets/nested-tabs.php
+++ modules/nested-tabs/widgets/nested-tabs.php
@@ -1028,9 +1028,6 @@ class NestedTabs extends Widget_Nested_Base {
 			]
 		);
 
-		// To do: remove after we update the container styling from physical to logical properties.
-		$padding_physical_properties = '--padding-top: {{TOP}}{{UNIT}}; --padding-right: {{RIGHT}}{{UNIT}}; --padding-bottom: {{BOTTOM}}{{UNIT}}; --padding-left: {{LEFT}}{{UNIT}};';
-
 		$this->add_responsive_control(
 			'box_padding',
 			[
@@ -1038,7 +1035,7 @@ class NestedTabs extends Widget_Nested_Base {
 				'type' => Controls_Manager::DIMENSIONS,
 				'size_units' => [ 'px', '%', 'em', 'rem', 'vw', 'custom' ],
 				'selectors' => [
-					$nested_tabs_content_selector_class => "$padding_physical_properties --padding-block-start: {{TOP}}{{UNIT}}; --padding-inline-end: {{RIGHT}}{{UNIT}}; --padding-block-end: {{BOTTOM}}{{UNIT}}; --padding-inline-start: {{LEFT}}{{UNIT}};",
+					$nested_tabs_content_selector_class => '--padding-block-start: {{TOP}}{{UNIT}}; --padding-inline-end: {{RIGHT}}{{UNIT}}; --padding-block-end: {{BOTTOM}}{{UNIT}}; --padding-inline-start: {{LEFT}}{{UNIT}};',
 				],
 			]
 		);
