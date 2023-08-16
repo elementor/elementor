c9c936d7b1e7e2531e359599a08b34d503243a5a
Barco van Rhijn <barcov@elementor.red>
2023-08-16 12:57:41 +0200

    Revert NA to shorthand directional properties

--- modules/nested-accordion/widgets/nested-accordion.php
+++ modules/nested-accordion/widgets/nested-accordion.php
@@ -418,7 +418,7 @@ class Nested_Accordion extends Widget_Nested_Base {
 				'type' => Controls_Manager::DIMENSIONS,
 				'size_units' => [ 'px', '%', 'em', 'rem', 'custom' ],
 				'selectors' => [
-					'{{WRAPPER}}' => '--n-accordion-padding-block-start: {{TOP}}{{UNIT}}; --n-accordion-padding-block-end: {{BOTTOM}}{{UNIT}}; --n-accordion-padding-inline-start: {{RIGHT}}{{UNIT}}; --n-accordion-padding-inline-end: {{LEFT}}{{UNIT}};',
+					'{{WRAPPER}} ' => '--n-accordion-padding: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
 				],
 			]
 		);
