5eae53e2f09e4c603b7ea35e605493552d0381e1
Barco van Rhijn <barcov@elementor.red>
2023-08-16 14:06:24 +0200

    Reset Content Template Indent

--- modules/nested-tabs-html/widgets/nested-tabs-html.php
+++ modules/nested-tabs-html/widgets/nested-tabs-html.php
@@ -1105,15 +1105,15 @@ class NestedTabsHtml extends Widget_Nested_Base {
 
 		ob_start();
 		?>
-		<button <?php echo $render_attributes; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>>
-				<span <?php echo $icon_class; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>>
-					<?php echo $icon_html; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
-					<?php echo $icon_active_html; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
-				</span>
-			<span <?php echo $text_class; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>>
-					<?php echo esc_html( $title ); ?>
-				</span>
-		</button>
+			<button <?php echo $render_attributes; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>>
+					<span <?php echo $icon_class; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>>
+						<?php echo $icon_html; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
+						<?php echo $icon_active_html; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
+					</span>
+				<span <?php echo $text_class; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>>
+						<?php echo esc_html( $title ); ?>
+					</span>
+			</button>
 		<?php
 		return ob_get_clean();
 	}
