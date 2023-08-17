4d120efc32401e48b80e1aac41858dc2858497fe
Hady Shaltout <Hady.Shaltout@gmail.com>
2023-08-17 15:46:21 +0300

    Make the container extendable
    Modify the access modifiers from 'private' to 'protected' to allow the extension of [before_render, after_render] functions in third-party plugins

--- includes/elements/container.php
+++ includes/elements/container.php
@@ -188,7 +188,7 @@ class Container extends Element_Base {
 	 *
 	 * @return void
 	 */
-	private function render_video_background() {
+	protected function render_video_background() {
 		$settings = $this->get_settings_for_display();
 
 		if ( 'video' !== $settings['background_background'] ) {
@@ -233,7 +233,7 @@ class Container extends Element_Base {
 	 *
 	 * @return void
 	 */
-	private function render_shape_divider( $side ) {
+	protected function render_shape_divider( $side ) {
 		$settings = $this->get_active_settings();
 		$base_setting_key = "shape_divider_$side";
 		$negative = ! empty( $settings[ $base_setting_key . '_negative' ] );
@@ -259,7 +259,7 @@ class Container extends Element_Base {
 	 *
 	 * @return void
 	 */
-	private function print_html_tag() {
+	protected function print_html_tag() {
 		$html_tag = $this->get_settings( 'html_tag' );
 
 		if ( empty( $html_tag ) ) {
@@ -313,7 +313,7 @@ class Container extends Element_Base {
 		<?php
 	}
 
-	private function is_boxed_container( array $settings ) {
+	protected function is_boxed_container( array $settings ) {
 		return ! empty( $settings['content_width'] ) && 'boxed' === $settings['content_width'];
 	}
 
