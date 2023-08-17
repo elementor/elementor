2fc19329d9d9b8c58a6b6036fb758097e047ed1e
Nikita Murzin <mykytamu@elementor.com>
2023-08-17 09:12:51 +0200

    Tweak: Hide grid widget experiment in this branch [ED-11692]

--- core/experiments/manager.php
+++ core/experiments/manager.php
@@ -431,7 +431,7 @@ class Manager extends Base_Object {
 			'title' => esc_html__( 'Grid Widget', 'elementor' ),
 			'release_status' => self::RELEASE_STATUS_ALPHA,
 			'default' => self::STATE_INACTIVE,
-			'hidden' => false,
+			'hidden' => true,
 			'dependencies' => [
 				'container',
 				'container_grid',
