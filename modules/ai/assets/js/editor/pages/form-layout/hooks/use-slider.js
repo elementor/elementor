d22ea65626e67425657cedf994ba9ddf1a438649
Rotem Ben-Itzhak <rotem@elementor.com>
2023-08-17 17:30:47 +0300

    wip

--- modules/ai/assets/js/editor/pages/form-layout/hooks/use-slider.js
+++ modules/ai/assets/js/editor/pages/form-layout/hooks/use-slider.js
@@ -9,9 +9,9 @@ const useSlider = ( { slidesCount = 0, slidesPerPage = 3, gapPercentage = 2 } = 
 	const pagesCount = Math.ceil( slidesCount / slidesPerPage );
 
 	useEffect( () => {
-		// In cases when the slidesCount value was reduced, we need to navigate back to the previous page.
-		if ( currentPage > 1 && currentPage > slidesCount / slidesPerPage ) {
-			setCurrentPage( ( prev ) => --prev );
+		// In cases when the slidesCount value was reduced, we need to navigate to the last page.
+		if ( currentPage > 1 && currentPage > pagesCount ) {
+			setCurrentPage( pagesCount );
 		}
 	}, [ pagesCount ] );
 
