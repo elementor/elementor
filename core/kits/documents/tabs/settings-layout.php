f06a640912aa502ad3e4fb24c0e1f38d19554eef
Barco van Rhijn <barcov@elementor.red>
2023-08-18 11:26:12 +0200

    Update to logical margin

--- core/kits/documents/tabs/settings-layout.php
+++ core/kits/documents/tabs/settings-layout.php
@@ -120,7 +120,7 @@ class Settings_Layout extends Tab_Base {
 				],
 				'description' => esc_html__( 'Sets the default space between widgets (Default: 20px)', 'elementor' ),
 				'selectors' => [
-					'.elementor-widget:not(:last-child)' => 'margin-bottom: {{SIZE}}{{UNIT}}',
+					'.elementor-widget:not(:last-child)' => 'margin-block-end: {{SIZE}}{{UNIT}}',
 					'.elementor-element' => '--widgets-spacing: {{SIZE}}{{UNIT}}',
 				],
 			]
