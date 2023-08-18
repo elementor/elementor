2f5ba4069d3b0d7f22af08efbdd533ff4ae488e3
Barco van Rhijn <barcov@elementor.red>
2023-08-18 10:59:42 +0200

    Revert NA Logical to shorthand directional padding.

--- modules/nested-accordion/widgets/nested-accordion.php
+++ modules/nested-accordion/widgets/nested-accordion.php
@@ -418,7 +418,7 @@ class Nested_Accordion extends Widget_Nested_Base {
 				'type' => Controls_Manager::DIMENSIONS,
 				'size_units' => [ 'px', '%', 'em', 'rem', 'custom' ],
 				'selectors' => [
-					'{{WRAPPER}} ' => '--padding-block-start: {{TOP}}{{UNIT}}; --padding-inline-end: {{RIGHT}}{{UNIT}}; --padding-block-end: {{BOTTOM}}{{UNIT}}; --padding-inline-start: {{LEFT}}{{UNIT}};',
+					'{{WRAPPER}} ' => 'n-accordion-padding: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
 				],
 			]
 		);
