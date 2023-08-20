2d3ce339bd9fde25911c741779900b3cfb561479
Nevo Golan <20494189+Nevoss@users.noreply.github.com>
2023-08-20 13:09:40 +0300

    Fix: Can't edit elements after accessing the page settings panel [ED-11638] (#23471)

--- assets/dev/js/editor/regions/panel/pages/editor/component.js
+++ assets/dev/js/editor/regions/panel/pages/editor/component.js
@@ -37,7 +37,7 @@ export default class Component extends ComponentBase {
 			/* Translators: %s: Element name. */
 			title = sprintf( __( 'Edit %s', 'elementor' ), elementor.getElementData( model ).title );
 
-		if ( this.wasOutOfFocus() || this.activeModelId !== args.model.id || tab !== this.activeTabs[ args.model.id ] ) {
+		if ( this.shouldRenderPage( tab, args.model.id ) ) {
 			this.activeModelId = args.model.id;
 			this.activeTabs[ args.model.id ] = tab;
 
@@ -52,12 +52,14 @@ export default class Component extends ComponentBase {
 		this.activateControl( activeControl );
 	}
 
-	wasOutOfFocus() {
-		const history = $e.routes.getHistory( 'panel' );
-		const lastRoute = history[ history.length - 1 ].route;
-		const lastRouteParts = lastRoute.split( '/' );
+	shouldRenderPage( tab, modelId ) {
+		const currentPanelView = elementor.getPanelView();
 
-		return 'categories' === lastRouteParts[ lastRouteParts.length - 1 ];
+		const isSamePage = 'editor' === currentPanelView.getCurrentPageName();
+		const isSameTab = tab === currentPanelView.getCurrentPageView().activeTab;
+		const isEditingSameModel = modelId === this.activeModelId;
+
+		return ! isSamePage || ! isSameTab || ! isEditingSameModel;
 	}
 
 	setDefaultTab( args ) {
