214044bf1db7ec4b137edabcc6755228548c2cff
Louis <louiswol94@gmail.com>
2023-08-21 13:39:41 +0200

    Tweak: Ensure downgrade scripts will run [ED-11791]

--- includes/controls/groups/flex-container.php
+++ includes/controls/groups/flex-container.php
@@ -152,7 +152,7 @@ class Group_Control_Flex_Container extends Group_Control_Base {
 			'responsive' => true,
 		];
 
-		$fields['gaps'] = [
+		$fields['gap'] = [
 			'label' => esc_html__( 'Gaps', 'elementor' ),
 			'type' => Controls_Manager::GAPS,
 			'size_units' => [ 'px', '%', 'em', 'rem', 'vm', 'custom' ],
@@ -164,6 +164,10 @@ class Group_Control_Flex_Container extends Group_Control_Base {
 				'{{SELECTOR}}' => '--gap: {{ROW}}{{UNIT}} {{COLUMN}}{{UNIT}}',
 			],
 			'responsive' => true,
+			'conversion_map' => [
+				'old_key' => 'size',
+				'new_key' => 'column',
+			],
 		];
 
 		$fields['wrap'] = [
