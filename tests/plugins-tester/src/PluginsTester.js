48ed638e58ee6fa71632e40f30fc4f0d1e3957ba
Dmytro Maziar <dmytrom@elementor.com>
2023-08-28 14:07:21 +0200

    Internal: fixed cmd

--- tests/plugins-tester/src/PluginsTester.js
+++ tests/plugins-tester/src/PluginsTester.js
@@ -32,9 +32,9 @@ export class PluginsTester {
 	}
 
 	runWP( cmd ) {
-		// If ( ! this.options.runServer ) {
-		// return this.cmd( `cd ../../ && ${ cmd }` );
-		// }
+		if ( ! this.options.runServer ) {
+			return this.cmd( `cd ../../ && ${ cmd }` );
+		}
 		return this.cmd( cmd );
 	}
 
