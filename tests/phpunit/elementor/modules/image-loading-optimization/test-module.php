3287d8d5e1de14ac399b871fe2dd947331e99bf9
Karthik Thayyil <karthik.thayyil@get10up.com>
2023-08-29 21:02:53 +0100

    remove print

--- tests/phpunit/elementor/modules/image-loading-optimization/test-module.php
+++ tests/phpunit/elementor/modules/image-loading-optimization/test-module.php
@@ -13,9 +13,6 @@ class Elementor_Image_Loading_Optimization_Test_Module extends Elementor_Test_Ba
 	 * @dataProvider get_page_template
 	 */
 	public function test_loading_optimization_without_logo( $page_template ) {
-
-		print_r( $page_template );
-
 		$document = self::factory()->create_post();
 		$content = '<img width="800" height="530" src="featured_image.jpg" /><img width="640" height="471" src="image_1.jpg" /><img width="800" height="800" src="image_2.jpg" /><img width="566" height="541" src="image_3.jpg" /><img width="691" height="1024" src="image_4.jpg" />';
 
@@ -28,7 +25,6 @@ class Elementor_Image_Loading_Optimization_Test_Module extends Elementor_Test_Ba
 		query_posts( [ 'p' => $document->get_id() ] );
 		$template_path = locate_template('single.php');
 		$template_path = $page_templates_module->template_include( $template_path );
-		print_r($template_path);
 
 		ob_start();
 		load_template($template_path);
