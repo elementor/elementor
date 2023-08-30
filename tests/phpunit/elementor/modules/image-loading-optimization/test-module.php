6251746db69b9bbcdb889271e0169b569455ee79
Karthik Thayyil <karthik.thayyil@get10up.com>
2023-08-30 14:36:16 +0100

    path issue fix

--- tests/phpunit/elementor/modules/image-loading-optimization/test-module.php
+++ tests/phpunit/elementor/modules/image-loading-optimization/test-module.php
@@ -17,14 +17,26 @@ class Elementor_Image_Loading_Optimization_Test_Module extends Elementor_Test_Ba
 		$content = '<img width="800" height="530" src="featured_image.jpg" /><img width="640" height="471" src="image_1.jpg" /><img width="800" height="800" src="image_2.jpg" /><img width="566" height="541" src="image_3.jpg" /><img width="691" height="1024" src="image_4.jpg" />';
 
 		// Update the post content.
-		$updated_post = array( 'ID' => $document->get_id(), 'post_content' => $content );
+		$updated_post = array( 'ID' => $document->get_main_id(), 'post_content' => $content );
 		wp_update_post( $updated_post );
 		$page_templates_module = Plugin::$instance->modules_manager->get_modules( 'page-templates' );
 		$document->update_main_meta( '_wp_page_template', $page_template );
+
+		print_r($page_template);
+		print_r($document->get_main_id());
+		// print_r($document);
 		
-		query_posts( [ 'p' => $document->get_id() ] );
+		query_posts( [ 'p' => $document->get_main_id() ] );
+		
+		// Need to do the_post as `template_include` depends on `wp_query` for the post.
+		the_post();
+		
+		// get template path from post. If it's not set switch to default single page template.
 		$template_path = locate_template('single.php');
 		$template_path = $page_templates_module->template_include( $template_path );
+
+		// Template usually contains a loop so need to rewind it.
+		rewind_posts();
 
 		ob_start();
 		load_template($template_path);
