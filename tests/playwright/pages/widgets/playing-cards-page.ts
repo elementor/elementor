03b518d1f56f26d12af596bbcf0f8c6c2a4d98b5
aviranl <aviranl@elementor.com>
2023-09-06 16:43:56 +0300

    added tests to playing cards

---
+++ tests/playwright/pages/widgets/playing-cards-page.ts
@@ -0,0 +1,17 @@
+import { type Page, type TestInfo } from '@playwright/test';
+import EditorPage from '../editor-page';
+
+export default class PlayingCardsContent {
+    readonly page: Page;
+    readonly editorPage: EditorPage;
+    constructor( page: Page, testInfo: TestInfo ) {
+        this.page = page;
+        this.editorPage = new EditorPage( this.page, testInfo );
+    }
+    async addNewCard( cardNumber: string, cardType: string ) {
+        await this.page.getByRole( 'button', { name: 'Add Item' } ).click();
+        await this.page.locator('.elementor-control-playing_card_number select').nth(1).selectOption(cardNumber);
+        await this.page.locator('.elementor-control-playing_card_type select').nth(1).selectOption(cardType);
+    }
+}
+
