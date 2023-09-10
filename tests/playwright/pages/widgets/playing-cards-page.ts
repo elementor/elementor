809ec7affeefc288affa5ca630b226e948336707
aviranl <aviranl@elementor.com>
2023-09-10 11:23:44 +0300

    tests locator classes update

--- tests/playwright/pages/widgets/playing-cards-page.ts
+++ tests/playwright/pages/widgets/playing-cards-page.ts
@@ -4,14 +4,16 @@ import EditorPage from '../editor-page';
 export default class PlayingCardsContent {
 	readonly page: Page;
 	readonly editorPage: EditorPage;
+
 	constructor( page: Page, testInfo: TestInfo ) {
 		this.page = page;
 		this.editorPage = new EditorPage( this.page, testInfo );
 	}
+
 	async addNewCard( cardNumber: string, cardType: string ) {
 		await this.page.getByRole( 'button', { name: 'Add Item' } ).click();
-		await this.page.locator( '.elementor-control-playing_card_number select' ).nth( 1 ).selectOption( cardNumber );
-		await this.page.locator( '.elementor-control-playing_card_type select' ).nth( 1 ).selectOption( cardType );
+		await this.page.locator( '.e-control-playing_card_number select' ).nth( 1 ).selectOption( cardNumber );
+		await this.page.locator( '.e-control-playing_card_type select' ).nth( 1 ).selectOption( cardType );
 	}
 }
 
