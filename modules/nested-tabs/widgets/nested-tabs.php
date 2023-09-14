9d82575671a73e5a072d402a3741b2e97aea067a
Hein van Vlastuin <94352322+hein-obox@users.noreply.github.com>
2023-09-14 13:27:29 +0200

    Fix: Display issues when using long titles inside the tabs widget - beta [ED-12054] (#23788)
    ## PR Checklist
    <!-- 
    Please check if your PR fulfills the following requirements:
    **Filling out the template is required.** Any pull request that does not
    include enough information to be reviewed in a timely manner may be
    closed at the maintainers' discretion.
     -->
    - [ ] The commit message follows our guidelines:
    https://github.com/elementor/elementor/blob/master/.github/CONTRIBUTING.md
    
    
    ## PR Type
    What kind of change does this PR introduce?
    <!-- Please check the one that applies to this PR using "x" with no
    spaces eg: [x]. -->
    - [ ] Bugfix
    - [ ] Feature
    - [ ] Code style update (formatting, local variables)
    - [ ] Refactoring (no functional changes, no api changes)
    - [ ] Build related changes
    - [ ] CI related changes
    - [ ] Documentation content changes
    - [ ] Other... Please describe:
    
    ## Summary
    
    This PR can be summarized in the following changelog entry:
    
    *
    
    ## Description
    An explanation of what is done in this PR
    
    *
    
    ## Test instructions
    This PR can be tested by following these steps:
    
    *
    
    ## Quality assurance
    
    - [ ] I have tested this code to the best of my abilities
    - [ ] I have added unittests to verify the code works as intended
    - [ ] Docs have been added / updated (for bug fixes / features)
    
    Fixes #
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: Davids Seveloff <94905340+davseve@users.noreply.github.com>
    Co-authored-by: Dmytro Maziar <dmytrom@elementor.com>
    Co-authored-by: Louis Wolmarans <louiswol94@gmail.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: StyleShit <32631382+StyleShit@users.noreply.github.com>
    Co-authored-by: TzviRabinovitch <99259629+TzviRabinovitch@users.noreply.github.com>
    Co-authored-by: Nevo Golan <20494189+Nevoss@users.noreply.github.com>
    Co-authored-by: Nikita (Mykyta) <134918658+mykytamurzin@users.noreply.github.com>
    Co-authored-by: Yakir Sitbon <kingyes1@gmail.com>
    Co-authored-by: Ariel Klikstein <ariel@elementor.com>
    Co-authored-by: Manor Hazaz <95690086+ManorHazazE@users.noreply.github.com>
    Co-authored-by: Barco <130742176+Barcov@users.noreply.github.com>
    Co-authored-by: Arman <34239421+ArmanPurtoian@users.noreply.github.com>
    Co-authored-by: Mark Tyrin <104833451+mark-elementor@users.noreply.github.com>
    Co-authored-by: rotemee <61740845+rotemee@users.noreply.github.com>
    Co-authored-by: Rami Yushuvaev <92088692+rami-elementor@users.noreply.github.com>
    Co-authored-by: yotamselementor <83912471+yotamselementor@users.noreply.github.com>
    Co-authored-by: Yotam Shapira <yotams@MacBook-Pro.local>
    Co-authored-by: Hady Shaltout <Hady.Shaltout@gmail.com>
    Co-authored-by: Pierre <thedebian@users.noreply.github.com>
    Co-authored-by: elementorbot <admin@elementor.com>
    Co-authored-by: Mati Horowitz <21468434+matipojo@users.noreply.github.com>

--- modules/nested-tabs/widgets/nested-tabs.php
+++ modules/nested-tabs/widgets/nested-tabs.php
@@ -339,9 +339,9 @@ class NestedTabs extends Widget_Nested_Base {
 				],
 			],
 			'selectors_dictionary' => [
-				'start' => '--n-tabs-title-justify-content: flex-start; --n-tabs-title-align-items: flex-start;',
-				'center' => '--n-tabs-title-justify-content: center; --n-tabs-title-align-items: center;',
-				'end' => '--n-tabs-title-justify-content: flex-end; --n-tabs-title-align-items: flex-end;',
+				'start' => '--n-tabs-title-justify-content: flex-start; --n-tabs-title-align-items: flex-start; --n-tabs-title-text-align: start;',
+				'center' => '--n-tabs-title-justify-content: center; --n-tabs-title-align-items: center; --n-tabs-title-text-align: center;',
+				'end' => '--n-tabs-title-justify-content: flex-end; --n-tabs-title-align-items: flex-end; --n-tabs-title-text-align: end;',
 			],
 			'selectors' => [
 				'{{WRAPPER}}' => '{{VALUE}}',
