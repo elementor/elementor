460b27c7c8cf23fc6a10cb31939e4827f39d84b1
StyleShit <evyatar.daud@gmail.com>
2023-09-14 10:17:31 +0300

    wip

--- core/utils/assets-translation-loader.php
+++ core/utils/assets-translation-loader.php
@@ -7,55 +7,20 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 class Assets_Translation_Loader {
-	private $sources_map;
 
-	private $replace_callback;
-
-	private $domain;
-
-	public function __construct( $handles, $domain = null, $replace_callback = null ) {
-		$this->sources_map = $this->map_handles_to_src( $handles );
-		$this->domain = $domain;
-		$this->replace_callback = $replace_callback;
+	public static function for_handles( array $handles, $domain = null, $replace_callback = null ) {
+		self::set_domain( $handles, $domain );
+		self::replace_translation_path( $handles, $replace_callback );
 	}
 
-	public static function for_handles( $handles, $domain = null, $replace_callback = null ) {
-		$instance = new self( $handles, $domain, $replace_callback );
-
-		$instance->set_domain();
-		$instance->register_hooks();
-	}
-
-	private function map_handles_to_src( $handles ) {
-		return Collection::make( $handles )
-			->map_with_keys( function ( $handle ) {
-				$src = wp_scripts()->registered[ $handle ]->src;
-
-				return [ $src => $handle ];
-			} )
-			->all();
-	}
-
-	private function set_domain() {
-		if ( empty( $this->domain ) || ! is_string( $this->domain ) ) {
+	private static function set_domain( array $handles, $domain = null ) {
+		if ( empty( $domain ) || ! is_string( $domain ) ) {
 			return;
 		}
 
-		$handles = array_values( $this->sources_map );
-
 		foreach ( $handles as $handle ) {
-			wp_set_script_translations( $handle, $this->domain );
+			wp_set_script_translations( $handle, $domain );
 		}
-	}
-
-	private function register_hooks() {
-		add_filter( 'load_script_textdomain_relative_path', function ( $relative_path, $src ) {
-			if ( ! isset( $this->sources_map[ $src ] ) ) {
-				return $relative_path;
-			}
-
-			return $this->replace_requested_translation_file( $relative_path, $src );
-		}, 10, 2 );
 	}
 
 	/**
@@ -70,22 +35,32 @@ class Assets_Translation_Loader {
 	 * Want to go deeper? Read the following article:
 	 * @see https://developer.wordpress.com/2022/01/06/wordpress-plugin-i18n-webpack-and-composer/
 	 *
-	 * @param string $relative_path
-	 * @param string $src
-	 *
-	 * @return string
+	 * @param array $handles
+	 * @param callable|null $replace_callback
 	 */
-	private function replace_requested_translation_file( $relative_path, $src ) {
-		if ( is_callable( $this->replace_callback ) ) {
-			$callback = $this->replace_callback;
+	private static function replace_translation_path( array $handles, $replace_callback = null ) {
+		$sources = self::map_handles_to_src( $handles );
 
-			return $callback( $relative_path, $src );
-		}
+		add_filter( 'load_script_textdomain_relative_path', function ( $relative_path, $src ) use ( $sources, $replace_callback ) {
+			if ( ! in_array( $src, $sources, true ) ) {
+				return $relative_path;
+			}
 
-		return $this->default_replace_callback( $relative_path );
+			if ( is_callable( $replace_callback ) ) {
+				return $replace_callback( $relative_path, $src );
+			}
+
+			return self::default_replace_translation( $relative_path );
+		}, 10, 2 );
 	}
 
-	private function default_replace_callback( $relative_path ) {
+	private static function map_handles_to_src( array $handles ) {
+		return array_map( function ( $handle ) {
+			return wp_scripts()->registered[ $handle ]->src;
+		}, $handles );
+	}
+
+	private static function default_replace_translation( $relative_path ) {
 		// Translations are always based on the non-minified filename.
 		$relative_path_without_ext = preg_replace( '/(\.min)?\.js$/i', '', $relative_path );
 
