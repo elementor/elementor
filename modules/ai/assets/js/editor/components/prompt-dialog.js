cac1f92506d0d94e4d9a32c08e876da7087a6e2b
Rotem Ben-Itzhak <rotem@elementor.com>
2023-09-19 07:36:29 +0300

    fix: ai builder dialog should be opened downwards

--- modules/ai/assets/js/editor/components/prompt-dialog.js
+++ modules/ai/assets/js/editor/components/prompt-dialog.js
@@ -1,12 +1,50 @@
+import { useState, useRef, useEffect } from 'react';
 import { Dialog, DialogContent, Paper } from '@elementor/ui';
 import Draggable from 'react-draggable';
 import DialogHeader from './dialog-header';
 
-const DraggablePaper = ( props ) => (
-	<Draggable handle=".MuiAppBar-root" cancel={ '[class*="MuiDialogContent-root"]' } bounds="parent">
-		<Paper { ...props } />
-	</Draggable>
-);
+const DraggablePaper = ( props ) => {
+	const [ controlledPosition, setControlledPosition ] = useState( { x: 0, y: 0 } );
+	const paperRef = useRef( null );
+	const timeout = useRef( null );
+
+	const onControlledDrag = ( _e, { x, y } ) => setControlledPosition( { x, y } );
+
+	const handlePositionBoundries = () => {
+		clearTimeout( timeout.current );
+
+		// Ensuring the dialog header, which is used as the dialog dragging handle, does not exceed the screen.
+		timeout.current = setTimeout( () => {
+			const dialogTop = paperRef.current?.getBoundingClientRect().top;
+
+			if ( dialogTop < 0 ) {
+				setControlledPosition( ( prev ) => ( { ...prev, y: prev.y - dialogTop } ) );
+			}
+		}, 50 );
+	};
+
+	useEffect( () => {
+		const myObserver = new ResizeObserver( handlePositionBoundries );
+
+		myObserver.observe( paperRef.current );
+
+		return () => {
+			myObserver.disconnect();
+		};
+	}, [] );
+
+	return (
+		<Draggable
+			position={ controlledPosition }
+			onDrag={ onControlledDrag }
+			handle=".MuiAppBar-root"
+			cancel={ '[class*="MuiDialogContent-root"]' }
+			bounds="parent"
+		>
+			<Paper { ...props } ref={ paperRef } />
+		</Draggable>
+	);
+};
 
 const PromptDialog = ( props ) => {
 	return (
