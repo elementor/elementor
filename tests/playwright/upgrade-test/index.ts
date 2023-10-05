e09294a700cbb1e3506a3e67dafd0a5c955aba87
Dmytro Maziar <dmytrom@elementor.com>
2023-10-05 12:46:39 +0200

    Internal: added proper loggin

--- tests/playwright/upgrade-test/index.ts
+++ tests/playwright/upgrade-test/index.ts
@@ -1,13 +1,24 @@
+/* eslint-disable no-console */
 
 import { execSync } from 'child_process';
-
 export class UpgradeElementor {
 	cmd( cmd: string ) {
-		return execSync( cmd ).toString();
+		console.log( cmd );
+		const text = execSync( cmd ).toString();
+		console.log( text );
+	}
+
+	cleanUpBeforeTest() {
+		if ( ! process.env.CI ) {
+			this.cmd( 'rm -rf  plugin' );
+			this.cmd( 'rm -rf  ../../../elementor' );
+		}
 	}
 
 	getBuild() {
-		this.cmd( 'cd ../../../ && mv ./build/* elementor && zip -r elementor elementor' );
+		this.cmd( 'mkdir plugin' );
+		this.cmd( 'cd ../../../ && mkdir elementor' );
+		this.cmd( 'cd ../../../ && cp -r ./build/* elementor && zip -r elementor elementor' );
 		this.cmd( 'cd ../../../ && mv ./elementor.zip ./tests/playwright/upgrade-test/plugin' );
 	}
 
@@ -17,14 +28,17 @@ export class UpgradeElementor {
 
 	installLatestPluginFromWP() {
 		this.cmd( `npx wp-env run cli wp plugin install elementor --activate` );
+		this.cmd( 'npx wp-env run cli wp plugin list' );
 	}
 
 	installCurrentPlugin() {
-		this.cmd( `npx wp-env run cli wp plugin install ./plugin/elementor.zip --force --activate` );
+		this.cmd( `npx wp-env run cli wp plugin install ./plugin/elementor.zip --force` );
+		this.cmd( 'npx wp-env run cli wp plugin list' );
 	}
 }
 
 const runner = new UpgradeElementor();
+runner.cleanUpBeforeTest();
 runner.getBuild();
 runner.runServer();
 runner.installLatestPluginFromWP();
