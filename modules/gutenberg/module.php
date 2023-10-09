2fcc21ced75fa23f1a1dcb8575e455828fb518da
ariel Klikstein <ariel@elementor.com>
2023-10-09 22:29:03 +0300

    Tweak: Changed Gutenberg asset loading experiment to Optimized Gutenberg Loading for better explanation [ED-12391]

--- modules/gutenberg/module.php
+++ modules/gutenberg/module.php
@@ -145,8 +145,8 @@ class Module extends BaseModule {
 	public function register_experiments() {
 		Plugin::$instance->experiments->add_feature( [
 			'name' => 'block_editor_assets_optimize',
-			'title' => esc_html__( 'Gutenberg Asset Loading', 'elementor' ),
-			'description' => esc_html__( 'Use this experiment to disable redundant file loading to optimize your site by excluding block editor files (styles and scripts) when they\'re not in use.', 'elementor' ),
+			'title' => esc_html__( 'Optimized Gutenberg Loading', 'elementor' ),
+			'description' => esc_html__( 'Description: Use this experiment to reduce unnecessary render-blocking loads, enhancing site performance by dequeuing unused Gutenberg block editor files (styles and scripts).', 'elementor' ),
 			'release_status' => Experiments_Manager::RELEASE_STATUS_BETA,
 			'default' => Experiments_Manager::STATE_INACTIVE,
 			'new_site' => [
