162a0587eb80e5600ece84a8371805945d6c5e3a
Dmytro Maziar <dmytrom@elementor.com>
2023-10-25 11:05:32 +0200

    Internal: reverting piotnet-addons-for-elementor

--- tests/plugins-tester/backstop_data/engine_scripts/reload-page.cjs
+++
@@ -1,5 +0,0 @@
-// Lazy scroll to bottom in order to allow some animations and motion effects to complete.
-module.exports = async ( page ) => {
-	await page.goto( 'http://localhost:7777/law-firm-about/?elementor' );
-	await page.waitForSelector( '#elementor-panel-elements-categories' );
-};
