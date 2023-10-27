9520937ff28b388aa05d16e05a047316e35de7d2
Dmytro Maziar <dmytrom@elementor.com>
2023-10-27 15:48:15 +0200

    Internal: added widget scenario

--- tests/playwright/sanity/lighthouse.test.ts
+++ tests/playwright/sanity/lighthouse.test.ts
@@ -1,18 +1,25 @@
 import { playAudit } from 'playwright-lighthouse';
 import { test } from '@playwright/test';
 import config from 'lighthouse/lighthouse-core/config/desktop-config';
+import WpAdminPage from '../pages/wp-admin-page';
+import _path from 'path';
 
-test.describe( 'audit example', () => {
-	test( 'open browser', async ( { page } ) => {
-		await page.goto( 'http://localhost:8888/law-firm-about/' );
+test.describe( 'Lighthouse tests', () => {
+	test( 'Accordion widget test', async ( { page }, testInfo ) => {
+		const filePath = _path.resolve( __dirname, `../../elements-regression/tests/templates/accordion.json` );
+		const wpAdmin = new WpAdminPage( page, testInfo );
+		const editorPage = await wpAdmin.openNewPage();
+		await editorPage.closeNavigatorIfOpen();
+		await editorPage.loadTemplate( filePath, true );
+		await editorPage.publishAndViewPage();
 		await playAudit( {
 			page,
 			config,
 			thresholds: {
-				performance: 50,
-				accessibility: 50,
-				'best-practices': 50,
-				seo: 50,
+				performance: 85,
+				accessibility: 85,
+				'best-practices': 85,
+				seo: 80,
 			},
 			port: 9222,
 		} );
