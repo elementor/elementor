83acb056a4b01ca84f12d1ba303a31cc35a8b4d5
Yakir Sitbon <kingyes1@gmail.com>
2023-10-30 13:34:31 +0200

    Tweak: Background Image Size Options are missing when choosing a dynamic image [ED-9824]

--- includes/controls/media.php
+++ includes/controls/media.php
@@ -287,6 +287,8 @@ class Control_Media extends Control_Base_Multiple {
 						</select>
 					</div>
 				</div>
+
+				<div class="elementor-control-field-description"><?php echo esc_html__( 'Image size settings don’t apply to Dynamic Images.', 'elementor' ); ?></div>
 			</div>
 			<# } #>
 
