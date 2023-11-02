2711cae6bbd89f86ed4b66cb6c9cb49e0cc78f7a
Mati Horowitz <mati@elementor.com>
2023-11-02 16:47:35 +0200

    Added caption

--- modules/ai/assets/js/editor/pages/form-layout/components/prompt-chips.js
+++ modules/ai/assets/js/editor/pages/form-layout/components/prompt-chips.js
@@ -1,4 +1,5 @@
-import { Stack } from '@elementor/ui';
+import { Stack, Typography } from '@elementor/ui';
+import { __ } from '@wordpress/i18n';
 import PropTypes from 'prop-types';
 
 export const PromptChips = ( props ) => {
@@ -8,6 +9,9 @@ export const PromptChips = ( props ) => {
 			spacing={ 1 }
 			marginBottom={ 2 }
 		>
+			<Typography variant="caption">
+				{ __( 'Prompt should affect:', 'elementor' ) }
+			</Typography>
 			{ props.children }
 		</Stack>
 	);
