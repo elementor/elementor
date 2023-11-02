b9050bc462f9916d7bbf089ee3def2c37edda4f9
Mati Horowitz <mati@elementor.com>
2023-11-02 16:10:32 +0200

    Review Fixes

--- modules/ai/module.php
+++ modules/ai/module.php
@@ -115,6 +115,7 @@ class Module extends BaseModule {
 			'title' => esc_html__( 'Build with AI', 'elementor' ),
 			'default' => Experiments_Manager::STATE_INACTIVE,
 			'status' => Experiments_Manager::RELEASE_STATUS_ALPHA,
+			'hidden' => true,
 			'dependencies' => [
 				'container',
 			],
