cfa14b58456eeed61530b32f754b35f63356962f
Mati Horowitz <mati@elementor.com>
2023-11-09 16:22:11 +0200

    Fix use timeout

--- modules/ai/assets/js/editor/hooks/use-timeout.js
+++ modules/ai/assets/js/editor/hooks/use-timeout.js
@@ -2,16 +2,15 @@ import { useEffect, useState } from 'react';
 
 export const useTimeout = ( delay ) => {
 	const [ isTimeout, setIsTimeout ] = useState( false );
-	const turnOffTimeout = () => setIsTimeout( false );
+
+	const timeout = setTimeout( () => {
+		setIsTimeout( true );
+	}, delay );
+
+	const turnOffTimeout = () => clearTimeout( timeout );
 
 	useEffect( () => {
-		const timeout = setTimeout( () => {
-			setIsTimeout( true );
-		}, delay );
-
-		return () => {
-			clearTimeout( timeout );
-		};
+		return turnOffTimeout;
 	}, [] );
 
 	return [
