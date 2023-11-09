5937c5a2e1acbf0a271535c4df91f0c00512f842
Yakir Sitbon <kingyes1@gmail.com>
2023-11-09 14:44:20 +0200

    remove double caching

--- modules/element-manager/options.php
+++ modules/element-manager/options.php
@@ -7,19 +7,12 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 class Options {
 
-	private static $disabled_elements = null;
-
 	public static function get_disabled_elements() {
-		if ( null === self::$disabled_elements ) {
-			self::$disabled_elements = (array) get_option( 'elementor_disabled_elements', [] );
-		}
-		return self::$disabled_elements;
+		return (array) get_option( 'elementor_disabled_elements', [] );
 	}
 
 	public static function update_disabled_elements( $elements ) {
 		update_option( 'elementor_disabled_elements', (array) $elements );
-
-		self::$disabled_elements = null;
 	}
 
 	public static function is_element_disabled( $element_name ) {
