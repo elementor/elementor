5326360bf17fe08b781c749e1bc2e57c7379abe8
elchugreeva <elenac@elemntor.com>
2023-11-16 15:53:31 +0200

    added test steps and act/assert

--- tests/playwright/sanity/experiment-messages.test.ts
+++ tests/playwright/sanity/experiment-messages.test.ts
@@ -18,13 +18,24 @@ test.describe( 'Experiment Modal message with multiple dependancies', async () =
 	} );
 
 	test( 'Experiment Modal message with multiple dependancies', async ( { page } ) => {
+		// Act
 		await page.goto( '/wp-admin/admin.php?page=elementor#tab-experiments' );
-		await page.locator( '#e-experiment-mega-menu' ).selectOption( 'active' );
-		const dialogModal = await page.locator( '.dialog-widget' );
-		await expect.soft( dialogModal ).toBeVisible();
-		const dialogConfirmMessage = await page.locator( '.dialog-confirm-message' ),
-			dialogConfirmMessageText = await dialogConfirmMessage.innerText();
-		expect( dialogConfirmMessageText ).toBe( 'In order to use Menu, first you need to activate Flexbox Container Nested Elements.' );
+
+		await test.step( 'change experiment status', async () => {
+			// Act
+			await page.locator( '#e-experiment-mega-menu' ).selectOption( 'active' );
+			const dialogModal = await page.locator( '.dialog-widget' );
+			// Assert
+			await expect.soft( dialogModal ).toBeVisible();
+		} );
+
+		await test.step( 'check message text matches target text', async () => {
+			// Act
+			const dialogConfirmMessage = await page.locator( '.dialog-confirm-message' ),
+				dialogConfirmMessageText = await dialogConfirmMessage.innerText();
+			// Assert
+			expect( dialogConfirmMessageText ).toBe( 'In order to use Menu, first you need to activate Flexbox Container Nested Elements.' );
+		} );
 	} );
 } );
 
