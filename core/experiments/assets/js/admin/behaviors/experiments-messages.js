08b16dd2f512e04f14ea3fa9f129ceaf29ca40d8
elchugreeva <elenac@elemntor.com>
2023-11-16 15:01:48 +0200

    wip

--- core/experiments/assets/js/admin/behaviors/experiments-messages.js
+++ core/experiments/assets/js/admin/behaviors/experiments-messages.js
@@ -171,7 +171,9 @@ export default class ExperimentsMessages {
 	showDependenciesDialog( experimentId ) {
 		const experiment = this.getExperimentData( experimentId ),
 			experimentName = experiment.title,
-			dialogMessage = this.joinDependenciesNames( this.getExperimentDependencies( experimentId ).map( ( d ) => d.title ) );
+			dependanciesArray = this.getExperimentDependencies( experimentId ).map( ( d ) => d.title ),
+			glue = 1 < dependanciesArray.length ? ' ' : '',
+			dialogMessage = this.joinDependenciesNames( dependanciesArray, glue );
 
 		// Translators: %1$s: Experiment title, %2$s: Comma-separated dependencies list
 		const message = __( 'In order to use %1$s, first you need to activate %2$s.', 'elementor' )
