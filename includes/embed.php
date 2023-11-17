5487254758adba4641516d3c1a5ea1ccd0cb5154
JxxIT <110342008+JxxIT@users.noreply.github.com>
2023-11-17 18:46:48 +0100

    Autoplay in embed.php

--- includes/embed.php
+++ includes/embed.php
@@ -197,6 +197,10 @@ class Embed {
 			$default_frame_attributes['data-lazy-load'] = $video_embed_url;
 		}
 
+		if ( isset( $embed_url_params['autoplay'] ) ) {
+			$default_frame_attributes['allow'] = 'autoplay';
+		}
+
 		$frame_attributes = array_merge( $default_frame_attributes, $frame_attributes );
 
 		$attributes_for_print = [];
