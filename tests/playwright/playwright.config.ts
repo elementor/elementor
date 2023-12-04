b57172851bb2b5a3db2142d2127049c1746c32cd
Dmytro Maziar <dmytrom@elementor.com>
2023-12-04 12:19:51 +0100

    Internal: added threshold

--- tests/playwright/playwright.config.ts
+++ tests/playwright/playwright.config.ts
@@ -44,6 +44,7 @@ export default {
 	expect: {
 		timeout: 5_000,
 		toMatchSnapshot: { maxDiffPixelRatio: 0.03 },
+		toHaveScreenshot: { maxDiffPixelRatio: 0.03 },
 	},
 	forbidOnly: !! process.env.CI,
 	retries: process.env.CI ? 1 : 0,
