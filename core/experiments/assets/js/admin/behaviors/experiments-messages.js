5fe7176e6c7388be35a079f01c56e989a213e99e
elchugreeva <elenac@elementor.com>
2023-12-13 17:19:00 +0200

    wip

--- core/experiments/assets/js/admin/behaviors/experiments-messages.js
+++ core/experiments/assets/js/admin/behaviors/experiments-messages.js
@@ -174,6 +174,7 @@ export default class ExperimentsMessages {
 			dependenciesArray = this.getExperimentDependencies( experimentId ).map( ( d ) => d.title ),
 			glue = 1 < dependenciesArray.length ? ' ' : '',
 			dialogMessage = this.joinDependenciesNames( dependenciesArray, glue );
+		//empty comment
 
 		// Translators: %1$s: Experiment title, %2$s: Comma-separated dependencies list
 		const message = __( 'In order to use %1$s, first you need to activate %2$s.', 'elementor' )
