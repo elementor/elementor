9d561983ef85a86058b74883c18229845f20081c
Hein van Vlastuin <94352322+hein-obox@users.noreply.github.com>
2023-12-20 22:45:06 +0200

    WIP

--- tests/playwright/sanity/assets/dev/js/editor/regions/panel/pages/menu/exit-to.test.ts
+++ tests/playwright/sanity/assets/dev/js/editor/regions/panel/pages/menu/exit-to.test.ts
@@ -12,29 +12,28 @@ test( 'Exit to user preference sanity test', async ( { page }, testInfo ) => {
 
 	await editor.page.click( '#elementor-panel-header-menu-button' );
 
-	const exit = page.locator( '.elementor-panel-menu-item-exit a' );
 	let exitHref = '';
 
 	// Select dashboard
 	await setExitUserPreference( editor, 'dashboard' );
 
-	exitHref = await exit.evaluate( ( element ) => element.getAttribute( 'href' ) );
+	exitHref = await page.locator( '.elementor-panel-menu-item-exit a' ).evaluate( ( element ) => element.getAttribute( 'href' ) );
 
-	expect( exitHref ).toContain( '/wp-admin/' );
+	expect.soft( exitHref ).toContain( '/wp-admin/' );
 
 	// Select wp_post_type
 	await setExitUserPreference( editor, 'this_post' );
 
-	exitHref = await exit.getAttribute( 'href' );
+	exitHref = await page.locator( '.elementor-panel-menu-item-exit a' ).evaluate( ( element ) => element.getAttribute( 'href' ) );
 
-	expect( exitHref ).toContain( '/wp-admin/post.php?post=' );
+	expect.soft( exitHref ).toContain( '/wp-admin/post.php?post=' );
 
 	// Select all_posts
 	await setExitUserPreference( editor, 'all_posts' );
 
-	exitHref = await exit.getAttribute( 'href' );
+	exitHref = await page.locator( '.elementor-panel-menu-item-exit a' ).evaluate( ( element ) => element.getAttribute( 'href' ) );
 
-	expect( exitHref ).toContain( '/wp-admin/edit.php' );
+	expect.soft( exitHref ).toContain( '/wp-admin/edit.php' );
 } );
 
 const setExitUserPreference = async ( editor: EditorPage, option: string ) => {
