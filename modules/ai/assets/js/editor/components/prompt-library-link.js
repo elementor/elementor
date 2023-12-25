d5d417c58552ed54805b7f691d2854afdbc39dfb
Dennis Nerush <dennisn@elementor.com>
2023-12-25 11:00:15 +0200

    Tweak: Open Library in new tab [ED-12174]

--- modules/ai/assets/js/editor/components/prompt-library-link.js
+++ modules/ai/assets/js/editor/components/prompt-library-link.js
@@ -6,7 +6,7 @@ const PromptLibraryLink = ( props ) => {
 	return (
 		<Typography variant="body2" color="text.secondary">
 			{ __( 'For more inspiration, try experimenting with proven prompts from our' ) }{ ' ' }
-			<Link href={ props.libraryLink } className="elementor-clickable">
+			<Link href={ props.libraryLink } className="elementor-clickable" target="_blank" rel="noopener noreferrer">
 				{ __( 'prompt library' ) }
 			</Link>
 		</Typography>
