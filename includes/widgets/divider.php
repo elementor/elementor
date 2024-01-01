f6a72e8c92b37287e99ec49831a678e98d90d347
Rami Yushuvaev <ramiy@elementor.com>
2024-01-01 14:01:08 +0200

    Internal: Remove redundant hidden 'view' controls from various widgets

--- includes/widgets/divider.php
+++ includes/widgets/divider.php
@@ -495,14 +495,6 @@ class Widget_Divider extends Widget_Base {
 			]
 		);
 
-		$this->add_control(
-			'view',
-			[
-				'label' => esc_html__( 'View', 'elementor' ),
-				'type' => Controls_Manager::HIDDEN,
-				'default' => 'traditional',
-			]
-		);
 
 		$this->add_control(
 			'text',
