a254f438788093abc7aa01b23d89b4732ea57ae3
Dmytro Maziar <dmytrom@elementor.com>
2024-01-04 14:21:25 +0100

    Internal: added workflow dispatch

--- tests/playwright/playwright.config.ts
+++ tests/playwright/playwright.config.ts
@@ -59,7 +59,7 @@ export default {
 		ignoreHTTPSErrors: true,
 		actionTimeout: 10_000,
 		navigationTimeout: 10_000,
-		trace: 'retain-on-failure',
+		trace: 'on',
 		video: process.env.CI ? 'retain-on-failure' : 'off',
 		baseURL: process.env.BASE_URL || 'http://localhost:8888',
 		viewport: { width: 1920, height: 1080 },
