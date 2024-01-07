5d5f7f0335e507039d1aafe744760431d7e00992
ranshoshani3 <rans@elementor.com>
2024-01-07 13:36:54 +0200

    Lint

--- modules/site-navigation/data/endpoints/homepage.php
+++ modules/site-navigation/data/endpoints/homepage.php
@@ -26,6 +26,6 @@ class Homepage extends Endpoint {
 		$homepage_id = get_option( 'page_on_front' );
 		$show_on_front = get_option( 'show_on_front' );
 
-		return 'page' === $show_on_front ? intval( $homepage_id ) : 0 ;
+		return 'page' === $show_on_front ? intval( $homepage_id ) : 0;
 	}
 }
