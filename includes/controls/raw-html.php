e34b820307cf84cdd15dd9da35e9c67ca7bb5b05
Rami Yushuvaev <ramiy@elementor.com>
2024-01-11 15:55:20 +0200

    update

--- includes/controls/raw-html.php
+++ includes/controls/raw-html.php
@@ -46,7 +46,7 @@ class Control_Raw_Html extends Base_UI_Control {
 			<# if ( data.label ) { #>
 				<div class="elementor-control-raw-html-title">{{{ data.label }}}</div>
 			<# } #>
-			<div class="elementor-control-raw-html-content">{{{ data.raw }}}</div>
+			{{{ data.raw }}}
 		</div>
 		<?php
 	}
