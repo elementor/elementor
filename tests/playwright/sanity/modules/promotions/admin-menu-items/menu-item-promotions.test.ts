ccd00c7f3c2a204c717867dbe92842ccb363ecc0
David Seveloff <davids@elementor.com>
2024-01-14 09:21:40 +0200

    Internal: Implement new 'Pro Features' promotion - Form submissions [ED-13168]

---
+++ tests/playwright/sanity/modules/promotions/admin-menu-items/menu-item-promotions.test.ts
@@ -0,0 +1,14 @@
+import { test, expect } from '@playwright/test';
+import WpAdminPage from '../../../../pages/wp-admin-page';
+
+test( 'Promotion screenshot', async ( { page }, testInfo ) => {
+	const wpAdminPage = new WpAdminPage( page, testInfo ),
+		promotionContainer = '.e-feature-promotion';
+
+	await wpAdminPage.login();
+	await page.goto( '/wp-admin/admin.php?page=e-form-submissions/' );
+	await page.waitForSelector( promotionContainer );
+	expect( await page.locator( promotionContainer ).screenshot( {
+		type: 'png',
+	} ) ).toMatchSnapshot( 'promotion-menu-item-desktop.png' );
+} );
