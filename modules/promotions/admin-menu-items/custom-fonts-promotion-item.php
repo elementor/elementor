ef5612e424716f86cd317b510319369cb2bb2363
Barco van Rhijn <barcov@elementor.red>
2024-01-22 14:56:56 +0200

    WIP

--- modules/promotions/admin-menu-items/custom-fonts-promotion-item.php
+++ modules/promotions/admin-menu-items/custom-fonts-promotion-item.php
@@ -15,18 +15,45 @@ class Custom_Fonts_Promotion_Item extends Base_Promotion_Item {
 		return esc_html__( 'Custom Fonts', 'elementor' );
 	}
 
+	public function get_image_url() {
+			$promotion['image'] = 'images/go-pro-wp-dashboard.svg';
+
+			$promotion= apply_filters( 'elementor/fonts/restrictions/custom_promotion', $promotion );
+			return ELEMENTOR_ASSETS_URL . $promotion['image'];
+	}
+
+	public function get_cta_text() {
+		$promotion['upgrade_text'] = 'Upgrade';
+		$promotion= apply_filters( 'elementor/fonts/restrictions/custom_promotion', $promotion );
+		return esc_html__( $promotion['upgrade_text'], 'elementor' );
+	}
+
 	public function get_promotion_title() {
-		return esc_html__( 'Add Your Custom Fonts', 'elementor' );
+		$promotion['title'] = esc_html__( 'Add Your Custom Fonts', 'elementor' );
+
+		$promotion= apply_filters( 'elementor/fonts/restrictions/custom_promotion', $promotion );
+		return $promotion['title'];
 	}
 
 	public function render_promotion_description() {
-		echo esc_html__(
+		$promotion['description'] = esc_html__(
 			'Custom Fonts allows you to add your self-hosted fonts and use them on your Elementor projects to create a unique brand language.',
 			'elementor'
 		);
+
+		$promotion = apply_filters( 'elementor/fonts/restrictions/custom_promotion', $promotion );
+		echo $promotion['description'];
 	}
 
 	public function get_cta_url() {
-		return 'https://go.elementor.com/go-pro-custom-fonts/';
+		$upgrade_url = 'https://go.elementor.com/go-pro-custom-fonts/';
+		$promotion['upgrade_url'] = $upgrade_url;
+
+		$promotion = apply_filters( 'elementor/fonts/restrictions/custom_promotion', $promotion );
+
+		if( strpos( $promotion['upgrade_url'] ?? '', 'elementor.com' ) === false ) {
+			$promotion['upgrade_url'] = $upgrade_url;
+		}
+		return (string) $promotion['upgrade_url'];
 	}
 }
