1552f1ee2a3c99aedaf17b3b650741bf145f1e9a
Nikita Murzin <mykytamu@elementor.com>
2024-01-25 12:26:23 +0100

    Internal: Reworked editor config setting [ED-13336]

--- modules/editor-events/module.php
+++ modules/editor-events/module.php
@@ -11,35 +11,21 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 class Module extends BaseModule {
 
-	public function __construct() {
-		parent::__construct();
-
-		add_action( 'elementor/editor/localize_settings', function( array $settings ) {
-			return $this->admin_localize_settings( $settings );
-		} );
-	}
-
 	public function get_name() {
 		return 'editor-events';
 	}
 
-	private function admin_localize_settings( $settings ) {
-		$can_send_events = $this->can_send_events();
+	public static function get_editor_events_config() {
+		$can_send_events = Tracker::is_allow_track() && defined( 'ELEMENTOR_EDITOR_EVENTS_DATA_SYSTEM_URL' );
 
-		$additional_settings = [
-			'editor_events' => [
-				'can_send_events' => $can_send_events,
-			],
+		$settings = [
+			'can_send_events' => $can_send_events,
 		];
 
 		if ( $can_send_events ) {
-			$additional_settings['editor_events']['data_system_url'] = ELEMENTOR_EDITOR_EVENTS_DATA_SYSTEM_URL;
+			$settings['data_system_url'] = ELEMENTOR_EDITOR_EVENTS_DATA_SYSTEM_URL;
 		}
 
-		return array_replace_recursive( $settings, $additional_settings );
-	}
-
-	private function can_send_events() {
-		return Tracker::is_allow_track() && defined( 'ELEMENTOR_EDITOR_EVENTS_DATA_SYSTEM_URL' );
+		return $settings;
 	}
 }
