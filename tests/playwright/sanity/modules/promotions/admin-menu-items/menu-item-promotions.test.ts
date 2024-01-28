2de454029138e9ed470d6d1ebf3d8c32a7517107
David Seveloff <davids@elementor.com>
2024-01-28 20:41:45 +0200

    Internal: Implement new Custom Fonts (Free to Pro) promotion [ED-13503]

--- tests/playwright/sanity/modules/promotions/admin-menu-items/menu-item-promotions.test.ts
+++ tests/playwright/sanity/modules/promotions/admin-menu-items/menu-item-promotions.test.ts
@@ -1,13 +1,23 @@
 import { test, expect } from '@playwright/test';
 import WpAdminPage from '../../../../pages/wp-admin-page';
 
-test( 'Promotion screenshot', async ( { page }, testInfo ) => {
+test.only( 'Promotion screenshots', async ( { page }, testInfo ) => {
 	const wpAdminPage = new WpAdminPage( page, testInfo ),
 		promotionContainer = '.e-feature-promotion';
 
 	await wpAdminPage.login();
-	await page.goto( '/wp-admin/admin.php?page=e-form-submissions/' );
-	const promoContainer = page.locator( promotionContainer );
-	await promoContainer.waitFor();
-	await expect( promoContainer ).toHaveScreenshot( 'promotion-menu-item-desktop.png' );
+
+	await test.step( 'Submissions Free to Pro Promotion', async () => {
+		await page.goto( '/wp-admin/admin.php?page=e-form-submissions/' );
+		const promoContainer = page.locator( promotionContainer );
+		await promoContainer.waitFor();
+		await expect( promoContainer ).toHaveScreenshot( 'promotion-menu-item-desktop.png' );
+	} );
+
+	await test.step( 'Custom Icons Free to Pro Promotion', async () => {
+		await page.goto( '/wp-admin/admin.php?page=elementor_custom_icons/' );
+		const promoContainer = page.locator( promotionContainer );
+		await promoContainer.waitFor();
+		await expect( promoContainer ).toHaveScreenshot( 'custom-icon-menu-item-desktop.png' );
+	} );
 } );
