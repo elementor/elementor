5b498279aa993449762afe57964d5fa92d9498c4
David Seveloff <davids@elementor.com>
2024-01-28 21:47:02 +0200

    Custom fonts task code added

--- tests/playwright/sanity/modules/promotions/admin-menu-items/menu-item-promotions.test.ts
+++ tests/playwright/sanity/modules/promotions/admin-menu-items/menu-item-promotions.test.ts
@@ -1,4 +1,4 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, type Page } from '@playwright/test';
 import WpAdminPage from '../../../../pages/wp-admin-page';
 
 test( 'Promotion screenshots', async ( { page }, testInfo ) => {
@@ -7,17 +7,22 @@ test( 'Promotion screenshots', async ( { page }, testInfo ) => {
 
 	await wpAdminPage.login();
 
-	await test.step( 'Submissions Free to Pro Promotion', async () => {
-		await page.goto( '/wp-admin/admin.php?page=e-form-submissions/' );
-		const promoContainer = page.locator( promotionContainer );
-		await promoContainer.waitFor();
-		await expect( promoContainer ).toHaveScreenshot( 'promotion-menu-item-desktop.png' );
+	await test.step( 'Free to Pro - Submissions', async () => {
+		await promotionPageScreenshotTest( page, promotionContainer, 'e-form-submissions', 'submissions-menu-item-desktop' );
 	} );
 
-	await test.step( 'Custom Icons Free to Pro Promotion', async () => {
-		await page.goto( '/wp-admin/admin.php?page=elementor_custom_icons/' );
-		const promoContainer = page.locator( promotionContainer );
-		await promoContainer.waitFor();
-		await expect( promoContainer ).toHaveScreenshot( 'custom-icon-menu-item-desktop.png' );
+	await test.step( 'Free to Pro - Custom Icons', async () => {
+		await promotionPageScreenshotTest( page, promotionContainer, 'elementor_custom_icons', 'custom-icons-menu-item-desktop' );
+	} );
+
+	await test.step( 'Free to Pro - Custom Fonts', async () => {
+		await promotionPageScreenshotTest( page, promotionContainer, 'elementor_custom_fonts', 'custom-fonts-menu-item-desktop' );
 	} );
 } );
+
+async function promotionPageScreenshotTest( page: Page, promotionContainer: string, pageUri: string, screenshotName: string ) {
+	await page.goto( `/wp-admin/admin.php?page=${ pageUri }/` );
+	const promoContainer = page.locator( promotionContainer );
+	await promoContainer.waitFor();
+	await expect( promoContainer ).toHaveScreenshot( `${ screenshotName }.png` );
+}
