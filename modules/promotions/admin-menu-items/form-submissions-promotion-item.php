ffe1a3f4fa926947ef6c42c57abeda27073680d4
Barco van Rhijn <barcov@elementor.red>
2024-01-29 13:48:42 +0200

    Update Promotion

--- modules/promotions/admin-menu-items/form-submissions-promotion-item.php
+++ modules/promotions/admin-menu-items/form-submissions-promotion-item.php
@@ -24,17 +24,15 @@ class Form_Submissions_Promotion_Item extends Base_Promotion_Template {
 	public function get_promotion_title() {
 		/* translators: %s: br  */
 		echo sprintf(
-			esc_html( 'Create Forms and Collect Leads %s with Elementor Pro', 'elementor' ),
+			esc_html( 'Seal the deal on your Form Submissions', 'elementor' ),
 			'<br />'
 		);
 	}
 
 	public function set_list() {
 		return [
-			esc_html__( 'Create single or multi-step forms to engage and convert visitors', 'elementor' ),
-			esc_html__( 'Use any field to collect the information you need', 'elementor' ),
-			esc_html__( 'Integrate your favorite marketing software*', 'elementor' ),
-			esc_html__( 'Collect lead submissions directly within your WordPress Admin to manage, analyze and perform bulk actions on the submitted lead*', 'elementor' ),
+			esc_html__( 'Integrate your favorite marketing software.', 'elementor' ),
+			esc_html__( 'Collect lead submissions directly within your WordPress Admin to manage, analyze and perform bulk actions on the submitted lead.', 'elementor' ),
 		];
 	}
 
@@ -43,10 +41,10 @@ class Form_Submissions_Promotion_Item extends Base_Promotion_Template {
 	}
 
 	public function get_video_url() {
-		return 'https://www.youtube-nocookie.com/embed/LNfnwba9C-8?si=JLHk3UAexnvTfU1a';
+		return 'https://www.youtube.com/embed/LNfnwba9C-8?si=fo0faOT4Ub0jAk8z';
 	}
 
 	public function get_side_note() {
-		return esc_html__( '* Requires an Advanced subscription or higher', 'elementor' );
+		return esc_html__( '', 'elementor' );
 	}
 }
