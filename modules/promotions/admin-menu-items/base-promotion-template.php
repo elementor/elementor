afb23e1cc7b80482e931af09cd25bbfd659d41ab
TzviRabinovitch <tzvir@elementor.com>
2024-02-04 14:07:31 +0200

    Internal: Allow hosting to replace more promotions [ED-13518]

--- modules/promotions/admin-menu-items/base-promotion-template.php
+++ modules/promotions/admin-menu-items/base-promotion-template.php
@@ -3,6 +3,7 @@
 namespace Elementor\Modules\Promotions\AdminMenuItems;
 
 use Elementor\Core\Admin\Menu\Interfaces\Admin_Menu_Item_With_Page;
+use Elementor\Core\Utils\Promotions\Validate_Promotion;
 use Elementor\Settings;
 use Elementor\Utils;
 
@@ -60,27 +61,60 @@ abstract class Base_Promotion_Template implements Admin_Menu_Item_With_Page {
 	}
 
 	public function render() {
+		$promotion_data = $this->get_promotion_data();
 		?>
 			<div class="e-feature-promotion">
 				<div class="e-feature-promotion_data">
-					<h3><?php Utils::print_unescaped_internal_string( $this->get_promotion_title() ); ?></h3>
+					<h3><?php Utils::print_unescaped_internal_string( $promotion_data['promotion_title'] ); ?></h3>
 
-					<?php Utils::print_unescaped_internal_string( $this->get_list() ); ?>
+					<?php Utils::print_unescaped_internal_string( $promotion_data['list'] ); ?>
 
-					<a class="elementor-button go-pro" href="<?php echo esc_url( $this->get_cta_url() ); ?>" target="_blank">
-						<?php Utils::print_unescaped_internal_string( $this->get_cta_text() ); ?>
+					<a class="elementor-button go-pro" href="<?php echo esc_url( $promotion_data['cta_url'] ); ?>" target="_blank">
+						<?php Utils::print_unescaped_internal_string( $promotion_data['cta_text'] ); ?>
 					</a>
 
-					<?php if ( ! empty( $this->get_side_note() ) ) { ?>
+					<?php if ( ! empty( $promotion_data['side_note'] ) ) { ?>
 						<div class="side-note">
-							<p><?php Utils::print_unescaped_internal_string( $this->get_side_note() ); ?></p>
+							<p><?php Utils::print_unescaped_internal_string( $promotion_data['side_note'] ); ?></p>
 						</div>
 					<?php } ?>
 
 				</div>
 
-				<iframe class="e-feature-promotion_iframe" src="<?php Utils::print_unescaped_internal_string( $this->get_video_url() ); ?>&rel=0" title="Elementor" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+				<iframe class="e-feature-promotion_iframe" src="<?php Utils::print_unescaped_internal_string( $promotion_data['video_url'] ); ?>&rel=0" title="Elementor" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
 			</div>
 		<?php
+	}
+
+	/**
+	 * @return array|null
+	 */
+	private function get_promotion_data(): ?array {
+		$promotion_data = $this->build_promotion_data_array();
+
+		$new_promotion_data = apply_filters( 'elementor/' . $this->get_name() . '/custom_promotion', $promotion_data );
+
+		$new_promotion_data = array_replace( $promotion_data, $new_promotion_data );
+
+
+		if ( ! Validate_Promotion::domain_is_on_elementor_dot_com( $new_promotion_data['cta_url'] ) ) {
+			$new_promotion_data['cta_url'] = $promotion_data['cta_url'];
+		}
+
+		return $new_promotion_data;
+	}
+
+	/**
+	 * @return array
+	 */
+	private function build_promotion_data_array(): array {
+		return [
+			'promotion_title' => $this->get_promotion_title(),
+			'cta_url' => $this->get_cta_url(),
+			'cta_text' => $this->get_cta_text(),
+			'video_url' => $this->get_video_url(),
+			'list' => $this->get_list(),
+			'side_note' => $this->get_side_note(),
+		];
 	}
 }
