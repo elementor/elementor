7e3ea96e209d4dc4229b4374aa50c67e2d63e14e
Rami Yushuvaev <ramiy@elementor.com>
2024-02-05 18:16:25 +0200

    fix test

--- tests/playwright/pages/editor-page.ts
+++ tests/playwright/pages/editor-page.ts
@@ -477,9 +477,9 @@ export default class EditorPage extends BasePage {
 
 	async changeUiTheme( uiMode: string ) {
 		const uiThemeOptions = {
-			light: '.eicon-circle-o',
-			dark: '.eicon-circle',
-			auto: '.eicon-device-desktop',
+			light: '.eicon-light-mode',
+			dark: '.eicon-dark-mode',
+			auto: '.eicon-header',
 		};
 		await this.page.locator( '#elementor-panel-header-menu-button' ).click();
 		await this.page.click( '.elementor-panel-menu-item-editor-preferences' );
