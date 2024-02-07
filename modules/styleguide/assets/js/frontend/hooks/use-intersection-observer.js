17f941af9ffa3bbc1a6203aab3e24a3ccf840f52
Mark <markt@elementor.com>
2024-02-07 18:29:10 +0200

    Some more fixes

--- modules/styleguide/assets/js/frontend/hooks/use-intersection-observer.js
+++ modules/styleguide/assets/js/frontend/hooks/use-intersection-observer.js
@@ -1,11 +1,11 @@
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 
 export default function useIntersectionObserver( callback ) {
-	let observer;
+	const observer = useRef( null );
 	let elements = [];
 
 	useEffect( () => {
-		observer = new IntersectionObserver( ( entries ) => {
+		observer.current = new IntersectionObserver( ( entries ) => {
 			const intersectingArea = entries.find( ( entry ) => entry.isIntersecting );
 
 			if ( intersectingArea ) {
@@ -14,25 +14,27 @@ export default function useIntersectionObserver( callback ) {
 		}, {} );
 
 		return () => {
-			observer.disconnect();
+			if ( observer.current ) {
+				observer.current.disconnect();
+			}
 		};
 	}, [] );
 
 	const observe = () => {
-		if ( elements.length !== 0 ) {
+		if ( elements.length !== 0 && observer.current ) {
 			elements.forEach( ( element ) => {
 				if ( element ) {
-					observer.observe( element );
+					observer.current.observe( element );
 				}
 			} );
 		}
 	};
 
 	const unobserve = () => {
-		if ( elements.length !== 0 ) {
+		if ( observer.current && elements.length !== 0 ) {
 			elements.forEach( ( element ) => {
 				if ( element ) {
-					observer.unobserve( element );
+					observer.current.unobserve( element );
 				}
 			} );
 		}
