977214b8087e4c1fb3e7bf47ace5f7af254b4dd8
David Seveloff <davids@elementor.com>
2024-02-11 18:45:18 +0200

    Lint

--- modules/element-manager/ajax.php
+++ modules/element-manager/ajax.php
@@ -113,17 +113,17 @@ class Ajax {
 		return array_replace( $promotion_data, $filtered_data );
 	}
 
-	private function validate_promotion_data( $filtered_data, $promotion_data ){
-		if ( Validate_Promotion::domain_is_on_elementor_dot_com( $filtered_data[ 'url' ] ) ) {
-			$filtered_data[ 'url' ] = esc_url( $filtered_data[ 'url' ] );
+	private function validate_promotion_data( $filtered_data, $promotion_data ) {
+		if ( Validate_Promotion::domain_is_on_elementor_dot_com( $filtered_data['url'] ) ) {
+			$filtered_data[ 'url' ] = esc_url( $filtered_data['url'] );
 		}
 
-		$filtered_data[ 'text' ] = $this->validate_promotion_button_text( $filtered_data[ 'text' ], $promotion_data[ 'text' ] );
+		$filtered_data['text'] = $this->validate_promotion_button_text( $filtered_data['text'], $promotion_data['text'] );
 
 		return $filtered_data;
 	}
 
-	private function validate_promotion_button_text($filtered_text, $promotion_text ) {
+	private function validate_promotion_button_text( $filtered_text, $promotion_text ) {
 		return $filtered_text ?? $promotion_text;
 	}
 
