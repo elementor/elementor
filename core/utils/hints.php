973418315715530f6699c4b93a0846ff065f9b79
Rami Yushuvaev <ramiy@elementor.com>
2024-02-21 22:37:27 +0200

    Update

--- core/utils/hints.php
+++ core/utils/hints.php
@@ -130,9 +130,9 @@ class Hints {
 		}
 
 		if ( $notice_settings['dismissible'] ) {
-			$dismissible = '<button class="elementor-control-notice-dismiss tooltip-target" data-event="' . $notice_settings['dismissible'] . '" data-tooltip="' . esc_attr__( "Dismiss and don't show again.", 'elementor' ) . '">
+			$dismissible = '<button class="elementor-control-notice-dismiss tooltip-target" data-event="' . $notice_settings['dismissible'] . '" data-tooltip="' . esc_attr__( 'Don’t show again.', 'elementor' ) . '">
 				<i class="eicon eicon-close" aria-hidden="true"></i>
-				<span class="elementor-screen-only">' . __( "Dismiss and don't show again.", 'elementor' ) . '</span>
+				<span class="elementor-screen-only">' . esc_html__( 'Don’t show again.', 'elementor' ) . '</span>
 			</button>';
 		}
 
