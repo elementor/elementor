3d1834eae5bd7ef4a796b3dad449755b7367f7d6
Mark Tyrin <markt@elementor.com>
2024-02-22 19:51:35 +0200

    JS lint

--- assets/dev/js/utils/react.js
+++ assets/dev/js/utils/react.js
@@ -1,15 +1,14 @@
 import * as ReactDOM from 'react-dom';
 import { createRoot } from 'react-dom/client';
 
-//
 /**
  * Support conditional rendering of a React App to the DOM, based on the React version.
  * We use `createRoot` when available, but fallback to `ReactDOM.render` for older versions.
  *
- * @param {React.ReactNode} app - The app to render.
- * @param {HTMLElement} domElement - The DOM element to render the app into.
+ * @param app - The app to render.
+ * @param domElement - The DOM element to render the app into.
  *
- * @return {Root} The root object, if it was created. `null` otherwise.
+ * @returns {{root: null, render}} The root object, if it was created, `null` otherwise; and the render.
  */
 function render( app, domElement ) {
 	let root = null;
@@ -28,11 +27,11 @@ function render( app, domElement ) {
 		};
 	}
 
-	const render = renderFn();
+	const renderResult = renderFn();
 
 	return {
 		root,
-		render,
+		renderResult,
 	};
 }
 
@@ -48,4 +47,4 @@ function unmount( root, domElement ) {
 export default {
 	render,
 	unmount,
-}
+};
