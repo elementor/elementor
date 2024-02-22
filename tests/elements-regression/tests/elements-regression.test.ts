17f984ab0981bb5e2d2a3d509afa8cdbd7141371
Mark Tyrin <markt@elementor.com>
2024-02-22 09:04:22 +0200

    Small changes

--- tests/elements-regression/tests/elements-regression.test.ts
+++ tests/elements-regression/tests/elements-regression.test.ts
@@ -37,7 +37,7 @@ test.describe( 'Elementor regression tests with templates for CORE', () => {
 		'icon_hover',
 		'social_icons_hover',
 		'text_path_hover',
-		// 'icons_brands',
+		'icons_brands',
 	];
 
 	for ( const widgetType of testData ) {
