680a8c94f66454ee0e5c193e8d3b7c16db4f38ef
Hein van Vlastuin <heinvanvlastuin@gmail.com>
2024-03-01 15:03:00 +0200

    Update index values.

--- modules/nested-accordion/assets/js/frontend/handlers/nested-accordion.js
+++ modules/nested-accordion/assets/js/frontend/handlers/nested-accordion.js
@@ -15,10 +15,15 @@ export default class NestedAccordion extends Base {
 				accordionContentContainers: '.e-n-accordion > .e-con',
 				accordionItems: '.e-n-accordion-item',
 				accordionItemTitles: '.e-n-accordion-item-title',
+				accordionItemTitlesText: '.e-n-accordion-item-title-text',
 				accordionContent: '.e-n-accordion-item > .e-con',
 				accordionWrapper: '.e-n-accordion-item',
 			},
 			default_state: 'expanded',
+			attributes: {
+				index: 'data-accordion-index',
+				ariaLabelledBy: 'aria-labelledby',
+			},
 		};
 	}
 
@@ -70,14 +75,32 @@ export default class NestedAccordion extends Base {
 		if ( id === currentId ) {
 			const containers = view.find( `${ this.getSettings( 'selectors.accordionContentContainers' ) }, ${ this.getSettings( 'selectors.accordionContent' ) }` ),
 				accordionItems = view.find( this.getSettings( 'selectors.accordionItems' ) ),
-				targetIndex = index !== undefined ? index + 1 : accordionItems.length - 1,
+				isSpecificIndex = index !== undefined,
+				targetIndex = isSpecificIndex ? index + 1 : accordionItems.length - 1,
 				targetContentContainer = targetContainer !== undefined ? targetContainer.view.$el[ 0 ] : containers[ containers.length - 1 ],
 				targetAccordionItem = accordionItems[ targetIndex ] || accordionItems[ accordionItems.length - 1 ];
 
 			targetAccordionItem.appendChild( targetContentContainer );
 
+			if ( isSpecificIndex ) {
+				this.updateIndexValues();
+			}
+
 			this.updateListeners( view );
 		}
+	}
+
+	updateIndexValues() {
+		const { $accordionContent, $accordionItems } = this.getDefaultElements(),
+			settings = this.getSettings(),
+			itemIdBase = $accordionItems[ 0 ].getAttribute( 'id' ).slice( 0, -1 );
+
+		$accordionItems.each( ( index, element ) => {
+			element.querySelector( settings.selectors.accordionItemTitles ).setAttribute( settings.attributes.index, index + 1 );
+			element.setAttribute( 'id', `${ itemIdBase }${ index }` );
+			element.querySelector( settings.selectors.accordionItemTitlesText ).setAttribute( 'data-binding-index', index + 1 );
+			$accordionContent[ index ].setAttribute( settings.attributes.ariaLabelledBy, index + 1 );
+		} );
 	}
 
 	updateListeners( view ) {
