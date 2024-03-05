2d51c95a771bcae5ad69bd703b77606c1ccd2ea0
Hein van Vlastuin <heinvanvlastuin@gmail.com>
2024-03-05 07:13:08 +0200

    Clean up.

--- modules/nested-elements/assets/js/editor/nested-repeater/hooks/data/document/repeater/duplicate/nested-repeater-duplicate-container.js
+++ modules/nested-elements/assets/js/editor/nested-repeater/hooks/data/document/repeater/duplicate/nested-repeater-duplicate-container.js
@@ -1,6 +1,5 @@
 import Base from '../../../base';
-import { findChildContainerOrFail, shouldUseImprovedRepeaters, maybeSortContainerViews, sortViewsByModels } from 'elementor/modules/nested-elements/assets/js/editor/utils';
-import BaseElementView from 'elementor-elements/views/base';
+import { findChildContainerOrFail, shouldUseImprovedRepeaters, sortViewsByModels } from 'elementor/modules/nested-elements/assets/js/editor/utils';
 
 export class NestedRepeaterDuplicateContainer extends Base {
 	getId() {
@@ -32,36 +31,7 @@ export class NestedRepeaterDuplicateContainer extends Base {
 					} },
 				) );
 
-			// container.view.children._views = maybeSortContainerViews( container.view.children._views, index );
 			container.view.children._views = sortViewsByModels( container.model.get( 'elements' ).models, container.view.children._views );
-			// container.view.container = null;
-			// container = container.view.getContainer();
-			// // container = BaseElementView.prototype.getContainer.apply( container.view, [ container.args ] );
-			// container = BaseElementView.prototype.render.apply( container.view, container.args );
-			// const test = container;
-			//
-			// Marionette.CollectionView.prototype._renderChildren.call(this);
-
-
-			// const showCollection = function() {
-			// 	let ChildView;
-			//
-			// 	const models = container.model.get( 'elements' ).models;
-			//
-			// 	// eslint-disable-next-line no-shadow
-			// 	_.each( models, function( child, index ) {
-			// 		delete container.view.children._views[ index ];
-			// 		ChildView = container.view.getChildView( child );
-			// 		container.view.addChild( child, ChildView, index );
-			// 	}, this );
-			// };
-			//
-			// // Marionette.CompositeView.prototype.destroyChildren.apply( container.view );
-			// showCollection();
-
-			// const test = container;
-
-			// Marionette.CompositeView.prototype.reorder.call( container );
 		} else {
 			container.render();
 		}
