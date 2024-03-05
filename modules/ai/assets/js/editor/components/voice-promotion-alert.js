6932f6af4fc6eaacae094d56dd51bb2021243cb8
Adir Duchan <adirdu@MacBook-Pro.local>
2024-03-05 10:45:25 +0200

    CR update

--- modules/ai/assets/js/editor/components/voice-promotion-alert.js
+++ modules/ai/assets/js/editor/components/voice-promotion-alert.js
@@ -5,16 +5,17 @@ import useIntroduction from '../hooks/use-introduction';
 
 export const VoicePromotionAlert = ( props ) => {
 	const { isViewed, markAsViewed } = useIntroduction( props.introductionKey );
+	if ( ! isViewed ) {
+		return null;
+	}
 
 	return (
-		! isViewed
-			?			<Box sx={ { mt: 2, ...props.sx } } alignItems="top">
-				<Alert severity="info" variant="standard" icon={ <BulbIcon sx={ { alignSelf: 'flex-start' } } /> } onClose={ markAsViewed }
-				>
-					{ __( 'Get improved results from AI by adding some personal context. Go to Site Settings > AI Context to get started.' ) }
-				</Alert>
-			</Box>
-			: null
+		<Box sx={ { mt: 2, ...props.sx } } alignItems="top">
+			<Alert severity="info" variant="standard" icon={ <BulbIcon sx={ { alignSelf: 'flex-start' } } /> } onClose={ markAsViewed }
+			>
+				{ __( 'Get improved results from AI by adding some personal context. Go to Site Settings > AI Context to get started.' ) }
+			</Alert>
+		</Box>
 	);
 };
 
