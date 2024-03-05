dacdacbbaae31aaac3ac97e15e9926582c834bad
Hein van Vlastuin <heinvanvlastuin@gmail.com>
2024-03-05 21:15:43 +0200

    Fix await in test.

--- tests/playwright/sanity/container.test.ts
+++ tests/playwright/sanity/container.test.ts
@@ -999,9 +999,9 @@ async function captureJustifySnapShot(
 	await editor.page.click( `.elementor-control-responsive-${ breakpoints[ i ] } .eicon-arrow-${ direction }` );
 
 	const justifyControlsClass = `.elementor-group-control-justify_content.elementor-control-responsive-${ breakpoints[ i ] }`;
-	const justifyControlsContent = await page.locator( `${ justifyControlsClass } .elementor-control-content` ).nth( 0 );
+	const justifyControlsContent = page.locator( `${ justifyControlsClass } .elementor-control-content` ).nth( 0 );
 	await page.waitForLoadState( 'networkidle' ); // Let the icons rotate
-	expect.soft( justifyControlsContent.screenshot( {
+	expect.soft( await justifyControlsContent.screenshot( {
 		type: 'jpeg',
 		quality: 90,
 	} ) ).toMatchSnapshot( `container-justify-controls-${ snapshotPrefix }-${ direction }-${ breakpoints[ i ] }.jpeg` );
