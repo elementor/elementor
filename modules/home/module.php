6b958cba5af9786f7f2ba7914493eb9a0f2a864e
Hein van Vlastuin <heinvanvlastuin@gmail.com>
2024-03-06 13:38:29 +0200

    Update module.php

--- modules/home/module.php
+++ modules/home/module.php
@@ -21,13 +21,6 @@ class Module extends BaseApp {
 		return 'home';
 	}
 
-	private function register_admin_menu( MainMenu $menu ) {
-		$menu->add_submenu( [
-			'page_title' => __( 'Home', 'elementor' ),
-			'menu_title' => '<span id="e-admin-menu__home">' . __( 'Home', 'elementor' ) . '</span>',
-			'menu_slug' => Plugin::$instance->app->get_base_url() . '#/elementor-home',
-			'index' => 41,
-		] );
 	private function enqueue_main_script() {
 		$min_suffix = Utils::is_script_debug() ? '' : '.min';
 
@@ -50,9 +43,6 @@ class Module extends BaseApp {
 	public function __construct() {
 		parent::__construct();
 
-		// @David: what is the comment about?
-		//     Admin_Pointers::add_hooks();
-
 		add_action( 'admin_init', function() { // HVV: Not sure about which hook we should use here.
 			$this->enqueue_main_script();
 		} );
@@ -61,54 +51,10 @@ class Module extends BaseApp {
 			$admin_menu->register( static::PAGE_ID, new Home_Menu_item() );
 		}, 115 );
 
-		add_action( 'elementor/admin/menu/after_register', function ( Admin_Menu_Manager $admin_menu, array $hooks ) {
-			if ( ! empty( $hooks[ static::PAGE_ID ] ) ) {
-				add_action( "admin_print_scripts-{$hooks[ static::PAGE_ID ]}", [ $this, 'enqueue_assets' ] );
-			}
-		}, 10, 2 );
-
-		// Add the Elementor Apps link to the plugin install action links.
-		add_filter( 'install_plugins_tabs', [ $this, 'add_elementor_plugin_install_action_link' ] );
-		add_action( 'install_plugins_pre_elementor', [ $this, 'maybe_open_elementor_tab' ] );
-		add_action( 'admin_print_styles-plugin-install.php', [ $this, 'add_plugins_page_styles' ] );
-	}
-
-	/**
-	 * Register the admin menu the old way.
-	 */
-	private function register_admin_menu_legacy() {
-		add_submenu_page(
-			'elementor',
-			__( 'Home', 'elementor' ),
-			__( 'Home', 'elementor' ),
-			'manage_options',
-			Plugin::$instance->app->get_base_url() . '#/home-screen'
-		);
-	}
-
-	public function __construct() {
-		parent::__construct();
-
-
-		// For cloud purposes
-		if ( Plugin::$instance->experiments->is_feature_active( 'admin_menu_rearrangement' ) ) {
-			add_action( 'elementor/admin/menu_registered/elementor', function ( MainMenu $menu ) {
-				$this->register_admin_menu( $menu );
-			} );
-		} else {
-			add_action( 'admin_menu', function () {
-				$this->register_admin_menu_legacy();
-			}, 51 /* after Elementor page */ );
-		}
-//		add_action( 'elementor/admin/menu/register', function( Admin_Menu_Manager $admin_menu ) {
-//			$admin_menu->register( static::PAGE_ID, new Home_Menu_item() );
-//		}, 115 );
-//
 //		add_action( 'elementor/admin/menu/after_register', function ( Admin_Menu_Manager $admin_menu, array $hooks ) {
 //			if ( ! empty( $hooks[ static::PAGE_ID ] ) ) {
-//				add_action( "admin_print_scripts-{$hooks[ static::PAGE_ID ]}", [ $this, 'enqueue_assets' ] );
+//				add_action( "admin_print_scripts-{$hooks[ static::PAGE_ID ]}", [ $this, 'enqueue_main_script' ] );
 //			}
 //		}, 10, 2 );
-
 	}
 }
