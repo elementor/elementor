700769a364395d2dec73a864b90c161f628644ac
Hein van Vlastuin <heinvanvlastuin@gmail.com>
2024-03-08 20:41:19 +0200

    Clean up home screen.

--- modules/home/assets/js/components/home-screen.js
+++ modules/home/assets/js/components/home-screen.js
@@ -7,14 +7,15 @@ import BlockSection from './block-section';
 const HomeScreen = () => {
 	const baseUrl = elementorAppConfig.pages_url;
 	const [ topScreenProps ] = useState( { videoUrl: 'https://elementor.com/academy/getting-started-with-elementor/', ctaUrl: baseUrl, embedUrl: 'https://www.youtube.com/embed/icTcREd1tAg?si=MPamCEWNeRR_VdAn&amp;controls=0' } );
+
 	return (
-    <Container sx={ { py: 4 } }>
+    	<Container sx={ { py: 4 } }>
 			<Grid sx={ { display: 'grid', gap: 3 } }>
-        <TopSection
-          ctaUrl={ topScreenProps.ctaUrl }
-          videoUrl={ topScreenProps.videoUrl }
-          embedUrl={ topScreenProps.embedUrl }
-        />
+				<TopSection
+				  ctaUrl={ topScreenProps.ctaUrl }
+				  videoUrl={ topScreenProps.videoUrl }
+				  embedUrl={ topScreenProps.embedUrl }
+				/>
 				<BlockSection />
 			</Grid>
 		</Container>
