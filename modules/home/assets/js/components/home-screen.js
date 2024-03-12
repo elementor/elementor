e3c96332229751d8a6bbb237cd09d8d7d3464b3a
elchugreeva <143627257+elchugreeva@users.noreply.github.com>
2024-03-12 14:51:20 +0200

    Internal: Home Screen - create static top section [ED-13943] (#25376)

--- modules/home/assets/js/components/home-screen.js
+++ modules/home/assets/js/components/home-screen.js
@@ -1,18 +1,15 @@
-import { Container, Grid } from '@elementor/ui';
+import { Container } from '@elementor/ui';
 
 import TopSection from './top-section';
-import BlockSection from './block-section';
 
 const HomeScreen = ( props ) => {
+	const videoUrl = 'https://elementor.com/academy/getting-started-with-elementor/',
+		ctaUrl = elementorAppConfig.pages_url,
+		embedUrl = 'https://www.youtube.com/embed/icTcREd1tAg?si=40E8D1hdnu26-TXM';
+
 	return (
-		<Container sx={ { py: 4 } }>
-			<Grid sx={ { display: 'grid', gap: 3 } }>
-				<TopSection
-					topData={ props.homeScreenData.data.top[ 0 ] }
-					createNewPageUrl={ props.homeScreenData.createNewPageUrl }
-				/>
-				<BlockSection />
-			</Grid>
+		<Container sx={ { display: 'flex', flexDirection: 'column', gap: { xs: 1, md: 3 }, py: { xs: 2, md: 6 }, maxWidth: { md: '990px' } } }>
+			<TopSection videoUrl={ videoUrl } ctaUrl={ ctaUrl } embedUrl={ embedUrl } topData={ props.homeScreenData.data.top[ 0 ] } createNewPageUrl={ props.homeScreenData.createNewPageUrl } />
 		</Container>
 	);
 };
--- modules/home/assets/js/components/top-section.js
+++ modules/home/assets/js/components/top-section.js
@@ -1,12 +1,40 @@
-const TopSection = () => {
+import { Box, Stack } from '@elementor/ui';
+import Typography from '@elementor/ui/Typography';
+import Button from '@elementor/ui/Button';
+import YoutubeIcon from '../icons/youtube-icon';
+
+const TopSection = ( { ctaUrl, videoUrl, embedUrl } ) => {
 	return (
-		<p>Add the TopSection component here</p>
+		<Stack direction={ { xs: 'column', md: 'row' } } sx={ { backgroundColor: 'background.default', p: { xs: 2, md: 4 }, borderRadius: 1 } }>
+			<Box sx={ { display: 'flex', flexDirection: 'column', pr: { xs: 0, md: 10 }, pb: 3, gap: 3 } }>
+				<Box>
+					<Typography variant="body2">{ __( 'Hi!', 'elementor' ) }</Typography>
+					<Typography variant="h6">{ __( 'Welcome to Elementor', 'elementor' ) }</Typography>
+					<Typography variant="body2" >{ __( 'Get introduced to Elementor by watching our &ldquo;Getting Started&rdquo; video series. It will guide you through the steps needed to create your website. Then click to create your first page.', 'elementor' ) }</Typography>
+				</Box>
+				<Box sx={ { display: 'flex', gap: 1 } }>
+					<Button variant="contained" size="small" href={ ctaUrl } target="_blank" >{ __( 'Create a Page', 'elementor' ) }</Button>
+					<Button variant="outlined" color="secondary" size="small" startIcon={ <YoutubeIcon /> } href={ videoUrl } target="_blank">{ __( 'Watch a quick starter', 'elementor' ) }</Button>
+				</Box>
+			</Box>
+			<Box component="iframe"
+				src={ embedUrl }
+				title="YouTube video player"
+				frameBorder="0"
+				allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+				allowFullScreen={ true }
+				sx={ { aspectRatio: '16/9', borderRadius: 1, display: 'flex', width: '100%', maxWidth: '365px' } }>
+			</Box>
+		</Stack>
 	);
 };
 
 TopSection.propTypes = {
-	topData: PropTypes.object,
-	createNewPageUrl: PropTypes.string,
+	ctaUrl: PropTypes.string.isRequired,
+	videoUrl: PropTypes.string.isRequired,
+	embedUrl: PropTypes.string.isRequired,
+	topData: PropTypes.object.isRequired,
+	createNewPageUrl: PropTypes.string.isRequired,
 };
 
 export default TopSection;
