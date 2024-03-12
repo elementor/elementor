47d7133dacac7eeab1eb9d6b3af15b9399963e41
Hein van Vlastuin <heinvanvlastuin@gmail.com>
2024-03-12 14:58:34 +0200

    Update props.

--- modules/home/assets/js/components/top-section.js
+++ modules/home/assets/js/components/top-section.js
@@ -3,7 +3,7 @@ import Typography from '@elementor/ui/Typography';
 import Button from '@elementor/ui/Button';
 import YoutubeIcon from '../icons/youtube-icon';
 
-const TopSection = ( { videoUrl, embedUrl } ) => {
+const TopSection = ( props ) => {
 	return (
 		<Stack direction={ { xs: 'column', md: 'row' } } sx={ { backgroundColor: 'background.default', p: { xs: 2, md: 4 }, borderRadius: 1 } }>
 			<Box sx={ { display: 'flex', flexDirection: 'column', pr: { xs: 0, md: 10 }, pb: 3, gap: 3 } }>
@@ -13,12 +13,12 @@ const TopSection = ( { videoUrl, embedUrl } ) => {
 					<Typography variant="body2" >{ __( 'Get introduced to Elementor by watching our &ldquo;Getting Started&rdquo; video series. It will guide you through the steps needed to create your website. Then click to create your first page.', 'elementor' ) }</Typography>
 				</Box>
 				<Box sx={ { display: 'flex', gap: 1 } }>
-					<Button variant="contained" size="small" href={ createNewPageUrl } target="_blank" >{ __( 'Create a Page', 'elementor' ) }</Button>
-					<Button variant="outlined" color="secondary" size="small" startIcon={ <YoutubeIcon /> } href={ videoUrl } target="_blank">{ __( 'Watch a quick starter', 'elementor' ) }</Button>
+					<Button variant="contained" size="small" href={ props.createNewPageUrl }>{ __( 'Create a Page', 'elementor' ) }</Button>
+					<Button variant="outlined" color="secondary" size="small" startIcon={ <YoutubeIcon /> } href={ props.videoUrl } target="_blank">{ __( 'Watch a quick starter', 'elementor' ) }</Button>
 				</Box>
 			</Box>
 			<Box component="iframe"
-				src={ embedUrl }
+				src={ props.embedUrl }
 				title="YouTube video player"
 				frameBorder="0"
 				allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
