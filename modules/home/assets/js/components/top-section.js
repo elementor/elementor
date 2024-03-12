8eee3d726f494ede3f42ad59cb5305a8f5f58cd6
elchugreeva <elenac@elementor.com>
2024-03-12 11:28:58 +0200

    add translation

--- modules/home/assets/js/components/top-section.js
+++ modules/home/assets/js/components/top-section.js
@@ -8,13 +8,13 @@ const TopSection = ( { ctaUrl, videoUrl, embedUrl } ) => {
 		<Stack direction={ { xs: 'column', md: 'row' } } sx={ { backgroundColor: 'background.default', p: { xs: 2, md: 4 } } }>
 			<Box sx={ { display: 'flex', flexDirection: 'column', pr: { xs: 0, md: 10 }, pb: 3, gap: 3 } }>
 				<Box>
-					<Typography variant="body2">Hi!</Typography>
-					<Typography variant="h6">Welcome to Elementor</Typography>
-					<Typography variant="body2" >Get introduced to Elementor by watching our &ldquo;Getting Started&rdquo; video series. It will guide you through the steps needed to create your website. Then click to create your first page.</Typography>
+					<Typography variant="body2">{ __( 'Hi!', 'elementor' ) }</Typography>
+					<Typography variant="h6">{ __( 'Welcome to Elementor', 'elementor' ) }</Typography>
+					<Typography variant="body2" >{ __( 'Get introduced to Elementor by watching our &ldquo;Getting Started&rdquo; video series. It will guide you through the steps needed to create your website. Then click to create your first page.', 'elementor' ) }</Typography>
 				</Box>
 				<Box sx={ { display: 'flex', gap: 1 } }>
-					<Button variant="contained" size="small" href={ ctaUrl } target="_blank" >Create a Page</Button>
-					<Button variant="outlined" color="secondary" size="small" startIcon={ <YoutubeIcon /> } href={ videoUrl } target="_blank">Watch a quick starter</Button>
+					<Button variant="contained" size="small" href={ ctaUrl } target="_blank" >{ __( 'Create a Page', 'elementor' ) }</Button>
+					<Button variant="outlined" color="secondary" size="small" startIcon={ <YoutubeIcon /> } href={ videoUrl } target="_blank">{ __( 'Watch a quick starter', 'elementor' ) }</Button>
 				</Box>
 			</Box>
 			<Box component="iframe"
