89fd48f8940621f8e612ad029fa632da07c1b44f
Ron Rostovsky <ronros@elementor.com>
2024-03-14 16:12:46 +0200

    resolved conflicts

--- modules/memory-game/widgets/memory-game.php
+++ modules/memory-game/widgets/memory-game.php
@@ -44,8 +44,8 @@ class MemoryGame extends Widget_Base {
 		$repeater->add_control(
 			'card_caption',
 			[
-				'type'    => Controls_Manager::TEXT,
-				'label'   => esc_html__( 'Caption', 'elementor' ),
+				'type' => Controls_Manager::TEXT,
+				'label' => esc_html__( 'Caption', 'elementor' ),
 				'default' => esc_html__( 'A photo of stuff', 'elementor' ),
 			]
 		);
@@ -77,20 +77,20 @@ class MemoryGame extends Widget_Base {
 			'memory_game_style_section',
 			[
 				'label' => esc_html__( 'Style', 'elementor' ),
-				'tab'   => Controls_Manager::TAB_STYLE,
+				'tab' => Controls_Manager::TAB_STYLE,
 			]
 		);
 
 		$this->add_control(
 			'card_border',
 			[
-				'label'      => esc_html__( 'Border width', 'elementor' ),
-				'type'       => Controls_Manager::DIMENSIONS,
+				'label' => esc_html__( 'Border width', 'elementor' ),
+				'type' => Controls_Manager::DIMENSIONS,
 				'size_units' => [ 'px', '%', 'em', 'rem', 'vw', 'custom' ],
-				'selectors'  => [
+				'selectors' => [
 					'{{WRAPPER}} .card' => 'border-width: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
 				],
-				'default'    => [
+				'default' => [
 					'size' => 4
 				]
 			]
@@ -134,22 +134,22 @@ class MemoryGame extends Widget_Base {
 		$this->add_responsive_control(
 			'columns',
 			[
-				'label'              => esc_html__( 'Columns', 'elementor' ),
-				'type'               => Controls_Manager::SLIDER,
-				'range'              => [
+				'label' => esc_html__( 'Columns', 'elementor' ),
+				'type' => Controls_Manager::SLIDER,
+				'range' => [
 					'fr' => [
-						'min'  => 1,
-						'max'  => 8,
+						'min' => 1,
+						'max' => 12,
 						'step' => 1,
 					],
 				],
-				'default'            => [
+				'default' => [
 					'size' => 4,
 				],
-				'mobile_default'     => [
+				'mobile_default' => [
 					'size' => 2,
 				],
-				'selectors'          => [
+				'selectors' => [
 					'{{WRAPPER}}' => '--e-memory-game-columns: {{SIZE}}',
 				],
 				'frontend_available' => true,
