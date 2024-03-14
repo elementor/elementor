4629bbf423a1e033d92417e3565e704cc93b59f4
elchugreeva <elenac@elementor.com>
2024-03-14 15:32:46 +0200

    fixed comment

--- modules/home/assets/js/components/home-screen.js
+++ modules/home/assets/js/components/home-screen.js
@@ -7,8 +7,7 @@ const HomeScreen = ( props ) => {
 		embedUrl = 'https://www.youtube.com/embed/icTcREd1tAg?si=40E8D1hdnu26-TXM';
 
 	return (
-		<Box maxWidth="xl" sx={ { pr: 1 } }>
-			{ /* Box wrapper around the Container is needed to neutralize wp-content area left-padding, that becomes too apparent on smaller screens, and ensure that Container is getting desired width*/ }
+		<Box maxWidth="xl" sx={ { pr: 1 } }> { /* Box wrapper around the Container is needed to neutralize wp-content area left-padding*/ }
 			<Container disableGutters={ true } maxWidth="lg" sx={ { display: 'flex', flexDirection: 'column', gap: { xs: 1, md: 3 }, py: { xs: 2, md: 6 } } }>
 				<TopSection
 					videoUrl={ videoUrl }
