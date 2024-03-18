2e1d9d30a5d6e477922ad51413164dea8cabf60e
elchugreeva <143627257+elchugreeva@users.noreply.github.com>
2024-03-18 10:35:39 +0200

    Fix: [Internal]: Make sure Home screen is centered also on wide screens [ED-14087] (#25450)

--- modules/home/assets/js/components/home-screen.js
+++ modules/home/assets/js/components/home-screen.js
@@ -5,7 +5,7 @@ import TopSection from './top-section';
 const HomeScreen = ( props ) => {
 	return (
 		// Box wrapper around the Container is needed to neutralize wp-content area left-padding
-		<Box maxWidth="xl" sx={ { pr: 1 } }>
+		<Box sx={ { pr: 1 } }>
 			<Container disableGutters={ true } maxWidth="lg" sx={ { display: 'flex', flexDirection: 'column', gap: { xs: 1, md: 3 }, py: { xs: 2, md: 6 } } }>
 				<TopSection
 					topData={ props.homeScreenData.data.top }
