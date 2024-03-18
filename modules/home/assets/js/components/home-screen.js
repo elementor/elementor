2d1b2b8eba8d74a971dd711c6b4391c886a6f3e2
elchugreeva <elenac@elementor.com>
2024-03-18 13:34:09 +0200

    icons and layout added

--- modules/home/assets/js/components/home-screen.js
+++ modules/home/assets/js/components/home-screen.js
@@ -6,15 +6,18 @@ const HomeScreen = ( props ) => {
 	return (
 		// Box wrapper around the Container is needed to neutralize wp-content area left-padding
 		<Box sx={ { pr: 1 } }>
-			<Container disableGutters={ true } maxWidth="lg" sx={ { display: 'flex', flexDirection: 'column', gap: { xs: 1, md: 3 }, py: { xs: 2, md: 6 } } }>
+			<Container disableGutters={ true } maxWidth="lg" sx={ { display: 'flex', flexDirection: 'column', gap: { xs: 1, md: 3 }, pt: { xs: 2, md: 6 }, pb: 2 } }>
 				<TopSection
 					topData={ props.homeScreenData.data.top }
 					createNewPageUrl={ props.homeScreenData.createNewPageUrl }
 				/>
 			</Container>
-			<Container maxWidth="xs" disableGutters={ true } sx={ { alignItems: 'space-between' } }>
-				<Container flexGrow={ 1 } disableGutters={ true } sx={ { display: 'flex', flexDirection: 'column', gap: 3, py: 3 } }></Container>
-				<PromotionBar sideData={ props.homeScreenData.data.sidebar_upgrade } />
+			<Container disableGutters={ true } sx={ { display: 'flex', flexDirection: { xs: 'column', sm: 'row' }, justifyContent: 'space-between' } }>
+				{ /* Placeholder container to ensure layout until other components are added*/ }
+				<Container maxWidth="md"></Container>
+				<Container maxWidth="xs" disableGutters={ true } sx={ { width: { md: '305px' } } }>
+					<PromotionBar sideData={ props.homeScreenData.data.sidebar_upgrade } />
+				</Container>
 			</Container>
 		</Box>
 	);
--- modules/home/assets/js/components/side-bar-promotion.js
+++ modules/home/assets/js/components/side-bar-promotion.js
@@ -2,27 +2,25 @@ import { Box, Paper, Typography, Stack } from '@elementor/ui';
 import Button from '@elementor/ui/Button';
 import List from '@elementor/ui/List';
 import ListItem from '@elementor/ui/ListItem';
-import ListItemIcon from '@elementor/ui/ListItemIcon';
 import ListItemText from '@elementor/ui/ListItemText';
-import SideBarIcon from "../icons/side-bar-cta-icon";
+import SideBarIcon from '../icons/side-bar-cta-icon';
+import SideBarCheckIcon from '../icons/side-bar-check-icon';
 
 const PromotionBar = ( { ...props } ) => {
 	return (
-		<Paper elevation={ 0 } sx={ { p: 3, gap: 2 } }>
-			<Stack gap={ 1 } sx={ { alignItems: 'center' } }>
-				<Box component="img" src={ props.sideData.header.image } sx={ { width: '35%' } }></Box>
+		<Paper elevation={ 0 } sx={ { p: 3 } }>
+			<Stack gap={ 1.5 } sx={ { alignItems: 'center', textAlign: 'center', pb: 4 } }>
+				<Box component="img" src={ props.sideData.header.image } sx={ { width: '130px' } }></Box>
 				<Typography variant="h6">{ props.sideData.header.title }</Typography>
 				<Typography variant="body2">{ props.sideData.header.description }</Typography>
-				<Button variant="contained" size="medium" color="promotion" href={ props.sideData.cta.url } startIcon={ <SideBarIcon url={ props.sideData.cta.image } /> } target="_blank" sx={ { maxWidth: 'fit-content' } }>{ props.sideData.cta.label }</Button>
+				<Button variant="contained" size="medium" color="promotion" href={ props.sideData.cta.url } startIcon={ <SideBarIcon /> } target="_blank" sx={ { maxWidth: 'fit-content' } }>{ props.sideData.cta.label }</Button>
 			</Stack>
-			<List>
+			<List sx={ { alignItems: 'flex-start' } }>
 				{
 					props.sideData.repeater.map( ( item, index ) => {
 						return (
-							<ListItem key={ index }>
-								<ListItemIcon>
-									<Box component="image" src={ item.icon }></Box>
-								</ListItemIcon>
+							<ListItem key={ index } sx={ { px: 0, gap: 2 } }>
+								<SideBarCheckIcon />
 								<ListItemText variant="body2">{ item.title }</ListItemText>
 							</ListItem>
 								)
