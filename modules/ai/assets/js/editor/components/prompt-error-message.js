2c5ce8115dacf24e2ff1b1269ed538b5b8caf7a2
Naor Sabag <32329815+naorsabag@users.noreply.github.com>
2024-03-19 17:19:37 +0200

    Tweak: AI free trial editor update UI 3.20 [ED-14078] (#25466)
    ## PR Checklist
    <!-- 
    Please check if your PR fulfills the following requirements:
    **Filling out the template is required.** Any pull request that does not
    include enough information to be reviewed in a timely manner may be
    closed at the maintainers' discretion.
     -->
    - [x] The commit message follows our guidelines:
    https://github.com/elementor/elementor/blob/master/.github/CONTRIBUTING.md
    
    
    ## PR Type
    What kind of change does this PR introduce?
    <!-- Please check the one that applies to this PR using "x" with no
    spaces eg: [x]. -->
    - [ ] Bugfix
    - [ ] Feature
    - [ ] Code style update (formatting, local variables)
    - [ ] Refactoring (no functional changes, no api changes)
    - [ ] Build related changes
    - [ ] CI related changes
    - [ ] Documentation content changes
    - [x] Other... Please describe:
    
    ## Summary
    
    This PR can be summarized in the following changelog entry:
    
    *
    
    ## Description
    An explanation of what is done in this PR
    
    *
    
    ## Test instructions
    This PR can be tested by following these steps:
    
    *
    
    ## Quality assurance
    
    - [x] I have tested this code to the best of my abilities
    - [ ] I have added unittests to verify the code works as intended
    - [ ] Docs have been added / updated (for bug fixes / features)
    
    Fixes #
    
    ---------
    
    Co-authored-by: ElementorBot <48412871+elementorbot@users.noreply.github.com>

--- modules/ai/assets/js/editor/components/prompt-error-message.js
+++ modules/ai/assets/js/editor/components/prompt-error-message.js
@@ -1,61 +1,82 @@
-import { Alert, Button, AlertTitle, Box } from '@elementor/ui';
+import { Alert, AlertTitle, Box, Button } from '@elementor/ui';
 import { __ } from '@wordpress/i18n';
 import PropTypes from 'prop-types';
 
 const PromptErrorMessage = ( { error, onRetry = () => {}, actionPosition = 'default', ...props } ) => {
-	const messages = {
-		default: {
-			text: <AlertTitle>{ __( 'Unknown error. Please try again later.', 'elementor' ) }</AlertTitle>,
-			description: __( 'Error code:', 'elementor' ) + ' ' + error,
-			buttonText: __( 'Try Again', 'elementor' ),
-			buttonAction: onRetry,
-		},
-		service_outage_internal: {
-			text: <AlertTitle>{ __( 'Elementor AI is temporarily unavailable', 'elementor' ) }</AlertTitle>,
-			description: __( 'Seems like we are experiencing technical difficulty. We should be up and running shortly.', 'elementor' ),
-			buttonText: __( 'Try Again', 'elementor' ),
-			buttonAction: onRetry,
-		},
-		invalid_connect_data: {
-			text: <AlertTitle>{ __( 'Reconnect your account', 'elementor' ) }</AlertTitle>,
-			description: (
-				<>
-					{ __( 'We couldn\'t connect to your account due to technical difficulties on our end. Reconnect your account to continue.', 'elementor' ) }
-					{ ' ' }<a href="https://elementor.com/help/disconnecting-reconnecting-your-elementor-account/" target="_blank" rel="noreferrer">{ __( 'Show me how', 'elementor' ) }</a>
-				</>
-			),
-			buttonText: __( 'Reconnect', 'elementor' ),
-			buttonAction: () => window.open( window.ElementorAiConfig.connect_url ),
-		},
-		not_connected: {
-			text: <AlertTitle>{ __( 'You aren\'t connected to Elementor AI.', 'elementor' ) }</AlertTitle>,
-			description: __( 'Elementor AI is just a few clicks away. Connect your account to instantly create texts and custom code.', 'elementor' ),
-			buttonText: __( 'Connect', 'elementor' ),
-			buttonAction: () => window.open( window.ElementorAiConfig.connect_url ),
-		},
-		quota_reached_trail: {
-			text: <AlertTitle>{ __( 'It\'s time to upgrade.', 'elementor' ) }</AlertTitle>,
-			description: __( 'Enjoy the free trial? Upgrade now for unlimited access to built-in image, text and custom code generators.', 'elementor' ),
-			buttonText: __( 'Upgrade', 'elementor' ),
-			buttonAction: () => window.open( 'https://go.elementor.com/ai-popup-purchase-limit-reached/', '_blank' ),
-		},
-		quota_reached_subscription: {
-			text: <AlertTitle>{ __( 'It\'s time to upgrade.', 'elementor' ) }</AlertTitle>,
-			description: __( 'Love Elementor AI? Upgrade to continue creating with built-in image, text and custom code generators.', 'elementor' ),
-			buttonText: __( 'Upgrade', 'elementor' ),
-			buttonAction: () => window.open( 'https://go.elementor.com/ai-popup-purchase-limit-reached/', '_blank' ),
-		},
-		rate_limit_network: {
-			text: <AlertTitle>{ __( 'Whoa! Slow down there.', 'elementor' ) }</AlertTitle>,
-			description: __( 'We can’t process that many requests so fast. Try again in 15 minutes.', 'elementor' ),
-		},
-		invalid_prompts: {
-			text: <AlertTitle>{ __( 'We were unable to generate that prompt.', 'elementor' ) }</AlertTitle>,
-			description: __( 'Seems like the prompt contains words that could generate harmful content. Write a different prompt to continue.', 'elementor' ),
-		},
-	};
+	function getQuotaReachedTrailMessage( featureName ) {
+		if ( ! featureName ) {
+			return {
+				text: <AlertTitle>{ __( 'It\'s time to upgrade.', 'elementor' ) }</AlertTitle>,
+				description: __( 'Enjoy the free trial? Upgrade now for unlimited access to built-in image, text and custom code generators.', 'elementor' ),
+				buttonText: __( 'Upgrade', 'elementor' ),
+				buttonAction: () => window.open( 'https://go.elementor.com/ai-popup-purchase-limit-reached/', '_blank' ),
+			};
+		}
 
-	const message = messages[ error ] || messages.default;
+		return {
+			text: <AlertTitle>{ __( 'Your free trial is up!', 'elementor' ) }</AlertTitle>,
+			// Translators: %s is the feature name.
+			description: sprintf( __( 'Upgrade now to keep using %s', 'elementor' ), featureName ),
+			buttonText: __( 'Upgrade now', 'elementor' ),
+			buttonAction: () => window.open( 'https://go.elementor.com/ai-popup-purchase-limit-reached/', '_blank' ),
+		};
+	}
+
+	function getErrorMessage() {
+		const errMsg = error.message || error;
+		const featureName = error.extra_data?.featureName;
+
+		const messages = {
+			default: {
+				text: <AlertTitle>{ __( 'Unknown error. Please try again later.', 'elementor' ) }</AlertTitle>,
+				description: __( 'Error code:', 'elementor' ) + ' ' + error,
+				buttonText: __( 'Try Again', 'elementor' ),
+				buttonAction: onRetry,
+			},
+			service_outage_internal: {
+				text: <AlertTitle>{ __( 'Elementor AI is temporarily unavailable', 'elementor' ) }</AlertTitle>,
+				description: __( 'Seems like we are experiencing technical difficulty. We should be up and running shortly.', 'elementor' ),
+				buttonText: __( 'Try Again', 'elementor' ),
+				buttonAction: onRetry,
+			},
+			invalid_connect_data: {
+				text: <AlertTitle>{ __( 'Reconnect your account', 'elementor' ) }</AlertTitle>,
+				description: (
+					<>
+						{ __( 'We couldn\'t connect to your account due to technical difficulties on our end. Reconnect your account to continue.', 'elementor' ) }
+						{ ' ' }<a href="https://elementor.com/help/disconnecting-reconnecting-your-elementor-account/" target="_blank" rel="noreferrer">{ __( 'Show me how', 'elementor' ) }</a>
+					</>
+				),
+				buttonText: __( 'Reconnect', 'elementor' ),
+				buttonAction: () => window.open( window.ElementorAiConfig.connect_url ),
+			},
+			not_connected: {
+				text: <AlertTitle>{ __( 'You aren\'t connected to Elementor AI.', 'elementor' ) }</AlertTitle>,
+				description: __( 'Elementor AI is just a few clicks away. Connect your account to instantly create texts and custom code.', 'elementor' ),
+				buttonText: __( 'Connect', 'elementor' ),
+				buttonAction: () => window.open( window.ElementorAiConfig.connect_url ),
+			},
+			quota_reached_trail: getQuotaReachedTrailMessage( featureName ),
+			quota_reached_subscription: {
+				text: <AlertTitle>{ __( 'It\'s time to upgrade.', 'elementor' ) }</AlertTitle>,
+				description: __( 'Love Elementor AI? Upgrade to continue creating with built-in image, text and custom code generators.', 'elementor' ),
+				buttonText: __( 'Upgrade', 'elementor' ),
+				buttonAction: () => window.open( 'https://go.elementor.com/ai-popup-purchase-limit-reached/', '_blank' ),
+			},
+			rate_limit_network: {
+				text: <AlertTitle>{ __( 'Whoa! Slow down there.', 'elementor' ) }</AlertTitle>,
+				description: __( 'We can’t process that many requests so fast. Try again in 15 minutes.', 'elementor' ),
+			},
+			invalid_prompts: {
+				text: <AlertTitle>{ __( 'We were unable to generate that prompt.', 'elementor' ) }</AlertTitle>,
+				description: __( 'Seems like the prompt contains words that could generate harmful content. Write a different prompt to continue.', 'elementor' ),
+			},
+		};
+
+		return messages[ errMsg ] || messages.default;
+	}
+
+	const message = getErrorMessage();
 
 	const action = message?.buttonText && (
 		<Button
