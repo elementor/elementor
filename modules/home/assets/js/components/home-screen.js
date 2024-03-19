892eafdeac7a38c56db2d65ddb55b9a0f202973d
elchugreeva <elenac@elementor.com>
2024-03-19 12:26:31 +0200

    comment fix

--- modules/home/assets/js/components/home-screen.js
+++ modules/home/assets/js/components/home-screen.js
@@ -1,22 +1,22 @@
 import { Container, Box } from '@elementor/ui';
 import TopSection from './top-section';
-import PromotionBar from './side-bar-promotion';
+import SideBarPromotion from './sidebar-promotion';
 
 const HomeScreen = ( props ) => {
 	return (
-		// Box wrapper around the Container is needed to neutralize wp-content area left-padding
+		/*  Box wrapper around the Container is needed to neutralize wp-content area left-padding */
 		<Box sx={ { pr: 1 } }>
-			<Container disableGutters={ true } maxWidth="lg" sx={ { display: 'flex', flexDirection: 'column', gap: { xs: 1, md: 3 }, pt: { xs: 2, md: 6 }, pb: 2 } }>
+			<Container disableGutters={ true } maxWidth="lg" sx={ { display: 'flex', flexDirection: 'column', pt: { xs: 2, md: 6 }, pb: 2 } }>
 				<TopSection
 					topData={ props.homeScreenData.data.top }
 					createNewPageUrl={ props.homeScreenData.createNewPageUrl }
 				/>
 			</Container>
 			<Container disableGutters={ true } sx={ { display: 'flex', flexDirection: { xs: 'column', sm: 'row' }, justifyContent: 'space-between' } }>
-				{ /* Placeholder container to ensure layout until other components are added*/ }
+				{ /* Placeholder container to ensure layout until other components are added */ }
 				<Container maxWidth="md"></Container>
 				<Container maxWidth="xs" disableGutters={ true } sx={ { width: { md: '305px' } } }>
-					<PromotionBar sideData={ props.homeScreenData.data.sidebar_upgrade } />
+					<SideBarPromotion sideData={ props.homeScreenData.data.sidebar_upgrade } />
 				</Container>
 			</Container>
 		</Box>
--- modules/home/assets/js/components/sidebar-promotion.js
+++ modules/home/assets/js/components/sidebar-promotion.js
@@ -5,16 +5,18 @@ import ListItem from '@elementor/ui/ListItem';
 import ListItemText from '@elementor/ui/ListItemText';
 import SideBarCheckIcon from '../icons/side-bar-check-icon';
 
-const PromotionBar = ( { ...props } ) => {
+const SideBarPromotion = ( { ...props } ) => {
 	return (
 		<Paper elevation={ 0 } sx={ { p: 3 } }>
 			<Stack gap={ 1.5 } sx={ { alignItems: 'center', textAlign: 'center', pb: 4 } }>
 				<Box component="img" src={ props.sideData.header.image } sx={ { width: '130px' } }></Box>
-				<Typography variant="h6">{ props.sideData.header.title }</Typography>
-				<Typography variant="body2">{ props.sideData.header.description }</Typography>
+				<Box>
+					<Typography variant="h6">{ props.sideData.header.title }</Typography>
+					<Typography variant="body2" color="text.secondary">{ props.sideData.header.description }</Typography>
+				</Box>
 				<Button variant="contained" size="medium" color="promotion" href={ props.sideData.cta.url } startIcon={ <Box component="img" src={ props.sideData.cta.image } sx={ { width: '16px' } }></Box> } target="_blank" sx={ { maxWidth: 'fit-content' } }>{ props.sideData.cta.label }</Button>
 			</Stack>
-			<List sx={ { alignItems: 'flex-start' } }>
+			<List>
 				{
 					props.sideData.repeater.map( ( item, index ) => {
 						return (
@@ -30,8 +32,8 @@ const PromotionBar = ( { ...props } ) => {
 	);
 };
 
-export default PromotionBar;
+export default SideBarPromotion;
 
-PromotionBar.propTypes = {
+SideBarPromotion.propTypes = {
 	sideData: PropTypes.object.isRequired,
 };
