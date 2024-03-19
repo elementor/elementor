2b211807a9f6756ddb14b4c6023d6c0d6c4c2cbc
DevR0101 <razn@MacBook-Pro.local>
2024-03-19 18:50:52 +0200

    Tweak: Adjust all error messages to be readable with proper terms [ ED-13977 ]

--- modules/ai/connect/ai.php
+++ modules/ai/connect/ai.php
@@ -4,6 +4,7 @@ namespace Elementor\Modules\Ai\Connect;
 use Elementor\Core\Common\Modules\Connect\Apps\Library;
 use Elementor\Modules\Ai\Module;
 use Elementor\Utils as ElementorUtils;
+use Elementor\Plugin;
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly
@@ -563,6 +564,13 @@ class Ai extends Library {
 			$context['features'] = [
 				'subscriptions' => [ 'Pro' ],
 			];
+		}
+
+		if ( Plugin::$instance->experiments->is_feature_active( 'container_grid' ) ) {
+			$context['features'] = [
+				'supportedFeatures' => [ 'Grid' ],
+			];
+			var_dump('asas ', $context); // This will print the content of $context
 		}
 
 		$metadata = [
