5448f96c5a5910b6e60ca746d429c4b0de53a644
elchugreeva <elenac@elementor.com>
2024-03-19 15:17:14 +0200

    layout fix

--- modules/home/assets/js/components/home-screen.js
+++ modules/home/assets/js/components/home-screen.js
@@ -6,17 +6,17 @@ const HomeScreen = ( props ) => {
 	return (
 		/*  Box wrapper around the Container is needed to neutralize wp-content area left-padding */
 		<Box sx={ { pr: 1 } }>
-			<Container disableGutters={ true } maxWidth="lg" sx={ { display: 'flex', flexDirection: 'column', pt: { xs: 2, md: 6 }, pb: 2 } }>
+			<Container disableGutters={ true } maxWidth="lg" sx={ { display: 'flex', flexDirection: 'column', gap: { xs: 1, md: 3 }, pt: { xs: 2, md: 6 }, pb: 2 } }>
 				<TopSection
 					topData={ props.homeScreenData.top }
 					createNewPageUrl={ props.homeScreenData.create_new_page_url }
 				/>
-			</Container>
-			<Container disableGutters={ true } sx={ { display: 'flex', flexDirection: { xs: 'column', sm: 'row' }, justifyContent: 'space-between' } }>
-				{ /* Placeholder container to ensure layout until other components are added */ }
-				<Container maxWidth="md"></Container>
-				<Container maxWidth="xs" disableGutters={ true } sx={ { width: { md: '305px' } } }>
-					<SideBarPromotion sideData={ props.homeScreenData.data.sidebar_upgrade } />
+				<Container disableGutters={ true } sx={ { display: 'flex', flexDirection: { xs: 'column', sm: 'row' }, justifyContent: 'space-between' } }>
+					{ /* Placeholder container to ensure layout until other components are added */ }
+					<Container maxWidth="md"></Container>
+					<Container maxWidth="xs" disableGutters={ true } sx={ { width: { md: '305px' } } }>
+						<SideBarPromotion sideData={ props.homeScreenData.sidebar_upgrade } />
+					</Container>
 				</Container>
 			</Container>
 		</Box>
