9a9031e8ce1c2de58c339ec1d3016ef1b180a573
Hein van Vlastuin <heinvanvlastuin@gmail.com>
2024-03-19 09:39:07 +0200

    Only run home screen enqueue once.

--- modules/home/module.php
+++ modules/home/module.php
@@ -15,6 +15,8 @@ class Module extends BaseApp {
 
 	const PAGE_ID = 'home_screen';
 
+	static private bool $is_home_screen_scripts_enqueued = false;
+
 	public function get_name(): string {
 		return 'home';
 	}
@@ -35,7 +37,7 @@ class Module extends BaseApp {
 	}
 
 	public function enqueue_home_screen_scripts(): void {
-		if ( ! current_user_can( 'manage_options' ) ) {
+		if ( ! current_user_can( 'manage_options' ) || static::$is_home_screen_scripts_enqueued ) {
 			return;
 		}
 
@@ -61,6 +63,8 @@ class Module extends BaseApp {
 			'elementorHomeScreenData',
 			$this->get_app_js_config()
 		);
+
+		static::$is_home_screen_scripts_enqueued = true;
 	}
 
 	public function is_experiment_active(): bool {
