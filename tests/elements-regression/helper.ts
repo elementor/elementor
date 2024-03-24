be30f36256467a8a35e3723a90c231f6bd44dc44
Rami Yushuvaev <ramiy@elementor.com>
2024-03-24 15:36:03 +0200

    Revert "Test - replace timeout with .waitFor()"
    This reverts commit 484451d013cefb47c5b20d2c48890937718aee31.

--- tests/elements-regression/helper.ts
+++ tests/elements-regression/helper.ts
@@ -36,10 +36,8 @@ export default class ElementRegressionHelper {
 				iframe.style.height = '3000px';
 			} );
 		}
-
-		await locator.waitFor();
 		await expect.soft( locator )
-			.toHaveScreenshot( `${ widgetType }_${ label }.png`, { maxDiffPixels: 200 } );
+			.toHaveScreenshot( `${ widgetType }_${ label }.png`, { maxDiffPixels: 200, timeout: 10000 } );
 	}
 
 	async doHoverScreenshot( args:Omit<ScreenShot, 'device'> ) {
