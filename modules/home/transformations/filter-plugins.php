d5b286c55cfb394519facba6a1c4675657618049
Hein van Vlastuin <heinvanvlastuin@gmail.com>
2024-03-26 09:54:54 +0200

    PR Review.

--- modules/home/transformations/filter-plugins.php
+++ modules/home/transformations/filter-plugins.php
@@ -2,12 +2,21 @@
 namespace Elementor\Modules\Home\Transformations;
 
 use Elementor\Modules\Home\Transformations\Base\Transformations_Abstract;
+use Elementor\Utils;
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly
 }
 
 class Filter_Plugins extends Transformations_Abstract {
+
+	private array $installed_plugins;
+
+	public function __construct( $args ) {
+		parent::__construct( $args );
+
+		$this->installed_plugins = $this->get_installed_plugins();
+	}
 
 	public function transform( array $home_screen_data ): array {
 		$home_screen_data['add_ons']['repeater'] = $this->get_add_ons_installation_status( $home_screen_data['add_ons']['repeater'] );
@@ -28,7 +37,7 @@ class Filter_Plugins extends Transformations_Abstract {
 			$is_plugin = array_key_exists( 'file_path', $add_on );
 
 			if ( $is_plugin ) {
-				$is_installed_plugin = in_array( $add_on['file_path'], $this->get_installed_plugins() );
+				$is_installed_plugin = in_array( $add_on['file_path'], $this->installed_plugins );
 				$add_ons[ $index ]['is_installed'] = $is_installed_plugin;
 			}
 
