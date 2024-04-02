c72b65a1655a6774386a1da0a4db053e5ecf93d7
Rami Yushuvaev <ramiy@elementor.com>
2024-04-02 11:51:38 +0300

    Tweak: Hide icon position & spacing controls if there is icon but no text in Button widget

--- includes/widgets/traits/button-trait.php
+++ includes/widgets/traits/button-trait.php
@@ -148,7 +148,7 @@ trait Button_Trait {
 						'icon' => 'eicon-h-align-right',
 					],
 				],
-				'condition' => array_merge( $args['section_condition'], [ 'selected_icon[value]!' => '' ] ),
+				'condition' => array_merge( $args['section_condition'], [ 'selected_icon[value]!' => '', 'selected_icon[value]!' => '' ] ),
 			]
 		);
 
@@ -172,7 +172,7 @@ trait Button_Trait {
 				'selectors' => [
 					'{{WRAPPER}} .elementor-button .elementor-button-content-wrapper' => 'gap: {{SIZE}}{{UNIT}};',
 				],
-				'condition' => array_merge( $args['section_condition'], [ 'selected_icon[value]!' => '' ] ),
+				'condition' => array_merge( $args['section_condition'], [ 'selected_icon[value]!' => '', 'selected_icon[value]!' => '' ] ),
 			]
 		);
 
