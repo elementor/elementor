314d502a6641ecda9c49ed4f605766130c3c0ac4
Louis Wolmarans <louiswol94@gmail.com>
2024-04-03 15:00:24 +0200

    Update tests/phpunit/elementor/modules/home/transformations/test-filter-sidebar-upgrade_by_license.php

--- tests/phpunit/elementor/modules/home/transformations/test-filter-sidebar-upgrade_by_license.php
+++ tests/phpunit/elementor/modules/home/transformations/test-filter-sidebar-upgrade_by_license.php
@@ -30,6 +30,7 @@ class Test_Filter_Sidebar_Upgrade_By_License extends PHPUnit_TestCase {
 		// Act
 		$transformed_data = $transformation->transform( $original_data );
 		$expected_data = $this->mock_home_screen_data_transformed_pro();
+
 		// Assert
 		$this->assertEquals( $transformed_data, $expected_data );
 	}
