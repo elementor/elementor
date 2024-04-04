8560d56274f3045fb774475a8194f09005120d01
Louis Wolmarans <louiswol94@gmail.com>
2024-04-04 11:19:08 +0200

    Update modules/home/transformations/filter-get-started-by-license.php

--- modules/home/transformations/filter-get-started-by-license.php
+++ modules/home/transformations/filter-get-started-by-license.php
@@ -20,6 +20,7 @@ class Filter_Get_Started_By_License extends Transformations_Abstract {
 	private function valid_item( $item ) {
 		$has_pro_json_not_free = $this->has_pro && 'pro' === $item['license'][0];
 		$is_not_pro_json_not_pro = ! $this->has_pro && 'free' === $item['license'][0];
+
 		return $has_pro_json_not_free || $is_not_pro_json_not_pro;
 	}
 
