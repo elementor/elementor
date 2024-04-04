ed129e9ae1271c8f45938656f6babc3d9772e776
Batsirai Muchareva <batsiraim@elementor.red>
2024-04-04 10:33:03 +0200

    Auto stash before cherry pick of "Add new playwright screenshots"

--- includes/editor-templates/panel-elements.php
+++ includes/editor-templates/panel-elements.php
@@ -44,7 +44,8 @@ if ( ! defined( 'ABSPATH' ) ) {
 		<img class="elementor-nerd-box-icon" src="<?php echo ELEMENTOR_ASSETS_URL . 'images/unlock-sticky.svg'; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>" loading="lazy" alt="<?php echo esc_attr__( 'Upgrade', 'elementor' ); ?>"/>
 		<div class="elementor-get-pro-sticky-message">
 			<?php echo esc_html( $promotion_data_sticky['message'] ); ?>
-			<a target="_blank" href="<?php echo esc_url( $promotion_data_sticky['url'] ); ?>"><?php echo esc_html( $promotion_data_sticky['button_text'] ); ?></a></div>
+			<a target="_blank" href="<?php echo esc_url( $promotion_data_sticky['url'] ); ?>"><?php echo esc_html( $promotion_data_sticky['button_text'] ); ?></a>
+		</div>
 	</div>
 	<?php endif; ?>
 </script>
