71d5b595c241c0eeb9c364efeb4a644a1aab39c1
Dennis Nerush <dennisn@elementor.com>
2024-04-07 11:33:08 +0300

    text

--- tests/playwright/pages/editor-page.ts
+++ tests/playwright/pages/editor-page.ts
@@ -96,10 +96,10 @@ export default class EditorPage extends BasePage {
 	}
 
 	async hideAiPromotion() {
-		// Await this.page.evaluate( () => {
-		// 	// @ts-expect-error Property 'EDITOR_SESSION_ID' exists on window from ai.
-		// 	window.sessionStorage.setItem( 'ai_promotion_introduction_editor_session_key', window.EDITOR_SESSION_ID );
-		// } );
+		await this.page.evaluate( () => {
+			// @ts-expect-error Property 'EDITOR_SESSION_ID' exists on window from ai.
+			window.sessionStorage.setItem( 'ai_promotion_introduction_editor_session_key', window.EDITOR_SESSION_ID );
+		} );
 	}
 
 	async ensurePanelLoaded() {
