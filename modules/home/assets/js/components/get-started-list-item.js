ef2dbbcad5fb2914840237852970b00e10e56501
elchugreeva <elenac@elementor.com>
2024-04-09 11:47:47 +0300

    Internal: Home Screen remove Get Started section dependency from Sidebar [ED-14300-3.21]

--- modules/home/assets/js/components/get-started-list-item.js
+++ modules/home/assets/js/components/get-started-list-item.js
@@ -5,8 +5,7 @@ import Box from '@elementor/ui/Box';
 
 import CreateNewPageDialog from './create-new-page-dialog';
 
-const GetStartedListItem = ( { item, image, hasSidebarUpgrade } ) => {
-	// Will be replaced with a backend solution
+const GetStartedListItem = ( { item, image } ) => {
 	const adminUrl = elementorAppConfig.admin_url;
 	const url = item.is_relative_url ? adminUrl + item.url : item.url;
 
@@ -26,10 +25,7 @@ const GetStartedListItem = ( { item, image, hasSidebarUpgrade } ) => {
 			<Box component="img" src={ image }></Box>
 			<Box>
 				<ListItemText primary={ item.title } primaryTypographyProps={ { variant: 'subtitle1' } } sx={ { my: 0 } } />
-				{ hasSidebarUpgrade
-					? <Link variant="body2" color="text.tertiary" underline="hover" href={ url } target="_blank" onClick={ handleLinkClick }>{ item.title_small }</Link>
-					: <Link variant="body2" color={ ( item.title_small_color ) ? item.title_small_color : 'text.tertiary' } underline="hover" href={ url } target="_blank" onClick={ handleLinkClick }>{ ( item.title_small_color ) ? 'Upgrade' : item.title_small }</Link>
-				}
+				<Link variant="body2" color={ ( item.title_small_color ) ? item.title_small_color : 'text.tertiary' } underline="hover" href={ url } target="_blank" onClick={ handleLinkClick }>{ item.title_small }</Link>
 			</Box>
 
 			{ item.new_page && <CreateNewPageDialog url={ url } isOpen={ isOpen } closedDialogCallback={ () => openDialog( false ) } /> }
@@ -48,6 +44,5 @@ GetStartedListItem.propTypes = {
 		is_relative_url: PropTypes.bool,
 		title_small_color: PropTypes.string,
 	} ).isRequired,
-	hasSidebarUpgrade: PropTypes.bool,
 	image: PropTypes.string,
 };
