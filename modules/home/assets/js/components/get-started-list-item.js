241743bbbe4f97a86fcfdf6c20cb0129e85ff90e
elchugreeva <elenac@elementor.com>
2024-04-09 11:36:48 +0300

    Internal: Home Screen extract elementorAppConfig to props [ED-14299]

--- modules/home/assets/js/components/get-started-list-item.js
+++ modules/home/assets/js/components/get-started-list-item.js
@@ -5,9 +5,7 @@ import Box from '@elementor/ui/Box';
 
 import CreateNewPageDialog from './create-new-page-dialog';
 
-const GetStartedListItem = ( { item, image, hasSidebarUpgrade } ) => {
-	// Will be replaced with a backend solution
-	const adminUrl = elementorAppConfig.admin_url;
+const GetStartedListItem = ( { item, image, adminUrl } ) => {
 	const url = item.is_relative_url ? adminUrl + item.url : item.url;
 
 	const [ isOpen, openDialog ] = React.useState( false );
@@ -26,10 +24,7 @@ const GetStartedListItem = ( { item, image, hasSidebarUpgrade } ) => {
 			<Box component="img" src={ image }></Box>
 			<Box>
 				<ListItemText primary={ item.title } primaryTypographyProps={ { variant: 'subtitle1' } } sx={ { my: 0 } } />
-				{ hasSidebarUpgrade
-					? <Link variant="body2" color="text.tertiary" underline="hover" href={ url } target="_blank" onClick={ handleLinkClick }>{ item.title_small }</Link>
-					: <Link variant="body2" color={ ( item.title_small_color ) ? item.title_small_color : 'text.tertiary' } underline="hover" href={ url } target="_blank" onClick={ handleLinkClick }>{ ( item.title_small_color ) ? 'Upgrade' : item.title_small }</Link>
-				}
+				<Link variant="body2" color={ ( item.title_small_color ) ? item.title_small_color : 'text.tertiary' } underline="hover" href={ url } target="_blank" onClick={ handleLinkClick }>{ item.title_small }</Link>
 			</Box>
 
 			{ item.new_page && <CreateNewPageDialog url={ url } isOpen={ isOpen } closedDialogCallback={ () => openDialog( false ) } /> }
@@ -48,6 +43,6 @@ GetStartedListItem.propTypes = {
 		is_relative_url: PropTypes.bool,
 		title_small_color: PropTypes.string,
 	} ).isRequired,
-	hasSidebarUpgrade: PropTypes.bool,
+	adminUrl: PropTypes.string.isRequired,
 	image: PropTypes.string,
 };
