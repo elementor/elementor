c7220a69be5464216306e719b0f81f9142353281
Hein van Vlastuin <heinvanvlastuin@gmail.com>
2024-04-12 15:33:21 +0200

    Add query args.

--- core/isolation/plugin-status-adapter.php
+++ core/isolation/plugin-status-adapter.php
@@ -17,11 +17,27 @@ class Plugin_Status_Adapter implements Plugin_Status_Adapter_Interface {
 
 	public function get_install_plugin_url( $plugin_path ): string {
 		$slug = dirname( $plugin_path );
-		$admin_url = $this->wordpress_adapter->self_admin_url( 'update.php?action=install-plugin&plugin=' . $slug );
+		$admin_base_url = $this->wordpress_adapter->self_admin_url( 'update.php' );
+
+		$admin_url = add_query_arg( [
+			'action' => 'install-plugin',
+			'plugin' => $slug,
+		], $admin_base_url );
+
 		return $this->wordpress_adapter->wp_nonce_url( $admin_url, 'install-plugin_' . $slug );
 	}
 
 	public function get_activate_plugin_url( $plugin_path ): string {
-		return $this->wordpress_adapter->wp_nonce_url( 'plugins.php?action=activate&amp;plugin=' . $plugin_path . '&amp;plugin_status=all&amp;paged=1&amp;s', 'activate-plugin_' . $plugin_path );
+		$admin_base_url = $this->wordpress_adapter->self_admin_url( 'plugins.php' );
+
+		$admin_url = add_query_arg( [
+			'action' => 'activate',
+			'plugin' => $plugin_path,
+			'plugin_status' => 'all',
+			'paged' => 1,
+			's' => '',
+		], $admin_base_url );
+
+		return $this->wordpress_adapter->wp_nonce_url( $admin_url, 'activate-plugin_' . $plugin_path );
 	}
 }
