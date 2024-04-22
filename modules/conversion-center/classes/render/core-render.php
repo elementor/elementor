2b3ae382f1ad17e5fe0894efa22204b7fc6313c3
Will Howat-Comandre <willhowat@gmail.com>
2024-04-22 14:57:34 +0200

    Fix: Conversion center - link in bio - removed check for URL on icons before rendering

--- modules/conversion-center/classes/render/core-render.php
+++ modules/conversion-center/classes/render/core-render.php
@@ -115,10 +115,6 @@ class Core_Render extends Render_Base {
 			foreach ( $icons_value as $key => $icon ) {
 
 				$formatted_link = $this->get_formatted_link_for_icon( $icon );
-				// Bail if no link
-				if ( empty( $formatted_link ) ) {
-					break;
-				}
 
 				$this->widget->add_render_attribute( 'icon-' . $key, [
 					'class' => 'e-link-in-bio__icon has-size-' . $icons_props_size,
