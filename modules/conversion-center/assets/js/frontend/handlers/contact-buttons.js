cedea0b994e3256f3fea94934e6fee8488c3ffe9
Maggie Serino <maggie.serino@10up.com>
2024-04-25 12:07:02 +0200

    Tweak: Conversion Center - Contact Buttons - remove $ reference to jQuery

--- modules/conversion-center/assets/js/frontend/handlers/contact-buttons.js
+++ modules/conversion-center/assets/js/frontend/handlers/contact-buttons.js
@@ -14,22 +14,22 @@ export default class ContactButtonsHandler extends Base {
 	getDefaultElements() {
 		const selectors = this.getSettings( 'selectors' );
 		return {
-			$content: this.$element[ 0 ].querySelector( selectors.content ),
-			$chatButton: this.$element[ 0 ].querySelector( selectors.chatButton ),
-			$closeButton: this.$element[ 0 ].querySelector( selectors.closeButton ),
+			content: this.$element[ 0 ].querySelector( selectors.content ),
+			chatButton: this.$element[ 0 ].querySelector( selectors.chatButton ),
+			closeButton: this.$element[ 0 ].querySelector( selectors.closeButton ),
 		};
 	}
 
 	bindEvents() {
-		this.elements.$closeButton.addEventListener( 'click', this.onCloseButtonClick.bind( this ) );
-		this.elements.$chatButton.addEventListener( 'click', this.onChatButtonClick.bind( this ) );
+		this.elements.closeButton.addEventListener( 'click', this.onCloseButtonClick.bind( this ) );
+		this.elements.chatButton.addEventListener( 'click', this.onChatButtonClick.bind( this ) );
 	}
 
 	onChatButtonClick() {
-		this.elements.$content.classList.toggle( 'is-visible' );
+		this.elements.content.classList.toggle( 'is-visible' );
 	}
 
 	onCloseButtonClick() {
-		this.elements.$content.classList.remove( 'is-visible' );
+		this.elements.content.classList.remove( 'is-visible' );
 	}
 }
