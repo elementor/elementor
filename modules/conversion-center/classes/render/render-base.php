5f74f9608dd77b0cb5bac1ad42bb542478d55ad4
Nicola Peluchetti <nicola.peluchetti@gmail.com>
2024-05-02 11:45:39 +0200

    small fixes

--- modules/conversion-center/classes/render/render-base.php
+++ modules/conversion-center/classes/render/render-base.php
@@ -216,21 +216,25 @@ abstract class Render_Base {
 				<?php if ( $has_bio_heading ) {
 					$this->widget->add_render_attribute( 'heading', 'class', 'e-link-in-bio__heading' );
 					$bio_heading_output = sprintf( '<%1$s %2$s>%3$s</%1$s>', Utils::validate_html_tag( $bio_heading_props_tag ), $this->widget->get_render_attribute_string( 'heading' ), esc_html( $bio_heading_value ) );
+					// Escaped above
 					Utils::print_unescaped_internal_string( $bio_heading_output );
 				} ?>
 				<?php if ( $has_bio_title ) {
 					$this->widget->add_render_attribute( 'title', 'class', 'e-link-in-bio__title' );
 					$bio_title_output = sprintf( '<%1$s %2$s>%3$s</%1$s>', Utils::validate_html_tag( $bio_title_props_tag ), $this->widget->get_render_attribute_string( 'title' ), esc_html( $bio_title_value ) );
+					// Escaped above
 					Utils::print_unescaped_internal_string( $bio_title_output );
 				} ?>
 				<?php if ( $has_bio_about_heading ) {
 					$this->widget->add_render_attribute( 'about-heading', 'class', 'e-link-in-bio__about-heading' );
 					$bio_about_heading_output = sprintf( '<%1$s %2$s>%3$s</%1$s>', Utils::validate_html_tag( $bio_about_heading_props_tag ), $this->widget->get_render_attribute_string( 'about-heading' ), esc_html( $bio_about_heading_value ) );
+					// Escaped above
 					Utils::print_unescaped_internal_string( $bio_about_heading_output );
 				} ?>
 				<?php if ( $has_bio_description ) {
 					$this->widget->add_render_attribute( 'description', 'class', 'e-link-in-bio__description' );
 					$bio_description_output = sprintf( '<p %1$s>%2$s</p>', $this->widget->get_render_attribute_string( 'description' ), esc_html( $bio_description_value ) );
+					// Escaped above
 					Utils::print_unescaped_internal_string( $bio_description_output );
 				} ?>
 			</div>
@@ -258,11 +262,13 @@ abstract class Render_Base {
 				<?php if ( $has_bio_about_heading ) {
 					$this->widget->add_render_attribute( 'about-heading', 'class', 'e-link-in-bio__about-heading' );
 					$bio_about_heading_output = sprintf( '<%1$s %2$s>%3$s</%1$s>', Utils::validate_html_tag( $bio_about_heading_props_tag ), $this->widget->get_render_attribute_string( 'about-heading' ), esc_html( $bio_about_heading_value ) );
+					// Escaped above
 					Utils::print_unescaped_internal_string( $bio_about_heading_output );
 				} ?>
 				<?php if ( $has_bio_description ) {
 					$this->widget->add_render_attribute( 'description', 'class', 'e-link-in-bio__description' );
 					$bio_description_output = sprintf( '<p %1$s>%2$s</p>', $this->widget->get_render_attribute_string( 'description' ), esc_html( $bio_description_value ) );
+					// Escaped above
 					Utils::print_unescaped_internal_string( $bio_description_output );
 				} ?>
 			</div>
@@ -283,10 +289,6 @@ abstract class Render_Base {
 			return [];
 		}
 
-		if ( $negative ) {
-			$file_name .= '-negative';
-		}
-
 		return [
 			'negative' => $negative,
 			'svg' => Utils::file_get_contents( $shape_path ),
@@ -302,7 +304,7 @@ abstract class Render_Base {
 		<div
 			class="elementor-shape elementor-shape-<?php echo esc_attr( $side ); ?>"
 			data-negative="<?php
-				Utils::print_unescaped_internal_string( $shape_divider['negative'] ? 'true' : 'false' );
+				echo esc_attr( $shape_divider['negative'] ? 'true' : 'false' );
 			?>"
 		>
 			<?php
