220836d5eb2a3e4ae2c6d7db97cb2d93a95871f2
elchugreeva <elenac@elementor.com>
2024-05-12 14:15:15 +0300

    Internal: Enable different top section content for Free/Pro Users on the Home screen [ED-14519]

---
+++ modules/home/transformations/filter-top-section-by-license.php
@@ -0,0 +1,45 @@
+<?php
+namespace elementor\modules\home\transformations;
+
+use Elementor\Modules\Home\Transformations\Base\Transformations_Abstract;
+use Elementor\Utils;
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
+
+class Filter_Top_Section_By_License extends Transformations_Abstract {
+	public bool $has_pro;
+
+	public function __construct( $args ) {
+		parent::__construct( $args );
+
+		$this->has_pro = Utils::has_pro();
+	}
+
+	private function valid_item( $item ) {
+		if( isset( $item['license'] ) ) {
+			$has_pro_json_not_free = $this->has_pro && 'pro' === $item['license'][0];
+			$is_not_pro_json_not_pro = ! $this->has_pro && 'free' === $item['license'][0];
+
+			return $has_pro_json_not_free || $is_not_pro_json_not_pro;
+		}
+	}
+
+	public function transform( array $home_screen_data ): array {
+		if ( ! is_array( $home_screen_data['top'] ) ) {
+			return $home_screen_data;
+		} else {
+			$new_top = [];
+
+			foreach ( $home_screen_data['top'] as $index => $item ) {
+				if ( $this->valid_item( $item ) ) {
+					$new_top[] = $item;
+				}
+			}
+
+			$home_screen_data['top'] = reset( $new_top );
+
+			return $home_screen_data;
+		}
+	}
+}
