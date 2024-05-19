58a297642286582f36e449ee98a269ba3e273743
Nicola Peluchetti <nicola.peluchetti@gmail.com>
2024-05-19 21:50:55 +0400

    incorrect usage notices

--- tests/phpunit/elementor/includes/managers/test-controls.php
+++ tests/phpunit/elementor/includes/managers/test-controls.php
@@ -59,7 +59,7 @@ class Test_Controls extends Elementor_Test_Base {
 
 	/**
 	 *
-	 * @expectedDeprecated Elementor\Controls_Manager::add_control_to_stack
+	 * @expectedIncorrectUsage Elementor\Controls_Manager::add_control_to_stack
 	 */
 	public function test_control_not_added_if_min_value_greather_than_default_items() {
 		$control_data = [
@@ -76,6 +76,8 @@ class Test_Controls extends Elementor_Test_Base {
 		], [] );
 
 		$added = Plugin::$instance->controls_manager->add_control_to_stack( $widget, 'repeater_test', $control_data );
+
+		print_r( $added  ) . "ADDED";
 
 		$this->assertEquals( false, $added );
 	}
