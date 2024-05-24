90c06352252d791118b527e1ed05746498efa212
Nikita Murzin <mykytamu@elementor.com>
2024-05-24 07:18:02 +0200

    Internal: Clean up of old staff [ED-14630]

--- modules/editor-events/module.php
+++ modules/editor-events/module.php
@@ -7,14 +7,13 @@ use Elementor\Core\Common\Modules\Connect\Apps\Base_App;
 use Elementor\Core\Experiments\Manager as Experiments_Manager;
 use Elementor\Utils;
 use Elementor\Plugin;
+use Elementor\Tracker;
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 
 class Module extends BaseModule {
-
-	const ELEMENTOR_EDITOR_EVENTS_DATA_SYSTEM_URL = 'https://my.elementor.com/api/v1/editor-events';
 
 	const EXPERIMENT_NAME = 'editor_events';
 
@@ -29,7 +28,8 @@ class Module extends BaseModule {
 	}
 
 	public static function get_editor_events_config() {
-		$can_send_events = false;
+		$can_send_events = Tracker::is_allow_track() &&
+			Plugin::$instance->experiments->is_feature_active( self::EXPERIMENT_NAME );
 
 		$settings = [
 			'can_send_events' => $can_send_events,
@@ -41,10 +41,6 @@ class Module extends BaseModule {
 			'site_key' => get_option( Base_App::OPTION_CONNECT_SITE_KEY ),
 			'subscription_id' => null,
 		];
-
-		if ( $can_send_events ) {
-			$settings['data_system_url'] = self::ELEMENTOR_EDITOR_EVENTS_DATA_SYSTEM_URL;
-		}
 
 		return $settings;
 	}
