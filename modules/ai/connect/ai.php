3568045453241172b339771604bc950250f3ccfd
Naor Sabag <32329815+naorsabag@users.noreply.github.com>
2024-05-26 12:42:06 +0300

    New: UI for post excerpt generation [ED-14911] (#27468)
    ## PR Checklist
    <!-- 
    Please check if your PR fulfills the following requirements:
    **Filling out the template is required.** Any pull request that does not
    include enough information to be reviewed in a timely manner may be
    closed at the maintainers' discretion.
     -->
    - [x] The commit message follows our guidelines:
    https://github.com/elementor/elementor/blob/master/.github/CONTRIBUTING.md
    
    
    ## PR Type
    What kind of change does this PR introduce?
    <!-- Please check the one that applies to this PR using "x" with no
    spaces eg: [x]. -->
    - [ ] Bugfix
    - [x] Feature
    - [ ] Code style update (formatting, local variables)
    - [ ] Refactoring (no functional changes, no api changes)
    - [ ] Build related changes
    - [ ] CI related changes
    - [ ] Documentation content changes
    - [ ] Other... Please describe:
    
    ## Summary
    
    This PR can be summarized in the following changelog entry:
    
    * Add the ability to edit the excerpt with AI via gutenberg's editor
    
    ## Description
    An explanation of what is done in this PR
    
    * Add the ability to edit the excerpt with AI via gutenberg's editor
    
    ## Test instructions
    This PR can be tested by following these steps:
    
    *
    
    ## Quality assurance
    
    - [x] I have tested this code to the best of my abilities
    - [ ] I have added unittests to verify the code works as intended
    - [ ] Docs have been added / updated (for bug fixes / features)
    
    Fixes #
    
    ---------
    
    Co-authored-by: ElementorBot <48412871+elementorbot@users.noreply.github.com>

--- modules/ai/connect/ai.php
+++ modules/ai/connect/ai.php
@@ -197,6 +197,22 @@ class Ai extends Library {
 		);
 	}
 
+	public function get_excerpt( $prompt, $context, $request_ids ) {
+		$excerpt_length = apply_filters( 'excerpt_length', 55 );
+		return $this->ai_request(
+			'POST',
+			'text/get-excerpt',
+			[
+				'content' => $prompt,
+				'maxLength' => $excerpt_length,
+				'context' => wp_json_encode( $context ),
+				'ids' => $request_ids,
+				'api_version' => ELEMENTOR_VERSION,
+				'site_lang' => get_bloginfo( 'language' ),
+			]
+		);
+	}
+
 	/**
 	 * get_image_prompt_enhanced
 	 * @param $prompt
