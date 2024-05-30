8d9f21f88c5ada5b0e70bd67e198864596e3fbab
Ronen Boxer <145427686+ronenelementor@users.noreply.github.com>
2024-05-30 17:54:33 +0300

    Update get-changelog-from-change-txt.js

--- .github/scripts/get-changelog-from-change-txt.js
+++ .github/scripts/get-changelog-from-change-txt.js
@@ -16,8 +16,9 @@ for (let fileName of ['changelog', 'readme'] ) {
 			const changelogText = fs.readFileSync(`${fileName}.txt`, 'utf-8');
 			const data = marked.lexer(changelogText);
 			const headerIndex = data.findIndex((section, index) => {
-				if (index < 10) console.log(1, section.type, 2, section.text);
-				return section.type === 'paragraph' && section.text.match(new RegExp(`^=?\s*${VERSION}`, 'g'));
+				const text = section.text.match(/\d+\.d+\.\d+/'g')?.[0] || ''
+				if (index < 10) console.log(1, section.type, 2, section.text, 3, text);
+				return section.type === 'paragraph' && VERSION == text;
 			});
 			if (headerIndex === -1) {
 				console.error(`Failed to find version: ${VERSION} in ${fileName}.txt file`);
