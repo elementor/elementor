b23f21ac81a61638f6613fdbf2cab9ce952fe358
Ronen Boxer <ronenb@elementor.com>
2024-06-02 13:48:20 +0300

    Yotam's comments

--- .github/scripts/get-changelog-from-change-txt.js
+++ .github/scripts/get-changelog-from-change-txt.js
@@ -10,23 +10,22 @@ if (!VERSION) {
 	return;
 }
 
-for (let fileName of ['changelog', 'readme'] ) {
+for (let fileName of ['changelog.txt', 'readme.txt'] ) {
 	(async () => {
 		try {
-			const changelogText = fs.readFileSync(`${fileName}.txt`, 'utf-8');
+			const changelogText = fs.readFileSync(`${fileName}`, 'utf-8');
 			const data = marked.lexer(changelogText);
 			const headerIndex = data.findIndex((section) => {
 				const text = section?.text?.match(/\d+\.\d+\.\d+/g)?.[0] || '';
 				return section.type === 'paragraph' && VERSION == text;
 			});
 			if (headerIndex === -1) {
-				console.error(`Change-log for release ${version} not found in ${filename}.txt`);
+				console.error(`Change log for release ${version} not found in ${filename}`);
 				process.exit(1);
 				return;
 			}
 			const versionLog = data[headerIndex + 1].raw;
-			fs.writeFileSync(`temp-${fileName}.txt`, versionLog);
-			console.log('success: ', fileName);
+			fs.writeFileSync(`temp-${fileName}`, versionLog);
 		} catch (err) {
 			console.error(err)
 			process.exit(1);
