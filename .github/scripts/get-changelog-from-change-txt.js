6c9185454c7988317fb628f42a011af191005221
Ronen Boxer <ronenb@elementor.com>
2024-06-02 14:45:30 +0300

    Wip

--- .github/scripts/get-changelog-from-change-txt.js
+++ .github/scripts/get-changelog-from-change-txt.js
@@ -11,24 +11,22 @@ if (!VERSION) {
 }
 
 for (let fileName of ['changelog.txt', 'readme.txt'] ) {
-	(async () => {
-		try {
-			const changelogText = fs.readFileSync(`${fileName}`, 'utf-8');
-			const data = marked.lexer(changelogText);
-			const headerIndex = data.findIndex((section) => {
-				const text = section?.text?.match(/\d+\.\d+\.\d+/g)?.[0] || '';
-				return section.type === 'paragraph' && VERSION == text;
-			});
-			if (headerIndex === -1) {
-				console.error(`Change log for release ${version} not found in ${filename}`);
-				process.exit(1);
-				return;
-			}
-			const versionLog = data[headerIndex + 1].raw;
-			fs.writeFileSync(`temp-${fileName}`, versionLog);
-		} catch (err) {
-			console.error(err)
+	try {
+		const changelogText = fs.readFileSync(`${fileName}`, 'utf-8');
+		const data = marked.lexer(changelogText);
+		const headerIndex = data.findIndex((section) => {
+			const text = section?.text?.match(/\d+\.\d+\.\d+/g)?.[0] || '';
+			return section.type === 'paragraph' && VERSION == text;
+		});
+		if (headerIndex === -1) {
+			console.error(`Change log for release ${version} not found in ${filename}`);
 			process.exit(1);
+			return;
 		}
-	})();
+		const versionLog = data[headerIndex + 1].raw;
+		fs.writeFileSync(`temp-${fileName}`, versionLog);
+	} catch (err) {
+		console.error(err)
+		process.exit(1);
+	}
 }
