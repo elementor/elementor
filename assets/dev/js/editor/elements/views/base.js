a72d8f286f2e77451c08e763f239512c418e6815
David Seveloff <davids@elementor.com>
2024-06-04 06:41:49 +0300

    Review comment

--- assets/dev/js/editor/elements/views/base.js
+++ assets/dev/js/editor/elements/views/base.js
@@ -702,8 +702,10 @@ BaseElementView = BaseContainer.extend( {
 	 *
 	 * By adding the following example attributes inside the widget the element innerHTML will be linked to the 'testimonial_content' setting value.
 	 *
+	 *
 	 * Current Limitation:
 	 * Not working with dynamics, will required full re-render.
+	 * UPDATE: Support for dynamics has experimentally been added in v3.23
 	 */
 	linkDataBindings() {
 		/**
