0a8b554385b34d6da12d34fc711eb252c3ce5c84
Hein van Vlastuin <heinvanvlastuin@gmail.com>
2024-06-04 14:30:01 +0200

    Updating condition.

--- assets/dev/js/frontend/elements-handlers-manager.js
+++ assets/dev/js/frontend/elements-handlers-manager.js
@@ -162,7 +162,7 @@ module.exports = function( $ ) {
 	};
 
 	this.runReadyTrigger = function( scope ) {
-		const isDelayChildHandlers = 0 !== scope.closest( '[data-delay-child-handlers="true"]' ).length;
+		const isDelayChildHandlers = !! scope.closest( '[data-delay-child-handlers="true"]' ) && 0 !== scope.closest( '[data-delay-child-handlers="true"]' ).length;
 
 		if ( elementorFrontend.config.is_static || isDelayChildHandlers ) {
 			return;
