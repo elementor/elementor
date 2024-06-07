7499d719c288c447cf4c7f0c90e56d3d72dd306b
Louis Wolmarans <louiswol94@gmail.com>
2024-06-07 10:39:29 +0200

    WIP

--- tests/playwright/sanity/modules/nested-tabs/nested-tabs.test.ts
+++ tests/playwright/sanity/modules/nested-tabs/nested-tabs.test.ts
@@ -65,9 +65,6 @@ test.describe( 'Nested Tabs tests @nested-tabs', () => {
 				buttonBelow = frame.locator( '.elementor-button >> nth=3' );
 
 			// Assert.
-			await frame.locator( '.page-header' ).click();
-
-			await page.keyboard.press( 'Tab' );
 			await expect( tabTitleOne ).toBeFocused();
 			await expect( tabTitleOne ).toHaveAttribute( 'aria-selected', 'true' );
 			await page.keyboard.press( 'Tab' );
