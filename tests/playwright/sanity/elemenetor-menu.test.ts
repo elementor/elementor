44f8897bc2ebdf9e9c6558a4920c51b475919ee1
Tzvi Rabinovitch <tzvir@elementor.com>
2024-06-10 17:45:12 +0300

    WIP

--- tests/playwright/sanity/elemenetor-menu.test.ts
+++ tests/playwright/sanity/elemenetor-menu.test.ts
@@ -2,14 +2,13 @@ import { test, expect } from '@playwright/test';
 import WpAdminPage from '../pages/wp-admin-page';
 
 test.describe( 'General Settings', () => {
-	test( 'Is hidden if home is active',
-		async ( { page }, testInfo ) => {
-			// Arrange.
-			const wpAdmin = new WpAdminPage( page, testInfo );
-			await wpAdmin.setExperiments( { home_screen: true } );
-			await wpAdmin.page.locator( '.toplevel_page_elementor' ).click();
-			expect( await wpAdmin.page.getByText( `Getting Started` ).count() ).toEqual( 0 );
-		} );
+	test( 'Is hidden if home is active', async ( { page }, testInfo ) => {
+		// Arrange.
+		const wpAdmin = new WpAdminPage( page, testInfo );
+		await wpAdmin.setExperiments( { home_screen: true } );
+		await wpAdmin.page.locator( '.toplevel_page_elementor' ).click();
+		expect( await wpAdmin.page.getByText( `Getting Started` ).count() ).toEqual( 0 );
+	} );
 
 	test( 'Is visible if home is not active', async ( { page }, testInfo ) => {
 		// Arrange.
