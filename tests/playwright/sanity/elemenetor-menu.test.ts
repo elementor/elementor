f0f964d4774f7efc914fe097f4629689b974b713
Tzvi Rabinovitch <tzvir@elementor.com>
2024-06-11 10:26:24 +0300

    wip

--- tests/playwright/sanity/elemenetor-menu.test.ts
+++ tests/playwright/sanity/elemenetor-menu.test.ts
@@ -5,7 +5,8 @@ test.describe( 'General Settings', () => {
 	test( 'Is hidden if home is active (default for plugin users)', async ( { page }, testInfo ) => {
 		// Arrange.
 		const wpAdmin = new WpAdminPage( page, testInfo );
-		await wpAdmin.page.locator( 'li .toplevel_page_elementor' ).click();
+		await wpAdmin.gotoDashboard();
+		await wpAdmin.page.locator( 'li .toplevel_page_elementor').click();
 		expect( await wpAdmin.page.getByText( `Getting Started` ).count() ).toEqual( 0 );
 		await wpAdmin.page.goto( '/wp-admin/admin.php?page=elementor-getting-started' );
 		expect( await wpAdmin.page.locator( '.e-getting-started' ).count() ).toEqual( 1 );
@@ -15,6 +16,7 @@ test.describe( 'General Settings', () => {
 		// Arrange.
 		const wpAdmin = new WpAdminPage( page, testInfo );
 		await wpAdmin.setExperiments( { home_screen: false } );
+		await wpAdmin.gotoDashboard();
 		await wpAdmin.page.locator( 'li .toplevel_page_elementor' ).click();
 		expect( await wpAdmin.page.getByText( 'Getting Started' ).count() ).toEqual( 0 );
 		await wpAdmin.setExperiments( { home_screen: true } );
