3f671ad4cc334e7cfde1f7ed9e0d1e27aa44ee80
Tzvi Rabinovitch <tzvir@elementor.com>
2024-06-11 09:14:33 +0300

    WIP

--- tests/playwright/sanity/elemenetor-menu.test.ts
+++ tests/playwright/sanity/elemenetor-menu.test.ts
@@ -2,21 +2,21 @@ import { test, expect } from '@playwright/test';
 import WpAdminPage from '../pages/wp-admin-page';
 
 test.describe( 'General Settings', () => {
-	test( 'Is hidden if home is active', async ( { page }, testInfo ) => {
+	test( 'Is hidden if home is active (default for plugin users)', async ( { page }, testInfo ) => {
 		// Arrange.
 		const wpAdmin = new WpAdminPage( page, testInfo );
-		await wpAdmin.setExperiments( { home_screen: true } );
 		await wpAdmin.page.locator( 'li .toplevel_page_elementor' ).click();
 		expect( await wpAdmin.page.getByText( `Getting Started` ).count() ).toEqual( 0 );
 		await wpAdmin.page.goto( '/wp-admin/admin.php?page=elementor-getting-started' );
 		expect( await wpAdmin.page.locator( '.e-getting-started' ).count() ).toEqual( 1 );
 	} );
 
-	test( 'Is visible if home is not active', async ( { page }, testInfo ) => {
+	test( 'Is visible if home is not active (default for hosting users)', async ( { page }, testInfo ) => {
 		// Arrange.
 		const wpAdmin = new WpAdminPage( page, testInfo );
 		await wpAdmin.setExperiments( { home_screen: false } );
 		await wpAdmin.page.locator( 'li .toplevel_page_elementor' ).click();
 		expect( await wpAdmin.page.getByText( 'Getting Started' ).count() ).toEqual( 0 );
+		await wpAdmin.setExperiments( { home_screen: true } );
 	} );
 } );
