80d71be5655195ee6afb751feec69c18f500ac8d
Yotam Shapira <yotams@elementor.com>
2024-06-13 22:42:46 +0300

    add baseURL to config

--- tests/elements-regression/playwright.config.ts
+++ tests/elements-regression/playwright.config.ts
@@ -26,7 +26,7 @@ export default defineConfig( {
 		trace: 'retain-on-failure',
 		video: process.env.CI ? 'retain-on-failure' : 'off',
 		viewport: { width: 1920, height: 1080 },
-		baseURL: process.env.ELEMENTS_REGRESSION_BASE_URL || 'http://localhost:8888',
+		baseURL: process.env.BASE_URL || ( 1 === Number( process.env.TEST_PARALLEL_INDEX ) ) ? 'http://localhost:8889' : 'http://localhost:8888',
 		// storageState: `./storageState-${ process.env.TEST_PARALLEL_INDEX }.json`,
 	},
 } );
