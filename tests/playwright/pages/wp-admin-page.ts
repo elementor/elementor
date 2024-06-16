228e7c717e60c5f08d999a06adf3d4977d164d2b
Tzvi Rabinovitch <tzvir@elementor.com>
2024-06-16 10:57:57 +0300

    lint

--- tests/playwright/pages/wp-admin-page.ts
+++ tests/playwright/pages/wp-admin-page.ts
@@ -178,7 +178,7 @@ export default class WpAdminPage extends BasePage {
 	 *
 	 * TODO: The testing environment isn't clean between tests - Use with caution!
 	 *
-	 * @param {Object} experiments - Experiments settings ( `{ experiment_id: true / false }` );
+	 * @param {Object}            experiments - Experiments settings ( `{ experiment_id: true / false }` );
 	 *
 	 * @param {(boolean|string)=} oldUrl
 	 * @return {Promise<void>}
