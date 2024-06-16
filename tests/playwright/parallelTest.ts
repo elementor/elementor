5963ec52ff68cec78d32d84b457f35ce2f9de321
Yotam Shapira <yotams@elementor.com>
2024-06-16 12:32:41 +0300

    refactor parallelTest

--- tests/playwright/parallelTest.ts
+++ tests/playwright/parallelTest.ts
@@ -1,10 +1,27 @@
-import { request, test as baseTest } from '@playwright/test';
+import { APIRequest, request, test as baseTest } from '@playwright/test';
 import fs from 'fs';
 import path from 'path';
-import { createApiContext, createDefaultMedia, loginApi } from './assets/api-requests';
+import { createApiContext, createDefaultMedia } from './assets/api-requests';
 import { WindowType } from './types/types';
 
-export * from '@playwright/test';
+export async function loginApi( apiRequest: APIRequest, user: string, pw: string, url: string, storageStatePath: string ) {
+	// Important: make sure we authenticate in a clean environment by unsetting storage state.
+	const context = await apiRequest.newContext( { storageState: undefined } );
+
+	await context.post( `${ url }/wp-login.php`, {
+		form: {
+			log: user,
+			pwd: pw,
+			'wp-submit': 'Log In',
+			redirect_to: `${ url }/wp-admin/`,
+			testcookie: '1',
+		},
+		// maxBodyLength: Infinity,
+	} );
+	await context.storageState( { path: storageStatePath } );
+	await context.dispose();
+}
+
 export const parallelTest = baseTest.extend< NonNullable<unknown>, { workerStorageState: string, workerBaseURL: string }>( {
 	// Use the same storage state for all tests in this worker.
 	baseURL: ( { workerBaseURL }, use ) => use( workerBaseURL ),
@@ -27,17 +44,18 @@ export const parallelTest = baseTest.extend< NonNullable<unknown>, { workerStora
 			return;
 		}
 
-		// Important: make sure we authenticate in a clean environment by unsetting storage state.
+		// Send authentication request.
 		const baseURL = 1 === testInfo.parallelIndex ? 'http://localhost:8889' : 'http://localhost:8888';
-		const cookies = await loginApi(
+		await loginApi(
+			request,
 			process.env.USERNAME || 'admin',
 			process.env.PASSWORD || 'password',
 			baseURL,
+			fileName,
 		);
 		const page = await browser.newPage( { storageState: undefined } );
-		await page.context().addCookies( cookies );
 		await page.goto( `${ baseURL }/wp-admin` );
-		// Save the nonce and storage state in environment variables, to allow use them when creating the API context.
+		// Save the nonce in an environment variable, to allow use them when creating the API context.
 		const storageState = await page.context().storageState( { path: fileName } );
 
 		let window: WindowType;
