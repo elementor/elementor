17ffb479837e02f8f57bcbac96a470bdac68f9a5
Yotam Shapira <yotams@elementor.com>
2024-06-17 23:42:49 +0300

    global-setup - reuse nonce function

--- tests/elements-regression/global-setup.ts
+++ tests/elements-regression/global-setup.ts
@@ -1,32 +1,16 @@
 import { request, type FullConfig } from '@playwright/test';
 import ApiRequests from '../playwright/assets/api-requests';
 import path from 'path';
-import { login } from '../playwright/wp-authentication';
+import { fetchNonce, login } from '../playwright/wp-authentication';
 
 async function globalSetup( config: FullConfig ) {
 	const { baseURL } = config.projects[ 0 ].use;
 
-	const context = await login( request, process.env.USERNAME || 'admin', process.env.PASSWORD || 'password', baseURL );
-
-	// Save the nonce and storage state in environment variables, to allow use them when creating the API context.
-	const response = await context.get( `${ baseURL }/wp-admin/post-new.php` );
-
-	if ( ! response.ok() ) {
-		throw new Error( `
-				Failed to fetch nonce: ${ response.status() }.
-				${ await response.text() }
-				${ response.url() }
-				TEST_PARALLEL_INDEX: ${ process.env.TEST_PARALLEL_INDEX }
-			` );
-	}
-	const pageText = await response.text();
-	const nonceMatch = pageText.match( /var wpApiSettings = .*;/ );
-	if ( ! nonceMatch ) {
-		throw new Error( `Nonce not found on the page:\n"${ pageText }"` );
-	}
-
-	let nonce = nonceMatch[ 0 ];
-	nonce = nonce.replace( /^.*"nonce":"([^"]*)".*$/, '$1' );
+	let context = await login( request, process.env.USERNAME || 'admin', process.env.PASSWORD || 'password', baseURL );
+	const storageState = await context.storageState();
+	await context.dispose();
+	context = await request.newContext( { storageState } );
+	const nonce = await fetchNonce( context, baseURL );
 
 	const imageIds = [];
 	const image1 = {
