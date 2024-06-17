379a38c66289ccf620eb0f617ea4ab375d04ad35
Omer Haim Israeli <omeri@elementor.com>
2024-06-17 11:34:22 +0300

    wip

--- includes/widgets/v2/base.php
+++ includes/widgets/v2/base.php
@@ -4,12 +4,4 @@ namespace Elementor;
 
 abstract class Widget_Base_V2 extends Widget_Base {
 	abstract public function get_v2_controls();
-
-	function register_transform_section($element_selector = '') {
-		return [];
-	}
-
-	//  public function get_tabs_controls() {
-	//      return [];
-	//  }
 }
