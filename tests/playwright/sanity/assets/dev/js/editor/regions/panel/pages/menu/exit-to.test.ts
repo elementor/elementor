0c02499f27a43951adacca7c5bb118706714fafa
Rami Yushuvaev <ramiy@elementor.com>
2024-06-19 18:28:35 +0300

    Internal: Update tests to support the Top Bar

--- tests/playwright/sanity/assets/dev/js/editor/regions/panel/pages/menu/exit-to.test.ts
+++ tests/playwright/sanity/assets/dev/js/editor/regions/panel/pages/menu/exit-to.test.ts
@@ -5,44 +5,41 @@ test( 'Exit to user preference sanity test', async ( { page }, testInfo ) => {
 	const wpAdmin = new WpAdminPage( page, testInfo );
 	const editor = await wpAdmin.openNewPage();
 	const hasTopBar = await editor.hasTopBar();
+	let exitHref = '';
 
 	if ( hasTopBar ) {
-		let exitLink;
-
 		// Exit to `dashboard`
 		await editor.openUserPreferencesPanel();
 		await editor.setSelectControlValue( 'exit_to', 'dashboard' );
 		await editor.clickTopBarItem( 'Elementor Logo' );
 		await page.waitForTimeout( 100 );
-		exitLink = await page.locator( 'body a', { hasText: 'Exit to WordPress' } ).getAttribute( 'href' );
+		exitHref = await page.locator( 'body a', { hasText: 'Exit to WordPress' } ).getAttribute( 'href' );
 		await page.press( 'body', 'Escape' );
-		expect( exitLink ).toContain( '/wp-admin/' );
+		expect( exitHref ).toContain( '/wp-admin/' );
 
 		// Exit to `this_post`
 		await editor.openUserPreferencesPanel();
 		await editor.setSelectControlValue( 'exit_to', 'this_post' );
 		await editor.clickTopBarItem( 'Elementor Logo' );
 		await page.waitForTimeout( 100 );
-		exitLink = await page.locator( 'body a', { hasText: 'Exit to WordPress' } ).getAttribute( 'href' );
+		exitHref = await page.locator( 'body a', { hasText: 'Exit to WordPress' } ).getAttribute( 'href' );
 		await page.press( 'body', 'Escape' );
-		expect( exitLink ).toContain( '/wp-admin/post.php?post=' );
+		expect( exitHref ).toContain( '/wp-admin/post.php?post=' );
 
 		// Exit to `all_posts`
 		await editor.openUserPreferencesPanel();
 		await editor.setSelectControlValue( 'exit_to', 'all_posts' );
 		await editor.clickTopBarItem( 'Elementor Logo' );
 		await page.waitForTimeout( 100 );
-		exitLink = await page.locator( 'body a', { hasText: 'Exit to WordPress' } ).getAttribute( 'href' );
+		exitHref = await page.locator( 'body a', { hasText: 'Exit to WordPress' } ).getAttribute( 'href' );
 		await page.press( 'body', 'Escape' );
-		expect( exitLink ).toContain( '/wp-admin/edit.php?post_type=' );
+		expect( exitHref ).toContain( '/wp-admin/edit.php?post_type=' );
 	} else {
 		await editor.openMenuPanel();
 		await editor.page.click( 'text=Exit' );
 		await editor.page.click( 'a:has-text("User Preferences")' );
 		await editor.openMenuPanel();
-
 		const exit = page.locator( '.elementor-panel-menu-item-exit >> a' );
-		let exitHref = '';
 
 		// Exit to `dashboard`
 		await editor.page.click( '.elementor-panel-menu-item-editor-preferences' );
