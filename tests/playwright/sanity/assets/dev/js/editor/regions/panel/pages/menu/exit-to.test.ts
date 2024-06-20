3d457b65ea63ff20ba75f7451847a31d6232f5d6
Yotam Shapira <yotams@elementor.com>
2024-06-20 09:13:18 +0300

    fix linting issue

--- tests/playwright/sanity/assets/dev/js/editor/regions/panel/pages/menu/exit-to.test.ts
+++ tests/playwright/sanity/assets/dev/js/editor/regions/panel/pages/menu/exit-to.test.ts
@@ -1,4 +1,4 @@
-import { expect, type Page } from '@playwright/test';
+import { expect } from '@playwright/test';
 import { parallelTest as test } from '../../../../../../../../../parallelTest';
 import WpAdminPage from '../../../../../../../../../pages/wp-admin-page';
 
