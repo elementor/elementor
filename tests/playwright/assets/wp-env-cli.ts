4410f751df890cdb40024f0b40c01598d4fb463a
Yotam Shapira <yotams@elementor.com>
2024-06-23 14:30:54 +0300

    debuggability

--- tests/playwright/assets/wp-env-cli.ts
+++ tests/playwright/assets/wp-env-cli.ts
@@ -5,6 +5,7 @@ export const wpEnvCli = ( command: string ) => {
 	const cliCommand = ( 1 === Number( process.env.TEST_PARALLEL_INDEX ) ) ? 'tests-cli' : 'cli';
 	const cli = `npx wp-env run ${ cliCommand } `;
 	const logs = execSync( `${ cli }${ command }`, { encoding: 'utf-8' } );
+	// Print log for debuggability
 	// eslint-disable-next-line no-console
 	console.log( logs );
 	if ( process.env.CI ) {
