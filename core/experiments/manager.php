f9d2cedc7c69a993630b0eb2e2189c468ffdcf90
David Seveloff <davids@elementor.com>
2024-06-23 14:51:50 +0300

    test inactive to see if NC test fails

--- core/experiments/manager.php
+++ core/experiments/manager.php
@@ -442,7 +442,7 @@ class Manager extends Base_Object {
 			'description' => esc_html__( 'Improve the performance of the Nested widgets.', 'elementor' ),
 			static::TYPE_HIDDEN => true,
 			'release_status' => self::RELEASE_STATUS_DEV,
-			'default' => self::STATE_ACTIVE,
+			'default' => self::STATE_INACTIVE,
 		] );
 
 		$this->add_feature( [
