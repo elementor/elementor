aaefd97311f480cd5fad70a47b0ed27b8fd87708
DennisNerush <dennisn@elementor.com>
2024-06-30 12:52:07 +0300

    combine

--- modules/ai/assets/js/editor/ai-get-started-connect.js
+++ modules/ai/assets/js/editor/ai-get-started-connect.js
@@ -1,41 +1,35 @@
 import React from 'react';
-import Connect from './pages/connect';
-import GetStarted from './pages/get-started';
 import useUserInfo from './hooks/use-user-info';
 import Loader from './components/loader';
 import { onConnect } from './utils/editor-integration';
 import PropTypes from 'prop-types';
 import { WizardDialogWrapper } from './components/wizard-dialog-wrapper';
+import ConnectAndGetStarted from './pages/connect/connect-and-get-started';
+import { setGetStarted } from './api';
 
 export const AiGetStartedConnect = ( { onClose } ) => {
 	const { isLoading, isConnected, isGetStarted, connectUrl, fetchData } = useUserInfo();
 
 	if ( isLoading ) {
 		return (
-			<WizardDialogWrapper onClose={ onClose } >
+			<WizardDialogWrapper onClose={ onClose }>
 				<Loader BoxProps={ { sx: { px: 3 } } } />
 			</WizardDialogWrapper>
 		);
 	}
 
-	if ( ! isConnected ) {
+	if ( ! isConnected || ! isGetStarted ) {
 		return (
-			<WizardDialogWrapper onClose={ onClose } >
-				<Connect
+			<WizardDialogWrapper onClose={ onClose }>
+				<ConnectAndGetStarted
 					connectUrl={ connectUrl }
-					onSuccess={ ( data ) => {
+					onSuccess={ async ( data ) => {
 						onConnect( data );
+						fetchData();
+						await setGetStarted();
 						fetchData();
 					} }
 				/>
-			</WizardDialogWrapper>
-		);
-	}
-
-	if ( ! isGetStarted ) {
-		return (
-			<WizardDialogWrapper onClose={ onClose } >
-				<GetStarted onSuccess={ fetchData } />
 			</WizardDialogWrapper>
 		);
 	}
---
+++ modules/ai/assets/js/editor/pages/connect/connect-and-get-started.js
@@ -0,0 +1,74 @@
+import { useEffect, useRef, useState } from 'react';
+import { Stack, Button, Typography, Link, Checkbox } from '@elementor/ui';
+import { __ } from '@wordpress/i18n';
+import PropTypes from 'prop-types';
+import { AIIcon } from '@elementor/icons';
+
+const ConnectAndGetStarted = ( { connectUrl, onSuccess } ) => {
+	const approveButtonRef = useRef();
+	const [ isTermsChecked, setIsTermsChecked ] = useState( false );
+
+	useEffect( () => {
+		// On local dev (as a standalone app), the connect lib is not loaded.
+		if ( ! jQuery.fn.elementorConnect ) {
+			return;
+		}
+
+		jQuery( approveButtonRef.current ).elementorConnect( {
+			// Success: ( _, data ) => onSuccess( data ).then( onSuccess ),
+			success: ( _, data ) => {
+				onSuccess( data );
+			},
+			error: () => {
+				throw new Error( 'Elementor AI: Failed to connect.' );
+			},
+		} );
+	}, [] );
+
+	return (
+		<Stack alignItems="center" gap={ 2 }>
+			<AIIcon sx={ { color: 'text.primary', fontSize: '60px', mb: 1 } } />
+
+			<Typography variant="h4" sx={ { color: 'text.primary' } }>{ __( 'Step into the future with Elementor AI', 'elementor' ) }</Typography>
+
+			<Typography variant="body2">{ __( 'Create smarter with AI text and code generators built right into the editor.', 'elementor' ) }</Typography>
+
+			<Stack direction="row" gap={ 1.5 } alignItems="flex-start" >
+				<Checkbox id="e-ai-terms-approval" color="secondary" checked={ isTermsChecked } onClick={ () => setIsTermsChecked( ( prevState ) => ! prevState ) } />
+				<Stack>
+					<Typography variant="caption" sx={ { maxWidth: 520 } } component="label" htmlFor="e-ai-terms-approval">
+						{ __( 'I approve the ', 'elementor' ) }
+						<Link href="https://go.elementor.com/ai-terms/" target="_blank" color="info.main">{ __( 'Terms of Service', 'elementor' ) }</Link>
+						{ ' & ' }
+						<Link href="https://go.elementor.com/ai-privacy-policy/" target="_blank" color="info.main">{ __( 'Privacy Policy', 'elementor' ) }</Link>
+						{ __( ' of the Elementor AI service.', 'elementor' ) }
+						<br />
+						{ __( 'This includes consenting to the collection and use of data to improve user experience.', 'elementor' ) }
+					</Typography>
+				</Stack>
+			</Stack>
+
+			<Button
+				ref={ approveButtonRef }
+				disabled={ ! isTermsChecked }
+				href={ connectUrl }
+				variant="contained"
+				sx={ {
+					mt: 1,
+					'&:hover': {
+						color: 'primary.contrastText',
+					},
+				} }
+			>
+				{ __( 'Connect', 'elementor' ) }
+			</Button>
+		</Stack>
+	);
+};
+
+ConnectAndGetStarted.propTypes = {
+	connectUrl: PropTypes.string.isRequired,
+	onSuccess: PropTypes.func.isRequired,
+};
+
+export default ConnectAndGetStarted;
