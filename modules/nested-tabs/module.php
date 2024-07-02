c5ab127481a659b4bec0cb8c44e0f3f118003860
Rami Yushuvaev <ramiy@elementor.com>
2024-07-02 16:41:32 +0300

    Tweak: Load nested elements styles only when they are in use

--- modules/nested-tabs/module.php
+++ modules/nested-tabs/module.php
@@ -21,10 +21,36 @@ class Module extends \Elementor\Core\Base\Module {
 	public function __construct() {
 		parent::__construct();
 
-		add_action( 'elementor/editor/before_enqueue_scripts', function () {
-			wp_enqueue_script( $this->get_name(), $this->get_js_assets_url( $this->get_name() ), [
-				'nested-elements',
-			], ELEMENTOR_VERSION, true );
-		} );
+		add_action( 'elementor/editor/before_enqueue_scripts', [ $this, 'enqueue_scripts' ] );
+		add_action( 'elementor/frontend/after_register_styles', [ $this, 'register_style' ] );
+	}
+
+	/**
+	 * Enqueue scripts.
+	 *
+	 * @return void
+	 */
+	public function enqueue_scripts() {
+		wp_enqueue_script(
+			$this->get_name(),
+			$this->get_js_assets_url( $this->get_name() ),
+			[ 'nested-elements' ],
+			ELEMENTOR_VERSION,
+			true
+		);
+	}
+
+	/**
+	 * Register styles.
+	 *
+	 * @return void
+	 */
+	public function register_style() {
+		wp_register_style(
+			$this->get_name(),
+			$this->get_css_assets_url( 'frontend', 'assets/css/modules/nested-tabs/' ),
+			[],
+			ELEMENTOR_VERSION
+		);
 	}
 }
