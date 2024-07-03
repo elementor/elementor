2d0eb9c825ec5aabe3fcfd7e765a6a24b121068b
Omer Haim Israeli <omeri@elementor.com>
2024-07-03 11:44:34 +0300

    wip

---
+++ tests/playwright/sanity/atomic-widgets.test.ts
@@ -0,0 +1,53 @@
+import WpAdminPage from '../pages/wp-admin-page';
+import { parallelTest as test } from '../parallelTest';
+import { expect } from '@playwright/test';
+
+test.describe( 'Atomic Widgets', () => {
+	let editor;
+	let wpAdmin;
+	let context;
+
+	const atomicWidgets = [
+		{ name: 'atomic-heading', title: 'Atomic Heading' },
+	];
+
+	test.beforeAll( async ( { browser, apiRequests }, testInfo ) => {
+		context = await browser.newContext();
+
+		const page = await context.newPage();
+
+		wpAdmin = new WpAdminPage( page, testInfo, apiRequests );
+
+		await enableRequiredExperiments();
+
+		editor = await wpAdmin.openNewPage();
+	} );
+
+	test.afterAll( async () => {
+		await wpAdmin.resetExperiments();
+	} );
+
+	atomicWidgets.forEach( ( widget ) => {
+		test.describe( widget.name, () => {
+			test( 'Check widget is displayed in panel', async () => {
+				const layout = editor.page.locator( '#elementor-panel-category-general' );
+				await layout.isVisible();
+				const container = await layout.locator( '.title', { hasText: widget.title } );
+				await expect( container ).toBeVisible();
+			} );
+
+			test( 'Check widget is displayed in editor', async () => {
+				const widgetId = await editor.addWidget( widget.name );
+				const widgetSelector = '.elementor-element-' + widgetId;
+				await expect( editor.getPreviewFrame().locator( widgetSelector ) ).toBeVisible();
+			} );
+		} );
+	} );
+
+	async function enableRequiredExperiments() {
+		await wpAdmin.setExperiments( {
+			editor_v2_elements: 'active',
+		} );
+	}
+} );
+
