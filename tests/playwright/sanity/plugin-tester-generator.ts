ab86eaf8eb4aafc2a02504f7365c40c585711216
Yotam Shapira <yotams@elementor.com>
2024-07-04 17:51:35 +0300

    clean up plugin-tester

--- tests/playwright/sanity/plugin-tester-generator.ts
+++ tests/playwright/sanity/plugin-tester-generator.ts
@@ -69,16 +69,9 @@ export const generatePluginTests = ( testType: string ) => {
 					admin.remove();
 				}, adminBar );
 				await editor.removeClasses( 'elementor-motion-effects-element' );
-				await editor.scrollPage();
 				await expect.soft( page ).toHaveScreenshot( 'frontPage.png', { fullPage: true } );
 
-				if ( 'astra-sites' === plugin.pluginName ) {
-					await page.goto( '/wp-admin/index.php' );
-				}
 				await page.goto( '/law-firm-about/?elementor' );
-				if ( 'happy-elementor-addons' === plugin.pluginName ) {
-					await page.goto( '/law-firm-about/?elementor' );
-				}
 
 				await editor.getPreviewFrame().getByRole( 'heading', { name: 'About Us' } ).waitFor( { timeout: 15000 } );
 				await wpAdmin.closeAnnouncementsIfVisible();
