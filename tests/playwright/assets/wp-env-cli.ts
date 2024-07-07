68627e67110123801a97748d4b8d138728e6da5b
Yotam Shapira <yotams@elementor.com>
2024-07-07 13:05:35 +0300

    update playwright

--- tests/playwright/assets/wp-env-cli.ts
+++ tests/playwright/assets/wp-env-cli.ts
@@ -2,9 +2,9 @@ import { execSync } from 'child_process';
 import github from '@actions/core';
 
 export const wpEnvCli = ( command: string ) => {
-	const cliCommand = ( 1 === Number( process.env.TEST_PARALLEL_INDEX ) ) ? 'tests-cli' : 'cli';
-	const cli = `npx wp-env run ${ cliCommand } `;
-	const logs = execSync( `${ cli }${ command }`, { encoding: 'utf-8' } );
+	const port = ( 1 === Number( process.env.TEST_PARALLEL_INDEX ) ) ? '8889' : '8888';
+	const cli = `npm run cli ${ command } -- port=${ port }`;
+	const logs = execSync( `${ cli }${ command }`, { cwd: 'test-server', encoding: 'utf-8' } );
 	// Print log for debuggability
 	// eslint-disable-next-line no-console
 	console.log( logs );
