dd8c545846480b66eb55cc0660f9dd742e32dfe4
Yotam Shapira <yotams@elementor.com>
2024-07-07 18:36:50 +0300

    fix theme

--- tests/wp-env/config/setup.sh
+++ tests/wp-env/config/setup.sh
@@ -1,18 +1,10 @@
 #!/bin/bash
 set -eox pipefail
 
-wp theme activate hello-elementor
 wp plugin activate elementor
-
-wp --user=admin elementor library import-dir /var/www/html/elementor-templates
+wp theme activate hello-elementor
 
 WP_CLI_CONFIG_PATH=elementor-config/wp-cli.yml wp rewrite structure '/%postname%/' --hard
-
-wp cache flush
-wp rewrite flush --hard
-wp elementor flush-css
-
-wp post list --post_type=page
 
 # Remove the Guttenberg welcome guide popup
 wp user meta add admin wp_persisted_preferences 'a:2:{s:14:\"core/edit-post\";a:2:{b:1;s:12:\"welcomeGuide\";b:0;}}'
@@ -21,3 +13,11 @@ wp user meta add admin wp_persisted_preferences 'a:2:{s:14:\"core/edit-post\";a:
 for id in $(wp user list --field=ID)
 do wp user meta add "$id" "announcements_user_counter" 999
 done
+
+wp --user=admin elementor library import-dir /var/www/html/elementor-templates
+
+wp cache flush
+wp rewrite flush --hard
+wp elementor flush-css
+
+wp post list --post_type=page
