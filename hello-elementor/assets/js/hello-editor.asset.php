49d3a62d6a40a22c28d20b50ef8a7ef0928f9f7b
Yotam Shapira <yotams@elementor.com>
2024-07-07 15:30:31 +0300

    oops

--- hello-elementor/assets/js/hello-editor.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => 'b4d297e7938fe5a0c932');
--- hello-elementor/assets/js/hello-frontend.min.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => '2f03dc39ec58b53d53c8');
