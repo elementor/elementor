05250e6f3266aed76942c08464649a237c991201
Yotam Shapira <yotams@elementor.com>
2024-07-07 20:41:22 +0300

    fix wpEnvCli

--- tests/playwright/assets/wp-env-cli.ts
+++ tests/playwright/assets/wp-env-cli.ts
@@ -3,8 +3,8 @@ import github from '@actions/core';
 
 export const wpEnvCli = ( command: string ) => {
 	const port = ( 1 === Number( process.env.TEST_PARALLEL_INDEX ) ) ? '8889' : '8888';
-	const cli = `npm run cli ${ command } -- port=${ port }`;
-	const logs = execSync( `${ cli }${ command }`, { cwd: 'test-server', encoding: 'utf-8' } );
+	const cli = `npm run cli -- command=${ command } port=${ port }`;
+	const logs = execSync( `${ cli }`, { cwd: 'test-server', encoding: 'utf-8' } );
 	// Print log for debuggability
 	// eslint-disable-next-line no-console
 	console.log( logs );
