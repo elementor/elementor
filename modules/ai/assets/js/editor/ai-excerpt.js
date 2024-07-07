4b1441de71cb57a8446b4e03d6f7267784a15110
Roman <romans@elementor.com>
2024-07-07 09:43:37 +0300

    Fix: AI User consent modal is being loaded after the request has been made in Text feature [ED-15016]

--- modules/ai/assets/js/editor/ai-excerpt.js
+++ modules/ai/assets/js/editor/ai-excerpt.js
@@ -1,6 +1,6 @@
 import useUserInfo from './hooks/use-user-info';
 import useExcerptPrompt from './hooks/use-excerpt-prompt';
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import { useCallback, useEffect } from '@wordpress/element';
 import App from './app';
 import { __ } from '@wordpress/i18n';
@@ -22,9 +22,9 @@ const AIExcerpt = ( { onClose, currExcerpt, updateExcerpt, postTextualContent } 
 		credits,
 	} );
 	const generateExcerptOnce = useRef( false );
-	const isLoading = isLoadingExcerpt || isLoadingUserInfo;
+	const [isLoadingCombined, setIsLoadingCombined] = useState( true );
 	const initHook = () => ( {
-		isLoading,
+		isLoading: isLoadingCombined,
 		isConnected,
 		isGetStarted,
 		connectUrl,
@@ -36,14 +36,21 @@ const AIExcerpt = ( { onClose, currExcerpt, updateExcerpt, postTextualContent } 
 	const fetchAiExcerpt = useCallback( async () => {
 		if ( send && postTextualContent ) {
 			generateExcerptOnce.current = true;
-			await send( { content: postTextualContent } );
+			await send( { content: postTextualContent } ).then( () => {
+				setIsLoadingCombined( false );
+			} );
 		}
 	}, [ postTextualContent, send ] );
 	useEffect( () => {
-		if ( ! generateExcerptOnce.current && isConnected && ! newExcerpt?.result ) {
+		if ( ! generateExcerptOnce.current && isConnected && isGetStarted ) {
 			fetchAiExcerpt();
 		}
-	}, [ fetchAiExcerpt, isConnected, newExcerpt ] );
+	}, [ fetchAiExcerpt, isConnected, isGetStarted ] );
+	useEffect( () => {
+		if ( ! isLoadingUserInfo && ( ! isConnected || ! isGetStarted ) ) {
+			setIsLoadingCombined( false );
+		}
+	}, [ isConnected, isGetStarted, isLoadingUserInfo ] );
 	const isRTL = elementorCommon.config.isRTL;
 
 	return (
