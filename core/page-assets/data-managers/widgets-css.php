bf88c0f9329c9311958898ca65b2ad3cc9e9125e
Rami Yushuvaev <ramiy@elementor.com>
2024-07-08 08:42:39 +0300

    Tweak: Remove inline CSS from the "Improved CSS Loading" experiment

--- core/page-assets/data-managers/widgets-css.php
+++ core/page-assets/data-managers/widgets-css.php
@@ -16,21 +16,8 @@ class Widgets_Css extends Base {
 	protected $assets_category = 'widgets';
 
 	protected function get_asset_content() {
-		$asset_css_file_size = $this->get_file_data( 'size' );
-
-		$widget_css = '';
-
-		if ( $asset_css_file_size ) {
-			// If the file size is larger than 8KB then calling the external CSS file, otherwise, printing inline CSS.
-			if ( $asset_css_file_size > 8000 ) {
-				$asset_url = $this->get_config_data( 'file_url' );
-
-				$widget_css = sprintf( '<link rel="stylesheet" href="%s">', $asset_url );
-			} else {
-				$widget_css = $this->get_file_data( 'content' );
-				$widget_css = sprintf( '<style>%s</style>', $widget_css );
-			}
-		}
+		$asset_url = $this->get_config_data( 'file_url' );
+		$widget_css = sprintf( '<link rel="stylesheet" href="%s">', $asset_url );
 
 		return $widget_css;
 	}
