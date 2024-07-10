4cc59dba72cfe932a84ef1e9afb0b12069ec8dd4
Omer Haim Israeli <omeri@elementor.com>
2024-07-10 11:52:56 +0300

    wip

--- modules/atomic-widgets/widgets/atomic-heading.php
+++ modules/atomic-widgets/widgets/atomic-heading.php
@@ -1,6 +1,9 @@
 <?php
 namespace Elementor\Modules\AtomicWidgets\Widgets;
 
+use Elementor\Modules\AtomicWidgets\AtomicControls\Atomic_Control;
+use Elementor\Modules\AtomicWidgets\AtomicControls\Types\Select_Control;
+use Elementor\Modules\AtomicWidgets\AtomicControls\Types\Text_Control;
 use Elementor\Modules\AtomicWidgets\Base\Atomic_Widget_Base;
 use Elementor\Utils;
 
