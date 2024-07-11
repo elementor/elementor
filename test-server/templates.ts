b99cfd25bbcfe7172190bde978dfec0b177effb2
Yotam Shapira <yotams@elementor.com>
2024-07-11 17:59:26 +0300

    clean up

--- test-server/templates.ts
+++ test-server/templates.ts
@@ -78,31 +78,23 @@ volumes:
 
 export const generateWordPressDockerfileTemplate = ( config: Config ) => {
 	return `FROM wordpress:${ config.core }-php${ config.phpVersion }
-
-# Create the host's user so that we can match ownership in the container.
 ARG HOST_USERNAME
 ARG HOST_UID
 ARG HOST_GID
 # When the IDs are already in use we can still safely move on.
 RUN groupadd -o -g $HOST_GID $HOST_USERNAME || true
 RUN useradd -mlo -u $HOST_UID -g $HOST_GID $HOST_USERNAME || true
-
-# USER root
 `;
 };
 
 export const generateCliDockerfileTemplate = ( config: Config ) => {
 	return `FROM wordpress:cli-php${ config.phpVersion }
-# Create the host's user so that we can match ownership in the container.
 ARG HOST_USERNAME
 ARG HOST_UID
 ARG HOST_GID
 # When the IDs are already in use we can still safely move on.
 RUN addgroup -g $HOST_GID $HOST_USERNAME || true
 RUN adduser -h /home/$HOST_USERNAME -G $( getent group $HOST_GID | cut -d: -f1 ) -u $HOST_UID $HOST_USERNAME || true
-
-# Switch back to the original user now that we're done.
-# USER www-data
 
 # Have the container sleep infinitely to keep it alive for us to run commands on it.
 CMD [ "/bin/sh", "-c", "while true; do sleep 2073600; done" ]
