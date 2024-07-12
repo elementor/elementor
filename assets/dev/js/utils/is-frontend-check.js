59f28031b22f3ead951eb31b71e05a82a1c9588d
Hein van Vlastuin <heinvanvlastuin@gmail.com>
2024-07-12 13:54:27 +0200

    WIP

--- assets/dev/js/utils/is-frontend-check.js
+++ assets/dev/js/utils/is-frontend-check.js
@@ -2,8 +2,8 @@ export function isFrontend() {
 	const isWpAdmin = window.location.pathname.indexOf( 'wp-admin' ) !== -1 ||
 			document.body.classList.contains( 'wp-admin' ) ||
 			document.body.classList.contains( 'admin-bar' ),
-		isElementorEditor = document.body.classList.contains( 'elementor-editor-active' ) ||
-			document.body.classList.contains( 'elementor-page' ); // Maybe check if this is the Elementor Iframe
+		isElementorEditor = document.body.classList.contains( 'elementor-editor-active' )
+			// document.body.classList.contains( 'elementor-page' ); // Maybe check if this is the Elementor Iframe
 
 	return ! isWpAdmin && ! isElementorEditor;
 }
