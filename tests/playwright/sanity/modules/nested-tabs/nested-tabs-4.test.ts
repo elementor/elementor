9a90e242c3b4298d5875a7ad876687db18bb5895
baghdasarovelementor <norayrb@elementor.red>
2024-07-15 15:16:24 +0400

    wip

--- tests/playwright/sanity/modules/nested-tabs/nested-tabs-4.test.ts
+++ tests/playwright/sanity/modules/nested-tabs/nested-tabs-4.test.ts
@@ -64,7 +64,7 @@ test.describe( 'Nested Tabs tests @nested-tabs', () => {
 			contentContainerTwoId = await contentContainerTwo.getAttribute( 'data-id' ),
 			contentContainerThree = editor.getPreviewFrame().locator( `.e-n-tabs-content .e-con >> nth=2` ),
 			contentContainerThreeId = await contentContainerThree.getAttribute( 'data-id' ),
-			videoUrl = 'https://youtu.be/XNoaN8qu4fg',
+			videoUrl = 'https://www.youtube.com/watch?v=XNoaN8qu4fg',
 			videoContainer = editor.getPreviewFrame().locator( '.elementor-element-' + contentContainerOneId + ' .elementor-background-video-container iframe' ),
 			firstTabContainer = editor.getPreviewFrame().locator( '.elementor-element-' + contentContainerOneId ),
 			firstTabContainerModelCId = await firstTabContainer.getAttribute( 'data-model-cid' );
