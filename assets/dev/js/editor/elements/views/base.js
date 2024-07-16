a1e41e0295899960c77d65dfa9b94cf2bf313a00
David Seveloff <davids@elementor.com>
2024-07-16 12:36:24 +0300

    wip

--- assets/dev/js/editor/elements/views/base.js
+++ assets/dev/js/editor/elements/views/base.js
@@ -1176,16 +1176,15 @@ BaseElementView = BaseContainer.extend( {
 	},
 
 	getChangedDynamicControlKey( settings ) {
-		let changedControlKey = Object.keys( settings.changed )[ 0 ] !== '__dynamic__'
-			? Object.keys( settings.changed )[ 0 ]
-			: Object.keys( settings.changed.__dynamic__ )[ 0 ];
+		let changedControlKey = this.findUniqueKey( settings?.changed?.__dynamic__, settings?._previousAttributes?.__dynamic__ )[ 0 ];
 
-		if ( changedControlKey ) {
-			return changedControlKey;
+		if ( ! changedControlKey ) {
+			changedControlKey = Object.keys( settings.changed )[ 0 ] !== '__dynamic__'
+				? Object.keys( settings.changed )[ 0 ]
+				: Object.keys( settings.changed.__dynamic__ )[ 0 ];
 		}
 
-		return this.findUniqueKey( settings?.changed?.__dynamic__, settings?._previousAttributes?.__dynamic__ )[ 0 ];
-
+		return changedControlKey;
 	},
 
 	getRepeaterItemActiveIndex() {
