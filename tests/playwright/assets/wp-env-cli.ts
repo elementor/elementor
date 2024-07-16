4e4416d3e973840aeebdad8cc4ff1376f8de7b8c
Yotam Shapira <yotams@elementor.com>
2024-07-16 11:32:18 +0300

    dynamic import

--- tests/playwright/assets/wp-env-cli.ts
+++ tests/playwright/assets/wp-env-cli.ts
@@ -1,6 +1,5 @@
-const { cli } = await import( 'wp-test-server' );
-
 export const wpEnvCli = async ( command: string ) => {
 	const port = ( 1 === Number( process.env.TEST_PARALLEL_INDEX ) ) ? '8889' : '8888';
+	const { cli } = await import( 'wp-test-server' );
 	await cli( port, command );
 };
