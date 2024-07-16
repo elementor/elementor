ab5ac3007cadf6f4e5ebb3a6a6d64680e4fd0ac6
Mati Horowitz <mati@elementor.com>
2024-07-16 19:27:16 +0300

    Internal: Update the plugin's regression test with a plugin to avoid ED-15079 [15360]
    The plugin demonstrates the situation here:
    https://github.com/elementor/elementor/pull/27689

---
+++ tests/playwright/plugin-tester-plugins/enqueue-media-on-front/enqueue-media-on-front.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Plugin Name: Enqueue Media on Front
+ * Description: Enqueue media on front to avoid issues like https://github.com/elementor/elementor/pull/27689
+ */
+
+add_action( 'template_redirect', function () {
+    wp_enqueue_media();
+} );
+
