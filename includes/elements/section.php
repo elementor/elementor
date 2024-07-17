f7f079d677bfb85981d778be26753bcc090a14ec
Batsirai Muchareva <162136788+Batsirai-muchareva@users.noreply.github.com>
2024-07-17 20:26:31 +0200

    Tweak: Load shape dividers styles only when they are in use[ED-15211] (#28098)

--- includes/elements/section.php
+++ includes/elements/section.php
@@ -952,6 +952,22 @@ class Element_Section extends Element_Base {
 					'options' => $shapes_options,
 					'render_type' => 'none',
 					'frontend_available' => true,
+					'assets' => [
+						'styles' => [
+							[
+								'name' => 'e-shapes',
+								'conditions' => [
+									'terms' => [
+										[
+											'name' => $base_control_key,
+											'operator' => '!==',
+											'value' => '',
+										],
+									],
+								],
+							],
+						],
+					],
 				]
 			);
 
