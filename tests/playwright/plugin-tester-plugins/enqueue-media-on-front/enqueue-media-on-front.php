f008084044cadd6d75929e14ffb1f168698dedf6
Mati Horowitz <mati@elementor.com>
2024-07-17 10:55:19 +0300

    Added a note about the file

--- tests/playwright/plugin-tester-plugins/enqueue-media-on-front/enqueue-media-on-front.php
+++ tests/playwright/plugin-tester-plugins/enqueue-media-on-front/enqueue-media-on-front.php
@@ -1,6 +1,10 @@
 <?php
 
 /**
+ * The file shouldn't be changed, it's attached for readability about the zip file
+ */
+
+/**
  * Plugin Name: Enqueue Media on Front
  * Description: Enqueue media on front to avoid issues like https://github.com/elementor/elementor/pull/27689
  */
@@ -8,4 +12,3 @@
 add_action( 'template_redirect', function () {
     wp_enqueue_media();
 } );
-
