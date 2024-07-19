c27716b0aeeedb500b1fb8bb2ef600fc1dd2fb8d
Hein van Vlastuin <heinvanvlastuin@gmail.com>
2024-07-19 17:03:56 +0200

    WIP

--- assets/dev/js/utils/is-frontend-check.js
+++ assets/dev/js/utils/is-frontend-check.js
@@ -1,8 +1,10 @@
+// This file needs to be updated.
+// This helper function is used to check if the current page is the frontend.
 export function isFrontend() {
 	const isWpAdmin = window.location.pathname.indexOf( 'wp-admin' ) !== -1 ||
 			document.body.classList.contains( 'wp-admin' ) ||
 			document.body.classList.contains( 'admin-bar' ),
-		isElementorEditor = document.body.classList.contains( 'elementor-editor-active' )
+		isElementorEditor = document.body.classList.contains( 'elementor-editor-active' );
 			// document.body.classList.contains( 'elementor-page' ); // Maybe check if this is the Elementor Iframe
 
 	return ! isWpAdmin && ! isElementorEditor;
