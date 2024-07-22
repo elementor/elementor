0001082e45927b31104cd80d419be666988c4680
Dennis Nerush <dennisn@elementor.com>
2024-07-22 18:32:20 +0300

    Fix: Show AI on excerpt WP 6.6 [ED-15387] (#28161)

--- modules/ai/assets/js/gutenberg/index.js
+++ modules/ai/assets/js/gutenberg/index.js
@@ -18,16 +18,7 @@ import { EditTextWithAi } from './edit-text-with-ai';
 			if ( excerptPanel && ! document.querySelector( '.e-excerpt-ai' ) ) {
 				const rootElement = document.createElement( 'div' );
 				rootElement.classList.add( 'e-excerpt-ai' );
-				// Find the existing link with class "components-external-link"
-				const existingExcerptLink = excerptPanel.querySelector( '.components-external-link' );
-				if ( existingExcerptLink ) {
-					existingExcerptLink.classList.add( 'existing-excerpt-link' );
-					// Append the new link before the existing one
-					excerptPanel.insertBefore( rootElement, existingExcerptLink );
-				} else {
-					// Append the new link to the excerpt panel
-					excerptPanel.appendChild( rootElement );
-				}
+				excerptPanel.appendChild( rootElement );
 
 				const urlSearchParams = new URLSearchParams( window.location.search );
 				elementorCommon?.ajax?.addRequestConstant( 'editor_post_id', urlSearchParams?.get( 'post' ) );
