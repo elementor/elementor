6d2a5ccee798c0c94c8f90abfad31f1b532b71db
Maggie Serino <maggie.serino@10up.com>
2024-07-22 18:45:44 +0200

    Tweak: Floating Buttons - add accessible aria labels to buttons

--- modules/floating-buttons/base/widget-contact-button-base.php
+++ modules/floating-buttons/base/widget-contact-button-base.php
@@ -97,6 +97,7 @@ abstract class Widget_Contact_Button_Base extends Widget_Base {
 							'is_external' => true,
 						],
 					],
+					'has_accessible_name' => true,
 				],
 				'top_bar_section' => [
 					'section_name' => esc_html__( 'Top Bar', 'elementor' ),
@@ -163,6 +164,7 @@ abstract class Widget_Contact_Button_Base extends Widget_Base {
 							'contact_icon_platform' => Social_Network_Provider::MESSENGER,
 						],
 					],
+					'has_accessible_name' => true,
 				],
 				'send_button_section' => [
 					'section_name' => esc_html__( 'Send Button', 'elementor' ),
@@ -444,18 +446,20 @@ abstract class Widget_Contact_Button_Base extends Widget_Base {
 			]
 		);
 
-		$this->add_control(
-			'chat_aria_label',
-			[
-				'label' => esc_html__( 'Accessible name', 'elementor' ),
-				'type' => Controls_Manager::TEXT,
-				'default' => $config['content']['chat_button_section']['chat_aria_label'],
-				'placeholder' => esc_html__( 'Add accessible name', 'elementor' ),
-				'dynamic' => [
-					'active' => true,
+		if ( $config['content']['chat_button_section']['has_accessible_name']) {
+			$this->add_control(
+				'chat_aria_label',
+				[
+					'label' => esc_html__( 'Accessible name', 'elementor' ),
+					'type' => Controls_Manager::TEXT,
+					'default' => $config['content']['chat_button_section']['chat_aria_label'],
+					'placeholder' => esc_html__( 'Add accessible name', 'elementor' ),
+					'dynamic' => [
+						'active' => true,
+					],
 				],
-			],
-		);
+			);
+		}
 
 		if ( $config['content']['chat_button_section']['has_platform'] ) {
 
@@ -698,18 +702,20 @@ abstract class Widget_Contact_Button_Base extends Widget_Base {
 			]
 		);
 
-		$this->add_control(
-			'chat_aria_label',
-			[
-				'label' => esc_html__( 'Accessible name', 'elementor' ),
-				'type' => Controls_Manager::TEXT,
-				'default' => $config['content']['chat_button_section']['chat_aria_label'],
-				'placeholder' => esc_html__( 'Add accessible name', 'elementor' ),
-				'dynamic' => [
-					'active' => true,
+		if ( $config['content']['contact_section']['has_accessible_name']) {
+			$this->add_control(
+				'contact_aria_label',
+				[
+					'label' => esc_html__( 'Accessible name', 'elementor' ),
+					'type' => Controls_Manager::TEXT,
+					'default' => $config['content']['chat_button_section']['chat_aria_label'],
+					'placeholder' => esc_html__( 'Add accessible name', 'elementor' ),
+					'dynamic' => [
+						'active' => true,
+					],
 				],
-			],
-		);
+			);
+		}
 
 		if ( $config['content']['contact_section']['has_cta_text'] ) {
 			$this->add_control(
@@ -719,6 +725,7 @@ abstract class Widget_Contact_Button_Base extends Widget_Base {
 					'type' => Controls_Manager::TEXT,
 					'default' => esc_html__( 'Start conversation:', 'elementor' ),
 					'placeholder' => esc_html__( 'Type your text here', 'elementor' ),
+					'label_block' => true,
 				]
 			);
 		}
@@ -731,7 +738,7 @@ abstract class Widget_Contact_Button_Base extends Widget_Base {
 						'type' => Controls_Manager::ALERT,
 						'alert_type' => 'info',
 						'content' => sprintf(
-							__( 'Add between <b>%1$d</b> to <b>%2$d</b> icons', 'elementor' ),
+							__( 'Add between <b>%1$d</b> to <b>%2$d</b> contact buttons', 'elementor' ),
 							$config['content']['contact_section']['platform']['min_items'],
 							$config['content']['contact_section']['platform']['limit']
 						),
@@ -744,7 +751,7 @@ abstract class Widget_Contact_Button_Base extends Widget_Base {
 						'type' => Controls_Manager::ALERT,
 						'alert_type' => 'info',
 						'content' => sprintf(
-							__( 'Add up to <b>%d</b> icons', 'elementor' ),
+							__( 'Add up to <b>%d</b> contact buttons', 'elementor' ),
 							$config['content']['contact_section']['platform']['limit']
 						),
 					]
