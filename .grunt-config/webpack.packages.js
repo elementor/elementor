8ff9da2ec481f0f1d4406f02d5cb520b9d051332
Rotem Ben-Itzhak <rotem@elementor.com>
2024-07-25 17:23:37 +0300

    wip

--- .grunt-config/webpack.packages.js
+++ .grunt-config/webpack.packages.js
@@ -35,7 +35,8 @@ const common = {
 		new GenerateWordPressAssetFileWebpackPlugin( {
 			handle: ( entryName ) => `elementor-v2-${entryName}`,
 			map: [
-				{ request: /^@elementor\/(.+)$/, handle: 'elementor-v2-$1' },
+				{ request: /^@elementor\/(ui|icons)(\/.+)?$/, handle: 'elementor-v2-$1' },
+				{ request: /^@elementor\/([^\/]+)(\/.+)?$/, handle: 'elementor-v2-$1' },
 				{ request: /^@wordpress\/(.+)$/, handle: 'wp-$1' },
 				{ request: 'react', handle: 'react' },
 				{ request: 'react-dom', handle: 'react-dom' },
@@ -44,6 +45,7 @@ const common = {
 		new ExternalizeWordPressAssetsWebpackPlugin( {
 			global: ( entryName ) => [ 'elementorV2', entryName ],
 			map: [
+				{ request: /^@elementor\/(ui|icons)\/(.+)$/, global: [ 'elementorV2', '$1', '$2' ] },
 				{ request: /^@elementor\/(.+)$/, global: [ 'elementorV2', '$1' ] },
 				{ request: /^@wordpress\/(.+)$/, global: [ 'wp', '$1' ] },
 				{ request: 'react', global: 'React' },
@@ -142,6 +144,11 @@ function getLocalRepoPackagesEntries() {
 	packages.push( {
 		name: 'ui',
 		path: './node_modules/@elementor/ui/index.js'
+	} );
+
+	packages.push( {
+		name: 'icons',
+		path: './node_modules/@elementor/icons/index.js'
 	} );
 
 	return packages;
