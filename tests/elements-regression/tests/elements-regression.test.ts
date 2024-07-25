aeb82a22d622045992ffc365f2cc405abfc4bd41
Rotem Ben-Itzhak <rotem@elementor.com>
2024-07-25 16:24:54 +0300

    wip

--- tests/elements-regression/tests/elements-regression.test.ts
+++ tests/elements-regression/tests/elements-regression.test.ts
@@ -7,6 +7,7 @@ import ElementRegressionHelper from '../helper';
 test.describe( 'Elementor regression tests with templates for CORE', () => {
 	const testData = [
 		'divider',
+		'heading',
 		'text_editor',
 		'button',
 		'image',
