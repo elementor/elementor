4ca9e48b4af3a98ee5b459913de0d7d713bada3f
Rotem Ben-Itzhak <rotem@elementor.com>
2024-07-27 18:05:42 +0300

    wip

--- .grunt-config/webpack.packages.js
+++ .grunt-config/webpack.packages.js
@@ -35,8 +35,8 @@ const common = {
 		new GenerateWordPressAssetFileWebpackPlugin( {
 			handle: ( entryName ) => `elementor-v2-${entryName}`,
 			map: [
-				{ request: /^@elementor\/(ui|icons)(\/.+)?$/, handle: 'elementor-v2-$1' },
-				{ request: /^@elementor\/([^\/]+)(\/.+)?$/, handle: 'elementor-v2-$1' },
+				// { request: /^@elementor\/(ui|icons)(\/.+)?$/, handle: 'elementor-v2-$1' },
+				// { request: /^@elementor\/([^\/]+)(\/.+)?$/, handle: 'elementor-v2-$1' },
 				{ request: /^@elementor\/(.+)$/, handle: 'elementor-v2-$1' },
 				{ request: /^@wordpress\/(.+)$/, handle: 'wp-$1' },
 				{ request: 'react', handle: 'react' },
@@ -46,7 +46,7 @@ const common = {
 		new ExternalizeWordPressAssetsWebpackPlugin( {
 			global: ( entryName ) => [ 'elementorV2', entryName ],
 			map: [
-				{ request: /^@elementor\/(ui|icons)\/(.+)$/, global: [ 'elementorV2', '$1', '$2' ] },
+				// { request: /^@elementor\/(ui|icons)\/(.+)$/, global: [ 'elementorV2', '$1', '$2' ] },
 				{ request: /^@elementor\/(.+)$/, global: [ 'elementorV2', '$1' ] },
 				{ request: /^@wordpress\/(.+)$/, global: [ 'wp', '$1' ] },
 				{ request: 'react', global: 'React' },
