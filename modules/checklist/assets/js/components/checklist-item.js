850f97394d58839fa9643d0ee84c6844ed3c30b8
elchugreeva <elenac@elementor.com>
2024-08-12 15:19:41 +0300

    lint

--- modules/checklist/assets/js/components/checklist-item.js
+++ modules/checklist/assets/js/components/checklist-item.js
@@ -11,7 +11,7 @@ import {
 	ListItemText,
 	Collapse,
 	CardMedia,
-	Link
+	Link,
 } from '@elementor/ui';
 import { ChevronDownIcon, RadioButtonUncheckedIcon } from '@elementor/icons';
 
@@ -35,7 +35,7 @@ function CheckListItem( props ) {
 				{ index === expandedIndex && expanded ? <ChevronDownIcon sx={ { transform: 'rotate(180deg)' } } /> : <ChevronDownIcon /> }
 			</ListItemButton>
 			<Collapse in={ index === expandedIndex && expanded } >
-				<Card elevation= { 0 } square={ true }>
+				<Card elevation={ 0 } square={ true }>
 					<CardMedia
 						image="https://elementor.com/cdn-cgi/image/f=auto,w=1100/https://elementor.com/wp-content/uploads/2022/01/Frame-10879527.png"
 						sx={ { height: 180 } }
