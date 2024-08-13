ae741f4b3aaee310e8d1842605e212ed727b5309
Ron Rostovsky <ronros@elementor.com>
2024-08-13 11:35:25 +0300

    removed redundant jsdoc type

--- modules/atomic-widgets/assets/js/editor/commands/styles-updated.js
+++ modules/atomic-widgets/assets/js/editor/commands/styles-updated.js
@@ -1,6 +1,3 @@
-/**
- * @typedef {import('elementor/assets/dev/js/editor/container/container')} Container
- */
 export class StylesUpdated extends $e.modules.editor.CommandContainerBase {
 	validateArgs( args ) {
 		this.requireContainer( args );
