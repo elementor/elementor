4fe09aa2dcaaea2acb9cd299841765231eda74cc
Hein van Vlastuin <heinvanvlastuin@gmail.com>
2024-08-13 09:00:13 +0200

    WIP

--- tests/playwright/pages/widgets/image-carousel.ts
+++ tests/playwright/pages/widgets/image-carousel.ts
@@ -1,19 +1,8 @@
 import EditorSelectors from '../../selectors/editor-selectors';
 import Content from '../elementor-panel-tabs/content';
 import { expect } from '@playwright/test';
-import { resolve } from 'path';
 
 export default class ImageCarousel extends Content {
-	async addTestImageMetaData( args = { caption: 'Test caption!', description: 'Test description!' } ) {
-		await this.page.locator( EditorSelectors.media.images ).first().click();
-		await this.page.locator( EditorSelectors.media.imgCaption ).clear();
-		await this.page.locator( EditorSelectors.media.imgCaption ).type( args.caption );
-
-		await this.page.locator( EditorSelectors.media.images ).first().click();
-		await this.page.locator( EditorSelectors.media.imgDescription ).clear();
-		await this.page.locator( EditorSelectors.media.imgDescription ).type( args.description );
-	}
-
 	async verifyCaption( expectedData: string[], captionCount = 3 ) {
 		for ( let i = 0; i < captionCount; i++ ) {
 			await expect( this.editor.getPreviewFrame()
