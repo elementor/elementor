51550c4175f5d96a9e28811c7bb459f3f1c0a70a
Louis Wolmarans <louiswol94@gmail.com>
2024-08-15 09:09:38 +0200

    WIP

--- modules/wc-product-editor/assets/js/e-wc-product-editor.js
+++ modules/wc-product-editor/assets/js/e-wc-product-editor.js
@@ -5,58 +5,50 @@ const { registerPlugin } = wp.plugins;
 const { useDispatch, useSelect } = wp.data;
 
 function ElementorHeaderItem() {
-    const productId = useEntityId('postType', 'product');
-    const { saveEntityRecord } = useDispatch('core');
+	const productId = useEntityId( 'postType', 'product' );
+	const { saveEntityRecord } = useDispatch( 'core' );
 
-    const postStatus = useSelect((select) => {
-        return select('core').getEditedEntityRecord('postType', 'product', productId)?.status;
-    }, [productId]);
+	const postStatus = useSelect( ( select ) => {
+		return select( 'core' ).getEditedEntityRecord( 'postType', 'product', productId )?.status;
+	}, [ productId ] );
 
-    const handleClick = () => {
-        if (postStatus === 'auto-draft') {
-            // Only update the product if it's in 'auto-draft' status
-            saveEntityRecord('postType', 'product', {
-                id: productId,
-                name: `Elementor #${productId}`,
-                status: 'draft',
-            }).then(() => {
-                // Call the function to handle redirection after ensuring no ongoing save operation
-                redirectToElementor(productId);
-            }).catch((error) => {
-                console.error('Error updating product:', error);
-            });
-        } else {
-            // Directly handle redirection if the product is not in 'auto-draft'
-            redirectToElementor(productId);
-        }
-    };
+	const handleClick = () => {
+		if ( 'auto-draft' === postStatus ) {
+			saveEntityRecord( 'postType', 'product', {
+				id: productId,
+				name: `Elementor #${ productId }`,
+				status: 'draft',
+			} ).then( () => {
+				redirectToElementor( productId );
+			} ).catch( () => {} );
+		} else {
+			redirectToElementor( productId );
+		}
+	};
 
-    const redirectToElementor = (id) => {
-        const checkSaveStatus = () => {
-            // Check if the post is still being saved
-            if (wp.data.select('core/editor').isSavingPost()) {
-                // If saving is in progress, check again after a delay
-                setTimeout(checkSaveStatus, 300);
-            } else {
-                // Redirect to Elementor when saving is complete
-                window.location.href = `/wp-admin/post.php?post=${id}&action=elementor`;
-            }
-        };
+	const redirectToElementor = ( id ) => {
+		const checkSaveStatus = () => {
+			if ( wp.data.select( 'core/editor' ).isSavingPost() ) {
+				setTimeout( checkSaveStatus, 300 );
+			} else {
+				window.location.href = `/wp-admin/post.php?post=${ id }&action=elementor`;
+			}
+		};
 
-        // Start checking the saving status
-        checkSaveStatus();
-    };
+		checkSaveStatus();
+	};
 
-    return (
-        <WooHeaderItem name="product">
-            <Button onClick={handleClick}>
-                Edit with Elementor
-            </Button>
-        </WooHeaderItem>
-    );
+	return (
+		<WooHeaderItem name="product">
+			<Button variant="primary" onClick={ handleClick } style={ { display: 'flex', alignItems: 'center' } }>
+				<i className="eicon-elementor-square" aria-hidden="true" style={ { paddingInlineEnd: '8px' } }></i>
+				Edit with Elementor
+			</Button>
+		</WooHeaderItem>
+	);
 }
 
-registerPlugin('elementor-header-item', {
-    render: ElementorHeaderItem,
-    scope: 'woocommerce-product-block-editor',
-});
+registerPlugin( 'elementor-header-item', {
+	render: ElementorHeaderItem,
+	scope: 'woocommerce-product-block-editor',
+} );
