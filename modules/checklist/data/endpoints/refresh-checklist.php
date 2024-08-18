61573e3ce88c970b67e958198c8fe256e8a57702
mugurelLupu <mugurell@elementor.red>
2024-08-18 09:40:25 +0300

    use register_rest_route for controller

--- modules/checklist/data/endpoints/refresh-checklist.php
+++ modules/checklist/data/endpoints/refresh-checklist.php
@@ -3,6 +3,7 @@ namespace Elementor\Modules\Search\Data\Endpoints;
 
 use Elementor\Core\Editor\Data\Globals\Endpoints\Base;
 use Elementor\Modules\Checklist\Module as Checklist_Module;
+use Elementor\Plugin;
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
@@ -14,13 +15,21 @@ class Refresh_Checklist extends Base {
 		return 'refresh-checklist';
 	}
 
+
+	public function register_routes() {
+		register_rest_route( 'elementor/v1', '/refresh-checklist', [
+			'methods' => 'GET',
+			'callback' => [ $this, 'get_checklist_data' ],
+		] );
+	}
+
 	public function get_route() : string {
 		return 'checklist';
 	}
 
 	public function get_checklist_data( \WP_REST_Request $request ): array {
 		$data = $request->get_params();
-        $checklist_module = Plugin::elementor()->modules_manager->get_modules( 'checklist' );
+		$checklist_module = Plugin::elementor()->modules_manager->get_modules( 'checklist' );
 		$steps_data = $checklist_module->get_step_manager()->get_steps_for_frontend();
 
 		return [
