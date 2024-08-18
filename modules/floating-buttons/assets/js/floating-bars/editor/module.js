d34a0bef82e318ff55157c7859c70da5cc18dec7
Nicola Peluchetti <nicola.peluchetti@gmail.com>
2024-08-18 13:11:08 +0300

    fix removing experiment

--- modules/floating-buttons/assets/js/floating-bars/editor/module.js
+++ modules/floating-buttons/assets/js/floating-bars/editor/module.js
@@ -6,6 +6,7 @@ class FloatingBarsLibraryModule extends elementorModules.editor.utils.Module {
 	hideAdvancedTab( sectionName, editor ) {
 		const widgetType = editor?.model?.get( 'widgetType' ) || '';
 
+		console.log( widgetType);
 		if ( ! widgetType.startsWith( 'floating-bars' ) ) {
 			return;
 		}
