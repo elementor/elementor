41084561c56bb0052a15856fe56b51af7ca424ea
svitlanadykun <svitlanad@elementor.red>
2024-08-19 18:22:48 +0200

    UI login in case of failure

--- tests/elements-regression/global-setup.ts
+++ tests/elements-regression/global-setup.ts
@@ -1,16 +1,31 @@
-import { request, type FullConfig } from '@playwright/test';
+import { chromium, request, type FullConfig } from '@playwright/test';
 import ApiRequests from '../playwright/assets/api-requests';
 import path from 'path';
 import { fetchNonce, login } from '../playwright/wp-authentication';
 
 async function globalSetup( config: FullConfig ) {
-	const { baseURL } = config.projects[ 0 ].use;
+	const { baseURL, headless } = config.projects[ 0 ].use;
 
 	let context = await login( request, process.env.USERNAME || 'admin', process.env.PASSWORD || 'password', baseURL );
+
 	const storageState = await context.storageState();
 	await context.dispose();
 	context = await request.newContext( { storageState } );
-	const nonce = await fetchNonce( context, baseURL );
+	let nonce: string;
+	try {
+		nonce = await fetchNonce( context, baseURL );
+		return nonce;
+	} catch ( e ) {
+		const browser = await chromium.launch( { headless } );
+		const page = await browser.newPage();
+		// Experiment
+		await page.waitForSelector( 'text=Log In' );
+		await page.fill( 'input[name="log"]', process.env.USERNAME || 'admin' );
+		await page.fill( 'input[name="pwd"]', process.env.PASSWORD || 'password' );
+		await page.click( '#wp-submit' );
+		await page.waitForSelector( 'text=Dashboard' );
+		nonce = await fetchNonce( context, baseURL );
+	}
 
 	const imageIds = [];
 	const image1 = {
