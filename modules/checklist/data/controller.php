5bd229da55fcbb933c95c43d98293a59bd50d4f3
Louis Wolmarans <louiswol94@gmail.com>
2024-08-20 11:50:43 +0200

    WIP

--- modules/checklist/data/controller.php
+++ modules/checklist/data/controller.php
@@ -17,29 +17,11 @@ class Controller extends Controller_Base {
         $this->register_endpoint( new Refresh_Checklist($this) );
 	}
 
-    public function get_item_permisions_check ( $request ) {
+    public function get_item_permissions_check ( $request ) {
         return true;
     }
 
-    public function get_items_permisions_check ( $request ) {
-        return true;
-    }
-
-	public function get_item( $request ) {
-		return $this->get_checklist_data( $request );
-	}
-
-	public function get_items( $request ) {
-		return $this->get_checklist_data( $request );
-	}
-
-	public function get_checklist_data( \WP_REST_Request $request ): array {
-		$data = $request->get_params();
-		$checklist_module = Plugin::elementor()->modules_manager->get_modules( 'checklist' );
-		$steps_data = $checklist_module->get_step_manager()->get_steps_for_frontend();
-
-		return [
-			'data' => wp_json_encode( $steps_data ),
-		];
+	public function get_items_permissions_check( $request ) {
+		return true;
 	}
 }
--- modules/checklist/data/endpoints/refresh-checklist.php
+++ modules/checklist/data/endpoints/refresh-checklist.php
@@ -1,5 +1,5 @@
 <?php
-namespace Elementor\Modules\Search\Data\Endpoints;
+namespace Elementor\Modules\Checklist\Data\Endpoints;
 
 use Elementor\Data\V2\Base\Endpoint as Base;
 use Elementor\Modules\Checklist\Module as Checklist_Module;
@@ -19,10 +19,14 @@ class Refresh_Checklist extends Base {
 		return 'checklist';
 	}
 
+	public function get_items( $request ) {
+		return $this->get_checklist_data( $request );
+	}
+
 	public function get_checklist_data( \WP_REST_Request $request ): array {
 		$data = $request->get_params();
-		$checklist_module = Plugin::elementor()->modules_manager->get_modules( 'checklist' );
-		$steps_data = $checklist_module->get_step_manager()->get_steps_for_frontend();
+		$checklist_module = Plugin::$instance->modules_manager->get_modules( 'checklist' );
+		$steps_data = $checklist_module->get_steps_manager()->get_steps_for_frontend();
 
 		return [
 			'data' => wp_json_encode( $steps_data ),
