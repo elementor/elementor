23f69c898efee5124804af4c539698b4ec50810f
StyleShit <32631382+StyleShit@users.noreply.github.com>
2024-08-25 11:41:19 +0300

    wip

--- modules/atomic-widgets/prop-types/classes-type.php
+++ modules/atomic-widgets/prop-types/classes-type.php
@@ -17,14 +17,14 @@ class Classes_Type extends Transformable_Type {
 			throw new \Exception( 'Value must be an array, ' . gettype( $value ) . ' given.' );
 		}
 
-		if ( ! $this->all_are_strings( $value ) ) {
-			throw new \Exception( 'All classes must be strings.' );
+		if ( ! $this->all_are_valid_classes( $value ) ) {
+			throw new \Exception( 'All classes must start with an english letter, and contain only english letters, numbers, hyphens, and underscores.' );
 		}
 	}
 
-	private function all_are_strings( array $values ) {
+	private function all_are_valid_classes( array $values ) {
 		return array_reduce( $values, function ( $carry, $item ) {
-			return $carry && is_string( $item );
+			return $carry && is_string( $item ) && preg_match( '/^[a-z][a-z-_0-9]*$/i', $item );
 		}, true );
 	}
 }
