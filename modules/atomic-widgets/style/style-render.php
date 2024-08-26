dbf8b6be8c458a3181089aa12d89525862b290c5
Ron Rostovsky <ronros@elementor.com>
2024-08-26 10:55:24 +0300

    lint

--- modules/atomic-widgets/style/style-render.php
+++ modules/atomic-widgets/style/style-render.php
@@ -14,69 +14,69 @@ class Style_Render {
 	}
 
 	public function render(): string {
-		$cssStyle = [];
+		$css_style = [];
 
-		foreach ( $this->styles as $styleDef ) {
-			$style = $this->render_style( $styleDef );
-			$cssStyle[] = $style;
+		foreach ( $this->styles as $style_def ) {
+			$style = $this->render_style( $style_def );
+			$css_style[] = $style;
 		}
 
-		return implode( '', $cssStyle );
+		return implode( '', $css_style );
 	}
 
 	private function render_style( array $style ): string {
-		$baseSelector = $this->get_base_selector( $style );
+		$base_selector = $this->get_base_selector( $style );
 
-		if ( !$baseSelector ) {
+		if ( ! $base_selector ) {
 			return '';
 		}
 
 		$stylesheet = [];
 
-		foreach ( $style[ 'variants' ] as $variant ) {
-			$styleDeclaration = $this->variant_to_style_declaration( $baseSelector, $variant );
+		foreach ( $style['variants'] as $variant ) {
+			$style_declaration = $this->variant_to_style_declaration( $base_selector, $variant );
 
-			if ( $styleDeclaration ) {
-				$stylesheet[] = $styleDeclaration;
+			if ( $style_declaration ) {
+				$stylesheet[] = $style_declaration;
 			}
 		}
 
 		return implode( '', $stylesheet );
 	}
 
-	private function get_base_selector( array $styleDef ): string {
+	private function get_base_selector( array $style_def ): string {
 		$map = [
 			'class' => '.',
 		];
 
-		return $map[ $styleDef[ 'type' ] ] . $styleDef[ 'id' ];
+		return $map[ $style_def['type'] ] . $style_def['id'];
 	}
 
-	private function variant_to_style_declaration( string $baseSelector, array $variant ): string {
-		$css = $this->props_to_css( $variant[ 'props' ] );
+	private function variant_to_style_declaration( string $base_selector, array $variant ): string {
+		$css = $this->props_to_css( $variant['props'] );
 
-		if ( !$css ) {
+		if ( ! $css ) {
 			return '';
 		}
 
-		$state = isset( $variant[ 'meta' ][ 'state' ] ) ? ':' . $variant[ 'meta' ][ 'state' ] : '';
-		$selector = $baseSelector . $state;
+		$state = isset( $variant['meta']['state'] ) ? ':' . $variant['meta']['state'] : '';
+		$selector = $base_selector . $state;
 
-		$styleDeclaration = $selector . '{' . $css . '}';
+		$style_declaration = $selector . '{' . $css . '}';
 
-		if ( isset( $variant[ 'meta' ][ 'breakpoint' ] ) ) {
-			$styleDeclaration = $this->wrap_with_media_query( $variant[ 'meta' ][ 'breakpoint' ], $styleDeclaration );
+		if ( isset( $variant['meta']['breakpoint'] ) ) {
+			$style_declaration = $this->wrap_with_media_query( $variant['meta']['breakpoint'], $style_declaration );
 		}
 
-		return $styleDeclaration;
+		return $style_declaration;
 	}
 
 	private function props_to_css( array $props ): string {
 		$css = [];
 
-		foreach ( $props as $cssProp => $cssValue ) {
-			$prop = $this->camel_case_to_dash( $cssProp );
-			$value = $this->transform_value( $cssValue );
+		foreach ( $props as $css_prop => $css_value ) {
+			$prop = $this->camel_case_to_dash( $css_prop );
+			$value = $this->transform_value( $css_value );
 			$css[] = $prop . ':' . $value;
 		}
 
@@ -93,17 +93,17 @@ class Style_Render {
 	}
 
 	private function get_breakpoint_size( array $breakpoint ): ?string {
-		return isset( $breakpoint[ 'type' ] ) ? $breakpoint[ 'type' ] . ':' . $breakpoint[ 'width' ] . 'px' : null;
+		return isset( $breakpoint['type'] ) ? $breakpoint['type'] . ':' . $breakpoint['width'] . 'px' : null;
 	}
 
 	private function transform_value( $value ) {
 		if ( is_array( $value ) ) {
 			if (
-				isset( $value[ '$$type' ] ) &&
-				isset( $this->transformers[ $value[ '$$type' ] ] ) &&
-				is_callable( $this->transformers[ $value[ '$$type' ] ] )
+				isset( $value['$$type'] ) &&
+				isset( $this->transformers[ $value['$$type'] ] ) &&
+				is_callable( $this->transformers[ $value['$$type'] ] )
 			) {
-				return $this->transformers[ $value[ '$$type' ] ]( $value[ 'value' ] );
+				return $this->transformers[ $value['$$type'] ]( $value['value'] );
 			}
 			return 'unset';
 		}
