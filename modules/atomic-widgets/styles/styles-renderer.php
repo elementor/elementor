94814a37b9fe9250fd36c03d5aa18e75e26f9fa8
Ron Rostovsky <ronros@elementor.com>
2024-08-27 11:26:54 +0300

    changed to fn

--- modules/atomic-widgets/styles/styles-renderer.php
+++ modules/atomic-widgets/styles/styles-renderer.php
@@ -148,9 +148,10 @@ class Styles_Renderer {
 		$transformer = $this->get_transformer( $value['$$type'] );
 
 		if ( $transformer ) {
-			$transformed = $transformer( $value['value'], function( $value ) {
-				 return $this->transform_value( $value );
-			});
+			$transformed = $transformer(
+				$value['value'],
+				fn ( $value ) => $this->transform_value( $value )
+			);
 			return $this->transform_value( $transformed );
 		}
 
