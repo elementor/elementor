5c37ae8be0030724393929752e83405ef40f6ae9
Ronen Boxer <ronenb@elementor.com>
2024-08-28 00:54:24 +0300

    Test fix try #1

--- modules/checklist/data/controller.php
+++ modules/checklist/data/controller.php
@@ -18,6 +18,11 @@ class Controller extends Controller_Base {
 		$this->register_endpoint( new Steps( $this ) );
 	}
 
+	// Bypass, currently not required.
+	protected function register_index_endpoint() {
+		parent::register_index_endpoint();
+	}
+
 	public function get_item_permissions_check( $request ) {
 		return current_user_can( 'manage_options' );
 	}
