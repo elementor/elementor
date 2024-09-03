1d1268fdbff5cd9a0a5b02fc6d67f0358360f160
mugur <mugurell@elementor.red>
2024-09-03 19:35:30 +0300

    Internal: Added assign homepage step [ED-15447]

---
+++ tests/phpunit/elementor/modules/checklist/classes/test-assign-homepage.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Elementor\Tests\Phpunit\Elementor\Modules\Checklist\Classes;
+
+use Elementor\Modules\Checklist\Steps\Assign_Homepage;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+class Test_Assign_Homepage_Step extends Step_Test_Base {
+	public function setUp(): void {
+		$this->set_wordpress_adapter_mock( [ 'show_on_front' ], [
+			'show_on_front' => 'posts',
+		] );
+		parent::setUp();
+	}
+
+	public function tearDown(): void {
+		parent::tearDown();
+	}
+
+	public function test__step_is_completed_when_homepage_is_assigned() {
+		$steps_manager = $this->checklist_module->get_steps_manager();
+		$step = $steps_manager->get_step_by_id( Assign_Homepage::STEP_ID );
+		$this->assertFalse( $step->is_marked_as_completed() );
+		$this->assertFalse( $step->is_immutable_completed() );
+		$this->assertFalse( $step->is_absolute_completed() );
+
+		$step->mark_as_completed();
+		$this->assertTrue( $step->is_marked_as_completed() );
+		$this->assertFalse( $step->is_immutable_completed() );
+		$this->assertFalse( $step->is_absolute_completed() );
+
+		$step->unmark_as_completed();
+		$this->assertFalse( $step->is_marked_as_completed() );
+		$this->assertFalse( $step->is_immutable_completed() );
+		$this->assertFalse( $step->is_absolute_completed() );
+
+		$step = new Assign_Homepage( $this->checklist_module, $this->wordpress_adapter );
+		$this->assertFalse( $step->is_absolute_completed() );
+
+		$this->set_wordpress_adapter_mock( [ 'show_on_front' ], [
+			'show_on_front' => 'page',
+		] );
+
+		$step = new Assign_Homepage( $this->checklist_module, $this->wordpress_adapter );
+		$this->assertFalse( $step->is_marked_as_completed() );
+		$this->assertFalse( $step->is_immutable_completed() );
+		$this->assertTrue( $step->is_absolute_completed() );
+	}
+
+}
