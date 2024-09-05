08e23c59225063464b7c65f7a34593c6655deb3a
Ronen Boxer <ronenb@elementor.com>
2024-09-06 02:31:47 +0300

    Fix: Replace new editor url mechanism

--- modules/checklist/steps/create-pages.php
+++ modules/checklist/steps/create-pages.php
@@ -2,7 +2,7 @@
 
 namespace Elementor\Modules\Checklist\Steps;
 
-use Elementor\Plugin;
+use Elementor\Core\DocumentTypes\Page;
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
@@ -37,7 +37,7 @@ class Create_Pages extends Step_Base {
 	}
 
 	public function get_cta_url() : string {
-		return Plugin::$instance->documents->get_create_new_post_url( 'page' );
+		return Page::get_site_settings_url_config()['url'];
 	}
 
 	public function get_learn_more_url() : string {
