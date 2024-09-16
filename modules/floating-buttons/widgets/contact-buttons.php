5af44a7b40370ba7779a70f0da1bd379532d5ae0
Hein van Vlastuin <94352322+hein-obox@users.noreply.github.com>
2024-09-16 12:17:06 +0200

    Fix: Icons are not showing inside the new widgets when the icon experiment is inactive [ED-15773] (#28703)

--- modules/floating-buttons/widgets/contact-buttons.php
+++ modules/floating-buttons/widgets/contact-buttons.php
@@ -25,8 +25,4 @@ class Contact_Buttons extends Widget_Contact_Button_Base {
 	public function get_title(): string {
 		return esc_html__( 'Single Chat', 'elementor' );
 	}
-
-	public function get_style_depends(): array {
-		return [ 'widget-floating-buttons' ];
-	}
 }
