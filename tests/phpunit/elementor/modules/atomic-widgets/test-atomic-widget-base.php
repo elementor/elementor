0ea1716458fc97e1afa6f8840da1b3f9b3466e54
Nevo Golan <20494189+Nevoss@users.noreply.github.com>
2024-09-17 09:40:36 +0300

    Internal: Dynamic Transformer (#28665)

--- tests/phpunit/elementor/modules/atomic-widgets/test-atomic-widget-base.php
+++ tests/phpunit/elementor/modules/atomic-widgets/test-atomic-widget-base.php
@@ -2,6 +2,7 @@
 
 namespace Elementor\Testing\Modules\AtomicWidgets;
 
+use Elementor\Core\DynamicTags\Tag;
 use Elementor\Modules\AtomicWidgets\Base\Atomic_Widget_Base;
 use Elementor\Modules\AtomicWidgets\Controls\Section;
 use Elementor\Modules\AtomicWidgets\Controls\Types\Select_Control;
@@ -12,6 +13,7 @@ use Elementor\Modules\AtomicWidgets\PropTypes\Image_Prop_Type;
 use Elementor\Modules\AtomicWidgets\PropTypes\Number_Prop_Type;
 use Elementor\Modules\AtomicWidgets\PropTypes\Prop_Type;
 use Elementor\Modules\AtomicWidgets\PropTypes\String_Prop_Type;
+use Elementor\Plugin;
 use ElementorEditorTesting\Elementor_Test_Base;
 
 if ( ! defined( 'ABSPATH' ) ) {
@@ -23,72 +25,147 @@ class Test_Atomic_Widget_Base extends Elementor_Test_Base {
 	/**
 	 * @dataProvider get_atomic_settings_data_provider
 	 */
-	public function test_get_atomic_settings( $prop_types, $settings, $result ) {
+	public function test_get_atomic_settings( $args, $arrange_cb = null ) {
 		// Arrange.
+		$cleanup = $arrange_cb ? $arrange_cb() : fn() => null;
+
 		$widget = $this->make_mock_widget( [
-			'props_schema' => $prop_types,
-			'settings' => $settings,
+			'props_schema' => $args['prop_types'],
+			'settings' => $args['settings'],
 		] );
 
 		// Act.
 		$settings = $widget->get_atomic_settings();
 
 		// Assert.
-		$this->assertSame( $result, $settings );
+		$this->assertSame( $args['result'], $settings );
+
+		$cleanup();
 	}
 
 	public function get_atomic_settings_data_provider() {
 		return [
 			'basic' => [
-				'prop_types' => [
-					'text' => String_Prop_Type::make()->default( 'The greatest text' ),
-					'tag' => String_Prop_Type::make()->default( 'h2' ),
-				],
-				'settings' => [
-					'text' => 'This text is more great than the greatest text',
-					'invalid_prop' => 'This prop is not in the schema',
-				],
-				'result' => [
-					'text' => 'This text is more great than the greatest text',
-					'tag' => 'h2',
-				],
+				'args' => [
+					'prop_types' => [
+						'text' => String_Prop_Type::make()->default( 'The greatest text' ),
+						'tag' => String_Prop_Type::make()->default( 'h2' ),
+					],
+					'settings' => [
+						'text' => 'This text is more great than the greatest text',
+						'invalid_prop' => 'This prop is not in the schema',
+					],
+					'result' => [
+						'text' => 'This text is more great than the greatest text',
+						'tag' => 'h2',
+					],
+				]
 			],
 			'cannot transform value' => [
-				'prop_types' => [
-					'text' => String_Prop_Type::make()->default( 'Not transformable' ),
-				],
-				'settings' => [
-					'text' => [
-						'$$type' => 'not_transformable',
-						'value' => 'Not transformable',
+				'args' => [
+					'prop_types' => [
+						'text' => String_Prop_Type::make()->default( 'Not transformable' ),
 					],
-				],
-				'result' => [
-					'text' => null,
-				],
+					'settings' => [
+						'text' => [
+							'$$type' => 'not_transformable',
+							'value' => 'Not transformable',
+						],
+					],
+					'result' => [
+						'text' => null,
+					],
+				]
 			],
 			'transform classes' => [
-				'prop_types' => [
-					'classes' => Classes_Prop_Type::make()->default( [] ),
-					'inner_classes' => Classes_Prop_Type::make()->default( [] ),
-					'outer_classes' => Classes_Prop_Type::make()->default( [] ),
-				],
-				'settings' => [
-					'classes' => [
-						'$$type' => 'classes',
-						'value' => [ 'one', 'two', 'three' ],
+				'args' => [
+					'prop_types' => [
+						'classes' => Classes_Prop_Type::make()->default( [] ),
+						'inner_classes' => Classes_Prop_Type::make()->default( [] ),
+						'outer_classes' => Classes_Prop_Type::make()->default( [] ),
 					],
-					'outer_classes' => [
-						'$$type' => 'classes',
-						'value' => 111, // Invalid value for classes
+					'settings' => [
+						'classes' => [
+							'$$type' => 'classes',
+							'value' => [ 'one', 'two', 'three' ],
+						],
+						'outer_classes' => [
+							'$$type' => 'classes',
+							'value' => 111, // Invalid value for classes
+						],
 					],
-				],
-				'result' => [
-					'classes' => 'one two three',
-					'inner_classes' => '',
-					'outer_classes' => null,
+					'result' => [
+						'classes' => 'one two three',
+						'inner_classes' => '',
+						'outer_classes' => null,
+					],
 				],
 			],
+			'transform dynamic' => [
+				'args' => [
+					'prop_types' => [
+						'text' => String_Prop_Type::make()->default( 'Cool cool cool' ),
+						'text_2' => String_Prop_Type::make(),
+						'invalid_name' => String_Prop_Type::make(),
+						'invalid_settings' => String_Prop_Type::make(),
+					],
+					'settings' => [
+						'text' => [
+							'$$type' => 'dynamic',
+							'value' => [
+								'name' => 'dynamic-tag',
+								'settings' => [
+									'before' => 'Before text - '
+								],
+							],
+						],
+						'text_2' => [
+							'$$type' => 'dynamic',
+							'value' => [
+								'name' => 'not-exist-dynamic-tag',
+								'settings' => []
+							],
+						],
+						'invalid_name' => [
+							'$$type' => 'dynamic',
+							'value' => [
+								'name' => 123,
+								'settings' => []
+							],
+						],
+						'invalid_settings' => [
+							'$$type' => 'dynamic',
+							'value' => [
+								'name' => 'dynamic-tag',
+								'settings' => 'Invalid Before - ',
+							],
+						],
+					],
+					'result' => [
+						'text' => 'Before text - Dynamic tag content',
+						'text_2' => null,
+						'invalid_name' => null,
+						'invalid_settings' => null,
+					],
+				],
+				'arrange_cb' => function() {
+					Plugin::$instance->dynamic_tags->register(
+						new class() extends Tag {
+							public function get_name() { return 'dynamic-tag'; }
+							public function get_group() { return 'dynamic'; }
+							public function get_categories() { return [ 'text' ]; }
+							public function get_title() { return 'Dynamic Tag'; }
+							public function get_content( array $options = [] ) {
+								$settings = $this->get_settings();
+
+								return "{$settings['before']}Dynamic tag content";
+							}
+						}
+					);
+
+					return fn() => Plugin::$instance->dynamic_tags->unregister( 'dynamic-tag' );
+				},
+			]
 		];
 	}
 
