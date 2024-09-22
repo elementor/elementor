4e992444247486da4fdd3f86a37469a78952b128
Ronen Boxer <ronenb@elementor.com>
2024-09-22 15:02:37 +0300

    Response fix attempt #2

--- tests/playwright/sanity/modules/checklist/helper.ts
+++ tests/playwright/sanity/modules/checklist/helper.ts
@@ -55,7 +55,7 @@ export class ChecklistHelper {
 			: this.page;
 
 		await frame.locator( selectors.toggleExpandButton ).click();
-		await this.page.waitForResponse( ( response ) => response.request().url().includes( 'checklist/user-progress' ), { timeout: 30000 } );
+		await this.page.waitForResponse( new RegExp( 'checklist/user-progress' ), { timeout: 30000 } );
 		await frame.locator( `${ selectors.toggleExpandButton }[aria-expanded="${ shouldExpand.toString() }"]` ).waitFor();
 	}
 
