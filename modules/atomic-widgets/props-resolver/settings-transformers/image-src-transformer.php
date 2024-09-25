36fcc04de009ab0b8edca186b7cddf2171162b97
StyleShit <32631382+StyleShit@users.noreply.github.com>
2024-09-25 13:46:11 +0300

    wip

--- modules/atomic-widgets/props-resolver/settings-transformers/image-src-transformer.php
+++ modules/atomic-widgets/props-resolver/settings-transformers/image-src-transformer.php
@@ -14,6 +14,9 @@ class Image_Src_Transformer extends Transformer_Base {
 		return Image_Src_Prop_Type::get_key();
 	}
 
+	// This transformer (or rather this prop type) exists only to support dynamic images.
+	// Currently, the dynamic tags that return images return it with id & url no matter
+	// what, so we need to keep the same structure in the props.
 	public function transform( $value ) {
 		return [
 			'id' => isset( $value['id'] ) ? (int) $value['id'] : null,
