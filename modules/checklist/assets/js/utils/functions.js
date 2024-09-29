1e5177611c09963d320cab2a70f18b6260618329
Ronen Boxer <145427686+ronenelementor@users.noreply.github.com>
2024-09-29 23:05:51 +0300

    Internal: Trigger the Checklist on Editor for the 2nd visit [ED-15641] (#28817)
    Co-authored-by: ElementorBot <48412871+elementorbot@users.noreply.github.com>

--- modules/checklist/assets/js/utils/functions.js
+++ modules/checklist/assets/js/utils/functions.js
@@ -1,4 +1,4 @@
-import { STEP } from './consts';
+import { STEP, STEPS_ROUTE, USER_PROGRESS_ROUTE } from './consts';
 
 const { IS_MARKED_COMPLETED, IS_ABSOLUTE_COMPLETED, IS_IMMUTABLE_COMPLETED, PROMOTION_DATA } = STEP;
 
@@ -8,6 +8,26 @@ export function isStepChecked( step ) {
 
 export function toggleChecklistPopup() {
 	$e.run( 'checklist/toggle-popup' );
+}
+
+export async function fetchSteps() {
+	const response = await $e.data.get( STEPS_ROUTE, {}, { refresh: true } );
+
+	return response?.data?.data || null;
+}
+
+export async function fetchUserProgress() {
+	const response = await $e.data.get( USER_PROGRESS_ROUTE, {}, { refresh: true } );
+
+	return response?.data?.data || null;
+}
+
+export async function updateStep( id, data ) {
+	return await $e.data.update( STEPS_ROUTE, { id, ...data }, { id } );
+}
+
+export async function updateUserProgress( data ) {
+	return await $e.data.update( USER_PROGRESS_ROUTE, data );
 }
 
 export function getAndUpdateStep( id, step, key, value ) {
