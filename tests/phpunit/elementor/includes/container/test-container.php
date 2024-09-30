e7806364c3e13870cbc0bf0643b513153d22a5ee
Batsirai Muchareva <batsiraim@elementor.red>
2024-09-30 11:36:50 +0200

    wip

--- tests/phpunit/elementor/includes/container/test-container.php
+++ tests/phpunit/elementor/includes/container/test-container.php
@@ -4,7 +4,10 @@ namespace Elementor\Tests\Phpunit\Includes\Container;
 
 use ElementorEditorTesting\Elementor_Test_Base;
 
-class Test_Container extends Elementor_Test_Base {
+class Test_Container extends Elementor_Test_Base
 {
-
+	public function test_get()
+	{
+		$this->assertTrue(true);
+	}
 }
