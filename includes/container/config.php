5203a589ed8c8de33fb5911acdcaf78aeeabc39b
Batsirai Muchareva <batsiraim@elementor.red>
2024-10-01 12:57:12 +0200

    linting

--- includes/container/config.php
+++ includes/container/config.php
@@ -1,12 +1,8 @@
 <?php
 
-use Elementor\Core\Isolation\Wordpress_Adapter;
-use Elementor\Core\Isolation\Wordpress_Adapter_Interface;
-
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
-
 
 /**
  * Elementor Dependency Injection Configuration.
