fe68056e9a47903f53ea3c9398071a51734ce28f
Hein van Vlastuin <94352322+hein-obox@users.noreply.github.com>
2024-10-03 10:06:40 +0200

    Internal: Remove Nested Accordion module from module.js file [ED-15860] (#28869)
    ## PR Checklist
    <!-- 
    Please check if your PR fulfills the following requirements:
    **Filling out the template is required.** Any pull request that does not
    include enough information to be reviewed in a timely manner may be
    closed at the maintainers' discretion.
     -->
    - [ ] The commit message follows our guidelines:
    https://github.com/elementor/elementor/blob/master/.github/CONTRIBUTING.md
    
    
    ## PR Type
    What kind of change does this PR introduce?
    <!-- Please check the one that applies to this PR using "x" with no
    spaces eg: [x]. -->
    - [ ] Bugfix
    - [ ] Feature
    - [ ] Code style update (formatting, local variables)
    - [ ] Refactoring (no functional changes, no api changes)
    - [ ] Build related changes
    - [ ] CI related changes
    - [ ] Documentation content changes
    - [ ] Other... Please describe:
    
    ## Summary
    
    This PR can be summarized in the following changelog entry:
    
    *
    
    ## Description
    An explanation of what is done in this PR
    
    *
    
    ## Test instructions
    This PR can be tested by following these steps:
    
    *
    
    ## Quality assurance
    
    - [ ] I have tested this code to the best of my abilities
    - [ ] I have added unittests to verify the code works as intended
    - [ ] Docs have been added / updated (for bug fixes / features)
    
    Fixes #
    
    Co-authored-by: Hein van Vlastuin <heinvanvlastuin@gmail.com>

--- assets/dev/js/frontend/modules.js
+++ assets/dev/js/frontend/modules.js
@@ -6,7 +6,6 @@ import BaseHandler from './handlers/base';
 import SwiperBase from './handlers/base-swiper';
 import CarouselBase from './handlers/base-carousel';
 import NestedTabs from 'elementor/modules/nested-tabs/assets/js/frontend/handlers/nested-tabs';
-import NestedAccordion from 'elementor/modules/nested-accordion/assets/js/frontend/handlers/nested-accordion';
 import ContactButtonsHandler from 'elementor/modules/floating-buttons/assets/js/floating-buttons/frontend/handlers/contact-buttons';
 import FloatingBarsHandler from 'elementor/modules/floating-buttons/assets/js/floating-bars/frontend/handlers/floating-bars';
 import NestedTitleKeyboardHandler from './handlers/accessibility/nested-title-keyboard-handler';
@@ -22,7 +21,6 @@ elementorModules.frontend = {
 		SwiperBase,
 		CarouselBase,
 		NestedTabs,
-		NestedAccordion,
 		NestedTitleKeyboardHandler,
 		ContactButtonsHandler,
 		FloatingBarsHandler,
