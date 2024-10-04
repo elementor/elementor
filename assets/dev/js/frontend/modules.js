fd05cc93f79d353bd4c059413e3b7725bfe1efc3
Hein van Vlastuin <heinvanvlastuin@gmail.com>
2024-10-04 09:05:28 +0200

    wip

--- assets/dev/js/frontend/modules.js
+++ assets/dev/js/frontend/modules.js
@@ -5,7 +5,6 @@ import StretchedElement from './handlers/stretched-element';
 import BaseHandler from './handlers/base';
 import SwiperBase from './handlers/base-swiper';
 import CarouselBase from './handlers/base-carousel';
-// import NestedTabs from 'elementor/modules/nested-tabs/assets/js/frontend/handlers/nested-tabs';
 import ContactButtonsHandler from 'elementor/modules/floating-buttons/assets/js/floating-buttons/frontend/handlers/contact-buttons';
 import FloatingBarsHandler from 'elementor/modules/floating-buttons/assets/js/floating-bars/frontend/handlers/floating-bars';
 
