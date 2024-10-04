908effff2986c1ae4f31bc2e46f0f760cc836cf3
Hein van Vlastuin <heinvanvlastuin@gmail.com>
2024-10-04 09:04:48 +0200

    wip

--- assets/dev/js/frontend/modules.js
+++ assets/dev/js/frontend/modules.js
@@ -5,7 +5,7 @@ import StretchedElement from './handlers/stretched-element';
 import BaseHandler from './handlers/base';
 import SwiperBase from './handlers/base-swiper';
 import CarouselBase from './handlers/base-carousel';
-import NestedTabs from 'elementor/modules/nested-tabs/assets/js/frontend/handlers/nested-tabs';
+// import NestedTabs from 'elementor/modules/nested-tabs/assets/js/frontend/handlers/nested-tabs';
 import ContactButtonsHandler from 'elementor/modules/floating-buttons/assets/js/floating-buttons/frontend/handlers/contact-buttons';
 import FloatingBarsHandler from 'elementor/modules/floating-buttons/assets/js/floating-bars/frontend/handlers/floating-bars';
 
@@ -24,6 +24,10 @@ elementorModules.frontend = {
 	},
 };
 
-if ( ! elementorCommon.config.experimentalFeatures.update_core_js_modules_loading ) {
-	elementorModules.frontend.handlers.NestedTabs = NestedTabs;
+const isEditor = ! window.elementorCommon;
+
+// TODO: Remove in v3.27.0 [ED-15717]
+// Load NestedTabs module only when older Pro versions are active.
+if ( isEditor || ! elementorCommon.config.experimentalFeatures.update_core_js_modules_loading ) {
+	elementorModules.frontend.handlers.NestedTabs = () => import( /* webpackChunkName: 'nested-tabs-module' */ 'elementor/modules/nested-tabs/assets/js/frontend/handlers/nested-tabs' );
 }
--- modules/nested-tabs/assets/js/frontend/handlers/nested-tabs.js
+++ modules/nested-tabs/assets/js/frontend/handlers/nested-tabs.js
@@ -460,5 +460,3 @@ export default class NestedTabs extends Base {
 		};
 	}
 }
-
-window.NestedTabs = NestedTabs;
