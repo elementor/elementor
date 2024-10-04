6ead1fe628628912b1898ddc607673319ea4eda1
Hein van Vlastuin <heinvanvlastuin@gmail.com>
2024-10-04 10:52:19 +0200

    Remove redundant modules

--- assets/dev/js/frontend/modules.js
+++ assets/dev/js/frontend/modules.js
@@ -7,8 +7,6 @@ import SwiperBase from './handlers/base-swiper';
 import CarouselBase from './handlers/base-carousel';
 import NestedTabs from 'elementor/modules/nested-tabs/assets/js/frontend/handlers/nested-tabs';
 import NestedAccordion from 'elementor/modules/nested-accordion/assets/js/frontend/handlers/nested-accordion';
-import ContactButtonsHandler from 'elementor/modules/floating-buttons/assets/js/floating-buttons/frontend/handlers/contact-buttons';
-import FloatingBarsHandler from 'elementor/modules/floating-buttons/assets/js/floating-bars/frontend/handlers/floating-bars';
 import NestedTitleKeyboardHandler from './handlers/accessibility/nested-title-keyboard-handler';
 
 elementorModules.frontend = {
@@ -24,7 +22,5 @@ elementorModules.frontend = {
 		NestedTabs,
 		NestedAccordion,
 		NestedTitleKeyboardHandler,
-		ContactButtonsHandler,
-		FloatingBarsHandler,
 	},
 };
