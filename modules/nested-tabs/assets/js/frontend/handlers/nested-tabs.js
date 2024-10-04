56042917b6b0fed3ac1c351c496c20cdd17ca1d3
Hein van Vlastuin <heinvanvlastuin@gmail.com>
2024-10-04 12:40:52 +0200

    lint

--- modules/nested-tabs/assets/js/frontend/handlers/nested-tabs.js
+++ modules/nested-tabs/assets/js/frontend/handlers/nested-tabs.js
@@ -272,6 +272,7 @@ export default class NestedTabs extends Base {
 			import( /* webpackChunkName: 'nested-title-keyboard-handler' */ 'elementor-frontend/handlers/accessibility/nested-title-keyboard-handler' ).then( ( { default: NestedTitleKeyboardHandler } ) => {
 				new NestedTitleKeyboardHandler( this.getKeyboardNavigationSettings() );
 			} ).catch( ( error ) => {
+				// eslint-disable-next-line no-console
 				console.error( 'Error importing module:', error );
 			} );
 		}
