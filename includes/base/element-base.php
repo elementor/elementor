753a25adf400a142ab09bef5d4bcd798acf9b4b5
Hein van Vlastuin <heinvanvlastuin@gmail.com>
2024-10-07 17:53:47 +0200

    wip

--- includes/base/element-base.php
+++ includes/base/element-base.php
@@ -179,10 +179,6 @@ abstract class Element_Base extends Controls_Stack {
 		];
 
 		foreach ( $this->get_script_depends() as $script ) {
-			if ( 'imagesloaded' !== $script ) {
-				echo 'not images';
-			}
-
 			if ( isset( $deprecated_scripts[ $script ] ) ) {
 				Utils::handle_deprecation( $script, $deprecated_scripts[ $script ]['version'], $deprecated_scripts[ $script ]['replacement'] );
 			}
