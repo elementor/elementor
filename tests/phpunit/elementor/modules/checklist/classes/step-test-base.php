7055b45043a40e9fa5e91416c452f5462983d081
Ronen Boxer <ronenb@elementor.com>
2024-10-08 00:05:58 +0300

    Test progress #26

--- tests/phpunit/elementor/modules/checklist/classes/step-test-base.php
+++ tests/phpunit/elementor/modules/checklist/classes/step-test-base.php
@@ -200,6 +200,10 @@ abstract class Step_Test_Base extends PHPUnit_TestCase {
 
 		$this->set_kit_adapter_mock( [ 'is_active_kit_default' => $kit_key === self::DEFAULT_KIT ], true );
 
+		if ( $this->checklist_module->should_switch_preferences_off() ) {
+			$this->set_user_preference_switch( false );
+		}
+
 		return $this;
 	}
 
