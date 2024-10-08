f8997488ea2cade58ccf477bc487f5628a0f3a38
Hein van Vlastuin <heinvanvlastuin@gmail.com>
2024-10-08 10:54:30 +0200

    wip

--- includes/wordpress/script-modules.php
+++ includes/wordpress/script-modules.php
@@ -3,7 +3,7 @@ namespace Elementor\Includes\WordPress;
 
 use Elementor\Includes\WordPress\WP_Script_Modules;
 
-function wp_script_modules(): WP_Script_Modules {
+function custom_wp_script_modules(): WP_Script_Modules {
 	global $wp_script_modules;
 
 	if ( ! ( $wp_script_modules instanceof WP_Script_Modules ) ) {
@@ -13,10 +13,10 @@ function wp_script_modules(): WP_Script_Modules {
 	return $wp_script_modules;
 }
 
-function wp_register_script_module( string $id, string $src, array $deps = array(), $version = false ) {
-	wp_script_modules()->register( $id, $src, $deps, $version );
+function  custom_wp_register_script_module( string $id, string $src, array $deps = array(), $version = false ) {
+	custom_wp_script_modules()->register( $id, $src, $deps, $version );
 }
 
-function wp_enqueue_script_module( string $id, string $src = '', array $deps = array(), $version = false ) {
-	wp_script_modules()->enqueue( $id, $src, $deps, $version );
+function  custom_wp_enqueue_script_module( string $id, string $src = '', array $deps = array(), $version = false ) {
+	custom_wp_script_modules()->enqueue( $id, $src, $deps, $version );
 }
