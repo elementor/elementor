cc210e7ff0edb651817ae548761df7556f72edfc
Ronen Boxer <ronenb@elementor.com>
2024-10-08 12:23:13 +0300

    wip

--- tests/phpunit/elementor/modules/checklist/classes/test-kit-installation.php
+++ tests/phpunit/elementor/modules/checklist/classes/test-kit-installation.php
@@ -11,7 +11,7 @@ class Test_Kit_Installation extends Step_Test_Base {
 	const SHOULD_SWITCH_PREFERENCE_OFF = 'preference_switch_off';
 	const SHOULD_AUTO_OPEN_POPUP = 'popup_auto_open';
 
-	private $preferences_mock = [];
+	private $user_preferences_mock = [];
 
 	public function setUp(): void {
 		$this->set_kit_adapter_mock( [ 'is_active_kit_default' => true ] )
@@ -28,16 +28,33 @@ class Test_Kit_Installation extends Step_Test_Base {
 		$this->mock_editor_visit();
 
 		$this->set_wordpress_adapter_mock( [], [
-			'set_user_preferences' => function ( $key, $value ) {
-				$this->preferences_mock[ $key ] = $value;
-			},
-			'get_user_preferences' => function ( $key ) {
-				return $this->preferences_mock[ $key ] ?? null;
-			}
+			'set_user_preferences' => [ $this, 'set_user_preferences' ],
+			'get_user_preferences' => [ $this, 'get_user_preferences' ],
 		], true );
 
-		$this->assertFalse( $this->checklist_module->is_preference_switch_on() );
+		$this->assertTrue( $this->checklist_module->is_preference_switch_on() );
+		var_dump( $this->user_preferences_mock );
 	}
 
+	public function test__custom_kit_installed() {
+		parent::setup();
 
+		$this->set_kit_adapter_mock( [ 'is_active_kit_default' => false ] )
+			->set_counter_adapter_mock( [ 'get_count' => 0 ] )
+			->set_wordpress_adapter_mock( [], [
+				'set_user_preferences' => [ $this, 'set_user_preferences' ],
+				'get_user_preferences' => [ $this, 'get_user_preferences' ],
+				] );
+
+		$this->assertFalse( $this->checklist_module->is_preference_switch_on() );
+		var_dump( $this->user_preferences_mock );
+	}
+
+	private function set_user_preferences( $key, $value ) {
+		$this->user_preferences_mock[ $key ] = $value;
+	}
+
+	private function get_user_preferences( $key ) {
+		return $this->user_preferences_mock[ $key ] ?? null;
+	}
 }
