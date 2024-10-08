d06d0e496069854fba3087d9ca34e9335a6d7337
Hein van Vlastuin <heinvanvlastuin@gmail.com>
2024-10-08 11:08:25 +0200

    Auto stash before merge of "internal/ED-15863-script-module" and "elementor/internal/ED-15863-script-module"

--- core/isolation/wordpress-adapter.php
+++ core/isolation/wordpress-adapter.php
@@ -64,7 +64,7 @@ class Wordpress_Adapter implements Wordpress_Adapter_Interface {
 	public function wp_register_script_module(  string $id, string $src, array $deps = array(), $version = false ) : void {
 		if ( version_compare( get_bloginfo( 'version' ), '6.5', '<' ) ) {
 			require_once ELEMENTOR_PATH . 'includes/wordpress/script-modules.php';
-			custom_wp_register_script_module( $id, $src, $deps, $version );
+			\Elementor\Includes\WordPress\custom_wp_register_script_module( $id, $src, $deps, $version );
 			return;
 		}
 
@@ -74,7 +74,7 @@ class Wordpress_Adapter implements Wordpress_Adapter_Interface {
 	public function wp_enqueue_script_module( string $id, string $src = '', array $deps = array(), $version = false ) : void {
 		if ( version_compare( get_bloginfo( 'version' ), '6.5', '<' ) ) {
 			require_once ELEMENTOR_PATH . 'includes/wordpress/script-modules.php';
-			custom_wp_enqueue_script_module( $id, $src, $deps, $version );
+			\Elementor\Includes\WordPress\custom_wp_enqueue_script_module( $id, $src, $deps, $version );
 			return;
 		}
 
