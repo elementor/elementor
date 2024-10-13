7c499bda2a64e019a4d20b36c4b2324a3cfb27ba
David Seveloff <davids@elementor.com>
2024-10-13 17:58:06 +0300

    Internal: Fix Dependency Injection template file [ED-15947]

--- php-scoper/dependency-injection.inc.php
+++ php-scoper/dependency-injection.inc.php
@@ -1,6 +1,6 @@
 <?php
 
-declare( strict_types= 1 );
+declare( strict_types=1 );
 
 use Isolated\Symfony\Component\Finder\Finder;
 
@@ -13,5 +13,21 @@ return [
 	],
 	'exclude-files' => [
 		'vendor/php-di/php-di/src/Compiler/Template.php',
+	],
+	'patchers' => [
+		/**
+		 * Replaces the Adapter string references with the prefixed versions.
+		 *
+		 * @param string $filePath The path of the current file.
+		 * @param string $prefix   The prefix to be used.
+		 * @param string $content  The content of the specific file.
+		 *
+		 * @return string The modified content.
+		 */
+		function( $file_path, $prefix, $content ) {
+			if ( substr( $file_path, -46 ) !== 'vendor/php-di/php-di/src/Compiler/Template.php' ) {
+				return $content;
+			}
+		},
 	],
 ];
