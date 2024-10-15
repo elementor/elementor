f9f6cb865a3d6ef8f7d2d3455b0b33b56a447713
elchugreeva <elenac@elementor.com>
2024-10-15 13:50:03 +0200

    wip

--- modules/promotions/assets/js/react/components/promotion-card.js
+++ modules/promotions/assets/js/react/components/promotion-card.js
@@ -1,10 +1,8 @@
 import {
 	Image,
+	Box,
 	Chip,
-	CardMedia,
-	CardContent,
 	Typography,
-	CardActions,
 	Button,
 	CloseButton,
 	Stack,
@@ -25,28 +23,29 @@ const PromotionCard = ( { onClose, promotionsData } ) => {
 		<Box sx={ { width: 296 } }>
 			<Stack direction="row" alignItems="center" py={ 1 } px={ 2 }>
 				<Typography variant="subtitle2">{ title }</Typography>
-				<Chip label="PRO" size="small" variant="outlined" color="promotion" sx={ { ml: 1, fontWeight: 400 } } />
+				<Chip label="PRO" size="small" variant="outlined" color="promotion" sx={ { ml: 1 } } />
 				<CloseButton edge="end" sx={ { ml: 'auto' } } slotProps={ {
 					icon: {
 						fontSize: 'small',
 					},
 				} } onClose={ onClose } />
 			</Stack>
-			<Image src={ imgSrc } sx={ { height: 150 } }
+			<Image src={ imgSrc } sx={ { height: 150, width: '100%' } }
 			/>
-			<Box>
+			<Stack py={ 1 } px={ 2 }>
 				{ description.map( ( e, index ) => {
 					return ( <Typography key={ index } variant="body2" color="secondary">&bull; { e }</Typography> );
 				} ) }
-			</Box>
-			<Box sx={ { pt: 1 } }>
+			</Stack>
+			<Stack pt={ 1 } pb={ 1.5 } px={ 2 }>
 				<Button
 					variant="contained"
 					size="small"
 					color="promotion"
 					onClick={ redirectHandler }
+					sx={ { ml: 'auto' } }
 				>{ ctaText }</Button>
-			</Box>
+			</Stack>
 		</Box>
 	);
 };
