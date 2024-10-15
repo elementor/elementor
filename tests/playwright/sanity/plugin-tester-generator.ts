2fff30fab93b21ce6c4a3bd97ddd07759ebb3294
svitlanadykun <svitlanad@elementor.red>
2024-10-15 09:08:10 +0200

    remove timeout for goToPage

--- tests/playwright/sanity/plugin-tester-generator.ts
+++ tests/playwright/sanity/plugin-tester-generator.ts
@@ -84,13 +84,12 @@ export const generatePluginTests = ( testType: string ) => {
 
 				async function goToPage() {
 					await page.goto( '/law-firm-about/?elementor', {
-						timeout: 10000, // Reduced timeout for faster failure
-						waitUntil: 'load', // Ensures full page load (all resources)
+						waitUntil: 'load',
 					} );
 				}
 
 				await goToPage();
-				await editor.getPreviewFrame().getByRole( 'heading', { name: 'About Us' } ).waitFor( { timeout: 5000 } );
+				await editor.getPreviewFrame().getByRole( 'heading', { name: 'About Us' } ).waitFor( { timeout: 15000 } );
 
 				await wpAdmin.closeAnnouncementsIfVisible();
 
