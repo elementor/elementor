41ffad38b580a222b775c2b41a39bf028ff39c70
Louis Wolmarans <louiswol94@gmail.com>
2024-10-15 17:21:54 +0200

    Tweak: Optimized Dom [ED-15929] (#28967)

--- includes/widgets/accordion.php
+++ includes/widgets/accordion.php
@@ -104,6 +104,10 @@ class Widget_Accordion extends Widget_Base {
 		return ! Plugin::$instance->experiments->is_feature_active( 'nested-elements' );
 	}
 
+	public function has_widget_container(): bool {
+		return ! Plugin::$instance->experiments->is_feature_active( 'e_optimized_markup' );
+	}
+
 	/**
 	 * Register accordion widget controls.
 	 *
