e79610fc1216ad3d33f570acf1a1eee17d6182e7
Hein van Vlastuin <heinvanvlastuin@gmail.com>
2024-10-19 21:45:14 +0200

    WIP

--- assets/dev/js/frontend/modules.js
+++ assets/dev/js/frontend/modules.js
@@ -5,7 +5,6 @@ import StretchedElement from './handlers/stretched-element';
 import BaseHandler from './handlers/base';
 import SwiperBase from './handlers/base-swiper';
 import CarouselBase from './handlers/base-carousel';
-import NestedTabs from 'elementor/modules/nested-tabs/assets/js/frontend/handlers/nested-tabs';
 
 elementorModules.frontend = {
 	Document,
@@ -17,6 +16,15 @@ elementorModules.frontend = {
 		StretchedElement,
 		SwiperBase,
 		CarouselBase,
-		NestedTabs,
 	},
 };
+
+// TODO: Remove this check after Elementor 3.28 release.
+const isVersionLowerThan3Point28 = true;
+
+if ( !! document.querySelector( 'body.e-import-script-nested-tabs' ).length || isVersionLowerThan3Point28 ) {
+	( async () => {
+		const { default: NestedTabs } = await import( /* webpackChunkName: 'nested-tabs-module' */ 'elementor/modules/nested-tabs/assets/js/frontend/handlers/nested-tabs' );
+		elementorModules.frontend.handlers.NestedTabs = NestedTabs;
+	} )();
+}
