b6778448db24e3cfc004809c561176231caaa33c
Louis Wolmarans <louiswol94@gmail.com>
2024-10-20 21:32:15 +0200

    WIP

--- tests/playwright/sanity/plugin-tester-container.test.ts
+++ tests/playwright/sanity/plugin-tester-container.test.ts
@@ -9,10 +9,7 @@ test.describe( `Plugin tester tests: containers`, () => {
 		const page = await browser.newPage();
 		const wpAdmin = new WpAdminPage( page, testInfo, apiRequests );
 		await wpAdmin.resetExperiments();
-
-		await wpAdmin.setExperiments( {
-			e_optimized_markup: 'active',
-		} );
+		await wpAdmin.setExperiments( { e_optimized_markup: 'active' } );
 
 		await page.close();
 	} );
