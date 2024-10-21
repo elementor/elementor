b2632f1e319963f044c297f4a3733debc33ea29a
Hein van Vlastuin <heinvanvlastuin@gmail.com>
2024-10-21 16:01:29 +0200

    wip

--- modules/promotions/assets/js/react/module.js
+++ modules/promotions/assets/js/react/module.js
@@ -1,5 +1,3 @@
-import ReactPromotionBehavior from './behavior';
-import { ANIMATED_HEADLINE, CTA, IMAGE_CAROUSEL, VIDEO_PLAYLIST, TESTIMONIAL_WIDGET } from './utils/consts';
 import PromotionControl from './controls/promotion';
 
 export default class Module extends elementorModules.editor.utils.Module {
