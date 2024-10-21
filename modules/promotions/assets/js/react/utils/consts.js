361c74b1b7e13d77e30124e56e9b064bd64d79d2
Hein van Vlastuin <heinvanvlastuin@gmail.com>
2024-10-21 16:02:04 +0200

    wip

--- modules/promotions/assets/js/react/utils/consts.js
+++
@@ -1,5 +0,0 @@
-export const ANIMATED_HEADLINE = 'animated_headline';
-export const CTA = 'cta';
-export const VIDEO_PLAYLIST = 'video_playlist';
-export const TESTIMONIAL_WIDGET = 'testimonial_widget';
-export const IMAGE_CAROUSEL = 'image_carousel';
