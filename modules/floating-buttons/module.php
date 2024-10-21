4dde860605c900a8cc47ce1c62a36105669b0851
Nicola Peluchetti <nicola.peluchetti@gmail.com>
2024-10-21 17:40:43 +0200

    remove logic allow override

--- modules/floating-buttons/module.php
+++ modules/floating-buttons/module.php
@@ -100,18 +100,13 @@ class Module extends BaseModule {
 
 		$this->register_contact_pages_cpt();
 
-		if ( ! ElementorUtils::has_pro() ) {
-			add_action( 'elementor/documents/register', function ( Documents_Manager $documents_manager ) {
-				// we need to allow pro to override this if the license is active.
-				$install_floating_elements_doc = apply_filters( 'elementor/document/register/floating_elements', true );
-				if ( $install_floating_elements_doc ) {
-					$documents_manager->register_document_type(
-						static::FLOATING_BUTTONS_DOCUMENT_TYPE,
-						Floating_Buttons::get_class_full_name()
-					);
-				}
-			} );
-		}
+		add_action( 'elementor/documents/register', function ( Documents_Manager $documents_manager ) {
+			$documents_manager->register_document_type(
+				static::FLOATING_BUTTONS_DOCUMENT_TYPE,
+				Floating_Buttons::get_class_full_name()
+			);
+		} );
+
 
 		add_action( 'current_screen', function() {
 			$screen = get_current_screen();
