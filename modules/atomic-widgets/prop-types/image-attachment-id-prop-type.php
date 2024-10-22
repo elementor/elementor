6aee927fb091f685d0df02b065808f9bc70b4fa0
Nevo Golan <20494189+Nevoss@users.noreply.github.com>
2024-10-22 11:03:09 +0300

    Internal: Recreate prop-types [EDS-532] (#28939)

--- modules/atomic-widgets/prop-types/image-attachment-id-prop-type.php
+++ modules/atomic-widgets/prop-types/image-attachment-id-prop-type.php
@@ -3,17 +3,18 @@
 namespace Elementor\Modules\AtomicWidgets\PropTypes;
 
 use Elementor\Modules\AtomicWidgets\PropTypes\Base\Plain_Prop_Type;
+use Elementor\Plugin;
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 
-class Color_Prop_Type extends Plain_Prop_Type {
+class Image_Attachment_Id_Prop_Type extends Plain_Prop_Type {
 	public static function get_key(): string {
-		return 'color';
+		return 'image-attachment-id';
 	}
 
 	protected function validate_value( $value ): bool {
-		return is_string( $value );
+		return is_numeric( $value ) && Plugin::$instance->wp->wp_attachment_is_image( $value );
 	}
 }
