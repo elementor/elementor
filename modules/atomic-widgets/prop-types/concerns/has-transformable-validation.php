6d4dec64de14e40f89f85d0d35ea92894400f6f2
Nevo Golan <20494189+Nevoss@users.noreply.github.com>
2024-10-22 08:12:26 +0300

    wip

--- modules/atomic-widgets/prop-types/concerns/has-transformable-validation.php
+++ modules/atomic-widgets/prop-types/concerns/has-transformable-validation.php
@@ -2,8 +2,6 @@
 
 namespace Elementor\Modules\AtomicWidgets\PropTypes\Concerns;
 
-use Elementor\Modules\AtomicWidgets\PropTypes\Contracts\Transformable_Prop_Type;
-
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
