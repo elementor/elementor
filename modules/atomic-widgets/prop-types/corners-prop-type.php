8a8f4f435ff166ff4d3634c9ac9c747d9524b726
Ron Rostovsky <ronros@elementor.com>
2024-10-27 15:44:07 +0200

    adjustments to new prop type Object_Prop_Type structure + transformer better fallback

--- modules/atomic-widgets/prop-types/corners-prop-type.php
+++ modules/atomic-widgets/prop-types/corners-prop-type.php
@@ -2,33 +2,24 @@
 
 namespace Elementor\Modules\AtomicWidgets\PropTypes;
 
+use Elementor\Modules\AtomicWidgets\PropTypes\Base\Object_Prop_Type;
+
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 
-class Corners_Prop_Type extends Transformable_Prop_Type {
-	public function __construct() {
-		$this->internal_types['dimension'] = Size_Prop_Type::make();
-	}
+class Corners_Prop_Type extends Object_Prop_Type {
 
 	public static function get_key(): string {
 		return 'corners';
 	}
 
-	public function validate_value( $value ): void {
-		if ( ! is_array( $value ) ) {
-			throw new \Exception( 'Value must be an array, ' . gettype( $value ) . ' given.' );
-		}
-
-		$dimensions = [ 'topLeft', 'topRight', 'bottomRight', 'bottomLeft' ];
-
-		foreach ( $dimensions as $dimension ) {
-			if ( ! isset( $value[ $dimension ] ) ) {
-				continue;
-			}
-
-			$this->internal_types['dimension']->validate_with_additional( $value[ $dimension ] );
-		}
-
+	protected function define_shape(): array {
+		return [
+			'topLeft' => Size_Prop_Type::make(),
+			'topRight' => Size_Prop_Type::make(),
+			'bottomRight' => Size_Prop_Type::make(),
+			'bottomLeft' => Size_Prop_Type::make(),
+		];
 	}
 }
