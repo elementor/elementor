dc27135988508de36460d633150061eb4c9db30a
elchugreeva <elenac@elementor.com>
2024-10-27 15:32:08 +0100

    Internal: Replace Free to Pro Upsell Promotion  - fix text alignment [ED-15996]

--- modules/promotions/assets/js/react/components/promotion-card.js
+++ modules/promotions/assets/js/react/components/promotion-card.js
@@ -8,6 +8,8 @@ import {
 	Button,
 	CloseButton,
 	Stack,
+	List,
+	ListItem,
 } from '@elementor/ui';
 
 const PromotionCard = ( { doClose, promotionsData } ) => {
@@ -36,10 +38,15 @@ const PromotionCard = ( { doClose, promotionsData } ) => {
 					} } onClick={ doClose } />
 				</Stack>
 				<Image src={ imgSrc } alt={ imgAlt } sx={ { height: 150, width: '100%' } } />
-				<Stack py={ 1 } px={ 2 }>
-					{ description.map( ( e, index ) => {
-						return ( <Typography key={ index } variant="body2" color="secondary">&bull; { e }</Typography> );
-					} ) }
+				<Stack px={ 2 }>
+					<List sx={ { listStyle: 'disc', listStylePosition: 'outside', pl: 1 } }>
+						{ description.map( ( e, index ) => { return (
+							<ListItem key={ index } sx={ { listStyle: 'disc', display: 'list-item', color: 'text.secondary', p: 0 } }>
+								<Typography variant="body2" color="secondary">{ e }</Typography>
+							</ListItem>
+						)
+						} ) }
+					</List>
 				</Stack>
 				<Stack pt={ 1 } pb={ 1.5 } px={ 2 }>
 					<Button
