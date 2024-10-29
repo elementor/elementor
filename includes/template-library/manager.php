9d3f865055edd8b9fff0c895fdc9c1ed1ed9b011
David Seveloff <davids@elementor.com>
2024-10-29 23:52:53 +0200

    lint

--- includes/template-library/manager.php
+++ includes/template-library/manager.php
@@ -796,10 +796,10 @@ class Manager {
 		}
 
 		// TODO: Remove $isWidgetTemplate in 3.28.0 as there is a Pro dependency
-		$checkPermissions = isset( $args['check_permissions'] ) && false === $args['check_permissions'];
-		$isWidgetTemplate = 'widget' === $this->elementor_adapter->get_template_type( $args['template_id'] );
+		$check_permissions = isset( $args['check_permissions'] ) && false === $args['check_permissions'];
+		$is_widget_template = 'widget' === $this->elementor_adapter->get_template_type( $args['template_id'] );
 
-		if ( $checkPermissions || $isWidgetTemplate ) {
+		if ( $check_permissions || $is_widget_template ) {
 			return false;
 		}
 
