9e8afb731f2e5e6a455c17b457f890079f821305
Hein van Vlastuin <heinvanvlastuin@gmail.com>
2024-11-06 16:32:35 +0100

    Set scroll value

--- assets/dev/js/frontend/handlers/container/horizontal-scroll.js
+++ assets/dev/js/frontend/handlers/container/horizontal-scroll.js
@@ -1,38 +1,59 @@
 export default class HorizontalScroll extends elementorModules.frontend.handlers.Base {
+	__construct( ...args ) {
+		super.__construct( ...args );
+	}
+
+	getDefaultSettings() {
+		return {
+			selectors: {
+				horizontalParentContainer: '.e-con-horizontal-sticky',
+				horizontalContainer: '.e-con-horizontal',
+			},
+		};
+	}
+
+	getDefaultElements() {
+		const selectors = this.getSettings( 'selectors' );
+
+		return {
+			horizontalParentContainer: this.$element[ 0 ].closest( selectors.horizontalParentContainer ),
+			horizontalContainer: this.$element[ 0 ].closest( selectors.horizontalContainer ),
+		}
+	}
+
 	onInit() {
+		const elements = this.getDefaultElements();
 
-// Adding scroll event listener
-		document.addEventListener( 'scroll', this.horizontalScroll );
+		this.horizontalParentContainer = elements.horizontalParentContainer;
+		this.horizontalContainer = elements.horizontalContainer;
 
+		if ( ! this.horizontalParentContainer ) {
+			return;
+		}
+
+		this.scrollWidth = this.horizontalContainer.scrollWidth;
+		this.verticalScrollHeight = this.horizontalParentContainer.getBoundingClientRect().width + this.horizontalContainer.getBoundingClientRect().height;
+		this.horizontalParentContainer.style.setProperty( '--horizontal-scroll-height', `${ this.verticalScrollHeight }px` );
+
+		document.addEventListener( 'scroll', this.horizontalScroll.bind( this ) );
 	}
 
 	horizontalScroll() {
+		const stickyPosition = this.horizontalContainer.getBoundingClientRect().top;
 
-//Selecting Elements
-		let sticky = document.querySelector('.e-con-horizontal');
-		let stickyParent = document.querySelector('.e-con-horizontal-sticky');
+		console.log( 'stickyPosition', stickyPosition );
 
-		console.log( 'sticky: ', sticky );
-		console.log( 'stickyParent: ', stickyParent );
-
-		let scrollWidth = sticky.scrollWidth;
-		let verticalScrollHeight = stickyParent.getBoundingClientRect().height - sticky.getBoundingClientRect().height;
-
-		let stickyPosition = sticky.getBoundingClientRect().top;
-
-		console.log( 'stickyPosition: ', stickyPosition );
-
-		if (stickyPosition > 1) {
+		if ( stickyPosition > 1  ) {
 			return;
-		} else {
-			let scrolled = stickyParent.getBoundingClientRect().top; //how much is scrolled?
-
-			console.log( 'scrolled: ', scrolled );
-			console.log( 'verticalScrollHeight: ', verticalScrollHeight );
-			console.log( 'scrollWidth: ', scrollWidth );
-
-			sticky.scrollLeft = (scrollWidth / verticalScrollHeight) * (-scrolled) * 0.85;
-
 		}
+
+		const scrolledDistance = this.horizontalParentContainer.getBoundingClientRect().top;
+		this.horizontalContainer.scrollLeft = ( this.scrollWidth / this.verticalScrollHeight ) * ( -scrolledDistance ) * 0.85;
+
+
+		console.log( 'scrolledDistance', scrolledDistance );
+		console.log( 'scrollWidth', this.scrollWidth );
+		console.log( 'verticalScrollHeight', this.verticalScrollHeight );
+		console.log( 'scroll someting', ( this.scrollWidth / this.verticalScrollHeight ) );
 	}
 }
