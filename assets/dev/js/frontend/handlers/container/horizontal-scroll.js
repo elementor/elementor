9b0a1efae9e19b37d38302b3da042d108fd5e990
Hein van Vlastuin <heinvanvlastuin@gmail.com>
2024-11-06 12:22:58 +0100

    Wip

--- assets/dev/js/frontend/handlers/container/horizontal-scroll.js
+++ assets/dev/js/frontend/handlers/container/horizontal-scroll.js
@@ -2,29 +2,20 @@ import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 export default class HorizontalScroll extends elementorModules.frontend.handlers.Base {
-	// __construct( settings ) {
-	// 	super.__construct( settings );
-	// }
 	onInit() {
-		this.initScroll();
-
-		console.log( 'HorizontalScroll' );
-	}
-
-	initScroll() {
 		gsap.registerPlugin( ScrollTrigger );
 
-		gsap.to('.e-con-horizontal .elementor-element', {
+		gsap.to( '.e-con-horizontal .elementor-element', {
 			xPercent: -100, // Moves the content to the left to create the scroll effect
-			ease: "none",
+			ease: 'none',
 			scrollTrigger: {
 				trigger: '.e-con-horizontal',
-				start: "top top", // Start when the element is at the top of the viewport
-				end: () => "+=" + document.querySelector('.e-con-horizontal').offsetWidth,
+				start: 'top top', // Start when the element is at the top of the viewport
+				end: () => '+=' + document.querySelector( '.e-con-horizontal' ).offsetWidth,
 				scrub: true, // Smooth scroll effect
 				pin: true, // Pin the section in place while scrolling
 				anticipatePin: 1,
 			},
-		});
+		} );
 	}
 }
