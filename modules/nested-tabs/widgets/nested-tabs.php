db107807a4b9d1e7287e88357ba019914ffdccbe
Hein van Vlastuin <heinvanvlastuin@gmail.com>
2024-11-06 08:42:46 +0100

    wip

--- modules/nested-tabs/widgets/nested-tabs.php
+++ modules/nested-tabs/widgets/nested-tabs.php
@@ -47,7 +47,7 @@ class NestedTabs extends Widget_Nested_Base {
 	}
 
 	public function get_script_depends(): array {
-		return [ 'import-script-nested-tabs', '12345' ];
+		return [ 'import-script-nested-tabs' ];
 	}
 
 	public function has_widget_inner_wrapper(): bool {
