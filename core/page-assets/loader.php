fe4fdcdd7326d3950442773197f1ed95689f0411
Hein van Vlastuin <heinvanvlastuin@gmail.com>
2024-11-09 19:39:32 +0100

    wip

--- core/page-assets/loader.php
+++ core/page-assets/loader.php
@@ -124,7 +124,11 @@ class Loader extends Module {
 			foreach ( $assets_list as $asset_name ) {
 				$this->assets[ $assets_type ][ $asset_name ]['enabled'] = true;
 
-				if ( 'scripts' === $assets_type && ! $this->should_import_script( $asset_name ) ) {
+				if ( 'scripts' === $assets_type ) {
+					if ( $this->should_import_script( $asset_name ) ) {
+						continue;
+					}
+
 					wp_enqueue_script( $asset_name );
 				} else if ( 'styles' === $assets_type ) {
 					wp_enqueue_style( $asset_name );
