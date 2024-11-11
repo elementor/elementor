dcdcf8ee3ecaf1b3b1435d7e3657631cd8213174
Ronen Boxer <ronenb@elementor.com>
2024-11-11 17:30:14 +0200

    var dump try

--- core/isolation/elementor-adapter.php
+++ core/isolation/elementor-adapter.php
@@ -44,7 +44,7 @@ class Elementor_Adapter implements Elementor_Adapter_Interface {
 
 	public function get_template_type( $template_id ): string {
 		$document = Source_Local::get_template_type( $template_id );
-
+var_dump( $document );
 		return $document
 			? $document->get_template_type()
 			: '';
