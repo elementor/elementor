261d16626c9707abd2cf14fd1771acc9fbd192db
Omer Haim Israeli <omeri@elementor.com>
2024-11-12 09:33:49 +0200

    wip

--- modules/atomic-widgets/styles/utils.php
+++ modules/atomic-widgets/styles/utils.php
@@ -4,8 +4,6 @@ namespace Elementor\Modules\AtomicWidgets\Styles;
 
 class Utils {
 	public static function generate_id( string $prefix = '', $existing_ids = [] ): string {
-		$existing_ids = array_flip( $existing_ids );
-
 		do {
 			$id = $prefix . wp_rand( 1000000, 9999999 );
 		} while ( in_array( $id, $existing_ids, true ) );
