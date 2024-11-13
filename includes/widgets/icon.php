ea10b2a5aaa51fc2294b753ba5156b307478490a
Mike Kokhanov <mikek@elementor.com>
2024-11-13 10:30:37 +0200

    [ wip ]: restored missing logic after an experiment

--- includes/widgets/icon.php
+++ includes/widgets/icon.php
@@ -489,7 +489,7 @@ class Widget_Icon extends Widget_Base {
 
 		let link = '';
 		if ( settings.link.url ) {
-			view.addRenderAttribute( 'link_url', 'href', settings.link.url );
+			view.addRenderAttribute( 'link_url', 'href', elementor.helpers.sanitizeUrl( settings.link.url ) );
 			link = view.getRenderAttributeString( 'link_url' );
 		}
 
