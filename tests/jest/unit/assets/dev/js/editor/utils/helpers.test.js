44de9f5fa0e29dee104bfca2c82a6eec62570d89
Mike Kokhanov <mikek@elementor.com>
2024-11-17 14:50:07 +0200

    [ wip ]: improved test-coverage for editor's helper functions

---
+++ tests/jest/unit/assets/dev/js/editor/utils/helpers.test.js
@@ -0,0 +1,24 @@
+import { sanitizeUrl } from 'elementor-editor-utils/helpers';
+
+describe( 'elementor.helpers.sanitizeUrl', () => {
+	test( 'should not affect valid URL', () => {
+		expect( sanitizeUrl( 'https://example.com/?param=1' ) )
+			.toBe( 'https://example.com/?param=1' );
+
+		expect( sanitizeUrl( 'https://127.0.0.1/?page=homepage' ) )
+			.toBe( 'https://127.0.0.1/?page=homepage' );
+
+		expect( sanitizeUrl( 'https://mail.google.com/' ) )
+			.toBe( 'https://mail.google.com/' );
+	} );
+
+	test( 'should translate URL containing unicode letters', () => {
+		expect( sanitizeUrl( 'https://t.me/?page=garçon' ) )
+			.toBe( 'https://t.me/?page=gar%C3%A7on' );
+	} );
+
+	test( 'should transform " character to prevent attribute leaks during html-rendering', () => {
+		expect( sanitizeUrl( 'https://ex.com/?hack=22" style="display:block;"' ) )
+			.toBe( 'https://ex.com/?hack=22%22%20style=%22display:block;%22' );
+	} );
+} );
