db680034d564f17d26584c2b3bf44315f910df62
Mike Kokhanov <mikek@elementor.com>
2024-11-18 10:34:16 +0200

    [ wip ]: improved test-coverage

--- tests/jest/unit/assets/dev/js/editor/utils/helpers.test.js
+++ tests/jest/unit/assets/dev/js/editor/utils/helpers.test.js
@@ -21,4 +21,9 @@ describe( 'elementor.helpers.sanitizeUrl', () => {
 		expect( sanitizeUrl( 'https://ex.com/?hack=22" style="display:block;"' ) )
 			.toBe( 'https://ex.com/?hack=22%22%20style=%22display:block;%22' );
 	} );
+
+	test( 'should not allow injecting script tags', () => {
+		expect( sanitizeUrl( '"><script>alert( "my-script" );</script>' ) )
+			.toBe( '%22%3E%3Cscript%3Ealert(%20%22my-script%22%20);%3C/script%3E' );
+	} );
 } );
