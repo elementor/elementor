aa4da1c8cdf30d7e1c6f437c89bc7f2df1a9ce42
baghdasarovelementor <norayrb@elementor.red>
2024-11-18 12:59:30 +0400

    wip

--- tests/phpunit/elementor/core/settings/page/test-manager.php
+++ tests/phpunit/elementor/core/settings/page/test-manager.php
@@ -30,14 +30,14 @@ class Test_Manager extends Elementor_Test_Base {
 	}
 
 	public function test_ajax_before_save_settings__contributor_can_not_delete_post_thumbnail() {
-        // Arrange
-        $this->act_as('contributor');
+		// Arrange
+		$this->act_as('contributor');
 
-        $post_data = [
-            'post_author' => get_current_user_id(),
-            'post_status' => 'pending',
-            'post_type' => 'post',
-        ];
+		$post_data = [
+			'post_author' => get_current_user_id(),
+			'post_status' => 'pending',
+			'post_type' => 'post',
+		];
 
 		$document_post = $this->create_post_with_data( $post_data );
 
@@ -49,102 +49,101 @@ class Test_Manager extends Elementor_Test_Base {
 
 		$post_id = $document_post->get_id();
 
-        // Assert
-        $this->expectExceptionCode( Exceptions::FORBIDDEN );
-        $this->expectExceptionMessage( 'You do not have permission to modify the featured image.' );
+		// Assert
+		$this->expectExceptionCode( Exceptions::FORBIDDEN );
+		$this->expectExceptionMessage( 'You do not have permission to modify the featured image.' );
 
-        // Act
-        $this->manager->ajax_before_save_settings( $data, $post_id );
+		// Act
+		$this->manager->ajax_before_save_settings( $data, $post_id );
 	}
 
 	public function test_save_post_status__contributor_can_not_set_post_status_private() {
-        // Arrange
-        $this->act_as('contributor');
+		// Arrange
+		$this->act_as('contributor');
 
-        $post_data = [
-            'post_author' => get_current_user_id(),
-            'post_status' => 'pending',
-            'post_type' => 'post',
-        ];
+		$post_data = [
+			'post_author' => get_current_user_id(),
+			'post_status' => 'pending',
+			'post_type' => 'post',
+		];
 
-        $document_post = $this->create_post_with_data( $post_data );
-        $post_id = $document_post->get_id();
+		$document_post = $this->create_post_with_data( $post_data );
+		$post_id = $document_post->get_id();
 
-        $data = [
-            'post_title' => 'Test Post Title',
-            'post_excerpt' => 'Test Excerpt',
-            'post_status' => 'private',
-        ];
+		$data = [
+			'post_title' => 'Test Post Title',
+			'post_excerpt' => 'Test Excerpt',
+			'post_status' => 'private',
+		];
 
-        // Act
-        $this->manager->ajax_before_save_settings( $data, $post_id );
-        $document_post->refresh_post();
-        $post_status = $document_post->get_post()->post_status;
+		// Act
+		$this->manager->ajax_before_save_settings( $data, $post_id );
+		$document_post->refresh_post();
+		$post_status = $document_post->get_post()->post_status;
 
-        // Assert
-        $this->assertEquals( 'pending', $post_status );
+		// Assert
+		$this->assertEquals( 'pending', $post_status );
 	}
 
 	public function test_ajax_before_save_settings__admin_can_delete_post_thumbnail() {
-        // Arrange
-        $this->act_as_admin();
+		// Arrange
+		$this->act_as_admin();
 
-        $attachment = $this->factory()->post->create_and_get( [
-            'post_type' => 'attachment',
-        ] );
-        $post_data = [
-            'post_author' => get_current_user_id(),
-            'post_status' => 'publish',
-            'post_type' => 'post',
-            'meta_input' => [
-                '_thumbnail_id' => $attachment->ID,
-            ],
-        ];
-        $document_post = $this->create_post_with_data( $post_data );
-        $data = [
-            'post_title' => 'Test Post Title',
-            'post_excerpt' => 'Test Excerpt',
-            'post_status' => 'publish',
-            'post_featured_image' => 'Remove Featured Image',
-        ];
-        $post_id = $document_post->get_id();
+		$attachment = $this->factory()->post->create_and_get( [
+			'post_type' => 'attachment',
+		] );
+		$post_data = [
+			'post_author' => get_current_user_id(),
+			'post_status' => 'publish',
+			'post_type' => 'post',
+			'meta_input' => [
+				'_thumbnail_id' => $attachment->ID,
+			],
+		];
+		$document_post = $this->create_post_with_data( $post_data );
+		$data = [
+			'post_title' => 'Test Post Title',
+			'post_excerpt' => 'Test Excerpt',
+			'post_status' => 'publish',
+			'post_featured_image' => 'Remove Featured Image',
+		];
+		$post_id = $document_post->get_id();
 
+		$this->assertEquals( get_post_thumbnail_id($post_id), $attachment->ID );
 
-        $this->assertEquals( get_post_thumbnail_id($post_id), $attachment->ID );
+		// Act
+		$this->manager->ajax_before_save_settings( $data, $post_id );
 
-        // Act
-        $this->manager->ajax_before_save_settings( $data, $post_id );
-
-        // Assert
-        $this->assertNotEquals( get_post_thumbnail_id($post_id), $attachment->ID );
-        $this->assertEquals( 0, get_post_thumbnail_id($post_id));
+		// Assert
+		$this->assertNotEquals( get_post_thumbnail_id($post_id), $attachment->ID );
+		$this->assertEquals( 0, get_post_thumbnail_id($post_id));
 	}
 
 	public function test_save_post_status__admin_can_set_private_post_status() {
-        // Arrange
-        $this->act_as_admin();
+		// Arrange
+		$this->act_as_admin();
 
-        $post_data = [
-            'post_author' => get_current_user_id(),
-            'post_status' => 'pending',
-            'post_type' => 'post',
-        ];
+		$post_data = [
+			'post_author' => get_current_user_id(),
+			'post_status' => 'pending',
+			'post_type' => 'post',
+		];
 
-        $document_post = $this->create_post_with_data( $post_data );
-        $post_id = $document_post->get_id();
+		$document_post = $this->create_post_with_data( $post_data );
+		$post_id = $document_post->get_id();
 
-        $data = [
-            'post_title' => 'Test Post Title',
-            'post_excerpt' => 'Test Excerpt',
-            'post_status' => 'private',
-        ];
+		$data = [
+			'post_title' => 'Test Post Title',
+			'post_excerpt' => 'Test Excerpt',
+			'post_status' => 'private',
+		];
 
-        // Act
-        $this->manager->ajax_before_save_settings( $data, $post_id );
-        $document_post->refresh_post();
-        $post_status = $document_post->get_post()->post_status;
+		// Act
+		$this->manager->ajax_before_save_settings( $data, $post_id );
+		$document_post->refresh_post();
+		$post_status = $document_post->get_post()->post_status;
 
-        // Assert
-        $this->assertEquals( 'private', $post_status );
+		// Assert
+		$this->assertEquals( 'private', $post_status );
 	}
 }
