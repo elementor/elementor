c7d1712f3967207147ac7021bf194979142989cd
max-zu <zubovmaksim@gmail.com>
2024-11-19 14:49:39 +0100

    ED13846 updated tests

--- tests/phpunit/elementor/modules/playing-cards/test-module.php
+++ tests/phpunit/elementor/modules/playing-cards/test-module.php
@@ -3,7 +3,7 @@
 namespace Elementor\Modules\PlayingCards\Tests;
 
 use ElementorEditorTesting\Elementor_Test_Base;
-use Elementor\Modules\PlayingCards\Widgets\PlayingCardsWidget;
+use Elementor\Modules\PlayingCards\Widgets\Playing_Cards;
 
 class Test_PlayingCards_Module extends Elementor_Test_Base {
 	public function setUp(): void {
@@ -11,27 +11,27 @@ class Test_PlayingCards_Module extends Elementor_Test_Base {
 	}
 
 	public function test_widget_registration() {
-		$widget = new PlayingCardsWidget();
-		$this->assertInstanceOf(PlayingCardsWidget::class, $widget);
+		$widget = new Playing_Cards();
+		$this->assertInstanceOf(Playing_Cards::class, $widget);
 	}
 
 	public function test_widget_name() {
-		$widget = new PlayingCardsWidget();
+		$widget = new Playing_Cards();
 		$this->assertEquals('playing-cards', $widget->get_name());
 	}
 
 	public function test_widget_title() {
-		$widget = new PlayingCardsWidget();
+		$widget = new Playing_Cards();
 		$this->assertEquals('Playing Cards', $widget->get_title());
 	}
 
 	public function test_widget_icon() {
-		$widget = new PlayingCardsWidget();
+		$widget = new Playing_Cards();
 		$this->assertEquals('eicon-shape', $widget->get_icon());
 	}
 
 	public function test_widget_style_depends() {
-		$widget = new PlayingCardsWidget();
+		$widget = new Playing_Cards();
 		$this->assertContains('widget-playing-cards', $widget->get_style_depends());
 	}
 }
--- tests/phpunit/elementor/modules/playing-cards/widget/test-playing-card-widget.php
+++ tests/phpunit/elementor/modules/playing-cards/widget/test-playing-card-widget.php
@@ -2,7 +2,7 @@
 
 namespace Elementor\Modules\PlayingCards\Tests;
 
-use Elementor\Modules\PlayingCards\Widgets\PlayingCardsWidget;
+use Elementor\Modules\PlayingCards\Widgets\Playing_Cards;
 use ElementorEditorTesting\Elementor_Test_Base;
 
 class Test_PlayingCards_Widget extends Elementor_Test_Base {
@@ -12,39 +12,39 @@ class Test_PlayingCards_Widget extends Elementor_Test_Base {
 	}
 
 	public function test_widget_registration() {
-		$widget = new PlayingCardsWidget();
-		$this->assertInstanceOf(PlayingCardsWidget::class, $widget);
+		$widget = new Playing_Cards();
+		$this->assertInstanceOf(Playing_Cards::class, $widget);
 	}
 
 	public function test_widget_name() {
-		$widget = new PlayingCardsWidget();
+		$widget = new Playing_Cards();
 		$this->assertEquals('playing-cards', $widget->get_name());
 	}
 
 	public function test_widget_title() {
-		$widget = new PlayingCardsWidget();
+		$widget = new Playing_Cards();
 		$this->assertEquals('Playing Cards', $widget->get_title());
 	}
 
 	public function test_widget_icon() {
-		$widget = new PlayingCardsWidget();
+		$widget = new Playing_Cards();
 		$this->assertEquals('eicon-shape', $widget->get_icon());
 	}
 
 	public function test_widget_style_depends() {
-		$widget = new PlayingCardsWidget();
+		$widget = new Playing_Cards();
 		$this->assertContains('widget-playing-cards', $widget->get_style_depends());
 	}
 
 	public function test_register_controls() {
-		$widget = new PlayingCardsWidget();
+		$widget = new Playing_Cards();
 		$widget->register_controls();
 		$controls = $widget->get_controls();
 		$this->assertNotEmpty($controls);
 	}
 
 	public function test_render() {
-		$widget = new PlayingCardsWidget();
+		$widget = new Playing_Cards();
 		$widget->set_settings([
 			'cards_list' => [
 				[
