ddaad1bd26b14ff03f3fc89647d2c608e5132167
Omer Haim Israeli <omeri@elementor.com>
2024-11-20 12:04:21 +0200

    wip

--- modules/global-classes/global-classes-rest-api.php
+++ modules/global-classes/global-classes-rest-api.php
@@ -128,10 +128,11 @@ class Global_Classes_REST_API {
 		$id = $request->get_param( 'id' );
 		$class = $this->get_repository()->get( $id );
 
-		return new \WP_Error( 'log', Plugin::instance()->kits_manager->get_active_kit(), [ 'status' => 404 ] );
-
 		if ( null === $class ) {
-			return new \WP_Error( 'entity_not_found', __( 'Global class not found', 'elementor' ), [ 'status' => 404 ] );
+			return new \WP_Error( 'entity_not_found', __( 'Global class not found', 'elementor' ), [
+				'status' => 404,
+				'id' => Plugin::$instance->kits_manager->get_active_kit()->get_id(),
+			] );
 		}
 
 		return $class;
