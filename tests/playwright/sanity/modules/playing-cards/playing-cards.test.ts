7de1d80f15bf9aa72ee0aa37a7435fdbb99e82f8
max-zu <zubovmaksim@gmail.com>
2024-11-21 14:55:07 +0100

    ED13846 updated tests

--- tests/playwright/sanity/modules/playing-cards/playing-cards.test.ts
+++ tests/playwright/sanity/modules/playing-cards/playing-cards.test.ts
@@ -3,18 +3,24 @@ import { parallelTest as test } from '../../../parallelTest';
 import WpAdminPage from '../../../pages/wp-admin-page';
 
 test.describe( 'Playing cards widget sanity test', () => {
-
 	test.beforeEach( async ( { page, apiRequests }, testInfo ) => {
 		const wpAdminPage = new WpAdminPage( page, testInfo, apiRequests );
 		await wpAdminPage.resetExperiments();
 	} );
 
 	test( 'it successfully adds card', async ( { page, apiRequests }, testInfo ) => {
+		// Arrange.
 		const wpAdmin = new WpAdminPage( page, testInfo, apiRequests );
 		const editor = await wpAdmin.openNewPage();
+
+		// Act.
 		await editor.addWidget( 'playing-cards' );
 		await page.click( 'text=Add Item' );
 
-		expect( await page.screenshot() ).toMatchSnapshot();
+		// Assert.
+		expect( await editor.getPreviewFrame()
+			.locator( '.e-playing-cards' )
+			.screenshot( { type: 'jpeg', quality: 90 } ) )
+			.toMatchSnapshot( 'playing-cards.jpeg' );
 	} );
 } );
