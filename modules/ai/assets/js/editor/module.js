65387f98ade5c62dfabd77b94b7443ac57e3d79f
Naor Sabag <32329815+naorsabag@users.noreply.github.com>
2024-11-24 20:03:56 +0200

    New: Animate with AI [ED-16191] (#29393)
    ## PR Checklist
    <!-- 
    Please check if your PR fulfills the following requirements:
    **Filling out the template is required.** Any pull request that does not
    include enough information to be reviewed in a timely manner may be
    closed at the maintainers' discretion.
     -->
    - [x] The commit message follows our guidelines:
    https://github.com/elementor/elementor/blob/master/.github/CONTRIBUTING.md
    
    
    ## PR Type
    What kind of change does this PR introduce?
    <!-- Please check the one that applies to this PR using "x" with no
    spaces eg: [x]. -->
    - [ ] Bugfix
    - [ ] Feature
    - [ ] Code style update (formatting, local variables)
    - [ ] Refactoring (no functional changes, no api changes)
    - [ ] Build related changes
    - [ ] CI related changes
    - [ ] Documentation content changes
    - [x] Other... Please describe:
    
    ## Summary
    
    This PR can be summarized in the following changelog entry:
    
    * Add border to AI button
    
    ## Description
    An explanation of what is done in this PR
    
    *
    
    ## Test instructions
    This PR can be tested by following these steps:
    
    *
    
    ## Quality assurance
    
    - [x] I have tested this code to the best of my abilities
    - [ ] I have added unittests to verify the code works as intended
    - [ ] Docs have been added / updated (for bug fixes / features)
    
    Fixes #
    
    ---------
    
    Co-authored-by: ElementorBot <48412871+elementorbot@users.noreply.github.com>

--- modules/ai/assets/js/editor/module.js
+++ modules/ai/assets/js/editor/module.js
@@ -208,6 +208,7 @@ export default class Module extends elementorModules.editor.utils.Module {
 					additionalOptions: {
 						animationType: aiOptions.type,
 						widgetType,
+						buttonBorder: true,
 					},
 					context: this.getContextData( view, controlType ),
 				},
