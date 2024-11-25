639fa7c1e73461e5a01c5e139a1c1105257314ae
Naor Sabag <32329815+naorsabag@users.noreply.github.com>
2024-11-25 09:34:45 +0200

    New: Animate with AI - REVERT [ED-16191] (#29398)
    ## PR Checklist
    <!-- 
    Please check if your PR fulfills the following requirements:
    **Filling out the template is required.** Any pull request that does not
    include enough information to be reviewed in a timely manner may be
    closed at the maintainers' discretion.
     -->
    - [x] The commit message follows our guidelines:
    https://github.com/elementor/elementor/blob/master/.github/CONTRIBUTING.md
    
    
    ## PR Type
    What kind of change does this PR introduce?
    <!-- Please check the one that applies to this PR using "x" with no
    spaces eg: [x]. -->
    - [x] Bugfix
    - [ ] Feature
    - [ ] Code style update (formatting, local variables)
    - [ ] Refactoring (no functional changes, no api changes)
    - [ ] Build related changes
    - [ ] CI related changes
    - [ ] Documentation content changes
    - [ ] Other... Please describe:
    
    ## Summary
    
    This PR can be summarized in the following changelog entry:
    
    *
    
    ## Description
    An explanation of what is done in this PR
    
    *
    
    ## Test instructions
    This PR can be tested by following these steps:
    
    *
    
    ## Quality assurance
    
    - [x] I have tested this code to the best of my abilities
    - [ ] I have added unittests to verify the code works as intended
    - [ ] Docs have been added / updated (for bug fixes / features)
    
    Fixes #

--- modules/ai/assets/js/editor/helpers/features-enum.js
+++ modules/ai/assets/js/editor/helpers/features-enum.js
@@ -3,5 +3,4 @@ export const FREE_TRIAL_FEATURES_NAMES = {
 	IMAGES: __( 'Images', 'elementor' ),
 	CODE: __( 'Code', 'elementor' ),
 	CONTAINERS: __( 'Containers', 'elementor' ),
-	ANIMATION: __( 'Animation', 'elementor' ),
 };
