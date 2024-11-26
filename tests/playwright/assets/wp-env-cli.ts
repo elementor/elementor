9b933e64af530f0b33117d1057d57d67faa329b0
Yotam Shapira <yotams@elementor.com>
2024-11-26 14:55:34 +0200

    fix import

--- tests/playwright/assets/wp-env-cli.ts
+++ tests/playwright/assets/wp-env-cli.ts
@@ -1,11 +1,12 @@
 // import { execSync } from 'child_process';
 // import github from '@actions/core';
-import { cli } from '@elementor/wp-lite-env';
+// import { cli } from '@elementor/wp-lite-env';
 
 export const wpEnvCli = async ( command: string ) => {
 	const port = ( 1 === Number( process.env.TEST_PARALLEL_INDEX ) ) ? '8889' : '8888';
 
-	await cli( port, command );
+	const wpLiteEnv = await import('@elementor/wp-lite-env');
+	await wpLiteEnv.cli( port, command );
 	// const cliCommand = ( 1 === Number( process.env.TEST_PARALLEL_INDEX ) ) ? 'tests-cli' : 'cli';
 	// const cli = `npx wp-env run ${ cliCommand } `;
 	// const logs = execSync( `${ cli }${ command }`, { encoding: 'utf-8' } );
