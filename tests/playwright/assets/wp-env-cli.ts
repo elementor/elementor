812d5cf67e11b94935b332dce9ae5ef1c6844ed8
Yotam Shapira <yotams@elementor.com>
2024-11-26 11:59:48 +0200

    upgrade wp-lite-env

--- tests/playwright/assets/wp-env-cli.ts
+++ tests/playwright/assets/wp-env-cli.ts
@@ -1,25 +1,29 @@
-import { execSync } from 'child_process';
-import github from '@actions/core';
+// import { execSync } from 'child_process';
+// import github from '@actions/core';
+import { cli } from '@elementor/wp-lite-env';
 
-export const wpEnvCli = ( command: string ) => {
-	const cliCommand = ( 1 === Number( process.env.TEST_PARALLEL_INDEX ) ) ? 'tests-cli' : 'cli';
-	const cli = `npx wp-env run ${ cliCommand } `;
-	const logs = execSync( `${ cli }${ command }`, { encoding: 'utf-8' } );
+export const wpEnvCli = async ( command: string ) => {
+	const port = ( 1 === Number( process.env.TEST_PARALLEL_INDEX ) ) ? '8889' : '8888';
+
+	await cli( port, command );
+	// const cliCommand = ( 1 === Number( process.env.TEST_PARALLEL_INDEX ) ) ? 'tests-cli' : 'cli';
+	// const cli = `npx wp-env run ${ cliCommand } `;
+	// const logs = execSync( `${ cli }${ command }`, { encoding: 'utf-8' } );
 	// Print log for debuggability
 	// eslint-disable-next-line no-console
-	console.log( logs );
-
-	if ( ! process.env.CI ) {
-		return logs;
-	}
-
-	if ( logs.includes( 'Warning' ) ) {
-		github.warning( logs );
-	}
-
-	if ( logs.includes( 'Error' ) ) {
-		github.error( logs );
-	}
-
-	return logs;
+	// console.log( logs );
+	//
+	// if ( ! process.env.CI ) {
+	// 	return logs;
+	// }
+	//
+	// if ( logs.includes( 'Warning' ) ) {
+	// 	github.warning( logs );
+	// }
+	//
+	// if ( logs.includes( 'Error' ) ) {
+	// 	github.error( logs );
+	// }
+	//
+	// return logs;
 };
