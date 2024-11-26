db8528b1684cf9c9235b0f622c2a6e5efab5f7f1
Yotam Shapira <yotams@elementor.com>
2024-11-26 15:08:52 +0200

    clean up

--- tests/playwright/assets/wp-env-cli.ts
+++ tests/playwright/assets/wp-env-cli.ts
@@ -1,30 +1,6 @@
-// import { execSync } from 'child_process';
-// import github from '@actions/core';
-// import { cli } from '@elementor/wp-lite-env';
-
 export const wpEnvCli = async ( command: string ) => {
 	const port = ( 1 === Number( process.env.TEST_PARALLEL_INDEX ) ) ? '8889' : '8888';
 
-	const wpLiteEnv = await import('@elementor/wp-lite-env');
-	await wpLiteEnv.cli( port, command );
-	// const cliCommand = ( 1 === Number( process.env.TEST_PARALLEL_INDEX ) ) ? 'tests-cli' : 'cli';
-	// const cli = `npx wp-env run ${ cliCommand } `;
-	// const logs = execSync( `${ cli }${ command }`, { encoding: 'utf-8' } );
-	// Print log for debuggability
-	// eslint-disable-next-line no-console
-	// console.log( logs );
-	//
-	// if ( ! process.env.CI ) {
-	// 	return logs;
-	// }
-	//
-	// if ( logs.includes( 'Warning' ) ) {
-	// 	github.warning( logs );
-	// }
-	//
-	// if ( logs.includes( 'Error' ) ) {
-	// 	github.error( logs );
-	// }
-	//
-	// return logs;
+	const { cli } = await import('@elementor/wp-lite-env');
+	await cli( port, command );
 };
