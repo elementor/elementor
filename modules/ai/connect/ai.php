8f020f3ea519ade81a37157f9feb70c9579d9c74
netanelavr <netanelav@elementor.com>
2024-12-04 14:47:34 +0200

    Fix: Image edit with AI not working with WooCommerce [ED-16339]

--- modules/ai/connect/ai.php
+++ modules/ai/connect/ai.php
@@ -11,7 +11,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 class Ai extends Library {
-	const API_URL = 'http://host.docker.internal:3000/api/v2/ai/';
+	const API_URL = 'https://my.elementor.com/api/v2/ai/';
 
 	const STYLE_PRESET = 'style_preset';
 	const IMAGE_TYPE = 'image_type';
