a397b5484ff2d3b6a188e17abb484945cc1938ec
Mati Horowitz <mati@elementor.com>
2024-12-05 18:41:42 +0200

    Internal: AI - Prepare for installing AI kits

--- modules/ai/module.php
+++ modules/ai/module.php
@@ -9,6 +9,7 @@ use Elementor\Modules\Ai\Feature_Intro\Product_Image_Unification_Intro;
 use Elementor\Plugin;
 use Elementor\Core\Utils\Collection;
 use Elementor\Modules\Ai\Connect\Ai;
+use Elementor\Tracker;
 use Elementor\User;
 use Elementor\Utils;
 
@@ -39,6 +40,7 @@ class Module extends BaseModule {
 
 		if ( is_admin() ) {
 			( new Preferences() )->register();
+			add_action( 'elementor/import-export/import-kit/runner/after-run', [ $this, 'handle_kit_install' ] );
 		}
 
 		if ( ! Plugin::$instance->experiments->is_feature_active( 'container' ) ) {
@@ -163,6 +165,41 @@ class Module extends BaseModule {
 
 		add_action( 'elementor/element/common/section_effects/after_section_start', [ $this, 'register_ai_motion_effect_control' ], 10, 1 );
 		add_action( 'elementor/element/common/_section_transform/after_section_end', [ $this, 'register_ai_hover_effect_control' ], 10, 1 );
+	}
+
+	public function handle_kit_install( $imported_data ) {
+		if ( $imported_data['status'] !== 'success' ) {
+			return;
+		}
+
+		if ( $imported_data['runner'] !== 'site-settings' ) {
+			return;
+		}
+
+		$is_connected = $this->get_ai_app()->is_connected() && User::get_introduction_meta( 'ai_get_started' );
+
+		if ( ! Tracker::is_allow_track() && ! $is_connected ) {
+			return;
+		}
+
+		if ( ! isset( $imported_data['configData']['lastImportedSession']['instance_data']['site_settings']['settings']['ai'] ) ) {
+			return;
+		}
+
+		$lastImportedSession = $imported_data['configData']['lastImportedSession'];
+		$imported_ai_data = $lastImportedSession['instance_data']['site_settings']['settings']['ai'];
+
+		if ( $is_connected ) {
+			$this->get_ai_app()->send_event( [
+				'name' => 'kit_installed',
+				'data' => $imported_ai_data,
+				'client' => [
+					'name' => 'elementor',
+					'version' => ELEMENTOR_VERSION,
+					'session_id' => $lastImportedSession['session_id'],
+				],
+			] );
+		}
 	}
 
 	public function register_ai_hover_effect_control( Element_Base $element ) {
