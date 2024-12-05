a7e48939e07ab33fb2ebf125860d52aa9bdd6183
Rami Yushuvaev <ramiy@elementor.com>
2024-12-05 16:40:29 +0200

    Revert

--- includes/controls/groups/background.php
+++ includes/controls/groups/background.php
@@ -161,7 +161,7 @@ class Group_Control_Background extends Group_Control_Base {
 						'conditions' => [
 							'terms' => [
 								[
-									'name' => $fields['background'],
+									'name' => 'background',
 									'operator' => '!==',
 									'value' => '',
 								],
