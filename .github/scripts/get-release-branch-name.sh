8bb0f788d037fbab9f816e8c7aee6591ef15ac80
Yotam Shapira <yotams@elementor.com>
2024-12-15 15:35:39 +0200

    Use PNPM

--- .github/scripts/get-release-branch-name.sh
+++ .github/scripts/get-release-branch-name.sh
@@ -4,7 +4,7 @@ set -eo pipefail
 if [ -z "$INPUT_VERSION" ];
 then
 	PACKAGE_VERSION=$(node -p "require('./package.json').version")
-	npm install --no-package-lock --no-save semver@7.3.4
+	pnpm install --no-package-lock --no-save semver@7.3.4
 	NEXT_PACKAGE_VERSION=$(npx semver $PACKAGE_VERSION -i minor)
 	RELEASE_BRANCH="release/${NEXT_PACKAGE_VERSION}"
 else
