3bcd6f6d9e5b0b7fb7032d2d8810e405b488c45c
Mike Kokhanov <mikek@elementor.com>
2024-12-17 19:37:42 +0200

    [ misc ]: removed excessive use statement

--- modules/atomic-widgets/prop-types/background-color-overlay-prop-type.php
+++ modules/atomic-widgets/prop-types/background-color-overlay-prop-type.php
@@ -2,8 +2,6 @@
 
 namespace Elementor\Modules\AtomicWidgets\PropTypes;
 
-use Elementor\Modules\AtomicWidgets\PropTypes\Base\Object_Prop_Type;
-
 class Background_Color_Overlay_Prop_Type extends Color_Prop_Type {
 	public static function get_key(): string {
 		return 'background-color-overlay';
