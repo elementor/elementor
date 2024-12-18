be3940d8a7669f78effc3a2347c9c05fc3871a69
Hein van Vlastuin <heinvanvlastuin@gmail.com>
2024-12-18 16:26:16 +0200

    wip

--- core/experiments/manager.php
+++ core/experiments/manager.php
@@ -398,6 +398,14 @@ class Manager extends Base_Object {
 			'release_status' => self::RELEASE_STATUS_ALPHA,
 			'default' => self::STATE_ACTIVE,
 		] );
+
+		$this->add_feature( [
+			'name' => 'e_load_js_files_conditionally',
+			'title' => esc_html__( 'Conditionally Load JS Files', 'elementor' ),
+			'tag' => esc_html__( 'Performance', 'elementor' ),
+			static::TYPE_HIDDEN => true,
+			'default' => self::STATE_INACTIVE,
+		] );
 	}
 
 	/**
