273885b4e8ee75aec1e3a49ac1cecbca32772531
Nikita (Mykyta) <134918658+mykytamurzin@users.noreply.github.com>
2024-12-19 15:03:11 +0100

    Internal: Add link control to atomic heading, create some infra [ED-16302] (#29565)
    ## PR Checklist
    <!-- 
    Please check if your PR fulfills the following requirements:
    **Filling out the template is required.** Any pull request that does not
    include enough information to be reviewed in a timely manner may be
    closed at the maintainers' discretion.
     -->
    - [ ] The commit message follows our guidelines:
    https://github.com/elementor/elementor/blob/master/.github/CONTRIBUTING.md
    
    
    ## PR Type
    What kind of change does this PR introduce?
    <!-- Please check the one that applies to this PR using "x" with no
    spaces eg: [x]. -->
    - [ ] Bugfix
    - [ ] Feature
    - [ ] Code style update (formatting, local variables)
    - [ ] Refactoring (no functional changes, no api changes)
    - [ ] Build related changes
    - [ ] CI related changes
    - [ ] Documentation content changes
    - [ ] Other... Please describe:
    
    ## Summary
    
    This PR can be summarized in the following changelog entry:
    
    *
    
    ## Description
    An explanation of what is done in this PR
    
    *
    
    ## Test instructions
    This PR can be tested by following these steps:
    
    *
    
    ## Quality assurance
    
    - [ ] I have tested this code to the best of my abilities
    - [ ] I have added unittests to verify the code works as intended
    - [ ] Docs have been added / updated (for bug fixes / features)
    
    Fixes #
    
    ---------
    
    Co-authored-by: Louis Wolmarans <louiswol94@gmail.com>

--- modules/atomic-widgets/widgets/atomic-heading.php
+++ modules/atomic-widgets/widgets/atomic-heading.php
@@ -1,11 +1,14 @@
 <?php
 namespace Elementor\Modules\AtomicWidgets\Widgets;
 
+use Elementor\Modules\AtomicWidgets\Controls\Dynamic_Section;
 use Elementor\Modules\AtomicWidgets\Controls\Section;
+use Elementor\Modules\AtomicWidgets\Controls\Types\Link_Control;
 use Elementor\Modules\AtomicWidgets\Controls\Types\Select_Control;
 use Elementor\Modules\AtomicWidgets\Controls\Types\Textarea_Control;
 use Elementor\Modules\AtomicWidgets\Base\Atomic_Widget_Base;
 use Elementor\Modules\AtomicWidgets\PropTypes\Classes_Prop_Type;
+use Elementor\Modules\AtomicWidgets\PropTypes\Link_Prop_Type;
 use Elementor\Modules\AtomicWidgets\PropTypes\Primitives\String_Prop_Type;
 use Elementor\Utils;
 
@@ -29,19 +32,40 @@ class Atomic_Heading extends Atomic_Widget_Base {
 	protected function render() {
 		$settings = $this->get_atomic_settings();
 
+		$format = $this->get_heading_template( ! empty( $settings['link']['href'] ) );
+		$args = $this->get_template_args( $settings );
+
+		echo sprintf( $format, ...$args ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+	}
+
+	private function get_heading_template( bool $is_link_enabled ): string {
+		return $is_link_enabled ? '<%1$s %2$s><a %3$s>%4$s</a></%1$s>' : '<%1$s %2$s>%3$s</%1$s>';
+	}
+
+	private function get_template_args( array $settings ): array {
 		$tag = $settings['tag'];
-		$title = $settings['title'];
+		$title = esc_html( $settings['title'] );
 		$attrs = array_filter( [
 			'class' => $settings['classes'] ?? '',
 		] );
 
-		echo sprintf(
-			'<%1$s %2$s>%3$s</%1$s>',
-			// TODO: we should avoid using `validate html tag` and use the enum validation instead.
-			Utils::validate_html_tag( $tag ), // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
-			Utils::render_html_attributes( $attrs ), // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
-			esc_html( $title )
-		);
+		$default_args = [
+			Utils::validate_html_tag( $tag ),
+			Utils::render_html_attributes( $attrs ),
+		];
+
+		if ( ! empty( $settings['link']['href'] ) ) {
+			$link_args = [
+				Utils::render_html_attributes( $settings['link'] ),
+				esc_html( $title ),
+			];
+
+			return array_merge( $default_args, $link_args );
+		}
+
+		$default_args[] = $title;
+
+		return $default_args;
 	}
 
 	protected function define_atomic_controls(): array {
@@ -81,7 +105,9 @@ class Atomic_Heading extends Atomic_Widget_Base {
 					Textarea_Control::bind_to( 'title' )
 						->set_label( __( 'Title', 'elementor' ) )
 						->set_placeholder( __( 'Type your title here', 'elementor' ) ),
-				]),
+
+					Link_Control::bind_to( 'link' ),
+				] ),
 		];
 	}
 
@@ -96,6 +122,8 @@ class Atomic_Heading extends Atomic_Widget_Base {
 
 			'title' => String_Prop_Type::make()
 				->default( __( 'Your Title Here', 'elementor' ) ),
+
+			'link' => Link_Prop_Type::make(),
 		];
 	}
 }
