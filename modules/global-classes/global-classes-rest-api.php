64760a57f7d86020814a7ab3189179b6f5501688
Ron Rostovsky <ronros@elementor.com>
2024-12-19 15:21:27 +0200

    wip

--- modules/global-classes/global-classes-rest-api.php
+++ modules/global-classes/global-classes-rest-api.php
@@ -78,11 +78,6 @@ class Global_Classes_REST_API {
 
 					return $is_valid;
 				},
-				'parse_callback' => function( \WP_REST_Request $request ) {
-					Style_Parser::make( Style_Schema::get() )
-						->without_id()
-						->parse( $request->get_body_params() );
-				},
 				'permission_callback' => fn() => current_user_can( 'manage_options' ),
 			],
 		] );
@@ -97,11 +92,6 @@ class Global_Classes_REST_API {
 						->validate( $request->get_body_params() );
 
 					return $is_valid;
-				},
-				'parse_callback' => function( \WP_REST_Request $request ) {
-					Style_Parser::make( Style_Schema::get() )
-						->without_id()
-						->parse( $request->get_body_params() );
 				},
 				'permission_callback' => fn() => current_user_can( 'manage_options' ),
 			],
