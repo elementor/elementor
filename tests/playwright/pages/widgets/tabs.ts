47c6118b65e0db115c62c1d04eafab01973d3540
Rami Yushuvaev <92088692+rami-elementor@users.noreply.github.com>
2024-12-24 04:10:51 -0800

    Apply suggestions from code review
    Co-authored-by: Davids Seveloff <94905340+davseve@users.noreply.github.com>

--- tests/playwright/pages/widgets/tabs.ts
+++ tests/playwright/pages/widgets/tabs.ts
@@ -13,8 +13,9 @@ export default class TabsWidget extends Content {
 	async createNewTab( tabName: string, tabContent: string ): Promise<void> {
 		const itemCount = await this.page.locator( EditorSelectors.item ).count();
 		await this.page.getByRole( 'button', { name: 'Add Item' } ).click();
-		await this.page.getByRole( 'textbox', { name: 'Title' } ).click();
-		await this.page.getByRole( 'textbox', { name: 'Title' } ).fill( tabName );
+		const tabTitle = await this.page.getByRole('textbox', { name: 'Title' });
+		await tabTitle.click();
+		await tabTitle.fill(tabName);
 		const textEditor = this.page.frameLocator( EditorSelectors.tabs.textEditorIframe ).nth( itemCount );
 		await textEditor.locator( 'html' ).click();
 		await textEditor.getByText( 'Tab Content' ).click();
