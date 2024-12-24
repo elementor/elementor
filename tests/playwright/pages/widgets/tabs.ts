db57b83f658ee67eae4d1f152c27963d2f902a1b
Rami Yushuvaev <ramiy@elementor.com>
2024-12-24 15:48:19 +0200

    Fix JS lint

--- tests/playwright/pages/widgets/tabs.ts
+++ tests/playwright/pages/widgets/tabs.ts
@@ -13,9 +13,9 @@ export default class TabsWidget extends Content {
 	async createNewTab( tabName: string, tabContent: string ): Promise<void> {
 		const itemCount = await this.page.locator( EditorSelectors.item ).count();
 		await this.page.getByRole( 'button', { name: 'Add Item' } ).click();
-		const tabTitle = await this.page.getByRole('textbox', { name: 'Title' });
+		const tabTitle = this.page.getByRole( 'textbox', { name: 'Title' } );
 		await tabTitle.click();
-		await tabTitle.fill(tabName);
+		await tabTitle.fill( tabName );
 		const textEditor = this.page.frameLocator( EditorSelectors.tabs.textEditorIframe ).nth( itemCount );
 		await textEditor.locator( 'html' ).click();
 		await textEditor.getByText( 'Tab Content' ).click();
