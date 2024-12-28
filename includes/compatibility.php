94ea0b7db68d5234bc60eec7d45c08588532a881
Rami Yushuvaev <ramiy@elementor.com>
2024-12-28 10:48:17 +0200

    Fix: HTML markup validity by removing `type` attribute from `<script>` tag

--- includes/compatibility.php
+++ includes/compatibility.php
@@ -99,7 +99,7 @@ class Compatibility {
 		}
 
 		?>
-		<script type="text/javascript">
+		<script>
 			document.addEventListener( 'DOMContentLoaded', function() {
 				var dropdown = document.querySelector( '#split-page-title-action .dropdown' );
 
