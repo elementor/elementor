6c95c099c861e4c6462465cae8dd79e5d348e2e0
David Seveloff <davids@elementor.com>
2025-01-07 14:45:00 +0200

    wip

--- modules/atomic-widgets/assets/js/editor/container/div-block-empty-view.js
+++ modules/atomic-widgets/assets/js/editor/container/div-block-empty-view.js
@@ -1,4 +1,3 @@
-import ReactUtils from 'elementor-utils/react';
 import EmptyComponent from 'elementor-elements/views/container/empty-component';
 
 export default class DivBlockEmptyView extends Marionette.ItemView {
@@ -7,7 +6,7 @@ export default class DivBlockEmptyView extends Marionette.ItemView {
 	className = 'elementor-empty-view';
 
 	renderReactDefaultElement( container ) {
-		const { unmount } = ReactUtils.render( <EmptyComponent container={ container } />, this.el );
+		const { unmount } = elementor.react.utils.default.render( <EmptyComponent container={ container } />, this.el );
 		this.unmount = unmount;
 	}
 
