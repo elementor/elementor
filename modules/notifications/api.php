c0b74c82854f637f761086fbe79d527e68f01c6d
idan.magled <idan.magled@vesttoo.com>
2025-01-09 11:36:47 +0200

    - Fixed pr comments

--- modules/notifications/api.php
+++ modules/notifications/api.php
@@ -88,7 +88,7 @@ class API {
 			// Reset results for each condition.
 			$result = false;
 			switch ( $condition['type'] ) {
-				case 'WordPress': // phpcs:ignore WordPress.WP.CapitalPDangit.Misspelled
+				case 'Wordpress': // phpcs:ignore WordPress.WP.CapitalPDangit.Misspelled
 					// include an unmodified $wp_version
 					include ABSPATH . WPINC . '/version.php';
 					$result = version_compare( $wp_version, $condition['version'], $condition['operator'] );
