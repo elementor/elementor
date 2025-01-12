e13043cc4a7fb95f1bad46baabcee23a77b634c6
David Seveloff <davids@elementor.com>
2025-01-12 15:21:49 +0200

    wip

--- tests/phpunit/elementor/modules/atomic-widgets/test-atomic-paragraph.php
+++ tests/phpunit/elementor/modules/atomic-widgets/test-atomic-paragraph.php
@@ -52,13 +52,13 @@ class Elementor_Tests_Elementor_Modules_AtomicWidgets_TestAtomicParagraph extend
 		$actual = ob_get_clean();
 
 		// Assert.
-		$this->assertEq( $this->widgetStringOutput( $expected ), $this->widgetStringOutput( $actual ) );
+		$this->assertSame( $this->widgetStringOutput( $expected ), $this->widgetStringOutput( $actual ) );
 	}
 
 
 	public function widgetStringOutput( string $string ): void {
 		ob_start();
 		echo $string;
-		echo ob_get_clean();
+		ob_get_clean();
 	}
 }
