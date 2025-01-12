86ea0607edd3da66ce2ff98892f4cdf50718e7a6
ManorHazazE <manorh@elementor.com>
2025-01-12 11:31:48 +0200

    fix again

--- core/utils/import-export/wp-import.php
+++ core/utils/import-export/wp-import.php
@@ -61,8 +61,9 @@ class WP_Import extends \WP_Importer {
 	 */
 	private $id;
 
-	// Information to import from WXR file.
-
+	/**
+	 * Information to import from WXR file.
+	 */
 	private $version;
 	private $authors = [];
 	private $posts = [];
@@ -71,8 +72,9 @@ class WP_Import extends \WP_Importer {
 	private $page_on_front;
 	private $base_blog_url = '';
 
-	// Mappings from old information to new.
-
+	/**
+	 * Mappings from old information to new.
+	 */
 	private $processed_taxonomies;
 	private $processed_terms = [];
 	private $processed_posts = [];
