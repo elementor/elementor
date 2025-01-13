fd15ae9553ed23716b782e7e60be9aa04fe54180
David Seveloff <davids@elementor.com>
2025-01-13 17:45:28 +0200

    Review comments

--- tests/phpunit/elementor/modules/atomic-widgets/test-atomic-paragraph.php
+++ tests/phpunit/elementor/modules/atomic-widgets/test-atomic-paragraph.php
@@ -19,18 +19,16 @@ class Elementor_Tests_Elementor_Modules_AtomicWidgets_TestAtomicParagraph extend
 		$this->instance = Plugin::$instance->elements_manager->create_element_instance( self::MOCK );
 	}
 
-	public function test_get_paragraph_widget_name(): void {
-		$this->assertSame( 'a-paragraph', $this->instance->get_name() );
-	}
-
-	public function test_get_paragraph_widget_icon(): void {
-		$this->assertSame( 'eicon-text', $this->instance->get_icon() );
-	}
-
-	public function test_default_paragraph_value_render(): void {
+	/**
+	 * @dataProvider data_test_render_values
+	 */
+	public function test_render__paragraph( $default, $expected_content) {
 	    // Arrange.
+		if( ! $default ) {
+			$this->instance->set_settings( [ 'paragraph' => $expected_content ] );
+		}
+
 		$expected_tag = 'p';
-		$expected_content = 'Type your paragraph here';
 
 	    // Act.
 	    ob_start();
@@ -46,24 +44,10 @@ class Elementor_Tests_Elementor_Modules_AtomicWidgets_TestAtomicParagraph extend
 		$this->assertSame( $expected_content, $actual_content );
 	}
 
-	public function test_set_paragraph_value(): void {
-		// Arrange.
-		$new_content = 'Hello';
-		$this->instance->set_settings( [ 'paragraph' => $new_content ] );
-		$expected_tag = 'p';
-		$expected_content = $new_content;
-
-		// Act.
-		ob_start();
-		$this->instance->render_content();
-		$actual = ob_get_clean();
-
-		preg_match('/<\s*([a-zA-Z][a-zA-Z0-9]*)\b[^>]*>(.*?)<\/\s*\\1\s*>/s', $actual, $matches);
-		$actual_tag = $matches[1] ?? null;
-		$actual_content = $matches[2] ?? null;
-
-		// Assert.
-		$this->assertSame( $expected_tag, $actual_tag );
-		$this->assertSame( $expected_content, $actual_content );
+	public function data_test_render_values(): array {
+		return [
+			[ true, 'Type your paragraph here' ],
+			[ false, 'New Text' ],
+		];
 	}
 }
