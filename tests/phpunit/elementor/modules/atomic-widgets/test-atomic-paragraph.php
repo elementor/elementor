f1da8f5eff6b80ed77e6ea6138d72bf5ba8a802d
David Seveloff <davids@elementor.com>
2025-01-13 13:29:18 +0200

    wip

--- tests/phpunit/elementor/modules/atomic-widgets/test-atomic-paragraph.php
+++ tests/phpunit/elementor/modules/atomic-widgets/test-atomic-paragraph.php
@@ -2,10 +2,8 @@
 
 use Elementor\Plugin;
 use ElementorEditorTesting\Elementor_Test_Base;
-use Spatie\Snapshots\MatchesSnapshots;
 
 class Elementor_Tests_Elementor_Modules_AtomicWidgets_TestAtomicParagraph extends Elementor_Test_Base {
-	use MatchesSnapshots;
 	const MOCK = [
 		'id' => 'e8e55a1',
 		'elType' => 'widget',
@@ -29,26 +27,43 @@ class Elementor_Tests_Elementor_Modules_AtomicWidgets_TestAtomicParagraph extend
 		$this->assertSame( 'eicon-text', $this->instance->get_icon() );
 	}
 
-	public function test_paragraph_render(): void {
-		// Act.
-		ob_start();
-		$this->instance->render_content();
-		$rendered_output = ob_get_clean();
+	public function test_default_paragraph_value_render(): void {
+	    // Arrange.
+		$expected_tag = 'p';
+		$expected_content = 'Type your paragraph here';
+
+	    // Act.
+	    ob_start();
+	    $this->instance->render_content();
+	    $actual = ob_get_clean();
+
+		preg_match('/<\s*([a-zA-Z][a-zA-Z0-9]*)\b[^>]*>(.*?)<\/\s*\\1\s*>/s', $actual, $matches);
+		$actual_tag = $matches[1] ?? null;
+		$actual_content = $matches[2] ?? null;
 
 		// Assert.
-		$this->assertMatchesSnapshot( $rendered_output );
+		$this->assertSame( $expected_tag, $actual_tag );
+		$this->assertSame( $expected_content, $actual_content );
 	}
 
 	public function test_set_paragraph_value(): void {
 		// Arrange.
-		$this->instance->set_settings( [ 'paragraph' => 'Hello' ] );
+		$new_content = 'Hello';
+		$this->instance->set_settings( [ 'paragraph' => $new_content ] );
+		$expected_tag = 'p';
+		$expected_content = $new_content;
 
 		// Act.
 		ob_start();
 		$this->instance->render_content();
-		$rendered_output = ob_get_clean();
+		$actual = ob_get_clean();
+
+		preg_match('/<\s*([a-zA-Z][a-zA-Z0-9]*)\b[^>]*>(.*?)<\/\s*\\1\s*>/s', $actual, $matches);
+		$actual_tag = $matches[1] ?? null;
+		$actual_content = $matches[2] ?? null;
 
 		// Assert.
-		$this->assertMatchesSnapshot( $rendered_output );
+		$this->assertSame( $expected_tag, $actual_tag );
+		$this->assertSame( $expected_content, $actual_content );
 	}
 }
