d130b1470ab9a6a41376979fcf08dbb8f7f56cf7
David Seveloff <davids@elementor.com>
2025-01-14 11:57:45 +0200

    wip

--- tests/phpunit/elementor/modules/atomic-widgets/test-atomic-paragraph.php
+++ tests/phpunit/elementor/modules/atomic-widgets/test-atomic-paragraph.php
@@ -2,8 +2,10 @@
 
 use Elementor\Plugin;
 use ElementorEditorTesting\Elementor_Test_Base;
+use Spatie\Snapshots\MatchesSnapshots;
 
 class Elementor_Tests_Elementor_Modules_AtomicWidgets_TestAtomicParagraph extends Elementor_Test_Base {
+	use MatchesSnapshots;
 	const MOCK = [
 		'id' => 'e8e55a1',
 		'elType' => 'widget',
@@ -19,35 +21,13 @@ class Elementor_Tests_Elementor_Modules_AtomicWidgets_TestAtomicParagraph extend
 		$this->instance = Plugin::$instance->elements_manager->create_element_instance( self::MOCK );
 	}
 
-	/**
-	 * @dataProvider data_test_render_values
-	 */
-	public function test_render__paragraph( $default, $expected_content) {
-	    // Arrange.
-		if( ! $default ) {
-			$this->instance->set_settings( [ 'paragraph' => $expected_content ] );
-		}
-
-		$expected_tag = 'p';
-
-	    // Act.
-	    ob_start();
-	    $this->instance->render_content();
-	    $actual = ob_get_clean();
-
-		preg_match('/<\s*([a-zA-Z][a-zA-Z0-9]*)\b[^>]*>(.*?)<\/\s*\\1\s*>/s', $actual, $matches);
-		$actual_tag = $matches[1] ?? null;
-		$actual_content = $matches[2] ?? null;
+	public function test__paragraph_render(): void {
+		// Act.
+		ob_start();
+		$this->instance->render_content();
+		$rendered_output = ob_get_clean();
 
 		// Assert.
-		$this->assertSame( $expected_tag, $actual_tag );
-		$this->assertSame( $expected_content, $actual_content );
-	}
-
-	public function data_test_render_values(): array {
-		return [
-			[ true, 'Type your paragraph here' ],
-			[ false, 'New Text' ],
-		];
+		$this->assertMatchesSnapshot( $rendered_output );
 	}
 }
