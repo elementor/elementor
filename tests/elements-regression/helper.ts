f4a674181cbe2b42f7c556ec44f53bf987c41bc8
Hein van Vlastuin <94352322+hein-obox@users.noreply.github.com>
2025-01-15 14:40:17 +0200

    Update helper.ts

--- tests/elements-regression/helper.ts
+++ tests/elements-regression/helper.ts
@@ -78,7 +78,7 @@ export default class ElementRegressionHelper {
 		let label = '';
 		const deviceParams = { mobile: { width: 360, height: 736 }, tablet: { width: 768, height: 787 } };
 
-		// TODO: Fix separately in [ED-17812].
+		// TODO: Fix in a separate task.
 		if ( 'container_grid' === args.widgetType && args.isPublished && 'mobile' === args.device ) {
 			return;
 		}
