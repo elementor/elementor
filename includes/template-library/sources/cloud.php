ddbb4ea28d2d94c7ca53e0b5a2d1523139b05d79
Louis Wolmarans <louiswol94@gmail.com>
2025-01-15 08:48:25 +0200

    Internal: Cloud Library setup [ED-17782]

--- includes/template-library/sources/cloud.php
+++ includes/template-library/sources/cloud.php
@@ -7,11 +7,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 
-include 'remote-base.php';
-
-class Source_Cloud extends Source_Remote_Base {
-
-	const TEMPLATES_DATA_TRANSIENT_KEY_PREFIX = 'elementor_cloud_library_templates_data_';
+class Source_Cloud extends Source_Base {
 
 	public function get_id() {
 		return 'cloud';
@@ -23,24 +19,17 @@ class Source_Cloud extends Source_Remote_Base {
 
 	public function register_data() {}
 
-	protected function get_template_content( $template_id ) {
-		$app = Plugin::$instance->common->get_component( 'connect' )->get_app( 'cloud-library' );
+	public function get_items( $args = [] ) {}
+	
+	public function get_item( $template_id ) {}
 
-		return $app->get_resource( $template_id );
-	}
+	public function get_data( array $args ) {}
 
-	protected function prepare_template( array $template_data ): array {
-		return [
-			'template_id' => $template_data['id'],
-			'source' => $this->get_id(),
-			'type' => $template_data['type'],
-			'subtype' => $template_data['subtype'],
-			'title' => $template_data['title'],
-			'thumbnail' => $template_data['thumbnail'],
-			'date' => $template_data['tmpl_created'],
-			'author' => $template_data['author'],
-			'hasPageSettings' => ( '1' === $template_data['has_page_settings'] ),
-			'url' => $template_data['url'],
-		];
-	}
+	public function delete_template( $template_id ) {}
+
+	public function save_item( $template_data ) {}
+
+	public function update_item( $new_data ) {}
+
+	public function export_template( $template_id ) {}
 }
--- includes/template-library/sources/remote.php
+++ includes/template-library/sources/remote.php
@@ -1,13 +1,13 @@
 <?php
 namespace Elementor\TemplateLibrary;
 
+use Elementor\Api;
 use Elementor\Core\Common\Modules\Connect\Module as ConnectModule;
+use Elementor\Plugin;
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
-
-include 'remote-base.php';
 
 /**
  * Elementor template library remote source.
@@ -17,9 +17,21 @@ include 'remote-base.php';
  *
  * @since 1.0.0
  */
-class Source_Remote extends Source_Remote_Base {
+class Source_Remote extends Source_Base {
 
 	const API_TEMPLATES_URL = 'https://my.elementor.com/api/connect/v1/library/templates';
+
+	const TEMPLATES_DATA_TRANSIENT_KEY_PREFIX = 'elementor_remote_templates_data_';
+
+	public function __construct() {
+		parent::__construct();
+
+		$this->add_actions();
+	}
+
+	public function add_actions() {
+		add_action( 'elementor/experiments/feature-state-change/container', [ $this, 'clear_cache' ], 10, 0 );
+	}
 
 	/**
 	 * Get remote template ID.
@@ -59,6 +71,50 @@ class Source_Remote extends Source_Remote_Base {
 	 * @access public
 	 */
 	public function register_data() {}
+
+	/**
+	 * Get remote templates.
+	 *
+	 * Retrieve remote templates from Elementor.com servers.
+	 *
+	 * @since 1.0.0
+	 * @access public
+	 *
+	 * @param array $args Optional. Not used in remote source.
+	 *
+	 * @return array Remote templates.
+	 */
+	public function get_items( $args = [] ) {
+		$force_update = ! empty( $args['force_update'] ) && is_bool( $args['force_update'] );
+
+		$templates_data = $this->get_templates_data( $force_update );
+
+		$templates = [];
+
+		foreach ( $templates_data as $template_data ) {
+			$templates[] = $this->prepare_template( $template_data );
+		}
+
+		return $templates;
+	}
+
+	/**
+	 * Get remote template.
+	 *
+	 * Retrieve a single remote template from Elementor.com servers.
+	 *
+	 * @since 1.0.0
+	 * @access public
+	 *
+	 * @param int $template_id The template ID.
+	 *
+	 * @return array Remote template.
+	 */
+	public function get_item( $template_id ) {
+		$templates = $this->get_items();
+
+		return $templates[ $template_id ];
+	}
 
 	/**
 	 * Save remote template.
@@ -129,6 +185,75 @@ class Source_Remote extends Source_Remote_Base {
 	}
 
 	/**
+	 * Get remote template data.
+	 *
+	 * Retrieve the data of a single remote template from Elementor.com servers.
+	 *
+	 * @since 1.5.0
+	 * @access public
+	 *
+	 * @param array  $args    Custom template arguments.
+	 * @param string $context Optional. The context. Default is `display`.
+	 *
+	 * @return array|\WP_Error Remote Template data.
+	 */
+	public function get_data( array $args, $context = 'display' ) {
+		$data = Api::get_template_content( $args['template_id'] );
+
+		if ( is_wp_error( $data ) ) {
+			return $data;
+		}
+
+		// Set the Request's state as an Elementor upload request, in order to support unfiltered file uploads.
+		Plugin::$instance->uploads_manager->set_elementor_upload_state( true );
+
+		// BC.
+		$data = (array) $data;
+
+		$data['content'] = $this->replace_elements_ids( $data['content'] );
+		$data['content'] = $this->process_export_import_content( $data['content'], 'on_import' );
+
+		$post_id = $args['editor_post_id'];
+		$document = Plugin::$instance->documents->get( $post_id );
+		if ( $document ) {
+			$data['content'] = $document->get_elements_raw_data( $data['content'], true );
+		}
+
+		// After the upload complete, set the elementor upload state back to false
+		Plugin::$instance->uploads_manager->set_elementor_upload_state( false );
+
+		return $data;
+	}
+
+	/**
+	 * Get templates data from a transient or from a remote request.
+	 * In any of the following 2 conditions, the remote request will be triggered:
+	 * 1. Force update - "$force_update = true" parameter was passed.
+	 * 2. The data saved in the transient is empty or not exist.
+	 *
+	 * @param bool $force_update
+	 * @return array
+	 */
+	protected function get_templates_data( bool $force_update ): array {
+		$templates_data_cache_key = static::TEMPLATES_DATA_TRANSIENT_KEY_PREFIX . ELEMENTOR_VERSION;
+
+		$experiments_manager = Plugin::$instance->experiments;
+		$editor_layout_type = $experiments_manager->is_feature_active( 'container' ) ? 'container_flexbox' : '';
+
+		if ( $force_update ) {
+			return $this->get_templates( $editor_layout_type );
+		}
+
+		$templates_data = get_transient( $templates_data_cache_key );
+
+		if ( empty( $templates_data ) ) {
+			return $this->get_templates( $editor_layout_type );
+		}
+
+		return $templates_data;
+	}
+
+	/**
 	 * Get the templates from a remote server and set a transient.
 	 *
 	 * @param string $editor_layout_type
@@ -190,7 +315,7 @@ class Source_Remote extends Source_Remote_Base {
 	 * @since 2.2.0
 	 * @access private
 	 */
-	protected function prepare_template( array $template_data ): array {
+	protected function prepare_template( array $template_data ) {
 		$favorite_templates = $this->get_user_meta( 'favorites' );
 
 		// BC: Support legacy APIs that don't have access tiers.
@@ -221,5 +346,9 @@ class Source_Remote extends Source_Remote_Base {
 			'url' => $template_data['url'],
 			'favorite' => ! empty( $favorite_templates[ $template_data['id'] ] ),
 		];
+	}
+
+	public function clear_cache() {
+		delete_transient( static::TEMPLATES_DATA_TRANSIENT_KEY_PREFIX . ELEMENTOR_VERSION );
 	}
 }
