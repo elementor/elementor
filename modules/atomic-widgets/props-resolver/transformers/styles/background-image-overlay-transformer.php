a4114f138dd5e53c33e3e0e8ce4ccfd4b9f1a414
elchugreeva <elenac@elementor.com>
2025-01-15 09:35:37 +0200

    fix comments

--- modules/atomic-widgets/props-resolver/transformers/styles/background-image-overlay-transformer.php
+++ modules/atomic-widgets/props-resolver/transformers/styles/background-image-overlay-transformer.php
@@ -19,13 +19,11 @@ class Background_Image_Overlay_Transformer extends Transformer_Base {
 			return $this->get_image_src_by_url( $value['image-src']['url'] );
 		}
 
-		if ( empty( $value['image-src']['url'] ) ) {
-			throw new \Exception( 'Invalid image URL.' );
-		}
+		throw new \Exception( 'Invalid image URL.' );
 	}
 
 	private function get_image_src_by_id( $id ) {
-		$src = $this->fetch_image_url_by_id( $id );
+		$src = $this->get_image_url_by_id( $id );
 
 		return "url(\" $src \")";
 	}
@@ -35,7 +33,7 @@ class Background_Image_Overlay_Transformer extends Transformer_Base {
 		return "url(\" $url \")";
 	}
 
-	private function fetch_image_url_by_id( $id ) {
+	private function get_image_url_by_id( $id ) {
 		$image_src = wp_get_attachment_image_src(
 			(int) $id, 'full'
 		);
