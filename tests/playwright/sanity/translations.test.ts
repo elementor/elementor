6897f3f9f3a50b92f0f8aa55f04e36e125688383
MichaelLiamin <michaelli@elementor.com>
2025-01-16 13:57:49 +0200

    Test of 4 translations

---
+++ tests/playwright/sanity/translations.test.ts
@@ -0,0 +1,59 @@
+import { parallelTest as test } from '../parallelTest';
+import WpAdminPage from '../pages/wp-admin-page';
+import { expect } from '@playwright/test';
+import EditorPage from '../pages/editor-page';
+
+const testCaseslanguages = [
+  {
+    language: 'he_IL',
+    buttonText: 'פרסם',
+  },
+  {
+    language: 'en_NZ',
+    buttonText: 'Publish',
+  },
+  {
+    language: 'de_DE',
+    buttonText: 'Veröffentlichen',
+  },
+  {
+    language: 'fr_BE',
+    buttonText: 'Publier',
+  },
+];
+
+test.describe('Test site translation for different languages', () => {
+  testCaseslanguages.forEach((languageObj) => {
+    test(`Test of site translation for language ${languageObj.language} to Hebrew`, async ({ browser, apiRequests }, testInfo) => {
+      // Arrange.
+      const context = await browser.newContext();
+      const page = await context.newPage();
+      const wpAdmin = new WpAdminPage(page, testInfo, apiRequests);
+      const editor = new EditorPage(page, testInfo);
+      const translationButton = 'input[type="submit"][name="upgrade"]';
+
+      // Act
+      await wpAdmin.setSiteLanguage(languageObj.language);
+      await page.goto('/wp-admin/update-core.php');
+      await page.locator('.update-last-checked').click();
+      if (await page.locator(translationButton).nth(1).isVisible()) {
+        await page.locator(translationButton).last().click();
+      }
+      await wpAdmin.openNewPage();
+      await editor.closeNavigatorIfOpen();
+      const publishButton = page.locator('button.MuiButton-root').nth(1);
+
+      // Assert.
+      await expect(publishButton).toHaveText(languageObj.buttonText);
+      await page.close();
+    });
+  });
+});
+
+test.afterAll(async ({ browser, apiRequests}, testInfo ) => {
+  // Reset to default language (English)
+  const context = await browser.newContext();
+  const page = await context.newPage();
+  const wpAdmin = new WpAdminPage(page, testInfo, apiRequests);
+  await wpAdmin.setSiteLanguage('');
+});
