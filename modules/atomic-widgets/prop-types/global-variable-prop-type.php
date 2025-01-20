045d87b14bbecc998954ee4585381cf2d99feceb
Mike Kokhanov <mikek@elementor.com>
2025-01-20 10:57:21 +0200

    [ wip ]

---
+++ modules/atomic-widgets/prop-types/global-variable-prop-type.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Elementor\Modules\AtomicWidgets\PropTypes;
+
+use Elementor\Modules\AtomicWidgets\PropTypes\Base\Plain_Prop_Type;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+class Variable_Prop_Type extends Plain_Prop_Type {
+	public static function get_key(): string {
+		return 'global-color-var';
+	}
+
+	protected function validate_value( $value ): bool {
+		return ! empty( $value );
+	}
+
+	protected function sanitize_value( $value ) {
+		return $value;
+	}
+}
