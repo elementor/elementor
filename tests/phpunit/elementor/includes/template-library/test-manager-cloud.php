75dce9cd43eed33168b1fda0bd79384f2bea1b55
max-zu <zubovmaksim@gmail.com>
2025-01-21 16:15:22 +0100

    ED-17801 upd test

--- tests/phpunit/elementor/includes/template-library/test-manager-cloud.php
+++ tests/phpunit/elementor/includes/template-library/test-manager-cloud.php
@@ -8,40 +8,45 @@ class Elementor_Test_Manager_Cloud extends Elementor_Test_Base {
 	/**
 	 * @var \Elementor\TemplateLibrary\Manager
 	 */
-	protected static $manager;
+	protected $manager;
 
 	private $cloud_library_app_mock;
 
 	public function setUp(): void {
 		parent::setUp();
 		$this->cloud_library_app_mock = $this->getMockBuilder( '\Elementor\Modules\CloudLibrary\Connect\Cloud_Library' )
+			->onlyMethods( [ 'get_resources' ] )
 			->disableOriginalConstructor()
 			->getMock();
 
 		$module_mock = $this->getMockBuilder( \Elementor\Core\Common\Modules\Connect\Module::class )
-			->setMethods( [ 'get_app' ] )
+			->onlyMethods( [ 'get_app' ] )
 			->getMock();
 
-		$module_mock->method( 'get_app' )->willReturn( $this->ai_app_mock );
+		$module_mock->method( 'get_app' )->willReturn( $this->cloud_library_app_mock );
 
 		Plugin::$instance->common->add_component( 'connect', $module_mock );
+
+		$this->manager = Plugin::$instance->templates_manager;
 	}
 
-	public static function setUpBeforeClass(): void {
-		self::$manager = self::elementor()->templates_manager;
-	}
-
-	public function test_should_return_source() {
-		$this->assertInstanceOf( '\Elementor\TemplateLibrary\Source_Cloud', self::$manager->get_source( 'cloud' ) );
+	public function test_should_return_cloud_source() {
+		$this->assertInstanceOf( '\Elementor\TemplateLibrary\Source_Cloud', $this->manager->get_source( 'cloud' ) );
 	}
 
 	public function test_should_call_get_resources() {
-		self::$manager->get_templates([
-			'source' => 'cloud'
-		]);
+		// Arrange
+		$this->cloud_library_app_mock
+			->method('get_resources')
+			->willReturn([]);
 
-		$this->cloud_library_app_mock->expects( $this->once() )
-			->method( 'get_resources' )
-			->with( [ ] );
+		// Assert
+		$this->cloud_library_app_mock
+			->expects($this->once())
+			->method('get_resources')
+			->with([ 'force_update' => false]);
+
+		// Act
+		$this->manager->get_templates(['cloud']);
 	}
 }
