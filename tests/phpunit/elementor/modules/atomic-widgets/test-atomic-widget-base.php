b8f20bf9d241f2871d2e84ba3211ec983290021b
Batsirai Muchareva <batsiraim@elementor.red>
2025-01-21 13:01:38 +0200

    add test

--- tests/phpunit/elementor/modules/atomic-widgets/test-atomic-widget-base.php
+++ tests/phpunit/elementor/modules/atomic-widgets/test-atomic-widget-base.php
@@ -720,6 +720,30 @@ class Test_Atomic_Widget_Base extends Elementor_Test_Base {
 							'background' => [
 								'$$type' => 'background',
 								'value' => [
+									'background-overlay' => [
+										'$$type' => 'background-overlay',
+										'value' => [
+											[
+												'$$type' => 'background-image-overlay',
+												'value' => [
+													'resolution' => [
+														'$$type' => 'string',
+														'value' => 'large',
+													],
+													'image-src' => [
+														'$$type' => 'image-src',
+														'value' => [
+															'id' => [
+																'$$type' => 'image-attachment-id',
+																'value' => 3
+															],
+															'url' => null
+														]
+													],
+												]
+											]
+										]
+									],
 									'color' => [
 										'$$type' => 'color',
 										'value' => '#000000',
