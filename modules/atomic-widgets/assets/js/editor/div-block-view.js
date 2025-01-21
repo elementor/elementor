769b0b50b16a34336b5fc1a43e4e95acc6028ef5
baghdasarovelementor <norayrb@elementor.red>
2025-01-21 13:03:10 +0400

    Comment fix

--- modules/atomic-widgets/assets/js/editor/div-block-view.js
+++ modules/atomic-widgets/assets/js/editor/div-block-view.js
@@ -204,23 +204,31 @@ const DivBlockView = BaseElementView.extend( {
 
 				if ( 'horizontal' === this.getDroppableAxis() ) {
 					if ( isNotBeforeSibling ) {
-						const insertMethod = [ 'top', 'left' ].includes( side ) ? 'before' : 'after';
-						currentTarget[ insertMethod ]( placeholderElement );
+						this.handleDropSide( side, placeholderElement, currentTarget );
 					}
 
-					if ( $placeholder.css( 'height' ) !== `${ currentTargetHeight }px` ) {
-						$placeholder.css( {
-							display: 'block',
-							height: `${ currentTargetHeight }px`,
-							'background-color': '#eb8efb',
-							width: '10px',
-						} );
-					}
+					this.maybeShowCustomDropPlaceholder( $placeholder, currentTargetHeight )
 				} else {
 					$placeholder.removeAttr( 'style' );
 				}
 			},
 		};
+	},
+
+	handleDropSide( side, placeholderElement, currentTarget ) {
+		const insertMethod = [ 'top', 'left' ].includes( side ) ? 'before' : 'after';
+		currentTarget[ insertMethod ]( placeholderElement );
+	},
+
+	maybeShowCustomDropPlaceholder( $placeholder, currentTargetHeight ) {
+		if ( $placeholder.css( 'height' ) !== `${ currentTargetHeight }px` ) {
+			$placeholder.css( {
+				display: 'block',
+				height: `${ currentTargetHeight }px`,
+				'background-color': '#eb8efb',
+				width: '10px',
+			} );
+		}
 	},
 
 	getEditButtons() {
