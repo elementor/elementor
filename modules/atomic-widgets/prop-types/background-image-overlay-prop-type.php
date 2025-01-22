4b64074c52dded921bc7acf57e535b3c69aadf51
Hein van Vlastuin <94352322+hein-obox@users.noreply.github.com>
2025-01-22 11:32:24 +0200

    Update background-image-overlay-prop-type.php

--- modules/atomic-widgets/prop-types/background-image-overlay-prop-type.php
+++ modules/atomic-widgets/prop-types/background-image-overlay-prop-type.php
@@ -6,9 +6,9 @@ use Elementor\Modules\AtomicWidgets\PropTypes\Base\Object_Prop_Type;
 use Elementor\Modules\AtomicWidgets\PropTypes\Primitives\String_Prop_Type;
 
 class Background_Image_Overlay_Prop_Type extends Object_Prop_Type {
-    public static function get_key(): string {
-        return 'background-image-overlay';
-    }
+	public static function get_key(): string {
+		return 'background-image-overlay';
+	}
 
     protected function define_shape(): array {
         return [
