034d95e6c33d25c1e08b9a11e441499dbce648cf
Rami Yushuvaev <ramiy@elementor.com>
2025-01-23 10:42:18 +0200

    Add tooltips

--- assets/dev/js/editor/regions/sidebar/layout.js
+++ assets/dev/js/editor/regions/sidebar/layout.js
@@ -4,6 +4,35 @@ SidebarLayoutView = Marionette.LayoutView.extend( {
 	template: '#tmpl-elementor-sidebar',
 
 	id: 'elementor-sidebar-inner',
+
+	ui: {
+		buttons: 'button.tooltip-target',
+	},
+
+	onRender() {
+		this.addTooltip();
+	},
+
+	addTooltip() {
+		if ( ! this.ui.buttons?.length ) {
+			return;
+		}
+
+		this.ui.buttons.tipsy( {
+			gravity() {
+				var gravity = jQuery( this ).data( 'tooltip-pos' );
+
+				if ( undefined !== gravity ) {
+					return gravity;
+				}
+
+				return elementorCommon.config.isRTL ? 'e' : 'w';
+			},
+			title() {
+				return this.getAttribute( 'data-tooltip' );
+			},
+		} );
+	},
 } );
 
 module.exports = SidebarLayoutView;
