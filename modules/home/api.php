b14a0b05ffca37fa1806fd8ec0556d6b61dcd0de
elchugreeva <elenac@elementor.com>
2025-01-26 10:29:02 +0200

    ip

--- modules/home/api.php
+++ modules/home/api.php
@@ -3,6 +3,7 @@ namespace Elementor\Modules\Home;
 
 use Elementor\Includes\EditorAssetsAPI;
 use Elementor\Modules\Home\Classes\Transformations_Manager;
+use Elementor\Core\Utils\Promotions\Filtered_Promotions_Manager;
 
 class API {
 	protected EditorAssetsAPI $editor_assets_api;
@@ -13,6 +14,11 @@ class API {
 
 	public function get_home_screen_items( $force_request = false ): array {
 		$assets_data = $this->editor_assets_api->get_assets_data( $force_request );
+//		var_dump( $assets_data );
+
+		$assets_data = apply_filters('replace_homescreen_json_data', $assets_data);
+//		var_dump( $assets_data );
+//		die();
 
 		return $this->transform_home_screen_data( $assets_data );
 	}
