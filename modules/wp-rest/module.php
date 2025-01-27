1e31b182c2ef142861e9019df3b30e1f7f5e34ac
Mati Horowitz <21468434+matipojo@users.noreply.github.com>
2025-01-27 19:33:46 +0200

    Internal: REST API for Elementor post meta - fixed version [ED-17682] (#30030)

--- modules/wp-rest/module.php
+++ modules/wp-rest/module.php
@@ -3,6 +3,7 @@
 namespace Elementor\Modules\WpRest;
 
 use Elementor\Core\Base\Module as BaseModule;
+use Elementor\Modules\WpRest\Classes\Elementor_Post_Meta;
 use Elementor\Modules\WpRest\Classes\Elementor_Settings;
 use Elementor\Modules\WpRest\Classes\Elementor_User_Meta;
 
@@ -19,6 +20,7 @@ class Module extends BaseModule {
 	public function __construct() {
 		parent::__construct();
 		add_action( 'rest_api_init', function () {
+			( new Elementor_Post_Meta() )->register();
 			( new Elementor_Settings() )->register();
 			( new Elementor_User_Meta() )->register();
 		} );
