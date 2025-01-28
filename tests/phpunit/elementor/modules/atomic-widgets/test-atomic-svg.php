ecaad953093f45b32ffae54951a6824661e121bf
mugur <mugurell@elementor.red>
2025-01-28 10:21:36 +0200

    test fix

--- tests/phpunit/elementor/modules/atomic-widgets/test-atomic-svg.php
+++ tests/phpunit/elementor/modules/atomic-widgets/test-atomic-svg.php
@@ -5,29 +5,38 @@ use ElementorEditorTesting\Elementor_Test_Base;
 use Spatie\Snapshots\MatchesSnapshots;
 
 class Test_Atomic_Svg extends Elementor_Test_Base {
-	use MatchesSnapshots;
-	const MOCK = [
-		'id' => 'abcd123',
-		'elType' => 'widget',
-		'settings' => [],
-		'widgetType' => 'a-svg',
-	];
+    use MatchesSnapshots;
+    const MOCK = [
+        'id' => 'abcd123',
+        'elType' => 'widget',
+        'settings' => [
+            'svg' => [
+                'src' => [
+                    'url' => 'data:image/svg+xml;charset=utf-8,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%2024%2024%22%3E%3Cpath%20d%3D%22M0%200h24v24H0z%22%2F%3E%3C%2Fsvg%3E',
+                    'id' => null
+                ]
+            ],
+            'classes' => [],
+            'width' => '100%',
+            'height' => '100%'
+        ],
+        'widgetType' => 'a-svg',
+    ];
 
-	protected $instance;
+    protected $instance;
 
-	public function setUp(): void {
-		parent::setUp();
+    public function setUp(): void {
+        parent::setUp();
+        $this->instance = Plugin::$instance->elements_manager->create_element_instance( self::MOCK );
+    }
 
-		$this->instance = Plugin::$instance->elements_manager->create_element_instance( self::MOCK );
-	}
+    public function test__render_svg(): void {
+        // Act
+        ob_start();
+        $this->instance->render_content();
+        $rendered_output = ob_get_clean();
 
-	public function test__render_svg(): void {
-		// Act.
-		ob_start();
-		$this->instance->render_content();
-		$rendered_output = ob_get_clean();
-
-		// Assert.
-		$this->assertMatchesSnapshot( $rendered_output );
-	}
+        // Assert
+        $this->assertMatchesSnapshot( $rendered_output );
+    }
 }
