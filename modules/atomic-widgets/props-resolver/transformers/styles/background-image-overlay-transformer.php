40485093eed059b06f18f1114ff30d68d5db7696
YaelAvitan <YaelAvitan@github.com>
2025-02-05 16:22:27 +0200

    save changes

--- modules/atomic-widgets/props-resolver/transformers/styles/background-image-overlay-transformer.php
+++ modules/atomic-widgets/props-resolver/transformers/styles/background-image-overlay-transformer.php
@@ -4,80 +4,85 @@ namespace Elementor\Modules\AtomicWidgets\PropsResolver\Transformers\Styles;
 
 use Elementor\Modules\AtomicWidgets\PropsResolver\Transformer_Base;
 
-if ( ! defined( 'ABSPATH' ) ) {
+if (!defined('ABSPATH')) {
 	exit; // Exit if accessed directly.
 }
 
-class Background_Image_Overlay_Transformer extends Transformer_Base {
+class Background_Image_Overlay_Transformer extends Transformer_Base
+{
 	const DEFAULT_RESOLUTION = 'large';
 
-	public function transform( $value, $key ) {
-		if ( ! isset( $value['image-src'] ) ) {
+	public function transform($value, $key)
+	{
+		if (!isset($value['image-src'])) {
 			return '';
 		}
 
-		$image_url = $this->get_image_url( $value['image-src'], $value['resolution'] ?? self::DEFAULT_RESOLUTION );
+		$image_url = $this->get_image_url($value['image-src'], $value['resolution'] ?? self::DEFAULT_RESOLUTION);
 
 		$background_style = "url(\" $image_url \")";
 
-		if ( $value['repeat'] ) {
+		if ($value['repeat']) {
 			$background_style .= ' ' . $value['repeat'];
 		}
 
-		if ( $value['attachment'] ) {
+		if ($value['attachment']) {
 			$background_style .= ' ' . $value['attachment'];
 		}
 
-		$position_and_size_style = $this->get_position_and_size_style( $value );
+		$position_and_size_style = $this->get_position_and_size_style($value);
 
-		if ( ! empty( $position_and_size_style ) ) {
+		if (!empty($position_and_size_style)) {
 			$background_style .= ' ' . $position_and_size_style;
 		}
 
 		return $background_style;
 	}
 
-	private function get_image_url( array $image_src, string $resolution ): string {
-		if ( ! empty( $image_src['id'] ) ) {
-			return $this->get_image_url_by_id( $image_src['id'], $resolution );
+	private function get_image_url(array $image_src, string $resolution): string
+	{
+		if (!empty($image_src['id'])) {
+			return $this->get_image_url_by_id($image_src['id'], $resolution);
 		}
 
-		if ( empty( $image_src['url'] ) ) {
-			throw new \Exception( 'Invalid image URL.' );
+		if (empty($image_src['url'])) {
+			throw new \Exception('Invalid image URL.');
 		}
 
 		return $image_src['url'];
 	}
 
-	private function get_image_url_by_id( int $id, string $resolution ): string {
-		$image_src = wp_get_attachment_image_src( $id, $resolution );
+	private function get_image_url_by_id(int $id, string $resolution): string
+	{
+		$image_src = wp_get_attachment_image_src($id, $resolution);
 
-		if ( ! $image_src ) {
-			throw new \Exception( 'Cannot get image src.' );
+		if (!$image_src) {
+			throw new \Exception('Cannot get image src.');
 		}
 
-		[ $image_url ] = $image_src;
+		[$image_url] = $image_src;
 
 		return $image_url;
 	}
 
-	private function get_position_and_size_style( array $value ): string {
-		if ( ! isset( $value['size'] ) && ! isset( $value['position'] ) ) {
+	private function get_position_and_size_style(array $value): string
+	{
+		if (!isset($value['size']) && !isset($value['position'])) {
 			return '';
 		}
 
-		if ( ! isset( $value['size'] ) ) {
+		$position = $value['position'];
+		if (is_array($position)) { // Check if $position is an array
+			$position = $position['x'] . ' ' . $position['y'];
+		}
+
+		if (!isset($value['size'])) {
 			return $value['position'];
 		}
 
-		if ( $value['position'] === 'custom' ) {
-			return $value['position-custom-x'] . ' ' . $value['position-custom-y'];
+		if (!$position) {
+			$position = '0% 0%';
 		}
-
-		$default_position = '0% 0%';
-
-		$position = $value['position'] ?? $default_position;
-
 		return $position . ' / ' . $value['size'];
 	}
 }
