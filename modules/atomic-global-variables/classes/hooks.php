0acdd2d824e8276854af6f99986bb9b70fb384ec
Mike Kokhanov <mikek@elementor.com>
2025-02-05 23:02:27 +0200

    Internal: Gloval variables - register module [ED-17985]
     * added hook to render the css during page-load
     * added css rendering (global variables)

--- modules/atomic-global-variables/classes/hooks.php
+++ modules/atomic-global-variables/classes/hooks.php
@@ -6,12 +6,18 @@ use Elementor\Core\Isolation\Wordpress_Adapter_Interface;
 use Elementor\Modules\AtomicGlobalVariables\PropTypes\Color_Variable_Prop_Type;
 use Elementor\Modules\AtomicGlobalVariables\Transformers\Global_Variable as Global_Variable_Transformer;
 use Elementor\Modules\AtomicWidgets\PropsResolver\Transformers_Registry;
+use Elementor\Plugin;
+use Elementor\Core\Files\CSS\Post as Post_CSS;
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
 class Hooks {
+	const PRIORITY_HIGH = 1;
+	const PRIORITY_MID = 5;
+	const PRIORITY_DEFAULT = 10;
+
 	private Wordpress_Adapter_Interface $wp_adapter;
 
 	public function __construct( Wordpress_Adapter_Interface $wp_adapter ) {
@@ -27,6 +33,10 @@ class Hooks {
 			return $this->augment_style_schema( $schema );
 		} );
 
+		$this->wp_adapter->add_action( 'elementor/css-file/post/parse', function ( Post_CSS $post ) {
+			$this->inject_global_variables_css( $post );
+		} );
+
 		return $this;
 	}
 
@@ -36,5 +46,37 @@ class Hooks {
 
 	private function augment_style_schema( array $schema ): array {
 		return ( new Style_Schema() )->augment( $schema );
+	}
+
+	private function inject_global_variables_css( Post_CSS $post ): void {
+//		if ( ! Plugin::$instance->kits_manager->is_kit( $post->get_post_id() ) ) {
+//			return;
+//		}
+
+		$global_variables = [
+			'e-atomic-variable-gc-a01' => [
+				'value' => '#fff',
+				'name' => 'white',
+			],
+			'e-atomic-variable-gc-a02' => [
+				'value' => '#000',
+				'name' => 'black',
+			],
+			'e-atomic-variable-gc-a03' => [
+				'value' => '#f00',
+				'name' => 'red',
+			],
+		];
+
+		$raw_css = ':root {' . PHP_EOL;
+
+		foreach ( $global_variables as $idx => $variable ) {
+			$value = $variable['value'];
+			$raw_css = $raw_css . "--${idx}: ${value};" . PHP_EOL;
+		}
+
+		$raw_css .= '}' . PHP_EOL;
+
+		$post->get_stylesheet()->add_raw_css( $raw_css );
 	}
 }
