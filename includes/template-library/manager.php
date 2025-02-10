d667d79115adab079cc1edfb059406462d59d56f
Louis Wolmarans <louiswol94@gmail.com>
2025-02-10 09:49:31 +0200

    WIP

--- includes/template-library/manager.php
+++ includes/template-library/manager.php
@@ -495,16 +495,16 @@ class Manager {
 		if ( get_post_type( $post_id ) !== Source_Local::CPT ) {
 			return new \WP_Error( 'template_error', esc_html__( 'Invalid template type or template does not exist.', 'elementor' ) );
 		}
-	
+
 		$post_status = get_post_status( $post_id );
 		if ( 'private' === $post_status && ! current_user_can( 'read_private_posts', $post_id ) ) {
 			return new \WP_Error( 'template_error', esc_html__( 'You do not have permission to access this template.', 'elementor' ) );
 		}
-	
+
 		if ( 'publish' !== $post_status && ! current_user_can( 'edit_post', $post_id ) ) {
 			return new \WP_Error( 'template_error', esc_html__( 'You do not have permission to export this template.', 'elementor' ) );
 		}
-	
+
 		return null;
 	}
 
