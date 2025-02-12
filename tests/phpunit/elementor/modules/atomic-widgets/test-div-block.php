66a7868d42e9734cf38b754f71339ebda7ab290f
baghdasarovelementor <norayrb@elementor.red>
2025-02-13 01:42:31 +0400

    Add content to div block test

--- tests/phpunit/elementor/modules/atomic-widgets/test-div-block.php
+++ tests/phpunit/elementor/modules/atomic-widgets/test-div-block.php
@@ -6,6 +6,7 @@ use Spatie\Snapshots\MatchesSnapshots;
 
 class Test_Div_Block extends Elementor_Test_Base {
 	use MatchesSnapshots;
+
 	const MOCK = [
 		'id' => 'e8e55a1',
 		'elType' => 'div-block',
@@ -25,6 +26,13 @@ class Test_Div_Block extends Elementor_Test_Base {
 		'widgetType' => 'div-block',
 	];
 
+	const MOCK_CHILD = [
+		'id' => 'e8e55a1',
+		'elType' => 'widget',
+		'settings' => [],
+		'widgetType' => 'a-paragraph',
+	];
+
 	protected $instance;
 
 	public function setUp(): void {
@@ -34,6 +42,9 @@ class Test_Div_Block extends Elementor_Test_Base {
 	}
 
 	public function test__render_div_block(): void {
+		// Arrange.
+		$this->instance->add_child(self::MOCK_CHILD);
+
 		// Act.
 		ob_start();
 		$this->instance->print_element();
@@ -46,6 +57,7 @@ class Test_Div_Block extends Elementor_Test_Base {
 	public function test__render_div_block_with_link_control(): void {
 		// Arrange.
 		$widget_instance = Plugin::$instance->elements_manager->create_element_instance( self::MOCK_LINK );
+		$widget_instance->add_child(self::MOCK_CHILD);
 
 		// Act.
 		ob_start();
