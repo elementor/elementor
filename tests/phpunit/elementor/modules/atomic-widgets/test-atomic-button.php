5140106793908fe8ce188f70f461243b86442e34
elchugreeva <elenac@elementor.com>
2025-02-12 16:02:30 +0200

    Internal: Button Atom  Text input [ED-18051]

---
+++ tests/phpunit/elementor/modules/atomic-widgets/test-atomic-button.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Elementor\Modules\AtomicWidgets;
+
+use Elementor\Plugin;
+use ElementorEditorTesting\Elementor_Test_Base;
+use Spatie\Snapshots\MatchesSnapshots;
+
+class Test_Atomic_Button extends Elementor_Test_Base {
+	use MatchesSnapshots;
+	const MOCK = [
+		'id' => 'e8e55a1',
+		'elType' => 'widget',
+		'settings' => [],
+		'widgetType' => 'a-button',
+	];
+
+	protected $instance;
+
+	public function setUp(): void {
+		parent::setUp();
+
+		$this->instance = Plugin::$instance->elements_manager->create_element_instance( self::MOCK );
+	}
+
+	public function test__render_button(): void {
+		// Act.
+		ob_start();
+		$this->instance->render_content();
+		$rendered_output = ob_get_clean();
+
+		// Assert.
+		$this->assertMatchesSnapshot( $rendered_output );
+	}
+}
