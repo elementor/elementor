2cd40246239deefe727d68905ae267a17c8ec777
svitlanadykun <svitlanad@elementor.red>
2025-02-12 13:41:08 +0100

    skip Menu Items Promotions - screenshot

--- tests/playwright/sanity/modules/promotions/promotions.test.ts
+++ tests/playwright/sanity/modules/promotions/promotions.test.ts
@@ -5,7 +5,7 @@ import PromotionsHelper from '../../../pages/promotions/helper';
 import EditorSelectors from '../../../selectors/editor-selectors';
 
 test.describe( 'Promotion tests @promotions', () => {
-	test( 'Menu Items Promotions - screenshots', async ( { page, apiRequests }, testInfo ) => {
+	test.skip( 'Menu Items Promotions - screenshots', async ( { page, apiRequests }, testInfo ) => {
 		const wpAdmin = new WpAdminPage( page, testInfo, apiRequests ),
 			promotionContainer = '.e-feature-promotion';
 
