91e5def0b75b938f8d9fea9959b2b2eb21960b2e
Ronen Boxer <145427686+ronenelementor@users.noreply.github.com>
2025-02-13 15:38:10 +0200

    Internal: Implement new post query REST API (#30066)
    Co-authored-by: David Seveloff <davids@elementor.com>

--- modules/atomic-widgets/elements/atomic-heading/atomic-heading.php
+++ modules/atomic-widgets/elements/atomic-heading/atomic-heading.php
@@ -7,7 +7,6 @@ use Elementor\Modules\AtomicWidgets\Controls\Types\Select_Control;
 use Elementor\Modules\AtomicWidgets\Controls\Types\Textarea_Control;
 use Elementor\Modules\AtomicWidgets\Elements\Atomic_Widget_Base;
 use Elementor\Modules\AtomicWidgets\Elements\Has_Template;
-use Elementor\Modules\AtomicWidgets\Link_Query;
 use Elementor\Modules\AtomicWidgets\PropTypes\Classes_Prop_Type;
 use Elementor\Modules\AtomicWidgets\PropTypes\Color_Prop_Type;
 use Elementor\Modules\AtomicWidgets\PropTypes\Link_Prop_Type;
@@ -15,13 +14,13 @@ use Elementor\Modules\AtomicWidgets\PropTypes\Primitives\String_Prop_Type;
 use Elementor\Modules\AtomicWidgets\PropTypes\Size_Prop_Type;
 use Elementor\Modules\AtomicWidgets\Styles\Style_Definition;
 use Elementor\Modules\AtomicWidgets\Styles\Style_Variant;
+use Elementor\Modules\WpRest\Classes\WP_Post;
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 
 class Atomic_Heading extends Atomic_Widget_Base {
-	use Link_Query;
 	use Has_Template;
 
 	public static function get_element_type(): string {
@@ -79,8 +78,6 @@ class Atomic_Heading extends Atomic_Widget_Base {
 							],
 						]),
 					Link_Control::bind_to( 'link' )
-						->set_options( $this->get_post_query() )
-						->set_allow_custom_values( true )
 						->set_placeholder( __( 'Paste URL or type', 'elementor' ) ),
 				] ),
 		];
