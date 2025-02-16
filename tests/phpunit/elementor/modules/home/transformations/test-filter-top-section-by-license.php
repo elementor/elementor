407d3099f5ae0a098b910e10c71f757919eb0de3
Ronen Boxer <ronenb@elementor.com>
2025-02-16 14:37:31 +0200

    test fix wip

--- tests/phpunit/elementor/modules/home/transformations/test-filter-top-section-by-license.php
+++ tests/phpunit/elementor/modules/home/transformations/test-filter-top-section-by-license.php
@@ -6,11 +6,23 @@ use Elementor\Modules\Home\Transformations\Filter_Top_Section_By_License;
 use PHPUnit\Framework\TestCase as PHPUnit_TestCase;
 
 class Test_Filter_Top_Section_By_License extends PHPUnit_TestCase {
-	public function test_transform__core_plugin() {
-		// Arrange
+	public function setUp(): void {
+		parent::setUp();
+
 		add_filter( 'elementor/admin/homescreen_promotion_tier', function() {
 			return 'free';
 		} );
+	}
+	public function tearDown(): void {
+		add_filter( 'elementor/admin/homescreen_promotion_tier', function() {
+			return 'free';
+		} );
+
+		parent::tearDown();
+	}
+
+	public function test_transform__core_plugin() {
+		// Arrange
 		$original_data = $this->mock_top_section_data();
 
 		$transformation = new Filter_Top_Section_By_License();
@@ -31,6 +43,9 @@ class Test_Filter_Top_Section_By_License extends PHPUnit_TestCase {
 		$transformation->has_pro = true;
 
 		// Act
+		add_filter( 'elementor/admin/homescreen_promotion_tier', function() {
+			return 'pro';
+		} );
 		$transformed_data = $transformation->transform( $original_data );
 		$expected_data = $this->mock_top_section_data_transformed_pro();
 
@@ -54,11 +69,6 @@ class Test_Filter_Top_Section_By_License extends PHPUnit_TestCase {
 
 		// Assert
 		$this->assertEquals( $expected_data, $transformed_data );
-
-		// Tear down
-		add_filter( 'elementor/admin/homescreen_promotion_tier', function() {
-			return 'free';
-		} );
 	}
 
 	private function mock_top_section_data() {
