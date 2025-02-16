16a62bd733a5b101c43beb38eea735d0a59238de
mugur <mugurell@elementor.red>
2025-02-16 15:21:37 +0200

    phpunit

--- tests/phpunit/elementor/modules/atomic-widgets/test-atomic-svg.php
+++ tests/phpunit/elementor/modules/atomic-widgets/test-atomic-svg.php
@@ -53,6 +53,17 @@ class Test_Atomic_Svg extends Elementor_Test_Base {
 			}
 			return $preempt;
 		}, 10, 3);
+
+		add_filter('file_get_contents', function($content, $file) {
+			if (strpos($file, 'test.svg') !== false) {
+				return '<div style="margin-inline: auto"><svg width="100" height="100" xmlns="http://www.w3.org/2000/svg">
+                    <g transform="translate(50, 50) scale(1.2)" fill="#3F444A">
+                        <path d="M30.56 22.052c1.672 0 3.197.41 4.576 1.232a9.14 9.14 0 0 1 3.344 3.344c.821 1.408 1.232 2.948 1.232 4.62 0 1.672-.41 3.212-1.232 4.62a9.248 9.248 0 0 1-3.344 3.3c-1.379.821-2.904 1.232-4.576 1.232-1.672 0-3.212-.41-4.62-1.232a9.248 9.248 0 0 1-3.344-3.3c-.821-1.408-1.232-2.948-1.232-4.62 0-1.672.41-3.212 1.232-4.62a9.14 9.14 0 0 1 3.344-3.344c1.408-.821 2.948-1.232 4.62-1.232Z" transform="translate(-20, -20)"/>
+                    </g>
+                </svg></div>';
+			}
+			return $content;
+		}, 10, 2);
 	}
 
 	public function tearDown(): void {
