f988b2d3d8a61e66fe833e70d6b18aaf6b2dba83
mugur <mugurell@elementor.red>
2025-02-17 11:33:34 +0200

    fix review comments

--- tests/phpunit/elementor/modules/atomic-widgets/test-atomic-svg.php
+++ tests/phpunit/elementor/modules/atomic-widgets/test-atomic-svg.php
@@ -31,28 +31,28 @@ class Test_Atomic_Svg extends Elementor_Test_Base {
 
 	protected $instance;
 
-	public function setUp(): void {
+	public function setUp() : void {
 		parent::setUp();
 
-		add_filter('wp_get_attachment_image_src', function($image, $attachment_id, $size) {
-			if ($attachment_id === 123) {
-				return [ELEMENTOR_ASSETS_URL . 'images/test.svg'];
+		add_filter( 'wp_get_attachment_image_src', function( $image, $attachment_id, $size ) {
+			if ( $attachment_id === 123 ) {
+				return [ ELEMENTOR_ASSETS_URL . 'images/test.svg' ];
 			}
 			return $image;
-		}, 10, 3);
+		}, 10, 3 );
 
-		add_filter('pre_http_request', function($preempt, $args, $url) {
-			if ($url === ELEMENTOR_ASSETS_URL . 'images/test.svg' || $url === ELEMENTOR_ASSETS_URL . 'images/a-default-svg.svg') {
+		add_filter( 'pre_http_request', function( $preempt, $args, $url ) {
+			if ( $url === ELEMENTOR_ASSETS_URL . 'images/test.svg' || $url === ELEMENTOR_ASSETS_URL . 'images/a-default-svg.svg' ) {
 				return [
 					'body' => '<svg width="100" height="100" xmlns="http://www.w3.org/2000/svg"><path d="M0 0h100v100H0z"/></svg>',
 				];
 			}
 			return $preempt;
-		}, 10, 3);
+		}, 10, 3 );
 
-		add_filter('pre_filesystem_method', function() {
+		add_filter( 'pre_filesystem_method', function() {
 			return 'direct';
-		});
+		} );
 
 		WP_Filesystem();
 		global $wp_filesystem;
@@ -62,8 +62,8 @@ class Test_Atomic_Svg extends Elementor_Test_Base {
 		);
 	}
 
-	public function test__render_svg_from_id(): void {
-		$this->instance = Plugin::$instance->elements_manager->create_element_instance(self::MOCK_ID);
+	public function test__render_svg_from_id() : void {
+		$this->instance = Plugin::$instance->elements_manager->create_element_instance( self::MOCK_ID );
 
 		// Act
 		ob_start();
@@ -71,11 +71,11 @@ class Test_Atomic_Svg extends Elementor_Test_Base {
 		$rendered_output = ob_get_clean();
 
 		// Assert
-		$this->assertMatchesSnapshot($rendered_output);
+		$this->assertMatchesSnapshot( $rendered_output );
 	}
 
-	public function test__render_svg_from_url(): void {
-		$this->instance = Plugin::$instance->elements_manager->create_element_instance(self::MOCK_URL);
+	public function test__render_svg_from_url() : void {
+		$this->instance = Plugin::$instance->elements_manager->create_element_instance( self::MOCK_URL );
 
 		// Act
 		ob_start();
@@ -83,6 +83,6 @@ class Test_Atomic_Svg extends Elementor_Test_Base {
 		$rendered_output = ob_get_clean();
 
 		// Assert
-		$this->assertMatchesSnapshot($rendered_output);
+		$this->assertMatchesSnapshot( $rendered_output );
 	}
 }
