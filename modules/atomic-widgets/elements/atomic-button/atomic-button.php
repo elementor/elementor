e7ddc764e211430358e7f55de339c94e1f542dd0
Hein van Vlastuin <heinvanvlastuin@gmail.com>
2025-02-18 12:38:45 +0200

    Add button link control.

--- modules/atomic-widgets/elements/atomic-button/atomic-button.php
+++ modules/atomic-widgets/elements/atomic-button/atomic-button.php
@@ -5,15 +5,18 @@ namespace Elementor\Modules\AtomicWidgets\Elements\Atomic_Button;
 use Elementor\Modules\AtomicWidgets\Controls\Types\Textarea_Control;
 use Elementor\Modules\AtomicWidgets\Elements\Atomic_Widget_Base;
 use Elementor\Modules\AtomicWidgets\Controls\Section;
+use Elementor\Modules\AtomicWidgets\Controls\Types\Link_Control;
 use Elementor\Modules\AtomicWidgets\Elements\Has_Template;
 use Elementor\Modules\AtomicWidgets\PropTypes\Background_Prop_Type;
 use Elementor\Modules\AtomicWidgets\PropTypes\Classes_Prop_Type;
 use Elementor\Modules\AtomicWidgets\PropTypes\Color_Prop_Type;
+use Elementor\Modules\AtomicWidgets\PropTypes\Link_Prop_Type;
 use Elementor\Modules\AtomicWidgets\PropTypes\Dimensions_Prop_Type;
 use Elementor\Modules\AtomicWidgets\PropTypes\Primitives\String_Prop_Type;
 use Elementor\Modules\AtomicWidgets\PropTypes\Size_Prop_Type;
 use Elementor\Modules\AtomicWidgets\Styles\Style_Definition;
 use Elementor\Modules\AtomicWidgets\Styles\Style_Variant;
+use Elementor\Modules\WpRest\Classes\WP_Post;
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
@@ -41,6 +44,8 @@ class Atomic_Button extends Atomic_Widget_Base {
 
 			'text' => String_Prop_Type::make()
 				->default( __( 'Click here', 'elementor' ) ),
+
+			'link' => Link_Prop_Type::make(),
 		];
 	}
 
@@ -52,6 +57,18 @@ class Atomic_Button extends Atomic_Widget_Base {
 					Textarea_Control::bind_to( 'text' )
 						->set_label( __( 'Button text', 'elementor' ) )
 						->set_placeholder( __( 'Type your button text here', 'elementor' ) ),
+
+					Link_Control::bind_to( 'link' )
+					            ->set_endpoint( WP_Post::ENDPOINT )
+					            ->set_request_params( [
+						            WP_Post::KEYS_FORMAT_MAP_KEY => wp_json_encode( [
+							            'ID' => 'id',
+							            'post_title' => 'label',
+							            'post_type' => 'groupLabel',
+						            ] ),
+					            ] )
+					            ->set_allow_custom_values( true )
+					            ->set_placeholder( __( 'Paste URL or type', 'elementor' ) ),
 				] ),
 		];
 	}
