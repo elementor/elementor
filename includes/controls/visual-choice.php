f4a08e68a3a6eb9635c2c3c88137d3fbc50827e2
Nurit <nurit@elementor.com>
2025-02-25 12:16:37 +0200

    naming and inheritance change

--- includes/controls/visual-choice.php
+++ includes/controls/visual-choice.php
@@ -5,8 +5,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 
-use Elementor\Control_Choose;
-
 /**
  * Elementor Visual Choice control.
  *
@@ -14,7 +12,7 @@ use Elementor\Control_Choose;
  *
  * @since 1.0.0
  */
-class Control_Visual_Choice extends Control_Choose {
+class Control_Visual_Choice extends Base_Data_Control {
 
 	public function get_type() {
 		return 'visual_choice';
@@ -26,15 +24,13 @@ class Control_Visual_Choice extends Control_Choose {
 		<div class="elementor-control-field">
 			<label class="elementor-control-title">{{{ data.label }}}</label>
 			<div class="elementor-control-input-wrapper">
-				<div
-						class="elementor-choices elementor-choices-visual-choice"
-						style="--elementor-choices-columns: {{ data.columns }};">
+				<div class="elementor-visual-choice-choices" style="--elementor-visual-choice-columns: {{ data.columns }};">
 
 					<# _.each( data.options, function( options, value ) { #>
-					<div class="elementor-choices-element">
-						<input id="<?php $this->print_control_uid( $control_uid_input_type ); ?>" type="radio" name="elementor-choose-{{ data.name }}-{{ data._cid }}" value="{{ value }}">
-						<label class="elementor-choices-label elementor-control-unit-2 tooltip-target" for="<?php $this->print_control_uid( $control_uid_input_type ); ?>" data-tooltip="{{ options.title }}" title="{{ options.title }}">
-							<img class="elementor-choices-image" src="{{ options.image }}" aria-hidden="true" alt="{{ options.title }}" data-hover="{{ value }}" />
+					<div class="elementor-visual-choice-element">
+						<input id="<?php $this->print_control_uid( $control_uid_input_type ); ?>" type="radio" name="elementor-visual-choice-{{ data.name }}-{{ data._cid }}" value="{{ value }}">
+						<label class="elementor-visual-choice-label tooltip-target" for="<?php $this->print_control_uid( $control_uid_input_type ); ?>" data-tooltip="{{ options.title }}" title="{{ options.title }}">
+							<img class="elementor-visual-choice-image" src="{{ options.image }}" aria-hidden="true" alt="{{ options.title }}" data-hover="{{ value }}" />
 							<span class="elementor-screen-only">{{{ options.title }}}</span>
 						</label>
 					</div>
