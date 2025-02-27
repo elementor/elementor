df158e11389f85ebe95d2884532e747f55b04df1
Louis Wolmarans <louiswol94@gmail.com>
2025-02-27 20:39:48 +0200

    WIP

--- modules/cloud-library/connect/cloud-library.php
+++ modules/cloud-library/connect/cloud-library.php
@@ -14,8 +14,8 @@ class Cloud_Library extends Library {
 		return esc_html__( 'Cloud Library', 'elementor' );
 	}
 
-	protected function get_api_url(): string {
-		return 'https://cloud-library.prod.builder.elementor.red/api/v1/cloud-library';
+	protected function get_slug(): string {
+		return 'cloud-library';
 	}
 
 	public function get_resources( $args = [] ): array {
