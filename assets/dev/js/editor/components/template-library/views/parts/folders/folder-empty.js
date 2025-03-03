42e795a3e0cca602a57d344f4e160a987f2be66b
Louis Wolmarans <louiswol94@gmail.com>
2025-03-03 15:45:27 +0200

    WIP

--- assets/dev/js/editor/components/template-library/views/parts/folders/folder-empty.js
+++ assets/dev/js/editor/components/template-library/views/parts/folders/folder-empty.js
@@ -1,5 +1,10 @@
 module.exports = Marionette.ItemView.extend( {
     tagName: 'li',
     className: 'no-results',
-    template: _.template('<p>Folders you create will appear here.<br>To create a new one, go to Cloud library.</p>'),
+    template: _.template( sprintf(
+        /* Translators: 1: Empty message, 2: CTA. */
+        '<p>%s<br>%s</p>',
+        __( 'Folders you create will appear here.', 'elementor' ),
+        __( 'To create a new one, go to Cloud library.', 'elementor' ),
+    ) ),
 } );
