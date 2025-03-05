af69da0d12ad42f63c4d9789cfae488d6dbbfbb7
StyleShit <32631382+StyleShit@users.noreply.github.com>
2025-03-05 19:18:05 +0200

    wip

--- karma.conf.js
+++ karma.conf.js
@@ -37,7 +37,7 @@ module.exports = function( config ) {
 
 			// Dev tools.
 			'tests/qunit/setup/dev-tools.js',
-			'assets/js/dev-tools.js',
+			'assets/js/dev-tools.min.js',
 
 			// Elementor Common.
 			'tests/qunit/setup/elementor-common.js',
