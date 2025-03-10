703775a51b8e70cd1cd4fa9f9635c1622bb01517
Nami printz <namipr3@gmail.com>
2025-03-10 18:11:03 +0200

    wip

--- modules/atomic-widgets/styles/style-schema.php
+++ modules/atomic-widgets/styles/style-schema.php
@@ -33,20 +33,18 @@ class Style_Schema {
 		);
 	}
 
+	public static function get_extended_size_prop() {
+		return Union_Prop_Type::make()
+			->add_prop_type( Size_Prop_Type::make() )
+			->add_prop_type( String_Prop_Type::make()->enum( [ 'auto' ] ) );
+	}
+
 	private static function get_size_props() {
 		return [
-			'width' => Union_Prop_Type::make()
-				->add_prop_type( Size_Prop_Type::make() )
-				->add_prop_type( String_Prop_Type::make()->enum( [ 'auto' ] ) ),
-			'height' => Union_Prop_Type::make()
-				->add_prop_type( Size_Prop_Type::make() )
-				->add_prop_type( String_Prop_Type::make()->enum( [ 'auto' ] ) ),
-			'min-width' => Union_Prop_Type::make()
-				->add_prop_type( Size_Prop_Type::make() )
-				->add_prop_type( String_Prop_Type::make()->enum( [ 'auto' ] ) ),
-			'min-height' => Union_Prop_Type::make()
-				->add_prop_type( Size_Prop_Type::make() )
-				->add_prop_type( String_Prop_Type::make()->enum( [ 'auto' ] ) ),
+			'width' => self::get_extended_size_prop(),
+			'height' => self::get_extended_size_prop(),
+			'min-width' => self::get_extended_size_prop(),
+			'min-height' => self::get_extended_size_prop(),
 			'max-width' => Size_Prop_Type::make(),
 			'max-height' => Size_Prop_Type::make(),
 			'overflow' => String_Prop_Type::make()->enum([
@@ -66,18 +64,10 @@ class Style_Schema {
 				'fixed',
 				'sticky',
 			]),
-			'top' => Union_Prop_Type::make()
-				->add_prop_type( Size_Prop_Type::make() )
-				->add_prop_type( String_Prop_Type::make()->enum( [ 'auto' ] ) ),
-			'right' => Union_Prop_Type::make()
-				->add_prop_type( Size_Prop_Type::make() )
-				->add_prop_type( String_Prop_Type::make()->enum( [ 'auto' ] ) ),
-			'bottom' => Union_Prop_Type::make()
-				->add_prop_type( Size_Prop_Type::make() )
-				->add_prop_type( String_Prop_Type::make()->enum( [ 'auto' ] ) ),
-			'left' => Union_Prop_Type::make()
-				->add_prop_type( Size_Prop_Type::make() )
-				->add_prop_type( String_Prop_Type::make()->enum( [ 'auto' ] ) ),
+			'top' => self::get_extended_size_prop(),
+			'right' => self::get_extended_size_prop(),
+			'bottom' => self::get_extended_size_prop(),
+			'left' => self::get_extended_size_prop(),
 			'z-index' => Number_Prop_Type::make(),
 		];
 	}
@@ -210,9 +200,7 @@ class Style_Schema {
 			]),
 			'flex-grow' => Number_Prop_Type::make(),
 			'flex-shrink' => Number_Prop_Type::make(),
-			'flex-basis' => Union_Prop_Type::make()
-				->add_prop_type( Size_Prop_Type::make() )
-				->add_prop_type( String_Prop_Type::make()->enum( [ 'auto' ] ) ),
+			'flex-basis' => self::get_extended_size_prop(),
 		];
 	}
 
