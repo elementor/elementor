6b14cac8c61c8c5a568fbce65867e227cf2d4728
max-zu <zubovmaksim@gmail.com>
2025-03-11 15:44:06 +0100

    fixed linter, fixed tests

--- assets/dev/js/editor/components/template-library/manager.js
+++ assets/dev/js/editor/components/template-library/manager.js
@@ -647,12 +647,6 @@ const TemplateLibraryManager = function() {
 
 		const parentId = this.getFilter( 'parent' );
 
-		console.log('!_MORE_!', {
-			source,
-			offset: templatesCollection.length,
-			search,
-			parentId,
-		})
 		const ajaxOptions = {
 			data: {
 				source,
@@ -687,8 +681,6 @@ const TemplateLibraryManager = function() {
 
 		self.loadTemplates( function() {
 			var templatesToShow = self.filterTemplates();
-
-			console.log("!_TEMPLATES_!", templatesToShow);
 
 			self.layout.showTemplatesView( new TemplateLibraryCollection( templatesToShow ) );
 		} );
