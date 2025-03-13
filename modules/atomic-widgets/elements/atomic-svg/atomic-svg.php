def7eedd251d6e847d18f99fdf2fd16c7626e808
TzviRabinovitch <99259629+TzviRabinovitch@users.noreply.github.com>
2025-03-13 10:19:47 +0200

    Internal: Update content in Panel [ED-18421]  (#30490)

--- modules/atomic-widgets/elements/atomic-svg/atomic-svg.php
+++ modules/atomic-widgets/elements/atomic-svg/atomic-svg.php
@@ -28,7 +28,7 @@ class Atomic_Svg extends Atomic_Widget_Base {
 	}
 
 	public function get_title() {
-		return esc_html__( 'Atomic SVG', 'elementor' );
+		return esc_html__( 'SVG', 'elementor' );
 	}
 
 	public function get_icon() {
@@ -49,8 +49,7 @@ class Atomic_Svg extends Atomic_Widget_Base {
 				->set_label( esc_html__( 'Content', 'elementor' ) )
 				->set_items( [
 					Svg_Control::bind_to( 'svg' ),
-					Link_Control::bind_to( 'link' )
-							->set_placeholder( __( 'Paste URL or type', 'elementor' ) ),
+					Link_Control::bind_to( 'link' ),
 				] ),
 		];
 	}
