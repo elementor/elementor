5bd4420f05b6c75d0bdcf9a78827485b46d1ffc9
Maksim Zubov <zubovmaksim@gmail.com>
2025-03-14 12:38:51 +0100

    Update includes/template-library/manager.php
    Co-authored-by: Louis Wolmarans <louiswol94@gmail.com>

--- includes/template-library/manager.php
+++ includes/template-library/manager.php
@@ -898,7 +898,7 @@ class Manager {
 			'load_more_templates',
 			'create_folder',
 			'get_folders',
-			'screenshot_save_template',
+			'save_template_screenshot',
 		];
 
 		foreach ( $library_ajax_requests as $ajax_request ) {
