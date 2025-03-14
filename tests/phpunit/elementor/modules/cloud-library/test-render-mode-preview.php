d724f7c09bf4c97b15683e5cd5a251815796971d
max-zu <zubovmaksim@gmail.com>
2025-03-14 12:25:04 +0100

    ED-18369 cs

--- tests/phpunit/elementor/modules/cloud-library/test-render-mode-preview.php
+++ tests/phpunit/elementor/modules/cloud-library/test-render-mode-preview.php
@@ -7,15 +7,16 @@ use Elementor\Plugin;
 use ElementorEditorTesting\Elementor_Test_Base;
 
 class Test_Render_Mode_Preview extends Elementor_Test_Base {
-	/**
-	 * @var \PHPUnit\Framework\MockObject\MockObject
-	 */
-	private $document_mock;
+    /**
+     * @var \PHPUnit\Framework\MockObject\MockObject
+     */
+    private $document_mock;
 
-	private $source_cloud_mock;
-	private $templates_manager_mock;
+    private $source_cloud_mock;
 
-	private $templates_manager;
+    private $templates_manager_mock;
+
+    private $templates_manager;
 
     private $documents_mock;
 
@@ -27,29 +28,29 @@ class Test_Render_Mode_Preview extends Elementor_Test_Base {
         $this->templates_manager = Plugin::$instance->templates_manager;
         $this->documents = Plugin::$instance->documents;
 
-		$this->document_mock = $this->getMockBuilder( '\Elementor\Core\Base\Document' )
-			->disableOriginalConstructor()
-			->getMock();
+        $this->document_mock = $this->getMockBuilder( '\Elementor\Core\Base\Document' )
+            ->disableOriginalConstructor()
+            ->getMock();
 
-		$this->document_mock->method( 'get_main_id' )
-			->willReturn( 123 );
+        $this->document_mock->method( 'get_main_id' )
+            ->willReturn( 123 );
 
-		$this->templates_manager_mock = $this->getMockBuilder( '\Elementor\TemplateLibrary\Manager' )
-			->disableOriginalConstructor()
-			->getMock();
+        $this->templates_manager_mock = $this->getMockBuilder( '\Elementor\TemplateLibrary\Manager' )
+            ->disableOriginalConstructor()
+            ->getMock();
 
-		$this->source_cloud_mock = $this->getMockBuilder( '\Elementor\TemplateLibrary\Source_Cloud' )
-			->disableOriginalConstructor()
-			->setMethods( [ 'create_document_for_preview', 'get_resource', 'save_document_for_preview' ] )
-			->getMock();
+        $this->source_cloud_mock = $this->getMockBuilder( '\Elementor\TemplateLibrary\Source_Cloud' )
+            ->disableOriginalConstructor()
+            ->setMethods( [ 'create_document_for_preview', 'get_resource', 'save_document_for_preview' ] )
+            ->getMock();
 
-		$this->source_cloud_mock->method( 'create_document_for_preview' )
-			->with( 123 )
-			->willReturn( $this->document_mock );
+        $this->source_cloud_mock->method( 'create_document_for_preview' )
+            ->with( 123 )
+            ->willReturn( $this->document_mock );
 
-		$this->templates_manager_mock->method( 'get_source' )
-			->with( 'cloud' )
-			->willReturn( $this->source_cloud_mock );
+        $this->templates_manager_mock->method( 'get_source' )
+            ->with( 'cloud' )
+            ->willReturn( $this->source_cloud_mock );
 
         $this->documents_mock = $this->getMockBuilder( '\Elementor\Core\Documents_Manager' )
             ->disableOriginalConstructor()
@@ -70,10 +71,10 @@ class Test_Render_Mode_Preview extends Elementor_Test_Base {
 
     public function test_prepare_render() {
         // Arrange
-		add_filter( 'show_admin_bar', function( $show ) {
-			$this->assertFalse( $show );
-			return $show;
-		});
+        add_filter( 'show_admin_bar', function( $show ) {
+            $this->assertFalse( $show );
+            return $show;
+        });
 
         $render_mode = new Render_Mode_Preview( 123 );
 
@@ -81,40 +82,40 @@ class Test_Render_Mode_Preview extends Elementor_Test_Base {
         $render_mode->prepare_render();
 
         // Assert
-		$this->assertEquals( 10, has_filter('template_include', [ $render_mode, 'filter_template' ] ) );
+        $this->assertEquals( 10, has_filter('template_include', [ $render_mode, 'filter_template' ] ) );
 	}
 
 	public function test_constructor() {
-		// Arrange & Act
-		$render_mode = new Render_Mode_Preview( 123 );
+        // Arrange & Act
+        $render_mode = new Render_Mode_Preview( 123 );
 
-		// Assert
-		$this->assertEquals( 123, $render_mode->get_document()->get_main_id() );
+        // Assert
+        $this->assertEquals( 123, $render_mode->get_document()->get_main_id() );
 	}
 
 	public function test_create_document() {
-		// Arrange
-		$template_data = [
-			'content' => [ 'test_content' ],
-			'page_settings' => [ 'test_settings' ]
-		];
+        // Arrange
+        $template_data = [
+            'content' => [ 'test_content' ],
+            'page_settings' => [ 'test_settings' ]
+        ];
 
-		$this->source_cloud_mock->method( 'get_resource' )
-			->with( [ 'id' => 123 ] )
-			->willReturn( [
-				'content' => json_encode( $template_data )
-			] );
+        $this->source_cloud_mock->method( 'get_resource' )
+            ->with( [ 'id' => 123 ] )
+            ->willReturn( [
+                'content' => json_encode( $template_data )
+            ] );
 
-		$render_mode = new Render_Mode_Preview( 123 );
+        $render_mode = new Render_Mode_Preview( 123 );
 
-		$reflection = new \ReflectionClass( $render_mode );
-		$method = $reflection->getMethod( 'create_document' );
-		$method->setAccessible( true );
+        $reflection = new \ReflectionClass( $render_mode );
+        $method = $reflection->getMethod( 'create_document' );
+        $method->setAccessible( true );
 
-		// Act
-		$method->invoke( $render_mode );
+        // Act
+        $method->invoke( $render_mode );
 
-		// Assert
-		$this->assertInstanceOf( get_class( $this->document_mock ), $render_mode->get_document() );
+        // Assert
+        $this->assertInstanceOf( get_class( $this->document_mock ), $render_mode->get_document() );
 	}
 }
