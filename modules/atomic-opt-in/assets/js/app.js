1f3fec2ee35f1f5e58797f3c4e29af05b3660855
Mike Kokhanov <mikek@elementor.com>
2025-03-17 18:25:05 +0200

    Internal: Create a V4 section under Elementor Settings in WP dashboard [ED-18403]

---
+++ modules/atomic-opt-in/assets/js/app.js
@@ -0,0 +1,111 @@
+import React from 'react';
+import ReactUtils from 'elementor-utils/react';
+import {
+	DirectionProvider,
+	LocalizationProvider,
+	ThemeProvider,
+	Container,
+	Button,
+	Box,
+	Stack,
+	Typography,
+	Image,
+	Chip,
+	Link,
+	styled,
+} from '@elementor/ui';
+import { AlertTriangleFilledIcon } from '@elementor/icons';
+import { __ } from '@wordpress/i18n';
+
+const OptIn = () => {
+	const i18n = {
+		title: __( 'The road to Editor V4', 'elementor' ),
+		chip: __( 'Alpha', 'elementor' ),
+		welcomeText: __( 'Welcome to Elementor v4, an evolution in web creation. Experience a more streamlined, flexible, and powerful editor with a fresh approach to structure and styling.', 'elementor' ),
+		advantages: [
+			__( 'A new way to build with atomic components', 'elementor' ),
+			__( 'A unified styling system for consistency', 'elementor' ),
+			__( 'More flexibility with local and global styles', 'elementor' ),
+			__( 'Enhanced class management for scalable designs', 'elementor' ),
+		],
+		warning: __( 'V4 is still in alpha and should not be used on live sites yet', 'elementor' ),
+		andMore: __( 'and much more', 'elementor' ),
+		readMore: __( 'Read here', 'elementor' ),
+		optInButton: __( 'Opt-in to V4', 'elementor' ),
+		optOutButton: __( 'Opt-out from V4', 'elementor' ),
+		image: __( 'Editor V4', 'elementor' ),
+	};
+
+	const readMoreUrl = 'https://go.elementor.com/wp-dash-opt-in-v4-help-center/';
+
+	return (
+		<React.Fragment>
+			<Container maxWidth = "lg">
+				<Stack direction="column-reverse">
+					<Container maxWidth="md">
+						<Stack direction="column" gap={ 2.5 }>
+							<Box display="flex" alignItems="center" gap={ 1 }>
+								<Typography variant="h4">{ i18n.title }</Typography>
+								<Chip size="small" color="secondary" variant="filled" label={ i18n.chip } />
+							</Box>
+							<Typography variant="body1">{ i18n.welcomeText }</Typography>
+							<Box component="ul">
+								{ i18n.advantages.map(
+									( desc, i ) => <BoxItem component="li" key={ i }>{ desc }</BoxItem>
+								) }
+								<BoxItem component="li" key="e-0">
+									{ i18n.andMore } <Link href={ readMoreUrl } target="_blank">{ i18n.readMore }</Link>
+								</BoxItem>
+							</Box>
+							<Stack direction="row" gap={ 1 }>
+								<Typography><AlertTriangleFilledIcon /> { i18n.warning }</Typography>
+							</Stack>
+							<Stack direction="column" maxWidth="xs" gap={ 1 }>
+								<Button size="large" color="primary" variant="contained" sx={ { flexGrow: 1 } }>{ i18n.optInButton }</Button>
+								<Button size="large" color="secondary" variant="outlined" sx={ { flexGrow: 1 } }>{ i18n.optOutButton }</Button>
+							</Stack>
+						</Stack>
+					</Container>
+					<Container maxWidth="md">
+						<Image alt={ i18n.image } />
+					</Container>
+				</Stack>
+			</Container>
+		</React.Fragment>
+	);
+};
+
+const BoxItem = styled( Typography )( ( { theme } ) => ( {
+	listStyle: 'disc',
+	marginInlineStart: theme.spacing( 4 ),
+} ) );
+
+const App = ( props ) => {
+	return (
+		<DirectionProvider rtl = { props.isRTL }>
+			<LocalizationProvider>
+				<ThemeProvider colorScheme = { 'light' }>
+					<OptIn />
+				</ThemeProvider>
+			</LocalizationProvider>
+		</DirectionProvider>
+	);
+};
+
+const init = () => {
+	const rootElement = document.querySelector( '#page-editor-v4-opt-in' );
+
+	console.log( 'immediate run' );
+
+	if ( ! rootElement ) {
+		return;
+	}
+
+	ReactUtils.render( (
+		<App
+			isRTL = { ! ! elementorCommon.config.isRTL }
+		/>
+	), rootElement );
+};
+
+init();
---
+++ modules/atomic-opt-in/module.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace Elementor\Modules\AtomicOptIn;
+
+use Elementor\Core\Base\Module as BaseModule;
+use Elementor\Core\Experiments\Manager as Experiments_Manager;
+
+use Elementor\Plugin;
+use Elementor\Settings;
+use Elementor\Utils;
+
+class Module extends BaseModule {
+	const MODULE_NAME = 'editor-v4-opt-in';
+	const EXPERIMENT_NAME = 'editor_v4';
+
+	public static function get_experimental_data(): array {
+		return [
+			'name' => self::EXPERIMENT_NAME,
+			'title' => esc_html__( 'Editor V4', 'elementor' ),
+			'description' => esc_html__( 'Enable Editor V4.', 'elementor' ),
+			'hidden' => true,
+			'default' => Experiments_Manager::STATE_INACTIVE,
+			'release_status' => Experiments_Manager::RELEASE_STATUS_ALPHA,
+		];
+	}
+
+	private function is_experiment_active() {
+		return Plugin::$instance->experiments->is_feature_active( self::EXPERIMENT_NAME );
+	}
+
+	public function __construct() {
+		if ( ! $this->is_experiment_active() || ! is_admin() ) {
+			return;
+		}
+
+		$this->register_settings_tab();
+		$this->register_scripts();
+	}
+
+	public function get_name() {
+		return self::MODULE_NAME;
+	}
+
+	private function register_settings_tab() {
+		$page_id = Settings::PAGE_ID;
+
+		add_action( "elementor/admin/after_create_settings/{$page_id}", function( Settings $settings ) {
+			$this->add_new_tab_to( $settings );
+		}, 11 );
+	}
+
+	private function add_new_tab_to( Settings $settings ) {
+		$settings->add_tab( self::MODULE_NAME, [
+			'label' => esc_html__( 'Editor V4', 'elementor' ),
+			'sections' => [
+				'opt-in' => [
+					'callback' => function() {
+						echo '<div id="page-editor-v4-opt-in"></div>';
+					},
+					'fields' => [],
+				],
+			],
+		] );
+
+		return $this;
+	}
+
+	private function reguster_scripts() {
+		add_action( "elementor_page_elementor-settings", [ $this, 'enqueue_optin_scripts' ] );
+	}
+
+	public function enqueue_optin_scripts() {
+		$min_suffix = Utils::is_script_debug() ? '' : '.min';
+
+		wp_enqueue_script(
+			$this->get_name(),
+			ELEMENTOR_ASSETS_URL . 'js/atomic-opt-in' . $min_suffix . '.js',
+			[
+				'react',
+				'react-dom',
+				'elementor-common',
+				'elementor-v2-ui',
+			],
+			ELEMENTOR_VERSION,
+			true
+		);
+	}
+}
