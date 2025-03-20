2d603ae18920fa0d66a581c4ab19d38c42ebeeca
Louis Wolmarans <louiswol94@gmail.com>
2025-03-20 09:27:51 +0200

    add max's delete code

--- includes/template-library/sources/cloud.php
+++ includes/template-library/sources/cloud.php
@@ -360,4 +360,8 @@ class Source_Cloud extends Source_Base {
 	public function get_bulk_items( array $args = [] ) {
 		return $this->get_app()->get_bulk_resources_with_content( $args );
 	}
+
+	public function bulk_delete_items( array $template_ids ) {
+		return $this->get_app()->bulk_delete_items( $template_ids );
+	}
 }
