e412144fee9ad09d9a54b3954a56f21c1140d72f
elchugreeva <elenac@elementor.com>
2025-03-23 16:10:40 +0200

    test

--- modules/atomic-widgets/assets/js/editor/component.js
+++ modules/atomic-widgets/assets/js/editor/component.js
@@ -1,4 +1,5 @@
 import * as hooks from './hooks';
+import { AppManager } from "../../../../promotions/assets/js/react/app-manager";
 
 export default class Component extends $e.modules.ComponentBase {
 	getNamespace() {
@@ -7,5 +8,14 @@ export default class Component extends $e.modules.ComponentBase {
 
 	defaultHooks() {
 		return this.importHooks( hooks );
+	}
+
+	defaultCommands() {
+		return {
+			alphachip: ( args ) => {
+				const PopupManager = new AppManager();
+				PopupManager.mount( args.event.target, args.selectors );
+			},
+		};
 	}
 }
