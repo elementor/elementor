404054932633d18718e497220aa26e345b9adbdc
Hein van Vlastuin <heinvanvlastuin@gmail.com>
2025-03-25 09:00:52 +0200

    Load v2 mock

---
+++ tests/qunit/setup/elementor-v2.js
@@ -0,0 +1,8 @@
+window.elementorV2 = {
+  ui: {
+    DirectionProvider: function(props) { return props.children || null; },
+    Infotip: function(props) { return props.children || null; },
+    LocalizationProvider: function(props) { return props.children || null; },
+    ThemeProvider: function(props) { return props.children || null; },
+  }
+};
