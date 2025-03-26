8b816b7c73560793c98097d3598311a2916c5c6c
Hein van Vlastuin <heinvanvlastuin@gmail.com>
2025-03-26 19:09:49 +0200

    Url check

--- tests/playwright/sanity/modules/home/home.test.ts
+++ tests/playwright/sanity/modules/home/home.test.ts
@@ -5,6 +5,15 @@ test.describe( 'Home screen tests', () => {
 	test( `'Create a new page' button opens the editor in a new page`, async ( { page } ) => {
 		await page.goto( 'wp-admin/admin.php?page=elementor' );
 
+		await page.waitForURL( ( url ) => {
+			const urlString = url.toString();
+			if ( urlString.includes( 'onboarding' ) ) {
+				page.goto( 'wp-admin/admin.php?page=elementor' );
+				return false;
+			}
+			return true;
+		} );
+
 		const createNewPageButton = page.getByRole( 'link', { name: 'Create a Page' } );
 
 		await expect( createNewPageButton ).toHaveAttribute( 'href', /wp-admin\/edit\.php\?action=elementor_new_post&post_type=page&_wpnonce=.*/ );
