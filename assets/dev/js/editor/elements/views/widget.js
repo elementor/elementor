b0e3a7af3f0fac8bab6890ea0ceba4b5806eb5ad
Hein van Vlastuin <94352322+hein-obox@users.noreply.github.com>
2025-03-27 16:29:15 +0200

    Add optional chaining to widgetType check

--- assets/dev/js/editor/elements/views/widget.js
+++ assets/dev/js/editor/elements/views/widget.js
@@ -38,7 +38,7 @@ const WidgetView = BaseWidget.extend( {
 
 	getContextMenuGroups() {
 		const groups = BaseWidget.prototype.getContextMenuGroups.apply( this, arguments );
-		const isAtomicWidget = this.options.model.get( 'widgetType' ).startsWith( 'e-' );
+		const isAtomicWidget = this.options?.model?.get( 'widgetType' )?.startsWith( 'e-' );
 
 		if ( isAtomicWidget ) {
 			return groups;
