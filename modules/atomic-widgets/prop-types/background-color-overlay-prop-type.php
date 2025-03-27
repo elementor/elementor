a4f94396baa50fb80d3dda47cd9f405d615639ea
Mike Kokhanov <mikek@elementor.com>
2025-03-27 15:57:29 +0200

    [ wip ]: lint

--- modules/atomic-widgets/prop-types/background-color-overlay-prop-type.php
+++ modules/atomic-widgets/prop-types/background-color-overlay-prop-type.php
@@ -4,8 +4,7 @@ namespace Elementor\Modules\AtomicWidgets\PropTypes;
 
 use Elementor\Modules\AtomicWidgets\PropTypes\Base\Object_Prop_Type;
 
-class Background_Color_Overlay_Prop_Type extends Object_Prop_Type
- {
+class Background_Color_Overlay_Prop_Type extends Object_Prop_Type {
 	public static function get_key(): string {
 		return 'background-color-overlay';
 	}
