ccdfd87bbd8da43a9b889d4e7c3fd626dbdc4629
Tzvi Rabinovitch <tzvir@elementor.com>
2025-03-30 09:30:25 +0300

    Using experiment constant

--- modules/library/module.php
+++ modules/library/module.php
@@ -2,6 +2,7 @@
 namespace Elementor\Modules\Library;
 
 use Elementor\Core\Base\Module as BaseModule;
+use Elementor\Modules\AtomicWidgets\Module as AtomicWidgets_Module;
 use Elementor\Plugin;
 
 if ( ! defined( 'ABSPATH' ) ) {
@@ -57,7 +58,7 @@ class Module extends BaseModule {
 				->register_document_type( 'container', Documents\Container::get_class_full_name() );
 		}
 
-		if ( $experiments_manager->is_feature_active( 'atomic_widgets' ) ) {
+		if ( $experiments_manager->is_feature_active( AtomicWidgets_Module::EXPERIMENT_NAME ) ) {
 			Plugin::$instance->documents
 				->register_document_type( 'e-div-block', Documents\Div_Block::get_class_full_name() );
 			Plugin::$instance->documents
