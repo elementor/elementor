6168408639f24413c2fef4c205ecc46b6f4d4f36
asafdl <asafd@elementor.com>
2025-04-01 15:43:31 +0300

    fixed move and paste

--- assets/dev/js/editor/document/hooks/data/document/elements/move/link-in-link-restriction-move.js
+++ assets/dev/js/editor/document/hooks/data/document/elements/move/link-in-link-restriction-move.js
@@ -1,12 +1,24 @@
-import LinkInLinkRestrictionBase from '../base/link-in-link-restriction-base';
+import Dependency from 'elementor-api/modules/hooks/data/dependency';
 
-export class LinkInLinkMove extends LinkInLinkRestrictionBase {
+export class LinkInLinkMove extends Dependency {
 	getCommand() {
-		return 'document/elements/move';
+		return 'document/elements/paste';
 	}
 
 	getId() {
 		return 'link-in-link-restriction-move';
+	}
+
+	apply( args ) {
+		const { containers = [ args.container ], target } = args;
+		const sourceElements = containers;
+		const targetElement = target;
+
+		const hasChildWithAnchor = sourceElements.some( ( src ) =>
+			elementor.helpers.findChildWithAnchor( src.view.el ),
+		);
+
+		return ! ( hasChildWithAnchor && elementor.helpers.findParentWithAnchor( targetElement.view.el ) );
 	}
 }
 
--- assets/dev/js/editor/document/hooks/data/document/elements/paste/link-in-link-restriction-paste.js
+++ assets/dev/js/editor/document/hooks/data/document/elements/paste/link-in-link-restriction-paste.js
@@ -1,12 +1,32 @@
-import LinkInLinkRestrictionBase from '../base/link-in-link-restriction-base';
+import Dependency from 'elementor-api/modules/hooks/data/dependency';
 
-export class LinkInLinkPaste extends LinkInLinkRestrictionBase {
+export class LinkInLinkPaste extends Dependency {
 	getCommand() {
-		return 'document/elements/move';
+		return 'document/elements/paste';
 	}
 
 	getId() {
 		return 'link-in-link-restriction-paste';
+	}
+
+	apply( args ) {
+		const { containers = [ args.container ], storageKey } = args;
+		const targetElements = containers;
+		const data = elementorCommon.storage.get( storageKey );
+		const sourceElements = data.clipboard.elements;
+
+		const hasChildWithAnchor = sourceElements.some( ( src ) =>
+			src.htmlCache.indexOf( '<a' ) !== -1,
+		);
+
+		if ( ! hasChildWithAnchor ) {
+			return true;
+		}
+
+		const hasParentWithAnchor = targetElements.some( ( target ) =>
+			elementor.helpers.findParentWithAnchor( target.view.el ),
+		);
+		return ! hasParentWithAnchor;
 	}
 }
 
