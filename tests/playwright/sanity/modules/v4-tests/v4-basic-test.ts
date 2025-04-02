9a5daed1b6aef216b4595a83068d0d282d6d1fc3
svitlanadykun <svitlanad@elementor.red>
2025-04-02 20:04:33 +0200

    v4 slack channel

---
+++ tests/playwright/sanity/modules/v4-tests/v4-basic-test.ts
@@ -0,0 +1,6 @@
+import { test, expect } from '@playwright/test';
+
+test('Failing test', async ({ page }) => {
+	await page.goto('https://example.com');
+	await expect(page.locator('h1')).toHaveText('This will fail');
+});
