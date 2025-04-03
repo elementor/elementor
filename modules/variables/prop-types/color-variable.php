31715293ed7a21a7d5047a61507fa84aca16562f
Mike Kokhanov <mikek@elementor.com>
2025-04-03 15:00:13 +0300

    [ wip ]: review feedback

--- modules/variables/prop-types/color-variable.php
+++ modules/variables/prop-types/color-variable.php
@@ -2,22 +2,14 @@
 
 namespace Elementor\Modules\Variables\PropTypes;
 
-use Elementor\Modules\AtomicWidgets\PropTypes\Base\Plain_Prop_Type;
+use Elementor\Modules\AtomicWidgets\PropTypes\Primitives\String_Prop_Type;
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 
-class Color_Variable extends Plain_Prop_Type {
+class Color_Variable extends String_Prop_Type {
 	public static function get_key(): string {
 		return 'global-color-variable';
-	}
-
-	protected function validate_value( $value ): bool {
-		return ! empty( $value );
-	}
-
-	protected function sanitize_value( $value ) {
-		return $value;
 	}
 }
