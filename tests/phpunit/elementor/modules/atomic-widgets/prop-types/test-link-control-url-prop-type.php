60fe74521711abfac0c7ea7befaa5f8d92519068
baghdasarovelementor <norayrb@elementor.red>
2025-04-03 10:18:16 +0400

    Update test for sanitize

--- tests/phpunit/elementor/modules/atomic-widgets/prop-types/test-link-control-url-prop-type.php
+++ tests/phpunit/elementor/modules/atomic-widgets/prop-types/test-link-control-url-prop-type.php
@@ -22,27 +22,48 @@ class Test_Link_Control_Url_Prop_Type extends Elementor_Test_Base {
 		$this->assertTrue( $result );
 	}
 
+	/**
+	 *	@dataProvider urls_provider
+	 */
+	public function test_sanitize( $value, $expected )
+	{
+		// Arrange.
+		$prop_type = new Link_Control_Url_Prop_Type();
+
+		// Act.
+		$result = $prop_type->sanitize( ["value" => $value] );
+
+		// Assert.
+		$this->assertEquals( $expected, $result['value'] );
+	}
+
 	public function urls_provider(): array {
 		$xss_injected_url = "example.com/?id=1<script>alert(1)</script>";
 		$sql_injected_url = "' OR 1=1 --";
 		$js_injected_url = "<script>alert('hacked')</script>";
+		$japanese_url = "https://example.com/こんにちは";
+		$japanese_domain = "https://例え.テスト";
 
 		return [
-			[ "#" ],
-			[ "#some-id" ],
-			[ "google.com" ],
-			[ "google" ],
-			[ "google.com/about-us/#section1" ],
-			[ "http://example.com" ],
-			[ "https://example.com" ],
-			[ "192.168.1.1" ],
-			[ "2001:db8::ff00:42:8329" ],
-			[ $xss_injected_url ],
-			[ $sql_injected_url ],
-			[ $js_injected_url ],
-			[ "http://" ],
-			[ "http:///example.com" ],
+			[ "value" => "#", "expected" => "#" ],
+			[ "value" => "#some-id", "expected" => "#some-id" ],
+			[ "value" => "google.com", "expected" => "google.com" ],
+			[ "value" => "google . com", "expected" => "google.com" ],
+			[ "value" => "google", "expected" => "google" ],
+			[ "value" => "google.com/about-us/#section1", "expected" => "google.com/about-us/#section1" ],
+			[ "value" => "http://example.com", "expected" => "http://example.com" ],
+			[ "value" => "https://example.com", "expected" => "https://example.com" ],
+			[ "value" => "192.168.1.1", "expected" => "192.168.1.1" ],
+			[ "value" => "2001:db8::ff00:42:8329", "expected" => "2001:db8::ff00:42:8329" ],
+			[ "value" => $xss_injected_url, "expected" => $xss_injected_url ],
+			[ "value" => $sql_injected_url, "expected" => "'OR1=1--" ],
+			[ "value" => $js_injected_url, "expected" => $js_injected_url ],
+			[ "value" => "http://", "expected" => "http://" ],
+			[ "value" => "http:///example.com", "expected" => "http:///example.com" ],
+			[ "value" => $japanese_url, "expected" => "https://example.com/" ],
+			[ "value" => $japanese_domain, "expected" => "https://." ],
 		];
 	}
+
 
 }
