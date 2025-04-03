ab606676c0f8be2527e00db2e7f364d6e2686088
Maggie Serino <7341759+mserino@users.noreply.github.com>
2025-04-03 14:46:17 +0200

    Tweak: Hello biz onboarding [ED-18684] (#30762)

--- app/modules/onboarding/module.php
+++ app/modules/onboarding/module.php
@@ -76,7 +76,7 @@ class Module extends BaseModule {
 			'isLibraryConnected' => $library->is_connected(),
 			// Used to check if the Hello Elementor theme is installed but not activated.
 			'helloInstalled' => empty( $hello_theme_errors['theme_not_found'] ),
-			'helloActivated' => 'hello-elementor' === get_option( 'template' ),
+			'helloActivated' => 'hello-elementor' === get_option( 'template' ) || 'hello-biz' === get_option( 'template' ),
 			// The "Use Hello theme on my site" checkbox should be checked by default only if this condition is met.
 			'helloOptOut' => count( $pages_and_posts->posts ) < 5,
 			'siteName' => esc_html( $site_name ),
@@ -318,7 +318,7 @@ class Module extends BaseModule {
 			return $this->get_permission_error_response();
 		}
 
-		switch_theme( 'hello-elementor' );
+		switch_theme( 'hello-biz' );
 
 		return [
 			'status' => 'success',
