a81cfeeadfd865ab60b727246f2c9aca477cbe2f
Maksim Zubov <zubovmaksim@gmail.com>
2025-04-04 09:04:10 +0200

    Update assets/dev/js/editor/components/template-library/constants.js
    Co-authored-by: Louis Wolmarans <louiswol94@gmail.com>

--- assets/dev/js/editor/components/template-library/constants.js
+++ assets/dev/js/editor/components/template-library/constants.js
@@ -8,7 +8,7 @@ export const SAVE_CONTEXTS = Object.freeze( {
 
 export const QUOTA_WARNINGS = Object.freeze( {
 	/* Translators: 1: Quota usage percentage */
-	warning: __( 'You’ve reached %1$d%% of your storage.', 'elementor' ),
+	warning: __( 'You\'ve reached %1$d%% of your storage.', 'elementor' ),
 	/* Translators: 1: Quota usage percentage */
 	alert: __( 'You\'ve reached %1$d1%% of the space available in your plan. Upgrade to save more templates', 'elementor' ),
 } );
