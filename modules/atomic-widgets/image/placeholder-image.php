b2dd9b92e06c904b62b0a71156e8ae7bc81fd9f1
Miryam Oren <77922014+MiryamOren@users.noreply.github.com>
2025-04-07 12:56:48 +0300

    rename image placeholder file

--- modules/atomic-widgets/image/placeholder-image.php
+++ modules/atomic-widgets/image/placeholder-image.php
@@ -9,7 +9,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 class Placeholder_Image {
 
 	public static function get_placeholder_image() {
-		return ELEMENTOR_ASSETS_URL . 'images/placeholder-v4.svg';
+		return ELEMENTOR_ASSETS_URL . 'images/image-placeholder.svg';
 	}
 
 	public static function get_background_placeholder_image() {
