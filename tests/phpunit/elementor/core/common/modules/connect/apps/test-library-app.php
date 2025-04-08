055bb2836f36a6228fc656033d7971468b5df597
elchugreeva <elenac@elementor.com>
2025-04-08 14:56:15 +0300

    extra test cases

--- tests/phpunit/elementor/core/common/modules/connect/apps/test-library-app.php
+++ tests/phpunit/elementor/core/common/modules/connect/apps/test-library-app.php
@@ -38,6 +38,58 @@ class Test_Library_App extends Elementor_Test_Base {
 		$this->assertEquals($test_user_id, $result['library_connect']['user_id']);
 	}
 
+	public function test_localize_settings__user_id_is_null() {
+		$jwt_token = null;
+
+		$library_mock = $this->getMockBuilder(Library::class)
+			->disableOriginalConstructor()
+			->setMethods(['get', 'is_connected'])
+			->getMock();
+
+		$library_mock->expects($this->once())
+			->method('get')
+			->with('access_token')
+			->willReturn($jwt_token);
+
+		$library_mock->expects($this->once())
+			->method('is_connected')
+			->willReturn(true);
+
+		Plugin::$instance->editor->set_edit_mode( false );
+
+		$result = $library_mock->localize_settings([]);
+
+		$this->assertArrayHasKey('library_connect', $result);
+		$this->assertTrue($result['library_connect']['is_connected']);
+		$this->assertEquals(null, $result['library_connect']['user_id']);
+	}
+
+	public function test_localize_settings__user_id_is_empty() {
+		$jwt_token = '';
+
+		$library_mock = $this->getMockBuilder(Library::class)
+			->disableOriginalConstructor()
+			->setMethods(['get', 'is_connected'])
+			->getMock();
+
+		$library_mock->expects($this->once())
+			->method('get')
+			->with('access_token')
+			->willReturn($jwt_token);
+
+		$library_mock->expects($this->once())
+			->method('is_connected')
+			->willReturn(true);
+
+		Plugin::$instance->editor->set_edit_mode( false );
+
+		$result = $library_mock->localize_settings([]);
+
+		$this->assertArrayHasKey('library_connect', $result);
+		$this->assertTrue($result['library_connect']['is_connected']);
+		$this->assertEquals(null, $result['library_connect']['user_id']);
+	}
+
 	private function createTestJwtToken($user_id) {
 		$header = json_encode(['alg' => 'HS256', 'typ' => 'JWT']);
 		$payload = json_encode(['sub' => $user_id, 'name' => 'Test User']);
