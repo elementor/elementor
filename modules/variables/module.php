1eedad6f95a3d8e493e71305f16e85d389f2f04e
Batsirai Muchareva <162136788+Batsirai-muchareva@users.noreply.github.com>
2025-04-24 12:29:58 +0200

    enqueur the editor-global-variable package

--- modules/variables/module.php
+++ modules/variables/module.php
@@ -14,6 +14,9 @@ if ( ! defined( 'ABSPATH' ) ) {
 class Module extends BaseModule {
 	const MODULE_NAME = 'e-variables';
 	const EXPERIMENT_NAME = 'e_variables';
+	const PACKAGE = [
+		'editor-global-variables'
+	];
 
 	public function get_name() {
 		return self::MODULE_NAME;
@@ -41,6 +44,8 @@ class Module extends BaseModule {
 			return;
 		}
 
+		add_filter( 'elementor/editor/v2/packages', fn( $packages ) => $this->add_packages( $packages ) );
+
 		$this->hooks()
 			->register_styles_transformers()
 			->filter_for_style_schema();
@@ -49,5 +54,13 @@ class Module extends BaseModule {
 	private function is_experiment_active(): bool {
 		return Plugin::$instance->experiments->is_feature_active( self::EXPERIMENT_NAME )
 			&& Plugin::$instance->experiments->is_feature_active( AtomicWidgetsModule::EXPERIMENT_NAME );
+	}
+
+	private function add_packages( $packages ) {
+		if ( ! current_user_can( 'manage_options' ) ) {
+			return $packages;
+		}
+
+		return array_merge( $packages, self::PACKAGE );
 	}
 }
