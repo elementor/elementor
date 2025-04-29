1aafa778378cd656778e1df90d656cd5f91ce870
Gal Oz <galo@elementor.com>
2025-04-29 07:21:52 +0300

    Base URL fixes

--- modules/announcements/module.php
+++ modules/announcements/module.php
@@ -13,7 +13,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 class Module extends BaseApp {
 
-	const AI_ASSETS_BASE_URL = 'https://assets.elementor.com/ai/v1/images/';
+	const AI_ASSETS_BASE_URL = 'https://assets.elementor.com/ai/v1/';
 
 	/**
 	 * @return bool
@@ -111,7 +111,7 @@ class Module extends BaseApp {
 			'description' => __( '<p>With AI for text, code, image generation and editing, you can bring your vision to life faster than ever. Start your free trial now - <b>no credit card required!</b></p>', 'elementor' ),
 			'media' => [
 				'type' => 'image',
-				'src' => self::AI_ASSETS_BASE_URL . 'ai-social-hd.gif',
+				'src' => self::AI_ASSETS_BASE_URL . 'images/' . 'ai-social-hd.gif',
 			],
 			'cta' => [
 				[
