d1893856b61fce0fa95f5dfcc32f088dc16e7461
Ronen Boxer <ronenb@elementor.com>
2025-05-06 14:24:52 +0300

    wip

--- modules/atomic-widgets/module.php
+++ modules/atomic-widgets/module.php
@@ -87,7 +87,7 @@ class Module extends BaseModule {
 	public function __construct() {
 		parent::__construct();
 
-		$this->register_feature();
+		$this->register_experiments();
 
 		( new Opt_In() )->init();
 
@@ -113,15 +113,28 @@ class Module extends BaseModule {
 		}
 	}
 
-	private function register_feature() {
-		Plugin::$instance->experiments->add_feature([
+	public static function get_experimental_data() {
+		return [
 			'name' => self::EXPERIMENT_NAME,
 			'title' => esc_html__( 'Atomic Widgets', 'elementor' ),
 			'description' => esc_html__( 'Enable atomic widgets.', 'elementor' ),
 			'hidden' => true,
 			'default' => Experiments_Manager::STATE_INACTIVE,
 			'release_status' => Experiments_Manager::RELEASE_STATUS_ALPHA,
-		]);
+		];
+	}
+
+	private function register_experiments() {
+		if ( self::is_active() ) {
+			Plugin::$instance->experiments->add_feature( [
+				'name' => 'e_display_none',
+				'title' => esc_html__( 'V4 Display None', 'elementor' ),
+				'description' => esc_html__( 'The None display setting enables you to hide an element completely.', 'elementor' ),
+				'hidden' => true,
+				'default' => Experiments_Manager::STATE_INACTIVE,
+				'release_status' => Experiments_Manager::RELEASE_STATUS_BETA,
+			] );
+		}
 	}
 
 	private function add_packages( $packages ) {
@@ -206,6 +219,10 @@ class Module extends BaseModule {
 		$transformers->register_fallback( new Import_Export_Plain_Transformer() );
 
 		$transformers->register( Image_Src_Prop_Type::get_key(), new Image_Src_Export_Transformer() );
+	}
+
+	public static function is_active(): bool {
+		return Plugin::$instance->experiments->is_feature_active( self::EXPERIMENT_NAME );
 	}
 
 	private function get_element_usage_name( $title, $type ) {
