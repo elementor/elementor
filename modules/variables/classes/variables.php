a05d1e7eb645cae2f6d7974f028754ef50579a04
Mike Kokhanov <mikek@elementor.com>
2025-05-07 14:45:57 +0300

    Internal: Variables list [ED-19070] (#31102)
    ## PR Checklist
    <!-- 
    Please check if your PR fulfills the following requirements:
    **Filling out the template is required.** Any pull request that does not
    include enough information to be reviewed in a timely manner may be
    closed at the maintainers' discretion.
     -->
    - [ ] The commit message follows our guidelines:
    https://github.com/elementor/elementor/blob/master/.github/CONTRIBUTING.md
    
    
    ## PR Type
    What kind of change does this PR introduce?
    <!-- Please check the one that applies to this PR using "x" with no
    spaces eg: [x]. -->
    - [ ] Bugfix
    - [ ] Feature
    - [ ] Code style update (formatting, local variables)
    - [ ] Refactoring (no functional changes, no api changes)
    - [ ] Build related changes
    - [ ] CI related changes
    - [ ] Documentation content changes
    - [ ] Other... Please describe:
    
    ## Summary
    
    This PR can be summarized in the following changelog entry:
    
    *
    
    ## Description
    An explanation of what is done in this PR
    
    *
    
    ## Test instructions
    This PR can be tested by following these steps:
    
    *
    
    ## Quality assurance
    
    - [ ] I have tested this code to the best of my abilities
    - [ ] I have added unittests to verify the code works as intended
    - [ ] Docs have been added / updated (for bug fixes / features)
    
    Fixes #

--- modules/variables/classes/variables.php
+++ modules/variables/classes/variables.php
@@ -23,9 +23,73 @@ class Variables {
 				'value' => '#404040',
 				'label' => 'Main: text',
 			],
-			'e-gc-004' => [
-				'value' => '#00000033',
-				'label' => 'Main: overlay',
+
+			'e-gc-a01' => [
+				'value' => '#FF0000',
+				'label' => 'Danger: red',
+			],
+			'e-gc-a02' => [
+				'value' => '#0000FF',
+				'label' => 'Informative: blue',
+			],
+			'e-gc-a03' => [
+				'value' => '#FF7BE5',
+				'label' => 'Elementor: pink',
+			],
+			'e-gc-a04' => [
+				'value' => '#808080',
+				'label' => 'Gray: background',
+			],
+
+			'e-gc-b01' => [
+				'value' => '#213555',
+				'label' => 'Navy: primary',
+			],
+			'e-gc-b02' => [
+				'value' => '#3E5879',
+				'label' => 'Navy: secondary',
+			],
+			'e-gc-b03' => [
+				'value' => '#D8C4B6',
+				'label' => 'Navy: light',
+			],
+			'e-gc-b04' => [
+				'value' => '#F5EFE7',
+				'label' => 'Navy long text variable name: background',
+			],
+
+			'e-gc-d01' => [
+				'value' => '#123524',
+				'label' => 'Green: primary',
+			],
+			'e-gc-d02' => [
+				'value' => '#3E7B27',
+				'label' => 'Green: secondary',
+			],
+			'e-gc-d03' => [
+				'value' => '#85A947',
+				'label' => 'Green: light',
+			],
+			'e-gc-d04' => [
+				'value' => '#85A947',
+				'label' => 'Green: background',
+			],
+
+			'e-gc-c01' => [
+				'value' => '#3B1E54',
+				'label' => 'Violet: primary',
+			],
+			'e-gc-c02' => [
+				'value' => '#9B7EBD',
+				'label' => 'Violet: secondary',
+			],
+			'e-gc-c03' => [
+				'value' => '#D4BEE4',
+				'label' => 'Violet: light',
+			],
+			'e-gc-c04' => [
+				'value' => '#EEEEEE',
+				'label' => 'Violet: background',
 			],
 		] );
 	}
