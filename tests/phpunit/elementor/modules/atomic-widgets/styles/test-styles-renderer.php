be0348d659307e59a0a727930afa99b8164902f5
Ron Rostovsky <ronros@elementor.com>
2025-05-07 17:28:39 +0300

    wip

--- tests/phpunit/elementor/modules/atomic-widgets/styles/test-styles-renderer.php
+++ tests/phpunit/elementor/modules/atomic-widgets/styles/test-styles-renderer.php
@@ -585,7 +585,7 @@ class Test_Styles_Renderer extends Elementor_Test_Base {
 													],
 													'size' => 'cover',
 													'position' => 'bottom right',
-													'repeat' => 'repeat',
+													'repeat' => 'no-repeat',
 													'attachment' => 'fixed',
 												]
 											],
