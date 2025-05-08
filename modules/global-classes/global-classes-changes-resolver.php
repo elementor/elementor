b5c15321c60bc34ba96f8aab1f15d7e2b3633f3b
Nevo Golan <20494189+Nevoss@users.noreply.github.com>
2025-05-08 14:21:05 +0300

    Fix: Global class lost when publishing from multi tabs [ED-19004]

---
+++ modules/global-classes/global-classes-changes-resolver.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Elementor\Modules\GlobalClasses;
+
+use Elementor\Core\Utils\Collection;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+class Global_Classes_Changes_Resolver {
+	private Global_Classes_Repository $repository;
+
+	private Collection $added;
+
+	private Collection $deleted;
+
+	private Collection $modified;
+
+	public function __construct( Global_Classes_Repository $repository, array $changes ) {
+		$this->repository = $repository;
+		$this->added = Collection::make( $changes['added'] ?? [] );
+		$this->deleted = Collection::make( $changes['deleted'] ?? [] );
+		$this->modified = Collection::make( $changes['modified'] ?? [] );
+	}
+
+	public static function make( Global_Classes_Repository $repository, array $changes ): self {
+		return new self( $repository, $changes );
+	}
+
+	public function resolve_items( array $payload ) {
+		$touched = $this->added->merge( $this->modified )->values();
+
+		$items_to_save = Collection::make( $payload )->only( $touched );
+
+		return $this->repository
+			->all()
+			->get_items()
+			->except( $this->deleted->values() )
+			->merge( $items_to_save )
+			->all();
+	}
+
+	public function resolve_order( array $payload ) {
+		$missing_in_payload = $this->repository
+			->all()
+			->get_order()
+			->filter( fn( $item ) => ! $this->deleted->contains( $item ) )
+			->diff( $payload );
+
+		return $missing_in_payload
+			->merge( $payload )
+			->all();
+	}
+}
