3498bb9425bee09a20268cccd379710ffab7056a
Ron Rostovsky <ronros@elementor.com>
2025-05-08 13:53:46 +0300

    wip

--- modules/atomic-widgets/module.php
+++ modules/atomic-widgets/module.php
@@ -69,8 +69,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 class Module extends BaseModule {
 	const EXPERIMENT_NAME = 'e_atomic_elements';
-	const CSSID_EXPERIMENT_NAME = 'e_css_id';
-
 	const PACKAGES = [
 		'editor-canvas',
 		'editor-current-user',
@@ -124,23 +122,6 @@ class Module extends BaseModule {
 			'default' => Experiments_Manager::STATE_INACTIVE,
 			'release_status' => Experiments_Manager::RELEASE_STATUS_ALPHA,
 		]);
-
-		Plugin::$instance->experiments->add_feature( [
-			'name' => self::CSSID_EXPERIMENT_NAME,
-			'title' => esc_html__( 'V4 - CSS ID', 'elementor' ),
-			'description' => esc_html__( 'Enable CSS ID control for V4.', 'elementor' ),
-			'hidden' => true,
-			'default' => Experiments_Manager::STATE_INACTIVE,
-			'release_status' => Experiments_Manager::RELEASE_STATUS_ALPHA,
-		] );
-
-		Plugin::$instance->experiments->add_feature( [
-			'name' => 'e_indications_popover',
-			'title' => esc_html__( 'V4 Indications Popover', 'elementor' ),
-			'description' => esc_html__( 'Enable V4 Indication Popovers', 'elementor' ),
-			'hidden' => true,
-			'default' => Experiments_Manager::STATE_INACTIVE,
-		] );
 	}
 
 	private function add_packages( $packages ) {
