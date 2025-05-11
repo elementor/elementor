bf0e65e76be565c00ec2a68344a4d96c1553be4f
Nurit <nurit@elementor.com>
2025-05-11 17:59:32 +0300

    wip

--- assets/dev/js/admin/admin.js
+++ assets/dev/js/admin/admin.js
@@ -422,7 +422,7 @@ import FloatingButtonsHandler from 'elementor/modules/floating-buttons/assets/js
 				$importNowButton = self.elements.$importNowButton,
 				$importFormFileInput = self.elements.$importFormFileInput;
 
-			if ( ! Array.isArray( $importNowButton ) ) {
+			if ( undefined === $importNowButton[0] ) {
 				return;
 			}
 
