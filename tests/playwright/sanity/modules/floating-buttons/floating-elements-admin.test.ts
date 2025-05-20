250388e2ee96fd47f4cd9ad7c767f58009c3683b
Rami Yushuvaev <ramiy@elementor.com>
2025-05-20 14:21:00 +0300

    Internal: Remove unneeded `title` attributes from the Editor

--- tests/playwright/sanity/modules/floating-buttons/floating-elements-admin.test.ts
+++ tests/playwright/sanity/modules/floating-buttons/floating-elements-admin.test.ts
@@ -127,7 +127,7 @@ test.describe( 'Verify floating buttons editor, admin page and front page behavi
 		} );
 	} );
 
-	test( 'Verify floating elements admin page behavior', async ( {
+	test.only( 'Verify floating elements admin page behavior', async ( {
 		page,
 		apiRequests,
 	}, testInfo ) => {
@@ -176,7 +176,7 @@ test.describe( 'Verify floating buttons editor, admin page and front page behavi
 				await expect( footer.first() ).toBeVisible();
 
 				const closeIcon = page.locator( '.elementor-templates-modal__header__close i' );
-				await expect( closeIcon ).toHaveAttribute( 'title', 'Go To Dashboard' );
+				await expect( closeIcon ).toHaveAttribute( 'class', 'eicon-close' );
 				await closeIcon.click();
 				await page.waitForURL( '/wp-admin/edit.php?post_type=e-floating-buttons' );
 			} );
