f828a3bb9a4b9f249fb9f95be628b4ff3b59c607
Hein van Vlastuin <94352322+hein-obox@users.noreply.github.com>
2025-05-26 08:56:57 +0200

    Internal: Update shortcode handling - GA [ED-18986] (#31283)
    ## PR Checklist
    <!-- 
    Please check if your PR fulfills the following requirements:
    **Filling out the template is required.** Any pull request that does not
    include enough information to be reviewed in a timely manner may be
    closed at the maintainers' discretion.
     -->
    - [ ] The commit message follows our guidelines:
    https://github.com/elementor/elementor/blob/master/.github/CONTRIBUTING.md
    
    
    ## PR Type
    What kind of change does this PR introduce?
    <!-- Please check the one that applies to this PR using "x" with no
    spaces eg: [x]. -->
    - [ ] Bugfix
    - [ ] Feature
    - [ ] Code style update (formatting, local variables)
    - [ ] Refactoring (no functional changes, no api changes)
    - [ ] Build related changes
    - [ ] CI related changes
    - [ ] Documentation content changes
    - [ ] Other... Please describe:
    
    ## Summary
    
    This PR can be summarized in the following changelog entry:
    
    *
    
    ## Description
    An explanation of what is done in this PR
    
    *
    
    ## Test instructions
    This PR can be tested by following these steps:
    
    *
    
    ## Quality assurance
    
    - [ ] I have tested this code to the best of my abilities
    - [ ] I have added unittests to verify the code works as intended
    - [ ] Docs have been added / updated (for bug fixes / features)
    
    Fixes #
    
    ---------
    
    Co-authored-by: Hein van Vlastuin <heinvanvlastuin@gmail.com>

--- includes/base/element-base.php
+++ includes/base/element-base.php
@@ -452,7 +452,9 @@ abstract class Element_Base extends Controls_Stack {
 		$element_type = $this->get_type();
 
 		if ( $this->should_render_shortcode() ) {
-			echo '[elementor-element data="' . esc_attr( base64_encode( wp_json_encode( $this->get_raw_data() ) ) ) . '"]';
+			$unique_id = apply_filters( 'elementor/element_cache/unique_id', '' );
+
+			echo '[elementor-element k="' . esc_attr( $unique_id ) . '" data="' . esc_attr( base64_encode( wp_json_encode( $this->get_raw_data() ) ) ) . '"]';
 			return;
 		}
 
