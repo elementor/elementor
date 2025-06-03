4821bdc48a248db743e6348a3b0753ceff026fe3
Louis Wolmarans <louiswol94@gmail.com>
2025-06-03 12:47:04 +0200

    wip

--- app/modules/kit-library/connect/cloud-kits.php
+++ app/modules/kit-library/connect/cloud-kits.php
@@ -36,5 +36,77 @@ class Cloud_Kits extends Library {
 		] );
 	}
 
+	public function create_kit( $title, $content_file_data, $preview_file_data ) {
+		$endpoint = 'kits';
+
+		$boundary = wp_generate_password( 24, false );
+
+		$headers = [
+			'Content-Type' => 'multipart/form-data; boundary=' . $boundary,
+		];
+
+		$body = $this->create_multipart_body(
+			[
+				'title' => $title,
+			],
+			[
+				'file' => [
+					'filename' => 'content.zip',
+					'content' => $content_file_data,
+					'content_type' => 'application/zip',
+				],
+				'previewFile' => [
+					'filename' => 'preview.png',
+					'content' => $preview_file_data,
+					'content_type' => 'image/png',
+				],
+			],
+			$boundary
+		);
+
+		$payload = [
+			'headers' => $headers,
+			'body' => $body,
+			'timeout' => 120,
+		];
+
+		$response = $this->http_request( 'POST', $endpoint, $payload, [
+			'return_type' => static::HTTP_RETURN_TYPE_ARRAY,
+		] );
+
+		if ( empty( $response['id'] ) ) {
+			$error_message = esc_html__( 'Failed to create kit: Invalid response', 'elementor' );
+			throw new \Exception( $error_message, Exceptions::INTERNAL_SERVER_ERROR ); // phpcs:ignore WordPress.Security.EscapeOutput.ExceptionNotEscaped
+		}
+
+		return $response;
+	}
+
+	private function create_multipart_body( $fields, $files, $boundary ): string {
+		$eol = "\r\n";
+		$body = '';
+
+		foreach ( $fields as $name => $value ) {
+			$body .= "--{$boundary}{$eol}";
+			$body .= "Content-Disposition: form-data; name=\"{$name}\"{$eol}{$eol}";
+			$body .= "{$value}{$eol}";
+		}
+
+		foreach ( $files as $name => $file ) {
+			$filename = basename( $file['filename'] );
+			$content_type = $file['content_type'];
+			$content = $file['content'];
+
+			$body .= "--{$boundary}{$eol}";
+			$body .= "Content-Disposition: form-data; name=\"{$name}\"; filename=\"{$filename}\"{$eol}";
+			$body .= "Content-Type: {$content_type}{$eol}{$eol}";
+			$body .= $content . $eol;
+		}
+
+		$body .= "--{$boundary}--{$eol}";
+
+		return $body;
+	}
+
 	protected function init() {}
 }
