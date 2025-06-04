7db3d070365867233855de8d98792d39b8d5ff2e
Louis Wolmarans <louiswol94@gmail.com>
2025-06-04 12:47:28 +0200

    wip

--- app/modules/import-export/module.php
+++ app/modules/import-export/module.php
@@ -613,7 +613,7 @@ class Module extends BaseModule {
 	 * Handle upload kit ajax request.
 	 */
 	private function handle_upload_kit() {
-		$file_url = isset( $_POST['e_import_file'] ) ? $_POST['e_import_file'] : ''; // phpcs:ignore WordPress.Security.NonceVerification.Missing
+		$file_url = isset( $_POST['e_import_file'] ) ? wp_unslash( $_POST['e_import_file'] ) : ''; // phpcs:ignore WordPress.Security.NonceVerification.Missing
 		$kit_id = ElementorUtils::get_super_global_value( $_POST, 'kit_id' ); // phpcs:ignore WordPress.Security.NonceVerification.Missing
 
 		$is_import_from_library = ! empty( $file_url );
