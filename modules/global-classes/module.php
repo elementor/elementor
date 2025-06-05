1d6a34fc12bab3d22c1b4bebb21fd9c07740c8c8
IshayMaya <ishaym@elementor.com>
2025-06-05 13:55:06 +0300

    activate capabilities experiments

--- modules/global-classes/module.php
+++ modules/global-classes/module.php
@@ -63,7 +63,7 @@ class Module extends BaseModule {
 			'title' => esc_html__( 'Enforce global classes capabilities', 'elementor' ),
 			'description' => esc_html__( 'Enforce global classes capabilities.', 'elementor' ),
 			'hidden' => true,
-			'default' => Experiments_Manager::STATE_INACTIVE,
+			'default' => Experiments_Manager::STATE_ACTIVE,
 			'release_status' => Experiments_Manager::RELEASE_STATUS_DEV,
 		]);
 	}
