17e4b302d6113f206abd66b80bf6805f2fdcdedd
Davids Seveloff <94905340+davseve@users.noreply.github.com>
2025-06-05 13:05:48 +0300

    Internal: Activate 3.30 experiment [ED-19390] (#31419)

--- modules/atomic-widgets/module.php
+++ modules/atomic-widgets/module.php
@@ -155,7 +155,7 @@ class Module extends BaseModule {
 			'title' => esc_html__( 'Version 3.30', 'elementor' ),
 			'description' => esc_html__( 'Features for version 3.30.', 'elementor' ),
 			'hidden' => true,
-			'default' => Experiments_Manager::STATE_INACTIVE,
+			'default' => Experiments_Manager::STATE_ACTIVE,
 			'release_status' => Experiments_Manager::RELEASE_STATUS_DEV,
 		]);
 	}
