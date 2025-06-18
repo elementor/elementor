0aff92c0725d06f29f1f3c8d998316a6f62f758f
Nikita Murzin <mykytamu@elementor.com>
2025-06-18 08:32:46 +0200

    Internal: Initialise transform + move [ED-19407]

---
+++ modules/atomic-widgets/prop-types/transform/move-transform-prop-type.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Elementor\Modules\AtomicWidgets\PropTypes\Transform;
+
+use Elementor\Modules\AtomicWidgets\PropTypes\Base\Object_Prop_Type;
+use Elementor\Modules\AtomicWidgets\PropTypes\Size_Prop_Type;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+class Move_Transform_Prop_Type extends Object_Prop_Type {
+
+	public static function get_key(): string {
+		return 'move';
+	}
+
+	protected function define_shape(): array {
+		return [
+			'x' => Size_Prop_Type::make()->default( 0 ),
+			'y' => Size_Prop_Type::make()->default( 0 ),
+			'z' => Size_Prop_Type::make()->default( 0 ),
+		];
+	}
+}
