3ad67505d891e746a44fc0390c0ea67664526ed5
YaelAvitan <yael6768889@gmail.com>
2025-06-18 13:08:27 +0300

    fix

--- modules/atomic-widgets/elements/atomic-image/atomic-image.php
+++ modules/atomic-widgets/elements/atomic-image/atomic-image.php
@@ -14,86 +14,86 @@ use Elementor\Modules\AtomicWidgets\Styles\Style_Definition;
 use Elementor\Modules\AtomicWidgets\Styles\Style_Variant;
 
 if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly.
+    exit; // Exit if accessed directly.
 }
 
 class Atomic_Image extends Atomic_Widget_Base {
-	use Has_Template;
+    use Has_Template;
 
-	const LINK_BASE_STYLE_KEY = 'link-base';
-	const BASE_STYLE_KEY = 'base';
+    const LINK_BASE_STYLE_KEY = 'link-base';
+    const BASE_STYLE_KEY = 'base';
 
-	public static function get_element_type(): string {
-		return 'e-image';
-	}
+    public static function get_element_type(): string {
+        return 'e-image';
+    }
 
-	public function get_title() {
-		return esc_html__( 'Image', 'elementor' );
-	}
+    public function get_title() {
+        return esc_html__( 'Image', 'elementor' );
+    }
 
-	public function get_keywords() {
-		return [ 'ato', 'atom', 'atoms', 'atomic' ];
-	}
+    public function get_keywords() {
+        return [ 'ato', 'atom', 'atoms', 'atomic' ];
+    }
 
-	public function get_icon() {
-		return 'eicon-e-image';
-	}
+    public function get_icon() {
+        return 'eicon-e-image';
+    }
 
-	protected static function define_props_schema(): array {
-		$props = [
-			'classes' => Classes_Prop_Type::make()
-				->default( [] ),
+    protected static function define_props_schema(): array {
+        $props = [
+            'classes' => Classes_Prop_Type::make()
+                ->default( [] ),
 
-			'image' => Image_Prop_Type::make()
-				->default_url( Placeholder_Image::get_placeholder_image() )
-				->default_size( 'full' ),
+            'image' => Image_Prop_Type::make()
+                ->default_url( Placeholder_Image::get_placeholder_image() )
+                ->default_size( 'full' ),
 
-			'link' => Link_Prop_Type::make(),
-                ];
+            'link' => Link_Prop_Type::make(),
+        ];
 
-		return $props;
-	}
+        return $props;
+    }
 
-	protected function define_atomic_controls(): array {
-		return [
-			Section::make()
-				->set_label( esc_html__( 'Content', 'elementor' ) )
-				->set_items( [
-					Image_Control::bind_to( 'image' )
-						->set_show_mode( 'media' ),
-				] ),
-		];
-	}
+    protected function define_atomic_controls(): array {
+        return [
+            Section::make()
+                ->set_label( esc_html__( 'Content', 'elementor' ) )
+                ->set_items( [
+                    Image_Control::bind_to( 'image' )
+                        ->set_show_mode( 'media' ),
+                ] ),
+        ];
+    }
 
-	protected function get_settings_controls(): array {
-		return [
-			Image_Control::bind_to( 'image' )
-				->set_show_mode( 'sizes' ),
-			Link_Control::bind_to( 'link' )->set_meta( [
-				'topDivider' => true,
-			] ),
-		];
-	}
+    protected function get_settings_controls(): array {
+        return [
+            Image_Control::bind_to( 'image' )
+                ->set_show_mode( 'sizes' ),
+            Link_Control::bind_to( 'link' )->set_meta( [
+                'topDivider' => true,
+            ] ),
+        ];
+    }
 
-	protected function define_base_styles(): array {
-		return [
-			self::LINK_BASE_STYLE_KEY => Style_Definition::make()
-				->add_variant(
-					Style_Variant::make()
-						->add_prop( 'display', 'inherit' )
-						->add_prop( 'width', 'fit-content' )
-				),
-			self::BASE_STYLE_KEY => Style_Definition::make()
-				->add_variant(
-					Style_Variant::make()
-						->add_prop( 'display', 'block' )
-				),
-		];
-	}
+    protected function define_base_styles(): array {
+        return [
+            self::LINK_BASE_STYLE_KEY => Style_Definition::make()
+                ->add_variant(
+                    Style_Variant::make()
+                        ->add_prop( 'display', 'inherit' )
+                        ->add_prop( 'width', 'fit-content' )
+                ),
+            self::BASE_STYLE_KEY => Style_Definition::make()
+                ->add_variant(
+                    Style_Variant::make()
+                        ->add_prop( 'display', 'block' )
+                ),
+        ];
+    }
 
-	protected function get_templates(): array {
-		return [
-			'elementor/elements/atomic-image' => __DIR__ . '/atomic-image.html.twig',
-		];
-	}
+    protected function get_templates(): array {
+        return [
+            'elementor/elements/atomic-image' => __DIR__ . '/atomic-image.html.twig',
+        ];
+    }
 }
