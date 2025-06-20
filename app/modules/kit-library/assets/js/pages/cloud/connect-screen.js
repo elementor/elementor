21de8cb377d67f85a40dbaa8d80a8f29f5b11ea9
Louis Wolmarans <louiswol94@gmail.com>
2025-06-20 11:17:05 +0200

    wip

--- app/modules/kit-library/assets/js/pages/cloud/connect-screen.js
+++ app/modules/kit-library/assets/js/pages/cloud/connect-screen.js
@@ -30,7 +30,7 @@ export default function ConnectScreen( {
 				elementorCommon.config.library_connect.current_access_level = data.kits_access_level || data.access_level || 0;
 				elementorCommon.config.library_connect.current_access_tier = data.access_tier;
 
-				onConnectSuccess?.( data );
+				onConnectSuccess?.();
 			},
 			error: () => {
 				elementorCommon.config.library_connect.is_connected = false;
