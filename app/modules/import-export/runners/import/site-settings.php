312ae91bfe1325a50e8ffeb1321d246ada92a4ac
max-zu <zubovmaksim@gmail.com>
2025-06-20 11:16:22 +0200

    ED-19570 lint

--- app/modules/import-export/runners/import/site-settings.php
+++ app/modules/import-export/runners/import/site-settings.php
@@ -96,7 +96,8 @@ class Site_Settings extends Import_Runner_Base {
 					$import = $this->install_theme( $theme['slug'], $theme['version'] );
 
 					if ( is_wp_error( $import ) ) {
-						$result['theme']['failed'][ $theme['slug'] ] = __( sprintf( 'Failed to install theme: %1$s', $theme['name'] ), 'elementor' );
+						$result['theme']['failed'][ $theme['slug'] ] = sprintf( __( 'Failed to install theme: %1$s', 'elementor' ), $theme['name'] );
+
 					} else {
 						$result['theme']['succeed'][ $theme['slug'] ] = $import;
 						$this->installed_theme = $theme['slug'];
