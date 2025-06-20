c6f0833ed280ea025021c01358f6e72225dabd0a
David Seveloff <davids@elementor.com>
2025-06-20 12:42:47 +0300

    lint

--- tests/playwright/selectors/editor-selectors.ts
+++ tests/playwright/selectors/editor-selectors.ts
@@ -17,7 +17,7 @@ const EditorSelectors = {
 			wrapper: '#elementor-panel-page-elements',
 			v4elements: '#elementor-panel-category-v4-elements',
 			footerButton: '#elementor-panel-header-add-button i',
-			elementorPanel: '#elementor-panel'
+			elementorPanel: '#elementor-panel',
 		},
 		pageSettings: {
 			wrapper: '#elementor-panel-page-settings',
