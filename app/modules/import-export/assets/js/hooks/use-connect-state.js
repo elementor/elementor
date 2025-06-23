23f4927760748b24759099d18a9dcba1249b0a7a
Louis Wolmarans <louiswol94@gmail.com>
2025-06-23 11:18:38 +0200

    wip

--- app/modules/import-export/assets/js/hooks/use-connect-state.js
+++ app/modules/import-export/assets/js/hooks/use-connect-state.js
@@ -3,10 +3,10 @@ import { ConnectStateContext } from '../context/connect-state-context';
 
 export default function useConnectState() {
 	const context = useContext( ConnectStateContext );
-	
+
 	if ( ! context ) {
 		throw new Error( 'useConnectState must be used within a ConnectStateProvider' );
 	}
-	
+
 	return context;
 }
