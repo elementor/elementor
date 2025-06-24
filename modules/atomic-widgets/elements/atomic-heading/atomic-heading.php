06b02087c517b62191ca9f08280e1cbd259b53ed
Omer Haim Israeli <omeri@elementor.com>
2025-06-24 13:55:07 +0300

    wip

--- modules/atomic-widgets/elements/atomic-heading/atomic-heading.php
+++ modules/atomic-widgets/elements/atomic-heading/atomic-heading.php
@@ -54,7 +54,7 @@ class Atomic_Heading extends Atomic_Widget_Base {
 
 			'link' => Link_Prop_Type::make(),
 
-			'repeater' => Key_Value_Array_Prop_Type::make(),
+			'attributes' => Key_Value_Array_Prop_Type::make(),
 		];
 
 		return $props;
@@ -104,9 +104,11 @@ class Atomic_Heading extends Atomic_Widget_Base {
 						'label' => 'H6',
 					],
 				]),
-			Link_Control::bind_to( 'link' )->set_meta( [
-				'topDivider' => true,
-			] ),
+			Link_Control::bind_to( 'link' )
+				->set_label( __( 'Link', 'elementor' ) )
+				->set_meta( [
+					'topDivider' => true,
+				] ),
 		];
 	}
 
