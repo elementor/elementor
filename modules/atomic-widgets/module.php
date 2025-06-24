ce484ea9c7fa2c6b7bfadf44b36bd96543fcf571
Omer Haim Israeli <omeri@elementor.com>
2025-06-24 13:58:41 +0300

    wip

--- modules/atomic-widgets/module.php
+++ modules/atomic-widgets/module.php
@@ -300,7 +300,7 @@ class Module extends BaseModule {
 
 	private function render_panel_category_chip() {
 		?><# if ( 'v4-elements' === name )  { #>
-		<span class="elementor-panel-heading-category-chip">
+			<span class="elementor-panel-heading-category-chip">
 				<?php echo esc_html__( 'Alpha', 'elementor' ); ?><i class="eicon-info"></i>
 				<span class="e-promotion-react-wrapper" data-promotion="v4_chip"></span>
 			</span>
