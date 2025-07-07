6be665bec87bca55ddc72bf5b8106671fb65c79e
Marcin Skowronek <marcins@elementor.red>
2025-07-07 17:17:07 +0200

    fixes

--- packages/packages/libs/editor-controls/src/controls/transform-control/functions/scale-axis-row.tsx
+++ packages/packages/libs/editor-controls/src/controls/transform-control/functions/scale-axis-row.tsx
@@ -11,19 +11,19 @@ type ScaleAxisRowProps = {
 	label: string;
 	bindValue: 'x' | 'y' | 'z';
 	startIcon: React.ReactNode;
-	anchorRef?: RefObject< HTMLDivElement | null >;
+	anchorRef?: RefObject<HTMLDivElement | null>;
 };
 
-export const ScaleAxisRow = ( { label, bindValue, startIcon, anchorRef }: ScaleAxisRowProps ) => {
+export const ScaleAxisRow = ({ label, bindValue, startIcon, anchorRef }: ScaleAxisRowProps) => {
 	return (
-		<Grid item xs={ 12 }>
-			<PopoverGridContainer ref={ anchorRef }>
-				<Grid item xs={ 6 }>
-					<ControlLabel>{ label }</ControlLabel>
+		<Grid item xs={12}>
+			<PopoverGridContainer ref={anchorRef}>
+				<Grid item xs={6}>
+					<ControlLabel>{label}</ControlLabel>
 				</Grid>
-				<Grid item xs={ 6 }>
-					<PropKeyProvider bind={ bindValue }>
-            <NumberControl step={ 0.1 } placeholder="1" startIcon={ startIcon } />
+				<Grid item xs={6}>
+					<PropKeyProvider bind={bindValue}>
+						<NumberControl step={0.1} placeholder="1" startIcon={startIcon} />
 					</PropKeyProvider>
 				</Grid>
 			</PopoverGridContainer>
