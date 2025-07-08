1f78009741ffc62152cdf31b4465b096e9fd864e
mulituriel_tikal <muli.turiel@tikalk.com>
2025-07-08 17:08:16 +0300

    update type

--- packages/packages/core/editor-global-classes/src/components/css-class-usage/hooks/use-css-class-usage-by-id.tsx
+++ packages/packages/core/editor-global-classes/src/components/css-class-usage/hooks/use-css-class-usage-by-id.tsx
@@ -6,7 +6,9 @@ const EMPTY_CLASS_USAGE: EnhancedCssClassUsageContent = {
 	content: [],
 };
 
-export const useCssClassUsageByID = ( id: string ): { data: EnhancedCssClassUsageContent; isLoading: boolean } => {
+export const useCssClassUsageByID = (
+	id: string
+): { data: EnhancedCssClassUsageContent; isLoading: boolean; isSuccess: boolean } => {
 	const { data, ...rest } = useCssClassUsage();
 	const classData = data?.[ id ] ?? EMPTY_CLASS_USAGE;
 	return { ...rest, data: classData };
