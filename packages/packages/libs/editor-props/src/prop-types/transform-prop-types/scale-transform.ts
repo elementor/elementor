8ced17e25c00503e9063c5f62f920d5dd4a1d5e7
Nikita Murzin <mykytamu@elementor.com>
2025-07-08 14:15:59 +0200

    Internal: Fix proptype [ED-19407]

--- packages/packages/libs/editor-props/src/prop-types/transform-prop-types/scale-transform.ts
+++ packages/packages/libs/editor-props/src/prop-types/transform-prop-types/scale-transform.ts
@@ -7,9 +7,9 @@ import { TransformFunctionKeys } from './types';
 export const scaleTransformPropTypeUtil = createPropUtils(
 	TransformFunctionKeys.scale,
 	z.strictObject( {
-		x: numberPropTypeUtil.schema.default( { value: 1, $$type: "number" } ),
-		y: numberPropTypeUtil.schema.default( { value: 1, $$type: "number" } ),
-		z: numberPropTypeUtil.schema.default( { value: 1, $$type: "number" } ),
+		x: numberPropTypeUtil.schema.default( { value: 1, $$type: "number" } ).nullable(),
+		y: numberPropTypeUtil.schema.default( { value: 1, $$type: "number" } ).nullable(),
+		z: numberPropTypeUtil.schema.default( { value: 1, $$type: "number" } ).nullable(),
 	} )
 );
 
