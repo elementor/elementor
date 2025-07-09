0801527e5397651e339721534e06c6cf0f4513e1
YaelAvitan <yael6768889@gmail.com>
2025-07-09 11:45:25 +0300

    Update transform-rotate-transformer.php

--- modules/atomic-widgets/props-resolver/transformers/transform-rotate-transformer.php
+++ modules/atomic-widgets/props-resolver/transformers/transform-rotate-transformer.php
@@ -11,12 +11,13 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 class Transform_Rotate_Transformer extends Transformer_Base {
 	public function transform( $value, Props_Resolver_Context $context ): string {
-		$default_Rotate = '0deg';
+		$default_rotate = '0deg';
+
 		return sprintf(
-			'rotateX(%sdeg) rotateY(%sdeg) rotateZ(%sdeg)',
-			$value['x'] ?? $default_Rotate,
-			$value['y'] ?? $default_Rotate,
-			$value['z'] ??$default_Rotate
+			'rotateX(%s) rotateY(%s) rotateZ(%s)',
+			$value['x'] ?? $default_rotate,
+			$value['y'] ?? $default_rotate,
+			$value['z'] ?? $default_rotate
 		);
 	}
 }
