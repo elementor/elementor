bb44ea43c7e05fcf9fbbe17ca1114281b6d85a82
Batsirai Muchareva <batsiraim@elementor.red>
2025-07-11 14:36:07 +0200

    wip

--- packages/packages/libs/editor-controls/src/components/size-control/size-input.tsx
+++ packages/packages/libs/editor-controls/src/components/size-control/size-input.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { useRef } from 'react';
-import type { SizePropValue } from '@elementor/editor-props';
+import type { PropValue, SizePropValue } from '@elementor/editor-props';
 import { PencilIcon } from '@elementor/icons';
 import { Box, InputAdornment, type PopupState } from '@elementor/ui';
 
