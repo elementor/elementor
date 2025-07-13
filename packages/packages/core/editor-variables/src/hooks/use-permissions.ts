9d640cf4f047746eda56262421807b3bcb58c6d0
Mike Kokhanov <mikek@elementor.com>
2025-07-13 17:51:17 +0300

    [ wip ]: review feedback

--- packages/packages/core/editor-variables/src/hooks/use-permissions.ts
+++ packages/packages/core/editor-variables/src/hooks/use-permissions.ts
@@ -1,30 +1,15 @@
-import { useState } from 'react';
 import { useCurrentUserCapabilities } from '@elementor/editor-current-user';
 
 export const usePermissions = () => {
 	const { canUser: userCan } = useCurrentUserCapabilities();
 
-	const [ permissions ] = useState( () => {
-		return {
-			assign: userCan( 'edit_posts' ),
-			unlink: userCan( 'edit_posts' ),
-
-			add: userCan( 'manage_options' ),
-			edit: userCan( 'manage_options' ),
-			delete: userCan( 'manage_options' ),
-			restore: userCan( 'manage_options' ),
-
-			manageSettings: userCan( 'manage_options' ),
-		};
-	} );
-
 	return {
-		canAssign: () => !! permissions?.assign,
-		canUnlink: () => !! permissions?.unlink,
-		canAdd: () => !! permissions?.add,
-		canDelete: () => !! permissions?.delete,
-		canEdit: () => !! permissions?.edit,
-		canRestore: () => !! permissions?.restore,
-		canManageSettings: () => !! permissions?.manageSettings,
+		canAssign: () => userCan( 'edit_posts' ),
+		canUnlink: () => userCan( 'edit_posts' ),
+		canAdd: () => userCan( 'manage_options' ),
+		canDelete: () => userCan( 'manage_options' ),
+		canEdit: () => userCan( 'manage_options' ),
+		canRestore: () => userCan( 'manage_options' ),
+		canManageSettings: () => userCan( 'manage_options' ),
 	};
 };
