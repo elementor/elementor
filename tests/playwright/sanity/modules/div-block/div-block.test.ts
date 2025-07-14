d18b2db646a5221ffdc3bc9bf591b87d9f51bdba
Svitlana-Dykun <svitlanad@elementor.red>
2025-07-14 22:25:27 +0200

    Test 3 (#31860)
    ## PR Checklist
    <!-- 
    Please check if your PR fulfills the following requirements:
    **Filling out the template is required.** Any pull request that does not
    include enough information to be reviewed in a timely manner may be
    closed at the maintainers' discretion.
     -->
    - [ ] The commit message follows our guidelines:
    https://github.com/elementor/elementor/blob/master/.github/CONTRIBUTING.md
    
    
    ## PR Type
    What kind of change does this PR introduce?
    <!-- Please check the one that applies to this PR using "x" with no
    spaces eg: [x]. -->
    - [ ] Bugfix
    - [ ] Feature
    - [ ] Code style update (formatting, local variables)
    - [ ] Refactoring (no functional changes, no api changes)
    - [ ] Build related changes
    - [ ] CI related changes
    - [ ] Documentation content changes
    - [ ] Other... Please describe:
    
    ## Summary
    
    This PR can be summarized in the following changelog entry:
    
    *
    
    ## Description
    An explanation of what is done in this PR
    
    *
    
    ## Test instructions
    This PR can be tested by following these steps:
    
    *
    
    ## Quality assurance
    
    - [ ] I have tested this code to the best of my abilities
    - [ ] I have added unittests to verify the code works as intended
    - [ ] Docs have been added / updated (for bug fixes / features)
    
    Fixes #

--- tests/playwright/sanity/modules/div-block/div-block.test.ts
+++ tests/playwright/sanity/modules/div-block/div-block.test.ts
@@ -29,10 +29,6 @@ test.describe( 'Div Block tests @div-block', () => {
 		expect( true ).toEqual( false );
 	} );
 
-	test( 'fail', async () => {
-		expect( true ).toEqual( false );
-	} );
-
 	test( 'Sort items in a Div Block using DnD', async ( { page, apiRequests }, testInfo ) => {
 		const wpAdmin = new WpAdminPage( page, testInfo, apiRequests );
 		const editor = await wpAdmin.openNewPage(),
@@ -63,7 +59,7 @@ test.describe( 'Div Block tests @div-block', () => {
 		const sourceContainer = await editor.addElement( { elType: 'container' }, 'document' );
 		const heading = await editor.addWidget( { widgetType: widgets.heading, container: sourceContainer } );
 
-		const flexbox = await editor.addElement( { elType: 'e-flexbox' }, 'document' );
+		const flexbox = await editor.addElement( { elType: 'e-flexboxl' }, 'document' );
 		const divBlock = await editor.addElement( { elType: 'e-div-block' }, 'document' );
 
 		const testDragToEmptyContainer = async ( targetContainer: string ) => {
