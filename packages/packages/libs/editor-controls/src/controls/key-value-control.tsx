4f03e30164712a186eb774a466e3cb075358b22e
David Seveloff <davids@elementor.com>
2025-07-15 09:58:48 +0300

    Internal [V4]: Fix Dynamic tag floating button location in attributes fields  [ED-20026]

--- packages/packages/libs/editor-controls/src/controls/key-value-control.tsx
+++ packages/packages/libs/editor-controls/src/controls/key-value-control.tsx
@@ -105,21 +105,20 @@ export const KeyValueControl = createControl( ( props: KeyValueControlProps = {}
 	return (
 		<PropProvider { ...propContext } value={ value } setValue={ handleChange }>
 			<Grid container gap={ 1.5 }>
-				<Grid item xs={ 12 }>
-					<FormLabel size="tiny">{ keyLabel }</FormLabel>
+				<Grid item xs={ 12 } sx={ { display: 'flex', flexDirection: 'column' } }>
+					<FormLabel size="tiny" sx={ { pb: 1 } }>{ keyLabel }</FormLabel>
 					<PropKeyProvider bind={ 'key' }>
-						<TextControl inputValue={ sessionState.key } error={ !! keyError } sx={ { pt: 1 } } />
+						<TextControl inputValue={ sessionState.key } error={ !! keyError } />
 					</PropKeyProvider>
 					{ !! keyError && <FormHelperText error>{ keyError }</FormHelperText> }
 				</Grid>
-				<Grid item xs={ 12 }>
-					<FormLabel size="tiny">{ valueLabel }</FormLabel>
+				<Grid item xs={ 12 } sx={ { display: 'flex', flexDirection: 'column' } }>
+					<FormLabel size="tiny" sx={ { pb: 1 } }>{ valueLabel }</FormLabel>
 					<PropKeyProvider bind={ 'value' }>
 						<TextControl
 							inputValue={ sessionState.value }
 							error={ !! valueError }
 							inputDisabled={ !! keyError }
-							sx={ { pt: 1 } }
 						/>
 					</PropKeyProvider>
 					{ !! valueError && <FormHelperText error>{ valueError }</FormHelperText> }
