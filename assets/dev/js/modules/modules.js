a7184e100262df70f5b9b3fc89f6c1b69529f7d5
Louis Wolmarans <louiswol94@gmail.com>
2025-07-23 16:47:06 +0200

    wip

--- assets/dev/js/modules/modules.js
+++ assets/dev/js/modules/modules.js
@@ -4,7 +4,7 @@ import ArgsObject from './imports/args-object';
 import Masonry from './imports/utils/masonry';
 import Scroll from './imports/utils/scroll';
 import ForceMethodImplementation from './imports/force-method-implementation';
-import { templateRegistry } from '../../../../app/modules/import-export-customization/assets/js/shared/template-registry';
+import { templateRegistry } from '../../../../app/modules/import-export-customization/assets/js/shared/registry/templates';
 
 export default window.elementorModules = {
 	Module,
