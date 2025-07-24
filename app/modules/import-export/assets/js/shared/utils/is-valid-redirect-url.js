fb30b8fcb6a576a82a20da4239f3843ecf5c668a
Nicola Peluchetti <nicola.peluchetti@gmail.com>
2025-07-24 16:12:20 +0200

    Update app/modules/import-export/assets/js/shared/utils/is-valid-redirect-url.js
    Co-authored-by: gitstream-cm[bot] <111687743+gitstream-cm[bot]@users.noreply.github.com>

--- app/modules/import-export/assets/js/shared/utils/is-valid-redirect-url.js
+++ app/modules/import-export/assets/js/shared/utils/is-valid-redirect-url.js
@@ -1,8 +1,9 @@
 export default function isValidRedirectUrl( url ) {
-	try {
-		const parsedUrl = new URL( url );
-		return parsedUrl.hostname === window.location.hostname;
-	} catch ( e ) {
-		return false;
-	}
+        try {
+                const parsedUrl = new URL( url );
+                return parsedUrl.hostname === window.location.hostname && 
+                        ( parsedUrl.protocol === 'http:' || parsedUrl.protocol === 'https:' );
+        } catch ( e ) {
+                return false;
+        }
 }
