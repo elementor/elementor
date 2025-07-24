4cca301aefc48dd15bb1a80dc68776dad004775a
Davids Seveloff <94905340+davseve@users.noreply.github.com>
2025-07-24 10:28:33 +0000

    Revert "Internal: Fix Dynamic tag floating button location in attributes fields  [ED-20026] (#31867)"
    This reverts commit 1d683fa00fded55eb42cdf9d5772c47ae0c9f83f.

--- packages/packages/libs/editor-controls/src/controls/key-value-control.tsx
+++ packages/packages/libs/editor-controls/src/controls/key-value-control.tsx
@@ -105,24 +105,21 @@ export const KeyValueControl = createControl( ( props: KeyValueControlProps = {}
 	return (
 		<PropProvider { ...propContext } value={ value } setValue={ handleChange }>
 			<Grid container gap={ 1.5 }>
-				<Grid item xs={ 12 } display="flex" flexDirection="column">
-					<FormLabel size="tiny" sx={ { pb: 1 } }>
-						{ keyLabel }
-					</FormLabel>
+				<Grid item xs={ 12 }>
+					<FormLabel size="tiny">{ keyLabel }</FormLabel>
 					<PropKeyProvider bind={ 'key' }>
-						<TextControl inputValue={ sessionState.key } error={ !! keyError } />
+						<TextControl inputValue={ sessionState.key } error={ !! keyError } sx={ { pt: 1 } } />
 					</PropKeyProvider>
 					{ !! keyError && <FormHelperText error>{ keyError }</FormHelperText> }
 				</Grid>
-				<Grid item xs={ 12 } display="flex" flexDirection="column">
-					<FormLabel size="tiny" sx={ { pb: 1 } }>
-						{ valueLabel }
-					</FormLabel>
+				<Grid item xs={ 12 }>
+					<FormLabel size="tiny">{ valueLabel }</FormLabel>
 					<PropKeyProvider bind={ 'value' }>
 						<TextControl
 							inputValue={ sessionState.value }
 							error={ !! valueError }
 							inputDisabled={ !! keyError }
+							sx={ { pt: 1 } }
 						/>
 					</PropKeyProvider>
 					{ !! valueError && <FormHelperText error>{ valueError }</FormHelperText> }
