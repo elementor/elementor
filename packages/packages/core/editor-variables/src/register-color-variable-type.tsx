e780701238f91d9c69a1850d6d6cf7d7e6ee41ea
Batsirai Muchareva <batsiraim@elementor.red>
2025-07-25 16:35:54 +0200

    update test

--- packages/packages/core/editor-variables/src/register-color-variable-type.tsx
+++ packages/packages/core/editor-variables/src/register-color-variable-type.tsx
@@ -1,17 +1,15 @@
 import * as React from 'react';
 import { styleTransformersRegistry } from '@elementor/editor-canvas';
-import { registerControlReplacement, stylesInheritanceTransformersRegistry } from '@elementor/editor-editing-panel';
+import { stylesInheritanceTransformersRegistry } from '@elementor/editor-editing-panel';
 import { colorPropTypeUtil } from '@elementor/editor-props';
 import { BrushIcon } from '@elementor/icons';
 
 import { ColorField } from './components/fields/color-field';
 import { ColorIndicator } from './components/ui/color-indicator';
-import { ColorVariableControl } from './controls/color-variable-control';
 import { colorVariablePropTypeUtil } from './prop-types/color-variable-prop-type';
 import { registerRepeaterInjections } from './repeater-injections';
 import { inheritanceTransformer } from './transformers/inheritance-transformer';
 import { variableTransformer } from './transformers/variable-transformer';
-import { hasAssignedColorVariable } from './utils';
 import { registerVariableType } from './variables-registry/variable-type-registry';
 
 export function registerColorVariableType() {
@@ -22,11 +20,6 @@ export function registerColorVariableType() {
 		fallbackPropTypeUtil: colorPropTypeUtil,
 		variableType: 'color',
 		startIcon: ( { value } ) => <ColorIndicator size="inherit" component="span" value={ value } />,
-	} );
-
-	registerControlReplacement( {
-		component: ColorVariableControl,
-		condition: ( { value } ) => hasAssignedColorVariable( value ),
 	} );
 
 	styleTransformersRegistry.register( colorVariablePropTypeUtil.key, variableTransformer );
--- packages/packages/core/editor-variables/src/register-font-variable-type.tsx
+++ packages/packages/core/editor-variables/src/register-font-variable-type.tsx
@@ -1,14 +1,12 @@
 import { styleTransformersRegistry } from '@elementor/editor-canvas';
-import { registerControlReplacement, stylesInheritanceTransformersRegistry } from '@elementor/editor-editing-panel';
+import { stylesInheritanceTransformersRegistry } from '@elementor/editor-editing-panel';
 import { stringPropTypeUtil } from '@elementor/editor-props';
 import { TextIcon } from '@elementor/icons';
 
 import { FontField } from './components/fields/font-field';
-import { FontVariableControl } from './controls/font-variable-control';
 import { fontVariablePropTypeUtil } from './prop-types/font-variable-prop-type';
 import { inheritanceTransformer } from './transformers/inheritance-transformer';
 import { variableTransformer } from './transformers/variable-transformer';
-import { hasAssignedFontVariable } from './utils';
 import { registerVariableType } from './variables-registry/variable-type-registry';
 
 export function registerFontVariableType() {
@@ -18,11 +16,6 @@ export function registerFontVariableType() {
 		propTypeUtil: fontVariablePropTypeUtil,
 		fallbackPropTypeUtil: stringPropTypeUtil,
 		variableType: 'font',
-	} );
-
-	registerControlReplacement( {
-		component: FontVariableControl,
-		condition: ( { value } ) => hasAssignedFontVariable( value ),
 	} );
 
 	styleTransformersRegistry.register( fontVariablePropTypeUtil.key, variableTransformer );
