aeec21d08f091974f2d4b3a7ee0748ccf31cb303
Batsirai Muchareva <batsiraim@elementor.red>
2025-07-26 07:20:43 +0200

    linting

--- packages/packages/core/editor-variables/src/index.ts
+++ packages/packages/core/editor-variables/src/index.ts
@@ -1,2 +1,2 @@
 export { init } from './init';
-export { registerVariableType } from './variables-registry/variable-type-registry'
+export { registerVariableType } from './variables-registry/variable-type-registry';
