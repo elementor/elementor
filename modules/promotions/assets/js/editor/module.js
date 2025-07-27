8f179c189b354b31c38320fc79f8079b9ecd62d0
Ohad Raz <ohad@elementor.com>
2025-07-27 22:08:57 +0700

    js lint

--- modules/promotions/assets/js/editor/module.js
+++ modules/promotions/assets/js/editor/module.js
@@ -11,7 +11,7 @@ export default class Module extends elementorModules.editor.utils.Module {
 		elementor.hooks.addFilter( 'element/view', function( DefaultView, model ) {
 			const widgetType = model.get( 'widgetType' );
 			const isProWidget = elementor.config.promotionWidgets.find( ( item ) => widgetType === item.name );
-			
+
 			if ( isProWidget ) {
 				return require( './widget/view' ).default;
 			}
