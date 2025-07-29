0eeb6813770642764666fbf9f5e13e20bf1f191d
Marcin Skowronek <marcins@elementor.red>
2025-07-29 16:14:23 +0200

    wip

--- packages/jest.config.js
+++ packages/jest.config.js
@@ -5,6 +5,10 @@ module.exports = {
 	transform: {
 		'^.+\\.(t|j)sx?$': '@swc/jest',
 	},
+	transformIgnorePatterns: [
+		'node_modules/(?!(constrained-editor-plugin)/)',
+	],
+	extensionsToTreatAsEsm: ['.ts', '.tsx'],
 	moduleNameMapper: {
 		'^@elementor/(?!ui|icons|design-tokens)(.*)$': [
 			'<rootDir>/packages/core/$1/src',
