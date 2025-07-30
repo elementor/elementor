25a63e9263cfc7a254ced2e4dd0ff2d3642cb514
Batsirai Muchareva <batsiraim@elementor.red>
2025-07-30 13:57:53 +0200

    safe guard validation

--- modules/variables/classes/variable-types-registry.php
+++ modules/variables/classes/variable-types-registry.php
@@ -3,6 +3,7 @@
 namespace Elementor\Modules\Variables\Classes;
 
 use Elementor\Modules\AtomicWidgets\PropTypes\Contracts\Transformable_Prop_Type;
+use InvalidArgumentException;
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
@@ -12,6 +13,10 @@ class Variable_Types_Registry {
 	private array $types;
 
 	public function register( string $key, Transformable_Prop_Type $prop_type ): void {
+		if ( isset( $this->types[ $key ] ) ) {
+			throw new InvalidArgumentException( "Key '{$key}' is already registered." );
+		}
+
 		$this->types[ $key ] = $prop_type;
 	}
 
