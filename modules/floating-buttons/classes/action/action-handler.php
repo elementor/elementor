49fb08b38740667b2c1b20a5a1ff4995f7a9b49e
Nicola Peluchetti <nicola.peluchetti@gmail.com>
2025-07-30 14:07:26 +0200

    refactor code

--- modules/floating-buttons/classes/action/action-handler.php
+++ modules/floating-buttons/classes/action/action-handler.php
@@ -6,86 +6,75 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 
+use Elementor\Modules\FloatingButtons\Classes\Conditions\Conditions_Cache;
 use Elementor\Modules\FloatingButtons\Documents\Floating_Buttons;
 use Elementor\Modules\FloatingButtons\Module;
 
 class Action_Handler {
 
-	const CONDITIONS_CACHE_META_KEY = 'elementor_pro_theme_builder_conditions';
-
 	protected string $action;
-
 	protected array $menu_args;
+	protected Conditions_Cache $conditions_cache;
 
 	public function __construct( string $action, array $menu_args ) {
-		$this->action    = $action;
+		$this->action = $action;
 		$this->menu_args = $menu_args;
+		$this->conditions_cache = new Conditions_Cache();
 	}
 
 	public function process_action() {
 		switch ( $this->action ) {
 			case 'remove_from_entire_site':
-				$post_id = filter_input( INPUT_GET, 'post', FILTER_VALIDATE_INT );
-				check_admin_referer( 'remove_from_entire_site_' . $post_id );
-				delete_post_meta( $post_id, '_elementor_conditions' );
-				$this->remove_from_conditions_cache( $post_id );
-
-				wp_redirect( $this->menu_args['menu_slug'] );
-				exit;
+				$this->handle_remove_from_entire_site();
+				break;
 			case 'set_as_entire_site':
-				$post_id = filter_input( INPUT_GET, 'post', FILTER_VALIDATE_INT );
-				check_admin_referer( 'set_as_entire_site_' . $post_id );
-
-				$posts = get_posts( [
-					'post_type'              => Module::CPT_FLOATING_BUTTONS,
-					'posts_per_page'         => - 1,
-					'post_status'            => 'publish',
-					'fields'                 => 'ids',
-					'no_found_rows'          => true,
-					'update_post_term_cache' => false,
-					'update_post_meta_cache' => false,
-					'meta_query'             => Floating_Buttons::get_meta_query_for_floating_buttons(
-						Floating_Buttons::get_floating_element_type( $post_id )
-					),
-				] );
-
-				foreach ( $posts as $post_id_to_delete ) {
-					delete_post_meta( $post_id_to_delete, '_elementor_conditions' );
-					$this->remove_from_conditions_cache( $post_id_to_delete );
-				}
-
-				update_post_meta( $post_id, '_elementor_conditions', [ 'include/general' ] );
-				$this->add_to_conditions_cache( $post_id );
-
-				wp_redirect( $this->menu_args['menu_slug'] );
-				exit;
+				$this->handle_set_as_entire_site();
+				break;
 			default:
 				break;
 		}
 	}
 
-	private function remove_from_conditions_cache( $post_id ): void {
-		$conditions = get_option( self::CONDITIONS_CACHE_META_KEY, [] );
+	private function handle_remove_from_entire_site(): void {
+		$post_id = filter_input( INPUT_GET, 'post', FILTER_VALIDATE_INT );
+		check_admin_referer( 'remove_from_entire_site_' . $post_id );
+		delete_post_meta( $post_id, '_elementor_conditions' );
+		$this->conditions_cache->remove_from_cache( $post_id );
 
-		if ( isset( $conditions['floating_buttons'][ $post_id ] ) ) {
-			unset( $conditions['floating_buttons'][ $post_id ] );
-
-			if ( empty( $conditions['floating_buttons'] ) ) {
-				unset( $conditions['floating_buttons'] );
-			}
-
-			update_option( self::CONDITIONS_CACHE_META_KEY, $conditions );
-		}
+		wp_redirect( $this->menu_args['menu_slug'] );
+		exit;
 	}
 
-	private function add_to_conditions_cache( $post_id ): void {
-		$conditions = get_option( self::CONDITIONS_CACHE_META_KEY, [] );
-		if ( ! isset( $conditions['floating_buttons'] ) ) {
-			$conditions['floating_buttons'] = [];
+	private function handle_set_as_entire_site(): void {
+		$post_id = filter_input( INPUT_GET, 'post', FILTER_VALIDATE_INT );
+		check_admin_referer( 'set_as_entire_site_' . $post_id );
+
+		$posts = $this->get_published_floating_elements( $post_id );
+
+		foreach ( $posts as $post_id_to_delete ) {
+			delete_post_meta( $post_id_to_delete, '_elementor_conditions' );
+			$this->conditions_cache->remove_from_cache( $post_id_to_delete );
 		}
 
-		$conditions['floating_buttons'][ $post_id ] = [ 'include/general' ];
+		update_post_meta( $post_id, '_elementor_conditions', [ 'include/general' ] );
+		$this->conditions_cache->add_to_cache( $post_id );
 
-		update_option( self::CONDITIONS_CACHE_META_KEY, $conditions );
+		wp_redirect( $this->menu_args['menu_slug'] );
+		exit;
+	}
+
+	private function get_published_floating_elements( int $post_id ): array {
+		return get_posts( [
+			'post_type'              => Module::CPT_FLOATING_BUTTONS,
+			'posts_per_page'         => -1,
+			'post_status'            => 'publish',
+			'fields'                 => 'ids',
+			'no_found_rows'          => true,
+			'update_post_term_cache' => false,
+			'update_post_meta_cache' => false,
+			'meta_query'             => Floating_Buttons::get_meta_query_for_floating_buttons(
+				Floating_Buttons::get_floating_element_type( $post_id )
+			),
+		] );
 	}
 }
