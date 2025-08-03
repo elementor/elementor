6dbac08bb9d6a68d9c8d3480ada88de12f8738ca
asafdl <asafd@elementor.com>
2025-08-03 14:47:06 +0300

    merge from master

--- packages/packages/core/editor-app-bar/src/extensions/connect/hooks/use-connect-link-config.tsx
+++ packages/packages/core/editor-app-bar/src/extensions/connect/hooks/use-connect-link-config.tsx
@@ -5,18 +5,18 @@ import { type ExtendedWindow } from '../../../types';
 
 export default function useConnectLinkConfig() {
 	const extendedWindow = window as unknown as ExtendedWindow;
-	const isUserConnected = extendedWindow?.elementor?.config.library_connect.is_connected ?? false;
+	const isUserConnected = extendedWindow?.elementorCommon?.config.library_connect.is_connected ?? false;
 
 	return isUserConnected
 		? {
 				title: __( 'My Elementor', 'elementor' ),
-				href: extendedWindow?.elementor?.config.user.top_bar.my_elementor_url,
+				href: extendedWindow?.elementorCommon?.config.user.top_bar.my_elementor_url,
 				icon: UserIcon,
 				target: '_blank',
 		  }
 		: {
 				title: __( 'Connect my account', 'elementor' ),
-				href: extendedWindow?.elementor?.config.user.top_bar.connect_url,
+				href: extendedWindow?.elementorCommon?.config.user.top_bar.connect_url,
 				icon: UserIcon,
 				target: '_blank',
 		  };
