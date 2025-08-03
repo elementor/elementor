1a29a316e4e93056a7d1629ce5750832cb8434c8
Robert Ferentz <robertf@elementor.red>
2025-08-03 10:10:42 +0000

    Revert "Fix: Duration field not showing for most transition properties [ED-20285] (#32103)"
    This reverts commit 41fa2f66fa1cc07dc9002c4f14385c6b0b49cbf2.

--- packages/packages/libs/editor-controls/src/controls/transition-control/transition-repeater-control.tsx
+++ packages/packages/libs/editor-controls/src/controls/transition-control/transition-repeater-control.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import { selectionSizePropTypeUtil } from '@elementor/editor-props';
-import { __ } from '@wordpress/i18n';
 
 import { createControl } from '../../create-control';
 import { RepeatableControl } from '../repeatable-control';
@@ -14,49 +13,43 @@ const DURATION_CONFIG = {
 	defaultUnit: 'ms',
 };
 
-// this config needs to be loaded at runtime/render since it's the transitionProperties object will be mutated by the pro plugin.
-// See: https://elementor.atlassian.net/browse/ED-20285
-const getSelectionSizeProps = () => {
-	return {
-		selectionLabel: __( 'Type', 'elementor' ),
-		sizeLabel: __( 'Duration', 'elementor' ),
-		selectionConfig: {
-			component: TransitionSelector,
-			props: {},
-		},
-		sizeConfigMap: {
-			...transitionProperties.reduce(
-				( acc, category ) => {
-					category.properties.forEach( ( property ) => {
-						acc[ property.value ] = DURATION_CONFIG;
-					} );
-					return acc;
-				},
-				{} as Record< string, typeof DURATION_CONFIG >
-			),
-		},
-	};
+const SELECTION_SIZE_PROPS = {
+	selectionLabel: 'Type',
+	sizeLabel: 'Duration',
+	selectionConfig: {
+		component: TransitionSelector,
+		props: {},
+	},
+	sizeConfigMap: {
+		...transitionProperties.reduce(
+			( acc, category ) => {
+				category.properties.forEach( ( property ) => {
+					acc[ property.value ] = DURATION_CONFIG;
+				} );
+				return acc;
+			},
+			{} as Record< string, typeof DURATION_CONFIG >
+		),
+	},
 };
 
-function getChildControlConfig() {
-	return {
-		propTypeUtil: selectionSizePropTypeUtil,
-		component: SelectionSizeControl as unknown as React.ComponentType< Record< string, unknown > >,
-		props: getSelectionSizeProps(),
-	};
-}
+const CHILD_CONTROL_CONFIG = {
+	propTypeUtil: selectionSizePropTypeUtil,
+	component: SelectionSizeControl as unknown as React.ComponentType< Record< string, unknown > >,
+	props: SELECTION_SIZE_PROPS,
+};
 
 export const TransitionRepeaterControl = createControl( () => {
 	return (
 		<RepeatableControl
-			label={ __( 'Transitions', 'elementor' ) }
-			repeaterLabel={ __( 'Transitions', 'elementor' ) }
+			label="Transitions"
+			repeaterLabel="Transitions"
 			patternLabel="${value.selection.value.key.value}: ${value.size.value.size}${value.size.value.unit}"
-			placeholder={ __( 'Empty Transition', 'elementor' ) }
+			placeholder="Empty Transition"
 			showDuplicate={ false }
 			showToggle={ true }
 			initialValues={ initialTransitionValue }
-			childControlConfig={ getChildControlConfig() }
+			childControlConfig={ CHILD_CONTROL_CONFIG }
 			propKey="transition"
 		/>
 	);
