0821a8f77793b3fc4b601c851ba93fb7e848d3e6
Batsirai Muchareva <batsiraim@elementor.red>
2025-08-05 21:08:32 +0200

    wip

--- packages/packages/core/editor-variables/src/components/ui/form-field.tsx
+++ packages/packages/core/editor-variables/src/components/ui/form-field.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import type { PropsWithChildren } from 'react';
 import { FormHelperText, FormLabel, Grid } from '@elementor/ui';
-import { __ } from '@wordpress/i18n';
 
 type Props = PropsWithChildren< {
 	id?: string;
@@ -10,12 +9,12 @@ type Props = PropsWithChildren< {
 	noticeMsg?: string;
 } >;
 
-export const FormField = ( { id, errorMsg, noticeMsg, children }: Props ) => {
+export const FormField = ( { id, label, errorMsg, noticeMsg, children }: Props ) => {
 	return (
 		<Grid container gap={ 0.75 } alignItems="center">
 			<Grid item xs={ 12 }>
 				<FormLabel htmlFor={ id } size="tiny">
-					{ __( 'Name', 'elementor' ) }
+					{ label }
 				</FormLabel>
 			</Grid>
 
