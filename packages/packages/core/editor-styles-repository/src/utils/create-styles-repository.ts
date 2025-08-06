4efec36475ec5b09bf067cc962e1bd42a1ff2e45
Omer Haim Israeli <omeri@elementor.com>
2025-08-06 14:56:36 +0300

    wip

--- packages/packages/core/editor-styles-repository/src/utils/create-styles-repository.ts
+++ packages/packages/core/editor-styles-repository/src/utils/create-styles-repository.ts
@@ -2,8 +2,6 @@ import { type Meta, type StylesCollection, type StylesProvider } from '../types'
 
 export const createStylesRepository = () => {
 	const providers: StylesProvider[] = [];
-	const subscribers: Array< ( previous?: StylesCollection, current?: StylesCollection ) => void > = [];
-	const unsubscribes: Array< () => void > = [];
 
 	const getProviders = () => {
 		return providers.slice( 0 ).sort( ( a, b ) => ( a.priority > b.priority ? -1 : 1 ) );
@@ -11,10 +9,6 @@ export const createStylesRepository = () => {
 
 	const register = ( provider: StylesProvider ) => {
 		providers.push( provider );
-
-		subscribers.forEach( ( subscriber ) => {
-			unsubscribes.push( provider.subscribe( subscriber ) );
-		} );
 	};
 
 	const all = ( meta: Meta = {} ) => {
@@ -22,10 +16,8 @@ export const createStylesRepository = () => {
 	};
 
 	const subscribe = ( cb: ( previous?: StylesCollection, current?: StylesCollection ) => void ) => {
-		subscribers.push( cb );
-
-		providers.forEach( ( provider ) => {
-			unsubscribes.push( provider.subscribe( cb ) );
+		const unsubscribes = providers.map( ( provider ) => {
+			return provider.subscribe( cb );
 		} );
 
 		return () => {
