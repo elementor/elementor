19b659f96675dc33c76f73d8b17bd133434ba356
Louis Wolmarans <louiswol94@gmail.com>
2025-08-09 09:29:20 +0200

    Update cloud-library.php

--- modules/cloud-library/connect/cloud-library.php
+++ modules/cloud-library/connect/cloud-library.php
@@ -336,6 +336,10 @@ class Cloud_Library extends Library {
 	 * @return array|\WP_Error
 	 */
 	public function get_quota() {
+		if ( ! $this->is_connected() ) {
+			return false;
+		}
+
 		return $this->http_request( 'GET', 'quota', [], [
 			'return_type' => static::HTTP_RETURN_TYPE_ARRAY,
 		] );
