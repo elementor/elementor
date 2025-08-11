d1f5c020dd4955e9b6b35678b1d930eed682644b
Batsirai Muchareva <batsiraim@elementor.red>
2025-08-11 09:43:15 +0200

    wip

--- packages/packages/libs/editor-props/src/prop-types/filter-prop-types/filter.ts
+++ packages/packages/libs/editor-props/src/prop-types/filter-prop-types/filter.ts
@@ -1,16 +1,14 @@
 import { z } from '@elementor/schema';
 
 import { createPropUtils } from '../../utils/create-prop-utils';
-import { sizePropTypeUtil } from '../size';
 import { stringPropTypeUtil } from '../string';
-import { dropShadowFilterPropTypeUtil } from './drop-shadow-filter';
 import { unknownChildrenSchema } from '../utils';
 
 export const cssFilterFunctionPropUtil = createPropUtils(
 	'css-filter-func',
 	z.object( {
 		func: stringPropTypeUtil.schema,
-		args: z.union( [ sizePropTypeUtil.schema, dropShadowFilterPropTypeUtil.schema, unknownChildrenSchema ] ),
+		args: unknownChildrenSchema,
 	} )
 );
 
