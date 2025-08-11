3737043b3f27de8fad35434cc88007d37930198c
Omer Haim Israeli <omeri@elementor.com>
2025-08-11 10:56:27 +0300

    wip

--- modules/atomic-widgets/assets/js/editor/module.js
+++ modules/atomic-widgets/assets/js/editor/module.js
@@ -1,7 +1,7 @@
 import Component from './component';
 import EmptyComponent from 'elementor-elements/views/container/empty-component';
 import Model from './atomic-element-model';
-import createAtomicElementView from './atomic-element-view';
+import { createAtomicElementView } from './atomic-element-view';
 
 class DynamicAtomicElementType extends elementor.modules.elements.types.Base {
 	constructor( elementType, view ) {
