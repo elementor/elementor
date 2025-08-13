45693519e5fe9283f975451d4beeea2912f4f6a0
Netanel Baba <ntnelbaba@gmail.com>
2025-08-13 16:50:10 +0300

    Feat: Editable cell for variables manager [ED-20428]

---
+++ packages/packages/core/editor-variables/src/components/variables-manager/variable-editable-cell.tsx
@@ -0,0 +1,59 @@
+import * as React from 'react';
+import { useState } from 'react';
+import { Stack } from '@elementor/ui';
+
+import { type ValueFieldProps } from '../../variables-registry/create-variable-type-registry';
+
+export const VariableEditableCell = ( {
+	initialValue,
+	children,
+	editableElement,
+	onSave,
+	prefixElement,
+}: {
+	initialValue: string;
+	children: React.ReactNode;
+	editableElement: ( { value, onChange, onValidationChange }: ValueFieldProps ) => JSX.Element;
+	onSave: ( newValue: string ) => void;
+	prefixElement?: React.ReactNode;
+} ) => {
+	const [ value, setValue ] = useState( initialValue );
+	const [ isEditing, setIsEditing ] = useState( false );
+
+	const handleDoubleClick = () => {
+		setIsEditing( true );
+	};
+
+	const handleSave = () => {
+		onSave( value );
+		setIsEditing( false );
+	};
+
+	const handleKeyDown = ( event: React.KeyboardEvent< HTMLDivElement > ) => {
+		if ( event.key === 'Enter' ) {
+			handleSave();
+		} else if ( event.key === 'Escape' ) {
+			setIsEditing( false );
+		}
+	};
+
+	const handleChange = ( newValue: string ) => {
+		setValue( newValue );
+	};
+
+	const editableContent = editableElement( { value, onChange: handleChange } );
+
+	return (
+		<Stack
+			direction="row"
+			alignItems="center"
+			gap={ 1 }
+			onDoubleClick={ handleDoubleClick }
+			onBlur={ handleSave }
+			onKeyDown={ handleKeyDown }
+		>
+			{ prefixElement }
+			{ isEditing ? editableContent : children }
+		</Stack>
+	);
+};
