fa9ed071b700e4143895b836bd078cc876f0d3a0
Mike Kokhanov <mikek@elementor.com>
2025-08-18 14:03:02 +0300

    [ wip ]: additional css color converters

--- tests/phpunit/elementor/modules/css-converter/test-variables-mapping.php
+++ tests/phpunit/elementor/modules/css-converter/test-variables-mapping.php
@@ -7,6 +7,9 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
+/**
+ * @group css-converter
+ */
 class Test_Variables_Mapping extends Elementor_Test_Base {
 
 	public function test_upserts_variables_into_repository() {
--- tests/phpunit/elementor/modules/css-converter/test-variables-service.php
+++ tests/phpunit/elementor/modules/css-converter/test-variables-service.php
@@ -6,6 +6,9 @@ use Elementor\Modules\CssConverter\Services\Variables_Service;
 use Elementor\Modules\CssConverter\Parsers\CssParser;
 use Elementor\Modules\CssConverter\Parsers\ParsedCss;
 
+/**
+ * @group css-converter
+ */
 class Test_Variables_Service extends Elementor_Test_Base {
 	public function test_variables_from_css_string__hex_color_is_converted_and_normalized() {
 		$mockParser = $this->createMock(CssParser::class);
@@ -22,7 +25,8 @@ class Test_Variables_Service extends Elementor_Test_Base {
 		$values = array_column( $result, 'value' );
 		$this->assertContains( '#eeeeee', $values );
 		$ids = array_column( $result, 'id' );
-		$this->assertContains( 'primary-color', $ids );
+		print_r( $ids );
+		$this->assertContains( 'e-gv-color-hex-primary-color-variable', $ids );
 	}
 
 	public function test_variables_from_css_string__non_hex_variables_are_skipped_in_mvp() {
@@ -37,5 +41,31 @@ class Test_Variables_Service extends Elementor_Test_Base {
 		$this->assertIsArray( $result );
 		$this->assertCount( 1, $result );
 		$this->assertSame( '#aabbcc', $result[0]['value'] );
+	}
+
+	public function test_variables_from_css_string__color_variables_variations() {
+		$mockParser = $this->createMock(CssParser::class);
+		$mockParsedCss = $this->createMock(ParsedCss::class);
+		$mockParser->method('parse')->willReturn($mockParsedCss);
+
+		$mockParser->method('extract_variables')->willReturn([
+			['name' => '--color-1', 'value' => '#aabbcc'],
+			['name' => '--color-2', 'value' => '#000'],
+			['name' => '--color-3', 'value' => '#000000f0'],
+			['name' => '--color-4', 'value' => 'rgb(0, 255, 0)'],
+			['name' => '--color-5', 'value' => 'rgba(0, 255, 0, 0.5)'],
+		]);
+
+		$service = new Variables_Service($mockParser);
+		$result = $service->variables_from_css_string(':root { /* ... any css ... */ }');
+
+		$this->assertIsArray( $result );
+
+		$this->assertSame( '#aabbcc', $result[0]['value'] );
+		$this->assertSame( '#000000', $result[1]['value'] );
+		$this->assertSame( '#000000f0', $result[2]['value'] );
+
+		$this->assertSame( 'rgb(0, 255, 0)', $result[3]['value'] );
+		$this->assertSame( 'rgba(0, 255, 0, 0.5)', $result[4]['value'] );
 	}
 }
