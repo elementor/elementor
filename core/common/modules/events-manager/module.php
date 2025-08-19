73420c137da14b7d97f817e78ca6597a29f95f85
Nikita Murzin <mykytamu@elementor.com>
2025-08-19 15:10:03 +0200

    Internal: Update experiment and check for tracking [ED-20377]

--- core/common/modules/events-manager/module.php
+++ core/common/modules/events-manager/module.php
@@ -16,14 +16,14 @@ if ( ! defined( 'ABSPATH' ) ) {
 class Module extends BaseModule {
 
 	const EXPERIMENT_NAME = 'editor_events';
+	const MIXPANEL_TOKEN = '';
 
 	public function get_name() {
 		return 'events-manager';
 	}
 
 	public static function get_editor_events_config() {
-		$can_send_events = defined( 'ELEMENTOR_EDITOR_EVENTS_MIXPANEL_TOKEN' ) &&
-			Tracker::is_allow_track() &&
+		$can_send_events = self::MIXPANEL_TOKEN && Tracker::is_allow_track() &&
 			! Tracker::has_terms_changed( '2025-07-07' ) &&
 			Plugin::$instance->experiments->is_feature_active( self::EXPERIMENT_NAME );
 
@@ -37,7 +37,7 @@ class Module extends BaseModule {
 			'site_key' => get_option( Base_App::OPTION_CONNECT_SITE_KEY ),
 			'subscription_id' => self::get_subscription_id(),
 			'subscription' => self::get_subscription(),
-			'token' => defined( 'ELEMENTOR_EDITOR_EVENTS_MIXPANEL_TOKEN' ) ? ELEMENTOR_EDITOR_EVENTS_MIXPANEL_TOKEN : '',
+			'token' => self::MIXPANEL_TOKEN,
 		];
 
 		return $settings;
@@ -51,6 +51,10 @@ class Module extends BaseModule {
 			'hidden' => true,
 			'release_status' => Experiments_Manager::RELEASE_STATUS_ALPHA,
 			'default' => Experiments_Manager::STATE_INACTIVE,
+			'new_site' => [
+				'default_active' => true,
+				'minimum_installation_version' => '3.32.0',
+			],
 		];
 	}
 
@@ -62,7 +66,6 @@ class Module extends BaseModule {
 
 	private static function get_subscription() {
 		if ( ! Utils::has_pro() ) {
-
 			return null;
 		}
 
