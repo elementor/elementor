b26f1409a90e3088859d9db6d7921625cf493367
IshayMaya <ishaym@elementor.com>
2025-08-20 14:23:33 +0300

    rename generate id function

--- packages/packages/libs/editor-elements/src/index.ts
+++ packages/packages/libs/editor-elements/src/index.ts
@@ -23,7 +23,7 @@ export { getCurrentDocumentId } from './sync/get-current-document-id';
 export { getSelectedElements } from './sync/get-selected-elements';
 export { getWidgetsCache } from './sync/get-widgets-cache';
 export { updateElementSettings, type UpdateElementSettingsArgs } from './sync/update-element-settings';
-export { createElementId } from './sync/create-element-id';
+export { generateElementId } from './sync/generate-element-id';
 
 export { ELEMENT_STYLE_CHANGE_EVENT, styleRerenderEvents } from './styles/consts';
 export {
--- packages/packages/libs/editor-elements/src/sync/create-element-id.ts
+++
@@ -1,7 +0,0 @@
-import { type ExtendedWindow } from './types';
-
-export const createElementId = () => {
-	const extendedWindow = window as unknown as ExtendedWindow;
-
-	return extendedWindow.elementorCommon?.helpers?.getUniqueId?.() ?? `el-${ Date.now() }`;
-};
