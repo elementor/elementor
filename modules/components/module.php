00d32902cf1b667a6ea6b46541fd4524f71eb63f
Miryam Oren <77922014+MiryamOren@users.noreply.github.com>
2025-08-20 14:14:16 +0300

    component doc

--- modules/components/module.php
+++ modules/components/module.php
@@ -4,6 +4,7 @@ namespace Elementor\Modules\Components;
 use Elementor\Core\Base\Module as BaseModule;
 use Elementor\Core\Experiments\Manager as Experiments_Manager;
 use Elementor\Modules\Components\Styles\Component_Styles;
+use Elementor\Modules\Components\Documents\Component as Component_Document;
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
@@ -20,7 +21,11 @@ class Module extends BaseModule {
 	public function __construct() {
 		parent::__construct();
 
-		$this->register_hooks();
+		add_filter( 'elementor/editor/v2/packages', fn ( $packages ) => $this->add_packages( $packages ) );
+		add_action('elementor/documents/register', fn ( $documents_manager ) => $this->register_document_type( $documents_manager ) );
+
+		( new Component_Styles() )->register_hooks();
+		( new Components_REST_API() )->register_hooks();
 	}
 
 	public static function get_experimental_data() {
@@ -34,13 +39,21 @@ class Module extends BaseModule {
 		];
 	}
 
-	private function register_hooks() {
-		add_filter( 'elementor/editor/v2/packages', fn ( $packages ) => $this->add_packages( $packages ) );
-
-		( new Component_Styles() )->register_hooks();
-	}
-
 	private function add_packages( $packages ) {
 		return array_merge( $packages, self::PACKAGES );
+	}
+
+	private function register_document_type( $documents_manager ) {
+		$documents_manager->register_document_type(
+			Component_Document::TYPE,
+			Component_Document::get_class_full_name()
+		);
+
+		register_post_type( Component_Document::TYPE, [
+			'label'                 => Component_Document::get_title(),
+            'labels' => Component_Document::get_labels(),
+            'public' => false,
+            'supports' => Component_Document::get_supported_features(),
+        ] );
 	}
 }
