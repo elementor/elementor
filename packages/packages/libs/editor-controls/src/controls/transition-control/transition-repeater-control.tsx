4f2b7b4f9918698527ecc4637c19d7b440e4ee47
Robert Ferentz <robertf@elementor.red>
2025-08-20 13:31:12 +0300

    Fix: [V4] Changed recently use logic to use prop-resolver in transition selector [ED-20468] (#32339)

--- packages/packages/libs/editor-controls/src/controls/transition-control/transition-repeater-control.tsx
+++ packages/packages/libs/editor-controls/src/controls/transition-control/transition-repeater-control.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { useEffect, useState } from 'react';
 import { selectionSizePropTypeUtil } from '@elementor/editor-props';
 import { type StyleDefinitionState } from '@elementor/editor-styles';
 import { InfoCircleFilledIcon } from '@elementor/icons';
@@ -71,13 +72,19 @@ const disableAddItemTooltipContent = (
 
 export const TransitionRepeaterControl = createControl(
 	( {
-		recentlyUsedList,
+		recentlyUsedListGetter,
 		currentStyleState,
 	}: {
-		recentlyUsedList: string[];
+		recentlyUsedListGetter: () => Promise< string[] >;
 		currentStyleState: StyleDefinitionState;
 	} ) => {
 		const currentStyleIsNormal = currentStyleState === null;
+		const [ recentlyUsedList, setRecentlyUsedList ] = useState< string[] >( [] );
+
+		useEffect( () => {
+			recentlyUsedListGetter().then( setRecentlyUsedList );
+		}, [ recentlyUsedListGetter ] );
+
 		return (
 			<RepeatableControl
 				label={ __( 'Transitions', 'elementor' ) }
