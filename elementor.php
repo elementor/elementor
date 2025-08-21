7bf528ef3dbf41da142c068424604e2a3c54d7cf
Michael Liamin <144707177+MichaelLiamin@users.noreply.github.com>
2025-08-21 07:24:13 +0000

    Revert "Internal: Update WP min version on main [ED-20509] (#32364)"
    This reverts commit 9a8e1a84fede5224fd488191617b52782c68b35c.

--- elementor.php
+++ elementor.php
@@ -6,9 +6,9 @@
  * Version: 3.32.0
  * Author: Elementor.com
  * Author URI: https://elementor.com/?utm_source=wp-plugins&utm_campaign=author-uri&utm_medium=wp-dash
- * Requires PHP: 7.4
- * Requires at least: 6.6
  * Text Domain: elementor
+ * Requires at least: 6.5
+ * Requires PHP: 7.4
  *
  * @package Elementor
  * @category Core
