60a627aee6daa9e7b6fb95a31a9b60ed19906902
Avichay Eyal <avichaye@elementor.red>
2025-08-21 19:34:55 +0300

    demo tool

--- packages/packages/core/editor-mcp/src/init.ts
+++ packages/packages/core/editor-mcp/src/init.ts
@@ -1,3 +1,38 @@
+import { z } from 'zod';
+import { AngieMcpSdk } from '@elementor-external/angie-sdk';
+import { McpServer } from '@modelcontextprotocol/sdk/server/mcp.js';
+
 export async function init() {
-	console.log( 'Initializing Editor MCP...' );
+	// AngieMcpSdk;
+	// McpServer;
+	const mcpServer = new McpServer( {
+		name: 'elementor-editor',
+		version: '1.0.0',
+	} );
+	const sdk = new AngieMcpSdk();
+
+	mcpServer.tool(
+		'greet',
+		'Create a nice greeting message for anyone you like',
+		{
+			name: z.string().nonempty().describe( 'Name of the person to greet' ),
+		},
+		async ( { name }: { name: string } ) => {
+			return {
+				structuredContent: {
+					metadata: `Hello, ${ name }! Welcome to Elementor pro!`,
+					message: 'No message provided',
+				},
+				content: [ { type: 'text', text: 'Here is the data (JSON Formatted structured content)' } ],
+			};
+		}
+	);
+
+	await sdk.registerServer( {
+		name: 'editor',
+		version: '1.0.0',
+		description: 'Elementor tools for editing website pages',
+		server: mcpServer,
+	} );
+	// console.log( 'Initializing Editor MCP...' );
 }
