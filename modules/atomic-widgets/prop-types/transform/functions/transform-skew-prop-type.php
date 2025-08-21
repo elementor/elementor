53720a75003095443f08062395ecf4beeecdb2a6
Ronen Boxer <ronenb@elementor.com>
2025-08-21 15:50:43 +0300

    wip

--- modules/atomic-widgets/prop-types/transform/functions/transform-skew-prop-type.php
+++ modules/atomic-widgets/prop-types/transform/functions/transform-skew-prop-type.php
@@ -7,21 +7,25 @@ use Elementor\Modules\AtomicWidgets\PropTypes\Traits\Dimensional_Prop_Type;
 use Elementor\Modules\AtomicWidgets\Styles\Size_Constants;
 
 if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly.
+    exit; // Exit if accessed directly.
 }
 
 class Transform_Skew_Prop_Type extends Object_Prop_Type {
-	use Dimensional_Prop_Type;
+    use Dimensional_Prop_Type;
 
-	public static function get_key(): string {
-		return 'transform-skew';
-	}
+    public static function get_key(): string {
+        return 'transform-skew';
+    }
 
-	protected function units(): ?array {
-		return Size_Constants::rotate();
-	}
+    protected function units(): ?array {
+        return Size_Constants::rotate();
+    }
 
-	protected function get_default_value_unit(): string {
-		return Size_Constants::UNIT_ANGLE_DEG;
-	}
+    protected function get_default_value_unit(): string {
+        return Size_Constants::UNIT_ANGLE_DEG;
+    }
+
+    protected function get_dimensions(): array {
+        return [ 'x', 'y' ];
+    }
 }
