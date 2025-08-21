e7e04e19ea6ce7e5270ed2e8eb372a2fb492c2b8
asafdl <asafd@elementor.com>
2025-08-21 15:26:58 +0300

    wip

--- packages/packages/core/editor-editing-panel/src/components/custom-css.tsx
+++ packages/packages/core/editor-editing-panel/src/components/custom-css.tsx
@@ -1,7 +1,9 @@
 import * as React from 'react';
 import { CssEditor } from '@elementor/editor-controls';
+import { __ } from '@wordpress/i18n';
 
 import { useCustomCss } from '../hooks/use-custom-css';
+import { ControlLabel } from './control-label';
 import { SectionContent } from './section-content';
 
 export const CustomCss = () => {
@@ -13,6 +15,7 @@ export const CustomCss = () => {
 
 	return (
 		<SectionContent>
+			<ControlLabel>{ __( 'Custom CSS code editor', 'elementor' ) }</ControlLabel>
 			<CssEditor value={ customCss?.raw || '' } onChange={ handleChange } />
 		</SectionContent>
 	);
