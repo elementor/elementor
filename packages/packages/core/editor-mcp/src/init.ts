b8e6a78cba09932794ee9d9bf29c9aa67466ec9a
Avichay Eyal <avichaye@elementor.red>
2025-08-24 18:24:22 +0300

    refactor to dynamically add mcp tools

--- packages/packages/core/editor-mcp/src/init.ts
+++ packages/packages/core/editor-mcp/src/init.ts
@@ -1,38 +1,58 @@
-import { z } from 'zod';
+import { type z, type ZodRawShape } from 'zod';
 import { AngieMcpSdk } from '@elementor-external/angie-sdk';
 import { McpServer } from '@modelcontextprotocol/sdk/server/mcp.js';
 
+const sdk = new AngieMcpSdk();
+
+const toolRegistrations: ( ( mcpServer: McpServer ) => void )[] = [];
+
+type ToolRegistrationOptions< InputArgs extends ZodRawShape > = {
+	name: string;
+	description: string;
+	schema?: InputArgs;
+	handler: ( params: z.objectOutputType< InputArgs, z.ZodTypeAny > ) => unknown | Promise< unknown >;
+};
+
+export function addTool< T extends ZodRawShape >( opts: ToolRegistrationOptions< T > ) {
+	const register = ( mcpServer: McpServer ) =>
+		mcpServer.tool( opts.name, opts.description, opts.schema || {}, async ( args ) => {
+			return {
+				content: [
+					{
+						type: 'text',
+						text: JSON.stringify( await opts.handler( args as T ) ),
+					},
+				],
+			};
+		} );
+	toolRegistrations.push( register );
+}
+
 export async function init() {
-	// AngieMcpSdk;
-	// McpServer;
+	// await ready();
+}
+
+export async function startMCPServer() {
+	await sdk.waitForReady();
 	const mcpServer = new McpServer( {
-		name: 'elementor-editor',
+		name: 'v4-editor-server',
 		version: '1.0.0',
 	} );
-	const sdk = new AngieMcpSdk();
-
-	mcpServer.tool(
-		'greet',
-		'Create a nice greeting message for anyone you like',
-		{
-			name: z.string().nonempty().describe( 'Name of the person to greet' ),
-		},
-		async ( { name }: { name: string } ) => {
-			return {
-				structuredContent: {
-					metadata: `Hello, ${ name }! Welcome to Elementor pro!`,
-					message: 'No message provided',
-				},
-				content: [ { type: 'text', text: 'Here is the data (JSON Formatted structured content)' } ],
-			};
-		}
-	);
-
+	toolRegistrations.forEach( ( register ) => register( mcpServer ) );
 	await sdk.registerServer( {
-		name: 'editor',
+		name: 'page-editor',
 		version: '1.0.0',
-		description: 'Elementor tools for editing website pages',
+		description: 'Elementor tools server for editing elementor website pages',
 		server: mcpServer,
 	} );
-	// console.log( 'Initializing Editor MCP...' );
 }
+
+document.addEventListener(
+	'DOMContentLoaded',
+	() => {
+		startMCPServer();
+	},
+	{
+		once: true,
+	}
+);
