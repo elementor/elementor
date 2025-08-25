c3427c37d32117639e79b70212995dc641ddadc9
Batsirai Muchareva <batsiraim@elementor.red>
2025-08-25 13:28:02 +0200

    wip

---
+++ packages/packages/libs/editor-controls/src/controls/filter-control/filter-icon.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import type { FilterItemPropValue } from '@elementor/editor-props';
+import { styled, UnstableColorIndicator } from '@elementor/ui';
+
+export const FilterIcon = ( { value }: { value: FilterItemPropValue } ) => {
+	if ( value.value.func.value !== 'drop-shadow' ) {
+		return null;
+	}
+
+	return (
+		<StyledUnstableColorIndicator size="inherit" component="span" value={ value.value.args.value.color.value } />
+	);
+};
+
+const StyledUnstableColorIndicator = styled( UnstableColorIndicator )( ( { theme } ) => ( {
+	borderRadius: `${ theme.shape.borderRadius / 2 }px`,
+} ) );
