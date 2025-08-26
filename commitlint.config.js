14ece02890a3784643210cfc9fb1ac9d4eb666a8
Netanel Baba <ntnelbaba@gmail.com>
2025-08-26 14:36:50 +0300

    Add our own plugin

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,14 +1,21 @@
 module.exports = {
 	extends: [ '@commitlint/config-conventional' ],
+	plugins: [ {
+		rules: {
+			'header-max-length-with-cherry-pick': ( { header, subject } ) => {
+				const limit = subject?.includes( 'Cherry-pick' ) ? 110 : 100;
+				return [
+					header.length <= limit,
+					`header must not be longer than ${ limit } characters, current length is ${ header.length }`,
+				];
+			},
+		},
+	} ],
 	rules: {
 		'subject-case': [ 2, 'always', 'sentence-case' ],
 		'type-case': [ 2, 'always', 'sentence-case' ],
 		'type-enum': [ 2, 'always', [ 'Feature', 'CI', 'New', 'Tweak', 'Fix', 'Experiment', 'Deprecate', 'Deprecated', 'Revert', 'Internal' ] ],
-		'header-max-length': [ 2, 'always', ( parsed ) => {
-			const { subject } = parsed;
-			// Increase limit to 150 characters for Cherry-pick PRs
-			const maxLength = subject?.includes( 'Cherry-pick' ) ? 110 : 100;
-			return parsed.header.length <= maxLength;
-		} ],
+		'header-max-length': [ 0 ], // Disable default header-max-length
+		'header-max-length-with-cherry-pick': [ 2, 'always' ],
 	},
 };
