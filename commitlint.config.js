8f58aab9ead8141930dbc260abac0d1f2b22f5c1
Netanel Baba <ntnelbaba@gmail.com>
2025-08-26 14:19:38 +0300

    CI: commit lint configuration header-max-length + new enum types

--- commitlint.config.js
+++ commitlint.config.js
@@ -3,6 +3,12 @@ module.exports = {
 	rules: {
 		'subject-case': [ 2, 'always', 'sentence-case' ],
 		'type-case': [ 2, 'always', 'sentence-case' ],
-		'type-enum': [ 2, 'always', [ 'New', 'Tweak', 'Fix', 'Experiment', 'Deprecate', 'Deprecated', 'Revert', 'Internal' ] ],
+		'type-enum': [ 2, 'always', [ 'Feature', 'CI', 'New', 'Tweak', 'Fix', 'Experiment', 'Deprecate', 'Deprecated', 'Revert', 'Internal' ] ],
+		'header-max-length': [ 2, 'always', ( parsed ) => {
+			const { subject } = parsed;
+			// Increase limit to 150 characters for Cherry-pick PRs
+			const maxLength = subject?.includes( 'Cherry-pick' ) ? 110 : 100;
+			return parsed.header.length <= maxLength;
+		} ],
 	},
 };
