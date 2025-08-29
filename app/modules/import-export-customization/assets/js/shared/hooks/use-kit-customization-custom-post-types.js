7ab12dcb1c9870bc73d7839c42606145eda12b86
Louis Wolmarans <louiswol94@gmail.com>
2025-08-29 11:06:32 +0200

    Cherry-pick PR #32511 with conflicts - manual resolution needed

--- app/modules/import-export-customization/assets/js/shared/hooks/use-kit-customization-custom-post-types.js
+++ app/modules/import-export-customization/assets/js/shared/hooks/use-kit-customization-custom-post-types.js
@@ -11,11 +11,41 @@ export function useKitCustomizationCustomPostTypes( { data } ) {
 			return builtInCustomPostTypes;
 		}
 
+<<<<<<< HEAD
+=======
+		const customPostTypesTitles = Object.values( data?.uploadedData?.manifest?.[ 'custom-post-type-title' ] || {} ).map( ( postType ) => {
+			return {
+				value: postType.name,
+				label: postType.label,
+			};
+		} );
+
+		if ( ! customPostTypesTitles.some( ( postType ) => 'post' === postType.value ) ) {
+			customPostTypesTitles.push( {
+				value: 'post',
+				label: 'Post',
+			} );
+		}
+
+>>>>>>> 5d6130b5be (Fix: Export and import all without customization missing the CPT [ED-20595] (#32511))
 		const wpContent = data?.uploadedData?.manifest?.[ 'wp-content' ] || {};
 
+<<<<<<< HEAD
 		return builtInCustomPostTypes.filter( ( postType ) => {
 			const contentArray = wpContent[ postType.value ];
 			return contentArray && Array.isArray( contentArray ) && contentArray.length > 0;
+=======
+		return customPostTypesTitles.filter( ( postType ) => {
+			const postTypeValue = postType.value;
+
+			const wpContentArray = wpContent[ postTypeValue ];
+			const isInWpContent = wpContentArray && Array.isArray( wpContentArray ) && wpContentArray.length > 0;
+
+			const contentObject = content[ postTypeValue ];
+			const isInElementorContent = contentObject && 'object' === typeof contentObject && Object.keys( contentObject ).length > 0;
+
+			return isInWpContent || isInElementorContent;
+>>>>>>> 5d6130b5be (Fix: Export and import all without customization missing the CPT [ED-20595] (#32511))
 		} );
 	}, [ isImport, data?.uploadedData, builtInCustomPostTypes ] );
 
