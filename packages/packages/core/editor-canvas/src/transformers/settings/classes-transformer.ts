89ddd91b5e44918f6cbf20724d20d658117c7ee0
Omer Haim Israeli <omeri@elementor.com>
2025-08-31 15:37:31 +0300

    Fix: Classes are not applied to elements after rename [ED-20634]

--- packages/packages/core/editor-canvas/src/transformers/settings/classes-transformer.ts
+++ packages/packages/core/editor-canvas/src/transformers/settings/classes-transformer.ts
@@ -3,16 +3,10 @@ import { stylesRepository } from '@elementor/editor-styles-repository';
 import { createTransformer } from '../create-transformer';
 
 export function createClassesTransformer() {
-	const cache = new Map< string, string >();
-
 	return createTransformer( ( value: string[] ) => {
 		return value
 			.map( ( id ) => {
-				if ( ! cache.has( id ) ) {
-					cache.set( id, getCssName( id ) );
-				}
-
-				return cache.get( id );
+				return getCssName( id );
 			} )
 			.filter( Boolean );
 	} );
