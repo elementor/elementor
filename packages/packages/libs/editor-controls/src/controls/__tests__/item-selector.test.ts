c2dcdcc59627573cdf56cf12dc9d5cab92553444
Robert Ferentz <robertf@elementor.red>
2025-08-31 03:46:18 +0300

    Internal: Disable already used transitions in selector [ED-19785] (#32490)

--- packages/packages/libs/editor-controls/src/controls/__tests__/item-selector.test.ts
+++ packages/packages/libs/editor-controls/src/controls/__tests__/item-selector.test.ts
@@ -59,11 +59,11 @@ describe( 'ItemSelector', () => {
 		// Assert.
 		const expected = [
 			{ type: 'category', value: 'System' },
-			{ type: 'item', value: 'Font1' },
-			{ type: 'item', value: 'Font2' },
+			{ type: 'item', value: 'Font1', disabled: false },
+			{ type: 'item', value: 'Font2', disabled: false },
 			{ type: 'category', value: 'Google' },
-			{ type: 'item', value: 'Font3' },
-			{ type: 'item', value: 'Font4' },
+			{ type: 'item', value: 'Font3', disabled: false },
+			{ type: 'item', value: 'Font4', disabled: false },
 		];
 		expect( capturedItems ).toStrictEqual( expected );
 	} );
