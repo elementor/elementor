22326cb35bdce2b3fc063cd59e28b6abdbd777b0
Netanel Baba <ntnelbaba@gmail.com>
2025-08-31 16:04:22 +0300

    Lint issues

--- packages/packages/core/editor-variables/e2e/base.fixture.ts
+++ packages/packages/core/editor-variables/e2e/base.fixture.ts
@@ -1,13 +1,14 @@
 import { setDefaultTemplate } from 'playwright';
-import { type Page, test } from '@playwright/test';
+import { Page, test } from '@playwright/test';
+
 import { openVariableManager } from './utils';
 
 export const variablesManagerFixture = test.extend< {
-    openVariableManager: Page;
+	openVariableManager: Page;
 } >( {
-    openVariableManager: async ( { page }, use ) => {
-        await setDefaultTemplate( page );
-        await openVariableManager( page );
-        await use( page );
-    },
+	openVariableManager: async ( { page }, use ) => {
+		await setDefaultTemplate( page );
+		await openVariableManager( page );
+		await use( page );
+	},
 } );
--- packages/packages/core/editor-variables/e2e/variable-manager.e2e.ts
+++ packages/packages/core/editor-variables/e2e/variable-manager.e2e.ts
@@ -2,6 +2,6 @@ import { expect } from '@playwright/test';
 
 import { variablesManagerFixture } from './base.fixture';
 
-variablesManagerFixture( 'Variable Manager', async ( { page, openVariableManager } ) => {
+variablesManagerFixture( 'Variable Manager', async ( { openVariableManager } ) => {
 	expect( openVariableManager ).toBeDefined();
 } );
