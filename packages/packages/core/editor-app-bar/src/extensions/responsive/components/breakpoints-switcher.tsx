ecd6670fc888e0644f4b02b656afee1239af22d7
elchugreeva <elenac@elementor.com>
2025-09-02 10:24:18 +0300

    check with mistake

--- packages/packages/core/editor-app-bar/src/extensions/responsive/components/breakpoints-switcher.tsx
+++ packages/packages/core/editor-app-bar/src/extensions/responsive/components/breakpoints-switcher.tsx
@@ -37,6 +37,7 @@ export default function BreakpointsSwitcher() {
 				location: config.locations.topBar,
 				secondaryLocation: config.secondaryLocations.responsiveControls,
 				trigger: config.triggers.click,
+				element: config.elements?.buttonIcon,
 				mode: value,
 			} );
 		}
