cb33601a05282cbe4e76c89afd1508e42ff55d7c
Louis Wolmarans <louiswol94@gmail.com>
2025-09-04 11:16:29 +0200

    Fix: Remove kit is not presentable [ED-20656]

--- app/modules/import-export-customization/module.php
+++ app/modules/import-export-customization/module.php
@@ -168,7 +168,7 @@ class Module extends BaseModule {
 		$user_time_format = get_option( 'time_format' );
 		$date_format = $user_date_format . ' ' . $user_time_format;
 
-		$should_show_revert_section = $this->should_show_revert_section( $last_imported_kit );
+		$should_show_revert_section = ! empty( $last_imported_kit );
 
 		if ( $should_show_revert_section ) {
 			if ( ! empty( $penultimate_imported_kit ) ) {
@@ -667,25 +667,6 @@ class Module extends BaseModule {
 		}
 
 		return $summary_titles;
-	}
-
-	public function should_show_revert_section( $last_imported_kit ) {
-		if ( empty( $last_imported_kit ) ) {
-			return false;
-		}
-
-		// TODO: BC - remove in the future
-		// The 'templates' runner was in core and moved to the Pro plugin. (Part of it still exits in the Core for BC)
-		// The runner that is in the core version is missing the revert functionality,
-		// therefore we shouldn't display the revert section if the import process done with the core version.
-		$is_import_templates_ran = isset( $last_imported_kit['runners']['templates'] );
-		if ( $this->has_pro() && $is_import_templates_ran ) {
-			$has_imported_templates = ! empty( $last_imported_kit['runners']['templates'] );
-
-			return $has_imported_templates;
-		}
-
-		return true;
 	}
 
 	public function has_pro(): bool {
