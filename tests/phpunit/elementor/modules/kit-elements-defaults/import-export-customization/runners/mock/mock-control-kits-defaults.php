8d985ed4287e59fad3fbe71b18558f112172db60
Louis Wolmarans <louiswol94@gmail.com>
2025-09-04 14:00:49 +0200

    wip

--- tests/phpunit/elementor/modules/kit-elements-defaults/import-export-customization/runners/mock/mock-control-kits-defaults.php
+++ tests/phpunit/elementor/modules/kit-elements-defaults/import-export-customization/runners/mock/mock-control-kits-defaults.php
@@ -1,5 +1,5 @@
 <?php
-namespace Elementor\Testing\Modules\KitElementsDefaults\ImportExport\Runners;
+namespace Elementor\Testing\Modules\KitElementsDefaults\ImportExportCustomization\Runners;
 
 use Elementor\Base_Control;
 
--- tests/phpunit/elementor/modules/kit-elements-defaults/import-export-customization/runners/mock/mock-widget-kits-defaults.php
+++ tests/phpunit/elementor/modules/kit-elements-defaults/import-export-customization/runners/mock/mock-widget-kits-defaults.php
@@ -1,5 +1,5 @@
 <?php
-namespace Elementor\Testing\Modules\KitElementsDefaults\ImportExport\Runners;
+namespace Elementor\Testing\Modules\KitElementsDefaults\ImportExportCustomization\Runners;
 
 use Elementor\Widget_Base;
 use Elementor\Controls_Manager;
