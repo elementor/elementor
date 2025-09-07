247c1e7173b5dbbc3ce84654f83e582bb2340eae
YaelAvitan <yael6768889@gmail.com>
2025-09-07 12:56:32 +0300

    second v

--- packages/packages/libs/editor-controls/src/components/infotip-modal.tsx
+++ packages/packages/libs/editor-controls/src/components/infotip-modal.tsx
@@ -1,11 +1,8 @@
 import * as React from 'react';
-import { useState } from 'react';
-import { useCurrentUserCapabilities } from '@elementor/editor-current-user';
-import { Button, Card, CardActions, CardContent, CardHeader, CircularProgress, Typography } from '@elementor/ui';
+import { InfoCircleFilledIcon } from '@elementor/icons';
+import { Card, CardContent, CardHeader, Typography } from '@elementor/ui';
 import Infotip from '@elementor/ui/Infotip';
 import { __ } from '@wordpress/i18n';
-
-import { useUpdateUnfilteredFilesUpload } from '../hooks/use-unfiltered-files-upload';
 
 type InfotipModalProps = {
 	open: boolean;
@@ -14,55 +11,12 @@ type InfotipModalProps = {
 };
 
 export const InfotipModal = ( props: InfotipModalProps ) => {
-	const { mutateAsync, isPending } = useUpdateUnfilteredFilesUpload();
-	const { canUser } = useCurrentUserCapabilities();
-	const [ isError, setIsError ] = useState( false );
-	const canManageOptions = canUser( 'manage_options' );
-
-	const onClose = ( enabled: boolean ) => {
-		props.onClose( enabled );
-		setTimeout( () => setIsError( false ), 300 );
-	};
-
-	const handleEnable = async () => {
-		try {
-			const response = await mutateAsync( { allowUnfilteredFilesUpload: true } );
-			if ( response?.data?.success === false ) {
-				setIsError( true );
-			} else {
-				props.onClose( true );
-			}
-		} catch {
-			setIsError( true );
-		}
-	};
-
 	return (
-		<Infotip
-			placement="right"
-			content={
-				<InfotipCard
-					// canManageOptions={ canManageOptions }
-					// isPending={ isPending }
-					// isError={ isError }
-					// onClose={ onClose }
-					// handleEnable={ handleEnable }
-				/>
-			}
-		>
+		<Infotip placement="right" content={ <InfotipCard /> }>
 			{ props.children }
 		</Infotip>
 	);
 };
-
-// type InfotipCardProps = {
-// 	canManageOptions: boolean;
-// 	isPending: boolean;
-// 	isError: boolean;
-// 	onClose: ( enabled: boolean ) => void;
-// 	handleEnable: () => void;
-// };
-
 function InfotipCard() {
 	const NON_ADMIN_TITLE_TEXT = __( "Sorry, you can't upload that file yet.", 'elementor' );
 	const NON_ADMIN_CONTENT_TEXT = __(
@@ -71,10 +25,10 @@ function InfotipCard() {
 	);
 
 	return (
-		<Card elevation={ 0 } sx={ { maxWidth: 300 } }>
-			<CardHeader title={ NON_ADMIN_TITLE_TEXT } subheader="" />
-			<CardContent>
-				<Typography variant="body2" color="text.secondary" sx={{ whiteSpace: 'pre-line' }}>
+		<Card elevation={ 0 } sx={ { maxWidth: 400 } } color={ 'error' }>
+			<CardHeader title={ NON_ADMIN_TITLE_TEXT } avatar={ <InfoCircleFilledIcon color={ 'secondary' } /> } />
+			<CardContent sx={ { paddingTop: '1px' } }>
+				<Typography variant="body1" sx={ { whiteSpace: 'pre-line' } }>
 					{ NON_ADMIN_CONTENT_TEXT }
 				</Typography>
 			</CardContent>
