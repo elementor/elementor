63bccada71a5d74eee7dfb805ae56109838b6f7c
Miryam Oren <77922014+MiryamOren@users.noreply.github.com>
2025-09-07 10:44:13 +0300

    ???

--- modules/atomic-widgets/assets/js/editor/component-instance-view.js
+++ modules/atomic-widgets/assets/js/editor/component-instance-view.js
@@ -5,17 +5,30 @@ const BaseElementView = elementor.modules.elements.views.BaseElement;
 const ComponentInstanceView = BaseElementView.extend( {
 	template: Marionette.TemplateCache.get( `#tmpl-elementor-e-component-content` ),
 
-	async initialize() {
-		await this.setupComponentData();
-		BaseElementView.prototype.initialize.apply( this, arguments );
+	// async initialize() {
+	// 	await this.setupComponentData();
+	// 	BaseElementView.prototype.initialize.apply( this, arguments );
+	// },
+
+	// async render() {
+	// 	await this.setupComponentData();
+	// 	BaseElementView.prototype.render.apply( this, arguments );
+	// },
+
+	async _renderTemplate() {
+		const elements = await this.getComponentData();
+		const firstElement = elements.at( 0 );
+		const firstElementView = this.getChildView( firstElement );
+		var view = this.buildChildView( firstElement, firstElementView, { model: firstElement } );
+
+		// this.children.add(view);
+    //   this.renderChildView(view, 0);
+		view.render();
+		const html = view.$el[0].innerHTML;
+		this.$el.html( '<div class="e-component">' + html + '</div>' );
 	},
 
-	async render() {
-		await this.setupComponentData();
-		BaseElementView.prototype.render.apply( this, arguments );
-	},
-
-	async setupComponentData() {
+	async getComponentData() {
 		const instanceSettings = this.model.get( 'settings' ).toJSON();
 		const componentId = instanceSettings.component_id.value;
 
@@ -27,7 +40,7 @@ const ComponentInstanceView = BaseElementView.extend( {
 		}
 		const elements = this.createBackboneElementsCollection( componentData.elements );
 
-		this.model.set( 'elements', elements );
+		return elements;
 	},
 
 	createBackboneElementsCollection( json ) {
