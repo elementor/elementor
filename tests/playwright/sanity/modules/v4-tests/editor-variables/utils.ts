9e4012f331f4fb230fa3508c10c40ee65f4ba690
Netanel Baba <ntnelbaba@gmail.com>
2025-09-08 14:29:08 +0300

    Move template to the inner folder

--- tests/playwright/sanity/modules/v4-tests/editor-variables/utils.ts
+++ tests/playwright/sanity/modules/v4-tests/editor-variables/utils.ts
@@ -7,7 +7,7 @@ export const initTemplate = async ( page: Page, testInfo: TestInfo, apiRequests:
 	const wpAdminPage = new WpAdminPage( page, testInfo, apiRequests );
 	await wpAdminPage.setExperiments( { e_variables: 'active', e_atomic_elements: 'active', e_variables_manager: 'active' } );
 	const editorPage = await wpAdminPage.openNewPage();
-	await editorPage.loadTemplate( 'tests/playwright/templates/default-v4.json' );
+	await editorPage.loadTemplate( 'tests/playwright/sanity/modules/v4-tests/editor-variables/variables-manager-template.json' );
 	return wpAdminPage;
 };
 
