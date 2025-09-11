e5557b30e5b1c516af60cdf990c12df23e6f9fd2
Rami Yushuvaev <92088692+rami-elementor@users.noreply.github.com>
2025-09-11 14:36:48 +0300

    lint

--- packages/packages/core/editor-app-bar/src/extensions/documents-settings/hooks/use-action-props.ts
+++ packages/packages/core/editor-app-bar/src/extensions/documents-settings/hooks/use-action-props.ts
@@ -2,12 +2,10 @@ import {
 	__useActiveDocument as useActiveDocument,
 	__useHostDocument as useHostDocument,
 } from '@elementor/editor-documents';
-
 import {
 	__privateOpenRoute as openRoute,
 	__privateUseRouteStatus as useRouteStatus,
 } from '@elementor/editor-v1-adapters';
-
 import { SettingsIcon } from '@elementor/icons';
 import { __ } from '@wordpress/i18n';
 
@@ -21,8 +19,8 @@ export default function useActionProps() {
 	const document = activeDocument && activeDocument.type.value !== 'kit' ? activeDocument : hostDocument;
 
 	const ButtonTitle = document
-		/* translators: %s: Post type label. */
-		? __( '%s Settings', 'elementor' ).replace( '%s', document.type.label )
+		? /* translators: %s: Post type label. */
+		  __( '%s Settings', 'elementor' ).replace( '%s', document.type.label )
 		: __( 'Document Settings', 'elementor' );
 
 	return {
@@ -37,15 +35,12 @@ export default function useActionProps() {
 			const config = extendedWindow?.elementorCommon?.eventsManager?.config;
 
 			if ( config ) {
-				extendedWindow.elementorCommon.eventsManager.dispatchEvent(
-					config.names.topBar.documentSettings,
-					{
-						location: config.locations.topBar,
-						secondaryLocation: config.secondaryLocations[ 'document-settings' ],
-						trigger: config.triggers.click,
-						element: config.elements.buttonIcon,
-					}
-				);
+				extendedWindow.elementorCommon.eventsManager.dispatchEvent( config.names.topBar.documentSettings, {
+					location: config.locations.topBar,
+					secondaryLocation: config.secondaryLocations[ 'document-settings' ],
+					trigger: config.triggers.click,
+					element: config.elements.buttonIcon,
+				} );
 			}
 
 			openRoute( 'panel/page-settings/settings' );
