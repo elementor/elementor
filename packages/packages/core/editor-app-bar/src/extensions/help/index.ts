663a3a6186f55b0da54ba578cd555cb5f7b0002a
Rami Yushuvaev <92088692+rami-elementor@users.noreply.github.com>
2025-09-11 09:55:24 +0300

    Move "help" link

--- packages/packages/core/editor-app-bar/src/extensions/help/index.ts
+++ packages/packages/core/editor-app-bar/src/extensions/help/index.ts
@@ -1,34 +1,10 @@
-import { HelpIcon } from '@elementor/icons';
-import { __ } from '@wordpress/i18n';
-
-import { utilitiesMenu } from '../../locations';
-import { type ExtendedWindow } from '../../types';
+import { mainMenu } from '../../locations';
+import useActionProps from './hooks/use-action-props';
 
 export function init() {
-	utilitiesMenu.registerLink( {
+	mainMenu.registerLink( {
 		id: 'open-help-center',
-		priority: 25, // After Finder.
-		useProps: () => {
-			return {
-				title: __( 'Help', 'elementor' ),
-				href: 'https://go.elementor.com/editor-top-bar-learn/',
-				icon: HelpIcon,
-				target: '_blank',
-				showExternalLinkIcon: true,
-				onClick: () => {
-					const extendedWindow = window as unknown as ExtendedWindow;
-					const config = extendedWindow?.elementorCommon?.eventsManager?.config;
-
-					if ( config ) {
-						extendedWindow.elementorCommon.eventsManager.dispatchEvent( config.names.topBar.help, {
-							location: config.locations.topBar,
-							secondaryLocation: config.secondaryLocations.help,
-							trigger: config.triggers.click,
-							element: config.elements.buttonIcon,
-						} );
-					}
-				},
-			};
-		},
+		priority: 45,
+		useProps: useActionProps,
 	} );
 }
