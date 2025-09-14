8c1d23c8e1a7b27e0251dccee4707be1e842957e
MuliElementor <mulit@elementor.red>
2025-09-14 21:01:41 +0300

    update

--- packages/packages/core/editor-components/src/components/components.tsx
+++ packages/packages/core/editor-components/src/components/components.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { ThemeProvider } from '@elementor/editor-ui';
-
 import { SearchProvider } from '@elementor/utils';
+
 import { ComponentSearch } from './component-search';
 import { ComponentsList } from './components-list';
 
