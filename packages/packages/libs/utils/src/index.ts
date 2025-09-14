f55abd66f65c248c6b528e18e82710947cde1504
MuliElementor <mulit@elementor.red>
2025-09-14 15:03:30 +0300

    update

--- packages/packages/libs/utils/src/index.ts
+++ packages/packages/libs/utils/src/index.ts
@@ -4,3 +4,4 @@ export { useDebounceState, type UseDebounceStateOptions, type UseDebounceStateRe
 export { debounce } from './debounce';
 export { encodeString, decodeString } from './encoding';
 export { hash } from './hash';
+export { SearchProvider, useSearch } from './search-provider';
