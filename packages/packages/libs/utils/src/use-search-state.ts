05977afefe2379715b1e0a1f0174fb40172fe842
MuliElementor <mulit@elementor.red>
2025-09-15 13:22:10 +0300

    Add components for enhanced component management
    Introduce several new components for the editor's component tab, including `ComponentSearch`, `ComponentsList`, `ComponentItem`, and `LoadingComponents`. These components facilitate searching, displaying, and managing components within the editor. Additionally, a new `SearchProvider` is implemented to manage search state effectively, replacing the previous search context. This update enhances user experience by providing a structured and interactive way to handle components.

---
+++ packages/packages/libs/utils/src/use-search-state.ts
@@ -0,0 +1,24 @@
+import { useDebounceState } from '../use-debounce-state';
+
+export function useSearchState( { localStorageKey }: { localStorageKey: string } ) {
+	const getInitialSearchValue = () => {
+		if ( localStorageKey ) {
+			const storedValue = localStorage.getItem( localStorageKey );
+			if ( storedValue ) {
+				localStorage.removeItem( localStorageKey );
+				return storedValue;
+			}
+		}
+		return '';
+	};
+	const { debouncedValue, inputValue, handleChange, setInputValue } = useDebounceState( {
+		delay: 300,
+		initialValue: getInitialSearchValue(),
+	} );
+	return {
+		debouncedValue,
+		inputValue,
+		handleChange,
+		setInputValue,
+	};
+}
