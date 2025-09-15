4227834139cbbc9d42c0c0bf0c8c7babf2debdc9
David Seveloff <davids@elementor.com>
2025-09-15 10:10:50 +0300

    Review comments

--- tests/playwright/sanity/modules/v4-tests/atomic-repeaters-display.test.ts
+++ tests/playwright/sanity/modules/v4-tests/atomic-repeaters-display.test.ts
@@ -1,17 +1,13 @@
 import { parallelTest as test } from '../../../parallelTest';
 import WpAdminPage from '../../../pages/wp-admin-page';
-import EditorPage from '../../../pages/editor-page';
 import { BrowserContext, Page, expect } from '@playwright/test';
 
 test.describe( 'Atomic repeaters display @atomic-widgets', () => {
-	let editor: EditorPage;
 	let wpAdmin: WpAdminPage;
-	let context: BrowserContext;
-	let page: Page;
 
 	test.beforeAll( async ( { browser, apiRequests }, testInfo ) => {
-		context = await browser.newContext();
-		page = await context.newPage();
+		const context = await browser.newContext();
+		const page = await context.newPage();
 		wpAdmin = new WpAdminPage( page, testInfo, apiRequests );
 		await wpAdmin.setExperiments( {
 			e_atomic_elements: 'active',
@@ -20,30 +16,27 @@ test.describe( 'Atomic repeaters display @atomic-widgets', () => {
 
 	test.afterAll( async () => {
 		await wpAdmin.resetExperiments();
-		await context.close();
 	} );
 
-	const repeaterControls = [ 'filter', 'Backdrop filter', 'transform', 'Box shadow', 'transitions' ];
-
-	repeaterControls.forEach( ( contorl ) => {
-		let containerId: string;
-
-		test( `repeater control ${ contorl } stability`, async () => {
-			editor = await wpAdmin.openNewPage();
-			containerId = await editor.addElement( { elType: 'container' }, 'document' );
-			await editor.addWidget( { widgetType: 'e-heading', container: containerId } );
+	// const repeaterControls = [ 'filter', 'Backdrop filter', 'transform', 'Box shadow', 'transitions' ];
+	const repeaterControls = [ 'filter' ];
+		
+	for ( const control of repeaterControls ) {
+		test.only( `repeater control ${ control } stability`, async () => {
+			const editor = await wpAdmin.openNewPage();
+			await editor.addWidget( { widgetType: 'e-heading' } );
 			await editor.openV2PanelTab( 'style' );
 			await editor.openV2Section( 'effects' );
 
-			const controlRepeaterAdditionButton = page.getByRole( 'button', { name: `Add ${ contorl } item` } );
-			await editor.page.pause();
+			const controlRepeaterAdditionButton = editor.page.getByRole( 'button', { name: `Add ${ control } item` } );
+
 			await controlRepeaterAdditionButton.click();
-			await page.locator( '.MuiBackdrop-root' ).click();
+			await editor.page.locator( '.MuiBackdrop-root' ).click();
 
 			const parentDiv = controlRepeaterAdditionButton.locator( '../../..' );
-			const controlName = contorl.trim().toLowerCase().replace( /\s+/g, '-' );
+			const controlName = control.trim().toLowerCase().replace( /\s+/g, '-' );
 
 			await expect.soft( parentDiv ).toHaveScreenshot( 'transform-parent-' + controlName + '.png' );
 		} );
-	} );
+	};
 } );
