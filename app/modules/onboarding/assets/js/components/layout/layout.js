f9f3a09c448af5421f61257ea78726aa4656a361
YaelAvitan <yael6768889@gmail.com>
2025-09-16 13:31:52 +0300

    wip

--- app/modules/onboarding/assets/js/components/layout/layout.js
+++ app/modules/onboarding/assets/js/components/layout/layout.js
@@ -5,7 +5,6 @@ import Header from './header';
 import ProgressBar from '../progress-bar/progress-bar';
 import Content from '../../../../../../assets/js/layout/content';
 import Connect from '../../utils/connect';
-import { trackUpgradeNowClickEvent } from '../../utils/mixpanel-events';
 
 export default function Layout( props ) {
 	useEffect( () => {
@@ -78,7 +77,6 @@ export default function Layout( props ) {
 	if ( ! state.hasPro ) {
 		headerButtons.push( {
 			id: 'go-pro',
-			text: __( 'Upgradehjkhoiwheiog', 'elementor' ),
 			hideText: false,
 			className: 'eps-button__go-pro-btn',
 			url: 'https://elementor.com/pro/?utm_source=onboarding-wizard&utm_campaign=gopro&utm_medium=wp-dash&utm_content=top-bar&utm_term=' + elementorAppConfig.onboarding.onboardingVersion,
@@ -93,9 +91,6 @@ export default function Layout( props ) {
 						step: state.currentStep,
 					},
 				} );
-
-				// Track upgrade now button click
-				trackUpgradeNowClickEvent();
 			},
 		} );
 	}
