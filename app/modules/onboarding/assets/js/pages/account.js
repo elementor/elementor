6e5c6471297b19c5a2cabbaab5d67ba70640cf61
YaelAvitan <yael6768889@gmail.com>
2025-09-16 13:34:43 +0300

    Update account.js

--- app/modules/onboarding/assets/js/pages/account.js
+++ app/modules/onboarding/assets/js/pages/account.js
@@ -28,7 +28,7 @@ export default function Account() {
 				},
 			);
 		}
-		// eslint-disable-next-line react-hooks/exhaustive-deps
+	// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [] );
 
 	let skipButton;
