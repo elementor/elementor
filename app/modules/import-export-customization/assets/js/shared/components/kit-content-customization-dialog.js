78877a9fd2bef5fa6db065652c516131eb3f3272
Louis Wolmarans <louiswol94@gmail.com>
2025-09-17 11:15:38 +0200

    Tweak: On export adding the UI for the media [ED-20955]

--- app/modules/import-export-customization/assets/js/shared/components/kit-content-customization-dialog.js
+++ app/modules/import-export-customization/assets/js/shared/components/kit-content-customization-dialog.js
@@ -1,6 +1,6 @@
 import { useState, useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
-import { Stack } from '@elementor/ui';
+import { Stack, Typography, Alert, SvgIcon } from '@elementor/ui';
 import { __ } from '@wordpress/i18n';
 import { KitCustomizationDialog } from './kit-customization-dialog';
 import { ListSettingSection } from './customization-list-setting-section';
@@ -67,6 +67,38 @@ export function KitContentCustomizationDialog( {
 		} ) );
 	};
 
+	const renderMediaFormatSection = () => {
+		if ( isImport ) {
+			return null;
+		}
+
+		return (
+			<SettingSection
+				title={ __( 'Media format', 'elementor' ) }
+				settingKey="mediaFormat"
+				hasToggle={ false }
+			>
+				<Alert 
+					icon={ 
+						<SvgIcon color="info" viewBox="0 0 24 24">
+							<path d="M11.8623 14.7549C12.3665 14.8061 12.7598 15.2322 12.7598 15.75C12.7598 16.2678 12.3665 16.6939 11.8623 16.7451L11.7598 16.75H11.75C11.1977 16.75 10.75 16.3023 10.75 15.75C10.75 15.1977 11.1977 14.75 11.75 14.75H11.7598L11.8623 14.7549Z" fill="currentColor"/>
+							<path d="M11.75 7C12.1642 7 12.5 7.33579 12.5 7.75V12.75C12.5 13.1642 12.1642 13.5 11.75 13.5C11.3358 13.5 11 13.1642 11 12.75V7.75C11 7.33579 11.3358 7 11.75 7Z" fill="currentColor"/>
+							<path fillRule="evenodd" clipRule="evenodd" d="M11.75 2C17.1348 2 21.5 6.36522 21.5 11.75C21.5 17.1348 17.1348 21.5 11.75 21.5C6.36522 21.5 2 17.1348 2 11.75C2 6.36522 6.36522 2 11.75 2ZM11.75 3.5C7.19365 3.5 3.5 7.19365 3.5 11.75C3.5 16.3063 7.19365 20 11.75 20C16.3063 20 20 16.3063 20 11.75C20 7.19365 16.3063 3.5 11.75 3.5Z" fill="currentColor"/>
+						</SvgIcon>
+					}
+					sx={ {
+						backgroundColor: 'transparent',
+						p: 0
+					} }
+				>
+					<Typography variant="body2" color="text.primary">
+						<strong>{ __( 'Note:', 'elementor' ) }</strong> { __( 'The media will be uploaded automatically, just as it was saved during export', 'elementor' ) }
+					</Typography>
+				</Alert>
+			</SettingSection>
+		);
+	};
+
 	return (
 		<KitCustomizationDialog
 			open={ open }
@@ -105,6 +137,7 @@ export function KitContentCustomizationDialog( {
 						items={ customPostTypes }
 					/>
 				) }
+				{ renderMediaFormatSection() }
 			</Stack>
 		</KitCustomizationDialog>
 	);
