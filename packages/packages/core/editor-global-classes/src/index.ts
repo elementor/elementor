0dc7a24220f7c7b61cf4059a882a81e507b8539c
Avichay Eyal <avichaye@elementor.red>
2025-09-18 11:48:40 +0300

    removing unused code

--- packages/packages/core/editor-global-classes/src/index.ts
+++ packages/packages/core/editor-global-classes/src/index.ts
@@ -1,11 +1,4 @@
-import { deleteClass, onDelete } from './components/class-manager/delete-class';
-
 export { GLOBAL_CLASSES_PROVIDER_KEY } from './global-classes-styles-provider';
 export { selectOrderedClasses } from './store';
-
-export const StoreActions = {
-	onDelete,
-	deleteClass,
-};
 
 export { init } from './init';
