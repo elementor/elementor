450020fb8e751972775bbc6f53455ed80a082fd6
Batsirai Muchareva <batsiraim@elementor.red>
2025-09-18 10:51:25 +0200

    wip

--- packages/packages/core/editor-variables/src/components/ui/no-search-results.tsx
+++ packages/packages/core/editor-variables/src/components/ui/no-search-results.tsx
@@ -10,7 +10,14 @@ type Props = {
 
 export const NoSearchResults = ( { searchValue, onClear, icon }: Props ) => {
 	return (
-		<Stack gap={ 1 } p={ 2.5 } color="text.secondary" sx={ { pb: 3.5 } }>
+		<Stack
+			gap={ 1 }
+			alignItems="center"
+			justifyContent="center"
+			p={ 2.5 }
+			color="text.secondary"
+			sx={ { pb: 3.5 } }
+		>
 			{ icon }
 			<Typography align="center" variant="subtitle2">
 				{ __( 'Sorry, nothing matched', 'elementor' ) }
