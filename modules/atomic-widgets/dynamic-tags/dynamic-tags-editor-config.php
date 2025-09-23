05d20a9530eb53ee3f29d9d8759da1f7d5734f16
marcin-elementor <marcins@elementor.red>
2025-09-23 11:57:01 +0000

    Revert "Internal: Add 'ACF URL' URL tag [ED-20506] (#32855)"
    This reverts commit 1007c24fb8c7aa676fcc0a70c834d9ca6f0ac4f9.

--- modules/atomic-widgets/dynamic-tags/dynamic-tags-editor-config.php
+++ modules/atomic-widgets/dynamic-tags/dynamic-tags-editor-config.php
@@ -88,7 +88,7 @@ class Dynamic_Tags_Editor_Config {
 		}
 
 		try {
-			$atomic_controls = $this->convert_controls_to_atomic( $tag );
+			$atomic_controls = $this->convert_controls_to_atomic( $tag['controls'], $tag['force_convert_to_atomic'] ?? false );
 		} catch ( \Exception $e ) {
 			return null;
 		}
@@ -102,22 +102,15 @@ class Dynamic_Tags_Editor_Config {
 		return $converted_tag;
 	}
 
-	private function convert_controls_to_atomic( $tag ) {
+	private function convert_controls_to_atomic( $controls, $force = false ) {
 		$atomic_controls = [];
-
-		$controls = $tag['controls'] ?? null;
-		$force = $tag['force_convert_to_atomic'] ?? false;
-
-		if ( ! is_array( $controls ) ) {
-			return null;
-		}
 
 		foreach ( $controls as $control ) {
 			if ( 'section' === $control['type'] ) {
 				continue;
 			}
 
-			$atomic_control = $this->convert_control_to_atomic( $control, $tag );
+			$atomic_control = $this->convert_control_to_atomic( $control );
 
 			if ( ! $atomic_control ) {
 				if ( $force ) {
@@ -140,9 +133,9 @@ class Dynamic_Tags_Editor_Config {
 		return array_values( $atomic_controls );
 	}
 
-	private function convert_control_to_atomic( $control, $tag = [] ) {
+	private function convert_control_to_atomic( $control ) {
 		$map = [
-			'select'   => fn( $control ) => $this->convert_select_control_to_atomic( $control, $tag ),
+			'select'   => fn( $control ) => $this->convert_select_control_to_atomic( $control ),
 			'text'     => fn( $control ) => $this->convert_text_control_to_atomic( $control ),
 			'textarea' => fn( $control ) => $this->convert_textarea_control_to_atomic( $control ),
 			'switcher' => fn( $control ) => $this->convert_switch_control_to_atomic( $control ),
@@ -171,22 +164,18 @@ class Dynamic_Tags_Editor_Config {
 	 * @return Select_Control
 	 * @throws \Exception If control is missing options.
 	 */
-	private function convert_select_control_to_atomic( $control, $tag = [] ) {
-		$options = $this->extract_select_options_from_control( $control );
-
-		if ( empty( $options ) ) {
+	private function convert_select_control_to_atomic( $control ) {
+		if ( empty( $control['options'] ) ) {
 			throw new \Exception( 'Select control must have options' );
 		}
-
-		$options = apply_filters( 'elementor/atomic/dynamic_tags/select_control_options', $options, $control, $tag );
 
 		$options = array_map(
 			fn( $key, $value ) => [
 				'value' => $key,
 				'label' => $value,
 			],
-			array_keys( $options ),
-			$options
+			array_keys( $control['options'] ),
+			$control['options']
 		);
 
 		$select_control = Select_Control::bind_to( $control['name'] )
@@ -199,36 +188,6 @@ class Dynamic_Tags_Editor_Config {
 		}
 
 		return $select_control;
-	}
-
-	private function extract_select_options_from_control( $control ): array {
-		$options = $control['options'] ?? [];
-
-		if ( ! empty( $options ) ) {
-			return $options;
-		}
-
-		if ( empty( $control['groups'] ) || ! is_array( $control['groups'] ) ) {
-			return $options;
-		}
-
-		foreach ( $control['groups'] as $group ) {
-			if ( empty( $group['options'] ) || ! is_array( $group['options'] ) ) {
-				continue;
-			}
-
-			$filtered = array_filter(
-				$group['options'],
-				static function ( $label, $key ) {
-					return is_string( $key );
-				},
-				ARRAY_FILTER_USE_BOTH
-			);
-
-			$options = array_merge( $options, $filtered );
-		}
-
-		return $options;
 	}
 
 	/**
