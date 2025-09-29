418e9b5cc2143cdf7f4619b6eb364cd71bb38b27
MuliElementor <mulit@elementor.red>
2025-09-29 15:39:41 +0300

    update

--- packages/packages/core/editor-components/src/components/__tests__/components-tab.test.tsx
+++ packages/packages/core/editor-components/src/components/__tests__/components-tab.test.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import { renderWithStore, renderWithTheme } from 'test-utils';
-import { endDragElementFromPanel, startDragElementFromPanel } from '@elementor/editor-canvas';
 import {
 	__createStore,
 	__dispatch as dispatch,
