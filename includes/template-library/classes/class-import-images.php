579db9f87f7c1712498a561a7eca35b33758bcd3
Louis Wolmarans <louiswol94@gmail.com>
2025-09-30 10:29:16 +0200

    Tweak: On import, using the media zip [ED-20913]

--- includes/template-library/classes/class-import-images.php
+++ includes/template-library/classes/class-import-images.php
@@ -215,6 +215,78 @@ class Import_Images {
 	}
 
 	/**
+	 * Import local file.
+	 *
+	 * Import a local file directly to WordPress media library.
+	 * Used for importing files that are already downloaded locally (e.g., from extracted ZIP).
+	 *
+	 * @since 3.33.0
+	 * @access public
+	 *
+	 * @param string $local_file_path The local file path.
+	 * @param int    $parent_post_id  Optional. Parent post ID.
+	 *
+	 * @return false|array Imported image data, or false on failure.
+	 */
+	public function import_local_file( $local_file_path, $parent_post_id = null ) {
+		if ( ! file_exists( $local_file_path ) ) {
+			return false;
+		}
+
+		$filename = basename( $local_file_path );
+		$file_content = file_get_contents( $local_file_path );
+
+		if ( ! $file_content ) {
+			return false;
+		}
+
+		$upload = wp_upload_bits( $filename, null, $file_content );
+
+		if ( $upload['error'] ) {
+			return false;
+		}
+
+		$filetype = wp_check_filetype( $upload['file'] );
+
+		// If the file type is not recognized by WordPress, exit here to avoid creation of an empty attachment document.
+		if ( ! $filetype['ext'] ) {
+			return false;
+		}
+
+		$attachment_data = [
+			'post_title' => sanitize_file_name( pathinfo( $filename, PATHINFO_FILENAME ) ),
+			'post_content' => '',
+			'post_status' => 'inherit',
+			'post_mime_type' => $filetype['type'],
+			'guid' => $upload['url'],
+		];
+
+		$attachment_id = wp_insert_attachment( $attachment_data, $upload['file'], $parent_post_id );
+
+		if ( is_wp_error( $attachment_id ) ) {
+			return false;
+		}
+
+		if ( ! function_exists( 'wp_generate_attachment_metadata' ) ) {
+			require_once ABSPATH . 'wp-admin/includes/image.php';
+		}
+
+		if ( ! function_exists( 'wp_read_video_metadata' ) ) {
+			require_once ABSPATH . 'wp-admin/includes/media.php';
+		}
+
+		$attachment_metadata = wp_generate_attachment_metadata( $attachment_id, $upload['file'] );
+		wp_update_attachment_metadata( $attachment_id, $attachment_metadata );
+
+		apply_filters( 'elementor/template_library/import_images/new_attachment', $attachment_id );
+
+		return [
+			'id' => $attachment_id,
+			'url' => $upload['url'],
+		];
+	}
+
+	/**
 	 * Template library import images constructor.
 	 *
 	 * Initializing the images import class used by the template library through
