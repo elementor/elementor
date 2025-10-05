ad0f545c63600d69c8334ee28188c3c054204a4d
Netanel Baba <50736016+Ntnelbaba@users.noreply.github.com>
2025-10-05 19:15:47 +0300

    Update element.js

--- assets/dev/js/editor/regions/panel/pages/elements/views/element.js
+++ assets/dev/js/editor/regions/panel/pages/elements/views/element.js
@@ -173,19 +173,20 @@ module.exports = Marionette.ItemView.extend( {
 			this.model.set( 'settings', this.model.get( 'custom' ).preset_settings );
 		}
 
+		const modelData = this.model.toJSON();
+
 		$e.run( 'preview/drop', {
 			container,
 			options: {
 				...options,
 				scrollIntoView: true,
 			},
-			model: this.model.toJSON(),
+			model: modelData,
 		} );
 
-		if ( elementorCommon.eventsManager ) {
-			const modelData = this.model.toJSON();
-			const elType = modelData.elType;
-			const widgetType = modelData.widgetType;
+		if ( elementorCommon?.eventsManager?.dispatchEvent ) {
+			const elType = modelData?.elType ?? '';
+			const widgetType = modelData?.widgetType ?? '';
 			const elementName = 'widget' === elType ? widgetType : elType;
 
 			elementorCommon.eventsManager.dispatchEvent( 'add_element', {
