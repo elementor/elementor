4e9a7124afa36261ee67c70ff5dfa684d01b98d0
Nami printz <namipr3@gmail.com>
2025-10-05 18:08:49 +0300

    Revert "Fix: Add Mixpanel event `add_element` for editor_panel location [ED-20328]"
    This reverts commit 67f3b741b8c41a83fb1b7142dcc93eac38e3fc50.

--- assets/dev/js/editor/regions/panel/pages/elements/views/element.js
+++ assets/dev/js/editor/regions/panel/pages/elements/views/element.js
@@ -184,16 +184,16 @@ module.exports = Marionette.ItemView.extend( {
 
 		if ( elementorCommon.eventsManager ) {
 			const modelData = this.model.toJSON();
-			const elType = modelData.elType;
-			const widgetType = modelData.widgetType;
 			const elementName = 'widget' === elType ? widgetType : elType;
 
-			elementorCommon.eventsManager.dispatchEvent( 'add_element', {
+			const eventData = {
 				location: 'editor_panel',
 				element_name: elementName,
-				element_type: elType,
-				widget_type: widgetType,
-			} );
+				element_type: modelData.elType,
+				widget_type: modelData.widgetType,
+			};
+
+			elementorCommon.eventsManager.dispatchEvent( 'add_element', eventData );
 		}
 	},
 
