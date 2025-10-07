94c5cf58d7d6147b2ccb31cdcd58d8a67b035673
Nicola Peluchetti <nicola.peluchetti@gmail.com>
2025-10-07 10:19:36 +0000

    Revert "Internal: Update dashboard event handling [ED-21099] (#32997)"
    This reverts commit 1a9fc09fd89c5b5ade0399f5c225bcda255c254a.

--- assets/dev/js/modules/modules.js
+++ assets/dev/js/modules/modules.js
@@ -7,7 +7,6 @@ import ForceMethodImplementation from './imports/force-method-implementation';
 import { createGetInitialState } from '../../../../app/modules/import-export-customization/assets/js/shared/utils/template-registry-helpers';
 import { customizationDialogsRegistry } from '../../../../app/modules/import-export-customization/assets/js/shared/registry/customization-dialogs';
 import { AppsEventTracking } from 'elementor-app/event-track/apps-event-tracking';
-import WpDashboardTracking from 'elementor-app/event-track/wp-dashboard-tracking';
 
 export default window.elementorModules = {
 	Module,
@@ -27,9 +26,5 @@ export default window.elementorModules = {
 
 	appsEventTracking: {
 		AppsEventTracking,
-	},
-
-	wpDashboardTracking: {
-		WpDashboardTracking,
 	},
 };
