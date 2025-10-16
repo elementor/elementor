922edda17c8a2a7d6c6d61bcd5f7774f38265be6
Miryam Oren <77922014+MiryamOren@users.noreply.github.com>
2025-10-16 17:30:50 +0300

    fix type

--- packages/packages/core/editor-components/src/utils/before-save.ts
+++ packages/packages/core/editor-components/src/utils/before-save.ts
@@ -76,7 +76,7 @@ function shouldUpdateElement(
 	tempIdToComponentId: Map< number, number >
 ): { shouldUpdate: true; newComponentId: number } | { shouldUpdate: false; newComponentId: null } {
 	if ( element.widgetType === 'e-component' ) {
-		const currentComponentId = ( element.settings?.component as TransformablePropValue< 'number', number > )?.value;
+		const currentComponentId = ( element.settings?.component as TransformablePropValue< 'component-id', number > )?.value;
 		if ( currentComponentId && tempIdToComponentId.has( currentComponentId ) ) {
 			return { shouldUpdate: true, newComponentId: tempIdToComponentId.get( currentComponentId ) as number };
 		}
