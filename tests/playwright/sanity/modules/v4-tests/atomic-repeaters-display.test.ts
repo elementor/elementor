40a22957511ce1bb4a594474c3c28a1c5f78c220
Ronen Boxer <145427686+ronenelementor@users.noreply.github.com>
2025-10-19 19:21:28 +0300

    Internal: [V4] NaN value appears in custom size in repeaters [ED-21274]

--- tests/playwright/sanity/modules/v4-tests/atomic-repeaters-display.test.ts
+++ tests/playwright/sanity/modules/v4-tests/atomic-repeaters-display.test.ts
@@ -2,6 +2,8 @@ import { parallelTest as test } from '../../../parallelTest';
 import WpAdminPage from '../../../pages/wp-admin-page';
 import { expect } from '@playwright/test';
 
+const CUSTOM_VALUE = 'my custom value';
+
 test.describe( 'Atomic repeaters display @atomic-widgets', () => {
 	let wpAdmin: WpAdminPage;
 
@@ -36,6 +38,33 @@ test.describe( 'Atomic repeaters display @atomic-widgets', () => {
 			const controlName = control.trim().toLowerCase().replace( /\s+/g, '-' );
 
 			await expect.soft( parentDiv ).toHaveScreenshot( 'transform-parent-' + controlName + '.png' );
+		} );
+
+		test( `repeater control ${ control } custom size stability`, async () => {
+			const editor = await wpAdmin.openNewPage();
+			await editor.addWidget( { widgetType: 'e-heading' } );
+			await editor.v4Panel.openTab( 'style' );
+			await editor.openV2Section( 'effects' );
+
+			const controlRepeaterAdditionButton = editor.page.getByRole( 'button', { name: `Add ${ control } item` } );
+			const inputUnitButton = editor.page.locator( 'button', { hasText: /^(px|ms)$/ } ).last();
+			const customSizeButton = editor.page.locator( '.MuiPaper-root ul li .MuiListItemText-root svg' );
+			const customSizeInput = editor.page.locator( '.MuiPaper-root .MuiPaper-root .MuiPaper-root input[type="text"]' );
+
+			await controlRepeaterAdditionButton.click();
+			await inputUnitButton.click();
+			await customSizeButton.click();
+			await customSizeInput.fill( CUSTOM_VALUE );
+
+			await expect( customSizeInput ).toHaveValue( CUSTOM_VALUE );
+
+			while ( await editor.page.locator( '.MuiBackdrop-root' ).count() ) {
+				await editor.page.locator( '.MuiBackdrop-root' ).last().click();
+			}
+
+			const repeaterItem = editor.page.locator( '.MuiTag-content' );
+
+			expect( await repeaterItem.textContent() ).toContain( CUSTOM_VALUE );
 		} );
 	}
 } );
