0c47eb5e393b5e07c1e510abe04c9ba44fc22a87
Rami Yushuvaev <92088692+rami-elementor@users.noreply.github.com>
2025-10-22 21:17:27 +0300

    Internal: Add playwright test for icon-list widget item alignment in LTR & RTL

--- tests/playwright/sanity/includes/widgets/icon-list.test.ts
+++ tests/playwright/sanity/includes/widgets/icon-list.test.ts
@@ -1,8 +1,45 @@
 import { expect } from '@playwright/test';
 import { parallelTest as test } from '../../../parallelTest';
 import WpAdminPage from '../../../pages/wp-admin-page';
+import _path from 'path';
 
-test.describe( 'Icon List', () => {
+test.describe( 'Icon List widget tests', () => {
+	test( 'Test icon list alignment of list items', async ( { page, apiRequests }, testInfo ) => {
+		// Arrange.
+		const wpAdmin = new WpAdminPage( page, testInfo, apiRequests );
+		await wpAdmin.setSiteLanguage( '' );
+
+		const editor = await wpAdmin.openNewPage();
+		await editor.closeNavigatorIfOpen();
+
+		// Act.
+		const filePath = _path.resolve( __dirname, `./../../templates/icon-list.json` );
+		await editor.loadTemplate( filePath, false );
+
+		const pageId = await editor.getPageId();
+		await editor.publishAndViewPage();
+
+		// Assert.
+		await test.step( 'LTR layouts', async () => {
+			const iconBoxes = page.locator( '.e-con-inner' ).first();
+			await iconBoxes.waitFor();
+			await expect.soft( iconBoxes ).toHaveScreenshot( 'icon-list-items-alignment-ltr.png' );
+		} );
+
+		await test.step( 'RTL layouts', async () => {
+			await wpAdmin.setSiteLanguage( 'he_IL' );
+
+			await editor.page.goto( `/?p=${ pageId }` );
+			await editor.page.waitForLoadState();
+
+			const iconBoxes = page.locator( '.e-con-inner' ).first();
+			await iconBoxes.waitFor();
+			await expect.soft( iconBoxes ).toHaveScreenshot( 'icon-list-items-alignment-rtl.png' );
+		} );
+
+		await wpAdmin.setSiteLanguage( '' );
+	} );
+
 	test( 'Test vertical alignment of the icons', async ( { page, apiRequests }, testInfo ) => {
 		// Arrange.
 		const wpAdmin = new WpAdminPage( page, testInfo, apiRequests );
