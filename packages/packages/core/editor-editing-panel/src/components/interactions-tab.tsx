96b0f39f64d7af93fa2c3dae5271e4a3aabac712
Nami printz <144731882+nami-p@users.noreply.github.com>
2025-10-26 09:47:30 +0000

    Revert "Internal: Replace interaction icon with swipe icon [ED-21344] (#33228)"
    This reverts commit ead4b258230fe31d9f91d4f214b5a19328a82d78.

--- packages/packages/core/editor-editing-panel/src/components/interactions-tab.tsx
+++ packages/packages/core/editor-editing-panel/src/components/interactions-tab.tsx
@@ -1,9 +1,11 @@
 import * as React from 'react';
-import { SwipeIcon } from '@elementor/icons';
+import { CopyIcon } from '@elementor/icons';
 import { Button, Stack, Typography } from '@elementor/ui';
 import { __ } from '@wordpress/i18n';
 
 export const InteractionsTab = () => {
+	const handleCreateInteraction = () => {};
+
 	return (
 		<Stack
 			alignItems="center"
@@ -13,7 +15,7 @@ export const InteractionsTab = () => {
 			sx={ { p: 2.5, pt: 8, pb: 5.5 } }
 			gap={ 1.5 }
 		>
-			<SwipeIcon fontSize="large" />
+			<CopyIcon fontSize="large" />
 
 			<Typography align="center" variant="subtitle2">
 				{ __( 'Animate elements with Interactions', 'elementor' ) }
@@ -26,7 +28,13 @@ export const InteractionsTab = () => {
 				) }
 			</Typography>
 
-			<Button variant="outlined" color="secondary" size="small" sx={ { mt: 1 } }>
+			<Button
+				variant="outlined"
+				color="secondary"
+				size="small"
+				onClick={ handleCreateInteraction }
+				sx={ { mt: 1 } }
+			>
 				{ __( 'Create an interaction', 'elementor' ) }
 			</Button>
 		</Stack>
