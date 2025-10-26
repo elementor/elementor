1cf1e2f15bab8eb545ccee38a9133fbb8ac59448
YaelAvitan <yael6768889@gmail.com>
2025-10-26 13:58:45 +0200

    lint issues

--- packages/packages/core/editor-canvas/src/transformers/settings/date-time-transformer.ts
+++ packages/packages/core/editor-canvas/src/transformers/settings/date-time-transformer.ts
@@ -1,16 +1,14 @@
 import { createTransformer } from '../create-transformer';
 
 export const dateTimeTransformer = createTransformer( ( values: { date?: string; time?: string }[] ) => {
-    return values
-        .map( ( value ) => {
-            const date = ( value.date || '' ).trim();
-            const time = ( value.time || '' ).trim();
-
-            if ( ! date && ! time ) {
-                return '';
-            }
-
-            return ( `${ date } ${ time }` ).trim();
-        } )
-        .join( ' ' );
+	return values
+		.map( ( value ) => {
+			const date = ( value.date || '' ).trim();
+			const time = ( value.time || '' ).trim();
+			if ( ! date && ! time ) {
+				return '';
+			}
+			return ( `${ date } ${ time }` ).trim();
+		} )
+		.join( ' ' );
 } );
