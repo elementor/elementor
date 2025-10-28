cc33d675c9498e5b2f6470cebe67b628a451ea29
Robert Ferentz <robiferentz@gmail.com>
2025-10-28 09:17:45 +0200

    added simple render test

---
+++ packages/packages/libs/editor-controls/src/controls/__tests__/display-conditions-control.test.tsx
@@ -0,0 +1,27 @@
+import * as React from 'react';
+import { createMockPropType, renderControl } from 'test-utils';
+import { screen } from '@testing-library/react';
+
+import { DisplayConditionsControl } from '../display-conditions-control';
+
+const propType = createMockPropType( { kind: 'plain' } );
+
+describe( 'DisplayConditionsControl', () => {
+	it( 'should render with a button containing aria-label and svg icon', () => {
+		// Arrange.
+		const setValue = jest.fn();
+		const props = { setValue, value: null, bind: 'display-conditions', propType };
+
+		// Act.
+		renderControl( <DisplayConditionsControl />, props );
+
+		// Assert.
+		const button = screen.getByRole( 'button', { name: 'Display Conditions' } );
+		expect( button ).toBeInTheDocument();
+		expect( button ).toHaveAttribute( 'aria-label', 'Display Conditions' );
+		
+		const svg = button.querySelector( 'svg' );
+		expect( svg ).toBeInTheDocument();
+	} );
+} );
+
