0d9f9dab42c7e04e55e59ae86278007b89fa96ca
elchugreeva <elenac@elementor.com>
2025-10-28 15:28:49 +0200

    lint

--- modules/atomic-widgets/assets/js/interactions.js
+++ modules/atomic-widgets/assets/js/interactions.js
@@ -1,51 +1,51 @@
 function initInteractions() {
-    if (typeof animate === 'undefined' && !window.Motion?.animate) {
-        setTimeout(initInteractions, 100);
-        return;
-    }
+	if ( typeof animate === 'undefined' && !window.Motion?.animate ) {
+		setTimeout( initInteractions, 100 );
+		return;
+	}
 
-    const animateFunc = typeof animate !== 'undefined' ? animate : window.Motion?.animate;
-    const inViewFunc = typeof inView !== 'undefined' ? inView : window.Motion?.inView;
-    
-    if (!inViewFunc || !animateFunc) {
-        return;
-    }
-    
-    const elements = document.querySelectorAll('[data-interactions]');
+	const animateFunc = typeof animate !== 'undefined' ? animate : window.Motion?.animate;
+	const inViewFunc = typeof inView !== 'undefined' ? inView : window.Motion?.inView;
 
-    elements.forEach((element) => {
+	if ( !inViewFunc || !animateFunc ) {
+		return;
+	}
 
-        const interactionsData = element.getAttribute('data-interactions');
-        let interactions = [];
-        
-        try {
-            interactions = JSON.parse(interactionsData);
-        } catch (error) {
-            return;
-        }
+	const elements = document.querySelectorAll( '[data-interactions]' );
 
-        interactions.forEach(() => {
-            try {
-                inViewFunc(element, () => {
-                    animateFunc(element, { 
-                        opacity: [0, 1], 
-                        x: [-100, 0] 
-                    }, { duration: 1, easing: 'ease-in-out' });
-                    
-                    return () => {}
-                
-                }, { 
-                    root: null, 
-                    amount: 0.1 
-                });
-            } catch (error) {
-            }
-        });
-    });
+	elements.forEach( ( element ) => {
+		const interactionsData = element.getAttribute( 'data-interactions' );
+		let interactions = [];
+
+		try {
+			interactions = JSON.parse( interactionsData );
+		} catch ( error ) {
+			return;
+		}
+
+		interactions.forEach( () => {
+			try {
+				inViewFunc( element, () => {
+					animateFunc( element, {
+						opacity: [ 0, 1 ],
+						x: [ -100, 0 ]
+					}, {
+						duration: 1,
+						easing: 'ease-in-out'
+					} );
+
+					return () => {};
+				}, {
+					root: null,
+					amount: 0.1
+				} );
+			} catch ( error ) {}
+		} );
+	} );
 }
 
-if (document.readyState === 'loading') {
-    document.addEventListener('DOMContentLoaded', initInteractions);
+if ( document.readyState === 'loading' ) {
+	document.addEventListener( 'DOMContentLoaded', initInteractions );
 } else {
-    initInteractions();
+	initInteractions();
 }
