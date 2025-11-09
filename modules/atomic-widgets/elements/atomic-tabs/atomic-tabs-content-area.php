8667fddd9354378ecc94b3daff2735af098970d4
IshayMaya <47317545+IshayMaya@users.noreply.github.com>
2025-11-09 15:29:05 +0200

    Internal: Add base tabs styles [ED-21442] (#33380)

--- modules/atomic-widgets/elements/atomic-tabs/atomic-tabs-content-area.php
+++ modules/atomic-widgets/elements/atomic-tabs/atomic-tabs-content-area.php
@@ -66,31 +66,17 @@ class Atomic_Tabs_Content_Area extends Atomic_Element_Base {
 	}
 
 	protected function define_base_styles(): array {
-		$display = String_Prop_Type::generate( 'block' );
+		$styles = [
+			'display' => String_Prop_Type::generate( 'block' ),
+		];
 
 		return [
 			static::BASE_STYLE_KEY => Style_Definition::make()
 				->add_variant(
 					Style_Variant::make()
-						->add_prop( 'display', $display )
-						->add_prop( 'padding', $this->get_base_padding() )
-						->add_prop( 'min-width', $this->get_base_min_width() )
+						->add_props( $styles )
 				),
 		];
-	}
-
-	protected function get_base_padding(): array {
-		return Size_Prop_Type::generate( [
-			'size' => 10,
-			'unit' => 'px',
-		] );
-	}
-
-	protected function get_base_min_width(): array {
-		return Size_Prop_Type::generate( [
-			'size' => 30,
-			'unit' => 'px',
-		] );
 	}
 
 	protected function add_render_attributes() {
