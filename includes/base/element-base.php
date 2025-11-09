b760007353e1929c8520772abb7f0087ad2de5be
Omer Haim Israeli <omeri@elementor.com>
2025-11-09 15:44:35 +0200

    wip

--- includes/base/element-base.php
+++ includes/base/element-base.php
@@ -524,10 +524,8 @@ abstract class Element_Base extends Controls_Stack {
 			echo $content; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 			$this->after_render();
 
-			$this->register_frontend_handlers();
-
 			// TODO: Remove this in the future
-			// Since version 3.24.0 page scripts/styles are handled by `page_assets`.			
+			// Since version 3.24.0 page scripts/styles are handled by `page_assets`.
 			$this->enqueue_scripts();
 			$this->enqueue_styles();
 		}
