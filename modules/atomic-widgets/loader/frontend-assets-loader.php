5ad0522a8b3816d90885c482cf1cce97d60ac860
Omer Haim Israeli <omeri@elementor.com>
2025-11-09 14:20:20 +0200

    wip

--- modules/atomic-widgets/loader/frontend-assets-loader.php
+++ modules/atomic-widgets/loader/frontend-assets-loader.php
@@ -8,7 +8,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 class Frontend_Assets_Loader {
-	const ALPINEJS_HANDLE = 'elementor-v2-alpinejs';
 	const FRONTEND_HANDLERS_HANDLE = 'elementor-v2-frontend-handlers';
 
 	/**
@@ -28,14 +27,6 @@ class Frontend_Assets_Loader {
 			self::FRONTEND_HANDLERS_HANDLE,
 			"{$assets_url}js/packages/frontend-handlers/frontend-handlers{$min_suffix}.js",
 			[ 'jquery' ],
-			ELEMENTOR_VERSION,
-			true
-		);
-
-		wp_register_script(
-			self::ALPINEJS_HANDLE,
-			"{$assets_url}js/packages/alpinejs/alpinejs{$min_suffix}.js",
-			[],
 			ELEMENTOR_VERSION,
 			true
 		);
