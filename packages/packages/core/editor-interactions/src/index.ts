2babf7c8d5601d1a1a027185983005a641a52d29
Netanel Baba <ntnelbaba@gmail.com>
2025-11-10 12:14:47 +0200

    wip

--- packages/packages/core/editor-interactions/src/index.ts
+++ packages/packages/core/editor-interactions/src/index.ts
@@ -2,7 +2,6 @@ export * from './components/empty-state';
 export * from './components/interactions-tab';
 export * from './types';
 export * from './utils/get-interactions-config';
-export * from './contexts/popup-state-contex';
 
 export { interactionsRepository } from './interactions-repository';
 export {
