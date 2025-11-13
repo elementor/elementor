edff8b9b04515ec19b63d2425e283026848430c7
Omer Haim Israeli <omeri@elementor.com>
2025-11-13 15:17:21 +0200

    wip

--- modules/atomic-widgets/elements/atomic-tabs/atomic-tabs-content-area.php
+++ modules/atomic-widgets/elements/atomic-tabs/atomic-tabs-content-area.php
@@ -94,7 +94,6 @@ class Atomic_Tabs_Content_Area extends Atomic_Element_Base {
 			],
 			'data-id' => $this->get_id(),
 			'data-interactions' => json_encode( $this->interactions ),
-			'x-ref' => 'tabsContentArea',
 		];
 
 		if ( ! empty( $settings['_cssid'] ) ) {
