57825c9baed42c36cbcced95e6812b88c679248c
IshayMaya <ishaym@elementor.com>
2025-11-13 10:34:10 +0200

    rearrange utils

--- modules/atomic-widgets/elements/atomic-tabs/atomic-tabs-handler.js
+++ modules/atomic-widgets/elements/atomic-tabs/atomic-tabs-handler.js
@@ -1,6 +1,9 @@
 import { register } from '@elementor/frontend-handlers';
 import { Alpine } from '@elementor/alpinejs';
 
+const TAB_ELEMENT_TYPE = 'e-tab';
+const TAB_CONTENT_ELEMENT_TYPE = 'e-tab-content';
+
 const SELECTED_CLASS = 'e--selected';
 const NAVIGATE_UP_KEYS = [ 'ArrowUp', 'ArrowLeft' ];
 const NAVIGATE_DOWN_KEYS = [ 'ArrowDown', 'ArrowRight' ];
@@ -9,48 +12,6 @@ register( {
 	elementType: 'e-tabs',
 	uniqueId: 'e-tabs-handler',
 	callback: ( { element, settings } ) => {
-		const TAB_ELEMENT_TYPE = 'e-tab';
-		const TAB_CONTENT_ELEMENT_TYPE = 'e-tab-content';
-
-		const getTabId = ( tabIndex ) => {
-			const tabsId = element.dataset.id;
-			return `${ tabsId }-tab-${ tabIndex }`;
-		};
-
-		const getTabContentId = ( tabIndex ) => {
-			const tabsId = element.dataset.id;
-			return `${ tabsId }-tab-content-${ tabIndex }`;
-		};
-
-		const getChildren = ( el, elementType ) => {
-			const parent = el.parentElement;
-
-			return Array.from( parent.children ).filter( ( child ) => {
-				return child.dataset.element_type === elementType;
-			} );
-		};
-
-		const getIndex = ( el, elementType ) => {
-			const children = getChildren( el, elementType );
-
-			return children.indexOf( el );
-		};
-
-		const getNextTab = ( key, tab ) => {
-			const tabs = getChildren( tab, TAB_ELEMENT_TYPE );
-			const tabsLength = tabs.length;
-
-			const currentIndex = getIndex( tab, TAB_ELEMENT_TYPE );
-
-			if ( NAVIGATE_DOWN_KEYS.includes( key ) ) {
-				return tabs[ ( currentIndex + 1 ) % tabsLength ];
-			}
-
-			if ( NAVIGATE_UP_KEYS.includes( key ) ) {
-				return tabs[ ( currentIndex - 1 + tabsLength ) % tabsLength ];
-			}
-		};
-
 		Alpine.data( 'atomicTabs', () => ( {
 			activeTab: settings[ 'default-active-tab' ],
 
@@ -129,6 +90,45 @@ register( {
 				},
 			},
 		} ) );
+
+		const getTabId = ( tabIndex ) => {
+			const tabsId = element.dataset.id;
+			return `${ tabsId }-tab-${ tabIndex }`;
+		};
+
+		const getTabContentId = ( tabIndex ) => {
+			const tabsId = element.dataset.id;
+			return `${ tabsId }-tab-content-${ tabIndex }`;
+		};
+
+		const getChildren = ( el, elementType ) => {
+			const parent = el.parentElement;
+
+			return Array.from( parent.children ).filter( ( child ) => {
+				return child.dataset.element_type === elementType;
+			} );
+		};
+
+		const getIndex = ( el, elementType ) => {
+			const children = getChildren( el, elementType );
+
+			return children.indexOf( el );
+		};
+
+		const getNextTab = ( key, tab ) => {
+			const tabs = getChildren( tab, TAB_ELEMENT_TYPE );
+			const tabsLength = tabs.length;
+
+			const currentIndex = getIndex( tab, TAB_ELEMENT_TYPE );
+
+			if ( NAVIGATE_DOWN_KEYS.includes( key ) ) {
+				return tabs[ ( currentIndex + 1 ) % tabsLength ];
+			}
+
+			if ( NAVIGATE_UP_KEYS.includes( key ) ) {
+				return tabs[ ( currentIndex - 1 + tabsLength ) % tabsLength ];
+			}
+		};
 	},
 } );
 
