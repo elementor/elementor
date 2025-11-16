5522944cf1247108f5501b16134bbc6f77e788f7
Avichay Eyal <avichaye@elementor.red>
2025-11-16 17:55:55 +0200

    unused

--- modules/atomic-widgets/prop-types/link-prop-type.php
+++ modules/atomic-widgets/prop-types/link-prop-type.php
@@ -15,29 +15,6 @@ class Link_Prop_Type extends Object_Prop_Type {
 		return 'link';
 	}
 
-	static function getDefaultMeta(): array {
-		return [
-			'llm' => [
-				'propType' => 'object',
-				'description' => 'Defines a link with destination URL and whether it opens in a new tab.',
-				'schema' => [
-					'type' => 'object',
-					'required' => [ 'destination' ],
-					'properties' => [
-						'destination' => [
-							'type' => [ 'string' ],
-							'description' => 'The URL or query object for the link destination.',
-						],
-						'isTargetBlank' => [
-							'type' => 'boolean',
-							'description' => 'Whether to open the link in a new tab.',
-						],
-					],
-				]
-			],
-		];
-	}
-
 	protected function define_shape(): array {
 		$target_blank_dependencies = Dependency_Manager::make()
 		->where( [
