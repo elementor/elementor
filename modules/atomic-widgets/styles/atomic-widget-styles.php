f0be25ae08f7ebc1463b4e72a9a310b7d30fc569
Ron Rostovsky <ronros@elementor.com>
2025-11-23 12:18:30 +0200

    cr

--- modules/atomic-widgets/styles/atomic-widget-styles.php
+++ modules/atomic-widgets/styles/atomic-widget-styles.php
@@ -71,9 +71,12 @@ class Atomic_Widget_Styles {
 			return;
 		}
 
+		$is_post_status_publish = self::CONTEXT_FRONTEND === $context;
+
+		// When a user publishes a post, we should invalidate the styles of the draft too
 		foreach ( $post_ids as $post_id ) {
 			do_action( 'elementor/atomic-widgets/styles/clear',
-				empty( $context ) || self::CONTEXT_FRONTEND === $context
+				empty( $context ) || $is_post_status_publish
 					? [ self::STYLES_KEY, $post_id ]
 					: [ self::STYLES_KEY, $post_id, $context ]
 			);
