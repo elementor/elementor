9fab74c83f384c857ab0a9c6fd8cea02bb81d728
Ron Rostovsky <ronros@elementor.com>
2025-11-23 08:12:53 +0200

    wip

--- packages/packages/core/editor-global-classes/src/sync-with-document-save.ts
+++ packages/packages/core/editor-global-classes/src/sync-with-document-save.ts
@@ -1,6 +1,6 @@
 import { getCurrentUser } from '@elementor/editor-current-user';
 import { setDocumentModifiedStatus } from '@elementor/editor-documents';
-import { __privateListenTo as listenTo, type CommandEvent, commandStartEvent } from '@elementor/editor-v1-adapters';
+import { registerDataHook } from '@elementor/editor-v1-adapters';
 import { __getState as getState, __subscribeWithSelector as subscribeWithSelector } from '@elementor/store';
 
 import { UPDATE_CLASS_CAPABILITY_KEY } from './capabilities';
@@ -26,25 +26,21 @@ function syncDirtyState() {
 }
 
 function bindSaveAction( panelActions?: { open: () => void } ) {
-	listenTo( commandStartEvent( 'document/save/save' ), ( e ) => {
-		if ( e.type !== 'command' ) {
-			return;
-		}
-
-		const commandEvent = e as CommandEvent< { status: string } >;
-
+	registerDataHook( 'dependency', 'document/save/save', ( args ) => {
 		const user = getCurrentUser();
 
 		const canEdit = user?.capabilities.includes( UPDATE_CLASS_CAPABILITY_KEY );
 
 		if ( ! canEdit ) {
-			return;
+			return true;
 		}
 
 		saveGlobalClasses( {
-			context: commandEvent.args.status === 'publish' ? 'frontend' : 'preview',
+			context: args.status === 'publish' ? 'frontend' : 'preview',
 			onApprove: panelActions?.open,
 		} );
+
+		return true;
 	} );
 }
 
