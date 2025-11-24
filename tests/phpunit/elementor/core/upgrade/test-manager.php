6ab72d66c7afc567d405e26f1acb7f9092676647
Marcin Skowronek <marcins@elementor.red>
2025-11-24 08:40:54 +0100

    pr fixes

--- tests/phpunit/elementor/core/upgrade/test-manager.php
+++ tests/phpunit/elementor/core/upgrade/test-manager.php
@@ -1,7 +1,6 @@
 <?php
 namespace Elementor\Tests\Phpunit\Elementor\Core\Upgrade;
 
-use Elementor\Core\Upgrade\Manager;
 use Elementor\Plugin;
 use ElementorEditorTesting\Elementor_Test_Base;
 
@@ -28,29 +27,5 @@ class Test_Manager extends Elementor_Test_Base {
 		$this->assertContains( 'Elementor/Upgrades - upgrade_callback Start ', $messages );
 		$this->assertContains( 'Elementor/Upgrades - upgrade_callback Finished', $messages );
 		$this->assertContains( 'Elementor data updater process has been completed.', $messages );
-	}
-
-	public function test_had_install_prior_to_returns_false_for_empty_history() {
-		delete_option( Manager::get_install_history_meta() );
-
-		$this->assertFalse( Manager::had_install_prior_to( '3.33.2' ) );
-	}
-
-	public function test_had_install_prior_to_returns_false_when_first_install_matches_target() {
-		update_option( Manager::get_install_history_meta(), [
-			'3.33.2' => time(),
-			'3.35.0' => time() + 10,
-		] );
-
-		$this->assertFalse( Manager::had_install_prior_to( '3.33.2' ) );
-	}
-
-	public function test_had_install_prior_to_returns_true_when_history_contains_older_version() {
-		update_option( Manager::get_install_history_meta(), [
-			'3.35.0' => time() + 10,
-			'3.30.0' => time(),
-		] );
-
-		$this->assertTrue( Manager::had_install_prior_to( '3.33.2' ) );
 	}
 }
