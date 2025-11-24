01ad1eed185f7143b1a553544e8c4a88d6ebd6d0
Ron Rostovsky <ronros@elementor.com>
2025-11-24 17:16:47 +0200

    wip

--- packages/packages/core/editor-editing-panel/src/components/settings-tab.tsx
+++ packages/packages/core/editor-editing-panel/src/components/settings-tab.tsx
@@ -1,23 +1,14 @@
 import * as React from 'react';
-import { ControlAdornmentsProvider } from '@elementor/editor-controls';
 import {
 	type Control,
 	type ControlItem,
-	type ControlLayout,
 	type Element,
 	type ElementControl,
 } from '@elementor/editor-elements';
 import { SessionStorageProvider } from '@elementor/session';
-import { Divider } from '@elementor/ui';
 
 import { useElement } from '../contexts/element-context';
-import { Control as BaseControl } from '../controls-registry/control';
-import { ControlTypeContainer } from '../controls-registry/control-type-container';
-import { controlsRegistry, type ControlType } from '../controls-registry/controls-registry';
-import { SettingsField } from '../controls-registry/settings-field';
-import { getFieldIndicators } from '../field-indicators-registry';
 import { useDefaultPanelSettings } from '../hooks/use-default-panel-settings';
-import { ControlLabel } from './control-label';
 import { Section } from './section';
 import { SectionsList } from './sections-list';
 import { SettingsControl } from './settings-control';
@@ -65,63 +56,6 @@ export const SettingsTab = () => {
 	);
 };
 
-const Control = ( { control: { value, type } }: { control: Control | ElementControl } ) => {
-	if ( ! controlsRegistry.get( value.type as ControlType ) ) {
-		return null;
-	}
-
-	const layout = value.meta?.layout || controlsRegistry.getLayout( value.type as ControlType );
-	const controlProps = populateChildControlProps( value.props );
-
-	if ( layout === 'custom' ) {
-		controlProps.label = value.label;
-	}
-
-	if ( type === 'element-control' ) {
-		return <ControlLayout control={ value } layout={ layout } controlProps={ controlProps } />;
-	}
-
-	return (
-		<SettingsField bind={ value.bind } propDisplayName={ value.label || value.bind }>
-			<ControlLayout control={ value } layout={ layout } controlProps={ controlProps } />
-		</SettingsField>
-	);
-};
-
-const ControlLayout = ( {
-	control,
-	layout,
-	controlProps,
-}: {
-	control: Control[ 'value' ] | ElementControl[ 'value' ];
-	layout: ControlLayout;
-	controlProps: Record< string, unknown >;
-} ) => (
-	<ControlAdornmentsProvider items={ getFieldIndicators( 'settings' ) }>
-		{ control.meta?.topDivider && <Divider /> }
-		<ControlTypeContainer layout={ layout }>
-			{ control.label && layout !== 'custom' ? <ControlLabel>{ control.label }</ControlLabel> : null }
-			<BaseControl type={ control.type as ControlType } props={ controlProps } />
-		</ControlTypeContainer>
-	</ControlAdornmentsProvider>
-);
-
-function populateChildControlProps( props: Record< string, unknown > ) {
-	if ( props.childControlType ) {
-		const childComponent = controlsRegistry.get( props.childControlType as ControlType );
-		const childPropType = controlsRegistry.getPropTypeUtil( props.childControlType as ControlType );
-		props = {
-			...props,
-			childControlConfig: {
-				component: childComponent,
-				props: props.childControlProps || {},
-				propTypeUtil: childPropType,
-			},
-		};
-	}
-
-	return props;
-}
 
 function getKey( control: Control | ElementControl, element: Element ) {
 	if ( control.type === 'control' ) {
