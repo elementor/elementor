279e632d94f882a8e5241e9f639097e4c3059422
Mike Kokhanov <mikek@elementor.com>
2025-11-25 09:31:38 +0200

    Internal: Interactions - id logic implementation [ED-21719]

--- tests/phpunit/elementor/modules/interactions/test-validation.php
+++ tests/phpunit/elementor/modules/interactions/test-validation.php
@@ -2,6 +2,8 @@
 
 use PHPUnit\Framework\TestCase;
 
+use \Exception;
+
 use Elementor\Modules\Interactions\Presets;
 use Elementor\Modules\Interactions\Validation;
 
@@ -103,8 +105,18 @@ class Test_Validation extends TestCase {
 					'settings' => [],
 					'interactions' => json_encode([
 						'items' => [
-							'unknown-interaction-id1',
-							'unknown-interaction-id2',
+							[
+								'animation' => [
+									'animation_type' => 'full-preset',
+									'animation_id' => 'unknown-interaction-id1',
+								],
+							],
+							[
+								'animation' => [
+									'animation_type' => 'full-preset',
+									'animation_id' => 'unknown-interaction-id2',
+								],
+							],
 						],
 						'version' => 1,
 					]),
@@ -115,8 +127,18 @@ class Test_Validation extends TestCase {
 							'widgetType' => 'e-heading',
 							'interactions' => json_encode([
 								'items' => [
-									'unknown-interaction-id3',
-									'unknown-interaction-id4',
+									[
+										'animation' => [
+											'animation_type' => 'full-preset',
+											'animation_id' => 'unknown-interaction-id3',
+										],
+									],
+									[
+										'animation' => [
+											'animation_type' => 'full-preset',
+											'animation_id' => 'unknown-interaction-id4',
+										],
+									],
 								],
 								'version' => 1,
 							]),
@@ -147,7 +169,12 @@ class Test_Validation extends TestCase {
 					'settings' => [],
 					'interactions' => json_encode([
 						'items' => [
-							'load-fade-in--100-0',
+							[
+								'animation' => [
+									'animation_type' => 'full-preset',
+									'animation_id' => 'load-fade-in--100-0',
+								],
+							],
 						],
 						'version' => 1,
 					]),
@@ -158,7 +185,12 @@ class Test_Validation extends TestCase {
 							'widgetType' => 'e-heading',
 							'interactions' => json_encode([
 								'items' => [
-									'load-fade-in-top-200-0',
+									[
+										'animation' => [
+											'animation_type' => 'full-preset',
+											'animation_id' => 'load-fade-in-top-200-0',
+										],
+									]
 								],
 								'version' => 1,
 							]),
@@ -180,8 +212,18 @@ class Test_Validation extends TestCase {
 					'settings' => [],
 					'interactions' => json_encode( [
 						'items' => [
-							'load-fade-in--100-0',
-							'unknown-interaction-id1',
+							[
+								'animation' => [
+									'animation_type' => 'full-preset',
+									'animation_id' => 'load-fade-in--100-0',
+								],
+							],
+							[
+								'animation' => [
+									'animation_type' => 'full-preset',
+									'animation_id' => 'unknown-interaction-id1',
+								],
+							],
 						],
 						'version' => 1,
 					] ),
@@ -192,13 +234,82 @@ class Test_Validation extends TestCase {
 							'widgetType' => 'e-heading',
 							'interactions' => json_encode( [
 								'items' => [
-									'load-fade-in-top-200-0',
-									'unknown-interaction-id2',
+									[
+										'animation' => [
+											'animation_type' => 'full-preset',
+											'animation_id' => 'load-fade-in-top-200-0',
+										],
+									],
+									[
+										'animation' => [
+											'animation_type' => 'full-preset',
+											'animation_id' => 'unknown-interaction-id2',
+										],
+									],
 								],
 								'version' => 1,
 							] ),
 						],
 					],
+				],
+			],
+		];
+	}
+
+	public function test_sanitize__will_throw_if_number_of_interactions_per_element_exceeds_the_limit() {
+		$document = $this->mock_document_data__with_interactions_overflow( 6 );
+
+		$this->expectException( Exception::class );
+
+		$validation = $this->validation( [
+			[
+				'value' => 'load-fade-in--100-0',
+				'label' => 'Page load: Fade In (100ms/0ms)',
+			],
+		] );
+
+		$validation->sanitize( $document );
+		$validation->validate();
+	}
+
+	public function test_validate__will_not_throw_if_number_of_interactions_per_element_within_the_limit() {
+		$document = $this->mock_document_data__with_interactions_overflow( 5 );
+
+		$validation = $this->validation( [
+			[
+				'value' => 'load-fade-in--100-0',
+				'label' => 'Page load: Fade In (100ms/0ms)',
+			],
+		] );
+
+		$validation->sanitize( $document );
+		$validation->validate();
+
+		$this->assertTrue( true, 'No exception was thrown' );
+	}
+
+	private function mock_document_data__with_interactions_overflow( $number_of_interactions ) {
+		$interactions = [];
+		while ( 0 < $number_of_interactions ) {
+			$interactions[] = [
+				'animation' => [
+					'animation_type' => 'full-preset',
+					'animation_id' => 'load-fade-in--100-0',
+				],
+			];
+			--$number_of_interactions;
+		}
+
+		return [
+			'elements' => [
+				[
+					'id' => '1',
+					'elType' => 'e-flexbox',
+					'settings' => [],
+					'interactions' => json_encode( [
+						'items' => $interactions,
+						'version' => 1,
+					] ),
 				],
 			],
 		];
