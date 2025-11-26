f742a22d0f0c59fbf33002e37d03cbc9a2204ffa
Nicola Peluchetti <nicola.peluchetti@gmail.com>
2025-11-27 01:04:29 +0400

    Internal: create experiment [ED-21800]

---
+++ modules/editor-one/module.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Elementor\Modules\EditorOne;
+
+use Elementor\Core\Base\Module as BaseModule;
+use Elementor\Core\Experiments\Manager as Experiments_Manager;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+class Module extends BaseModule {
+
+	const EXPERIMENT_NAME = 'e_editor_one';
+
+	public function get_name() {
+		return 'editor-one';
+	}
+
+	public static function get_experimental_data() {
+		return [
+			'name'           => static::EXPERIMENT_NAME,
+			'title'          => esc_html__( 'Editor One', 'elementor' ),
+			'description'    => esc_html__( 'General', 'elementor' ),
+			'hidden'         => true,
+			'default'        => Experiments_Manager::STATE_INACTIVE,
+			'release_status' => Experiments_Manager::RELEASE_STATUS_DEV,
+		];
+	}
+}
+
