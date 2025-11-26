e3f1c6f2ab947221eae46a57b18d0c1e57fe32fc
YaelAvitan <yael6768889@gmail.com>
2025-11-26 12:37:56 +0200

    Update packages/packages/core/editor-canvas/src/components/elements-overlays.tsx
    Co-authored-by: Ronen Boxer <145427686+ronenelementor@users.noreply.github.com>

--- packages/packages/core/editor-canvas/src/components/elements-overlays.tsx
+++ packages/packages/core/editor-canvas/src/components/elements-overlays.tsx
@@ -42,19 +42,16 @@ export function ElementsOverlays() {
 		const isSelected = selected.element?.id === id;
 
 		return overlayRegistry
-			.filter( ( overlay ) => overlay.shouldRender( { id, element, isSelected } ) )
-			.map( ( overlay, index ) => {
-				const OverlayComponent = overlay.component;
-
-				return (
-					<OverlayComponent
+			.map( ( { shouldRender, component: Overlay }, index ) =>
+				shouldRender( { id, element, isSelected } ) && (
+					<Overlay
 						key={ `${ id }-${ index }` }
 						id={ id }
 						element={ element }
 						isSelected={ isSelected }
 					/>
-				);
-			} );
+				)
+			);
 	} );
 }
 
