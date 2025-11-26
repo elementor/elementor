a658f6caa427db28794cd87311c92f62290af443
YaelAvitan <yael6768889@gmail.com>
2025-11-26 10:56:58 +0200

    change to Coded list

--- packages/packages/core/editor-canvas/src/utils/inline-editing-utils.ts
+++ packages/packages/core/editor-canvas/src/utils/inline-editing-utils.ts
@@ -1,13 +1,11 @@
 import { getContainer, getElementType, type V1Element } from '@elementor/editor-elements';
 
+const INLINE_EDITABLE_WIDGET_TYPES = new Set( [ 'e-heading', 'e-paragraph' ] );
+
 const getHtmlPropertyName = ( container: V1Element | null ): string => {
 	const widgetType = container?.model?.get( 'widgetType' ) ?? container?.model?.get( 'elType' );
 
-	if ( ! widgetType ) {
-		return '';
-	}
-
-	const propsSchema = getElementType( widgetType )?.propsSchema;
+	const propsSchema = widgetType ? getElementType( widgetType )?.propsSchema : null;
 
 	if ( ! propsSchema ) {
 		return '';
@@ -20,7 +18,13 @@ const getHtmlPropertyName = ( container: V1Element | null ): string => {
 
 export const hasInlineEditableProperty = ( containerId: string ): boolean => {
 	const container = getContainer( containerId );
-	return !! getHtmlPropertyName( container );
+	const widgetType = container?.model?.get( 'widgetType' ) ?? container?.model?.get( 'elType' );
+
+	if ( ! widgetType ) {
+		return false;
+	}
+
+	return INLINE_EDITABLE_WIDGET_TYPES.has( widgetType );
 };
 
 export const getInlineEditablePropertyName = ( container: V1Element | null ): string => {
