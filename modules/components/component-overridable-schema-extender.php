77536c2f38c7a0eb4ea3b2898d2ade384c6384fa
Miryam Oren <77922014+MiryamOren@users.noreply.github.com>
2025-11-27 17:15:44 +0200

    fix Component_Overridable_Schema_Extender::get_prop_types_to_add signture

--- modules/components/component-overridable-schema-extender.php
+++ modules/components/component-overridable-schema-extender.php
@@ -16,7 +16,7 @@ class Component_Overridable_Schema_Extender extends Prop_Types_Schema_Extender {
 		return new static();
 	}
 
-	protected function get_prop_types_to_add( Prop_Type $prop_type, array $nested_prop_types ): array {
+	protected function get_prop_types_to_add( Prop_Type $prop_type ): array {
 		$is_ignore_overridable_applied = ! $prop_type->get_meta_item( Component_Overridable_Prop_Type::META_KEY, true );
 		$is_classes_prop = Atomic_Elements_Utils::is_classes_prop( $prop_type );
 
