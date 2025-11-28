605efbbb729d7f30a2ae671b6572fd322044fba7
Hein van Vlastuin <hein@obox.co.za>
2025-11-28 16:19:00 +0200

    wip

--- core/common/modules/events-manager/assets/js/module.js
+++ core/common/modules/events-manager/assets/js/module.js
@@ -19,9 +19,7 @@ export default class extends elementorModules.Module {
 				record_max_ms: 300000,
 				record_mask_text_selector: '',
 				flags: true,
-				api_host: {
-					flags: 'https://api-eu.mixpanel.com',
-				},
+				api_host: 'https://api-eu.mixpanel.com',
 			},
 		);
 
