9cd451beae5559fe17c06638b1dc69b7adf34183
Netanel Baba <ntnelbaba@gmail.com>
2025-11-30 16:02:53 +0200

    lint

--- packages/packages/core/editor-interactions/src/components/interactions-tab.tsx
+++ packages/packages/core/editor-interactions/src/components/interactions-tab.tsx
@@ -20,7 +20,7 @@ export const InteractionsTab = ( { elementId }: { elementId: string } ) => {
 function InteractionsTabContent( { elementId }: { elementId: string } ) {
 	const existingInteractions = useElementInteractions( elementId );
 	const firstInteractionState = useState< boolean >( false );
-	const hasInteractions = existingInteractions?.items?.length || firstInteractionState[0];
+	const hasInteractions = existingInteractions?.items?.length || firstInteractionState[ 0 ];
 
 	return (
 		<SessionStorageProvider prefix={ elementId }>
@@ -31,7 +31,7 @@ function InteractionsTabContent( { elementId }: { elementId: string } ) {
 			) : (
 				<EmptyState
 					onCreateInteraction={ () => {
-						firstInteractionState[1]( true );
+						firstInteractionState[ 1 ]( true );
 					} }
 				/>
 			) }
@@ -39,12 +39,16 @@ function InteractionsTabContent( { elementId }: { elementId: string } ) {
 	);
 }
 
-function InteractionsContent( { firstInteractionState }: { firstInteractionState: [ boolean, ( value: boolean ) => void ] } ) {
+function InteractionsContent( {
+	firstInteractionState,
+}: {
+	firstInteractionState: [ boolean, ( value: boolean ) => void ];
+} ) {
 	const { interactions, setInteractions, playInteractions } = useInteractionsContext();
 
 	const applyInteraction = useCallback(
 		( newInteractions: ElementInteractions ) => {
-			firstInteractionState[1]( false );
+			firstInteractionState[ 1 ]( false );
 			if ( ! newInteractions ) {
 				setInteractions( undefined );
 				return;
@@ -52,13 +56,13 @@ function InteractionsContent( { firstInteractionState }: { firstInteractionState
 
 			setInteractions( newInteractions );
 		},
-		[ setInteractions ]
+		[ setInteractions, firstInteractionState ]
 	);
 
 	return (
 		<Stack sx={ { m: 1, p: 1.5 } } gap={ 2 }>
 			<InteractionsList
-				triggerCreateOnShowEmpty={ firstInteractionState[0] }
+				triggerCreateOnShowEmpty={ firstInteractionState[ 0 ] }
 				interactions={ interactions }
 				onSelectInteractions={ applyInteraction }
 				onPlayInteraction={ playInteractions }
