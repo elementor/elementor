2ddfc9b86ac213dfa3113ecf72c83565ccfdb219
IshayMaya <47317545+IshayMaya@users.noreply.github.com>
2025-12-02 15:02:12 +0200

    Internal: Update document status on components changes [ED-21390] (#33673)

--- packages/packages/core/editor-components/src/components/edit-component/__tests__/edit-component.test.tsx
+++ packages/packages/core/editor-components/src/components/edit-component/__tests__/edit-component.test.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { createDOMElement, createMockDocument, renderWithStore } from 'test-utils';
-import { getV1DocumentsManager, type V1Document } from '@elementor/editor-documents';
+import { getV1DocumentsManager, type V1Document, type V1DocumentsManager } from '@elementor/editor-documents';
 import { __privateListenTo, __privateRunCommand } from '@elementor/editor-v1-adapters';
 import { __createStore, __registerSlice as registerSlice, type SliceState, type Store } from '@elementor/store';
 import { act, fireEvent, screen } from '@testing-library/react';
@@ -65,7 +65,7 @@ describe( '<EditComponent />', () => {
 			getInitialId: () => MOCK_DOCUMENT_ID,
 			invalidateCache: jest.fn(),
 			documents: {},
-		} );
+		} as unknown as V1DocumentsManager );
 
 		jest.mocked( switchDocumentCallback ).mockImplementation( ( { id }: { id: number } ) => {
 			editedDocument = MOCK_POST_DATA[ id as keyof typeof MOCK_POST_DATA ];
