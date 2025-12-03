36d48d3dc1d33332ecb1b011f458a4c11f1e1250
Ron Rostovsky <ronros@elementor.com>
2025-12-03 09:30:07 +0200

    cr

--- packages/packages/core/editor-components/src/store/set-overridable-prop.ts
+++ packages/packages/core/editor-components/src/store/set-overridable-prop.ts
@@ -3,7 +3,7 @@ import { __dispatch as dispatch, __getState as getState } from '@elementor/store
 import { generateUniqueId } from '@elementor/utils';
 import { __ } from '@wordpress/i18n';
 
-import { type OverridableProp, type OverridablePropGroup, type OverridableProps } from '../types';
+import { type OverridableProp, type OverridableProps, type OverridablePropsGroup } from '../types';
 import { selectOverridableProps, slice } from './store';
 
 type Props = {
@@ -34,26 +34,17 @@ export function setOverridableProp( {
 		return;
 	}
 
-	const existingOverridableProp = overrideKey
-		? overridableProps.props[ overrideKey ]
-		: null;
+	const existingOverridableProp = overrideKey ? overridableProps.props[ overrideKey ] : null;
 
-	let currentGroupId = groupId || existingOverridableProp?.groupId;
+	const { props: existingProps, groups: existingGroups } = { ...overridableProps };
 
-	const { props, groups } = { ...overridableProps };
-
-	const updatedGroups = { ...groups };
-
-	if ( ! currentGroupId && updatedGroups.order.length > 0 ) {
-		currentGroupId = updatedGroups.order[ 0 ];
-	} else if ( ! currentGroupId && updatedGroups.order.length === 0 ) {
-		currentGroupId = addNewGroup( updatedGroups );
-	} else if ( ! currentGroupId || ! updatedGroups.items[ currentGroupId ] ) {
-		currentGroupId = addNewGroup( updatedGroups, currentGroupId as string );
-	}
+	const { updatedGroups, currentGroupId } = getUpdatedGroups(
+		existingGroups,
+		groupId || existingOverridableProp?.groupId
+	);
 
 	const overridableProp = {
-		overrideKey: existingOverridableProp?.overrideKey || generateUniqueId( 'overridable-prop' ),
+		overrideKey: existingOverridableProp?.overrideKey || generateUniqueId( 'prop' ),
 		label,
 		elementId,
 		propKey,
@@ -63,48 +54,71 @@ export function setOverridableProp( {
 		groupId: currentGroupId,
 	};
 
-	const group: OverridablePropGroup = { ...updatedGroups.items[ currentGroupId ] };
+	const props = {
+		...existingProps,
+		[ overridableProp.overrideKey ]: overridableProp,
+	};
 
-	if ( existingOverridableProp && existingOverridableProp.groupId !== currentGroupId ) {
-		const oldGroup = updatedGroups.items[ existingOverridableProp.groupId ];
-
-		if ( oldGroup ) {
-			oldGroup.props = oldGroup.props.filter( ( key ) => key !== overridableProp.overrideKey );
-		}
-	}
-
-	if ( ! group.props.includes( overridableProp.overrideKey ) ) {
-		group.props = [ ...group.props, overridableProp.overrideKey ];
-	}
-
-	const newOverridableProps = {
-		props: {
-			...props,
-			[ overridableProp.overrideKey ]: overridableProp,
+	const groups = {
+		items: {
+			...updatedGroups.items,
+			[ currentGroupId ]: getGroupWithProp( updatedGroups, currentGroupId, overridableProp ),
 		},
-		groups: {
-			items: {
-				...updatedGroups.items,
-				[ currentGroupId ]: group,
-			},
-			order: updatedGroups.order.includes( currentGroupId )
-				? updatedGroups.order
-				: [ ...updatedGroups.order, currentGroupId ],
-		},
-	} satisfies OverridableProps;
+		order: updatedGroups.order.includes( currentGroupId )
+			? updatedGroups.order
+			: [ ...updatedGroups.order, currentGroupId ],
+	};
+
+	const isChangingGroups = existingOverridableProp && existingOverridableProp.groupId !== currentGroupId;
+
+	if ( isChangingGroups ) {
+		groups.items[ existingOverridableProp.groupId ] = getGroupWithoutProp(
+			updatedGroups,
+			existingOverridableProp.groupId,
+			overridableProp
+		);
+	}
 
 	dispatch(
 		slice.actions.setOverridableProps( {
 			componentId,
-			overridableProps: newOverridableProps,
+			overridableProps: {
+				props,
+				groups,
+			},
 		} )
 	);
 
 	return overridableProp;
 }
 
+function getUpdatedGroups(
+	groups: OverridableProps[ 'groups' ],
+	groupId: string | undefined
+): { updatedGroups: OverridableProps[ 'groups' ]; currentGroupId: string } {
+	const updatedGroups = { ...groups };
+
+	if ( ! groupId ) {
+		// use first existing group
+		if ( updatedGroups.order.length > 0 ) {
+			return { updatedGroups, currentGroupId: updatedGroups.order[ 0 ] };
+		}
+
+		// create the first group (default)
+		return { updatedGroups, currentGroupId: addNewGroup( updatedGroups ) };
+	}
+
+	if ( ! updatedGroups.items[ groupId ] ) {
+		// fallback - if for any reason there's no such group - create it
+		return { updatedGroups, currentGroupId: addNewGroup( updatedGroups, groupId ) };
+	}
+
+	// use the existing group
+	return { updatedGroups, currentGroupId: groupId };
+}
+
 function addNewGroup( groups: OverridableProps[ 'groups' ], groupId?: string | undefined ): string {
-	const currentGroupId = groupId || generateUniqueId( 'overridable-group' );
+	const currentGroupId = groupId || generateUniqueId( 'group' );
 
 	groups.items = {
 		...groups.items,
@@ -118,4 +132,32 @@ function addNewGroup( groups: OverridableProps[ 'groups' ], groupId?: string | u
 	groups.order = [ ...groups.order, currentGroupId ];
 
 	return currentGroupId;
+}
+
+function getGroupWithProp(
+	groups: OverridableProps[ 'groups' ],
+	groupId: string,
+	overridableProp: OverridableProp
+): OverridablePropsGroup {
+	const group: OverridablePropsGroup = { ...groups.items[ groupId ] };
+
+	if ( ! group.props.includes( overridableProp.overrideKey ) ) {
+		group.props = [ ...group.props, overridableProp.overrideKey ];
+	}
+
+	return group;
+}
+
+function getGroupWithoutProp(
+	groups: OverridableProps[ 'groups' ],
+	groupId: string,
+	overridableProp: OverridableProp
+): OverridablePropsGroup {
+	const group = { ...groups.items[ groupId ] };
+
+	if ( group ) {
+		group.props = group.props.filter( ( key ) => key !== overridableProp.overrideKey );
+	}
+
+	return group;
 }
