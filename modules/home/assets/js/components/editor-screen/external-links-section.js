ce47caa27907ab3353cb68343bb3a0cccf76bfcb
Hein van Vlastuin <hein@obox.co.za>
2025-12-04 15:35:13 +0200

    wip

--- modules/home/assets/js/components/editor-screen/external-links-section.js
+++ modules/home/assets/js/components/editor-screen/external-links-section.js
@@ -6,11 +6,11 @@ import ListItemText from '@elementor/ui/ListItemText';
 const ExternalLinksSection = ( { ...props } ) => {
 	return (
 		<Paper elevation={ 0 } sx={ { py: 3, px: { xs: 3, md: 4 }, borderRadius: 1, border: '1px solid rgba(0, 0, 0, 0.12)' } }>
-			<List sx={ { display: 'flex', flexDirection: 'row', rowGap: 2, columnGap: 15 } }>
+			<List sx={ { display: 'flex', flexDirection: 'row', rowGap: 2, columnGap: 7.5 } }>
 				{
 					props.externalLinksData.map( ( item ) => {
 						return (
-							<Box key={ item.label } sx={ { display: 'flex', alignItems: 'center', width: 15.5 } }>
+							<Box key={ item.label } sx={ { display: 'flex', alignItems: 'center' } }>
 								<ListItemButton href={ item.url } target="_blank" sx={ { '&:hover': { backgroundColor: 'initial' }, gap: 2, px: 0, py: 0 } }>
 									<Box component="img" src={ item.image } sx={ { width: '38px' } }></Box>
 									<ListItemText sx={ { color: 'text.secondary' } } primary={ item.label } />
