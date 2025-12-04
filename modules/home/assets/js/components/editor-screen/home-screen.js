8b3391c48f696b1fdaa9a35eb4058d66f448b029
Hein van Vlastuin <hein@obox.co.za>
2025-12-04 15:37:54 +0200

    Update main gap

--- modules/home/assets/js/components/editor-screen/home-screen.js
+++ modules/home/assets/js/components/editor-screen/home-screen.js
@@ -13,10 +13,10 @@ const EditorScreen = ( props ) => {
 	return (
 		/*  Box wrapper around the Container is needed to neutralize wp-content area left-padding */
 		<Box sx={ { pr: 1 } }>
-			<Container disableGutters={ true } maxWidth="lg" sx={ { display: 'flex', flexDirection: 'column', gap: { xs: 1, md: 3 }, pt: { xs: 2, md: 6 }, pb: 2 } }>
+			<Container disableGutters={ true } maxWidth="lg" sx={ { display: 'flex', flexDirection: 'column', gap: { xs: 1, md: 2.5 }, pt: { xs: 2, md: 6 }, pb: 2 } }>
 				{ props.homeScreenData.top_with_licences && <TopSection topData={ props.homeScreenData.top_with_licences } buttonCtaUrl={ props.homeScreenData.button_cta_url } /> }
-				<Box sx={ { display: 'flex', flexDirection: { xs: 'column', sm: 'row' }, justifyContent: 'space-between', gap: 3 } }>
-					<Stack sx={ { flex: 1, gap: 3 } }>
+				<Box sx={ { display: 'flex', flexDirection: { xs: 'column', sm: 'row' }, justifyContent: 'space-between', gap: 2.5 } }>
+					<Stack sx={ { flex: 1, gap: 2.5 } }>
 						{ props.homeScreenData.create_with_ai && <CreateWithAIBanner createWithAIData={ props.homeScreenData.create_with_ai } /> }
 						<GetStarted
 							getStartedData={ props.homeScreenData.get_started }
@@ -30,7 +30,7 @@ const EditorScreen = ( props ) => {
 						) }
 						<ExternalLinksSection externalLinksData={ props.homeScreenData.external_links } />
 					</Stack>
-					<Container maxWidth="xs" disableGutters={ true } sx={ { width: { sm: '305px' }, display: 'flex', flexDirection: 'column', gap: 3 } }>
+					<Container maxWidth="xs" disableGutters={ true } sx={ { width: { sm: '305px' }, display: 'flex', flexDirection: 'column', gap: 2.5 } }>
 						{ hasSidebarPromotion &&
 							<SideBarPromotion sideData={ props.homeScreenData.sidebar_promotion_variants } />
 						}
