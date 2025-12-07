b033910ed6223ac7d76e15eef52246820c208ec4
Omer Haim Israeli <omeri@elementor.com>
2025-12-07 16:57:43 +0200

    wip

---
+++ packages/packages/core/editor-components/src/components/component-panel-header/component-badge.tsx
@@ -0,0 +1,60 @@
+import * as React from 'react';
+import { useEffect, useRef } from 'react';
+import { SettingsIcon } from '@elementor/icons';
+import { Badge, Box, keyframes, styled, ToggleButton } from '@elementor/ui';
+import { __ } from '@wordpress/i18n';
+
+export const ComponentsBadge = ( { overridesCount }: { overridesCount: number } ) => {
+	const prevCount = usePrevious( overridesCount );
+
+	const isFirstOverride = prevCount === 0 && overridesCount === 1;
+
+	return (
+		<StyledBadge
+			color="primary"
+			key={ overridesCount }
+			invisible={ overridesCount === 0 }
+			animate={ isFirstOverride }
+			anchorOrigin={ { vertical: 'top', horizontal: 'right' } }
+			badgeContent={
+				<Box sx={ { animation: isFirstOverride ? `${ slideUp } 300ms ease-out` : 'none' } }>
+					{ overridesCount }
+				</Box>
+			}
+		>
+			<ToggleButton value="overrides" size="tiny" aria-label={ __( 'View overrides', 'elementor' ) }>
+				<SettingsIcon fontSize="tiny" />
+			</ToggleButton>
+		</StyledBadge>
+	);
+};
+
+const StyledBadge = styled( Badge )( ( { theme, animate } ) => ( {
+	'& .MuiBadge-badge': {
+		minWidth: theme.spacing( 2 ),
+		height: theme.spacing( 2 ),
+		minHeight: theme.spacing( 2 ),
+		maxWidth: theme.spacing( 2 ),
+		fontSize: theme.typography.caption.fontSize as string,
+		animation: animate ? `${ bounceIn } 300ms ease-out` : 'none',
+	},
+} ) );
+
+function usePrevious< T >( value: T ) {
+	const ref = useRef< T >( value );
+	useEffect( () => {
+		ref.current = value;
+	}, [ value ] );
+	return ref.current;
+}
+
+const bounceIn = keyframes`
+	0% { transform: scale(0) translate(50%, 50%); opacity: 0; }
+	70% { transform: scale(1.1) translate(50%, -50%); opacity: 1; }
+	100% { transform: scale(1) translate(50%, -50%); opacity: 1; }
+`;
+
+const slideUp = keyframes`
+	from { transform: translateY(100%); opacity: 0; }
+	to { transform: translateY(0); opacity: 1; }
+`;
--- packages/packages/core/editor-components/src/components/component-panel-header/component-panel-header.tsx
+++ packages/packages/core/editor-components/src/components/component-panel-header/component-panel-header.tsx
@@ -1,44 +1,22 @@
 import * as React from 'react';
-import { useEffect, useRef } from 'react';
 import { getV1DocumentsManager } from '@elementor/editor-documents';
-import { ArrowLeftIcon, ComponentsIcon, SettingsIcon } from '@elementor/icons';
-import {
-	Badge,
-	Box,
-	Divider,
-	IconButton,
-	keyframes,
-	Stack,
-	styled,
-	ToggleButton,
-	Tooltip,
-	Typography,
-} from '@elementor/ui';
+import { ArrowLeftIcon, ComponentsIcon } from '@elementor/icons';
+import { __useSelector as useSelector } from '@elementor/store';
+import { Box, Divider, IconButton, Stack, Tooltip, Typography } from '@elementor/ui';
 import { __ } from '@wordpress/i18n';
 
-import { useCurrentComponent, useNavigateBack } from '../edit-component/edit-component';
+import { useNavigateBack } from '../../hooks/use-navigate-back';
+import { selectCurrentComponentId } from '../../store/store';
+import { ComponentsBadge } from './component-badge';
 import { useOverridableProps } from './use-overridable-props';
 
-const bounceIn = keyframes`
-	0% { transform: scale(0) translate(50%, 50%); opacity: 0; }
-	70% { transform: scale(1.1) translate(50%, -50%); opacity: 1; }
-	100% { transform: scale(1) translate(50%, -50%); opacity: 1; }
-`;
-const slideUp = keyframes`
-	from { transform: translateY(100%); opacity: 0; }
-	to { transform: translateY(0); opacity: 1; }
-`;
-
 export const ComponentPanelHeader = () => {
-	const { path, currentComponentId } = useCurrentComponent();
+	const currentComponentId = useSelector( selectCurrentComponentId );
 	const overridableProps = useOverridableProps( currentComponentId );
-	const onBack = useNavigateBack( path );
+	const onBack = useNavigateBack();
 	const componentName = getComponentName();
 
 	const overridesCount = overridableProps ? Object.keys( overridableProps.props ).length : 0;
-	const prevCount = usePrevious( overridesCount );
-
-	const isFirstOverride = prevCount === 0 && overridesCount === 1;
 
 	if ( ! currentComponentId ) {
 		return null;
@@ -65,31 +43,7 @@ export const ComponentPanelHeader = () => {
 						</Typography>
 					</Stack>
 				</Stack>
-				<Badge
-					key={ overridesCount }
-					invisible={ overridesCount === 0 }
-					badgeContent={
-						<StyledBadgeContent shouldAnimate={ ! isFirstOverride }>{ overridesCount }</StyledBadgeContent>
-					}
-					color="primary"
-					anchorOrigin={ { vertical: 'top', horizontal: 'right' } }
-					sx={ {
-						'& .MuiBadge-badge': {
-							minWidth: 16,
-							height: 16,
-							minHeight: 16,
-							maxWidth: 16,
-							fontSize: 10,
-							transformOrigin: '100% 0%',
-							transition: 'none',
-							animation: isFirstOverride ? `${ bounceIn } 300ms ease-out` : 'none',
-						},
-					} }
-				>
-					<ToggleButton value="overrides" size="tiny" aria-label={ __( 'View overrides', 'elementor' ) }>
-						<SettingsIcon fontSize="tiny" />
-					</ToggleButton>
-				</Badge>
+				<ComponentsBadge overridesCount={ overridesCount } />
 			</Stack>
 			<Divider />
 		</Box>
@@ -101,16 +55,4 @@ function getComponentName() {
 	const currentDocument = documentsManager.getCurrent();
 
 	return currentDocument?.container?.settings?.get( 'post_title' ) ?? '';
-}
-
-const StyledBadgeContent = styled( 'span' )< { shouldAnimate: boolean } >( ( { shouldAnimate } ) => ( {
-	animation: shouldAnimate ? `${ slideUp } 300ms ease-out` : 'none',
-} ) );
-
-function usePrevious< T >( value: T ) {
-	const ref = useRef< T >( value );
-	useEffect( () => {
-		ref.current = value;
-	}, [ value ] );
-	return ref.current;
 }
