b131f717d7f1d3ddd6f2c912fc7e41c63361907a
Omer Haim Israeli <omeri@elementor.com>
2025-12-07 15:50:05 +0200

    wip

--- modules/atomic-widgets/base/atomic-control-base.php
+++ modules/atomic-widgets/base/atomic-control-base.php
@@ -11,9 +11,4 @@ if ( ! defined( 'ABSPATH' ) ) {
 /**
  * @deprecated 3.34 Use \Elementor\Modules\AtomicWidgets\Controls\Base\Atomic_Control_Base instead.
  */
-abstract class Atomic_Control_Base extends New_Atomic_Control_Base {
-	public function __construct( string $prop_name ) {
-		_deprecated_class( __CLASS__, '3.34', New_Atomic_Control_Base::class );
-		parent::__construct( $prop_name );
-	}
-}
+abstract class Atomic_Control_Base extends New_Atomic_Control_Base {}
