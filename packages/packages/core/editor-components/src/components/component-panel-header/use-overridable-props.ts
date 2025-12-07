25ffde4a03fe781c93dd1e585ffd69298106acc8
Omer Haim Israeli <omeri@elementor.com>
2025-12-07 17:03:13 +0200

    wip

--- packages/packages/core/editor-components/src/components/component-panel-header/use-overridable-props.ts
+++ packages/packages/core/editor-components/src/components/component-panel-header/use-overridable-props.ts
@@ -1,14 +1,14 @@
 import { __useSelector as useSelector } from '@elementor/store';
 
-import { selectOverridableProps } from '../../store/store';
+import { type ComponentsSlice, selectOverridableProps } from '../../store/store';
 import { type ComponentId, type OverridableProps } from '../../types';
 
 export function useOverridableProps( componentId: ComponentId | null ): OverridableProps | undefined {
-	return useSelector( ( state ) => {
+	return useSelector( ( state: ComponentsSlice ) => {
 		if ( ! componentId ) {
 			return undefined;
 		}
 
-		return selectOverridableProps( state as Parameters< typeof selectOverridableProps >[ 0 ], componentId );
+		return selectOverridableProps( state, componentId );
 	} );
 }
