653df0b0e721e89afe5a6d1f72558f6b4c97ad62
svitlanadykun <svitlanad@elementor.red>
2025-12-08 16:19:18 +0100

    call experiment cli

--- tests/playwright/sanity/modules/v4-tests/inline-text-editing/canvas-editing.test.ts
+++ tests/playwright/sanity/modules/v4-tests/inline-text-editing/canvas-editing.test.ts
@@ -3,6 +3,7 @@ import { parallelTest as test } from '../../../../parallelTest';
 import WpAdminPage from '../../../../pages/wp-admin-page';
 import EditorPage from '../../../../pages/editor-page';
 import { INLINE_EDITING_SELECTORS } from './selectors/selectors';
+import { wpCli } from '../../../../assets/wp-cli';
 
 test.describe( 'Inline Editing Canvas @v4-tests', () => {
 	let wpAdminPage: WpAdminPage;
@@ -15,8 +16,7 @@ test.describe( 'Inline Editing Canvas @v4-tests', () => {
 		page = await context.newPage();
 		wpAdminPage = new WpAdminPage( page, testInfo, apiRequests );
 
-		await wpAdminPage.setExperiments( { e_atomic_elements: 'active' } );
-		await wpAdminPage.setExperiments( { 'v4-inline-text-editing': 'active' } );
+		await wpCli( 'wp elementor experiments activate e_atomic_elements,v4-inline-text-editing' );
 
 		editor = await wpAdminPage.openNewPage();
 	} );
