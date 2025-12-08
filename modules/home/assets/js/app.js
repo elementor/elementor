361c88ae5bcc5f3f70f9942c3981397e6e09415c
Hein van Vlastuin <hein@obox.co.za>
2025-12-08 16:48:10 +0200

    debug

--- modules/home/assets/js/app.js
+++ modules/home/assets/js/app.js
@@ -7,31 +7,64 @@ const App = ( props ) => {
 	const isEditorOneActive = props.homeScreenData?.isEditorOneActive || false;
 	const ScreenComponent = isEditorOneActive ? EditorScreen : HomeScreen;
 
-	// eslint-disable-next-line no-console
-	console.log( '[HOME_SCREEN_DEBUG] ========================================' );
-	// eslint-disable-next-line no-console
-	console.log( '[HOME_SCREEN_DEBUG] JS: isEditorOneActive:', isEditorOneActive );
-	// eslint-disable-next-line no-console
-	console.log( '[HOME_SCREEN_DEBUG] JS: Selected component:', isEditorOneActive ? 'EditorScreen' : 'HomeScreen' );
-	// eslint-disable-next-line no-console
-	console.log( '[HOME_SCREEN_DEBUG] JS: props.homeScreenData.isEditorOneActive:', props.homeScreenData?.isEditorOneActive );
-	// eslint-disable-next-line no-console
-	console.log( '[HOME_SCREEN_DEBUG] JS: props.homeScreenData._debug:', props.homeScreenData?._debug );
-	// eslint-disable-next-line no-console
-	console.log( '[HOME_SCREEN_DEBUG] JS: elementorHomeScreenData global exists:', typeof elementorHomeScreenData !== 'undefined' );
-	if ( typeof elementorHomeScreenData !== 'undefined' ) {
+	const debugInfo = {
+		isEditorOneActive,
+		selectedComponent: isEditorOneActive ? 'EditorScreen' : 'HomeScreen',
+		propsIsEditorOneActive: props.homeScreenData?.isEditorOneActive,
+		propsDebug: props.homeScreenData?._debug,
+		globalExists: typeof elementorHomeScreenData !== 'undefined',
+		globalIsEditorOneActive: typeof elementorHomeScreenData !== 'undefined' ? elementorHomeScreenData?.isEditorOneActive : 'N/A',
+		globalDebug: typeof elementorHomeScreenData !== 'undefined' ? elementorHomeScreenData?._debug : 'N/A',
+	};
+
+	const debugMessages = [
+		'[HOME_SCREEN_DEBUG] ========================================',
+		`[HOME_SCREEN_DEBUG] JS: isEditorOneActive: ${ isEditorOneActive }`,
+		`[HOME_SCREEN_DEBUG] JS: Selected component: ${ debugInfo.selectedComponent }`,
+		`[HOME_SCREEN_DEBUG] JS: props.homeScreenData.isEditorOneActive: ${ debugInfo.propsIsEditorOneActive }`,
+		`[HOME_SCREEN_DEBUG] JS: props.homeScreenData._debug: ${ JSON.stringify( debugInfo.propsDebug ) }`,
+		`[HOME_SCREEN_DEBUG] JS: elementorHomeScreenData global exists: ${ debugInfo.globalExists }`,
+		`[HOME_SCREEN_DEBUG] JS: elementorHomeScreenData.isEditorOneActive: ${ debugInfo.globalIsEditorOneActive }`,
+		`[HOME_SCREEN_DEBUG] JS: elementorHomeScreenData._debug: ${ JSON.stringify( debugInfo.globalDebug ) }`,
+		'[HOME_SCREEN_DEBUG] ========================================',
+	];
+
+	debugMessages.forEach( ( msg ) => {
 		// eslint-disable-next-line no-console
-		console.log( '[HOME_SCREEN_DEBUG] JS: elementorHomeScreenData.isEditorOneActive:', elementorHomeScreenData?.isEditorOneActive );
-		// eslint-disable-next-line no-console
-		console.log( '[HOME_SCREEN_DEBUG] JS: elementorHomeScreenData._debug:', elementorHomeScreenData?._debug );
-	}
-	// eslint-disable-next-line no-console
-	console.log( '[HOME_SCREEN_DEBUG] ========================================' );
+		console.log( msg );
+	} );
 
 	return (
 		<DirectionProvider rtl={ props.isRTL }>
 			<LocalizationProvider>
 				<ThemeProvider colorScheme={ 'light' }>
+					<div
+						id="home-screen-debug-info"
+						style={ {
+							position: 'fixed',
+							top: 0,
+							left: 0,
+							right: 0,
+							background: '#000',
+							color: '#0f0',
+							padding: '10px',
+							fontFamily: 'monospace',
+							fontSize: '12px',
+							zIndex: 99999,
+							maxHeight: '200px',
+							overflow: 'auto',
+							borderBottom: '2px solid #0f0',
+						} }
+					>
+						<div style={ { fontWeight: 'bold', marginBottom: '5px' } }>HOME SCREEN DEBUG INFO</div>
+						<div>isEditorOneActive: { String( isEditorOneActive ) }</div>
+						<div>Selected Component: { debugInfo.selectedComponent }</div>
+						<div>props.homeScreenData.isEditorOneActive: { String( debugInfo.propsIsEditorOneActive ) }</div>
+						<div>props._debug: { JSON.stringify( debugInfo.propsDebug ) }</div>
+						<div>Global exists: { String( debugInfo.globalExists ) }</div>
+						<div>Global isEditorOneActive: { String( debugInfo.globalIsEditorOneActive ) }</div>
+						<div>Global _debug: { JSON.stringify( debugInfo.globalDebug ) }</div>
+					</div>
 					<ScreenComponent
 						homeScreenData={ props.homeScreenData }
 						adminUrl={ props.adminUrl }
