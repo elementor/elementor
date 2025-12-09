f21ac7010c26bbdc4baba81e6edb359dda8821fc
Hein van Vlastuin <hein@obox.co.za>
2025-12-09 10:35:02 +0200

    wip

--- tests/playwright/pages/wp-admin-page.ts
+++ tests/playwright/pages/wp-admin-page.ts
@@ -273,7 +273,9 @@ export default class WpAdminPage extends BasePage {
 				}
 			}, `.elementor_experiment-${ id }` );
 
-			await this.page.selectOption( selector, state ? 'active' : 'inactive' );
+			const selectLocator = this.page.locator( selector );
+			await selectLocator.waitFor( { state: 'visible' } );
+			await selectLocator.selectOption( state ? 'active' : 'inactive' );
 
 			// Click to confirm any experiment that has dependencies.
 			await this.confirmExperimentModalIfOpen();
