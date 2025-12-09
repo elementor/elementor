480db19ea242551efef9f43a67f286c2d9b6f0dd
Hein van Vlastuin <hein@obox.co.za>
2025-12-09 13:55:21 +0200

    wip

--- tests/playwright/sanity/modules/home/home-screen-ui.test.ts
+++ tests/playwright/sanity/modules/home/home-screen-ui.test.ts
@@ -10,8 +10,6 @@ test.describe( 'Home screen visual regression tests', () => {
 		const context = await browser.newContext();
 		const page = await context.newPage();
 		const wpAdmin = new WpAdminPage( page, testInfo, apiRequests );
-		await wpAdmin.setExperiments( { e_editor_one: 'inactive' } );
-		await page.waitForTimeout( 1000 );
 		await wpAdmin.enableAdvancedUploads();
 		await page.close();
 		await context.close();
