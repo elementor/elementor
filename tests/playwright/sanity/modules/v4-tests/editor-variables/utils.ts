ec914e7ba361986bb70b126f5f7cf4c2ae7cff10
svitlanadykun <svitlanad@elementor.red>
2025-12-09 09:26:08 +0100

    fix experiment

--- tests/playwright/sanity/modules/v4-tests/editor-variables/utils.ts
+++ tests/playwright/sanity/modules/v4-tests/editor-variables/utils.ts
@@ -1,11 +1,13 @@
 import { TestInfo, type Page } from '@playwright/test';
 import WpAdminPage from '../../../../pages/wp-admin-page';
 import ApiRequests from '../../../../assets/api-requests';
+import { wpCli } from '../../../../assets/wp-cli';
 
 export const initTemplate = async ( page: Page, testInfo: TestInfo, apiRequests: ApiRequests ) => {
 	const wpAdminPage = new WpAdminPage( page, testInfo, apiRequests );
-	await wpAdminPage.setExperiments( { e_variables: 'active', e_atomic_elements: 'active' } );
-	await wpAdminPage.setExperiments( { e_variables_manager: 'active' } );
+	await wpCli( 'wp elementor experiments activate e_atomic_elements' );
+	await wpCli( 'wp elementor experiments activate e_variables' );
+	await wpCli( 'wp elementor experiments activate e_variables_manager' );
 	const editorPage = await wpAdminPage.openNewPage();
 	await editorPage.loadTemplate( 'tests/playwright/sanity/modules/v4-tests/editor-variables/variables-manager-template.json' );
 	return wpAdminPage;
