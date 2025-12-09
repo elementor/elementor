6e62f7c19b29aaacf3fb5650ac9bef9413a028f3
Nikita Murzin <mykytamu@elementor.com>
2025-12-10 00:06:41 +0100

    Internal: style fixes to toolbar [ED-21878]

--- packages/packages/libs/editor-controls/src/components/inline-editor.tsx
+++ packages/packages/libs/editor-controls/src/components/inline-editor.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { type DependencyList, type ForwardedRef, useEffect, useRef } from 'react';
+import { type DependencyList, type ForwardedRef, useEffect, useRef, useState } from 'react';
 import { Box, type SxProps, type Theme } from '@elementor/ui';
 import Bold from '@tiptap/extension-bold';
 import Document from '@tiptap/extension-document';
@@ -41,6 +41,8 @@ export const InlineEditor = React.forwardRef(
 		{ value, setValue, attributes = {}, showToolbar = false, sx }: InlineEditorProps,
 		ref: ForwardedRef< HTMLDivElement >
 	) => {
+		const [ hasTextSelection, setHasTextSelection ] = useState( false );
+
 		const editor = useEditor( {
 			extensions: [
 				Document.extend( {
@@ -66,6 +68,16 @@ export const InlineEditor = React.forwardRef(
 			] as AnyExtension[],
 			content: value,
 			onUpdate: ( { editor: updatedEditor } ) => setValue( updatedEditor.getHTML() ),
+			onSelectionUpdate: ( { editor: updatedEditor } ) => {
+				const { selection } = updatedEditor.state;
+				const hasSelection = ! selection.empty && selection.from !== selection.to;
+				setHasTextSelection( hasSelection );
+			},
+			onCreate: ( { editor: createdEditor } ) => {
+				const { selection } = createdEditor.state;
+				const hasSelection = ! selection.empty && selection.from !== selection.to;
+				setHasTextSelection( hasSelection );
+			},
 		} );
 
 		useOnUpdate( () => {
@@ -79,6 +91,12 @@ export const InlineEditor = React.forwardRef(
 				editor.commands.setContent( value, { emitUpdate: false } );
 			}
 		}, [ editor, value ] );
+
+		useEffect( () => {
+			if ( ! showToolbar ) {
+				setHasTextSelection( false );
+			}
+		}, [ showToolbar ] );
 
 		return (
 			<Box
@@ -110,7 +128,7 @@ export const InlineEditor = React.forwardRef(
 				} }
 				{ ...attributes }
 			>
-				{ showToolbar && <InlineEditorToolbar editor={ editor } /> }
+				{ showToolbar && hasTextSelection && editor && <InlineEditorToolbar editor={ editor } /> }
 				<EditorContent editor={ editor } />
 			</Box>
 		);
