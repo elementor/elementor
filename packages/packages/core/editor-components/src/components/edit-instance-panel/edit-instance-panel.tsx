1600aeaa481c039124dc98ea38f54fa61923584b
Ron Rostovsky <ronros@elementor.com>
2025-12-11 11:54:42 +0200

    wip

--- packages/packages/core/editor-components/src/components/edit-instance-panel/edit-instance-panel.tsx
+++ packages/packages/core/editor-components/src/components/edit-instance-panel/edit-instance-panel.tsx
@@ -10,7 +10,9 @@ import { __ } from '@wordpress/i18n';
 
 import { componentInstancePropTypeUtil } from '../../prop-types/component-instance-prop-type';
 import { selectComponent, selectOverridableProps } from '../../store/store';
+import { type OverridablePropsGroup } from '../../types';
 import { switchToComponent } from '../../utils/switch-to-component';
+import { EmptyState } from './empty-state';
 import { OverridePropsGroup } from './override-props-group';
 
 export function EditInstancePanel() {
@@ -30,6 +32,14 @@ export function EditInstancePanel() {
 	/* translators: %s: component name. */
 	const panelTitle = __( 'Edit %s', 'elementor' ).replace( '%s', component.name );
 
+	const handleEditComponent = () => switchToComponent( componentId, componentInstanceId );
+
+	const groups = overridableProps.groups.order
+		.map( ( groupId ) =>
+			overridableProps.groups.items[ groupId ] ? overridableProps.groups.items[ groupId ] : null
+		)
+		.filter( Boolean ) as OverridablePropsGroup[];
+
 	return (
 		<>
 			<PanelHeader sx={ { justifyContent: 'start' } }>
@@ -39,28 +49,22 @@ export function EditInstancePanel() {
 						<PanelHeaderTitle>{ component.name }</PanelHeaderTitle>
 					</Stack>
 					<Tooltip title={ panelTitle }>
-						<IconButton
-							size="tiny"
-							onClick={ () => switchToComponent( componentId, componentInstanceId ) }
-							aria-label={ panelTitle }
-						>
+						<IconButton size="tiny" onClick={ handleEditComponent } aria-label={ panelTitle }>
 							<PencilIcon fontSize="tiny" />
 						</IconButton>
 					</Tooltip>
 				</Stack>
 			</PanelHeader>
 			<PanelBody>
-				<Stack direction="column" alignItems="stretch">
-					{ overridableProps.groups.order.map( ( groupId ) =>
-						overridableProps.groups.items[ groupId ] ? (
-							<OverridePropsGroup
-								key={ groupId }
-								group={ overridableProps.groups.items[ groupId ] }
-								props={ overridableProps.props }
-							/>
-						) : null
-					) }
-				</Stack>
+				{ groups.length === 0 ? (
+					<EmptyState onEditComponent={ handleEditComponent } />
+				) : (
+					<Stack direction="column" alignItems="stretch">
+						{ groups.map( ( group ) => (
+							<OverridePropsGroup key={ group.id } group={ group } props={ overridableProps.props } />
+						) ) }
+					</Stack>
+				) }
 			</PanelBody>
 		</>
 	);
