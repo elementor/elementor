0cde41789156e34e4889ea0e3e24f78715fa0960
Netanel Baba <ntnelbaba@gmail.com>
2025-12-11 12:07:40 +0200

    More vars to use

--- packages/packages/core/editor-variables/src/utils/llm-propvalue-label-resolver.ts
+++ packages/packages/core/editor-variables/src/utils/llm-propvalue-label-resolver.ts
@@ -8,4 +8,18 @@ export const globalVariablesLLMResolvers = {
 			value: service.variables()[ idOrLabel ] ? idOrLabel : service.findIdByLabel( idOrLabel ),
 		};
 	},
+	'global-font-variable': ( value: unknown ) => {
+		const idOrLabel = String( value );
+		return {
+			$$type: 'global-font-variable',
+			value: service.variables()[ idOrLabel ] ? idOrLabel : service.findIdByLabel( idOrLabel ),
+		};
+	},
+	'global-size-variable': ( value: unknown ) => {
+		const idOrLabel = String( value );
+		return {
+			$$type: 'global-size-variable',
+			value: service.variables()[ idOrLabel ] ? idOrLabel : service.findIdByLabel( idOrLabel ),
+		};
+	},
 };
