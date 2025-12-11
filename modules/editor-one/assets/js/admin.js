2caa103e702d886019e76030262b9a67a9a29571
max-zu <zubovmaksim@gmail.com>
2025-12-11 14:45:36 +0100

    resolved conflicts

--- modules/editor-one/assets/js/admin.js
+++ modules/editor-one/assets/js/admin.js
@@ -1,8 +1,3 @@
 document.addEventListener( 'DOMContentLoaded', function() {
-	const urlParams = new URLSearchParams( window.location.search );
-
-	if ( 'elementor-element-manager' === urlParams.get( 'page' ) ) {
-		const links = document.querySelectorAll( 'link[href*="/wp-admin/css/forms.css"]' );
-		links.forEach( ( link ) => link.remove() );
-	}
+	// Header
 } );
