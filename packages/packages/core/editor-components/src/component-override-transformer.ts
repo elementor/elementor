a214c0f6eca81e43be6a2e95b50b20cb41f13683
IshayMaya <ishaym@elementor.com>
2025-12-14 17:10:26 +0200

    removed async from componentOverrideTransformer

--- packages/packages/core/editor-components/src/component-override-transformer.ts
+++ packages/packages/core/editor-components/src/component-override-transformer.ts
@@ -2,7 +2,7 @@ import { createTransformer } from '@elementor/editor-canvas';
 
 import { type ComponentOverridePropValue } from './types';
 
-export const componentOverrideTransformer = createTransformer( async ( override: ComponentOverridePropValue ) => {
+export const componentOverrideTransformer = createTransformer( ( override: ComponentOverridePropValue ) => {
 	const { override_key: key, override_value: overrideValue } = override;
 
 	return { [ key ]: overrideValue };
