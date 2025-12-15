a06626d9cc1090266428b1d4077ce1caeab151e8
max-zu <zubovmaksim@gmail.com>
2025-12-15 09:31:16 +0100

    upd

--- assets/dev/js/editor/components/template-library/views/parts/header-parts/menu.js
+++ assets/dev/js/editor/components/template-library/views/parts/header-parts/menu.js
@@ -3,6 +3,14 @@ module.exports = Marionette.ItemView.extend( {
 
 	id: 'elementor-template-library-header-menu',
 
+	ui: {
+		tabs: '[role="tab"]',
+	},
+
+	events: {
+		'keydown @ui.tabs': 'onTabKeyDown',
+	},
+
 	templateHelpers() {
 		return {
 			tabs: $e.components.get( 'library' ).getTabs(),
@@ -14,5 +22,46 @@ module.exports = Marionette.ItemView.extend( {
 			role: 'tablist',
 			'aria-label': __( 'Library sections', 'elementor' ),
 		};
+	},
+
+	onTabKeyDown( event ) {
+		const $tabs = this.ui.tabs;
+		const $currentTab = jQuery( event.currentTarget );
+		const currentIndex = $tabs.index( $currentTab );
+		let targetIndex = -1;
+
+		switch ( event.key ) {
+			case 'ArrowLeft':
+				event.preventDefault();
+				targetIndex = currentIndex > 0 ? currentIndex - 1 : $tabs.length - 1;
+				break;
+			case 'ArrowRight':
+				event.preventDefault();
+				targetIndex = currentIndex < $tabs.length - 1 ? currentIndex + 1 : 0;
+				break;
+			case 'Home':
+				event.preventDefault();
+				targetIndex = 0;
+				break;
+			case 'End':
+				event.preventDefault();
+				targetIndex = $tabs.length - 1;
+				break;
+			default:
+				return;
+		}
+
+		if ( targetIndex >= 0 && targetIndex < $tabs.length ) {
+			const $targetTab = $tabs.eq( targetIndex );
+			const targetTabName = $targetTab.data( 'tab' );
+
+			if ( targetTabName ) {
+				const libraryComponent = $e.components.get( 'library' );
+				if ( libraryComponent ) {
+					libraryComponent.activateTab( targetTabName );
+					$targetTab.focus();
+				}
+			}
+		}
 	},
 } );
