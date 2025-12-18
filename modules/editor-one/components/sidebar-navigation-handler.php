882afb817fb69069a6b56aff514b663d0f38f2f5
Hein van Vlastuin <hein@obox.co.za>
2025-12-18 15:33:00 +0200

    Revert sidebar handling

--- modules/editor-one/components/sidebar-navigation-handler.php
+++ modules/editor-one/components/sidebar-navigation-handler.php
@@ -4,7 +4,6 @@ namespace Elementor\Modules\EditorOne\Components;
 
 use Elementor\Core\Utils\Promotions\Filtered_Promotions_Manager;
 use Elementor\Modules\EditorOne\Classes\Active_Menu_Resolver;
-use Elementor\Modules\EditorOne\Classes\Menu_Config;
 use Elementor\Modules\EditorOne\Classes\Menu_Data_Provider;
 use Elementor\Modules\EditorOne\Classes\Url_Matcher;
 use Elementor\Utils;
@@ -91,14 +90,11 @@ class Sidebar_Navigation_Handler {
 		$level4_groups = $this->menu_data_provider->get_level4_flyout_data();
 		$promotion = $this->get_promotion_data();
 
-		$filtered_items = $this->filter_menu_items_for_edit_posts_users( $flyout_data['items'] );
-		$filtered_level4_groups = $this->filter_level4_groups_for_edit_posts_users( $level4_groups );
-
-		$active_state = $this->get_active_menu_state( $filtered_items, $filtered_level4_groups );
+		$active_state = $this->get_active_menu_state( $flyout_data['items'], $level4_groups );
 
 		return [
-			'menuItems' => $filtered_items,
-			'level4Groups' => $filtered_level4_groups,
+			'menuItems' => $flyout_data['items'],
+			'level4Groups' => $level4_groups,
 			'activeMenuSlug' => $active_state['menu_slug'],
 			'activeChildSlug' => $active_state['child_slug'],
 			'isRTL' => is_rtl(),
@@ -127,95 +123,4 @@ class Sidebar_Navigation_Handler {
 		return $this->active_menu_resolver->resolve( $menu_items, $level4_groups, $current_page, $current_uri );
 	}
 
-	private function filter_menu_items_for_edit_posts_users( array $menu_items ): array {
-		$user_capabilities = Menu_Data_Provider::get_current_user_capabilities();
-		$user = $user_capabilities['user'];
-
-		if ( ! $user_capabilities['is_edit_posts_user'] ) {
-			return apply_filters( 'elementor/editor-one/menu/filter_level3_items', $menu_items, $user );
-		}
-
-		$templates_group_id = Menu_Config::TEMPLATES_GROUP_ID;
-		$level3_items = $this->menu_data_provider->get_level3_items();
-		$level4_items = $this->menu_data_provider->get_level4_items();
-		$filtered = [];
-
-		foreach ( $menu_items as $item_data ) {
-			$item_slug = $item_data['slug'] ?? '';
-			$item_group_id = $item_data['group_id'] ?? '';
-
-			if ( $item_group_id !== $templates_group_id ) {
-				continue;
-			}
-
-			$original_item = null;
-			foreach ( $level3_items as $group_items ) {
-				if ( isset( $group_items[ $item_slug ] ) ) {
-					$original_item = $group_items[ $item_slug ];
-					break;
-				}
-			}
-
-			if ( $original_item ) {
-				$is_accessible = $this->menu_data_provider->is_item_accessible( $original_item );
-
-				if ( ! $is_accessible && $original_item->has_children() ) {
-					$child_items = $level4_items[ $item_group_id ] ?? [];
-					$has_accessible_child = false;
-
-					foreach ( $child_items as $child_slug => $child_item ) {
-						if ( $this->menu_data_provider->is_item_accessible( $child_item ) ) {
-							$has_accessible_child = true;
-							break;
-						}
-					}
-
-					$is_accessible = $has_accessible_child;
-				}
-
-				if ( $is_accessible ) {
-					$filtered[] = $item_data;
-				}
-			}
-		}
-
-		return apply_filters( 'elementor/editor-one/menu/filter_edit_posts_user_items', $filtered, $menu_items, $user, 'level3' );
-	}
-
-	private function filter_level4_groups_for_edit_posts_users( array $level4_groups ): array {
-		$user_capabilities = Menu_Data_Provider::get_current_user_capabilities();
-		$user = $user_capabilities['user'];
-
-		if ( ! $user_capabilities['is_edit_posts_user'] ) {
-			return $level4_groups;
-		}
-
-		$templates_group_id = Menu_Config::TEMPLATES_GROUP_ID;
-		$filtered_groups = [];
-
-		if ( isset( $level4_groups[ $templates_group_id ] ) ) {
-			$group_items = $level4_groups[ $templates_group_id ]['items'] ?? [];
-			$filtered_items = [];
-
-			$level4_items = $this->menu_data_provider->get_level4_items();
-			$templates_items = $level4_items[ $templates_group_id ] ?? [];
-
-			foreach ( $group_items as $item_data ) {
-				$item_slug = $item_data['slug'] ?? '';
-				$original_item = $templates_items[ $item_slug ] ?? null;
-
-				if ( $original_item && $this->menu_data_provider->is_item_accessible( $original_item ) ) {
-					$filtered_items[] = $item_data;
-				}
-			}
-
-			if ( ! empty( $filtered_items ) ) {
-				$filtered_groups[ $templates_group_id ] = [
-					'items' => $filtered_items,
-				];
-			}
-		}
-
-		return apply_filters( 'elementor/editor-one/menu/filter_edit_posts_user_items', $filtered_groups, $level4_groups, $user, 'level4' );
-	}
 }
