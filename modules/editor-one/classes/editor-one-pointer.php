fff520c22aa1ff2d8293f0965c6bfe408962bcf3
Hein van Vlastuin <hein@obox.co.za>
2025-12-19 12:51:06 +0200

    wip

--- modules/editor-one/classes/editor-one-pointer.php
+++ modules/editor-one/classes/editor-one-pointer.php
@@ -10,30 +10,35 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 class Editor_One_Pointer {
 
-	const CURRENT_POINTER_SLUG = 'e-editor-one-new-home';
+	const CURRENT_POINTER_SLUG = 'e-editor-one-notice-pointer';
 
 	public function __construct() {
 		add_action( 'admin_print_footer_scripts-index.php', [ $this, 'admin_print_script' ] );
 	}
 
 	public function admin_print_script() {
-		if ( ! $this->is_admin_user() || $this->is_dismissed() || $this->is_new_installation() ) {
+		if ( ! $this->is_admin_user() || $this->is_new_installation() || $this->is_dismissed() ) {
 			return;
 		}
 
 		wp_enqueue_script( 'wp-pointer' );
 		wp_enqueue_style( 'wp-pointer' );
 
+		$learn_more_url = 'https://go.elementor.com/wp-dash-editor-one-learn-more/';
 		$pointer_content = '<h3>' . esc_html__( 'The Editor has a new home', 'elementor' ) . '</h3>';
-		$pointer_content .= '<p>' . esc_html__( 'Editor tools are now grouped together for quick access. Build and grow your site with everything you need in one place.', 'elementor' ) . '</p>';
+		$pointer_content .= sprintf(
+			'<p>%s <a href="%s" target="_blank">%s</a></p>',
+			esc_html__( 'Editor tools are now grouped together for quick access. Build and grow your site with everything you need in one place.', 'elementor' ),
+			esc_url( $learn_more_url ),
+			esc_html__( 'Learn more', 'elementor' )
+		);
 
 		$got_it_url = admin_url( 'admin.php?page=elementor' );
 		$pointer_content .= sprintf(
-			'<p><a class="button button-primary" href="%s" onclick="jQuery(this).closest(\'.wp-pointer\').pointer(\'close\'); setTimeout(function(){window.location.href=\'%s\';}, 100); return false;">%s</a> <button class="button" onclick="jQuery(this).closest(\'.wp-pointer\').pointer(\'close\');">%s</button></p>',
+			'<p><a class="button button-primary" href="%s" onclick="jQuery(this).closest(\'.wp-pointer\').pointer(\'close\'); setTimeout(function(){window.location.href=\'%s\';}, 100); return false;">%s</a></p>',
 			esc_url( $got_it_url ),
 			esc_js( $got_it_url ),
-			esc_html__( 'Got it', 'elementor' ),
-			esc_html__( 'Dismiss', 'elementor' )
+			esc_html__( 'Got it', 'elementor' )
 		);
 
 		?>
@@ -41,7 +46,7 @@ class Editor_One_Pointer {
 			jQuery( document ).ready( function( $ ) {
 				setTimeout( function () {
 					$( '#toplevel_page_elementor' ).pointer( {
-						content: '<?php echo $pointer_content; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>',
+						content: <?php echo wp_json_encode( $pointer_content ); ?>,
 						position: {
 							edge: <?php echo is_rtl() ? "'right'" : "'left'"; ?>,
 							align: 'center'
