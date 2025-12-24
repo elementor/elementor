708877a80eeb32c542e95134d17860f83827029b
Nicola Peluchetti <nicola.peluchetti@gmail.com>
2025-12-24 14:24:49 +0100

    fix

--- modules/editor-one/assets/js/top-bar/app.js
+++ modules/editor-one/assets/js/top-bar/app.js
@@ -2,18 +2,18 @@ import ReactUtils from 'elementor-utils/react';
 import { ElementorOneHeader, ElementorOneAssetsProvider } from '@elementor/elementor-one-assets';
 
 const App = () => {
-    return (
-        <ElementorOneAssetsProvider env="production" language="en">
-            <ElementorOneHeader appType="PLUGIN" />
-        </ElementorOneAssetsProvider>
-    );
+	return (
+		<ElementorOneAssetsProvider env="production" language="en">
+			<ElementorOneHeader appType="PLUGIN" />
+		</ElementorOneAssetsProvider>
+	);
 };
 
 const rootElement = document.getElementById( 'editor-one-top-bar' );
 
 if ( rootElement ) {
-    ReactUtils.render(
-        <App />,
-        rootElement,
-    );
+	ReactUtils.render(
+		<App />,
+		rootElement,
+	);
 }
