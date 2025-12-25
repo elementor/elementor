711204105b73a6e1d622f1c8f24db48164ba2656
Nicola Peluchetti <nicola.peluchetti@gmail.com>
2025-12-25 18:18:15 +0100

    tweaks

--- modules/editor-one/module.php
+++ modules/editor-one/module.php
@@ -6,12 +6,10 @@ use Elementor\Core\Admin\Admin;
 use Elementor\Core\Base\Module as BaseModule;
 use Elementor\Core\Experiments\Manager as Experiments_Manager;
 use Elementor\Modules\EditorOne\Classes\Editor_One_Pointer;
-use Elementor\Modules\EditorOne\Components\Connect;
 use Elementor\Modules\EditorOne\Classes\Menu_Config;
 use Elementor\Modules\EditorOne\Components\Elementor_One_Menu_Manager;
 use Elementor\Modules\EditorOne\Components\Sidebar_Navigation_Handler;
 use Elementor\Modules\EditorOne\Components\Top_Bar_Handler;
-use Elementor\Plugin;
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
@@ -48,7 +46,6 @@ class Module extends BaseModule {
 		parent::__construct();
 
 		if ( is_admin() ) {
-			Connect::instance();
 			$this->add_component( 'editor-one-menu-manager', new Elementor_One_Menu_Manager() );
 			$this->add_component( 'sidebar-navigation-handler', new Sidebar_Navigation_Handler() );
 			$this->add_component( 'top-bar-handler', new Top_Bar_Handler() );
