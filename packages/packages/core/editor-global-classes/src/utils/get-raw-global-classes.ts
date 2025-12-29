d9d6ed338d154f5bd79651abefb3286d0648e173
Netanel Baba <ntnelbaba@gmail.com>
2025-12-29 21:27:21 +0200

    Classes resource

---
+++ packages/packages/core/editor-global-classes/src/utils/get-raw-global-classes.ts
@@ -0,0 +1,4 @@
+export const getRawGlobalClassesCSS = () => {
+	const styles = document.querySelectorAll('style[data-provider-key="global-classes"]');
+	return Array.from( styles ).map( ( style ) => style.textContent?.replace('.elementor ', '') ).join( '\n' );
+};
