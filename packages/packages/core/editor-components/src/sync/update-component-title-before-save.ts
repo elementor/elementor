3650c2ee2ffba40307311232d86bb811e8df3dad
Omer Haim Israeli <omeri@elementor.com>
2025-12-30 12:16:33 +0200

    wip

--- packages/packages/core/editor-components/src/sync/update-component-title-before-save.ts
+++ packages/packages/core/editor-components/src/sync/update-component-title-before-save.ts
@@ -1,20 +1,7 @@
-import { type NotificationData, notify } from '@elementor/editor-notifications';
 import { __dispatch as dispatch, __getState as getState } from '@elementor/store';
 
 import { apiClient } from '../api';
 import { selectUpdatedComponentNames, slice } from '../store/store';
-
-const failedNotification = ( message: string ): NotificationData => ( {
-	type: 'error',
-	message: `Failed to update component title: ${ message }`,
-	id: 'failed-update-component-title-notification',
-} );
-
-const successNotification = ( message: string ): NotificationData => ( {
-	type: 'success',
-	message: `Successfully updated component title: ${ message }`,
-	id: 'success-update-component-title-notification',
-} );
 
 export const updateComponentTitleBeforeSave = async () => {
 	const updatedComponentNames = selectUpdatedComponentNames( getState() );
@@ -23,14 +10,7 @@ export const updateComponentTitleBeforeSave = async () => {
 		return;
 	}
 
-	const result = await apiClient.updateComponentTitle( updatedComponentNames );
+	await apiClient.updateComponentTitle( updatedComponentNames );
 
 	dispatch( slice.actions.cleanUpdatedComponentNames() );
-
-	if ( result.failedIds.length ) {
-		notify( failedNotification( result.failedIds.join( ', ' ) ) );
-	}
-	if ( result.successIds.length ) {
-		notify( successNotification( result.successIds.join( ', ' ) ) );
-	}
 };
