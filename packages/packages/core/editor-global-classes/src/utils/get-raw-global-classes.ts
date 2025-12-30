a5d57a99aa9f4bafba404b7480ae13ce48ef8c18
Netanel Baba <ntnelbaba@gmail.com>
2025-12-30 15:33:22 +0200

    Push from where we get style tags

--- packages/packages/core/editor-global-classes/src/utils/get-raw-global-classes.ts
+++ packages/packages/core/editor-global-classes/src/utils/get-raw-global-classes.ts
@@ -1,6 +1,6 @@
 export const getRawGlobalClassesCSS = () => {
-	const styles = document.querySelectorAll( 'style[data-provider-key="global-classes"]' );
-	return Array.from( styles )
+	const styles = window?.elementor?.getContainer?.('document')?.document?.$element[0].ownerDocument.querySelectorAll( 'style[data-provider-key="global-classes"]' );
+	return Array.from( styles ?? [] )
 		.map( ( style ) => style.textContent?.replace( '.elementor ', '' ) )
 		.join( '\n' );
 };
