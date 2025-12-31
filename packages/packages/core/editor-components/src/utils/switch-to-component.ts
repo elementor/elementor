15be0c322a16e574539bf0d60cf821525b79bcea
IshayMaya <47317545+IshayMaya@users.noreply.github.com>
2025-12-31 14:57:02 +0200

    Update packages/packages/core/editor-components/src/utils/switch-to-component.ts

--- packages/packages/core/editor-components/src/utils/switch-to-component.ts
+++ packages/packages/core/editor-components/src/utils/switch-to-component.ts
@@ -30,7 +30,7 @@ function getSelector( element?: HTMLElement | null, componentInstanceId?: string
 
 export function buildUniqueSelector( element: HTMLElement ): string {
 	const selectors: string[] = [];
-	let current: HTMLElement | null = element.closest( '[data-id]' ) as HTMLElement | null;
+	let current = element.closest< HTMLElement >( '[data-id]' );
 
 	while ( current ) {
 		const dataId = current.dataset.id;
@@ -40,11 +40,11 @@ export function buildUniqueSelector( element: HTMLElement ): string {
 			selectors.unshift( `[data-id="${ dataId }"]` );
 		}
 
-		current = current.parentElement?.closest( '[data-id]' ) as HTMLElement | null;
+		current = current.parentElement?.closest( '[data-id]' ) ?? null;
 	}
 
 	if ( selectors.length === 0 ) {
-		const closestElement = element.closest( '[data-id]' ) as HTMLElement | null;
+		const closestElement = element.closest< HTMLElement >( '[data-id]' );
 
 		if ( closestElement?.dataset?.id ) {
 			return `[data-id="${ closestElement.dataset.id }"]`;
