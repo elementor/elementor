8a6fb7e15087608d37ec2ca8380bfae4e0d75e6d
Nami printz <namipr3@gmail.com>
2025-12-31 12:16:35 +0200

    Internal: Add promotions popover to Pro Size Variables Manager  [ED-21168]

--- packages/packages/core/editor-variables/src/components/variables-manager/variables-manager-create-menu.tsx
+++ packages/packages/core/editor-variables/src/components/variables-manager/variables-manager-create-menu.tsx
@@ -1,9 +1,9 @@
 import * as React from 'react';
-import { createElement, useMemo, useRef } from 'react';
-import { PromotionChip } from '@elementor/editor-ui';
+import { createElement, useMemo, useRef, useState } from 'react';
+import { PromotionChip, PromotionPopover } from '@elementor/editor-ui';
 import { PlusIcon } from '@elementor/icons';
 import { bindMenu, bindTrigger, Box, IconButton, Menu, MenuItem, type PopupState, Typography } from '@elementor/ui';
-import { __ } from '@wordpress/i18n';
+import { __, sprintf } from '@wordpress/i18n';
 
 import { useQuotaPermissions } from '../../hooks/use-quota-permissions';
 import { type TVariablesList } from '../../storage';
@@ -108,8 +108,11 @@ const MenuOption = ( {
 	onCreate: VariableManagerCreateMenuProps[ 'onCreate' ];
 	onClose: () => void;
 } ) => {
-	const displayName = config.variableType.charAt( 0 ).toUpperCase() + config.variableType.slice( 1 );
+	const [ isPopoverOpen, setIsPopoverOpen ] = useState( false );
 	const userQuotaPermissions = useQuotaPermissions( config.propTypeKey );
+
+	const ucFirst = ( string: string ) => string.charAt( 0 ).toUpperCase() + string.slice( 1 );
+	const displayName = ucFirst( config.variableType );
 	const isDisabled = ! userQuotaPermissions.canAdd();
 
 	const handleClick = () => {
@@ -124,6 +127,18 @@ const MenuOption = ( {
 		onClose();
 	};
 
+	const title = sprintf(
+		/* translators: %s: Variable Type. */
+		__( '%s variables', 'elementor' ),
+		ucFirst( config.variableType )
+	);
+
+	const content = sprintf(
+		/* translators: %s: Variable Type. */
+		__( 'Upgrade to continue creating and editing %s variables.', 'elementor' ),
+		config.variableType
+	);
+
 	return (
 		<MenuItem onClick={ handleClick } sx={ { gap: 1.5, cursor: isDisabled ? 'default' : 'pointer' } }>
 			{ createElement( config.icon, { fontSize: SIZE, color: isDisabled ? 'disabled' : 'action' } ) }
@@ -132,11 +147,26 @@ const MenuOption = ( {
 			</Typography>
 			{ isDisabled && (
 				<Box
-					onClick={ () => {
-						event?.stopPropagation();
+					onClick={ ( event: React.MouseEvent ) => {
+						event.stopPropagation();
 					} }
 				>
-					<PromotionChip onClick={ () => {} } />
+					<PromotionPopover
+						open={ isPopoverOpen }
+						title={ title }
+						content={ content }
+						ctaText={ __( 'Upgrade now', 'elementor' ) }
+						ctaUrl={ `https://go.elementor.com/go-pro-manager-${ config.variableType }-variable/` }
+						onClose={ () => {
+							setIsPopoverOpen( false );
+						} }
+					>
+						<PromotionChip
+							onClick={ () => {
+								setIsPopoverOpen( true );
+							} }
+						/>
+					</PromotionPopover>
 				</Box>
 			) }
 		</MenuItem>
---
+++ packages/packages/libs/editor-ui/src/components/promotion-popover.tsx
@@ -0,0 +1,90 @@
+import * as React from 'react';
+import { CrownFilledIcon } from '@elementor/icons';
+import {
+	Alert,
+	AlertAction,
+	AlertTitle,
+	Box,
+	ClickAwayListener,
+	Infotip,
+	type InfotipProps,
+	Typography,
+} from '@elementor/ui';
+
+type PromotionPopoverCardProps = {
+	title: string;
+	content: string;
+	ctaUrl: string;
+	ctaText?: string;
+	onClose: () => void;
+};
+
+type PromotionPopoverProps = React.PropsWithChildren<
+	PromotionPopoverCardProps & {
+		open: boolean;
+		placement?: InfotipProps[ 'placement' ];
+		slotProps?: InfotipProps[ 'slotProps' ];
+	}
+>;
+
+export const PromotionPopover = ( {
+	children,
+	open,
+	placement = 'right',
+	slotProps,
+	...cardProps
+}: PromotionPopoverProps ) => {
+	const defaultSlotProps: InfotipProps[ 'slotProps' ] = {
+		popper: {
+			modifiers: [
+				{
+					name: 'offset',
+					options: {
+						offset: [ 0, 10 ],
+					},
+				},
+			],
+		},
+	};
+
+	return (
+		<Infotip
+			placement={ placement }
+			arrow={ false }
+			content={ <PopoverAlert { ...cardProps } /> }
+			open={ open }
+			slotProps={ slotProps || defaultSlotProps }
+		>
+			{ children }
+		</Infotip>
+	);
+};
+
+function PopoverAlert( { title, content, ctaUrl, ctaText, onClose }: PromotionPopoverCardProps ) {
+	return (
+		<ClickAwayListener
+			disableReactTree={ true }
+			mouseEvent="onMouseDown"
+			touchEvent="onTouchStart"
+			onClickAway={ onClose }
+		>
+			<Alert
+				variant="standard"
+				color="promotion"
+				icon={ <CrownFilledIcon fontSize="tiny" /> }
+				onClose={ onClose }
+				action={
+					<AlertAction variant="contained" color="promotion" href={ ctaUrl } target="_blank">
+						{ ctaText }
+					</AlertAction>
+				}
+				sx={ { maxWidth: 296 } }
+			>
+				<Box sx={ { gap: 0.5, display: 'flex', flexDirection: 'column' } }>
+					<AlertTitle>{ title }</AlertTitle>
+					<Typography variant="body2">{ content }</Typography>
+				</Box>
+			</Alert>
+		</ClickAwayListener>
+	);
+}
