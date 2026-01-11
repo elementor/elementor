fbd5e85b278a77c6235522f77f16f128354d44bb
Nami printz <namipr3@gmail.com>
2026-01-11 11:01:01 +0200

    comment fix

--- packages/packages/libs/editor-v1-adapters/src/canvas/get-canvas-iframe-document.ts
+++ packages/packages/libs/editor-v1-adapters/src/canvas/get-canvas-iframe-document.ts
@@ -1,11 +1,3 @@
-type CanvasExtendedWindow = Window & {
-	elementor?: {
-		$preview?: [ HTMLIFrameElement ];
-	};
-};
-
 export function getCanvasIframeDocument() {
-	const extendedWindow = window as unknown as CanvasExtendedWindow;
-
-	return extendedWindow.elementor?.$preview?.[ 0 ]?.contentDocument;
+	return window.elementor?.$preview?.[ 0 ]?.contentDocument;
 }
