6c5b17c9f52dd5f68a51c9a49bd27e313de48bbf
Hein van Vlastuin <hein@obox.co.za>
2026-01-12 12:45:11 +0200

    wip

--- modules/editor-one/module.php
+++ modules/editor-one/module.php
@@ -36,7 +36,7 @@ class Module extends BaseModule {
 			'title'          => esc_html__( 'Editor one', 'elementor' ),
 			'description'    => esc_html__( 'General', 'elementor' ),
 			'hidden'         => true,
-			'default'        => Experiments_Manager::STATE_INACTIVE,
+			'default'        => Experiments_Manager::STATE_ACTIVE,
 			'release_status' => Experiments_Manager::RELEASE_STATUS_STABLE,
 			'mutable'        => false,
 		];
