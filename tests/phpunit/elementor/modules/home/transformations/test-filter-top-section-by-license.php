0fcc5b52e3c5148fbe5df4ccb5cf70394e020dde
Hein van Vlastuin <hein@obox.co.za>
2026-01-13 12:00:51 +0200

    Add 'one' test

--- tests/phpunit/elementor/modules/home/transformations/test-filter-top-section-by-license.php
+++ tests/phpunit/elementor/modules/home/transformations/test-filter-top-section-by-license.php
@@ -71,11 +71,30 @@ class Test_Filter_Top_Section_By_License extends PHPUnit_TestCase {
 		$this->assertEquals( $expected_data, $transformed_data );
 	}
 
+	public function test__should_transform_data_for_one_tier() {
+		// Arrange
+		$original_data = $this->mock_top_section_data();
+
+		$transformation = new Filter_Top_Section_By_License();
+		$transformation->has_pro = true;
+
+		// Act
+		add_filter( 'elementor/admin/homescreen_promotion_tier', function() {
+			return 'one';
+		} );
+		$transformed_data = $transformation->transform( $original_data );
+		$expected_data = $this->mock_top_section_data_transformed_one();
+
+		// Assert
+		$this->assertEquals( $expected_data, $transformed_data );
+	}
+
 	private function mock_top_section_data() {
 		return [
 			'top_with_licences' => [
 				$this->mock_top_section_data_transformed_core()['top_with_licences'],
 				$this->mock_top_section_data_transformed_pro()['top_with_licences'],
+				$this->mock_top_section_data_transformed_one()['top_with_licences'],
 				$this->mock_top_section_data_transformed_essential()['top_with_licences'],
 			],
 			'misc' => [
@@ -140,7 +159,7 @@ class Test_Filter_Top_Section_By_License extends PHPUnit_TestCase {
 		return [
 			'top_with_licences' => [
 				'thing' => [
-					'key' => 'value',
+					'key' => 'one_value',
 				],
 				'license' => [
 					'one'
