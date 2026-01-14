0d374c992cf0b2464519317595e4252d94f2c593
Ron Rostovsky <ronros@elementor.com>
2026-01-14 16:13:31 +0200

    cr

--- packages/packages/core/editor-components/src/sync/load-component-data-after-instance-added.ts
+++ packages/packages/core/editor-components/src/sync/load-component-data-after-instance-added.ts
@@ -4,10 +4,6 @@ import { registerDataHook } from '@elementor/editor-v1-adapters';
 import { loadComponentsAssets } from '../store/actions/load-components-assets';
 
 export function initLoadComponentDataAfterInstanceAdded() {
-	registerDataHook( 'after', 'document/elements/duplicate', ( _args, result: V1Element | V1Element[] ) => {
-		load( result );
-	} );
-
 	registerDataHook( 'after', 'document/elements/paste', ( _args, result: V1Element | V1Element[] ) => {
 		load( result );
 	} );
