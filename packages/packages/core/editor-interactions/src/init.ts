a5951a4804a26b73fe9d077d880c20ac7a1381a1
elchugreeva <elenac@elementor.com>
2026-01-15 12:05:58 +0200

    Internal: Trigger control added to registry and while scrolling data [ED-22487]

--- packages/packages/core/editor-interactions/src/init.ts
+++ packages/packages/core/editor-interactions/src/init.ts
@@ -1,11 +1,18 @@
 import { initCleanInteractionIdsOnDuplicate } from './hooks/on-duplicate';
 import { interactionsRepository } from './interactions-repository';
 import { documentElementsInteractionsProvider } from './providers/document-elements-interactions-provider';
+import { registerInteractionsControl } from './interactions-controls-registry';
+import { Trigger } from './components/controls/trigger';
 
 export function init() {
 	try {
 		interactionsRepository.register( documentElementsInteractionsProvider );
 		initCleanInteractionIdsOnDuplicate();
+		registerInteractionsControl( {
+			type: 'trigger',
+			component: Trigger,
+			options: [ 'load', 'scrollIn' ],
+		} );
 	} catch ( error ) {
 		throw error;
 	}
