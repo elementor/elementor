5a2cd79db505515a3fbca848447559fcb17d3004
YaelAvitan <yael6768889@gmail.com>
2026-01-18 11:38:26 +0200

    WIP on fix-the-transform-bug
    wip

--- packages/packages/core/editor-canvas/src/legacy/replacements/inline-editing/inline-editing-utils.ts
+++ packages/packages/core/editor-canvas/src/legacy/replacements/inline-editing/inline-editing-utils.ts
@@ -4,6 +4,7 @@ import { type LegacyWindow } from '../../types';
 
 export const INLINE_EDITING_PROPERTY_PER_TYPE: Record< string, string > = {
 	'e-heading': 'title',
+	'e-label': 'label_text',
 	'e-paragraph': 'paragraph',
 };
 
