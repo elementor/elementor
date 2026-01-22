2f0984edb2a7c112bc6d3854755300ecca004936
Netanel Baba <ntnelbaba@gmail.com>
2026-01-22 16:27:02 +0200

    And fix types run issues

--- packages/packages/libs/twing/src/@types/twing.d.ts
+++
@@ -1,7 +0,0 @@
-// TODO: Types are broken in the browser version, we will fix it later.
-declare module 'twing/dist/lib.min' {
-	import { createArrayLoader as createArrayLoaderBase, createEnvironment as createEnvironmentBase } from 'twing';
-
-	export const createArrayLoader: typeof createArrayLoaderBase;
-	export const createEnvironment: typeof createEnvironmentBase;
-}
--- packages/packages/libs/twing/src/index.ts
+++ packages/packages/libs/twing/src/index.ts
@@ -1,2 +1,1 @@
-export { type TwingArrayLoader, type TwingEnvironment } from 'twing';
-export { createArrayLoader, createEnvironment } from 'twing/dist/lib.min';
+export { type TwingArrayLoader, type TwingEnvironment, createArrayLoader, createEnvironment } from 'twing';
