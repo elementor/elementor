f4fdd687e1ee65ee1be05e2435de051e2b042bb7
Netanel Baba <ntnelbaba@gmail.com>
2026-01-25 13:40:04 +0200

    Lint

--- packages/packages/core/editor-editing-panel/src/reset-style-props.tsx
+++ packages/packages/core/editor-editing-panel/src/reset-style-props.tsx
@@ -1,6 +1,6 @@
 import { useBoundProp } from '@elementor/editor-controls';
-import { hasVariable } from '@elementor/editor-variables';
 import { type TransformablePropValue } from '@elementor/editor-props';
+import { hasVariable } from '@elementor/editor-variables';
 import { BrushBigIcon } from '@elementor/icons';
 import { controlActionsMenu } from '@elementor/menus';
 import { __ } from '@wordpress/i18n';
@@ -24,12 +24,12 @@ export function useResetStyleValueProps() {
 	const hasInitial = propType.initial_value !== undefined && propType.initial_value !== null;
 	const isRequired = !! propType.settings?.required;
 	const shouldHide = !! propType.settings?.hide_reset;
-	const isPropTypeValue = value as TransformablePropValue<string, string>;
-	const isVariable = isPropTypeValue?.$$type?.includes('variable');
-	const variableExists = isVariable && hasVariable(isPropTypeValue?.value);
+	const isPropTypeValue = value as TransformablePropValue< string, string >;
+	const isVariable = isPropTypeValue?.$$type?.includes( 'variable' );
+	const variableExists = isVariable && hasVariable( isPropTypeValue?.value );
 
 	function calculateVisibility() {
-		if ( ! isStyle || ! hasValue || shouldHide || (isVariable && ! variableExists) ) {
+		if ( ! isStyle || ! hasValue || shouldHide || ( isVariable && ! variableExists ) ) {
 			return false;
 		}
 
