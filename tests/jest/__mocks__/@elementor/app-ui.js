088b36e6ad9779065dd5466aa8c90ef51eb29652
Nicola Peluchetti <nicola.peluchetti@gmail.com>
2026-01-26 23:55:55 +0100

    lint

--- tests/jest/__mocks__/@elementor/app-ui.js
+++ tests/jest/__mocks__/@elementor/app-ui.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 export const Heading = ( { children, className, tag: Tag = 'h3', variant, ...props } ) => (
 	<Tag className={ className } { ...props }>
@@ -6,11 +7,24 @@ export const Heading = ( { children, className, tag: Tag = 'h3', variant, ...pro
 	</Tag>
 );
 
+Heading.propTypes = {
+	children: PropTypes.node,
+	className: PropTypes.string,
+	tag: PropTypes.string,
+	variant: PropTypes.string,
+};
+
 export const Text = ( { children, className, variant, ...props } ) => (
 	<p className={ className } { ...props }>
 		{ children }
 	</p>
 );
+
+Text.propTypes = {
+	children: PropTypes.node,
+	className: PropTypes.string,
+	variant: PropTypes.string,
+};
 
 export const Grid = ( { children, className, container, alignItems, justify, direction, ...props } ) => (
 	<div className={ className } { ...props }>
@@ -18,8 +32,25 @@ export const Grid = ( { children, className, container, alignItems, justify, dir
 	</div>
 );
 
+Grid.propTypes = {
+	children: PropTypes.node,
+	className: PropTypes.string,
+	container: PropTypes.bool,
+	alignItems: PropTypes.string,
+	justify: PropTypes.string,
+	direction: PropTypes.string,
+};
+
 export const Button = ( { text, url, onClick, target, className, ...props } ) => (
-	<a href={ url } onClick={ onClick } target={ target } className={ className } role="link" { ...props }>
+	<a href={ url } onClick={ onClick } target={ target } className={ className } { ...props }>
 		{ text }
 	</a>
 );
+
+Button.propTypes = {
+	text: PropTypes.string,
+	url: PropTypes.string,
+	onClick: PropTypes.func,
+	target: PropTypes.string,
+	className: PropTypes.string,
+};
