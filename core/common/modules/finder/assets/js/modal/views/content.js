239bb586e4e2aaa377f4b2ec4779fca428e756b4
max-zu <zubovmaksim@gmail.com>
2026-01-28 22:18:02 +0100

    ED-22167 Events implementation - Editor [Activation]

--- core/common/modules/finder/assets/js/modal/views/content.js
+++ core/common/modules/finder/assets/js/modal/views/content.js
@@ -1,3 +1,5 @@
+import { debounce } from 'lodash';
+
 import CategoriesView from './categories';
 
 export default class extends Marionette.LayoutView {
@@ -27,6 +29,10 @@ export default class extends Marionette.LayoutView {
 		};
 	}
 
+	initialize() {
+		this.trackSearchInputDebounced = debounce( this.trackSearchInput.bind( this ), 300 );
+	}
+
 	showCategoriesView() {
 		this.content.show( new CategoriesView() );
 	}
@@ -42,8 +48,40 @@ export default class extends Marionette.LayoutView {
 			if ( ! ( this.content.currentView instanceof CategoriesView ) ) {
 				this.showCategoriesView();
 			}
+
+			this.trackSearchInputDebounced();
 		}
 
 		this.content.currentView.$el.toggle( ! ! value );
+	}
+
+	trackSearchInput() {
+		const config = elementorCommon?.eventsManager?.config;
+		const resultsCount = this.getResultsCount();
+		const hasResults = resultsCount > 0;
+
+		elementorCommon?.eventsManager?.dispatchEvent?.( config?.names?.editorOne?.finderSearchInput, {
+			app_type: config?.appTypes?.editor,
+			window_name: config?.appTypes?.editor,
+			interaction_type: config?.triggers?.typing,
+			target_type: config?.targetTypes?.searchInput,
+			target_name: 'finder',
+			interaction_result: hasResults ? config?.interactionResults?.resultsUpdated : config?.interactionResults?.noResults,
+			target_location: config?.locations?.topBar,
+			location_l1: config?.secondaryLocations?.finder,
+			interaction_description: 'Finder search input, follows debounce behavior',
+			metadata: {
+				results_count: resultsCount,
+			},
+		} );
+	}
+
+	getResultsCount() {
+		if ( ! this.content?.currentView ) {
+			return 0;
+		}
+
+		const $items = this.content.currentView.$el.find( '.elementor-finder__results__item' );
+		return $items.filter( ':visible' ).length;
 	}
 }
