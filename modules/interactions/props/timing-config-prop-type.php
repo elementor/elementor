ee6235b325785890b647f5a7180a70080bccd728
Netanel Baba <ntnelbaba@gmail.com>
2026-02-02 14:45:50 +0200

    Lint

--- modules/interactions/props/timing-config-prop-type.php
+++ modules/interactions/props/timing-config-prop-type.php
@@ -3,17 +3,9 @@
 namespace Elementor\Modules\Interactions\Props;
 
 use Elementor\Modules\AtomicWidgets\PropTypes\Base\Object_Prop_Type;
-use Elementor\Modules\AtomicWidgets\PropTypes\Size_Prop_Type;
-use Elementor\Modules\AtomicWidgets\Styles\Size_Constants;
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
-}
-
-class Time_Size_Prop_Type extends Size_Prop_Type {
-	public static function make() {
-		return parent::make()->units( Size_Constants::TIME_UNITS )->default_unit( Size_Constants::UNIT_MILLI_SECOND );
-	}
 }
 
 class Timing_Config_Prop_Type extends Object_Prop_Type {
