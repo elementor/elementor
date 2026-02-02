90d1b02e8eafce2f99deef0fecb89cb3e755cec9
Netanel Baba <ntnelbaba@gmail.com>
2026-02-02 14:23:07 +0200

    Time config prop change

--- modules/interactions/props/timing-config-prop-type.php
+++ modules/interactions/props/timing-config-prop-type.php
@@ -4,9 +4,16 @@ namespace Elementor\Modules\Interactions\Props;
 
 use Elementor\Modules\AtomicWidgets\PropTypes\Base\Object_Prop_Type;
 use Elementor\Modules\AtomicWidgets\PropTypes\Size_Prop_Type;
+use Elementor\Modules\AtomicWidgets\Styles\Size_Constants;
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
+}
+
+class Time_Size_Prop_Type extends Size_Prop_Type {
+	public static function make() {
+		return parent::make()->units( Size_Constants::TIME_UNITS )->default_unit( Size_Constants::UNIT_MILLI_SECOND );
+	}
 }
 
 class Timing_Config_Prop_Type extends Object_Prop_Type {
@@ -16,8 +23,8 @@ class Timing_Config_Prop_Type extends Object_Prop_Type {
 
 	protected function define_shape(): array {
 		return [
-			'duration' => Size_Prop_Type::make()->description( 'The duration to use for the animation' ),
-			'delay' => Size_Prop_Type::make()->description( 'The delay to use for the animation' ),
+			'duration' => Time_Size_Prop_Type::make()->description( 'The duration to use for the animation' ),
+			'delay' => Time_Size_Prop_Type::make()->description( 'The delay to use for the animation' ),
 		];
 	}
 }
