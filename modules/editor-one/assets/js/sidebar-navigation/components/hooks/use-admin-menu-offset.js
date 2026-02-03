0f495417c4363d68ca710b66f7ffc7c9be0b299c
Hein van Vlastuin <hein@obox.co.za>
2026-02-03 11:55:08 +0200

    wip

--- modules/editor-one/assets/js/sidebar-navigation/components/hooks/use-admin-menu-offset.js
+++ modules/editor-one/assets/js/sidebar-navigation/components/hooks/use-admin-menu-offset.js
@@ -2,7 +2,11 @@ import { useEffect, useRef } from '@wordpress/element';
 
 const ADMIN_MENU_WRAP_ID = 'adminmenuwrap';
 const WPCONTENT_ID = 'wpcontent';
+const EDITOR_ONE_TOP_BAR_ID = 'editor-one-top-bar';
+const WPADMINBAR_ID = 'wpadminbar';
 const INITIALIZED_DATA_ATTR = 'data-editor-one-offset-initialized';
+const WPFOOTER_ID = 'wpfooter';
+const WPBODY_CONTENT_ID = 'wpbody-content';
 
 const getIsRTL = () => {
 	return 'rtl' === document.dir || document.body.classList.contains( 'rtl' );
@@ -14,10 +18,14 @@ export const useAdminMenuOffset = () => {
 	useEffect( () => {
 		const adminMenuWrap = document.getElementById( ADMIN_MENU_WRAP_ID );
 		const wpcontent = document.getElementById( WPCONTENT_ID );
+		const wpfooter = document.getElementById( WPFOOTER_ID );
 
 		if ( ! adminMenuWrap || ! wpcontent || wpcontent.hasAttribute( INITIALIZED_DATA_ATTR ) ) {
 			return;
 		}
+
+		const wpbodyContent = document.getElementById( WPBODY_CONTENT_ID );
+		wpbodyContent?.insertBefore( wpfooter, wpbodyContent.querySelector( ':scope > .clear' ) );
 
 		const updateOffset = () => {
 			const isRTL = getIsRTL();
@@ -26,6 +34,16 @@ export const useAdminMenuOffset = () => {
 			const offset = isRTL ? window.innerWidth - rect.left : rect.right;
 
 			wpcontent.style.setProperty( '--editor-one-sidebar-left-offset', `${ offset }px` );
+
+			const wpadminbar = document.getElementById( WPADMINBAR_ID );
+			const topBarHeader = document.getElementById( EDITOR_ONE_TOP_BAR_ID )?.querySelector( ':scope > header' );
+			const windowHeightPx = `${ window.innerHeight }px`;
+			const adminBarHeightPx = wpadminbar ? `${ wpadminbar.clientHeight }px` : '0px';
+			const topBarHeaderHeightPx = topBarHeader ? `${ topBarHeader.clientHeight }px` : '0px';
+
+			wpcontent.style.setProperty( '--e-window-height', windowHeightPx );
+			wpcontent.style.setProperty( '--e-admin-bar-height', adminBarHeightPx );
+			wpcontent.style.setProperty( '--e-top-bar-header-height', topBarHeaderHeightPx );
 		};
 
 		updateOffset();
@@ -33,6 +51,10 @@ export const useAdminMenuOffset = () => {
 		const resizeObserver = new ResizeObserver( updateOffset );
 
 		resizeObserver.observe( wpcontent );
+		const topBarForObserve = document.getElementById( EDITOR_ONE_TOP_BAR_ID );
+		if ( topBarForObserve ) {
+			resizeObserver.observe( topBarForObserve );
+		}
 		window.addEventListener( 'resize', updateOffset );
 
 		wpcontent.setAttribute( INITIALIZED_DATA_ATTR, 'true' );
