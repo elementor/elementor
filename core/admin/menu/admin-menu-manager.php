657ad087fc44f228878e6e4921d6ecd9201e5a39
Hein van Vlastuin <hein@obox.co.za>
2026-02-04 16:49:34 +0200

    Add deprecation notices

--- core/admin/menu/admin-menu-manager.php
+++ core/admin/menu/admin-menu-manager.php
@@ -7,9 +7,12 @@ use Elementor\Core\Admin\Menu\Interfaces\Admin_Menu_Item_Has_Position;
 use Elementor\Core\Admin\Menu\Interfaces\Admin_Menu_Item_With_Page;
 
 if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly.
+	exit;
 }
 
+/**
+ * @deprecated 4.0.0 This class is deprecated. Use the new admin menu system instead.
+ */
 class Admin_Menu_Manager {
 
 	/**
@@ -17,15 +20,27 @@ class Admin_Menu_Manager {
 	 */
 	private $items = [];
 
+	/**
+	 * @deprecated 4.0.0
+	 */
 	public function register( $item_slug, Admin_Menu_Item $item ) {
+		$this->trigger_deprecation_notice( __METHOD__, '4.0.0' );
 		$this->items[ $item_slug ] = $item;
 	}
 
+	/**
+	 * @deprecated 4.0.0
+	 */
 	public function unregister( $item_slug ) {
+		$this->trigger_deprecation_notice( __METHOD__, '4.0.0' );
 		unset( $this->items[ $item_slug ] );
 	}
 
+	/**
+	 * @deprecated 4.0.0
+	 */
 	public function get( $item_slug ) {
+		$this->trigger_deprecation_notice( __METHOD__, '4.0.0' );
 		if ( empty( $this->items[ $item_slug ] ) ) {
 			return null;
 		}
@@ -33,11 +48,20 @@ class Admin_Menu_Manager {
 		return $this->items[ $item_slug ];
 	}
 
+	/**
+	 * @deprecated 4.0.0
+	 */
 	public function get_all() {
+		$this->trigger_deprecation_notice( __METHOD__, '4.0.0' );
 		return $this->items;
 	}
 
+	/**
+	 * @deprecated 4.0.0
+	 */
 	public function register_actions() {
+		$this->trigger_deprecation_notice( __METHOD__, '4.0.0' );
+
 		add_action( 'admin_menu', function () {
 			$this->register_wp_menus();
 		}, 20 );
@@ -48,11 +72,11 @@ class Admin_Menu_Manager {
 	}
 
 	private function register_wp_menus() {
-		do_action( 'elementor/admin/menu/register', $this );
+		$this->trigger_deprecated_action( 'elementor/admin/menu/register', [ $this ], '4.0.0' );
 
 		$hooks = [];
 
-		foreach ( $this->get_all() as $item_slug => $item ) {
+		foreach ( $this->items as $item_slug => $item ) {
 			$is_top_level = empty( $item->get_parent_slug() );
 
 			if ( $is_top_level ) {
@@ -62,7 +86,7 @@ class Admin_Menu_Manager {
 			}
 		}
 
-		do_action( 'elementor/admin/menu/after_register', $this, $hooks );
+		$this->trigger_deprecated_action( 'elementor/admin/menu/after_register', [ $this, $hooks ], '4.0.0' );
 	}
 
 	private function register_top_level_menu( $item_slug, Admin_Menu_Item $item ) {
@@ -101,7 +125,7 @@ class Admin_Menu_Manager {
 	}
 
 	private function hide_invisible_menus() {
-		foreach ( $this->get_all() as $item_slug => $item ) {
+		foreach ( $this->items as $item_slug => $item ) {
 			if ( $item->is_visible() ) {
 				continue;
 			}
@@ -114,5 +138,23 @@ class Admin_Menu_Manager {
 				remove_submenu_page( $item->get_parent_slug(), $item_slug );
 			}
 		}
+	}
+
+	private function trigger_deprecation_notice( $function_name, $version, $replacement = '' ) {
+		\Elementor\Plugin::$instance->modules_manager
+			->get_modules( 'dev-tools' )
+			->deprecation
+			->deprecated_function( $function_name, $version, $replacement );
+	}
+
+	private function trigger_deprecated_action( $hook, $args, $version, $replacement = '' ) {
+		if ( ! has_action( $hook ) ) {
+			return;
+		}
+
+		\Elementor\Plugin::$instance->modules_manager
+			->get_modules( 'dev-tools' )
+			->deprecation
+			->do_deprecated_action( $hook, $args, $version, $replacement );
 	}
 }
