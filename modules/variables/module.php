68c51fe48234c89572f0de3dc8460965dbf80dfc
Davids Seveloff <94905340+davseve@users.noreply.github.com>
2026-02-05 21:00:03 +0200

    Add blank line before Module class definition

--- modules/variables/module.php
+++ modules/variables/module.php
@@ -16,6 +16,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 
+
 class Module extends BaseModule {
 	const MODULE_NAME = 'e-variables';
 	const EXPERIMENT_NAME = 'e_variables';
