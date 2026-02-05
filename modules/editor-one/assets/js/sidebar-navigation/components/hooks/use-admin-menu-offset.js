efdbfe879b1c670376796ff2b2de0d1be6b10c74
Hein van Vlastuin <94352322+hein-obox@users.noreply.github.com>
2026-02-05 12:22:10 +0200

    Fix: Revert main Elementor One menu update [ED-22819] (#34652)

--- modules/editor-one/assets/js/sidebar-navigation/components/hooks/use-admin-menu-offset.js
+++ modules/editor-one/assets/js/sidebar-navigation/components/hooks/use-admin-menu-offset.js
@@ -2,11 +2,7 @@ import { useEffect, useRef } from '@wordpress/element';
 
 const ADMIN_MENU_WRAP_ID = 'adminmenuwrap';
 const WPCONTENT_ID = 'wpcontent';
-const EDITOR_ONE_TOP_BAR_ID = 'editor-one-top-bar';
-const WPADMINBAR_ID = 'wpadminbar';
 const INITIALIZED_DATA_ATTR = 'data-editor-one-offset-initialized';
-const WPFOOTER_ID = 'wpfooter';
-const WPBODY_CONTENT_ID = 'wpbody-content';
 
 const getIsRTL = () => {
 	return 'rtl' === document.dir || document.body.classList.contains( 'rtl' );
@@ -23,24 +19,13 @@ export const useAdminMenuOffset = () => {
 			return;
 		}
 
-		const wpfooter = document.getElementById( WPFOOTER_ID );
-		const wpbodyContent = document.getElementById( WPBODY_CONTENT_ID );
-		wpbodyContent?.insertBefore( wpfooter, wpbodyContent.querySelector( ':scope > .clear' ) );
-
-		const wpAdminBar = document.getElementById( WPADMINBAR_ID );
-		const topBarHeader = document.getElementById( EDITOR_ONE_TOP_BAR_ID )?.querySelector( ':scope > header' );
-
 		const updateOffset = () => {
 			const isRTL = getIsRTL();
 			const rect = adminMenuWrap.getBoundingClientRect();
 
 			const offset = isRTL ? window.innerWidth - rect.left : rect.right;
-			const adminBarHeightPx = `${ wpAdminBar?.clientHeight ?? 0 }px`;
-			const topBarHeaderHeightPx = `${ topBarHeader?.clientHeight ?? 0 }px`;
 
 			wpcontent.style.setProperty( '--editor-one-sidebar-left-offset', `${ offset }px` );
-			wpcontent.style.setProperty( '--e-admin-bar-height', adminBarHeightPx );
-			wpcontent.style.setProperty( '--e-top-bar-header-height', topBarHeaderHeightPx );
 		};
 
 		updateOffset();
