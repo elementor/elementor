09c8bf7e49e5778a2ca106b3295f8b544b221b35
Hein van Vlastuin <hein@obox.co.za>
2026-02-06 13:53:58 +0200

    Unskip tests

--- tests/playwright/sanity/core/app/modules/onboarding/onboarding.test.ts
+++ tests/playwright/sanity/core/app/modules/onboarding/onboarding.test.ts
@@ -8,8 +8,7 @@ const BUTTON_CLASSES = {
 	disabled: /e-onboarding__button--disabled/,
 };
 
-// Unskip: ED-18816 - Refactor onboarding test
-test.describe.skip( 'On boarding @onBoarding', async () => {
+test.describe( 'On boarding @onBoarding', async () => {
 	let originalActiveTheme: string;
 	test.beforeAll( async ( { browser, apiRequests }, testInfo ) => {
 		const context = await browser.newContext();
