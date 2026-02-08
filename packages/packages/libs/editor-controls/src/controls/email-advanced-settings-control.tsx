c955067477ee523d3cc361bdb422d074f186d191
YaelAvitan <yael6768889@gmail.com>
2026-02-08 15:33:02 +0200

    show more-less

--- packages/packages/libs/editor-controls/src/controls/email-advanced-settings-control.tsx
+++ packages/packages/libs/editor-controls/src/controls/email-advanced-settings-control.tsx
@@ -1,9 +1,9 @@
 import * as React from 'react';
 import { useState } from 'react';
 import { emailAdvancedPropTypeUtil } from '@elementor/editor-props';
-import { MinusIcon, PlusIcon } from '@elementor/icons';
-import { Collapse, Grid, IconButton, Stack } from '@elementor/ui';
+import { Grid, Stack } from '@elementor/ui';
 import { __ } from '@wordpress/i18n';
+import { CollapsibleContent } from './collapsible-content';
 
 import { PropKeyProvider, PropProvider, useBoundProp } from '../bound-prop-context';
 import { ControlFormLabel } from '../components/control-form-label';
@@ -12,109 +12,68 @@ import { createControl } from '../create-control';
 import { type ControlProps } from '../utils/types';
 import { TextControl } from './text-control';
 
-const SIZE = 'tiny';
-
 type Props = ControlProps< {
 	label?: string;
 } >;
 
 export const EmailAdvancedSettingsControl = createControl( ( props: Props ) => {
-	const { value, path, setValue, ...propContext } = useBoundProp( emailAdvancedPropTypeUtil );
-	const [ isActive, setIsActive ] = useState( false );
+	const { value, setValue, ...propContext } = useBoundProp( emailAdvancedPropTypeUtil );
 
-	const {
-		label = __( 'Advanced Settings', 'elementor' ),
-	} = props || {};
-
-	const onEnabledChange = () => {
-		setIsActive( ! isActive );
-	};
+	const { label = __( 'Advanced Settings', 'elementor' ) } = props || {};
 
 	return (
 		<PropProvider { ...propContext } value={ value } setValue={ setValue }>
 			<Stack gap={ 1.5 }>
-				<Stack
-					direction="row"
-					sx={ {
-						justifyContent: 'space-between',
-						alignItems: 'center',
-						marginInlineEnd: -0.75,
-					} }
-				>
-					<ControlLabel>{ label }</ControlLabel>
-					<ToggleIconControl
-						active={ isActive }
-						onIconClick={ onEnabledChange }
-						label={ __( 'Toggle advanced settings', 'elementor' ) }
-					/>
-				</Stack>
-				<Collapse in={ isActive } timeout="auto" unmountOnExit>
+				<ControlLabel>{ label }</ControlLabel>
+
+				<CollapsibleContent defaultOpen={ false }>
 					<Stack gap={ 1.5 }>
-						<PropKeyProvider bind={ 'from-name' }>
+						<PropKeyProvider bind="from-name">
 							<Grid container direction="column" gap={ 0.5 }>
 								<Grid item>
 									<ControlFormLabel>{ __( 'From name', 'elementor' ) }</ControlFormLabel>
 								</Grid>
 								<Grid item>
-									<TextControl
-										placeholder={ __( '....', 'elementor' ) }
-									/>
+									<TextControl placeholder={ __( '....', 'elementor' ) } />
 								</Grid>
 							</Grid>
 						</PropKeyProvider>
-						<PropKeyProvider bind={ 'reply-to' }>
+
+						<PropKeyProvider bind="reply-to">
 							<Grid container direction="column" gap={ 0.5 }>
 								<Grid item>
 									<ControlFormLabel>{ __( 'Reply-to', 'elementor' ) }</ControlFormLabel>
 								</Grid>
 								<Grid item>
-									<TextControl
-										placeholder={ __( '....', 'elementor' ) }
-									/>
+									<TextControl placeholder={ __( '....', 'elementor' ) } />
 								</Grid>
 							</Grid>
 						</PropKeyProvider>
-						<PropKeyProvider bind={ 'cc' }>
+
+						<PropKeyProvider bind="cc">
 							<Grid container direction="column" gap={ 0.5 }>
 								<Grid item>
 									<ControlFormLabel>{ __( 'Cc', 'elementor' ) }</ControlFormLabel>
 								</Grid>
 								<Grid item>
-									<TextControl
-										placeholder={ __( '....', 'elementor' ) }
-									/>
+									<TextControl placeholder={ __( '....', 'elementor' ) } />
 								</Grid>
 							</Grid>
 						</PropKeyProvider>
-						<PropKeyProvider bind={ 'bcc' }>
+
+						<PropKeyProvider bind="bcc">
 							<Grid container direction="column" gap={ 0.5 }>
 								<Grid item>
 									<ControlFormLabel>{ __( 'Bcc', 'elementor' ) }</ControlFormLabel>
 								</Grid>
 								<Grid item>
-									<TextControl
-										placeholder={ __( '....', 'elementor' ) }
-									/>
+									<TextControl placeholder={ __( '....', 'elementor' ) } />
 								</Grid>
 							</Grid>
 						</PropKeyProvider>
 					</Stack>
-				</Collapse>
+				</CollapsibleContent>
 			</Stack>
 		</PropProvider>
 	);
 } );
-
-type ToggleIconControlProps = {
-	active: boolean;
-	onIconClick: () => void;
-	label?: string;
-};
-
-const ToggleIconControl = ( { active, onIconClick, label }: ToggleIconControlProps ) => {
-	return (
-		<IconButton size={ SIZE } onClick={ onIconClick } aria-label={ label }>
-			{ active ? <MinusIcon fontSize={ SIZE } /> : <PlusIcon fontSize={ SIZE } /> }
-		</IconButton>
-	);
-};
