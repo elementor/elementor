8ed699121db7f55e8c76c38a4181b8e9dc66e7a8
Nikita Murzin <mykytamu@elementor.com>
2026-02-09 16:16:53 +0100

    Internal: return prop type usage [ED-22619]

--- packages/packages/core/editor-editing-panel/src/controls-registry/controls-registry.tsx
+++ packages/packages/core/editor-editing-panel/src/controls-registry/controls-registry.tsx
@@ -32,6 +32,7 @@ import {
 	type PropTypeUtil,
 	queryPropTypeUtil,
 	sizePropTypeUtil,
+	stringArrayPropTypeUtil,
 	stringPropTypeUtil,
 } from '@elementor/editor-props';
 
@@ -50,7 +51,7 @@ const controlTypes = {
 	textarea: { component: TextAreaControl, layout: 'full', propTypeUtil: stringPropTypeUtil },
 	size: { component: SizeControl, layout: 'two-columns', propTypeUtil: sizePropTypeUtil },
 	select: { component: SelectControlWrapper, layout: 'two-columns', propTypeUtil: stringPropTypeUtil },
-	chips: { component: ChipsControl, layout: 'full', propTypeUtil: undefined },
+	chips: { component: ChipsControl, layout: 'full', propTypeUtil: stringArrayPropTypeUtil },
 	link: { component: LinkControl, layout: 'custom', propTypeUtil: linkPropTypeUtil },
 	query: { component: QueryControl, layout: 'full', propTypeUtil: queryPropTypeUtil },
 	url: { component: UrlControl, layout: 'full', propTypeUtil: stringPropTypeUtil },
