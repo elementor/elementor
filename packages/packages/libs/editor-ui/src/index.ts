562d6eb3805701d11c5249502adb3b505e3a720e
Nami printz <namipr3@gmail.com>
2026-02-10 15:11:35 +0200

    resolve conflicts

--- packages/packages/libs/editor-ui/src/index.ts
+++ packages/packages/libs/editor-ui/src/index.ts
@@ -12,18 +12,10 @@ export { GlobalDialog, openDialog, closeDialog } from './components/global-dialo
 export { SearchField } from './components/search-field';
 export { Form } from './components/form';
 export { CtaButton } from './components/cta-button';
-<<<<<<< HEAD
-export { PromotionInfotip } from './components/promotion-infotip';
-export { PromotionPopover } from './components/promotion-popover';
-export { PromotionChip } from './components/promotion-chip';
-export { PromotionAlert } from './components/promotion-alert';
-=======
 export { PromotionInfotip } from './components/promotions/promotion-infotip';
 export { PromotionPopover } from './components/promotions/promotion-popover';
 export { PromotionChip } from './components/promotions/promotion-chip';
 export { PromotionAlert } from './components/promotions/promotion-alert';
-export { FloatingActionsBar, useFloatingActionsBar } from './components/floating-bar';
->>>>>>> 10d9820496 (Internal: Enhances variable manager with renew promotions [ED-21889] (#34699))
 
 export * from './components/popover';
 export * from './components/save-changes-dialog';
@@ -31,8 +23,4 @@ export { ConfirmationDialog } from './components/confirmation-dialog';
 
 // hooks
 export { useEditable } from './hooks/use-editable';
-<<<<<<< HEAD
-=======
-export { useTextFieldAutoSelect } from './hooks/use-text-field-auto-select';
 export { useCanvasClickHandler } from './hooks/use-canvas-click-handler';
->>>>>>> 10d9820496 (Internal: Enhances variable manager with renew promotions [ED-21889] (#34699))
