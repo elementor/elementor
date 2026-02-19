fc56c84c87e97061d00c4aa30e843f9cefb85f6d
Netanel Baba <ntnelbaba@gmail.com>
2026-02-19 19:13:50 +0200

    Internal: Change variables limit to 1000 [ED-20171]

---
+++ modules/variables/storage/constants.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Elementor\Modules\Variables\Storage;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+class Constants {
+    public const FORMAT_VERSION_V1 = 1;
+    public const FORMAT_VERSION_V2 = 2;
+    public const TOTAL_VARIABLES_COUNT = 1000;
+    public const VARIABLES_META_KEY = '_elementor_global_variables';
+}
