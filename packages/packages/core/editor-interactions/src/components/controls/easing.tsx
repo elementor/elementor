c6ccf0f522f4d49edaadc573400cd841dc7a16a7
Nami printz <namipr3@gmail.com>
2026-02-22 15:06:35 +0200

    Internal: Centralize interaction control options as single source of truth [ED-22093]

--- packages/packages/core/editor-interactions/src/components/controls/easing.tsx
+++ packages/packages/core/editor-interactions/src/components/controls/easing.tsx
@@ -5,11 +5,8 @@ import { type FieldProps } from '../../types';
 import { PromotionSelect } from '../../ui/promotion-select';
 import { DEFAULT_VALUES } from '../interaction-details';
 
-const BASE_OPTIONS = {
+export const EASING_OPTIONS = {
 	easeIn: __( 'Ease In', 'elementor' ),
-};
-
-const DISABLED_OPTIONS = {
 	easeInOut: __( 'Ease In Out', 'elementor' ),
 	easeOut: __( 'Ease Out', 'elementor' ),
 	backIn: __( 'Back In', 'elementor' ),
@@ -18,12 +15,22 @@ const DISABLED_OPTIONS = {
 	linear: __( 'Linear', 'elementor' ),
 };
 
+export const BASE_EASINGS: string[] = [ 'easeIn' ];
+
 export function Easing( {}: FieldProps ) {
+	const baseOptions = Object.fromEntries(
+		Object.entries( EASING_OPTIONS ).filter( ( [ key ] ) => BASE_EASINGS.includes( key ) ),
+	);
+
+	const disabledOptions = Object.fromEntries(
+		Object.entries( EASING_OPTIONS ).filter( ( [ key ] ) => ! BASE_EASINGS.includes( key ) ),
+	);
+
 	return (
 		<PromotionSelect
 			value={ DEFAULT_VALUES.easing }
-			baseOptions={ BASE_OPTIONS }
-			disabledOptions={ DISABLED_OPTIONS }
+			baseOptions={ baseOptions }
+			disabledOptions={ disabledOptions }
 			promotionLabel={ __( 'PRO features', 'elementor' ) }
 			promotionContent={ __( 'Upgrade to control the smoothness of the interaction.', 'elementor' ) }
 			upgradeUrl="https://go.elementor.com/go-pro-interactions-easing-modal/"
--- packages/packages/core/editor-interactions/src/index.ts
+++ packages/packages/core/editor-interactions/src/index.ts
@@ -12,3 +12,6 @@ export { ELEMENTS_INTERACTIONS_PROVIDER_KEY_PREFIX } from './providers/document-
 export { init } from './init';
 export { registerInteractionsControl } from './interactions-controls-registry';
 export type { InteractionItemPropValue, FieldProps } from './types';
+export { TRIGGER_OPTIONS, BASE_TRIGGERS } from './components/controls/trigger';
+export { EASING_OPTIONS, BASE_EASINGS } from './components/controls/easing';
+export { REPLAY_OPTIONS, BASE_REPLAY } from './components/controls/replay';
