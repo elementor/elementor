6df421d044380043ece4c3576f61be87244d3749
Nami printz <namipr3@gmail.com>
2026-02-22 15:54:47 +0200

    wip

--- packages/packages/core/editor-interactions/src/index.ts
+++ packages/packages/core/editor-interactions/src/index.ts
@@ -11,7 +11,7 @@ export {
 export { ELEMENTS_INTERACTIONS_PROVIDER_KEY_PREFIX } from './providers/document-elements-interactions-provider';
 export { init } from './init';
 export { registerInteractionsControl } from './interactions-controls-registry';
-export type { InteractionItemPropValue, FieldProps } from './types';
+export type { InteractionItemPropValue, FieldProps, ReplayFieldProps } from './types';
 export { TRIGGER_OPTIONS, BASE_TRIGGERS } from './components/controls/trigger';
 export { EASING_OPTIONS, BASE_EASINGS } from './components/controls/easing';
 export { REPLAY_OPTIONS, BASE_REPLAY } from './components/controls/replay';
