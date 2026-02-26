8dab8627996b6f45f4462cfe742da57da5848cfd
elchugreeva <elenac@elementor.com>
2026-02-26 17:38:18 +0200

    wip

--- modules/interactions/presets.php
+++ modules/interactions/presets.php
@@ -13,56 +13,6 @@ class Presets {
 	const DEFAULT_SCALE_START = 0;
 	const DEFAULT_EASING = 'easeIn';
 
-	public function defaults() {
-		return [
-			'defaultDuration' => self::DEFAULT_DURATION,
-			'defaultDelay' => self::DEFAULT_DELAY,
-			'slideDistance' => self::DEFAULT_SLIDE_DISTANCE,
-			'scaleStart' => self::DEFAULT_SCALE_START,
-			'defaultEasing' => self::DEFAULT_EASING,
-		];
-	}
-}
-<?php
-
-namespace Elementor\Modules\Interactions;
-
-if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly.
-}
-
-class Presets {
-	const DEFAULT_DURATION = 600;
-	const DEFAULT_DELAY = 0;
-	const DEFAULT_SLIDE_DISTANCE = 100;
-	const DEFAULT_SCALE_START = 0;
-	const DEFAULT_EASING = 'easeIn';
-
-	public function defaults() {
-		return [
-			'defaultDuration' => self::DEFAULT_DURATION,
-			'defaultDelay' => self::DEFAULT_DELAY,
-			'slideDistance' => self::DEFAULT_SLIDE_DISTANCE,
-			'scaleStart' => self::DEFAULT_SCALE_START,
-			'defaultEasing' => self::DEFAULT_EASING,
-		];
-	}
-}
-<?php
-
-namespace Elementor\Modules\Interactions;
-
-if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly.
-}
-
-class Presets {
-	const DEFAULT_DURATION = 600;
-	const DEFAULT_DELAY = 0;
-	const DEFAULT_SLIDE_DISTANCE = 100;
-	const DEFAULT_SCALE_START = 0;
-	const DEFAULT_EASING = 'easeIn';
-
 	const TRIGGERS = [ 'load', 'scrollIn', 'scrollOut', 'scrollOn', 'hover', 'click' ];
 
 	const EFFECTS = [ 'fade', 'slide', 'scale', 'custom' ];
