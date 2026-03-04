59967438d8284b36bda89fcb53cde0659ab69f9c
Ron Rostovsky <ronros@elementor.com>
2026-03-04 10:58:18 +0200

    wip

--- assets/dev/js/editor/regions/panel/pages/elements/views/widget-creation.js
+++ assets/dev/js/editor/regions/panel/pages/elements/views/widget-creation.js
@@ -1,16 +1,16 @@
 var PanelElementsWidgetCreationView;
 
-PanelElementsWidgetCreationView = Marionette.ItemView.extend( {
 const TEMPLATES = {
 	EMPTY_STATE: '#tmpl-elementor-panel-elements-widget-creation-empty-state',
 	SEARCH_FOOTER: '#tmpl-elementor-panel-elements-widget-creation-search-footer',
 };
 
-getTemplate() {
-	return this.options.emptyResults
-		? TEMPLATES.EMPTY_STATE
-		: TEMPLATES.SEARCH_FOOTER;
-},
+PanelElementsWidgetCreationView = Marionette.ItemView.extend( {
+	getTemplate() {
+		return this.options.emptyResults
+			? TEMPLATES.EMPTY_STATE
+			: TEMPLATES.SEARCH_FOOTER;
+	},
 
 	className() {
 		const baseClass = 'elementor-panel-elements-widget-creation';
