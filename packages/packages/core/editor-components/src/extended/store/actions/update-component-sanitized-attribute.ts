7cc760c78c2a3ac8dce1a9f4664f93145ac4beed
IshayMaya <47317545+IshayMaya@users.noreply.github.com>
2026-03-04 13:16:14 +0200

    Internal: Expose the components apis [ED-23035] (#34930)

--- packages/packages/core/editor-components/src/extended/store/actions/update-component-sanitized-attribute.ts
+++ packages/packages/core/editor-components/src/extended/store/actions/update-component-sanitized-attribute.ts
@@ -1,8 +1,7 @@
-import { __dispatch as dispatch } from '@elementor/store';
-
-import { type SanitizeAttributes, slice } from '../../../store/store';
+import { componentsActions } from '../../../store/dispatchers';
+import { type SanitizeAttributes } from '../../../store/store';
 import { type ComponentId } from '../../../types';
 
 export function updateComponentSanitizedAttribute( componentId: ComponentId, attribute: SanitizeAttributes ) {
-	dispatch( slice.actions.updateComponentSanitizedAttribute( { componentId, attribute } ) );
+	componentsActions.updateComponentSanitizedAttribute( componentId, attribute );
 }
