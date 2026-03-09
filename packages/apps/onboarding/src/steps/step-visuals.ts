02afc8e662d4abb648308598a07867036005df1d
Hein van Vlastuin <hein@obox.co.za>
2026-03-09 11:19:40 +0200

    Update images

--- packages/apps/onboarding/src/steps/step-visuals.ts
+++ packages/apps/onboarding/src/steps/step-visuals.ts
@@ -23,32 +23,32 @@ const buildBackground = ( fileName: string ) => {
 };
 
 const DEFAULT_CONFIG: StepVisualConfig = {
-	background: buildBackground( 'step-1.png' ),
+	background: buildBackground( 'step-1.webp' ),
 };
 
 export const LOGIN_CONFIG: StepVisualConfig = {
-	background: buildBackground( 'login.png' ),
+	background: buildBackground( 'login.webp' ),
 };
 
 const stepVisuals: Record< StepIdType, StepVisualConfig > = {
 	[ StepId.BUILDING_FOR ]: {
-		background: buildBackground( 'step-1.png' ),
+		background: buildBackground( 'step-1.webp' ),
 	},
 	[ StepId.SITE_ABOUT ]: {
-		background: buildBackground( 'step-2.png' ),
+		background: buildBackground( 'step-2.webp' ),
 		video: getOnboardingVideoUrl( 'step-2.webm' ),
 	},
 	[ StepId.EXPERIENCE_LEVEL ]: {
-		background: buildBackground( 'step-3.png' ),
+		background: buildBackground( 'step-3.webp' ),
 		video: getOnboardingVideoUrl( 'step-3.webm' ),
 	},
 	[ StepId.THEME_SELECTION ]: {
-		background: buildBackground( 'step-4.png' ),
+		background: buildBackground( 'step-4.webp' ),
 		video: getOnboardingVideoUrl( 'step-4.webm' ),
 		contentMaxWidth: CONTENT_MAX_WIDTH_WIDE_ELEMENT,
 	},
 	[ StepId.SITE_FEATURES ]: {
-		background: buildBackground( 'step-5.png' ),
+		background: buildBackground( 'step-5.webp' ),
 		video: getOnboardingVideoUrl( 'step-5.webm' ),
 		contentMaxWidth: CONTENT_MAX_WIDTH_WIDE_ELEMENT,
 	},
