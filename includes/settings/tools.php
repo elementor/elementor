10069f3a72defa9ea4afc2b521e83e192490ebd5
Louis Wolmarans <louiswol94@gmail.com>
2026-03-10 10:47:48 +0200

    Fix: Editor admin UI changes due to WP 7.0 [ED-23094]

--- includes/settings/tools.php
+++ includes/settings/tools.php
@@ -381,11 +381,11 @@ class Tools extends Settings_Page {
 								'label' => esc_html__( 'Rollback Version', 'elementor' ),
 								'field_args' => [
 									'type' => 'raw_html',
-									'html' => sprintf(
-										$rollback_html . '<a data-placeholder-text="%1$s v{VERSION}" href="#" data-placeholder-url="%2$s" class="button elementor-button-spinner elementor-rollback-button">%1$s</a>',
-										esc_html__( 'Reinstall', 'elementor' ),
-										wp_nonce_url( admin_url( 'admin-post.php?action=elementor_rollback&version=VERSION' ), 'elementor_rollback' )
-									),
+								'html' => sprintf(
+									$rollback_html . '<button data-placeholder-text="%1$s v{VERSION}" data-placeholder-url="%2$s" class="button elementor-button-spinner elementor-rollback-button">%1$s</button>',
+									esc_html__( 'Reinstall', 'elementor' ),
+									wp_nonce_url( admin_url( 'admin-post.php?action=elementor_rollback&version=VERSION' ), 'elementor_rollback' )
+								),
 									'desc' => $this->get_warning_span( esc_html__( 'Warning: Please backup your database before making the rollback.', 'elementor' ) ),
 								],
 							],
