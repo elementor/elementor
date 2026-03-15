0eaaf315af9ee4ddb128c1550b4910a039bb1ee0
Rami Yushuvaev <ramiy@elementor.com>
2026-03-15 16:33:02 +0200

    Revert

--- core/admin/admin.php
+++ core/admin/admin.php
@@ -1181,7 +1181,7 @@ class Admin extends App {
 	}
 
 	public function register_ajax_hints( $ajax_manager ) {
-		$ajax_manager->register_ajax_action( 'elementor_image_optimization_campaign', [ $this, 'ajax_image_optimization_campaign' ] );
+		$ajax_manager->register_ajax_action( 'elementor_image_optimization_campaign', [ $this, 'ajax_set_image_optimization_campaign' ] );
 		$ajax_manager->register_ajax_action( 'elementor_core_site_mailer_campaign', [ $this, 'ajax_site_mailer_campaign' ] );
 		$ajax_manager->register_ajax_action( 'elementor_core_ally_campaign', [ $this, 'ajax_ally_campaign' ] );
 	}
@@ -1200,7 +1200,7 @@ class Admin extends App {
 		set_transient( 'elementor_ea11y_campaign', $campaign_data, 30 * DAY_IN_SECONDS );
 	}
 
-	public function ajax_image_optimization_campaign( $request ) {
+	public function ajax_set_image_optimization_campaign( $request ) {
 		if ( ! current_user_can( 'install_plugins' ) ) {
 			return;
 		}
