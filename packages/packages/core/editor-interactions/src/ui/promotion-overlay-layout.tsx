48b14bf3ea360b184c363956beea9f8758d353f1
Nami printz <namipr3@gmail.com>
2026-03-18 16:38:16 +0200

    lint

--- packages/packages/core/editor-interactions/src/ui/promotion-overlay-layout.tsx
+++ packages/packages/core/editor-interactions/src/ui/promotion-overlay-layout.tsx
@@ -2,8 +2,8 @@ import * as React from 'react';
 import { forwardRef, type ReactNode } from 'react';
 import { Box } from '@elementor/ui';
 
-const PROMOTION_OVERLAY_GRID = '1 / 1';
-const PROMOTION_CHIP_OFFSET = '50%';
+const OVERLAY_GRID = '1 / 1';
+const CHIP_OFFSET = '50%';
 
 type PromotionOverlayLayoutProps = {
 	children: ReactNode;
@@ -13,8 +13,8 @@ type PromotionOverlayLayoutProps = {
 export const PromotionOverlayLayout = forwardRef< HTMLDivElement, PromotionOverlayLayoutProps >(
 	( { children, promotionChip }, ref ) => (
 		<Box ref={ ref } sx={ { display: 'grid', alignItems: 'center' } }>
-			<Box sx={ { gridArea: PROMOTION_OVERLAY_GRID } }>{ children }</Box>
-			<Box sx={ { gridArea: PROMOTION_OVERLAY_GRID, marginInlineEnd: PROMOTION_CHIP_OFFSET, justifySelf: 'end' } }>
+			<Box sx={ { gridArea: OVERLAY_GRID } }>{ children }</Box>
+			<Box sx={ { gridArea: OVERLAY_GRID, marginInlineEnd: CHIP_OFFSET, justifySelf: 'end' } }>
 				{ promotionChip }
 			</Box>
 		</Box>
