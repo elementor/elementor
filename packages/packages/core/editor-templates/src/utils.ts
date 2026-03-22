9b1c03804350059f3b9119aa44e264dd3bc8a6e8
Ron Rostovsky <ronros@elementor.com>
2026-03-22 17:37:24 +0200

    change version comparison

--- packages/packages/core/editor-templates/src/utils.ts
+++ packages/packages/core/editor-templates/src/utils.ts
@@ -1,6 +1,6 @@
 import { isProActive, isProAtLeast } from '@elementor/utils';
 
-const MIN_PRO_VERSION_FOR_SELF_HANDLED_STYLES = '4.0';
+const MIN_PRO_VERSION_FOR_SELF_HANDLED_STYLES = '4.1';
 
 export const isHandlingTemplateStyles = () =>
 	isProActive() && ! isProAtLeast( MIN_PRO_VERSION_FOR_SELF_HANDLED_STYLES );
