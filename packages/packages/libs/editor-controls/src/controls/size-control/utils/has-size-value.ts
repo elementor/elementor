8403757d976e210e5271ca61c2912bd8f531c36e
Batsirai Muchareva <batsiraim@elementor.red>
2026-03-23 18:01:45 +0200

    wip

--- packages/packages/libs/editor-controls/src/controls/size-control/utils/has-size-value.ts
+++
@@ -1,5 +0,0 @@
-import { type SizePropValue } from '@elementor/editor-props';
-
-export const hasSizeValue = ( size: SizePropValue[ 'value' ][ 'size' ] ): boolean => {
-	return Boolean( size ) || size === 0;
-};
