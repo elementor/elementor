9ad6d30429a82569ecaea4c6292b4c9872fb23f5
svitlanadykun <svitlanad@elementor.red>
2026-03-24 08:27:13 +0100

    wp 7 beta fixes

--- tests/playwright/sanity/core/base/document.test.ts
+++ tests/playwright/sanity/core/base/document.test.ts
@@ -49,13 +49,7 @@ test.describe( 'Document tests', async () => {
 
 async function addElement( wpAdmin: WpAdminPage, elementType: string ) {
 	const frame = wpAdmin.page.frame( { name: 'editor-canvas' } );
-	if ( ! await wpAdmin.page.frameLocator( 'iframe[name="editor-canvas"]' ).locator( 'p[role="document"]' ).isVisible() ) {
-		await frame.locator( '.block-editor-inserter__toggle' ).click();
-	} else {
-		await wpAdmin.page.frameLocator( 'iframe[name="editor-canvas"]' ).locator( 'p[role="document"]' ).click();
-		await wpAdmin.page.click( '.block-editor-inserter__toggle' );
-	}
-
+	await wpAdmin.page.getByRole( 'button', { name: 'Block Inserter' } ).click();
 	await wpAdmin.page.click( `[class*="editor-block-list-item-${ elementType }"]` );
 	await frame.click( '.editor-styles-wrapper' );
 }
