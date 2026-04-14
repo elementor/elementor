4ca0fdd4383fc386bcb8219ba082a5ecaa2de979
Ron Rostovsky <ronros@elementor.com>
2026-04-14 13:38:23 +0300

    fix tests

--- modules/global-classes/utils/global-classes-sanitization.php
+++ modules/global-classes/utils/global-classes-sanitization.php
@@ -13,6 +13,7 @@ class Global_Classes_Sanitization {
 		}
 
 		$item_ids = array_keys( $items );
+		$order = array_filter( $order, 'is_string' );
 		$order = array_unique( $order );
 		$order_existing = array_values( array_intersect( $order, $item_ids ) );
 		$missing = array_diff( $item_ids, $order_existing );
