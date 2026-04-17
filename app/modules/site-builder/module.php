f197c6e0326ed34dea8097b8528c284a6976aeff
Hein van Vlastuin <hein@obox.co.za>
2026-04-17 13:55:21 +0200

    Revert extraction to library.php

--- app/modules/site-builder/module.php
+++ app/modules/site-builder/module.php
@@ -111,6 +111,34 @@ class Module extends BaseModule {
 			return null;
 		}
 
-		return $app->get_connect_auth();
+		$access_token = $app->get( 'access_token' );
+		$client_id = $app->get( 'client_id' );
+		$home_url = trailingslashit( home_url() );
+		$site_key = $app->get_site_key();
+		$access_token_secret = $app->get( 'access_token_secret' );
+
+		$connect_data = [
+			'access-token' => $access_token,
+			'app' => 'library',
+			'client-id' => $client_id,
+			'home-url' => $home_url,
+			'site-key' => $site_key,
+		];
+
+		ksort( $connect_data );
+
+		$signature = hash_hmac(
+			'sha256',
+			wp_json_encode( $connect_data, JSON_NUMERIC_CHECK ),
+			$access_token_secret
+		);
+
+		return [
+			'signature' => $signature,
+			'accessToken' => $access_token,
+			'clientId' => $client_id,
+			'homeUrl' => $home_url,
+			'siteKey' => $site_key,
+		];
 	}
 }
