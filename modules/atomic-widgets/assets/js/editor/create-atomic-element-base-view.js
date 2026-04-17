2c04a821411b633e3bba7a309da2fbdb39833e06
Ronen Boxer <ronenb@elementor.com>
2026-04-17 15:17:36 +0300

    cheey pick

--- modules/atomic-widgets/assets/js/editor/create-atomic-element-base-view.js
+++ modules/atomic-widgets/assets/js/editor/create-atomic-element-base-view.js
@@ -29,15 +29,19 @@ export default function createAtomicElementBaseView( type ) {
 
 		_resolveTag() {
 			const renderContext = this.getResolverRenderContext?.();
+
 			const tagSetting = this.model.getSetting( 'tag' );
 			const resolvedTag = this._resolvePropValue( tagSetting, renderContext );
-			const tagValue = resolvedTag?.value ?? resolvedTag;
 
-			if ( this._hasLink( renderContext ) ) {
-				return 'a';
-			}
+			const linkSetting = this.model.getSetting( 'link' );
+			const resolvedLinkTag = this._resolvePropValue( linkSetting?.value?.tag, renderContext );
 
-			return tagValue || this.model.config.default_html_tag || 'div';
+			const linkTag = resolvedLinkTag?.value ?? resolvedLinkTag;
+			const baseTag = resolvedTag?.value ?? resolvedTag;
+
+			const resultTag = linkTag ?? baseTag;
+
+			return resultTag || this.model.config.default_html_tag || 'div';
 		},
 
 		getChildViewContainer() {
@@ -326,11 +330,15 @@ export default function createAtomicElementBaseView( type ) {
 			this.$el.removeAttr( 'href' );
 			this.$el.removeAttr( 'data-action-link' );
 
-			const link = this.getLink();
+			const link = this.getLinkAttributes();
 
-			if ( link ) {
-				this.$el.attr( link.attr, link.value );
+			if ( ! link ) {
+				return;
 			}
+
+			Object.entries( link ).forEach( ( [ key, value ] ) => {
+				this.$el.attr( key, value );
+			} );
 		},
 
 		async _waitForChildrenToComplete() {
@@ -402,7 +410,7 @@ export default function createAtomicElementBaseView( type ) {
 			return !! destination?.value;
 		},
 
-		getLink() {
+		getLinkAttributes() {
 			const renderContext = this.getResolverRenderContext?.();
 			const linkSetting = this.model.getSetting( 'link' );
 			const resolvedLink = this._resolvePropValue( linkSetting, renderContext );
@@ -426,9 +434,10 @@ export default function createAtomicElementBaseView( type ) {
 					return null;
 				}
 
+				const attributeKey = 'action' === value?.group ? 'data-action-link' : 'href';
+
 				return {
-					attr: 'action' === value.settings?.group ? 'data-action-link' : 'href',
-					value: resolvedValue,
+					[ attributeKey ]: resolvedValue,
 				};
 			}
 
@@ -436,8 +445,7 @@ export default function createAtomicElementBaseView( type ) {
 			const hrefPrefix = isPostId ? elementor.config.home_url + '/?p=' : '';
 
 			return {
-				attr: 'href',
-				value: hrefPrefix + value,
+				href: hrefPrefix + value,
 			};
 		},
 
