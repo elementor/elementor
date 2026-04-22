02cc2ee51a111d294e3bd5ca68e4d1e0585a4148
Hein van Vlastuin <hein@obox.co.za>
2026-04-22 21:43:55 +0200

    Revert test changes

--- tests/phpunit/elementor/modules/wp-rest/test-elementor-settings.php
+++ tests/phpunit/elementor/modules/wp-rest/test-elementor-settings.php
@@ -51,13 +51,13 @@ class Test_Elementor_Settings extends Elementor_Test_Base {
 		$this->assertEquals( 400, $response->get_status() );
 	}
 
-	public function test_update_setting_with_allowlisted_key() {
+	public function test_update_setting_with_valid_key() {
 		// Arrange
 		$this->act_as_admin();
 		do_action( 'rest_api_init' );
 
-		$request = new \WP_REST_Request( 'POST', '/elementor/v1/settings/elementor_active_kit' );
-		$request->set_param( 'value', 123 );
+		$request = new \WP_REST_Request( 'POST', '/elementor/v1/settings/elementor_test_key' );
+		$request->set_param( 'value', 'new_test_value' );
 
 		// Act
 		$response = rest_get_server()->dispatch( $request );
@@ -66,15 +66,15 @@ class Test_Elementor_Settings extends Elementor_Test_Base {
 		$this->assertEquals( 200, $response->get_status() );
 		$data = $response->get_data();
 		$this->assertTrue( $data['success'] );
-		$this->assertEquals( 123, get_option( 'elementor_active_kit' ) );
+		$this->assertEquals( 'new_test_value', get_option( 'elementor_test_key' ) );
 	}
 
 	public function test_update_setting_as_a_guest() {
 		// Arrange
 		do_action( 'rest_api_init' );
 
-		$request = new \WP_REST_Request( 'POST', '/elementor/v1/settings/elementor_active_kit' );
-		$request->set_param( 'value', 123 );
+		$request = new \WP_REST_Request( 'POST', '/elementor/v1/settings/elementor_test_key' );
+		$request->set_param( 'value', 'new_test_value' );
 
 		// Act
 		$response = rest_get_server()->dispatch( $request );
@@ -88,8 +88,8 @@ class Test_Elementor_Settings extends Elementor_Test_Base {
 		$this->act_as_subscriber();
 		do_action( 'rest_api_init' );
 
-		$request = new \WP_REST_Request( 'POST', '/elementor/v1/settings/elementor_active_kit' );
-		$request->set_param( 'value', 123 );
+		$request = new \WP_REST_Request( 'POST', '/elementor/v1/settings/elementor_test_key' );
+		$request->set_param( 'value', 'new_test_value' );
 
 		// Act
 		$response = rest_get_server()->dispatch( $request );
@@ -102,26 +102,9 @@ class Test_Elementor_Settings extends Elementor_Test_Base {
 		// Arrange
 		$this->act_as_admin();
 		do_action( 'rest_api_init' );
-		update_option( 'elementor_active_kit', 456 );
+		update_option( 'elementor_test_key', 'test_value' );
 
-		$request = new \WP_REST_Request( 'POST', '/elementor/v1/settings/elementor_active_kit' );
-		$request->set_param( 'value', 456 );
-
-		// Act
-		$response = rest_get_server()->dispatch( $request );
-
-		// Assert
-		$this->assertEquals( 200, $response->get_status() );
-		$data = $response->get_data();
-		$this->assertTrue( $data['success'] );
-	}
-
-	public function test_update_any_elementor_setting_succeeds() {
-		// Arrange
-		$this->act_as_admin();
-		do_action( 'rest_api_init' );
-
-		$request = new \WP_REST_Request( 'POST', '/elementor/v1/settings/elementor_other_setting' );
+		$request = new \WP_REST_Request( 'POST', '/elementor/v1/settings/elementor_test_key' );
 		$request->set_param( 'value', 'test_value' );
 
 		// Act
@@ -129,46 +112,7 @@ class Test_Elementor_Settings extends Elementor_Test_Base {
 
 		// Assert
 		$this->assertEquals( 200, $response->get_status() );
-		$this->assertEquals( 'test_value', get_option( 'elementor_other_setting' ) );
-	}
-
-	public function test_update_setting_stores_value_as_is() {
-		// Arrange
-		$this->act_as_admin();
-		do_action( 'rest_api_init' );
-
-		$request = new \WP_REST_Request( 'POST', '/elementor/v1/settings/elementor_active_kit' );
-		$request->set_param( 'value', '-123' );
-
-		// Act
-		$response = rest_get_server()->dispatch( $request );
-
-		// Assert
-		$this->assertEquals( 200, $response->get_status() );
-		$this->assertEquals( '-123', get_option( 'elementor_active_kit' ) );
-	}
-
-	public function test_update_theme_builder_conditions_with_array() {
-		// Arrange
-		$this->act_as_admin();
-		do_action( 'rest_api_init' );
-
-		$conditions = [
-			'header' => [ [ 'include' => 'general' ] ],
-			'footer' => [ [ 'include' => 'singular' ] ],
-		];
-
-		$request = new \WP_REST_Request( 'POST', '/elementor/v1/settings/elementor_pro_theme_builder_conditions' );
-		$request->set_param( 'value', $conditions );
-
-		// Act
-		$response = rest_get_server()->dispatch( $request );
-
-		// Assert
-		$this->assertEquals( 200, $response->get_status() );
 		$data = $response->get_data();
 		$this->assertTrue( $data['success'] );
-		$this->assertEquals( $conditions, get_option( 'elementor_pro_theme_builder_conditions' ) );
 	}
-
 }
