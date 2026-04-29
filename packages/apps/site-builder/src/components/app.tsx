496857e40a96adba9d86fc413a9afeb58200e3d5
Manor Hazaz <manorhazaz@gmail.com>
2026-04-29 16:30:29 +0300

    Tweak: Exit from iframe app [ED-23807]

--- packages/apps/site-builder/src/components/app.tsx
+++ packages/apps/site-builder/src/components/app.tsx
@@ -30,6 +30,7 @@ function sendReferrerInfo( iframe: HTMLIFrameElement, event: MessageEvent, targe
 			instanceId: event.data?.payload?.instanceId ?? '',
 			info: {
 				connectAuth: config?.connectAuth,
+				exitTo: config?.exitTo,
 				page: {
 					url: window.location.href,
 					elementorAiCurrentContext: getElementorAiCurrentContext(),
@@ -111,6 +112,11 @@ export function App() {
 
 			if ( type === 'site-planner/deploy-website' ) {
 				await handleDeploy( iframeRef.current, event );
+			}
+
+			if ( type === 'element-selector/close' ) {
+				window.top.location.href = getConfig()?.exitTo ?? '';
+				return;
 			}
 		},
 		[ allowedOrigin ]
