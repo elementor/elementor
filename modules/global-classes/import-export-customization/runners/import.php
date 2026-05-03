49faff92613d090dadfc4053872ed95f30e2ae78
Miryam Oren <77922014+MiryamOren@users.noreply.github.com>
2026-05-03 18:20:45 +0300

    extend import EP api

--- modules/global-classes/import-export-customization/runners/import.php
+++ modules/global-classes/import-export-customization/runners/import.php
@@ -3,12 +3,7 @@
 namespace Elementor\Modules\GlobalClasses\ImportExportCustomization\Runners;
 
 use Elementor\App\Modules\ImportExportCustomization\Runners\Import\Import_Runner_Base;
-use Elementor\App\Modules\ImportExportCustomization\Utils as ImportExportUtils;
-use Elementor\Modules\GlobalClasses\Global_Classes_Parser;
-use Elementor\Modules\GlobalClasses\Global_Classes_Repository;
-use Elementor\Modules\GlobalClasses\ImportExportCustomization\Import_Export_Customization;
 use Elementor\Modules\GlobalClasses\ImportExportUtils\Import_Utils;
-use Elementor\Plugin;
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
@@ -20,9 +15,11 @@ class Import extends Import_Runner_Base {
 	}
 
 	public function should_import( array $data ): bool {
+		$is_settings_import = in_array( 'settings', $data['include'] ?? [], true );
+		$is_design_system_import = in_array( 'design-system', $data['include'] ?? [], true );
+
 		return (
-			isset( $data['include'] ) &&
-			in_array( 'settings', $data['include'], true ) &&
+			( $is_settings_import || $is_design_system_import ) &&
 			! empty( $data['extracted_directory_path'] ) &&
 			$this->is_classes_enabled( $data )
 		);
@@ -37,15 +34,8 @@ class Import extends Import_Runner_Base {
 	}
 
 	public function import( array $data, array $imported_data ): array {
-		// $kit = Plugin::$instance->kits_manager->get_active_kit();
+		$conflict_resolution = $data['customization']['design-system']['conflict_resolution'] ?? 'skip';
 
-		// $file_name = Import_Export_Customization::FILE_NAME;
-		// $global_classes = ImportExportUtils::read_json_file( "{$data['extracted_directory_path']}/{$file_name}.json" );
-
-		// if ( ! $kit || ! $global_classes ) {
-		// 	return [];
-		// }
-
-		return Import_Utils::import_classes( $data['extracted_directory_path'] . '/global-classes', [ 'conflict_resolution' => 'skip' ] );
+		return Import_Utils::import_classes( $data['extracted_directory_path'] . '/global-classes', [ 'conflict_resolution' => $conflict_resolution ] );
 	}
 }
