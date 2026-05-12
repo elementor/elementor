2892fb5da34875cb5bf8a308c92cb48666c033bc
Hein van Vlastuin <hein@obox.co.za>
2026-05-12 14:23:59 +0200

    wip

--- tests/jest/unit/app/event-track/screen-view.test.js
+++ tests/jest/unit/app/event-track/screen-view.test.js
@@ -13,11 +13,11 @@ describe( 'ScreenViewTracking', () => {
 
 	test( 'init should track initial screen view if it is an Elementor page', () => {
 		DashboardUtils.isElementorPage.mockReturnValue( true );
-		
+
 		// Mock getScreenData
-		const screenData = { screenId: 'elementor-settings', screenType: 'top_level_page' };
+		const screenData = { screenId: 'elementor-settings', screenType: 'top_level_page', };
 		jest.spyOn( ScreenViewTracking, 'getScreenData' ).mockReturnValue( screenData );
-		
+
 		ScreenViewTracking.init();
 
 		expect( WpDashboardTracking.trackScreenViewed ).toHaveBeenCalledWith(
@@ -28,7 +28,7 @@ describe( 'ScreenViewTracking', () => {
 
 	test( 'init should not track initial screen view if it is not an Elementor page', () => {
 		DashboardUtils.isElementorPage.mockReturnValue( false );
-		
+
 		ScreenViewTracking.init();
 
 		expect( WpDashboardTracking.trackScreenViewed ).not.toHaveBeenCalled();
