b3534b0cdd5775aa2dcfc36441f3111bd7054b7c
Bartosz Rekawek <bartoszr@elementor.red>
2026-05-15 12:22:33 +0200

    Fix no-unused-vars in checklist.ts

--- tests/playwright/types/checklist.ts
+++ tests/playwright/types/checklist.ts
@@ -1,6 +1,9 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const stepIds = [ 'add_logo', 'set_fonts_and_colors', 'create_pages', 'setup_header', 'assign_homepage' ] as const;
-export type StepId = typeof stepIds[ number ];
+export type StepId =
+	| 'add_logo'
+	| 'set_fonts_and_colors'
+	| 'create_pages'
+	| 'setup_header'
+	| 'assign_homepage';
 
 export const immutableStepIds: StepId[] = [ 'create_pages' ] as const;
 
