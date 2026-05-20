db4fe082031d347d6d9b912125dd1e9c184af5e8
Nami printz <namipr3@gmail.com>
2026-05-20 10:42:41 +0300

    fix

--- packages/packages/core/editor-design-system/src/components/design-system-panel-content.tsx
+++ packages/packages/core/editor-design-system/src/components/design-system-panel-content.tsx
@@ -185,6 +185,7 @@ export function DesignSystemPanelContent( { onRequestClose }: DesignSystemPanelC
 								onExposeCloseAttempt={ ( fn ) => {
 									classesCloseAttemptRef.current = fn;
 								} }
+								isActive={ currentTab === 'classes' }
 							/>
 						</Box>
 					</Stack>
