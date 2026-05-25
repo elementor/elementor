229539dcee366041b155a00710f5fa083a150491
Mike Kokhanov <mikek@elementor.com>
2026-05-25 15:55:41 +0300

    [ wip ]: assets management improvements

--- modules/assets-manager/assets/js/utils.js
+++ modules/assets-manager/assets/js/utils.js
@@ -3,14 +3,29 @@
 const cssMap = new Map();
 const jsMap = new Map();
 
+function jsElementId( handle ) {
+	return `${ handle }-js`;
+}
+
+function cssElementId( handle ) {
+	return `${ handle }-css`;
+}
+
 export function appendCss( handle, uri, options = {} ) {
 	if ( cssMap.has( handle ) ) {
 		return cssMap.get( handle );
 	}
 
+	if ( document.getElementById( cssElementId( handle ) ) ) {
+		const resolved = Promise.resolve();
+		cssMap.set( handle, resolved );
+		return resolved;
+	}
+
 	const promise = new Promise( ( resolve, reject ) => {
 		const link = document.createElement( 'link' );
 
+		link.id = cssElementId( handle );
 		link.rel = 'stylesheet';
 		link.href = uri;
 		link.media = options.media ?? 'all';
@@ -31,9 +46,16 @@ export function appendJs( handle, uri, options = {} ) {
 		return jsMap.get( handle );
 	}
 
+	if ( document.getElementById( jsElementId( handle ) ) ) {
+		const resolved = Promise.resolve();
+		jsMap.set( handle, resolved );
+		return resolved;
+	}
+
 	const promise = new Promise( ( resolve, reject ) => {
 		const script = document.createElement( 'script' );
 
+		script.id = jsElementId( handle );
 		script.src = uri;
 		script.type = 'text/javascript';
 		script.async = options.async ?? true;
