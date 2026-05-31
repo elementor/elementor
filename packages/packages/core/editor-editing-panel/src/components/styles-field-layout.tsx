18e01b67ed59c9eb7e9cbfca25765ccc3cd1a156
Omer Haim Israeli <omeri@elementor.com>
2026-05-31 10:59:05 +0300

    wip

--- packages/packages/core/editor-editing-panel/src/components/styles-field-layout.tsx
+++ packages/packages/core/editor-editing-panel/src/components/styles-field-layout.tsx
@@ -24,41 +24,30 @@ type LayoutProps = {
 	infoTooltip?: string;
 };
 
-const Row = React.forwardRef< HTMLDivElement, LayoutProps >(
-	( { label, children, infoTooltip }, ref ) => {
-		return (
-			<Grid
-				container
-				gap={ 2 }
-				alignItems="center"
-				flexWrap="nowrap"
-				ref={ ref }
-				aria-label={ `${ label } control` }
-			>
-				<Grid item xs={ 6 }>
-					<ControlLabel infoTooltip={ infoTooltip }>{ label }</ControlLabel>
-				</Grid>
-				<Grid
-					item
-					xs={ 6 }
-					sx={ ( theme: Theme ) => ( {
-						width: `calc(50% - ${ theme.spacing( 2 ) })`,
-					} ) }
-				>
-					{ children }
-				</Grid>
-			</Grid>
-		);
-	}
-);
-
-const Column = React.forwardRef< HTMLDivElement, LayoutProps >(
-	( { label, children, infoTooltip }, ref ) => {
-		return (
-			<Stack gap={ 0.75 } ref={ ref }>
+const Row = React.forwardRef< HTMLDivElement, LayoutProps >( ( { label, children, infoTooltip }, ref ) => {
+	return (
+		<Grid container gap={ 2 } alignItems="center" flexWrap="nowrap" ref={ ref } aria-label={ `${ label } control` }>
+			<Grid item xs={ 6 }>
 				<ControlLabel infoTooltip={ infoTooltip }>{ label }</ControlLabel>
+			</Grid>
+			<Grid
+				item
+				xs={ 6 }
+				sx={ ( theme: Theme ) => ( {
+					width: `calc(50% - ${ theme.spacing( 2 ) })`,
+				} ) }
+			>
 				{ children }
-			</Stack>
-		);
-	}
-);
+			</Grid>
+		</Grid>
+	);
+} );
+
+const Column = React.forwardRef< HTMLDivElement, LayoutProps >( ( { label, children, infoTooltip }, ref ) => {
+	return (
+		<Stack gap={ 0.75 } ref={ ref }>
+			<ControlLabel infoTooltip={ infoTooltip }>{ label }</ControlLabel>
+			{ children }
+		</Stack>
+	);
+} );
