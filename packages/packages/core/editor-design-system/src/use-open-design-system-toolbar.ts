1a1ddda4fdd9b2d074dff562a37b93dd7519057a
Ron Rostovsky <ronros@elementor.com>
2026-06-03 16:14:19 +0300

    cr

--- packages/packages/core/editor-design-system/src/use-open-design-system-toolbar.ts
+++ packages/packages/core/editor-design-system/src/use-open-design-system-toolbar.ts
@@ -1,4 +1,3 @@
-import { useCallback } from 'react';
 import { type ToggleActionProps } from '@elementor/editor-app-bar';
 import { DropletHalfFilledIcon } from '@elementor/icons';
 import { __ } from '@wordpress/i18n';
@@ -12,24 +11,22 @@ import { getActiveDesignSystemTab } from './initial-tab';
 export function useOpenDesignSystemToolbar(): ToggleActionProps {
 	const { isOpen } = usePanelStatus();
 
-	const onClick = useCallback( () => {
-		if ( ! isOpen ) {
-			trackDesignSystem( { event: 'opened' } );
-		}
-
-		const tab = getActiveDesignSystemTab() ?? 'variables';
-
-		window.dispatchEvent(
-			new CustomEvent( EVENT_TOGGLE, {
-				detail: { tab },
-			} )
-		);
-	}, [ isOpen ] );
-
 	return {
 		title: __( 'Design System', 'elementor' ),
 		icon: DropletHalfFilledIcon,
-		onClick,
+		onClick: () => {
+			if ( ! isOpen ) {
+				trackDesignSystem( { event: 'opened' } );
+			}
+
+			const tab = getActiveDesignSystemTab() ?? 'variables';
+
+			window.dispatchEvent(
+				new CustomEvent( EVENT_TOGGLE, {
+					detail: { tab },
+				} )
+			);
+		},
 		selected: isOpen,
 	};
 }
