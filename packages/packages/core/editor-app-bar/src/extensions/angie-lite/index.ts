61fb0b6a758dfb180b084e34b29e0326342f5017
Miryam Oren <77922014+MiryamOren@users.noreply.github.com>
2026-06-08 18:11:55 +0300

    toggle button in app bar - temp

---
+++ packages/packages/core/editor-app-bar/src/extensions/angie-lite/index.ts
@@ -0,0 +1,46 @@
+import { getAngieSdk, isAngieAvailable } from '@elementor/editor-mcp';
+import { toolsMenu } from '../../locations';
+import { useEffect } from 'react';
+import { __ } from '@wordpress/i18n';
+import { AIIcon } from '@elementor/icons';
+
+export function init() {
+	toolsMenu.registerToggleAction( {
+		id: 'toggle-angie-lite',
+		priority: 4,
+		useProps: useActionProps,
+	} );
+}
+
+function useActionProps() {
+    console.log( 'angie-lite - useActionProps' );
+	const hasAngieInstalled = isAngieAvailable();
+
+    const id = 'angie-lite-sidebar-toggle';
+    useEffect( () => {
+        const sdk = getAngieSdk();
+
+        sdk.loadSidebarV2( {
+            host: { appId: 'elementor' },
+            container: {
+                layout: 'sidebar',
+                styleTheme: 'wordpress',
+                chatToggleButton: {
+                    enabled: true,
+                    selector: `#${id}`,
+                },
+            },
+            widgetConfig: {
+                promptLibrary: { enabled: false },
+                fileUpload: { enabled: false },
+                modeSwitcher: { enabled: false, default: 'ask' },
+            }
+        } );
+    }, [] );
+
+	return {
+		title: __( 'Angie lite', 'elementor' ),
+		icon: AIIcon,
+        id,
+	};
+}
