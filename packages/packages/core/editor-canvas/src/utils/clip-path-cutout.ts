a7fde446ef09a62e75f6ddfa50817315edfd1b01
Nikita Murzin <mykytamu@elementor.com>
2026-06-10 13:55:07 +0200

    Internal: lint fix [ED-22577]

--- packages/packages/core/editor-canvas/src/utils/clip-path-cutout.ts
+++ packages/packages/core/editor-canvas/src/utils/clip-path-cutout.ts
@@ -5,7 +5,7 @@ export function rectCutoutClipPath( outer: Size, inner: CutoutRect ): string {
 	const { width: ow, height: oh } = outer;
 	const { x, y, width: iw, height: ih } = inner;
 
-	return `path(evenodd, 'M 0 0 L ${ ow } 0 L ${ ow } ${ oh } L 0 ${ oh } Z M ${ x } ${ y } L ${
+	return `path(evenodd, 'M 0 0 L ${ ow } 0 L ${ ow } ${ oh } L 0 ${ oh } Z M ${ x } ${ y } L ${ x + iw } ${ y } L ${
 		x + iw
-	} ${ y } L ${ x + iw } ${ y + ih } L ${ x } ${ y + ih } L ${ x } ${ y } Z')`;
+	} ${ y + ih } L ${ x } ${ y + ih } L ${ x } ${ y } Z')`;
 }
