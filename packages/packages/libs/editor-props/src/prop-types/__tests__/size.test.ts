27afd498bbfa6301ba7c2b34493e88ae0743cd47
IshayMaya <ishaym@elementor.com>
2026-07-01 18:40:10 +0300

    expend tests

--- packages/packages/libs/editor-props/src/prop-types/__tests__/size.test.ts
+++ packages/packages/libs/editor-props/src/prop-types/__tests__/size.test.ts
@@ -1,10 +1,29 @@
-import { sizePropTypeUtil } from '../size';
+import { sizePropTypeUtil, type SizePropValue } from '../size';
+
+const sizeUnitCases: SizePropValue[ 'value' ][] = [
+	{ unit: 'px', size: 10 },
+	{ unit: 'em', size: 1.5 },
+	{ unit: 'rem', size: 2 },
+	{ unit: '%', size: 50 },
+	{ unit: 'vw', size: 100 },
+	{ unit: 'vh', size: 50 },
+	{ unit: 'ch', size: 40 },
+	{ unit: 'fr', size: 1 },
+	{ unit: 'deg', size: 90 },
+	{ unit: 'rad', size: 1 },
+	{ unit: 'grad', size: 100 },
+	{ unit: 'turn', size: 0.5 },
+	{ unit: 's', size: 1 },
+	{ unit: 'ms', size: 300 },
+	{ unit: 'auto', size: '' },
+	{ unit: 'custom', size: 'calc(100% - 10px)' },
+];
 
 describe( 'sizePropTypeUtil', () => {
-	it( 'should accept fr unit values used by grid auto track styles', () => {
-		const value = sizePropTypeUtil.create( { size: 1, unit: 'fr' } );
+	it.each( sizeUnitCases )( 'should accept $unit unit values', ( sizeValue ) => {
+		const value = sizePropTypeUtil.create( sizeValue );
 
 		expect( sizePropTypeUtil.isValid( value ) ).toBe( true );
-		expect( sizePropTypeUtil.extract( value ) ).toEqual( { size: 1, unit: 'fr' } );
+		expect( sizePropTypeUtil.extract( value ) ).toEqual( sizeValue );
 	} );
 } );
