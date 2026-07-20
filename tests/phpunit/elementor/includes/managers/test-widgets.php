753559a65cd67868622dbb7f62a0be3a47f73028
max-zu <zubovmaksim@gmail.com>
2026-07-20 07:56:13 +0200

    ED-24947: Tweak: Remove WordPress section from widget panel

--- tests/phpunit/elementor/includes/managers/test-widgets.php
+++ tests/phpunit/elementor/includes/managers/test-widgets.php
@@ -3,6 +3,8 @@
 namespace Elementor\Testing\Includes\Managers;
 
 use Elementor\Controls_Manager;
+use Elementor\Core\Experiments\Manager as Experiments_Manager;
+use Elementor\Elements_Manager;
 use Elementor\Plugin;
 use ElementorEditorTesting\Elementor_Test_Base;
 
@@ -100,6 +102,38 @@ class Test_Widgets extends Elementor_Test_Base {
 		// Assert.
 		$this->assertArrayHasKey( 'basic', $response['categories'] );
 		$this->assertArrayHasKey( 'title', $response['categories']['basic'] );
+	}
+
+	public function test_ajax_refresh_widgets_config__excludes_wordpress_category_when_experiment_active() {
+		// Arrange.
+		wp_set_current_user( $this->factory()->create_and_get_administrator_user()->ID );
+
+		$post_id = $this->factory()->create_and_get_default_post()->ID;
+
+		Plugin::$instance->experiments->set_feature_default_state(
+			Elements_Manager::EXPERIMENT_HIDE_WORDPRESS_WIDGETS,
+			Experiments_Manager::STATE_ACTIVE
+		);
+
+		$elements_manager = Plugin::$instance->elements_manager;
+		$prop = new \ReflectionProperty( $elements_manager, 'categories' );
+		$prop->setAccessible( true );
+		$prop->setValue( $elements_manager, null );
+
+		// Act.
+		$response = Plugin::$instance->widgets_manager->ajax_refresh_widgets_config( [
+			'editor_post_id' => $post_id,
+		] );
+
+		// Cleanup.
+		Plugin::$instance->experiments->set_feature_default_state(
+			Elements_Manager::EXPERIMENT_HIDE_WORDPRESS_WIDGETS,
+			Experiments_Manager::STATE_ACTIVE
+		);
+		$prop->setValue( $elements_manager, null );
+
+		// Assert.
+		$this->assertArrayNotHasKey( Elements_Manager::CATEGORY_WORDPRESS, $response['categories'] );
 	}
 
 	public function test_ajax_get_widgets_default_value_translations__doesnt_return_empty_controls() {
