259454eb65e2585d4931e64131f7393cb2935281
Avichay Eyal <avichaye@elementor.red>
2026-08-12 16:40:54 +0300

    changes per code review

--- modules/atomic-widgets/prop-types/utils/plain-llm-schema-converter.php
+++ modules/atomic-widgets/prop-types/utils/plain-llm-schema-converter.php
@@ -34,6 +34,11 @@ class Plain_Llm_Schema_Converter {
 
 		if ( isset( $schema['anyOf'] ) && is_array( $schema['anyOf'] ) ) {
 			$schema['anyOf'] = $this->walk_any_of( $schema['anyOf'] );
+			if ( count( $schema['anyOf'] ) === 1) {
+				$shape = $schema['anyOf'][0];
+				unset( $schema['anyOf'] );
+				$schema = array_merge( $schema, $shape );
+			}
 		}
 
 		if ( isset( $schema['properties'] ) && is_array( $schema['properties'] ) ) {
