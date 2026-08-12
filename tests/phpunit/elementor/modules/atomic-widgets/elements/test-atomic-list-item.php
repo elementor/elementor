90a4e2fb1f87d7dbeffb5ae4f282708395ccb6a8
Mike Kokhanov <mikek@elementor.com>
2026-08-12 22:35:31 +0300

    wip

--- tests/phpunit/elementor/modules/atomic-widgets/elements/test-atomic-list-item.php
+++ tests/phpunit/elementor/modules/atomic-widgets/elements/test-atomic-list-item.php
@@ -3,11 +3,15 @@
 use Elementor\Modules\AtomicWidgets\Elements\Atomic_List\Atomic_List_Item\Atomic_List_Item;
 use ElementorEditorTesting\Elementor_Test_Base;
 
+/**
+ * @group atomic-widgets-e-list
+ */
 class Test_Atomic_List_Item extends Elementor_Test_Base {
 	public function test_get_data_for_save__preserves_sanitized_editor_settings_label(): void {
 		// Arrange.
 		$instance = new Atomic_List_Item( [
 			'id' => 'list-item-1',
+			'elType' => 'e-list-item',
 			'editor_settings' => [
 				'title' => '<b>Primary item</b>',
 			],
