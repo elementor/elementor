05c01128ca7fd348b8a40e637b22e850e90781f9
Mike Kokhanov <mikek@elementor.com>
2026-08-12 22:23:44 +0300

    wip

--- packages/packages/core/editor-editing-panel/src/controls-registry/element-controls/registry.ts
+++ packages/packages/core/editor-editing-panel/src/controls-registry/element-controls/registry.ts
@@ -5,14 +5,14 @@ import { ListItemsControl } from './list-items-control/list-items-control';
 import { TabsControl } from './tabs-control/tabs-control';
 
 const controlTypes = {
-  'list-items': { component: ListItemsControl as ControlComponent, layout: 'full' },
-  tabs: { component: TabsControl as ControlComponent, layout: 'full' },
+	'list-items': { component: ListItemsControl as ControlComponent, layout: 'full' },
+	tabs: { component: TabsControl as ControlComponent, layout: 'full' },
 } as const satisfies ControlRegistry;
 
 export const registerElementControls = () => {
-  Object.entries< ( typeof controlTypes )[ keyof typeof controlTypes ] >( controlTypes ).forEach(
-    ( [ type, { component, layout } ] ) => {
-      controlsRegistry.register( type, component, layout );
-    }
-  );
+	Object.entries< ( typeof controlTypes )[ keyof typeof controlTypes ] >( controlTypes ).forEach(
+		( [ type, { component, layout } ] ) => {
+			controlsRegistry.register( type, component, layout );
+		}
+	);
 };
