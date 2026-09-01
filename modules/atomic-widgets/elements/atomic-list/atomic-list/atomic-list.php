73710c1cdf0ce2c85e1e59966f1ccb507aacfc17
Mike Kokhanov <mikek@elementor.com>
2026-09-01 13:58:51 +0300

    [ wip ]: e-list tweaks

--- modules/atomic-widgets/elements/atomic-list/atomic-list/atomic-list.php
+++ modules/atomic-widgets/elements/atomic-list/atomic-list/atomic-list.php
@@ -33,11 +33,11 @@ class Atomic_List extends Atomic_Element_Base {
 
 	public function __construct( $data = [], $args = null ) {
 		parent::__construct( $data, $args );
-		// $this->meta( 'is_container', true );
+		$this->meta( 'is_container', true );
 	}
 
 	public static function get_type() {
-		return 'e-list';
+		return 'e-atomic-list';
 	}
 
 	public static function get_element_type(): string {
