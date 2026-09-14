0d32b81bf1e59244f2431b299ca97680fd707ded
Davids Seveloff <94905340+davseve@users.noreply.github.com>
2026-09-14 16:15:30 +0300

    Clean up comments in define_allowed_child_types method
    Removed comments explaining the empty allowed child types.

--- modules/atomic-widgets/elements/atomic-form/form-message/form-message.php
+++ modules/atomic-widgets/elements/atomic-form/form-message/form-message.php
@@ -57,9 +57,6 @@ abstract class Form_Message extends Atomic_Element_Base {
 	}
 
 	protected function define_allowed_child_types() {
-		// Empty: same as accordion item content. Panel atoms are created as `elType: widget`
-		// (widgetType e-paragraph / e-heading). A whitelist of those type strings rejects
-		// `widget`, and addElement then calls children.last() which is undefined when empty.
 		return [];
 	}
 
